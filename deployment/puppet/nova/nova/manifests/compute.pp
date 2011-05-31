179b92b606e00d22a0156557644941f5746d934f
Chris Blumentritt <cblument@gmail.com>
2011-05-31 10:40:53 -0500

    adding parameters to nova and nova::compute class

--- deployment/puppet/nova/nova/manifests/compute.pp
+++ deployment/puppet/nova/nova/manifests/compute.pp
@@ -1,6 +1,13 @@
 class nova::compute( 
   $enabled = false,
-  $nova_config = '/etc/nova/nova.conf' 
+  $nova_config = '/etc/nova/nova.conf',
+  $host,
+  $connection_type,
+  # There will need to be a map of host compute vm to host instance
+  $xenapi_connection_url,
+  $xenapi_connection_username,
+  $xenapi_connection_password,
+  $xenapi_inject_image
 ) {
   
   Nova_config<| |>~>Service['nova_compute']
--- deployment/puppet/nova/nova/manifests/init.pp
+++ deployment/puppet/nova/nova/manifests/init.pp
@@ -1,9 +1,33 @@
 class nova(
   $verbose = false,
   $nodaemon = false,
-  $logdir = '',
-  $sql_connection 
-  # just for network?
+  $logdir = '/var/log/nova',
+  $sql_connection,
+  $network_manager,
+  $image_service,
+  # is flat_network_bridge valid if network_manager is not FlatManager?
+  $flat_network_bridge,
+  $glance_host,
+  $glance_port, # default is 9292
+  $allow_admin_api,
+  $rabbit_host,
+  $rabbit_password,
+  $rabbit_port,
+  $rabbit_userid,
+  $rabbit_virtual_host,
+  # Following may need to be broken out to different nova services
+  $state_path,
+  $lock_path,
+  $service_down_time,
+  $quota_instances,
+  $quota_cores,
+  $quota_volumes,
+  $quota_gigabytes,
+  $quota_floating_ips,
+  $quota_metadata_items,
+  $quota_max_injected_files,
+  $quota_max_injected_file_content_bytes,
+  $quota_max_injected_file_path_bytes
 ) {
 
   class { 'puppet': }
