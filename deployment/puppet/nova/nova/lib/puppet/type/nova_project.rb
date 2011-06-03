0fe7665562d38353c2cbc8630a4e83e780538135
Adam Gandelman <adamg@canonical.com>
2011-06-03 10:59:46 -0700

    whitespace cleanup in nova_project type

--- deployment/puppet/nova/nova/lib/puppet/type/nova_project.rb
+++ deployment/puppet/nova/nova/lib/puppet/type/nova_project.rb
@@ -1,13 +1,14 @@
 Puppet::Type.newtype(:nova_project) do
 
-    @doc = "Manage creation/deletion of nova projects."
+  @doc = "Manage creation/deletion of nova projects."
        
-    ensurable
+  ensurable
 
-    newparam(:name) do
-        desc "The name of the project."
-    end
-    newparam(:owner) do
-        desc "Owner of this project."
-    end
+  newparam(:name) do
+    desc "The name of the project."
+  end
+
+  newparam(:owner) do
+    desc "Owner of this project."
+  end
 end
