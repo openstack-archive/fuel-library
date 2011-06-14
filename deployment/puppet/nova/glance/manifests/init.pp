8d40a64f3f7c142e08f807a69edb8869f1a23d1a
Dan Bode <dan@puppetlabs.com>
2011-06-14 15:34:11 -0700

    added glance code
    even though it should be in its own repo.

--- deployment/puppet/nova/glance/manifests/init.pp
+++ deployment/puppet/nova/glance/manifests/init.pp
@@ -1,16 +1,10 @@
-class glance (
-  $verbose = 'false',
-  $default_store = 'file',
-  $bind_port = '9292',
-  $registry_port = '9191',
-  $auth_domain = '127.0.0.1',
-  $swift_store_user,
-  $swift_store_key
-  $db_user,
-  $db_password,
-  $glance_ip
-) {
-  file { '/etc/glance/glance.conf':
-    content => template('glance/glance.conf.erb'),
+class glance {
+  file { '/etc/glance/':
+    ensure  => directory,
+    owner   => 'glance',
+    group   => 'root',
+    mode    => 770,
+    require => Package['glance']
   }
+  package { 'glance': ensure => present }
 }
