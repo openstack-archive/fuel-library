32cfeb68b23aeb546717b7ef0caf013518f4dcaa
Dan Bode <dan@puppetlabs.com>
2011-06-16 09:04:55 -0700

    re-implemented nova::rabbitmq
    - it now uses the separate rabbitmq module

--- deployment/puppet/nova/nova/manifests/rabbitmq.pp
+++ deployment/puppet/nova/nova/manifests/rabbitmq.pp
@@ -1,11 +1,36 @@
-class nova::rabbitmq {
-  package { 'rabbitmq-server':
-    ensure => installed,
+#
+# class for installing rabbitmq server for nova
+#
+#
+class nova::rabbitmq(
+  $userid='guest',
+  $password='guest',
+  $port='5672',
+  $virtual_host='/',
+  $install_repo = false
+) {
+  if $install_repo {
+    # this is debian specific
+    class { 'rabbitmq::repo::apt':
+      pin    => 900,
+      before => Class['rabbitmq::server']
+    }
   }
-  service { 'rabbitmq-server':
-    ensure => running,
-    enable => true,
-    hasstatus => true,
-    require => Package["rabbitmq-server"],
+  if $userid == 'guest' {
+    $delete_guest_user = false
+  } else {
+    $delete_guest_user = true
+    rabbitmq_user{ $userid:
+      admin     => true,
+      password  => $password,
+      require   => Class['rabbitmq::server'],
+    }
+  }
+  class { 'rabbitmq::server':
+    port              => $port,
+    delete_guest_user => $delete_guest_user,
+  }
+  rabbitmq_vhost{ $virtual_host:
+    require => Class['rabbitmq::server'],
   }
 }
