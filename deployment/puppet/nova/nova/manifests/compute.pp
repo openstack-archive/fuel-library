4cd866800949f55db0c2a535bc2f82aeb492d0fe
Dan Bode <dan@puppetlabs.com>
2011-06-16 17:53:27 -0700

    converted aws_address to a class param.
    just in case it needs to be configured.

--- deployment/puppet/nova/nova/manifests/compute.pp
+++ deployment/puppet/nova/nova/manifests/compute.pp
@@ -3,7 +3,8 @@
 class nova::compute(
   $api_server,
   $enabled = false,
-  $api_port = 8773
+  $api_port = 8773,
+  $aws_address = '169.254.169.254'
 ) {
 
   Nova_config<| |>~>Service['nova-compute']
@@ -27,8 +28,8 @@ class nova::compute(
 
   # forward guest metadata requests to correct API server
   exec { "forward_api_requests":
-    command => "/sbin/iptables -t nat -A PREROUTING -d 169.254.169.254/32 -p tcp -m tcp --dport 80 -j DNAT --to-destination ${api_server}:${api_port}",
-    unless => "/sbin/iptables -L PREROUTING -t nat -n | egrep 'DNAT[ ]+tcp+[ ]+--[ ]+0.0.0.0\\/0+[ ]+169.254.169.254+[ ]+tcp+[ ]+dpt:80+[ ]+to:${api_server}:${api_port}'"
+    command => "/sbin/iptables -t nat -A PREROUTING -d ${aws_address}/32 -p tcp -m tcp --dport 80 -j DNAT --to-destination ${api_server}:${api_port}",
+    unless => "/sbin/iptables -L PREROUTING -t nat -n | egrep 'DNAT[ ]+tcp+[ ]+--[ ]+0.0.0.0\\/0+[ ]+${aws_address}+[ ]+tcp+[ ]+dpt:80+[ ]+to:${api_server}:${api_port}'"
   }
 
 }
