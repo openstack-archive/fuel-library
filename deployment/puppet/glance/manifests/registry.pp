696bbd760f763cd3abed6e562dbe518f4b642c85
Dan Bode <dan@puppetlabs.com>
2011-06-16 20:51:02 -0700

    Added missing comma
    resolved parse error.

--- deployment/puppet/glance/manifests/registry.pp
+++ deployment/puppet/glance/manifests/registry.pp
@@ -4,7 +4,7 @@ class glance::registry(
   $bind_host = '0.0.0.0',
   $bind_port = '9191',
   $log_file = '/var/log/glance/registry.log',
-  $sql_connection = 'sqlite:///var/lib/glance/glance.sqlite'
+  $sql_connection = 'sqlite:///var/lib/glance/glance.sqlite',
   $sql_idle_timeout = '3600'
 ) inherits glance {
   file { "/etc/glance/glance-registry.conf":
