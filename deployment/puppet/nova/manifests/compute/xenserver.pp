8b00998bb0302ddaf177f3de9961d2684047f159
Dan Bode <dan@puppetlabs.com>
2011-06-24 13:34:56 -0700

    refactor of xenserver.
    - removed config that was moved to nova
    - removed nova::compute

--- deployment/puppet/nova/manifests/compute/xenserver.pp
+++ deployment/puppet/nova/manifests/compute/xenserver.pp
@@ -1,26 +1,18 @@
 class nova::compute::xenserver(
-  # TODO - what does this host do?
-  $host,
-  $api_server,
   $xenapi_connection_url,
   $xenapi_connection_username,
   $xenapi_connection_password,
   $xenapi_inject_image=false
 ) {
 
-  class { 'nova::compute':
-    api_server => $api_server,
-    enabled    => $enabled,
-  }
   nova_config {
     'connection_type': value => 'xenapi';
     'xenapi_connection_url': value => $xenapi_connection_url;
     'xenapi_connection_username': value => $xenapi_connection_username;
     'xenapi_connection_password': value => $xenapi_connection_password;
     'xenapi_inject_image': value => $xenapi_inject_image;
-    'network_manager': value => $network_manager;
-    'flat_network_bridge': value => $flat_network_bridge;
   }
+
   package { 'xenapi':
     ensure   => installed,
     provider => pip
