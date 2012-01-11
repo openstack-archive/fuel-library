3bd4309878f0d448c816bd74e0b0ed630a0abe2a
Dan Bode <dan@puppetlabs.com>
2012-01-11 11:42:04 -0800

    Add rdoc to glance::api

--- deployment/puppet/glance/manifests/api.pp
+++ deployment/puppet/glance/manifests/api.pp
@@ -1,3 +1,47 @@
+# = Class: glance::api
+#
+# This class installs and configures the glance api server.
+#
+# == Paremeters:
+#
+#  $log_verbose - rather to log the glance api service at verbose level.
+#  Optional. Default: false
+#
+#  $log_debug - rather to log the glance api service at debug level.
+#  Optional. Default: false
+#
+#  $default_store - Backend used to store glance dist images.
+#  Optional. Default: file
+#
+#  $bind_host - The address of the host to bind to.
+#  Optional. Default: 0.0.0.0
+#
+#  $bind_port - The port the server should bind to.
+#  Optional. Default: 9292
+#
+#  $registry_host - The address used to connecto to the registy service.
+#  Optional. Default:
+#
+#  $registry_port - The port of the Glance registry service.
+#  Optional. Default: 9191
+#
+#  $log_file - The path of file used for logging
+#  Optional. Default: /var/log/glance/api.log
+#
+#  $filesystem_store_datadir - Location where dist images are stored when
+#  default_store == file.
+#  Optional. Default: /var/lib/glance/images/
+#
+#  $swift_store_auth_address - Optional. Default: '127.0.0.1:8080/v1.0/',
+#
+#  $swift_store_user - Optional. Default:'jdoe',
+#
+#  $swift_store_key - Optional. Default: 'a86850deb2742ec3cb41518e26aa2d89',
+#
+#  $swift_store_container - 'glance',
+#
+#  $swift_store_create_container_on_put - 'False'
+#
 class glance::api(
   $log_verbose = false,
   $log_debug = false,
