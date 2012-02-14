37920760420a0f7f9c0966eb390fe970c3b5c85a
Dan Bode <dan@puppetlabs.com>
2012-02-14 14:14:23 -0800

    remove puppetmaster node declaration
    Removes the puppetmaster node declaration so that
    this manifest can be imported from the openstack's
    manifests/site.pp (so that the openstack project
    and the swift project can use the same node
    declarations when swift is added as a submodule to
    the openstack project)

--- deployment/puppet/swift/examples/multi.pp
+++ deployment/puppet/swift/examples/multi.pp
@@ -50,32 +50,9 @@ node 'swift_proxy' {
 
 }
 
-class role_puppetmaster {
 node 'swift_ringbuilding' {
 
-  class { 'mysql::server':
-    config_hash => {'bind_address' => '127.0.0.1'}
-  }
-  class { 'mysql::ruby': }
-  package { 'activerecord':
-    ensure   => '2.3.5',
-    provider => 'gem',
-  }
 
-  class { 'puppet::master':
-    modulepath              => '/vagrant/modules',
-    manifest                => '/vagrant/manifests/site.pp',
-    storeconfigs            => true,
-    storeconfigs_dbuser     => 'dan',
-    storeconfigs_dbpassword => 'foo',
-    storeconfigs_dbadapter  => 'mysql',
-    storeconfigs_dbserver   => 'localhost',
-    storeconfigs_dbsocket   => '/var/run/mysqld/mysqld.sock',
-    version                 => installed,
-    puppet_master_package   => 'puppet',
-    autosign                => 'true',
-    certname                => $clientcert,
-  }
 }
 
 class role_swift {
