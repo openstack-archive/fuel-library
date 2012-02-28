fa2aa25f8a0f46f4accc7f01c429e7451a52380c
Dan Bode <dan@puppetlabs.com>
2012-02-27 16:44:45 -0800

    Add everything needed by travis-ci
    This commit adds all of the config files
    needed for travisci

--- deployment/puppet/swift/spec/spec_helper.rb
+++ deployment/puppet/swift/spec/spec_helper.rb
@@ -7,5 +7,7 @@ def param_value(subject, type, title, param)
 end
 
 RSpec.configure do |c|
-  c.module_path = File.join(File.dirname(__FILE__), '../../')
+  c.module_path  = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/modules'))
+  # Using an empty site.pp file to avoid: https://github.com/rodjek/rspec-puppet/issues/15
+  c.manifest_dir = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/manifests'))
 end
