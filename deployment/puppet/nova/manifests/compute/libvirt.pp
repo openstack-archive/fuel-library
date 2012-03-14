9770de3383329c4d66c359a934d604b67839d5c5
Dan Bode <dan@puppetlabs.com>
2012-03-14 16:02:18 -0700

    (#13120) Add connection_type to nova::compute::libvirt
    As of essex, the option connection_type is required
    for compute nodes.
    
    This commit adds the nova configuration option to the
    nova::compute::libvirt class and sets it to libvirt.

--- deployment/puppet/nova/manifests/compute/libvirt.pp
+++ deployment/puppet/nova/manifests/compute/libvirt.pp
@@ -5,6 +5,7 @@ class nova::compute::libvirt (
   $flat_network_bridge_netmask
 ) {
   nova_config { 'libvirt_type': value => $libvirt_type }
+  nova_config { 'connection_type': value => 'libvirt' }
   nova::network::bridge { $flat_network_bridge:
     ip      => $flat_network_bridge_ip,
     netmask => $flat_network_bridge_netmask,
