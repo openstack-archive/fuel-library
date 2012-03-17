e3c01896d91c56f1b433de2e92351452fff44ab5
Dan Bode <dan@puppetlabs.com>
2012-03-17 01:42:15 -0700

    Move generic::upstart to its own file
    I am not so sure that having multiple resource
    types in the same file is supported as of 2.7.11
    
    I am refactoring this code so that the upstart define
    resides in its own file according to our standards.

--- deployment/puppet/swift/manifests/storage/generic.pp
+++ deployment/puppet/swift/manifests/storage/generic.pp
@@ -52,13 +52,3 @@ define swift::storage::generic(
   }
 
 }
-# TODO this should be removed when the upstart packages are fixed.
-define swift::storage::generic::upstart() {
-  file { "/etc/init/swift-${name}.conf":
-    mode   => '0644',
-    owner  => 'root',
-    group  => 'root',
-    source => "puppet:///modules/swift/swift-${name}.conf.upstart",
-    before => Service["swift-${name}"],
-  }
-}
