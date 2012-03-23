f761929a81452bf30efd9faeaa247fd146be32d7
Dan Bode <dan@puppetlabs.com>
2012-03-23 10:31:49 -0700

    Add empty manifest file in fixtures
    To prevent rspec from failing when site.pp
    has not been specified

--- deployment/puppet/keystone/spec/spec_helper.rb
+++ deployment/puppet/keystone/spec/spec_helper.rb
@@ -7,5 +7,6 @@ def param_value(subject, type, title, param)
 end
 
 RSpec.configure do |c|
+  c.manifest_dir = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/manifests'))
   c.module_path = File.join(File.dirname(__FILE__), '../../')
 end
