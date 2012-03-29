1aebf818c6bc2010a3d20bfaa1508eac4a16bd37
Dan Bode <dan@puppetlabs.com>
2012-03-29 16:43:23 -0700

    Add code for db and host_access
    Glance can be configured to use a mysql db.
    
    This commit adds the classes db and db/host_access.

---
+++ deployment/puppet/glance/manifests/db.pp
@@ -0,0 +1,27 @@
+class glance::db(
+  $password,
+  $dbname = 'glance',
+  $user = 'glance',
+  $host = '127.0.0.1',
+  $allowed_hosts = undef,
+  $cluster_id = 'localzone'
+) {
+
+  mysql::db { $dbname:
+    user         => $user,
+    password     => $password,
+    host         => $host,
+    charset      => 'latin1',
+    # I may want to inject some sql
+    require      => Class['mysql::server'],
+  }
+
+  if $allowed_hosts {
+     # TODO this class should be in the mysql namespace
+     glance::db::host_access { $allowed_hosts:
+      user      => $user,
+      password  => $password,
+      database  => $dbname,
+    }
+  }
+}
