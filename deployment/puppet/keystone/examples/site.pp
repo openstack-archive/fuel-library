dd1473435dddf7fc5d666be84a6dd75e773a7abe
Dan Bode <dan@puppetlabs.com>
2012-03-29 14:42:23 -0700

    Add example manifest for installing keystone

---
+++ deployment/puppet/keystone/examples/site.pp
@@ -0,0 +1,41 @@
+# this is a hack that I have to do b/c openstack nova
+# sets up a route to reroute calls to the metadata server
+# to its own server which fails
+
+Exec { logoutput => 'on_failure' }
+
+stage { 'keystone_ppa': }
+
+class { 'apt':
+  stage => 'keystone_ppa',
+}
+class { 'keystone::repo::trunk':
+  stage => 'keystone_ppa',
+}
+
+
+# example of how to build a single node
+# keystone instance backed by sqlite
+# with all of the default admin roles
+node keystone {
+  class { 'concat::setup': }
+  class { 'keystone::sqlite': }
+  class { 'keystone':
+    log_verbose  => true,
+    log_debug    => true,
+    catalog_type => 'sql',
+  }->
+  class { 'keystone::roles::admin': }
+}
+
+node keystone_mysql {
+  class { 'concat::setup': }
+  class { 'keystone::mysql':
+    password => 'keystone',
+  }
+  class { 'keystone': }
+}
+
+node default {
+  fail("could not find a matching node entry for ${clientcert}")
+}
