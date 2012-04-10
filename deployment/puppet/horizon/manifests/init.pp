892590825a53ac538257a95888aa989cf5d5fc8d
Dan Bode <dan@puppetlabs.com>
2012-04-10 14:26:56 -0700

    initial checkin
    contains a basic class for configuring horizon
    on precise.

---
+++ deployment/puppet/horizon/manifests/init.pp
@@ -0,0 +1,30 @@
+#
+# installs a horizon server
+#
+class horizon(
+  $cache_server_ip   = '127.0.0.1',
+  $cache_server_port = '11211'
+) {
+
+  if $cache_server_ip =~ /^127\.0\.0\.1/ {
+    Class['memcached'] -> Class['horizon']
+  }
+
+  package { 'openstack-dashboard':
+    ensure => present,
+  }
+
+  package { 'libapache2-mod-wsgi':
+    ensure => present,
+  }
+
+  file { '/etc/openstack-dashboard/local_settings.py':
+    content => template('horizon/local_settings.py.erb'),
+    mode    => '0644',
+  }
+
+  service { 'httpd':
+    name      => 'apache2',
+    subscribe => File['/etc/openstack-dashboard/local_settings.py']
+  }
+}
