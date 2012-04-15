12ed71a3015e1e2707fa6a2e815190f1e5252cdd
Dan Bode <dan@puppetlabs.com>
2012-04-15 11:37:14 -0700

    Manage nova-compute-$type package
    This commit ensures that the virt specific
    package is installed on Ubuntu.
    
    The virt specific package needs to be explicitly
    installed on Ubuntu to ensure that the kvm package
    is not installed automatically when the compute
    package is installed.

--- deployment/puppet/nova/manifests/compute/libvirt.pp
+++ deployment/puppet/nova/manifests/compute/libvirt.pp
@@ -4,12 +4,19 @@ class nova::compute::libvirt (
 
   include nova::params
 
+  if($::nova::params::compute_package_name) {
+    package { "nova-compute-${libvirt_type}":
+      ensure => present,
+      before => Package['nova-compute'],
+    }
+  }
+
   package { 'libvirt':
     name   => $::nova::params::libvirt_package_name,
     ensure => present,
   }
 
-  service {"libvirt" :
+  service { 'libvirt' :
     name     => $::nova::params::libvirt_service_name,
     ensure   => running,
     provider => $::nova::params::special_service_provider,
