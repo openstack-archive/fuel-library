6e2c156b20387898783ed236943185698c2b1f94
Joe Topjian <joe@topjian.net>
2012-04-27 18:49:41 -0600

    Made the Keystone admin tenant a parameter that defaults to 'openstack'

--- deployment/puppet/keystone/manifests/roles/admin.pp
+++ deployment/puppet/keystone/manifests/roles/admin.pp
@@ -6,17 +6,18 @@
 #
 # It creates the following keystone objects:
 #   - service tenant
-#   - admin tenant
-#   - admin user (that defaults to admin tenant)
+#   - "admin" tenant (defaults to "openstack")
+#   - admin user (that defaults to the "admin" tenant)
 #   - admin role
 #   - Member role
-#   - adds admin role to admin user on admin tenant
+#   - adds admin role to admin user on the "admin" tenant
 # [*Parameters*]
 #
 # [email] The email address for the admin. Optional. Defaults to demo@puppetlabs.com.
 #    TODO should be required.
 # [password] The admin password. Optional. Defaults to ChangeMe
 #    TODO should be required.
+# [admin_tenant] The name of the tenant to be used for admin privileges. Optional. Defaults to openstack.
 #
 # == Dependencies
 # == Examples
@@ -29,8 +30,9 @@
 # Copyright 2012 Puppetlabs Inc, unless otherwise noted.
 #
 class keystone::roles::admin(
-  $email    = 'demo@puppetlabs.com',
-  $password = 'ChangeMe'
+  $email        = 'demo@puppetlabs.com',
+  $password     = 'ChangeMe',
+  $admin_tenant = 'openstack'
 ) {
 
   keystone_tenant { 'services':
@@ -38,7 +40,7 @@ class keystone::roles::admin(
     enabled     => 'True',
     description => 'Tenant for the openstack services',
   }
-  keystone_tenant { 'admin':
+  keystone_tenant { $admin_tenant:
     ensure      => present,
     enabled     => 'True',
     description => 'admin tenant',
@@ -46,14 +48,14 @@ class keystone::roles::admin(
   keystone_user { 'admin':
     ensure      => present,
     enabled     => 'True',
-    tenant      => 'admin',
+    tenant      => $admin_tenant,
     email       => $email,
     password    => $password,
   }
   keystone_role { ['admin', 'Member']:
     ensure => present,
   }
-  keystone_user_role { 'admin@admin':
+  keystone_user_role { "admin@${admin_tenant}":
     roles  => 'admin',
     ensure => present,
   }
