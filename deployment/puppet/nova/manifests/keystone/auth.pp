fa3d3709584bf2a57b1cc429276502a6fc039451
Dan Bode <dan@puppetlabs.com>
2012-04-30 09:42:28 -0700

    Refactor endpoint code
    This commit refactors the nova::keystone::auth code.
    
    - makes the configuration of public,internal,
    and admin address separate.
    
    makes region configurable.

--- deployment/puppet/nova/manifests/keystone/auth.pp
+++ deployment/puppet/nova/manifests/keystone/auth.pp
@@ -1,12 +1,14 @@
 class nova::keystone::auth(
-  $auth_name    = 'nova',
-  $password     = 'nova_password',
-  $service      = 'compute',
-  $address      = '127.0.0.1',
-  $compute_port = '8774',
-  $volume_port  = '8776',
-  $ec2_port     = '8773',
-  $version      = 'v1.1'
+  $auth_name        = 'nova',
+  $password         = 'nova_password',
+  $public_address   = '127.0.0.1',
+  $admin_address    = '127.0.0.1',
+  $internal_address = '127.0.0.1',
+  $compute_port     = '8774',
+  $volume_port      = '8776',
+  $ec2_port         = '8773',
+  $version          = 'v1.1',
+  $region           = 'RegionOne'
 ) {
 
   keystone_user { $auth_name:
@@ -16,19 +18,18 @@ class nova::keystone::auth(
   keystone_user_role { "${auth_name}@services":
     ensure  => present,
     roles   => 'admin',
-    require => Keystone_user[$auth_name]
   }
   keystone_service { $auth_name:
-    ensure       => present,
+    ensure      => present,
     type        => 'compute',
     description => "Openstack Compute Service",
   }
   keystone_endpoint { $auth_name:
     ensure       => present,
-    region       => 'RegionOne',
-    public_url   => "http://${address}:${compute_port}/${version}/%(tenant_id)s",
-    admin_url    => "http://${address}:${compute_port}/${version}/%(tenant_id)s",
-    internal_url => "http://${address}:${compute_port}/${version}/%(tenant_id)s",
+    region       => $region,
+    public_url   => "http://${public_address}:${compute_port}/${version}/%(tenant_id)s",
+    admin_url    => "http://${admin_address}:${compute_port}/${version}/%(tenant_id)s",
+    internal_url => "http://${internal_address}:${compute_port}/${version}/%(tenant_id)s",
   }
 
   keystone_service { "${auth_name}_volume":
@@ -38,23 +39,23 @@ class nova::keystone::auth(
   }
   keystone_endpoint { "${auth_name}_volume":
     ensure       => present,
-    region       => 'RegionOne',
-    public_url   => "http://${address}:${volume_port}/${version}/%(tenant_id)s",
-    admin_url    => "http://${address}:${volume_port}/${version}/%(tenant_id)s",
-    internal_url => "http://${address}:${volume_port}/${version}/%(tenant_id)s",
+    region       => $region,
+    public_url   => "http://${public_address}:${volume_port}/${version}/%(tenant_id)s",
+    admin_url    => "http://${admin_address}:${volume_port}/${version}/%(tenant_id)s",
+    internal_url => "http://${internal_address}:${volume_port}/${version}/%(tenant_id)s",
   }
 
   keystone_service { "${auth_name}_ec2":
     ensure      => present,
     type        => 'ec2',
-    description => 'EC2 service',
+    description => 'EC2 Service',
   }
   keystone_endpoint { "${auth_name}_ec2":
     ensure       => present,
-    region       => 'RegionOne',
-    public_url   => "http://${address}:${ec2_port}/services/Cloud",
-    admin_url    => "http://${address}:${ec2_port}/services/Admin",
-    internal_url => "http://${address}:${ec2_port}/services/Cloud",
+    region       => $region,
+    public_url   => "http://${public_address}:${ec2_port}/services/Cloud",
+    admin_url    => "http://${admin_address}:${ec2_port}/services/Admin",
+    internal_url => "http://${internal_address}:${ec2_port}/services/Cloud",
   }
 
 }
