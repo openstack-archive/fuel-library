5374cd68ec5cfc91ceed1e08ef245fbc5cd7c17f
John Chilton <jmchilton@gmail.com>
2012-05-03 16:07:10 -0500

    Refactoring keystone::python to inherit from keystone::params.

--- deployment/puppet/keystone/manifests/python.pp
+++ deployment/puppet/keystone/manifests/python.pp
@@ -1,7 +1,7 @@
 class keystone::python (
-  $client_package_name = $::keystone::params::client_package_name,
+  $client_package_name = $keystone::params::client_package_name,
   $ensure = 'present',	
-) {
+) inherits keystone::params {
 
   package { 'python-keystone' :
     name   => $client_package_name,
