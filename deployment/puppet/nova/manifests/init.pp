481bce816975279d6ded99565f3324ade54975fd
Dan Bode <dan@puppetlabs.com>
2012-05-06 00:26:11 -0700

    Remove deprecated parameters from nova class
    Removes deprecated nova params nodaemon and
    allow_admin_api.

--- deployment/puppet/nova/manifests/init.pp
+++ deployment/puppet/nova/manifests/init.pp
@@ -36,7 +36,6 @@ class nova(
   # these glance params should be optional
   # this should probably just be configured as a glance client
   $glance_api_servers = 'localhost:9292',
-  $allow_admin_api = false,
   $rabbit_host = 'localhost',
   $rabbit_password='guest',
   $rabbit_port='5672',
@@ -53,7 +52,6 @@ class nova(
   $state_path = '/var/lib/nova',
   $lock_path = $::nova::params::lock_path,
   $verbose = false,
-  $nodaemon = false,
   $periodic_interval = '60',
   $report_interval = '10',
   $root_helper = $::nova::params::root_helper
@@ -182,7 +180,6 @@ class nova(
 
   nova_config {
     'verbose': value => $verbose;
-    'nodaemon': value => $nodaemon;
     'logdir': value => $logdir;
     'image_service': value => $image_service;
     # Following may need to be broken out to different nova services
