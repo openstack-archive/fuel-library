ba05b9a3a66ee8589f3f170c6f822095c05263a2
Branan Purvine-Riley <branan@puppetlabs.com>
2012-05-29 09:50:13 -0700

    Add files for CI support
    These changes make it easier for both humans and computers to run the spec tests
    
    * Add `rake spec_full` and friends, which automatically checkout dependency modules
      prior to running spec tests
    * Prepend fixtures modulepath instead of overriding
    * Add a Travis-CI configuration file to enable easy CI

--- deployment/puppet/glance/spec/spec_helper.rb
+++ deployment/puppet/glance/spec/spec_helper.rb
@@ -1,5 +1,5 @@
-require 'puppet'
 require 'rubygems'
+require 'puppet'
 require 'rspec-puppet'
 
 def param_value(subject, type, title, param)
@@ -11,7 +11,13 @@ def verify_contents(subject, title, expected_lines)
   (content.split("\n") & expected_lines).should == expected_lines
 end
 
+Puppet.parse_config
+puppet_module_path = Puppet[:modulepath]
+
+fixture_path = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))
+
 RSpec.configure do |c|
-  c.manifest_dir = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/manifests'))
-  c.module_path = File.join(File.dirname(__FILE__), '../../')
+  fixture_module_path = File.join(fixture_path, 'modules')
+  c.module_path = [fixture_module_path, puppet_module_path].join(":")
+  c.manifest_dir = File.join(fixture_path, 'manifests')
 end
