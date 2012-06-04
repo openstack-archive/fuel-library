38f179c588f2f44cea4ba817b3d586840c8b52eb
François Charlier <francois.charlier@enovance.com>
2012-06-04 17:10:10 +0200

    Add doc for dispersion

--- deployment/puppet/swift/manifests/dispersion.pp
+++ deployment/puppet/swift/manifests/dispersion.pp
@@ -1,3 +1,53 @@
+# == Class: swift::dispersion
+#
+# This class creates a configuration file for swift-dispersion-report and
+# and swift-dispersion-populate tools.
+#
+# These tools need access to all the storage nodes and are generally ran
+# on the swift proxy node.
+#
+# For more details, see :
+#   http://swift.openstack.org/admin_guide.html#cluster-health
+#
+# === Parameters
+#
+# [*auth_url*]
+#  String. The full URL to the authentication endpoint (eg. keystone)
+#  Optional. Defaults to '127.0.0.1'.
+# [*auth_user*]
+#  String. The Swift username to use to run the tools.
+#  Optional. Defaults to 'dispersion'.
+# [*auth_tenant*]
+#  String. The user's tenant/project.
+#  Optional. Defaults to 'services'.
+# [*auth_pass*]
+#  String. The user's password.
+#  Optional. Defaults to 'dispersion_password'.
+# [*auth_version*]
+#  String. The version to pass to the 'swift' command.
+#  Use '2.0' when using Keystone.
+#  Optional. Defaults to '2.0'
+# [*swift_dir*]
+#  String. The path to swift configuration folder
+#  Optional. Defaults to '/etc/swift'.
+# [*coverage*]
+#  Integer. The percentage of partitions to cover.
+#  Optional. Defaults to 1
+# [*retries*]
+#  Integer. Number of retries.
+#  Optional. Defaults to 5.
+# [*concurrency*]
+#  Integer. Process concurrency.
+#  Optional. Defaults to 25.
+# [*dump_json*]
+#  'yes' or 'no'. Should 'swift-dispersion-report' dump json results ?
+#  Optional. Defaults to no.
+#
+# === Authors
+#
+# François Charlier fcharlier@ploup.net
+#
+
 class swift::dispersion (
   $auth_url = 'http://127.0.0.1:5000/v2.0/',
   $auth_user = 'dispersion',
--- deployment/puppet/swift/manifests/keystone/dispersion.pp
+++ deployment/puppet/swift/manifests/keystone/dispersion.pp
@@ -1,3 +1,27 @@
+# == Class: swift::keystone::dispersion
+#
+# This class creates a user in Keystone for the swift-dispersion-report
+# and swift-dispersion-populate tools.
+#
+# The user is given the admin role in the services tenant.
+#
+# Use the class swift::dispersion to create the matching config file.
+#
+# === Parameters
+#
+# [*auth_user*]
+#  String. The name of the user.
+#  Optional. Defaults to 'dispersion'.
+#
+# [*auth_pass*]
+#  String. The user's password.
+#  Optional. Defaults to 'dispersion_password'.
+#
+# === Authors
+#
+# François Charlier fcharlier@ploup.net
+#
+
 class swift::keystone::dispersion(
   $auth_user = 'dispersion',
   $auth_pass = 'dispersion_password'
