80f94729b6162d654721d4714a89d325e8fa7bd1
François Charlier <francois.charlier@enovance.com>
2012-06-28 16:21:30 +0200

    Add more parameters:
    * secret key
    * django_debug
    * api_result_limit

--- deployment/puppet/horizon/manifests/init.pp
+++ deployment/puppet/horizon/manifests/init.pp
@@ -3,9 +3,7 @@
 #
 #
 # - Parameters
-# $keystone_host        ip address/hostname of the keystone service
-# $keystone_port        public port of the keystone service
-# $keystone_scheme      http or https
+# $secret_key           the application secret key (used to crypt cookies, etc. …). mandatory
 # $cache_server_ip      memcached ip address (or VIP)
 # $cache_server_port    memcached port
 # $swift                (bool) is swift installed
@@ -14,8 +12,15 @@
 #   There is no specific requirement for these apps to be for monitoring, that's just the defacto purpose.
 #   Each app is defined in two parts, the display name, and the URI
 # [horizon_app_links]     array as in '[ ["Nagios","http://nagios_addr:port/path"],["Ganglia","http://ganglia_addr"] ]'
+# $keystone_host        ip address/hostname of the keystone service
+# $keystone_port        public port of the keystone service
+# $keystone_scheme      http or https
+# $keystone_default_role default keystone role for new users
+# $django_debug         True/False. enable/disables debugging. defaults to false
+# $api_result_limit     max number of Swift containers/objects to display on a single page
 #
 class horizon(
+  $secret_key,
   $cache_server_ip       = '127.0.0.1',
   $cache_server_port     = '11211',
   $swift                 = false,
@@ -25,6 +30,8 @@ class horizon(
   $keystone_port         = 5000,
   $keystone_scheme       = 'http',
   $keystone_default_role = 'Member',
+  $django_debug          = 'False',
+  $api_result_limit      = 1000,
 ) {
 
   include horizon::params
