54e3d97c6db0295a8ebfc5d903fae87014322494
Branan Purvine-Riley <branan@puppetlabs.com>
2012-07-06 14:43:52 -0700

    Revert "Allow multiple instances of swift::storage::server on one host"
    This reverts commit 02efd7afe06f029edee3bfcc6395857e1459b3ec.

--- deployment/puppet/swift/spec/defines/swift_storage_server_spec.rb
+++ deployment/puppet/swift/spec/defines/swift_storage_server_spec.rb
@@ -121,7 +121,7 @@ describe 'swift::storage::server' do
           req_params
         end
 
-        it { should contain_rsync__server__module("#{t}_8000").with(
+        it { should contain_rsync__server__module("#{t}").with(
           :path            => '/srv/node',
           :lock_file       => "/var/lock/#{t}.lock",
           :uid             => 'swift',
