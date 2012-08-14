a101f366b41238665f54add3bceae53ce5474305
Eugene Kirpichov <ekirpichov@gmail.com>
2012-08-14 15:45:17 -0700

    Added keepalived and common

---
+++ deployment/puppet/keepalived/manifests/config.pp
@@ -0,0 +1,18 @@
+class keepalived::config (
+  $notification_email_to,
+  $notification_email_from,
+  $smtp_server,
+  $smtp_connect_timeout,
+  $router_id,
+) {
+
+  concat { $keepalived::variables::keepalived_conf:
+    warn    => true,
+  }
+
+  concat::fragment { 'global_config':
+    target  => $keepalived::variables::keepalived_conf,
+    content => template( "${module_name}/global_config.erb" ),
+    order   => '01',
+  }
+}
