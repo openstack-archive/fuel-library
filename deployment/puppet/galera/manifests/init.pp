ed9fbfa84f8f0816eddc321857d3161321e7d024
Eugene Kirpichov <ekirpichov@gmail.com>
2012-08-15 13:04:51 -0700

    Preliminary galera support

--- deployment/puppet/galera/manifests/init.pp
+++ deployment/puppet/galera/manifests/init.pp
@@ -8,7 +8,7 @@
 # vi /etc/mysql/conf.d/wsrep.cnf 
 # /etc/init.d/mysql start
 # 
-class galera($cluster_name, $master_ip = false) {
+class galera($cluster_name, $master_ip = false, $node_address = $ipaddress_eth0) {
 
   $mysql_user     = "wsrep_sst"
   $mysql_password = "password"
@@ -16,7 +16,8 @@ class galera($cluster_name, $master_ip = false) {
   service { "mysql-galera" :
     name        => "mysql",
     ensure      => "running",
-    require     => [Package["mysql-server-wsrep", "galera"], File["/etc/mysql/conf.d/wsrep.cnf"]],
+    require     => [Package["mysql-server-wsrep", "galera"]],
+    notify      => Exec["set-mysql-password"],
     hasrestart  => true,
     # hasstatus   => true, // http://projects.puppetlabs.com/issues/5610
   }
@@ -55,13 +56,20 @@ class galera($cluster_name, $master_ip = false) {
     ensure      => present,
     content     => template("galera/wsrep.cnf.erb"),
     require     => Package["mysql-server-wsrep", "galera"],
+    notify      => Exec["restart-mysql-galera"],
+  }
+
+  exec { "restart-mysql-galera": # Can't just notify the service as this would introduce a dependency loop.
+    command => "sudo service mysql restart; sleep 10s", # sleep to make initial state transfer possible before service is restarted again by mysql::config [BUG; TODO create better fix]
+    path => ["/usr/bin", "/usr/sbin", "/sbin", "/bin"],
+    refreshonly => true,
   }
 
   exec { "set-mysql-password" :
     unless      => "/usr/bin/mysql -u${mysql_user} -p${mysql_password}",
     command     => "/usr/bin/mysql -uroot -e \"set wsrep_on='off'; delete from mysql.user where user=''; grant all on *.* to '${mysql_user}'@'%' identified by '${mysql_password}';flush privileges;\"",
     require     => Service["mysql-galera"],
-    subscribe   => Service["mysql-galera"],
+    notify      => File["/etc/mysql/conf.d/wsrep.cnf"],
     refreshonly => true,
   }
 
