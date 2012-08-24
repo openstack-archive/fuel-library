69205d294158e9fe15811989c67ed24d7d7fbbbd
Michael Polenchuk <mpolenchuk@mirantis.com>
2012-08-24 20:21:57 +0400

    Add centos support for galera/nova/rabbitmq modules

--- deployment/puppet/galera/manifests/init.pp
+++ deployment/puppet/galera/manifests/init.pp
@@ -8,68 +8,60 @@
 # vi /etc/mysql/conf.d/wsrep.cnf 
 # /etc/init.d/mysql start
 # 
-class galera($cluster_name, $master_ip = false, $node_address = $ipaddress_eth0) {
+class galera($cluster_name, $master_ip = false) {
 
-  $mysql_user     = "wsrep_sst"
-  $mysql_password = "password"
+  include galera::params
+
+  $mysql_user     = $::galera::params::mysql_user
+  $mysql_password = $::galera::params::mysql_password
 
   service { "mysql-galera" :
     name        => "mysql",
     ensure      => "running",
-    require     => [Package["mysql-server-wsrep", "galera"]],
-    notify      => Exec["set-mysql-password"],
+    require     => [Package["mysql-server-wsrep", "galera"], File["/etc/mysql/conf.d/wsrep.cnf"]],
     hasrestart  => true,
     # hasstatus   => true, // http://projects.puppetlabs.com/issues/5610
   }
 
-  package { ["mysql-client", "libssl0.9.8", "libaio1"] :
+  package { [$::galera::params::mysql_client_package, $::galera::params::libssl_package] :
     ensure      => present,
-  }
-
-  package { ["mysql-server-5.5", "mysql-server-core-5.5"] :
-    ensure 		=> purged,
   }
 
   package { "mysql-server-wsrep" :
     ensure      => present,
-    provider    => "dpkg",
-    source      => "/tmp/mysql-server-wsrep.deb",
-    require     => [Package["mysql-server-5.5"], Package["mysql-server-core-5.5"], Package["libaio1"], File["/tmp/mysql-server-wsrep.deb"], Package["mysql-client"]],
+    provider    => $::galera::params::pkg_provider,
+    source      => "/tmp/${::galera::params::mysql_server_package}",
+    require     => [Exec["download-wsrep"], Package[$::galera::params::mysql_client_package]],
   }
-
-  file { "/tmp/mysql-server-wsrep.deb" :
-    source => "puppet:///modules/galera/mysql-server-wsrep-5.5.23-23.6-amd64.deb"
+  
+  exec { "download-wsrep" :
+    command     => "/usr/bin/wget -P/tmp https://launchpad.net/codership-mysql/${::galera::params::mysql_version}/+download/${::galera::params::mysql_server_package}",
+    creates     => "/tmp/${::galera::params::mysql_server_package}"
   }
 
   package { "galera" :
     ensure      => present,
-    provider    => "dpkg",
-    source      => "/tmp/galera.deb",
-    require     => [File["/tmp/galera.deb"], Package["libssl0.9.8"]],
+    provider    => $::galera::params::pkg_provider,
+    source      => "/tmp/${::galera::params::galera_package}",
+    require     => [Exec["download-galera"], Package[$::galera::params::libssl_package]],
   }
 
-  file { "/tmp/galera.deb" :
-    source => "puppet:///modules/galera/galera-23.2.1-amd64.deb"
+  exec { "download-galera" :
+    command     => "/usr/bin/wget -P/tmp https://launchpad.net/galera/2.x/23.2.1/+download/${::galera::params::galera_package}",
+    creates     => "/tmp/${::galera::params::galera_package}",
   }
 
   file { "/etc/mysql/conf.d/wsrep.cnf" :
     ensure      => present,
     content     => template("galera/wsrep.cnf.erb"),
     require     => Package["mysql-server-wsrep", "galera"],
-    notify      => Exec["restart-mysql-galera"],
-  }
-
-  exec { "restart-mysql-galera": # Can't just notify the service as this would introduce a dependency loop.
-    command => "sudo service mysql restart; sleep 10s", # sleep to make initial state transfer possible before service is restarted again by mysql::config [BUG; TODO create better fix]
-    path => ["/usr/bin", "/usr/sbin", "/sbin", "/bin"],
-    refreshonly => true,
   }
 
   exec { "set-mysql-password" :
     unless      => "/usr/bin/mysql -u${mysql_user} -p${mysql_password}",
     command     => "/usr/bin/mysql -uroot -e \"set wsrep_on='off'; delete from mysql.user where user=''; grant all on *.* to '${mysql_user}'@'%' identified by '${mysql_password}';flush privileges;\"",
     require     => Service["mysql-galera"],
-    notify      => File["/etc/mysql/conf.d/wsrep.cnf"],
+    subscribe   => Service["mysql-galera"],
     refreshonly => true,
   }
 
