3c8765e74b46ca63042b903be4b22fe59bad3bb2
Hunter Haugen <h.haugen@gmail.com>
2012-08-27 17:08:07 -0700

    Add puppetlabs_spec_helper gem code

--- deployment/puppet/haproxy/Rakefile
+++ deployment/puppet/haproxy/Rakefile
@@ -1,8 +1,1 @@
-require 'rake'
-require 'puppet-lint/tasks/puppet-lint'
-require 'rspec/core/rake_task'
-
-RSpec::Core::RakeTask.new(:spec) do |t|
-  t.rspec_opts = '-c'
-  t.pattern = 'spec/*/*_spec.rb'
-end
+require 'puppetlabs_spec_helper/rake_tasks'
--- deployment/puppet/haproxy/spec/spec_helper.rb
+++ deployment/puppet/haproxy/spec/spec_helper.rb
@@ -1,13 +1,1 @@
-require 'puppet'
-require 'rubygems'
-require 'rspec-puppet'
-
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
-RSpec.configure do |c|
- c.module_path = File.join(fixture_path, 'modules')
- c.manifest_dir = File.join(fixture_path, 'manifests')
-end
-
-class Object
-    alias :must :should
-end
+require 'puppetlabs_spec_helper/module_spec_helper'
