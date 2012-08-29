51184793d7fe65c49ba1ff35c0718dbbda2edd1d
Ivan Ponovarev <iponomarev@mirantis.com>
2012-08-29 15:20:46 +0400

    fix previous commit

---
+++ deployment/puppet/openstack/manifests/repo/yum.pp
@@ -0,0 +1,27 @@
+class openstack::repo::yum (
+  $repo_name,
+  $location,
+  $key_source,
+  $include_src = false,
+  $priority = 1,
+)
+  {
+
+  if defined(Package['yum-plugin-priorities']) {}
+  else {
+    package { 'yum-plugin-priorities':
+      ensure => present,
+    }
+  }
+
+  Package['yum-plugin-priorities'] -> Yumrepo['openstack-ci']
+
+  yumrepo {$repo_name:
+    baseurl  => $location,
+    gpgcheck => 1,
+    gpgkey   => $key_source,
+    priority => $priority,
+    enabled  => 1,
+  }
+
+}
