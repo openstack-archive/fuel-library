d5d16996f0ddf9e59014dcd212b520f073351f6c
Michael Polenchuk <mpolenchuk@mirantis.com>
2012-08-30 16:20:34 +0400

    update/rewrite galera module

--- deployment/puppet/galera/manifests/init.pp
+++ deployment/puppet/galera/manifests/init.pp
@@ -12,30 +12,96 @@ class galera($cluster_name, $master_ip = false, $node_address = $ipaddress_eth0)
 
   include galera::params
 
-  $mysql_user     = $::galera::params::mysql_user
-  $mysql_password = $::galera::params::mysql_password
+  $mysql_user       = $::galera::params::mysql_user
+  $mysql_password   = $::galera::params::mysql_password
+  $libgalera_prefix = $::galera::params::libgalera_prefix
+
+
+  case $::osfamily {
+    'RedHat': {
+      #$pkg_prefix = 'ftp://ftp.sunet.se/pub/databases/relational/mysql/Downloads/MySQL-5.5/'
+      $pkg_prefix = '/tmp/'
+
+      package { "mysql-libs" : 
+        ensure   => purged,
+        before   => [Package['MySQL-client', 'MySQL-shared', 'MySQL-shared-compat', 'galera'], File['/etc/my.cnf']]
+      }
+
+      package { 'MySQL-client' :
+        ensure   => present,
+        provider => $::galera::params::pkg_provider,
+        source   => "${pkg_prefix}MySQL-client-5.5.27-1.el6.x86_64.rpm",
+        before   => Package['MySQL-shared']
+      } 
+
+      package { 'MySQL-shared' :
+        ensure   => present,
+        provider => $::galera::params::pkg_provider,
+        source  => "${pkg_prefix}MySQL-shared-5.5.27-1.el6.x86_64.rpm",
+        before   => Package['MySQL-shared-compat']
+      } 
+
+      package { 'MySQL-shared-compat' :
+        ensure   => present,
+        provider => $::galera::params::pkg_provider,
+        source => "${pkg_prefix}MySQL-shared-compat-5.5.27-1.el6.x86_64.rpm",
+        before   => Package['MySQL-server']
+      } 
+
+      file { '/etc/my.cnf' :
+        ensure  => present,
+        source  => 'puppet:///modules/galera/my.cnf',
+        before  => Service['mysql-galera']
+      }
+
+      class { 'selinux' :
+        mode   => 'disabled',
+        before => Package['mysql-libs']
+      }
+
+      package { 'wget' :
+        ensure => present,
+        before => Exec['download-wsrep', 'download-galera']
+      }
+
+      exec { "bugfix_create_db" :
+        command => "/bin/sleep 15; /usr/bin/mysql_install_db --user=mysql",
+        require => Package["MySQL-server"],
+        before  => Service['mysql-galera'],
+        unless  => "/bin/ls /var/lib/mysql/performance_schema"
+      }
+
+    }
+    'Debian': {
+      package { "mysql-client" :
+        ensure => present,
+        before => Package["MySQL-server"]
+      }
+    }
+  }
 
   service { "mysql-galera" :
     name        => "mysql",
     ensure      => "running",
-    require     => [Package["mysql-server-wsrep", "galera"], File["/etc/mysql/conf.d/wsrep.cnf"]],
+    require     => [Package["MySQL-server", "galera"], File["/etc/mysql/conf.d/wsrep.cnf"]],
     hasrestart  => true,
-    # hasstatus   => true, // http://projects.puppetlabs.com/issues/5610
+    hasstatus   => true,
   }
 
-  package { [$::galera::params::mysql_client_package, $::galera::params::libssl_package] :
+  package { [$::galera::params::libssl_package, $::galera::params::libaio_package] :
     ensure      => present,
+    before      => Package["galera", "MySQL-server"]
   }
 
-  package { "mysql-server-wsrep" :
+  package { "MySQL-server" :
     ensure      => present,
     provider    => $::galera::params::pkg_provider,
     source      => "/tmp/${::galera::params::mysql_server_package}",
-    require     => [Exec["download-wsrep"], Package[$::galera::params::mysql_client_package]],
+    require     => Exec["download-wsrep"],
   }
-  
+
   exec { "download-wsrep" :
-    command     => "/usr/bin/wget -P/tmp https://launchpad.net/codership-mysql/${::galera::params::mysql_version}/+download/${::galera::params::mysql_server_package}",
+    command     => "/usr/bin/wget -P/tmp https://launchpad.net/codership-mysql/5.5/5.5.23-23.6/+download/${::galera::params::mysql_server_package}",
     creates     => "/tmp/${::galera::params::mysql_server_package}"
   }
 
@@ -43,7 +109,7 @@ class galera($cluster_name, $master_ip = false, $node_address = $ipaddress_eth0)
     ensure      => present,
     provider    => $::galera::params::pkg_provider,
     source      => "/tmp/${::galera::params::galera_package}",
-    require     => [Exec["download-galera"], Package[$::galera::params::libssl_package]],
+    require     => Exec["download-galera"],
   }
 
   exec { "download-galera" :
@@ -51,10 +117,15 @@ class galera($cluster_name, $master_ip = false, $node_address = $ipaddress_eth0)
     creates     => "/tmp/${::galera::params::galera_package}",
   }
 
+  file { ["/etc/mysql", "/etc/mysql/conf.d" ] :
+    ensure => directory,
+    before => File["/etc/mysql/conf.d/wsrep.cnf"]
+  }
+
   file { "/etc/mysql/conf.d/wsrep.cnf" :
     ensure      => present,
     content     => template("galera/wsrep.cnf.erb"),
-    require     => Package["mysql-server-wsrep", "galera"],
+    require     => Package["MySQL-server", "galera"],
   }
 
   exec { "set-mysql-password" :
--- deployment/puppet/galera/manifests/params.pp
+++ deployment/puppet/galera/manifests/params.pp
@@ -9,18 +9,22 @@ class galera::params {
     'RedHat': {
       $pkg_provider         = 'rpm'
       $libssl_package       = 'openssl098e'
-      $mysql_client_package = 'mysql'
-      $mysql_version        = '5.1/5.1.62-23.4'
-      $mysql_server_package = 'MySQL-server-wsrep-5.1.62_23.4-1.rhel5.x86_64.rpm'
+      $libaio_package       = 'libaio'
+      # $mysql_client_package = 'mysql'
+      # $mysql_version        = '5.1/5.1.62-23.4'
+      $mysql_server_package = 'MySQL-server-5.5.23_wsrep_23.6-1.rhel5.x86_64.rpm'
       $galera_package       = 'galera-23.2.1-1.rhel5.x86_64.rpm'
+      $libgalera_prefix     = '/usr/lib64'
     }
     'Debian': {
       $pkg_provider         = 'dpkg'
       $libssl_package       = 'libssl0.9.8'
-      $mysql_client_package = 'mysql-client'
-      $mysql_version        = '5.5/5.5.23-23.6'
+      $libaio_package       = 'libaio1'
+      # $mysql_client_package = 'mysql-client'
+      # $mysql_version        = '5.5/5.5.23-23.6'
       $mysql_server_package = 'mysql-server-wsrep-5.5.23-23.6-amd64.deb'
       $galera_package       = 'galera-23.2.1-amd64.deb'
+      $libgalera_prefix     = '/usr/lib'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only support osfamily RedHat and Debian")
