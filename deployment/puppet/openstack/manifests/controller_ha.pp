35ce3faf15d6c4baaaed0010ca5adbccf8239e7f
Eugene Kirpichov <ekirpichov@gmail.com>
2012-08-30 17:14:24 -0700

    Removed unneeded haproxy configuration from controller_ha.pp

--- deployment/puppet/openstack/manifests/controller_ha.pp
+++ deployment/puppet/openstack/manifests/controller_ha.pp
@@ -47,28 +47,6 @@ class openstack::controller_ha (
     $internal_address = $controller_internal_addresses[$which]
     class { 'haproxy':
       enable => true, 
-      haproxy_global_options   => {'log'      => "${internal_address} local0",
-                                    'chroot'  => '/var/lib/haproxy',
-                                    'pidfile' => '/var/run/haproxy.pid',
-                                    'maxconn' => '4000',
-                                    'user'    => 'haproxy',
-                                    'group'   => 'haproxy',
-                                    'daemon'  => '',
-                                    'stats'   => 'socket /var/lib/haproxy/stats'
-                                  },
-      haproxy_defaults_options => {'log'      => 'global',
-                                    'stats'   => 'enable',
-                                    'mode'    => 'http',
-                                    'option'  => 'redispatch',
-                                    'retries' => '3',
-                                    'timeout' => ['http-request 10s',
-                                    'queue 1m',
-                                    'connect 10s',
-                                    'client 1m',
-                                    'server 1m',
-                                    'check 10s'],
-                                    'maxconn' => '8000'
-                                  },
       require => Exec['create-virtual-ip'],
     }
 
