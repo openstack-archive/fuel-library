88a74365cfa7594c11edf09831da8e66d9b33bec
Michael Polenchuk <mpolenchuk@mirantis.com>
2012-09-13 16:00:15 +0400

    Fix selinux config file path

--- deployment/puppet/selinux/manifests/config.pp
+++ deployment/puppet/selinux/manifests/config.pp
@@ -29,8 +29,8 @@ class selinux::config(
   # Check to see if the mode set is valid.
   if $mode == 'enforcing' or $mode == 'permissive' or $mode == 'disabled' {
     exec { "set-selinux-config-to-${mode}":
-      command => "sed -i \"s@^\\(SELINUX=\\).*@\\1${mode}@\" /etc/sysconfig/selinux",
-      unless  => "grep -q \"SELINUX=${mode}\" /etc/sysconfig/selinux",
+      command => "sed -i \"s@^\\(SELINUX=\\).*@\\1${mode}@\" /etc/selinux/config",
+      unless  => "grep -q \"SELINUX=${mode}\" /etc/selinux/config",
     }
 
     case $mode {
