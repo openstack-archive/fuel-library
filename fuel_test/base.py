6f0ad4bdd67faa1f837a225d0038e0f2d3f558fb
vic <vgalkin@mirantis.com>
2012-09-17 23:45:37 +0400

    Copy puppet recipes to /etc/puppet/module

--- fuel_test/base.py
+++ fuel_test/base.py
@@ -1,11 +1,12 @@
 import unittest
-from devops.helpers import ssh
+from devops.helpers import ssh, os
 from ci import get_environment
 from root import root
 
 class RecipeTestCase(unittest.TestCase):
 
     def setUp(self):
+        self.upload_recipes()
         self.environment = get_environment()
         master = self.environment.node['master']
         self.revert_snapshot()
@@ -15,9 +16,11 @@ class RecipeTestCase(unittest.TestCase):
 
     def upload_recipes(self):
         recipes_dir = root('fuel','deployment','puppet')
-        remote_dir = "/etc/puppet/modules/"
-        self.remote.mkdir(remote_dir)
-        self.remote.upload(recipes_dir, remote_dir)
+        for dir in os.listdir(recipes_dir):
+            recipe_dir = os.path.join(recipes_dir, dir)
+            remote_dir = "/etc/puppet/modules/"
+            self.remote.mkdir(remote_dir)
+            self.remote.upload(recipe_dir, remote_dir)
 
     def revert_snapshot(self):
         try:
--- fuel_test/test_apply_noop.py
+++ fuel_test/test_apply_noop.py
@@ -19,11 +19,13 @@ class MyTestCase(RecipeTestCase):
         return errors, warnings
 
     def test_apply_all_modules_with_noop(self):
-        result = self.remote.execute("for i in `find /etc/puppet/modules/ | grep tests/.*pp`; do puppet apply  --modulepath=/etc/puppet/modules/puppet/ --noop $i ; done")
+        result = self.remote.execute("for i in `find /etc/puppet/modules/ | grep tests/.*pp`; do puppet apply  --modulepath=/etc/puppet/modules/ --noop $i ; done")
         self.assertEqual([], result['stderr'], result['stderr'])
         errors, warnings = self.parse_out(result['stdout'])
         self.assertEqual([], errors, errors)
         self.assertEqual([], warnings, warnings)
+
+
 
 if __name__ == '__main__':
     unittest.main()
