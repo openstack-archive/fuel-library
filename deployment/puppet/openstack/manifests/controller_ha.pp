a727f0129e64b288ac5511baeb8c2a4546b12e21
Vladimir Kuklin <Vladimir Kuklin vkuklin@mirantis.com>
2012-09-19 18:54:19 +0400

    add public and internal virtual_ips [FUEL-24]

--- deployment/puppet/openstack/manifests/controller_ha.pp
+++ deployment/puppet/openstack/manifests/controller_ha.pp
@@ -45,38 +45,46 @@ class openstack::controller_ha (
 
     $which = $::hostname ? { $master_hostname => 0, default => 1 }
 
-    $vip = $virtual_ip
-    $hosts = $controller_hostnames
-    $ips = $controller_internal_addresses
+    #    $vip = $virtual_ip
+    #    $hosts = $controller_hostnames
+    #    $ips = $controller_internal_addresses
+
 
     # haproxy
     include haproxy::data
 
     Haproxy_service {
-      virtual_ip => $vip,
-      hostnames => $hosts,
-      balancer_ips => $ips
+#      virtual_ip => $vip,
+      hostnames => $controller_hostnames,
+      balancer_ips => $controller_internal_addresses
     }
 
-    haproxy_service { 'horizon':    order => 15, port => 80 }
-    haproxy_service { 'keystone-1': order => 20, port => 5000 }
-    haproxy_service { 'keystone-2': order => 30, port => 35357 }
-    haproxy_service { 'nova-api-1': order => 40, port => 8773 }
-    haproxy_service { 'nova-api-2': order => 50, port => 8774 }
-    haproxy_service { 'nova-api-3': order => 60, port => 8775 }
-    haproxy_service { 'nova-api-4': order => 70, port => 8776 }
-    haproxy_service { 'glance-api': order => 80, port => 9292 }
-    haproxy_service { 'glance-reg': order => 90, port => 9191 }
-    haproxy_service { 'mysqld':     order => 95, port => 3306 }
+    haproxy_service { 'horizon':    order => 15, port => 80, virtual_ips => [$public_virtual_ip] }
+    haproxy_service { 'keystone-1': order => 20, port => 5000, virtual_ips => [$public_virtual_ip, $internal_virtual_ip]  }
+    haproxy_service { 'keystone-2': order => 30, port => 35357, virtual_ips => [$public_virtual_ip, $internal_virtual_ip]  }
+    haproxy_service { 'nova-api-1': order => 40, port => 8773, virtual_ips => [$public_virtual_ip, $internal_virtual_ip]  }
+    haproxy_service { 'nova-api-2': order => 50, port => 8774, virtual_ips => [$public_virtual_ip, $internal_virtual_ip]  }
+    haproxy_service { 'nova-api-3': order => 60, port => 8775, virtual_ips => [$public_virtual_ip, $internal_virtual_ip]  }
+    haproxy_service { 'nova-api-4': order => 70, port => 8776, virtual_ips => [$public_virtual_ip, $internal_virtual_ip]  }
+    haproxy_service { 'glance-api': order => 80, port => 9292, virtual_ips => [$public_virtual_ip, $internal_virtual_ip]  }
+    haproxy_service { 'glance-reg': order => 90, port => 9191, virtual_ips => [$internal_virtual_ip]  }
+    haproxy_service { 'mysqld':     order => 95, port => 3306, virtual_ips => [$internal_virtual_ip]  }
 
     if $which == 0 {
-      exec { 'create-virtual-ip':
-        command => "ip addr add ${virtual_ip} dev ${internal_interface}",
-        unless => "ip addr show dev ${internal_interface} | grep ${virtual_ip}",
+      exec { 'create-public-virtual-ip':
+        command => "ip addr add ${public_virtual_ip} dev ${public_interface}",
+        unless => "ip addr show dev ${public_interface} | grep ${public_virtual_ip}",
         path => ['/usr/bin', '/usr/sbin', '/sbin', '/bin'],
       }
     }
 
+    if $which == 0 {
+      exec { 'create-internal-virtual-ip':
+        command => "ip addr add ${internal_virtual_ip} dev ${internal_interface}",
+        unless => "ip addr show dev ${internal_interface} | grep ${internal_virtual_ip}",
+        path => ['/usr/bin', '/usr/sbin', '/sbin', '/bin'],
+      }
+    }
     sysctl::value { 'net.ipv4.ip_nonlocal_bind': value => '1' }
 
     $internal_address = $controller_internal_addresses[$which]
@@ -97,9 +105,15 @@ class openstack::controller_ha (
 
     # keepalived
     class { 'keepalived': require => Class['haproxy'] }
-    keepalived::instance { '42':
+    keepalived::instance { '41':
       interface => $public_interface,
-      virtual_ips => [$virtual_ip],
+      virtual_ips => [$public_virtual_ip],
+      state    => $which ? { 0 => 'MASTER', default => 'BACKUP' },
+      priority => $which ? { 0 => 101,      default => 100      },
+    }
+    keepalived::instance { '42':
+      interface => $internal_interface,
+      virtual_ips => [$internal_virtual_ip],
       state    => $which ? { 0 => 'MASTER', default => 'BACKUP' },
       priority => $which ? { 0 => 101,      default => 100      },
     }
@@ -118,10 +132,10 @@ class openstack::controller_ha (
 #    Class['openstack::controller']->Class['galera']
     
     class { 'openstack::controller':
-      public_address          => $virtual_ip,
+      public_address          => $public_virtual_ip,
       public_interface        => $public_interface,
       private_interface       => $private_interface,
-      internal_address        => $virtual_ip,
+      internal_address        => $internal_virtual_ip,
       floating_range          => $floating_range,
       fixed_range             => $fixed_range,
       multi_host              => $multi_host,
@@ -148,14 +162,14 @@ class openstack::controller_ha (
       cache_server_ip         => $memcached_servers,
       export_resources        => false,
       api_bind_address        => $controller_internal_addresses[$which],
-      mysql_host              => $virtual_ip,
-      service_endpoint        => $virtual_ip,
+      mysql_host              => $internal_virtual_ip,
+      service_endpoint        => $internal_virtual_ip,
     }
 
     class { 'openstack::auth_file':
       admin_password          => $admin_password,
       keystone_admin_token    => $keystone_admin_token,
-      controller_node         => $virtual_ip,
+      controller_node         => $internal_virtual_ip,
     }
 }
 
