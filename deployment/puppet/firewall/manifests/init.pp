091c1f278644108425d01f0f56c05f5170e74ca0
Vladimir Kuklin <vkuklin@mirantis.com>
2012-09-21 15:00:00 +0400

    parametrize ports and add mysql_backend_port

--- deployment/puppet/firewall/manifests/init.pp
+++ deployment/puppet/firewall/manifests/init.pp
@@ -1,25 +1,44 @@
 
-class firewall {
+class firewall (
+	$ssh_port = 22,
+	$http_port = 80,
+	$https_port = 443,
+	$mysql_port = 3306,
+	$mysql_backend_port = 3307,
+	$keystone_public_port =  5000,
+        $keystone_admin_port = 35357,
+	$glance_api_port = 9292,
+	$glance_reg_port = 9191,
+	$glance_nova_api_ec2_port = 8773,
+	$nova_api_compute_port =   8774,
+	$nova_api_metadata_port =  8775,
+	$nova_api_volume_port =  8776,
+	$nova_vncproxy_port =  6080,
+	$erlang_epmd_port  =   4369,
+	$erlang_rabbitmq_port =  5672,
+	$memcached_port =  11211,
+) {
 
   case $::osfamily {
     'RedHat': {
       firewall::allow {[
-          22,     # ssh
-          80,     # http
-          3306,   # mysql
-          4567,   # mysql/galera
-          5000,   # keystone/public
-          35357,  # keystone/admin
-          9292,   # glance/api
-          9191,   # glance/reg
-          8773,   # nova/api/ec2
-          8774,   # nova/api/compute
-          8775,   # nova/api/metadata
-          8776,   # nova/api/volume
-          6080,   # nova/vncproxy
-          4369,   # erlang/epmd
-          5672,   # erlang/rabbitmq
-          11211,  # memcached
+		$ssh_port,
+		$http_port,
+		$https_port,
+		$mysql_port,
+		$mysql_backend_port,
+		$keystone_public_port,
+		$keystone_admin_port, 
+		$glance_api_port,
+		$glance_reg_port,
+		$glance_nova_api_ec2_port,
+		$nova_api_compute_port,
+		$nova_api_metadata_port,
+		$nova_api_volume_port,
+		$nova_vncproxy_port,
+		$erlang_epmd_port,
+		$erlang_rabbitmq_port,
+		$memcached_port,
       ]: }
      }
      default: {
