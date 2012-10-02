ba70151a37c4103cb9d41302af5120c914e4f71f
Vladimir Kuklin <Vladimir Kuklin vkuklin@mirantis.com>
2012-10-02 17:44:32 +0400

    add bigger quotas

--- deployment/puppet/openstack/manifests/nova/controller.pp
+++ deployment/puppet/openstack/manifests/nova/controller.pp
@@ -133,7 +133,17 @@ if ($rabbit_nodes)
   }
  
  }
-  
+  class {'nova::quota':
+    quota_instances => 100,
+    quota_cores => 100,
+    quota_volumes => 100,
+    quota_gigabytes => 1000,
+    quota_floating_ips => 100,
+    quota_metadata_items => 1024,
+    quota_max_injected_files => 50,
+    quota_max_injected_file_content_bytes => 102400,
+    quota_max_injected_file_path_bytes => 4096
+  }
   # Configure nova-api
   class { 'nova::api':
     enabled           => $enabled,
