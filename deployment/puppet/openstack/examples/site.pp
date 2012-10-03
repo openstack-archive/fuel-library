f53a65c522d9f1bc8fad79d4c0d3fbaa069272ab
Dmitry Pyzhov <dpyzhov@mirantis.com>
2012-10-03 18:01:54 +0400

    Add centos gpg key for epel-fuel repo

--- deployment/puppet/openstack/examples/site.pp
+++ deployment/puppet/openstack/examples/site.pp
@@ -51,7 +51,7 @@ case $::osfamily {
       repo_name  => 'openstack-epel-fuel',
       #      location   => $repo_baseurl,
       mirrorlist => $mirrorlist,
-      key_source => 'https://fedoraproject.org/static/0608B895.txt',
+      key_source => "https://fedoraproject.org/static/0608B895.txt\n  http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-6",
       stage      => 'openstack-custom-repo',
     }
   }
