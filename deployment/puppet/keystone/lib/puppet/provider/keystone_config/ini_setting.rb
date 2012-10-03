a211ca2a4082423487286195581a982c6a0a12be
Dan Bode <dan@puppetlabs.com>
2012-10-02 20:20:45 -0700

    Add native types for managing keystone
    Adds native types that can manage individual
    keystone_config entries. These types were added
    to provide flexibility to partially manage individual
    lines based on the base config file layer down
    by the package.
    
    It will also make it easier for users to override
    whatever config they care about at top scope and
    not have to submit patches for everything.

---
+++ deployment/puppet/keystone/lib/puppet/provider/keystone_config/ini_setting.rb
@@ -0,0 +1,24 @@
+Puppet::Type.type(:ini_setting)#.providers
+
+Puppet::Type.type(:keystone_config).provide(
+  :ini_setting,
+  :parent => Puppet::Type::Ini_setting::ProviderRuby
+) do
+
+  def section
+    resource[:name].split('/', 2).first
+  end
+
+  def setting
+    resource[:name].split('/', 2).last
+  end
+
+  def separator
+    '='
+  end
+
+  def file_path
+    '/etc/keystone/keystone.conf'
+  end
+
+end
---
+++ deployment/puppet/keystone/lib/puppet/type/keystone_config.rb
@@ -0,0 +1,18 @@
+Puppet::Type.newtype(:keystone_config) do
+
+  ensurable
+
+  newparam(:name, :namevar => true) do
+    desc 'Section/setting name to manage from keystone.conf'
+    newvalues(/\S+\/\S+/)
+  end
+
+  newproperty(:value) do
+    desc 'The value of the setting to be defined.'
+    munge do |v|
+      v.to_s.strip
+    end
+  end
+
+
+end
