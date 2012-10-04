7c6722fead0f8d7733230720e4e232b5c485c292
vic <vgalkin@mirantis.com>
2012-10-04 18:14:10 +0400

    Fix for tempest at several controllers

--- fuel_test/prepare_for_tempest.py
+++ fuel_test/prepare_for_tempest.py
@@ -4,33 +4,32 @@ from ci import get_environment
 from helpers import tempest_write_config, tempest_add_images, tempest_share_glance_images, tempest_mount_glance_images, get_auth_url
 from openstack_site_pp_base import OpenStackSitePPBaseTestCase
 import unittest
-from settings import NODES
+from settings import NODES, controllers
 
 
 class PrepareTempest(OpenStackSitePPBaseTestCase):
     def setUp(self):
         self.environment = get_environment()
-        self.controller1 = self.environment.node[NODES[0]]
-        self.controller2 = self.environment.node[NODES[1]]
+        self.controller1 = self.environment.node[controllers[0]]
 
     def prepare_for_tempest(self):
         host = self.get_public_virtual_ip()
         remote = ssh(
             self.controller1.ip_address, username='root',
             password='r00tme').sudo.ssh
-        remote_controller2 = ssh(
-            self.controller2.ip_address, username='root',
-            password='r00tme').sudo.ssh
         tempest_share_glance_images(remote, self.get_internal_network())
-        tempest_mount_glance_images(remote_controller2, )
-
+        for name in controllers[1:]:
+            controller = self.environment.node[name]
+            remote_controller = ssh(
+                controller.ip_address, username='root',
+                password='r00tme').sudo.ssh
+            tempest_mount_glance_images(remote_controller)
         keystone = keystoneclient.v2_0.client.Client(
             username='admin', password='nova', tenant_name='openstack', auth_url=get_auth_url(host))
         tenant1 = keystone.tenants.create('tenant1')
         tenant2 = keystone.tenants.create('tenant2')
         keystone.users.create('tempest1','secret', 'tempest1@example.com', tenant_id=tenant1.id)
         keystone.users.create('tempest2','secret', 'tempest1@example.com', tenant_id=tenant2.id)
-
         openstack_tenant_id = filter(lambda item: item.name=='openstack', keystone.tenants.list())[0].id
         image_ref, image_ref_any = tempest_add_images(remote, host, openstack_tenant_id)
         tempest_write_config(host, image_ref, image_ref_any)
