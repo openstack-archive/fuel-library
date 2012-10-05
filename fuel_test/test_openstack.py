db44452fe0d88603c00869d578a47d775cf6f382
vic <vgalkin@mirantis.com>
2012-10-05 19:30:22 +0400

    Don't take snapshots

--- fuel_test/test_openstack.py
+++ fuel_test/test_openstack.py
@@ -7,8 +7,6 @@ class OpenStackCase(OpenStackSitePPBaseTestCase):
         self.validate(
             [self.controller1,self.controller2,self.compute1,self.compute2],
             'puppet agent --test')
-        for node in self.environment.nodes:
-            node.save_snapshot('openstack')
 
 if __name__ == '__main__':
     unittest.main()
