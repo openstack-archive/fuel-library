179cb3562454937f348b6c4dbccb6c1a1e02d1ca
vic <vgalkin@mirantis.com>
2012-10-05 18:02:41 +0400

    Add sleep for mysql

--- fuel_test/prepare_for_tempest.py
+++ fuel_test/prepare_for_tempest.py
@@ -18,7 +18,8 @@ class PrepareTempest(OpenStackSitePPBaseTestCase):
             node.restore_snapshot('openstack')
             sleep(4)
             sync_time(ssh(node.ip_address, username='root', password='r00tme').sudo.ssh)
-        host = self.get_public_virtual_ip()
+        sleep(30) # mysql don't want to sync
+        auth_host = self.get_public_virtual_ip()
         remote = ssh(
             self.controller1.ip_address, username='root',
             password='r00tme').sudo.ssh
@@ -30,13 +31,13 @@ class PrepareTempest(OpenStackSitePPBaseTestCase):
                 password='r00tme').sudo.ssh
             tempest_mount_glance_images(remote_controller)
         keystone = keystoneclient.v2_0.client.Client(
-            username='admin', password='nova', tenant_name='openstack', auth_url=get_auth_url(host))
+            username='admin', password='nova', tenant_name='openstack', auth_url=get_auth_url(auth_host))
         tenant1 = keystone.tenants.create('tenant1')
         tenant2 = keystone.tenants.create('tenant2')
         keystone.users.create('tempest1','secret', 'tempest1@example.com', tenant_id=tenant1.id)
         keystone.users.create('tempest2','secret', 'tempest1@example.com', tenant_id=tenant2.id)
-        image_ref, image_ref_any = tempest_add_images(remote, host, 'openstack')
-        tempest_write_config(host, image_ref, image_ref_any)
+        image_ref, image_ref_any = tempest_add_images(remote, auth_host, 'openstack')
+        tempest_write_config(auth_host, image_ref, image_ref_any)
 
 if __name__ == '__main__':
     unittest.main()
