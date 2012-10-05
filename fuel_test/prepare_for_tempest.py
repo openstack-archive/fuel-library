5f4c3976c2d23b465cd1cc09678611a24ce5f69d
vic <vgalkin@mirantis.com>
2012-10-05 18:04:43 +0400

    Add logging

--- fuel_test/prepare_for_tempest.py
+++ fuel_test/prepare_for_tempest.py
@@ -17,13 +17,16 @@ class PrepareTempest(OpenStackSitePPBaseTestCase):
         for node in self.environment.nodes:
             node.restore_snapshot('openstack')
             sleep(4)
+            print 0
             sync_time(ssh(node.ip_address, username='root', password='r00tme').sudo.ssh)
         sleep(30) # mysql don't want to sync
         auth_host = self.get_public_virtual_ip()
+        print 1
         remote = ssh(
             self.controller1.ip_address, username='root',
             password='r00tme').sudo.ssh
         tempest_share_glance_images(remote, self.get_internal_network())
+        print 2
         for name in controllers[1:]:
             controller = self.environment.node[name]
             remote_controller = ssh(
