8aead89dddf79b8b0383433b91a1fba8fadd52a3
vic <vgalkin@mirantis.com>
2012-10-08 18:27:01 +0400

    Delete old snapshot

--- fuel_test/test_openstack.py
+++ fuel_test/test_openstack.py
@@ -8,7 +8,7 @@ class OpenStackCase(OpenStackSitePPBaseTestCase):
             [self.controller1,self.controller2,self.compute1,self.compute2],
             'puppet agent --test')
         for node in self.environment.nodes:
-            node.save_snapshot('openstack')
+            node.save_snapshot('openstack', force=True)
 
 if __name__ == '__main__':
     unittest.main()
