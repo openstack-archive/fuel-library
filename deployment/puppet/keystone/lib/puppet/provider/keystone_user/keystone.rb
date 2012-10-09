195ae1d10b11909c857287080e3c7879cabacd65
Dan Bode <dan@puppetlabs.com>
2012-10-09 00:29:12 -0700

    Better error handling when tenants need to be updated
    This resolves some bad error handling in the case
    where puppet tries to update the tentant.

--- deployment/puppet/keystone/lib/puppet/provider/keystone_user/keystone.rb
+++ deployment/puppet/keystone/lib/puppet/provider/keystone_user/keystone.rb
@@ -99,6 +99,10 @@ Puppet::Type.type(:keystone_user).provide(
     user_hash[resource[:name]][:tenant]
   end
 
+  def tenant=(value)
+    fail("tenant cannot be updated. Transition requested: #{user_hash[resource[:name]][:tenant]} -> #{value}")
+  end
+
   def email
     user_hash[resource[:name]][:email]
   end
@@ -108,7 +112,7 @@ Puppet::Type.type(:keystone_user).provide(
       "user-update",
       '--email', value,
       user_hash[resource[:name]][:id]
-    ) 
+    )
   end
 
   def id
@@ -121,7 +125,11 @@ Puppet::Type.type(:keystone_user).provide(
       hash = {}
       list_keystone_objects('user', 4).each do |user|
         tenantId = get_keystone_object('user', user[0], 'tenantId')
-        tenant   = tenantId == 'None' ? 'None' : get_keystone_object('tenant', tenantId, 'name')
+        if tenantId.nil? or tenantId == 'None' or tenantId.empty?
+          tenant = 'None'
+        else
+          tenant = get_keystone_object('tenant', tenantId, 'name')
+        end
         password = 'nil'
         hash[user[1]] = {
           :id          => user[0],
