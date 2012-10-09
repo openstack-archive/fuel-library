8bb1880eba3be8a22d3c9b71cbc55601dc13fe0f
Vladimir Kuklin <vkuklin@mirantis.com>
2012-10-09 17:02:31 +0400

    rewrite test

--- fuel_test/test_openstack_swift_compact.py
+++ fuel_test/test_openstack_swift_compact.py
@@ -7,7 +7,7 @@ class OpenStackSwiftCompactCase(OpenStackSwiftCompactSitePPBaseTestCase):
     def test_deploy_open_stack_swift_compact(self):
         self.validate(
             [self.controller1,self.controller2,self.controller3],
-            'puppet agent --test --tags "compact_controller"')
+            'puppet agent --test')
         self.validate(
             [self.compute1,self.compute2],
             'puppet agent --test')
@@ -16,14 +16,13 @@ class OpenStackSwiftCompactCase(OpenStackSwiftCompactSitePPBaseTestCase):
         results=[]
         for node in [self.controller1,self.controller2,self.controller3]:
             remote = ssh(node.ip_address, username='root', password='r00tme')
-            results.append(execute(remote.sudo.ssh, 'puppet agent --test --tags "role_swift_storage"'))
-            results.append(execute(remote.sudo.ssh, 'puppet agent --test --tags "role_swift_storage"'))
+            results.append(execute(remote.sudo.ssh, 'puppet agent --test'))
         for node in [self.controller1,self.controller2,self.controller3]:
             remote = ssh(node.ip_address, username='root', password='r00tme')
-            results.append(execute(remote.sudo.ssh, 'puppet agent --test --tags "role_swift_proxy"'))
+            results.append(execute(remote.sudo.ssh, 'puppet agent --test'))
         for node in [self.controller1,self.controller2,self.controller3]:
             remote = ssh(node.ip_address, username='root', password='r00tme')
-            results.append(execute(remote.sudo.ssh, 'puppet agent --test --tags "role_swift_storage"'))
+            results.append(execute(remote.sudo.ssh, 'puppet agent --test'))
 
 if __name__ == '__main__':
     unittest.main()
