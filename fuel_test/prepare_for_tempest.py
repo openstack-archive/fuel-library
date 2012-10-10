0a0919d39389f8bc0f48b29b2603eb0ae931c460
vic <vgalkin@mirantis.com>
2012-10-10 14:40:22 +0400

    prepare without shared storage

--- fuel_test/prepare_for_tempest.py
+++ fuel_test/prepare_for_tempest.py
@@ -13,20 +13,7 @@ class PrepareTempest(OpenStackSitePPBaseTestCase):
         self.environment = get_environment()
         self.controller1 = self.environment.node[controllers[0]]
 
-    def prepare_for_tempest(self):
-        for node in self.environment.nodes:
-            try:
-                node.stop()
-            except:
-                pass
-        for node in self.environment.nodes:
-            node.restore_snapshot('openstack')
-            sleep(4)
-#            sync_time(ssh(node.ip_address, username='root', password='r00tme').sudo.ssh)
-        auth_host = self.get_public_virtual_ip()
-        remote = ssh(
-            self.controller1.ip_address, username='root',
-            password='r00tme').sudo.ssh
+    def make_shared_storage(self, remote):
         tempest_share_glance_images(remote, self.get_internal_network())
         execute(remote, '/etc/init.d/iptables stop')
         sleep(5)
@@ -36,13 +23,49 @@ class PrepareTempest(OpenStackSitePPBaseTestCase):
                 controller.ip_address, username='root',
                 password='r00tme').sudo.ssh
             tempest_mount_glance_images(remote_controller)
-        keystone = retry(10,keystoneclient.v2_0.client.Client,
-                    username='admin', password='nova', tenant_name='openstack', auth_url=get_auth_url(auth_host))
-        tenant1 = retry(10, keystone.tenants.create, tenant_name = 'tenant1')
-        tenant2 = retry(10, keystone.tenants.create, tenant_name = 'tenant2')
-        retry(10, keystone.users.create,name='tempest1',password='secret', email='tempest1@example.com', tenant_id=tenant1.id)
-        retry(10, keystone.users.create,name='tempest2',password='secret', email='tempest1@example.com', tenant_id=tenant2.id)
-        image_ref, image_ref_any = tempest_add_images(remote, auth_host, 'openstack')
+
+    def safely_revert_nodes(self):
+        for node in self.environment.nodes:
+            try:
+                node.stop()
+            except:
+                pass
+        for node in self.environment.nodes:
+            node.restore_snapshot('openstack')
+            sleep(4)
+        #            sync_time(ssh(node.ip_address, username='root', password='r00tme').sudo.ssh)
+
+    def make_tempest_objects(self, auth_host, remote):
+        keystone = retry(10, keystoneclient.v2_0.client.Client,
+            username='admin', password='nova', tenant_name='openstack',
+            auth_url=get_auth_url(auth_host))
+        tenant1 = retry(10, keystone.tenants.create, tenant_name='tenant1')
+        tenant2 = retry(10, keystone.tenants.create, tenant_name='tenant2')
+        retry(10, keystone.users.create, name='tempest1', password='secret',
+            email='tempest1@example.com', tenant_id=tenant1.id)
+        retry(10, keystone.users.create, name='tempest2', password='secret',
+            email='tempest1@example.com', tenant_id=tenant2.id)
+        image_ref, image_ref_any = tempest_add_images(remote, auth_host,
+            'openstack')
+        return image_ref, image_ref_any
+
+    def prepare_for_tempest(self):
+        self.safely_revert_nodes()
+        auth_host = self.get_public_virtual_ip()
+        remote = ssh(
+            self.controller1.ip_address, username='root',
+            password='r00tme').sudo.ssh
+        self.make_shared_storage(remote)
+        image_ref, image_ref_any = self.make_tempest_objects(auth_host, remote)
+        tempest_write_config(auth_host, image_ref, image_ref_any)
+
+    def prepare_for_tempest_if_swift(self):
+        self.safely_revert_nodes()
+        auth_host = self.get_public_virtual_ip()
+        remote = ssh(
+            self.controller1.ip_address, username='root',
+            password='r00tme').sudo.ssh
+        image_ref, image_ref_any = self.make_tempest_objects(auth_host, remote)
         tempest_write_config(auth_host, image_ref, image_ref_any)
 
 if __name__ == '__main__':
