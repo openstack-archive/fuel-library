ff01a5c4b2a80b20bf65ad7791b74ffa06b9ec40
Dan Bode <dan@puppetlabs.com>
2012-10-14 11:52:08 -0700

    minor style refactor
    move reps to top of manifest

--- deployment/puppet/keystone/manifests/db/mysql.pp
+++ deployment/puppet/keystone/manifests/db/mysql.pp
@@ -42,6 +42,7 @@ class keystone::db::mysql(
   Class['mysql::server']       -> Class['keystone::db::mysql']
   Class['keystone::db::mysql'] -> Exec<|    title == 'keystone-manage db_sync' |>
   Class['keystone::db::mysql'] -> Service<| title == 'keystone' |>
+  Mysql::Db[$dbname] ~> Exec<| title == 'keystone-manage db_sync' |>
 
   require 'mysql::python'
 
@@ -61,8 +62,5 @@ class keystone::db::mysql(
       database => $dbname,
     }
   }
-
-
-  Mysql::Db[$dbname] ~> Exec<| title == 'keystone-manage db_sync' |>
 
 }
