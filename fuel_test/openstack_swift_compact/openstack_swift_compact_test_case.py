8d72e95532f108c9e4d7896748c93a968242bea8
Vladimir Kuklin <vkuklin@mirantis.com>
2012-10-16 19:11:13 +0400

    small refactor

--- fuel_test/openstack_swift_compact/openstack_swift_compact_test_case.py
+++ fuel_test/openstack_swift_compact/openstack_swift_compact_test_case.py
@@ -10,10 +10,23 @@ class OpenStackSwiftCompactTestCase(BaseTestCase):
 
     def setUp(self):
         super(OpenStackSwiftCompactTestCase, self).setUp()
-        self.write_openstack_sitepp(self.nodes.controllers[0],
-            self.nodes.controllers[1], self.nodes.controllers[2])
+        self.write_openstack_sitepp(self.nodes.controllers)
 
-    def write_openstack_sitepp(self, controller1, controller2, controller3):
+    def write_openstack_sitepp(self, controllers):
+        controller_public_addresses="{"
+        controller_internal_addresses="{"
+        for controller in controllers:
+            controller_public_addresses +="'%s' => '%s'" % (controller.name,controller.ip_address_by_network['public'])
+            if controller != controllers[-1]:
+                controller_public_addresses +=","
+            else:
+                controller_public_addresses +="}"
+        for controller in controllers:
+            controller_internal_addresses +="'%s' => '%s'" % (controller.name,controller.ip_address_by_network['internal'])
+            if controller != controllers[-1]:
+                controller_internal_addresses +=","
+            else:
+                controller_internal_addresses +="}"
         self.write_site_pp_manifest(
             root('fuel', 'deployment', 'puppet', 'openstack', 'examples',
                 'site_openstack_swift_compact.pp'),
@@ -21,23 +34,11 @@ class OpenStackSwiftCompactTestCase(BaseTestCase):
             public_virtual_ip="'%s'" % self.ci().get_public_virtual_ip(),
             floating_range="'%s'" % self.ci().get_floating_network(),
             fixed_range="'%s'" % self.ci().get_fixed_network(),
-            master_hostname="'%s'" % controller1.name,
+            master_hostname = "'%s'" % controllers[0].name,
+            controller_public_addresses = controller_public_addresses,
+            controller_internal_addresses = controller_internal_addresses,
+            controller_hostnames=["%s" % controller.name for controller in controllers],
             swift_proxy_address="'%s'" % self.ci().get_internal_virtual_ip(),
-            controller_public_addresses="{ '%s' => '%s', '%s' => '%s', '%s' => '%s' }"
-                                        % (
-                controller1.name, controller1.ip_address_by_network['public'],
-                controller2.name, controller2.ip_address_by_network['public'],
-                controller3.name, controller3.ip_address_by_network['public']),
-            controller_internal_addresses="{ '%s' => '%s', '%s' => '%s', '%s' => '%s' }"
-                                          % (
-                controller1.name, controller1.ip_address_by_network['internal'],
-                controller2.name, controller2.ip_address_by_network['internal'],
-                controller3.name,
-                controller3.ip_address_by_network['internal']),
-            controller_hostnames=[
-                "%s" % controller1.name,
-                "%s" % controller2.name,
-                "%s" % controller3.name],
             public_interface="'eth2'",
             internal_interface="'eth0'",
             internal_address="$ipaddress_eth0",
