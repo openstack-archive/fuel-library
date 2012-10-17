c25269e38d471fdf5b306ec4e415095bfea5edce
Vladimir Kuklin <vkuklin@mirantis.com>
2012-10-17 19:39:23 +0400

    Revert "add quantum module"
    This reverts commit 893e7fce8305a313c5a3e8442041e621f0004fef.

--- deployment/puppet/quantum/manifests/client.pp
+++
@@ -1,8 +0,0 @@
-class glance::client (
-  $ensure = present
-) {
-  package { "python-quantumclient":
-    name   => $::glance::params::client_package_name,
-    ensure => $ensure
-  }
-}
--- deployment/puppet/quantum/manifests/plugins/ovs/port.pp
+++
@@ -1,8 +0,0 @@
-define quantum::plugins::ovs::port {
-  $mapping = split($name, ":")
-  vs_port {$mapping[1]:
-    ensure => present,
-    bridge => $mapping[0]
-  }
-}
-
