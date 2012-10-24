4bc3368cfc95ea01c6509ac96198938d753d9e1a
Vladimir Kuklin <vkuklin@mirantis.com>
2012-10-24 14:10:58 +0400

    add qemu-kvm package name

--- deployment/puppet/nova/manifests/params.pp
+++ deployment/puppet/nova/manifests/params.pp
@@ -1,6 +1,7 @@
 # these parameters need to be accessed from several locations and
 # should be considered to be constant
 class nova::params {
+$libvirt_type_kvm = 'qemu-kvm'
 
   case $::osfamily {
     'RedHat': {
