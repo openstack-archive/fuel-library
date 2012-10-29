65a70a43e2e4740eef3f383254cb4dad6213d996
vic <vgalkin@mirantis.com>
2012-10-29 22:15:42 +0400

    Add resume and destroy

--- fuel_test/destroy.py
+++ fuel_test/destroy.py
@@ -5,18 +5,28 @@ import os
 
 def get_params():
     parser = argparse.ArgumentParser(description="Integration test suite")
-    parser.add_argument('command', choices=('setup', 'destroy'),
+    parser.add_argument('command',
+        choices=('setup', 'destroy', 'resume', 'suspend'),
         default='setup',
         help="command to execute")
     return parser.parse_args()
+
 
 def main():
     params = get_params()
     logging.getLogger().setLevel(logging.DEBUG)
 
+    name = os.environ.get('ENV_NAME', 'recipes')
+    environment = devops.load(name)
+
     if params.command == 'destroy':
-        name = os.environ.get('ENV_NAME', 'recipes')
-        devops.destroy(devops.load(name))
+        devops.destroy(environment)
+    if params.command == 'suspend':
+        for node in environment.nodes:
+            node.suspend()
+    if params.command == 'resume':
+        for node in environment.nodes:
+            node.suspend()
 
 if __name__ == "__main__":
     main()
