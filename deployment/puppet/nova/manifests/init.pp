231e6838a8f8d41bb7faac640c2fe13013219b00
Vladimir Kuklin <vkuklin@mirantis.com>
2012-10-29 18:57:31 +0400

    disable rabbit_host option

--- deployment/puppet/nova/manifests/init.pp
+++ deployment/puppet/nova/manifests/init.pp
@@ -103,11 +103,12 @@ class nova(
       source => 'puppet:///modules/nova/rmq-ha.patch'
     }
 
-    exec { 'patch-nova':
-      unless  => "/bin/grep x-ha-policy /usr/lib/${::nova::params::python_path}/nova/rpc/impl_kombu.py",
-      command => "/usr/bin/patch -p1 -d /usr/lib/${::nova::params::python_path}/nova </tmp/rmq-ha.patch",
-      require => [ [File['/tmp/rmq-ha.patch']],[Package['patch', 'python-nova']]], 
-    } -> exec { 'update-kombu':
+#    exec { 'patch-nova':
+#      unless  => "/bin/grep x-ha-policy /usr/lib/${::nova::params::python_path}/nova/rpc/impl_kombu.py",
+#      command => "/usr/bin/patch -p1 -d /usr/lib/${::nova::params::python_path}/nova </tmp/rmq-ha.patch",
+#      require => [ [File['/tmp/rmq-ha.patch']],[Package['patch', 'python-nova']]], 
+#    } ->
+ exec { 'update-kombu':
       command => "/usr/bin/easy_install pip; /usr/bin/pip uninstall -y kombu; /usr/bin/pip uninstall -y anyjson; /usr/bin/pip install kombu==2.4.7; /usr/bin/pip install anyjson==0.3.3; /usr/bin/pip install amqp"
     }
   }
@@ -183,16 +184,16 @@ class nova(
 
   nova_config { 'DEFAULT/auth_strategy': value => $auth_strategy }
 
-  if $rabbit_host {
-    nova_config { 'DEFAULT/rabbit_host': value => $rabbit_host }
-  }
-  else {
-     Nova_config <<| title == 'rabbit_host' |>>
-  }
+#  if $rabbit_host {
+#    nova_config { 'DEFAULT/rabbit_host': value => $rabbit_host }
+#  }
+#  else {
+#     Nova_config <<| title == 'rabbit_host' |>>
+#  }
 
 
   if $rabbit_nodes {
-    nova_config { 'rabbit_addresses': value => inline_template("<%= @rabbit_nodes.map {|x| x+':5672'}.join ',' %>") }
+    nova_config { 'DEFAULT/rabbit_addresses': value => inline_template("<%= @rabbit_nodes.map {|x| x+':5672'}.join ',' %>") }
   } else {
     Nova_config <<| title == 'rabbit_addresses' |>>
   }
