c454edd1d8d31f3dd8be051354bffc9fe95b0e16
vic <vgalkin@mirantis.com>
2012-10-30 16:15:14 +0400

    Customize log parsing for puppet 3.0

--- fuel_test/settings.py
+++ fuel_test/settings.py
@@ -3,3 +3,32 @@ import os
 BASE_IMAGE = os.environ.get('BASE_IMAGE',
     '/var/lib/libvirt/images/vgalkin_centos-base.qcow2')
 OS_FAMILY = os.environ.get('OS_FAMILY', "centos")
+PUPPET_GEN = os.environ.get('PUPPET_GEN', "2")
+
+PUPPET_VERSIONS = {
+    'centos': {
+        "2": '2.7.19-1.el6',
+        "3": '3.0.1-1.el6',
+    },
+    'ubuntu': {
+        "2": '2.7.19-1puppetlabs1',
+        "3": '3.0.1-1puppetlabs1'
+    },
+}
+
+ERROR_PREFIXES = {
+    "2": "err: ",
+    "3": "Error: ",
+}
+
+ERROR_PREFIX = ERROR_PREFIXES.get(PUPPET_GEN)
+
+WARNING_PREFIXES = {
+    "2": "warning: ",
+    "3": "Warning: ",
+}
+
+WARNING_PREFIX = WARNING_PREFIXES.get(PUPPET_GEN)
+
+def get_puppet_version():
+    return PUPPET_VERSIONS.get(OS_FAMILY).get(PUPPET_GEN)
