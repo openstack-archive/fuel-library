eda2e7cb2fbebae439b0977b97ee0e99cc7d7785
Dmitry Pyzhov <dpyzhov@mirantis.com>
2012-10-30 17:27:51 +0400

    Minor fixes in site_simple.pp

--- deployment/puppet/openstack/examples/site_simple.pp
+++ deployment/puppet/openstack/examples/site_simple.pp
@@ -1,11 +1,11 @@
-$floating_range          = '10.0.1.0/28'
+$floating_range          = '10.0.0.128/28'
 $fixed_range             = '10.0.2.0/28'
 $controller_hostnames    = ['fuel-01']
 $controller_ip           = '10.0.0.101'
 $public_interface        = 'eth1'
 $internal_address        = $ipaddress_eth1
 $private_interface       = 'eth2'
-$multi_host              = false
+$multi_host              = true
 $network_manager         = 'nova.network.manager.FlatDHCPManager'
 $verbose                 = true
 $auto_assign_floating_ip = false
@@ -20,7 +20,7 @@ $nova_db_password        = 'nova'
 $nova_user_password      = 'nova'
 $rabbit_password         = 'nova'
 $rabbit_user             = 'nova'
-$glance_backend          ='file'
+$glance_backend          = 'file'
 $public_address          = '10.0.0.101'
 case $::osfamily {
   'RedHat': {
