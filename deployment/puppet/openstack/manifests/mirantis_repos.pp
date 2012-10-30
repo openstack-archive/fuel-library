356c1b9b99e56b71f0bb40b50474ea48fce610f0
Vladimir Kuklin <vkuklin@mirantis.com>
2012-10-30 13:54:57 +0400

    add rabbitmq key

--- deployment/puppet/openstack/manifests/mirantis_repos.pp
+++ deployment/puppet/openstack/manifests/mirantis_repos.pp
@@ -20,7 +20,7 @@ class openstack::mirantis_repos {
           repo_name  => 'openstack-epel-fuel',
           #      location   => $repo_baseurl,
           mirrorlist => $mirrorlist,
-          key_source => "https://fedoraproject.org/static/0608B895.txt\n  http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-6",
+          key_source => "https://fedoraproject.org/static/0608B895.txt\n  http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-6\n http://download.mirantis.com/epel-fuel/rabbit.key",
           stage      => 'openstack-custom-repo',
           gpgcheck	=> '0'
         }
