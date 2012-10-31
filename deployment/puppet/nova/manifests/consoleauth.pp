95b15ac14ee77709f55dedbb2e231c9f4763c60f
Vladimir Kuklin <vkuklin@mirantis.com>
2012-10-31 14:34:31 +0400

    add consoleauth patch

--- deployment/puppet/nova/manifests/consoleauth.pp
+++ deployment/puppet/nova/manifests/consoleauth.pp
@@ -11,6 +11,18 @@ class nova::consoleauth(
 
   include nova::params
 
+    file { "/tmp/consoleauth-memcached.patch":
+      ensure => present,
+      source => 'puppet:///modules/nova/consoleauth-memcached.patch'
+    }
+    exec { 'patch-consoleauth':
+      path    => ["/usr/bin", "/usr/sbin"],
+      onlyif  => "patch -t -N -p1 --dry-run  -d /usr/lib/${::nova::params::python_path}/nova/consoleauth/ --reject-file=/dev/null < /tmp/consoleauth-memcached.patch",
+      command => "/usr/bin/patch -p1 -d /usr/lib/${::nova::params::python_path}/nova/consoleauth </tmp/consoleauth-memcached.patch",
+      require => [ [File['/tmp/consoleauth-memcached.patch']],[Package['patch', 'python-nova']]], 
+    } ->
+
+
   nova::generic_service { 'consoleauth':
     enabled        => $enabled,
     package_name   => $::nova::params::consoleauth_package_name,
