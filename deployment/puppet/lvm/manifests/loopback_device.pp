fafd9b47400b9cceeffe14aa34274ee62e4b8153
Michael Polenchuk <mpolenchuk@mirantis.com>
2012-10-31 17:11:56 +0400

    Refactor module (split physical/loopback devices)

---
+++ deployment/puppet/lvm/manifests/loopback_device.pp
@@ -0,0 +1,28 @@
+
+class lvm::loopback_device(
+  $volume_name     = 'nova-volumes',
+  $size            = '2G',
+  $loopback_device = '/dev/loop2'
+) {
+
+  Exec {
+    cwd => '/tmp/',
+  }
+
+  exec { "/bin/dd if=/dev/zero of=${volume_name} bs=1 count=0 seek=${size}":
+    unless => "/sbin/vgdisplay ${volume_name}"
+  } ~>
+
+  exec { "/sbin/losetup ${loopback_device} ${volume_name}":
+    refreshonly => true,
+  } 
+
+  # exec { "/sbin/pvcreate ${loopback_device}":
+  #   refreshonly => true,
+  # } ~>
+
+  # exec { "/sbin/vgcreate ${volume_name} ${loopback_device}":
+  #   refreshonly => true,
+  # }
+
+}
