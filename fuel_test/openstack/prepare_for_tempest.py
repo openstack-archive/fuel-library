77ab80414d3e326f9656a5ab131cadcbb104f42f
vic <vgalkin@mirantis.com>
2012-11-02 22:39:28 +0400

    Settings for admin credentials

--- fuel_test/openstack/prepare_for_tempest.py
+++ fuel_test/openstack/prepare_for_tempest.py
@@ -2,6 +2,7 @@ from devops.helpers import ssh
 import unittest
 from fuel_test.helpers import safety_revert_nodes, make_shared_storage, make_tempest_objects, tempest_write_config, tempest_build_config_essex, tempest_build_config_folsom
 from fuel_test.openstack.openstack_test_case import OpenStackTestCase
+from fuel_test.settings import ADMIN_USERNAME, ADMIN_PASSWORD, ADMIN_TENANT_ESSEX, ADMIN_TENANT_FOLSOM
 
 
 class PrepareOpenStackForTempest(OpenStackTestCase):
@@ -20,7 +21,12 @@ class PrepareOpenStackForTempest(OpenStackTestCase):
             self.ci().nodes().controllers[1:],
             self.ci().get_internal_network()
         )
-        image_ref, image_ref_alt = make_tempest_objects(auth_host, remote)
+        image_ref, image_ref_alt = make_tempest_objects(
+            auth_host, remote,
+            username=ADMIN_USERNAME,
+            password=ADMIN_PASSWORD,
+            tenant_name=ADMIN_TENANT_ESSEX,
+        )
         tempest_write_config(tempest_build_config_essex(auth_host, image_ref, image_ref_alt))
 
     def prepare_for_tempest_folsom(self):
@@ -38,7 +44,11 @@ class PrepareOpenStackForTempest(OpenStackTestCase):
         compute_db_uri = 'mysql://nova:nova@%s/nova' % self.ci().get_internal_virtual_ip()
 
         image_ref, image_ref_alt = make_tempest_objects(
-            auth_host, remote, tenant_name='admin')
+            auth_host, remote,
+            username=ADMIN_USERNAME,
+            password=ADMIN_PASSWORD,
+            tenant_name=ADMIN_TENANT_FOLSOM,
+        )
         tempest_write_config(
             tempest_build_config_folsom(
                 host=auth_host,
