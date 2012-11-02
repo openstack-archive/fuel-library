62a305712e22b297bf0d3d293dde9978da3ecae1
Vladimir Kuklin <vkuklin@mirantis.com>
2012-11-02 17:57:26 +0400

    add compact scenario example

--- deployment/puppet/openstack/examples/site_openstack_swift_compact.pp
+++ deployment/puppet/openstack/examples/site_openstack_swift_compact.pp
@@ -1,11 +1,12 @@
-$internal_virtual_ip = '10.0.113.253'
-$public_virtual_ip = '10.0.57.253'
+$internal_virtual_ip = '10.0.126.253'
+$public_virtual_ip = '10.0.215.253'
 $master_hostname = 'fuel-01'
-$controller_public_addresses = { 'fuel-01' => '10.0.57.3', 'fuel-02' => '10.0.57.4', 'fuel-03' => '10.0.57.5' }
-$controller_internal_addresses = { 'fuel-01' => '10.0.113.3', 'fuel-02' => '10.0.113.4', 'fuel-03' => '10.0.113.5' }
+$swift_master = $master_hostname
+$controller_public_addresses = {'fuel-01' => '10.0.215.3','fuel-02' => '10.0.215.4','fuel-03' => '10.0.215.5'}
+$controller_internal_addresses = {'fuel-01' => '10.0.126.3','fuel-02' => '10.0.126.4','fuel-03' => '10.0.126.5'}
 $swift_proxies = $controller_internal_addresses 
-$floating_range = '10.0.57.128/27'
-$fixed_range = '10.0.202.128/27'
+$floating_range = '10.0.215.128/27'
+$fixed_range = '10.0.198.128/27'
 $controller_hostnames = ['fuel-01', 'fuel-02', 'fuel-03']
 $public_interface = 'eth2'
 $internal_interface = 'eth0'
@@ -30,7 +31,7 @@ $swift_user_password  = 'swift_pass'
 # swift specific configurations
 $swift_shared_secret  = 'changeme'
 $swift_local_net_ip   = $ipaddress_eth0
-$swift_proxy_address = '10.0.113.253'
+$swift_proxy_address = '10.0.126.253'
 $controller_node_public = $internal_virtual_ip 
 $glance_backend         = 'swift'
 $openstack_version = {
@@ -89,28 +90,28 @@ class compact_controller {
 
 
 node /fuel-01/ {
-  include compact_controller
-  include swift_base
+  class { compact_controller: }
   $swift_zone = 1
-  include role_swift_storage
-  include role_swift_proxy
+  class { openstack::swift::storage-node: swift_zone => $swift_zone }
+  class { openstack::swift::proxy: swift_proxies => $swift_proxies, swift_master => $swift_master, controller_node_address =>  $internal_virtual_ip }
 }
+
 
 node /fuel-02/ {
-  include compact_controller
-  include swift_base
+  class { compact_controller: }
   $swift_zone = 2
-  include role_swift_storage
-  include role_swift_proxy
+  class { openstack::swift::storage-node: swift_zone => $swift_zone }
+  class { openstack::swift::proxy: swift_proxies => $swift_proxies, swift_master => $swift_master, controller_node_address =>  $internal_virtual_ip }
 }
 
+
 node /fuel-03/ {
-  include compact_controller
-  include swift_base
+  class { compact_controller: }
   $swift_zone = 3
-  include role_swift_storage
-  include role_swift_proxy
+  class { openstack::swift::storage-node: swift_zone => $swift_zone }
+  class { openstack::swift::proxy: swift_proxies => $swift_proxies, swift_master => $swift_master, controller_node_address =>  $internal_virtual_ip }
 }
+
 
 node /fuel-0[45]/ {
     class { 'openstack::compute':
@@ -134,141 +135,6 @@ node /fuel-0[45]/ {
       cache_server_ip         => $controller_hostnames,
       service_endpoint	=> $internal_virtual_ip,
     }
-}
-class role_swift_storage {
-
-
-  # create xfs partitions on a loopback device and mount them
-  swift::storage::loopback { ['dev1', 'dev2']:
-    base_dir     => '/srv/loopback-device',
-    mnt_base_dir => '/srv/node',
-    seek         => '1048756',
-    require      => Class['swift'],
-  }
-
-  # install all swift storage servers together
-  class { 'swift::storage::all':
-    storage_local_net_ip => $swift_local_net_ip,
-    swift_zone => $swift_zone,
-  }
-
-  # collect resources for synchronizing the ring databases
-  Swift::Ringsync<<||>>
-
-}
-
-class swift_base {
-  class { 'ssh::server::install': }
-
-  class { 'swift':
-    # not sure how I want to deal with this shared secret
-    swift_hash_suffix => 'swift_shared_secret',
-    package_ensure    => latest,
-  }
-
-
-}
-
-class role_swift_proxy {
-
-  # curl is only required so that I can run tests
-  package { 'curl': ensure => present }
-
-  class { 'memcached':
-    listen_ip => '127.0.0.1',
-  }
-
-  # specify swift proxy and all of its middlewares
-  class { 'swift::proxy':
-    proxy_local_net_ip => $swift_local_net_ip,
-    pipeline           => [
-      'catch_errors',
-      'healthcheck',
-      'cache',
-      'ratelimit',
-      'swift3',
-#      's3token',
-      'authtoken',
-      'keystone',
-      'proxy-server'
-    ],
-    account_autocreate => true,
-    # TODO where is the  ringbuilder class?
-#    require            => Class['swift::ringbuilder'],
-  }
-
-  # configure all of the middlewares
-  class { [
-    'swift::proxy::catch_errors',
-    'swift::proxy::healthcheck',
-    'swift::proxy::swift3',
-  ]: }
-  class { 'swift::proxy::ratelimit':
-    clock_accuracy         => 1000,
-    max_sleep_time_seconds => 60,
-    log_sleep_time_seconds => 0,
-    rate_buffer_seconds    => 5,
-    account_ratelimit      => 0
-  }
-#  class { 'swift::proxy::s3token':
-    # assume that the controller host is the swift api server
-#    auth_host     => $controller_node_public,
-#    auth_port     => '35357',
-#  }
-   $cache_addresses =  inline_template("<%= @swift_proxies.keys.uniq.sort.collect {|ip| ip + ':11211' }.join ',' %>")
-   class { 'swift::proxy::cache':
-        memcache_servers => split($cache_addresses,',')
-        }
-  class { 'swift::proxy::keystone':
-    operator_roles => ['admin', 'SwiftOperator'],
-  }
-  class { 'swift::proxy::authtoken':
-    admin_user        => $admin_user,
-    admin_tenant_name => 'openstack',
-    admin_password    => $admin_password,
-    # assume that the controller host is the swift api server
-    auth_host         => $controller_node_public,
-  }
-
-  # collect all of the resources that are needed
-  # to balance the ring
-
-
-  if $::hostname==$master_hostname {
-     Ring_object_device <<| |>>
-     Ring_container_device <<| |>>
-     Ring_account_device <<| |>>
-    
-  # create the ring
-  class { 'swift::ringbuilder':
-    # the part power should be determined by assuming 100 partitions per drive
-    part_power     => '18',
-    replicas       => '3',
-    min_part_hours => 1,
-    require        => Class['swift'],
-  }
-  Class['swift::ringbuilder'] -> Class['swift::proxy']
-
-  # sets up an rsync db that can be used to sync the ring DB
-  
-  class { 'swift::ringserver':
-    local_net_ip => $swift_local_net_ip,
-  }
-    # exports rsync gets that can be used to sync the ring files
-    @@swift::ringsync { ['account', 'object', 'container']:
-      ring_server => $swift_local_net_ip
-    }
-  }
-  else {
-    Package["swift"] -> Class["swift::proxy"] 
-    Swift::Ringsync<<||>>
-    Swift::Ringsync<| |> ~> Service["swift-proxy"]
-  }
-
-  # deploy a script that can be used for testing
-  file { '/tmp/swift_keystone_test.rb':
-    source => 'puppet:///modules/swift/swift_keystone_test.rb'
-  }
 }
 
 
