993fcec09c6547db2b51cf71da930aa9f769c9a3
Vladimir Kuklin <vkuklin@mirantis.com>
2012-11-02 17:52:58 +0400

    check parted package for duplicates

--- deployment/puppet/nova/manifests/utilities.pp
+++ deployment/puppet/nova/manifests/utilities.pp
@@ -1,6 +1,10 @@
 # unzip swig screen parted curl euca2ools - extra packages
 class nova::utilities {
-  package { ['unzip', 'screen', 'parted', 'curl', 'euca2ools']:
+  package { ['unzip', 'screen', 'curl', 'euca2ools']:
     ensure => present
   }
+  if !(defined(Package['parted'])) {
+    package {"parted": ensure => 'present' }
+  }
+
 }
