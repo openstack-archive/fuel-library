0bce98be9db7bfbbc015c679c5d189e09e5cb372
Vladimir Kuklin <vkuklin@mirantis.com>
2012-11-06 20:55:05 +0400

    Refactor openstack::cinder
        1. call all cinder classes from within openstack::cinder
        2. add required parameters to openstack::cinder

--- deployment/puppet/openstack/manifests/cinder.pp
+++ deployment/puppet/openstack/manifests/cinder.pp
@@ -1,25 +1,53 @@
 class openstack::cinder(
   $sql_connection,
   $rabbit_password,
-  $rabbit_host     = '127.0.0.1',
-  $volume_group    = 'nova-volumes',
-  $enabled         = true
+  $rabbit_host     = false,
+  $rabbit_nodes    = ['127.0.0.1']
+  $volume_group    = 'cinder-volumes',
+  $physical_volume = undef,
+  $manage_volumes  = false,
+  $enabled         = true,
+  $purge_cinder_config = true,
+  $auth_host          = '127.0.0.1',
+  $bind_host          = '127.0.0.1',
 ) {
 
+  if ($purge_nova_config) {
+    resources { 'cinder_config':
+      purge => true,
+    }   
+  }
+
+  if $rabbit_nodes {
+    $rabbit_hosts = inline_template("<%= @rabbit_nodes.map {|x| x + ':5672'}.join ',' %>")
+  }
   class { 'cinder::base':
+    package_ensure => $::openstack_version['cinder'],
     rabbit_password => $rabbit_password,
-    rabbit_host     => $rabbit_host,
+    rabbit_hosts     => $rabbit_hosts,
     sql_connection  => $sql_connection,
     verbose         => $verbose,
   }
+  class { 'cinder::api':
+      package_ensure => $::openstack_version['cinder'],
+      keystone_auth_host => $auth_host,
+      keystone_password => $cinder_user_password,
+  }   
 
-  # Install / configure nova-volume
-  class { 'cinder::volume':
-    enabled => $enabled,
-  }
-  if $enabled {
+
+
+if $manage_volumes {
+
+    class { 'cinder::volume':
+      package_ensure => $::openstack_version['cinder'],
+      enabled        => true,
+    }   
+
     class { 'cinder::volume::iscsi':
-      volume_group     => $volume_group,
-    }
+      iscsi_ip_address => $internal_address,
+      physical_volume  => $nv_physical_volume,
+    } 
   }
 }
+
+  
