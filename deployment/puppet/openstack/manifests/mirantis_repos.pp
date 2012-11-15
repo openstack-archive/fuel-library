8eaf0c18d99648e3784ac53c260f8eef1e00ef81
Michael Polenchuk <mpolenchuk@mirantis.com>
2012-11-15 12:35:12 +0400

    Added EPEL repository for missing packages

--- deployment/puppet/openstack/manifests/mirantis_repos.pp
+++ deployment/puppet/openstack/manifests/mirantis_repos.pp
@@ -1,7 +1,7 @@
 #
 class openstack::mirantis_repos (
   $type        = 'external',
-  $enable_epel = false
+  $enable_epel = true
 ) {
   case $::osfamily {
     'Debian': {
@@ -33,13 +33,21 @@ class openstack::mirantis_repos (
       }
 
       if $enable_epel {
-        yumrepo { 'epel-testing':
-          descr          => 'Extra Packages for Enterprise Linux 6 - Testing - $basearch',
-          mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=testing-epel6&arch=$basearch',
+        Yumrepo {
           failovermethod => 'priority',
           gpgkey         => 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6',
           gpgcheck       => 1,
           enabled        => 1,
+        }
+
+        yumrepo { 'epel-testing':
+          descr      => 'Extra Packages for Enterprise Linux 6 - Testing - $basearch',
+          mirrorlist => 'http://mirrors.fedoraproject.org/metalink?repo=testing-epel6&arch=$basearch',
+        }
+
+        yumrepo { 'epel':
+          descr      => 'Extra Packages for Enterprise Linux 6 - $basearch',
+          mirrorlist => 'http://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
         }
       }
 
