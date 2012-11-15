096d674173f642f86a7ed32ee67407bbf8ae6af1
Vladimir Kuklin <vkuklin@mirantis.com>
2012-11-15 16:35:16 +0400

    clean up obsolete try/except in openstack test

--- fuel_test/openstack/openstack_test_case.py
+++ fuel_test/openstack/openstack_test_case.py
@@ -15,60 +15,33 @@ class OpenStackTestCase(BaseTestCase):
             self.nodes.controllers[1])
 
     def write_openstack_sitepp(self, controller1, controller2):
-        try:
-            self.write_site_pp_manifest(
-                root('fuel', 'deployment', 'puppet', 'openstack', 'examples',
-                    'site.pp'),
-                internal_virtual_ip="'%s'" % self.ci().get_internal_virtual_ip(),
-                public_virtual_ip="'%s'" % self.ci().get_public_virtual_ip(),
-                floating_range="'%s'" % self.ci().get_floating_network(),
-                fixed_range="'%s'" % self.ci().get_fixed_network(),
-                master_hostname="'%s'" % controller1.name,
-                controller_public_addresses="{ '%s' => '%s', '%s' => '%s' }"
-                                            % (
-                    controller1.name, controller1.ip_address_by_network['public'],
-                    controller2.name, controller2.ip_address_by_network['public']),
-                controller_internal_addresses="{ '%s' => '%s', '%s' => '%s' }"
-                                              % (
-                    controller1.name, controller1.ip_address_by_network['internal'],
-                    controller2.name,
-                    controller2.ip_address_by_network['internal']),
-                controller_hostnames=[
-                    "%s" % controller1.name,
-                    "%s" % controller2.name],
-                public_interface="'eth2'",
-                internal_interface="'eth0'",
-                internal_address="$ipaddress_eth0",
-                private_interface="'eth1'",
-                nv_physical_volume= ["/dev/vdb"]
-            )
-        except:
-            self.write_site_pp_manifest(
-                root('fuel', 'deployment', 'puppet', 'openstack', 'examples',
-                    'site.pp'),
-                internal_virtual_ip="'%s'" % self.ci().get_internal_virtual_ip(),
-                public_virtual_ip="'%s'" % self.ci().get_public_virtual_ip(),
-                floating_range="'%s'" % self.ci().get_floating_network(),
-                fixed_range="'%s'" % self.ci().get_fixed_network(),
-                master_hostname="'%s'" % controller1.name,
-                controller_public_addresses="{ '%s' => '%s', '%s' => '%s' }"
-                                            % (
-                    controller1.name, controller1.ip_address_by_network['public'],
-                    controller2.name, controller2.ip_address_by_network['public']),
-                controller_internal_addresses="{ '%s' => '%s', '%s' => '%s' }"
-                                              % (
-                    controller1.name, controller1.ip_address_by_network['internal'],
-                    controller2.name,
-                    controller2.ip_address_by_network['internal']),
-                controller_hostnames=[
-                    "%s" % controller1.name,
-                    "%s" % controller2.name],
-                public_interface="'eth2'",
-                internal_interface="'eth0'",
-                internal_address="$ipaddress_eth0",
-                private_interface="'eth1'",
-            )
-
+        self.write_site_pp_manifest(
+            root('fuel', 'deployment', 'puppet', 'openstack', 'examples',
+                 'site.pp'),
+            internal_virtual_ip="'%s'" % self.ci().get_internal_virtual_ip(),
+            public_virtual_ip="'%s'" % self.ci().get_public_virtual_ip(),
+            floating_range="'%s'" % self.ci().get_floating_network(),
+            fixed_range="'%s'" % self.ci().get_fixed_network(),
+            master_hostname="'%s'" % controller1.name,
+            controller_public_addresses="{ '%s' => '%s', '%s' => '%s' }"
+            % (
+                controller1.name, controller1.ip_address_by_network['public'],
+                controller2.name, controller2.ip_address_by_network['public']),
+            controller_internal_addresses="{ '%s' => '%s', '%s' => '%s' }"
+            % (
+                controller1.name, controller1.ip_address_by_network['internal'],
+                controller2.name,
+                controller2.ip_address_by_network['internal']),
+            controller_hostnames=[
+                "%s" % controller1.name,
+                "%s" % controller2.name],
+            public_interface="'eth2'",
+            internal_interface="'eth0'",
+            internal_address="$ipaddress_eth0",
+            private_interface="'eth1'",
+            nv_physical_volume= ["/dev/vdb"]
+        )
+        
 if __name__ == '__main__':
     unittest.main()
 
