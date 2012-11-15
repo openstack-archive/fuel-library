5cf5d983f6a556ee94b4739186ed334007384f93
Vladimir Kuklin <vkuklin@mirantis.com>
2012-11-15 17:16:41 +0400

    Resolve conflicts

---
+++ fuel_test/openstack_simple/openstack_simple_test_case.py
@@ -0,0 +1,36 @@
+import unittest
+from fuel_test.base_test_case import BaseTestCase
+from fuel_test.ci.ci_openstack_simple import CiOpenStackSimple
+from fuel_test.root import root
+
+class OpenStackSimpleTestCase(BaseTestCase):
+    def ci(self):
+        if not hasattr(self, '_ci'):
+            self._ci = CiOpenStackSimple()
+        return self._ci
+
+    def setUp(self):
+        super(OpenStackSimpleTestCase, self).setUp()
+        self.write_openstack_sitepp(self.nodes.controllers[0])
+
+    def write_openstack_sitepp(self, controller):
+            self.write_site_pp_manifest(
+                root('fuel', 'deployment', 'puppet', 'openstack', 'examples',
+                    'site_simple.pp'),
+                floating_network_range="'%s'" % self.ci().get_floating_network(),
+                fixed_network_range="'%s'" % self.ci().get_fixed_network(),
+                public_interface="'eth2'",
+                internal_interface="'eth0'",
+                internal_address="$ipaddress_eth0",
+                private_interface="'eth1'",
+                controller_node_address=controller.ip_address_by_network['internal'],
+                controller_node_public=controller.ip_address_by_network['public'],
+                nv_physical_volume= ["/dev/vdb"]
+            )
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
