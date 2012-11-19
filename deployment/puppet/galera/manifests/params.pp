d63228de345d4b1b3dd1cbceae7732901a7be6da
Vladimir Kuklin <vkuklin@mirantis.com>
2012-11-19 22:57:04 +0400

    Check galera package is installed on debian
    due to debian naming conventions mysql-server package name is mysql-server-wsrep

--- deployment/puppet/galera/manifests/params.pp
+++ deployment/puppet/galera/manifests/params.pp
@@ -13,6 +13,7 @@ class galera::params {
       # $mysql_client_package = 'mysql'
       # $mysql_version        = '5.1/5.1.62-23.4'
       $mysql_server_package = 'MySQL-server-5.5.23_wsrep_23.6-1.rhel5.x86_64.rpm'
+      $mysql_server_name    = 'MySQL-server'
       $galera_package       = 'galera-23.2.2-1.rhel5.x86_64.rpm'
       $libgalera_prefix     = '/usr/lib64'
     }
@@ -23,6 +24,7 @@ class galera::params {
       # $mysql_client_package = 'mysql-client'
       # $mysql_version        = '5.5/5.5.23-23.6'
       $mysql_server_package = 'mysql-server-wsrep-5.5.23-23.6-amd64.deb'
+      $mysql_server_name    = 'mysql-server-wsrep'
       $galera_package       = 'galera-23.2.2-amd64.deb'
       $libgalera_prefix     = '/usr/lib'
     }
