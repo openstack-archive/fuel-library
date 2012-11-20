a03ca76774dacf56d71b92cc52a1416c5b9eb80b
Roman Alekseenkov <ralekseenkov@mirantis.com>
2012-11-20 15:48:11 -0800

    use external repos for CentOS by default

--- deployment/puppet/openstack/manifests/mirantis_repos.pp
+++ deployment/puppet/openstack/manifests/mirantis_repos.pp
@@ -1,7 +1,9 @@
 #
 class openstack::mirantis_repos (
-  $type        = 'internal',
+  # DO NOT change this value to 'internal'. all our customers are relying on external repositories
+  $type        = 'external',
   $enable_epel = false
+
 ) {
   case $::osfamily {
     'Debian': {
@@ -28,15 +30,12 @@ class openstack::mirantis_repos (
 #        }
     }
     'RedHat': {
-      #$repo_baseurl='http://download.mirantis.com/epel-fuel-folsom'
-
       #added internal/external network mirror
       $mirrorlist="http://download.mirantis.com/epel-fuel-folsom/mirror.${type}.list"
 
       class { 'openstack::repo::yum':
         descr      => 'Mirantis OpenStack Custom Packages',
         repo_name  => 'openstack-epel-fuel',
-        #      location   => $repo_baseurl,
         mirrorlist => $mirrorlist,
         key_source => "https://fedoraproject.org/static/0608B895.txt\n  http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-6\n http://download.mirantis.com/epel-fuel-folsom/rabbit.key",
         stage      => 'openstack-custom-repo',
