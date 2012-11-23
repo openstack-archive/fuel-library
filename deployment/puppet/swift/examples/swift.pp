9d0caf11819cc755841ea4a0157b253c6cb94d50
vic <vgalkin@mirantis.com>
2012-11-23 18:05:44 +0400

    Fix swift test log_verbose  => verbose, log_debug    => debug,

--- deployment/puppet/swift/examples/swift.pp
+++ deployment/puppet/swift/examples/swift.pp
@@ -61,8 +61,8 @@ class testkeystone {
     # we are binding keystone on all interfaces
     # the end user may want to be more restrictive
     bind_host    => '0.0.0.0',
-    log_verbose  => $verbose,
-    log_debug    => $verbose,
+    verbose  => $verbose,
+    debug    => $verbose,
     catalog_type => 'sql',
   }
 
