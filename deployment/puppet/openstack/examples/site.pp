a9f0755adb033ea74b6650291a3667a594343fe6
vic <vgalkin@mirantis.com>
2012-11-23 21:42:45 +0400

    Quantum to each site.pp

--- deployment/puppet/openstack/examples/site.pp
+++ deployment/puppet/openstack/examples/site.pp
@@ -87,6 +87,8 @@ node /fuel-0[12]/ {
       quantum                 => $quantum,
       quantum_user_password   => $quantum_user_password,
       quantum_db_password     => $quantum_db_password,
+      quantum_db_user         => $quantum_db_user,
+      quantum_db_dbname       => $quantum_db_dbname,
       cinder                  => $cinder,
       manage_volumes          => $manage_volumes,
       nv_physical_volume      => $nv_physical_volume,
