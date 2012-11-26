4314313777cf1219573f331a7009075d1c897fb4
manashkin <manashkin@mirantis.com>
2012-11-26 22:42:03 +0400

    Added guestmount to dependancies.

--- deployment/puppet/nova/manifests/utilities.pp
+++ deployment/puppet/nova/manifests/utilities.pp
@@ -1,6 +1,6 @@
 # unzip swig screen parted curl euca2ools - extra packages
 class nova::utilities {
-  package { ['unzip', 'screen', 'parted', 'curl', 'euca2ools']:
+  package { ['unzip', 'screen', 'parted', 'curl', 'euca2ools', 'guestmount']:
     ensure => present
   }
 }
