e2b048aa2de02a3bf1421c129fa3a23cbb5739aa
Vladimir Kuklin <vkuklin@mirantis.com>
2012-12-04 18:11:24 +0400

    fix [FUEL-216]

--- deployment/puppet/nova/manifests/utilities.pp
+++ deployment/puppet/nova/manifests/utilities.pp
@@ -1,11 +1,24 @@
 # unzip swig screen parted curl euca2ools - extra packages
 class nova::utilities {
   include nova::params
-  package { ['unzip', 'screen', 'curl', 'euca2ools', $::nova::params::guestmount_package_name]:
+  package { ['unzip', 'screen', 'curl', 'euca2ools']:
     ensure => present
   }
   if !(defined(Package['parted'])) {
     package {"parted": ensure => 'present' }
   }
-
+  case $::osfamily {
+ 'Debian': {
+      file { "/tmp/guestfs.seed":   
+        ensure => present,
+        source => 'puppet:///modules/nova/guestfs.seed'
+      }->
+      package {"$::nova::params::guestmount_package_name": ensure => present, responsefile=>"/tmp/guestfs.seed"}
+ }
+'RedHat': {
+  package {"$::nova::params::guestmount_package_name": ensure => present}
 }
+}
+}
+
+
