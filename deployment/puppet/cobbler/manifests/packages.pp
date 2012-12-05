6cd997f6e4f4e536f28fd2c308966666613b9b05
vic <vgalkin@mirantis.com>
2012-12-05 18:25:47 +0400

    Fix django version for cobbler

--- deployment/puppet/cobbler/manifests/packages.pp
+++ deployment/puppet/cobbler/manifests/packages.pp
@@ -6,12 +6,16 @@ class cobbler::packages {
       $cobbler_web_package = "cobbler-web"
       $dnsmasq_package = "dnsmasq"
       $cobbler_additional_packages = ["xinetd", "tftp-server", "syslinux", "wget"]
+      $django_package = "Django"
+      $django_version = "1.3.4-1.el6"
     }
     /(?i)(debian|ubuntu)/:  {
       $cobbler_package = "cobbler"
       $cobbler_web_package = "cobbler-web"
       $dnsmasq_package = "dnsmasq"
       $cobbler_additional_packages = ["tftpd-hpa", "syslinux", "wget", "python-ipaddr"]
+      $django_package = "python_django"
+      $django_version = "1.3.1-4ubuntu1.4"
     }
   }
 
@@ -23,11 +27,16 @@ class cobbler::packages {
 
   cobbler_safe_package { $cobbler_additional_packages : }
 
+  package { $django_package :
+        ensure => $django_version
+  }
+
   package { $cobbler_package :
     ensure => installed,
     require => [
                 Package[$dnsmasq_package],
                 Package[$cobbler_additional_packages],
+                Package[$django_package],
                 ],
   }
 
