fdbc68c0e8078062bf972da2781964b2f77bb307
Vladimir Kuklin <vkuklin@mirantis.com>
2012-12-08 00:58:33 +0400

    fix galera facter

--- deployment/puppet/galera/lib/facter/galera_gcomm_empty.rb
+++ deployment/puppet/galera/lib/facter/galera_gcomm_empty.rb
@@ -1,10 +1,12 @@
 # galera_gcomm.rb
 
-$result = ""
-if gcomm=open("/etc/mysql/conf.d/wsrep.cnf").read.grep(/^\s*wsrep_cluster_address=[\"\']gcomm:\/\/\s*[\"\']\s*/)
-    result="true"
-else
-    result="false"
+$result = "false"
+if File.exists?("/etc/mysql/conf.d/wsrep.cnf")
+    if open("/etc/mysql/conf.d/wsrep.cnf").read.grep(/^\s*wsrep_cluster_address=[\"\']gcomm:\/\/\s*[\"\']\s*/)
+        result="true"
+    else
+        result="false"
+    end
 end
 
 Facter.add("galera_gcomm_empty") do
