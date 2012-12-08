ff472f5388c8e492ebbe90b6b8776a1e65a76a9d
Vladimir Kuklin <vkuklin@mirantis.com>
2012-12-08 07:31:51 +0400

    fix apparmor

--- deployment/puppet/openstack/manifests/apparmor/disable.pp
+++ deployment/puppet/openstack/manifests/apparmor/disable.pp
@@ -2,7 +2,7 @@ class openstack::apparmor::disable ()
 {
 
  exec {'disable_apparmor':
-    onlyif => "dpkg -l | grep apparmor",
+    onlyif => "dpkg -l | grep -w apparmor",
     command => "invoke-rc.d apparmor teardown && apt-get purge -y apparmor && rm -rf /etc/apparmor /etc/apparmor.d",
     path => ['/usr/bin', '/usr/sbin', '/sbin', '/bin'],
     }
