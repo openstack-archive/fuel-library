f519ee66a5ea1074e219cee1f6a6e32a94045f42
vic <vgalkin@mirantis.com>
2012-12-08 07:57:53 +0400

    Fix vswitch types

--- deployment/puppet/vswitch/lib/puppet/type/vs_port.rb
+++ deployment/puppet/vswitch/lib/puppet/type/vs_port.rb
@@ -1,24 +1,20 @@
-require "puppet"
+Puppet::Type.newtype(:vs_port) do
+  @doc = "A Virtual Switch Port"
 
-module Puppet
-  Puppet::Type.newtype(:vs_port) do
-    @doc = "A Virtual Switch Port"
+  ensurable
 
-    ensurable
+  newparam(:interface) do
+    isnamevar
+    desc "The interface to attach to the bridge"
+  end
 
-    newparam(:interface) do
-      isnamevar
-      desc "The interface to attach to the bridge"
-    end
+  newparam(:bridge) do
+    desc "What bridge to use"
+  end
 
-    newparam(:bridge) do
-      desc "What bridge to use"
-    end
-
-    autorequire(:vs_bridge) do
-      [self[:bridge]]
-    end
-
+  autorequire(:vs_bridge) do
+    [self[:bridge]]
   end
 end
+
 
