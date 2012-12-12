5f3af1ecaac1e033823b6f01c73716c269a02112
vic <vgalkin@mirantis.com>
2012-12-12 16:54:42 +0400

    Second pass for change gcom address

--- fuel_test/openstack/test_openstack.py
+++ fuel_test/openstack/test_openstack.py
@@ -6,6 +6,7 @@ class OpenStackCase(OpenStackTestCase):
     def test_deploy_open_stack(self):
         self.validate(
             [self.nodes.controllers[0], self.nodes.controllers[1],
+             self.nodes.controllers[0],
              self.nodes.computes[0], self.nodes.computes[1]],
             'puppet agent --test')
         for node in self.environment.nodes:
