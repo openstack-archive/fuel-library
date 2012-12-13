f641809c9305f16279ab6d99ea0d6d57932867e7
Ivan Ponovarev <iponomarev@mirantis.com>
2012-12-13 11:50:25 +0400

    create documentation

--- deployment/puppet/openstack/examples/site_openstack_swift_compact.pp
+++ deployment/puppet/openstack/examples/site_openstack_swift_compact.pp
@@ -1,49 +1,79 @@
+##
+# These parameters should be edit
+##
+
+# This interface will be giving away internet
+$public_interface = 'eth0'
+# This interface will look to local network
+$internal_interface = 'eth1'
+# This interface for internal services
+$private_interface = 'eth2'
+
+# Public and Internal IP
 $internal_virtual_ip = '10.0.126.253'
 $public_virtual_ip = '10.0.215.253'
-$master_hostname = 'fuel-01'
-$swift_master = $master_hostname
+
+$controller_public_addresses = {'fuel-01' => '10.0.215.3','fuel-02' => '10.0.215.4','fuel-03' => '10.0.215.5'}
 $controller_internal_addresses = {'fuel-01' => '10.0.126.3','fuel-02' => '10.0.126.4','fuel-03' => '10.0.126.5'}
-$swift_proxies = $controller_internal_addresses 
-$floating_range = '10.0.215.128/27'
+
 $fixed_range = '10.0.198.128/27'
-$controller_hostnames = ['fuel-01', 'fuel-02', 'fuel-03']
-$public_interface = 'eth2'
-$internal_interface = 'eth0'
-$internal_address = getvar("::ipaddress_${internal_interface}")
-$private_interface = 'eth1'
-$multi_host = true
-$network_manager = 'nova.network.manager.FlatDHCPManager'
-$verbose = true
+$floating_range = '10.0.74.128/28'
+
+$swift_proxy_address = '10.0.126.253'
+
+##
+# These parameters to change by necessity
+##
+
+# Enabled or disabled different services
+$multi_host              = true
+$quantum                 = true
+$manage_volumes          = true
+$cinder                  = true
 $auto_assign_floating_ip = false
+$glance_backend          = 'swift'
+
+# Set default hostname
+$master_hostname = 'fuel-01'
+$controller_hostnames = ['fuel-01', 'fuel-02', 'fuel-03']
+$swift_master = $master_hostname
+$swift_proxies = $controller_internal_addresses
+$network_manager = 'nova.network.manager.FlatDHCPManager'
+$mirror_type='external'
+
+# Add physical volume to cinder, value must be different
+$nv_physical_volume     = ['/dev/sdz', '/dev/sdy', '/dev/sdx'] 
+
+# Set credential for different services
+$swift_shared_secret  = 'changeme'
+$swift_user_password     = 'swift_pass'
+
 $mysql_root_password     = 'nova'
 $admin_email             = 'openstack@openstack.org'
 $admin_password          = 'nova'
+
 $keystone_db_password    = 'nova'
 $keystone_admin_token    = 'nova'
+
 $glance_db_password      = 'nova'
 $glance_user_password    = 'nova'
+
 $nova_db_password        = 'nova'
 $nova_user_password      = 'nova'
+
 $rabbit_password         = 'nova'
 $rabbit_user             = 'nova'
-$swift_user_password  = 'swift_pass'
-# swift specific configurations
-$swift_shared_secret  = 'changeme'
-$swift_local_net_ip   = $ipaddress_eth0
-$swift_proxy_address = '10.0.126.253'
-$controller_node_public = $internal_virtual_ip 
-$glance_backend         = 'swift'
-$manage_volumes         = true 
-$nv_physical_volume     = ['/dev/sdz', '/dev/sdy', '/dev/sdx'] 
-$quantum                = true
-$quantum_host             = $internal_virtual_ip
-$quantum_sql_connection   = "mysql://${quantum_db_user}:${quantum_db_password}@${quantum_host}/${quantum_db_dbname}"
+
 $quantum_user_password  = 'quantum_pass'
 $quantum_db_password    = 'quantum_pass'
 $quantum_db_user        = 'quantum'
 $quantum_db_dbname      = 'quantum'
-$tenant_network_type    = 'gre'
-$cinder                 = true
+$quantum_sql_connection   = "mysql://${quantum_db_user}:${quantum_db_password}@${quantum_host}/${quantum_db_dbname}"
+
+$controller_node_public   = $internal_virtual_ip
+$quantum_host             = $internal_virtual_ip
+$swift_local_net_ip       = $ipaddress_eth0
+
 $openstack_version = {
   'keystone'   => 'latest',
   'glance'     => 'latest',
@@ -52,69 +82,69 @@ $openstack_version = {
   'novncproxy' => 'latest',
   'cinder' => latest,
 }
-$mirror_type="external"
 
+$tenant_network_type    = 'gre'
+$internal_address = getvar("::ipaddress_${internal_interface}")
+$verbose = true
 Exec { logoutput => true }
 
 stage { 'openstack-custom-repo': before => Stage['main'] }
 class { 'openstack::mirantis_repos': stage => 'openstack-custom-repo', type=> $mirror_type }
-if $::operatingsystem == 'Ubuntu'
-{
+
+if $::operatingsystem == 'Ubuntu' {
   class { 'openstack::apparmor::disable': stage => 'openstack-custom-repo' }
 }
 
 class compact_controller {
-  class { 'openstack::controller_ha': 
-      controller_public_addresses => $controller_public_addresses,
-      public_interface        => $public_interface,
-      internal_interface      => $internal_interface,
-      private_interface       => $private_interface,
-      internal_virtual_ip     => $internal_virtual_ip,
-      public_virtual_ip       => $public_virtual_ip,
-      controller_internal_addresses => $controller_internal_addresses,
-      master_hostname         => $master_hostname,
-      floating_range          => $floating_range,
-      fixed_range             => $fixed_range,
-      multi_host              => $multi_host,
-      network_manager         => $network_manager,
-      verbose                 => $verbose,
-      auto_assign_floating_ip => $auto_assign_floating_ip,
-      mysql_root_password     => $mysql_root_password,
-      admin_email             => $admin_email,
-      admin_password          => $admin_password,
-      keystone_db_password    => $keystone_db_password,
-      keystone_admin_token    => $keystone_admin_token,
-      glance_db_password      => $glance_db_password,
-      glance_user_password    => $glance_user_password,
-      nova_db_password        => $nova_db_password,
-      nova_user_password      => $nova_user_password,
-      rabbit_password         => $rabbit_password,
-      rabbit_user             => $rabbit_user,
-      rabbit_nodes            => $controller_hostnames,
-      memcached_servers       => $controller_hostnames,
-      export_resources        => false,
-      glance_backend          => $glance_backend,
-      swift_proxies           => $swift_proxies,
-      quantum                 => $quantum,
-      quantum_user_password   => $quantum_user_password,
-      quantum_db_password     => $quantum_db_password,
-      quantum_db_user         => $quantum_db_user,
-      quantum_db_dbname       => $quantum_db_dbname,
-      tenant_network_type     => $tenant_network_type,
-      cinder                  => $cinder,
-      manage_volumes          => $manage_volumes,
-      galera_nodes            => $controller_hostnames,
-      nv_physical_volume      => $nv_physical_volume,
-      }
-      class { 'swift::keystone::auth':
-             password => $swift_user_password,
-             public_address  => $public_virtual_ip,
-             internal_address  => $internal_virtual_ip,
-             admin_address  => $internal_virtual_ip,
-      }
-
+  class { 'openstack::controller_ha':
+    controller_public_addresses   => $controller_public_addresses,
+    controller_internal_addresses => $controller_internal_addresses,
+    public_interface        => $public_interface,
+    internal_interface      => $internal_interface,
+    private_interface       => $private_interface,
+    internal_virtual_ip     => $internal_virtual_ip,
+    public_virtual_ip       => $public_virtual_ip,
+    master_hostname         => $master_hostname,
+    floating_range          => $floating_range,
+    fixed_range             => $fixed_range,
+    multi_host              => $multi_host,
+    network_manager         => $network_manager,
+    verbose                 => $verbose,
+    auto_assign_floating_ip => $auto_assign_floating_ip,
+    mysql_root_password     => $mysql_root_password,
+    admin_email             => $admin_email,
+    admin_password          => $admin_password,
+    keystone_db_password    => $keystone_db_password,
+    keystone_admin_token    => $keystone_admin_token,
+    glance_db_password      => $glance_db_password,
+    glance_user_password    => $glance_user_password,
+    nova_db_password        => $nova_db_password,
+    nova_user_password      => $nova_user_password,
+    rabbit_password         => $rabbit_password,
+    rabbit_user             => $rabbit_user,
+    rabbit_nodes            => $controller_hostnames,
+    memcached_servers       => $controller_hostnames,
+    export_resources        => false,
+    glance_backend          => $glance_backend,
+    swift_proxies           => $swift_proxies,
+    quantum                 => $quantum,
+    quantum_user_password   => $quantum_user_password,
+    quantum_db_password     => $quantum_db_password,
+    quantum_db_user         => $quantum_db_user,
+    quantum_db_dbname       => $quantum_db_dbname,
+    tenant_network_type     => $tenant_network_type,
+    cinder                  => $cinder,
+    manage_volumes          => $manage_volumes,
+    galera_nodes            => $controller_hostnames,
+    nv_physical_volume      => $nv_physical_volume,
+  }
+  class { 'swift::keystone::auth':
+    password         => $swift_user_password,
+    public_address   => $public_virtual_ip,
+    internal_address => $internal_virtual_ip,
+    admin_address    => $internal_virtual_ip,
+  }
 }
-
 
 node /fuel-01/ {
   class { compact_controller: }
@@ -135,7 +165,7 @@ node /fuel-01/ {
 
 
 node /fuel-02/ {
-  class { compact_controller: }
+  class { 'compact_controller': }
   $swift_zone = 2
 
   class { 'openstack::swift::storage-node':
@@ -151,9 +181,8 @@ node /fuel-02/ {
   }
 }
 
-
 node /fuel-03/ {
-  class { compact_controller: }
+  class { 'compact_controller': }
   $swift_zone = 3
 
   class { 'openstack::swift::storage-node':
@@ -171,38 +200,33 @@ node /fuel-03/ {
 
 
 node /fuel-0[45]/ {
-    class { 'openstack::compute':
-      public_interface   => $public_interface,
-      private_interface  => $private_interface,
-      internal_address   => $internal_address,
-      libvirt_type       => 'qemu',
-      fixed_range        => $fixed_range,
-      network_manager    => $network_manager,
-      multi_host         => $multi_host,
-      sql_connection     => "mysql://nova:${nova_db_password}@${internal_virtual_ip}/nova",
-      rabbit_nodes       => $controller_hostnames,
-      rabbit_password    => $rabbit_password,
-      rabbit_user        => $rabbit_user,
-      glance_api_servers => "${internal_virtual_ip}:9292",
-      vncproxy_host      => $public_virtual_ip,
-      verbose            => $verbose,
-      vnc_enabled        => true,
-      manage_volumes     => false,
-      nova_user_password	=> $nova_user_password,
-      cache_server_ip         => $controller_hostnames,
-      service_endpoint	=> $internal_virtual_ip,
-      quantum                => $quantum,
-      quantum_host           => $quantum_host,
-      quantum_sql_connection => $quantum_sql_connection,
-      quantum_user_password  => $quantum_user_password,
-      tenant_network_type     => $tenant_network_type,
-      cinder                  => $cinder,
-      ssh_private_key    => 'puppet:///ssh_keys/openstack',
-      ssh_public_key     => 'puppet:///ssh_keys/openstack.pub',
-    }
+  class { 'openstack::compute':
+    public_interface       => $public_interface,
+    private_interface      => $private_interface,
+    internal_address       => $internal_address,
+    libvirt_type           => 'qemu',
+    fixed_range            => $fixed_range,
+    network_manager        => $network_manager,
+    multi_host             => $multi_host,
+    sql_connection         => "mysql://nova:${nova_db_password}@${internal_virtual_ip}/nova",
+    rabbit_nodes           => $controller_hostnames,
+    rabbit_password        => $rabbit_password,
+    rabbit_user            => $rabbit_user,
+    glance_api_servers     => "${internal_virtual_ip}:9292",
+    vncproxy_host          => $public_virtual_ip,
+    verbose                => $verbose,
+    vnc_enabled            => true,
+    manage_volumes         => false,
+    nova_user_password     => $nova_user_password,
+    cache_server_ip        => $controller_hostnames,
+    service_endpoint       => $internal_virtual_ip,
+    quantum                => $quantum,
+    quantum_host           => $quantum_host,
+    quantum_sql_connection => $quantum_sql_connection,
+    quantum_user_password  => $quantum_user_password,
+    tenant_network_type    => $tenant_network_type,
+    cinder                 => $cinder,
+    ssh_private_key        => 'puppet:///ssh_keys/openstack',
+    ssh_public_key         => 'puppet:///ssh_keys/openstack.pub',
+  }
 }
-
-
-
-# deprecated. keep it for backward compatibility
-$controller_public_addresses = {'fuel-01' => '10.0.215.3','fuel-02' => '10.0.215.4','fuel-03' => '10.0.215.5'}
--- deployment/puppet/openstack/examples/site_openstack_swift_standalone.pp
+++ deployment/puppet/openstack/examples/site_openstack_swift_standalone.pp
@@ -1,49 +1,79 @@
-$internal_virtual_ip = '10.0.48.253'
-$public_virtual_ip = '10.0.111.253'
-$master_hostname = 'fuel-01'
-$swift_master = 'fuel-08'
-$controller_internal_addresses = {'fuel-01' => '10.0.48.3','fuel-02' => '10.0.48.4'}
-$swift_proxies = {'fuel-08' => '10.0.48.10', 'fuel-09' => '10.0.48.11'}
-$floating_range = '10.0.111.128/27'
-$fixed_range = '10.0.251.128/27'
-$controller_hostnames = ['fuel-01', 'fuel-02']
-$public_interface = 'eth2'
-$internal_interface = 'eth0'
-$internal_address = getvar("::ipaddress_${internal_interface}")
-$private_interface = 'eth1'
-$multi_host = true
-$network_manager = 'nova.network.manager.FlatDHCPManager'
-$verbose = true
+##
+# These parameters should be edit
+##
+
+# This interface will be giving away internet
+$public_interface    = 'eth0'
+# This interface will look to local network
+$internal_interface  = 'eth1'
+# This interface for internal services
+$private_interface   = 'eth2'
+
+# Public and Internal IP
+$internal_virtual_ip = '10.0.0.253'
+$public_virtual_ip = '10.0.2.253'
+
+$controller_public_addresses = {'fuel-01' => '10.0.2.15','fuel-02' => '10.0.2.16','fuel-03' => '10.0.2.17'}
+$controller_internal_addresses = {'fuel-01' => '10.0.0.101','fuel-02' => '10.0.0.102','fuel-03' => '10.0.0.103'}
+
+$floating_range = '10.0.2.128/27'
+$fixed_range = '10.0.198.128/27'
+
+$swift_proxy_address = '10.0.0.253'
+
+##
+# These parameters to change by necessity
+##
+
+# Enabled or disabled different services
+$cinder                  = true
+$multi_host              = true
+$manage_volumes          = true
+$quantum                 = true
 $auto_assign_floating_ip = false
+$glance_backend          = 'swift'
+
+# Set default hostname
+$master_hostname = 'fuel-01'
+$controller_hostnames = ['fuel-01', 'fuel-02', 'fuel-03']
+$network_manager = 'nova.network.manager.FlatDHCPManager'
+
+# Add physical volume to cinder, value must be different
+$nv_physical_volume     = ['/dev/sdz', '/dev/sdy', '/dev/sdx']
+
+# Set credential for different services
 $mysql_root_password     = 'nova'
 $admin_email             = 'openstack@openstack.org'
 $admin_password          = 'nova'
+
 $keystone_db_password    = 'nova'
 $keystone_admin_token    = 'nova'
+
 $glance_db_password      = 'nova'
 $glance_user_password    = 'nova'
+
 $nova_db_password        = 'nova'
 $nova_user_password      = 'nova'
+
 $rabbit_password         = 'nova'
 $rabbit_user             = 'nova'
-$swift_user_password  = 'swift_pass'
-# swift specific configurations
-$swift_shared_secret  = 'changeme'
-$swift_local_net_ip   = $ipaddress_eth0
-$swift_proxy_address = '10.0.48.253'
-$controller_node_public = $internal_virtual_ip 
-$glance_backend         = 'swift'
-$quantum                = true
-$quantum_host             = $internal_virtual_ip
-$quantum_sql_connection   = "mysql://${quantum_db_user}:${quantum_db_password}@${quantum_host}/${quantum_db_dbname}"
+
+$swift_user_password    = 'swift_pass'
+$swift_shared_secret    = 'changeme'
 $quantum_user_password  = 'quantum_pass'
 $quantum_db_password    = 'quantum_pass'
 $quantum_db_user        = 'quantum'
 $quantum_db_dbname      = 'quantum'
 $tenant_network_type    = 'gre'
-$cinder                 = true
-$manage_volumes         = true 
-$nv_physical_volume     = ['/dev/sdz', '/dev/sdy', '/dev/sdx'] 
+$quantum_sql_connection = "mysql://${quantum_db_user}:${quantum_db_password}@${quantum_host}/${quantum_db_dbname}"
+
+$quantum_host           = $internal_virtual_ip
+$controller_node_public = $internal_virtual_ip
+$swift_local_net_ip     = $ipaddress_eth1
+$swift_master           = $master_hostname
+$swift_proxies          = $controller_internal_addresses
+
+# Packages version
 $openstack_version = {
   'keystone'   => 'latest',
   'glance'     => 'latest',
@@ -52,98 +82,102 @@ $openstack_version = {
   'novncproxy' => 'latest',
   'cinder' => latest,
 }
-$mirror_type = "external"
+
+$mirror_type='external'
+
+$internal_address = getvar("::ipaddress_${internal_interface}")
+$verbose = true
 Exec { logoutput => true }
 stage { 'openstack-custom-repo': before => Stage['main'] }
 class { 'openstack::mirantis_repos': stage => 'openstack-custom-repo', type => $mirror_type }
-if $::operatingsystem == 'Ubuntu'
-{
+
+if $::operatingsystem == 'Ubuntu' {
   class { 'openstack::apparmor::disable': stage => 'openstack-custom-repo' }
 }
 
 node /fuel-0[12]/ {
-    class { 'openstack::controller_ha': 
-      controller_public_addresses => $controller_public_addresses,
-      public_interface        => $public_interface,
-      internal_interface      => $internal_interface,
-      private_interface       => $private_interface,
-      internal_virtual_ip     => $internal_virtual_ip,
-      public_virtual_ip       => $public_virtual_ip,
-      controller_internal_addresses => $controller_internal_addresses,
-      master_hostname         => $master_hostname,
-      floating_range          => $floating_range,
-      fixed_range             => $fixed_range,
-      multi_host              => $multi_host,
-      network_manager         => $network_manager,
-      verbose                 => $verbose,
-      auto_assign_floating_ip => $auto_assign_floating_ip,
-      mysql_root_password     => $mysql_root_password,
-      admin_email             => $admin_email,
-      admin_password          => $admin_password,
-      keystone_db_password    => $keystone_db_password,
-      keystone_admin_token    => $keystone_admin_token,
-      glance_db_password      => $glance_db_password,
-      glance_user_password    => $glance_user_password,
-      nova_db_password        => $nova_db_password,
-      nova_user_password      => $nova_user_password,
-      rabbit_password         => $rabbit_password,
-      rabbit_user             => $rabbit_user,
-      rabbit_nodes            => $controller_hostnames,
-      memcached_servers       => $controller_hostnames,
-      export_resources        => false,
-      glance_backend          => $glance_backend,
-      swift_proxies           => $swift_proxies,
-      quantum                 => $quantum,
-      quantum_user_password   => $quantum_user_password,
-      quantum_db_password     => $quantum_db_password,
-      quantum_db_user         => $quantum_db_user,
-      quantum_db_dbname       => $quantum_db_dbname,
-      tenant_network_type     => $tenant_network_type,
-      cinder                  => $cinder,
-      manage_volumes          => $manage_volumes,
-      galera_nodes            => $controller_hostnames,
-      nv_physical_volume      => $nv_physical_volume,
-      }
+  class { 'openstack::controller_ha':
+    controller_public_addresses   => $controller_public_addresses,
+    controller_internal_addresses => $controller_internal_addresses,
+    public_interface        => $public_interface,
+    internal_interface      => $internal_interface,
+    private_interface       => $private_interface,
+    internal_virtual_ip     => $internal_virtual_ip,
+    public_virtual_ip       => $public_virtual_ip,
+    master_hostname         => $master_hostname,
+    floating_range          => $floating_range,
+    fixed_range             => $fixed_range,
+    multi_host              => $multi_host,
+    network_manager         => $network_manager,
+    verbose                 => $verbose,
+    auto_assign_floating_ip => $auto_assign_floating_ip,
+    mysql_root_password     => $mysql_root_password,
+    admin_email             => $admin_email,
+    admin_password          => $admin_password,
+    keystone_db_password    => $keystone_db_password,
+    keystone_admin_token    => $keystone_admin_token,
+    glance_db_password      => $glance_db_password,
+    glance_user_password    => $glance_user_password,
+    nova_db_password        => $nova_db_password,
+    nova_user_password      => $nova_user_password,
+    rabbit_password         => $rabbit_password,
+    rabbit_user             => $rabbit_user,
+    rabbit_nodes            => $controller_hostnames,
+    memcached_servers       => $controller_hostnames,
+    export_resources        => false,
+    glance_backend          => $glance_backend,
+    swift_proxies           => $swift_proxies,
+    quantum                 => $quantum,
+    quantum_user_password   => $quantum_user_password,
+    quantum_db_password     => $quantum_db_password,
+    quantum_db_user         => $quantum_db_user,
+    quantum_db_dbname       => $quantum_db_dbname,
+    tenant_network_type     => $tenant_network_type,
+    cinder                  => $cinder,
+    manage_volumes          => $manage_volumes,
+    galera_nodes            => $controller_hostnames,
+    nv_physical_volume      => $nv_physical_volume,
+  }
 
-      class { 'swift::keystone::auth':
-             password => $swift_user_password,
-             public_address  => $public_virtual_ip,
-             internal_address  => $internal_virtual_ip,
-             admin_address  => $internal_virtual_ip,
-      }
+  class { 'swift::keystone::auth':
+    password         => $swift_user_password,
+    public_address   => $public_virtual_ip,
+    internal_address => $internal_virtual_ip,
+    admin_address    => $internal_virtual_ip,
+  }
 }
 
 node /fuel-0[34]/ {
-    class { 'openstack::compute':
-      public_interface   => $public_interface,
-      private_interface  => $private_interface,
-      internal_address   => $internal_address,
-      libvirt_type       => 'qemu',
-      fixed_range        => $fixed_range,
-      network_manager    => $network_manager,
-      multi_host         => $multi_host,
-      sql_connection     => "mysql://nova:${nova_db_password}@${internal_virtual_ip}/nova",
-      rabbit_nodes       => $controller_hostnames,
-      rabbit_password    => $rabbit_password,
-      rabbit_user        => $rabbit_user,
-      glance_api_servers => "${internal_virtual_ip}:9292",
-      vncproxy_host      => $public_virtual_ip,
-      verbose            => $verbose,
-      vnc_enabled        => true,
-      manage_volumes     => false,
-      nova_user_password => $nova_user_password,
-      cache_server_ip    => $controller_hostnames,
-      service_endpoint   => $internal_virtual_ip,
-      quantum                => $quantum,
-      quantum_host           => $quantum_host,
-      quantum_sql_connection => $quantum_sql_connection,
-      quantum_user_password  => $quantum_user_password,
-      tenant_network_type     => $tenant_network_type,
-      cinder                  => $cinder,
-      ssh_private_key    => 'puppet:///ssh_keys/openstack',
-      ssh_public_key     => 'puppet:///ssh_keys/openstack.pub',
- 
-    }
+  class { 'openstack::compute':
+    public_interface       => $public_interface,
+    private_interface      => $private_interface,
+    internal_address       => $internal_address,
+    libvirt_type           => 'qemu',
+    fixed_range            => $fixed_range,
+    network_manager        => $network_manager,
+    multi_host             => $multi_host,
+    sql_connection         => "mysql://nova:${nova_db_password}@${internal_virtual_ip}/nova",
+    rabbit_nodes           => $controller_hostnames,
+    rabbit_password        => $rabbit_password,
+    rabbit_user            => $rabbit_user,
+    glance_api_servers     => "${internal_virtual_ip}:9292",
+    vncproxy_host          => $public_virtual_ip,
+    verbose                => $verbose,
+    vnc_enabled            => true,
+    manage_volumes         => false,
+    nova_user_password     => $nova_user_password,
+    cache_server_ip        => $controller_hostnames,
+    service_endpoint       => $internal_virtual_ip,
+    quantum                => $quantum,
+    quantum_host           => $quantum_host,
+    quantum_sql_connection => $quantum_sql_connection,
+    quantum_user_password  => $quantum_user_password,
+    tenant_network_type    => $tenant_network_type,
+    cinder                 => $cinder,
+    ssh_private_key        => 'puppet:///ssh_keys/openstack',
+    ssh_public_key         => 'puppet:///ssh_keys/openstack.pub',
+
+  }
 }
 
 node /fuel-05/ {
@@ -187,10 +221,4 @@ node /fuel-0[89]/ {
     controller_node_address => $internal_virtual_ip,
     swift_local_net_ip      => $internal_address,
   }
-
 }
-
-
-
-# deprecated. keep it for backward compatibility
-$controller_public_addresses = {'fuel-01' => '10.0.111.3','fuel-02' => '10.0.111.4'}
