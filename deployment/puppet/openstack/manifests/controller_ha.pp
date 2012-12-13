a55f5f1728789983e7235528f2fd1705f3555dc5
vic <vgalkin@mirantis.com>
2012-12-13 21:10:37 +0400

    Moving final config to galera module

--- deployment/puppet/openstack/manifests/controller_ha.pp
+++ deployment/puppet/openstack/manifests/controller_ha.pp
@@ -282,32 +282,3 @@ class openstack::controller_ha (
 }
 
 #Class['openstack::controller_ha']->Class['galera-master-final-config']
-
-stage { 'after_main': require => Stage['main'] }
-
-class openstack::galera_master_final_config($master_hostname, $controller_internal_addresses) {
-# This class changes config file on first Galera node to allow safe restart of this node without leaving cluster.
-#    require => Class['openstack::controller_ha'],
-
-    $is_master = $::hostname ? { $master_hostname => 0, default => 1 }
-
-    if $is_master == 0 {
-      $galera_gcomm_string = inline_template("<%= @controller_internal_addresses.keys.collect {|ip| ip + ':' + 4567.to_s }.join ',' %>")
-      $check_galera = "show status like 'wsrep_cluster_size';"
-      $mysql_user = $::galera::params::mysql_user
-      $mysql_password = $::galera::params::mysql_password
-
-      exec {"first-galera-node-final-config":
-        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
-        command => "sed -i 's/wsrep_cluster_address=\"gcomm:\\/\\/\"/wsrep_cluster_address=\"gcomm:\\/\\/${galera_gcomm_string}\"/' /etc/mysql/conf.d/wsrep.cnf",
-        onlyif => "mysql -e -u${mysql_user} -p${mysql_password} ${check_galera} | awk '\$1 == \"wsrep_cluster_size\" {print \$2}' | awk '{if (\$0 > 1) exit 0; else exit 1}' ",
-      }
-    }
-}
-
-
-class {'openstack::galera_master_final_config':
-    stage => 'after_main',
-    master_hostname => $master_hostname,
-    controller_internal_addresses => $controller_internal_addresses,
-}
