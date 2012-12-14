fe6ffd33bb9985a21a3394a6e5bcf7035f2daf93
Sergey Vasilenko <svasilenko@mirantis.com>
2012-12-14 20:22:07 +0400

    FUEL-269 (autocreate LVM groups for Cinder if need. Centos.)

--- deployment/puppet/cobbler/manifests/snippets.pp
+++ deployment/puppet/cobbler/manifests/snippets.pp
@@ -26,6 +26,9 @@ class cobbler::snippets {
 
   cobbler_snippet {"post_install_network_config":}
 
+  cobbler_snippet {"cinder_create_lvm_group":}
+  cobbler_snippet {"cinder_create_lvm_group__ubuntu":}
+
   cobbler_snippet {"ubuntu_disable_pxe":}
   cobbler_snippet {"ubuntu_packages":}
   cobbler_snippet {"ubuntu_puppet_config":}
