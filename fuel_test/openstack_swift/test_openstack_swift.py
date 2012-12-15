3a8ed74f8b273e18c8d621747e4aea2a6c4a703a
vic <vgalkin@mirantis.com>
2012-12-15 16:58:07 +0400

    Fix tests

--- fuel_test/openstack_swift/test_openstack_swift.py
+++ fuel_test/openstack_swift/test_openstack_swift.py
@@ -1,27 +1,25 @@
-from devops.helpers import ssh
 import unittest
-from fuel_test.helpers import execute
+from fuel_test.helpers import is_not_essex
 from fuel_test.openstack_swift.openstack_swift_test_case import OpenStackSwiftTestCase
 from fuel_test.settings import OPENSTACK_SNAPSHOT
 
 class OpenStackSwiftCase(OpenStackSwiftTestCase):
     def test_deploy_open_stack_swift(self):
         self.validate(
-            [self.nodes.controllers[0], self.nodes.controllers[1], self.nodes.controllers[0],
-             self.nodes.quantums[0], self.nodes.computes[0], self.nodes.computes[1]],
+            self.nodes.controllers[:1] + self.nodes.controllers[1:] + self.nodes.controllers[:1],
             'puppet agent --test')
-        results = []
-        for node in self.nodes.storages:
-            remote = ssh(node.ip_address_by_network['internal'], username='root', password='r00tme')
-            results.append(execute(remote.sudo.ssh, 'puppet agent --test'))
-            results.append(execute(remote.sudo.ssh, 'puppet agent --test'))
-        remote = ssh(self.nodes.proxies[0].ip_address_by_network['internal'], username='root',
-            password='r00tme')
-        results.append(execute(remote.sudo.ssh, 'puppet agent --test'))
-        self.validate(self.nodes.storages,
-                                'puppet agent --test')
-        self.validate(self.nodes.proxies,
-                                'puppet agent --test')
+        if is_not_essex():
+            self.validate(
+                 self.nodes.quantums,
+                'puppet agent --test')
+        self.validate(
+            self.nodes.computes,
+            'puppet agent --test')
+        self.do(self.nodes.storages, 'puppet agent --test')
+        self.do(self.nodes.storages, 'puppet agent --test')
+        self.do(self.nodes.proxies, 'puppet agent --test')
+        self.validate(self.nodes.storages, 'puppet agent --test')
+        self.validate(self.nodes.proxies, 'puppet agent --test')
         for node in self.environment.nodes:
             node.save_snapshot(OPENSTACK_SNAPSHOT, force=True)
 
