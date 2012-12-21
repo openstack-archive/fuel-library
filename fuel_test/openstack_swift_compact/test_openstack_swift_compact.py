78f579f148d05befddd8e31bbe49e68689b25ce9
vic <vgalkin@mirantis.com>
2012-12-21 22:46:05 +0400

    Add test without quantum

--- fuel_test/openstack_swift_compact/test_openstack_swift_compact.py
+++ fuel_test/openstack_swift_compact/test_openstack_swift_compact.py
@@ -4,7 +4,7 @@ from fuel_test.openstack_swift_compact.openstack_swift_compact_test_case import 
 from fuel_test.settings import OPENSTACK_SNAPSHOT
 
 class OpenStackSwiftCompactCase(OpenStackSwiftCompactTestCase):
-    def test_deploy_open_stack_swift_compact(self):
+    def deploy_compact(self):
         self.do(self.nodes.controllers[:1], 'puppet agent --test')
         self.do(self.nodes.controllers[1:], 'puppet agent --test')
         self.do(self.nodes.controllers, 'puppet agent --test')
@@ -15,6 +15,14 @@ class OpenStackSwiftCompactCase(OpenStackSwiftCompactTestCase):
         self.validate(self.nodes.computes, 'puppet agent --test')
         for node in self.environment.nodes:
             node.save_snapshot(OPENSTACK_SNAPSHOT, force=True)
+
+    def test_deploy_compact(self):
+        self.write_openstack_sitepp(self.nodes.controllers, quantum=True)
+        self.deploy_compact()
+
+    def test_deploy_compact_wo_quantum(self):
+        self.write_openstack_sitepp(self.nodes.controllers, quantum=False)
+        self.deploy_compact()
 
 if __name__ == '__main__':
     unittest.main()
