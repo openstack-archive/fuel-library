723f2be839b7ab4f74baba0395930d9d5729f37c
Ivan Ponovarev <iponomarev@mirantis.com>
2012-12-25 15:31:26 +0400

    set force rebalance

--- deployment/puppet/swift/manifests/ringbuilder/rebalance.pp
+++ deployment/puppet/swift/manifests/ringbuilder/rebalance.pp
@@ -11,7 +11,9 @@ define swift::ringbuilder::rebalance() {
   validate_re($name, '^object|container|account$')
 
   exec { "rebalance_${name}":
-    command     => "swift-ring-builder /etc/swift/${name}.builder rebalance",
+    command     => "wift-ring-builder /etc/swift/${name}.builder \
+                    pretend_min_part_hours_passed && swift-ring-builder \
+                    /etc/swift/${name}.builder rebalance",
     path        => ['/usr/bin'],
     refreshonly => true,
   }
