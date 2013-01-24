ddded9c6aee5cef37cedac0e304cb6f7a3429151
Ivan Ponovarev <iponomarev@mirantis.com>
2013-01-24 17:38:06 +0400

    add custom check and time parameters

--- deployment/puppet/nagios/examples/master.pp
+++ deployment/puppet/nagios/examples/master.pp
@@ -1,7 +1,16 @@
+## proj_name       => isolated configuration for project
+## templatehost    => checks,intervals parameters for hosts (as Hash)
+# name - name of this template
+# check_interval check command interval for hosts included in this group
+## templateservice => checks,intervals parameters for services (as Hash)
+# name - name of this template
+# check_interval check command interval for services included in this group
+## hostgroups      =>  create hostgroups
+# Put all hostgroups from nrpe here (as Array)
 class {'nagios::master':
   proj_name       => 'test',
-  templatehost    => 'default-host',
-  templateservice => 'default-service',
+  templatehost    => {'name' => 'default-host','check_interval' => '10'},
+  templateservice => {'name' => 'default-service' ,'check_interval'=>'10'},
   hostgroups      => ['compute','controller'],
   contactgroups   => {'group' => 'admins', 'alias' => 'Admins'},
   contacts        => {'user' => 'hotkey', 'alias' => 'Dennis Hoppe',
--- deployment/puppet/nagios/examples/nrpe.pp
+++ deployment/puppet/nagios/examples/nrpe.pp
@@ -1,3 +1,9 @@
+## proj_name => isolated configuration for project
+## services  =>  array of services which you want use
+## whitelist =>  array of IP addreses which NRPE trusts
+## hostgroup =>  group wich will use in nagios master
+# do not forget create it in nagios master
+
 class {'nagios':
   proj_name       => 'test',
   services        => ['nova-compute','nova-network','libvirt'],
