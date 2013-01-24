5d074190cacafbd66da5ba1c8b7250925de2aad9
Ivan Ponovarev <iponomarev@mirantis.com>
2013-01-24 17:33:12 +0400

    fix all services in one group

--- deployment/puppet/nagios/manifests/service/services.pp
+++ deployment/puppet/nagios/manifests/service/services.pp
@@ -1,14 +1,14 @@
 define nagios::service::services(
-$command = false, 
-$group = false,
+$command = false,
+$group   = false,
 ) {
 
   @@nagios_service { "${::hostname}_${name}":
     ensure              => present,
-    use                 => $templateservice,
     hostgroup_name      => $hostgroup,
     check_command       => $command,
     service_description => $name,
+    hostname            => $::fqdn,
     target              => "/etc/nagios3/${proj_name}/${::hostname}_services.cfg",
   }
 }
