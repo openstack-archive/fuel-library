b1362beeb4ca9cba54862f5f0813f0ab6edb3aef
Ivan Bondar <ibondar@mirantis.com>
2013-01-28 16:04:38 +0400

    FUEL-195 ntp force sync

--- deployment/puppet/ntpd/manifests/init.pp
+++ deployment/puppet/ntpd/manifests/init.pp
@@ -17,10 +17,16 @@ class ntpd {
   
   package { $package_name: ensure => present }
 
+  exec{ 'ntp_init_force':
+    command => "service ${service_name} stop ; awk '\$1==\"peer\"||\$1==\"server\"{ res=system(\"ntpdate \" \$2); if(res==0) exit 0; }' /etc/ntp.conf",
+    path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+    require => Package[$package_name],
+  }
+
   service { $service_name:
     enable  => true,
     ensure  => running,
-    require => Package[$package_name],
+    require => Exec['ntp_init_force'],
   }
 
 }
