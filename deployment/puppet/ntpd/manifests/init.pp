2d240182c3f93b2554cf7439d5c9cad77e43055a
Ivan Bondar <ibondar@mirantis.com>
2013-01-29 19:14:06 +0400

    FUEL-195 path to ntp config file

--- deployment/puppet/ntpd/manifests/init.pp
+++ deployment/puppet/ntpd/manifests/init.pp
@@ -8,17 +8,19 @@ class ntpd {
     'RedHat': {
       $package_name = 'ntp'
       $service_name = 'ntpd'
+      $config_file = '/etc/ntp.conf'
     }
     'Debian': {
       $package_name = 'openntpd'
       $service_name = 'openntpd'
+      $config_file = '/etc/openntpd/ntpd.conf'
     }
   }
   
   package { $package_name: ensure => present }
 
   exec{ 'ntp_init_force':
-    command => "service ${service_name} stop ; awk '\$1==\"peer\"||\$1==\"server\"{ res=system(\"ntpdate \" \$2); if(res==0) exit 0; }' /etc/ntp.conf",
+    command => "service ${service_name} stop ; awk '\$1==\"peer\"||\$1==\"server\"{ res=system(\"ntpdate \" \$2); if(res==0) exit 0; }' ${config_file}",
     path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
     require => Package[$package_name],
   }
