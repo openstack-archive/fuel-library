4bc15458e387a4f11e01963e0bd0737d578f6c45
vic <vgalkin@mirantis.com>
2013-02-06 18:39:16 +0400

    fix quantum variables

--- deployment/puppet/nova/manifests/network/quantum.pp
+++ deployment/puppet/nova/manifests/network/quantum.pp
@@ -12,25 +12,26 @@ class nova::network::quantum (
   $quantum_admin_password,
   #$use_dhcp                  = 'True',
   #$public_interface          = undef,
-  $quantum_connection_host   = localhost,
+  $quantum_connection_host   = 'localhost',
   $quantum_auth_strategy     = 'keystone',
   $quantum_url               = 'http://127.0.0.1:9696',
   $quantum_admin_tenant_name = 'services',
   $quantum_admin_username    = 'quantum',
-  $quantum_admin_auth_url    = 'http://127.0.0.1:35357/v2.0'
+  $quantum_admin_auth_url    = 'http://127.0.0.1:35357/v2.0',
+  $public_interface          = undef,
 ) {
 
   if $public_interface {
     nova_config { 'DEFAULT/public_interface': value => $public_interface }
   }
 
-  if $quantum_host != 'localhost' {
+  if $quantum_connection_host != 'localhost' {
     nova_config { 'DEFAULT/quantum_connection_host': value => $quantum_connection_host }
   }
 
   nova_config {
-    'DEFAULT/fixed_range':               value => $fixed_range;
-    'DEFAULT/quantum_use_dhcp':          value => $use_dhcp;
+#    'DEFAULT/fixed_range':               value => $fixed_range;
+#    'DEFAULT/quantum_use_dhcp':          value => $use_dhcp;
     'DEFAULT/quantum_auth_strategy':     value => $quantum_auth_strategy;
     'DEFAULT/network_api_class':         value => 'nova.network.quantumv2.api.API';
     'DEFAULT/quantum_url':               value => $quantum_url;
