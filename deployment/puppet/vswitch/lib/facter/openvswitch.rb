d7859687b9e3fceb21b4f943b94d5b9bad089963
vic <vgalkin@mirantis.com>
2013-02-08 15:56:01 +0400

    Safe require for kernel_modules

--- deployment/puppet/vswitch/lib/facter/openvswitch.rb
+++ deployment/puppet/vswitch/lib/facter/openvswitch.rb
@@ -6,6 +6,16 @@
 #
 # Caveats:
 
+begin
+  require 'facter/kernel_modules.rb'
+rescue LoadError => e
+  # puppet apply does not add module lib directories to the $LOAD_PATH (See
+  # #4248). It should (in the future) but for the time being we need to be
+  # defensive which is what this rescue block is doing.
+  rb_file = File.join(File.dirname(__FILE__), 'kernel_modules.rb')
+  load rb_file if File.exists?(rb_file) or raise e
+end
+
 def ovs_vsctl
   "/usr/bin/ovs-vsctl"
 end
