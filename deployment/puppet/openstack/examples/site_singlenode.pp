8047e8942b922447ddb7909983604784013507e2
vic <vgalkin@mirantis.com>
2013-02-11 19:05:20 +0400

    Fix variables

--- deployment/puppet/openstack/examples/site_singlenode.pp
+++ deployment/puppet/openstack/examples/site_singlenode.pp
@@ -31,8 +31,8 @@ $horizon_secret_key      = 'dummy_secret_key'
 $mysql_root_password     = 'sql_pass'
 $rabbit_password         = 'openstack_rabbit_password'
 $rabbit_user             = 'openstack_rabbit_user'
-$fixed_network_range     = '10.0.58.0/24'
-$floating_network_range  = '10.0.75.128/27'
+$fixed_range     = '10.0.58.0/24'
+$floating_range  = '10.0.75.128/27'
 $vlan_start              = 300
 # switch this to true to have all service log at verbose
 $verbose                 = true
@@ -74,8 +74,8 @@ node 'fuel-controller-02' {
     rabbit_password         => $rabbit_password,
     rabbit_user             => $rabbit_user,
     libvirt_type            => 'qemu',
-    floating_range          => $floating_network_range,
-    fixed_range             => $fixed_network_range,
+    floating_range          => $floating_range,
+    fixed_range             => $fixed_range,
     verbose                 => $verbose,
     auto_assign_floating_ip => $auto_assign_floating_ip,
     network_config          => { 'vlan_start' => $vlan_start },
