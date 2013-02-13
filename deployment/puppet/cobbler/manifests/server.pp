d5f5fe832a5ae8dafe9ad44dec3bea1ea9349c16
vic <vgalkin@mirantis.com>
2013-02-13 21:06:08 +0400

    Reformat code

--- deployment/puppet/cobbler/manifests/server.pp
+++ deployment/puppet/cobbler/manifests/server.pp
@@ -1,37 +1,36 @@
 class cobbler::server {
-
-  Exec {path => '/usr/bin:/bin:/usr/sbin:/sbin'}
+  Exec {
+    path => '/usr/bin:/bin:/usr/sbin:/sbin'
+  }
 
   case $operatingsystem {
-    /(?i)(centos|redhat)/:  {
-
-      $cobbler_service = "cobblerd"
+    /(?i)(centos|redhat)/ : {
+      $cobbler_service     = "cobblerd"
       $cobbler_web_service = "httpd"
-      $dnsmasq_service = "dnsmasq"
+      $dnsmasq_service     = "dnsmasq"
 
       service { "xinetd":
-        enable => true,
-        ensure => running,
+        enable     => true,
+        ensure     => running,
         hasrestart => true,
-        require => Package[$cobbler::packages::cobbler_additional_packages],
+        require    => Package[$cobbler::packages::cobbler_additional_packages],
       }
 
       file { "/etc/xinetd.conf":
         content => template("cobbler/xinetd.conf.erb"),
-        owner => root,
-        group => root,
-        mode => 0600,
+        owner   => root,
+        group   => root,
+        mode    => 0600,
         require => Package[$cobbler::packages::cobbler_additional_packages],
-        notify => Service["xinetd"],
+        notify  => Service["xinetd"],
       }
 
     }
-    /(?i)(debian|ubuntu)/:  {
-
-      $cobbler_service = "cobbler"
+    /(?i)(debian|ubuntu)/ : {
+      $cobbler_service     = "cobbler"
       $cobbler_web_service = "apache2"
-      $dnsmasq_service = "dnsmasq"
-      $apache_ssl_module = "ssl"
+      $dnsmasq_service     = "dnsmasq"
+      $apache_ssl_module   = "ssl"
 
     }
   }
@@ -39,131 +38,109 @@ class cobbler::server {
   Service[$cobbler_service] -> Exec["cobbler_sync"] -> Service[$dnsmasq_service]
 
   service { $cobbler_service:
-    enable => true,
-    ensure => running,
+    enable     => true,
+    ensure     => running,
     hasrestart => true,
-    require => Package[$cobbler::packages::cobbler_package],
+    require    => Package[$cobbler::packages::cobbler_package],
   }
 
   service { $dnsmasq_service:
-    enable => true,
-    ensure => running,
+    enable     => true,
+    ensure     => running,
     hasrestart => true,
-    require => Package[$cobbler::packages::dnsmasq_package],
-    subscribe => Exec["cobbler_sync"],
+    require    => Package[$cobbler::packages::dnsmasq_package],
+    subscribe  => Exec["cobbler_sync"],
   }
 
   if $apache_ssl_module {
-    file {'/etc/apache2/mods-enabled/ssl.load':
-      ensure  => link,
-      target  => '/etc/apache2/mods-available/ssl.load',
-    }->
-
-    file {'/etc/apache2/mods-enabled/ssl.conf':
-      ensure  => link,
-      target  => '/etc/apache2/mods-available/ssl.conf',
-    }->
-
-    file {'/etc/apache2/sites-enabled/default-ssl':
-      ensure  => link,
-      target  => '/etc/apache2/sites-available/default-ssl',
-      before  => Service[$cobbler_web_service],
-      notify  => Service[$cobbler_web_service],
+    file { '/etc/apache2/mods-enabled/ssl.load':
+      ensure => link,
+      target => '/etc/apache2/mods-available/ssl.load',
+    } -> file { '/etc/apache2/mods-enabled/ssl.conf':
+      ensure => link,
+      target => '/etc/apache2/mods-available/ssl.conf',
+    } -> file { '/etc/apache2/sites-enabled/default-ssl':
+      ensure => link,
+      target => '/etc/apache2/sites-available/default-ssl',
+      before => Service[$cobbler_web_service],
+      notify => Service[$cobbler_web_service],
     }
   }
 
   service { $cobbler_web_service:
-    enable => true,
-    ensure => running,
+    enable     => true,
+    ensure     => running,
     hasrestart => true,
-    require => Package[$cobbler::packages::cobbler_web_package],
+    require    => Package[$cobbler::packages::cobbler_web_package],
   }
 
-  exec {"cobbler_sync":
-    command => "cobbler sync",
+  exec { "cobbler_sync":
+    command     => "cobbler sync",
     refreshonly => true,
-    require => [
-                Package[$cobbler::packages::cobbler_package],
-                Package[$cobbler::packages::dnsmasq_package],
-                ],
-    subscribe => Service[$cobbler_service],
-    notify => Service[$dnsmasq_service],
-    tries => 10,
-    try_sleep=> 3,
+    require     => [
+      Package[$cobbler::packages::cobbler_package],
+      Package[$cobbler::packages::dnsmasq_package],],
+    subscribe   => Service[$cobbler_service],
+    notify      => Service[$dnsmasq_service],
+    tries       => 10,
+    try_sleep   => 3,
   }
 
   file { "/etc/cobbler/modules.conf":
     content => template("cobbler/modules.conf.erb"),
-    owner => root,
-    group => root,
-    mode => 0644,
-    require => [
-                Package[$cobbler::packages::cobbler_package],
-                ],
-    notify => [
-               Service[$cobbler_service],
-               Exec["cobbler_sync"],
-               ],
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    require => [Package[$cobbler::packages::cobbler_package],],
+    notify  => [Service[$cobbler_service], Exec["cobbler_sync"],],
   }
 
-  file {"/etc/cobbler/settings":
+  file { "/etc/cobbler/settings":
     content => template("cobbler/settings.erb"),
-    owner => root,
-    group => root,
-    mode => 0644,
+    owner   => root,
+    group   => root,
+    mode    => 0644,
     require => Package[$cobbler::packages::cobbler_package],
-    notify => [
-               Service[$cobbler_service],
-               Exec["cobbler_sync"],
-               ],
+    notify  => [Service[$cobbler_service], Exec["cobbler_sync"],],
   }
 
-  file {"/etc/cobbler/dnsmasq.template":
+  file { "/etc/cobbler/dnsmasq.template":
     content => template("cobbler/dnsmasq.template.erb"),
-    owner => root,
-    group => root,
-    mode => 0644,
+    owner   => root,
+    group   => root,
+    mode    => 0644,
     require => [
-                Package[$cobbler::packages::cobbler_package],
-                Package[$cobbler::packages::dnsmasq_package],
-                ],
-    notify => [
-               Service[$cobbler_service],
-               Exec["cobbler_sync"],
-               Service[$dnsmasq_service],
-               ],
+      Package[$cobbler::packages::cobbler_package],
+      Package[$cobbler::packages::dnsmasq_package],],
+    notify  => [
+      Service[$cobbler_service],
+      Exec["cobbler_sync"],
+      Service[$dnsmasq_service],],
   }
 
-  file {"/etc/cobbler/pxe/pxedefault.template":
+  file { "/etc/cobbler/pxe/pxedefault.template":
     content => template("cobbler/pxedefault.template.erb"),
-    owner => root,
-    group => root,
-    mode => 0644,
+    owner   => root,
+    group   => root,
+    mode    => 0644,
     require => Package[$cobbler::packages::cobbler_package],
-    notify => [
-               Service[$cobbler_service],
-               Exec["cobbler_sync"],
-               ],
+    notify  => [Service[$cobbler_service], Exec["cobbler_sync"],],
   }
 
-  file {"/etc/cobbler/pxe/pxelocal.template":
-      content => template("cobbler/pxelocal.template.erb"),
-      owner => root,
-      group => root,
-      mode => 0644,
-      require => Package[$cobbler::packages::cobbler_package],
-      notify => [
-                 Service[$cobbler_service],
-                 Exec["cobbler_sync"],
-                 ],
+  file { "/etc/cobbler/pxe/pxelocal.template":
+    content => template("cobbler/pxelocal.template.erb"),
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    require => Package[$cobbler::packages::cobbler_package],
+    notify  => [Service[$cobbler_service], Exec["cobbler_sync"],],
   }
 
- exec { "/var/lib/tftpboot/chain.c32":
-     command => "cp /usr/share/syslinux/chain.c32 /var/lib/tftpboot/chain.c32",
-     unless => "test -e /var/lib/tftpboot/chain.c32",
-     require => [
-                 Package[$cobbler::packages::cobbler_additional_packages],
-                 Package[$cobbler::packages::cobbler_package],
-                 ]
- }
+  exec { "/var/lib/tftpboot/chain.c32":
+    command => "cp /usr/share/syslinux/chain.c32 /var/lib/tftpboot/chain.c32",
+    unless  => "test -e /var/lib/tftpboot/chain.c32",
+    require => [
+      Package[$cobbler::packages::cobbler_additional_packages],
+      Package[$cobbler::packages::cobbler_package],]
+  }
 }
