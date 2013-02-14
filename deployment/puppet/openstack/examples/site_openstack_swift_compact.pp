8b3154dfc2c460314a59b180313b9b84184eba05
Vladimir Kuklin <vkuklin@mirantis.com>
2013-02-14 15:53:02 +0400

    [FUEL-467] site*compact.pp docs fix

--- deployment/puppet/openstack/examples/site_openstack_swift_compact.pp
+++ deployment/puppet/openstack/examples/site_openstack_swift_compact.pp
@@ -5,6 +5,10 @@
 # Please consult with the latest Fuel User Guide before making edits.
 #
 
+### GENERAL CONFIG ###
+
+# This section sets main parameters such as hostnames and IP addresses of different nodes
+
 # This is a name of public interface. Public network provides address space for Floating IPs, as well as public IP accessibility to the API endpoints.
 $public_interface    = 'eth1'
 
@@ -21,62 +25,25 @@ $public_virtual_ip   = '10.0.215.253'
 # Map of controller IP addresses on internal interfaces. Must have an entry for every controller node.
 $controller_internal_addresses = {'fuel-controller-01' => '10.0.126.3','fuel-controller-02' => '10.0.126.4','fuel-controller-03' => '10.0.126.5'}
 
-# Specify pools for Floating IP and Fixed IP.
-# Floating IP addresses are used for communication of VM instances with the outside world (e.g. Internet).
-# Fixed IP addresses are typically used for communication between VM instances.
-$create_networks = true
-$fixed_range     = '10.0.198.128/27'
-$floating_range  = '10.0.74.128/28'
-$num_networks    = 1
-$network_size    = 31
-$vlan_start      = 300
+# Set internal address on which services should listen.
+# We assume that this IP will is equal to one of the haproxy 
+# backends. If it is not equal, this may break things
+# So leave it unchanged unless you know what you are doing
 
-# If $external_ipinfo option is not defined the addresses will be calculated automatically from $floating_range:
-# the first address will be defined as an external default router
-# second address will be set to an uplink bridge interface (br-ex)
-# remaining addresses are utilized for ip floating pool
-$external_ipinfo = {}
-## $external_ipinfo = {
-##   'public_net_router' => '10.0.74.129',
-##   'ext_bridge'        => '10.0.74.130',
-##   'pool_start'        => '10.0.74.131',
-##   'pool_end'          => '10.0.74.142',
-## }
-
-# For VLAN networks: valid VLAN VIDs are 1 through 4094.
-# For GRE networks: Valid tunnel IDs are any 32 bit unsigned integer.
-$segment_range   = '900:999'
-
-# it should be set to an integer value (valid range is 0..254)
-$deployment_id = '79'
-
-# Here you can enable or disable different services, based on the chosen deployment topology.
-$multi_host              = true
-$quantum                 = true
-$manage_volumes          = true
-$cinder                  = true
-$cinder_on_computes      = $cinder
-$auto_assign_floating_ip = false
-$glance_backend          = 'swift'
-
-# Use loopback device for swift
-# set 'loopback' or false
-$swift_loopback = 'loopback'
+$internal_address = getvar("::ipaddress_${internal_interface}")
 
 # Set master hostname for the HA cluster of controller nodes, as well as hostnames for every controller in the cluster.
 $master_hostname = 'fuel-controller-01'
+
+# Controllers hostnames array. Used by some services. MUST be equal to $controller_internal_addresses keys
+
 $controller_hostnames = ['fuel-controller-01', 'fuel-controller-02', 'fuel-controller-03']
 
-# Set up OpenStack network manager
-$network_manager = 'nova.network.manager.FlatDHCPManager'
+#Specify if your installation is single-node or not. Defaults to true as this is the most common scenario
 
-# Setup network interface, which Cinder used for export iSCSI targets.
-$cinder_iscsi_bind_iface = $internal_interface
+$multi_host              = true
 
-# Here you can add physical volumes to cinder. Please replace values with the actual names of devices.
-$nv_physical_volume     = ['/dev/sdz', '/dev/sdy', '/dev/sdx'] 
-
-# Specify credentials for different services
+# Specify DB credentials for different services
 $mysql_root_password     = 'nova'
 $admin_email             = 'openstack@openstack.org'
 $admin_password          = 'nova'
@@ -100,9 +67,154 @@ $quantum_user_password   = 'quantum_pass'
 $quantum_db_password     = 'quantum_pass'
 $quantum_db_user         = 'quantum'
 $quantum_db_dbname       = 'quantum'
+
+
+### GENERAL CONFIG END ###
+
+### NETWORK/QUANTUM ###
+# Specify network/quantum specific settings
+
+# Should we use quantum or nova-network(deprecated).
+# Consult openstack docs for differences between them
+$quantum                 = true
+
+# Specify networks creation details.
+
+# Should puppet manifests create networks ?
+$create_networks = true
+# Fixed IP addresses are typically used for communication between VM instances.
+$fixed_range     = '10.0.198.128/27'
+# Floating IP addresses are used for communication of VM instances with the outside world (e.g. Internet).
+$floating_range  = '10.0.74.128/28'
+
+# These parameters are passed to  openstack network creation utility (e.g. nova-manage network create).
+# Not used in quantum.
+# Consult openstack docs for corresponding network manager.
+
+$num_networks    = 1
+$network_size    = 31
+$vlan_start      = 300
+
+# Quantum
+
+# Segmentation type for isolating traffic between tenants
+# Consult Openstack Quantum docs 
+
 $tenant_network_type     = 'gre'
+
+#Which IP to use to communicate with quantum ?
+
 $quantum_host            = $internal_virtual_ip
 
+# If $external_ipinfo option is not defined the addresses will be calculated automatically from $floating_range:
+# the first address will be defined as an external default router
+# second address will be set to an uplink bridge interface (br-ex)
+# remaining addresses are utilized for ip floating pool
+
+$external_ipinfo = {}
+## $external_ipinfo = {
+##   'public_net_router' => '10.0.74.129',
+##   'ext_bridge'        => '10.0.74.130',
+##   'pool_start'        => '10.0.74.131',
+##   'pool_end'          => '10.0.74.142',
+## }
+
+# Quantum segmentation range.
+# For VLAN networks: valid VLAN VIDs are 1 through 4094.
+# For GRE networks: Valid tunnel IDs are any 32 bit unsigned integer.
+$segment_range   = '900:999'
+
+# Set up OpenStack network manager. It is used ONLY in nova-network.
+# Consult Openstack nova-network docs for possible values.
+$network_manager = 'nova.network.manager.FlatDHCPManager'
+
+# Assign floating IPs to VMs on startup automatically?
+$auto_assign_floating_ip = false
+
+# Which string to put into quantum.conf
+
+$quantum_sql_connection  = "mysql://${quantum_db_user}:${quantum_db_password}@${quantum_host}/${quantum_db_dbname}"
+
+### NETWORK/QUANTUM END ###
+
+
+# This parameter specifies the id of current deployment. This is needed in case of multiple environment
+# instalation. Set it for unique integer value for every environment. 
+# It should be set to an integer value (valid range is 0..254)
+$deployment_id = '79'
+
+# Here you can enable or disable different services, based on the chosen deployment topology.
+
+### CINDER/VOLUME ###
+
+# Should we use cinder or nova-volume(obsolete)
+# Consult openstack docs for differences between them
+$cinder                  = true
+
+# Should we install cinder on compute nodes?
+$cinder_on_computes      = $cinder
+
+#Set it to true if your want cinder-volume been installed to the host
+#Otherwise it will install api and scheduler services
+$manage_volumes          = true
+
+# Setup network interface, which Cinder used for export iSCSI targets.
+# This interface defines which IP to use to listen on iscsi port for
+# incoming connections of initiators
+
+$cinder_iscsi_bind_iface = $internal_interface
+
+# Here you can add physical volumes to cinder. Please replace values with the actual names of devices.
+# This parameter defines which partitions to aggregate into cinder-volumes or nova-volumes LVM VG
+# !!
+# BE REALLY CAREFUL WITH THIS. IF THIS PARAMETER IS DEFINED, IT WILL AGGREGATE THE VOLUMES INTO LVM VG
+# AND THE DATA THAT RESIDES ON THIS VOLUMES WILL BE LOST.
+# !!
+# Leave this parameter empty if you want to create [cinder|nova]-volumes VG by yourself
+
+$nv_physical_volume     = ['/dev/sdz', '/dev/sdy', '/dev/sdx'] 
+
+
+### CINDER/VOLUME END ###
+
+### GLANCE and SWIFT ###
+
+# Which backend to use for glance
+# Currently supported are "swift" and "file"
+$glance_backend          = 'swift'
+
+# Use loopback device for swift
+# set 'loopback' or false
+# This controls where swift partiotions
+# are located: on physical partitions
+# or inside loopback devices
+$swift_loopback = 'loopback'
+
+# Which ip to bind particular service during swift components
+# configuration: e.g., which ip swift-proxy should listen on
+
+$swift_local_net_ip      = $internal_address
+
+# IP node of controller used during swift installation
+# and put into swift configs
+
+$controller_node_public  = $internal_virtual_ip
+
+# Set fqdn of swift_master.
+# It tells on which swift proxy node to build 
+# *ring.gz files. Other swift proxies/storages
+# will rsync them
+
+$swift_master            = $master_hostname
+
+# Hash of proxies hostname|fqdn => ip mappings.
+# This is used by controller_ha.pp manifests for haproxy setup
+# of swift_proxy backends
+$swift_proxies           = $controller_internal_addresses
+
+### Glance and swift END ###
+
+### Syslog ###
 $use_syslog = false
 if $use_syslog {
 class { "::rsyslog::client": 
@@ -112,6 +224,9 @@ class { "::rsyslog::client":
     port => '514'
  }
 }
+
+### Syslog END ###
+
 # OpenStack packages to be installed
 $openstack_version = {
   'keystone'   => 'latest',
@@ -122,28 +237,22 @@ $openstack_version = {
   'cinder'     => 'latest',
 }
 
+# Which package repo mirror to use. Currently "external" is used.
+# "internal" is used by Mirantis for testing purposes
+# Customization of internal repo is planned to be allowed in
+# future releases. 
+
+# If you want to setup it, you will need to rewrite mirantis_repos.pp,
+# though it is NOT  recommended.
+
 $mirror_type = 'external'
 
-$internal_address = getvar("::ipaddress_${internal_interface}")
-$quantum_sql_connection  = "mysql://${quantum_db_user}:${quantum_db_password}@${quantum_host}/${quantum_db_dbname}"
 
-$swift_local_net_ip      = $internal_address
-$controller_node_public  = $internal_virtual_ip
-$swift_master            = $master_hostname
-$swift_proxies           = $controller_internal_addresses
+# This parameter specifies the verboseness of log messages
+# in openstack components config. Currently, it disables or enables
+# debug
 
 $verbose = true
-Exec { logoutput => true }
-
-# Globally apply an environment-based tag to all resources on each node.
-tag("${::deployment_id}::${::environment}")
-
-stage { 'openstack-custom-repo': before => Stage['main'] }
-class { 'openstack::mirantis_repos': stage => 'openstack-custom-repo', type=> $mirror_type }
-
-if $::operatingsystem == 'Ubuntu' {
-  class { 'openstack::apparmor::disable': stage => 'openstack-custom-repo' }
-}
 
 #Rate Limits for cinder and Nova
 #Cinder and Nova can rate-limit your requests to API services
@@ -160,6 +269,23 @@ $cinder_rate_limits = { 'POST' => 1000,
  'POST_SERVERS' => 1000,
  'PUT' => 1000, 'GET' => 1000,
  'DELETE' => 1000 }
+
+
+Exec { logoutput => true }
+
+### END OF PUBLIC CONFIGURATION PART ###
+
+# Normally, you do not need to change anything after this string 
+
+# Globally apply an environment-based tag to all resources on each node.
+tag("${::deployment_id}::${::environment}")
+
+stage { 'openstack-custom-repo': before => Stage['main'] }
+class { 'openstack::mirantis_repos': stage => 'openstack-custom-repo', type=> $mirror_type }
+
+if $::operatingsystem == 'Ubuntu' {
+  class { 'openstack::apparmor::disable': stage => 'openstack-custom-repo' }
+}
 
 sysctl::value { 'net.ipv4.conf.all.rp_filter': value => '0' }
 
