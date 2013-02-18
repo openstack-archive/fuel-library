e999c0f26797dc42252d3b8d44e7ede39809f11a
Sergey Vasilenko <svasilenko@mirantis.com>
2013-02-18 15:32:18 +0400

    recommended by QA

--- deployment/puppet/l23network/lib/facter/fqdn_hostname.rb
+++ deployment/puppet/l23network/lib/facter/fqdn_hostname.rb
@@ -3,9 +3,7 @@
 # Purpose: Return FQDN or hostname if FQDN undefined
 #
 Facter.add(:l3_fqdn_hostname) do
-  hostname_simple = Facter.value(:hostname)
-  hostname_fqdn = Facter.value(:fqdn)
   setcode do
-    rv = hostname_fqdn || hostname_simple
+    rv = Facter.value(:fqdn) || Facter.value(:hostname)
   end
 end
