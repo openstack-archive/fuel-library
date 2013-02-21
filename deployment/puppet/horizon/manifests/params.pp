3f9ec1c5ab88e8e5770582e75cf7678b2e996814
Michael Polenchuk <mpolenchuk@mirantis.com>
2013-02-21 11:13:01 +0400

    (FUEL-450) Horizon https support

--- deployment/puppet/horizon/manifests/params.pp
+++ deployment/puppet/horizon/manifests/params.pp
@@ -6,30 +6,43 @@ class horizon::params {
 
   case $::osfamily {
     'RedHat': {
-      $http_service                = 'httpd'
-      $http_modwsgi                = 'mod_wsgi'
-      $package_name                = 'openstack-dashboard'
-      $httpd_listen_config_file    = '/etc/httpd/conf/httpd.conf'
-      $config_file                 = '/etc/httpd/conf.d/openstack-dashboard.conf'
-      $local_settings_path         = '/etc/openstack-dashboard/local_settings'
+      $http_service              = 'httpd'
+      $vhosts_file               = '/etc/httpd/conf.d/ssl.conf'
+      $http_modwsgi              = 'mod_wsgi'
+      $package_name              = 'openstack-dashboard'
+      $httpd_listen_config_file  = '/etc/httpd/conf.d/ports.conf'
+      $local_settings_path       = '/etc/openstack-dashboard/local_settings'
+      $root_url                  = '/dashboard'
+      $apache_user               = 'apache'
+      $apache_group              = 'apache'
+      $ssl_key_group             = 'root'
+      $ssl_dir                   = '/etc/pki/tls'
     }
     'Debian': {
-      $http_service                = 'apache2'
-      $config_file                 = '/etc/apache2/conf.d/openstack-dashboard.conf'
-      $httpd_listen_config_file    = '/etc/apache2/ports.conf'
-      $http_modwsgi                = 'libapache2-mod-wsgi'
+      $http_service              = 'apache2'
+      $vhosts_file               = '/etc/apache2/sites-available/openstack-dashboard.conf'
+      $local_settings_path       = '/etc/openstack-dashboard/local_settings.py'
+      $httpd_listen_config_file  = '/etc/apache2/ports.conf'
+      $http_modwsgi              = 'libapache2-mod-wsgi'
+      $root_url                  = '/horizon'
+      $apache_user               = 'www-data'
+      $apache_group              = 'www-data'
+      $ssl_key_group             = 'ssl-cert'
+      $ssl_dir                   = '/etc/ssl'
       case $::operatingsystem {
         'Debian': {
-            $package_name          = 'openstack-dashboard-apache'
+            $package_name        = 'openstack-dashboard-apache'
         }
         default: {
-            $package_name          = 'openstack-dashboard'
+            $package_name        = 'openstack-dashboard'
         }
       }
-      $local_settings_path         = '/etc/openstack-dashboard/local_settings.py'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only support osfamily RedHat and Debian")
     }
   }
+
+  $ssl_cert_file = "${ssl_dir}/certs/horizon.pem"
+  $ssl_key_file  = "${ssl_dir}/private/horizon.key"
 }
