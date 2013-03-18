0dfefd81ec1918843314cca675ddea5ead8a617c
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2013-03-18 23:41:20 +0400

    Added rsyslog one client multiple servers

--- deployment/puppet/rsyslog/manifests/client.pp
+++ deployment/puppet/rsyslog/manifests/client.pp
@@ -6,8 +6,13 @@ class rsyslog::client (
   $custom_config  = undef,
   $server         = 'log',
   $port           = '514',
-  $escapenewline  = false
-) inherits rsyslog {
+  $escapenewline  = false,
+  $rservers       = undef
+  ) inherits rsyslog {
+
+  if $rservers == undef {
+    $rservers = [{'remote_type'=>$remote_type, 'server'=>$server, 'port'=>$port}]
+  }
 
   $content_real = $custom_config ? {
     ''      => template("${module_name}/client.conf.erb"),
