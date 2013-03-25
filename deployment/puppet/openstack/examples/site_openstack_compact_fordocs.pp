b825c05b8c36568fd74407768cb1c905b4d4b99a
Bartosz Kupidura <bkupidura@mirantis.com>
2013-03-25 13:19:22 +0100

    Better keepalived priorities

--- deployment/puppet/openstack/examples/site_openstack_compact_fordocs.pp
+++ deployment/puppet/openstack/examples/site_openstack_compact_fordocs.pp
@@ -521,7 +521,10 @@ class compact_controller (
     cinder_rate_limits      => $cinder_rate_limits,
     horizon_use_ssl         => $horizon_use_ssl,
     use_unicast_corosync    => $use_unicast_corosync,
-    ha_provider             => $ha_provider
+    ha_provider             => $ha_provider,
+    internal_virtual_ip_mask => '24',
+    public_virtual_ip_mask  => '24',
+    keepalived_vrrp_script  => "killall -0 haproxy"
   }
   class { 'swift::keystone::auth':
     password         => $swift_user_password,
