12c14d8a7b37412ea7232978b20a791407d73b92
Bogdan Dobrelya <bdobrelia@mirantis.com>
2013-04-15 12:56:09 +0300

    Fix runas for keystone-manage pki_setup
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/keystone/manifests/init.pp
+++ deployment/puppet/keystone/manifests/init.pp
@@ -245,14 +245,11 @@ class keystone(
     # keystone-manage pki_setup Should be run as the same system user that will be running the Keystone service to ensure 
     # proper ownership for the private key file and the associated certificates
     exec { 'keystone-manage pki_setup':
-      creates => '/etc/keystone/ssl/private/signing_key.pem',
       path        => '/usr/bin',
       user        => 'keystone',
-      group       => 'keystone',
       refreshonly => true,
       notify      => Service['keystone'],
       subscribe   => Package['keystone'],
-      require     => User['keystone'],
     }
   }
 
