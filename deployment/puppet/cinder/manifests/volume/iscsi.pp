5543870e328be5f511865c0d750b45e9d7c5fb30
Sergey Vasilenko <svasilenko@mirantis.com>
2013-04-18 14:25:26 +0400

    reformat

--- deployment/puppet/cinder/manifests/volume/iscsi.pp
+++ deployment/puppet/cinder/manifests/volume/iscsi.pp
@@ -3,7 +3,7 @@ class cinder::volume::iscsi (
   $iscsi_ip_address,
   $volume_group      = 'cinder-volumes',
   $iscsi_helper      = 'tgtadm',
-  $physical_volume = undef,
+  $physical_volume   = undef,
 ) {
 
   include cinder::params
@@ -16,56 +16,54 @@ class cinder::volume::iscsi (
 
   case $iscsi_helper {
     'tgtadm': {
-      package { 'tgt':
-        name   => $::cinder::params::tgt_package_name,
-        ensure => present,
-      }
-      service { 'tgtd':
-        name    => $::cinder::params::tgt_service_name,
-        ensure  => running,
-        enable  => true,
-        require => Class['cinder::volume'],
-      }
-  file_line { 'tgtd_include':
-   path => '/etc/tgt/targets.conf',
-   line => 'include /etc/cinder/volumes/*',
-   require => Package["tgt"],
-   before => Service["tgtd"],
-  notify => Service["tgtd"]
-  }
+        package { 'tgt':
+          name    => $::cinder::params::tgt_package_name,
+          ensure  => present,
+        }
+        service { 'tgtd':
+          name    => $::cinder::params::tgt_service_name,
+          ensure  => running,
+          enable  => true,
+          require => Class['cinder::volume'],
+        }
+        file_line { 'tgtd_include':
+          path    => '/etc/tgt/targets.conf',
+          line    => 'include /etc/cinder/volumes/*',
+          require => Package["tgt"],
+          before  => Service["tgtd"],
+          notify  => Service["tgtd"]
+        }
     }
 
     'ietadm': {
-      package { ['iscsitarget', 'iscsitarget-dkms']:
-        # name   => $::nova::params::iet_package_name,
-        ensure => present,
-      }
-
-      exec { 'enable_iscsitarget':
-        command => "/bin/sed -i 's/false/true/g' /etc/default/iscsitarget",
-        unless  => "/bin/grep -q true /etc/default/iscsitarget",
-        require => Package['iscsitarget'],
-        notify  => Service['iscsitarget'],
-      }
-
-      service { 'iscsitarget':
-        name     => $::nova::params::iet_service_name,
-        # provider => $::nova::params::special_service_provider,
-        ensure   => running,
-        enable   => true,
-        require  => [Class['cinder::volume'], Package['iscsitarget', 'iscsitarget-dkms']],
-      }
+        package { ['iscsitarget', 'iscsitarget-dkms']:
+          # name   => $::nova::params::iet_package_name,
+          ensure => present,
+        }
+        exec { 'enable_iscsitarget':
+          command => "/bin/sed -i 's/false/true/g' /etc/default/iscsitarget",
+          unless  => "/bin/grep -q true /etc/default/iscsitarget",
+          require => Package['iscsitarget'],
+          notify  => Service['iscsitarget'],
+        }
+        service { 'iscsitarget':
+          name     => $::nova::params::iet_service_name,
+          # provider => $::nova::params::special_service_provider,
+          ensure   => running,
+          enable   => true,
+          require  => [Class['cinder::volume'], Package['iscsitarget', 'iscsitarget-dkms']],
+        }
     }
     default: {
-      fail("Unsupported iscsi helper: ${iscsi_helper}.")
+        fail("Unsupported iscsi helper: ${iscsi_helper}.")
     }
   }
 
   if ($physical_volume) {
-  class { 'lvm':
-    vg     => $volume_group,
-    pv     => $physical_volume,
-    before => Service['cinder-volume'],
+      class { 'lvm':
+        vg     => $volume_group,
+        pv     => $physical_volume,
+        before => Service['cinder-volume'],
+      }
   }
-}
 }
