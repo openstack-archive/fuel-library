e2b3e78f9debe8f072b914a51d35f4612e673bdd
Ivan Ponovarev <iponomarev@mirantis.com>
2013-04-23 14:59:56 +0400

    split firewall rules

--- deployment/puppet/openstack/manifests/firewall.pp
+++ deployment/puppet/openstack/manifests/firewall.pp
@@ -46,25 +46,96 @@ class openstack::firewall (
 #  }
 
   firewall { "000 accept all icmp requests":
-    proto  => "icmp",
-    action => "accept",
+    proto  => 'icmp',
+    action => 'accept',
+  }->
+
+  firewall { '001 accept all to lo interface':
+    proto   => 'all',
+    iniface => 'lo',
+    action  => 'accept',
+  }->
+
+  firewall { '002 accept related established rules':
+    proto   => 'all',
+    state   => ['RELATED', 'ESTABLISHED'],
+    action  => 'accept',
   }
 
-  firewall { '100 openstack-ports':
-    port   => [$ssh_port, $http_port, $https_port,$mysql_port, $mysql_backend_port, 
-      $mysql_gcomm_port, $galera_ist_port, $keystone_public_port, $swift_proxy_port,
-      $swift_object_port, $swift_container_port, $swift_account_port, $keystone_admin_port,
-      $glance_api_port, $glance_reg_port, $glance_nova_api_ec2_port, $nova_api_compute_port,
-      $glance_reg_port, $glance_nova_api_ec2_port, $nova_api_compute_port, $nova_api_metadata_port,
-      $nova_api_volume_port, $nova_vncproxy_port, $erlang_epmd_port, $erlang_rabbitmq_port,
-      $erlang_inet_dist_port, $memcached_port, $rsync_port, $iscsi_port, $quantum_api_port,
-    ],
-    proto  => tcp,
-    action => accept,
+  firewall {'020 ssh':
+    port   => $ssh_port,
+    proto  => 'tcp',
+    action => 'accept',
   }
 
-  firewall { "999 drop all other requests":
-    action => "drop",
+  firewall { '100 http':
+    port   => [$http_port, $https_port],
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+  firewall {'101 mysql':
+    port   => [$mysql_port, $mysql_backend_port, $mysql_gcomm_port, $galera_ist_port],
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+  firewall {'102 keystone':
+    port   => [$keystone_public_port,$keystone_admin_port],
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+  firewall {'103 swift':
+    port   => [$swift_proxy_port, $swift_object_port, $swift_container_port, $swift_account_port],
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+  firewall {'104 glance':
+    port   => [$glance_api_port, $glance_reg_port, $glance_nova_api_ec2_port,],
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+  firewall {'105 nova ':
+    port   => [$nova_api_compute_port,$nova_api_metadata_port,$nova_api_volume_port, $nova_vncproxy_port],
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+  firewall {'106 rabbitmq ':
+    port   => [$erlang_epmd_port, $erlang_rabbitmq_port, $erlang_inet_dist_port],
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+  firewall {'107 memcached ':
+    port   => $memcached_port,
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+  firewall {'108 rsync':
+    port   => $rsync_port,
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+  firewall {'109 iscsi ':
+    port   => $iscsi_port,
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+  firewall {'110 quantum ':
+    port   => $quantum_api_port,
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+  firewall { '999 drop all other requests':
+    action => 'drop',
   }
   
 }
