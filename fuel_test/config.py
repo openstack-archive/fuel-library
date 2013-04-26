88782181718f7ef463bc52fdf2e03d1836a51f87
monroid <a.urlapova@gmail.com>
2013-04-26 20:22:50 +0400

    Major fixes for generation of config.yaml

--- fuel_test/config.py
+++ fuel_test/config.py
@@ -1,87 +1,73 @@
+import yaml
 from fuel_test.ci.ci_vm import CiVM
-from fuel_test.settings import CURRENT_PROFILE, PUPPET_VERSION, INTERFACES
+from fuel_test.manifest import Manifest
+from fuel_test.settings import CURRENT_PROFILE, PUPPET_VERSION
 
-class Config(object):
-    @classmethod
-    def generate(cls, ci):
+class Config():
+    def generate(self, ci):
         config = {
-            "common":
-                { "orchestrator_common": cls.orchestrator_common(ci),
-                  "openstack_common": cls.openstack_common(ci),
-                  "cobbler_common": cls.cobbler_common(ci),
-                  }
-        }.update(cls.cobbler_nodes(ci.nodes))
+           "common":
+               {"orchestrator_common": self.orchestrator_common(ci),
+                "openstack_common": self.openstack_common(ci),
+                "cobbler_common": self.cobbler_common(ci),
+              }
+        }
 
-    @classmethod
-    def orchestrator_common(cls):
+        config.update(self.cobbler_nodes(ci))
+
+        return config
+
+    def orchestrator_common(self, ci):
         config = {"task_uuid": "deployment_task"}
         attributes = {"attributes": {"deployment_mode": "multinode_compute", "deployment_engine": "simplepuppet"}}
         config.update(attributes)
 
         return config
 
-    @classmethod
-    def openstack_common(cls, ci):
-        external_ip_info = {"ext_bridge": "10.49.54.15",
-                            "pool_end": "10.49.54.239",
-                            "pool_start": "10.49.54.225",
-                            "public_net_router": "10.49.54.1"
-        }
-
+    def openstack_common(self, ci, quantums=[]):
         nodes = []
-        for node in ci.nodes().cobblers + ci.nodes().computes:
-            nodes.appent({"internal_address": "10.0.0.100",
-                   "name": str(node.name),
-                   "public_address": node.get_ip_address_by_network_name('public'),
-                   "role": str(node.role)})
 
-        for node in ci.nodes().controllers + ci.nodes().storages:
-            nodes.appent({"internal_address": "10.0.0.101",
-                          "mountpoints": "1 2\n 2 1",
-                          "name": str(node.name),
-                          "public_address": "10.0.1.101",
-                          "role": str(node.role),
-                          "storage_local_net_ip": "10.0.0.101",
-                          "swift_zone": "1"})
+        for node in Manifest().generate_nodes_configs_list(ci):
+            if node["role"] is not "master":
+                nodes.append(node)
 
-        config = {"auto_assign_floating_ip": "true",
-                  "cinder": "true",
-                  "cinder_on_computes": "true",
-                  "create_networks": "true",
-                  "default_gateway": "10.0.1.100",
-                  "deployment_id": "53",
-                  "dns_nameservers": ["10.0.0.100", "8.8.8.8"],
-                  "external_ip_info": external_ip_info,
-                  "fixed_range": "192.168.0.0/16",
-                  "floating_range": "10.49.54.0/24",
-                  "internal_interface": "eth0",
-                  "internal_netmask": "255.255.255.0",
-                  "internal_virtual_ip": "10.49.63.127",
-                  "mirror_type": "custom",
-                  "nagios_master": "%s.your-domain-name.com" % ci.nodes().controllers[0].name,
-                  "network_manager": "nova.network.manager.FlatDHCPManager",
-                  "nv_physical_volumes": ["/dev/sdz", "/dev/sdy"],
-                  "private_interface": INTERFACES["private"],
-                  "public_interface": INTERFACES["public"],
-                  "public_netmask": "255.255.255.0",
-                  "public_virtual_ip": "10.49.54.127",
-                  "quantum": "true",
-                  "quantum_netnode_on_cnt": "true",
-                  "repo_proxy": "http://10.0.0.100:3128",
-                  "segment_range": "900:999",
-                  "swift": "true",
-                  "swift_loopback": "loopback",
-                  "syslog_server": "10.49.63.12",
-                  "use_syslog": "true",
+        config = {"auto_assign_floating_ip": True,
+                   "cinder": True,
+                   "cinder_on_computes": True,
+                   "create_networks": True,
+                   "default_gateway": ci.public_router(),
+                   "deployment_id": Manifest().deployment_id(ci),
+                   "dns_nameservers": Manifest().generate_dns_nameservers_list(ci),
+                   #"external_ip_info": Manifest().external_ip_info(ci, quantums),
+                   "fixed_range": Manifest().fixed_network(ci),
+                   "floating_range": Manifest().floating_network(ci),
+                   "internal_interface": Manifest().internal_interface(),
+                   "internal_netmask": ci.internal_net_mask(),
+                   "internal_virtual_ip": ci.internal_virtual_ip(),
+                   "mirror_type": Manifest().mirror_type(),
+                   "nagios_master": "%s.your-domain-name.com" % ci.nodes().controllers[0].name,
+                   "network_manager": "nova.network.manager.FlatDHCPManager",
+                   "nv_physical_volumes": ["/dev/sdz", "/dev/sdy"],
+                   "private_interface": Manifest().private_interface(),
+                   "public_interface": Manifest().public_interface(),
+                   "public_netmask": ci.public_net_mask(),
+                   "public_virtual_ip": ci.public_virtual_ip(),
+                   "quantum": True,
+                   "quantum_netnode_on_cnt": True,
+                   "repo_proxy": "http://10.0.0.100:3128",
+                   "segment_range": "900:999",
+                   "swift": True,
+                   "swift_loopback": "loopback",
+                   "syslog_server": "10.49.63.12",
+                   "use_syslog": True,
                   "nodes": nodes
         }
 
         return config
 
-    @classmethod
-    def cobbler_common(cls, ci):
-        config = {"gateway": "10.0.0.100",
-                  "name-servers": "10.0.0.100",
+    def cobbler_common(self, ci):
+        config = {"gateway": ci.public_router(),
+                  "name-servers": ci.public_router(),
                   "name-servers-search": "your-domain-name.com",
                   "profile": CURRENT_PROFILE}
 
@@ -104,14 +90,51 @@ class Config(object):
 
         return config
 
-    @classmethod
-    def cobbler_nodes(cls, nodes):
+    def cobbler_nodes(self, ci):
+        nodes = Manifest().generate_nodes_configs_list(ci)
         all_nodes = {}
-        for node in nodes():
-            node_name = str(node.name)
+        for node in nodes:
+            node_name = str(node["name"])
+            interfaces = {"eth0":
+                              {"mac": ci.public_net_mask(),
+                               "static": 1,
+                               "ip-address": node["public_address"],
+                               "netmask": "255.255.255.0",
+                               "dns-name": "%s.your-domain-name.com" % node_name,
+                               "management": "1"},
+                          "eth1":
+                              {"mac": ci.internal_net_mask(),
+                               "static": "0"},
+                          "eth2":
+                              {"mac": ci.public_net_mask(),
+                               "static": "1"}
+            }
+
+            interfaces_extra = {"eth0":
+                                    {"peerdns": 'no'},
+                                "eth1":
+                                    {"peerdns": 'no'},
+                                "eth2":
+                                    {"peerdns": 'no',
+                                     "promisc": 'yes',
+                                     "userctl": 'yes'}
+            }
+
             all_nodes.update({node_name: {"hostname": node_name,
-                                          "interfaces": interfacse,
+                                          "interfaces": interfaces,
                                           "interfaces_extra": interfaces_extra,
-                                          "role": node.role}})
+                                          "role": node["role"]}
+                            }
+            )
 
         return all_nodes
+
+
+if __name__ == "__main__":
+    ci = CiVM()
+    config = Config().generate(ci)
+    print yaml.safe_dump(config, default_flow_style=False)
+
+    #with open("/home/alan/fuel/deployment/mcollective/astute/samples/config_test.yaml", "r") as f:
+    #    config = yaml.safe_load(f)
+    #   print config
