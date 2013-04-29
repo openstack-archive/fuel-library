8a74eedfa2a2b9ca8bc036a3737ef9458c7f780d
Matthew Mosesohn <mmosesohn@mirantis.com>
2013-04-29 16:11:23 +0400

    updated nose tests for cinder_nodes

--- fuel_test/cobbler/test_compact.py
+++ fuel_test/cobbler/test_compact.py
@@ -48,6 +48,19 @@ class CompactTestCase(CobblerTestCase):
         if CREATE_SNAPSHOTS:
             self.environment().snapshot('compact_wo_quantum', force=True)
 
+    def test_deploy_compact_wo_quantum_cinder_all(self): 
+        Manifest().write_openstack_manifest( 
+            remote=self.remote(), 
+            template=Template.compact(), ci=self.ci(), 
+            controllers=self.nodes().controllers, 
+            cinder=True,
+            cinder_nodes=['all'],
+            quantums=self.nodes().quantums, 
+            quantum=False) 
+        self.deploy_compact(quantum_node=False) 
+        if CREATE_SNAPSHOTS: 
+            self.environment().snapshot('compact_wo_quantum', force=True) 
+ 
     def test_deploy_compact_wo_loopback(self):
         Manifest().write_openstack_manifest(
             remote=self.remote(),
