7fd4c35dca3a50ad9cff02fd03644814005ae3ba
Bogdan Dobrelya <bogdando@mail.ru>
2013-05-28 12:18:54 +0300

    Change default keystone to PKI
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- deployment/puppet/keystone/manifests/init.pp
+++ deployment/puppet/keystone/manifests/init.pp
@@ -22,7 +22,6 @@
 #   [catalog_type] Type of catalog that keystone uses to store endpoints,services. Optional.
 #     Defaults to sql. (Also accepts template)
 #   [token_format] Format keystone uses for tokens. Optional. Defaults to PKI.
-#   [token_format] Format keystone uses for tokens. Optional. Defaults to UUID.
 #     Supports PKI and UUID.
 #   [cache_dir] Directory created when token_format is PKI. Optional.
 #     Defaults to /var/cache/keystone.
@@ -59,9 +58,7 @@ class keystone(
   $debug          = 'False',
   $use_syslog     = false,
   $catalog_type   = 'sql',
-  $token_format   = 'UUID',
-# TODO fix "undefined method `<<' for {}:Hash" issue if PKI was choosed
-#  $token_format   = 'PKI',
+  $token_format   = 'PKI',
   $cache_dir      = '/var/cache/keystone',
   $enabled        = true,
   $sql_connection = 'sqlite:////var/lib/keystone/keystone.db',
