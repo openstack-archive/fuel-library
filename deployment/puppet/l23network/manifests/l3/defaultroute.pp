ac8e2b1483e0832457322d9b5aadefe6cbf39fa9
Sergey Vasilenko <svasilenko@mirantis.com>
2013-06-12 17:56:55 +0400

    L23network 'save default gateway' mechanics more powerful

--- deployment/puppet/l23network/manifests/l3/defaultroute.pp
+++ deployment/puppet/l23network/manifests/l3/defaultroute.pp
@@ -3,18 +3,21 @@
 # Do not use this directly,
 # use l23network::l3::route instead
 #
-class l23network::l3::defaultroute (
+define l23network::l3::defaultroute (
     $gateway = $name,
-    $metric      = undef,
+    $metric  = undef,
 ){
   case $::osfamily {
     /(?i)debian/: {
         fail("Unsupported for ${::osfamily}/${::operatingsystem}!!! Specify gateway directly for network interface.")
     }
     /(?i)redhat/: {
-        cfg { 'GATEWAY':
-            file => '/etc/sysconfig/network',
-            value => $gateway,
+        if ! defined(Cfg[$gateway]) {
+          cfg { $gateway:
+              file  => '/etc/sysconfig/network',
+              key   => 'GATEWAY',
+              value => $gateway,
+          }
         }
     }
     default: {
