c3401fa7df3f3dea701b31e3ba3351bee9d240f9
Sergey Vasilenko <svasilenko@mirantis.com>
2013-06-24 19:46:13 +0400

    remove very-very-very-old puppet workarround

--- deployment/puppet/concat/manifests/setup.pp
+++ deployment/puppet/concat/manifests/setup.pp
@@ -25,16 +25,11 @@ class concat::setup {
     fail ("\$concat_basedir not defined. Try running again with pluginsync enabled")
   }
 
-  $majorversion = regsubst($::puppetversion, '^[0-9]+[.]([0-9]+)[.][0-9]+$', '\1')
-
   file{"${concatdir}/bin/concatfragments.sh":
     owner  => $id,
     group  => $root_group,
     mode   => '0755',
-    source => $majorversion ? {
-      24      => 'puppet:///concat/concatfragments.sh',
-      default => 'puppet:///modules/concat/concatfragments.sh'
-    };
+    source => 'puppet:///modules/concat/concatfragments.sh';
 
   [ $concatdir, "${concatdir}/bin" ]:
     ensure => directory,
