3e07713a4933db5ec56807174fadb4601468d6c9
Sergey Vasilenko <svasilenko@mirantis.com>
2013-07-08 18:15:20 +0400

    secret for Q-M-agent

--- deployment/puppet/openstack/examples/site_openstack_ha_compact.pp
+++ deployment/puppet/openstack/examples/site_openstack_ha_compact.pp
@@ -172,6 +172,10 @@ $quantum_db_dbname       = 'quantum'
 $quantum                 = true
 $quantum_netnode_on_cnt  = true
 
+# a string "password" value that should be configured to authenticate requests for metadata
+# from quantum-metadata-proxy to nova-api
+$quantum_metadata_proxy_shared_secret = "connecting_nova-api_and_quantum-metadata-agent"
+
 # Specify network creation criteria:
 # Should puppet automatically create networks?
 $create_networks = true
