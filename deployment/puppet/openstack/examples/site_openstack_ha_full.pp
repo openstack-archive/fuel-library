ee77f42682177c3529ca6b4880823945bcb2d4e3
Matthew Mosesohn <mmosesohn@mirantis.com>
2013-07-18 18:42:42 +0400

    fixed missing dollar sign

--- deployment/puppet/openstack/examples/site_openstack_ha_full.pp
+++ deployment/puppet/openstack/examples/site_openstack_ha_full.pp
@@ -160,7 +160,7 @@ $multi_host              = true
 $mysql_root_password     = 'nova'
 $admin_email             = 'openstack@openstack.org'
 $admin_password          = 'nova'
-custom_mysql_setup_class = 'pacemaker_mysql'
+$custom_mysql_setup_class = 'pacemaker_mysql'
 validate_re($custom_mysql_setup_class,'galera|pacemaker_mysql')
 
 $keystone_db_password    = 'nova'
