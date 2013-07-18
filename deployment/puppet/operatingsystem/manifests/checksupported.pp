92c653bf331ae385188c76d7404c9d3f05b0d487
Sergey Vasilenko <svasilenko@mirantis.com>
2013-07-18 14:21:36 +0400

    Revert "add Limits module for Puppet"
    This reverts commit 98cedba41e7e8cabb7b1dad6cb752973ef464b87.
    
    * remove limit set.
    * this settings broke deploy
    * Revert "rising file limits" 11a7c94cdd2142c330950cbeaf3b134f0902a92b.

--- deployment/puppet/operatingsystem/manifests/checksupported.pp
+++ deployment/puppet/operatingsystem/manifests/checksupported.pp
@@ -18,12 +18,4 @@ class operatingsystem::checksupported (
   notify { 'operatingsystem':
     message => "Detected OS $::operatingsystem, architecture $::architecture"
   }
-
-  limits::fragment {
-    "*/soft/nofile":
-      value => "32768";
-    "*/hard/nofile":
-      value => "32768";
-  }
-
 }
