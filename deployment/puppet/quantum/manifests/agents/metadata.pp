e360a6cc0bf6c38f39f97cdb668a09d4352caead
Vladimir Kuklin <vkuklin@mirantis.com>
2013-07-24 21:12:57 +0400

    add cleanup for quantum metadata agent

--- deployment/puppet/quantum/manifests/agents/metadata.pp
+++ deployment/puppet/quantum/manifests/agents/metadata.pp
@@ -91,8 +91,10 @@ class quantum::agents::metadata (
     Cs_commit <| title == 'ovs' |> -> Cs_shadow <| title == "$res_name" |>
 
     cs_shadow { $res_name: cib => $cib_name }
+    cs_commit { $res_name: cib => $cib_name } -> ::Corosync::Cleanup["$res_name"]
     cs_commit { $res_name: cib => $cib_name } ~> ::Corosync::Cleanup["$res_name"]
     ::corosync::cleanup { $res_name: }
+    ::Corosync::Cleanup["$res_name"] -> Service[$res_name]
 
     File<| title=='quantum-logging.conf' |> ->
     cs_resource { "$res_name":
