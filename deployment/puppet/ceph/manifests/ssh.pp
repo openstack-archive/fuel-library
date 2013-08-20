7c832ed22553d02bab1bf24c80850e5b0ea8d523
Andrew Woodward <awoodward@mirantis.com>
2013-08-20 15:25:51 -0700

    move ssh key from file to ssh_authorized_key builtin

--- deployment/puppet/ceph/manifests/ssh.pp
+++ deployment/puppet/ceph/manifests/ssh.pp
@@ -18,8 +18,13 @@ class ceph::ssh{
     ensure => directory,
     mode   => '0700',
   }
-  file { '/root/.ssh/authorized_keys':
-    source => $ssh_public_key,
+  #file { '/root/.ssh/authorized_keys':
+  #  source => $ssh_public_key,
+  #}
+  ssh_authorized_key {'ceph-ssh-key':
+    ensure => present,
+    key = 'AAAAB3NzaC1yc2EAAAABIwAAAQEAnHMPz2XBZSiPoIeTcagq5fuuvOH393szgx+Qp6Ue97VUu1l/13WNTHeYpwrvtZSgdag6AGygyeGjcZwZLOXDyIMY0xIMsAA/0te+tbhuL80wUzVGtuBE73JBz0+NBxiiwJFeOEfalblS/Oa1XhMhnifMSbtyOvGLocIJjUKcE29XNPIyiwWGBl5YaxYMAQimgZtrrIrOl/lVgWT434Io6B24OwXiB8tC+puN/S0phpxK9m+k1tNGQRCaSlL060hhg9EnSzcTjJ3xHVkYNJUchtHJmZ/zjCQUJK8NPxSw9efRk4/lGrST/7/rGkr+Vycj/Ll4GFIvCAmFSx1Q7No7IQ==',
+    user = 'root',
   }
   file { '/root/.ssh/id_rsa':
     source => $ssh_private_key,
