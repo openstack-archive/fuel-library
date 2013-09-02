a5177a491af39ebcd6ecac73bf541bf18959f9fd
Andrey Korolyov <andrey@xdel.ru>
2013-09-02 18:57:15 +0400

    those files should be licensed for sure

--- deployment/puppet/galera/manifests/init.pp
+++ deployment/puppet/galera/manifests/init.pp
@@ -1,3 +1,18 @@
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+#
 # == Define: galera
 #
 # Class for installation and configuration of galer Master/Master cluster.
@@ -29,15 +44,6 @@
 # [*node_addresses*]
 #  Array with IPs/hostnames of cluster members.
 #
-# === Authors
-#
-# Mirantis Inc. <product@mirantis.com>
-#
-# === Copyright
-#
-# FIXME: Insert copyrights and licenses
-#
-
 
 class galera (
   $cluster_name,
