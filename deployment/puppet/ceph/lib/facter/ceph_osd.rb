46c4da70d5b2b5c648db203d4c80b9000980f93a
Dmitry Borodaenko <angdraug@gmail.com>
2013-09-06 10:23:30 -0700

    use "cephosd" label for osd to differentiate from osd journal

--- deployment/puppet/ceph/lib/facter/ceph_osd.rb
+++ deployment/puppet/ceph/lib/facter/ceph_osd.rb
@@ -1,6 +1,6 @@
 Facter.add("osd_devices_list") do
     setcode do
-        # Use any filesystem labeled "ceph" as an osd
-        %x{blkid -o list | awk '{if ($3 == "ceph") print $1}'}
+        # Use any filesystem labeled "cephosd" as an osd
+        %x{blkid -o list | awk '{if ($3 == "cephosd") print $1}'}
     end
 end
