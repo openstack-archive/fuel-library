1b4d91e2090a50354ae8edac51cec5ca4bbc3799
Andrew Woodward <awoodward@mirantis.com>
2013-09-13 12:15:08 -0700

    move package from ceph::glance to glance::backend::ceph

--- deployment/puppet/ceph/manifests/glance.pp
+++ deployment/puppet/ceph/manifests/glance.pp
@@ -1,3 +1,4 @@
+#ceph:glance will configure glance parts if present on the system
 class ceph::glance (
   $default_store         = $::ceph::default_store,
   $rbd_store_user        = $::ceph::rbd_store_user,
@@ -5,23 +6,23 @@ class ceph::glance (
   $show_image_direct_url = $::ceph::show_image_direct_url,
 ) {
   if str2bool($::glance_api_conf) {
-    package {['python-ceph']:
-      ensure  => latest,
-    }
     exec {'Copy config':
       command => "scp -r ${::ceph::primary_mon}:/etc/ceph/* /etc/ceph/",
       require => Package['ceph'],
       returns => 0,
     }
     if ! defined('glance::backend::ceph') {
+      package {['python-ceph']:
+        ensure  => latest,
+      }
       glance_api_config {
         'DEFAULT/default_store':           value => $default_store;
         'DEFAULT/rbd_store_user':          value => $rbd_store_user;
         'DEFAULT/rbd_store_pool':          value => $rbd_store_pool;
         'DEFAULT/show_image_direct_url':   value => $show_image_direct_url;
-      }~> Service["$::ceph::params::service_glance_api"]
-        service { "$::ceph::params::service_glance_api":
-          ensure     => "running",
+      }~> Service["${::ceph::params::service_glance_api}"]
+        service { "${::ceph::params::service_glance_api}":
+          ensure     => 'running',
           enable     => true,
           hasstatus  => true,
           hasrestart => true,
@@ -32,7 +33,7 @@ class ceph::glance (
       before  => File['/etc/ceph/ceph.client.images.keyring'],
       creates => '/etc/ceph/ceph.client.images.keyring',
       require => [Package['ceph'], Exec['Copy config']],
-      notify  => Service["$::ceph::params::service_glance_api"],
+      notify  => Service["${::ceph::params::service_glance_api}"],
       returns => 0,
     }
     file { '/etc/ceph/ceph.client.images.keyring':
--- deployment/puppet/glance/manifests/backend/ceph.pp
+++ deployment/puppet/glance/manifests/backend/ceph.pp
@@ -1,3 +1,4 @@
+#configures the glance blacked for ceph (rbd) driver
 class glance::backend::ceph(
   $default_store         = 'rbd',
   $rbd_store_user        = $::ceph::rbd_store_user,
@@ -5,6 +6,9 @@ class glance::backend::ceph(
   $show_image_direct_url = $::ceph::show_image_direct_url,
 ) inherits glance::api {
 
+  package {'python-ceph':
+    ensure => latest,
+  }
   glance_api_config {
   'DEFAULT/default_store':           value => $default_store;
   'DEFAULT/rbd_store_user':          value => $rbd_store_user;
