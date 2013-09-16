703e5ff35cfbb67f35146030f00456cc646e4acd
Dmitry Borodaenko <dborodaenko@mirantis.com>
2013-09-16 09:48:12 -0700

    use cephjournal volume label

--- deployment/puppet/ceph/lib/facter/ceph_osd.rb
+++ deployment/puppet/ceph/lib/facter/ceph_osd.rb
@@ -2,7 +2,7 @@ Facter.add("osd_devices_list") do
     setcode do
         # Use any filesystem labeled "cephosd" as an osd
         devs = %x{blkid -o list | awk '{if ($3 == "cephosd") print $1}'}.split("\n")
-        journal = %x{blkid -o list | awk '{if ($3 == "ceph-journal") print $4}'}.strip
+        journal = %x{blkid -o list | awk '{if ($3 == "cephjournal") print $4}'}.strip
 
         devs.collect! do |d|
             if journal == ''
