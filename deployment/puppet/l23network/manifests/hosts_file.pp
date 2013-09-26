5d2260bb50392a28ec097fdc6f7a01f6fa3d1d60
Vladimir Kuklin <vkuklin@mirantis.com>
2013-09-26 16:01:27 +0400

    [PRD-999] Add custom function and use Puppet Host
    Add nodes_to_hosts converter function
    Rewrite manifest to use Puppet builtin Host type
    Trigger create_resources for Host generation

--- deployment/puppet/l23network/manifests/hosts_file.pp
+++ deployment/puppet/l23network/manifests/hosts_file.pp
@@ -2,26 +2,13 @@ class l23network::hosts_file ($hosts,$hosts_file="/etc/hosts") {
 
 #Move original hosts file
 
-exec {'copy hosts file':
-  command => "cp ${hosts_file} /etc/hosts.header"
+$hosts=nodes_to_hosts($nodes)
+Host {
+    ensure   => present,
+    target => $hosts_file
 }
-#
-concat{ $hosts_file:
-  owner => root,
-  group => root,
-  mode  => '0644'
-}
-
-concat::fragment{"hosts_local":
-  target => $hosts_file,
-  ensure => "/etc/hosts.header",
-  order  => 01,
-}
-concat::fragment{"added_hosts":
-  target  => $hosts_file,
-  content => template("l23network/hosts.erb"),
-  order   => 02
-}
+notify{"$hosts":}
+create_resources(host,$hosts)
 }
 
 
