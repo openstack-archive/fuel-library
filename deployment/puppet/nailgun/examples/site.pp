1c6ef68995a88385255ce6e7b23237669da70ccd
Dmitry Ilyin <dilyin@mirantis.com>
2013-10-18 15:55:45 +0400

    Remove postgresql_version fact that slows down facter

--- deployment/puppet/nailgun/examples/site.pp
+++ deployment/puppet/nailgun/examples/site.pp
@@ -1,5 +1,8 @@
 $fuel_settings = parseyaml($astute_settings_yaml)
 
+# this replaces removed postgresql version fact
+$postgres_default_version = '8.4'
+
 node default {
 
   Exec  {path => '/usr/bin:/bin:/usr/sbin:/sbin'}
