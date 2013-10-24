35b615195b29dfcd1e15f6bed46db655e5a55688
Vladmir Sharhsov(warpc) <vsharshov@gmail.com>
2013-10-24 17:18:29 +0400

    Fix for Rsync daemon

--- deployment/puppet/nailgun/manifests/puppetsync.pp
+++ deployment/puppet/nailgun/manifests/puppetsync.pp
@@ -1,25 +1,32 @@
 class nailgun::puppetsync(
   $puppet_folder = '/etc/puppet',
-  $xinetd_folder = '/etc/xinet.d',
+  $xinetd_config = '/etc/xinetd.d/rsync',
   $rsync_config  = '/etc/rsyncd.conf',
 ){
+
+  File {
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
 
   file { 'rsync_conf' :
     path    => $rsync_config,
     content => template('nailgun/rsyncd.conf.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
   }
     
   file { 'rsync_xinetd' :
-    path    => "${xinetd_folder}/rsyncd.conf",
+    path    => $xinetd_config,
     content => template('nailgun/rsyncd_xinetd.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+  }
+  
+  if !defined(Package['rsync']) {
+     package { 'rsync':
+       ensure => installed,
+     }
   }
 
-  File['rsync_conf', 'rsync_xinetd'] ~> Service['xinetd']
+  Package['rsync'] -> File['rsync_conf', 'rsync_xinetd'] ~> Service['xinetd']
     
 }
