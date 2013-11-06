99377d27ac47affd8053e65308add356c9a9a0c3
Vladimir Kuklin <vkuklin@mirantis.com>
2013-11-06 22:17:03 +0400

    [PRD-2488] fix horizon css file generation

--- deployment/puppet/horizon/manifests/init.pp
+++ deployment/puppet/horizon/manifests/init.pp
@@ -226,6 +226,12 @@ class horizon(
                       $::horizon::params::logdir],
   }
 
+  exec {"refresh horizon static":
+    path    => ['/bin','/sbin','/usr/sbin','/usr/bin'],
+    command => "su $wsgi_user -s '/bin/bash' -c 'cd /usr/share/openstack-dashboard && python manage.py compress --force'",
+    require => [Package['dashboard']]
+  }
+
   if $cache_server_ip =~ /^127\.0\.0\.1/ {
     Class['memcached'] -> Class['horizon']
   }
