304c2484c78d0825d0ff120e3def832fd2fe7fec
Ryan Moe <rmoe@mirantis.com>
2013-11-27 10:31:52 -0800

    Make live migrations work for non volume-backed instances with RBD
    * Add a new ceph user and pool for ephemeral storage
    * Update nova configuration to store ephemeral volumes in RBD pool
    * Open libvirt ports requred for live migration
    
    Change-Id: I772c6d94de4537ad82d4b62822bdc4f702e8ff56
    Implements: blueprint ceph-live-migration

---
+++ deployment/puppet/ceph/manifests/ephemeral.pp
@@ -0,0 +1,20 @@
+class ceph::ephemeral (
+  $rbd_secret_uuid     = $::ceph::rbd_secret_uuid,
+  $libvirt_images_type = $::ceph::libvirt_images_type,
+  $pool                = $::ceph::compute_pool,
+) {
+
+  firewall {'117 libvirt ephemeral allow':
+    chain   => 'INPUT',
+    dport   => '49152-49215',
+    proto   => 'tcp',
+    action  => accept,
+  }
+
+  nova_config {
+    'DEFAULT/libvirt_images_type':      value => $libvirt_images_type;
+    'DEFAULT/libvirt_inject_key':       value => false;
+    'DEFAULT/libvirt_images_rbd_pool':  value => $pool;
+    'DEFAULT/live_migration_flag':      value => 'VIR_MIGRATE_UNDEFINE_SOURCE,VIR_MIGRATE_PEER2PEER,VIR_MIGRATE_LIVE';
+  }
+}
