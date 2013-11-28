43e26fe8fc2f439b3266cd2b4dc670bd788f64a2
Vladimir Kuklin <vkuklin@mirantis.com>
2013-11-28 17:09:26 +0400

    Fix start/cleanup race condition
    Do service cleanup on the particluar node
    if it is not installed on the node.

--- deployment/puppet/corosync/manifests/cleanup.pp
+++ deployment/puppet/corosync/manifests/cleanup.pp
@@ -14,16 +14,21 @@
 #
 # Copyright 2012 Puppet Labs, LLC.
 #
-define corosync::cleanup () {
-  Cs_resource <| name == $name |> ~> Exec["crm resource cleanup $name"]
+define corosync::cleanup (
+  $force = false,
+  $wait_before = 15
+) {
+  if $force   {
+    Cs_resource <| name == $name |> ~> Exec["crm resource cleanup $name"]
 
-  ##FIXME: we need to create a better way to workaround crm commit <-> cleanup race condition than a simple sleep 
-  #Workaround for hostname bugs with FQDN vs short hostname
-  exec { "crm resource cleanup $name":
-    command     => "bash -c \"(sleep 5 && crm_resource --resource ${name}  --cleanup --node `uname -n`) || :\"",
-    path        => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-    returns     => [0,""],
-    refreshonly => true,
-    timeout     => 600,
-  }
-}  
+    ##FIXME: we need to create a better way to workaround crm commit <-> cleanup race condition than a simple sleep 
+    #Workaround for hostname bugs with FQDN vs short hostname
+    exec { "crm resource cleanup $name":
+      command     => "bash -c \"(sleep ${wait_before} && crm_resource --resource ${name}  --cleanup --node `uname -n`) || :\"",
+      path        => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+      returns     => [0,""],
+      refreshonly => true,
+      timeout     => 600,
+    }
+  } 
+} 
