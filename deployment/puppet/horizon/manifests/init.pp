ce5d24351290cd64c9925400fcf16381b2ce847b
Andrey Korolyov <andrey@xdel.ru>
2013-12-13 17:49:05 +0400

    Move chown from puppet to the regular exec
    Change-Id: Ibc06fa6cfed154c67f3ffbda8a39e0a15b900e54
    Closes-Bug: #1255412

--- deployment/puppet/horizon/manifests/init.pp
+++ deployment/puppet/horizon/manifests/init.pp
@@ -78,8 +78,9 @@ class horizon(
     mode    => '0644',
   }
 
-  file {'/usr/share/openstack-dashboard/':
-    recurse   => true,
+  exec { 'chown-dashboard':
+    command => "chown -R ${wsgi_user}:${wsgi_group} /usr/share/openstack-dashboard",
+    path    => '/usr/bin:/usr/sbin:/bin:/sbin',
     subscribe => Package['dashboard']
   }
 
