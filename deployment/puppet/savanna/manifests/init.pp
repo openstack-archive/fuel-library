735e1a2d96abe6b1f195128d936c5e772ec56e34
iberezovskiy <iberezovskiy@mirantis.com>
2013-12-18 20:14:37 +0400

    Fix Savanna manifests to configure syslogging
    Fixes bug #1261479
    
    Change-Id: Ie41904e7c0642dbfb62698b500735a2db43a3000

--- deployment/puppet/savanna/manifests/init.pp
+++ deployment/puppet/savanna/manifests/init.pp
@@ -27,6 +27,12 @@ class savanna (
   $savanna_firewall_rule                = '201 savanna-api',
   $use_neutron                          = false,
   $use_floating_ips                     = false,
+
+  $use_syslog                           = false,
+  $debug                                = false,
+  $verbose                              = false,
+  $syslog_log_level                     = 'WARNING',
+  $syslog_log_facility_savanna          = 'LOG_LOCAL0',
 ) {
 
   $savanna_sql_connection               = "mysql://${savanna_db_user}:${savanna_db_password}@${savanna_db_host}/${savanna_db_name}"
@@ -56,6 +62,11 @@ class savanna (
     idh_plugin_class                    => $savanna_idh_plugin_class,
     sql_connection                      => $savanna_sql_connection,
     use_neutron                         => $use_neutron,
+    debug                               => $debug,
+    use_syslog                          => $use_syslog,
+    verbose                             => $verbose,
+    syslog_log_level                    => $syslog_log_level,
+    syslog_log_facility_savanna         => $syslog_log_facility_savanna,
   }
 
   class { 'savanna::keystone::auth' :
