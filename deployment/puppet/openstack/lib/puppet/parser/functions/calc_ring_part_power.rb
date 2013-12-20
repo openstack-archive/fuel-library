e3ac711bf30240750f5ef31461f2d9dd0d835ca9
Vladimir Kuklin <vkuklin@mirantis.com>
2013-12-20 16:19:29 +0400

    Calculate partition power for swift
    Calculage partition power according to
    https://answers.launchpad.net/swift/+question/211929
    
    power = int(log2(devnumber*100)) + 1
    
    Change-Id: Icbc518b05f8170b8656b6aad701bf3cece89f74f
    Related-bug: #1261659

---
+++ deployment/puppet/openstack/lib/puppet/parser/functions/calc_ring_part_power.rb
@@ -0,0 +1,15 @@
+module Puppet::Parser::Functions
+  newfunction(:calc_ring_part_power, :type => :rvalue) do |args|
+    nodes = args[0].is_a?(Array) ? args[0] : [ args[0] ]
+     dev_number = nodes.inject(0) do |num,node|
+       if node['mountpoints']
+         add  = node['mountpoints'].split('\n')[0].split.length
+         num += add
+       else
+         num += 2
+       end
+       num
+     end  
+     ring_power = (Math.log(dev_number * 100)/Math.log(2)).to_int+1
+  end 
+end
