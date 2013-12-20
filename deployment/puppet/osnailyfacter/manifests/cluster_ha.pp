1627b3869e2d903dd9b47196c876b4392b7e4617
Dmitry Ilyin <dilyin@mirantis.com>
2013-12-20 19:13:09 +0400

    Add syslog support to heat module
    Change-Id: Ifc89d19152393394ffdcf35fd4728ce9f3a41758

--- deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
@@ -413,21 +413,22 @@ class osnailyfacter::cluster_ha {
           class { 'heat' :
             pacemaker              => true,
             external_ip            => $controller_node_public,
-
+            
             heat_keystone_host     => $controller_node_address,
             heat_keystone_user     => 'heat',
             heat_keystone_password => 'heat',
             heat_keystone_tenant   => 'services',
-
             heat_rabbit_host       => $controller_node_address,
             heat_rabbit_login      => $rabbit_hash['user'],
             heat_rabbit_password   => $rabbit_hash['password'],
             heat_rabbit_port       => '5672',
-
+            
             heat_db_host           => $controller_node_address,
             heat_db_password       => $heat_hash['db_password'],
-          }
-      }
+            
+            use_syslog             => $::fuel_settings['use_syslog'],
+        }
+
 
       if $murano_hash['enabled'] {
 
