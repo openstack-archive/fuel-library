3c77f6a59d1b2ffde273d0dc6e9895e168fec3a5
Dmitry Teselkin <dteselkin@mirantis.com>
2013-12-23 14:46:08 +0400

    Setting to use syslog in Murano added.
    * Local logs added
    * Logrotate configured
    * Typos
    * murano_use_syslog -> use_syslog
    * murano_debug -> debug
    * murano_verbose -> verbose
    * syslog_log_facility
    * murano user/group added back
    * use bool values for debug/use_syslog
    
    Replaces review https://review.openstack.org/#/c/62853/
    
    Closes-Bug: 1261477
    
    Change-Id: I81c0b87b94f2cb3e0aba7e3d90300aee8b844b1d

--- deployment/puppet/murano/manifests/conductor.pp
+++ deployment/puppet/murano/manifests/conductor.pp
@@ -1,8 +1,10 @@
 # Installs & configure the murano conductor  service
 
 class murano::conductor (
-  $log_file                            = '/var/log/murano/conductor.log',
-  $debug                               = 'True',
+  $log_file                            = '/var/log/murano/murano-conductor.log',
+  $use_syslog                          = true,
+  $syslog_log_facility                 = 'local0',
+  $debug                               = true,
   $verbose                             = 'True',
   $data_dir                            = '/var/cache/murano',
   $max_environments                    = '20',
@@ -17,7 +19,7 @@ class murano::conductor (
   $rabbit_virtual_host                 = '/',
   $init_scripts_dir                    = '/etc/murano/init-scripts',
   $agent_config_dir                    = '/etc/murano/agent-config',
-  $use_neutron                         = 'true'
+  $use_neutron                         = 'True'
 ) {
 
   include murano::params
@@ -41,15 +43,47 @@ class murano::conductor (
     $network_topology = 'nova'
   }
 
+  if $use_syslog and !$debug {
+    murano_conductor_config {
+      'DEFAULT/log_config_append'   : ensure => absent;
+      'DEFAULT/use_syslog'          : value  => true;
+      'DEFAULT/use_stderr'          : ensure => absent;
+      'DEFAULT/syslog_log_facility' : value  => $syslog_log_facility;
+      'DEFAULT/log_file'            : ensure => absent;
+    }
+
+    file { 'murano-conductor-logging.conf':
+      content => template('murano/logging.conf.erb'),
+      path    => '/etc/murano/murano-conductor-logging.conf',
+    }
+  }
+  else {
+    murano_conductor_config {
+      'DEFAULT/log_config_append'   : value  => '/etc/murano/murano-conductor-logging.conf';
+      'DEFAULT/use_syslog'          : ensure => absent;
+      'DEFAULT/use_stderr'          : ensure => absent;
+      'DEFAULT/syslog_log_facility' : ensure => absent;
+      'DEFAULT/log_file'            : value  => $log_file;
+    }
+
+    file { 'murano-conductor-logging.conf':
+      content => template('murano/logging.conf-nosyslog.erb'),
+      path    => '/etc/murano/murano-conductor-logging.conf',
+    }
+  }
+
   murano_conductor_config {
-    'DEFAULT/log_file'                 : value => $log_file;
     'DEFAULT/debug'                    : value => $debug;
     'DEFAULT/verbose'                  : value => $verbose;
     'DEFAULT/data_dir'                 : value => "${data_dir}/muranoconductor-cache";
     'DEFAULT/max_environments'         : value => $max_environments;
     'DEFAULT/init_scripts_dir'         : value => $init_scripts_dir;
     'DEFAULT/agent_config_dir'         : value => $agent_config_dir;
-    'DEFAULT/anetwork_topology'        : value => $network_topology;
+    'DEFAULT/network_topology'        : value => $network_topology;
+    'DEFAULT/logging_context_format_string':
+    value => 'murano-conductor %(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [%(request_id)s %(user)s %(tenant)s] %(instance)s%(message)s';
+    'DEFAULT/logging_default_format_string':
+    value => 'murano-conductor %(asctime)s %(levelname)s %(name)s [-] %(instance)s %(message)s';
     'keystone/auth_url'                : value => $auth_url;
     'rabbitmq/host'                    : value => $rabbit_host;
     'rabbitmq/port'                    : value => $rabbit_port;
@@ -61,6 +95,6 @@ class murano::conductor (
   }
 
   Package['murano_conductor'] -> Murano_conductor_config<||> ~> Service['murano_conductor']
+  File['murano-conductor-logging.conf'] ~> Service['murano_conductor']
 
 }
-
