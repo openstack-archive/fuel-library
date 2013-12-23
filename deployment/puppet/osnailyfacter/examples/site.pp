13d00c8229c3989c15d5275c4e84f64cc0bb1382
Dmitry Teselkin <dteselkin@mirantis.com>
2013-12-23 14:46:08 +0400

    Setting to use syslog in Murano added.
    * Local logs added
    * Logrotate configured
    * Typos
    * murano_use_syslog -> use_syslog
    * murano_debug -> debug
    * murano_verbose -> verbose
    * syslog_log_facility
    * murano user/group added back
    * use bool values for debug/use_syslog
    
    Replaces review https://review.openstack.org/#/c/62853/
    Replaces review https://review.openstack.org/#/c/63705/
    
    Change-Id: I9011d008d34ddc31c2fd7cc426cf63f2acd0c2db
    Closes-Bug: 1261477

--- deployment/puppet/osnailyfacter/examples/site.pp
+++ deployment/puppet/osnailyfacter/examples/site.pp
@@ -104,6 +104,7 @@ $syslog_log_level             = 'ERROR'
 # Syslog facilities for main openstack services, choose any, may overlap if needed
 # local0 is reserved for HA provisioning and orchestration services,
 # local1 is reserved for openstack-dashboard
+$syslog_log_facility_murano   = 'LOG_LOCAL0'
 $syslog_log_facility_glance   = 'LOG_LOCAL2'
 $syslog_log_facility_cinder   = 'LOG_LOCAL3'
 $syslog_log_facility_neutron  = 'LOG_LOCAL4'
@@ -191,6 +192,7 @@ class os_common {
       # should be true, if client is running at virtual node
       virtual        => str2bool($::is_virtual),
       # facilities
+      syslog_log_facility_murano   => $syslog_log_facility_murano,
       syslog_log_facility_glance   => $syslog_log_facility_glance,
       syslog_log_facility_cinder   => $syslog_log_facility_cinder,
       syslog_log_facility_neutron  => $syslog_log_facility_neutron,
