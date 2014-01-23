7bead422c00991b133fa863fd3c63b61ef6369bf
Dmitry Borodaenko <dborodaenko@mirantis.com>
2014-01-23 09:52:01 -0800

    Disable file injection when using RBD Nova backend
    Change-Id: Ib607ffb8a3c50918f10716718a2e7c7219efa9d4

--- deployment/puppet/ceph/manifests/ephemeral.pp
+++ deployment/puppet/ceph/manifests/ephemeral.pp
@@ -15,6 +15,7 @@ class ceph::ephemeral (
   nova_config {
     'DEFAULT/libvirt_images_type':      value => $libvirt_images_type;
     'DEFAULT/libvirt_inject_key':       value => false;
+    'DEFAULT/libvirt_inject_partition': value => '-2';
     'DEFAULT/libvirt_images_rbd_pool':  value => $pool;
     'DEFAULT/live_migration_flag':      value => 'VIR_MIGRATE_UNDEFINE_SOURCE,VIR_MIGRATE_PEER2PEER,VIR_MIGRATE_LIVE,VIR_MIGRATE_PERSIST_DEST';
   }
