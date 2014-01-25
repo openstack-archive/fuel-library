725a8fadffa03158b1de42abec3e7bda1e357f5a
Andrew Woodward <awoodward@mirantis.com>
2014-01-25 13:56:19 -0800

    Multiple Cluster Networks: IP routes
    Adds new attributes 'is_default_route' and 'other_nets' to
     l23network::l3::ifconfig.
    
    Adds ability for ns_IPaddr2 to consume other_nets
    
    In multiple cluster networks it is necessary for interfaces each have their
     own gateway. This adds 'is_default_route' so that only one interface will
     receive the default gateway route. This is assured by nailgun.
    
    It is also necessary for each interface to know the route to the other
     networks of the same network role from other NodeGroups in the cluster. These
     networks are expressed in 'other_nets' and with the 'gateway' and interface
     name will be used to add routes to the node
    
    data used from asute.yaml
    
    Neutron network provider:
    network_scheme:
      endpoints:
    ...
        br-fw-admin:
          IP:
          - 10.108.47.3/24
          default_gateway: true
          gateway: 10.108.47.1
          other_nets:
          - 10.108.52.0/24
        br-mgmt:
          IP:
          - 10.108.49.2/24
          gateway: 10.108.49.1
          other_nets:
          - 10.108.54.0/24
    ...
    
    Nova network provider:
    network_data:
    ...
      eth1:
        default_gateway: true
        gateway: 10.108.48.1
        interface: eth1
        ipaddr:
        - 10.108.48.4/24
    ...
    
    Adds disabled code to configure haproxy to keep vip points together
     There are some deployment cases where the deployer may need to
     enable this and this serves as the code example to perform it.
    
    Resolves-bug #1272766
    implments: bp multiple-cluster-networks
    
    Change-Id: I06e09fd9b292cac53b925933006de064e725bdb0

--- deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
@@ -118,6 +118,10 @@ class osnailyfacter::cluster_ha {
   $vip_management_cidr_netmask = netmask_to_cidr($primary_controller_nodes[0]['internal_netmask'])
   $vip_public_cidr_netmask = netmask_to_cidr($primary_controller_nodes[0]['public_netmask'])
 
+  if $::use_quantum {
+    $vip_mgmt_other_nets = join($::fuel_settings['network_scheme']['endpoints']["$::internal_int"]['other_nets'], ' ')
+  }
+
   $vips = { # Do not convert to ARRAY, It can't work in 2.7
     management_old   => {
       namespace            => 'haproxy',
@@ -125,9 +129,10 @@ class osnailyfacter::cluster_ha {
       base_veth            => "${::internal_int}-hapr",
       ns_veth              => "hapr-m",
       ip                   => $::fuel_settings['management_vip'],
-      cidr_netmask         => $vip_management_cidr_netmask ,
+      cidr_netmask         => $vip_management_cidr_netmask,
       gateway              => 'link',
       gateway_metric       => '20',
+      other_networks       => $vip_mgmt_other_nets,
       iptables_start_rules => "iptables -t mangle -I PREROUTING -i ${::internal_int}-hapr -j MARK --set-mark 0x2b ; iptables -t nat -I POSTROUTING -m mark --mark 0x2b ! -o ${::internal_int} -j MASQUERADE",
       iptables_stop_rules  => "iptables -t mangle -D PREROUTING -i ${::internal_int}-hapr -j MARK --set-mark 0x2b ; iptables -t nat -D POSTROUTING -m mark --mark 0x2b ! -o ${::internal_int} -j MASQUERADE",
       iptables_comment     => "masquerade-for-management-net",
@@ -137,6 +142,11 @@ class osnailyfacter::cluster_ha {
   }
 
   if $::public_int {
+
+    if $::use_quantum {
+      $vip_publ_other_nets = join($::fuel_settings['network_scheme']['endpoints']["$::public_int"]['other_nets'], ' ')
+    }
+
     $vips[public_old] = {
       namespace            => 'haproxy',
       nic                  => $::public_int,
@@ -146,6 +156,7 @@ class osnailyfacter::cluster_ha {
       cidr_netmask         => $vip_public_cidr_netmask,
       gateway              => 'link',
       gateway_metric       => '10',
+      other_networks       => $vip_publ_other_nets,
       iptables_start_rules => "iptables -t mangle -I PREROUTING -i ${::public_int}-hapr -j MARK --set-mark 0x2a ; iptables -t nat -I POSTROUTING -m mark --mark 0x2a ! -o ${::public_int} -j MASQUERADE",
       iptables_stop_rules  => "iptables -t mangle -D PREROUTING -i ${::public_int}-hapr -j MARK --set-mark 0x2a ; iptables -t nat -D POSTROUTING -m mark --mark 0x2a ! -o ${::public_int} -j MASQUERADE",
       iptables_comment     => "masquerade-for-public-net",
@@ -383,6 +394,17 @@ class osnailyfacter::cluster_ha {
     cluster::virtual_ips { $::osnailyfacter::cluster_ha::vip_keys:
       vips => $::osnailyfacter::cluster_ha::vips,
     }
+
+    # Some topologies might need to keep the vips on the same node during
+    # deploymenet. This wouldls only need to be changed by hand.
+    $keep_vips_together = false
+    if ($keep_vips_together) {
+      cs_colocation { 'ha_vips':
+        ensure      => present,
+        primitives  => [prefix(keys($::osnailyfacter::cluster_ha::vips),"vip__")],
+        after       => Cluster::Virtual_ips[$::osnailyfacter::cluster_ha::vip_keys]
+      }
+    } # End If keep_vips_together
   }
 
   if ($::mellanox_mode != 'disabled') {
--- deployment/puppet/osnailyfacter/manifests/network_setup.pp
+++ deployment/puppet/osnailyfacter/manifests/network_setup.pp
@@ -7,14 +7,16 @@ define setup_main_interfaces (
     if ! defined(L23network::L3::Ifconfig[$interface]) {
       $ipaddr = $network_settings[$interface]['ipaddr']
       $gateway = $network_settings[$interface]['gateway']
+      $default_gateway = $network_settings[$interface]['default_gateway']
       # TODO implement bond slaves options support
       #$bond_master = $network_settings[$interface]['bond_master']
       notify{"${interface} => ${ipaddr}, ${gateway}":} ->
       l23network::l3::ifconfig{$interface:
-        ipaddr        => $ipaddr,
-        gateway       => $gateway,
+        ipaddr          => $ipaddr,
+        gateway         => $gateway,
+        default_gateway => $default_gateway,
         #bond_master   => $bond_master,
-        check_by_ping => 'none'
+        check_by_ping    => 'none'
       }
     }
   }
@@ -53,14 +55,16 @@ define setup_sub_interfaces (
     if ! defined(L23network::L3::Ifconfig[$interface]) {
       $ipaddr = $network_settings[$interface]['ipaddr']
       $gateway = $network_settings[$interface]['gateway']
+      $default_gateway = $network_settings[$interface]['default_gateway']
       # TODO implement bond slaves options support
       #$bond_master = $network_settings[$interface]['bond_master']
       notify{"${interface} => ${ipaddr}, ${gateway}":} ->
       l23network::l3::ifconfig{$interface:
-        ipaddr        => $ipaddr,
-        gateway       => $gateway,
+        ipaddr          => $ipaddr,
+        gateway         => $gateway,
+        default_gateway => $default_gateway,
         #bond_master   => $bond_master,
-        check_by_ping => 'none'
+        check_by_ping   => 'none'
       }
     }
   }
