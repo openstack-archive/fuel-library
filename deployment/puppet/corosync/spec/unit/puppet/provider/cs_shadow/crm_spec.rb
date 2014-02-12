c309af46a485cc850b77d7c5422bbaa7e3bd5bb5
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-02-12 12:10:24 +0200

    Add missed block_until_ready calls for corosync provider
    Closes-bug: #1279211
    
    Change-Id: I995d01bd28ea8ef41f752feb252be49d19b62c83
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/corosync/spec/unit/puppet/provider/cs_shadow/crm_spec.rb
+++ deployment/puppet/corosync/spec/unit/puppet/provider/cs_shadow/crm_spec.rb
@@ -5,6 +5,10 @@ describe Puppet::Type.type(:cs_shadow).provider(:crm) do
   let(:resource) { Puppet::Type.type(:cs_shadow).new(:name => 'myshadow',  :provider=> :crm, :cib => 'myshadow' ) }
   let(:provider) { resource.provider }
 
+  before :each do
+    provider.class.stubs(:block_until_ready).returns(true)
+  end
+
   describe "#create" do
     it "should create  non-empty shadow" do
       provider.expects(:crm).with('cib','delete','myshadow')
