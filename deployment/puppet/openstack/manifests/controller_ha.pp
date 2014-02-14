4012992d6f49624b91e6d03ba5ef1844ac71abe3
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-02-14 17:11:36 +0200

    Tune sqlalchemy backend settings for OS on scale
    - Tune SQLAlchemy backend settings for Openstack (QueuePool related)
    - Evaluate sqlalchemy backend scaling parameters at osnailyfactor
    - Fix deprecated in I [DEFAULT] usage for database config
    - For Neutron use neutron_config[:database] as well
    
    Closes-bug: #1274784
    Partial blueprint: optimize-db-performance-at-scale
    
    Change-Id: I0e843022f04f53150da52858280034aad6aee987

--- deployment/puppet/openstack/manifests/controller_ha.pp
+++ deployment/puppet/openstack/manifests/controller_ha.pp
@@ -37,6 +37,10 @@ class openstack::controller_ha (
    $use_unicast_corosync    = false,
    $ha_mode                 = true,
    $nameservers             = undef,
+   $idle_timeout            = '3600',
+   $max_pool_size           = '10',
+   $max_overflow            = '30',
+   $max_retries             = '-1',
  ) {
 
     class { '::openstack::ha::haproxy':
@@ -133,6 +137,11 @@ class openstack::controller_ha (
       horizon_use_ssl              => $horizon_use_ssl,
       ha_mode                      => $ha_mode,
       nameservers                  => $nameservers,
+      # SQLALchemy backend
+      max_retries                  => $max_retries,
+      max_pool_size                => $max_pool_size,
+      max_overflow                 => $max_overflow,
+      idle_timeout                 => $idle_timeout,
     }
 
     if $quantum and $quantum_network_node {
