fc229142bf667f61fdc76e60f6f0f7babe724622
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-02-14 17:11:36 +0200

    Tune mysql, galera, sqlalchemy backend settings
    - Tune MySQL config settings to be more clouds-specific (reads much
      more common, reads:writes rate is 10(100):1)
      * Limit innodb_buffer_pool_size to 10G
      * Recommend innodb_log_file_size as 25% of innodb_buffer_pool_size
        (max 2G). See https://tickets.opscode.com/browse/COOK-2100.
      * Reduce innodb_write_io_threads
      * Use low_priority_updates
      * Raise FD imits to 102400, max connections, open tables limits etc.
    - Tune SQLAlchemy backend settings for Openstack (QueuePool related)
    - Sync some MySQL+galera class params for MySQL class as well
    
    Closes-bug: #1274784
    Implements blueprint: optimize-db-performance-at-scale
    
    Change-Id: I0e843022f04f53150da52858280034aad6aee987
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/neutron/manifests/init.pp
+++ deployment/puppet/neutron/manifests/init.pp
@@ -18,6 +18,7 @@ class neutron (
   $ssh_private_key      = '/var/lib/astute/neutron/neutron',
   $ssh_public_key       = '/var/lib/astute/neutron/neutron.pub',
   $server_ha_mode       = false,
+  $idle_timeout         = '3600',
 ) {
   include 'neutron::params'
 
@@ -139,6 +140,14 @@ class neutron (
     'keystone_authtoken/admin_password':    value => $neutron_config['keystone']['admin_password'];
   }
 
+  $mps=min($::processorcount * 5 + 0, 30 + 0)
+  $mpo=min($::processorcount * 5 + 0, 60 + 0)
+  neutron_config {
+    'DATABASE/max_pool_size': value => $mps;
+    'DATABASE/max_retries':   value => '-1';
+    'DATABASE/max_overflow':  value => $mpo;
+    'DATABASE/idle_timeout':  value => $idle_timeout;
+  }
 
   if defined(Anchor['neutron-server-config-done']) {
     $endpoint_neutron_main_configuration = 'neutron-server-config-done'
