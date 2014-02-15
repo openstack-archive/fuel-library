5e0378990546e54e934563d5688bc771b2f1951b
Kir Vondreth <kir.vondreth@gmail.com>
2014-02-15 15:05:21 +0100

    Add new snippet ubuntu_authorized_keys
    Adds a new snippet for setting up the /root/.ssh/authorized_keys in Ubuntu
    carriage returns in keys are escaped in the preseed
    they are put back with command echo -e
    used existing snippet authorized_keys as template
    
    Change-Id: Ia9b77d418a27f86665671c323ae0e23002907456
    Closes-Bug: #1277253

--- deployment/puppet/cobbler/manifests/snippets.pp
+++ deployment/puppet/cobbler/manifests/snippets.pp
@@ -59,6 +59,7 @@ class cobbler::snippets {
   cobbler_snippet {"cinder_create_lvm_group__ubuntu":}
   cobbler_snippet {"centos_blacklist_i2c_piix4":}
   cobbler_snippet {"centos_static_net":}
+  cobbler_snippet {"ubuntu_authorized_keys":}  
   cobbler_snippet {"ubuntu_blacklist_i2c_piix4":}
   cobbler_snippet {"ubuntu_disable_pxe":}
   cobbler_snippet {"ubuntu_packages":}
