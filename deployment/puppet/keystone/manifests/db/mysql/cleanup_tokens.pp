7bac2dee91d1d0fb2e14b752b2e38ff8bc9b36df
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-02-19 20:40:21 +0400

    Added keystone token cleanup script
    pt-archiver script will run on a cron task
    to purge tokens from keystone tokens table
    periodically to prevent the database from
    growing too large.
    
    Added necessary dependencies for
    installation on Red Hat.
    
    Change-Id: Ic3aa5489069f8fdbc9a251eaaac7f90a805db1ea
    Closes-Bug: 1274756

---
+++ deployment/puppet/keystone/manifests/db/mysql/cleanup_tokens.pp
@@ -0,0 +1,22 @@
+class keystone::db::cleanup_tokens(
+  $db_type     = "mysql",
+  $db_host     = "localhost",
+  $db_name     = "keystone",
+  $db_user     = "root",
+  $db_password = "root",
+) {
+
+  if ( $db_type == "mysql" ) {
+    package { "percona-toolkit":
+      ensure => "installed",
+    }
+  }
+  file { '/etc/cron.daily/cleanup-keystone-tokens.sh':
+    content    => template('keystone/cleanup-keystone-tokens.sh.erb'),
+    owner      => 'root',
+    group      => 'root',
+    mode       => '0700',
+  }
+
+}
+
