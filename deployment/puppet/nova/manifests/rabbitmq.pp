59cac0ea4b4b81004831704db0ca4f427c83e966
Sergey Vasilenko <svasilenko@mirantis.com>
2014-02-28 22:04:15 +0400

    Rabbitmq ocf master/slave
    Closes-bug: #1318936
    Change-Id: Ieab7156fee2b70b32dbf5a2852627495cf1b650e

--- deployment/puppet/nova/manifests/rabbitmq.pp
+++ deployment/puppet/nova/manifests/rabbitmq.pp
@@ -10,7 +10,9 @@ class nova::rabbitmq(
   $cluster = false,
   $cluster_nodes = [], #Real node names to install RabbitMQ server onto.
   $enabled = true,
-  $rabbit_node_ip_address = 'UNSET'
+  $rabbit_node_ip_address = 'UNSET',
+  $ha_mode = false,
+  $primary_controller = false
 ) {
 
   # only configure nova after the queue is up
@@ -21,19 +23,6 @@ class nova::rabbitmq(
       $delete_guest_user = false
     } else {
       $delete_guest_user = true
-      rabbitmq_user { $userid:
-        admin     => true,
-        password  => $password,
-        provider => 'rabbitmqctl',
-        require   => Class['rabbitmq::server'],
-      }
-      # I need to figure out the appropriate permissions
-      rabbitmq_user_permissions { "${userid}@${virtual_host}":
-        configure_permission => '.*',
-        write_permission     => '.*',
-        read_permission      => '.*',
-        provider             => 'rabbitmqctl',
-      }->Anchor<| title == 'nova-start' |>
     }
     $service_ensure = 'running'
   } else {
@@ -56,4 +45,90 @@ class nova::rabbitmq(
       require => Class['rabbitmq::server'],
     }
   }
+
+  if ($ha_mode) {
+    # OCF script for pacemaker
+    # and his dependences
+    file {'rabbitmq-ocf':
+      path   =>'/usr/lib/ocf/resource.d/mirantis/rabbitmq-server',
+      mode   => '0755',
+      owner  => root,
+      group  => root,
+      source => "puppet:///modules/nova/ocf/rabbitmq",
+    }
+
+    Service['rabbitmq-server'] -> File['rabbitmq-ocf']
+    Package['pacemaker'] -> File['rabbitmq-ocf']
+    File<| title == 'ocf-mirantis-path' |> -> File['rabbitmq-ocf']
+    File['rabbitmq-ocf'] -> Cs_resource<| title == "p_rabbitmq-server" |>
+
+    if ($primary_controller) {
+      cs_resource { "p_rabbitmq-server":
+        ensure          => present,
+        #cib             => 'rabbitmq',
+        primitive_class => 'ocf',
+        provided_by     => 'mirantis',
+        primitive_type  => 'rabbitmq-server',
+        parameters      => {
+          'debug'         => false,
+          'service_name'  => 'rabbitmq-server',
+          'node_port'     => $port,
+        },
+        multistate_hash => {
+          'type' => 'master',
+        },
+        ms_metadata => {
+          'notify'      => 'true',
+          'ordered'     => 'true',
+          'interleave'  => 'true',
+          'master-max'  => '1',
+          'master-node-max' => '1',
+          'target-role' => 'Master'
+        },
+        operations => {
+          'monitor' => {
+            'interval' => '10',
+            'timeout'  => '60'
+          },
+          'monitor:Master' => { # name:role
+            'role' => 'Master',
+            'interval' => '7', # should be non-intercectable with interval from ordinary monitor
+            'timeout'  => '60'
+          },
+          'start' => {
+            'timeout' => '60'
+          },
+          'stop' => {
+            'timeout' => '60'
+          },
+          'promote' => {
+            'timeout' => '60'
+          },
+          'demote' => {
+            'timeout' => '60'
+          },
+          'notify' => {
+            'timeout' => '60'
+          },
+        },
+      }
+      # exec {'rmq-co-location': # Don't use INFINITI in co-location for prevent go down VIP-resource while Rabbitmq not running.
+      #   path     => "/sbin:/bin:/usr/sbin:/usr/bin",
+      #   command  => "pcs constraint colocation add vip__management_old with master master_p_rabbitmq-server 10000",
+      #   onlyif   => [ "pcs resource show p_rabbitmq-server", "pcs resource show vip__management_old"],
+      #   unless   => "pcs constraint | grep 'vip__management_old with master_p_rabbitmq-server'",
+      #   provider => "shell",
+      # }
+    }
+
+    File['rabbitmq-ocf'] -> Exec['cleanup_rmq_resource']
+    Cs_resource<| title == 'p_rabbitmq-server' |> -> Exec['cleanup_rmq_resource']
+    exec {'cleanup_rmq_resource':
+      path     => "/sbin:/bin:/usr/sbin:/usr/bin",
+      command  => "sleep 20 ; crm_resource --cleanup --node=$::l3_fqdn_hostname --resource=p_rabbitmq-server",
+      returns  => [0,1,""],
+      provider => "shell",
+    }
+  }
+
 }
