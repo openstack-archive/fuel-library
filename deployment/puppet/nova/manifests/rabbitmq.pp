4e4dc3acbe1c0648c471be2f4f2d1505d1568f88
Sergey Vasilenko <svasilenko@mirantis.com>
2014-02-28 22:04:15 +0400

    Move RabbitMQ under Pacemaker management. (alternative edition)
    POC of master-slave script for RabbitMQ. Using Management VIP.
    Management VIP migrates to the RabbitMQ master.
    
    Partial-Bug: #1285449
    
    Change-Id: Ida5631d2491be052f2cb03dae9f6cb9c35c9098a

--- deployment/puppet/nova/manifests/rabbitmq.pp
+++ deployment/puppet/nova/manifests/rabbitmq.pp
@@ -10,7 +10,8 @@ class nova::rabbitmq(
   $cluster = false,
   $cluster_nodes = [], #Real node names to install RabbitMQ server onto.
   $enabled = true,
-  $rabbit_node_ip_address = 'UNSET'
+  $rabbit_node_ip_address = 'UNSET',
+  $ha_mode = false,
 ) {
 
   # only configure nova after the queue is up
@@ -42,12 +43,12 @@ class nova::rabbitmq(
 
   class { 'rabbitmq::server':
     service_ensure     => $service_ensure,
-    port               => $port,
+    port               => '5672', #$port,
     delete_guest_user  => $delete_guest_user,
     config_cluster     => $cluster,
     cluster_disk_nodes => $cluster_nodes,
     version            => $::openstack_version['rabbitmq_version'],
-    node_ip_address    => $rabbit_node_ip_address,
+    node_ip_address    => 'UNSET', #$rabbit_node_ip_address,
   }
 
   if ($enabled) {
@@ -56,4 +57,85 @@ class nova::rabbitmq(
       require => Class['rabbitmq::server'],
     }
   }
+
+  if ($ha_mode) {
+    # OCF script for pacemaker
+    # and his dependences
+    file {'rabbitmq-ocf':
+      path   =>'/usr/lib/ocf/resource.d/mirantis/rabbitmq-server',
+      mode   => '0755',
+      owner  => root,
+      group  => root,
+      source => "puppet:///modules/nova/ocf/rabbitmq",
+    }
+
+    Service['rabbitmq-server'] -> File['rabbitmq-ocf']
+    Package['pacemaker'] -> File['rabbitmq-ocf']
+    File<| title == 'ocf-mirantis-path' |> -> File['rabbitmq-ocf']
+    File['rabbitmq-ocf'] -> Cs_resource["p_rabbitmq-server"]
+
+    #File['rabbitmq-ocf'] -> Cs_shadow['rabbitmq']
+    #cs_shadow { 'rabbitmq': cib => 'rabbitmq' }
+
+    cs_resource { "p_rabbitmq-server":
+      ensure          => present,
+      #cib             => 'rabbitmq',
+      primitive_class => 'ocf',
+      provided_by     => 'mirantis',
+      primitive_type  => 'rabbitmq-server',
+      parameters      => {
+        'debug'         => false,
+        'service_name'  => 'rabbitmq-server',
+      },
+      multistate_hash => {
+        'type' => 'master',
+      },
+      ms_metadata     => {
+        'interleave' => 'true',
+        'master-max' => '1',
+        'master-node-max' => '1',
+        'target-role' => 'Master'
+      },
+      operations => {
+        'monitor' => {
+          'interval' => '10',
+          'timeout'  => '30'
+        },
+        'monitor:Master' => { # name:role
+          'role' => 'Master',
+          'interval' => '7', # should be non-intercectable with interval from ordinary monitor
+          'timeout'  => '30'
+        },
+        'start' => {
+          'timeout' => '60'
+        },
+        'stop' => {
+          'timeout' => '60'
+        },
+        'promote' => {
+          'timeout' => '60'
+        },
+        'demote' => {
+          'timeout' => '60'
+        },
+        'notify' => {
+          'timeout' => '60'
+        },
+      },
+    } ->
+    exec {'cleanup_rabbitmq_resource':
+      path     => "/sbin:/bin:/usr/sbin:/usr/bin",
+      command  => "sleep 20 ; crm_resource --cleanup --node=#{::l3_fqdn_hostname} --resource=p_rabbitmq-server",
+      returns  => [0,1,""],
+      provider => "shell",
+    } ->
+    exec {'co-location': # Don't use INFINITI in co-location for prevent go down VIP-resource while Rabbitmq not running.
+      path     => "/sbin:/bin:/usr/sbin:/usr/bin",
+      command  => "pcs constraint colocation add vip__management_old with master master_p_rabbitmq-server 10000",
+      onlyif   => [ "pcs resource show p_rabbitmq-server", "pcs resource show vip__management_old"],
+      unless   => "pcs constraint | grep 'vip__management_old with master_p_rabbitmq-server'",
+      provider => "shell",
+    }
+  }
+
 }
