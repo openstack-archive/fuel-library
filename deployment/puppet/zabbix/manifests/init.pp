d52f4196b74674c19ccddd253950a0ccc81e3c06
Dmitry Nikishov <dnikishov@mirantis.com>
2014-03-20 11:28:47 +0400

    Zabbix agent installation Basic OS monitoring
    Change-Id: I540dd7a5c4491b737cd5208d4fabe5d2fec74bc7
    Implements: blueprint monitoring-system

--- deployment/puppet/zabbix/manifests/init.pp
+++ deployment/puppet/zabbix/manifests/init.pp
@@ -3,7 +3,8 @@ class zabbix {
   if has_key($::fuel_settings, 'zabbix') and $enabled {
     include zabbix::params
     if $::zabbix::params::server {
-      Anchor<| title == 'zabbix_server_end'|> -> Anchor<| title == 'zabbix_config_start' |>
+      Anchor<| title == 'zabbix_server_end' |> -> Anchor<| title == 'zabbix_config_start' |>
+      Anchor<| title == 'zabbix_config_end' |> -> Anchor<| title == 'zabbix_monitoring_start' |>
 
       anchor { 'zabbix_server_start': } ->
       class { 'zabbix::server': } ->
@@ -12,6 +13,12 @@ class zabbix {
       anchor { 'zabbix_config_start': } ->
       class { 'zabbix::server::config': } ->
       anchor { 'zabbix_config_end': }
+
+      anchor { 'zabbix_monitoring_start': } ->
+      class { 'zabbix::monitoring': } ->
+      anchor { 'zabbix_monitoring_end': }
+    } else {
+      class { 'zabbix::monitoring': }
     }
   }
 }
---
+++ deployment/puppet/zabbix/manifests/monitoring.pp
@@ -0,0 +1,63 @@
+class zabbix::monitoring {
+
+  include zabbix::params
+
+  zabbix_usermacro { "$zabbix::params::host_name IP_PUBLIC":
+    host  => $zabbix::params::host_name,
+    macro => '{$IP_PUBLIC}',
+    value => $::public_address,
+    api   => $zabbix::params::api_hash,
+  }
+
+  zabbix_usermacro { "$zabbix::params::host_name IP_MANAGEMENT":
+    host  => $zabbix::params::host_name,
+    macro => '{$IP_MANAGEMENT}',
+    value => $::internal_address,
+    api   => $zabbix::params::api_hash,
+  }
+
+  zabbix_usermacro { "$zabbix::params::host_name IP_STORAGE":
+    host  => $zabbix::params::host_name,
+    macro => '{$IP_STORAGE}',
+    value => $::storage_address,
+    api   => $zabbix::params::api_hash,
+  }
+  #Zabbix::agent::userparameter { require => Class['zabbix::agent::scripts'] }
+
+  Anchor<| title == 'zabbix_agent_end' |> -> Anchor<| title == 'zabbix_agent_scripts_begin' |>
+
+  class { 'zabbix::agent': }
+  anchor { 'zabbix_agent_begin': } -> Class['zabbix::agent'] -> anchor { 'zabbix_agent_end': }
+
+  class { 'zabbix::agent::scripts': }
+  anchor { 'zabbix_agent_scripts_begin': } -> Class['zabbix::agent::scripts'] -> anchor { 'zabbix_agent_scripts_end': }
+
+  zabbix::agent::userparameter {
+    'vfs.dev.discovery':
+      ensure => 'present',
+      command => '/etc/zabbix/scripts/vfs.dev.discovery.sh';
+    'vfs.mdadm.discovery':
+      ensure => 'present',
+      command => '/etc/zabbix/scripts/vfs.mdadm.discovery.sh';
+    'proc.vmstat':
+      key => 'proc.vmstat[*]',
+      command => 'grep \'$1\' /proc/vmstat | awk \'{print $$2}\''
+  }
+
+  #Linux
+  zabbix_template_link { "$zabbix::params::host_name Template Fuel OS Linux":
+    host => $zabbix::params::host_name,
+    template => 'Template Fuel OS Linux',
+    api => $zabbix::params::api_hash,
+  }
+
+  #Zabbix Agent
+  zabbix_template_link { "$zabbix::params::host_name Template App Zabbix Agent":
+    host => $zabbix::params::host_name,
+    template => 'Template App Zabbix Agent',
+    api => $zabbix::params::api_hash,
+  }
+
+  Zabbix_usermacro { require => Class['zabbix::agent'] }
+  Zabbix_template_link { require => Class['zabbix::agent'] }
+}
