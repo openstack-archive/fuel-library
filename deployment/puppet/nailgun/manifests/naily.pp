01d272a05c67029902bc499c0e3fcf049c1d3161
Vladimir Sharshov <vsharshov@mirantis.com>
2014-03-20 09:27:08 +0400

    Use a new ruby 2.1 in the master node as a single ruby version.
    * setup supervisor to use system ruby instead of rbenv ruby;
    * code is given in accordance with Ruby 1.9 or higher;
    * remove symbolic link to astute bin agent.
    
    Implements: blueprint fuel-new-system-ruby-in-master-node
    Change-Id: I41d23330443b592d045186dc989df9e4dc72d387

--- deployment/puppet/nailgun/manifests/naily.pp
+++ deployment/puppet/nailgun/manifests/naily.pp
@@ -2,34 +2,18 @@ class nailgun::naily(
   $rabbitmq_naily_user = 'naily',
   $rabbitmq_naily_password = 'naily',
   $version,
-  $gem_source = "http://rubygems.org/",
+  $gem_source = "https://rubygems.org/",
   ){
 
   exec { 'install-naily-gem':
-    command => "/opt/rbenv/bin/rbenv exec gem install naily --source $gem_source --version $version --no-ri --no-rdoc",
-    environment => ['RBENV_ROOT=/opt/rbenv', 'RBENV_VERSION=1.9.3-p392'],
-    require => Exec['configure-rubygems'],
+    command => "gem install naily --source $gem_source --version $version --no-ri --no-rdoc",
     logoutput => true,
   }
 
-  exec { 'configure-rubygems':
-    command => '/opt/rbenv/bin/rbenv exec gem sources -r http://rubygems.org/',
-    environment => ['RBENV_ROOT=/opt/rbenv', 'RBENV_VERSION=1.9.3-p392'],
-    require => Package['rbenv-ruby-1.9.3-p392-0.0.1-1'],
-    logoutput => true,
-  }
-
-  package { 'rbenv-ruby-1.9.3-p392-0.0.1-1': }
+  package { 'ruby-2.1.1-1.1.mira1.x86_64': }
 
   file { '/usr/bin/nailyd':
     content => template('nailgun/nailyd.erb'),
-    owner => 'root',
-    group => 'root',
-    mode => 0755,
-  }
-
-  file { '/usr/bin/astute':
-    content => template('nailgun/astute.erb'),
     owner => 'root',
     group => 'root',
     mode => 0755,
