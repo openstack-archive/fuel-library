b4c23b29d7a8015a12a3e13ec5d8b5c18ec5ac59
Dmitry Nikishov <dnikishov@mirantis.com>
2014-03-20 13:59:41 +0400

    Add nova monitoring with zabbix
    Change-Id: I8bfffd0ce7bcd64e7a6d06e5fbd6e5b256178bc7

--- deployment/puppet/zabbix/manifests/server/config.pp
+++ deployment/puppet/zabbix/manifests/server/config.pp
@@ -29,4 +29,60 @@ class zabbix::server::config {
     xml_file => '/etc/zabbix/import/Template_Fuel_OS_Linux.xml',
   }
 
+  # Nova templates
+  zabbix_configuration_import { 'Template_App_OpenStack_Nova_API_EC2.xml Import':
+    ensure   => present,
+    xml_file => '/etc/zabbix/import/Template_App_OpenStack_Nova_API_EC2.xml',
+    api => $zabbix::params::api_hash,
+  }
+  zabbix_configuration_import { 'Template_App_OpenStack_Nova_API.xml Import':
+    ensure   => present,
+    xml_file => '/etc/zabbix/import/Template_App_OpenStack_Nova_API.xml',
+    api => $zabbix::params::api_hash,
+  }
+  zabbix_configuration_import { 'Template_App_OpenStack_Nova_API_Metadata.xml Import':
+    ensure   => present,
+    xml_file => '/etc/zabbix/import/Template_App_OpenStack_Nova_API_Metadata.xml',
+    api => $zabbix::params::api_hash,
+  }
+  zabbix_configuration_import { 'Template_App_OpenStack_Nova_API_OSAPI.xml Import':
+    ensure   => present,
+    xml_file => '/etc/zabbix/import/Template_App_OpenStack_Nova_API_OSAPI.xml',
+    api => $zabbix::params::api_hash,
+  }
+  zabbix_configuration_import { 'Template_App_OpenStack_Nova_API_OSAPI_check.xml Import':
+    ensure   => present,
+    xml_file => '/etc/zabbix/import/Template_App_OpenStack_Nova_API_OSAPI_check.xml',
+    api => $zabbix::params::api_hash,
+  }
+  zabbix_configuration_import { 'Template_App_OpenStack_Nova_Cert.xml Import':
+    ensure   => present,
+    xml_file => '/etc/zabbix/import/Template_App_OpenStack_Nova_Cert.xml',
+    api => $zabbix::params::api_hash,
+  }
+  zabbix_configuration_import { 'Template_App_OpenStack_Nova_ConsoleAuth.xml Import':
+    ensure   => present,
+    xml_file => '/etc/zabbix/import/Template_App_OpenStack_Nova_ConsoleAuth.xml',
+    api => $zabbix::params::api_hash,
+  }
+  zabbix_configuration_import { 'Template_App_OpenStack_Nova_Scheduler.xml Import':
+    ensure   => present,
+    xml_file => '/etc/zabbix/import/Template_App_OpenStack_Nova_Scheduler.xml',
+    api => $zabbix::params::api_hash,
+  }
+  zabbix_configuration_import { 'Template_App_OpenStack_Nova_Compute.xml Import':
+    ensure   => present,
+    xml_file => '/etc/zabbix/import/Template_App_OpenStack_Nova_Compute.xml',
+    api => $zabbix::params::api_hash,
+  }
+  zabbix_configuration_import { 'Template_App_OpenStack_Libvirt.xml Import':
+    ensure   => present,
+    xml_file => '/etc/zabbix/import/Template_App_OpenStack_Libvirt.xml',
+    api => $zabbix::params::api_hash,
+  }
+  zabbix_configuration_import { 'Template_App_OpenStack_Nova_Network.xml Import':
+    ensure   => present,
+    xml_file => '/etc/zabbix/import/Template_App_OpenStack_Nova_Network.xml',
+    api => $zabbix::params::api_hash,
+  }
 }
