5d6c6475c85b0e26aac4049927b7dc92fef5a853
Dmitry Nikishov <dnikishov@mirantis.com>
2014-03-20 11:28:47 +0400

    Zabbix agent installation Basic OS monitoring
    Change-Id: I540dd7a5c4491b737cd5208d4fabe5d2fec74bc7
    Implements: blueprint monitoring-system

--- deployment/puppet/zabbix/manifests/init.pp
+++ deployment/puppet/zabbix/manifests/init.pp
@@ -3,11 +3,15 @@ class zabbix {
     include zabbix::params
     if $::zabbix::params::enabled {
       if $::zabbix::params::server {
-        class {'zabbix::server': }
+        class {'zabbix::server':
+          before => Class['zabbix::monitoring']
+        }
         class {'zabbix::server::config':
           require => Class['zabbix::server'],
+          before  => Class['zabbix::monitoring']
         }
       }
+      class {'zabbix::monitoring': }
     }
   }
 }
