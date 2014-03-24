88fb1d0d1a31530fd7c11648e04bc8767988c2a9
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-03-24 16:55:34 +0400

    Allow nginx to deploy separate from nailgun
    Added new nginx.pp which owns the entire
    nginx service, indepdent from main init.pp
    for nailgun. Service start is optional to
    compensate for LXC envs.
    
    Added new fields for nailgun and ostf hosts
    in case they are on a separate server or
    container.
    
    blueprint fuel-containerization-of-services
    
    Change-Id: I79f10b7abc97eeb94574718e1dcf14cf5771c8cd

---
+++ deployment/puppet/nailgun/manifests/nginx.pp
@@ -0,0 +1,53 @@
+class nailgun::nginx(
+  $repo_root = "/var/www/nailgun",
+  $staticdir = "/opt/nailgun/share/nailgun/static",
+  $templatedir = "/opt/nailgun/share/nailgun/static",
+  $logdumpdir = "/var/www/nailgun/dump",
+  $service_enabled = true,
+  $ostf_host = '127.0.0.1',
+  $nailgun_host = '127.0.0.1',
+  $production = "production",
+  ) {
+
+  Exec  {path => '/usr/bin:/bin:/usr/sbin:/sbin'}
+
+  anchor { "nginx-begin": }
+  anchor { "nginx-end": }
+
+  Anchor<| title == "nginx-begin" |> ->
+  Class["nailgun::nginx-repo"] ->
+  Class["nailgun::nginx-nailgun"] ->
+  Anchor<| title == "nginx-end" |>
+
+  package { 'nginx':
+    ensure => latest,
+  }
+
+  file { ["/etc/nginx/conf.d/default.conf",
+          "/etc/nginx/conf.d/virtual.conf",
+          "/etc/nginx/conf.d/ssl.conf"]:
+    ensure => "absent",
+    notify => Service["nginx"],
+    before => [
+               Class["nailgun::nginx-repo"],
+               Class["nailgun::nginx-nailgun"],
+               ],
+  }
+
+  class { "nailgun::nginx-repo":
+    repo_root => $repo_root,
+    notify => Service["nginx"],
+  }
+  class { "nailgun::nginx-service":
+    service_enabled => $service_enabled,
+  }
+
+  class { 'nailgun::nginx-nailgun':
+    staticdir    => $staticdir,
+    logdumpdir   => $logdumpdir,
+    ostf_host    => $ostf_host,
+    nailgun_host => $nailgun_host,
+    notify       => Service["nginx"],
+  }
+}
+
