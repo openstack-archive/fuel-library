a39c4382dd80aa5e5f2e14e3fbc9df81cb0297f2
Nikolay Markov <nmarkov@mirantis.com>
2014-03-25 15:23:59 +0400

    Nginx & uWSGI configs for Nailgun
    Change-Id: I05b2d98b19e1d8f1b26ae79f338590ee52af80fd

--- deployment/puppet/nailgun/manifests/nginx-nailgun.pp
+++ deployment/puppet/nailgun/manifests/nginx-nailgun.pp
@@ -5,13 +5,12 @@ class nailgun::nginx-nailgun(
   $ostf_host = '127.0.0.1',
   ) {
 
-  file { "/etc/nginx/conf.d/nailgun.conf":
-    content => template("nailgun/nginx_nailgun.conf.erb"),
-    owner => 'root',
-    group => 'root',
-    mode => 0644,
-    require => Package["nginx"],
-    notify => Service["nginx"],
+  file { '/etc/nginx/conf.d/nailgun.conf':
+    content => template('nailgun/nginx_nailgun.conf.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0644,
+    require => Package['nginx'],
+    notify  => Service['nginx'],
   }
-
 }
