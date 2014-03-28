1278f5a1299f0728b15b9bb75df25efe07142c55
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-03-28 16:58:12 +0400

    Add capability for installing astute from pkg
    * Added rabbitmq_host to nailgun::astute
    class.
    * Added rbenv versionto nailgun::astute
    for dev environments
    * Updated astute daemon launcher for
    non-rbenv'd start
    * FIXME added for naily package
    * FIXME added for astute conf and log refs
    
    blueprint fuel-containerization-of-services
    
    Change-Id: I7eb83da970d7c8d4268641112f560820e77bd6c6

--- deployment/puppet/nailgun/manifests/packages.pp
+++ deployment/puppet/nailgun/manifests/packages.pp
@@ -1,4 +1,5 @@
 class nailgun::packages(
+  $production = 'prod',
   $gem_source = "http://rubygems.org/",
   ){
 
@@ -10,17 +11,17 @@ class nailgun::packages(
 
   nailgun_safe_package { "supervisor": }
   nailgun_safe_package { "nginx": }
-  nailgun_safe_package { "python-virtualenv": }
+  if ( $production == 'dev' ) {
+    nailgun_safe_package { "python-virtualenv": }
+    nailgun_safe_package { "python-devel": }
+    nailgun_safe_package { "postgresql-devel": }
+    nailgun_safe_package { "ruby-devel": }
+    nailgun_safe_package { "gcc": }
+    nailgun_safe_package { "gcc-c++": }
+    nailgun_safe_package { "make": }
+  }
 
-  #FIXME: provide only binary packages, do not build gems and eggs
-
-  nailgun_safe_package { "python-devel": }
   nailgun_safe_package { "postgresql-libs": }
-  nailgun_safe_package { "postgresql-devel": }
-  nailgun_safe_package { "ruby-devel": }
-  nailgun_safe_package { "gcc": }
-  nailgun_safe_package { "gcc-c++": }
-  nailgun_safe_package { "make": }
   nailgun_safe_package { "rsyslog": }
 
   #  nailgun_safe_package { "cman": }
