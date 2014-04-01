ac1d6f5a19506c4c393c32d6b47ac2e48514061a
Aleksey Kolodyazhnyy <akolodyazhnyy@mirantis.com>
2014-04-01 12:28:49 -0700

    vCenter integration added
    Added class in nova for vcenter driver and vcenter integration module.
    
    Implements: blueprint fuel-vmware-hypervisor
    Change-Id: I67cc6d31a937123c06fb5cb7f4e59508295241f4

---
+++ deployment/puppet/nova/manifests/compute/vmware.pp
@@ -0,0 +1,84 @@
+# File was taken from https://github.com/stackforge/puppet-nova
+# Commit 06c99bcf5e3b75756e8344c3d66c4b60ccac04b1
+#
+# Configure the VMware compute driver for nova.
+#
+# === Parameters
+#
+# [*host_ip*]
+#   The IP address of the VMware vCenter server.
+#
+# [*host_username*]
+#   The username for connection to VMware vCenter server.
+#
+# [*host_password*]
+#   The password for connection to VMware vCenter server.
+#
+# [*cluster_name*]
+#   The name of a vCenter cluster compute resource.
+#
+# [*api_retry_count*]
+#   (optional) The number of times we retry on failures,
+#   e.g., socket error, etc.
+#   Defaults to 5.
+#
+# [*maximum_objects*]
+#   (optional) The maximum number of ObjectContent data objects that should
+#   be returned in a single result. A positive value will cause
+#   the operation to suspend the retrieval when the count of
+#   objects reaches the specified maximum. The server may still
+#   limit the count to something less than the configured value.
+#   Any remaining objects may be retrieved with additional requests.
+#   Defaults to 100.
+#
+# [*task_poll_interval*]
+#   (optional) The interval in seconds used for polling of remote tasks.
+#   Defaults to 5.0
+#
+# [*use_linked_clone*]
+#   (optional) Whether to use linked clone strategy while creating VM's.
+#   Defaults to true.
+#
+# [*wsdl_location*]
+#   (optional) VIM Service WSDL Location e.g
+#   http://<server>/vimService.wsdl. Optional over-ride to
+#   default location for bug work-arounds.
+#   Defaults to None.
+#
+
+class nova::compute::vmware(
+  $host_ip,
+  $host_username,
+  $host_password,
+  $cluster_name,
+  $api_retry_count=5,
+  $maximum_objects=100,
+  $task_poll_interval=5.0,
+  $use_linked_clone=true,
+  $wsdl_location=undef
+) {
+
+  nova_config {
+    'DEFAULT/compute_driver':      value => 'vmwareapi.VMwareVCDriver';
+    'vmware/host_ip':              value => $host_ip;
+    'vmware/host_username':        value => $host_username;
+    'vmware/host_password':        value => $host_password;
+    'vmware/cluster_name':         value => $cluster_name;
+    'vmware/api_retry_count' :     value => $api_retry_count;
+    'vmware/maximum_objects' :     value => $maximum_objects;
+    'vmware/task_poll_interval' :  value => $task_poll_interval;
+    'vmware/use_linked_clone':     value => $use_linked_clone;
+  }
+
+  if $wsdl_location {
+    nova_config {
+      'vmware/wsdl_location' : value => $wsdl_location;
+    }
+  }
+
+  package { 'python-suds':
+    ensure   => present
+  }
+
+}
+
---
+++ deployment/puppet/vmware/manifests/network.pp
@@ -0,0 +1,32 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# VMWare related network configuration class
+# It handles whether we use neutron or nova-network and call for an appropriate class
+
+class vmware::network (
+
+  $use_quantum = false,
+
+)
+
+{ # begin of class
+
+  if $use_quantum { # for quantum
+    class { 'vmware::network::neutron': }
+  } else { # for nova network
+    class { 'vmware::network::nova': }
+  } # end of network check
+
+} # end of class
