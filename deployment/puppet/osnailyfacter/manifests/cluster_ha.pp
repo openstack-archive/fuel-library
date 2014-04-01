155729c780526a26439e00c7fd87fb4bb66ae01e
Dmitry Ilyin <dilyin@mirantis.com>
2014-04-01 17:52:01 +0400

    Add ha_nodes option to murano-repository
    Change-Id: Iba68d14e9732fe8f53b869869dfc529392d88686

--- deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
@@ -480,6 +480,8 @@ class osnailyfacter::cluster_ha {
           debug                    => $::debug,
           verbose                  => $::verbose,
           syslog_log_facility      => $::syslog_log_facility_murano,
+
+          repository_replication_nodes => $controllers,
         }
 
        Class['heat'] -> Class['murano']
