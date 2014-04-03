cc893fd34c8eb662547d988ad524d4ffa9154ed1
Dmitry Ilyin <dilyin@mirantis.com>
2014-04-03 13:44:04 +0400

    Add package providers and version lookup
    This commits adds modified version of the
    Puppet Package type. It includes code that
    looks for package version in /etc/versions.yaml
    file if the package is going to be installed instead
    of just installing the first available version.
    
    This lookup will be initiated only is you
    pass :installed or :present to the package's
    ensure property. Passing :latest, :absent
    or explicit version string will cause
    the default behaviour.
    
    Lookups will be performed only for apt and
    yum providers.
    
    Yum and Apt providers in this patch are
    hacked to support transactional rollbacks.
    When the provider is updating the package from
    one version to another it saves the diff of
    packages to a file and if it’s ever asked to
    install the previous version again the provider
    takes the diff file and installs the previous
    package versions and their dependencies.
    
    Change-Id: I84e4b7540f9e72ceb2e1dee0762184d3e2b4356c
    Implements: blueprint patch-openstack

--- deployment/puppet/package/lib/puppet/provider/package/apt.rb
+++ deployment/puppet/package/lib/puppet/provider/package/apt.rb
@@ -9,6 +9,7 @@ Puppet::Type.type(:package).provide :apt, :parent => :dpkg, :source => :dpkg do
   commands :aptget => "/usr/bin/apt-get"
   commands :aptcache => "/usr/bin/apt-cache"
   commands :preseed => "/usr/bin/debconf-set-selections"
+  commands :dpkgquery => '/usr/bin/dpkg-query'
 
   defaultfor :operatingsystem => [:debian, :ubuntu]
 
@@ -36,36 +37,178 @@ Puppet::Type.type(:package).provide :apt, :parent => :dpkg, :source => :dpkg do
     end
   end
 
+  # @param pkg <Hash,TrueClass,FalseClass,Symbol,String>
+  # @param action <Symbol>
+  def install_cmd(pkg)
+    cmd = %w{-q -y}
+
+    config = @resource[:configfiles]
+    if config == :keep
+      cmd << "-o" << 'DPkg::Options::=--force-confold'
+    else
+      cmd << "-o" << 'DPkg::Options::=--force-confnew'
+    end
+
+    cmd << '--force-yes'
+    cmd << :install
+
+    if pkg.is_a? Hash
+      # make install string from package hash
+      cmd += pkg.map do |p|
+        if p[1] == :absent
+          "#{p[0]}-"
+        else
+          "#{p[0]}=#{p[1]}"
+        end
+      end
+    elsif pkg.is_a? String
+      # install a specific version
+      cmd << "#{@resource[:name]}=#{pkg}"
+    else
+      # install any version
+      cmd << @resource[:name]
+    end
+
+    cmd
+  end
+
   # Install a package using 'apt-get'.  This function needs to support
   # installing a specific version.
   def install
     self.run_preseed if @resource[:responsefile]
     should = @resource[:ensure]
+    @file_dir = '/var/lib/puppet/rollback'
 
     checkforcdrom
-    cmd = %w{-q -y}
 
-    if config = @resource[:configfiles]
-      if config == :keep
-        cmd << "-o" << 'DPkg::Options::=--force-confold'
-      else
-        cmd << "-o" << 'DPkg::Options::=--force-confnew'
+    from = @property_hash[:ensure]
+    to = @resource[:ensure]
+    name = @resource[:name]
+    if to == :latest
+      to = latest
+    end
+
+    Puppet.debug "Installing package #{name} from #{from} to #{to}"
+
+    # check if there is a rollback file
+    rollback_file = File.join @file_dir, "#{name}_#{to}_#{from}.yaml"
+    diff = read_diff rollback_file
+    if diff
+      Puppet.debug "Found rollback file at #{rollback_file}"
+      installed = diff['installed']
+      removed = diff['removed']
+
+      # calculate package sets
+      to_update = package_updates removed, installed
+      to_install = package_diff removed, installed
+      to_remove = package_diff installed, removed, true
+
+      Puppet.debug "Install: #{to_install.map {|p| "#{p[0]}=#{p[1]}" }. join ' '}" if to_install.any?
+      Puppet.debug "Remove: #{to_remove.map {|p| "#{p[0]}=#{p[1]}" }. join ' '}" if to_remove.any?
+      Puppet.debug "Update: #{to_update.map {|p| "#{p[0]}=#{p[1]}" }. join ' '}" if to_update.any?
+
+      # combine package lists to a single list
+      all_packages = to_install
+      all_packages = all_packages.merge to_update
+      to_remove.each_pair {|k,v| to_remove.store k, :absent}
+      all_packages = all_packages.merge to_remove
+
+      if all_packages.any?
+        Puppet.debug "All: #{all_packages.map {|p| "#{p[0]}=#{p[1]}" }. join ' '}" if all_packages.any?
+        cmd = install_cmd all_packages
+        aptget(*cmd)
+      end
+
+      return
+    end
+
+    cmd = install_cmd(should)
+
+    # check if we are updating
+    statuses = [ :purged ,:absent, :held, :latest, :installed, :present ]
+    if to.is_a?(String) && from.is_a?(String) && !statuses.include?(from) && !statuses.include?(to)
+      before,after = aptget_with_changes cmd
+      diff = make_package_diff before, after
+      file_path = File.join @file_dir, "#{name}_#{from}_#{to}.yaml"
+      Puppet.notice "Saving diff file to #{file_path}"
+      save_diff file_path, diff
+      return
+    end
+
+    # just install the package
+    aptget(*cmd)
+  end
+
+  def aptget_with_changes(cmd)
+    before = pkg_list
+    aptget(*cmd)
+    after = pkg_list
+    [ before, after ]
+  end
+
+  def make_package_diff(before, after)
+    installed = package_diff after, before
+    removed = package_diff before, after
+    {
+        'installed' => installed,
+        'removed'   => removed,
+    }
+  end
+
+  def save_diff(file_path, diff)
+    require 'yaml'
+    Dir.mkdir @file_dir unless File.directory? @file_dir
+    File.open(file_path, 'w') { |file| file.write YAML.dump(diff) + "\n" }
+  end
+
+  def read_diff(file_path)
+    return nil unless File.readable? file_path
+    require 'yaml'
+    diff = YAML.load_file file_path
+    return nil unless diff.is_a? Hash
+    diff
+  end
+
+  # Substract packages in hash b from packages in hash a
+  # in noval is true only package name matters and version is ignored
+  # @param a <Hash[String]>
+  # @param b <Hash[String]>
+  # @param ignore_versions <TrueClass,FalseClass>
+  def package_diff(a, b, ignore_versions = false)
+    result = a.dup
+    b.each_pair do |k, v|
+      if a.key? k
+        if a[k] == v or ignore_versions
+          result.delete k
+        end
       end
     end
+    result
+  end
 
-    str = @resource[:name]
-    case should
-    when true, false, Symbol
-      # pass
-    else
-      # Add the package version and --force-yes option
-      str += "=#{should}"
-      cmd << "--force-yes"
+  # find package names in both a and b hashes
+  # values are taken from a
+  # @param a <Hash[String]>
+  # @param b <Hash[String]>
+  def package_updates(a, b)
+    common_keys = a.keys & b.keys
+    result = {}
+    common_keys.each { |p| result.store p, a[p] }
+    result
+  end
+
+  def pkg_list
+    packages = {}
+    raw_pkgs = dpkgquery [ '--show', '-f=${Package}|${Version}|${Status}\n' ]
+    raw_pkgs.split("\n").each do |l|
+      line = l.split('|')
+      next unless line[2] == 'install ok installed'
+      name = line[0]
+      version = line[1]
+      next unless name and version
+      packages.store name, version
     end
-
-    cmd << :install << str
-
-    aptget(*cmd)
+    packages
   end
 
   # What's the latest package version available?
---
+++ deployment/puppet/package/spec/unit/provider/package/apt_spec.rb
@@ -0,0 +1,264 @@
+#! /usr/bin/env ruby
+require 'spec_helper'
+
+provider = Puppet::Type.type(:package).provider(:apt)
+
+describe provider do
+  before do
+    @resource = stub 'resource', :[] => "asdf"
+    @provider = provider.new(@resource)
+
+    @fakeresult = <<-EOF
+install ok installed asdf 1.0 "asdf summary
+ asdf multiline description
+ with multiple lines
+EOF
+  end
+
+  it "should be versionable" do
+    provider.should be_versionable
+  end
+
+  it "should use :install to update" do
+    @provider.expects(:install)
+    @provider.update
+  end
+
+  it "should use 'apt-get remove' to uninstall" do
+    @provider.expects(:aptget).with("-y", "-q", :remove, "asdf")
+
+    @provider.uninstall
+  end
+
+  it "should use 'apt-get purge' and 'dpkg purge' to purge" do
+    @provider.expects(:aptget).with("-y", "-q", :remove, "--purge", "asdf")
+    @provider.expects(:dpkg).with("--purge", "asdf")
+
+    @provider.purge
+  end
+
+  it "should use 'apt-cache policy' to determine the latest version of a package" do
+    @provider.expects(:aptcache).with(:policy, "asdf").returns "asdf:
+Installed: 1:1.0
+Candidate: 1:1.1
+Version table:
+1:1.0
+  650 http://ftp.osuosl.org testing/main Packages
+*** 1:1.1
+  100 /var/lib/dpkg/status"
+
+    @provider.latest.should == "1:1.1"
+  end
+
+  it "should print and error and return nil if no policy is found" do
+    @provider.expects(:aptcache).with(:policy, "asdf").returns "asdf:"
+
+    @provider.expects(:err)
+    @provider.latest.should be_nil
+  end
+
+  it "should be able to preseed" do
+    @provider.should respond_to(:run_preseed)
+  end
+
+  it "should preseed with the provided responsefile when preseeding is called for" do
+    @resource.expects(:[]).with(:responsefile).returns "/my/file"
+    Puppet::FileSystem::File.expects(:exist?).with("/my/file").returns true
+
+    @provider.expects(:info)
+    @provider.expects(:preseed).with("/my/file")
+
+    @provider.run_preseed
+  end
+
+  it "should not preseed if no responsefile is provided" do
+    @resource.expects(:[]).with(:responsefile).returns nil
+
+    @provider.expects(:info)
+    @provider.expects(:preseed).never
+
+    @provider.run_preseed
+  end
+
+  describe "when installing" do
+    it "should preseed if a responsefile is provided" do
+      @resource.expects(:[]).with(:responsefile).returns "/my/file"
+      @provider.expects(:run_preseed)
+
+      @provider.stubs(:aptget)
+      @provider.install
+    end
+
+    it "should check for a cdrom" do
+      @provider.expects(:checkforcdrom)
+
+      @provider.stubs(:aptget)
+      @provider.install
+    end
+
+    it "should use 'apt-get install' with the package name if no version is asked for" do
+      @resource.expects(:[]).with(:ensure).returns :installed
+      @provider.expects(:aptget).with { |*command| command[-1] == "asdf" and command[-2] == :install }
+
+      @provider.install
+    end
+
+    it "should specify the package version if one is asked for" do
+      @resource.expects(:[]).with(:ensure).returns "1.0"
+      @provider.expects(:aptget).with { |*command| command[-1] == "asdf=1.0" }
+
+      @provider.install
+    end
+
+    it "should use --force-yes if a package version is specified" do
+      @resource.expects(:[]).with(:ensure).returns "1.0"
+      @provider.expects(:aptget).with { |*command| command.include?("--force-yes") }
+
+      @provider.install
+    end
+
+    it "should do a quiet install" do
+      @provider.expects(:aptget).with { |*command| command.include?("-q") }
+
+      @provider.install
+    end
+
+    it "should default to 'yes' for all questions" do
+      @provider.expects(:aptget).with { |*command| command.include?("-y") }
+
+      @provider.install
+    end
+
+    it "should keep config files if asked" do
+      @resource.expects(:[]).with(:configfiles).returns :keep
+      @provider.expects(:aptget).with { |*command| command.include?("DPkg::Options::=--force-confold") }
+
+      @provider.install
+    end
+
+    it "should replace config files if asked" do
+      @resource.expects(:[]).with(:configfiles).returns :replace
+      @provider.expects(:aptget).with { |*command| command.include?("DPkg::Options::=--force-confnew") }
+
+      @provider.install
+    end
+  end
+
+  describe "with transactional rollback support" do
+    describe "when updating" do
+      it "saves diff only when version is changing and there is no rollback file" do
+        @resource.stubs(:[]).with(:ensure).returns "2.0"
+        @provider.stubs(:query).returns({:ensure => '1.0', :status => '1.0', :name => 'asdf', :error => 'ok'})
+        file_dir = '/var/lib/puppet/rollback'
+        rollback_file = '/var/lib/puppet/rollback/asdf_2.0_1.0.yaml'
+        @provider.stubs(:read_diff).with(rollback_file).returns nil
+        File.stubs(:directory?).with(file_dir).returns true
+
+        @provider.stubs(:aptget_with_changes).with { |command| command[-1] == "asdf=2.0" }
+        @provider.stubs(:make_package_diff).times(1)
+        @provider.stubs(:save_diff).times(1)
+
+        @provider.properties
+        @provider.install
+      end
+      it "doesn't save diff when package is being installed the first time" do
+        @resource.stubs(:[]).with(:ensure).returns :present
+        @provider.stubs(:query).returns({:ensure => :absent, :status => 'missing', :name => 'asdf', :error => 'ok'})
+
+        @provider.stubs(:aptget).with { |*command| command[-1] == "asdf" }
+        @provider.stubs(:save_diff).times(0)
+
+        @provider.properties
+        @provider.install
+      end
+      it "doesn't save diff when package is being removed" do
+        @resource.stubs(:[]).with(:ensure).returns :absent
+        @provider.stubs(:query).returns({:ensure => '1.0', :status => '1.0', :name => 'asdf', :error => 'ok'})
+
+        @provider.stubs(:aptget).with { |*command| command[-1] == "asdf" }
+        @provider.stubs(:save_diff).times(0)
+
+        @provider.properties
+        @provider.install
+      end
+      it "saves correct diff to the rollback file" do
+        @resource.stubs(:[]).with(:ensure).returns "2.0"
+        @provider.stubs(:query).returns({:ensure => '1.0', :status => '1.0', :name => 'asdf', :error => 'ok'})
+        file_dir = '/var/lib/puppet/rollback'
+        rollback_file = '/var/lib/puppet/rollback/asdf_1.0_2.0.yaml'
+
+        before = {
+            'otherpkg' => '1',
+            'asdf' => '1.0',
+            'asdf-lib' => '1.0',
+            'asdf-old-dep' => '1',
+        }
+
+        after = {
+            'otherpkg' => '1',
+            'asdf' => '2.0',
+            'asdf-lib' => '2.0',
+            'asdf-new-dep' => '1',
+        }
+
+        diff = {
+            'installed' => {
+                'asdf' => '2.0',
+                'asdf-lib' => '2.0',
+                'asdf-new-dep' => '1'
+            },
+            'removed' => {
+                'asdf' => '1.0',
+                'asdf-lib' => '1.0',
+                'asdf-old-dep' => '1'
+            }
+        }
+
+        File.stubs(:directory?).with(file_dir).returns true
+        @provider.stubs(:aptget_with_changes).with { |command| command[-1] == "asdf=2.0" }.returns([before,after])
+        @provider.stubs(:save_diff).with(rollback_file, diff)
+        @provider.properties
+        @provider.install
+      end
+    end
+
+    describe "when rolling back" do
+      it 'only when rollback file is found' do
+        @resource.stubs(:[]).with(:ensure).returns "1.0"
+        @provider.stubs(:query).returns({:ensure => '2.0', :status => '2.0', :name => 'asdf', :error => 'ok'})
+        rollback_file = '/var/lib/puppet/rollback/asdf_1.0_2.0.yaml'
+        @provider.stubs(:read_diff).with(rollback_file).returns nil
+        @provider.stubs(:aptget).with { |*command| command[-2,2] == [:install, 'asdf=1.0'] }
+        @provider.stubs(:save_diff).times(1)
+        @provider.properties
+        @provider.install
+      end
+      it "calls apt-get with correct package list" do
+        @resource.stubs(:[]).with(:ensure).returns "1.0"
+        @provider.stubs(:query).returns({:ensure => '2.0', :status => '1.0', :name => 'asdf', :error => 'ok'})
+        rollback_file = '/var/lib/puppet/rollback/asdf_1.0_2.0.yaml'
+        diff = {
+            'installed' => {
+                'asdf' => '2.0',
+                'asdf-lib' => '2.0',
+                'asdf-new-dep' => '1'
+            },
+            'removed' => {
+                'asdf' => '1.0',
+                'asdf-lib' => '1.0',
+                'asdf-old-dep' => '1'
+            }
+        }
+
+        pkgs = [ 'asdf=1.0', 'asdf-lib=1.0', 'asdf-old-dep=1', 'asdf-new-dep-' ]
+
+        @provider.stubs(:read_diff).with(rollback_file).returns(diff)
+        @provider.stubs(:aptget).with { |*command| command[-5,5] == [ :install ] + pkgs }
+        @provider.properties
+        @provider.install
+      end
+    end
+
+  end
+
+end
