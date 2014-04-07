a4d0ab55a4b58af3208b08185e3a8c5682f9e50f
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-04-07 15:31:19 +0300

    Adapt puppet-keystone for Fuel
    Partial blueprint merge-openstack-puppet-modules
    
    - Includes fix for rhbz#1129760 bug.
      Upstream Change-Id: I3f0102b184a484f5e5a85cadf26ed651829fbf8c
    
    Change-Id: I46d872bc6ec1ef33243dce2588847b8be7f2df4b
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/keystone/spec/classes/keystone_roles_admin_spec.rb
+++ deployment/puppet/keystone/spec/classes/keystone_roles_admin_spec.rb
@@ -28,9 +28,7 @@ describe 'keystone::roles::admin' do
       :email       => 'foo@bar',
       :password    => 'ChangeMe'
     )}
-    ['admin', '_member_'].each do |role_name|
-      it { should contain_keystone_role(role_name).with_ensure('present') }
-    end
+    it { should contain_keystone_role('admin').with_ensure('present') }
     it { should contain_keystone_user_role('admin@openstack').with(
       :roles  => 'admin',
       :ensure => 'present'
