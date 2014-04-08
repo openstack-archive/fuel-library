93996dccd56834ce5b28faf0d6244f0af492f7d1
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-04-08 19:20:39 +0400

    Added base host nailgun site manifest
    Change-Id: Idb016dda6ec64213a7175826de7aae60d3a95158

---
+++ deployment/puppet/nailgun/examples/host-only.pp
@@ -0,0 +1,22 @@
+$fuel_settings = parseyaml($astute_settings_yaml)
+$fuel_version = parseyaml($fuel_version_yaml)
+
+if is_hash($::fuel_version) and $::fuel_version['VERSION'] and
+$::fuel_version['VERSION']['production'] {
+    $production = $::fuel_version['VERSION']['production']
+}
+else {
+    $production = 'dev'
+}
+
+
+class { 'nailgun::host':
+  production => $production,
+  nailgun_group => $nailgun_group,
+  nailgun_user => $nailgun_user,
+}
+
+class { "openstack::clocksync":
+  ntp_servers     => $ntp_servers,
+  config_template => "ntp/ntp.conf.centosserver.erb",
+}
