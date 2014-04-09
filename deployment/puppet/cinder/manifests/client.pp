82c9d937278d13fbdd1030ad7df12d20eed79ff3
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-04-09 13:26:53 +0300

    Adapt synced puppet-nova for Fuel usage
    poke ci
    Partial blueprint merge-openstack-puppet-modules
    
    Change-Id: I14cba6fa3c378fb83ce0738c4b20acb94bcf7026
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

---
+++ deployment/puppet/cinder/manifests/client.pp
@@ -0,0 +1,21 @@
+# == Class: cinder::client
+#
+# Installs Cinder python client.
+#
+# === Parameters
+#
+# [*ensure*]
+# Ensure state for package. Defaults to 'present'.
+#
+class cinder::client(
+  $package_ensure = 'present'
+) {
+
+  include cinder::params
+
+  package { 'python-cinderclient':
+    ensure => $package_ensure,
+    name => $::cinder::params::client_package,
+  }
+}
+
