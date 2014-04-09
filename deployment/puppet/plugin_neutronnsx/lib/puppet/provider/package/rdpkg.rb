5b990e0c4ca2f83995b12fd0e47a90bc8b4172f7
Ukov Dmitry <dukov@mirantis.com>
2014-04-09 17:35:17 +0400

    Implemantation of VMware NSX integration
    Added new puppet module for Neutron NSX plugin integration.
    * Separate classes for compute/controller
    * Some additional paramenters required from Nailgun
    * Hide some strange hacks with Anchors
    * Add rdpkg and rrpm package managers in order to download
      packages from remote web-server using mask for a package name.
    * Fix sanitaze_neutron_config to merge boolean values.
    
    Change-Id: I94e7bf2fccc289d0d52fc3d3018db268ec4f7fff
    Implements: blueprint neutron-nsx-plugin-integration

---
+++ deployment/puppet/plugin_neutronnsx/lib/puppet/provider/package/rdpkg.rb
@@ -0,0 +1,37 @@
+require 'net/https'
+require 'open-uri'
+
+Puppet::Type.type(:package).provide :rdpkg, :parent => :dpkg, :source => :dpkg do
+  desc "Remote .deb packages management"
+
+  def get_packages(url)
+    list = Net::HTTP.get(URI(url)).scan(/\S*\.deb\"\>/)
+    return list.map { |x| x.gsub(/.*\"(.*)../, '\1') }
+  end
+
+  def get_package_file(name,url)
+    Puppet.debug "RDPKG: URL '#{url}' contains packages:"
+    get_packages(url).each do |package|
+      Puppet.debug "RDPKG:    #{package}"
+      if package.start_with?(name)
+        return package
+      end
+    end
+    Puppet.warning "RDPKG: package '#{name}' not found by URL '#{url}'"
+    nil
+  end
+
+  def download
+    package = get_package_file(@resource[:name],@resource[:source])
+    path = "#{@resource[:source]}/#{package}"
+    File.open("/tmp/#{package}", 'wb') do |fo|
+      fo.write open(path).read
+    end
+    @resource[:source] = "/tmp/#{package}"
+  end
+
+  def install
+    download
+    super
+  end
+end
