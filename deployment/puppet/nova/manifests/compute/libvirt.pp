93d26a230c90b5073089f311bbad8c3dfe716978
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-04-09 17:27:39 +0300

    Adapt synced puppet-nova for Fuel usage
    partially implements blueprint deploy-icehouse
    
    Change-Id: I14cba6fa3c378fb83ce0738c4b20acb94bcf7026
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/nova/manifests/compute/libvirt.pp
+++ deployment/puppet/nova/manifests/compute/libvirt.pp
@@ -2,17 +2,88 @@
 class nova::compute::libvirt (
   $libvirt_type      = 'kvm',
   $vncserver_listen  = '127.0.0.1',
-  $migration_support = false
+  $migration_support = true
 ) {
 
   include nova::params
 
   Service['libvirt'] -> Service['nova-compute']
 
-  if($::osfamily == 'Debian') {
+  if $::osfamily == 'RedHat' {
+
+    exec { 'symlink-qemu-kvm':
+      command => "/bin/ln -sf /usr/libexec/qemu-kvm /usr/bin/qemu-system-x86_64",
+    }
+
+    stdlib::safe_package {'dnsmasq-utils':}
+
+    package { 'avahi':
+      ensure => present;
+    } ->
+
+    service { 'messagebus':
+      ensure => running,
+      require => Package['avahi'];
+    } ->
+
+    service { 'avahi-daemon':
+      ensure  => running,
+      require => Package['avahi'];
+    }
+
+    Service['avahi-daemon'] -> Service['libvirt']
+
+    service { 'libvirt-guests':
+      name       => 'libvirt-guests',
+      enable     => false,
+      ensure     => true,
+      hasstatus  => false,
+      hasrestart => false,
+    }
+
+  }
+
+  if $::operatingsystem == 'Ubuntu' {
+
+    package { 'cpufrequtils':
+      ensure => present;
+    }
+    file { '/etc/default/cpufrequtils':
+      content => "GOVERNOR=\"performance\" \n",
+      require => Package['cpufrequtils'],
+      notify => Service['cpufrequtils'],
+    }
+    service { 'cpufrequtils':
+      name       => 'cpufrequtils',
+      enable     => true,
+      ensure     => true,
+    }
+    Package<| title == 'cpufrequtils'|> ~> Service<| title == 'cpufrequtils'|>
+    if !defined(Service['cpufrequtils']) {
+      notify{ "Module ${module_name} cannot notify service cpufrequtils\
+ on package update": }
+    }
+  }
+
+  if $::operatingsystem == 'Centos' {
+    package { 'cpufreq-init':
+      ensure => present;
+    }
+  }
+
+  Service['libvirt'] -> Service['nova-compute']
+  Service<| title == 'libvirt'|> ~> Service<| title == 'nova-compute'|>
+
+  if($::nova::params::compute_package_name and $::operatingsystem=='Ubuntu') {
     package { "nova-compute-${libvirt_type}":
       ensure => present,
-      before => Package['nova-compute'],
+      before => Package[$::nova::params::compute_package_name],
+    }
+    Package<| title == "nova-compute-${libvirt_type}"|> ~>
+    Service<| title == 'nova-compute'|>
+    if !defined(Service['nova-compute']) {
+      notify{ "Module ${module_name} cannot notify service nova-compute\
+ on packages update": }
     }
   }
 
@@ -39,11 +110,31 @@ class nova::compute::libvirt (
     name   => $::nova::params::libvirt_package_name,
   }
 
+  file_line { 'no_qemu_selinux':
+    path    => '/etc/libvirt/qemu.conf',
+    line    => 'security_driver="none"',
+    require => Package[$::nova::params::libvirt_package_name],
+    notify  => Service['libvirt']
+  }
+
   service { 'libvirt' :
     ensure   => running,
     name     => $::nova::params::libvirt_service_name,
     provider => $::nova::params::special_service_provider,
     require  => Package['libvirt'],
+  }
+  Package<| title == 'libvirt'|> ~> Service<| title == 'libvirt'|>
+  if !defined(Service['libvirt']) {
+    notify{ "Module ${module_name} cannot notify service libvirt on package update": }
+  }
+
+  case $libvirt_type {
+    'kvm': {
+      package { $::nova::params::libvirt_type_kvm:
+        ensure => present,
+        before => Package[$::nova::params::compute_package_name],
+      }
+    }
   }
 
   nova_config {
@@ -51,5 +142,16 @@ class nova::compute::libvirt (
     'DEFAULT/libvirt_type':     value => $libvirt_type;
     'DEFAULT/connection_type':  value => 'libvirt';
     'DEFAULT/vncserver_listen': value => $vncserver_listen;
+    'DEFAULT/disk_cachemodes': value => '"file=writethrough"';
+  }
+
+if str2bool($::is_virtual) {
+    nova_config {
+      'DEFAULT/libvirt_cpu_mode': value => 'none';
+    }
+  } else {
+    nova_config {
+      'DEFAULT/libvirt_cpu_mode': value => 'host-model';
+    }
   }
 }
--- deployment/puppet/nova/manifests/params.pp
+++ deployment/puppet/nova/manifests/params.pp
@@ -2,9 +2,15 @@
 # should be considered to be constant
 class nova::params {
 
+#FIXME(bogdando) some of the options may be obsolete and not used anymore
   case $::osfamily {
     'RedHat': {
       # package names
+      $libvirt_type_kvm             = $::operatingsystem ? {
+                                    redhat =>  'qemu-kvm-rhev',
+                                    default => 'qemu-kvm',
+                                  }
+      $guestmount_package_name      = 'libguestfs-tools-c'
       $api_package_name             = 'openstack-nova-api'
       $cells_package_name           = 'openstack-nova-cells'
       $cert_package_name            = 'openstack-nova-cert'
@@ -19,7 +25,10 @@ class nova::params {
       $objectstore_package_name     = 'openstack-nova-objectstore'
       $scheduler_package_name       = 'openstack-nova-scheduler'
       $tgt_package_name             = 'scsi-target-utils'
-      $vncproxy_package_name        = 'openstack-nova-novncproxy'
+      $volume_package_name          = 'openstack-nova-volume'
+      $meta_api_package_name        = 'openstack-nova-api'
+      $vncproxy_package_name        = ['novnc','openstack-nova-novncproxy']
+      $pymemcache_package_name      = 'python-memcached'
       $spicehtml5proxy_package_name = 'openstack-nova-console'
       # service names
       $api_service_name             = 'openstack-nova-api'
@@ -28,14 +37,18 @@ class nova::params {
       $compute_service_name         = 'openstack-nova-compute'
       $conductor_service_name       = 'openstack-nova-conductor'
       $consoleauth_service_name     = 'openstack-nova-consoleauth'
+      $console_service_name	    = 'openstack-nova-console'
       $libvirt_service_name         = 'libvirtd'
       $network_service_name         = 'openstack-nova-network'
       $objectstore_service_name     = 'openstack-nova-objectstore'
       $scheduler_service_name       = 'openstack-nova-scheduler'
       $tgt_service_name             = 'tgtd'
       $vncproxy_service_name        = 'openstack-nova-novncproxy'
+      $volume_service_name          = 'openstack-nova-volume'
+      $meta_api_service_name        = 'openstack-nova-metadata-api'
       $spicehtml5proxy_service_name = 'openstack-nova-spicehtml5proxy'
       # redhat specific config defaults
+      $python_path		    = 'python2.6/site-packages'
       $root_helper                  = 'sudo nova-rootwrap'
       $lock_path                    = '/var/lib/nova/tmp'
       case $::operatingsystem {
@@ -49,20 +62,26 @@ class nova::params {
     }
     'Debian': {
       # package names
+      $libvirt_type_kvm             = 'qemu-kvm'
+      $guestmount_package_name      = 'guestmount'
       $api_package_name             = 'nova-api'
       $cells_package_name           = 'nova-cells'
       $cert_package_name            = 'nova-cert'
       $common_package_name          = 'nova-common'
       $compute_package_name         = 'nova-compute'
       $conductor_package_name       = 'nova-conductor'
-      $consoleauth_package_name     = 'nova-consoleauth'
       $doc_package_name             = 'nova-doc'
       $libvirt_package_name         = 'libvirt-bin'
       $network_package_name         = 'nova-network'
+      $vncproxy_package_name        = ['novnc', 'nova-novncproxy']
+      $consoleauth_package_name     = 'nova-consoleauth'
       $numpy_package_name           = 'python-numpy'
       $objectstore_package_name     = 'nova-objectstore'
       $scheduler_package_name       = 'nova-scheduler'
       $tgt_package_name             = 'tgt'
+      $volume_package_name          = 'nova-volume'
+      $meta_api_package_name        = 'nova-api-metadata'
+      $pymemcache_package_name      = 'python-memcache'
       # service names
       $api_service_name             = 'nova-api'
       $cells_service_name           = 'nova-cells'
@@ -70,24 +89,30 @@ class nova::params {
       $compute_service_name         = 'nova-compute'
       $conductor_service_name       = 'nova-conductor'
       $consoleauth_service_name     = 'nova-consoleauth'
+      $console_service_name	    = 'nova-console'
       $libvirt_service_name         = 'libvirt-bin'
       $network_service_name         = 'nova-network'
       $objectstore_service_name     = 'nova-objectstore'
       $scheduler_service_name       = 'nova-scheduler'
+      $volume_service_name          = 'nova-volume'
+      $meta_api_service_name        = 'nova-api-metadata'
       $spicehtml5proxy_service_name = 'nova-spicehtml5proxy'
       $vncproxy_service_name        = 'nova-novncproxy'
       $tgt_service_name             = 'tgt'
       # debian specific nova config
+      $python_path		    = 'python2.7/dist-packages'
       $root_helper                  = 'sudo nova-rootwrap'
       $lock_path                    = '/var/lock/nova'
       case $::operatingsystem {
         'Debian': {
+          $consoleauth_package_name = 'nova-console'
           $spicehtml5proxy_package_name = 'nova-consoleproxy'
           $vncproxy_package_name    = 'nova-consoleproxy'
           # Use default provider on Debian
           $special_service_provider = undef
         }
         default: {
+          $consoleauth_package_name = 'nova-consoleauth'
           $spicehtml5proxy_package_name = 'nova-spiceproxy'
           $vncproxy_package_name    = 'nova-novncproxy'
           # some of the services need to be started form the special upstart provider
