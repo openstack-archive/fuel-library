23f81a34cbfed4564634c791e50eb1a18647384f
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-04-09 17:54:26 +0400

    Add standalone nailgun role puppetsync
    This allows the rsync puppet container to
    deploy on its own. Additionally, fixed
    package and service definitions to allow
    xinetd to be defined only in this class.
    
    Change-Id: I599c6eb61eabd64bae1c8c13e93ea4053ca8eaa8

--- deployment/puppet/nailgun/manifests/puppetsync.pp
+++ deployment/puppet/nailgun/manifests/puppetsync.pp
@@ -15,18 +15,29 @@ class nailgun::puppetsync(
     path    => $rsync_config,
     content => template('nailgun/rsyncd.conf.erb'),
   }
-    
+
   file { 'rsync_xinetd' :
     path    => $xinetd_config,
     content => template('nailgun/rsyncd_xinetd.erb'),
   }
-  
+
+  if !defined(Package['xinetd']) {
+     package { 'xinetd':
+       ensure => installed,
+     }
+  }
   if !defined(Package['rsync']) {
      package { 'rsync':
        ensure => installed,
      }
   }
+  if ! defined(Service['xinetd']) {
+    service { 'xinetd':
+      ensure => running,
+      enable => true,
+      require => Package['xinetd'],
+    }
+  }
 
   Package['rsync'] -> File['rsync_conf', 'rsync_xinetd'] ~> Service['xinetd']
-    
 }
