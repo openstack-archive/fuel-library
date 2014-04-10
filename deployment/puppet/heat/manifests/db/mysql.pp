c798f198da49d71070bd2540af1cd4a6597e74f4
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-04-10 15:38:10 +0300

    Sync puppet-heat
    a64082e4f8ec75ecadaf20a02678647a7af22fb6 3.1.0
    
    partially implements blueprint deploy-icehouse
    
    Change-Id: Icbe9f8c87ea0b6c0a3287c1c36137302bf293009
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/heat/manifests/db/mysql.pp
+++ deployment/puppet/heat/manifests/db/mysql.pp
@@ -1,32 +1,62 @@
+# The heat::db::mysql class creates a MySQL database for heat.
+# It must be used on the MySQL server
+#
+# == Parameters
+#
+#  [*password*]
+#    password to connect to the database. Mandatory.
+#
+#  [*dbname*]
+#    name of the database. Optional. Defaults to heat.
+#
+#  [*user*]
+#    user to connect to the database. Optional. Defaults to heat.
+#
+#  [*host*]
+#    the default source host user is allowed to connect from.
+#    Optional. Defaults to 'localhost'
+#
+#  [*allowed_hosts*]
+#    other hosts the user is allowd to connect from.
+#    Optional. Defaults to undef.
+#
+#  [*charset*]
+#    the database charset. Optional. Defaults to 'latin1'
+#
 class heat::db::mysql(
-  $password      = 'heat',
+  $password      = false,
   $dbname        = 'heat',
   $user          = 'heat',
-  $dbhost        = 'localhost',
-  $charset       = 'utf8',
+  $host          = 'localhost',
   $allowed_hosts = undef,
+  $charset       = 'latin1',
 ) {
 
-  include 'heat::params'
+  validate_string($password)
 
-  mysql::db { $dbname :
+  Class['heat::db::mysql'] -> Exec<| title == 'heat-manage db_sync' |>
+  Mysql::Db[$dbname] ~> Exec<| title == 'heat-manage db_sync' |>
+
+  mysql::db { $dbname:
     user         => $user,
     password     => $password,
-    host         => $dbhost,
+    host         => $host,
     charset      => $charset,
-    grant        => ['all'],
+    require      => Class['mysql::config'],
   }
-  
-  if $allowed_hosts {
-    heat::db::mysql::host_access { $allowed_hosts:
+
+  # Check allowed_hosts to avoid duplicate resource declarations
+  if is_array($allowed_hosts) and delete($allowed_hosts,$host) != [] {
+    $real_allowed_hosts = delete($allowed_hosts,$host)
+  } elsif is_string($allowed_hosts) and ($allowed_hosts != $host) {
+    $real_allowed_hosts = $allowed_hosts
+  }
+
+  if $real_allowed_hosts {
+    heat::db::mysql::host_access { $real_allowed_hosts:
       user      => $user,
       password  => $password,
       database  => $dbname,
     }
   }
-
-  Database[$dbname] ~> Exec['heat_db_sync']
-  Database_user["${user}@${dbhost}"] ~> Exec['heat_db_sync']
-  Database_grant["${user}@${dbhost}/${dbname}"] ~> Exec['heat_db_sync']
-
 }
