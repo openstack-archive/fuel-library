2379fba3412c113ef94723a7c7d5707e5992c911
Sergey Vasilenko <svasilenko@mirantis.com>
2014-04-11 16:39:03 +0400

    Neutron fixes for ICE-HOUSE
    Closes-Bug: #1305895
    Closes-Bug: #1306084
    
    Change-Id: If35dbef3121621d853798b371bf4aff495f2c68f

--- deployment/puppet/neutron/manifests/plugins/ovs.pp
+++ deployment/puppet/neutron/manifests/plugins/ovs.pp
@@ -53,40 +53,59 @@ class neutron::plugins::ovs (
     ensure  => directory,
     mode    => '0755',
   } ->
+  file { '/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini':
+    ensure  => present,
+    mode    => '0640',
+    owner   => 'root',
+    group   => 'neutron',
+  } ->
   file { '/etc/neutron/plugin.ini':
     ensure  => link,
     target  => '/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini',
   }
+  Package['neutron-plugin-ovs'] -> File['/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini']
+
+  if $::operatingsystem == 'Ubuntu' {
+    File['/etc/neutron/plugin.ini'] ->
+    file { '/etc/default/neutron-server':
+      mode   => '0644',
+      owner  => root,
+      group  => root,
+      source => "puppet:///modules/neutron/default__neutron-server",
+    } ->
+    Service<| title == 'neutron-server' |>
+  }
+
   neutron_plugin_ovs {
     'DEFAULT/log_dir':             ensure => absent;
     'DEFAULT/log_file':            ensure => absent;
     'DEFAULT/log_config':          ensure => absent;
     'DEFAULT/use_syslog':          ensure => absent;
     'DEFAULT/use_stderr':          ensure => absent;
-    'OVS/integration_bridge':       value => $neutron_config['L2']['integration_bridge'];
-    'OVS/tenant_network_type':      value => $neutron_config['L2']['segmentation_type'];
-    'OVS/enable_tunneling':         value => $neutron_config['L2']['enable_tunneling'];
-    'AGENT/polling_interval':       value => $neutron_config['polling_interval'];
-    'AGENT/root_helper':            value => $neutron_config['root_helper'];
-    'SECURITYGROUP/firewall_driver': value => 'neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver';
+    'ovs/integration_bridge':       value => $neutron_config['L2']['integration_bridge'];
+    'ovs/tenant_network_type':      value => $neutron_config['L2']['segmentation_type'];
+    'ovs/enable_tunneling':         value => $neutron_config['L2']['enable_tunneling'];
+    'agent/polling_interval':       value => $neutron_config['polling_interval'];
+    'agent/root_helper':            value => $neutron_config['root_helper'];
+    'securitygroup/firewall_driver': value => 'neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver';
   }
 
   if $neutron_config['L2']['enable_tunneling'] {
       neutron_plugin_ovs {
-        'OVS/tunnel_type':          value => $neutron_config['L2']['segmentation_type'];
-        'OVS/tunnel_bridge':        value => $neutron_config['L2']['tunnel_bridge'];
-        'OVS/tunnel_id_ranges':     value => $neutron_config['L2']['tunnel_id_ranges'];
-        'OVS/l2_population':        value => 'False';  #todo: test 'ml2'
-        'OVS/network_vlan_ranges':  value => join(keys($neutron_config['L2']['phys_nets']), ','); # do not belive OS documentation!!!
-        'OVS/bridge_mappings':      value => $neutron_config['L2']['bridge_mappings'];
+        'ovs/tunnel_type':          value => $neutron_config['L2']['segmentation_type'];
+        'ovs/tunnel_bridge':        value => $neutron_config['L2']['tunnel_bridge'];
+        'ovs/tunnel_id_ranges':     value => $neutron_config['L2']['tunnel_id_ranges'];
+        'ovs/l2_population':        value => 'False';  #todo: test 'ml2'
+        'ovs/network_vlan_ranges':  value => join(keys($neutron_config['L2']['phys_nets']), ','); # do not belive OS documentation!!!
+        'ovs/bridge_mappings':      value => $neutron_config['L2']['bridge_mappings'];
         #todo: remove ext_net from mappings. Affect NEutron
       }
   } else {
       neutron_plugin_ovs {
-        'OVS/network_vlan_ranges':  value => $neutron_config['L2']['network_vlan_ranges'];
-        'OVS/bridge_mappings':      value => $neutron_config['L2']['bridge_mappings'];
-        'OVS/tunnel_bridge':        ensure  => absent;
-        'OVS/tunnel_id_ranges':     ensure  => absent;
+        'ovs/network_vlan_ranges':  value => $neutron_config['L2']['network_vlan_ranges'];
+        'ovs/bridge_mappings':      value => $neutron_config['L2']['bridge_mappings'];
+        'ovs/tunnel_bridge':        ensure  => absent;
+        'ovs/tunnel_id_ranges':     ensure  => absent;
       }
   }
 
