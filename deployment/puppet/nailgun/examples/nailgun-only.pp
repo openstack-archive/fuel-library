ad5461d1eb4fe15640f96b58b9c6ad6bb7d85e47
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-04-13 20:20:49 +0400

    Add standalone nailgun role
    Contains modifications to specify rabbitmq and
    cobbler servers to allow nailgun to communicate
    these servers located on different host IPs.
    
    blueprint fuel-containerization-of-services
    
    Change-Id: I0c43bfb74f306830d8df9fc1f66096309faed369

---
+++ deployment/puppet/nailgun/examples/nailgun-only.pp
@@ -0,0 +1,101 @@
+$fuel_settings = parseyaml($astute_settings_yaml)
+$fuel_version = parseyaml($fuel_version_yaml)
+
+if is_hash($::fuel_version) and $::fuel_version['VERSION'] and $::fuel_version['VERSION']['production'] {
+    $production = $::fuel_version['VERSION']['production']
+}
+else {
+    $production = 'dev'
+}
+
+if $production == 'prod'{
+  $env_path = "/usr"
+  $staticdir = "/usr/share/nailgun/static"
+} else {
+  $env_path = "/opt/nailgun"
+  $staticdir = "/opt/nailgun/share/nailgun/static"
+}
+
+Class["nailgun::user"] ->
+Class["nailgun::packages"] ->
+Class["nailgun::venv"] ->
+Class["nailgun::supervisor"]
+
+Exec  {path => '/usr/bin:/bin:/usr/sbin:/sbin'}
+
+$centos_repos =
+[
+ {
+ "id" => "nailgun",
+ "name" => "Nailgun",
+ "url"  => "\$tree"
+ },
+]
+
+$repo_root = "/var/www/nailgun"
+#$pip_repo = "/var/www/nailgun/eggs"
+$pip_repo = "http://${::fuel_settings['ADMIN_NETWORK']['ipaddress']}:8080/eggs/"
+$gem_source = "http://${::fuel_settings['ADMIN_NETWORK']['ipaddress']}:8080/gems/"
+
+$package = "Nailgun"
+$version = "0.1.0"
+$astute_version = "0.0.2"
+$nailgun_group = "nailgun"
+$nailgun_user = "nailgun"
+$venv = $env_path
+
+$pip_index = "--no-index"
+$pip_find_links = "-f ${pip_repo}"
+
+$templatedir = $staticdir
+
+$rabbitmq_host = $::fuel_settings['ADMIN_NETWORK']['ipaddress']
+$rabbitmq_astute_user = "naily"
+$rabbitmq_astute_password = "naily"
+
+class { "nailgun::user":
+  nailgun_group => $nailgun_group,
+  nailgun_user => $nailgun_user,
+}
+class { "nailgun::packages": }
+class { "nailgun::venv":
+  venv => $venv,
+  venv_opts => "--system-site-packages",
+  package => $package,
+  version => $version,
+  pip_opts => "${pip_index} ${pip_find_links}",
+  production => $production,
+  nailgun_user => $nailgun_user,
+  nailgun_group => $nailgun_group,
+
+  database_name => "nailgun",
+  database_engine => "postgresql",
+  database_host => $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
+  database_port => "5432",
+  database_user => "nailgun",
+  database_passwd => "nailgun",
+
+  staticdir => $staticdir,
+  templatedir => $templatedir,
+  rabbitmq_host => $rabbitmq_host,
+  rabbitmq_astute_user => $rabbitmq_astute_user,
+  rabbitmq_astute_password => $rabbitmq_astute_password,
+
+  admin_network         => ipcalc_network_by_address_netmask($::fuel_settings['ADMIN_NETWORK']['ipaddress'], $::fuel_settings['ADMIN_NETWORK']['netmask']),
+  admin_network_cidr    => ipcalc_network_cidr_by_netmask($::fuel_settings['ADMIN_NETWORK']['netmask']),
+  admin_network_size    => ipcalc_network_count_addresses($::fuel_settings['ADMIN_NETWORK']['ipaddress'], $::fuel_settings['ADMIN_NETWORK']['netmask']),
+  admin_network_first   => $::fuel_settings['ADMIN_NETWORK']['static_pool_start'],
+  admin_network_last    => $::fuel_settings['ADMIN_NETWORK']['static_pool_end'],
+  admin_network_netmask => $::fuel_settings['ADMIN_NETWORK']['netmask'],
+  admin_network_ip      => $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
+
+  cobbler_url => $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
+  mco_host    => $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
+
+}
+class { "nailgun::supervisor":
+  nailgun_env   => $env_path,
+  ostf_env      => $env_path,
+  conf_file => "nailgun/supervisord.conf.nailgun.erb",
+}
+
