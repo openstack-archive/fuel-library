ddf839959c87d5b40cfccaff3c642d008ceb83e2
Dmitry Teselkin <dteselkin@mirantis.com>
2014-04-15 17:15:55 +0400

    Murano manifests update
    * 'murano-api' is going to be renamed to 'murano'.
      Multiple renames were made because of that.
    * 'murano-engine' is a part of 'murano' package.
      It was added to manifests.
    * Package name is still 'murano-api', this isn't an error.
    
    Closes-bug: #1307970
    
    Change-Id: If6955e97b9e55cf896c0b09bdbf325f372ccc934

--- deployment/puppet/murano/lib/puppet/type/murano_paste_ini_config.rb
+++ deployment/puppet/murano/lib/puppet/type/murano_paste_ini_config.rb
@@ -1,9 +1,9 @@
-Puppet::Type.newtype(:murano_api_paste_ini_config) do
+Puppet::Type.newtype(:murano_paste_ini_config) do
 
   ensurable
 
   newparam(:name, :namevar => true) do
-    desc 'Section/setting name to manage from murano-api-paste.ini'
+    desc 'Section/setting name to manage from murano-paste.ini'
     newvalues(/\S+\/\S+/)
   end
 
