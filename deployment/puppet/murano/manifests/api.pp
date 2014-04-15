aa7c1745232c802b5f78e624a649e97546af5b9f
Dmitry Teselkin <dteselkin@mirantis.com>
2014-04-15 17:15:55 +0400

    Murano manifests update
    * 'murano-api' is going to be renamed to 'murano'.
      Multiple renames were made because of that.
    * 'murano-engine' is a part of 'murano' package.
      It was added to manifests.
    * Package name is still 'murano-api', this isn't an error.
    
    Closes-bug: #1307970
    
    Change-Id: If6955e97b9e55cf896c0b09bdbf325f372ccc934

--- deployment/puppet/murano/manifests/api.pp
+++ deployment/puppet/murano/manifests/api.pp
@@ -1,49 +1,46 @@
 class murano::api (
-    $use_syslog                     = false,
-    $syslog_log_facility            = 'LOG_LOCAL0',
-    $verbose                        = false,
-    $debug                          = false,
-    $api_paste_inipipeline          = 'authtoken context apiv1app',
-    $api_paste_app_factory          = 'muranoapi.api.v1.router:API.factory',
-    $api_paste_filter_factory       = 'muranoapi.api.middleware.context:ContextMiddleware.factory',
-    $api_paste_paste_filter_factory = 'keystoneclient.middleware.auth_token:filter_factory',
-    $api_paste_auth_host            = '127.0.0.1',
-    $api_paste_auth_port            = '35357',
-    $api_paste_auth_protocol        = 'http',
-    $api_paste_admin_tenant_name    = 'admin',
-    $api_paste_admin_user           = 'admin',
-    $api_paste_admin_password       = 'admin',
-    $api_paste_signing_dir          = '/tmp/keystone-signing-muranoapi',
-    $api_bind_host                  = '0.0.0.0',
-    $api_bind_port                  = '8082',
-    $api_log_file                   = '/var/log/murano/murano-api.log',
-    $api_database_auto_create       = true,
-    $api_reports_results_exchange   = 'task-results',
-    $api_reports_results_queue      = 'task-results',
-    $api_reports_reports_exchange   = 'task-reports',
-    $api_reports_reports_queue      = 'task-reports',
-    $api_rabbit_host                = '127.0.0.1',
-    $api_rabbit_port                = '5672',
-    $api_rabbit_ssl                 = false,
-    $api_rabbit_ca_certs            = '',
-    $api_rabbit_login               = 'murano',
-    $api_rabbit_password            = 'murano',
-    $api_rabbit_virtual_host        = '/',
-    $firewall_rule_name             = '202 murano-api',
+    $use_syslog                 = false,
+    $syslog_log_facility        = 'LOG_LOCAL0',
+    $verbose                    = false,
+    $debug                      = false,
+    $paste_inipipeline          = 'authtoken context apiv1app',
+    $paste_app_factory          = 'muranoapi.api.v1.router:API.factory',
+    $paste_filter_factory       = 'muranoapi.api.middleware.context:ContextMiddleware.factory',
+    $paste_paste_filter_factory = 'keystoneclient.middleware.auth_token:filter_factory',
+    $auth_host                  = '127.0.0.1',
+    $auth_port                  = '35357',
+    $auth_protocol              = 'http',
+    $admin_tenant_name          = 'admin',
+    $admin_user                 = 'admin',
+    $admin_password             = 'admin',
+    $signing_dir                = '/tmp/keystone-signing-muranoapi',
+    $bind_host                  = '0.0.0.0',
+    $bind_port                  = '8082',
+    $log_file                   = '/var/log/murano/murano.log',
+    $rabbit_host                = '127.0.0.1',
+    $rabbit_port                = '5672',
+    $rabbit_use_ssl             = false,
+    $rabbit_ca_certs            = '',
+    $rabbit_login               = 'murano',
+    $rabbit_password            = 'murano',
+    $rabbit_virtual_host        = '/',
+    $firewall_rule_name         = '202 murano-api',
+    $murano_db_user             = 'murano',
+    $murano_db_password         = 'murano',
+    $murano_db_host             = 'localhost',
+    $murano_db_name             = 'murano',
 
-    $murano_db_password             = 'murano',
-    $murano_db_name                 = 'murano',
-    $murano_db_user                 = 'murano',
-    $murano_db_host                 = 'localhost',
+    $murano_user                = 'murano',
+    $stats_period               = '5',
 ) {
 
-  $api_database_connection = "mysql://${murano_db_name}:${murano_db_password}@${murano_db_host}:3306/${murano_db_name}?read_timeout=60"
+  $database_connection = "mysql://${murano_db_name}:${murano_db_password}@${murano_db_host}:3306/${murano_db_name}?read_timeout=60"
 
   include murano::params
 
-  package { 'murano_api':
+  package { 'murano':
     ensure => installed,
-    name   => $::murano::params::murano_api_package_name,
+    name   => $::murano::params::murano_package_name,
   }
 
   service { 'murano_api':
@@ -53,66 +50,66 @@ class murano::api (
     hasstatus  => true,
     hasrestart => true,
   }
-  Package<| title == 'murano_api'|> ~> Service<| title == 'murano_api'|>
+
+  service { 'murano_engine':
+    ensure     => 'running',
+    name       => $::murano::params::murano_engine_service_name,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
+  }
+
+  Package<| title == 'murano'|> ~> Service<| title == 'murano_api'|>
+  Package<| title == 'murano'|> ~> Service<| title == 'murano_engine'|>
+
   if !defined(Service['murano_api']) {
-    notify{ "Module ${module_name} cannot notify service murano api\
- on package update": }
+    notify{ "Module ${module_name} cannot notify service murano-api on package update": }
   }
 
-  $logging_file = '/etc/murano/murano-api-logging.conf'
-  if $use_syslog and !$debug { #syslog and nondebug case
-    murano_api_config {
-      'DEFAULT/log_config'         : value => $logging_file;
-      'DEFAULT/use_syslog'         : value => true;
-      'DEFAULT/syslog_log_facility': value => $syslog_log_facility;
-    }
-    file {"murano-api-logging.conf":
-      content => template('murano/logging.conf.erb'),
-      path    => $logging_file,
-      require => Package['murano_api'],
-      notify  => Service['murano_api'],
-    }
-  } else { #other syslog debug or nonsyslog debug/nondebug cases
-    murano_api_config {
-      'DEFAULT/log_config': ensure => absent;
-      'DEFAULT/log_file'  : value  => $api_log_file;
-      'DEFAULT/use_syslog': value  =>  false;
+  if !defined(Service['murano_engine']) {
+    notify{ "Module ${module_name} cannot notify service murano-engine on package update": }
+  }
+
+  if $use_syslog {
+    murano_config {
+      'DEFAULT/use_syslog'           : value => true;
+      'DEFAULT/use_syslog_rfc_format': value => true;
+      'DEFAULT/syslog_log_facility'  : value => $syslog_log_facility;
     }
   }
 
-  murano_api_config {
+  murano_config {
     'DEFAULT/verbose'                       : value => $verbose;
     'DEFAULT/debug'                         : value => $debug;
-    'DEFAULT/bind_host'                     : value => $api_bind_host;
-    'DEFAULT/bind_port'                     : value => $api_bind_port;
-    'database/connection'                   : value => $api_database_connection;
-    'database/max_retries'                  : value => '-1';
-    'database/auto_create'                  : value => $api_database_auto_create;
-    'reports/results_exchange'              : value => $api_reports_results_exchange;
-    'reports/results_queue'                 : value => $api_reports_results_queue;
-    'reports/reports_exchange'              : value => $api_reports_reports_exchange;
-    'reports/reports_queue'                 : value => $api_reports_reports_queue;
-    'rabbitmq/host'                         : value => $api_rabbit_host;
-    'rabbitmq/port'                         : value => $api_rabbit_port;
-    'rabbitmq/ssl'                          : value => $api_rabbit_ssl;
-    'rabbitmq/ca_certs'                     : value => $api_rabbit_ca_certs;
-    'rabbitmq/login'                        : value => $api_rabbit_login;
-    'rabbitmq/password'                     : value => $api_rabbit_password;
-    'rabbitmq/virtual_host'                 : value => $api_rabbit_virtual_host;
-    'keystone_authtoken/auth_host'          : value => $api_paste_auth_host;
-    'keystone_authtoken/auth_port'          : value => $api_paste_auth_port;
-    'keystone_authtoken/auth_protocol'      : value => $api_paste_auth_protocol;
-    'keystone_authtoken/admin_tenant_name'  : value => $api_paste_admin_tenant_name;
-    'keystone_authtoken/admin_user'         : value => $api_paste_admin_user;
-    'keystone_authtoken/admin_password'     : value => $api_paste_admin_password;
-    'keystone_authtoken/signing_dir'        : value => $api_paste_signing_dir;
+    'DEFAULT/bind_host'                     : value => $bind_host;
+    'DEFAULT/bind_port'                     : value => $bind_port;
+    'DEFAULT/log_file'                      : value => $log_file;
+
+    'DEFAULT/rabbit_host'                   : value => $rabbit_host;
+    'DEFAULT/rabbit_port'                   : value => $rabbit_port;
+    'DEFAULT/rabbit_use_ssl'                : value => $rabbit_use_ssl;
+    'DEFAULT/rabbit_userid'                 : value => $rabbit_login;
+    'DEFAULT/rabbit_password'               : value => $rabbit_password;
+    'DEFAULT/rabbit_virtual_host'           : value => $rabbit_virtual_host;
+
+    'DEFAULT/kombu_ssl_ca_certs'            : value => $rabbit_ca_certs;
+
+    'database/connection'                   : value => $database_connection;
+
+    'keystone_authtoken/auth_host'          : value => $auth_host;
+    'keystone_authtoken/auth_port'          : value => $auth_port;
+    'keystone_authtoken/auth_protocol'      : value => $auth_protocol;
+    'keystone_authtoken/admin_tenant_name'  : value => $admin_tenant_name;
+    'keystone_authtoken/admin_user'         : value => $admin_user;
+    'keystone_authtoken/admin_password'     : value => $admin_password;
+    'keystone_authtoken/signing_dir'        : value => $signing_dir;
   }
 
-  murano_api_paste_ini_config {
-    'pipeline:muranoapi/pipeline'           : value => $api_paste_inipipeline;
-    'app:apiv1app/paste.app_factory'        : value => $api_paste_app_factory;
-    'filter:context/paste.filter_factory'   : value => $api_paste_filter_factory;
-    'filter:authtoken/paste.filter_factory' : value => $api_paste_paste_filter_factory;
+  murano_paste_ini_config {
+    'pipeline:muranoapi/pipeline'           : value => $paste_inipipeline;
+    'app:apiv1app/paste.app_factory'        : value => $paste_app_factory;
+    'filter:context/paste.filter_factory'   : value => $paste_filter_factory;
+    'filter:authtoken/paste.filter_factory' : value => $paste_paste_filter_factory;
   }
 
   firewall { $firewall_rule_name :
@@ -121,10 +118,22 @@ class murano::api (
     action  => 'accept',
   }
 
-  Murano_api_config<||> ~> Service['murano_api']
-  Murano_api_paste_ini_config<||> ~> Service['murano_api']
-  Package['murano_api'] -> Murano_api_config<||>
-  Package['murano_api'] -> Murano_api_paste_ini_config<||>
-  Package['murano_api'] -> Service['murano_api']
+  exec { 'murano_manage_db_sync':
+    command => "murano-manage --config-file=/etc/murano/murano.conf db-sync",
+    user    => $murano_user,
+    group   => $murano_user,
+  }
+
+  Package['murano'] -> Murano_paste_ini_config<||> -> Murano_config<||> -> Exec['murano_manage_db_sync']
+
+  #Package['murano'] -> Service['murano_api']
+  Murano_config<||> ~> Service['murano_api']
+  Murano_paste_ini_config<||> ~> Service['murano_api']
+  Exec['murano_manage_db_sync'] ~> Service['murano_api']
+
+  #Package['murano'] -> Service['murano_engine']
+  Murano_config<||> ~> Service['murano_engine']
+  Murano_paste_ini_config<||> ~> Service['murano_engine']
+  Exec['murano_manage_db_sync'] ~> Service['murano_engine']
 
 }
