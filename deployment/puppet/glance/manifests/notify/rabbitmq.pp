e147cb9fd11ab61f5d03e543de07aca57410f165
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-04-17 17:54:00 +0300

    Use rabbitmq notifications for glance
    * Add support for rabbit_hosts and rabbit_ha_queues for glance.
    * Qpid support is TODO (sync it from upstream as well, then we start to support it again)
    * glance::notify::rabbitmq is synced from master trunc puppet-glance in upstream
    (synced its rspecs as well)
    
    Closes bug: #1308104
    
    Change-Id: Ie7e29aa8c9427ec890f7247b6e93116e7dd83b0c
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/glance/manifests/notify/rabbitmq.pp
+++ deployment/puppet/glance/manifests/notify/rabbitmq.pp
@@ -1,16 +1,52 @@
 #
-# used to configure qpid notifications for glance
+# used to configure rabbitmq notifications for glance
 #
 class glance::notify::rabbitmq(
   $rabbit_password,
-  $rabbit_userid   = 'guest',
-  $rabbit_host     = 'localhost'
-) inherits glance::api {
+  $rabbit_userid                = 'guest',
+  $rabbit_host                  = 'localhost',
+  $rabbit_port                  = '5672',
+  $rabbit_hosts                 = false,
+  $rabbit_virtual_host          = '/',
+  $rabbit_use_ssl               = false,
+  $rabbit_notification_exchange = 'glance',
+  $rabbit_notification_topic    = 'notifications',
+  $rabbit_durable_queues        = false,
+  $amqp_durable_queues          = false,
+) {
+
+  if $rabbit_durable_queues {
+    warning('The rabbit_durable_queues parameter is deprecated, use amqp_durable_queues.')
+    glance_api_config {
+      'DEFAULT/amqp_durable_queues':          value => $rabbit_durable_queues;
+    }
+  } else {
+    glance_api_config {
+      'DEFAULT/amqp_durable_queues':          value => $amqp_durable_queues;
+    }
+  }
+
+  if $rabbit_hosts {
+    glance_api_config {
+      'DEFAULT/rabbit_hosts':     value => join($rabbit_hosts, ',');
+      'DEFAULT/rabbit_ha_queues': value => true
+    }
+  } else {
+    glance_api_config {
+      'DEFAULT/rabbit_host':      value => $rabbit_host;
+      'DEFAULT/rabbit_port':      value => $rabbit_port;
+      'DEFAULT/rabbit_hosts':     value => "${rabbit_host}:${rabbit_port}";
+      'DEFAULT/rabbit_ha_queues': value => false
+    }
+  }
 
   glance_api_config {
-    'DEFAULT/notifier_strategy': value => 'rabbit';
-    'DEFAULT/rabbit_host':       value => $rabbit_host;
-    'DEFAULT/rabbit_password':   value => $rabbit_password;
-    'DEFAULT/rabbit_userid':     value => $rabbit_userid;
+    'DEFAULT/notifier_driver':              value => 'rabbit';
+    'DEFAULT/rabbit_virtual_host':          value => $rabbit_virtual_host;
+    'DEFAULT/rabbit_password':              value => $rabbit_password;
+    'DEFAULT/rabbit_userid':                value => $rabbit_userid;
+    'DEFAULT/rabbit_notification_exchange': value => $rabbit_notification_exchange;
+    'DEFAULT/rabbit_notification_topic':    value => $rabbit_notification_topic;
+    'DEFAULT/rabbit_use_ssl':               value => $rabbit_use_ssl;
   }
 }
--- deployment/puppet/glance/spec/classes/glance_notify_rabbitmq_spec.rb
+++ deployment/puppet/glance/spec/classes/glance_notify_rabbitmq_spec.rb
@@ -10,24 +10,55 @@ describe 'glance::notify::rabbitmq' do
     'class { "glance::api": keystone_password => "pass" }'
   end
 
-  let :params do
-    {:rabbit_password => 'pass'}
+  describe 'when defaults with rabbit pass specified' do
+    let :params do
+      {:rabbit_password => 'pass'}
+    end
+    it { should contain_glance_api_config('DEFAULT/notifier_driver').with_value('rabbit') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_password').with_value('pass') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_userid').with_value('guest') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_host').with_value('localhost') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_port').with_value('5672') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_hosts').with_value('localhost:5672') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_ha_queues').with_value('false') }
+    it { should contain_glance_api_config('DEFAULT/amqp_durable_queues').with_value('false') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_virtual_host').with_value('/') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_notification_exchange').with_value('glance') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_notification_topic').with_value('notifications') }
   end
 
-  it { should contain_glance_api_config('DEFAULT/notifier_strategy').with_value('rabbit') }
-  it { should contain_glance_api_config('DEFAULT/rabbit_password').with_value('pass') }
-  it { should contain_glance_api_config('DEFAULT/rabbit_userid').with_value('guest') }
-  it { should contain_glance_api_config('DEFAULT/rabbit_host').with_value('localhost') }
-
-  describe 'when passing params' do
+  describe 'when passing params for single rabbit host' do
     let :params do
       {
-        :rabbit_password => 'pass',
-        :rabbit_userid   => 'guest2',
-        :rabbit_host     => 'localhost2',
+        :rabbit_password        => 'pass',
+        :rabbit_userid          => 'guest2',
+        :rabbit_host            => 'localhost2',
+        :rabbit_port            => '5673',
+        :rabbit_use_ssl         => true,
+        :rabbit_durable_queues  => true,
       }
-      it { should contain_glance_api_config('DEFAULT/rabbit_userid').with_value('guest2') }
-      it { should contain_glance_api_config('DEFAULT/rabbit_host').with_value('localhost2') }
     end
+    it { should contain_glance_api_config('DEFAULT/rabbit_userid').with_value('guest2') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_host').with_value('localhost2') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_port').with_value('5673') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_hosts').with_value('localhost2:5673') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_use_ssl').with_value('true') }
+    it { should contain_glance_api_config('DEFAULT/amqp_durable_queues').with_value('true') }
+  end
+
+  describe 'when passing params for multiple rabbit hosts' do
+    let :params do
+      {
+        :rabbit_password        => 'pass',
+        :rabbit_userid          => 'guest3',
+        :rabbit_hosts           => ['nonlocalhost3:5673', 'nonlocalhost4:5673']
+      }
+    end
+    it { should contain_glance_api_config('DEFAULT/rabbit_userid').with_value('guest3') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_hosts').with_value(
+                                          'nonlocalhost3:5673,nonlocalhost4:5673') }
+    it { should contain_glance_api_config('DEFAULT/rabbit_ha_queues').with_value('true') }
+    it { should_not contain_glance_api_config('DEFAULT/rabbit_port') }
+    it { should_not contain_glance_api_config('DEFAULT/rabbit_host') }
   end
 end
