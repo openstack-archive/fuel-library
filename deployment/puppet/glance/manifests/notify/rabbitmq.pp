6e29b0805a1088018a54f023851e0468d6b0d8f3
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-04-17 17:54:00 +0300

    Use rabbitmq notifications for glance
    * glance::notify::rabbitmq is synced from v0.3.1 puppet-glance in upstream
    * durable queues is off (should be the same as neutron use?)
    * use rabbit_hosts for glance. Cuz he can.
    * Qpid support is TODO (sync it from upstream as well, then we start to support it again)
    
    Closes bug: #1308104
    
    Change-Id: Ie7e29aa8c9427ec890f7247b6e93116e7dd83b0c
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/glance/manifests/notify/rabbitmq.pp
+++ deployment/puppet/glance/manifests/notify/rabbitmq.pp
@@ -1,16 +1,41 @@
 #
-# used to configure qpid notifications for glance
+# used to configure rabbitmq notifications for glance
 #
 class glance::notify::rabbitmq(
   $rabbit_password,
-  $rabbit_userid   = 'guest',
-  $rabbit_host     = 'localhost'
-) inherits glance::api {
+  $rabbit_userid                = 'guest',
+  $rabbit_host                  = 'localhost',
+  $rabbit_port                  = '5672',
+  $rabbit_hosts                 = false,
+  $rabbit_virtual_host          = '/',
+  $rabbit_use_ssl               = false,
+  $rabbit_notification_exchange = 'glance',
+  $rabbit_notification_topic    = 'notifications',
+  $rabbit_durable_queues        = false
+) {
+
+  Glance_api_config <| title == 'DEFAULT/notifier_strategy' |> {
+    value => 'rabbit'
+  }
+
+  if $rabbit_hosts {
+    glance_api_config {
+      'DEFAULT/rabbit_hosts':                 value => $rabbit_hosts;
+    }
+  } else {
+    glance_api_config {
+      'DEFAULT/rabbit_host':                  value => $rabbit_host;
+      'DEFAULT/rabbit_port':                  value => $rabbit_port;
+    }
+  }
 
   glance_api_config {
-    'DEFAULT/notifier_strategy': value => 'rabbit';
-    'DEFAULT/rabbit_host':       value => $rabbit_host;
-    'DEFAULT/rabbit_password':   value => $rabbit_password;
-    'DEFAULT/rabbit_userid':     value => $rabbit_userid;
+    'DEFAULT/rabbit_virtual_host':          value => $rabbit_virtual_host;
+    'DEFAULT/rabbit_password':              value => $rabbit_password;
+    'DEFAULT/rabbit_userid':                value => $rabbit_userid;
+    'DEFAULT/rabbit_notification_exchange': value => $rabbit_notification_exchange;
+    'DEFAULT/rabbit_notification_topic':    value => $rabbit_notification_topic;
+    'DEFAULT/rabbit_use_ssl':               value => $rabbit_use_ssl;
+    'DEFAULT/rabbit_durable_queues':        value => $rabbit_durable_queues;
   }
 }
