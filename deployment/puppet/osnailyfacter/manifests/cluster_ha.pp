194043b64c966bae8d0307307b2f097ee4cbe88e
Vladimir Kuklin <vkuklin@mirantis.com>
2014-04-18 19:23:22 +0400

    Put novncproxy under haproxy
    make haproxy proxy vncproxy requests
    
    Change-Id: Ia07a6b0ef6c849c1e89885f002725fdee07b51dc
    Closes-Bug: #1309529

--- deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
@@ -318,6 +318,7 @@ class osnailyfacter::cluster_ha {
       ceilometer_metering_secret    => $ceilometer_hash[metering_secret],
       galera_nodes                  => $controller_nodes,
       savanna                       => $savanna_hash[enabled],
+      novnc_address                 => $::internal_address,
       custom_mysql_setup_class      => $custom_mysql_setup_class,
       mysql_skip_name_resolve       => true,
       use_syslog                    => $use_syslog,
