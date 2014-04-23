afe6f1d7c13335091e5485ee0c9b05c3b5868b04
Dmitry Teselkin <dteselkin@mirantis.com>
2014-04-23 16:52:15 +0400

    Murano HA mode
    Fix Murano HA mode:
    * oslo.messaging used for murano-api and for 'system' RabbitMQ
    * rabbit_hosts / rabbit_ha_queues are used
    * haproxy added
    
    Change-Id: I524179f76bc45525c4b9d4de75b655b5f8c52eed

--- deployment/puppet/murano/manifests/init.pp
+++ deployment/puppet/murano/manifests/init.pp
@@ -23,13 +23,14 @@ class murano (
   #  Separate rabbitmq is used to address security concern that instances
   #   managed by Murano have access to the 'system' RabbitMQ and thus could
   #   have access to OpenStack internal data.
-  $murano_rabbit_host                    = '127.0.0.1',
+  $murano_rabbit_nodes                   = [ '127.0.0.1', ],
   $murano_rabbit_port                    = '55572',
   $murano_rabbit_ssl                     = false,
   $murano_rabbit_ca_certs                = '',
   $murano_rabbit_login                   = 'murano',
   $murano_rabbit_password                = 'murano',
   $murano_rabbit_virtual_host            = '/',
+  $murano_rabbit_ha_queues               = false,
   # murano-api-paste.ini
   $murano_paste_inipipeline              = 'authtoken context apiv1app',
   $murano_paste_app_factory              = 'muranoapi.api.v1.router:API.factory',
@@ -56,6 +57,8 @@ class murano (
   User['murano'] -> Class['murano::api']
 
   $murano_keystone_auth_url = "${murano_keystone_protocol}://${murano_keystone_host}:${murano_keystone_port}/v2.0"
+
+  $murano_rabbit_hosts = inline_template("<%= @murano_rabbit_nodes.map {|x| x + ':' + @murano_rabbit_port}.join ',' %>")
 
   group { 'murano':
     ensure => present,
@@ -125,8 +128,10 @@ class murano (
     bind_host                            => $murano_bind_host,
     bind_port                            => $murano_bind_port,
 
-    rabbit_host                          => $murano_rabbit_host,
+    rabbit_host                          => $murano_rabbit_nodes[0],
     rabbit_port                          => $murano_rabbit_port,
+    rabbit_hosts                         => $murano_rabbit_hosts,
+    rabbit_ha_queues                     => $murano_rabbit_ha_queues,
     rabbit_use_ssl                       => $murano_rabbit_ssl,
     rabbit_ca_certs                      => $murano_rabbit_ca_certs,
     rabbit_login                         => $murano_rabbit_login,
