929db7a7d71f6aef732eac66d3de17afdd79f50f
Aleksandr Didenko <adidenko@mirantis.com>
2014-04-24 15:59:22 +0300

    Deploy fuel-dockerctl tools via puppet
    Docketctl is needed for docker containers deployment and
    management.
    
    Also updated nailgun/examples/host-only.pp manifest to make it
    deploy supervisord in the way we need it for proper work of
    docker containers.
    
    Related to: https://review.openstack.org/#/c/88280/
    
    blueprint fuel-containerization-of-services
    
    Change-Id: Ib929c1263a887efef766552588ba3716daa7d790

--- deployment/puppet/nailgun/examples/host-only.pp
+++ deployment/puppet/nailgun/examples/host-only.pp
@@ -9,6 +9,7 @@ else {
     $production = 'dev'
 }
 
+class { 'nailgun::packages': }
 
 class { 'nailgun::host':
   production => $production,
@@ -21,4 +22,37 @@ class { "openstack::clocksync":
   config_template => "ntp/ntp.conf.centosserver.erb",
 }
 
-class { "docker": }
+
+
+class { "docker::dockerctl":
+  release    => $::fuel_version['VERSION']['release'],
+  production => $production,
+} ->
+class { "docker": } ->
+
+class { "nailgun::supervisor":
+  nailgun_env => false,
+  ostf_env    => false,
+  require     => File["/etc/supervisord.d/current", "/etc/supervisord.d/${::fuel_version['VERSION']['release']}"],
+  conf_file   => "nailgun/supervisord.conf.base.erb",
+}
+
+file { "/etc/supervisord.d":
+  ensure  => directory,
+}
+
+file { "/etc/supervisord.d/${::fuel_version['VERSION']['release']}":
+  require => File["/etc/supervisord.d"],
+  owner   => root,
+  group   => root,
+  recurse => true,
+  ensure  => directory,
+  source  => "puppet:///modules/docker/supervisor",
+}
+
+file { "/etc/supervisord.d/current":
+  require => File["/etc/supervisord.d/${::fuel_version['VERSION']['release']}"],
+  replace => true,
+  ensure  => "/etc/supervisord.d/${::fuel_version['VERSION']['release']}",
+}
+
