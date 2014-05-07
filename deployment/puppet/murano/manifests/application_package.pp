95c0bc6d96a11563effa6bb3c09ab8f93244af76
Dmitry Teselkin <dteselkin@mirantis.com>
2014-05-07 12:12:27 +0400

    Install murano-apps before import its content
    Forgot to install package murano-apps, now fix it.
    Add category when importing packages.
    Option '-c' must be specified after the target directory name.
    Typos fixes.
    Add workaround to call import-package only once per package.
    
    Relates-bug: 1316069
    
    Change-Id: I49cc86e60b446355093a85747256113b5f6a8d16

--- deployment/puppet/murano/manifests/application_package.pp
+++ deployment/puppet/murano/manifests/application_package.pp
@@ -3,12 +3,13 @@ define murano::application_package (
   $package_category = '',
   $murano_manage    = 'murano-manage',
   $murano_user      = 'murano',
+  $mandatory        = false,
 ) {
   $package_path="/var/cache/murano/meta/${package_name}"
 
   $murano_manage_cmd = $package_category ? {
     ''       => "${murano_manage} --config-file=/etc/murano/murano.conf import-package '${package_path}'",
-    default  => "${murano_manage} --config-file=/etc/murano/murano.conf import-package -c '${package_category}' '${package_path}'",
+    default  => "${murano_manage} --config-file=/etc/murano/murano.conf import-package '${package_path}' -c '${package_category}'",
   }
 
   exec { "murano_import_package_${package_name}":
@@ -16,6 +17,12 @@ define murano::application_package (
     command => $murano_manage_cmd,
     user    => $murano_user,
     group   => $murano_user,
-    onlyif  => "test -d '${package_path}'",
+    onlyif  => "test -d '${package_path}' && test ! -f '${package_path}.imported'",
   }
+
+  file { "${package_path}.imported":
+    ensure => present,
+  }
+
+  Exec["murano_import_package_${package_name}"] -> File["${package_path}.imported"]
 }
--- deployment/puppet/murano/manifests/params.pp
+++ deployment/puppet/murano/manifests/params.pp
@@ -2,6 +2,7 @@ class murano::params {
 
   # package names
   $murano_package_name              = 'murano-api'
+  $murano_apps_package_name         = 'murano-apps'
   $murano_dashboard_package_name    = 'murano-dashboard'
   $python_muranoclient_package_name = 'python-muranoclient'
 
