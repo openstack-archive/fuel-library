5a2f8e1234d91acfb5a0d4114b47b22289c78e8d
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-05-07 16:53:53 +0300

    Add syslog support, fix verbose for mongo
    * Fix verbose hardcode to be controlled by verbose global
    * Add support for syslog (controlled by use_syslog global)
      Syslog facility is default (user), hence the local logs
      will go to the /var/log/user.log.
      Remote logs will go to /var/log/remote/node*/mongo*.log
    
    Partial-fix: #131708
    
    Change-Id: I79a32b18a1c601c40f695fb0266fab6ca2cc8906
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/openstack/manifests/mongo_secondary.pp
+++ deployment/puppet/openstack/manifests/mongo_secondary.pp
@@ -8,6 +8,8 @@ class openstack::mongo_secondary (
   $ceilometer_metering_secret   = "ceilometer",
   $mongodb_port                 = 27017,
   $mongodb_bind_address         = ['0.0.0.0'],
+  $use_syslog                   = true,
+  $verbose                      = false,
 ) {
 
   notify {"MongoDB params: $mongodb_bind_address": } ->
@@ -15,12 +17,12 @@ class openstack::mongo_secondary (
   class {'::mongodb::client':
   } ->
   class {'::mongodb::server':
-    port    => $mongodb_port,
-    verbose => true,
-    bind_ip => $mongodb_bind_address,
-    replset => 'ceilometer',
-    auth => true,
-    keyfile => '/etc/mongodb.key',
+    port       => $mongodb_port,
+    verbose    => $verbose,
+    use_syslog => $use_syslog,
+    bind_ip    => $mongodb_bind_address,
+    replset    => 'ceilometer',
+    auth       => true,
+    keyfile    => '/etc/mongodb.key',
   }
 }
-# vim: set ts=2 sw=2 et :
