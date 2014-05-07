d86d054fea13e646ce85a3432c3921d627ffff0c
Dmitry Teselkin <dteselkin@mirantis.com>
2014-05-07 12:12:27 +0400

    Install murano-apps before import its content
    Forgot to install package murano-apps, now fix it.
    Add category when importing packages.
    Option '-c' must be specified after the target directory name.
    Typos fixes.
    Add workaround to call import-package only once per package.
    
    Relates-bug: 1316069
    
    Change-Id: I49cc86e60b446355093a85747256113b5f6a8d16

--- deployment/puppet/murano/manifests/apps.pp
+++ deployment/puppet/murano/manifests/apps.pp
@@ -1,11 +1,32 @@
 class murano::apps (
     $primary_controller = true,
   ) {
+    package { 'murano-apps':
+      ensure => installed,
+      name   => $::murano::params::murano_apps_package_name,
+    }
+
     if $primary_controller {
-        murano::application_package{ 'io.murano.apps.PostgreSql': }
-        murano::application_package{ 'io.murano.apps.apache.Apache': }
-        murano::application_package{ 'io.murano.apps.apache.Tomcat': }
-        murano::application_package{ 'io.murano.apps.linux.Telnet': }
-        murano::application_package{ 'io.murano.windows.ActiveDirectory': }
+        murano::application_package { 'io.murano.apps.PostgreSql':
+            package_category => 'Databases',
+        }
+
+        murano::application_package { 'io.murano.apps.apache.Apache':
+            package_category => 'Web',
+        }
+
+        murano::application_package { 'io.murano.apps.apache.Tomcat':
+            package_category => 'Web',
+        }
+
+        murano::application_package { 'io.murano.apps.linux.Telnet':
+            package_category => 'Application Servers',
+        }
+
+        murano::application_package { 'io.murano.windows.ActiveDirectory':
+            package_category => 'Microsoft Services',
+        }
+
+        Package<| title == 'murano-apps'|> -> Murano::Application_package<| mandatory == false |>
     }
   }
