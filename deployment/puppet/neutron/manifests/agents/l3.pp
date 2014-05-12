c83a8d6291624ac7769f286fb06515013cb11834
Sergey Vasilenko <svasilenko@mirantis.com>
2014-05-12 16:51:45 +0400

    add stickiness=1 to some singletone PM resources
    Change-Id: I208435a4b03007604d25e907b916018b99e2a903
    Closes-bug: #1312177

--- deployment/puppet/neutron/manifests/agents/l3.pp
+++ deployment/puppet/neutron/manifests/agents/l3.pp
@@ -125,6 +125,7 @@ class neutron::agents::l3 (
         'username'    => $neutron_config['keystone']['admin_user'],
         'password'    => $neutron_config['keystone']['admin_password'],
       },
+      metadata        => { 'resource-stickiness' => '1' },
       operations      => {
         'monitor'  => {
           'interval' => '20',
