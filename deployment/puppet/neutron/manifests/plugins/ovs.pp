6bd126c2a474cb6f9aa18dfb8124154a998ccb52
Vladimir Kuklin <vkuklin@mirantis.com>
2014-06-03 09:36:10 +0400

    Run database migration before neutron-server starts
    Change-Id: Iafce36930c1c73e3e64e34d94ca1e52f850a1421
    Closes-bug: #1324859

--- deployment/puppet/neutron/manifests/plugins/ovs.pp
+++ deployment/puppet/neutron/manifests/plugins/ovs.pp
@@ -109,9 +109,12 @@ class neutron::plugins::ovs (
       }
   }
 
+
   File['/etc/neutron/plugin.ini'] ->
     Neutron_plugin_ovs<||> ->
       Anchor<| title=='neutron-server-config-done' |>
+
+  Neutron_plugin_ovs<||> -> Exec<| title == 'upgrade neutron head' |>
 
   File['/etc/neutron/plugin.ini'] ->
     Anchor['neutron-plugin-ovs-done']
