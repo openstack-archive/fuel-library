2e86225b3160412003efe3225226721888492463
Aleksandr Didenko <adidenko@mirantis.com>
2014-06-11 11:25:50 +0300

    Synced puppetlab-glance adaptation for Fuel usage
    Implements: blueprint merge-openstack-puppet-modules
    
    Key moments:
      - Since we have a bit outdated mysql module, we need to add few
        hacks in it in order to avoid changes in upstream "glance".
      - Upstream "glance" module has "rbd" backend which is almost
        equal to our former "ceph" backend.
    
    Change-Id: I208781854877e4fc97ddcd17395a26827225bfc1

--- deployment/puppet/mysql/manifests/config.pp
+++ deployment/puppet/mysql/manifests/config.pp
@@ -47,83 +47,85 @@ class mysql::config(
   $use_syslog        = false,
   $custom_setup_class = undef,
   $server_id         = $mysql::params::server_id,
+  $dryrun            = false,
 ) inherits mysql::params {
 
-  $mysql_buffer_pool_size = $::mysql::params::mysql_buffer_pool_size
-  $mysql_log_file_size    = $::mysql::params::mysql_log_file_size
-  $max_connections = $::mysql::params::max_connections
-  $table_open_cache = $::mysql::params::table_open_cache
-  $key_buffer_size = $::mysql::params::key_buffer_size
-  $myisam_sort_buffer_size = $::mysql::params::myisam_sort_buffer_size
-  $wait_timeout = $::mysql::params::wait_timeout
-  $open_files_limit= $::mysql::params::open_files_limit
+  if !$dryrun {
+    $mysql_buffer_pool_size = $::mysql::params::mysql_buffer_pool_size
+    $mysql_log_file_size    = $::mysql::params::mysql_log_file_size
+    $max_connections = $::mysql::params::max_connections
+    $table_open_cache = $::mysql::params::table_open_cache
+    $key_buffer_size = $::mysql::params::key_buffer_size
+    $myisam_sort_buffer_size = $::mysql::params::myisam_sort_buffer_size
+    $wait_timeout = $::mysql::params::wait_timeout
+    $open_files_limit= $::mysql::params::open_files_limit
 
-  if $custom_setup_class != "pacemaker_mysql" {
-    File {
-      owner  => 'root',
-      group  => $root_group,
-      mode   => '0400',
-      notify => Exec['mysqld-restart'],
+    if $custom_setup_class != "pacemaker_mysql" {
+      File {
+        owner  => 'root',
+        group  => $root_group,
+        mode   => '0400',
+        notify => Exec['mysqld-restart'],
+      }
+    } else {
+      File {
+        owner  => 'root',
+        group  => $root_group,
+        mode   => '0400',
+        notify => Service['mysql'],
+      }
     }
-  } else {
-    File {
-      owner  => 'root',
-      group  => $root_group,
-      mode   => '0400',
-      notify => Service['mysql'],
+
+    if $ssl and $ssl_ca == undef {
+      fail('The ssl_ca parameter is required when ssl is true')
+    }
+
+    if $ssl and $ssl_cert == undef {
+      fail('The ssl_cert parameter is required when ssl is true')
+    }
+
+    if $ssl and $ssl_key == undef {
+      fail('The ssl_key parameter is required when ssl is true')
+    }
+
+    # This kind of sucks, that I have to specify a difference resource for
+    # restart.  the reason is that I need the service to be started before mods
+    # to the config file which can cause a refresh
+    exec { 'mysqld-restart':
+      command     => "service ${service_name} restart",
+      logoutput   => on_failure,
+      refreshonly => true,
+      path        => '/sbin/:/usr/sbin/:/usr/bin/:/bin/',
+    }
+
+    file { '/etc/mysql':
+      ensure => directory,
+      mode   => '0755',
+    }
+    file { '/etc/mysql/conf.d':
+      ensure => directory,
+      mode   => '0755',
+    }
+
+    #FIXME(bogdando): dirtyhack to pervert imperative puppet nature.
+    if $::mysql_log_file_size_real != $mysql_log_file_size {
+      # delete MySQL ib_logfiles, if log file size does not match the one
+      # from params
+      exec { 'delete_logfiles':
+        command     => "rm -f ${datadir}/ib_logfile* || true",
+        path        => [ '/sbin/', '/usr/sbin/', '/usr/bin/' ,'/bin/' ],
+        before      => File[$config_file],
+      }
+      # use predefined value for log file size
+      $innodb_log_file_size_real = $mysql_log_file_size
+    } else {
+      # evaluate existing log file size and use it as a value
+      $innodb_log_file_size_real = $::mysql_log_file_size_real
+    }
+
+    file { $config_file:
+      content => template('mysql/my.cnf.erb'),
+      mode    => '0644',
     }
   }
-
-  if $ssl and $ssl_ca == undef {
-    fail('The ssl_ca parameter is required when ssl is true')
-  }
-
-  if $ssl and $ssl_cert == undef {
-    fail('The ssl_cert parameter is required when ssl is true')
-  }
-
-  if $ssl and $ssl_key == undef {
-    fail('The ssl_key parameter is required when ssl is true')
-  }
-
-  # This kind of sucks, that I have to specify a difference resource for
-  # restart.  the reason is that I need the service to be started before mods
-  # to the config file which can cause a refresh
-  exec { 'mysqld-restart':
-    command     => "service ${service_name} restart",
-    logoutput   => on_failure,
-    refreshonly => true,
-    path        => '/sbin/:/usr/sbin/:/usr/bin/:/bin/',
-  }
-
-  file { '/etc/mysql':
-    ensure => directory,
-    mode   => '0755',
-  }
-  file { '/etc/mysql/conf.d':
-    ensure => directory,
-    mode   => '0755',
-  }
-
-  #FIXME(bogdando): dirtyhack to pervert imperative puppet nature.
-  if $::mysql_log_file_size_real != $mysql_log_file_size {
-    # delete MySQL ib_logfiles, if log file size does not match the one
-    # from params
-    exec { 'delete_logfiles':
-      command     => "rm -f ${datadir}/ib_logfile* || true",
-      path        => [ '/sbin/', '/usr/sbin/', '/usr/bin/' ,'/bin/' ],
-      before      => File[$config_file],
-    }
-    # use predefined value for log file size
-    $innodb_log_file_size_real = $mysql_log_file_size
-  } else {
-    # evaluate existing log file size and use it as a value
-    $innodb_log_file_size_real = $::mysql_log_file_size_real
-  }
-
-  file { $config_file:
-    content => template('mysql/my.cnf.erb'),
-    mode    => '0644',
-  }
-
 }
