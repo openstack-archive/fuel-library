d53f4f95b300867eedcb97032006e3319bf8aca8
Aleksandr Didenko <adidenko@mirantis.com>
2014-06-17 13:39:09 +0300

    Refactor mysql for upstream modules
    Temporary workaround. Will not be needed after "mysql" module sync
    with upstream code.
    
    New OpenStack puppet modules (v4.0.0) require "mysql::config"
    class. We need to provide it.
    
    Implements: blueprint merge-openstack-puppet-modules
    
    Change-Id: I5f928113a45991f7f0eb5b6a2e5e6a83f3c08a7b

--- deployment/puppet/mysql/manifests/config.pp
+++ deployment/puppet/mysql/manifests/config.pp
@@ -74,56 +74,58 @@ class mysql::config(
     }
   }
 
-  if $ssl and $ssl_ca == undef {
-    fail('The ssl_ca parameter is required when ssl is true')
-  }
+  if ($custom_setup_class == undef) {
 
-  if $ssl and $ssl_cert == undef {
-    fail('The ssl_cert parameter is required when ssl is true')
-  }
-
-  if $ssl and $ssl_key == undef {
-    fail('The ssl_key parameter is required when ssl is true')
-  }
-
-  # This kind of sucks, that I have to specify a difference resource for
-  # restart.  the reason is that I need the service to be started before mods
-  # to the config file which can cause a refresh
-  exec { 'mysqld-restart':
-    command     => "service ${service_name} restart",
-    logoutput   => on_failure,
-    refreshonly => true,
-    path        => '/sbin/:/usr/sbin/:/usr/bin/:/bin/',
-  }
-
-  file { '/etc/mysql':
-    ensure => directory,
-    mode   => '0755',
-  }
-  file { '/etc/mysql/conf.d':
-    ensure => directory,
-    mode   => '0755',
-  }
-
-  #FIXME(bogdando): dirtyhack to pervert imperative puppet nature.
-  if $::mysql_log_file_size_real != $mysql_log_file_size {
-    # delete MySQL ib_logfiles, if log file size does not match the one
-    # from params
-    exec { 'delete_logfiles':
-      command     => "rm -f ${datadir}/ib_logfile* || true",
-      path        => [ '/sbin/', '/usr/sbin/', '/usr/bin/' ,'/bin/' ],
-      before      => File[$config_file],
+    if $ssl and $ssl_ca == undef {
+      fail('The ssl_ca parameter is required when ssl is true')
     }
-    # use predefined value for log file size
-    $innodb_log_file_size_real = $mysql_log_file_size
-  } else {
-    # evaluate existing log file size and use it as a value
-    $innodb_log_file_size_real = $::mysql_log_file_size_real
-  }
 
-  file { $config_file:
-    content => template('mysql/my.cnf.erb'),
-    mode    => '0644',
-  }
+    if $ssl and $ssl_cert == undef {
+      fail('The ssl_cert parameter is required when ssl is true')
+    }
 
+    if $ssl and $ssl_key == undef {
+      fail('The ssl_key parameter is required when ssl is true')
+    }
+
+    # This kind of sucks, that I have to specify a difference resource for
+    # restart.  the reason is that I need the service to be started before mods
+    # to the config file which can cause a refresh
+    exec { 'mysqld-restart':
+      command     => "service ${service_name} restart",
+      logoutput   => on_failure,
+      refreshonly => true,
+      path        => '/sbin/:/usr/sbin/:/usr/bin/:/bin/',
+    }
+
+    file { '/etc/mysql':
+      ensure => directory,
+      mode   => '0755',
+    }
+    file { '/etc/mysql/conf.d':
+      ensure => directory,
+      mode   => '0755',
+    }
+
+    #FIXME(bogdando): dirtyhack to pervert imperative puppet nature.
+    if $::mysql_log_file_size_real != $mysql_log_file_size {
+      # delete MySQL ib_logfiles, if log file size does not match the one
+      # from params
+      exec { 'delete_logfiles':
+        command     => "rm -f ${datadir}/ib_logfile* || true",
+        path        => [ '/sbin/', '/usr/sbin/', '/usr/bin/' ,'/bin/' ],
+        before      => File[$config_file],
+      }
+      # use predefined value for log file size
+      $innodb_log_file_size_real = $mysql_log_file_size
+    } else {
+      # evaluate existing log file size and use it as a value
+      $innodb_log_file_size_real = $::mysql_log_file_size_real
+    }
+
+    file { $config_file:
+      content => template('mysql/my.cnf.erb'),
+      mode    => '0644',
+    }
+  }
 }
