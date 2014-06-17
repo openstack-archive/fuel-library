3f91c5ec2dcac43f0e9bb70fa061bb68ce57daa2
Artem Panchenko <apanchenko@mirantis.com>
2014-06-17 23:07:36 +0300

    Fix path to astute.yaml
    To merge review # 98743 we need to change
    path to astute.yaml first, otherwise dhcrelay
    starts only on deocker0 interface and slaves
    are not being discovered.
    
    Change-Id: Icce71a9ca7095397419d5a5afb746f7313d54573

--- deployment/puppet/osnailyfacter/lib/facter/naily.rb
+++ deployment/puppet/osnailyfacter/lib/facter/naily.rb
@@ -5,6 +5,9 @@ astute_settings_path = ['/etc/fuel/astute.yaml', '/etc/astute.yaml']
 
 astute_settings_path.each do |astute_file|
   if File.exist?(astute_file)
+    Facter.add('astute_settings_file') do
+      setcode { astute_file }
+    end
     Facter.add('astute_settings_yaml') do
       setcode { File.read(astute_file) }
     end
