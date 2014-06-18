7f7434da137d0b371db65116f4a71bc1cb0e1684
TimurNurlygayanov <tnurlygayanov@mirantis.com>
2014-06-18 17:48:05 +0400

    Fixed default parameters for Nova and Neutron
    Default values of performance-critical parameters
    changed to more comfortable for production environments.
    
    Change-Id: I57dedea5245bc309474f2e127e88d86b35660f4f
    Closes-Bug: #1324914

--- deployment/puppet/neutron/manifests/server.pp
+++ deployment/puppet/neutron/manifests/server.pp
@@ -76,6 +76,8 @@ class neutron::server (
     'DEFAULT/nova_admin_password':  value => $neutron_config['server']['notify_nova_admin_password'];
     'DEFAULT/nova_admin_auth_url':  value => $neutron_config['server']['notify_nova_admin_auth_url'];
     'DEFAULT/send_events_interval': value => $neutron_config['server']['notify_nova_send_events_interval'];
+    'DEFAULT/api_workers':          value => min($::processorcount * 5 + 0, 100 + 0);
+    'DEFAULT/rpc_workers':          value => min($::processorcount * 5 + 0, 100 + 0);
     'database/connection':          value => $neutron_config['database']['url'];
     'database/max_retries':         value => $neutron_config['database']['reconnects'];
     'database/reconnect_interval':  value => $neutron_config['database']['reconnect_interval'];
