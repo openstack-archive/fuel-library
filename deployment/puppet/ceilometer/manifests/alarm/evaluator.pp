d1e4cc6bdffe89f9e4043969c0ab74f556ade9a8
Sergey Vasilenko <svasilenko@mirantis.com>
2014-06-19 16:20:54 +0400

    remove unnided CIB definition for some OCF resources
    Pacemaker resources will created only at deploy primary controller.
    At this stage we do not need pacemaker shadow mechanick, because
    we have only one running cluster node.
    
    For bunch of pacemaker resources using shadows is a unnecessarily.
    
    Blueprint: ha-pacemaker-improvements
    Closes-Bug: #1283062
    
    Change-Id: Iead6d37b6050905dba6042c4d58aafeb6d614664

--- deployment/puppet/ceilometer/manifests/alarm/evaluator.pp
+++ deployment/puppet/ceilometer/manifests/alarm/evaluator.pp
@@ -16,13 +16,12 @@ class ceilometer::alarm::evaluator (
   $enabled       = true,
   $eval_interval = 600,
   $ha_mode       = false,
+  $primary_controller = false
 )
 {
   include ceilometer::params
 
   Ceilometer_config<||> ~> Service['ceilometer-alarm-evaluator']
-  Package['ceilometer-common'] -> Service['ceilometer-alarm-evaluator']
-  Package[$::ceilometer::params::alarm_package] -> Service['ceilometer-alarm-evaluator']
 
   if ! defined(Notify['ceilometer-alarm']) {
     package { $::ceilometer::params::alarm_package:
@@ -49,8 +48,9 @@ class ceilometer::alarm::evaluator (
   if $ha_mode {
 
     $res_name = "p_${::ceilometer::params::alarm_evaluator_service}"
-    $cib_name = "${::ceilometer::params::alarm_evaluator_service}"
 
+    Package['ceilometer-common'] -> File['ceilometer-alarm-evaluator-ocf']
+    Package[$::ceilometer::params::alarm_package] -> File['ceilometer-alarm-evaluator-ocf']
     Package['pacemaker'] -> File['ceilometer-alarm-evaluator-ocf']
     file {'ceilometer-alarm-evaluator-ocf':
       path   =>'/usr/lib/ocf/resource.d/mirantis/ceilometer-alarm-evaluator',
@@ -60,33 +60,33 @@ class ceilometer::alarm::evaluator (
       source => 'puppet:///modules/ceilometer/ocf/ceilometer-alarm-evaluator',
     }
 
-    File['ceilometer-alarm-evaluator-ocf'] -> Cs_resource[$res_name]
-    cs_resource { $res_name:
-      ensure          => present,
-      cib             => $cib_name,
-      primitive_class => 'ocf',
-      provided_by     => 'mirantis',
-      primitive_type  => 'ceilometer-alarm-evaluator',
-      metadata        => { 'target-role' => 'stopped' },
-      parameters      => { 'user' => 'ceilometer' },
-      operations      => {
-        'monitor'  => {
-          'interval' => '20',
-          'timeout'  => '30'
-        }
-        ,
-        'start'    => {
-          'timeout' => '360'
-        }
-        ,
-        'stop'     => {
-          'timeout' => '360'
-        }
-      },
+    if $primary_controller {
+      cs_resource { $res_name:
+        ensure          => present,
+        primitive_class => 'ocf',
+        provided_by     => 'mirantis',
+        primitive_type  => 'ceilometer-alarm-evaluator',
+        metadata        => { 'target-role' => 'stopped' },
+        parameters      => { 'user' => 'ceilometer' },
+        operations      => {
+          'monitor'  => {
+            'interval' => '20',
+            'timeout'  => '30'
+          }
+          ,
+          'start'    => {
+            'timeout' => '360'
+          }
+          ,
+          'stop'     => {
+            'timeout' => '360'
+          }
+        },
+      }
+      File['ceilometer-alarm-evaluator-ocf'] -> Cs_resource[$res_name] -> Service['ceilometer-alarm-evaluator']
+    } else {
+      File['ceilometer-alarm-evaluator-ocf'] -> Service['ceilometer-alarm-evaluator']
     }
-
-    cs_shadow { $res_name: cib => $cib_name }
-    cs_commit { $res_name: cib => $cib_name }
 
     service { 'ceilometer-alarm-evaluator':
       ensure     => $service_ensure,
@@ -97,12 +97,11 @@ class ceilometer::alarm::evaluator (
       provider   => "pacemaker",
     }
 
-    Cs_shadow[$res_name] ->
-      Cs_resource[$res_name] ->
-        Cs_commit[$res_name] ->
-          Service['ceilometer-alarm-evaluator']
+    Cs_resource[$res_name] -> Service['ceilometer-alarm-evaluator']
 
   } else {
+    Package['ceilometer-common'] -> Service['ceilometer-alarm-evaluator']
+    Package[$::ceilometer::params::alarm_package] -> Service['ceilometer-alarm-evaluator']
 
     service { 'ceilometer-alarm-evaluator':
       ensure     => $service_ensure,
