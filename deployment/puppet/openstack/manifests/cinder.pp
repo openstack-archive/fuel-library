57bcadcb3786ca411e0e13276eadcc65bd293de5
Aleksandr Didenko <adidenko@mirantis.com>
2014-06-19 13:54:11 +0300

    Adapt synced puppetlab-cinder for Fuel usage
    Implements: blueprint merge-openstack-puppet-modules
    
    Key moments:
      - Avoid changing upstream "cinder" module and  move all our
        custom settings into openstack::cinder class.
    
    Change-Id: Ia972a2ab6bacba16c4a7d503786651f85a700fd0

--- deployment/puppet/openstack/manifests/cinder.pp
+++ deployment/puppet/openstack/manifests/cinder.pp
@@ -41,39 +41,73 @@ class openstack::cinder(
   #  localhost anyway.
 
 
-  cinder_config { 'DEFAULT/auth_strategy': value => 'keystone' }
   cinder_config { 'DEFAULT/glance_api_servers': value => $glance_api_servers }
 
   if $queue_provider == 'rabbitmq' and $rabbit_ha_queues {
     Cinder_config['DEFAULT/rabbit_ha_queues']->Service<| title == 'cinder-api'|>
     Cinder_config['DEFAULT/rabbit_ha_queues']->Service<| title == 'cinder-volume' |>
     Cinder_config['DEFAULT/rabbit_ha_queues']->Service<| title == 'cinder-scheduler' |>
-    cinder_config { 'DEFAULT/rabbit_ha_queues': value => 'True' }
   }
 
-  class { 'cinder::base':
-    package_ensure      => $::openstack_version['cinder'],
-    queue_provider      => $queue_provider,
-    amqp_hosts          => $amqp_hosts,
-    amqp_user           => $amqp_user,
-    amqp_password       => $amqp_password,
-    sql_connection      => $sql_connection,
-    verbose             => $verbose,
-    use_syslog          => $use_syslog,
-    syslog_log_facility => $syslog_log_facility,
-    debug               => $debug,
-    max_retries         => $max_retries,
-    max_pool_size       => $max_pool_size,
-    max_overflow        => $max_overflow,
-    idle_timeout        => $idle_timeout,
+  case $queue_provider {
+    'rabbitmq': {
+      if $rabbit_ha_queues {
+        if !is_array($amqp_hosts) {
+          $rabbit_hosts_real = split($amqp_hosts, ',')
+        } else {
+          $rabbit_hosts_real = $amqp_hosts
+        }
+        # ::cinder module accepts rabbit_host and rabbit_port and then
+        # makes "rabbit_host:rabbit_port" from it. So we need to split
+        # our aqmp_hosts or provide defaults
+        $rabbit_host_array[0] = '127.0.0.1'
+        $rabbit_host_array[1] = '5672'
+      }
+      else {
+        $rabbit_hosts_real = false
+        # ::cinder module accepts rabbit_host and rabbit_port and then
+        # makes "rabbit_host:rabbit_port" from it. So we need to split
+        # our aqmp_hosts or provide defaults
+        $rabbit_host_array = split($amqp_hosts, ':')
+      }
+      class { '::cinder':
+        package_ensure        => $::openstack_version['cinder'],
+        rpc_backend           => 'cinder.openstack.common.rpc.impl_kombu',
+        rabbit_host           => $rabbit_host_array[0],
+        rabbit_port           => $rabbit_host_array[1],
+        rabbit_hosts          => $rabbit_hosts_real,
+        rabbit_userid         => $amqp_user,
+        rabbit_password       => $amqp_password,
+        rabbit_virtual_host   => $rabbit_virtual_host,
+        database_connection   => $sql_connection,
+        verbose               => $verbose,
+        use_syslog            => $use_syslog,
+        log_facility          => $syslog_log_facility,
+        debug                 => $debug,
+        database_idle_timeout => $idle_timeout,
+      }
+      cinder_config {
+        'DEFAULT/kombu_reconnect_delay': value => '5.0';
+      }
+    }
+    'qpid': {
+      $rpc_backend = 'cinder.openstack.common.rpc.impl_qpid'
+      cinder_config {
+        'DEFAULT/qpid_hosts':    value => $amqp_hosts;
+        'DEFAULT/qpid_username': value => $amqp_user;
+        'DEFAULT/qpid_password': value => $amqp_password;
+      }
+    }
   }
+
   if ($bind_host) {
     class { 'cinder::api':
+      keystone_enabled   => true,
       package_ensure     => $::openstack_version['cinder'],
       keystone_auth_host => $auth_host,
       keystone_password  => $cinder_user_password,
       bind_host          => $bind_host,
-      cinder_rate_limits => $cinder_rate_limits
+      ratelimits         => $cinder_rate_limits
     }
 
     class { 'cinder::scheduler':
@@ -88,16 +122,38 @@ class openstack::cinder(
     }
     case $manage_volumes {
       true, 'iscsi': {
+        if ($physical_volume) {
+          class { 'lvm':
+            vg     => $volume_group,
+            pv     => $physical_volume,
+            before => Service['cinder-volume'],
+          }
+        }
         class { 'cinder::volume::iscsi':
           iscsi_ip_address => $iscsi_bind_host,
-          physical_volume  => $physical_volume,
           volume_group     => $volume_group,
         }
       }
       'ceph': {
-        class {'cinder::volume::ceph': }
+        class {'cinder::volume::rbd':
+          rbd_pool        => $::ceph::cinder_pool,
+          rbd_user        => $::ceph::cinder_user,
+          rbd_secret_uuid => $::ceph::rbd_secret_uuid,
+        }
       }
     }
   }
-}
 
+  if $use_syslog {
+    cinder_config {
+      'DEFAULT/use_syslog_rfc_format': value => true;
+    }
+  }
+
+  cinder_config {
+    'database/max_pool_size': value => $max_pool_size;
+    'database/max_retries':   value => $max_retries;
+    'database/max_overflow':  value => $max_overflow;
+  }
+
+}
