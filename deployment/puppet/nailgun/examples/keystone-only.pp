c1cb05650d609bba9fcd45da94f2d344c729a1a8
Łukasz Oleś <loles@mirantis.com>
2014-06-24 00:24:31 +0200

    Add keystone container
    * add keystone do dockerctl
    * add puppet manifest which configures container
    
    Change-Id: I24fbfe6b4fc33d5c3abeb36e1b403cd167ba73f6
    Implements: blueprint access-control-master-node

---
+++ deployment/puppet/nailgun/examples/keystone-only.pp
@@ -0,0 +1,46 @@
+$fuel_settings = parseyaml($astute_settings_yaml)
+$fuel_version = parseyaml($fuel_version_yaml)
+
+if is_hash($::fuel_version) and $::fuel_version['VERSION'] and $::fuel_version['VERSION']['production'] {
+    $production = $::fuel_version['VERSION']['production']
+}
+else {
+    $production = 'prod'
+}
+
+package { 'python-psycopg2': 
+  ensure => installed,
+}
+
+case $production {
+  'prod', 'docker': {
+    class { 'keystone':
+      admin_token     => 'ADMIN',
+      catalog_type    => 'sql',
+      sql_connection => "postgresql://${::fuel_settings['postgres']['keystone_user']}:${::fuel_settings['postgres']['keystone_password']}@${::fuel_settings['ADMIN_NETWORK']['ipaddress']}/${::fuel_settings['postgres']['keystone_dbname']}",
+    }
+
+    keystone_tenant { 'admin' :
+      enabled => 'True',
+      ensure  => present
+    }
+
+    keystone_role {'admin' :
+      ensure => present
+    }
+
+    keystone_user { 'admin':
+      password => $::fuel_setting['FUEL_ACCESS']['password'],
+      ensure   => present,
+      enabled  => 'True',
+      tenant   => 'admin'
+    }
+
+    keystone_user_role { 'admin@admin':
+      roles  => ['admin'],
+      ensure => present
+    }
+  }
+  'docker-build': {
+  }
+}
