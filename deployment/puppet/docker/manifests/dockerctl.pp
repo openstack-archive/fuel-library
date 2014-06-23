a236021cf2238beeb0a1c7c87fd01b4086cd12de
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-06-23 21:41:37 +0400

    Improve firewall rules for Fuel Master
    Replaces hardcoded functions to create iptables
    rules for Nailgun and Cobbler with firewall module
    managed rules. Restricts access to Fuel Master
    for all services (except Fuel Web/API) to Admin
    Network, plus restricts postgres to localhost
    access only.
    
    blueprint master-node-iptables-ruleset
    
    Change-Id: Ib5d0c554bf97957c45b206b8bf4f6e64c9be109f

--- deployment/puppet/docker/manifests/dockerctl.pp
+++ deployment/puppet/docker/manifests/dockerctl.pp
@@ -1,8 +1,9 @@
 class docker::dockerctl (
-  $bin_dir     = '/usr/bin',
-  $share_dir   = '/usr/share/dockerctl',
-  $config_dir  = '/etc/dockerctl',
-  $profile_dir = '/etc/profile.d',
+  $bin_dir         = '/usr/bin',
+  $share_dir       = '/usr/share/dockerctl',
+  $config_dir      = '/etc/dockerctl',
+  $profile_dir     = '/etc/profile.d',
+  $admin_ipaddress = $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
   $release,
   $production,
 ) {
