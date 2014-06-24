a8d3fdbfc3facabad7a2815488b0cf9cdee48b4d
iberezovskiy <iberezovskiy@mirantis.com>
2014-06-24 19:52:38 +0400

    Update ceilometer-agent-notification puppet scripts
    Using own ceilometer-agent-notification rpm package to install
    
    Change-Id: Ic7bde3416ca40dbefcd30d3a1a595473530d37e5
    Closes-bug: #1333722

--- deployment/puppet/ceilometer/manifests/agent_notification.pp
+++ deployment/puppet/ceilometer/manifests/agent_notification.pp
@@ -20,22 +20,18 @@ class ceilometer::agent_notification (
 
   Ceilometer_config<||> ~> Service['ceilometer-agent-notification']
 
+  package { 'ceilometer-agent-notification':
+    ensure => installed,
+    name   => $::ceilometer::params::agent_notification_package,
+  }
+
   case $::osfamily {
-    'RedHat': {
-      Package['ceilometer-common'] -> Service['ceilometer-agent-notification']
-    }
     'Debian': {
-      package { 'ceilometer-agent-notification':
-        ensure => installed,
-        name   => $::ceilometer::params::agent_notification_package,
-      }
-
       tweaks::ubuntu_service_override { 'ceilometer-agent-notification' :}
-
-      Package['ceilometer-agent-notification'] -> Service['ceilometer-agent-notification']
     }
   }
 
+  Package['ceilometer-agent-notification'] -> Service['ceilometer-agent-notification']
 
   ###FIXME(aglarendil): remove this stupid cross-class dependency 
   ###move notification driver configuration into particular
@@ -49,60 +45,27 @@ class ceilometer::agent_notification (
       }
     }
 
-
   if $use_neutron {
     include neutron::notify::ceilometer
-
-    service { 'ceilometer-agent-notification':
-      ensure     => $service_ensure,
-      name       => $::ceilometer::params::agent_notification_service,
-      enable     => $enabled,
-      hasstatus  => true,
-      hasrestart => true,
-      require    => [
-        Class['ceilometer::db'],
-        Class['ceilometer::api'],
-        Class['neutron'],
-        Class['nova'],
-        Class['glance'],
-        Class['heat'],
-        Exec['ceilometer-dbsync'],
-      ],
-    }
-  } else {
-    service { 'ceilometer-agent-notification':
-      ensure     => $service_ensure,
-      name       => $::ceilometer::params::agent_notification_service,
-      enable     => $enabled,
-      hasstatus  => true,
-      hasrestart => true,
-      require    => [
-        Class['ceilometer::db'],
-        Class['ceilometer::api'],
-        Class['nova'],
-        Class['glance'],
-        Class['heat'],
-        Exec['ceilometer-dbsync'],
-      ],
-    }
   }
 
-  case $::osfamily {
-    'RedHat': {
-      Package<| title == 'ceilometer-collector' or title == 'ceilometer-common'|> ~>
-      Service<| title == 'ceilometer-agent-notification'|>
-      if !defined(Service['ceilometer-agent-notification']) {
-      notify{ "Module ${module_name} cannot notify service ceilometer-notification\
+  service { 'ceilometer-agent-notification':
+    ensure     => $service_ensure,
+    name       => $::ceilometer::params::agent_notification_service,
+    enable     => $enabled,
+    hasstatus  => true,
+    hasrestart => true,
+    require    => [
+      Class['ceilometer::db'],
+      Class['ceilometer::api'],
+      Exec['ceilometer-dbsync'],
+    ],
+  }
+
+  Package<| title == 'ceilometer-agent-notification' or title == 'ceilometer-common'|> ~>
+  Service<| title == 'ceilometer-agent-notification'|>
+  if !defined(Service['ceilometer-agent-notification']) {
+    notify{ "Module ${module_name} cannot notify service ceilometer-agent-notification\
  on packages update": }
-      }
-    }
-    'Debian': {
-      Package<| title == 'ceilometer-agent-notification' or title == 'ceilometer-common'|> ~>
-      Service<| title == 'ceilometer-agent-notification'|>
-      if !defined(Service['ceilometer-agent-notification']) {
-      notify{ "Module ${module_name} cannot notify service ceilometer-agent-notification\
- on packages update": }
-      }
-    }
   }
 }
