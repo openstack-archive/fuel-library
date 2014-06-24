2b91e8280aab6077d633b149fae373aba4c17fb3
Bartosz Kupidura <bkupidura@mirantis.com>
2014-06-24 09:40:14 +0200

    New RA for galera/pacemaker.
        Implements: blueprint galera-improvements
        Closes-Bug: 1297355
    
        Change-Id: I593d113b430d7607f92ff68ea269e071898a5068
    
    Change-Id: I3d6de157d39b843f1f44c880f05100bb95214090

--- deployment/puppet/galera/manifests/init.pp
+++ deployment/puppet/galera/manifests/init.pp
@@ -157,35 +157,44 @@ class galera (
     }
   }
 
-  cs_shadow { $res_name: cib => $cib_name }
-  cs_commit { $res_name: cib => $cib_name }
-
-  Cs_commit["$res_name"] -> Service["mysql"]
+  $galera_pid = $osfamily ? {
+    'RedHat' => '/var/run/mysql/mysqld.pid',
+    'Debian' => '/var/run/mysqld/mysqld.pid',
+  }
+  $galera_socket = $osfamily ? {
+    'RedHat' => '/var/lib/mysql/mysql.sock',
+    'Debian' => '/var/run/mysqld/mysqld.sock',
+  }
 
   cs_resource { "$res_name":
-      ensure => present,
-      cib => $cib_name,
-      primitive_class => 'ocf',
-      provided_by     => 'mirantis',
-      primitive_type => 'mysql-wss',
-      multistate_hash => {
-        'type' => 'clone',
+    ensure => present,
+    primitive_class => 'ocf',
+    provided_by     => 'mirantis',
+    primitive_type => 'mysql-wss',
+    multistate_hash => {
+      'type' => 'clone',
+    },
+    ms_metadata => {
+      'interleave' => 'true',
+    },
+    parameters => {
+      'test_user'   => "${mysql_user}",
+      'test_passwd' => "${mysql_password}",
+      'pid'         => "${galera_pid}",
+      'socket'      => "${galera_socket}",
+    },
+    operations => {
+      'monitor' => {
+        'interval' => '60',
+        'timeout' => '55'
       },
-      ms_metadata => {
-        'interleave' => 'true',
+      'start' => {
+        'timeout' => '475'
       },
-      operations => {
-        'monitor' => {
-          'interval' => '60',
-          'timeout' => '55'
-        },
-        'start' => {
-          'timeout' => '475'
-        },
-        'stop' => {
-          'timeout' => '175'
-        },
+      'stop' => {
+        'timeout' => '175'
       },
+    },
   }
 
   file {'mysql-wss-ocf':
@@ -209,9 +218,7 @@ class galera (
   }
 
   Anchor['galera']       ->
-  Cs_shadow[$res_name]   ->
   Cs_resource[$res_name] ->
-  Cs_commit[$res_name]   ->
   Service[$cib_name]     ->
   Anchor['galera-done']
 
