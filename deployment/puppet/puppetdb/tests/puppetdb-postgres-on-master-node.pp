c62d9ce2dbf59030730b2e289d69cf446e293529
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2014-06-25 17:05:31 +0400

    Remove unused puppet module puppetdb
    Change-Id: I13f2141b7b8c121e41da67401e49aeefac484f43
    Implements: blueprint merge-openstack-puppet-modules

--- deployment/puppet/puppetdb/tests/puppetdb-postgres-on-master-node.pp
+++
@@ -1,9 +0,0 @@
-# This is an example of how to get puppetdb up and running on the same node
-# where your puppet master is running, using our recommended database server
-# (postgresql).
-
-# Configure puppetdb and its postgres database:
-include puppetdb
-
-# Configure the puppet master to use puppetdb.
-include puppetdb::master::config
