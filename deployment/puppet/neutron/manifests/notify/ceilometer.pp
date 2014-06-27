79271ffdaa34167d0a2cbae9e85de56018e0b7a1
Andrew Woodward <awoodward@mirantis.com>
2014-06-27 15:14:21 -0700

    *WIP* Purge neutron folder before sync
    Will be droped, is a paceholder to make rebase easier
    
    Change-Id: I9b69c2ab4cb4e476083ec9516f442d9b1911b2a7

--- deployment/puppet/neutron/manifests/notify/ceilometer.pp
+++
@@ -1,8 +0,0 @@
-class neutron::notify::ceilometer (
-  $driver = "neutron.openstack.common.notifier.rpc_notifier",
-)
-{
-  neutron_config {
-    'DEFAULT/notification_driver': value => $driver;
-  }
-}
