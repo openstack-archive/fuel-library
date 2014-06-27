8be150c1fdd06413301a9bbf85c4e6f1d14db645
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2014-06-27 10:21:27 +0000

    Sync puppet-ceilometer
    4.0.0 b231c9681d476a05f437f828b95870ab4cbc4327
    
    Change-Id: Ifa403d8066ede566dddbf49d67308c080b97ea77
    Implements: blueprint merge-openstack-puppet-modules
    Signed-Off: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/ceilometer/manifests/db/mysql/host_access.pp
+++ deployment/puppet/ceilometer/manifests/db/mysql/host_access.pp
@@ -13,18 +13,35 @@
 #  [*database*]
 #    the database name
 #
-define ceilometer::db::mysql::host_access ($user, $password, $database)  {
+define ceilometer::db::mysql::host_access ($user, $password, $database, $mysql_module='0.9')  {
+  # New types for mysql module v2.0+
+  if ($mysql_module >= 2.2) {
+    mysql_user { "${user}@${name}":
+      password_hash => mysql_password($password),
+      provider      => 'mysql',
+      require       => Mysql_database[$database],
+    }
 
-  database_user { "${user}@${name}":
-    password_hash => mysql_password($password),
-    provider      => 'mysql',
-    require       => Database[$database],
-  }
+    mysql_grant { "${user}@${name}/${database}":
+      privileges => 'all',
+      provider   => 'mysql',
+      table      => "${database}.*",
+      user       => "${user}@${name}",
+      require    => Mysql_user["${user}@${name}"]
+    }
 
-  database_grant { "${user}@${name}/${database}":
-    # TODO figure out which privileges to grant.
-    privileges => 'all',
-    provider   => 'mysql',
-    require    => Database_user["${user}@${name}"]
+  } else {
+    database_user { "${user}@${name}":
+      password_hash => mysql_password($password),
+      provider      => 'mysql',
+      require       => Database[$database],
+    }
+
+    database_grant { "${user}@${name}/${database}":
+      # TODO figure out which privileges to grant.
+      privileges => 'all',
+      provider   => 'mysql',
+      require    => Database_user["${user}@${name}"]
+    }
   }
 }
