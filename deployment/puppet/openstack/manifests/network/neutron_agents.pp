998f5b2c1a4a8993c61962fe04dc1e498d384754
Andrew Woodward <awoodward@mirantis.com>
2014-06-27 15:40:32 -0700

    Adapt upstream puppet-neutron into fuel
    Fix up new entry points
    Refactor openstack:: to enter neutron instead of old sanitize_network
    Plug in new neutron HA composition
    Add in some changes that need to make it back into upstream eventually.
    
    partial blueprint: merge-openstack-puppet-modules
    blueprint: ml2-neutron
    
    Change-Id: I4af4b70173007e3d780ca4fcde3d6ae30d5d9659

---
+++ deployment/puppet/openstack/manifests/network/neutron_agents.pp
@@ -0,0 +1,189 @@
+# Not a doc string
+
+# agents should be a list of agents we want enabled
+#
+# ha_agents should be false, "primary", or "slave"
+#  primary ensures that any relevant cluster resources will be created.
+#  slave ensures that agents are stopped but doesn't attempt to manage them
+#  the asumption here is that the slave will get the membership from the
+#  cluster and it will manage the resources
+
+
+class openstack::network::neutron_agents (
+  $agents     = ['ml2-ovs'],
+  $ha_agents  = false,
+  $verbose    = false,
+  $debug      = false,
+
+  # ovs
+  $enable_tunneling     = false,
+  $tunnel_bridge        = 'br-tun',
+  $tunnel_id_ranges     = ['20:100'],
+  $integration_bridge   = 'br-int',
+  $bridge_mappings      = [],
+  $network_vlan_ranges  = ['physnet1:1000:2999'],
+  $local_ip             = false,
+
+  # ML2 settings
+  $type_drivers          = ['local', 'flat', 'vlan', 'gre', 'vxlan'],
+  $tenant_network_types  = ['local', 'flat', 'vlan', 'gre', 'vxlan'],
+  $mechanism_drivers     = ['openvswitch', 'linuxbridge'],
+  $flat_networks         = ['*'],
+  $vxlan_group           = '224.0.0.1',
+  $vni_ranges            = ['10:100'],
+
+  # metadata-agent
+  $shared_secret,
+  $metadata_ip = '127.0.0.1',
+
+  # dhcp-agent
+  $resync_interval = 30,
+  $use_namespaces = true,
+
+  # l3-agent
+  $metadata_port = 9697,
+  $send_arp_for_ha = 3,
+  $external_network_bridge = 'br-ex',
+
+  # keystone params
+  $admin_password    = 'asdf123',
+  $admin_tenant_name = 'services',
+  $admin_username    = 'neutron',
+  $auth_url          = 'http://localhost:35357/v2.0',
+  $auth_region       = 'RegionOne',
+) {
+
+  if 'ovs' in $agents {
+    class { '::neutron::plugins::ovs':
+      tunnel_id_ranges    => $tunnel_id_ranges[0],
+      tenant_network_type => $tenant_network_types[0],
+      network_vlan_ranges => $network_vlan_ranges[0],
+    }
+    class { 'neutron::agents::ovs':
+      integration_bridge  => $integration_bridge,
+      tunnel_bridge       => $tunnel_bridge,
+      bridge_mappings     => $bridge_mappings,
+      enable_tunneling    => $enable_tunneling,
+      local_ip            => $local_ip,
+    }
+    Service<| title == 'neutron-server' |> -> Service<| title == 'neutron-ovs-agent-service' |>
+    Service<| title == 'neutron-server' |> -> Service<| title == 'ovs-cleanup-service' |>
+    if $ha_agents {
+      class {'cluster::neutron::ovs':
+        primary => $ha_agents ? { 'primary' => true, default => false},
+        require => Class['::neutron::agents::ovs']
+      }
+    }
+  }
+
+  if 'ml2-ovs' in $agents {
+    class { 'neutron::plugins::ml2':
+      type_drivers          => $type_drivers,
+      tenant_network_types  => $tenant_network_types,
+      mechanism_drivers     => $mechanism_drivers,
+      flat_networks         => $flat_networks,
+      network_vlan_ranges   => $network_vlan_ranges,
+      tunnel_id_ranges      => $tunnel_id_ranges,
+      vxlan_group           => $vxlan_group,
+      vni_ranges            => $vni_ranges,
+    }
+    class { 'neutron::agents::ml2::ovs':
+      integration_bridge  => $integration_bridge,
+      tunnel_bridge       => $tunnel_bridge,
+      bridge_mappings     => $bridge_mappings,
+      enable_tunneling    => $enable_tunneling,
+      local_ip            => $local_ip,
+    }
+
+    # Workaround for https://bugs.launchpad.net/fuel/+bug/1335869
+    file {'/etc/neutron/plugins/openvswitch':
+      ensure  => directory,
+      recurse => true,
+      require => File['/etc/neutron/plugin.ini'],
+    } ->
+    file {'/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini':
+      ensure  => link,
+      target  => '/etc/neutron/plugins/ml2/ml2_conf.ini',
+      require => File['/etc/neutron/plugin.ini'],
+      before  => Service['neutron-ovs-agent-service']
+    }
+    Service<| title == 'neutron-server' |> -> Service<| title == 'neutron-ovs-agent-service' |>
+    Service<| title == 'neutron-server' |> -> Service<| title == 'ovs-cleanup-service' |>
+
+    if $ha_agents {
+      class {'cluster::neutron::ovs':
+        primary => $ha_agents ? { 'primary' => true, default => false},
+        require => Class['::neutron::agents::ml2::ovs']
+      }
+    }
+  }
+
+  if 'linuxbridge' in $agents {
+    # FIXME(xarses): needs neutron-plugin-linuxbridge* packages
+  }
+
+  if 'metadata' in $agents {
+    class {'::neutron::agents::metadata':
+      debug         => $debug,
+      auth_region   => $auth_region,
+      auth_url      => $auth_url,
+      auth_user     => $admin_username,
+      auth_tenant   => $admin_tenant_name,
+      auth_password => $admin_password,
+      shared_secret => $shared_secret,
+      metadata_ip   => $metadata_ip
+    }
+    Service<| title == 'neutron-server' |> -> Service<| title == 'neutron-metadata' |>
+    if $ha_agents {
+      class {'cluster::neutron::metadata':
+          primary => $ha_agents ? { 'primary' => true, default => false},
+          require => Class['::neutron::agents::metadata']
+      }
+    }
+  }
+
+  if 'dhcp' in $agents {
+    class { '::neutron::agents::dhcp':
+      debug           => $debug,
+      resync_interval => $resync_interval,
+      use_namespaces  => $use_namespaces,
+    }
+    Service<| title == 'neutron-server' |> -> Service<| title == 'neutron-dhcp-service' |>
+    if $ha_agents {
+      class {'cluster::neutron::dhcp':
+        ha_agents         => $agents,
+        admin_password    => $admin_password,
+        admin_tenant_name => $admin_tenant_name,
+        admin_username    => $admin_username,
+        auth_url          => $auth_url,
+        primary           => $ha_agents ? { 'primary' => true, default => false},
+        require           => Class['::neutron::agents::dhcp']
+      }
+    }
+  }
+
+  if 'l3' in $agents {
+    class { '::neutron::agents::l3':
+      debug                   => $debug,
+      metadata_port           => $metadata_port,
+      send_arp_for_ha         => $send_arp_for_ha,
+      external_network_bridge => $external_network_bridge,
+    }
+    Service<| title == 'neutron-server' |> -> Service<| title == 'neutron-l3' |>
+    if $ha_agents {
+      # Yes, l3 is supposed to be a defined resource, this will become
+      # necessary when we start supporting multiple external routers.
+      cluster::neutron::l3 {'default-l3':
+        debug             => $debug,
+        verbose           => $verbose,
+        ha_agents         => $agents,
+        admin_password    => $admin_password,
+        admin_tenant_name => $admin_tenant_name,
+        admin_username    => $admin_username,
+        auth_url          => $auth_url,
+        primary           => $ha_agents ? { 'primary' => true, default => false},
+        require           => Class['::neutron::agents::metadata']
+      }
+    }
+  }
+}
--- deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
@@ -4,16 +4,21 @@ class osnailyfacter::cluster_ha {
 
   $primary_controller = $::fuel_settings['role'] ? { 'primary-controller'=>true, default=>false }
 
-
-  if $::use_quantum {
+  if $::use_neutron {
     $novanetwork_params  = {}
-    $quantum_config = sanitize_neutron_config($::fuel_settings, 'quantum_settings')
+    $neutron_config      = $::fuel_settings['quantum_settings']
+    $network_provider    = 'neutron'
+    $neutron_db_password       = $neutron_config['database']['password']
+    $neutron_user_password     = $neutron_config['keystone']['admin_password']
+    $neutron_meta_proxy_secret = $neutron_config['metadata']['metadata_proxy_shared_secret']
+    $base_mac                  = $neutron_config['L2']['base_mac']
   } else {
-    $quantum_config = {}
-    $novanetwork_params  = $::fuel_settings['novanetwork_parameters']
+    $neutron_config = {}
+    $novanetwork_params   = $::fuel_settings['novanetwork_parameters']
     $network_size         = $novanetwork_params['network_size']
     $num_networks         = $novanetwork_params['num_networks']
     $vlan_start           = $novanetwork_params['vlan_start']
+    $network_provider     = 'nova'
   }
 
   if $cinder_nodes {
@@ -88,7 +93,7 @@ class osnailyfacter::cluster_ha {
     $rabbit_hash['user'] = 'nova'
   }
 
-  if ! $::use_quantum {
+  if ! $::use_neutron {
     $floating_ips_range = $::fuel_settings['floating_network_range']
   }
   $floating_hash = {}
@@ -248,14 +253,11 @@ class osnailyfacter::cluster_ha {
   #HARDCODED PARAMETERS
 
   $multi_host              = true
-  $quantum_netnode_on_cnt  = true
   $mirror_type = 'external'
   Exec { logoutput => true }
 
   class compact_controller (
     $primary_controller,
-    $quantum_network_node = $quantum_netnode_on_cnt
-
   ) {
 
     class {'osnailyfacter::apache_api_proxy':}
@@ -266,12 +268,12 @@ class osnailyfacter::cluster_ha {
       controller_internal_addresses  => $::osnailyfacter::cluster_ha::controller_internal_addresses,
       internal_address               => $::internal_address,
       public_interface               => $::public_int,
-      private_interface              => $::use_quantum ? { true=>false, default=>$::fuel_settings['fixed_interface']},
+      private_interface              => $::use_neutron ? { true=>false, default=>$::fuel_settings['fixed_interface']},
       internal_virtual_ip            => $::fuel_settings['management_vip'],
       public_virtual_ip              => $::fuel_settings['public_vip'],
       primary_controller             => $primary_controller,
-      floating_range                 => $::use_quantum ? { true=>$floating_hash, default=>false},
-      fixed_range                    => $::use_quantum ? { true=>false, default=>$::fuel_settings['fixed_network_range']},
+      floating_range                 => $::neutron ? { true=>$floating_hash, default=>false},
+      fixed_range                    => $::neutron ? { true=>false, default=>$::fuel_settings['fixed_network_range']},
       multi_host                     => $::osnailyfacter::cluster_ha::multi_host,
       network_manager                => $::osnailyfacter::cluster_ha::network_manager,
       num_networks                   => $::osnailyfacter::cluster_ha::num_networks,
@@ -305,10 +307,14 @@ class osnailyfacter::cluster_ha {
       glance_backend                 => $::osnailyfacter::cluster_ha::glance_backend,
       swift_proxies                  => $::osnailyfacter::cluster_ha::swift_proxies,
       rgw_servers                    => $::osnailyfacter::cluster_ha::rgw_servers,
-      quantum                        => $::use_quantum,
-      quantum_config                 => $::osnailyfacter::cluster_ha::quantum_config,
-      quantum_network_node           => $::use_quantum,
-      quantum_netnode_on_cnt         => $::use_quantum,
+
+      network_provider               => $::osnailyfacter::cluster_ha::network_provider,
+      neutron_db_password            => $::osnailyfacter::cluster_ha::neutron_db_password,
+      neutron_user_password          => $::osnailyfacter::cluster_ha::neutron_user_password,
+      neutron_meta_proxy_secret      => $::osnailyfacter::cluster_ha::neutron_meta_proxy_secret,
+      neutron_ha_agents              => $::osnailyfacter::cluster_ha::primary_controller ? {true => 'primary', default => 'slave'},
+      base_mac                       => $::osnailyfacter::cluster_ha::base_mac,
+
       cinder                         => true,
       cinder_user_password           => $::osnailyfacter::cluster_ha::cinder_hash[user_password],
       cinder_iscsi_bind_addr         => $::osnailyfacter::cluster_ha::cinder_iscsi_bind_addr,
@@ -388,7 +394,8 @@ class osnailyfacter::cluster_ha {
         # so we just need to make sure package glance-common (dependency for glance-api) is already installed
         # before creating swift device directories
 
-        Package[$glance::params::api_package_name] -> Swift::Storage::All::Device_directory <| |>
+        Package<| title == $::glance::params::api_package_name |> -> Swift::Storage::All::Device_directory <| |>
+        Package<| title == $::glance::params::api_package_name |> -> File <| title == $swift_partition |>
 
         class { 'openstack::swift::storage_node':
           storage_type          => $swift_loopback,
@@ -436,7 +443,7 @@ class osnailyfacter::cluster_ha {
       nova_config { 'DEFAULT/use_cow_images':            value => $::fuel_settings['use_cow_images'] }
       nova_config { 'DEFAULT/compute_scheduler_driver':  value => $::fuel_settings['compute_scheduler_driver'] }
 
-      if ! $::use_quantum {
+      if ! $::use_neutron {
         if $primary_controller {
           exec { 'wait-for-haproxy-nova-backend':
             command   => "echo show stat | socat unix-connect:///var/lib/haproxy/stats stdio | grep -q '^nova-api-2,BACKEND,.*,UP,'",
@@ -478,7 +485,7 @@ class osnailyfacter::cluster_ha {
           sahara_keystone_password   => $sahara_hash['user_password'],
           sahara_keystone_tenant     => 'services',
 
-          use_neutron                => $::use_quantum,
+          use_quantum                => $::use_neutron,
           use_floating_ips           => $::fuel_settings['auto_assign_floating_ip'],
 
           syslog_log_facility_sahara => $syslog_log_facility_sahara,
@@ -486,10 +493,11 @@ class osnailyfacter::cluster_ha {
           verbose                    => $::verbose,
           use_syslog                 => $::use_syslog,
         }
-          $scheduler_default_filters = [ 'DifferentHostFilter' ]
+        $scheduler_default_filters = [ 'DifferentHostFilter' ]
       } else {
         $scheduler_default_filters = []
       }
+
 
       class { '::nova::scheduler::filter':
         cpu_allocation_ratio       => '8.0',
@@ -563,7 +571,7 @@ class osnailyfacter::cluster_ha {
           murano_keystone_password => $murano_hash['user_password'],
           murano_keystone_tenant   => 'services',
 
-          use_neutron              => $::use_quantum,
+          use_quantum              => $::use_neutron,
 
           use_syslog               => $::use_syslog,
           debug                    => $::debug,
@@ -585,7 +593,7 @@ class osnailyfacter::cluster_ha {
           vcenter_password  => $vcenter_hash['vc_password'],
           vcenter_host_ip   => $vcenter_hash['host_ip'],
           vcenter_cluster   => $vcenter_hash['cluster'],
-          use_quantum       => $::use_quantum,
+          use_quantum       => $::use_neutron,
           ha_mode           => true,
         }
       }
@@ -599,10 +607,10 @@ class osnailyfacter::cluster_ha {
 
       class { 'openstack::compute':
         public_interface            => $::public_int,
-        private_interface           => $::use_quantum ? { true=>false, default=>$::fuel_settings['fixed_interface'] },
+        private_interface           => $::use_neutron ? { true=>false, default=>$::fuel_settings['fixed_interface'] },
         internal_address            => $::internal_address,
         libvirt_type                => $::fuel_settings['libvirt_type'],
-        fixed_range                 => $::use_quantum ? { true=>false, default=>$::fuel_settings['fixed_network_range']},
+        fixed_range                 => $::use_neutron ? { true=>false, default=>$::fuel_settings['fixed_network_range']},
         network_manager             => $network_manager,
         network_config              => $network_config,
         multi_host                  => $multi_host,
@@ -632,8 +640,11 @@ class osnailyfacter::cluster_ha {
         ceilometer_metering_secret  => $ceilometer_hash[metering_secret],
         ceilometer_user_password    => $ceilometer_hash[user_password],
         db_host                     => $::fuel_settings['management_vip'],
-        quantum                     => $::use_quantum,
-        quantum_config              => $quantum_config,
+
+        network_provider            => $::osnailyfacter::cluster_ha::network_provider,
+        neutron_user_password       => $::osnailyfacter::cluster_ha::neutron_user_password,
+        base_mac                    => $::osnailyfacter::cluster_ha::base_mac,
+
         use_syslog                  => $use_syslog,
         syslog_log_facility         => $::syslog_log_facility_nova,
         syslog_log_facility_neutron => $::syslog_log_facility_neutron,
