39ec21cce6067d4439b7ab1d0abfbf23e69ae52c
Andrew Woodward <awoodward@mirantis.com>
2014-06-27 15:31:43 -0700

    Neutron HA composition model
    Refactor all of the corosync resources to be outside of the neutron module
    
    This allows for seperate management of the HA scripts and classes from the
     neutron module and will allow the module to continue to track upstream with
     out modifiation in regards to HA support
    
    partial blueprint: merge-openstack-puppet-modules
    blueprint: ml2-neutron
    
    Change-Id: If471a371c11a26f5b4182bc3afcf391d000124aa

---
+++ deployment/puppet/cluster/manifests/corosync/cs_service.pp
@@ -0,0 +1,96 @@
+# Not a doc string
+
+define cluster::corosync::cs_service (
+  $ocf_script,
+  $service_name,
+  $csr_multistate_hash = undef,
+  $csr_ms_metadata = undef,
+  $csr_parameters = undef,
+  $csr_metadata = undef,
+  $csr_mon_intr = 20,
+  $csr_mon_timeout = 20,
+  $csr_timeout = 60,
+  $mangle_real_service = false,
+  $service_alias = undef,
+  $package = false,
+  $primary = true,
+  $hasrestart = true,
+  )
+{
+  # OCF script for pacemaker
+  file {$ocf_script:
+    path   => "/usr/lib/ocf/resource.d/mirantis/${ocf_script}",
+    mode   => '0755',
+    owner  => root,
+    group  => root,
+    source => "puppet:///modules/cluster/ocf/${ocf_script}",
+    before => Cs_resource["p_${service_name}"],
+  }
+
+  if $primary {
+    cs_resource { "p_${service_name}":
+      ensure          => present,
+      primitive_class => 'ocf',
+      provided_by     => 'mirantis',
+      primitive_type  => $ocf_script,
+      multistate_hash => $csr_multistate_hash,
+      ms_metadata     => $csr_ms_metadata,
+      parameters      => $csr_parameters,
+      metadata        => $csr_metadata,
+      operations      => {
+        'monitor' => {
+          'interval' => $csr_mon_intr,
+          'timeout'  => $csr_mon_timeout
+        }
+        ,
+        'start'   => {
+          'timeout' => $csr_timeout
+        }
+        ,
+        'stop'    => {
+          'timeout' => $csr_timeout
+        }
+      },
+      before          => Service["p_${service_name}"],
+      require         => File[$ocf_script]
+    }
+
+  }
+
+  if $mangle_real_service {
+    # If the service is defined elsewhere, then we need to disable it. Some
+    # service manifests will do this for us (which is preferred)
+    service { "${service_name}-disable-init":
+      name       => $service_name,
+      enable     => false,
+      ensure     => stopped,
+      hasstatus  => true,
+      hasrestart => true,
+      before     => Service["p_$service_name"]
+    }
+  }
+
+  # Ubuntu packages like to auto-start, this is annoying and makes it harder
+  # to put them under pacemaker. In these cases, we need to inject the
+  # override file before the package is installed. When upstart sees this it
+  # will cause it to ignore the autostart that the service might of had.
+  if $::operatingsystem == 'Ubuntu' and $package {
+    file {"/etc/init/${service_name}.override":
+      replace => 'no',
+      ensure  => present,
+      content => 'manual',
+      mode    => '0644'
+    } -> Package <| title == $package |>
+  }
+
+  Service <| title == $service_name or name == $service_name |> ->
+  service { "p_${service_name}":
+    enable     => true,
+    alias      => $service_alias,
+    ensure     => running,
+    hasstatus  => true,
+    hasrestart => $hasrestart,
+    provider   => "pacemaker",
+  }
+
+}
