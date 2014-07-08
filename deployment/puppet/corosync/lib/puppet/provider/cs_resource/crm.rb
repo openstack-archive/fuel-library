bf12bcf378db702a9234a46a792bf890b3864ada
Vladimir Kuklin <vkuklin@mirantis.com>
2014-07-08 15:24:07 +0400

    Apply CIB changes using patch method
    Generate XML diff patch and apply it.
    
    1) generate config changes in shadow
    2) generate diff only for specific scope
    3) apply it
    
    This will not touch cluster attributes and
    other stuff it should not touch, thus not
    breaking functionality of OCF scripts using
    CIB attributes.
    
    Change-Id: Ibcfaa82707d1aed01b79c8307d17f4e71fd60906
    Closes-bug: #1338594

--- deployment/puppet/corosync/lib/puppet/provider/cs_resource/crm.rb
+++ deployment/puppet/corosync/lib/puppet/provider/cs_resource/crm.rb
@@ -11,8 +11,14 @@ Puppet::Type.type(:cs_resource).provide(:crm, :parent => Puppet::Provider::Coros
         better model since these values can be almost anything.'
 
   # Path to the crm binary for interacting with the cluster configuration.
+
+
+  commands :cibadmin => 'cibadmin'
+  commands :crm_shadow => 'crm_shadow'
   commands :crm => 'crm'
+  commands :crm_diff => 'crm_diff'
   commands :crm_attribute => 'crm_attribute'
+
   def self.instances
 
     block_until_ready
@@ -125,6 +131,8 @@ Puppet::Type.type(:cs_resource).provide(:crm, :parent => Puppet::Provider::Coros
     debug('Stopping primitive before removing it')
     crm('resource', 'stop', @resource[:name])
     debug('Removing primitive')
+    ## FIXME(aglarendil): may be we need to apply crm_diff related approach
+    ## FIXME(aglarendil): due to 1338594 bug and do this in flush section
     try_command("delete",@resource[:name])
     @property_hash.clear
   end
@@ -258,9 +266,9 @@ Puppet::Type.type(:cs_resource).provide(:crm, :parent => Puppet::Provider::Coros
       Tempfile.open('puppet_crm_update') do |tmpfile|
         tmpfile.write(updated)
         tmpfile.flush
-        env = {}
-        env["CIB_shadow"] = @resource[:cib].to_s if !@resource[:cib].nil?
-        exec_withenv("#{command(:crm)} configure load update #{tmpfile.path.to_s}",env)
+        #env["CIB_shadow"] = @resource[:cib].to_s if !@resource[:cib].nil?
+        ##LP1338594 part: should be put into separate method, I guess
+        apply_changes(@resource[:name],tmpfile,'resource')
       end
     end
   end
