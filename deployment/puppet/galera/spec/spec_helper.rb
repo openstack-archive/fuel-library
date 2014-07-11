41b6d54e6fb58363584bf4711ded328bed465b02
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2014-07-11 19:36:28 +0000

    Refactor Galera module
    - Implements clustercheck script
    - Switch haproxy to clustercheck
    - Refactor Galera module
    - Fix linting
    - Add Rakefile, Gemfile
    - Update Modulefile
    
    Closes-Bug: 1331488
    Change-Id: I14aaf056ec7a086ae07ab699bcb9b8f1403d8490
    Implements: blueprint galera-improvements

---
+++ deployment/puppet/galera/spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'puppetlabs_spec_helper/module_spec_helper'
+
+RSpec.configure do |c|
+  c.alias_it_should_behave_like_to :it_configures, 'configures'
+  c.alias_it_should_behave_like_to :it_raises, 'raises'
+end
