bb5157c928370519d3bbf4dc97a4fd48f7cf9581
Vladimir Kuklin <vkuklin@mirantis.com>
2014-07-22 22:20:27 +0400

    WIP: rabbitmqctl node fencing draft
    This script should be used when corosync
    notifies that particular node is dead.
    
    DO NOT TOUCH. WORK IN PROGRESS
    
    Change-Id: I691363386efe01421acc317ef6371ce45a0d4d11

---
+++ deployment/puppet/nova/files/rabbitmq-kick.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+node_name=$1
+
+test -n ${node_name} || exit 1
+
+node_to_remove=rabbit@${node_name}
+
+rabbitmqctl eval \'"mnesia_lib:del({schema,active_replicas},list_to_atom(\"${node_to_remove}\"))."\'
+rabbitmqctl eval \'"mnesia_lib:del({current,db_nodes},list_to_atom(\"${node_to_remove}\"))."\'
+rabbitmqctl eval \'"mnesia_lib:del({current,extra_db_nodes},list_to_atom(\"${node_to_remove}\"))."\'
+rabbitmqctl forget_cluster_node ${node_to_remove}
