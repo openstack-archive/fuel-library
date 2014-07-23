508c785b4c1ad4b30db4f9fe6c7f8f54ef0195bb
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2014-07-23 12:57:19 +0400

    Replace ntp package with ntp-dev
    Change-Id: Ie8170c358510de900e7ae1d9c6c373e4296a49f3
    Related-Bug: 1336748

--- deployment/puppet/openstack/manifests/clocksync.pp
+++ deployment/puppet/openstack/manifests/clocksync.pp
@@ -1,26 +1,25 @@
 class openstack::clocksync ($ntp_servers = undef, $config_template = undef)
 {
 
-  if !$ntp_servers {
-    $one_shot_ntp_server = 'pool.ntp.org'
-  } else {
-    $one_shot_ntp_server = $ntp_servers[0]
-  }
-  class { 'ntp': 
-    servers         => $ntp_servers, 
-    config_template => $config_template
-  }
+ if !$ntp_servers {
+   $one_shot_ntp_server = 'pool.ntp.org'
+ } else {
+   $one_shot_ntp_server = $ntp_servers[0]
+ }
+ class { 'ntp': 
+   servers         => $ntp_servers,
+   package_name    => ['ntp-dev'],
+   config_template => $config_template,
+ }
 
-  Exec['clocksync'] -> Service <| title == 'ntp' |>
+ Exec['clocksync'] -> Service <| title == 'ntp' |>
 
-  package {'ntpdate': ensure => present}
-
-  exec {'clocksync':
-    unless  => "pidof ntpd",
-    require => Package['ntpdate'],
-    command => "bash -c 'for i in {1..10}; do ntpdate $one_shot_ntp_server &&  break; sleep 1; done'",
-    path    => ['/usr/bin', '/usr/sbin', '/sbin', '/bin'],
-  }
+ exec {'clocksync':
+   unless  => "pidof ntpd",
+   require => Package['ntp-dev'],
+   command => "bash -c 'for i in {1..10}; do sntp $one_shot_ntp_server &&  break; sleep 1; done'",
+   path    => ['/usr/bin', '/usr/sbin', '/sbin', '/bin'],
+ }
 }
 
 
