e533688882d43dd7c5d135451fe380f8ff387e14
Sergey Vasilenko <svasilenko@mirantis.com>
2014-07-23 15:51:16 +0400

    More stict naming of OVS packets in L23network
    Fix losted dependency.
    Remove unnided if for MLNX
    
    Change-Id: I0bceb2d9c81e378c16d5416a5125bb67bf81fae5

--- deployment/puppet/l23network/manifests/params.pp
+++ deployment/puppet/l23network/manifests/params.pp
@@ -3,7 +3,6 @@ class l23network::params {
     /(?i)debian/: {
       $ovs_service_name   = 'openvswitch-switch'
       $ovs_status_cmd     = '/etc/init.d/openvswitch-switch status'
-      $ovs_packages       = ['openvswitch-switch']
       $lnx_vlan_tools     = 'vlan'
       $lnx_bond_tools     = 'ifenslave'
       $lnx_ethernet_tools = 'ethtool'
@@ -11,25 +10,9 @@ class l23network::params {
     /(?i)redhat/: {
       $ovs_service_name   = 'openvswitch' #'ovs-vswitchd'
       $ovs_status_cmd     = '/etc/init.d/openvswitch status'
-      $ovs_packages       = ['openvswitch']
       $lnx_vlan_tools     = 'vconfig'
       $lnx_bond_tools     = undef
       $lnx_ethernet_tools = 'ethtool'
-    }
-    /(?i)linux/: {
-      case $::operatingsystem {
-        /(?i)archlinux/: {
-          $ovs_service_name   = 'openvswitch.service'
-          $ovs_status_cmd     = 'systemctl status openvswitch'
-          $ovs_packages       = ['aur/openvswitch']
-          $lnx_vlan_tools     = 'aur/vconfig'
-          $lnx_bond_tools     = 'core/ifenslave'
-          $lnx_ethernet_tools = 'extra/ethtool'
-        }
-        default: {
-          fail("Unsupported OS: ${::osfamily}/${::operatingsystem}")
-        }
-      }
     }
     default: {
       fail("Unsupported OS: ${::osfamily}/${::operatingsystem}")
