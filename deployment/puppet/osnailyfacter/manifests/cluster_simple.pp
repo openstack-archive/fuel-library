637a996ed087ef7decbdd41c80bb9ba7712c42f9
Igor Yozhikov <iyozhikov@mirantis.com>
2014-08-01 21:59:50 +0400

    osnailyfacter changes for heat
    * Add keystone_ec2_uri parameter to the openstack:heat section.
    This changeset rewrites default values for heat::keystone_ec2_uri,
    which points to localhost instead of controller_node_address.
    * Remove RedHat OS check for openstack:heat.
    
    Change-Id: Id170eb1f6061f08154db110ef15d55bfbbfdf67c

--- deployment/puppet/osnailyfacter/manifests/cluster_simple.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_simple.pp
@@ -328,36 +328,38 @@ class osnailyfacter::cluster_simple {
         'DEFAULT/ram_weight_multiplier':        value => '1.0'
       }
 
-      if ($::operatingsystem != 'RedHat') {
-        class { 'openstack::heat' :
-          pacemaker           => false,
-          external_ip         => $controller_node_public,
 
-          keystone_host       => $controller_node_address,
-          keystone_user       => 'heat',
-          keystone_password   => $heat_hash['user_password'],
-          keystone_tenant     => 'services',
+      class { 'openstack::heat' :
+        pacemaker           => false,
+        external_ip         => $controller_node_public,
 
-          rpc_backend         => 'heat.openstack.common.rpc.impl_kombu',
-          amqp_hosts          => [$amqp_hosts],
-          amqp_user           => $rabbit_hash['user'],
-          amqp_password       => $rabbit_hash['password'],
+        keystone_host       => $controller_node_address,
+        keystone_user       => 'heat',
+        keystone_password   => $heat_hash['user_password'],
+        keystone_tenant     => 'services',
 
-          sql_connection      =>
-            "mysql://heat:${heat_hash['db_password']}@${$controller_node_address}/heat?read_timeout=60",
-          db_host             => $controller_node_address,
-          db_password         => $heat_hash['db_password'],
-          max_retries         => $max_retries,
-          max_pool_size       => $max_pool_size,
-          max_overflow        => $max_overflow,
-          idle_timeout        => $idle_timeout,
+        keystone_ec2_uri    => "http://${controller_node_address}:5000/v2.0",
 
-          debug               => $::debug,
-          verbose             => $::verbose,
-          use_syslog          => $::use_syslog,
-          syslog_log_facility => $::syslog_log_facility_heat,
-        }
+        rpc_backend         => 'heat.openstack.common.rpc.impl_kombu',
+        amqp_hosts          => [$amqp_hosts],
+        amqp_user           => $rabbit_hash['user'],
+        amqp_password       => $rabbit_hash['password'],
+
+        sql_connection      =>
+          "mysql://heat:${heat_hash['db_password']}@${$controller_node_address}/heat?read_timeout=60",
+        db_host             => $controller_node_address,
+        db_password         => $heat_hash['db_password'],
+        max_retries         => $max_retries,
+        max_pool_size       => $max_pool_size,
+        max_overflow        => $max_overflow,
+        idle_timeout        => $idle_timeout,
+
+        debug               => $::debug,
+        verbose             => $::verbose,
+        use_syslog          => $::use_syslog,
+        syslog_log_facility => $::syslog_log_facility_heat,
       }
+
 
       if $murano_hash['enabled'] {
 
