bf3af8016a3bf04f3cca299a335bd4cdf5231382
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-08-04 19:58:43 +0300

    Provide oslo.messaging debug logs for Nova
    Configure oslo.messaging logger for Nova
    to provide debug messages in order to better
    troubleshooting RPC dialogs after HA failover
    
    Related-bug: #1340711
    
    Change-Id: I30a1dd556bd60cc6067c52eba5881efc20906f83
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
@@ -419,6 +419,9 @@ class osnailyfacter::cluster_ha {
       nova_config { 'DEFAULT/start_guests_on_host_boot': value => $::fuel_settings['start_guests_on_host_boot'] }
       nova_config { 'DEFAULT/use_cow_images':            value => $::fuel_settings['use_cow_images'] }
       nova_config { 'DEFAULT/compute_scheduler_driver':  value => $::fuel_settings['compute_scheduler_driver'] }
+      if ($::debug) {
+        nova_config { 'DEFAULT/default_log_levels':      value => 'oslo.messaging=DEBUG' }
+      }
 
       if ! $::use_quantum {
        if $::fuel_settings['role'] == 'primary-controller' {
