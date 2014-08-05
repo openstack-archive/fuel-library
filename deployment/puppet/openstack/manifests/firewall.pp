a0e69332808ef387158cac6511bb537e2468659d
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2014-08-05 13:03:59 +0400

    Accept iptables ports for libvirt live migration
    Change-Id: I1f317ebbd170b7e48d9bf2afa382a5abae10278a
    Closes-Bug: 1352222

--- deployment/puppet/openstack/manifests/firewall.pp
+++ deployment/puppet/openstack/manifests/firewall.pp
@@ -223,6 +223,12 @@ class openstack::firewall (
     action => 'accept',
   }
 
+  firewall {'120 libvirt migration':
+    port   => '49152-49215',
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
   firewall { '333 accept gre':
     chain   => 'PREROUTING',
     table   => 'raw',
