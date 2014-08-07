c47354492c67df2b67574b9665e84ca34541ca96
Dmitry Borodaenko <dborodaenko@mirantis.com>
2014-08-07 13:18:18 -0700

    cron job to clean up rabbitmq connections
    Install a cleanup script to terminate RabbitMQ connections that don't
    have any channels, set up a cron job to run the script every minute.
    
    Change-Id: I1d9e79a9ef28076f83dd9426d17de7d8d4b97d30
    Related-Bug: #856764

---
+++ deployment/puppet/openstack/manifests/ha/rabbitmq_connections_cleanup.pp
@@ -0,0 +1,38 @@
+# RabbitMQ connections cleanup
+class openstack::ha::rabbitmq_connections_cleanup (
+  $rabbit_virtual_host = '/',
+  $rabbit_userid       = $::openstack::controller_ha::amqp_user,
+  $rabbit_password     = $::openstack::controller_ha::amqp_password,
+) {
+
+  exec {'rabbitmq-plugins enable rabbitmq_management':
+    require => Package[$::rabbitmq::server::package_name],
+    notify  => Service[$::rabbitmq::service::service_name],
+  }
+
+  package {'python-rabbit': }
+
+  file {'rabbitmq-connections-cleanup.py':
+    path    => '/usr/local/bin/rabbitmq-connections-cleanup.py',
+    content => template('openstack/rabbitmq-connections-cleanup.py'),
+    owner   => '0',
+    group   => '0',
+    mode    => '0755',
+    require => Package['python-rabbit'],
+  }
+
+  file {'rabbitmq-connections-cleanup.conf':
+    path    => '/root/rabbitmq-connections-cleanup.conf',
+    content => template('openstack/rabbitmq-connections-cleanup.conf.erb'),
+  }
+
+  cron {'rabbitmq-connections-cleanup':
+    command => '/usr/local/bin/rabbitmq-connections-cleanup.py /root/rabbitmq-connections-cleanup.conf',
+    user    => 'root',
+    minute  => '*/1',
+    require => [File['rabbitmq-connections-cleanup.py',
+                     'rabbitmq-connections-cleanup.conf'],
+                Exec['rabbitmq-plugins enable rabbitmq_management'],
+                Service[$::rabbitmq::service::service_name]],
+  }
+}
