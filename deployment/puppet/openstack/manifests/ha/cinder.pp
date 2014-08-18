408b4293183961a17f84b8c6f437b720a256eb2b
Guillaume Thouvenin <gthouvenin@mirantis.com>
2014-08-18 21:34:13 +0200

    Ensure Puppet CM for SSL in OpenStack deployment.
    Add the option ssl crt to the bind parameter in the ha config to use
    SSL for openstack services that expose public API.
    
    To enable ssl for public endpoint you need to:
      - set ssl_public_endpoint = true
        + available in openstack/manifest/all.pp
        + and in opentstack/manifest/controller.pp
      - set ssl_certificate = "/etc/haproxy/cert.pem"
        + in openstack/manifests/ha/haproxy.pp use
            class { 'openstack::ha::glance':
                ssl_certificate = "/etc/haproxy/cert.pem"
            }
          and the same for other services.
    
    TODO: Other modifications are needed in
    osnailyfacter/manifests/cluster_{ha, simple}.pp to set the certificate from
    astute.yaml file.
    
    Some services need to be configured to support SSL:
    - Horizon: OPENSTACK_ENDPOINT_TYPE = "internalURL"
    - Keystone: Use https instead of http for public url
    
    Change-Id: Ie2a108413f9baa6e38cd418ae530946c957dd3cb
    Implements: blueprint ssl-endpoints

--- deployment/puppet/openstack/manifests/ha/cinder.pp
+++ deployment/puppet/openstack/manifests/ha/cinder.pp
@@ -1,11 +1,14 @@
 # HA configuration for OpenStack Nova
-class openstack::ha::cinder {
+class openstack::ha::cinder (
+    $ssl_certificate = undef,
+) {
 
   openstack::ha::haproxy_service { 'cinder-api':
     order           => '070',
     listen_port     => 8776,
     public          => true,
     require_service => 'cinder-api',
+    ssl_certificate => $ssl_certificate,
   }
 
   Openstack::Ha::Haproxy_service<|title == 'keystone-1' or title == 'keystone-2'|> -> Service['cinder-api']
