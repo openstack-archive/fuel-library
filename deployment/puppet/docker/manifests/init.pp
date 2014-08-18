4cf1259a01e6d040b8e5e1fedc575802ca14a5f8
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-08-18 13:37:58 +0400

    Restart containers on docker service refresh
    Devicemapper bugs in docker 0.10 cause containers
    to incorrectly restart when docker service is
    restarted when Puppet upgrades the RPM. Added
    lazy unmounts to dockerctl and a refreshonly
    exec to start all containers when Docker service
    is restarted (because of package upgrade).
    
    Includes bugfixes for purging iptables rules and
    starting nailgun and ostf containers while nginx
    container is not yet started.
    
    Change-Id: I9c6d9b04bdaa5cf1fcc9752cc1e8f42cf693de1b
    Closes-Bug: #1358144

--- deployment/puppet/docker/manifests/init.pp
+++ deployment/puppet/docker/manifests/init.pp
@@ -22,6 +22,8 @@ $dependent_dirs = ["/var/log/docker-logs", "/var/log/docker-logs/remote",
     enable => true,
     ensure => running,
     require => Package[$docker_package],
+    hasrestart => true,
+    restart => "/etc/init.d/docker restart;/usr/bin/dockerctl start all",
   }
   file { "/etc/sysconfig/docker":
     content => template("docker/settings.erb"),
