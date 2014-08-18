f2e6f6a73d034688d26b4b73c8995c8e97a76fa3
Guillaume Thouvenin <gthouvenin@mirantis.com>
2014-08-18 21:34:13 +0200

    Ensure Puppet CM for SSL in OpenStack deployment.
    Add the option ssl crt to the bind parameter in the ha config to use
    SSL for openstack services that expose public API.
    
    Change-Id: Ie2a108413f9baa6e38cd418ae530946c957dd3cb
    Implements: blueprint ssl-endpoints

--- deployment/puppet/openstack/manifests/ha/keystone.pp
+++ deployment/puppet/openstack/manifests/ha/keystone.pp
@@ -2,15 +2,17 @@
 class openstack::ha::keystone {
 
   openstack::ha::haproxy_service { 'keystone-1':
-    order           => '020',
-    listen_port     => 5000,
-    public          => true,
+    order        => '020',
+    listen_port  => 5000,
+    public       => true,
+    bind_options => 'ssl crt /etc/haproxy/haproxy_ca.pem',
   }
 
   openstack::ha::haproxy_service { 'keystone-2':
-    order           => '030',
-    listen_port     => 35357,
-    public          => true,
+    order        => '030',
+    listen_port  => 35357,
+    public       => true,
+    bind_options => 'ssl crt /etc/haproxy/haproxy_ca.pem',
   }
   Openstack::Ha::Haproxy_service['keystone-1', 'keystone-2']->Service<| title=='keystone' |>
 }
