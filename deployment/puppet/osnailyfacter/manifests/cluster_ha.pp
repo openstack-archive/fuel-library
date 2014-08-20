0dd78a72b92a8be386bab899d0e525479c409137
Dmitry Ilyin <dilyin@mirantis.com>
2014-08-20 16:21:24 +0400

    Add remove_swift_object class
    This class removes swift-object package on Ubuntu
    nodes before other swit packages are installed or
    updated. This is a workaround for the broken swift-object
    package in 5.0 release that conflicts with other packages
    durung the update.
    
    Change-Id: I83fb7f78415309d088a6894a54a57708e8b40b91
    Closes-Bug: 1356873

--- deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
@@ -269,7 +269,7 @@ class osnailyfacter::cluster_ha {
   ) {
 
     class {'osnailyfacter::apache_api_proxy':}
-
+    class {'osnailyfacter::remove_swift_object':}
     class { 'openstack::controller_ha':
       controllers                    => $::osnailyfacter::cluster_ha::controllers,
       controller_public_addresses    => $::osnailyfacter::cluster_ha::controller_public_addresses,
---
+++ deployment/puppet/osnailyfacter/manifests/remove_swift_object.pp
@@ -0,0 +1,15 @@
+# A workaround to remove badly packaged swift-object
+# that prevents other swift packages from upgrading
+class osnailyfacter::remove_swift_object {
+  if $::osfamily == 'Debian' {
+    exec { 'remove-swift-object' :
+      command   => '/usr/bin/dpkg -r --force-all swift swift-account swift-object swift-container',
+      onlyif    => '/usr/bin/test "$(/usr/bin/dpkg-query --show -f=\'${Status}|${Version}\' swift-object)" == \'install ok installed|1.13.1.fuel5.0~mira1\'',
+      logoutput => 'on_failure',
+    }
+    Exec['remove-swift-object'] -> Package <| title == 'swift' |>
+    Exec['remove-swift-object'] -> Package <| title == 'swift-account' |>
+    Exec['remove-swift-object'] -> Package <| title == 'swift-object' |>
+    Exec['remove-swift-object'] -> Package <| title == 'swift-container' |>
+  }
+}
