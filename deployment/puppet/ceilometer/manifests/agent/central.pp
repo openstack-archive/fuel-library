22af4528fbde214d60c32b8a9503a4d1f1df9486
Dmitry Ilyin <dilyin@mirantis.com>
2014-08-25 23:37:12 +0400

    Refactor corosync resources to definitions
    * Move cs_resources to definition.
    * Add wrapper for manual operation or debugging of OCF scripts.
    
    OCF Wrapper is handy for Cloud Operators when it's required to debug OCF script
    manually. It includes all necessary environment variables. In case of problems just run
    wrapper and see output.
    
    DocImpact
    Partial-Blueprint: pacemaker-improvements
    Change-Id: Id5fc2217558371998e91165cfb813003335bd885

--- deployment/puppet/ceilometer/manifests/agent/central.pp
+++ deployment/puppet/ceilometer/manifests/agent/central.pp
@@ -33,4 +33,83 @@ class ceilometer::agent::central (
     hasrestart => true,
   }
 
+  if ($auth_tenant_id != '') {
+    ceilometer_config {
+      'DEFAULT/os_tenant_id'        : value => $auth_tenant_id;
+    }
+  }
+
+  if $ha_mode {
+
+    $res_name = "p_${::ceilometer::params::agent_central_service_name}"
+
+    Package['pacemaker'] -> File['ceilometer-agent-central-ocf']
+    Package['ceilometer-common'] -> File['ceilometer-agent-central-ocf']
+    Package['ceilometer-agent-central'] -> File['ceilometer-agent-central-ocf']
+
+    file {'ceilometer-agent-central-ocf':
+      path   =>'/usr/lib/ocf/resource.d/mirantis/ceilometer-agent-central',
+      mode   => '0755',
+      owner  => root,
+      group  => root,
+      source => 'puppet:///modules/ceilometer/ocf/ceilometer-agent-central',
+    }
+
+    if $primary_controller {
+      corosync::resource { $res_name:
+        ensure          => present,
+        primitive_class => 'ocf',
+        provided_by     => 'mirantis',
+        primitive_type  => 'ceilometer-agent-central',
+        metadata        => { 'target-role' => 'stopped', 'resource-stickiness' => '1' },
+        parameters      => { 'user' => 'ceilometer' },
+        operations      => {
+          'monitor'  => {
+            'interval' => '20',
+            'timeout'  => '30'
+          },
+          'start'    => {
+            'timeout' => '360'
+          },
+          'stop'     => {
+            'timeout' => '360'
+          }
+        },
+      }
+      File['ceilometer-agent-central-ocf'] ->
+        Cs_resource[$res_name] ->
+          Service['ceilometer-agent-central']
+    } else {
+      File['ceilometer-agent-central-ocf'] ->
+        Service['ceilometer-agent-central']
+    }
+
+    service { 'ceilometer-agent-central':
+      ensure     => $service_ensure,
+      name       => $res_name,
+      enable     => $enabled,
+      hasstatus  => true,
+      hasrestart => true,
+      provider   => "pacemaker",
+    }
+
+  } else {
+
+    Package['ceilometer-common'] -> Service['ceilometer-agent-central']
+    Package['ceilometer-agent-central'] -> Service['ceilometer-agent-central']
+    service { 'ceilometer-agent-central':
+      ensure     => $service_ensure,
+      name       => $::ceilometer::params::agent_central_service_name,
+      enable     => $enabled,
+      hasstatus  => true,
+      hasrestart => true,
+    }
+
+  }
+  Package<| title == 'ceilometer-agent-central' or title == 'ceilometer-common'|> ~>
+  Service<| title == 'ceilometer-agent-central'|>
+  if !defined(Service['ceilometer-agent-central']) {
+    notify{ "Module ${module_name} cannot notify service ceilometer-agent-central\
+ on packages update": }
+  }
 }
