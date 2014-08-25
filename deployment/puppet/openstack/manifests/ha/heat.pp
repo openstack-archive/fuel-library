031010ff3df88b3df22d6ba3dfe3840e9e83c7d2
Denis Egorenko <degorenko@mirantis.com>
2014-08-25 19:08:55 +0400

    Fix HA mode troubles with Heat
    HAProxy configuration doesn't include Heat services CFN and
    Cloudwatch API (on port 8000 and 8003) as a result this services
    unavailable via public interface.
    
    Change-Id: Iafb5c119445e37e85b2f0d95b38aaee3b2e14b45
    Closes-bug: #1353348

--- deployment/puppet/openstack/manifests/ha/heat.pp
+++ deployment/puppet/openstack/manifests/ha/heat.pp
@@ -8,5 +8,21 @@ class openstack::ha::heat {
     require_service => 'heat-api',
   }
 
+  openstack::ha::haproxy_service { 'heat-api-cfn':
+    order           => '161',
+    listen_port     => 8003,
+    public          => true,
+    require_service => 'heat-api',
+  }
+
+  openstack::ha::haproxy_service { 'heat-api-cloudwatch':
+    order           => '162',
+    listen_port     => 8000,
+    public          => true,
+    require_service => 'heat-api',
+  }
+
   Openstack::Ha::Haproxy_service<|title == 'keystone-1' or title == 'keystone-2'|> -> Service['heat-api']
+  Openstack::Ha::Haproxy_service<|title == 'keystone-1' or title == 'keystone-2'|> -> Service['heat-api-cfn']
+  Openstack::Ha::Haproxy_service<|title == 'keystone-1' or title == 'keystone-2'|> -> Service['heat-api-cloudwatch']
 }
