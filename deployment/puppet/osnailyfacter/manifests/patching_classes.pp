b850f5e2afc6520284312e1f3daa747b5c47d6bd
Vladimir Kuklin <vkuklin@mirantis.com>
2014-08-29 00:07:57 +0400

    Add openstack dependencies installation for 5.0.2
    Add explicit 5.0.2 packages installation for 5.0.2
    Fuel release
    
    Change-Id: I62140d5c8bfe2a4f8a5262b84f513d81225a29b8
    Closes-bug: #1359705

---
+++ deployment/puppet/osnailyfacter/manifests/patching_classes.pp
@@ -0,0 +1,26 @@
+class osnailyfacter::patching_classes ()
+{
+  case $::osfamily {
+    'Debian': {
+      $packages=[
+        'python-oslo.messaging',
+        'python-pastedeploy',
+        'python-routes',
+        'python-sqlalchemy-ext',
+        'python-sqlalchemy',
+        'python-testtools'
+      ]
+    }
+    'RedHat': {
+      $packages=[
+        'python-oslo-messaging',
+        'python-paste-deploy',
+        'python-routes',
+        'python-six',
+        'python-sqlalchemy'
+      ]
+    }
+  }
+}
+
+
