dd3c5525eff462c687e3fe546fe93763caa098db
Evgeniy L <eli@mirantis.com>
2014-08-28 15:09:40 +0400

    Install latest iptables on the master node
    When we run upgrade, during puppet run
    it should install latest iptables from
    the repository.
    
    Change-Id: I9f0175ea2e35a6c1118804c7826d71f5a818a567
    Closes-bug: #1359096

--- deployment/puppet/nailgun/manifests/packages.pp
+++ deployment/puppet/nailgun/manifests/packages.pp
@@ -8,16 +8,14 @@ class nailgun::packages(
     }
   }
 
+  nailgun_safe_package { "iptables": }
   nailgun_safe_package { "supervisor": }
   nailgun_safe_package { "nginx": }
   nailgun_safe_package { "crontabs": }
   nailgun_safe_package { "cronie-anacron": }
-
   nailgun_safe_package { "postgresql-libs": }
   nailgun_safe_package { "rsyslog": }
   nailgun_safe_package { "rsync": }
-
-  #  nailgun_safe_package { "cman": }
   nailgun_safe_package { "fence-agents": }
   nailgun_safe_package { "nailgun-redhat-license": }
   nailgun_safe_package { "python-fuelclient": }
