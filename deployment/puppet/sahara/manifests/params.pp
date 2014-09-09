9633daed11c07ae8c4650155d3ba82f180ac7fc3
Denis Egorenko <degorenko@mirantis.com>
2014-09-09 16:08:13 +0400

    Fix Sahara puppet manifests for Fuel 6.0 (Juno)
    * change name for sahara service;
    * migrate from custom auth_token middleware config options to
    the common config options;
    * remove sahara-dashboard installing, because now sahara-dashboard
    is integrated into Horizon Openstack Dashboard;
    * add icehouse backward compatibility for dashboard
    and keystone settings
    
    Closes-bug: #1364934
    Change-Id: I29d931c1ee0f9a22bcdddc38b59680285ab68f36
    blueprint fuel-deploy-juno

--- deployment/puppet/sahara/manifests/params.pp
+++ deployment/puppet/sahara/manifests/params.pp
@@ -1,24 +1,33 @@
 class sahara::params {
   # package names
   $sahara_package_name = 'sahara'
-  # dashboard package
-  $sahara_dashboard_package_name = 'sahara-dashboard'
-  # service names
-  $sahara_service_name = 'sahara-api'
-
-  $settings_path       = '/usr/share/openstack-dashboard/openstack_dashboard/settings.py'
-  $default_url_string  = "SAHARA_URL = 'http://localhost:8386/v1.0'"
-
-  case $::osfamily {
-    'RedHat': {
-      $local_settings_path = '/etc/openstack-dashboard/local_settings'
+  #NOTE(mattymo): Backward compatibility for Icehouse
+  case $::fuel_settings['openstack_version'] {
+    /2014.2-6./: {
+       $sahara_service_name = 'sahara-all'
     }
-    'Debian': {
-      $local_settings_path = '/etc/openstack-dashboard/local_settings.py'
+    /2014.1.*/: {
+      $sahara_service_name = 'sahara-api'
+      $sahara_dashboard_package_name = 'sahara-dashboard'
+
+      $settings_path       = '/usr/share/openstack-dashboard/openstack_dashboard/settings.py'
+      $default_url_string  = "SAHARA_URL = 'http://localhost:8386/v1.0'"
+
+      case $::osfamily {
+        'RedHat': {
+          $local_settings_path = '/etc/openstack-dashboard/local_settings'
+        }
+        'Debian': {
+          $local_settings_path = '/etc/openstack-dashboard/local_settings.py'
+        }
+        default: {
+          fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only support osfamily RedHat and
+Debian")
+        }
+      }
     }
     default: {
-      fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only support osfamily RedHat and Debian")
-    }  
+     fail("Unsupported OpenStack version: ${::fuel_settings['openstack_version']}")
+    }
   }
-
 }
