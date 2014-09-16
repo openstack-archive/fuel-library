dac4627ad7fb2058937c26a689741e0e6a151d04
Igor Zinovik <izinovik@mirantis.com>
2014-09-17 03:07:28 +0400

    tie one nova-compute with one vsphere cluster
    - in simple mode thing are left as they are right now, single
      nova-compute service manages multiple vSphere clusters managed by one
      vCenter server
    - split template `nova-compute.conf.erb' into two (for simple and HA
      deployment modes)
    - in HA mode create directory `/etc/nova/nova-compute.d' which will hold
      all configuration files related to vSphere clusters
    - create individual configuration file per nova-compute; file is named
      `/etc/nova/nova-compute.d/vcenter-%CLUSTER_NAME%.conf', e.g.
      /etc/nova/nova-compute.d/vcenter-Cluster1.conf
    - in HA configuration template set `log_file' stanza for each
      nova-compute, so that deployer or field engineer can easily determine
      which log file nova-compute service instance writes its diagnostics
    - in HA configuration template set `pid' parameter for each corosync
      resource, without invidiual PID file pacemaker cannot start several
      nova-compute instances on same controller, becase monitor() function
      in OCF file will be checking PID file of first started nova-compute
      service
    - create appropriate corosync resource/primitive per nova-compute
      service
    - we use same credentials for all nova-computes
    - consolidate nova-compute service provisioning into separate manifest
      vmware/manifests/compute.pp
    - change owner and group of /etc/nova/nova-compute.conf to `nova'
    - set more strict access permissions (0600) on nova-compute.conf and
      generated files in `nova-compute.d' directory; these files hold
      sensitive information (credentials to vCenter server)
    
    Change-Id: I675dd2b7cdeacae1e703b82001dc2c855511e320
    Impelements: blueprint 1-1-nova-compute-vsphere-cluster

---
+++ deployment/puppet/vmware/manifests/compute.pp
@@ -0,0 +1,77 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# This type creates nova-compute service for provided vSphere
+# cluster (cluster that is formed of ESXi hosts and is managed by vCenter
+# server).
+define vmware::compute(
+  $amqp_port = '5673',
+  $api_retry_count = 5,
+  $compute_driver = 'vmwareapi.VMwareVCDriver',
+  $maximum_objects = 100,
+  $nova_conf = '/etc/nova/nova.conf',
+  $task_poll_interval = 5.0,
+  $use_linked_clone = true,
+  $wsdl_location = undef
+)
+{
+  # We expect that resource name contains vSphere cluster name, e.g. 'Cluster1'
+  $nova_compute_conf = "/etc/nova/nova-compute.d/vcenter-${name}.conf"
+
+  File["${nova_compute_conf}"]->
+  Cs_resource["p_vcenter_nova_compute_${name}"]->
+  Service["p_vcenter_nova_compute_${name}"]
+
+  file { "${nova_compute_conf}":
+    content => template("vmware/nova-compute-ha.conf.erb"),
+    mode    => '0600',
+    owner   => 'nova',
+    group   => 'nova',
+    ensure  => present,
+  }
+
+  cs_resource { "p_vcenter_nova_compute_${name}":
+    ensure          => present,
+    primitive_class => 'ocf',
+    provided_by     => 'mirantis',
+    primitive_type  => 'nova-compute',
+    metadata        => {
+      resource-stickiness => '1'
+    },
+    parameters      => {
+      amqp_server_port      => $amqp_port,
+      config                => $nova_conf,
+      pid                   => "/var/run/nova/nova-compute-${name}.pid",
+      additional_parameters => "--config-file=${nova_compute_conf}",
+    },
+    operations      => {
+      monitor  => {
+        interval => '20',
+        timeout  => '10',
+      },
+        start  => {
+        timeout => '30',
+      },
+        stop   => {
+        timeout => '30',
+      }
+    }
+  }
+
+  service { "p_vcenter_nova_compute_${name}":
+    ensure => running,
+    enable => true,
+    provider => 'pacemaker',
+  }
+}
