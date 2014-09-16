55d3e6683307a1fc84aefdc7c5f94401a8675e36
Igor Zinovik <izinovik@mirantis.com>
2014-09-17 03:07:28 +0400

    configure nova-compute use one vsphere cluster
    - create separate directory '/etc/nova/nova-compute.d' where all
      configuration files that hold all configuration stanzas related to
      vmware compute driver
    - in simple mode we upload init script in appropriate directory and
      create symbolic links on it that are named like
      nova-compute-vmware-%cluster_id% ; script internally splits its own
      file name and extracts cluster name.  This allow user to selectively
      start/stop nova-compute services.
    - Ubuntu init system (Upstart) requires initd service to be restarted,
      on CentOS we run '/bin/true'
    - create individual nova-compute configuration file per vsphere cluster;
      file is named `/etc/nova/nova-compute.d/vmware-%cluster_id%.conf',
      e.g.  /etc/nova/nova-compute.d/vmware-0.conf
    - create appropriate corosync resource/primitive per nova-compute
      service in case of HA
    - currently we use same login and password for all nova-computes
    - set more strict access permissions (0600) on nova-compute.conf and
      generated files in `nova-compute.d' directory; these files hold
      sensitive information (credentials to vCenter server)
    - set 'multi_host' configuration stanza to False for both deployment
      modes (nonHA/HA)
    
    DocImpact
    Change-Id: I675dd2b7cdeacae1e703b82001dc2c855511e320
    Impelements: blueprint 1-1-nova-compute-vsphere-cluster-mapping

--- deployment/puppet/vmware/manifests/init.pp
+++ deployment/puppet/vmware/manifests/init.pp
@@ -13,16 +13,20 @@
 #    under the License.
 
 # This is the main VMWare integration class
-# It should check the variables and basing on them call needed subclasses in order to setup VMWare integration with OpenStack
+# It should check the variables and basing on them call needed
+# subclasses in order to setup VMWare integration with OpenStack
+
 # Variables:
-# vcenter_user - contents user name which should be used for configuring integration with vCenter
+# vcenter_user - contents user name which should be used for configuring
+#                integration with vCenter
 # vcenter_password - vCenter user password
 # vcenter_host_ip - contents IP address of the vCenter host
-# vcenter_cluster - contents vCenter cluster name (multi-cluster is not supported yet)
+# vcenter_cluster - contents vCenter cluster name (multi-cluster is
+#                   not supported yet)
 # use_quantum - shows if neutron enabled
+# ha_mode - flag that shows what deployment mode we are running
 
 class vmware (
-
   $vcenter_user = 'user',
   $vcenter_password = 'password',
   $vcenter_host_ip = '10.10.10.10',
@@ -31,17 +35,14 @@ class vmware (
   $ha_mode = false,
   $vnc_address = '0.0.0.0',
 )
-
-{ # begin of class
-
+{
   class { 'vmware::controller':
-    vcenter_user => $vcenter_user,
+    vcenter_user     => $vcenter_user,
     vcenter_password => $vcenter_password,
-    vcenter_host_ip => $vcenter_host_ip,
-    vcenter_cluster => $vcenter_cluster,
-    use_quantum => $use_quantum,
-    ha_mode => $ha_mode,
-    vnc_address => $vnc_address,
+    vcenter_host_ip  => $vcenter_host_ip,
+    vcenter_cluster  => $vcenter_cluster,
+    use_quantum      => $use_quantum,
+    ha_mode          => $ha_mode,
+    vnc_address      => $vnc_address,
   }
-
-} # end of class
+}
