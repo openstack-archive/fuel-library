6d0ce7d056cbe20dd758e2b67131dd6b95085d4c
Guillaume Thouvenin <gthouvenin@mirantis.com>
2014-09-16 13:10:43 +0200

    Add x509_sign provider to openssl module.
    It adds the x509_sign provider in order to be able to sign a CSR. It
    adds two parameters that are: infiles and template (that is the config
    file). You can use it as follow:
    
      x509_sign { "/tmp/signed_req.pem":
          ensure   => present,
          template => "ca_template",
          infiles  => "user_req_cert.pem"
      }
    
    Change-Id: I9be9df731be3bf64333eff366aaf9eb27a27b72d
    Implements: blueprint ssl-endpoints

---
+++ deployment/puppet/openssl/lib/puppet/provider/x509_sign/openssl.rb
@@ -0,0 +1,25 @@
+require 'pathname'
+
+Puppet::Type.type(:x509_sign).provide(:openssl) do
+  desc 'Signs certificate request with OpenSSL'
+
+  commands :openssl => 'openssl'
+
+  def exists?
+    return Pathname.new(resource[:path]).exist?
+  end
+
+  def create
+    openssl(
+      'ca',
+      '-out', resource[:path],
+      '-config', resource[:template],
+      '-batch',
+      '-infiles', resource[:infiles]
+    )
+  end
+
+  def destroy
+    Pathname.new(resource[:path]).delete
+  end
+end
