5efc17969ab66b54f5daf9472d8cad30fce9c9be
TimurNurlygayanov <tnurlygayanov@mirantis.com>
2014-09-25 13:44:12 +0400

    Fixed issue with max size for Swift objects
    Fixed issue with max size of object, which Glance
    should store in Swift without slicing.
    Default value doesn't work when user attempts to make
    a snapshots with size > 300 Mb.
    
    Change-Id: Ic3b3120605f1e1536a34bedc4ad5c0c2f4c8510b
    Closes-Bug: #1373813

--- deployment/puppet/glance/manifests/backend/swift.pp
+++ deployment/puppet/glance/manifests/backend/swift.pp
@@ -13,12 +13,15 @@
 #  $swift_store_auth_version - Optional. Default: '2'
 #
 #  $swift_store_create_container_on_put - Optional. Default: 'False'
+#
+#  $swift_store_large_object_size - Optional. Default: '5120'
 class glance::backend::swift(
   $swift_store_user,
   $swift_store_key,
   $swift_store_auth_address = '127.0.0.1:5000/v2.0/',
   $swift_store_container = 'glance',
   $swift_store_auth_version = '2',
+  $swift_store_large_object_size = '5120',
   $swift_store_create_container_on_put = false
 ) {
 
@@ -31,6 +34,8 @@ class glance::backend::swift(
     'DEFAULT/swift_store_auth_version': value => $swift_store_auth_version;
     'DEFAULT/swift_store_create_container_on_put':
       value => $swift_store_create_container_on_put;
+    'DEFAULT/swift_store_large_object_size':
+      value => $swift_store_large_object_size;
   }
 
   glance_cache_config {
@@ -41,6 +46,8 @@ class glance::backend::swift(
     'DEFAULT/swift_store_auth_version': value => $swift_store_auth_version;
     'DEFAULT/swift_store_create_container_on_put':
       value => $swift_store_create_container_on_put;
+    'DEFAULT/swift_store_large_object_size':
+      value => $swift_store_large_object_size;
   }
 
 }
