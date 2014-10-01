4d1329a9d7a710a52e10ed46f8dc2ea086527389
Stepan Rogov <srogov@mirantis.com>
2014-10-01 19:12:59 +0400

    Add support vSphere Datastore backend for Glance
    1. Added Glace parameters for vCenter in classes openstack::controller and
       openstack::controller_ha
    2. Created class for this type of glance backend
    3. Added Glace parameters for vCenter backend and case for our backend type
       in class openstack::glance
    4. Added parsing of required parameters from the UI for calsses
       osnailyfacter::cluster_simple and osnailyfacter::cluster_ha
    5. Disabled Swift installation in HA mode
    
    DocImpact
    Change-Id: Icbb46da1e486ceebc1f9f0ce7a4db09323e3605e
    Implements: blueprint vsphere-glance-backend

---
+++ deployment/puppet/glance/manifests/backend/vsphere.pp
@@ -0,0 +1,24 @@
+class glance::backend::vsphere(
+  $vcenter_api_insecure = 'False',
+  $vcenter_host,
+  $vcenter_user,
+  $vcenter_password,
+  $vcenter_datacenter,
+  $vcenter_datastore,
+  $vcenter_image_dir,
+  $vcenter_task_poll_interval = '5',
+  $vcenter_api_retry_count = '10',
+) {
+  glance_api_config {
+    'DEFAULT/default_store': value             => 'vsphere';
+    'DEFAULT/vmware_api_insecure': value       => $vcenter_api_insecure;
+    'DEFAULT/vmware_server_host': value        => $vcenter_host;
+    'DEFAULT/vmware_server_username': value    => $vcenter_user;
+    'DEFAULT/vmware_server_password': value    => $vcenter_password;
+    'DEFAULT/vmware_datastore_name': value     => $vcenter_datastore;
+    'DEFAULT/vmware_store_image_dir': value    => $vcenter_image_dir;
+    'DEFAULT/vmware_task_poll_interval': value => $vcenter_task_poll_interval;
+    'DEFAULT/vmware_api_retry_count': value    => $vcenter_api_retry_count;
+    'DEFAULT/vmware_datacenter_path': value    => $vcenter_datacenter;
+  }
+}
