b17d51ce8f7d031142463458f6a91c99df332906
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-10-03 12:42:40 +0200

    Add monit as an option for compute nodes
    * Add use_monit (Default false) for site.pp
      If enabled, will install and configure monit watchdogs for
      specified services and roles (compute & cinder).
    * Changes to cluster_ha.pp/cluster_simple.pp:
      - Add monit watchdogs for nova-compute/network/api for compute
        nodes by mathcing the python processes.
      - Add monit watchdogs for cinder-volume for cinder nodes
        by mathcing the python processes
      - Add monit watchdog for ovs service by its pid for computes.
    
    DocImpact
    Related blueprint pacemaker-improvements
    
    Change-Id: Ia622d3200db1b78a0cf4e6b1da1ceff0604e8a16
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
@@ -4,6 +4,30 @@ class osnailyfacter::cluster_ha {
 
   $primary_controller = $::fuel_settings['role'] ? { 'primary-controller'=>true, default=>false }
 
+  if $::use_monit {
+    # Configure service names for monit watchdogs and 'service' system path
+    # FIXME(bogdando) replace service_path to systemd, once supported
+    include nova::params
+    include cinder::params
+    include neutron::params
+    include l23network::params
+    $nova_compute_name   = $::nova::params::compute_service_name
+    $nova_api_name       = $::nova::params::api_service_name
+    $nova_network_name   = $::nova::params::network_service_name
+    $cinder_volume_name  = $::cinder::params::volume_service
+    $ovs_vswitchd_name   = $::l23network::params::ovs_service_name
+    case $::osfamily {
+      'RedHat' : {
+         $service_path   = '/sbin/service'
+      }
+      'Debian' : {
+        $service_path    = '/usr/sbin/service'
+      }
+      default  : {
+        fail("Unsupported osfamily: ${osfamily} for os ${operatingsystem}")
+      }
+    }
+  }
 
   if $::use_quantum {
     $novanetwork_params  = {}
@@ -730,6 +754,41 @@ class osnailyfacter::cluster_ha {
         }
       }
 
+    # Configure monit watchdogs
+    # FIXME(bogdando) replace service_path and action to systemd, once supported
+    if $::use_monit {
+      monit::process { $nova_compute_name :
+        ensure        => running,
+        matching      => '/usr/bin/python /usr/bin/nova-compute',
+        start_command => "${service_path} ${nova_compute_name} restart",
+        stop_command  => "${service_path} ${nova_compute_name} stop",
+        pidfile       => false,
+      }
+      if $::use_quantum {
+        monit::process { $ovs_vswitchd_name :
+          ensure        => running,
+          start_command => "${service_path} ${ovs_vswitchd_name} restart",
+          stop_command  => "${service_path} ${ovs_vswitchd_name} stop",
+          pidfile       => '/var/run/openvswitch/ovs-vswitchd.pid',
+        }
+      } else {
+        monit::process { $nova_network_name :
+          ensure        => running,
+          matching      => '/usr/bin/python /usr/bin/nova-network',
+          start_command => "${service_path} ${nova_network_name} restart",
+          stop_command  => "${service_path} ${nova_network_name} stop",
+          pidfile       => false,
+        }
+        monit::process { $nova_api_name :
+          ensure        => running,
+          matching      => '/usr/bin/python /usr/bin/nova-api',
+          start_command => "${service_path} ${nova_api_name} restart",
+          stop_command  => "${service_path} ${nova_api_name} stop",
+          pidfile       => false,
+        }
+      }
+    }
+
     } # COMPUTE ENDS
 
     "mongo" : {
@@ -791,12 +850,24 @@ class osnailyfacter::cluster_ha {
         vmware_host_username => $vcenter_hash['vc_user'],
         vmware_host_password => $vcenter_hash['vc_password']
       }
+
+      # FIXME(bogdando) replace service_path and action to systemd, once supported
+      if $::use_monit {
+        monit::process { $cinder_volume_name :
+          ensure        => running,
+          matching      => '/usr/bin/python /usr/bin/cinder-volume',
+          start_command => "${service_path} ${cinder_volume_name} restart",
+          stop_command  => "${service_path} ${cinder_volume_name} stop",
+          pidfile       => false,
+        }
+      }
     } # CINDER ENDS
 
     "ceph-osd" : {
       #Class Ceph is already defined so it will do it's thing.
       notify {"ceph_osd: ${::ceph::osd_devices}": }
       notify {"osd_devices:  ${::osd_devices_list}": }
+      # TODO(bogdando) add monit ceph-osd services monitoring, if required
     } # CEPH-OSD ENDS
 
     # Definition of the first OpenStack Swift node.
@@ -837,6 +908,8 @@ class osnailyfacter::cluster_ha {
         syslog_log_facility_cinder => $syslog_log_facility_cinder,
       }
 
+      # TODO(bogdando) add monit swift-storage services monitoring, if required
+      # NOTE(bogdando) we don't deploy swift as a separate role for now, but will do
     }
 
     # Definition of OpenStack Swift proxy nodes.
@@ -867,6 +940,8 @@ class osnailyfacter::cluster_ha {
   } # ROLE CASE ENDS
 
   class { 'zabbix': }
+  # TODO(bogdando) add monit zabbix services monitoring, if required
+  # NOTE(bogdando) for nodes with pacemaker, we should use OCF instead of monit
 
 } # CLUSTER_HA ENDS
 # vim: set ts=2 sw=2 et :
