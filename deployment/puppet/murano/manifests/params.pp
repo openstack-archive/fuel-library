8657f622123c8219a1ae3b0e3000c4d8b60215bc
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-10-06 16:45:37 +0400

    Rename package murano-api to murano
    Added backward compatibility logic to murano
    to enable Icehouse deployment, but depends on
    setting openstack_version to 2014.1.X-6.0
    
    blueprint fuel-deploy-juno
    
    Change-Id: I5b7f314c3e9ff8fd34c4e2a7044e282b55ee6b7c

--- deployment/puppet/murano/manifests/params.pp
+++ deployment/puppet/murano/manifests/params.pp
@@ -1,7 +1,18 @@
 class murano::params {
 
   # package names
-  $murano_package_name              = 'murano-api'
+  #NOTE(mattymo): Backward compatibility for Icehouse
+  case $::fuel_settings['openstack_version'] {
+    /2014.2-6./: {
+      $murano_package_name              = 'murano'
+    }
+    /2014.1.*/: {
+      $murano_package_name              = 'murano-api'
+    }
+    default: {
+      fail("Unsupported OpenStack version: ${::fuel_settings['openstack_version']}")
+    }
+  }
   $murano_apps_package_name         = 'murano-apps'
   $murano_dashboard_package_name    = 'murano-dashboard'
   $python_muranoclient_package_name = 'python-muranoclient'
