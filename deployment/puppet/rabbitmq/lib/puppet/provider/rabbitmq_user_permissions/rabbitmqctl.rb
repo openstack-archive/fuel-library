c4db6682e129d17dd637b584f701ca9245e97c8c
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-10-08 18:00:42 +0200

    Adapt rabbitmq module for Fuel
    * Add dependency modules: garethr-erlang, nanliu-staging
    * Add support for rabbimtq >1.0 <=4.1 for nova module and make it
      backward compatible (otherwise can't be contributed in upstream)
      (https://bugs.launchpad.net/puppet-nova/+bug/1386309)
    * Adjust other existing rabbit class calls for new 4.1 rabbit module
    * Remove deprecated usage of rabbitmq::server class and replace it
      to ::rabbitmq (cinder, murano, openstack, zabbix, mcollective,
      nailgun)
    * Move rabbit.pub.key template from rabbitmq to openstack module
      (Fuel uses it for offline installation of rabbitmq)
    * Move custom rabbit OCF file from nova to openstack and remove
      OS-aware services disable from rabbit's manifests
    * Make pacemaker provider HA wrapper for rabbitmq service and move
      it to pacemaker_wrappers::rabbitmq
      (TODO move to extras and contribute the change)
    * Fix linting
    * Fix rabbitmq service name for zabbix mon
    * Port fix for #MODULES-1488
    * Fix env_config for rabbitmq::nailgun, unify RABBIT_* env args.
    * Use key_content, package_gpg_key, config_*, cluster_*,
      environment_* params to ensure the same rabbit configuration as
      before and make offline installation possible.
    * Add cluster_partition_handling for nova::rabbitmq and
      openstack::controller
      (https://bugs.launchpad.net/puppet-nova/+bug/1386309)
    * Del unused rabbitmq_node_ip_address and rename as node_ip_address
    * Del unneeded rabbit_cluster due to it is being evaluated via
      passed rabbitmq_cluster_nodes as well
    * Adjust debug log_levels for rabbit config as well
    * For '$production !~ /docker/' case, remove custom init scripts
      for rabbit as non relevant anymore.
    
    Related blueprint merge-openstack-puppet-modules
    Related-bug: #1377491
    
    Change-Id: I6e631b975c5835d2d6f1984d5a070a45231fe9ab
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/rabbitmq/lib/puppet/provider/rabbitmq_user_permissions/rabbitmqctl.rb
+++ deployment/puppet/rabbitmq/lib/puppet/provider/rabbitmq_user_permissions/rabbitmqctl.rb
@@ -15,7 +15,7 @@ Puppet::Type.type(:rabbitmq_user_permissions).provide(:rabbitmqctl) do
     @users = {} unless @users
     unless @users[name]
       @users[name] = {}
-      rabbitmqctl('list_user_permissions', name).split(/\n/)[1..-2].each do |line|
+      rabbitmqctl('-q', 'list_user_permissions', name).split(/\n/).each do |line|
         if line =~ /^(\S+)\s+(\S*)\s+(\S*)\s+(\S*)$/
           @users[name][$1] =
             {:configure => $2, :read => $4, :write => $3}
@@ -51,7 +51,7 @@ Puppet::Type.type(:rabbitmq_user_permissions).provide(:rabbitmqctl) do
     resource[:configure_permission] ||= "''"
     resource[:read_permission]      ||= "''"
     resource[:write_permission]     ||= "''"
-    rabbitmqctl('set_permissions', '-p', should_vhost, should_user, resource[:configure_permission], resource[:write_permission], resource[:read_permission]) 
+    rabbitmqctl('set_permissions', '-p', should_vhost, should_user, resource[:configure_permission], resource[:write_permission], resource[:read_permission])
   end
 
   def destroy
