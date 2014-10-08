018874c33431024bc0505024cb2856d33a12308f
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-10-08 17:38:05 +0200

    Sync puppetlabs rabbitmq module
    commit 4832bd61b5b1bfea7c9cc985508e65cd10081652
    Source: https://github.com/puppetlabs/puppetlabs-rabbitmq.git
    
    Related blueprint merge-openstack-puppet-modules
    
    Fuel-CI: disable
    Change-Id: I924a2a4489dc329fefbee859a4b4bde0b79e86d1
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/rabbitmq/lib/puppet/provider/rabbitmq_vhost/rabbitmqctl.rb
+++ deployment/puppet/rabbitmq/lib/puppet/provider/rabbitmq_vhost/rabbitmqctl.rb
@@ -1,16 +1,15 @@
-require File.join File.dirname(__FILE__), '../rabbitmq_common.rb'
+Puppet::Type.type(:rabbitmq_vhost).provide(:rabbitmqctl) do
 
-Puppet::Type.type(:rabbitmq_vhost).provide(:rabbitmqctl, :parent => Puppet::Provider::Rabbitmq_common) do
-
-  #TODO: change optional_commands -> commands when puppet >= 3.0
-  optional_commands :rabbitmqctl => 'rabbitmqctl'
-  defaultfor :feature => :posix
+  if Puppet::PUPPETVERSION.to_f < 3
+    commands :rabbitmqctl => 'rabbitmqctl'
+  else
+     has_command(:rabbitmqctl, 'rabbitmqctl') do
+       environment :HOME => "/tmp"
+     end
+  end
 
   def self.instances
-    self.wait_for_online
-    self.run_with_retries {
-      rabbitmqctl('list_vhosts')
-    }.split(/\n/)[1..-2].map do |line|
+    rabbitmqctl('list_vhosts').split(/\n/)[1..-2].map do |line|
       if line =~ /^(\S+)$/
         new(:name => $1)
       else
@@ -28,10 +27,7 @@ Puppet::Type.type(:rabbitmq_vhost).provide(:rabbitmqctl, :parent => Puppet::Prov
   end
 
   def exists?
-    self.class.wait_for_online
-    out = self.class.run_with_retries {
-      rabbitmqctl('list_vhosts')
-    }.split(/\n/)[1..-2].detect do |line|
+    out = rabbitmqctl('list_vhosts').split(/\n/)[1..-2].detect do |line|
       line.match(/^#{Regexp.escape(resource[:name])}$/)
     end
   end
