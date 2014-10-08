cb37547be6ac20f48d08aeb1a122f7da810cd4ce
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-10-08 18:00:42 +0200

    Adapt rabbitmq module for Fuel
    * Add dependency modules: garethr-erlang, nanliu-staging
    * Add support for rabbimtq >1.0 <=4.1 for nova module and make it
      backward compatible (otherwise can't be contributed in upstream)
      (https://bugs.launchpad.net/puppet-nova/+bug/1386309)
    * Adjust other existing rabbit class calls for new 4.1 rabbit module
    * Remove deprecated usage of rabbitmq::server class and replace it
      to ::rabbitmq (cinder, murano, openstack, zabbix, mcollective,
      nailgun)
    * Move rabbit.pub.key template from rabbitmq to openstack module
      (Fuel uses it for offline installation of rabbitmq)
    * Move custom rabbit OCF file from nova to openstack and remove
      OS-aware services disable from rabbit's manifests
    * Make pacemaker provider HA wrapper for rabbitmq service and move
      it to pacemaker_wrappers::rabbitmq
      (TODO move to extras and contribute the change)
    * Fix linting
    * Fix rabbitmq service name for zabbix mon
    * Fix env_config for rabbitmq::nailgun, unify RABBIT_* env args.
    * Use key_content, package_gpg_key, config_*, cluster_*,
      environment_* params to ensure the same rabbit configuration as
      before and make offline installation possible.
    * Add cluster_partition_handling for nova::rabbitmq and
      openstack::controller
      (https://bugs.launchpad.net/puppet-nova/+bug/1386309)
    * Del unused rabbitmq_node_ip_address and rename as node_ip_address
    * Del unneeded rabbit_cluster due to it is being evaluated via
      passed rabbitmq_cluster_nodes as well
    * Adjust debug log_levels for rabbit config as well
    * For '$production !~ /docker/' case, remove custom init scripts
      for rabbit as non relevant anymore.
    
    Related blueprint merge-openstack-puppet-modules
    Related-bug: #1377491
    
    Change-Id: I6e631b975c5835d2d6f1984d5a070a45231fe9ab
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/nova/manifests/rabbitmq.pp
+++ deployment/puppet/nova/manifests/rabbitmq.pp
@@ -32,43 +32,113 @@
 # [*rabbitmq_class*]
 #   (optional) The rabbitmq puppet class to depend on,
 #   which is dependent on the puppet-rabbitmq version.
-#   Use the default for 1.x, use 'rabbitmq' for 3.x
+#   Use the default for 1.x, use '::rabbitmq' for 3.x
 #   Defaults to 'rabbitmq::server'
 #
+# [*rabbitmq_module*]
+#   (optional) The version of puppet-rabbitmq module
+#   to use. Implies the rabbitmq_class value and class
+#   param to be passed for it
+#   Defaults to '1.0'
+#
+# [*node_ip_address*]
+#   (optional) The value of RABBITMQ_NODE_IP_ADDRESS for new
+#   versions ofpuppet-rabbitmq module
+#   Defaults to 'UNSET'
+#
+# [*config_kernel_variables*]
+#   (optional) The kernel config stanza for new versions of
+#   puppet-rabbitmq module
+#   Defaults to {}
+#
+# [*config_variables*]
+#   (optional) The config stanza for new versions of
+#   puppet-rabbitmq module
+#   Defaults to {}
+#
+# [*environment_variables*]
+#   (optional) The environment variables config stanza for
+#   new versions of puppet-rabbitmq module
+#   Defaults to {}
+#
+# [*cluster_partition_handling*]
+#   (optional) The cluster partition handling method for
+#   new versions of puppet-rabbitmq module
+#   Defaults to 'ignore'
+#
+# [*key_source*]
+#   (optional) The package key source for new versions of
+#   puppet-rabbitmq module
+#   Defaults to 'http://www.rabbitmq.com/rabbitmq-signing-key-public.asc'
+#
+# [*key_content*]
+#   (optional) The package key content for new versions of
+#   puppet-rabbitmq module. Overrides key_source, if specified.
+#   Defaults to False
+#
+# [*version*]
+#   (optional) The desired version of RabbitMQ to be installed
+#   by new puppet-rabbitmq module
+#   Defaults to '3.3.0'
+#
 class nova::rabbitmq(
-  $userid             ='guest',
-  $password           ='guest',
-  $port               ='5672',
-  $virtual_host       ='/',
-  $cluster            = false,
-  $cluster_disk_nodes = false,
-  $enabled            = true,
-  $rabbitmq_class     = 'rabbitmq::server',
-  $rabbit_node_ip_address = 'UNSET',
-  $ha_mode            = false,
-  $primary_controller = false
+  $userid                     ='guest',
+  $password                   ='guest',
+  $port                       ='5672',
+  $virtual_host               ='/',
+  $cluster_disk_nodes         = false,
+  $enabled                    = true,
+  $rabbitmq_class             = 'rabbitmq::server',
+  $rabbitmq_module            = '1.0',
+  $node_ip_address            = 'UNSET',
+  $config_kernel_variables    = {},
+  $config_variables           = {},
+  $environment_variables      = {},
+  $cluster_partition_handling = 'ignore',
+  $key_source                 = 'http://www.rabbitmq.com/rabbitmq-signing-key-public.asc',
+  $key_content                = false,
+  # NOTE(bogdando) https://review.openstack.org/#/c/127087
+  $version                    = '3.3.0',
 ) {
 
+  # NOTE(bogdando) the class call should depend on rabbimq module version
+  #   new one may use new options, old one should not.
+  #   Otherwise, the call would not be backward compatible and would fail for
+  #   old rabbitmq modules
+  if ($rabbitmq_module < '4.0') {
+    $rabbitmq_class_real = $rabbitmq_class
+  } else {
+    $rabbitmq_class_real = '::rabbitmq'
+  }
+
   # only configure nova after the queue is up
-  Class[$rabbitmq_class] -> Anchor<| title == 'nova-start' |>
+  Class[$rabbitmq_class_real] -> Anchor<| title == 'nova-start' |>
 
   if ($enabled) {
     if $userid == 'guest' {
       $delete_guest_user = false
     } else {
       $delete_guest_user = true
+      rabbitmq_vhost { $virtual_host:
+        ensure   => present,
+        provider => 'rabbitmqctl',
+        require  => Class[$rabbitmq_class_real],
+      }
       rabbitmq_user { $userid:
+        ensure    => present,
         admin     => true,
         password  => $password,
         provider  => 'rabbitmqctl',
-        require   => Class[$rabbitmq_class],
+        require   => Rabbitmq_vhost[$virtual_host],
       }
       # I need to figure out the appropriate permissions
       rabbitmq_user_permissions { "${userid}@${virtual_host}":
+        ensure               => present,
         configure_permission => '.*',
         write_permission     => '.*',
         read_permission      => '.*',
         provider             => 'rabbitmqctl',
+        require              =>  Rabbitmq_user[$userid],
       }->Anchor<| title == 'nova-start' |>
     }
     $service_ensure = 'running'
@@ -78,138 +148,63 @@ class nova::rabbitmq(
     $service_enabled  = false
   }
 
-  if ($ha_mode) {
-    $service_provider = 'pacemaker'
-    $service_name     = 'p_rabbitmq-server'
-  } else {
-    $service_provider = undef
-    $service_name     = 'rabbitmq-server'
-  }
-
-  if ($ha_mode and ! $primary_controller) {
-    $real_delete_guest_user = false
-  } else {
-    $real_delete_guest_user = $delete_guest_user
-  }
-
   if $cluster_disk_nodes {
-    class { $rabbitmq_class:
-      service_name             => $service_name,
-      service_ensure           => $service_ensure,
-      service_provider         => $service_provider,
-      service_enabled          => $service_enabled,
-      port                     => $port,
-      delete_guest_user        => $real_delete_guest_user,
-      config_cluster           => $cluster,
-      cluster_disk_nodes       => $cluster_disk_nodes,
-      wipe_db_on_cookie_change => true,
-      version                  => $::openstack_version['rabbitmq_version'],
-      node_ip_address          => $rabbit_node_ip_address,
-    }
 
-    if ($ha_mode) {
-      # OCF script for pacemaker
-      # and his dependences
-      file {'rabbitmq-ocf':
-        path   =>'/usr/lib/ocf/resource.d/mirantis/rabbitmq-server',
-        mode   => '0755',
-        owner  => root,
-        group  => root,
-        source => "puppet:///modules/nova/ocf/rabbitmq",
+    # NOTE(bogdando) new config_*, environment_*, cluster_*, default_*
+    #  service_manage params could be used only with '::rabbitmq' class >=4.0
+    if $rabbitmq_class_real == '::rabbitmq' {
+      class { $rabbitmq_class_real:
+        package_gpg_key            => $key_source,
+        key_content                => $key_content,
+        service_ensure             => $service_ensure,
+        service_manage             => $service_enabled,
+        port                       => $port,
+        delete_guest_user          => $delete_guest_user,
+        config_cluster             => true,
+        cluster_disk_nodes         => $cluster_disk_nodes,
+        wipe_db_on_cookie_change   => true,
+        version                    => $version,
+        node_ip_address            => $node_ip_address,
+        config_kernel_variables    => $config_kernel_variables,
+        config_variables           => $config_variables,
+        environment_variables      => $environment_variables,
+        cluster_partition_handling => $cluster_partition_handling,
       }
-
-      File<| title == 'ocf-mirantis-path' |> -> File['rabbitmq-ocf']
-      Package['pacemaker'] -> File<| title == 'ocf-mirantis-path' |>
-      Package['pacemaker'] -> File['rabbitmq-ocf']
-      Package['rabbitmq-server'] ->
-        File['rabbitmq-ocf'] ->
-          Service["$service_name"]
-      if ($primary_controller) {
-        cs_resource {"$service_name":
-          ensure          => present,
-          #cib             => 'rabbitmq',
-          primitive_class => 'ocf',
-          provided_by     => 'mirantis',
-          primitive_type  => 'rabbitmq-server',
-          parameters      => {
-            'node_port'     => $port,
-            #'debug'         => true,
-          },
-          metadata                 => {
-             'migration-threshold' => 'INFINITY',
-             'failure-timeout'     => '60s'
-
-          },
-          multistate_hash => {
-            'type' => 'master',
-          },
-          ms_metadata => {
-            'notify'      => 'true',
-            'ordered'     => 'false', # We shouldn't enable ordered start for parallel start of RA.
-            'interleave'  => 'true',  
-            'master-max'  => '1',
-            'master-node-max' => '1',
-            'target-role' => 'Master'
-          },
-          operations => {
-            'monitor' => {
-              'interval' => '30',
-              'timeout'  => '60'
-            },
-            'monitor:Master' => { # name:role
-              'role' => 'Master',
-              'interval' => '27', # should be non-intercectable with interval from ordinary monitor
-              'timeout'  => '60'
-            },
-            'start' => {
-              'timeout' => '120'
-            },
-            'stop' => {
-              'timeout' => '60'
-            },
-            'promote' => {
-              'timeout' => '120'
-            },
-            'demote' => {
-              'timeout' => '60'
-            },
-            'notify' => {
-              'timeout' => '60'
-            },
-          },
-        }
-        File['rabbitmq-ocf'] ->
-          Cs_resource["$service_name"] ->
-            Service["$service_name"]
+    } else {
+      # backwards compatible call for rabbit class
+      class { $rabbitmq_class_real:
+        service_ensure           => $service_ensure,
+        port                     => $port,
+        delete_guest_user        => $delete_guest_user,
+        config_cluster           => true,
+        cluster_disk_nodes       => $cluster_disk_nodes,
+        wipe_db_on_cookie_change => true,
       }
-
-      exec {'wait for rabbitmq service':
-        path      => ['/usr/bin', '/usr/sbin', '/sbin', '/bin'],
-        try_sleep => 5,
-        tries     => 60,
-        command => "rabbitmqctl list_users",
-      }
-
-      Service["$service_name"] -> Exec<| title == 'wait for rabbitmq service' |>
-      Exec<| title == 'wait for rabbitmq service' |> -> Rabbitmq_user <||>
-      Exec<| title == 'wait for rabbitmq service' |> -> Rabbitmq_vhost <||>
-      Exec<| title == 'wait for rabbitmq service' |> -> Rabbitmq_user_permissions <||>
     }
   } else {
-    class { $rabbitmq_class:
-      service_ensure    => $service_ensure,
-      port              => $port,
-      delete_guest_user => $delete_guest_user,
-      config_cluster    => false,
-      version           => $::openstack_version['rabbitmq_version'],
-      node_ip_address   => $rabbit_node_ip_address,
-    }
-  }
-
-  if ($enabled) {
-    rabbitmq_vhost { $virtual_host:
-      provider => 'rabbitmqctl',
-      require  => Class[$rabbitmq_class],
+    # clusterless mode
+    if $rabbitmq_class_real == '::rabbitmq' {
+      class { $rabbitmq_class_real:
+        package_gpg_key         => $key_source,
+        key_content             => $key_content,
+        service_ensure          => $service_ensure,
+        service_manage          => $service_enabled,
+        port                    => $port,
+        delete_guest_user       => $delete_guest_user,
+        config_cluster          => false,
+        version                 => $version,
+        node_ip_address         => $node_ip_address,
+        config_kernel_variables => $config_kernel_variables,
+        config_variables        => $config_variables,
+        environment_variables   => $environment_variables,
+      }
+    } else {
+      # backwards compatible call
+      class { $rabbitmq_class_real:
+        service_ensure    => $service_ensure,
+        port              => $port,
+        delete_guest_user => $delete_guest_user,
+      }
     }
   }
 }
