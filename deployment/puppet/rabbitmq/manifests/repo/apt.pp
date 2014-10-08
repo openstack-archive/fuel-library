5075fb1bce6a2d0b3345065b1a76d0544c2284e9
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-10-08 17:38:05 +0200

    Sync puppetlabs rabbitmq module
    v4.1.0 df52e345349c031167cf0cf3501314f3738b4949
    
    Related blueprint merge-openstack-puppet-modules
    
    Change-Id: I924a2a4489dc329fefbee859a4b4bde0b79e86d1
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/rabbitmq/manifests/repo/apt.pp
+++ deployment/puppet/rabbitmq/manifests/repo/apt.pp
@@ -1,24 +1,29 @@
-# sets up the vmware hosted apt repo
 # requires
 #   puppetlabs-apt
 #   puppetlabs-stdlib
 class rabbitmq::repo::apt(
-  $pin = undef
-) {
+  $location    = 'http://www.rabbitmq.com/debian/',
+  $release     = 'testing',
+  $repos       = 'main',
+  $include_src = false,
+  $key         = '056E8E56',
+  $key_source  = 'http://www.rabbitmq.com/rabbitmq-signing-key-public.asc',
+  ) {
+
+  $pin = $rabbitmq::package_apt_pin
 
   Class['rabbitmq::repo::apt'] -> Package<| title == 'rabbitmq-server' |>
 
   apt::source { 'rabbitmq':
-    location    => 'http://www.rabbitmq.com/debian/',
-    release     => 'testing',
-    repos       => 'main',
-    include_src => false,
-    key         => 'RabbitMQ Release Signing Key <info@rabbitmq.com>',
-    key_content => template('rabbitmq/rabbit.pub.key'),
-    pin         => $pin,
+    location    => $location,
+    release     => $release,
+    repos       => $repos,
+    include_src => $include_src,
+    key         => $key,
+    key_source  => $key_source,
   }
 
-  if ! ($pin == undef) {
+  if $pin {
     validate_re($pin, '\d\d\d')
     apt::pin { 'rabbitmq':
       packages => 'rabbitmq-server',
