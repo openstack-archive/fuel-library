ea19e4a0cc08d89f190618c5221034e4d254858c
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-10-08 17:38:05 +0200

    Sync puppetlabs rabbitmq module
    commit 4832bd61b5b1bfea7c9cc985508e65cd10081652
    Source: https://github.com/puppetlabs/puppetlabs-rabbitmq.git
    
    Related blueprint merge-openstack-puppet-modules
    
    Fuel-CI: disable
    Change-Id: I924a2a4489dc329fefbee859a4b4bde0b79e86d1
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/rabbitmq/lib/puppet/provider/rabbitmq_plugin/rabbitmqplugins.rb
+++ deployment/puppet/rabbitmq/lib/puppet/provider/rabbitmq_plugin/rabbitmqplugins.rb
@@ -1,6 +1,23 @@
 Puppet::Type.type(:rabbitmq_plugin).provide(:rabbitmqplugins) do
 
-  commands :rabbitmqplugins => 'rabbitmq-plugins'
+  if Puppet::PUPPETVERSION.to_f < 3
+    if Facter.value(:osfamily) == 'RedHat'
+      commands :rabbitmqplugins => '/usr/lib/rabbitmq/bin/rabbitmq-plugins'
+    else
+      commands :rabbitmqplugins => 'rabbitmq-plugins'
+    end
+  else
+    if Facter.value(:osfamily) == 'RedHat'
+      has_command(:rabbitmqplugins, '/usr/lib/rabbitmq/bin/rabbitmq-plugins') do
+        environment :HOME => "/tmp"
+      end
+    else
+      has_command(:rabbitmqplugins, 'rabbitmq-plugins') do
+        environment :HOME => "/tmp"
+      end
+    end
+  end
+
   defaultfor :feature => :posix
 
   def self.instances
@@ -22,7 +39,7 @@ Puppet::Type.type(:rabbitmq_plugin).provide(:rabbitmqplugins) do
   end
 
   def exists?
-    out = rabbitmqplugins('list', '-E').split(/\n/).detect do |line|
+    rabbitmqplugins('list', '-E').split(/\n/).detect do |line|
       line.split(/\s+/)[1].match(/^#{resource[:name]}$/)
     end
   end
