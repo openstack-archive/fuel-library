504a0b8a213eb1738d2b1cf877107589a46edc7f
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-10-08 17:38:05 +0200

    Sync puppetlabs rabbitmq module
    commit 4832bd61b5b1bfea7c9cc985508e65cd10081652
    Source: https://github.com/puppetlabs/puppetlabs-rabbitmq.git
    
    Related blueprint merge-openstack-puppet-modules
    
    Fuel-CI: disable
    Change-Id: I924a2a4489dc329fefbee859a4b4bde0b79e86d1
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

---
+++ deployment/puppet/rabbitmq/manifests/install/rabbitmqadmin.pp
@@ -0,0 +1,26 @@
+#
+class rabbitmq::install::rabbitmqadmin {
+
+  $management_port = $rabbitmq::management_port
+  $default_user = $rabbitmq::default_user
+  $default_pass = $rabbitmq::default_pass
+  $protocol = $rabbitmq::ssl ? { false => 'http', default => 'https' }
+
+  staging::file { 'rabbitmqadmin':
+    target  => '/var/lib/rabbitmq/rabbitmqadmin',
+    source  => "${protocol}://${default_user}:${default_pass}@localhost:${management_port}/cli/rabbitmqadmin",
+    require => [
+      Class['rabbitmq::service'],
+      Rabbitmq_plugin['rabbitmq_management']
+    ],
+  }
+
+  file { '/usr/local/bin/rabbitmqadmin':
+    owner   => 'root',
+    group   => 'root',
+    source  => '/var/lib/rabbitmq/rabbitmqadmin',
+    mode    => '0755',
+    require => Staging::File['rabbitmqadmin'],
+  }
+
+}
