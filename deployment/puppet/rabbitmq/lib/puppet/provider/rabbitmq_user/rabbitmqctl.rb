4b16cfd2baebed743f8f6d069253d881382035fb
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-10-08 18:00:42 +0200

    Adapt rabbitmq module for Fuel
    * Add dependency modules: garethr-erlang, nanliu-staging
    * Add support for rabbimtq >1.0 <=4.1 for nova module and make it
      backward compatible (otherwise can't be contributed in upstream)
      (https://bugs.launchpad.net/puppet-nova/+bug/1386309)
    * Adjust other existing rabbit class calls for new 4.1 rabbit module
    * Remove deprecated usage of rabbitmq::server class and replace it
      to ::rabbitmq (cinder, murano, openstack, zabbix, mcollective,
      nailgun)
    * Move rabbit.pub.key template from rabbitmq to openstack module
      (Fuel uses it for offline installation of rabbitmq)
    * Move custom rabbit OCF file from nova to openstack and remove
      OS-aware services disable from rabbit's manifests
    * Make pacemaker provider HA wrapper for rabbitmq service and move
      it to pacemaker_wrappers::rabbitmq
    * Fix linting
    * Fix rabbitmq service name for zabbix mon
    * Port fix for #MODULES-1488
    * Fix env_config for rabbitmq::nailgun, unify RABBIT_* env args.
    * Use key_content, package_gpg_key, config_*, cluster_*,
      environment_* params to ensure the same rabbit configuration as
      before and make offline installation possible.
    * Add cluster_partition_handling for nova::rabbitmq and
      openstack::controller
      (https://bugs.launchpad.net/puppet-nova/+bug/1386309)
    * Del unused rabbitmq_node_ip_address and rename as node_ip_address
    * Del unneeded rabbit_cluster due to it is being evaluated via
      passed rabbitmq_cluster_nodes as well
    * Adjust debug log_levels for rabbit config as well
    * For '$production !~ /docker/' case, remove custom init scripts
      for rabbit as non relevant anymore.
    * Add retries in rabbitmq provider
      (https://tickets.puppetlabs.com/browse/MODULES-1452)
    * Add metadata.json from v4.1.0
    * Enable rspec tests for rabbitmq in utils
    
    Related blueprint merge-openstack-puppet-modules
    Related-bug: #1377491
    
    Change-Id: I6e631b975c5835d2d6f1984d5a070a45231fe9ab
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/rabbitmq/lib/puppet/provider/rabbitmq_user/rabbitmqctl.rb
+++ deployment/puppet/rabbitmq/lib/puppet/provider/rabbitmq_user/rabbitmqctl.rb
@@ -1,6 +1,6 @@
 require 'puppet'
-require 'set'
-Puppet::Type.type(:rabbitmq_user).provide(:rabbitmqctl) do
+require File.join File.dirname(__FILE__), '../rabbitmq_common.rb'
+Puppet::Type.type(:rabbitmq_user).provide(:rabbitmqctl, :parent => Puppet::Provider::Rabbitmq_common) do
 
   if Puppet::PUPPETVERSION.to_f < 3
     commands :rabbitmqctl => 'rabbitmqctl'
@@ -13,8 +13,11 @@ Puppet::Type.type(:rabbitmq_user).provide(:rabbitmqctl) do
   defaultfor :feature => :posix
 
   def self.instances
-    rabbitmqctl('list_users').split(/\n/)[1..-2].collect do |line|
-      if line =~ /^(\S+)(\s+\[.*?\]|)$/
+    self.wait_for_online
+    self.run_with_retries {
+      rabbitmqctl('-q', 'list_users')
+    }.split(/\n/).collect do |line|
+      if line =~ /^(\S+)(\s+\S+|)$/
         new(:name => $1)
       else
         raise Puppet::Error, "Cannot parse invalid user line: #{line}"
@@ -37,7 +40,10 @@ Puppet::Type.type(:rabbitmq_user).provide(:rabbitmqctl) do
   end
 
   def exists?
-    rabbitmqctl('list_users').split(/\n/)[1..-2].detect do |line|
+    self.class.wait_for_online
+    out = self.class.run_with_retries {
+      rabbitmqctl('-q', 'list_users')
+    }.split(/\n/).detect do |line|
       line.match(/^#{Regexp.escape(resource[:name])}(\s+(\[.*?\]|\S+)|)$/)
     end
   end
@@ -90,7 +96,7 @@ Puppet::Type.type(:rabbitmq_user).provide(:rabbitmqctl) do
 
   private
   def get_user_tags
-    match = rabbitmqctl('list_users').split(/\n/)[1..-2].collect do |line|
+    match = rabbitmqctl('-q', 'list_users').split(/\n/).collect do |line|
       line.match(/^#{Regexp.escape(resource[:name])}\s+\[(.*?)\]/)
     end.compact.first
     Set.new(match[1].split(/, /)) if match
