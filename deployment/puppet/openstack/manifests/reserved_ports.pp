6028f40d8f98b81e745c4387965366015ddf334d
Oleksiy Molchanov <omolchanov@mirantis.com>
2014-10-13 15:46:12 +0300

    Adds support for configuring kernel reserved ephemeral ports
    Adds support for configuring kernel reserved ephemeral ports
    using sysctl parameter net.ipv4.ip_local_reserved_ports
    
    Change-Id: I8a2f6dcf120aadb4ebd1ac17049cb2b90c776e7b
    Closes-Bug: 1353363

---
+++ deployment/puppet/openstack/manifests/reserved_ports.pp
@@ -0,0 +1,22 @@
+#
+# Configure kernel reserved ephemeral ports
+# (see https://bugs.launchpad.net/fuel/+bug/1353363)
+#
+# Specify the ports which are reserved for known
+# third-party applications. These ports will not
+# be used by automatic port assignments (e.g. when
+# calling connect() or bind() with port number 0).
+#
+# Parameters
+#
+# [*ports*]
+# Comma separated list of ranges to be reserved
+#
+# Examples:
+# class { 'openstack::reserved_ports': }
+#
+
+class openstack::reserved_ports ( $ports = '49000,35357,41055,58882' ) {
+  sysctl::value { 'net.ipv4.ip_local_reserved_ports': value => $ports }
+}
+
