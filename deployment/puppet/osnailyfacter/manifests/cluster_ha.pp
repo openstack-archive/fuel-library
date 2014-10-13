13682a226b5a157204c3a9cb3926f4a16133dfcc
iberezovskiy <iberezovskiy@mirantis.com>
2014-10-13 15:43:22 +0400

    Configure Sahara notifications
    Change-Id: I8417e7f8eb938ff331c6206bea567b4ebb52ea1a
    Closes-bug: #1375774

--- deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
@@ -592,6 +592,13 @@ class osnailyfacter::cluster_ha {
           debug                      => $::debug,
           verbose                    => $::verbose,
           use_syslog                 => $::use_syslog,
+          enable_notifications       => $ceilometer_hash['enabled'],
+          rpc_backend                => 'rabbit',
+          amqp_password              => $rabbit_hash['password'],
+          amqp_user                  => $rabbit_hash['user'],
+          amqp_port                  => $rabbitmq_bind_port,
+          amqp_hosts                 => $amqp_hosts,
+          rabbit_ha_queues           => $rabbit_ha_queues,
         }
         $scheduler_default_filters = [ 'DifferentHostFilter' ]
       } else {
