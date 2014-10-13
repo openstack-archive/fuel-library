47efa30747cfc014468b28343d005239f012aa74
Dmitry Teselkin <dteselkin@mirantis.com>
2014-10-13 18:57:25 +0400

    Add murano to keystone services
    This patch adds murano to the list of services in keystone.
    
    Change-Id: If65c5cac809f0a96798c5e8189acc6bf83ffc197
    Closes-bug: 1362037

--- deployment/puppet/murano/manifests/keystone.pp
+++ deployment/puppet/murano/manifests/keystone.pp
@@ -1,8 +1,13 @@
 class murano::keystone (
-  $user     = 'murano',
-  $password = 'swordfish',
-  $tenant   = 'services',
-  $email    = 'murano@localhost'
+  $user             = 'murano',
+  $password         = 'swordfish',
+  $tenant           = 'services',
+  $email            = 'murano@localhost',
+  $public_address   = '127.0.0.1',
+  $admin_address    = '127.0.0.1',
+  $internal_address = '127.0.0.1',
+  $region           = 'RegionOne',
+  $murano_api_port  = '8082',
 ) {
 
   keystone_user { $user:
@@ -16,6 +21,20 @@ class murano::keystone (
   keystone_user_role { "${user}@${tenant}":
     roles  => 'admin',
     ensure => present,
+  }
+
+  keystone_service { $user:
+    ensure      => present,
+    type        => 'application_catalog',
+    description => 'Application Catalog for OpenStack',
+  }
+
+  keystone_endpoint { $user:
+    ensure       => present,
+    region       => $region,
+    public_url   => "http://${public_address}:${murano_api_port}/v1/%(tenant_id)s",
+    internal_url => "http://${internal_address}:${murano_api_port}/v1/%(tenant_id)s",
+    admin_url    => "http://${admin_address}:${murano_api_port}/v1/%(tenant_id)s",
   }
 
 }
