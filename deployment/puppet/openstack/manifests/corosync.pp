69a36d4362f7db441517656f8e6d2a4eb8c27c61
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2014-10-14 09:36:39 +0200

    Refactor corosync module
    Change-Id: I3700edaff004d287396286f3d86ed211acc8fab1

--- deployment/puppet/openstack/manifests/corosync.pp
+++ deployment/puppet/openstack/manifests/corosync.pp
@@ -1,87 +1,90 @@
-# todo: move this file and ocf scripts to cluster module
-# todo: refactor neutron-* ocf scripts
 class openstack::corosync (
-  $bind_address = '127.0.0.1',
-  $multicast_address = '239.1.1.2',
-  $secauth = 'off',
-  $stonith = 'false',
-  $quorum_policy = 'ignore',
-  $expected_quorum_votes = "2",
-  $unicast_addresses = undef
+  $bind_address          = '127.0.0.1',
+  $multicast_address     = '239.1.1.2',
+  $secauth               = false,
+  $stonith               = false,
+  $quorum_policy         = 'ignore',
+  $expected_quorum_votes = '2',
+  $unicast_addresses     = undef
 ) {
 
+  file { 'limitsconf':
+    ensure  => present,
+    path    => '/etc/security/limits.conf',
+    source  => 'puppet:///modules/openstack/limits.conf',
+    replace => true,
+    owner   => '0',
+    group   => '0',
+    mode    => '0644',
+    before  => Service['corosync'],
+  }
 
-anchor {'corosync':}
+  anchor {'corosync':}
 
-Anchor['corosync'] -> Cs_property<||>
+  Anchor['corosync'] -> Cs_property<||>
 
-#Define shadow CIB
+  Class['::corosync']->Cs_shadow<||>
+  Class['::corosync']->Cs_property<||>->Cs_resource<||>
+  Cs_property<||>->Cs_shadow<||>
 
-#Cs_resource {cib => 'shadow'}
-#Cs_property {cib => 'shadow'}
-#Cs_order {cib => 'shadow'}
-#Cs_colocation {cib => 'shadow'}
-#Cs_group {cib => 'shadow'}
+  Cs_property['no-quorum-policy']->
+    Cs_property['stonith-enabled']->
+      Cs_property['start-failure-is-fatal']
 
-Class['::corosync']->Cs_shadow<||>
-Class['::corosync']->Cs_property<||>->Cs_resource<||>
-Cs_property<||>->Cs_shadow<||>
-Cs_property['no-quorum-policy']->Cs_property['stonith-enabled']->Cs_property['start-failure-is-fatal']
+  file {'filter_quantum_ports.py':
+    path   =>'/usr/bin/filter_quantum_ports.py',
+    mode   => '0744',
+    owner  => root,
+    group  => root,
+    source => 'puppet:///modules/openstack/filter_quantum_ports.py',
+  }
 
-file {'filter_quantum_ports.py':
-  path   =>'/usr/bin/filter_quantum_ports.py',
-  mode   => '0744',
-  #require =>[Package['corosync'],File['/root/openrc']],
-  #require =>Package['corosync'],
-  owner  => root,
-  group  => root,
-  source => "puppet:///modules/openstack/filter_quantum_ports.py",
-}
+  Anchor['corosync'] ->
+  corosync::service { 'pacemaker':
+    version => '0',
+  }
+  Corosync::Service['pacemaker'] ~> Service['corosync']
+  Corosync::Service['pacemaker'] -> Anchor['corosync-done']
 
-Anchor['corosync'] ->
-corosync::service { 'pacemaker':
-  version => '0',
-}
-Corosync::Service['pacemaker'] ~> Service['corosync']
-Corosync::Service['pacemaker'] -> Anchor['corosync-done']
 
-class { '::corosync':
-  enable_secauth    => $secauth,
-  bind_address      => $bind_address,
-  multicast_address => $multicast_address,
-  unicast_addresses => $unicast_addresses
-}
+  class { '::corosync':
+    enable_secauth    => $secauth,
+    bind_address      => $bind_address,
+    multicast_address => $multicast_address,
+    unicast_addresses => $unicast_addresses,
+    corosync_version  => '1',
+  }
 
-#cs_property { 'expected-quorum-votes':
-#  ensure => present,
-#  value  => $expected_quorum_votes
-#}
+  #cs_property { 'expected-quorum-votes':
+  #  ensure => present,
+  #  value  => $expected_quorum_votes
+  #}
 
-cs_property { 'no-quorum-policy':
-  ensure => present,
-  value  => $quorum_policy,
-  retries => 5
-} -> Anchor['corosync-done']
+  cs_property { 'no-quorum-policy':
+    ensure  => present,
+    value   => $quorum_policy,
+    retries => 5
+  } -> Anchor['corosync-done']
 
-cs_property { 'stonith-enabled':
-  ensure => present,
-  value  => $stonith,
-} -> Anchor['corosync-done']
+  cs_property { 'stonith-enabled':
+    ensure => present,
+    value  => $stonith,
+  } -> Anchor['corosync-done']
 
-cs_property { 'start-failure-is-fatal':
-  ensure => present,
-  value  => "false",
-} -> Anchor['corosync-done']
+  cs_property { 'start-failure-is-fatal':
+    ensure => present,
+    value  => false,
+  } -> Anchor['corosync-done']
 
-cs_property { 'symmetric-cluster':
-  ensure => present,
-  value  => "false",
-} -> Anchor['corosync-done']
+  cs_property { 'symmetric-cluster':
+    ensure => present,
+    value  => false,
+  } -> Anchor['corosync-done']
 
-#cs_property { 'placement-strategy':
-#  ensure => absent,
-#  value  => 'default',
-#}
+  #cs_property { 'placement-strategy':
+  #  ensure => absent,
+  #  value  => 'default',
+  #}
 
-anchor {'corosync-done':}
+  anchor {'corosync-done':}
 }
