62096e645b884ae1f9aa0e7d6cd62adda9484850
Dmitry Teselkin <dteselkin@mirantis.com>
2014-10-14 13:00:49 +0400

    Fix murano networking configuration
    Since the commits f1905be3367e8ed767794f2e54b55b15918628ff and
    09fe0567a20b211239a6d5486d6aa8e6d26de3bc were merged, murano requires
    implicitly defined router to connect to, or a external network name to
    create such router.
    
    This patch adds support for that behaviour.
    
    Closes-bug: 1390042
    
    Change-Id: Ifab63736d51ff41995d3d09ea49eb459612b3b3a

---
+++ deployment/puppet/murano/lib/puppet/parser/functions/get_ext_net_name.rb
@@ -0,0 +1,10 @@
+module Puppet::Parser::Functions
+  newfunction(:get_ext_net_name, :type => :rvalue) do |args|
+    networks, default_net = args
+    begin
+      networks.find {|key, value| value['L2']['router_ext'] }[0]
+    rescue NoMethodError
+      default_net
+    end
+  end
+end
