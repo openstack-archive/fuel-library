c86390f1f3e455e50d9e72c7550f153b0f79ea56
Oleksiy Molchanov <omolchanov@mirantis.com>
2014-10-24 18:32:33 +0300

    Add support of external DNS and NTP
    Add support of using external DNS and NTP in HA mode.
    
    Change-Id: I9d741c638c1bd034ec3b451cffa1e70a3fee802a
    Implements: blueprint external-dns-ntp-support

---
+++ deployment/puppet/osnailyfacter/manifests/resolvconf.pp
@@ -0,0 +1,31 @@
+# == Class: osnailyfacter::resolvconf
+#
+# Configure resolv.conf on fuel nodes
+#
+# === Parameters
+#
+# [*$management_vip*]
+# Management virtual ip address
+#
+# === Examples
+#
+#  class { osnailyfacter::resolvconf:
+#    management_ip => '1.1.1.1',
+#  }
+#
+# === Authors
+#
+# Mirantis
+#
+# === Copyright
+#
+# GNU GPL
+#
+class osnailyfacter::resolvconf (
+  $management_vip
+) {
+  file { '/etc/resolv.conf':
+    ensure  => present,
+    content => template('osnailyfacter/resolv.conf.erb')
+  }
+}
