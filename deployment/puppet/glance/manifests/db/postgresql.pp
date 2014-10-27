ce16a72e6499882ef85f6c0a09a9b209b3d6efd1
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-10-27 14:18:26 +0400

    Adapt puppetlabs-postgresql to Fuel
    Fuel uses a custom package name "postgresql" instead
    of "postgresql94", so this value needs to be passed,
    along with bindir value.
    
    Also adapted all manifest references to postgresql::db to
    postgresql::server::db.
    
    Change-Id: I1ec329ee31ecc6edfd357329bfc84df95ecf7ddc
    Closes-Bug: #1386118

--- deployment/puppet/glance/manifests/db/postgresql.pp
+++ deployment/puppet/glance/manifests/db/postgresql.pp
@@ -10,10 +10,10 @@ class glance::db::postgresql(
 
   require postgresql::python
 
-  Postgresql::Db[$dbname] ~> Exec<| title == 'glance-manage db_sync' |>
+  Postgresql::Server::Db[$dbname] ~> Exec<| title == 'glance-manage db_sync' |>
   Package['python-psycopg2'] -> Exec<| title == 'glance-manage db_sync' |>
 
-  postgresql::db { $dbname:
+  postgresql::server::db { $dbname:
     user      =>  $user,
     password  =>  $password,
   }
