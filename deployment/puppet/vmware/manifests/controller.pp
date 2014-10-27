b6ac5749f5d3ecfd6ae686b642aab9eb0746d4ee
Igor Zinovik <izinovik@mirantis.com>
2014-10-27 16:18:21 +0300

    set `reserved_host_memory_mb' to zero for vCenter compute driver
    - VMware recommends set `reserved_host_memory_mb' configuration stanza
      to zero for vCenter:
      http://docs.openstack.org/trunk/config-reference/content/vmware.html
    
    Change-Id: I0020481d68990fe1bc3bc3896b9166e6951550c4
    Closes-Bug: #1382539

--- deployment/puppet/vmware/manifests/controller.pp
+++ deployment/puppet/vmware/manifests/controller.pp
@@ -90,7 +90,8 @@ class vmware::controller (
   }
   # Set correct parameter for vnc access
   nova_config {
-    'DEFAULT/novncproxy_base_url': value => "http://${vnc_address}:6080/vnc_auto.html"
+    'DEFAULT/novncproxy_base_url':     value => "http://${vnc_address}:6080/vnc_auto.html";
+    'DEFAULT/reserved_host_memory_mb': value => 0;
   }
 
   # install cirros vmdk package
