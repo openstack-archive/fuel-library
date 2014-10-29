d08e46b322a621a2aa18c7fc7c4ebc9ba47ef41d
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2014-10-29 09:56:02 +0100

    Set nginx default settings for master node
    - increase number of workers from 2 to half available CPUs.
    - enable epoll by default
    - enable tcpnopush
    - enable tcpnodelay
    
    These settings allow to bootstrap many nodes in parallel allowing
    anaconda and debian installer to get all files without timeouts
    
    Change-Id: I44054cf2afa1fa5a70ba064f4957b964947b2b2c
    Implements: bp/100-nodes-support
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/nailgun/manifests/nginx-service.pp
+++ deployment/puppet/nailgun/manifests/nginx-service.pp
@@ -7,10 +7,16 @@ class nailgun::nginx-service (
   } else {
     $ensure = 'running'
   }
+  file { '/etc/nginx/nginx.conf':
+    ensure  => installed,
+    mode    => '0644',
+    content => template('nailgun/nginx.conf.erb'),
+    require => Package['nginx'],
+  }
   service { 'nginx':
-    enable => $service_enabled,
-    ensure => $ensure,
-    require => Package["nginx"],
+    enable  => $service_enabled,
+    ensure  => $ensure,
+    require => File['/etc/nginx/nginx.conf'],
   }
   Package<| title == 'nginx'|> ~> Service<| title == 'nginx'|>
   if !defined(Service['nginx']) {
