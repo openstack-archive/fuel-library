9e6ed997ede32238f75b113492cb704026c3a5a1
Sergey Kolekonov <skolekonov@mirantis.com>
2014-11-02 12:59:01 +0300

    Enable Neutron DVR support
    Neutron Distributed Virtual Router implements the L3 Routers across
    the Compute Nodes, so that tenants intra VM communication will occur
    without hiting the Network Node.
    
    Implements blueprint: neutron-dvr-deployment
    Change-Id: I2e81c05f2467d73f54279ea855114b7d4fa85dab

--- deployment/puppet/neutron/manifests/server.pp
+++ deployment/puppet/neutron/manifests/server.pp
@@ -149,6 +149,12 @@
 # [*mysql_module*]
 #   (optional) Deprecated. Does nothing.
 #
+# [*router_distributed*]
+#   (optional) Setting the "router_distributed" flag to "True" will default to the creation
+#   of distributed tenant routers.
+#   Also can be the type of the router on the create request (admin-only attribute).
+#   Defaults to false
+#
 class neutron::server (
   $package_ensure          = 'present',
   $enabled                 = true,
@@ -173,6 +179,7 @@ class neutron::server (
   $state_path              = '/var/lib/neutron',
   $lock_path               = '/var/lib/neutron/lock',
   $router_scheduler_driver = 'neutron.scheduler.l3_agent_scheduler.ChanceScheduler',
+  $router_distributed       = false,
   # DEPRECATED PARAMETERS
   # TODO(bogdando) undone the change once puppet-openstacklibs supported in Fuel
   $mysql_module            = '0.9',
@@ -305,6 +312,7 @@ class neutron::server (
     'DEFAULT/rpc_workers':             value => $rpc_workers;
     'DEFAULT/agent_down_time':         value => $agent_down_time;
     'DEFAULT/router_scheduler_driver': value => $router_scheduler_driver;
+    'DEFAULT/router_distributed':      value => $router_distributed;
     'DEFAULT/state_path':              value => $state_path;
     'DEFAULT/lock_path':               value => $lock_path;
     'database/connection':             value => $database_connection_real, secret => true;
--- deployment/puppet/openstack/manifests/network/neutron_agents.pp
+++ deployment/puppet/openstack/manifests/network/neutron_agents.pp
@@ -44,6 +44,7 @@ class openstack::network::neutron_agents (
   $metadata_port = 9697,
   $send_arp_for_ha = 3,
   $external_network_bridge = 'br-ex',
+  $agent_mode = 'legacy',
 
   # keystone params
   $admin_password    = 'asdf123',
@@ -91,13 +92,15 @@ class openstack::network::neutron_agents (
       vni_ranges            => $vni_ranges,
     }
     class { 'neutron::agents::ml2::ovs':
-      integration_bridge  => $integration_bridge,
-      tunnel_bridge       => $tunnel_bridge,
-      bridge_mappings     => $bridge_mappings,
-      enable_tunneling    => $enable_tunneling,
-      local_ip            => $local_ip,
-      manage_service      => true,
-      enabled             => true,
+      integration_bridge         => $integration_bridge,
+      tunnel_bridge              => $tunnel_bridge,
+      bridge_mappings            => $bridge_mappings,
+      enable_tunneling           => $enable_tunneling,
+      local_ip                   => $local_ip,
+      enable_distributed_routing => $agent_mode ? { 'legacy' => false, default => true},
+      l2_population              => $agent_mode ? { 'legacy' => false, default => true},
+      manage_service             => true,
+      enabled                    => true,
     }
 
     Service<| title == 'neutron-server' |> -> Service<| title == 'neutron-ovs-agent-service' |>
@@ -166,6 +169,7 @@ class openstack::network::neutron_agents (
       metadata_port           => $metadata_port,
       send_arp_for_ha         => $send_arp_for_ha,
       external_network_bridge => $external_network_bridge,
+      agent_mode              => $agent_mode,
       manage_service          => true,
       enabled                 => true,
     }
