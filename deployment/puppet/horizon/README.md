63d0315cab3a05c6ea259dcc6e3d76f9c9b7f13f
Aleksandr Didenko <adidenko@mirantis.com>
2014-11-03 16:08:47 +0200

    Sync horizon module with upstream
    I4c57a236518c57d8168425843cbbf1ce55726b94 v 4.1.0
    
    Fuel-CI: disable
    
    Change-Id: I64e267f12fac5602069500236538c41cf686532f
    Partial: merge-openstack-puppet-modules

--- deployment/puppet/horizon/README.md
+++ deployment/puppet/horizon/README.md
@@ -1,4 +1,144 @@
-OpenStack Dashboard
-===================
+horizon
+=======
 
-Puppet module for [Horizon](http://horizon.openstack.org/), ([OpenStack Dashboard](http://wiki.openstack.org/OpenStackDashboard))
+4.0.0 - 2014.1.0 - Icehouse
+
+#### Table of Contents
+
+1. [Overview - What is the horizon module?](#overview)
+2. [Module Description - What does the module do?](#module-description)
+3. [Setup - The basics of getting started with horizon](#setup)
+4. [Implementation - An under-the-hood peek at what the module is doing](#implementation)
+5. [Limitations - OS compatibility, etc.](#limitations)
+6. [Development - Guide for contributing to the module](#development)
+7. [Contributors - Those with commits](#contributors)
+8. [Release Notes - Notes on the most recent updates to the module](#release-notes)
+
+Overview
+--------
+
+The horizon module is a part of [Stackforge](https://github.com/stackforge), an effort by the Openstack infrastructure team to provide continuous integration testing and code review for Openstack and Openstack community projects not part of the core software.  The module its self is used to flexibly configure and manage the dashboard service for Openstack.
+
+Module Description
+------------------
+
+The horizon module is a thorough attempt to make Puppet capable of managing the entirety of horizon.  Horizon is a fairly classic django application, which results in a fairly simply Puppet module.
+
+This module is tested in combination with other modules needed to build and leverage an entire Openstack software stack.  These modules can be found, all pulled together in the [openstack module](https://github.com/stackforge/puppet-openstack).
+
+Setup
+-----
+
+**What the horizon module affects**
+
+* horizon, the dashboard service for Openstack.
+
+### Installing horizon
+
+    example% puppet module install puppetlabs/horizon
+
+### Beginning with horizon
+
+To utilize the horizon module's functionality you will need to declare multiple resources but you'll find that doing so is much less complicated than the other OpenStack component modules.  The following is a modified excerpt from the [openstack module](https://github.com/stackforge/puppet-openstack).  We recommend you consult and understand the [openstack module](https://github.com/stackforge/puppet-openstack) and the [core openstack](http://docs.openstack.org) documentation.
+
+**Define a horizon dashboard**
+
+```puppet
+class { 'memcached':
+  listen_ip => '127.0.0.1',
+  tcp_port  => '11211',
+  udp_port  => '11211',
+}
+
+class { '::horizon':
+  cache_server_ip       => '127.0.0.1',
+  cache_server_port     => '11211',
+  secret_key            => '12345',
+  swift                 => false,
+  django_debug          => 'True',
+  api_result_limit      => '2000',
+}
+```
+
+Implementation
+--------------
+
+### horizon
+
+Horizon is a simple module using the combination of a package, template, and the file_line type.  Most all the configuration lives inside the included local_settings template and the file_line type is for selectively inserting needed lines into configuration files that aren't explicitly managed by the horizon module.
+
+Limitations
+------------
+
+* Only supports Apache using mod_wsgi.
+
+Development
+-----------
+
+Developer documentation for the entire puppet-openstack project.
+
+* https://wiki.openstack.org/wiki/Puppet-openstack#Developer_documentation
+
+Contributors
+------------
+
+* https://github.com/stackforge/puppet-horizon/graphs/contributors
+
+Release Notes
+-------------
+
+**4.1.0**
+
+* Added option to set temporary upload directory for images.
+* Ensure ssl wsgi_process_group is the same as wsgi_daemon_process.
+* Pined major gems.
+
+**4.0.0**
+
+* Stable Icehouse release.
+* Added support to pass extra parameters to vhost.
+* Added support to ensure online cache is present and can be refreshed.
+* Added support to configure OPENSTACK_HYPERVISOR_FEATURES settings, AVAILABLE_REGIONS, OPENSTACK_NEUTRON_NETWORK.
+* Added support to disable configuration of Apache.
+* Fixed log ownership and WSGIProcess* settings for Red Hat releases.
+* Fixed overriding of policy files in local settings.
+* Fixed SSL bugs.
+* Improved WSGI configuration.
+
+**3.1.0**
+
+* Added option parameterize OPENSTACK_NEUTRON_NETWORK settings.
+
+**3.0.1**
+
+* Adds COMPRESS_OFFLINE option to local_settings to fix broken Ubuntu installation.
+
+**3.0.0**
+
+* Major release for OpenStack Havana.
+* Updated user and group for Debian family OSes.
+* Updated policy files for RedHat family OSes.
+* Enabled SSL support with cert/key.
+* Improved default logging configuration.
+* Fixed bug to set LOGOUT_URL properly.
+* Introduced new parameters: keystone_url, help_url, endpoint type.
+* Fixed user/group regression for Debian.
+* Changed keystone_default_role to _member_.
+
+**2.2.0**
+
+* Fixed apache 0.9.0 incompatability.
+* Various lint fixes.
+
+**2.1.0**
+
+* Updated local_settings.py.
+* Pinned Apache module version.
+* Various lint fixes.
+
+**2.0.0**
+
+* Upstream is now part of stackforge.
+* httpd config now managed on every platform.
+* Provides option to enable Horizon's display of block device mount points.
+
--- deployment/puppet/horizon/spec/classes/horizon_init_spec.rb
+++ deployment/puppet/horizon/spec/classes/horizon_init_spec.rb
@@ -1,65 +1,263 @@
 require 'spec_helper'
 
 describe 'horizon' do
+
   let :params do
-    {
-      'cache_server_ip' => '10.0.0.1',
-      'secret_key'      => 'elj1IWiLoWHgcyYxFVLj7cM5rGOOxWl0'
+    { 'secret_key' => 'elj1IWiLoWHgcyYxFVLj7cM5rGOOxWl0',
+      'fqdn'       => '*' }
+  end
+
+  let :pre_condition do
+    'include apache'
+  end
+
+  let :fixtures_path do
+    File.expand_path(File.join(__FILE__, '..', '..', 'fixtures'))
+  end
+
+  let :facts do
+    { :concat_basedir => '/var/lib/puppet/concat',
+      :fqdn           => 'some.host.tld'
     }
   end
 
-  describe 'when running on redhat' do
-    let :facts do
-      {
-        'osfamily' => 'RedHat'
+  shared_examples 'horizon' do
+
+    context 'with default parameters' do
+      it {
+          should contain_package('python-lesscpy').with_ensure('present')
+          should contain_package('horizon').with_ensure('present')
       }
-    end
+      it { should contain_exec('refresh_horizon_django_cache').with({
+          :command     => '/usr/share/openstack-dashboard/manage.py compress',
+          :refreshonly => true,
+      })}
 
-    it {
-      should contain_service('httpd').with_name('httpd')
-    }
-  end
-
-  describe 'when running on debian' do
-    let :facts do
-      {
-        'osfamily' => 'Debian'
-      }
-    end
-
-    it {
-      should contain_service('httpd').with_name('apache2')
-    }
-
-    describe 'with default parameters' do
-      it { should contain_file('/etc/openstack-dashboard/local_settings.py').with_content(/^SECRET_KEY = 'elj1IWiLoWHgcyYxFVLj7cM5rGOOxWl0'$/) }
-      it { should contain_file('/etc/openstack-dashboard/local_settings.py').with_content(/^OPENSTACK_HOST = "127.0.0.1"$/) }
-      it { should contain_file('/etc/openstack-dashboard/local_settings.py').with_content(/^OPENSTACK_KEYSTONE_URL = "http:\/\/%s:5000\/v2.0" % OPENSTACK_HOST$/) }
-      it { should contain_file('/etc/openstack-dashboard/local_settings.py').with_content(/^OPENSTACK_KEYSTONE_DEFAULT_ROLE = "Member"$/) }
-      it { should contain_file('/etc/openstack-dashboard/local_settings.py').with_content(/^DEBUG = False$/) }
-      it { should contain_file('/etc/openstack-dashboard/local_settings.py').with_content(/^API_RESULT_LIMIT = 1000$/) }
-    end
-
-    describe 'when overriding parameters' do
-      let :params do
-        {
-          :secret_key            => 'dummy',
-          :cache_server_ip       => '10.0.0.1',
-          :keystone_host         => 'keystone.example.com',
-          :keystone_port         => 4682,
-          :keystone_scheme       => 'https',
-          :keystone_default_role => 'SwiftOperator',
-          :django_debug          => true,
-          :api_result_limit      => 4682,
-        }
+      it 'configures apache' do
+        should contain_class('horizon::wsgi::apache').with({
+          :servername   => 'some.host.tld',
+          :listen_ssl   => false,
+          :servername   => 'some.host.tld',
+          :extra_params => {},
+        })
       end
 
-      it { should contain_file('/etc/openstack-dashboard/local_settings.py').with_content(/^SECRET_KEY = 'dummy'$/) }
-      it { should contain_file('/etc/openstack-dashboard/local_settings.py').with_content(/^OPENSTACK_HOST = "keystone.example.com"$/) }
-      it { should contain_file('/etc/openstack-dashboard/local_settings.py').with_content(/^OPENSTACK_KEYSTONE_URL = "https:\/\/%s:4682\/v2.0" % OPENSTACK_HOST$/) }
-      it { should contain_file('/etc/openstack-dashboard/local_settings.py').with_content(/^OPENSTACK_KEYSTONE_DEFAULT_ROLE = "SwiftOperator"$/) }
-      it { should contain_file('/etc/openstack-dashboard/local_settings.py').with_content(/^DEBUG = True$/) }
-      it { should contain_file('/etc/openstack-dashboard/local_settings.py').with_content(/^API_RESULT_LIMIT = 4682$/) }
+      it 'generates local_settings.py' do
+        verify_contents(subject, platforms_params[:config_file], [
+          'DEBUG = False',
+          "ALLOWED_HOSTS = ['*', ]",
+          "SECRET_KEY = 'elj1IWiLoWHgcyYxFVLj7cM5rGOOxWl0'",
+          'OPENSTACK_KEYSTONE_URL = "http://127.0.0.1:5000/v2.0"',
+          'OPENSTACK_KEYSTONE_DEFAULT_ROLE = "_member_"',
+          "    'can_set_mount_point': True,",
+          "    'can_set_password': False,",
+          "    'enable_lb': False,",
+          "    'enable_firewall': False,",
+          "    'enable_quotas': True,",
+          "    'enable_security_group': True,",
+          "    'enable_vpn': False,",
+          'API_RESULT_LIMIT = 1000',
+          "LOGIN_URL = '#{platforms_params[:root_url]}/auth/login/'",
+          "LOGOUT_URL = '#{platforms_params[:root_url]}/auth/logout/'",
+          "LOGIN_REDIRECT_URL = '#{platforms_params[:root_url]}'",
+          'COMPRESS_OFFLINE = True',
+          "FILE_UPLOAD_TEMP_DIR = '/tmp'"
+        ])
+      end
     end
+
+    context 'with overridden parameters' do
+      before do
+        params.merge!({
+          :cache_server_ip         => '10.0.0.1',
+          :keystone_default_role   => 'SwiftOperator',
+          :keystone_url            => 'https://keystone.example.com:4682',
+          :openstack_endpoint_type => 'internalURL',
+          :secondary_endpoint_type => 'ANY-VALUE',
+          :django_debug            => true,
+          :api_result_limit        => 4682,
+          :compress_offline        => 'False',
+          :hypervisor_options      => {'can_set_mount_point' => false, 'can_set_password' => true },
+          :neutron_options         => {'enable_lb' => true, 'enable_firewall' => true, 'enable_quotas' => false, 'enable_security_group' => false, 'enable_vpn' => true, 'profile_support' => 'cisco' },
+          :file_upload_temp_dir    => '/var/spool/horizon'
+        })
+      end
+
+      it 'generates local_settings.py' do
+        verify_contents(subject, platforms_params[:config_file], [
+          'DEBUG = True',
+          "ALLOWED_HOSTS = ['*', ]",
+          "SECRET_KEY = 'elj1IWiLoWHgcyYxFVLj7cM5rGOOxWl0'",
+          'OPENSTACK_KEYSTONE_URL = "https://keystone.example.com:4682"',
+          'OPENSTACK_KEYSTONE_DEFAULT_ROLE = "SwiftOperator"',
+          "    'can_set_mount_point': False,",
+          "    'can_set_password': True,",
+          "    'enable_lb': True,",
+          "    'enable_firewall': True,",
+          "    'enable_quotas': False,",
+          "    'enable_security_group': False,",
+          "    'enable_vpn': True,",
+          "    'profile_support': 'cisco',",
+          'OPENSTACK_ENDPOINT_TYPE = "internalURL"',
+          'SECONDARY_ENDPOINT_TYPE = "ANY-VALUE"',
+          'API_RESULT_LIMIT = 4682',
+          'COMPRESS_OFFLINE = False',
+          "FILE_UPLOAD_TEMP_DIR = '/var/spool/horizon'"
+        ])
+      end
+
+      it { should contain_exec('refresh_horizon_django_cache') }
+    end
+
+    context 'with deprecated parameters' do
+      before do
+        params.merge!({
+          :keystone_host       => 'keystone.example.com',
+          :keystone_port       => 4682,
+          :keystone_scheme     => 'https',
+          :can_set_mount_point => true,
+        })
+      end
+
+      it 'generates local_settings.py' do
+        verify_contents(subject, platforms_params[:config_file], [
+          'OPENSTACK_KEYSTONE_URL = "https://keystone.example.com:4682/v2.0"',
+          "    'can_set_mount_point': True,"
+        ])
+      end
+    end
+
+    context 'with vhost_extra_params' do
+      before do
+        params.merge!({
+          :vhost_extra_params   => { 'add_listen' => false },
+        })
+      end
+
+      it 'configures apache' do
+        should contain_class('horizon::wsgi::apache').with({
+          :extra_params => { 'add_listen' => false },
+        })
+      end
+    end
+
+
+    context 'with ssl enabled' do
+      before do
+        params.merge!({
+          :listen_ssl   => true,
+          :servername   => 'some.host.tld',
+          :horizon_cert => '/etc/pki/tls/certs/httpd.crt',
+          :horizon_key  => '/etc/pki/tls/private/httpd.key',
+          :horizon_ca   => '/etc/pki/tls/certs/ca.crt',
+        })
+      end
+
+      it 'configures apache' do
+        should contain_class('horizon::wsgi::apache').with({
+          :bind_address => nil,
+          :listen_ssl   => true,
+          :horizon_cert => '/etc/pki/tls/certs/httpd.crt',
+          :horizon_key  => '/etc/pki/tls/private/httpd.key',
+          :horizon_ca   => '/etc/pki/tls/certs/ca.crt',
+        })
+      end
+    end
+
+    context 'without apache' do
+      before do
+        params.merge!({ :configure_apache => false })
+      end
+
+      it 'does not configure apache' do
+        should_not contain_class('horizon::wsgi::apache')
+      end
+    end
+
+    context 'with available_regions parameter' do
+      before do
+        params.merge!({
+          :available_regions => [
+            ['http://region-1.example.com:5000/v2.0', 'Region-1'],
+            ['http://region-2.example.com:5000/v2.0', 'Region-2']
+          ]
+        })
+      end
+
+      it 'AVAILABLE_REGIONS is configured' do
+        verify_contents(subject, platforms_params[:config_file], [
+          "AVAILABLE_REGIONS = [",
+          "    ('http://region-1.example.com:5000/v2.0', 'Region-1'),",
+          "    ('http://region-2.example.com:5000/v2.0', 'Region-2'),",
+          "]"
+        ])
+      end
+    end
+
+    context 'with overriding local_settings_template' do
+      before do
+        params.merge!({
+          :django_debug            => 'True',
+          :help_url                => 'https://docs.openstack.org',
+          :local_settings_template => fixtures_path + '/override_local_settings.py.erb'
+        })
+      end
+
+      it 'uses the custom local_settings.py template' do
+        verify_contents(subject, platforms_params[:config_file], [
+          '# Custom local_settings.py',
+          'DEBUG = True',
+          "HORIZON_CONFIG = {",
+          "    'dashboards': ('project', 'admin', 'settings',),",
+          "    'default_dashboard': 'project',",
+          "    'user_home': 'openstack_dashboard.views.get_user_home',",
+          "    'ajax_queue_limit': 10,",
+          "    'auto_fade_alerts': {",
+          "        'delay': 3000,",
+          "        'fade_duration': 1500,",
+          "        'types': ['alert-success', 'alert-info']",
+          "    },",
+          "    'help_url': \"https://docs.openstack.org\",",
+          "    'exceptions': {'recoverable': exceptions.RECOVERABLE,",
+          "                   'not_found': exceptions.NOT_FOUND,",
+          "                   'unauthorized': exceptions.UNAUTHORIZED},",
+          "}",
+        ])
+      end
+    end
+  end
+
+  context 'on RedHat platforms' do
+    before do
+      facts.merge!({
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '6.0'
+      })
+    end
+
+    let :platforms_params do
+      { :config_file       => '/etc/openstack-dashboard/local_settings',
+        :package_name      => 'openstack-dashboard',
+        :root_url          => '/dashboard' }
+    end
+
+    it_behaves_like 'horizon'
+  end
+
+  context 'on Debian platforms' do
+    before do
+      facts.merge!({
+        :osfamily               => 'Debian',
+        :operatingsystemrelease => '6.0'
+      })
+    end
+
+    let :platforms_params do
+      { :config_file       => '/etc/openstack-dashboard/local_settings.py',
+        :package_name      => 'openstack-dashboard-apache',
+        :root_url          => '/horizon' }
+    end
+
+    it_behaves_like 'horizon'
   end
 end
