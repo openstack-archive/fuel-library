d3fa659f72c3fb43315952f18173c156fe90ea93
Przemyslaw Kaminski <pkaminski@mirantis.com>
2014-11-03 11:56:02 +0100

    Replace permanent Horizon redirect with temporary one
    Change-Id: Ib92685283e64100351af60f18eea393850665787
    Closes-Bug: #1385133

--- deployment/puppet/horizon/spec/classes/horizon_wsgi_apache_spec.rb
+++ deployment/puppet/horizon/spec/classes/horizon_wsgi_apache_spec.rb
@@ -43,7 +43,7 @@ describe 'horizon::wsgi::apache' do
           'serveraliases'        => '*',
           'docroot'              => '/var/www/',
           'ssl'                  => 'false',
-          'redirectmatch_status' => 'permanent',
+          'redirectmatch_status' => 'temp',
           'redirectmatch_regexp' => '^/$',
           'redirectmatch_dest'   => platforms_params[:root_url],
           'wsgi_script_aliases'  => { platforms_params[:root_url] => '/usr/share/openstack-dashboard/openstack_dashboard/wsgi/django.wsgi' },
