a5f6b3c62aa131fe1e36387cafa180315315d7c4
Oleksiy Molchanov <omolchanov@mirantis.com>
2014-11-14 16:53:08 +0200

    Provide atop and screen to all nodes
    Provide installation and configuration
    of atop and screen to all nodes
    
    By default, make snapshot of system status each 20 seconds and keep
    them for 1 day.
    It shows the occupation of the most critical hardware resources (from
    a performance point of view) on system level, i.e. cpu, memory, disk
    and network. Also:
    * Keep recorded data in binary format in /var/log/atop directory for
      1 days and delete an older ones by a logrotate job.
    * Will additionally consume maximum of 200M-300M of disk space for
      each node.
    * Recorded data will be added to diagnostic logs snapshot as well
      (related patch https://review.openstack.org/#/c/134136/).
    
    DocImpact
    Change-Id: Ib46ad049bc523f120da1ddaf20fb63a7b135153c
    Closes-Bug: 1385122

---
+++ deployment/puppet/osnailyfacter/manifests/atop.pp
@@ -0,0 +1,57 @@
+# == Class: osnailyfacter::atop
+#
+# Allow to install and configure atop.
+#
+# === Parameters
+#
+# [*service_enabled*]
+#   Enable atop service, default to true.
+#
+# [*service_state*]
+#   Start atop service, default to running.
+#
+# [*interval*]
+#   Interval between snapshots, default to 20 seconds.
+#
+# [*logpath*]
+#   Directory were the log will be saved by the service.
+#   Default is /var/log/atop.
+#
+# [*rotate*]
+#   How many days keep binary logs.
+#   Default is 7.
+class osnailyfacter::atop (
+  $service_enabled = true,
+  $service_state   = 'running',
+  $interval        = '20',
+  $logpath         = '/var/log/atop',
+  $rotate          = '7',
+  ) {
+  $conf_file = $osfamily ? {
+    'Debian' => '/etc/default/atop',
+    'RedHat' => '/etc/sysconfig/atop',
+    default  => fail('Unsupported Operating System.'),
+  }
+
+  package { 'atop':
+    ensure => 'installed',
+  } ->
+
+  file { $conf_file:
+    ensure  => present,
+    content => template('osnailyfacter/atop.erb'),
+  } ~>
+
+  service { 'atop':
+    ensure => $service_state,
+    enable => $service_enabled,
+  }
+
+  # This file is used for atop binary log rotations by (ana)cron
+  file { '/etc/logrotate.d/atop_logrotate.conf':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('osnailyfacter/atop_logrotate.conf.erb'),
+  }
+}
