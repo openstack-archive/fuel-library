ae9027caea3457ccdee9c534f96d21892c9dfacc
Alexei Sheplyakov <asheplyakov@mirantis.com>
2014-11-15 16:09:54 +0300

    Use in-tree openvswitch datapath module if the kernel is new enough
    The major part of the openvswitch datapath kernel module has been merged into
    Linux 3.13. In particular both GRE and VXLAN tunnels are supported (which are
    used by neutron). The out-of-tree module is no longer actively developed.
    In particular, version 1.10 and 1.11 do not support kernel newer than 3.8
    (although Debian maintainers made them work with kernels <= 3.11).
    
    Therefore use the out-of-tree module only with old kernels (the ones older
    than 3.13)
    
    Change-Id: Ic0f19bb451cb6a27ba4f17431810301414e711c3
    Related-bug: #1390044
    Related-bug: #1383641

--- deployment/puppet/l23network/manifests/params.pp
+++ deployment/puppet/l23network/manifests/params.pp
@@ -1,4 +1,10 @@
 class l23network::params {
+  if is_float($::kernelmajversion) and 0 + $::kernelmajversion < 3.13 {
+    $need_datapath_module = true
+  } else {
+    $need_datapath_module = false
+  }
+
   case $::osfamily {
     /(?i)debian/: {
       $ovs_service_name   = 'openvswitch-switch'
@@ -6,13 +12,23 @@ class l23network::params {
       $lnx_vlan_tools     = 'vlan'
       $lnx_bond_tools     = 'ifenslave'
       $lnx_ethernet_tools = 'ethtool'
+      $ovs_datapath_package_name = $need_datapath_module ? {
+        true    => 'openvswitch-datapath-lts-saucy-dkms',
+        default => false
+      }
+      $ovs_common_package_name = 'openvswitch-switch'
     }
     /(?i)redhat/: {
-      $ovs_service_name   = 'openvswitch' #'ovs-vswitchd'
+      $ovs_service_name   = 'openvswitch'
       $ovs_status_cmd     = '/etc/init.d/openvswitch status'
       $lnx_vlan_tools     = 'vconfig'
       $lnx_bond_tools     = undef
       $lnx_ethernet_tools = 'ethtool'
+      $ovs_datapath_package_name = $need_datapath_module ? {
+        true    => 'kmod-openvswitch',
+        default => false
+      }
+      $ovs_common_package_name   = 'openvswitch'
     }
     default: {
       fail("Unsupported OS: ${::osfamily}/${::operatingsystem}")
