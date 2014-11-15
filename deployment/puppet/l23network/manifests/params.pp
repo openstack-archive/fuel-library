e397219b1ec965fce36f08851c54db07c9344dfa
Alexei Sheplyakov <asheplyakov@mirantis.com>
2014-11-15 16:09:54 +0300

    ubuntu: use linux-image-lts-trusy (Linux 3.13)
    * deployment/puppet/l23network/manifests/l2.pp:
    - Don't install out-of-tree openvswitch datapath module. The module
      shipped with Linux 3.13 supports both GRE and VXLAN tunneling and
      is good enough for neutron/OpenStack
    
    * deployment/puppet/cobbler/templates/preseed/ubuntu-1204.preseed.erb:
    - Install linux-image-lts-trusty
    
    Change-Id: Ic0f19bb451cb6a27ba4f17431810301414e711c3
    Related-bug: #1390044
    Related-bug: #1383641

--- deployment/puppet/l23network/manifests/params.pp
+++ deployment/puppet/l23network/manifests/params.pp
@@ -1,11 +1,19 @@
 class l23network::params {
   case $::osfamily {
     /(?i)debian/: {
+      if is_float($::kernelmajversion) {
+        $float_kernelmajversion = 0 + $::kernelmajversion  # hack for convert string to float
+      }
       $ovs_service_name   = 'openvswitch-switch'
       $ovs_status_cmd     = '/etc/init.d/openvswitch-switch status'
       $lnx_vlan_tools     = 'vlan'
       $lnx_bond_tools     = 'ifenslave'
       $lnx_ethernet_tools = 'ethtool'
+      $ovs_datapath_package_name = is_float($float_kernelmajversion) and $float_kernelmajversion < 3.13 ? {
+        true    => 'openvswitch-datapath-lts-saucy-dkms',
+        default => undef
+      }
+      $ovs_common_package_name = 'openvswitch-switch'
     }
     /(?i)redhat/: {
       $ovs_service_name   = 'openvswitch' #'ovs-vswitchd'
@@ -13,6 +21,8 @@ class l23network::params {
       $lnx_vlan_tools     = 'vconfig'
       $lnx_bond_tools     = undef
       $lnx_ethernet_tools = 'ethtool'
+      $ovs_datapath_package_name = 'kmod-openvswitch'
+      $ovs_common_package_name   = 'openvswitch'
     }
     default: {
       fail("Unsupported OS: ${::osfamily}/${::operatingsystem}")
