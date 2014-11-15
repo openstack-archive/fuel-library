bace2c53cc91b206b331ca14f8416df8e54ef8c4
Alexei Sheplyakov <asheplyakov@mirantis.com>
2014-11-15 16:09:54 +0300

    Use in-tree openvswitch datapath module if the kernel is new enough
    The major part of the openvswitch datapath kernel module has been merged into
    Linux 3.13. In particular both GRE and VXLAN tunnels are supported (which are
    used by neutron). The out-of-tree module is no longer actively developed.
    In particular, version 1.10 and 1.11 do not support kernel newer than 3.8
    (although Debian maintainers made them work with kernels <= 3.11).
    
    Therefore use the out-of-tree module only with old kernels (the ones older
    than 3.13)
    
    Change-Id: Ic0f19bb451cb6a27ba4f17431810301414e711c3
    Related-bug: #1390044
    Related-bug: #1383641

--- deployment/puppet/l23network/manifests/params.pp
+++ deployment/puppet/l23network/manifests/params.pp
@@ -1,4 +1,10 @@
 class l23network::params {
+  if is_float($::kernelmajversion) {
+    $kernelmajversion_f = 0 + $::kernelmajversion  # just a hack for convert string to float
+  } else {
+    $kernelmajversion_f = -1
+  }
+
   case $::osfamily {
     /(?i)debian/: {
       $ovs_service_name   = 'openvswitch-switch'
@@ -6,13 +12,25 @@ class l23network::params {
       $lnx_vlan_tools     = 'vlan'
       $lnx_bond_tools     = 'ifenslave'
       $lnx_ethernet_tools = 'ethtool'
+      if $kernelmajversion_f > 0 and $kernelmajversion_f < 3.13 {
+        $ovs_datapath_package_name = 'openvswitch-datapath-lts-saucy-dkms'
+      } else {
+        $ovs_datapath_package_name = false
+      }
+      $ovs_common_package_name   = 'openvswitch-switch'
     }
     /(?i)redhat/: {
-      $ovs_service_name   = 'openvswitch' #'ovs-vswitchd'
+      $ovs_service_name   = 'openvswitch'
       $ovs_status_cmd     = '/etc/init.d/openvswitch status'
       $lnx_vlan_tools     = 'vconfig'
       $lnx_bond_tools     = undef
       $lnx_ethernet_tools = 'ethtool'
+      if $kernelmajversion_f > 0 and $kernelmajversion_f < 3.10 {
+        $ovs_datapath_package_name = 'kmod-openvswitch'
+      } else {
+        $ovs_datapath_package_name = false
+      }
+      $ovs_common_package_name   = 'openvswitch'
     }
     default: {
       fail("Unsupported OS: ${::osfamily}/${::operatingsystem}")
