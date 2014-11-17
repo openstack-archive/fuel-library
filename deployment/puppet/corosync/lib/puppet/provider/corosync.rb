6e2c9a9a65ca5887e9979677139feda61bde13d1
Dmitry Ilyin <dilyin@mirantis.com>
2014-11-17 17:49:01 +0300

    Fix idempotency of cs_resource
    * insync? to drop status metadata from checks
    * code cleanup
    * fix rspec for cs_resource type
    
    related-blueprint: pacemaker-improvements
    Related-Bug: 1391599
    Related-Bug: 1390480
    Related-Bug: 1396481
    
    Change-Id: I5410b91ea01fc8c6805de6becdf0800d0d486188
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/corosync/lib/puppet/provider/corosync.rb
+++ deployment/puppet/corosync/lib/puppet/provider/corosync.rb
@@ -1,9 +1,8 @@
-class Puppet::Provider::Corosync < Puppet::Provider
-  require "open3"
-  # Yep, that's right we are parsing XML...FUN! (It really wasn't that bad)
-  require 'rexml/document'
-  #require 'system'
+require 'pp'
+require 'open3'
+require 'rexml/document'
 
+class Puppet::Provider::Corosync < Puppet::Provider
 
   def self.dump_cib
     self.block_until_ready
@@ -68,8 +67,11 @@ class Puppet::Provider::Corosync < Puppet::Provider
 
   def exists?
     self.class.block_until_ready
-    debug(@property_hash.inspect)
-    !(@property_hash[:ensure] == :absent or @property_hash.empty?)
+    Puppet.debug "Call exists? on cs_resource '#{@resource[:name]}'"
+    out = !(@property_hash[:ensure] == :absent or @property_hash.empty?)
+    Puppet.debug "Return: #{out}"
+    Puppet.debug "Current state:\n#{@property_hash.pretty_inspect}" if @property_hash.any?
+    out
   end
 
   def get_scope(type)
