645a72ed62f29f94941e05802daedb83df17513f
Dmitry Ilyin <dilyin@mirantis.com>
2014-11-21 21:09:53 +0300

    Replace wait-for-haproxy-backends with pupprt type
    Implemented a special puppet type to wait until
    haproxy backend is online.
    
    Change-Id: I4eab6af7257270bf3cb2b40a34bcb21c952e8989
    Related-Blueprint: pacemaker-improvements

--- deployment/puppet/openstack/manifests/ha/haproxy.pp
+++ deployment/puppet/openstack/manifests/ha/haproxy.pp
@@ -12,7 +12,6 @@ class openstack::ha::haproxy (
   $ceilometer               = undef,
   $sahara                   = undef,
   $murano                   = undef,
-  $is_primary_controller    = false,
 ) {
 
   Haproxy::Service        { use_include => true }
@@ -37,7 +36,6 @@ class openstack::ha::haproxy (
 
   if $custom_mysql_setup_class == 'galera' {
     class { 'openstack::ha::mysqld':
-      is_primary_controller => $is_primary_controller,
       before_start          => true
       # At least one service must call Openstack::HA::Haproxy_service with
       #  before_start = true or HA proxy wont start because it won't have any
