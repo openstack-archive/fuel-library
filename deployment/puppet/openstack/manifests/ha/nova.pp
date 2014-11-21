539460f99cba534363650d6fa322cb0bb252d39e
Aleksandr Didenko <adidenko@mirantis.com>
2014-11-21 12:45:22 +0200

    Change haproxy health check from TCP to HTTP
    Switch from L4 healtch checks to L7 where possible.
    
    Change-Id: Ie2523d05a6a52cdac08b0d8c70c68642f64bcb8f
    Closes-bug: #1394195

--- deployment/puppet/openstack/manifests/ha/nova.pp
+++ deployment/puppet/openstack/manifests/ha/nova.pp
@@ -9,18 +9,26 @@ class openstack::ha::nova {
   }
 
   openstack::ha::haproxy_service { 'nova-api-2':
-    order           => '050',
-    listen_port     => 8774,
-    public          => true,
-    require_service => 'nova-api',
+    order                  => '050',
+    listen_port            => 8774,
+    public                 => true,
+    require_service        => 'nova-api',
+    haproxy_config_options => {
+        option => ['httpchk', 'httplog','httpclose'],
+    },
+    balancermember_options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3',
   }
 
   openstack::ha::haproxy_service { 'nova-metadata-api':
-    order           => '060',
-    listen_port     => 8775,
-    require_service => 'nova-api',
+    order                  => '060',
+    listen_port            => 8775,
+    require_service        => 'nova-api',
+    haproxy_config_options => {
+        option => ['httpchk', 'httplog','httpclose'],
+    },
+    balancermember_options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3',
   }
-  
+
   openstack::ha::haproxy_service { 'nova-novncproxy':
     order           => '170',
     listen_port     => 6080,
@@ -28,7 +36,6 @@ class openstack::ha::nova {
     internal        => false,
     require_service => 'nova-vncproxy',
   }
-
 
   Openstack::Ha::Haproxy_service<|title == 'keystone-1' or title == 'keystone-2'|> -> Service['nova-api']
 }
