00233178a8197435755ad4b6a090430fa18df3e4
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-11-25 16:05:53 +0400

    Remove all storage containers and use host mounts
    Middle storage containers add complexity and additional
    logic to deployment, upgrades, and downgrades. Removing
    them allows host mounts to function better without
    workarounds with path hacks.
    
    Additionally, most host mounts operated without a middle
    container already. This brings the remaining two (log and
    puppet) into line with the others.
    
    Change-Id: I9db2dcc45210affafe62ebe9509a58829fc62ea6
    Partial-Bug: #1382527

--- deployment/puppet/docker/manifests/init.pp
+++ deployment/puppet/docker/manifests/init.pp
@@ -21,10 +21,8 @@ $dependent_dirs = ["/var/log/docker-logs", "/var/log/docker-logs/remote",
   service {$docker_service:
     enable => true,
     ensure => running,
-    require => Package[$docker_package],
     hasrestart => true,
-    restart => "/etc/init.d/docker restart;/usr/bin/dockerctl start all",
-    start => "/etc/init.d/docker start;/usr/bin/dockerctl start all",
+    require => Package[$docker_package],
   }
   file { "/etc/sysconfig/docker":
     content => template("docker/settings.erb"),
