e70f8af41254bd1a8bf228c62ff8c4a0174f6913
Aleksandr Didenko <adidenko@mirantis.com>
2014-11-26 11:24:44 +0200

    Unit tests script for fuel-library
    Script that will run unit tests (rake spec) for needed modules as
    a part of our CI for fuel-library project. Currently it has a list
    of modules with disabled unit tests, due to large amount of broken
    or missing rspec tests. We'll fix those eventualy and reduce the
    list of modules without unit tests.
    
    Also includes:
      - fixes for swift unit tests broken by our modifications
      - fixes for murano syntax errors
    
    Implements blueprint: fuel-library-rspec-testing
    Partial-bug: #1396510
    
    Change-Id: Id53d8d6f36fdcdf7766e143bbb9b788af299882b
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/swift/spec/spec_helper.rb
+++ deployment/puppet/swift/spec/spec_helper.rb
@@ -2,4 +2,8 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
   c.alias_it_should_behave_like_to :it_configures, 'configures'
+  c.before do
+    # avoid "Only root can execute commands as other users"
+    Puppet.features.stubs(:root? => true)
+  end
 end
