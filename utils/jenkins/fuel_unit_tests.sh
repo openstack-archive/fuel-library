3ab6126d26c1e5291b6569d95c8b20bfb7911532
Aleksandr Didenko <adidenko@mirantis.com>
2014-11-26 11:24:44 +0200

    Unit tests script for fuel-library
    Script that will run unit tests (rake spec) for needed modules as
    a part of our CI for fuel-library project. Currently it has a list
    of modules with disabled unit tests, due to large amount of broken
    or missing rspec tests. We'll fix those eventualy and reduce the
    list of modules without unit tests.
    
    Implements blueprint: fuel-library-rspec-testing
    Partial-bug: #1396510
    
    Change-Id: Id53d8d6f36fdcdf7766e143bbb9b788af299882b
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

---
+++ utils/jenkins/fuel_unit_tests.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Some basic checks
+if ! [ -d "$WORKSPACE" ] ; then
+  echo "ERROR: WORKSPACE not found"
+  exit 1
+fi
+
+if [ -z "$PUPPET_GEM_VERSION" ] ; then
+  export PUPPET_GEM_VERSION='~> 3.4.0'
+fi
+
+# Check for bundle and exit if failed
+bundle --version || exit 1
+
+export GEM_HOME=$WORKSPACE/.bundled_gems
+
+# Function that runs rake spec using bundle
+function rake_spec {
+  MODULE=`basename $(pwd)`
+  echo "Checking module $MODULE"
+
+  if ! [ -f Gemfile ] ; then
+    echo "Gemfile not found. Skipping unit tests."
+    return 0
+  fi
+
+  if ! [ -f Rakefile ] ; then
+    echo "Rakefile not found. Skipping unit tests."
+    return 0
+  fi
+
+  if grep -qx $MODULE $WORKSPACE/utils/jenkins/modules.disable_rspec ; then
+    echo "Unit tests are disabled for $MODULE module"
+    return 0
+  fi
+
+  bundle update
+  bundle exec rake spec SPEC_OPTS='--format documentation'
+  return $?
+}
+
+# Iterate over the changed modules and run unit tests for them
+failed_modules=""
+modules=$(git diff --name-only HEAD~ | grep -o 'deployment/puppet/[^/]*/' | sort -u)
+git diff --name-only HEAD~ &>/dev/null || exit 1
+
+for mod in $modules; do
+  pushd $mod &> /dev/null
+  rake_spec || failed_modules="$failed_modules\n$mod"
+  popd &>/dev/null
+done
+
+if [ -z "$failed_modules" ] ; then
+  echo -e "RSpec Test SUCCEEDED: All modules successfully passed RSpec tests.\n"
+  exit 0
+else
+  echo -e "\nRSpec RSpec tests failed for the following modules:"
+  echo -e "$failed_modules\n"
+  exit 1
+fi
