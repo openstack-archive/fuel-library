271d1657af8d057d40126c65fad03e390faa319e
Dmitry Ilyin <dilyin@mirantis.com>
2014-12-02 20:42:06 +0300

    Sahara HA wrapper
    Change-Id: I58abab2bed9ca31210c046612fa1b362bb108ac6

---
+++ deployment/puppet/sahara_ha/manifests/api.pp
@@ -0,0 +1,35 @@
+class sahara_ha::api inherits sahara::api {
+  $primitive_type  = 'sahara-api'
+  $ocf_script_file = 'sahara_ha/sahara-api.ocf.sh'
+
+  $metadata = {
+    'resource-stickiness' => '1'
+  }
+
+  $operations = {
+    'monitor'  => {
+      'interval' => '20',
+      'timeout'  => '30',
+    },
+    'start'    => {
+      'timeout' => '60',
+    },
+    'stop'     => {
+      'timeout' => '60',
+    },
+  }
+
+  $ms_metadata = {
+    'interleave' => true,
+  }
+
+  pacemaker_wrappers::service { $service_name :
+    primitive_type      => $primitive_type,
+    metadata            => $metadata,
+    complex_type        => 'clone',
+    ms_metadata         => $ms_metadata,
+    operations          => $operations,
+    ocf_script_template => $ocf_script_template,
+  }
+
+}
