9723c507fba68036d86848631730a9dae8d3191c
Igor Yozhikov <iyozhikov@mirantis.com>
2014-12-03 22:27:47 +0300

    Update for proper murano apps names
    * altered murano applications names, they must equals to
    application names from murano-apps package(deb/rpm) during
    import process.
    
    Change-Id: I5b42abb65942cb6aa7768cc0dcefec610cba5d6c
    Closes-Bug: #1398925

--- deployment/puppet/murano/manifests/apps.pp
+++ deployment/puppet/murano/manifests/apps.pp
@@ -1,32 +1,44 @@
 class murano::apps (
-    $primary_controller = true,
+  $primary_controller = true,
   ) {
     package { 'murano-apps':
-      ensure => installed,
-      name   => $::murano::params::murano_apps_package_name,
-    }
-
-    if $primary_controller {
-        murano::application_package { 'io.murano.apps.PostgreSql':
-            package_category => 'Databases',
-        }
-
-        murano::application_package { 'io.murano.apps.apache.Apache':
-            package_category => 'Web',
-        }
-
-        murano::application_package { 'io.murano.apps.apache.Tomcat':
-            package_category => 'Web',
-        }
-
-        murano::application_package { 'io.murano.apps.linux.Telnet':
-            package_category => 'Application Servers',
-        }
-
-        murano::application_package { 'io.murano.windows.ActiveDirectory':
-            package_category => 'Microsoft Services',
-        }
-
-        Package<| title == 'murano-apps'|> -> Murano::Application_package<| mandatory == false |>
-    }
+    ensure => installed,
+    name   => $::murano::params::murano_apps_package_name,
   }
+
+  if $primary_controller {
+    murano::application_package { 'io.murano.databases.PostgreSql':
+    package_category => 'Databases',
+  }
+
+  murano::application_package { 'io.murano.databases.MySql':
+  package_category => 'Databases',
+}
+
+murano::application_package { 'io.murano.databases.SqlDatabase':
+package_category => 'Databases',
+}
+
+murano::application_package { 'io.murano.apps.apache.ApacheHttpServer':
+package_category => 'Web',
+}
+
+murano::application_package { 'io.murano.apps.apache.Tomcat':
+package_category => 'Web',
+}
+
+murano::application_package { 'io.murano.apps.WordPress':
+package_category => 'Application Servers',
+}
+
+murano::application_package { 'io.murano.apps.ZabbixAgent':
+package_category => 'Application Servers',
+}
+
+murano::application_package { 'io.murano.apps.ZabbixServer':
+package_category => 'Application Servers',
+}
+
+Package<| title == 'murano-apps'|> -> Murano::Application_package<| mandatory == false |>
+}
+}
