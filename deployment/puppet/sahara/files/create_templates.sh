d8755e2864da3799a6b993f563098043db4785d8
Sergey Reshetnyak <sreshetniak@mirantis.com>
2014-12-04 14:41:26 +0300

    Fix default Sahara templates for nova-network
    Change-Id: I1a11c48dd47a83686c5abc0a86c578faf5f9acdf
    Closes-bug: #1399145

--- deployment/puppet/sahara/files/create_templates.sh
+++ deployment/puppet/sahara/files/create_templates.sh
@@ -8,10 +8,11 @@ case $network_provider in
    neutron)
      FLOATING_IP_POOL=$(nova net-list | grep -e "net04_ext" | awk '{print $2}')
      PRIVATE_NETWORK_ID=$(nova net-list | grep -e "net04\ " | awk '{print $2}')
+     NEUTRON_MANAGEMENT_NETWORK="\"neutron_management_network\": \"$PRIVATE_NETWORK_ID\""
      ;;
    nova)
      FLOATING_IP_POOL=nova
-     PRIVATE_NETWORK_ID=$(nova net-list | grep -e "novanetwork" | awk '{print $2}')
+     NEUTRON_MANAGEMENT_NETWORK=""
      ;;
    *)
      echo "Undefined network provider. Skip creating Sahara templates."
@@ -30,7 +31,7 @@ case $plugin in
      # create vanilla cluster tempate
      sed -e "s/MASTER_NG_TEMPLATE/$van_master_template_id/g" \
      -e "s/WORKER_NG_TEMPLATE/$van_worker_template_id/g" \
-     -e "s/PRIVATE_NETWORK/$PRIVATE_NETWORK_ID/g" cl_tmpl_vanilla.json > $tmp_file
+     -e "s/NEUTRON_MANAGEMENT_NETWORK/$NEUTRON_MANAGEMENT_NETWORK/g" cl_tmpl_vanilla.json > $tmp_file
      sahara cluster-template-create --json $tmp_file 1>/dev/null
      ;;
    hdp)
@@ -47,7 +48,7 @@ case $plugin in
      sed -e "s/MANAGER_NG_TEMPLATE/$hdp_manager_template_id/g" \
      -e "s/MASTER_NG_TEMPLATE/$hdp_master_template_id/g" \
      -e "s/WORKER_NG_TEMPLATE/$hdp_worker_template_id/g" \
-     -e "s/PRIVATE_NETWORK/$PRIVATE_NETWORK_ID/g" cl_tmpl_hdp.json > $tmp_file
+     -e "s/NEUTRON_MANAGEMENT_NETWORK/$NEUTRON_MANAGEMENT_NETWORK/g" cl_tmpl_hdp.json > $tmp_file
      sahara cluster-template-create --json $tmp_file 1>/dev/null
      ;;
    cdh)
@@ -64,7 +65,7 @@ case $plugin in
      sed -e "s/MANAGER_NG_TEMPLATE/$cdh_manager_template_id/g" \
      -e "s/MASTER_NG_TEMPLATE/$cdh_master_template_id/g" \
      -e "s/WORKER_NG_TEMPLATE/$cdh_worker_template_id/g" \
-     -e "s/PRIVATE_NETWORK/$PRIVATE_NETWORK_ID/g" cl_tmpl_cdh.json > $tmp_file
+     -e "s/NEUTRON_MANAGEMENT_NETWORK/$NEUTRON_MANAGEMENT_NETWORK/g" cl_tmpl_cdh.json > $tmp_file
      sahara cluster-template-create --json $tmp_file 1>/dev/null
      ;;
    *)
