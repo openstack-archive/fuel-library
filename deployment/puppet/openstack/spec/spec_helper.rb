5141ee657cd1a47c1429478c1afa399fbc79cca0
Aleksandr Didenko <adidenko@mirantis.com>
2014-12-08 17:50:49 +0200

    Unit tests for openstack module
    Refactor RSpec tests for openstack module. RSpec tests from the
    original module were not working with all the modifications we
    made. So we need to write new rspecs.
    
    Partial blueprint fuel-library-rspec-testing.
    
    Fuel-CI: disable
    Change-Id: I0ba2fa6810460c32fc859d23222a749c6ace9d6c

--- deployment/puppet/openstack/spec/spec_helper.rb
+++ deployment/puppet/openstack/spec/spec_helper.rb
@@ -1,1 +1,8 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet'
+
+RSpec.configure do |c|
+  c.module_path = File.expand_path(File.join(__FILE__, '..', '..', '..'))
+  puts c.module_path
+end
+
