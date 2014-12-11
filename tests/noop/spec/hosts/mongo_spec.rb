c4e609c36924282977de9c7b38b40a1b43469ff0
Aleksandr Didenko <adidenko@mirantis.com>
2014-12-11 12:26:44 +0200

    Modular noop tests
    We can run noop tests via 'rake spec'. This will allow us to:
    
      - Make sure that catalog compiles and there are no dependency
        cycles in the graph.
      - Use RSpec tests to check that needed puppet resources present
        in the catalog for specific astute.yaml configuration.
    
    In order to test just execute these commands:
      export WORKSPACE=/tmp/fuel_noop_tests
      mkdir -p $WORKSPACE
      ./utils/jenkins/fuel_noop_tests.sh
    
    It iterates over astsute.yaml files and runs rspec tests for puppet
    tasks configured in the astute.yaml for the node.
    
    If you also want to store puppet logs in case of errors, please set
    PUPPET_LOGS_DIR env variable:
      export PUPPET_LOGS_DIR=/tmp/puppet_error_logs
    
    Related-bug: #1402738
    Implement blueprint deployment-dryrun
    
    Fuel CI temporarily disabled since this change does not affect
    MOS deplyoment process, only CI itself.
    
    Fuel-CI: disable
    
    Change-Id: I38b23832d1e8701440aacb300256f513c466c762

---
+++ tests/noop/spec/hosts/mongo_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'mongo.pp'
+
+describe manifest do
+  shared_examples 'puppet catalogue' do
+    it {
+      File.stubs(:exists?).with('/var/lib/astute/mongodb/mongodb.key').returns(true)
+      File.stubs(:exists?).returns(false)
+      should compile
+    }
+  end
+  test_ubuntu_and_centos manifest
+end
+
---
+++ tests/noop/spec/hosts/openstack-controller/openstack-controller_spec.rb
@@ -0,0 +1,103 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'openstack-controller/openstack-controller.pp'
+
+describe manifest do
+  shared_examples 'puppet catalogue' do
+
+    # TODO All this stuff should be moved to shared examples controller* tests.
+
+    settings = Noop.fuel_settings
+    internal_address = Noop.node_hash['internal_address']
+    rabbit_user = settings['rabbit']['user'] || 'nova'
+    use_neutron = settings['quantum'].to_s
+    role = settings['role']
+    rabbit_ha_queues = 'true'
+    primary_controller_nodes = filter_nodes(settings['nodes'],'role','primary-controller')
+    controllers = primary_controller_nodes + filter_nodes(settings['nodes'],'role','controller')
+    controller_internal_addresses = nodes_to_hash(controllers,'name','internal_address')
+    controller_nodes = ipsort(controller_internal_addresses.values)
+    memcached_servers = controller_nodes.map{ |n| n = n + ':11211' }.join(',')
+    horizon_bind_address = internal_address
+    admin_token = settings['keystone']['admin_token']
+    nova_quota = settings['nova_quota']
+
+    # Test that catalog compiles and there are no dependency cycles in the graph
+    it { should compile }
+
+    # Nova config options
+    it 'nova config should have report_interval set to 60' do
+      should contain_nova_config('DEFAULT/report_interval').with(
+        'value' => '60',
+      )
+    end
+    it 'nova config should have service_down_time set to 180' do
+      should contain_nova_config('DEFAULT/service_down_time').with(
+        'value' => '180',
+      )
+    end
+
+    # Keystone
+    it 'should declare keystone class with admin_token' do
+      should contain_class('keystone').with(
+        'admin_token' => admin_token,
+      )
+    end
+    it 'should configure memcache_pool keystone cache backend' do
+      should contain_keystone_config('token/caching').with(:value => 'false')
+      should contain_keystone_config('cache/enabled').with(:value => 'true')
+      should contain_keystone_config('cache/backend').with(:value => 'keystone.cache.memcache_pool')
+      should contain_keystone_config('cache/memcache_servers').with(:value => memcached_servers)
+      should contain_keystone_config('cache/memcache_dead_retry').with(:value => '300')
+      should contain_keystone_config('cache/memcache_socket_timeout').with(:value => '3')
+      should contain_keystone_config('cache/memcache_pool_maxsize').with(:value => '100')
+      should contain_keystone_config('cache/memcache_pool_unused_timeout').with(:value => '60')
+    end
+
+    # Horizon
+    it 'should declare openstack::horizon class' do
+      should contain_class('openstack::horizon').with(
+        'nova_quota'   => nova_quota,
+        'bind_address' => horizon_bind_address,
+      )
+    end
+
+    # Ceilometer
+    if settings['ceilometer']['enabled']
+      it 'should declare openstack::ceilometer class' do
+        should contain_class('openstack::ceilometer').with(
+          'amqp_user'        => rabbit_user,
+          'amqp_password'    => settings['rabbit']['password'],
+          'rabbit_ha_queues' => rabbit_ha_queues,
+          'on_controller'    => 'true',
+        )
+      end
+      if use_neutron == 'true'
+        it 'should configure notification_driver for neutron' do
+          should contain_neutron_config('DEFAULT/notification_driver').with(
+            'value' => 'messaging',
+          )
+        end
+      end
+    end
+
+    # Network
+    if settings['quantum']
+      it 'should declare openstack::network with neutron enabled' do
+        should contain_class('openstack::network').with(
+          'neutron_server' => 'true',
+        )
+      end
+    else
+      it 'should declare openstack::network with neutron disabled' do
+        should contain_class('openstack::network').with(
+          'neutron_server' => 'false',
+        )
+      end
+    end
+
+  end # end of shared_examples
+
+  test_ubuntu_and_centos manifest
+end
+
