7b8106306c0eb08813b2ad85b33d29f0f7c07415
Aleksandr Didenko <adidenko@mirantis.com>
2014-12-11 12:26:44 +0200

    Run noop testing via rake spec
    WORK IN PROGRESS
    
    We can run noop tests via 'rake spec'. This will allow us to:
    
      - make sure that catalog compilation is not broken
      - use RSpec tests to check that needed puppet resources present
        in the compiled catalog for specific astute.yaml configuration.
    
    In order to test just execute these commands:
      export WORKSPACE=/tmp/fuel_noop_tests
      mkdir -p $WORKSPACE
      ./utils/jenkins/fuel_noop_tests.sh
    
    Related-bug: #1402738
    
    Fuel CI temporarily disabled since this change does not affect
    MOS deplyoment process, only CI itself.
    
    Fuel-CI: disable
    Change-Id: I5a3b569e86d8838d5c20b590cc6cc106fb2242a7

---
+++ utils/noop/spec/hosts/ha_neut_vlan_sah_mur_cei.primary-controller_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+require 'yaml'
+require 'shared-examples'
+
+# Load settings from appropriate astute.yaml
+astute_filename = File.basename(__FILE__).gsub(/_spec.rb/, '.yaml')
+astute_file = File.expand_path(File.join(__FILE__, '..', '..', '..', 'astute.yaml', astute_filename))
+settings = YAML.load_file(astute_file)
+node = settings['fqdn']
+
+describe node do
+  # Facts
+  let :facts do
+    {
+      :fqdn                 => node,
+      :processorcount       => '4',
+      :astute_settings_yaml => File.read(astute_file),
+      :memorysize_mb        => '32138.66',
+      :memorysize           => '31.39 GB',
+      :kernel               => 'Linux',
+      :l3_fqdn_hostname     => node,
+      :l3_default_route     => '172.16.1.1',
+      :concat_basedir       => '/tmp/',
+    }
+  end
+
+  # Run tests for controller
+  shared_examples "controller (#{astute_filename})" do
+    internal_address = settings['nodes'].find{|n| n['fqdn'] == node}['internal_address']
+    primary_controller_nodes = filter_nodes(settings['nodes'],'role','primary-controller')
+    controllers = primary_controller_nodes + filter_nodes(settings['nodes'],'role','controller')
+    controller_internal_addresses = nodes_to_hash(controllers,'name','internal_address')
+    controller_nodes = ipsort(controller_internal_addresses.values)
+    memcached_servers = controller_nodes.map{ |n| n = n + ':11211' }.join(',')
+    rabbit_ha_queues = 'true'
+    rabbit_user = settings['rabbit']['user'] || 'nova'
+    use_neutron = 'true'
+
+    it_behaves_like 'controller with keystone', settings['keystone']['admin_token'], memcached_servers
+    it_behaves_like 'controller with horizon', settings['nova_quota'], internal_address
+    it_behaves_like 'primary controller with swift'
+    it_behaves_like 'controller with swift'
+    it_behaves_like 'controller with ceilometer', rabbit_user, settings['rabbit']['password'], rabbit_ha_queues
+    it_behaves_like 'controller with neutron'
+    it_behaves_like 'node with sahara' , settings['sahara']['db_password'],  settings['sahara']['user_password'], use_neutron, rabbit_ha_queues
+    it_behaves_like 'node with murano' , rabbit_user, settings['rabbit']['password'], use_neutron
+  end
+
+  # Ubuntu
+  context 'on Ubuntu platforms' do
+    before do
+      facts.merge!( :osfamily => 'Debian' )
+      facts.merge!( :operatingsystem => 'Ubuntu')
+    end
+    it_behaves_like "controller (#{astute_filename})"
+  end
+
+  # CentOS
+  context 'on CentOS platforms' do
+    before do
+      facts.merge!( :osfamily => 'RedHat' )
+      facts.merge!( :operatingsystem => 'CentOS')
+    end
+    it_behaves_like "controller (#{astute_filename})"
+  end
+
+end
---
+++ utils/noop/spec/hosts/simple_neut_gre_ceph_sah_mur_cei.controller_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+require 'yaml'
+require 'shared-examples'
+
+# Load settings from appropriate astute.yaml
+astute_filename = File.basename(__FILE__).gsub(/_spec.rb/, '.yaml')
+astute_file = File.expand_path(File.join(__FILE__, '..', '..', '..', 'astute.yaml', astute_filename))
+settings = YAML.load_file(astute_file)
+node = settings['fqdn']
+
+describe node do
+  # Facts
+  let :facts do
+    {
+      :fqdn                 => node,
+      :processorcount       => '4',
+      :astute_settings_yaml => File.read(astute_file),
+      :memorysize_mb        => '32138.66',
+      :memorysize           => '31.39 GB',
+      :kernel               => 'Linux',
+      :l3_fqdn_hostname     => node,
+      :l3_default_route     => '172.16.1.1',
+      :concat_basedir       => '/tmp/',
+    }
+  end
+
+  # Run tests for controller
+  shared_examples "controller (#{astute_filename})" do
+    memcached_servers = settings['management_vip'] + ':11211'
+    rabbit_ha_queues = 'false'
+    rabbit_user = settings['rabbit']['user'] || 'nova'
+    use_neutron = 'true'
+
+    it_behaves_like 'controller with keystone', settings['keystone']['admin_token'], memcached_servers
+    it_behaves_like 'controller with horizon', settings['nova_quota'], '0.0.0.0'
+    it_behaves_like 'controller with ceilometer', rabbit_user, settings['rabbit']['password'], rabbit_ha_queues
+    it_behaves_like 'controller with neutron'
+    it_behaves_like 'node with sahara' , settings['sahara']['db_password'],  settings['sahara']['user_password'], use_neutron, rabbit_ha_queues
+    it_behaves_like 'node with murano' , rabbit_user, settings['rabbit']['password'], use_neutron
+  end
+
+  # Ubuntu
+  context 'on Ubuntu platforms' do
+    before do
+      facts.merge!( :osfamily => 'Debian' )
+      facts.merge!( :operatingsystem => 'Ubuntu')
+    end
+    it_behaves_like "controller (#{astute_filename})"
+  end
+
+  # CentOS
+  context 'on CentOS platforms' do
+    before do
+      facts.merge!( :osfamily => 'RedHat' )
+      facts.merge!( :operatingsystem => 'CentOS')
+    end
+    it_behaves_like "controller (#{astute_filename})"
+  end
+
+end
