cae014ebe8ee12a5d6c44dbb0c03d08bd5728306
Aleksandr Didenko <adidenko@mirantis.com>
2014-12-11 12:26:44 +0200

    Modular noop tests
    We can run noop tests via 'rake spec'. This will allow us to:
    
      - Make sure that catalog compiles and there are no dependency
        cycles in the graph.
      - Use RSpec tests to check that needed puppet resources present
        in the catalog for specific astute.yaml configuration.
    
    In order to test just execute these commands:
      export WORKSPACE=/tmp/fuel_noop_tests
      mkdir -p $WORKSPACE
      ./utils/jenkins/fuel_noop_tests.sh
    
    It iterates over astsute.yaml files and runs rspec tests for puppet
    tasks configured in the astute.yaml for the node.
    
    In order to run specific test and/or specific astute.yaml, you can
    set appropriate env variables. For example:
      export NOOP_TEST="keystone/*"
      export NOOP_YAMLS="tests/noop/astute.yaml/novanet_flat.primary-controller.yaml"
      ./utils/jenkins/fuel_noop_tests.sh
    
    If you also want to store puppet logs in case of errors, please set
    PUPPET_LOGS_DIR env variable:
      export PUPPET_LOGS_DIR=/tmp/puppet_error_logs
    
    Related-bug: #1402738
    Implement blueprint deployment-dryrun
    
    Fuel CI temporarily disabled since this change does not affect
    MOS deplyoment process, only CI itself.
    
    Fuel-CI: disable
    
    Change-Id: I38b23832d1e8701440aacb300256f513c466c762

---
+++ tests/noop/spec/shared-examples.rb
@@ -0,0 +1,106 @@
+# Shared functions
+def filter_nodes(hash, name, value)
+  hash.select do |it|
+    it[name] == value
+  end
+end
+
+def nodes_to_hash(hash, name, value)
+  result = {}
+  hash.each do |element|
+    result[element[name]] = element[value]
+  end
+  return result
+end
+
+def ipsort (ips)
+  require 'rubygems'
+  require 'ipaddr'
+  sorted_ips = ips.sort { |a,b| IPAddr.new( a ) <=> IPAddr.new( b ) }
+  return sorted_ips
+end
+
+def test_ubuntu_and_centos(manifest)
+  # check if task is present in the task list
+  unless Noop.manifest_present? manifest
+    # puts "Manifest '#{manifest}' is not enabled on the node '#{Noop.hostname}'. Skipping tests."
+    return
+  end
+
+  # set manifest file
+  before(:all) { Noop.set_manifest manifest }
+
+  shared_examples 'should_compile' do
+    it { should compile }
+  end
+
+  shared_examples 'save_files_list' do
+    it 'should save the list of file resources' do
+      catalog = subject
+      catalog = subject.call if subject.is_a? Proc
+      file_resources = {}
+      catalog.resources.each do |resource|
+        next unless resource.type == 'File'
+        next unless %w(present file).include? resource[:ensure] or not resource[:ensure]
+
+        if resource[:source]
+          content = resource[:source]
+        elsif resource[:content]
+          content = 'TEMPLATE'
+        else
+          content = nil
+        end
+        next unless content
+        file_resources[resource[:path]] = content
+      end
+      if file_resources.any?
+        Noop.save_file_resources_list manifest, file_resources
+      end
+    end
+  end
+
+  shared_examples 'save_packages_list' do
+    it 'should save the list of file resources' do
+      catalog = subject
+      catalog = subject.call if subject.is_a? Proc
+      package_resources = {}
+      catalog.resources.each do |resource|
+        next unless resource.type == 'Package'
+        next if %w(absent purged).include? resource[:ensure] or not resource[:ensure]
+        package_resources[resource[:name]] = resource[:ensure]
+      end
+      if package_resources.any?
+        Noop.save_package_resources_list manifest, package_resources
+      end
+    end
+  end
+
+  #######################################
+  # Testing on different operating systems
+  # Ubuntu
+  context 'on Ubuntu platforms' do
+    let(:facts) { Noop.ubuntu_facts }
+    it_behaves_like 'should_compile'
+    it_behaves_like 'save_files_list'
+    it_behaves_like 'save_packages_list'
+    begin
+      it_behaves_like 'puppet catalogue'
+    rescue ArgumentError
+      true
+    end
+  end
+
+  # CentOS
+  context 'on CentOS platforms' do
+    let(:facts) { Noop.ubuntu_facts }
+    it_behaves_like 'should_compile'
+    it_behaves_like 'save_files_list'
+    it_behaves_like 'save_packages_list'
+    begin
+      it_behaves_like 'puppet catalogue'
+    rescue ArgumentError
+      true
+    end
+  end
+end
+
