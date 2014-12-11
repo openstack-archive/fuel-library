6d8d8a7ea78b7ea49a62f6d4924ce8a2007fc52e
Aleksandr Didenko <adidenko@mirantis.com>
2014-12-11 12:26:44 +0200

    Run noop testing via rake spec
    WORK IN PROGRESS
    
    We can run noop tests via 'rake spec'. This will allow us to:
    
      - make sure that catalog compilation is not broken
      - use RSpec tests to check that needed puppet resources present
        in the compiled catalog for specific astute.yaml configuration.
    
    In order to test just execute this command:
      ./utils/jenkins/fuel_noop_tests.sh
    
    Fuel CI temporarily disabled since this change does not affect
    MOS deplyoment process, only CI itself.
    
    Fuel-CI: disable
    Change-Id: I5a3b569e86d8838d5c20b590cc6cc106fb2242a7

---
+++ utils/noop/spec/shared-examples.rb
@@ -0,0 +1,76 @@
+shared_examples 'controller with keystone' do | admin_token, memcache_servers |
+  it 'should declare keystone class with admin_token' do
+    should contain_class('keystone').with(
+      'admin_token' => admin_token,
+    )
+  end
+
+  it 'should configure memcache_pool keystone cache backend' do
+    should contain_keystone_config('token/caching').with(:value => 'false')
+    should contain_keystone_config('cache/enabled').with(:value => 'true')
+    should contain_keystone_config('cache/backend').with(:value => 'keystone.cache.memcache_pool')
+    should contain_keystone_config('cache/memcache_servers').with(:value => memcache_servers)
+    should contain_keystone_config('cache/memcache_dead_retry').with(:value => '300')
+    should contain_keystone_config('cache/memcache_socket_timeout').with(:value => '3')
+    should contain_keystone_config('cache/memcache_pool_maxsize').with(:value => '100')
+    should contain_keystone_config('cache/memcache_pool_unused_timeout').with(:value => '60')
+  end
+end
+
+shared_examples 'controller with horizon' do | nova_quota, api_bind_address |
+  it 'should declare openstack::horizon class' do
+    should contain_class('openstack::horizon').with(
+      'nova_quota'   => nova_quota,
+      'bind_address' => api_bind_address,
+    )
+  end
+end
+
+shared_examples 'controller with ceilometer' do | amqp_user, amqp_password, rabbit_ha_queues |
+  it 'should declare openstack::ceilometer class' do
+    should contain_class('openstack::ceilometer').with(
+      'amqp_user'        => amqp_user,
+      'amqp_password'    => amqp_password,
+      'rabbit_ha_queues' => rabbit_ha_queues,
+      'on_controller'    => 'true',
+    )
+  end
+end
+
+shared_examples 'controller without neutron' do
+  it 'should declare openstack::network with neutron disabled' do
+    should contain_class('openstack::network').with(
+      'neutron_server' => 'false',
+    )
+  end
+end
+
+shared_examples 'controller with neutron' do
+  it 'should declare openstack::network with neutron enabled' do
+    should contain_class('openstack::network').with(
+      'neutron_server' => 'true',
+    )
+  end
+end
+
+shared_examples 'node with sahara' do | sahara_db_password, sahara_keystone_password, use_neutron, rabbit_ha_queues |
+  it 'should declare sahara class correctly' do
+    should contain_class('sahara').with(
+      'sahara_db_password'       => sahara_db_password,
+      'sahara_keystone_password' => sahara_keystone_password,
+      'use_neutron'              => use_neutron,
+      'rpc_backend'              => 'rabbit',
+      'rabbit_ha_queues'         => rabbit_ha_queues,
+    )
+  end
+end
+
+shared_examples 'node with murano' do | murano_os_rabbit_userid, murano_os_rabbit_passwd, use_neutron |
+  it 'should declare murano class correctly and after openstack::heat' do
+    should contain_class('murano').with(
+      'murano_os_rabbit_userid' => murano_os_rabbit_userid,
+      'murano_os_rabbit_passwd' => murano_os_rabbit_passwd,
+      'use_neutron'             => use_neutron,
+    ).that_requires('Class[openstack::heat]')
+  end
+end
