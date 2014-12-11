71c69645f070893d802e848ab6b5a5ac853d973b
Aleksandr Didenko <adidenko@mirantis.com>
2014-12-11 12:26:44 +0200

    Modular noop tests
    We can run noop tests via 'rake spec'. This will allow us to:
    
      - Make sure that catalog compiles and there are no dependency
        cycles in the graph.
      - Use RSpec tests to check that needed puppet resources present
        in the catalog for specific astute.yaml configuration.
    
    In order to test just execute these commands:
      export WORKSPACE=/tmp/fuel_noop_tests
      mkdir -p $WORKSPACE
      ./utils/jenkins/fuel_noop_tests.sh
    
    It iterates over astsute.yaml files and runs rspec tests for puppet
    tasks configured in the astute.yaml for the node.
    
    In order to run specific test and/or specific astute.yaml, you can
    set appropriate env variables. For example:
      export NOOP_TEST="keystone/*"
      export NOOP_YAMLS="tests/noop/astute.yaml/novanet_flat.primary-controller.yaml"
      ./utils/jenkins/fuel_noop_tests.sh
    
    If you also want to store puppet logs in case of errors, please set
    PUPPET_LOGS_DIR env variable:
      export PUPPET_LOGS_DIR=/tmp/puppet_error_logs
    
    Related-bug: #1402738
    Implement blueprint deployment-dryrun
    
    Fuel CI temporarily disabled since this change does not affect
    MOS deplyoment process, only CI itself.
    
    Fuel-CI: disable
    
    Change-Id: I38b23832d1e8701440aacb300256f513c466c762

---
+++ tests/noop/spec/hosts/roles/mongo_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'roles/mongo.pp'
+
+describe manifest do
+  shared_examples 'puppet catalogue' do
+    it {
+      File.stubs(:exists?).with('/var/lib/astute/mongodb/mongodb.key').returns(true)
+      File.stubs(:exists?).returns(false)
+    }
+  end
+  test_ubuntu_and_centos manifest
+end
+
---
+++ tests/noop/spec/hosts/swift/rebalance_cronjob_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'swift/rebalance_cronjob.pp'
+
+describe manifest do
+  test_ubuntu_and_centos manifest
+end
+
