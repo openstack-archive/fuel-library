09874bbf1b7dafe70a4fa018d31a6e999c065f04
Aleksandr Didenko <adidenko@mirantis.com>
2014-12-11 12:26:44 +0200

    Run noop testing via rake spec
    WORK IN PROGRESS
    
    We can run noop tests via 'rake spec'. This will allow us to:
    
      - Make sure that catalog compiles and there are no dependency
        cycles in the graph.
      - Use RSpec tests to check that needed puppet resources present
        in the catalog for specific astute.yaml configuration.
    
    In order to test just execute these commands:
      export WORKSPACE=/tmp/fuel_noop_tests
      mkdir -p $WORKSPACE
      ./utils/jenkins/fuel_noop_tests.sh
    
    Related-bug: #1402738
    Implement blueprint deployment-dryrun
    
    Fuel CI temporarily disabled since this change does not affect
    MOS deplyoment process, only CI itself.
    
    Fuel-CI: disable
    Change-Id: I5a3b569e86d8838d5c20b590cc6cc106fb2242a7

---
+++ utils/jenkins/fuel_noop_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if ! [ -d "$WORKSPACE" ] ; then
+  echo "ERROR: WORKSPACE not found"
+  exit 1
+fi
+
+if [ -z "$PUPPET_GEM_VERSION" ] ; then
+  export PUPPET_GEM_VERSION='~> 3.4.0'
+fi
+
+# Check for bundle and exit if failed
+bundle --version || exit 1
+
+export GEM_HOME=$WORKSPACE/.bundled_gems
+
+fail=0
+pushd ./utils/noop &>/dev/null
+  bundle update
+  bundle exec rake spec SPEC_OPTS='--format documentation' || fail=1
+popd &>/dev/null
+exit $fail
