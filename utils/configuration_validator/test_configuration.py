4f0ce4bee9900499fd9211634eb40823162ab42c
Dmitry Shulyak <dshulyak@mirantis.com>
2014-12-17 16:21:16 +0200

    Validation for granular deployment configuration
    If we are going to store configuration in fuel-library
    repository we will need to make sure it is valid,
    before it will be loaded to nailgun, so it is required
    to run next test for every patchset
    
    py.test utils/configuration_validator/  --dir deployment/
    
    Probably tasks will be discovered automatically by glob
    
    implements blueprint granular-deployment-based-on-tasks
    
    Change-Id: I6e0ba97ca5de01c1e9b9e57f64ca48a0da7368be

---
+++ utils/configuration_validator/test_configuration.py
@@ -0,0 +1,80 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+import jsonschema
+import networkx as nx
+import pytest
+import yaml
+
+
+TASK_SCHEMA = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'type': 'object',
+    'required': ['type', 'id'],
+    'properties': {
+        'id': {'type': 'string'},
+        'type': {'enum': ['puppet', 'shell', 'role', 'stage'],
+                 'type': 'string'},
+        'parameters': {'type': 'object'},
+        'required_for': {'type': 'array'},
+        'requires': {'type': 'array'}}}
+
+
+TASKS_SCHEMA = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'type': 'array',
+    'items': TASK_SCHEMA}
+
+
+def get_files(base_dir, patterns=('tasks.yaml',)):
+    for root, dirs, files in os.walk(base_dir):
+        for file_name in files:
+            if file_name in patterns:
+                yield os.path.join(root, file_name)
+
+
+@pytest.fixture
+def tasks(request):
+    tasks = []
+    for file_path in get_files(request.config.getoption('dir')):
+        with open(file_path) as f:
+            tasks.extend(yaml.load(f.read()))
+    return tasks
+
+
+def test_schema(tasks):
+    checker = jsonschema.FormatChecker()
+    jsonschema.validate(tasks, TASKS_SCHEMA, format_checker=checker)
+
+
+def test_for_cycles_in_graph(tasks):
+    graph = nx.DiGraph()
+    for task in tasks:
+        graph.add_node(task['id'])
+        if 'required_for' in task:
+            for req in task['required_for']:
+                graph.add_edge(task['id'], req)
+        if 'requires' in task:
+            for req in task['requires']:
+                graph.add_edge(req, task['id'])
+        if 'role' in task:
+            for req in task['role']:
+                graph.add_edge(task['id'], req)
+        if 'stage' in task:
+            graph.add_edge(task['id'], task['stage'])
+    #todo(dshulyak) show where cycle is
+    dag = nx.is_directed_acyclic_graph(graph)
+    assert dag, 'Graph is not acyclic.'
