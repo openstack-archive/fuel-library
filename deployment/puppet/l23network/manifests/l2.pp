b7b98b19c5ac5197988f7a810073b3267afc2a0c
Sergey Vasilenko <svasilenko@mirantis.com>
2014-12-17 19:22:24 +0300

    Install linux-bridge-utils while l23network initialized
    Fuel-CI: disable
    Change-Id: I3d7bf070ef6fc42ef82c14f30780672084db5e71
    Blueprint: refactor-l23-linux-bridges

--- deployment/puppet/l23network/manifests/l2.pp
+++ deployment/puppet/l23network/manifests/l2.pp
@@ -4,23 +4,27 @@
 # Requirements, packages and services.
 #
 class l23network::l2 (
-  $use_ovs   = true,
-  $use_lnxbr = true,
+  $use_ovs       = true,
+  $use_lnx       = true,
+  $install_ovs   = true,
+  $install_brctl = true,
 ){
   include ::l23network::params
 
   if $use_ovs {
-    if $::l23network::params::ovs_datapath_package_name {
-      package { 'openvswitch-datapath':
-        name => $::l23network::params::ovs_datapath_package_name
+    if $install_ovs {
+      if $::l23network::params::ovs_datapath_package_name {
+        package { 'openvswitch-datapath':
+          name => $::l23network::params::ovs_datapath_package_name
+        }
       }
-    }
-    package { 'openvswitch-common':
-      name => $::l23network::params::ovs_common_package_name
-    }
+      package { 'openvswitch-common':
+        name => $::l23network::params::ovs_common_package_name
+      }
 
-    Package<| title=='openvswitch-datapath' |> -> Package['openvswitch-common']
-    Package['openvswitch-common'] ~> Service['openvswitch-service']
+      Package<| title=='openvswitch-datapath' |> -> Package['openvswitch-common']
+      Package['openvswitch-common'] ~> Service['openvswitch-service']
+    }
     service {'openvswitch-service':
       ensure    => running,
       name      => $::l23network::params::ovs_service_name,
@@ -31,6 +35,14 @@ class l23network::l2 (
     Service['openvswitch-service'] -> L23network::L3::Ifconfig<||>
     if !defined(Service['openvswitch-service']) {
       notify{ "Module ${module_name} cannot notify service openvswitch-service on packages update": }
+    }
+  }
+
+  if $use_lnx {
+    if $install_brctl {
+      if !defined(Package["$l23network::params::lnx_bridge_tools"]) {
+        package {"$l23network::params::lnx_bridge_tools": }
+      }
     }
   }
 
