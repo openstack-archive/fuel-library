11928690b1b8c0257b10fe0a5209ec772f5ac579
Dmitry Ilyin <dilyin@mirantis.com>
2014-12-17 21:42:39 +0300

    The first group of separate roles
    * globals - a lot of global variables from
      our previous deployment left for compatibility.
      We should remove this dependency as soon as possible.
    * hiera - configure hiera to use astute.yaml
    * network - setup neutron or nova based network
    * hosts - update /etc/hosts on node
    * firewall - setup firewall rules
    * tools - install several tools that are needed for
      manual debugging and development
    
    * amqp_hosts function to generate the rabbitma host list
    
    Change-Id: I70883a675c96169b130946bb5651600056ed5bad
    Related-Blueprint: fuel-library-modularization
    Fuel-CI: disable

---
+++ deployment/puppet/osnailyfacter/modular/hiera.pp
@@ -0,0 +1,47 @@
+$data_dir            = '/etc/hiera'
+$data_name           = 'astute'
+$astute_data_file    = '/etc/astute.yaml'
+$hiera_main_config   = '/etc/hiera.yaml'
+$hiera_puppet_config = '/etc/puppet/hiera.yaml'
+$hiera_data_file     = "${data_dir}/${data_name}.yaml"
+
+File {
+  owner => 'root',
+  group => 'root',
+  mode  => '0755',
+}
+
+$hiera_config_content = inline_template('
+---
+:backends:
+  - yaml
+
+:hierarchy:
+  - <%= @data_name %>
+
+:yaml:
+  :datadir: <%= @data_dir %>
+')
+
+file { 'hiera_data_dir' :
+  ensure => 'directory',
+  path   => $data_dir,
+}
+
+file { 'hiera_config' :
+  ensure  => 'present',
+  path    => $hiera_main_config,
+  content => $hiera_config_content,
+}
+
+file { 'hiera_data_astute' :
+  ensure => 'symlink',
+  path   => $hiera_data_file,
+  target => $astute_data_file,
+}
+
+file { 'hiera_puppet_config' :
+  ensure => 'symlink',
+  path   => $hiera_puppet_config,
+  target => $hiera_main_config,
+}
