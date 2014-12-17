ba27c20189f39d9174eb8d46630ac6b9bff48891
Sergey Vasilenko <svasilenko@mirantis.com>
2014-12-17 19:55:47 +0300

    Move check-and-creating interface dir and file
    to l23network::init
    
    Fuel-CI: disable
    Change-Id: Ibb0ce63656266e362e2a2a36ffec0f3a2b16e85f
    Blueprint: refactor-l23-linux-bridges

--- deployment/puppet/l23network/manifests/init.pp
+++ deployment/puppet/l23network/manifests/init.pp
@@ -9,12 +9,46 @@ class l23network (
   $install_ovs   = true,
   $install_brctl = true,
 ){
-  class {'l23network::l2':
+
+  include ::l23network::params
+
+  class { 'l23network::l2':
     use_ovs       => $use_ovs,
     use_lnx       => $use_lnx,
     install_ovs   =>  $install_ovs,
     install_brctl =>  $install_brctl,
   }
+
+  if $::l23network::params::interfaces_file {
+    if ! defined(File["${::l23network::params::interfaces_file}"]) {
+      file {"${::l23network::params::interfaces_file}":
+        ensure  => present,
+        content => template('l23network/interfaces.erb'),
+      }
+    }
+    File<| title == "${::l23network::params::interfaces_file}" |> -> File<| title == "${::l23network::params::interfaces_dir}" |>
+  }
+
+  if ! defined(File["${::l23network::params::interfaces_dir}"]) {
+    file {"${::l23network::params::interfaces_dir}":
+      ensure  => directory,
+      owner   => 'root',
+      mode    => '0755',
+      recurse => true,    # for downstream files !!!
+    }
+  }
+  Class['l23network::l2'] -> File<| title == "${::l23network::params::interfaces_dir}" |>
+  Class['l23network::l2'] -> File<| title == "${::l23network::params::interfaces_file}" |>
+
+  # Centos interface up-n-down scripts
+  if $::osfamily =~ /(?i)redhat/ {
+    class{'l23network::l2::centos_upndown_scripts': stage=>$stage }
+    Anchor <| title == 'l23network::l2::centos_upndown_scripts' |> -> File<| title == "${::l23network::params::interfaces_dir}" |>
+  }
+
+  Anchor['l23network::l2::init'] -> Anchor['l23network::init']
+  anchor { 'l23network::init': }
+
 }
 #
 ###
