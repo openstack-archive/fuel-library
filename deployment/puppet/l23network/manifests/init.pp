fed03bf996c0dfa295be6e0bd9848d6ae2119749
Sergey Vasilenko <svasilenko@mirantis.com>
2014-12-17 19:22:24 +0300

    Install linux-bridge-utils while l23network initialized
    Fuel-CI: disable
    Change-Id: I3d7bf070ef6fc42ef82c14f30780672084db5e71
    Blueprint: refactor-l23-linux-bridges

--- deployment/puppet/l23network/manifests/init.pp
+++ deployment/puppet/l23network/manifests/init.pp
@@ -4,12 +4,16 @@
 # Requirements, packages and services.
 #
 class l23network (
-  $use_ovs   = true,
-  $use_lnxbr = true,
+  $use_ovs       = true,
+  $use_lnx       = true,
+  $install_ovs   = true,
+  $install_brctl = true,
 ){
   class {'l23network::l2':
-    use_ovs   => $use_ovs,
-    use_lnxbr => $use_lnxbr,
+    use_ovs       => $use_ovs,
+    use_lnx       => $use_lnx,
+    install_ovs   =>  $install_ovs,
+    install_brctl =>  $install_brctl,
   }
 }
 #
