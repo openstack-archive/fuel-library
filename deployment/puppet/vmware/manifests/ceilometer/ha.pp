eda3c42df67cd7804b9bc58c2cd5ab27a609adf9
Stepan Rogov <srogov@mirantis.com>
2014-12-23 18:27:44 +0300

    Added new role for vcenter compute nodes
    * Added compute-vcenter role and it processing
    * Refactored vmware class for new role and cases
    
    Implements: blueprint run-vcenter-compute-separately
    Change-Id: I2d49a7f3d543e878f33c971969b418535e3a1109

--- deployment/puppet/vmware/manifests/ceilometer/ha.pp
+++ deployment/puppet/vmware/manifests/ceilometer/ha.pp
@@ -1,5 +1,23 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# Variables:
+#   node_fqdn - used in erb template
+
 define vmware::ceilometer::ha (
   $index,
+  $node_fqdn,
   $amqp_port = '5673',
   $ceilometer_config   = '/etc/ceilometer/ceilometer.conf',
   $ceilometer_conf_dir = '/etc/ceilometer/ceilometer-compute.d',
