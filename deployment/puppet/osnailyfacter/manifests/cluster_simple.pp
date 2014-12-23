6dfbf6de692b06bba06d4e01e378a66582cc6ef5
Stepan Rogov <srogov@mirantis.com>
2014-12-23 19:05:42 +0300

    Added new roles for vcenter nodes
    Added compute-vcenter roles and it processing
    
    Change-Id: I443284a786df41a096bf59dd6d913ec437c05977
    Implements: blueprint vmware-dual-hypervisor

--- deployment/puppet/osnailyfacter/manifests/cluster_simple.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_simple.pp
@@ -644,6 +644,20 @@ class osnailyfacter::cluster_simple {
     }
 
     } # COMPUTE ENDS
+    "compute-vcenter" : {
+      class { 'vmware' :
+        vcenter_user            => $vcenter_hash['vc_user'],
+        vcenter_password        => $vcenter_hash['vc_password'],
+        vcenter_host_ip         => $vcenter_hash['host_ip'],
+        vcenter_cluster         => $vcenter_hash['cluster'],
+        vcenter_datastore_regex => $vcenter_hash['datastore_regex'],
+        vlan_interface          => $vcenter_hash['vlan_interface'],
+        vnc_address             => $controller_node_public,
+        use_quantum             => $::use_neutron,
+        ceilometer              => $ceilometer_hash['enabled'],
+        debug                   => $debug,
+      }
+    } # COMPUTE VCENTER ENDS
     "mongo" : {
       if !$ext_mongo {
          class { 'openstack::mongo_secondary':
