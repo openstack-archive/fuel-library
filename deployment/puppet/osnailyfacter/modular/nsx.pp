f8b7463a0372f40ea150d8be61673e9310ffcf49
Dmitry Ilyin <dilyin@mirantis.com>
2014-12-24 15:16:10 +0300

    Stub of other modular roles
    * NOT TO BE MERGED
    * Should be splited into other branches for farther development
    
    Change-Id: Idf593e2d9eb0a6d3757bb3df969e3bf23492d24b
    Related-Blueprint: fuel-library-modularization
    Fuel-CI: disable

---
+++ deployment/puppet/osnailyfacter/modular/nsx.pp
@@ -0,0 +1,9 @@
+import 'globals.pp'
+
+if $use_vmware_nsx {
+  class { 'plugin_neutronnsx':
+    neutron_config     => $neutron_config,
+    neutron_nsx_config => $neutron_nsx_config,
+    roles              => $roles,
+  }
+}
