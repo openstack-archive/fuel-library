78f434e6ad7c3bc8664a5b6bf00006ce1b98e0dd
Stepan Rogov <srogov@mirantis.com>
2014-12-25 14:35:11 +0300

    Added new role for vcenter cinder nodes
    Added cinder-vmdk role and it processing
    
    Change-Id: Ic315ae25a0aa4e1c5fdc5adcacb96f2f5a9090ae
    Implements: blueprint cinder-vmdk-role

--- deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
@@ -1027,6 +1027,59 @@ class osnailyfacter::cluster_ha {
       }
     } # CINDER ENDS
 
+    "cinder-vmdk" : {
+      include keystone::python
+      #FIXME(bogdando) notify services on python-amqp update, if needed
+      package { 'python-amqp':
+        ensure => present
+      }
+      if member($roles, 'controller') or member($roles, 'primary-controller') {
+        $bind_host = $::internal_address
+      } else {
+        $bind_host = false
+      }
+      class { 'openstack::cinder':
+        sql_connection                   => "mysql://cinder:${cinder_hash[db_password]}@${::fuel_settings['management_vip']}/cinder?charset=utf8&read_timeout=60",
+        glance_api_servers               => "${::fuel_settings['management_vip']}:9292",
+        bind_host                        => $bind_host,
+        queue_provider                   => $::queue_provider,
+        amqp_hosts                       => $amqp_hosts,
+        amqp_user                        => $rabbit_hash['user'],
+        amqp_password                    => $rabbit_hash['password'],
+        rabbit_ha_queues                 => $rabbit_ha_queues,
+        volume_group                     => 'cinder',
+        manage_volumes                   => $manage_volumes,
+        iser                             => $storage_hash['iser'],
+        enabled                          => true,
+        auth_host                        => $::fuel_settings['management_vip'],
+        iscsi_bind_host                  => $::storage_address,
+        cinder_user_password             => $cinder_hash[user_password],
+        syslog_log_facility              => $::syslog_log_facility_cinder,
+        debug                            => $::debug,
+        verbose                          => $::verbose,
+        use_syslog                       => $::use_syslog,
+        max_retries                      => $max_retries,
+        max_pool_size                    => $max_pool_size,
+        max_overflow                     => $max_overflow,
+        idle_timeout                     => $idle_timeout,
+        ceilometer                       => $ceilometer_hash[enabled],
+        vmware_host_ip                   => $vcenter_hash['host_ip'],
+        vmware_host_username             => $vcenter_hash['vc_user'],
+        vmware_host_password             => $vcenter_hash['vc_password'],
+        cinder_default_availability_zone => "vCenter",
+      }
+      # FIXME(bogdando) replace service_path and action to systemd, once supported
+      if $::use_monit {
+        monit::process { $cinder_volume_name :
+          ensure        => running,
+          matching      => '/usr/bin/python /usr/bin/cinder-volume',
+          start_command => "${service_path} ${cinder_volume_name} restart",
+          stop_command  => "${service_path} ${cinder_volume_name} stop",
+          pidfile       => false,
+        }
+      }
+    } # CINDER_VMDK END
+
     "ceph-osd" : {
       #Class Ceph is already defined so it will do it's thing.
       notify {"ceph_osd: ${::ceph::osd_devices}": }
--- deployment/puppet/osnailyfacter/manifests/cluster_simple.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_simple.pp
@@ -728,6 +728,60 @@ class osnailyfacter::cluster_simple {
       }
 
     } #CINDER ENDS
+ 
+    "cinder-vmdk" : {
+      include keystone::python
+      #FIXME(bogdando) notify services on python-amqp update, if needed
+      package { 'python-amqp':
+        ensure => present
+      }
+      if member($roles, 'controller') or member($roles, 'primary-controller') {
+        $bind_host = '0.0.0.0'
+      } else {
+        $bind_host = false
+      }
+
+      class { 'openstack::cinder':
+        sql_connection                   => "mysql://cinder:${cinder_hash[db_password]}@${controller_node_address}/cinder?charset=utf8&read_timeout=60",
+        glance_api_servers               => "${controller_node_address}:9292",
+        queue_provider                   => $::queue_provider,
+        amqp_hosts                       => $amqp_hosts,
+        amqp_user                        => $rabbit_hash['user'],
+        amqp_password                    => $rabbit_hash['password'],
+        bind_host                        => $bind_host,
+        volume_group                     => 'cinder',
+        manage_volumes                   => $manage_volumes,
+        iser                             => $storage_hash['iser'],
+        enabled                          => true,
+        auth_host                        => $controller_node_address,
+        iscsi_bind_host                  => $cinder_iscsi_bind_addr,
+        cinder_user_password             => $cinder_hash[user_password],
+        syslog_log_facility              => $::syslog_log_facility_cinder,
+        debug                            => $debug,
+        verbose                          => $verbose,
+        use_syslog                       => $use_syslog,
+        max_retries                      => $max_retries,
+        max_pool_size                    => $max_pool_size,
+        max_overflow                     => $max_overflow,
+        idle_timeout                     => $idle_timeout,
+        ceilometer                       => $ceilometer_hash[enabled],
+        vmware_host_ip                   => $vcenter_hash['host_ip'],
+        vmware_host_username             => $vcenter_hash['vc_user'],
+        vmware_host_password             => $vcenter_hash['vc_password'],
+        cinder_default_availability_zone => "vCenter",
+      }
+    
+      # FIXME(bogdando) replace service_path and action to systemd, once supported
+      if $::use_monit {
+        monit::process { $cinder_volume_name :
+          ensure        => running,
+          matching      => '/usr/bin/python /usr/bin/cinder-volume',
+          start_command => "${service_path} ${cinder_volume_name} restart",
+          stop_command  => "${service_path} ${cinder_volume_name} stop",
+          pidfile       => false,
+        }
+      }
+    } # CINDER_VMDK END
 
     "ceph-osd" : {
       #Nothing needs to be done Class Ceph is already defined
