9a8a6f5bff0f099026dc9d5ae32b19105d999198
Aleksandr Didenko <adidenko@mirantis.com>
2014-12-26 16:25:46 +0200

    Fix unit tests for swift
    The same default fact is being used in the upstream spec_helper.rb
    Checking color output.
    
    Change-Id: I31479ddf5f4074ff556760e9c2328944342570ab

--- deployment/puppet/swift/spec/spec_helper.rb
+++ deployment/puppet/swift/spec/spec_helper.rb
@@ -6,4 +6,6 @@ RSpec.configure do |c|
     # avoid "Only root can execute commands as other users"
     Puppet.features.stubs(:root? => true)
   end
+  c.default_facts = { :concat_basedir => '/var/lib/puppet/concat' }
+  c.tty = true
 end
