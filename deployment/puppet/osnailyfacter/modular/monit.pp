f65c272620104ba9f2c9c83f36bca5623becc754
Bogdan Dobrelya <bdobrelia@mirantis.com>
2015-01-07 14:10:52 +0100

    Separate step for monit configuration
    * Use custom fact pacemaker_hostname in order to exclude the nodes with
    Pacemaker/Corosync. Instead, we could exclude the multirole nodes
    with /controller/ role enabled, but it is not clear how to get *all* the
    node roles from puppet manifests.
    
    Related blueprint fuel-library-modularization
    
    Change-Id: I293004b27b05da3fbe4da043289a4a75b3bbf9b1
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/osnailyfacter/modular/monit.pp
+++ deployment/puppet/osnailyfacter/modular/monit.pp
@@ -1,8 +1,11 @@
 import 'globals.pp'
 
-if $use_monit {
+# NOTE(bogdando) for nodes with Corosync/Pacemaker we delegate
+#   all of the monitor functions to RA instead of monit.
+#   A custom fact 'pacemaker_hostname' will help to determine such cases.
+
+if ($use_monit and $::pacemaker_hostname=='') {
   # Configure service names for monit watchdogs and 'service' system path
-  # FIXME(bogdando) replace service_path to systemd, once supported
   include nova::params
   include cinder::params
   include neutron::params
@@ -12,6 +15,7 @@ if $use_monit {
   $nova_network_name   = $nova::params::network_service_name
   $cinder_volume_name  = $cinder::params::volume_service
   $ovs_vswitchd_name   = $l23network::params::ovs_service_name
+  # FIXME(bogdando) replace service_path to systemd, once supported
   case $::osfamily {
     'RedHat' : {
       $service_path   = '/sbin/service'
@@ -23,12 +27,9 @@ if $use_monit {
       fail("Unsupported osfamily: ${::osfamily} for os ${::operatingsystem}")
     }
   }
-}
 
-if $role == 'compute' {
   # Configure monit watchdogs
-  # FIXME(bogdando) replace service_path and action to systemd, once supported
-  if $use_monit {
+  if $role == 'compute' {
     monit::process { $nova_compute_name :
       ensure        => running,
       matching      => '/usr/bin/python /usr/bin/nova-compute',
@@ -60,75 +61,8 @@ if $role == 'compute' {
       }
     }
   }
-}
 
-if $role == 'controller' {
-  if $::use_monit {
-  # Configure service names for monit watchdogs and 'service' system path
-  # FIXME(bogdando) replace service_path to systemd, once supported
-    include nova::params
-    include cinder::params
-    include neutron::params
-    include l23network::params
-    $nova_compute_name   = $::nova::params::compute_service_name
-    $nova_api_name       = $::nova::params::api_service_name
-    $nova_network_name   = $::nova::params::network_service_name
-    $cinder_volume_name  = $::cinder::params::volume_service
-    $ovs_vswitchd_name   = $::l23network::params::ovs_service_name
-    case $::osfamily {
-      'RedHat' : {
-        $service_path   = '/sbin/service'
-      }
-      'Debian' : {
-        $service_path    = '/usr/sbin/service'
-      }
-      default  : {
-        fail("Unsupported osfamily: ${osfamily} for os ${operatingsystem}")
-      }
-    }
-  }
-}
-
-if $role == 'compute' {
-  # Configure monit watchdogs
-  # FIXME(bogdando) replace service_path and action to systemd, once supported
-  if $use_monit {
-    monit::process { $nova_compute_name :
-      ensure        => running,
-      matching      => '/usr/bin/python /usr/bin/nova-compute',
-      start_command => "${service_path} ${nova_compute_name} restart",
-      stop_command  => "${service_path} ${nova_compute_name} stop",
-      pidfile       => false,
-    }
-    if $use_neutron {
-      monit::process { $ovs_vswitchd_name :
-        ensure        => running,
-        start_command => "${service_path} ${ovs_vswitchd_name} restart",
-        stop_command  => "${service_path} ${ovs_vswitchd_name} stop",
-        pidfile       => '/var/run/openvswitch/ovs-vswitchd.pid',
-      }
-    } else {
-      monit::process { $nova_network_name :
-        ensure        => running,
-        matching      => '/usr/bin/python /usr/bin/nova-network',
-        start_command => "${service_path} ${nova_network_name} restart",
-        stop_command  => "${service_path} ${nova_network_name} stop",
-        pidfile       => false,
-      }
-      monit::process { $nova_api_name :
-        ensure        => running,
-        matching      => '/usr/bin/python /usr/bin/nova-api',
-        start_command => "${service_path} ${nova_api_name} restart",
-        stop_command  => "${service_path} ${nova_api_name} stop",
-        pidfile       => false,
-      }
-    }
-  }
-}
-
-if $role == 'cinder' {
-# FIXME(bogdando) replace service_path and action to systemd, once supported
-  if $use_monit {
+  if $role == 'cinder' {
     monit::process { $cinder_volume_name :
       ensure        => running,
       matching      => '/usr/bin/python /usr/bin/cinder-volume',
@@ -137,4 +71,9 @@ if $role == 'cinder' {
       pidfile       => false,
     }
   }
+
+  # TODO(bogdando) add monit ceph-osd services monitoring, if required
+  # TODO(bogdando) add monit zabbix services monitoring, if required
+  # TODO(bogdando) add monit swift-storage services monitoring, if required.
+  #   We don't deploy swift as a separate role for now, but will do, eventually
 }
