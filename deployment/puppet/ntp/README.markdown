585834a8106f15ddb630e5abcf451a60ac9ac20e
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-01-12 17:12:20 +0300

    Sync puppet ntp module from upstream
    Change-Id: Ib481907044ebd7f4f147ce1a26de5489620904ff
    Related-Bug: 1298360

--- deployment/puppet/ntp/README.markdown
+++ deployment/puppet/ntp/README.markdown
@@ -68,6 +68,17 @@ class { '::ntp':
 }
 ```
 
+###I only want to listen on specific interfaces, not on 0.0.0.0
+
+Restricting this is especially useful on Openstack nodes which may have numerous virtual interfaces.
+
+```puppet
+class { '::ntp':
+  servers  => [ 'ntp1.corp.com', 'ntp2.corp.com' ],
+  interfaces => ['127.0.0.1', '1.2.3.4']
+}
+```
+
 ###I'd like to opt out of having the service controlled; we use another tool for that.
 
 ```puppet
@@ -127,6 +138,14 @@ Disables monitoring of ntp.
 ####`driftfile`
 
 Sets the location of the drift file for ntp.
+
+####`iburst_enable`
+
+Set the iburst option in the ntp configuration. If enabled the option is set for every ntp peer.
+
+####`interfaces`
+
+Sets the list of interfaces NTP will listen on. This parameter must be an array.
 
 ####`keys_controlkey`
 
@@ -199,17 +218,19 @@ status as a virtual machine.
 
 ##Limitations
 
-This module has been built on and tested against Puppet 2.7 and higher.
+This module has been built on and tested against Puppet 3.
 
 The module has been tested on:
 
-* RedHat Enterprise Linux 5/6
+* RedHat Enterprise Linux 5/6/7
+* CentOS 5/6/7
+* Oracle Enterprise Linux 5/6/7
+* Scientific Linux 5/6/7
+* SLES 10SP4/11SP1/12
 * Debian 6/7
-* CentOS 5/6
-* Ubuntu 12.04
-* Gentoo
-* Arch Linux
-* FreeBSD
+* Ubuntu 10.04/12.04/14.04
+* Solaris 11
+* AIX 5.3/6.1/7.1
 
 Testing on other platforms has been light and cannot be guaranteed. 
 
--- deployment/puppet/ntp/spec/spec_helper_acceptance.rb
+++ deployment/puppet/ntp/spec/spec_helper_acceptance.rb
@@ -1,15 +1,29 @@
 require 'beaker-rspec'
 
-UNSUPPORTED_PLATFORMS = [ 'windows', 'Solaris' ]
+UNSUPPORTED_PLATFORMS = ['windows', 'Darwin']
 
 unless ENV['RS_PROVISION'] == 'no' or ENV['BEAKER_provision'] == 'no'
-  if hosts.first.is_pe?
-    install_pe
+  # This will install the latest available package on el and deb based
+  # systems fail on windows and osx, and install via gem on other *nixes
+  foss_opts = {:default_action => 'gem_install'}
+
+  if default.is_pe?; then
+    install_pe;
   else
-    install_puppet
+    install_puppet(foss_opts);
   end
+
   hosts.each do |host|
-    on hosts, "mkdir -p #{host['distmoduledir']}"
+    unless host.is_pe?
+      on host, "/bin/echo '' > #{host['hieraconf']}"
+    end
+    on host, "mkdir -p #{host['distmoduledir']}"
+    if host['platform'] =~ /sles-12/i || host['platform'] =~ /solaris-11/i
+      apply_manifest_on(host, 'package{"git":}')
+      on host, 'git clone -b 4.3.x https://github.com/puppetlabs/puppetlabs-stdlib /etc/puppetlabs/puppet/modules/stdlib'
+    else
+      on host, puppet('module install puppetlabs-stdlib'), {:acceptable_exit_codes => [0, 1]}
+    end
   end
 end
 
@@ -22,11 +36,11 @@ RSpec.configure do |c|
 
   # Configure all nodes in nodeset
   c.before :suite do
-    # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'ntp')
     hosts.each do |host|
-      shell("/bin/touch #{default['puppetpath']}/hiera.yaml")
-      shell('puppet module install puppetlabs-stdlib', :acceptable_exit_codes => [0,1])
+      on host, "mkdir -p #{host['distmoduledir']}/ntp"
+      %w(lib manifests templates metadata.json).each do |file|
+        scp_to host, "#{proj_root}/#{file}", "#{host['distmoduledir']}/ntp"
+      end
     end
   end
 end
