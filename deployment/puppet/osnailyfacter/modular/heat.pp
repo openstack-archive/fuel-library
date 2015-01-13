3803a8bc50ea82545ea3d2af26732ba2c4f56c0b
Dmitry Ilyin <dilyin@mirantis.com>
2015-01-13 18:57:12 +0300

    Separate step for Heat configuration
    Related blueprint fuel-library-modularization
    Fuel-CI: disable
    
    Change-Id: Ia6711c594516992064a4553111949adb90d7aa2c

---
+++ deployment/puppet/osnailyfacter/modular/heat.pp
@@ -0,0 +1,52 @@
+$controller_node_public   = hiera('controller_node_public')
+$controller_node_address  = hiera('controller_node_address')
+$heat_hash                = hiera('heat')
+$amqp_hosts               = hiera('amqp_hosts')
+$rabbit_hash              = hiera('rabbit')
+$max_retries              = hiera('max_retries')
+$max_pool_size            = hiera('max_pool_size')
+$max_overflow             = hiera('max_overflow')
+$idle_timeout             = hiera('$idle_timeout')
+$debug                    = hiera('debug', false)
+$verbose                  = hiera('verbose', true)
+$use_syslog               = hiera('use_syslog', true)
+$syslog_log_facility_heat = hiera('syslog_log_facility_heat')
+$deployment_mode          = hiera('deployment_mode')
+
+#################################################################
+
+class { 'openstack::heat' :
+  external_ip         => $controller_node_public,
+
+  keystone_host       => $controller_node_address,
+  keystone_user       => 'heat',
+  keystone_password   => $heat_hash['user_password'],
+  keystone_tenant     => 'services',
+
+  keystone_ec2_uri    => "http://${controller_node_address}:5000/v2.0",
+
+  rpc_backend         => 'heat.openstack.common.rpc.impl_kombu',
+  amqp_hosts          => [$amqp_hosts],
+  amqp_user           => $rabbit_hash['user'],
+  amqp_password       => $rabbit_hash['password'],
+
+  sql_connection      =>
+    "mysql://heat:${heat_hash['db_password']}@${$controller_node_address}/heat?read_timeout=60",
+  db_host             => $controller_node_address,
+  db_password         => $heat_hash['db_password'],
+  max_retries         => $max_retries,
+  max_pool_size       => $max_pool_size,
+  max_overflow        => $max_overflow,
+  idle_timeout        => $idle_timeout,
+
+  debug               => $debug,
+  verbose             => $verbose,
+  use_syslog          => $use_syslog,
+  syslog_log_facility => $syslog_log_facility_heat,
+
+  auth_encryption_key => $heat_hash['auth_encryption_key'],
+}
+
+if ($deployment_mode == 'ha') or ($deployment_mode == 'ha_compact') {
+  include heat_ha::engine
+}
