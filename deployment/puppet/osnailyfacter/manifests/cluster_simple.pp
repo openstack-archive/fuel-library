80ebf8778e69b9196d513cd75239b6327daafff7
Dmitry Ilyin <dilyin@mirantis.com>
2015-01-13 18:57:12 +0300

    Separate step for Heat configuration
    Related blueprint fuel-library-modularization
    Fuel-CI: disable
    
    Change-Id: Ia6711c594516992064a4553111949adb90d7aa2c

--- deployment/puppet/osnailyfacter/manifests/cluster_simple.pp
+++ deployment/puppet/osnailyfacter/manifests/cluster_simple.pp
@@ -47,12 +47,6 @@ class osnailyfacter::cluster_simple {
     $murano_hash = $::fuel_settings['murano']
   }
 
-  if !$::fuel_settings['heat'] {
-    $heat_hash = {}
-  } else {
-    $heat_hash = $::fuel_settings['heat']
-  }
-
   if !$::fuel_settings['ceilometer'] {
     $ceilometer_hash = {
       enabled => false,
@@ -445,40 +439,6 @@ class osnailyfacter::cluster_simple {
       nova_config {
         'DEFAULT/ram_weight_multiplier':        value => '1.0'
       }
-
-
-      class { 'openstack::heat' :
-        external_ip         => $controller_node_public,
-
-        keystone_host       => $controller_node_address,
-        keystone_user       => 'heat',
-        keystone_password   => $heat_hash['user_password'],
-        keystone_tenant     => 'services',
-
-        keystone_ec2_uri    => "http://${controller_node_address}:5000/v2.0",
-
-        rpc_backend         => 'heat.openstack.common.rpc.impl_kombu',
-        amqp_hosts          => [$amqp_hosts],
-        amqp_user           => $rabbit_hash['user'],
-        amqp_password       => $rabbit_hash['password'],
-
-        sql_connection      =>
-          "mysql://heat:${heat_hash['db_password']}@${$controller_node_address}/heat?read_timeout=60",
-        db_host             => $controller_node_address,
-        db_password         => $heat_hash['db_password'],
-        max_retries         => $max_retries,
-        max_pool_size       => $max_pool_size,
-        max_overflow        => $max_overflow,
-        idle_timeout        => $idle_timeout,
-
-        debug               => $::debug,
-        verbose             => $::verbose,
-        use_syslog          => $::use_syslog,
-        syslog_log_facility => $::syslog_log_facility_heat,
-
-        auth_encryption_key => $heat_hash['auth_encryption_key'],
-      }
-
 
       if $murano_hash['enabled'] {
 
