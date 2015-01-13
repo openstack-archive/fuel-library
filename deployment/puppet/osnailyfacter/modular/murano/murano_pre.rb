0733e5b39f302704f3fde5117f619f0ffb8a63e9
Dmitry Ilyin <dilyin@mirantis.com>
2015-01-13 20:11:31 +0300

    Separate step for Murano configuration
    * Make ia new granular manifest for Murano
      deployment
    
    * Refactor external_network parameter for both
      old and new manifests to get rid of
      fuel_settings dependency
    
    Related blueprint fuel-library-modularization
    
    Change-Id: I25efe73ac9f621cb9e01717f6b7d802c0688dc23

---
+++ deployment/puppet/osnailyfacter/modular/murano/murano_pre.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+
+class MuranoPreTest < Test::Unit::TestCase
+  def test_rabbitmq_available
+    #TODO
+  end
+
+  def test_mysql_available
+    #TODO
+  end
+
+  def test_heat_available
+    #TODO
+  end
+end
