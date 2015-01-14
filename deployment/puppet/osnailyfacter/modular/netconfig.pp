283cc9be96abb8178182ef0009a18eee041ca25e
Dmitry Ilyin <dilyin@mirantis.com>
2015-01-14 19:03:39 +0300

    Make lagacy manifests without other tasks
    Related-Blueprint: fuel-library-modularization
    Fuel-CI: disable
    
    Change-Id: I8dd55e0eda0b734101752d83a38dcf8f8a3e8794

--- deployment/puppet/osnailyfacter/modular/netconfig.pp
+++ deployment/puppet/osnailyfacter/modular/netconfig.pp
@@ -1,6 +1,4 @@
-import 'globals.pp'
-
-if $disable_offload {
+if  hiera('disable_offload') {
   L23network::L3::Ifconfig<||> {
     ethtool => {
       'K' => ['gso off',  'gro off'],
@@ -9,7 +7,7 @@ if $disable_offload {
 }
 
 class { 'l23network' :
-  use_ovs => $use_neutron,
+  use_ovs => hiera('use_neutron'),
 }
 
 class advanced_node_netconfig {
