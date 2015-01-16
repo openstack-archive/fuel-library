ef6e9895348b03f106476818196b1ac5dc5e196e
Sergey Vasilenko <svasilenko@mirantis.com>
2015-01-16 21:27:27 +0300

    move common-purposed methods to parent class
    Blueprint: refactor-l23-linux-bridges
    Fuel-CI: disable
    Change-Id: I9753a9ddf717d9bc2786db5e8a86a18ae6a67c67

--- deployment/puppet/l23network/lib/puppet/provider/l2_port/lnx.rb
+++ deployment/puppet/l23network/lib/puppet/provider/l2_port/lnx.rb
@@ -1,4 +1,6 @@
-Puppet::Type.type(:l2_port).provide(:lnx) do
+require File.join(File.dirname(__FILE__), '..','..','..','puppet/provider/lnx_base')
+
+Puppet::Type.type(:l2_port).provide(:lnx, :parent => Puppet::Provider::Lnx_base) do
   defaultfor :osfamily => :linux
   commands   :iproute => 'ip',
              :brctl   => 'brctl',
@@ -218,86 +220,7 @@ Puppet::Type.type(:l2_port).provide(:lnx) do
     @property_flush[:onboot] = val
   end
 
-  #-----------------------------------------------------------------
-  def self.get_bridge_list
-    bridges = {}
-    # obtain OVS bridges list
-    re_c = /^\s*([\w\-]+)/
-    begin
-      vsctl('list-br').split(/\n+/).select{|l| l.match(re_c)}.collect{|a| $1 if a.match(re_c)}.each do |br_name|
-        br_name.strip!
-        bridges[br_name] = {
-          :br_type => :ovs
-        }
-      end
-    rescue
-      debug("No OVS bridges found, because error while 'ovs-vsctl list-br' execution")
-    end
-    # obtain LNX bridges list
-    re_c = /([\w\-]+)\s+\d+/
-    begin
-      brctl('show').split(/\n+/).select{|l| l.match(re_c)}.collect{|a| $1 if a.match(re_c)}.each do |br_name|
-        br_name.strip!
-        bridges[br_name] = {
-          :br_type => :lnx
-        }
-      end
-    rescue
-      debug("No LNX bridges found, because error while 'brctl show' execution")
-    end
-    return bridges
-  end
 
-  def self.get_ovs_port_bridges_pairs
-    portlist = {}
-    ovs_bridges = vsctl('list-br').split(/\n+/).select{|l| l.match(/^\s*[\w\-]+/)}
-    #todo: handle error
-    ovs_bridges.each do |br_name|
-      br_name.strip!
-      ovs_portlist = vsctl('list-ports', br_name).split(/\n+/).select{|l| l.match(/^\s*[\w\-]+\s*/)}
-      #todo: handle error
-      ovs_portlist.each do |port_name|
-        port_name.strip!
-        portlist[port_name] = {
-          :bridge  => br_name,
-          :br_type => :ovs
-        }
-      end
-      # bridge also a port, but it don't show itself by list-ports
-      portlist[br_name] = {
-        :bridge  => br_name,
-        :br_type => :ovs
-      }
-    end
-    return portlist
-  end
-
-  def self.get_lnx_port_bridges_pairs
-    portlist = {}
-    brctl_show = brctl('show').split(/\n+/).select{|l| l.match(/^[\w\-]+\s+\d+/) or l.match(/^\s+[\w\.\-]+/)}
-    #todo: handle error
-    br_name = nil
-    brctl_show.each do |line|
-      line.rstrip!
-      case line
-      when /^([\w\-]+)\s+[\d\.abcdef]+\s+(yes|no)\s+([\w\-\.]+$)/i
-        br_name = $1
-        port_name = $3
-      when /^\s+([\w\.\-]+)$/
-        #br_name using from previous turn
-        port_name = $1
-      else
-        next
-      end
-      if br_name
-        portlist[port_name] = {
-          :bridge  => br_name,
-          :br_type => :lnx
-        }
-      end
-    end
-    return portlist
-  end
 
 end
 # vim: set ts=2 sw=2 et :
