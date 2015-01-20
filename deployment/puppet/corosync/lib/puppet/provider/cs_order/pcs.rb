da7de4e20877a9c688eb25b95e0bd5b65ae2ae0d
Bogdan Dobrelya <bdobrelia@mirantis.com>
2015-01-20 14:54:48 +0100

    Sync corosync upstream
    * Move Fuel fork for corosync as pacemaker module
    * cs_resource, cs_rsc_location now belong to Pacemaker class and provider
    * Sync puppetlabs-corosync v0.7.0 08c1fd87e57baaa60e57693a15f0329c76603291
    * Add corosync1,2 tempates as is for corosync module
      (TODO merge them to generic upstream corosync templates and contribute)
    
    Fuel-ci: disable
    related blueprint corosync-2
    
    Change-Id: I1f2238e758b93ef28f440fabea2ddea44c6acea7
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/corosync/lib/puppet/provider/cs_order/pcs.rb
+++ deployment/puppet/corosync/lib/puppet/provider/cs_order/pcs.rb
@@ -1,14 +1,16 @@
 require 'pathname'
-require Pathname.new(__FILE__).dirname.dirname.expand_path + 'crmsh'
+require Pathname.new(__FILE__).dirname.dirname.expand_path + 'pacemaker'
 
-Puppet::Type.type(:cs_order).provide(:crm, :parent => Puppet::Provider::Crmsh) do
+Puppet::Type.type(:cs_order).provide(:pcs, :parent => Puppet::Provider::Pacemaker) do
   desc 'Specific provider for a rather specific type since I currently have no plan to
         abstract corosync/pacemaker vs. keepalived. This provider will check the state
         of current primitive start orders on the system; add, delete, or adjust various
         aspects.'
 
+  defaultfor :operatingsystem => [:fedora, :centos, :redhat]
+
   # Path to the crm binary for interacting with the cluster configuration.
-  commands :crm => 'crm'
+  commands :pcs => 'pcs'
 
   def self.instances
 
@@ -16,13 +18,8 @@ Puppet::Type.type(:cs_order).provide(:crm, :parent => Puppet::Provider::Crmsh) d
 
     instances = []
 
-    cmd = [ command(:crm), 'configure', 'show', 'xml' ]
-    if Puppet::PUPPETVERSION.to_f < 3.4
-      raw, status = Puppet::Util::SUIDManager.run_and_capture(cmd)
-    else
-      raw = Puppet::Util::Execution.execute(cmd)
-      status = raw.exitstatus
-    end
+    cmd = [ command(:pcs), 'cluster', 'cib' ]
+    raw, status = run_pcs_command(cmd)
     doc = REXML::Document.new(raw)
 
     doc.root.elements['configuration'].elements['constraints'].each_element('rsc_order') do |e|
@@ -39,22 +36,20 @@ Puppet::Type.type(:cs_order).provide(:crm, :parent => Puppet::Provider::Crmsh) d
       else
         second = items['then']
       end
-
-      if items['symmetrical']
-        symmetrical = (items['symmetrical'] == 'true')
+      if items['score']
+        score = items['score']
       else
-        # Default: symmetrical is true unless explicitly defined.
-        symmetrical = true
+        score = 'INFINITY'
       end
 
       order_instance = {
-        :name           => items['id'],
-        :ensure         => :present,
-        :first          => first,
-        :second         => second,
-        :score          => items['score'],
-        :symmetrical    => symmetrical,
-        :provider       => self.name
+        :name       => items['id'],
+        :ensure     => :present,
+        :first      => first,
+        :second     => second,
+        :score      => score,
+        :provider   => self.name,
+        :new        => false
       }
       instances << new(order_instance)
     end
@@ -65,20 +60,21 @@ Puppet::Type.type(:cs_order).provide(:crm, :parent => Puppet::Provider::Crmsh) d
   # of actually doing the work.
   def create
     @property_hash = {
-      :name         => @resource[:name],
-      :ensure       => :present,
-      :first        => @resource[:first],
-      :second       => @resource[:second],
-      :score        => @resource[:score],
-      :symmetrical  => @resource[:symmetrical],
-      :cib          => @resource[:cib],
+      :name       => @resource[:name],
+      :ensure     => :present,
+      :first      => @resource[:first],
+      :second     => @resource[:second],
+      :score      => @resource[:score],
+      :cib        => @resource[:cib],
+      :new        => true,
     }
   end
 
   # Unlike create we actually immediately delete the item.
   def destroy
-    debug('Revmoving order directive')
-    crm('configure', 'delete', @resource[:name])
+    debug('Removing order directive')
+    cmd=[ command(:pcs), 'constraint', 'remove', @resource[:name]]
+    Puppet::Provider::Pacemaker::run_pcs_command(cmd)
     @property_hash.clear
   end
 
@@ -97,10 +93,6 @@ Puppet::Type.type(:cs_order).provide(:crm, :parent => Puppet::Provider::Crmsh) d
     @property_hash[:score]
   end
 
-  def symmetrical
-    @property_hash[:symmetrical]
-  end
-
   # Our setters for the first and second primitives and score.  Setters are
   # used when the resource already exists so we just update the current value
   # in the property hash and doing this marks it to be flushed.
@@ -116,25 +108,39 @@ Puppet::Type.type(:cs_order).provide(:crm, :parent => Puppet::Provider::Crmsh) d
     @property_hash[:score] = should
   end
 
-  def symmetrical=(should)
-    @property_hash[:symmetrical] = should
-  end
-
   # Flush is triggered on anything that has been detected as being
   # modified in the property_hash.  It generates a temporary file with
   # the updates that need to be made.  The temporary file is then used
-  # as stdin for the crm command.
+  # as stdin for the pcs command.
   def flush
     unless @property_hash.empty?
-      updated = 'order '
-      updated << "#{@property_hash[:name]} #{@property_hash[:score]}: "
-      updated << "#{@property_hash[:first]} #{@property_hash[:second]} symmetrical=#{@property_hash[:symmetrical].to_s}"
-      Tempfile.open('puppet_crm_update') do |tmpfile|
-        tmpfile.write(updated)
-        tmpfile.flush
-        ENV['CIB_shadow'] = @resource[:cib]
-        crm('configure', 'load', 'update', tmpfile.path.to_s)
+      if @property_hash[:new] == false
+        debug('Removing order directive')
+        cmd=[ command(:pcs), 'constraint', 'remove', @resource[:name]]
+        Puppet::Provider::Pacemaker::run_pcs_command(cmd)
       end
+
+      cmd = [ command(:pcs), 'constraint', 'order' ]
+      rsc = @property_hash[:first]
+      if rsc.include? ':'
+        items = rsc.split(':')
+        cmd << items[1]
+        cmd << items[0]
+      else
+        cmd << rsc
+      end
+      cmd << 'then'
+      rsc = @property_hash[:second]
+      if rsc.include? ':'
+        items = rsc.split(':')
+        cmd << items[1]
+        cmd << items[0]
+      else
+        cmd << rsc
+      end
+      cmd << @property_hash[:score]
+      cmd << "id=#{@property_hash[:name]}"
+      raw, status = Puppet::Provider::Pacemaker::run_pcs_command(cmd)
     end
   end
 end
