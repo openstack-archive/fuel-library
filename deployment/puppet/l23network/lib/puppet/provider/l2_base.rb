fb982085ee13ccc74f57e1631b3d5c12700bd06d
Sergey Vasilenko <svasilenko@mirantis.com>
2015-01-20 23:01:13 +0300

    Linux bond resources management
    * bond can be created/removed
    * bond can contains ports (interface_properties)
    * ports can add/remove to the bonds
    * bond can contain and changes bond-properties
    * bond can be a member of bridge
    * bond is a persistent object
    
    + some bugfixex in old logic
    
    Blueprint: refactor-l23-linux-bridges
    Fuel-CI: disable
    Change-Id: I4317abf027311aa8a4ebef5848cb7bf127a60e19

--- deployment/puppet/l23network/lib/puppet/provider/l2_base.rb
+++ deployment/puppet/l23network/lib/puppet/provider/l2_base.rb
@@ -2,6 +2,9 @@
 class Puppet::Provider::L2_base < Puppet::Provider
 
   def self.get_bridge_list
+    # search all (LXN and OVS) bridges on the host, and return hash with mapping
+    # bridge_name => { bridge options }
+    #
     bridges = {}
     # obtain OVS bridges list
     re_c = /^\s*([\w\-]+)/
@@ -31,32 +34,53 @@ class Puppet::Provider::L2_base < Puppet::Provider
   end
 
   def self.get_ovs_port_bridges_pairs
-    portlist = {}
-    ovs_bridges = vsctl('list-br').split(/\n+/).select{|l| l.match(/^\s*[\w\-]+/)}
-    #todo: handle error
+    # returns hash, which map ports to it's bridge.
+    # i.e {
+    #       qg37f65 => { :bridge => 'br-ex',  :br_type => :ovs },
+    #     }
+    #
+    port_mappings = {}
+    begin
+      ovs_bridges = vsctl('list-br').split(/\n+/).select{|l| l.match(/^\s*[\w\-]+/)}
+    rescue
+      debug("No OVS bridges found, because error while 'ovs-vsctl list-br' execution")
+      return {}
+    end
     ovs_bridges.each do |br_name|
       br_name.strip!
       ovs_portlist = vsctl('list-ports', br_name).split(/\n+/).select{|l| l.match(/^\s*[\w\-]+\s*/)}
       #todo: handle error
       ovs_portlist.each do |port_name|
         port_name.strip!
-        portlist[port_name] = {
+        port_mappings[port_name] = {
           :bridge  => br_name,
           :br_type => :ovs
         }
       end
-      # bridge also a port, but it don't show itself by list-ports
-      portlist[br_name] = {
+      # bridge also a port, but it don't show itself by list-ports, adding it manually
+      port_mappings[br_name] = {
         :bridge  => br_name,
         :br_type => :ovs
       }
     end
-    return portlist
+    return port_mappings
   end
 
   def self.get_lnx_port_bridges_pairs
-    portlist = {}
-    brctl_show = brctl('show').split(/\n+/).select{|l| l.match(/^[\w\-]+\s+\d+/) or l.match(/^\s+[\w\.\-]+/)}
+    # returns hash, which map ports to it's bridge.
+    # i.e {
+    #       eth0     => { :bridge => 'br0',    :br_type => :lnx },
+    #     }
+    # This function returns all visible in default namespace ports
+    # (lnx and ovs (with type internal)) included to the lnx bridge
+    #
+    port_mappings = {}
+    begin
+      brctl_show = brctl('show').split(/\n+/).select{|l| l.match(/^[\w\-]+\s+\d+/) or l.match(/^\s+[\w\.\-]+/)}
+    rescue
+      debug("No LNX bridges found, because error while 'brctl show' execution")
+      return {}
+    end
     #todo: handle error
     br_name = nil
     brctl_show.each do |line|
@@ -72,13 +96,103 @@ class Puppet::Provider::L2_base < Puppet::Provider
         next
       end
       if br_name
-        portlist[port_name] = {
+        port_mappings[port_name] = {
           :bridge  => br_name,
           :br_type => :lnx
         }
       end
     end
-    return portlist
+    return port_mappings
+  end
+
+  def self.get_port_bridges_pairs
+    # returns hash, which map ports to it's bridge.
+    # i.e {
+    #       eth0    => { :bridge => 'br0',    :br_type => :lnx },
+    #       qg37f65 => { :bridge => 'br-ex',  :br_type => :ovs },
+    #     }
+    # This function returns all visible in default namespace ports
+    # (lnx and ovs (with type internal)) included to the lnx bridge
+    #
+    # If port included to both bridges (ovs and lnx at one time),
+    # i.e. using as patchcord between bridges -- this port will be
+    # assigned to lnx-type bridge
+    #
+    port_bridges_hash = self.get_ovs_port_bridges_pairs()       # LNX bridges should overwrite OVS
+    port_bridges_hash.merge! self.get_lnx_port_bridges_pairs()  # because by design!
+  end
+
+  # ---------------------------------------------------------------------------
+
+  def self.get_lnx_bonds
+    # search all LXN bonds on the host, and return hash with
+    # bond_name => { bond options }
+    #
+    bond = {}
+    bondlist = File.open("/sys/class/net/bonding_masters").read.chomp.split(/\s+/).sort
+    bondlist.each do |bond_name|
+      #bond_config = IO.readlines("/proc/net/bonding/#{bond_name}")
+      bond[bond_name] = {
+        :mtu             => File.open("/sys/class/net/#{bond_name}/mtu").read.chomp,
+        :slaves           => File.open("/sys/class/net/#{bond_name}/bonding/slaves").read.chomp.split(/\s+/).sort,
+        :bond_properties => {
+          :mode             => File.open("/sys/class/net/#{bond_name}/bonding/mode").read.split(/\s+/)[0],
+          :miimon           => File.open("/sys/class/net/#{bond_name}/bonding/miimon").read.chomp,
+          #:xmit_hash_policy => File.open("/sys/class/net/#{bond_name}/bonding/xmit_hash_policy").read.split(/\s+/)[0],
+          :lacp_rate        => File.open("/sys/class/net/#{bond_name}/bonding/lacp_rate").read.split(/\s+/)[0],
+        }
+      }
+      bond[bond_name][:onboot] = !self.get_iface_state(bond_name).nil?
+    end
+    return bond
+  end
+
+  def self.lnx_bond_allowed_properties
+    {
+      :active_slave      => {},
+      :ad_select         => {},
+      :all_slaves_active => {},
+      :arp_interval      => {},
+      :arp_ip_target     => {},
+      :arp_validate      => {},
+      :arp_all_targets   => {},
+      :downdelay         => {},
+      :fail_over_mac     => {},
+      :lacp_rate         => {:need_reassemble => true},
+      :miimon            => {},
+      :min_links         => {},
+      :mode              => {:need_reassemble => true},
+      :num_grat_arp      => {},
+      :num_unsol_na      => {},
+      :packets_per_slave => {},
+      :primary           => {},
+      :primary_reselect  => {},
+      :tlb_dynamic_lb    => {},
+      :updelay           => {},
+      :use_carrier       => {},
+      :xmit_hash_policy  => {},
+      :resend_igmp       => {},
+      :lp_interval       => {}
+    }
+  end
+  def self.lnx_bond_allowed_properties_list
+    self.lnx_bond_allowed_properties.keys.sort
+  end
+
+
+  def self.get_iface_state(iface)
+    # returns:
+    #    true  -- interface in UP state
+    #    false -- interface in UP statu, but no-carrier
+    #    nil   -- interface in DOWN state
+    begin
+      1 == File.open("/sys/class/net/#{iface}/carrier").read.chomp.to_i
+    rescue
+      # if interface is down, this file can't be read
+      nil
+    end
   end
 end
+
+
 # vim: set ts=2 sw=2 et :
