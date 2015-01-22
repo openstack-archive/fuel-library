897ba845772031d04654f6e9f824d1cb43ceb1da
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-01-22 15:08:29 +0300

    Separate zabbix top-scope role
    * Move zabbix role out of legcay/cluster_ha manifests.
    * Update tasks.yaml accordingly.
    * Update existing classes to work properly with new task.
    
    Change-Id: I50dd06bd7432d7d8ea5fd05ee64c84ee3628a918
    Partial-blueprint: fuel-library-modularization

--- deployment/puppet/zabbix/manifests/init.pp
+++ deployment/puppet/zabbix/manifests/init.pp
@@ -1,8 +1,9 @@
 class zabbix(
       $mysql_server_pkg = $zabbix::params::mysql_server_pkg,
+      $mysql_client_pkg = $zabbix::params::mysql_client_pkg,
   ) inherits zabbix::params {
-  $enabled  = ! empty(get_server_by_role($::fuel_settings['nodes'], 'zabbix-server'))
-  if has_key($::fuel_settings, 'zabbix') and $enabled {
+  $enabled  = ! empty(get_server_by_role(hiera('nodes'), 'zabbix-server'))
+  if $enabled {
     if $::zabbix::params::server {
       Anchor<| title == 'zabbix_server_end' |> -> Anchor<| title == 'zabbix_config_start' |>
       Anchor<| title == 'zabbix_config_end' |> -> Anchor<| title == 'zabbix_monitoring_start' |>
