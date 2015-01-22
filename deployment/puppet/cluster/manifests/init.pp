a1d6e9eace3cb27e42dba79714b1fc908d8f4cd6
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2015-01-22 14:08:41 +0100

    Fixes for corosync 2 support
    * Increase scope for crm_diff
    * Fix typo in cs_rsc_order name
    * Fix rsc_location xml
    * Make corosync_version 1 as default for compatibility reasons
    * Install crmsh and pcs packages as dependency
    * Use ver 1 for /etc/corosync/service.d/pacemaker then Corosync v2
      selected, otherwise use ver 0
    
    related blueprint corosync-2
    
    Change-Id: I7dcdeafdc7c65042eab7edf327c329daac59ef47

--- deployment/puppet/cluster/manifests/init.pp
+++ deployment/puppet/cluster/manifests/init.pp
@@ -10,16 +10,35 @@ class cluster (
     #todo: move half of openstack::corosync
     #to this module, another half -- to Neutron
     if defined(Stage['corosync_setup']) {
+
+      case $::osfamily {
+        'RedHat': {
+          $pcs_package = 'pcs'
+        }
+        'Debian': {
+          $pcs_package = 'python-pcs'
+        }
+         default: {
+          fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem},\
+ module ${module_name} only support osfamily RedHat and Debian")
+        }
+      }
+
       class { 'openstack::corosync':
         bind_address      => $internal_address,
         unicast_addresses => $unicast_addresses,
         stage             => 'corosync_setup',
+        #FIXME(bogdando) use version 2 when Corosync 2.x packages merged
+        #corosync_version  => '2',
+        packages          => ['corosync', 'pacemaker', 'crmsh', $pcs_package],
       }
     } else {
       class { 'openstack::corosync':
         bind_address      => $internal_address,
         unicast_addresses => $unicast_addresses,
-      }
+        #FIXME(bogdando) use version 2 when Corosync 2.x packages merged
+        #corosync_version  => '2',
+        packages          => ['corosync', 'pacemaker', 'crmsh', $pcs_package],      }
     }
     file { 'ocf-fuel-path':
       ensure  => directory,
