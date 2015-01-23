d7e7c51b629cb44d8f84468ca59a6c737c4b6991
Kamil Sambor <ksambor@mirantis.com>
2015-01-23 10:18:48 +0100

    Move generate keys from astute
    implements blueprint granular-deployment-based-on-tasks
    
    Change-Id: I923d9977e66f3a71e9261c90439f1a35fc8e8123

--- utils/configuration_validator/test_configuration.py
+++ utils/configuration_validator/test_configuration.py
@@ -26,7 +26,8 @@ TASK_SCHEMA = {
     'required': ['type', 'id'],
     'properties': {
         'id': {'type': 'string'},
-        'type': {'enum': ['puppet', 'shell', 'group', 'stage'],
+        'type': {'enum': ['puppet', 'shell', 'group', 'stage', 'copy_files',
+                          'sync'],
                  'type': 'string'},
         'parameters': {'type': 'object'},
         'required_for': {'type': 'array'},
