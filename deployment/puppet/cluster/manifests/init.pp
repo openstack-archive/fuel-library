303852a105ffc53227e5d406e9226cc877a332f4
Bogdan Dobrelya <bdobrelia@mirantis.com>
2015-01-26 13:09:16 +0100

    Enable corosync 2
    * Enable corosync version 2 for Openstack
      infrastructure cluster
    
    DocImpact: operations guide changes
    * If Corosync service restarted, pacemaker service should
      be restarted next as well.
    * Corosync 1.x cannot be upgraded to 2.x without full
      cluster downtime. All Pacemaker resources, such as Neutron
      agents, DB and AMQP clusters and others, will be hit by
      downtime as well.
    * All location constraints for cloned Pacemaker resources
      must be removed as a part of Corosync version upgrade
      procedure.
    
    related blueprint corosync-2
    
    Change-Id: I8dadd480e3b32fcdc6743e76187c224bcedffc62
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/cluster/manifests/init.pp
+++ deployment/puppet/cluster/manifests/init.pp
@@ -9,36 +9,35 @@ class cluster (
 
     #todo: move half of openstack::corosync
     #to this module, another half -- to Neutron
-    if defined(Stage['corosync_setup']) {
 
-      case $::osfamily {
-        'RedHat': {
-          $pcs_package = 'pcs'
-        }
-        'Debian': {
-          $pcs_package = 'python-pcs'
-        }
-         default: {
-          fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem},\
- module ${module_name} only support osfamily RedHat and Debian")
-        }
+    case $::osfamily {
+      'RedHat': {
+        $pcs_package = 'pcs'
       }
+      'Debian': {
+        $pcs_package = 'python-pcs'
+      }
+       default: {
+        fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem},\
+ module ${module_name} only support osfamily RedHat and Debian")
+      }
+    }
 
+    if defined(Stage['corosync_setup']) {
       class { 'openstack::corosync':
         bind_address      => $internal_address,
         unicast_addresses => $unicast_addresses,
         stage             => 'corosync_setup',
-        #FIXME(bogdando) use version 2 when Corosync 2.x packages merged
-        #corosync_version  => '2',
+        corosync_version  => '2',
         packages          => ['corosync', 'pacemaker', 'crmsh', $pcs_package],
       }
     } else {
       class { 'openstack::corosync':
         bind_address      => $internal_address,
         unicast_addresses => $unicast_addresses,
-        #FIXME(bogdando) use version 2 when Corosync 2.x packages merged
-        #corosync_version  => '2',
-        packages          => ['corosync', 'pacemaker', 'crmsh', $pcs_package],      }
+        corosync_version  => '2',
+        packages          => ['corosync', 'pacemaker', 'crmsh', $pcs_package],
+      }
     }
     file { 'ocf-fuel-path':
       ensure  => directory,
