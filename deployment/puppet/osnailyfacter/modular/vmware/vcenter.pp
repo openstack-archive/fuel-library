161f3f221d4a0393492f6ff6023c36ae5c703800
Stepan Rogov <srogov@mirantis.com>
2015-01-26 18:59:54 +0300

    Refactored vmware class for multi HV
    * Refactored vmware class for multi hv support
    * Older functionality has beeh saved because UI part is not completed
    * Added new erb's templates
    * Added new resources for multi hv support
    * Added "if" cases for supporting new and old features
    * Added corresponding tasks
    * Removed simple mode support
    
    Change-Id: I181caf3dc76f5a511461067322fc6129ef47f59f
    Implements: blueprint vmware-dual-hypervisor

---
+++ deployment/puppet/osnailyfacter/modular/vmware/vcenter.pp
@@ -0,0 +1,48 @@
+#<task>
+#- id: vmware-vcenter
+#  type: puppet
+#  groups: [primary-controller, controller]
+#  required_for: [deploy]
+#  requires: [hiera, globals, netconfig, firewall]
+#  parameters:
+#  puppet_manifest: /etc/puppet/modules/osnailyfacter/modular/vmware/vcenter.pp
+#  puppet_modules: /etc/puppet/modules
+#  timeout: 3600
+#</task>
+# requires: controller?
+$libvirt_type = hiera('libvirt_type')
+$use_vcenter = hiera('use_vcenter', false)
+$vcenter_hash = hiera('vcenter_hash')
+$controller_node_public = hiera('controller_node_public')
+$use_neutron = hiera('use_neutron')
+$ceilometer_hash = hiera('ceilometer',{})
+$debug = hiera('debug', false)
+
+# vCenter integration
+if hiera('libvirt_type') == 'vcenter' {
+  class { 'vmware' :
+    vcenter_user            => $vcenter_hash['vc_user'],
+    vcenter_password        => $vcenter_hash['vc_password'],
+    vcenter_host_ip         => $vcenter_hash['host_ip'],
+    vcenter_cluster         => $vcenter_hash['cluster'],
+    vcenter_datastore_regex => $vcenter_hash['datastore_regex'],
+    vlan_interface          => $vcenter_hash['vlan_interface'],
+    use_quantum             => $use_neutron,
+    vnc_address             => $controller_node_public,
+    ceilometer              => $ceilometer_hash['enabled'],
+    debug                   => $debug,
+  }
+}
+# Fixme! This a temporary workaround to keep existing functioanality.
+# After fully implementation of the multi HV support it is need to delete
+# previos if statement
+if $use_vcenter {
+  class { 'vmware' :
+    vcenter_settings        => $vcenter_hash['computes'],
+    vlan_interface          => $vcenter_hash['vlan_interface'],
+    use_quantum             => $use_neutron,
+    vnc_address             => $controller_node_public,
+    ceilometer              => $ceilometer_hash['enabled'],
+    debug                   => $debug,
+  }
+}
