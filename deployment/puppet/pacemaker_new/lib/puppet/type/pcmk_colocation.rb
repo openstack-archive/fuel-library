d8edbf9b754040ff993598d57c159a20d9d73afa
Dmitry Ilyin <dilyin@mirantis.com>
2015-01-27 21:32:01 +0300

    Updated pacemaker providers
    * Updated providers with all fixes backported
    * Updated specs for pacemaker
    
    Change-Id: I3770c6beba9c35cf22659c0fa0d9627753e2e48a
    Fuel-CI: disable
    Related-Blueprint: pacemaker-improvements

---
+++ deployment/puppet/pacemaker_new/lib/puppet/type/pcmk_colocation.rb
@@ -0,0 +1,79 @@
+module Puppet
+  newtype(:pcmk_colocation) do
+    desc %q(Type for manipulating corosync/pacemaker colocation.  Colocation
+      is the grouping together of a set of primitives so that they travel
+      together when one of them fails.  For instance, if a web server vhost
+      is colocated with a specific ip address and the web server software
+      crashes, the ip address with migrate to the new host with the vhost.
+
+      More information on Corosync/Pacemaker colocation can be found here:
+
+      * http://www.clusterlabs.org/doc/en-US/Pacemaker/1.1/html/Clusters_from_Scratch/_ensuring_resources_run_on_the_same_host.html)
+
+    ensurable
+
+    newparam(:name) do
+      desc %q(Identifier of the colocation entry. This value needs to be unique
+        across the entire Corosync/Pacemaker configuration since it doesn't have
+        the concept of name spaces per type.)
+      isnamevar
+    end
+
+    newproperty(:primitives, :array_matching => :all) do
+      desc %q(Two Corosync primitives to be grouped together. Colocation groups
+        come in twos.  Property will raise an error if
+        you do not provide a two value array.)
+    end
+
+    newparam(:cib) do
+      desc %q(Corosync applies its configuration immediately. Using a CIB allows
+        you to group multiple primitives and relationships to be applied at
+        once. This can be necessary to insert complex configurations into
+        Corosync correctly.
+
+        This paramater sets the CIB this colocation should be created in. A
+        cs_shadow resource with a title of the same name as this value should
+        also be added to your manifest.)
+    end
+
+    newproperty(:score) do
+      desc %q(The priority of this colocation.  Primitives can be a part of
+        multiple colocation groups and so there is a way to control which
+        primitives get priority when forcing the move of other primitives.
+        This value can be an integer but is often defined as the string
+        INFINITY.)
+
+      defaultto 'INFINITY'
+
+      validate do |value|
+        break if %w(inf INFINITY -inf -INFINITY).include? value
+        break if value.to_i.to_s == value
+        fail 'Score parameter is invalid, should be +/- INFINITY(or inf) or Integer'
+      end
+
+      munge do |value|
+        value.gsub 'inf', 'INFINITY'
+      end
+
+      isrequired
+    end
+
+    autorequire(:pcmk_shadow) do
+      [self[:cib]] if self[:cib]
+    end
+
+    autorequire(:service) do
+      ['corosync']
+    end
+
+    autorequire(:pcmk_resource) do
+      self[:primitives] if self[:primitives].is_a? Array
+    end
+
+    validate do
+      break if self[:ensure] == :absent
+      fail 'The primitives property must be a two value array' unless self[:primitives].is_a? Array and self[:primitives].size == 2
+    end
+
+  end
+end
---
+++ deployment/puppet/pacemaker_new/spec/unit/puppet/provider/pcmk_colocation/ruby_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:pcmk_colocation).provider(:ruby) do
+
+  let(:resource) { Puppet::Type.type(:pcmk_colocation).new(
+      :name => 'my_colocation',
+      :provider => :ruby,
+      :primitives => %w(foo bar)
+  ) }
+  let(:provider) do
+    provider = resource.provider
+    #class << provider
+    #  def debug(msg)
+    #    puts msg
+    #  end
+    #
+    #  def cibadmin_apply_patch(xml)
+    #    debug "Apply XML patch:\n#{xml}"
+    #  end
+    #
+    #  def cibadmin_remove(xml)
+    #    debug "Remove XML section:\n#{xml}"
+    #  end
+    #end
+    provider.stubs(:cluster_debug_report).returns(true)
+    provider
+  end
+
+  describe '#create' do
+    it 'should create colocation with corresponding members' do
+      resource[:primitives] = %w(p_1 p_2)
+      resource[:score] = 'inf'
+      provider.expects(:cibadmin_apply_patch).with(<<-eos
+<diff>
+  <diff-added>
+    <cib>
+      <configuration>
+        <constraints>
+          <rsc_colocation id='my_colocation' rsc='p_1' score='INFINITY' with-rsc='p_2'/>
+        </constraints>
+      </configuration>
+    </cib>
+  </diff-added>
+</diff>
+      eos
+      )
+      provider.create
+      provider.flush
+    end
+  end
+
+  describe '#destroy' do
+    it 'should destroy colocation with corresponding name' do
+      provider.expects(:cibadmin_remove).with("<rsc_colocation id='my_colocation'/>")
+      provider.destroy
+      provider.flush
+    end
+  end
+
+
+end
+
