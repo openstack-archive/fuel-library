d87cfe6696d98cb9d192816ca6eaba9053b5d27a
Sergey Vasilenko <svasilenko@mirantis.com>
2015-01-27 20:13:04 +0300

    FIX prefetch functionality for l2_port resources
    * Now puppet can determine OVS, LNX and LNX-inside-ovs interfaces.
    * OVS interfaces are prefetch by his own provider
    * LNX provider skip OVS resources if it possible
    
    Blueprint: refactor-l23-linux-bridges
    Fuel-CI: disable
    Change-Id: I7508b6fa4d88d067d3f47ec191358e1fddfc5aad

--- deployment/puppet/l23network/lib/puppet/provider/l2_base.rb
+++ deployment/puppet/l23network/lib/puppet/provider/l2_base.rb
@@ -1,6 +1,286 @@
 
 class Puppet::Provider::L2_base < Puppet::Provider
 
+  def self.prefetch(resources)
+    interfaces = instances
+    resources.keys.each do |name|
+      if provider = interfaces.find{ |ii| ii.name == name }
+        resources[name].provider = provider
+      end
+    end
+  end
+
+  # ---------------------------------------------------------------------------
+
+  def self.get_lnx_vlan_interfaces
+    # returns hash, that contains ports (interfaces) configuration.
+    # i.e {
+    #       eth0.101 => { :vlan_dev => 'eth0',  :vlan_id => 101, vlan_mode => 'eth' },
+    #       vlan102  => { :vlan_dev => 'eth0',  :vlan_id => 102, vlan_mode => 'vlan' },
+    #     }
+    #
+    vlan_ifaces = {}
+    rc_c = /([\w+\.\-]+)\s*\|\s*(\d+)\s*\|\s*([\w+\-]+)/
+    File.open("/proc/net/vlan/config", "r").each do |line|
+      if (rv=line.match(rc_c))
+        vlan_ifaces[rv[1]] = {
+          :vlan_dev  => rv[3],
+          :vlan_id   => rv[2],
+          :vlan_mode => (rv[1].match('\.').nil?  ?  'vlan'  :  'eth'  )
+        }
+      end
+    end
+    return vlan_ifaces
+  end
+
+  def self.get_lnx_ports
+    # returns hash, that contains ports (interfaces) configuration.
+    # i.e {
+    #       eth0 => { :mtu => 1500,  :if_type => :ethernet, port_type => lnx:eth:unremovable },
+    #     }
+    #
+    # 'unremovable' flag for port_type means, that this port is a more complicated thing,
+    # than just a port and can't be removed just as port. For example you can't remove bond
+    #  as port. You should remove it as bond.
+    #
+    port = {}
+    #
+    # parse 802.1q vlan interfaces from /proc
+    vlan_ifaces = self.get_lnx_vlan_interfaces()
+    # Fetch information about interfaces, visible in network namespace from /sys/class/net
+    interfaces = Dir['/sys/class/net/*'].select{ |f| File.symlink? f}
+    interfaces.each do |if_dir|
+      if_name = if_dir.split('/')[-1]
+      port[if_name] = {
+        :name      => if_name,
+        :port_type => [],
+        :onboot    => self.get_iface_state(if_name),
+        :mtu       => File.open("#{if_dir}/mtu").read.chomp,
+        :provider  => (if_name == 'ovs-system')  ?  'ovs'  :  'lnx' ,
+      }
+      # determine port_type for this iface
+      if File.directory? "#{if_dir}/bonding"
+        # This interface is a baster of bond, get bonding properties
+        port[if_name][:slaves] = File.open("#{if_dir}/bonding/slaves").read.chomp.strip.split(/\s+/).sort
+        port[if_name][:port_type] << 'bond' << 'unremovable'
+      elsif File.directory? "#{if_dir}/bridge" and File.directory? "#{if_dir}/brif"
+        # this interface is a bridge, get bridge properties
+        port[if_name][:slaves] = Dir["#{if_dir}/brif/*"].map{|f| f.split('/')[-1]}.sort
+        port[if_name][:port_type] << 'bridge' << 'unremovable'
+      else
+        #pass
+      end
+      # Check, whether this interface is a slave of anything
+      if File.symlink?("#{if_dir}/master")
+        port[if_name][:has_master] = File.readlink("#{if_dir}/master").split('/')[-1]
+      end
+      # Check, whether this interface is a subinterface
+      if vlan_ifaces.has_key? if_name
+        # this interface is a 802.1q subinterface
+        port[if_name].merge! vlan_ifaces[if_name]
+        port[if_name][:port_type] << 'vlan'
+      end
+    end
+    # Check, whether port is a slave of anything another
+    port.keys.each do |p_name|
+      if port[p_name].has_key? :has_master
+        master = port[p_name][:has_master]
+        #debug("m='#{master}', name='#{p_name}', props=#{port[p_name]}")
+        master_flags = port[master][:port_type]
+        if master_flags.include? 'bond'
+          # this port is a bond_member
+          port[p_name][:bond_master] = master
+          port[p_name][:port_type] << 'bond-slave'
+        elsif master_flags.include? 'bridge'
+          # this port is a member of bridge
+          port[p_name][:bridge] = master
+          port[p_name][:port_type] << 'bridge-slave'
+        elsif master == 'ovs-system'
+          port[p_name][:port_type] << 'ovs-affected'
+        else
+          #pass
+        end
+        port[p_name].delete(:has_master)
+      end
+    end
+    return port
+  end
+
+  # ---------------------------------------------------------------------------
+  def self.ovs_parse_opthash(hh)
+    #if !(hh=~/^['"]/ and hh=~/['"]$/)
+    rv = {}
+    if hh =~ /^\{(.*)\}$/
+      $1.split(/\s*\,\s*/).each do |pair|
+        k,v = pair.split('=')
+        rv[k.tr("'\"",'').to_sym] = v.nil?  ?  nil  :  v.tr("'\"",'')
+      end
+    end
+    return rv
+  end
+
+  def self.get_ovs_ports
+    # return OVS interfaces hash if it possible
+    begin
+      vsctl_list_ports = vsctl('list', 'Port').split("\n")
+      vsctl_list_ports << :EOF  # last section of output should be processsed anyway.
+    rescue
+      debug("Can't find OVS ports, because error while 'ovs-vsctl list Port' execution")
+      return {}
+    end
+    #
+    buff = {}
+    rv = {}
+    # parse ovs-vsctl output and find OVS and OVS-affected interfaces
+    vsctl_list_ports.each do |line|
+      if line =~ /(\w+)\s*\:\s*(.*)\s*$/
+        key = $1.tr("'\"",'')
+        val = $2.tr("'\"",'')
+        buff[key] = val == '[]'  ?  ''  :  val
+      elsif line =~ /^\s*$/ or line == :EOF
+        rv[buff['name']] = {}
+        rv[buff['name']][:vlan_id] = buff['tag'] if ! buff['tag'].empty?
+        debug("Found OVS port '#{buff['name']}' with properties: #{rv[buff['name']]}")
+        buff = {}
+      else
+        debug("Output of 'ovs-vsctl list Port' contain misformated line: '#{line}'")
+      end
+    end
+    return rv
+  end
+
+  def self.get_ovs_interfaces
+    # return OVS interfaces hash if it possible
+    begin
+      vsctl_list_interfaces = vsctl('list', 'Interface').split("\n")
+      vsctl_list_interfaces << :EOF  # last section of output should be processsed anyway.
+    rescue
+      debug("Can't find OVS interfaces, because error while 'ovs-vsctl list Interface' execution")
+      return {}
+    end
+    #
+    buff = {}
+    rv = {}
+    # parse ovs-vsctl output and find OVS and OVS-affected interfaces
+    vsctl_list_interfaces.each do |line|
+      if line =~ /(\w+)\s*\:\s*(.*)\s*$/
+        key = $1.tr("'\"",'')
+        val = $2.tr("'\"",'')
+        buff[key] = val == '[]'  ?  ''  :  val
+      elsif line =~ /^\s*$/ or line == :EOF
+        rv[buff['name']] = {
+          :mtu        => buff['mtu'],
+          :port_type  => buff['type'].empty?  ?  []  :  [buff['type']],
+          :vendor_specific => {
+            :status     => ovs_parse_opthash(buff['status']),
+          }
+        }
+        driver = rv[buff['name']][:vendor_specific][:status][:driver_name]
+        if driver.nil? or driver.empty? or driver == 'openvswitch'
+            rv[buff['name']][:provider] = 'ovs'
+        else
+            rv[buff['name']][:provider] = nil
+        end
+        debug("Found OVS interface '#{buff['name']}' with properties: #{rv[buff['name']]}")
+        buff = {}
+      else
+        debug("Output of 'ovs-vsctl list Interface' contain misformated line: '#{line}'")
+      end
+    end
+    return rv
+  end
+
+  def self.ovs_vsctl_show
+    begin
+      #content = vsctl('show')
+      content = `ovs-vsctl show`
+    rescue
+      debug("Can't get OVS configuration, because error while 'ovs-vsctl show' execution")
+      return {}
+    end
+    ports = get_ovs_ports()
+    interfaces = get_ovs_interfaces()
+    ovs_config = {
+      :port      => {},
+      :interface => {},
+      #:bond      => {},  # bond in ovs is a internal only port !!!
+      :bridge    => {},
+    }
+    _br = nil
+    _po = nil
+    _if = nil
+    #_ift = nil
+    content.split("\n").each do |line|
+      line.rstrip!
+      case line
+        when /^\s+Bridge\s+"?([\w\-\.]+)\"?$/
+          _br = $1
+          _po = nil
+          _if = nil
+          ovs_config[:bridge][_br] = {
+            :port_type => ['bridge'],
+            :provider  => 'ovs'
+          }
+        when /^\s+Port\s+"?([\w\-\.]+)\"?$/
+          next if _br.nil?
+          _po = $1
+          _if = nil
+          ovs_config[:port][_po] = {
+            :bridge    => _br,
+            :port_type => [],
+            #:provider  => 'ovs'
+          }
+          if ports.has_key? _po
+            ovs_config[:port][_po].merge! ports[_po]
+          end
+          if _po == _br
+            ovs_config[:port][_po][:port_type] << 'bridge'
+          end
+        when /^\s+Interface\s+"?([\w\-\.]+)\"?$/
+          _if = $1
+          ovs_config[:interface][_if] = {
+            :port => _po,
+          }
+          if interfaces.has_key? _if
+            ovs_config[:interface][_if].merge! interfaces[_if]
+          end
+          #todo(sv): Check interface driver from Interfaces table
+          ovs_config[:port][_po][:provider] = ovs_config[:interface][_if][:provider]
+        when /^\s+type:\s+"?([\w\-\.]+)\"?$/
+          ovs_config[:interface][_if].merge!({
+            :type => $1
+          })
+        else
+          #debug("Misformated line for br='#{_br}', po='#{_po}', if='#{_if}' => '#{line}'")
+      end
+    end
+    debug("VSCTL-SHOW: #{ovs_config.inspect}")
+    ovs_config[:port].keys.each do |p_name|
+      ifaces = ovs_config[:interface].select{|k,v| v[:port]==p_name}
+      if ifaces.size > 1
+        # Bond found
+        #ovs_config[:bond][p_name] = ovs_config[:port][p_name]
+        #ovs_config[:port].delete(p_name)
+        ovs_config[:port][p_name][:port_type] << 'bond'
+        ovs_config[:port][p_name][:provider] = 'ovs'
+      else
+        # ordinary interface found
+        # pass
+      end
+      # get mtu value (from one of interfaces if bond) and up it to port layer
+      k = ifaces.keys
+      if k.size > 0
+        ovs_config[:port][p_name][:mtu] = ifaces[k[0]][:mtu]
+      end
+      # fix port-type=vlan for tagged ports
+      if !ovs_config[:port][p_name][:vlan_id].nil?
+        ovs_config[:port][p_name][:port_type] << 'vlan'
+      end
+    end
+    return ovs_config
+  end
+  # ---------------------------------------------------------------------------
+
   def self.get_bridge_list
     # search all (LXN and OVS) bridges on the host, and return hash with mapping
     # bridge_name => { bridge options }
