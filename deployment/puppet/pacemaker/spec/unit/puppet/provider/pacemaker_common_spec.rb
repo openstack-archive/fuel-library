e0ac1bbfe41545611203ef58c8e930e48d04a1e4
Dmitry Ilyin <dilyin@mirantis.com>
2015-01-28 16:34:21 +0300

    Cleanup primitives by their full names
    Change-Id: If570574899eba1513d854db556dc8a0ad0f604ef
    Related-Blueprint: pacemaker-improvements
    Related-Blueprint: corosync-2
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/pacemaker/spec/unit/puppet/provider/pacemaker_common_spec.rb
+++ deployment/puppet/pacemaker/spec/unit/puppet/provider/pacemaker_common_spec.rb
@@ -199,11 +199,10 @@ describe Puppet::Provider::Pacemaker_common do
       @class.wait_for_online
     end
 
-    it 'cleanups primitive and waits for it to become online again' do
-      @class.stubs(:cleanup_primitive).with('myprimitive', 'mynode').returns true
+    it 'waits for status to become known' do
       @class.stubs(:cib_reset).returns true
       @class.stubs(:primitive_status).returns 'stopped'
-      @class.cleanup_with_wait 'myprimitive', 'mynode'
+      @class.wait_for_status 'myprimitive'
     end
 
     it 'waits for the service to start' do
