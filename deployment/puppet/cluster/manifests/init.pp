851ec638f1a5c53a42d6e8981849a214846ff6d6
Stanislav Makar <smakar@mirantis.com>
2015-02-02 13:58:23 +0000

    Class cluster as separate step
    Move the class cluster out of controller.pp
    Clean up the stage corosync_setup in the class cluster
    
    Related blueprint fuel-library-modularization
    
    Change-Id: I43dbccc2b5ad16e607dd8fa8b157f2ee7df5c38c

--- deployment/puppet/cluster/manifests/init.pp
+++ deployment/puppet/cluster/manifests/init.pp
@@ -6,11 +6,7 @@ class cluster (
     $internal_address  = '127.0.0.1',
     $unicast_addresses = undef,
 ) {
-
-    #todo: move half of openstack::corosync
-    #to this module, another half -- to Neutron
-    if defined(Stage['corosync_setup']) {
-
+  #did not clean up due to it will be reworked later
       case $::osfamily {
         'RedHat': {
           $pcs_package = 'pcs'
@@ -27,19 +23,11 @@ class cluster (
       class { 'openstack::corosync':
         bind_address      => $internal_address,
         unicast_addresses => $unicast_addresses,
-        stage             => 'corosync_setup',
         #FIXME(bogdando) use version 2 when Corosync 2.x packages merged
         #corosync_version  => '2',
         packages          => ['corosync', 'pacemaker', 'crmsh', $pcs_package],
       }
-    } else {
-      class { 'openstack::corosync':
-        bind_address      => $internal_address,
-        unicast_addresses => $unicast_addresses,
-        #FIXME(bogdando) use version 2 when Corosync 2.x packages merged
-        #corosync_version  => '2',
-        packages          => ['corosync', 'pacemaker', 'crmsh', $pcs_package],      }
-    }
+
     file { 'ocf-fuel-path':
       ensure  => directory,
       path    =>'/usr/lib/ocf/resource.d/fuel',
@@ -50,17 +38,4 @@ class cluster (
     Package['corosync'] -> File['ocf-fuel-path']
     Package<| title == 'pacemaker' |> -> File['ocf-fuel-path']
 
-    file { 'ns-ipaddr2-ocf':
-      path   =>'/usr/lib/ocf/resource.d/fuel/ns_IPaddr2',
-      mode   => '0755',
-      owner  => root,
-      group  => root,
-      source => 'puppet:///modules/cluster/ocf/ns_IPaddr2',
-    }
-
-    Package['pacemaker'] -> File['ns-ipaddr2-ocf']
-    File<| title == 'ocf-fuel-path' |> -> File['ns-ipaddr2-ocf']
-
 }
-#
-###
