f32188bbe36d7f17664bf31006b2dde51c33fdf7
Stanislav Makar <smakar@mirantis.com>
2015-02-02 14:29:36 +0000

    Virtual IPs as separate step
    Related blueprint fuel-library-modularization
    
    Change-Id: I4af787305a92b272ced28fdbb4fe07f6001264ee

---
+++ deployment/puppet/osnailyfacter/modular/virtual_ips.pp
@@ -0,0 +1,78 @@
+notice('MODULAR: virtual_ips.pp')
+
+
+$internal_int             = hiera('internal_int')
+$public_int               = hiera('public_int',  undef)
+$primary_controller_nodes = hiera('primary_controller_nodes', false)
+$network_scheme           = hiera('network_scheme', {})
+$network_data             = hiera('network_data', {})
+$vip_management_cidr_netmask = netmask_to_cidr($primary_controller_nodes[0]['internal_netmask'])
+$vip_public_cidr_netmask = netmask_to_cidr($primary_controller_nodes[0]['public_netmask'])
+$use_neutron = hiera('use_neutron')
+
+if $use_neutron {
+  $vip_mgmt_other_nets = join($network_scheme['endpoints']["$internal_int"]['other_nets'], ' ')
+}
+
+$vips = { # Do not convert to ARRAY, It can't work in 2.7
+  management   => {
+    namespace            => 'haproxy',
+    nic                  => $internal_int,
+    base_veth            => "${internal_int}-hapr",
+    ns_veth              => "hapr-m",
+    ip                   => hiera('management_vip'),
+    cidr_netmask         => $vip_management_cidr_netmask,
+    gateway              => 'link',
+    gateway_metric       => '20',
+    other_networks       => $vip_mgmt_other_nets,
+    iptables_start_rules => "iptables -t mangle -I PREROUTING -i ${internal_int}-hapr -j MARK --set-mark 0x2b ; iptables -t nat -I POSTROUTING -m mark --mark 0x2b ! -o ${internal_int} -j MASQUERADE",
+    iptables_stop_rules  => "iptables -t mangle -D PREROUTING -i ${internal_int}-hapr -j MARK --set-mark 0x2b ; iptables -t nat -D POSTROUTING -m mark --mark 0x2b ! -o ${internal_int} -j MASQUERADE",
+    iptables_comment     => "masquerade-for-management-net",
+    tie_with_ping        => false,
+    ping_host_list       => "",
+  },
+}
+
+if $public_int {
+  if $use_neutron{
+    $vip_publ_other_nets = join($network_scheme['endpoints']["$public_int"]['other_nets'], ' ')
+  }
+
+  $vips[public] = {
+    namespace            => 'haproxy',
+    nic                  => $public_int,
+    base_veth            => "${public_int}-hapr",
+    ns_veth              => "hapr-p",
+    ip                   => $public_vip,
+    cidr_netmask         => $vip_public_cidr_netmask,
+    gateway              => 'link',
+    gateway_metric       => '10',
+    other_networks       => $vip_publ_other_nets,
+    iptables_start_rules => "iptables -t mangle -I PREROUTING -i ${public_int}-hapr -j MARK --set-mark 0x2a ; iptables -t nat -I POSTROUTING -m mark --mark 0x2a ! -o ${public_int} -j MASQUERADE",
+    iptables_stop_rules  => "iptables -t mangle -D PREROUTING -i ${public_int}-hapr -j MARK --set-mark 0x2a ; iptables -t nat -D POSTROUTING -m mark --mark 0x2a ! -o ${public_int} -j MASQUERADE",
+    iptables_comment     => "masquerade-for-public-net",
+    tie_with_ping        => hiera('run_ping_checker', true),
+    ping_host_list       => $use_neutron ? {
+      default => $network_data[$public_int]['gateway'],
+      true    => $network_scheme['endpoints']['br-ex']['gateway'],
+    },
+  }
+}
+$vip_keys = keys($vips)
+
+class virtual_ips () {
+  cluster::virtual_ips { $::vip_keys:
+    vips => $::vips,
+  }
+
+  # Some topologies might need to keep the vips on the same node during
+  # deploymenet. This would only need to be changed by hand.
+  $keep_vips_together = false
+  if ($keep_vips_together) {
+    cs_rsc_colocation { 'ha_vips':
+      ensure      => present,
+      primitives  => [prefix(keys($::vips),"vip__")],
+      after       => Cluster::Virtual_ips[$::vip_keys]
+    }
+  } # End If keep_vips_together
+}
