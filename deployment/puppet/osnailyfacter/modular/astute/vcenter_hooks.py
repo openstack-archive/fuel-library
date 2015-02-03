8b7ac15b2b93bf3f51a52d6a0dd1ae42d6fba3d3
Stepan Rogov <srogov@mirantis.com>
2015-02-03 15:57:44 +0300

    New astute post-deploy task for nova-network deletion from computes
    In case of 2 hypervisors we use nova-network in sigle host mode on
    controller under corosync, therefore it is need to delete unusable
    nova-network services from computes. Because ostf checks that it is
    no a dead services.
    
    Refactored pyhton script for options supporting.
    
    Change-Id: If49755d8c3e0ec9cce2b18bb3213aa2a06026016
    Implements: blueprint vmware-dual-hypervisor

---
+++ deployment/puppet/osnailyfacter/modular/astute/vcenter_hooks.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+import sys
+import json
+import subprocess
+from optparse import OptionParser
+from novaclient.client import Client
+
+
+def get_data_from_hiera(key):
+    cmd_data = subprocess.Popen("hiera " + key, shell=True,
+                                stdout=subprocess.PIPE).stdout.read(). \
+        replace('=>', ':').replace('nil', '"null"')
+    try:
+        data = json.loads(cmd_data)
+    except:
+        data = {key: cmd_data.strip()}
+    return data
+
+
+def check_availability_zones(nova_client, compute):
+    nova_zones = nova_client.availability_zones.list()
+    nova_aggregates = nova_client.aggregates.list()
+    nova_zones_and_aggregates = nova_aggregates + nova_zones
+    compute_zone = compute['availability_zone_name']
+    present = filter(lambda item: item.to_dict().get('zoneName') ==
+                     compute_zone or item.to_dict().get('availability_zone') ==
+                     compute_zone, nova_zones_and_aggregates)
+    if present:
+        print "Zone " + compute_zone + " already present."
+    else:
+        print "Zone " + compute_zone + " is missing, creating it."
+        nova_client.aggregates.create(compute_zone, compute_zone)
+
+
+def check_host_in_zone(nova_client, compute):
+    nova_zones = nova_client.availability_zones.list()
+    nova_aggregates = nova_client.aggregates.list()
+    compute_zone = compute['availability_zone_name']
+    compute_host = compute_zone + "-" + compute['service_name']
+
+    for aggregate in nova_aggregates:
+        if aggregate.to_dict()['name'] == compute_zone:
+            host_aggregate = aggregate
+
+    present = filter(lambda item: compute_host in item.to_dict()['hosts'] and
+                     item.to_dict()['zoneName'] == compute_zone, nova_zones)
+
+    if present:
+        print "Compute service " + compute['service_name'] + " already in " + \
+            compute_zone + " zone."
+    else:
+        print "Compute service " + compute['service_name'] + " not in " + \
+            compute_zone + " zone. " + "Adding."
+        nova_client.aggregates.add_host(host_aggregate, compute_host)
+
+
+def delete_nova_service(client, service_id):
+            client.services.delete(service_id)
+
+
+def main():
+    use_vcenter = get_data_from_hiera('use_vcenter')
+    if use_vcenter is not True:
+        sys.exit(0)
+
+    credentials = get_data_from_hiera('access')
+    USERNAME = credentials['user']
+    PASSWORD = credentials['password']
+    PROJECT_ID = credentials['tenant']
+    VERSION = 2
+    IP = get_data_from_hiera('management_vip')['management_vip']
+    AUTH_URL = "http://" + IP + ":5000/v2.0/"
+
+    parser = OptionParser()
+    parser.add_option("--create_zones", action="store_false", help="Create \
+                      needed availability zones and puts coresponding compute \
+                      services in corresponding availability zones")
+    parser.add_option("--delete_network_services", action="store_false",
+                      help="Delete unusable nova-network services from \
+                      computes")
+    (options, args) = parser.parse_args()
+
+    nova = Client(VERSION, USERNAME, PASSWORD, PROJECT_ID, AUTH_URL)
+    vcenter_settings = get_data_from_hiera('vcenter')
+
+    if options.create_zones is True:
+        for compute in vcenter_settings['computes']:
+            print "---Start of Compute service " + compute['service_name'] + \
+                " zone creation.---"
+            check_availability_zones(nova, compute)
+            check_host_in_zone(nova, compute)
+            print "----End of Compute service " + compute['service_name'] + \
+                "----"
+    elif options.delete_network_services is True:
+        for node in nodes:
+            if node['role'] == 'compute':
+                network_services = nova.services.list(node['name'],
+                                                      'nova-network')
+                for network_service in network_services:
+                    print "Found unusable nova-network on " + node['name'] + \
+                        " compute node, deleting it."
+                    delete_nova_service(nova, network_service.to_dict()['id'])
+
+
+if __name__ == '__main__':
+    main()
