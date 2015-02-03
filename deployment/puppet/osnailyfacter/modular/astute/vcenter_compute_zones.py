0c6d3c5aecee4583cb84ac55031ccbce1e671150
Stepan Rogov <srogov@mirantis.com>
2015-02-03 15:57:44 +0300

    New astute post-deploy task for nova-network deletion from computes
    In case of 2 hypervisors we use nova-network in sigle host mode on
    controller under corosync, therefore it is need to delete unusable
    nova-network services from computes. Because ostf checks that it is
    no a dead services.
    
    Refactored pyhton script for options supporting.
    
    Change-Id: If49755d8c3e0ec9cce2b18bb3213aa2a06026016
    Implements: blueprint vmware-dual-hypervisor

--- deployment/puppet/osnailyfacter/modular/astute/vcenter_compute_zones.py
+++ deployment/puppet/osnailyfacter/modular/astute/vcenter_compute_zones.py
@@ -4,6 +4,15 @@ import sys
 import json
 import subprocess
 from novaclient.client import Client
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("--create_zones", action="store_false", help="Create needed \
+                  availability zones and puts coresponding compute services \
+                  in corresponding availability zones")
+parser.add_option("--delete_network_services", action="store_false",
+                  help="Delete unusable nova-network services from computes")
+(options, args) = parser.parse_args()
 
 
 def get_data_from_hiera(key):
@@ -53,6 +62,10 @@ def check_host_in_zone(nova_client, compute):
         print "Compute service " + compute['service_name'] + " already in " + \
             compute_zone + " zone."
 
+
+def delete_nova_service(client, service_id):
+            client.services.delete(service_id)
+
 use_vcenter = get_data_from_hiera('use_vcenter')
 if use_vcenter is not True:
     sys.exit(0)
@@ -68,9 +81,19 @@ AUTH_URL = "http://" + IP + ":5000/v2.0/"
 nova = Client(VERSION, USERNAME, PASSWORD, PROJECT_ID, AUTH_URL)
 vcenter_settings = get_data_from_hiera('vcenter')
 
-for compute in vcenter_settings['computes']:
-    print "---Start of Compute service " + compute['service_name'] + \
-        " zone creation.---"
-    check_availability_zones(nova, compute)
-    check_host_in_zone(nova, compute)
-    print "----End of Compute service " + compute['service_name'] + "----\n"
+if options.create_zones. is True:
+    for compute in vcenter_settings['computes']:
+        print "---Start of Compute service " + compute['service_name'] + \
+            " zone creation.---"
+        check_availability_zones(nova, compute)
+        check_host_in_zone(nova, compute)
+        print "----End of Compute service " + compute['service_name'] + \
+            "----\n"
+elif options.delete_network_services is True:
+    for node in nodes:
+        if node['role'] == 'compute':
+            network_services = nova.services.list(node['name'], 'nova-network')
+            for network_service in network_services:
+                print "Found unusable nova-network on " + node['name'] + \
+                    " compute node, deleting it."
+                delete_nova_service(nova, network_service.to_dict()['id'])
