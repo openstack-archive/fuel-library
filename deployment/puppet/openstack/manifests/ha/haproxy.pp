b710c2f94ea4e1e2d67ee6c7a4b69715ad367b34
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-02-03 19:10:07 +0300

    Openstack-haproxy and openstack-controller as a separate step
    Includes updates for other classes to make them work correnctly
    with new granular deployment process.
    
    Related blueprint fuel-library-modularization
    
    Change-Id: Ie4389fe62a5516ede56c0e79791e40ef1e339bbd

--- deployment/puppet/openstack/manifests/ha/haproxy.pp
+++ deployment/puppet/openstack/manifests/ha/haproxy.pp
@@ -38,10 +38,6 @@ class openstack::ha::haproxy (
   if $custom_mysql_setup_class == 'galera' {
     class { 'openstack::ha::mysqld':
       is_primary_controller => $is_primary_controller,
-      before_start          => true
-      # At least one service must call Openstack::HA::Haproxy_service with
-      #  before_start = true or HA proxy wont start because it won't have any
-      #  listen configs.
     }
   }
 
