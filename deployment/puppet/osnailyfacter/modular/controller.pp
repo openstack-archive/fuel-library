6147fd213a4e44bf5854cff5d1f26f43ab9d26dc
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-02-03 19:10:07 +0300

    Openstack-haproxy and openstack-controller as a separate step
    Related blueprint fuel-library-modularization
    
    Change-Id: Ie4389fe62a5516ede56c0e79791e40ef1e339bbd

--- deployment/puppet/osnailyfacter/modular/controller.pp
+++ deployment/puppet/osnailyfacter/modular/controller.pp
@@ -1,7 +1,7 @@
 notice('MODULAR: controller.pp')
 
 # Pulling hiera
-$public_int                     = hiera('public_int', undef)
+#$public_int                     = hiera('public_int', undef) - moved to openstack-controller
 $public_vip                     = hiera('public_vip')
 $management_vip                 = hiera('management_vip')
 $internal_address               = hiera('internal_address')
@@ -59,8 +59,8 @@ $openstack_version = {
   'cinder'     => 'installed',
 }
 
-$queue_provider='rabbitmq'
-$custom_mysql_setup_class='galera'
+#$queue_provider='rabbitmq' - copied into openstack-controller and openstack-haproxy
+#$custom_mysql_setup_class='galera' - copied into openstack-controller and openstack-haproxy
 
 # Do the stuff
 if $neutron_mellanox {
@@ -309,118 +309,9 @@ if $use_monit_real {
 
 #HARDCODED PARAMETERS
 
-$multi_host = true
+#$multi_host = true - moved to openstack-controller as true
 $mirror_type = 'external'
 Exec { logoutput => true }
-
-class compact_controller (
-  $primary_controller,
-) {
-
-  class { 'openstack::controller_ha':
-    controllers                    => $::controllers,
-    controller_public_addresses    => $::controller_public_addresses,
-    controller_internal_addresses  => $::controller_internal_addresses,
-    internal_address               => $::internal_address,
-    public_interface               => $::public_int,
-    private_interface              => $::use_neutron ? { true=>false, default=>hiera('fixed_interface')},
-    internal_virtual_ip            => $::management_vip,
-    public_virtual_ip              => $::public_vip,
-    primary_controller             => $::primary_controller,
-    floating_range                 => $::use_neutron ? { true=>$floating_hash, default=>false},
-    fixed_range                    => $::use_neutron ? { true=>false, default=>hiera('fixed_network_range')},
-    multi_host                     => $::multi_host,
-    network_manager                => $::network_manager,
-    num_networks                   => $::num_networks,
-    network_size                   => $::network_size,
-    network_config                 => $::network_config,
-    debug                          => $::debug,
-    verbose                        => $::verbose,
-    auto_assign_floating_ip        => $::auto_assign_floating_ip,
-    mysql_root_password            => $::mysql_hash[root_password],
-    admin_email                    => $::access_hash[email],
-    admin_user                     => $::access_hash[user],
-    admin_password                 => $::access_hash[password],
-    keystone_db_password           => $::keystone_hash[db_password],
-    keystone_admin_token           => $::keystone_hash[admin_token],
-    keystone_admin_tenant          => $::access_hash[tenant],
-    glance_db_password             => $::glance_hash[db_password],
-    glance_user_password           => $::glance_hash[user_password],
-    glance_image_cache_max_size    => $::glance_hash[image_cache_max_size],
-    known_stores                   => $::glance_known_stores,
-    glance_vcenter_host            => $::storage_hash['vc_host'],
-    glance_vcenter_user            => $::storage_hash['vc_user'],
-    glance_vcenter_password        => $::storage_hash['vc_password'],
-    glance_vcenter_datacenter      => $::storage_hash['vc_datacenter'],
-    glance_vcenter_datastore       => $::storage_hash['vc_datastore'],
-    glance_vcenter_image_dir       => $::storage_hash['vc_image_dir'],
-    nova_db_password               => $::nova_hash[db_password],
-    nova_user_password             => $::nova_hash[user_password],
-    queue_provider                 => $::queue_provider,
-    amqp_hosts                     => $::amqp_hosts,
-    amqp_user                      => $::rabbit_hash['user'],
-    amqp_password                  => $::rabbit_hash['password'],
-    rabbit_ha_queues               => $::rabbit_ha_queues,
-    rabbitmq_bind_ip_address       => $::rabbitmq_bind_ip_address,
-    rabbitmq_bind_port             => $::rabbitmq_bind_port,
-    rabbitmq_cluster_nodes         => $::rabbitmq_cluster_nodes,
-    memcached_servers              => $::controller_nodes,
-    memcached_bind_address         => $::internal_address,
-    export_resources               => false,
-    glance_backend                 => $::glance_backend,
-    swift_proxies                  => $::swift_proxies,
-    rgw_servers                    => $::rgw_servers,
-
-    network_provider               => $::network_provider,
-    neutron_db_password            => $::neutron_db_password,
-    neutron_user_password          => $::neutron_user_password,
-    neutron_metadata_proxy_secret  => $::neutron_metadata_proxy_secret,
-    neutron_ha_agents              => $::primary_controller ? {true => 'primary', default => 'slave'},
-    base_mac                       => $::base_mac,
-
-    cinder                         => true,
-    cinder_user_password           => $::cinder_hash[user_password],
-    cinder_iscsi_bind_addr         => $::cinder_iscsi_bind_addr,
-    cinder_db_password             => $::cinder_hash[db_password],
-    cinder_volume_group            => "cinder",
-    manage_volumes                 => $::manage_volumes,
-    ceilometer                     => $::ceilometer_hash[enabled],
-    ceilometer_db_user             => $::ceilometer_db_user,
-    ceilometer_db_password         => $::ceilometer_db_password,
-    ceilometer_user_password       => $::ceilometer_hash[user_password],
-    ceilometer_metering_secret     => $::ceilometer_hash[metering_secret],
-    ceilometer_db_type             => 'mongodb',
-    swift_rados_backend            => $::storage_hash['objects_ceph'],
-    ceilometer_db_dbname           => $::ceilometer_db_name,
-    ceilometer_db_host             => $::mongo_hosts,
-    ceilometer_ext_mongo           => $::ext_mongo,
-    mongo_replicaset               => $::mongo_replicaset,
-    galera_nodes                   => $::controller_nodes,
-    novnc_address                  => $::internal_address,
-    sahara                         => $::sahara_hash[enabled],
-    murano                         => $::murano_hash['enabled'],
-    custom_mysql_setup_class       => $::custom_mysql_setup_class,
-    mysql_skip_name_resolve        => true,
-    use_syslog                     => $::use_syslog,
-    syslog_log_facility_glance     => $::syslog_log_facility_glance,
-    syslog_log_facility_cinder     => $::syslog_log_facility_cinder,
-    syslog_log_facility_neutron    => $::syslog_log_facility_neutron,
-    syslog_log_facility_nova       => $::syslog_log_facility_nova,
-    syslog_log_facility_keystone   => $::syslog_log_facility_keystone,
-    syslog_log_facility_ceilometer => $::syslog_log_facility_ceilometer,
-    nova_rate_limits               => $::nova_rate_limits,
-    cinder_rate_limits             => $::cinder_rate_limits,
-    horizon_use_ssl                => hiera('horizon_use_ssl', false),
-    use_unicast_corosync           => hiera('use_unicast_corosync', false),
-    nameservers                    => $::dns_nameservers,
-    max_retries                    => $::max_retries,
-    max_pool_size                  => $::max_pool_size,
-    max_overflow                   => $::max_overflow,
-    idle_timeout                   => $::idle_timeout,
-    nova_report_interval           => $::nova_report_interval,
-    nova_service_down_time         => $::nova_service_down_time,
-  }
-}
 
 if $use_vmware_nsx {
   class { 'plugin_neutronnsx':
@@ -473,10 +364,6 @@ if $use_ceph {
 }
 #################################################################
 include osnailyfacter::test_controller
-
-class { 'compact_controller':
-  primary_controller => $primary_controller
-}
 
 if ($use_swift) {
   $swift_zone = $node[0]['swift_zone']
