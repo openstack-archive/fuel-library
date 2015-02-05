64924e8315e5b0106df422674f3b4d2ddf596ed0
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-02-05 15:45:43 +0300

    WIP Docker on direct LVM
    Separates log and Docker data to minimize
    disk resource contention
    
    Change-Id: I5c50c266a43570126b912ed07582201e23af9871

--- deployment/puppet/docker/manifests/init.pp
+++ deployment/puppet/docker/manifests/init.pp
@@ -1,6 +1,8 @@
 class docker (
 $admin_ipaddress = $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
 $limit = "102400",
+$metadata_device = "/dev/os/docker_metadata",
+$data_device = "/dev/os/docker_data",
 $docker_package = "docker-io",
 $docker_service = "docker",
 $docker_engine = "native",
