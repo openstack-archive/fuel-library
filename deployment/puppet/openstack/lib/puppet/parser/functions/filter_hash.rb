eb3d90d1f1f3d04f4fb6243e74f44195b22d5803
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2015-02-05 21:47:23 +0100

    Add checkers to filter_hash function
    Change-Id: I07962c9acede0dcfeca9f01d218fdb001eb33593
    Closes-Bug: 1418058
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/openstack/lib/puppet/parser/functions/filter_hash.rb
+++ deployment/puppet/openstack/lib/puppet/parser/functions/filter_hash.rb
@@ -1,13 +1,23 @@
+#  vim: set ts=2 sw=2 tw=0 et :
 module Puppet::Parser::Functions
   newfunction(:filter_hash, :type => :rvalue,  :doc => <<-EOS
-    Map array of hashes $arg0 to an array yielding 
-    an element from each hash by key $arg1
+    Map array of hashes $arguments[0] to an array yielding
+    an element from each hash by key $arguments[1]
     EOS
- ) do |args|
-    hash = args[0]
-    field  = args[1]
-    hash.map do |e|
-      e[field]
-    end
-  end
+ ) do |arguments|
+
+   raise(Puppet::ParseError, "filter_hash(): Wrong number of arguments " +
+     "given (#{arguments.size} for 2") if arguments.size < 2
+
+   value = arguments[0]
+   field = arguments[1]
+
+   if value.is_a?(Array) or value.is_a?(Hash)
+     value.map do |e|
+       e[field]
+     end
+   else
+     []
+   end
+ end
 end
