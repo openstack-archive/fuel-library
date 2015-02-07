cf0eb1dd17fb7dce59b6ace2271126a1c7db0d72
Sergey Vasilenko <svasilenko@mirantis.com>
2015-02-07 15:53:54 +0300

    Set of small patches
    * simplify res_factory due we don't use low level create_resource function more
    * remove unnided code
    * move ethtool mapping to external function
    * FIX: check double-creating for subinterfaces and bridges
    * FIX: MTU from bond properties didn't pass to bond slave interfaces
    
    Blueprint: refactor-l23-linux-bridges
    Fuel-CI: disable
    Change-Id: Iadfc4ebef409bb6814970e31d1e4022c35bde5f6

--- deployment/puppet/l23network/lib/puppetx/l23_utils.rb
+++ deployment/puppet/l23network/lib/puppetx/l23_utils.rb
@@ -1,26 +1,27 @@
-def reccursive_sanitize_hash(data)
-  if data.is_a? Hash
-    new_data = {}
-    data.each do |key, value|
-      new_data.store(reccursive_sanitize_hash(key), reccursive_sanitize_hash(value))
+module L23network
+  def self.reccursive_sanitize_hash(data)
+    if data.is_a? Hash
+      new_data = {}
+      data.each do |key, value|
+        new_data.store(reccursive_sanitize_hash(key), reccursive_sanitize_hash(value))
+      end
+      new_data
+    elsif data.is_a? Array
+      data.map do |element|
+        reccursive_sanitize_hash(element)
+      end
+    elsif ['true', 'on', 'yes'].include? data.to_s.downcase
+      true
+    elsif ['false', 'off', 'no'].include? data.to_s.downcase
+      false
+    elsif data.nil?
+      nil
+    else
+      data.to_s
     end
-    new_data
-  elsif data.is_a? Array
-    data.map do |element|
-      reccursive_sanitize_hash(element)
-    end
-  elsif ['true', 'on', 'yes'].include? data.to_s.downcase
-    true
-  elsif ['false', 'off', 'no'].include? data.to_s.downcase
-    false
-  elsif data.nil?
-    nil
-  else
-    data.to_s
   end
-end
 
-# def reccursive_merge_hash(a,b)
+# def self.reccursive_merge_hash(a,b)
 #   rv = {}
 
 #   a.keys.each do |key|
@@ -57,3 +58,4 @@ end
 
 #   return rv
 # end
+end
