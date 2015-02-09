7098dbe2ce3864b05a7bdd4112a66e6675fca311
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-02-09 11:47:31 +0100

    Prefix neutron-agent logs filenames with "ocf-"
    Closes-Bug: 1401504
    Change-Id: I33eceaad5e5481a8525a8a15b87b178ef2f4aed4

--- deployment/puppet/cluster/manifests/neutron/dhcp.pp
+++ deployment/puppet/cluster/manifests/neutron/dhcp.pp
@@ -36,7 +36,7 @@ class cluster::neutron::dhcp (
 
   #TODO (bogdando) move to extras ha wrappers
   cluster::corosync::cs_service {'dhcp':
-    ocf_script      => 'neutron-agent-dhcp',
+    ocf_script      => 'ocf-neutron-agent-dhcp',
     csr_parameters  => {
       'os_auth_url'      => $auth_url,
       'tenant'           => $admin_tenant_name,
