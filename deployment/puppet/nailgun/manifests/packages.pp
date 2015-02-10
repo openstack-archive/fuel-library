479d09b7e28c0bd08349888906006bd7720ec2f3
Alexei Sheplyakov <asheplyakov@mirantis.com>
2015-02-10 09:58:54 +0300

    Fix nailgun and cobbler containers build failure.
    Recently a new package fence-agents-all (version 4.0.7) has been added
    to the OSCI yum repo. This package provides and obsoletes fence-agents
    version <= 3.1.13. Due to a poor integration of puppet with package
    management tools installing fence-agents with puppet fails:
    
    Debug: Package[fence-agents](provider=yum): Ensuring => latest
    Debug: Executing '/usr/bin/yum -d 0 -e 0 -y install fence-agents'
    Debug: Executing '/bin/rpm -q fence-agents --nosignature --nodigest --qf %{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH} :DESC: %%{SUMMARY}\n'
    Error: Could not update: Could not find package fence-agents
    
    puppet does not try to figure out if fence-agents is provided by some
    other package and query the status of that package, hence the error.
    As a work around update the manifests to require fence-agents-all package.
    A proper way to solve the problem is to fix dependencies of the packages
    (if cobbler needs fence-agents it should depend on it so yum picks a proper
    version of fence-agents without any tricks in manifests).
    
    Closes-Bug: #1419889
    Change-Id: I208e187def737d05d033e95f34ae5ca58ece9bd0

--- deployment/puppet/nailgun/manifests/packages.pp
+++ deployment/puppet/nailgun/manifests/packages.pp
@@ -16,7 +16,7 @@ class nailgun::packages(
   nailgun_safe_package { "postgresql-libs": }
   nailgun_safe_package { "rsyslog": }
   nailgun_safe_package { "rsync": }
-  nailgun_safe_package { "fence-agents": }
+  nailgun_safe_package { "fence-agents-all": }
   nailgun_safe_package { "nailgun-redhat-license": }
   nailgun_safe_package { "python-fuelclient": }
   nailgun_safe_package { "screen": }
