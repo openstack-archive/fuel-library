3ebeb734ba5180049e883bad81d6a1a297b53d04
Igor Kalnitsky <igor@kalnitsky.org>
2015-02-11 12:17:48 +0200

    Add support both dockers: v0.10 and v1.4.1
    We need this temporary patch in order to do not break BVT and let us
    work with both Docker versions: v0.10.0 (old) and v1.4.1 (new).
    
    The patch could work properly iff LXC >= 1.0.3 was merged.
    
    Change-Id: I4456cee61320c7b75a0dc5fcd51951169a3532be

--- deployment/puppet/nailgun/examples/host-upgrade.pp
+++ deployment/puppet/nailgun/examples/host-upgrade.pp
@@ -41,7 +41,10 @@ class { "docker::dockerctl":
   production      => $production,
   admin_ipaddress => $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
 }
-class { "docker": }
+
+class { "docker":
+  docker_engine => 'lxc',
+}
 
 class {'openstack::logrotate':
   role           => 'server',
