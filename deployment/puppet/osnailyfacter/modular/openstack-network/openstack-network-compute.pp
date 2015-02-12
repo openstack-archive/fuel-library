bcf0f5c52ac38eb3e0bf3e31356401b4104b8599
Stanislav Makar <smakar@mirantis.com>
2015-02-12 15:44:25 +0000

    Adapt Nova-network netconfig step to new network_scheme
    Related blueprint fuel-library-modularization
    Blueprint: refactor-l23-linux-bridges
    Fuel-CI: disable
    
    Change-Id: I51e46560bd73bfc34bc73f6107cf8bf26311238c

--- deployment/puppet/osnailyfacter/modular/openstack-network/openstack-network-compute.pp
+++ deployment/puppet/osnailyfacter/modular/openstack-network/openstack-network-compute.pp
@@ -1,6 +1,7 @@
 notice('MODULAR: openstack-network-compute.pp')
 
 $use_neutron                    = hiera('use_neutron')
+$network_provider               = hiera('network_provider')
 $neutron_nsx_config             = hiera('nsx_plugin')
 $nova_hash                      = hiera('nova', {})
 $internal_address               = hiera('internal_address')
@@ -28,7 +29,6 @@ if $use_neutron {
   include l23network::l2
   $novanetwork_params    = {}
   $neutron_config        = hiera('quantum_settings')
-  $network_provider      = 'neutron'
   $neutron_db_password   = $neutron_config['database']['passwd']
   $neutron_user_password = $neutron_config['keystone']['admin_password']
   $neutron_metadata_proxy_secret = $neutron_config['metadata']['metadata_proxy_shared_secret']
@@ -40,10 +40,6 @@ if $use_neutron {
   $floating_ips_range = hiera('floating_network_range')
   $neutron_config     = {}
   $novanetwork_params = hiera('novanetwork_parameters')
-  $network_size       = $novanetwork_params['network_size']
-  $num_networks       = $novanetwork_params['num_networks']
-  $vlan_start         = $novanetwork_params['vlan_start']
-  $network_provider   = 'nova'
 }
 $fixed_range = $use_neutron ? { true=>false, default=>hiera('fixed_network_range')}
 
@@ -64,14 +60,9 @@ $openstack_version = {
 
 $enabled_apis = 'metadata'
 $public_interface = $public_int ? { undef=>'', default=>$public_int}
-$network_manager = "nova.network.manager.${novanetwork_params['network_manager']}"
 $libvirt_vif_driver = 'nova.virt.libvirt.vif.LibvirtGenericVIFDriver'
 $neutron_integration_bridge = 'br-int'
 $neutron_settings = $neutron_config
-
-$network_config = {
-  'vlan_start'     => $vlan_start,
-}
 
 # if the compute node should be configured as a multi-host
 # compute installation
@@ -148,16 +139,9 @@ if $network_provider == 'nova' {
   }
 
   # From legacy network.pp
-  if $network_manager !~ /VlanManager$/ {
-    $config_overrides = delete($network_config, 'vlan_start')
-  } else {
-    $config_overrides = $network_config
-  }
-
-  # From legacy network.pp
   # I don't think this is applicable to Folsom...
   # If it is, the details will need changed. -jt
-  if $network_manager == 'nova.network.neutron.manager.NeutronManager' {
+  if hiera('network_manager') == 'nova.network.neutron.manager.NeutronManager' {
     $parameters = { fixed_range      => $fixed_range,
                     public_interface => $public_interface,
                   }
@@ -354,15 +338,15 @@ class { 'openstack::network':
   integration_bridge => $neutron_integration_bridge,
 
   # nova settings
-  private_interface => $use_neutron ? { true =>false, default =>hiera('fixed_interface')},
+  private_interface => $use_neutron ? { true=>false, default=>hiera('private_int')},
   public_interface  => hiera('public_int', undef),
   fixed_range       => $use_neutron ? { true =>false, default =>hiera('fixed_network_range')},
   floating_range    => $use_neutron ? { true =>$floating_hash, default  =>false},
-  network_manager   => $network_manager,
-  network_config    => $config_overrides,
+  network_manager   => hiera('network_manager'),
+  network_config    => hiera('network_config', {}),
   create_networks   => $create_networks,
-  num_networks      => $num_networks,
-  network_size      => $network_size,
+  num_networks      => hiera('num_networks'),
+  network_size      => hiera('network_size'),
   nameservers       => hiera('dns_nameservers'),
   enable_nova_net   => $enable_network_service,
 }
