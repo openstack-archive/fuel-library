b2b6e0c47669fe5c770eec20a4b254aa543a0451
iberezovskiy <iberezovskiy@mirantis.com>
2015-02-16 18:30:54 +0300

    Set parameter 'replset' to ceilometer for all mongo nodes
    Change-Id: I8e1adff9a9fc843414c2366185ab4b07be372740
    Closes-bug: #1421283

--- deployment/puppet/osnailyfacter/modular/mongo.pp
+++ deployment/puppet/osnailyfacter/modular/mongo.pp
@@ -10,4 +10,5 @@ class { 'openstack::mongo_secondary':
   mongodb_bind_address        => [ '127.0.0.1', $internal_address ],
   use_syslog                  => $use_syslog,
   debug                       => $debug,
+  replset                     => 'ceilometer',
 }
