d4882321bd799cb2a7258b1762f1a490173cd61f
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-02-16 17:23:54 +0300

    Stateless nailgun container
    Change-Id: I13f5038f9cae43d8006d6a3218fc059f5c74c564

--- deployment/puppet/nailgun/examples/host-only.pp
+++ deployment/puppet/nailgun/examples/host-only.pp
@@ -12,6 +12,34 @@ else {
 $ntp_servers = [$::fuel_settings['NTP1'], $::fuel_settings['NTP2'],
                 $::fuel_settings['NTP3']]
 
+# Nailgun settings
+$staticdir = "/usr/share/nailgun/static"
+$templatedir = $staticdir
+
+$rabbitmq_host = $::fuel_settings['ADMIN_NETWORK']['ipaddress']
+$rabbitmq_astute_user = $::fuel_settings['astute']['user']
+$rabbitmq_astute_password = $::fuel_settings['astute']['password']
+
+$cobbler_host = $::fuel_settings['ADMIN_NETWORK']['ipaddress']
+$cobbler_url = "http://${::fuel_settings['ADMIN_NETWORK']['ipaddress']}:80/cobbler_api"
+$cobbler_user = $::fuel_settings['cobbler']['user']
+$cobbler_password = $::fuel_settings['cobbler']['password']
+
+$mco_pskey = "unset"
+$mco_vhost = "mcollective"
+$mco_host = $::fuel_settings['ADMIN_NETWORK']['ipaddress']
+$mco_user = $::fuel_settings['mcollective']['user']
+$mco_password = $::fuel_settings['mcollective']['password']
+$mco_connector = "rabbitmq"
+
+$keystone_host = $::fuel_settings['ADMIN_NETWORK']['ipaddress']
+$keystone_nailgun_user = $::fuel_settings['keystone']['nailgun_user']
+$keystone_nailgun_pass = $::fuel_settings['keystone']['nailgun_password']
+
+#deprecated
+$puppet_master_hostname = "${::fuel_settings['HOSTNAME']}.${::fuel_settings['DNS_DOMAIN']}"
+
+
 Class['nailgun::packages'] ->
 Class['nailgun::client'] ->
 Class['nailgun::host'] ->
@@ -26,12 +54,51 @@ class { 'osnailyfacter::atop': }
 
 class { 'nailgun::host':
   production    => $production,
-  cobbler_host  => $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
   nailgun_group => $nailgun_group,
   nailgun_user  => $nailgun_user,
   dns_domain    => $::fuel_settings['DNS_DOMAIN'],
   dns_search    => $::fuel_settings['DNS_SEARCH'],
 
+  database_name   => $::fuel_settings['postgres']['nailgun_dbname'],
+  database_engine => "postgresql",
+  database_host   => $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
+  database_port   => "5432",
+  database_user   => $::fuel_settings['postgres']['nailgun_user'],
+  database_passwd => $::fuel_settings['postgres']['nailgun_password'],
+
+  staticdir                => $staticdir,
+  templatedir              => $templatedir,
+  rabbitmq_host            => $rabbitmq_host,
+  rabbitmq_astute_user     => $rabbitmq_astute_user,
+  rabbitmq_astute_password => $rabbitmq_astute_password,
+
+  admin_network         => ipcalc_network_by_address_netmask($::fuel_settings['ADMIN_NETWORK']['ipaddress'],$::fuel_settings['ADMIN_NETWORK']['netmask']),
+  admin_network_cidr    => ipcalc_network_cidr_by_netmask($::fuel_settings['ADMIN_NETWORK']['netmask']),
+  admin_network_size    => ipcalc_network_count_addresses($::fuel_settings['ADMIN_NETWORK']['ipaddress'],
+$::fuel_settings['ADMIN_NETWORK']['netmask']),
+  admin_network_first   => $::fuel_settings['ADMIN_NETWORK']['dhcp_pool_start'],
+  admin_network_last    => $::fuel_settings['ADMIN_NETWORK']['dhcp_pool_end'],
+  admin_network_netmask => $::fuel_settings['ADMIN_NETWORK']['netmask'],
+  admin_network_mac     => $::fuel_settings['ADMIN_NETWORK']['mac'],
+  admin_network_ip      => $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
+
+  cobbler_host     => $cobbler_host,
+  cobbler_url      => $cobbler_url,
+  cobbler_user     => $cobbler_user,
+  cobbler_password => $cobbler_password,
+
+  mco_pskey     => $mco_pskey,
+  mco_vhost     => $mco_vhost,
+  mco_host      => $mco_host,
+  mco_user      => $mco_user,
+  mco_password  => $mco_password,
+  mco_connector => $mco_connector,
+
+  puppet_master_hostname => $puppet_master_hostname,
+
+  keystone_host         => $keystone_host,
+  keystone_nailgun_user => $keystone_nailgun_user,
+  keystone_nailgun_pass => $keystone_nailgun_pass,
 }
 
 class { "openstack::clocksync":
