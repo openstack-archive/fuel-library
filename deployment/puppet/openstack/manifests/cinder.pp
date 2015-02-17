08e673bf133685aaa5017d6b4b3b7f2be2aff99a
Aleksandr Didenko <adidenko@mirantis.com>
2015-02-17 13:52:29 +0200

    Fix ubuntu_service_override ordering
    Fixes resource ordering in tweaks::ubuntu_service_override to make
    sure we do not leave any 'override' files.
    Replaces tabs with spaces in ubuntu_service_override.pp
    Added tweaks::ubuntu_service_override for cinder-volume in
    openstack::cinder class.
    
    Change-Id: I41950fc111b697c2948d6ba685b220785c8d8911
    Closes-bug: #1421595

--- deployment/puppet/openstack/manifests/cinder.pp
+++ deployment/puppet/openstack/manifests/cinder.pp
@@ -132,6 +132,17 @@ class openstack::cinder(
   }
 
   if $manage_volumes {
+
+    ####### Disable upstart startup on install #######
+    if($::operatingsystem == 'Ubuntu') {
+      # Ceph rbd backend configures its override on its own
+      if !$::fuel_settings['storage']['volumes_ceph'] {
+        tweaks::ubuntu_service_override { 'cinder-volume':
+          package_name => 'cinder-volume',
+        }
+      }
+    }
+
     class { 'cinder::volume':
       package_ensure => $::openstack_version['cinder'],
       enabled        => true,
