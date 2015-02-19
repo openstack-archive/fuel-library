5496d102740b34c682345c869bb8286e9d938df8
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-02-19 18:25:20 +0200

    Add support of external DNS and NTP.
    Add support of using external DNS and NTP in HA mode.
    
    Change-Id: I9d741c638c1bd034ec3b451cffa1e70a3fee802a
    Implements: blueprint external-dns-ntp-support
    Closes-Bug: 1368514

---
+++ deployment/puppet/osnailyfacter/modular/dns/dns-server.pp
@@ -0,0 +1,14 @@
+notice('MODULAR: dns-server.pp')
+
+$dns_servers        = hiera('external_dns')
+$primary_controller = hiera('primary_controller')
+$master_ip          = hiera('master_ip')
+
+class { 'osnailyfacter::dnsmasq':
+  external_dns => strip(split($dns_servers['dns_list'], ',')),
+  master_ip    => $master_ip,
+} ->
+
+class { 'cluster::dns_ocf':
+  primary_controller => $primary_controller,
+}
