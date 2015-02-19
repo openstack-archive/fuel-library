fe276fb54593af1998b810b09b1abdd412148589
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-02-19 18:25:20 +0200

    Add support of external DNS and NTP.
    Add support of using external DNS and NTP in HA mode.
    
    Change-Id: I9d741c638c1bd034ec3b451cffa1e70a3fee802a
    Implements: blueprint external-dns-ntp-support
    Closes-Bug: 1368514

---
+++ deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
@@ -0,0 +1,9 @@
+notice('MODULAR: ntp-client.pp')
+
+$management_vip = hiera('management_vrouter_vip')
+
+class { 'ntp':
+  servers        => $management_vip,
+  service_ensure => running,
+  service_enable => true,
+}
