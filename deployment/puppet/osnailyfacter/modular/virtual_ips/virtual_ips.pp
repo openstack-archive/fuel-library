583a1823b53481e455a17940640a105b2554df89
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-02-19 16:09:47 +0200

    Updated OCF plugs network interfaces of namespaces directly into bridges with bridge type autodetection.
    Change-Id: Ice193fed4bbd27d164edaebe8a959f8ce0e5d6a8
    Blueprint: virtual-router-for-env-nodes
    Fuel-CI: disable

--- deployment/puppet/osnailyfacter/modular/virtual_ips/virtual_ips.pp
+++ deployment/puppet/osnailyfacter/modular/virtual_ips/virtual_ips.pp
@@ -1,6 +1,5 @@
 notice('MODULAR: virtual_ips.pp')
 
-
 $internal_int                = hiera('internal_int')
 $public_int                  = hiera('public_int',  undef)
 $primary_controller_nodes    = hiera('primary_controller_nodes', false)
@@ -14,46 +13,76 @@ $use_neutron                 = hiera('use_neutron')
 # }
 
 $vips = { # Do not convert to ARRAY, It can't work in 2.7
-  management   => {
-    namespace            => 'haproxy',
-    nic                  => $internal_int,
-    base_veth            => "${internal_int}-hapr",
-    ns_veth              => "hapr-m",
-    ip                   => hiera('management_vip'),
-    cidr_netmask         => $vip_management_cidr_netmask,
-    gateway              => 'link',
-    gateway_metric       => '20',
-    #other_networks       => $vip_mgmt_other_nets,
-    iptables_start_rules => "iptables -t mangle -I PREROUTING -i ${internal_int}-hapr -j MARK --set-mark 0x2b ; iptables -t nat -I POSTROUTING -m mark --mark 0x2b ! -o ${internal_int} -j MASQUERADE",
-    iptables_stop_rules  => "iptables -t mangle -D PREROUTING -i ${internal_int}-hapr -j MARK --set-mark 0x2b ; iptables -t nat -D POSTROUTING -m mark --mark 0x2b ! -o ${internal_int} -j MASQUERADE",
-    iptables_comment     => "masquerade-for-management-net",
-    tie_with_ping        => false,
-    ping_host_list       => "",
-  },
+    management   => {
+      namespace            => 'haproxy',
+      nic                  => $internal_int,
+      base_veth            => "${internal_int}-hapr",
+      ns_veth              => "hapr-m",
+      ip                   => hiera('management_vip'),
+      cidr_netmask         => $vip_management_cidr_netmask,
+      gateway              => 'link',
+      gateway_metric       => '20',
+      bridge               => $network_scheme['roles']['management'],
+      other_networks       => $vip_mgmt_other_nets,
+      iptables_start_rules => "iptables -t mangle -I PREROUTING -i ${internal_int}-hapr -j MARK --set-mark 0x2b ; iptables -t nat -I POSTROUTING -m mark --mark 0x2b ! -o $network_scheme['roles']['management'] -j MASQUERADE",
+      iptables_stop_rules  => "iptables -t mangle -D PREROUTING -i ${internal_int}-hapr -j MARK --set-mark 0x2b ; iptables -t nat -D POSTROUTING -m mark --mark 0x2b ! -o $network_scheme['roles']['management'] -j MASQUERADE",
+      iptables_comment     => "masquerade-for-management-net",
+      with_ping            => false,
+      ping_host_list       => "",
+    },
+    management_vrouter => {
+      namespace            => 'vrouter',
+      nic                  => $internal_int,
+      base_veth            => "${internal_int}-vrouter",
+      ns                   => 'vrouter',
+      ns_veth              => 'vr-mgmt',
+      ip                   => hiera('management_vrouter_vip'),
+      cidr_netmask         => $vip_management_cidr_netmask,
+      gateway              => 'none',
+      gateway_metric       => '0',
+      bridge               => $network_scheme['roles']['management'],
+      tie_with_ping        => false,
+      ping_host_list       => "",
+    },
 }
 
 if $public_int {
-  # if $use_neutron{
-  #   vip_publ_other_nets = join($network_scheme['endpoints']["$public_int"]['other_nets'], ' ')
-  # }
-
-  $vips[public] = {
-    namespace            => 'haproxy',
-    nic                  => $public_int,
-    base_veth            => "${public_int}-hapr",
-    ns_veth              => "hapr-p",
-    ip                   => hiera('public_vip'),
-    cidr_netmask         => $vip_public_cidr_netmask,
-    gateway              => 'link',
-    gateway_metric       => '10',
-    #other_networks       => $vip_publ_other_nets,
-    iptables_start_rules => "iptables -t mangle -I PREROUTING -i ${public_int}-hapr -j MARK --set-mark 0x2a ; iptables -t nat -I POSTROUTING -m mark --mark 0x2a ! -o ${public_int} -j MASQUERADE",
-    iptables_stop_rules  => "iptables -t mangle -D PREROUTING -i ${public_int}-hapr -j MARK --set-mark 0x2a ; iptables -t nat -D POSTROUTING -m mark --mark 0x2a ! -o ${public_int} -j MASQUERADE",
-    iptables_comment     => "masquerade-for-public-net",
-    tie_with_ping        => hiera('run_ping_checker', true),
-    ping_host_list       => $network_scheme['endpoints']['br-ex']['gateway'],
+  if $use_neutron{
+    $vip_publ_other_nets = join($network_scheme['endpoints']["$public_int"]['other_nets'], ' ')
   }
-}
+    $vips[public] = {
+      namespace            => 'haproxy',
+      nic                  => $public_int,
+      base_veth            => "${public_int}-hapr",
+      ns_veth              => 'hapr-p',
+      ip                   => hiera('public_vip'),
+      cidr_netmask         => $vip_public_cidr_netmask,
+      gateway              => 'link',
+      gateway_metric       => '10',
+      bridge               => $network_scheme['roles']['ex'],
+      other_networks       => $vip_publ_other_nets,
+      iptables_start_rules => "iptables -t mangle -I PREROUTING -i ${public_int}-hapr -j MARK --set-mark 0x2a ; iptables -t nat -I POSTROUTING -m mark --mark 0x2a ! -o $network_scheme['roles']['ex'] -j MASQUERADE",
+      iptables_stop_rules  => "iptables -t mangle -D PREROUTING -i ${public_int}-hapr -j MARK --set-mark 0x2a ; iptables -t nat -D POSTROUTING -m mark --mark 0x2a ! -o $network_scheme['roles']['ex'] -j MASQUERADE",
+      iptables_comment     => "masquerade-for-public-net",      
+      tie_with_ping        => hiera('run_ping_checker', true),
+      ping_host_list       => $network_scheme['endpoints']['br-ex']['gateway'],
+    }
+    $vips[public_vrouter] = {
+      namespace               => 'vrouter',
+      nic                     => $public_int,
+      base_veth               => "${public_int}-vrouter",
+      ns_veth                 => 'vr-ex',
+      ns                      => 'vrouter',
+      ip                      => hiera('public_vrouter_vip'),
+      cidr_netmask            => $vip_public_cidr_netmask,
+      gateway                 => $network_scheme['endpoints']['br-ex']['gateway'],
+      gateway_metric          => '0',
+      bridge                  => $network_scheme['roles']['ex'],
+      ns_iptables_start_rules => "iptables -t nat -A POSTROUTING -o vr-ex -j MASQUERADE",
+      ns_iptables_stop_rules  => "iptables -t nat -D POSTROUTING -o vr-ex -j MASQUERADE",
+      collocation             => 'management_vrouter',
+    }
+  }
 $vip_keys = keys($vips)
 
 class virtual_ips () {
