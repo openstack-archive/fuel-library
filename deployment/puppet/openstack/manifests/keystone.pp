498cb1d2479464e03a2b349cd696c8e7fb4a8f08
Andrew Woodward <awoodward@mirantis.com>
2015-02-21 13:15:37 -0800

    Move admin user and openrc up so that its not relied on
    The admin user defined in nailgun should not be used for any service
    purpose. This pulls the user out of the lower levels of the manifests
    so that it's harder to abuse this as a usage.
    
    Commonly other modules have an their own admin {user,password,tenant}
    parameters which should not be confused with this admin user which is
    only for the enduser to consume.
    
    Related-bug: #1396594
    Related-bug: #1347542
    
    Change-Id: Ie991a64617919727724638d582af301ee4519655

--- deployment/puppet/openstack/manifests/keystone.pp
+++ deployment/puppet/openstack/manifests/keystone.pp
@@ -8,15 +8,12 @@
 # [db_host] Host where DB resides. Required.
 # [keystone_db_password] Password for keystone DB. Required.
 # [keystone_admin_token]. Auth token for keystone admin. Required.
-# [admin_email] Email address of system admin. Required.
-# [admin_password]
 # [glance_user_password] Auth password for glance user. Required.
 # [nova_user_password] Auth password for nova user. Required.
 # [public_address] Public address where keystone can be accessed. Required.
 # [db_type] Type of DB used. Currently only supports mysql. Optional. Defaults to  'mysql'
 # [keystone_db_user] Name of keystone db user. Optional. Defaults to  'keystone'
 # [keystone_db_dbname] Name of keystone DB. Optional. Defaults to  'keystone'
-# [keystone_admin_tenant] Name of keystone admin tenant. Optional. Defaults to  'admin'
 # [verbose] Rather to print more verbose (INFO+) output. Optional. Defaults to false.
 # [debug] Rather to print even more verbose (DEBUG+) output. If true, would ignore verbose option.
 #    Optional. Defaults to false.
@@ -50,9 +47,6 @@ class openstack::keystone (
   $db_host,
   $db_password,
   $admin_token,
-  $admin_email,
-  $admin_user                  = 'admin',
-  $admin_password,
   $glance_user_password,
   $nova_user_password,
   $cinder_user_password,
@@ -62,7 +56,6 @@ class openstack::keystone (
   $db_type                     = 'mysql',
   $db_user                     = 'keystone',
   $db_name                     = 'keystone',
-  $admin_tenant                = 'admin',
   $verbose                     = false,
   $debug                       = false,
   $public_bind_host            = '0.0.0.0',
@@ -313,13 +306,6 @@ class openstack::keystone (
 
   if ($enabled) {
     # Setup the admin user
-    class { 'keystone::roles::admin':
-      admin        => $admin_user,
-      email        => $admin_email,
-      password     => $admin_password,
-      admin_tenant => $admin_tenant,
-    }
-    Exec <| title == 'keystone-manage db_sync' |> -> Class['keystone::roles::admin']
 
     # Setup the Keystone Identity Endpoint
     class { 'keystone::endpoint':
