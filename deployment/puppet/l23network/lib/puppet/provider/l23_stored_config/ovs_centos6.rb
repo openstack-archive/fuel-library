5f6241a21329624539850401114864a6045e3f8e
Stanislav Makar <smakar@mirantis.com>
2015-02-21 11:33:25 +0000

    l23_stored_config provider for CentOS6
    Change-Id: I66464730c479cd10178a58d05ae158d61c4e2ccc
    Blueprint: refactor-l23-linux-bridges
    Fuel-CI: disable

--- deployment/puppet/l23network/lib/puppet/provider/l23_stored_config/ovs_centos6.rb
+++ deployment/puppet/l23network/lib/puppet/provider/l23_stored_config/ovs_centos6.rb
@@ -11,5 +11,55 @@ Puppet::Type.type(:l23_stored_config).provide(:ovs_centos6, :parent => Puppet::P
 
   self.unlink_empty_files = true
 
+  def self.property_mappings
+    rv = super
+    rv.merge!({
+      :devicetype => 'DEVICETYPE',
+    })
+    return rv
+  end
+
+  def self.properties_fake
+    rv = super
+     rv.push(:devicetype)
+    return rv
+  end
+
+  #Dirty hack which writes config files for OVS
+  #bridges into /tmp directory
+  def select_file
+    if name == 'br-prv' or name == 'br-floating'
+      "/tmp/ifcfg-#{name}"
+    else
+      "#{self.class.script_directory}/ifcfg-#{name}"
+    end
+  end
+
+  def self.unmangle__if_type(val)
+    if val == :bridge
+      val = :OVSBridge
+    else
+      val.to_s.capitalize.intern
+    end
+  end
+
+  def self.mangle__if_type(val)
+    if val == :OVSBridge
+      val = :bridge
+    else
+      val.to_s.downcase.intern
+    end
+  end
+
+  #Dirty hack which deletes OVS bridges from patch OVS
+  #interfaces
+  def self.unmangle__bridge(val)
+    if val.length == 2
+      val.delete('br-prv') if val.include?('br-prv')
+      val.delete('br-floating') if val.include?('br-floating')
+      val
+    end
+  end
+
 end
 # vim: set ts=2 sw=2 et :
