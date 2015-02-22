318832a980495c2520955f2b3cbe4d5d1f08290d
Andrew Woodward <awoodward@mirantis.com>
2015-02-22 08:58:09 -0800

    Remove openrc/admin-token usage in q-agent-cleanup.py
    Removes the openrc/admin-token deps in q-agent-cleanup.py. Instead it will
    read the neutron service credentials out of neutron.conf. This has the
    advantage that it must be up to date for neutron to function so if the
    credential is modified it will have to be updated by the administrator.
    
    Related-bug: #1347542
    Closes-bug: #1396594
    
    Change-Id: Iee0968a079deacef24b56f34e9314b0ebecad0ae

--- deployment/puppet/cluster/files/q-agent-cleanup.py
+++ deployment/puppet/cluster/files/q-agent-cleanup.py
@@ -1,44 +1,72 @@
 #!/usr/bin/env python
-# flake8: noqa
-import re
-import time
-import os
-import sys
-import random
-import string
-import json
+import StringIO
 import argparse
+import functools
+import json
 import logging
 import logging.handlers
-import shlex
-import subprocess
-import StringIO
+import os
+import re
 import socket
-from neutronclient.neutron import client as q_client
-from keystoneclient.v2_0 import client as ks_client
-from keystoneclient.apiclient.exceptions import NotFound as ks_NotFound
+import subprocess
+import sys
+
+from ConfigParser import SafeConfigParser
+from neutronclient.neutron import client as n_client
+from time import sleep
 
 LOG_NAME = 'q-agent-cleanup'
 
 API_VER = '2.0'
 PORT_ID_PART_LEN = 11
-TMP_USER_NAME = 'tmp_neutron_admin'
 
+AUTH_KEYS={
+    'tenant_name': 'admin_tenant_name',
+    'username': 'admin_user',
+    'password': 'admin_password',
+    'auth_url': 'auth_uri',
+}
 
-def get_authconfig(cfg_file):
-    # Read OS auth config file
-    rv = {}
-    stripchars=" \'\""
+def get_auth_data(cfg_file, section='keystone_authtoken', keys=AUTH_KEYS):
+    cfg = SafeConfigParser()
     with open(cfg_file) as f:
-        for line in f:
-            rg = re.match(r'\s*export\s+(\w+)\s*=\s*(.*)',line)
-            if rg :
-                #Use shlex to unescape bash shell escape characters
-                value = "".join(x for x in
-                                shlex.split(rg.group(2).strip(stripchars)))
-                rv[rg.group(1).strip(stripchars)] = value
-    return rv
+        cfg.readfp(f)
+    auth_data = {}
+    for key, value in keys.iteritems():
+        auth_data[key] = cfg.get(section, value)
+    return auth_data
 
+RECOVERABLE = re.compile("""(HTTP\s+400\))|
+(400-\{\'message\'\:\s+\'\'\})|
+(\[Errno 111\]\s+Connection\s+refused)|
+(503\s+Service\s+Unavailable)|
+(504\s+Gateway\s+Time-out)|
+(\:\s+Maximum\s+attempts\s+reached)|
+(Unauthorized\:\s+bad\s+credentials)|
+(Max\s+retries\s+exceeded)|
+('*NoneType'*\s+object\s+has\s+no\s+attribute\s+'*__getitem__'*$)|
+(No\s+route\s+to\s+host$)|
+(Lost\s+connection\s+to\s+MySQL\s+server)
+""")
+
+def retry(count=2, delay=50, pattern=RECOVERABLE):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            i = 0
+            while True:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    if not pattern and not re.search(pattern):
+                        raise e
+                    i += 1
+                    if i >= count:
+                        raise e
+                    sleep(delay)
+
+        return wrapper
+    return decorator
 
 class NeutronCleaner(object):
     PORT_NAME_PREFIXES_BY_DEV_OWNER = {
@@ -80,9 +108,9 @@ class NeutronCleaner(object):
 
     RE__port_in_portlist = re.compile(r"^\s*\d+\:\s+([\w-]+)\:")  # 14: tap-xxxyyyzzz:
 
-    def __init__(self, openrc, options, log=None):
+    def __init__(self, options, log=None):
         self.log = log
-        self.auth_config = openrc
+        self.auth_data = get_auth_data(cfg_file=options.get('authconf'))
         self.options = options
         self.agents = {}
         self.debug = options.get('debug')
@@ -91,152 +119,42 @@ class NeutronCleaner(object):
             'l3':   self._reschedule_agent_l3,
         }
 
-        self._token = None
-        self._keystone = None
         self._client = None
-        self._need_cleanup_tmp_admin = False
-
-    def __del__(self):
-        if self._need_cleanup_tmp_admin and self._keystone and self._keystone.username:
-            try:
-                self._keystone.users.delete(self._keystone.users.find(username=self._keystone.username))
-            except:
-                # if we get exception while cleaning temporary account -- nothing harm
-                pass
-
-    def generate_random_passwd(self, length=13):
-        chars = string.ascii_letters + string.digits + '!@#$%^&*()'
-        random.seed = (os.urandom(1024))
-        return ''.join(random.choice(chars) for i in range(length))
 
     @property
-    def keystone(self):
-        if self._keystone is None:
-            ret_count = self.options.get('retries', 1)
-            tmp_passwd = self.generate_random_passwd()
-            while True:
-                if ret_count <= 0:
-                    self.log.error(">>> Keystone error: no more retries for connect to keystone server.")
-                    sys.exit(1)
-                try:
-                    a_token = self.options.get('auth-token')
-                    a_url = self.options.get('admin-auth-url')
-                    if a_token and a_url:
-                        self.log.debug("Authentication by predefined token.")
-                        # create keystone instance, authorized by service token
-                        ks = ks_client.Client(
-                            token=a_token,
-                            endpoint=a_url,
-                        )
-                        service_tenant = ks.tenants.find(name='services')
-                        auth_url = ks.endpoints.find(
-                                        service_id=ks.services.find(type='identity').id
-                                   ).internalurl
-                        # find and re-create temporary rescheduling-admin user with random password
-                        try:
-                            user = ks.users.find(username=TMP_USER_NAME)
-                            ks.users.delete(user)
-                        except ks_NotFound:
-                            # user not found, it's OK
-                            pass
-                        user = ks.users.create(TMP_USER_NAME, tmp_passwd, tenant_id=service_tenant.id)
-                        ks.roles.add_user_role(user, ks.roles.find(name='admin'), service_tenant)
-                        # authenticate newly-created tmp neutron admin
-                        self._keystone = ks_client.Client(
-                            username=user.username,
-                            password=tmp_passwd,
-                            tenant_id=user.tenantId,
-                            auth_url=auth_url,
-                        )
-                        self._need_cleanup_tmp_admin = True
-                    else:
-                        self.log.debug("Authentication by given credentionals.")
-                        self._keystone = ks_client.Client(
-                            username=self.auth_config['OS_USERNAME'],
-                            password=self.auth_config['OS_PASSWORD'],
-                            tenant_name=self.auth_config['OS_TENANT_NAME'],
-                            auth_url=self.auth_config['OS_AUTH_URL'],
-                        )
-                    break
-                except Exception as e:
-                    errmsg = str(e.message).strip()  # str() need, because keystone may use int as message in exception
-                    if re.search(r"Connection\s+refused$", errmsg, re.I) or \
-                       re.search(r"Connection\s+timed\s+out$", errmsg, re.I) or\
-                       re.search(r"Lost\s+connection\s+to\s+MySQL\s+server", errmsg, re.I) or\
-                       re.search(r"Service\s+Unavailable$", errmsg, re.I) or\
-                       re.search(r"'*NoneType'*\s+object\s+has\s+no\s+attribute\s+'*__getitem__'*$", errmsg, re.I) or \
-                       re.search(r"No\s+route\s+to\s+host$", errmsg, re.I):
-                        self.log.info(">>> Can't connect to {0}, wait for server ready...".format(self.auth_config['OS_AUTH_URL']))
-                        time.sleep(self.options.sleep)
-                    else:
-                        self.log.error(">>> Keystone error:\n{0}".format(e.message))
-                        raise e
-                ret_count -= 1
-        return self._keystone
-    @property
-    def token(self):
-        if self._token is None:
-            self._token = self._keystone.auth_token
-            #self.log.debug("Auth_token: '{0}'".format(self._token))
-        #todo: Validate existing token
-        return self._token
-
-    @property
+    @retry(count=self.options.retries, delay=self.options.sleep)
     def client(self):
         if self._client is None:
-            self._client = q_client.Client(
-                API_VER,
-                endpoint_url=self.keystone.endpoints.find(
-                                service_id=self.keystone.services.find(type='network').id
-                             ).adminurl,
-                token=self.token,
-            )
+            self._client = n_client.Client(API_VER, **self.auth_data)
         return self._client
 
-    def _neutron_API_call(self, method, *args):
-        ret_count = self.options.get('retries')
-        while True:
-            if ret_count <= 0:
-                self.log.error("Q-server error: no more retries for connect to server.")
-                return []
-            try:
-                rv = method (*args)
-                break
-            except Exception as e:
-                errmsg = str(e.message).strip()
-                if re.search(r"Connection\s+refused", errmsg, re.I) or\
-                   re.search(r"Connection\s+timed\s+out", errmsg, re.I) or\
-                   re.search(r"Lost\s+connection\s+to\s+MySQL\s+server", errmsg, re.I) or\
-                   re.search(r"503\s+Service\s+Unavailable", errmsg, re.I) or\
-                   re.search(r"No\s+route\s+to\s+host", errmsg, re.I):
-                    self.log.info("Can't connect to {0}, wait for server ready...".format(self.keystone.service_catalog.url_for(service_type='network')))
-                    time.sleep(self.options.sleep)
-                else:
-                    self.log.error("Neutron error:\n{0}".format(e.message))
-                    raise e
-            ret_count -= 1
-        return rv
-
+    @retry(count=self.options.retries, delay=self.options.sleep)
     def _get_agents(self,use_cache=True):
-        return self._neutron_API_call(self.client.list_agents)['agents']
+        return self.client.list_agents()['agents']
 
+    @retry(count=self.options.retries, delay=self.options.sleep)
     def _get_routers(self, use_cache=True):
-        return self._neutron_API_call(self.client.list_routers)['routers']
+        return self.client.list_routers()['routers']
 
+    @retry(count=self.options.retries, delay=self.options.sleep)
     def _get_networks(self, use_cache=True):
-        return self._neutron_API_call(self.client.list_networks)['networks']
+        return self.client.list_networks()['networks']
 
+    @retry(count=self.options.retries, delay=self.options.sleep)
     def _list_networks_on_dhcp_agent(self, agent_id):
-        return self._neutron_API_call(self.client.list_networks_on_dhcp_agent, agent_id)['networks']
+        return self.client.list_networks_on_dhcp_agent(agent_id)['networks']
 
+    @retry(count=self.options.retries, delay=self.options.sleep)
     def _list_routers_on_l3_agent(self, agent_id):
-        return self._neutron_API_call(self.client.list_routers_on_l3_agent, agent_id)['routers']
+        return self.client.list_routers_on_l3_agent(agent_id)['routers']
 
+    @retry(count=self.options.retries, delay=self.options.sleep)
     def _list_l3_agents_on_router(self, router_id):
-        return self._neutron_API_call(self.client.list_l3_agent_hosting_routers, router_id)['agents']
+        return self.client.list_l3_agent_hosting_routers(router_id)['agents']
 
+    @retry(count=self.options.retries, delay=self.options.sleep)
     def _list_dhcp_agents_on_network(self, network_id):
-        return self._neutron_API_call(self.client.list_dhcp_agent_hosting_networks, network_id)['agents']
+        return self.client.list_dhcp_agent_hosting_networks(network_id)['agents']
 
     def _list_orphaned_networks(self):
         networks = self._get_networks()
@@ -258,13 +176,21 @@ class NeutronCleaner(object):
         self.log.debug("_list_orphaned_routers:, got list of orphaned routers {0}".format(orphaned_routers))
         return orphaned_routers
 
+    @retry(count=self.options.retries, delay=self.options.sleep)
     def _add_network_to_dhcp_agent(self, agent_id, net_id):
-        return self._neutron_API_call(self.client.add_network_to_dhcp_agent, agent_id, {"network_id": net_id})
-    def _add_router_to_l3_agent(self, agent_id, router_id):
-        return self._neutron_API_call(self.client.add_router_to_l3_agent, agent_id, {"router_id": router_id})
+        return self.client.add_network_to_dhcp_agent(agent_id, {"network_id": net_id})
 
+    @retry(count=self.options.retries, delay=self.options.sleep)
+    def _add_router_to_l3_agent(self, agent_id, router_id):
+        return self.client.add_router_to_l3_agent(agent_id, {"router_id": router_id})
+
+    @retry(count=self.options.retries, delay=self.options.sleep)
     def _remove_router_from_l3_agent(self, agent_id, router_id):
-        return self._neutron_API_call(self.client.remove_router_from_l3_agent, agent_id, router_id)
+        return self.client.remove_router_from_l3_agent(agent_id, router_id)
+
+    @retry(count=self.options.retries, delay=self.options.sleep)
+    def _delete_agent(self, agent_id):
+        return self.client.delete_agent(agent_id)
 
     def _get_agents_by_type(self, agent, use_cache=True):
         self.log.debug("_get_agents_by_type: start.")
@@ -404,7 +330,7 @@ class NeutronCleaner(object):
                 for agent in agents['dead']:
                     self.log.info("remove dead DHCP agent: {0}".format(agent['id']))
                     if not self.options.get('noop'):
-                        self._neutron_API_call(self.client.delete_agent, agent['id'])
+                        self._delete_agent(agent['id'])
         orphaned_networks=self._list_orphaned_networks()
         self.log.info("_reschedule_agent_dhcp: rescheduling orphaned networks")
         if orphaned_networks and agents['alive']:
@@ -450,7 +376,7 @@ class NeutronCleaner(object):
             for agent in agents['dead']:
                 self.log.info("remove dead L3 agent: {0}".format(agent['id']))
                 if not self.options.get('noop'):
-                    self._neutron_API_call(self.client.delete_agent, agent['id'])
+                    self._delete_agent(agent['id'])
             # move routers from dead to alive agent
             for rou in filter(lambda rr: not(rr[0]['id'] in lucky_ids), dead_routers):
                 # self.log.info("unschedule router {rou} from L3 agent {agent}".format(
@@ -484,7 +410,7 @@ class NeutronCleaner(object):
             if agent['host'] == socket.gethostname():
                self.log.info("_remove_self: deleting our own agent {0} of type {1}".format(agent['id'],agent_type))
                if not self.options.get('noop'):
-                   self._neutron_API_call(self.client.delete_agent, agent['id'])
+                   self._delete_agent(agent['id'])
         self.log.debug("_remove_self: end.")
 
 
@@ -521,12 +447,10 @@ class NeutronCleaner(object):
         return rc
 
 
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Neutron network node cleaning tool.')
-    parser.add_argument("-c", "--auth-config", dest="authconf", default="/root/openrc",
-                      help="Authenticating config FILE", metavar="FILE")
+    parser.add_argument("-c", "--auth-config", dest="authconf", default="/etc/neutron/neutron.conf",
+                      help="Read authconfig from service file", metavar="FILE")
     parser.add_argument("-t", "--auth-token", dest="auth-token", default=None,
                       help="Authenticating token (instead username/passwd)", metavar="TOKEN")
     parser.add_argument("-u", "--admin-auth-url", dest="admin-auth-url", default=None,
