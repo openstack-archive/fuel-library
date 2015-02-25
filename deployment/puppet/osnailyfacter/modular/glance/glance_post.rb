c1946ba59fb369f364dcd8a28899ae917718dab2
Andrew Woodward <awoodward@mirantis.com>
2015-02-24 17:12:12 -0800

    Dont rely on /root/openrc
    Instead of relying on the presence of /root/openrc file all calls should
    asume it doesn't exist and pass credentials, or set up the env vars
    themselves. In these cases we use existing service users instead of the
    more volitile admin credentials.
    
    We still create /root/openrc, but it's use is only for the end user.
    
    Also removes the old and unused openstack::test_file
    
    Related-bug: #1396594
    Related-bug: #1347542
    
    Change-Id: I66a20d09d8515f21a7c71a6b8056bc483f6ea3f6

--- deployment/puppet/osnailyfacter/modular/glance/glance_post.rb
+++ deployment/puppet/osnailyfacter/modular/glance/glance_post.rb
@@ -2,6 +2,12 @@ require File.join File.dirname(__FILE__), '../test_common.rb'
 
 PORT = 9292
 
+ENV['OS_TENANT_NAME']="services"
+ENV['OS_USERNAME']="glance"
+ENV['OS_PASSWORD']="#{Settings.glance['user_password']}"
+ENV['OS_AUTH_URL']="http://#{Settings.management_vip}:5000/v2.0"
+ENV['OS_ENDPOINT_TYPE'] = "internalURL"
+
 class GlancePostTest < Test::Unit::TestCase
 
   def test_glance_api_is_running
@@ -23,7 +29,7 @@ class GlancePostTest < Test::Unit::TestCase
   end
 
   def test_keystone_endpoint_list_run
-    cmd = 'source /root/openrc && glance image-list'
+    cmd = 'glance image-list'
     assert TestCommon::Process.run_successful?(cmd), "Could not run '#{cmd}'!"
   end
 
--- deployment/puppet/osnailyfacter/modular/keystone/keystone_post.rb
+++ deployment/puppet/osnailyfacter/modular/keystone/keystone_post.rb
@@ -3,6 +3,14 @@ require File.join File.dirname(__FILE__), '../test_common.rb'
 PUBLIC_PORT = 5000
 ADMIN_PORT = 35357
 
+# Keystone doen't have a user, so we'd have to use the admin token, or use
+# another user like nova.
+ENV['OS_TENANT_NAME']="services"
+ENV['OS_USERNAME']="nova"
+ENV['OS_PASSWORD']="#{Settings.nova['user_password']}"
+ENV['OS_AUTH_URL']="http://#{Settings.management_vip}:#{PUBLIC_PORT}/v2.0"
+ENV['OS_ENDPOINT_TYPE'] = "internalURL"
+
 class KeystonePostTest < Test::Unit::TestCase
 
   def test_keystone_is_running
@@ -20,7 +28,7 @@ class KeystonePostTest < Test::Unit::TestCase
   end
 
   def test_keystone_endpoint_list_run
-    cmd = 'source /root/openrc && keystone endpoint-list'
+    cmd = 'keystone endpoint-list'
     assert TestCommon::Process.run_successful?(cmd), "Could not run '#{cmd}'!"
   end
 
