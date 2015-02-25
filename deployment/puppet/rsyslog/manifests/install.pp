ccacb4a09b2f0afdcc4d7c0e17991140f189c86a
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-02-25 20:26:54 +0300

    Add crontabs and anacron to rsyslog deps
    Closes race condition for rsyslog container
    trying to add /etc/cron.daily/anacron before
    crontabs package is installed.
    
    Change-Id: I226776d4aaf34ff874c78d78d4264d1118df7617
    Closes-Bug: #1425605

--- deployment/puppet/rsyslog/manifests/install.pp
+++ deployment/puppet/rsyslog/manifests/install.pp
@@ -10,4 +10,9 @@ class rsyslog::install {
         ensure => $rsyslog::params::package_status
     }
   }
+  if $rsyslog::params::additional_packages {
+    package { $rsyslog::params::rsyslog_additional_packages:
+        ensure => $rsyslog::params::package_status
+    }
+  }
 }
