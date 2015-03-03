603bcebc5c1f25f0e88a77329384e9b5213ff79c
Stanislav Makar <smakar@mirantis.com>
2015-03-03 16:37:56 +0000

    Add xmit_hash_policy support for CentOS6
    Change-Id: I3542e84e0ba21f93ee2c15b83211e0ee50ce2aa9
    Blueprint: refactor-l23-linux-bridges

--- deployment/puppet/l23network/lib/puppet/type/l23_stored_config.rb
+++ deployment/puppet/l23network/lib/puppet/type/l23_stored_config.rb
@@ -199,9 +199,9 @@ Puppet::Type.newtype(:l23_stored_config) do
   end
 
   newproperty(:bond_mode)
-  newproperty(:bond_xmit_hash_policy)
   newproperty(:bond_miimon)
   newproperty(:bond_lacp_rate)
+  newproperty(:bond_xmit_hash_policy)
 
   # # `:options` provides an arbitrary passthrough for provider properties, so
   # # that provider specific behavior doesn't clutter up the main type but still
