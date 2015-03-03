5a298be12c48ff77666f0cb7911968342c98ee0d
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-03-03 16:03:59 +0400

    Add OSWL collector user
    Change-Id: Idfc669b882c2c32a3805b36ffc58b27c58048e6d
    Partial-blueprint: blueprint openstack-workload-statistics
    Closes-Bug: #142720

---
+++ deployment/puppet/openstack/manifests/workloads_collector.pp
@@ -0,0 +1,35 @@
+# == Class: openstack::workloads_collector
+#
+# Creates a keystone user to connect workload statistics
+# from a running OpenStack environment.
+#
+# === Parameters
+#
+# [*workloads_password*]
+#   (required) Password.
+# [*enabled*]
+#   (optional) Creates the user. Defaults to true.
+# [*workloads_user*]
+#   (optional) Defaults to 'workloads_collector'.
+# [*tenant*]
+#   (optional) Defaults to 'services'.
+#
+class openstack::workloads_collector(
+  $workloads_password,
+  $enabled            = true,
+  $workloads_username = 'workloads_collector',
+  $workloads_tenant   = 'services',
+) {
+
+  keystone_user { $workloads_username:
+    ensure          => present,
+    password        => $workloads_password,
+    enabled         => $enabled,
+    tenant          => 'services',
+  }
+
+  keystone_user_role { "$workloads_username@$workloads_tenant":
+    ensure => present,
+    roles  => ['admin'],
+  }
+}
