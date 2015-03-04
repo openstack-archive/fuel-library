4e1fac66071492199504bf2bcb5c797a5ac071ad
Stanislav Makar <smakar@mirantis.com>
2015-03-04 13:59:30 +0000

    Add the Ceph ready checking
    Fix the problem in environments where there are a lot of Ceph OSDs.
    
    Closes-bug: #1415954
    Change-Id: If7ae4948978047e1dd533e300eb1bfdc61b51035

---
+++ deployment/puppet/osnailyfacter/modular/astute/ceph_ready_check.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+require 'hiera'
+
+ENV['LANG'] = 'C'
+
+hiera = Hiera.new(:config => '/etc/hiera.yaml')
+
+def ready_ceph
+  #Getting the pool size
+  pool_size_output = `grep osd_pool_default_size /etc/ceph/ceph.conf`
+  return false if $?.exitstatus != 0
+  pool_size = pool_size_output.split('=')[1].to_i
+  #Getting the number of ceph osds, which are in cluster
+  osds_in_output = `ceph osd stat`
+  return false if $?.exitstatus != 0
+  osds_in = osds_in_output.split('osds:')[1].split(',')[1].split(' ')[0].to_i
+  #Check whether placement group is in active state
+  active_pg = false
+  active_pg_output = `ceph pg stat | grep -q active`
+  active_pg = true if $?.exitstatus == 0
+  #If we have enough osds in cluster and the placement group is in ready state then we can use ceph
+  #http://ceph.com/docs/master/rados/operations/monitoring-osd-pg/
+  return true  if osds_in >= pool_size and active_pg
+  false
+end
+
+# check if ceph cluster is ready
+def wait_for_ready_ceph
+  180.times do |retries|
+    return if ready_ceph
+    sleep 10
+  end
+  raise 'Ceph is not ready yet!'
+end
+
+storage_hash = hiera.lookup('storage', false, {})
+
+wait_for_ready_ceph if storage_hash['images_ceph'] or storage_hash['volumes_ceph'] or storage_hash['objects_ceph'] or storage_hash['ephemeral_ceph']
