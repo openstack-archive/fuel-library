16f8d2e46ba93510a83c04bb3a20b455e061be8b
Sergey Vasilenko <svasilenko@mirantis.com>
2015-03-10 17:31:12 +0300

    L23network::L3::Route and L3_route resources
    This need for:
    * re-implement multiple-l2
    * stored_config for ethtool
    
    done for l3_route:
    + prefetch
    + create
    + modify fields
    + delete
    + stored_config for Ubuntu
    
    Change-Id: Ic2e8fcd341c1e0d6281ceb3709b81a095d2d5b06
    Blueprint: refactor-l23-linux-bridges
    Partial-Bug: #1429190

---
+++ deployment/puppet/l23network/lib/puppet/parser/functions/get_route_resource_name.rb
@@ -0,0 +1,13 @@
+require 'puppetx/l23_utils'
+#
+module Puppet::Parser::Functions
+  newfunction(:get_route_resource_name, :type => :rvalue) do |argv|
+    if argv.size < 1 and argv.size > 2
+      raise(Puppet::ParseError, "get_route_resource_name(): Wrong arguments given. " +
+        "Should be network CIDR (or default) and optionat metric positive value.")
+    end
+
+    L23network.get_route_resource_name(argv[0], argv[1])
+  end
+end
+# vim: set ts=2 sw=2 et :
