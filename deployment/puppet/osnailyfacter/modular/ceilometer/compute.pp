6f28dfde279e1602a792fd173cc6f42cd92348b0
Dmitry Ilyin <dilyin@mirantis.com>
2015-03-11 17:35:06 +0300

    Ceilometer task
    * Compute and controller parts
    
    Related blueprint: fuel-library-modularization
    
    Change-Id: Id4b44549bf66b7df9034ae2556a2fd4b45f99f29

---
+++ deployment/puppet/osnailyfacter/modular/ceilometer/compute.pp
@@ -0,0 +1,48 @@
+notice('ceilometer/compute.pp')
+
+$verbose                  = hiera('verbose', true)
+$debug                    = hiera('debug', false)
+$use_syslog               = hiera('use_syslog', true)
+$syslog_log_facility      = hiera('syslog_log_facility_ceilometer', 'LOG_LOCAL0')
+$amqp_hosts               = hiera('amqp_hosts')
+$rabbit_hash              = hiera('rabbit_hash')
+$management_vip           = hiera('management_vip')
+
+$default_ceilometer_hash = {
+  'enabled'         => false,
+  'db_password'     => 'ceilometer',
+  'user_password'   => 'ceilometer',
+  'metering_secret' => 'ceilometer',
+}
+
+$ceilometer_hash          = hiera('ceilometer', $default_ceilometer_hash)
+
+$ceilometer_enabled         = $ceilometer_hash['enabled']
+$amqp_password              = $rabbit_hash['password']
+$amqp_user                  = $rabbit_hash['user']
+$service_endpoint           = $management_vip
+$ceilometer_user_password   = $ceilometer_hash['user_password']
+$ceilometer_metering_secret = $ceilometer_hash['metering_secret']
+
+if ($ceilometer_enabled) {
+  class { 'openstack::ceilometer':
+    verbose                        => $verbose,
+    debug                          => $debug,
+    use_syslog                     => $use_syslog,
+    syslog_log_facility            => $syslog_log_facility,
+    amqp_hosts                     => $amqp_hosts,
+    amqp_user                      => $amqp_user,
+    amqp_password                  => $amqp_password,
+    keystone_host                  => $service_endpoint,
+    keystone_password              => $ceilometer_user_password,
+    on_compute                     => true,
+    metering_secret                => $ceilometer_metering_secret,
+  }
+
+  # We need to restart nova-compute service in orderto apply new settings
+  include ::nova::params
+  service { 'nova-compute':
+    ensure => 'running',
+    name   => $::nova::params::compute_package_name,
+  }
+}
