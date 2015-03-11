57aa0d79db512e8af6f39397d9370be9d1fcf8ca
Dmitry Ilyin <dilyin@mirantis.com>
2015-03-11 17:35:06 +0300

    Ceilometer task
    * Compute and controller parts
    * Fixes typo in openstack-network-compute.pp
    * Pre/Post tests require I8769748ae79d5a1597eb0f320e0a0ed9581d5a0b
    
    Related-blueprint: fuel-library-modularization
    
    Change-Id: Id4b44549bf66b7df9034ae2556a2fd4b45f99f29

---
+++ deployment/puppet/osnailyfacter/modular/ceilometer/compute_post.rb
@@ -0,0 +1,17 @@
+require File.join File.dirname(__FILE__), '../test_common.rb'
+
+PORT = 8777
+
+PROCESSES = %w(
+ceilometer-agent-compute
+)
+
+class CeilometerComputePostTest < Test::Unit::TestCase
+
+  def test_ceilometer_processes_running
+    PROCESSES.each do |process|
+      assert TestCommon::Process.running?(process), "'#{process}' is not running!"
+    end
+  end
+
+end
