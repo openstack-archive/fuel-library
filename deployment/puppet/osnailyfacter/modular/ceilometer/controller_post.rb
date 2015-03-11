1973fd39b10960db261550beee2cf8e6620ac8c8
Dmitry Ilyin <dilyin@mirantis.com>
2015-03-11 17:35:06 +0300

    Ceilometer task
    * Compute and controller parts
    
    Related blueprint: fuel-library-modularization
    
    Fuel-CI: disable
    Change-Id: Id4b44549bf66b7df9034ae2556a2fd4b45f99f29

---
+++ deployment/puppet/osnailyfacter/modular/ceilometer/controller_post.rb
@@ -0,0 +1,47 @@
+require File.join File.dirname(__FILE__), '../test_common.rb'
+
+PORT = 8777
+
+PROCESSES = %w(
+ceilometer-collector
+ceilometer-agent-central
+ceilometer-alarm-notifier
+ceilometer-alarm-evaluator
+ceilometer-api
+)
+
+PACEMAKER_SERVICES = %w(
+p_openstack-ceilometer-central
+p_openstack-ceilometer-alarm-evaluator
+)
+
+class CeilometerControllerPostTest < Test::Unit::TestCase
+
+  def test_ceilometer_processes_running
+    PROCESSES.each do |process|
+      assert TestCommon::Process.running?(process), "'#{process}' is not running!"
+    end
+  end
+
+  def test_ceilometer_api_public_url_accessible
+    url = "http://#{TestCommon::Settings.public_vip}:#{PORT}"
+    assert TestCommon::Network.url_accessible?(url), "Public Ceilometer API URL '#{url}' is not accessible!"
+  end
+
+  def test_ceilometer_api_management_url_accessible
+    url = "http://#{TestCommon::Settings.management_vip}:#{PORT}"
+    assert TestCommon::Network.url_accessible?(url), "Admin Ceilometer API URL '#{url}' is not accessible!"
+  end
+
+  def test_ceilometer_meter_list_run
+    cmd = 'source /root/openrc && ceilometer meter-list'
+    assert TestCommon::Process.run_successful?(cmd), "Could not run '#{cmd}'!"
+  end
+
+  def test_pacemaker_services_running
+    PACEMAKER_SERVICES.each do |service|
+      assert TestCommon::Pacemaker.primitive_started?(service), "Pacemaker service '#{service}' is not running!"
+    end
+  end
+
+end
