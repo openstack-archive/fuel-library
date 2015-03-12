b219c61223c1f9b5b1f820f51aa361459cb0b445
Sergey Vasilenko <svasilenko@mirantis.com>
2015-03-12 13:43:07 +0300

    prepare env for use rspec for l23network
    Partial-bug: #1310646
    Blueprint: refactor-l23-linux-bridges
    Change-Id: If0d3f7d5599640df2586ee87f428b4e30b27e159

---
+++ deployment/puppet/l23network/spec/functions/get_pair_of_jack_names__spec.rb
@@ -0,0 +1,39 @@
+# require 'puppet'
+# require 'rspec'
+# require 'rspec-puppet'
+require 'spec_helper'
+require 'puppetlabs_spec_helper/puppetlabs_spec/puppet_internals'
+
+describe 'get_pair_of_jack_names' do
+  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
+
+  subject do
+    function_name = Puppet::Parser::Functions.function(:get_pair_of_jack_names)
+    scope.method(function_name)
+  end
+
+  context "t1" do
+
+    it 'should exist' do
+      subject == Puppet::Parser::Functions.function(:get_pair_of_jack_names)
+    end
+
+    it 'should throw an error on invalid types' do
+      should run.with_params({:foo => :bar}).and_raise_error(Puppet::ParseError)
+    end
+
+    it 'should throw an error on invalid arguments number' do
+      should run.with_params().and_raise_error(Puppet::ParseError)
+      should run.with_params([1,2],[3,4]).and_raise_error(Puppet::ParseError)
+    end
+
+    it 'should return numbered interface names' do
+      should run.with_params(['br-mgmt', 'br-ex']).and_return(["p_br-mgmt-0", "p_br-ex-1"])
+    end
+
+    it 'should cut interface names for long interfaces' do
+      should run.with_params(['br-mmmmmmmmmmmmmmmmmmmmmmmmgmt', 'br-ex']).and_return(["p_br-mmmmmmmm-0", "p_br-ex-1"])
+    end
+
+  end
+end
