bae13f55056eff7713417cbe8176df1a8d0746e4
Sergey Vasilenko <svasilenko@mirantis.com>
2015-03-12 13:43:07 +0300

    prepare env for use rspec for l23network
    Partial-bug: #1310646
    Blueprint: refactor-l23-linux-bridges
    Change-Id: If0d3f7d5599640df2586ee87f428b4e30b27e159

--- deployment/puppet/l23network/spec/spec_helper.rb
+++ deployment/puppet/l23network/spec/spec_helper.rb
@@ -2,6 +2,14 @@ require 'rspec-puppet'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
+
+# Add fixture lib dirs to LOAD_PATH. Work-around for PUP-3336
+if Puppet.version < "4.0.0"
+  Dir["#{fixture_path}/modules/*/lib"].entries.each do |lib_dir|
+    $LOAD_PATH << lib_dir
+  end
+end
+
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
