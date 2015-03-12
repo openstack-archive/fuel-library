e798958dd0d33da4f41eaf73337d81a9e8db0dd1
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-03-12 12:11:50 +0200

    Test timesync in CI
    Change-Id: I263e2fc3b9fe421616a378ad74327a7be03c7ec1

--- deployment/puppet/osnailyfacter/modular/ntp/timesync.pp
+++ deployment/puppet/osnailyfacter/modular/ntp/timesync.pp
@@ -1,17 +1,40 @@
 notice('MODULAR: timesync.pp')
 
-
-### To be updated by O.Molchanov ###
-#$server_list = hiera('external_ntp')
-#$ntp_list    = regsubst($server_list['ntp_list'], ',', ' ')
-$ntp_list = hiera('master_ip')
+$server_list = hiera('external_ntp')
+$ntp_list    = regsubst($server_list['ntp_list'], ',', ' ')
+#$ntp_list = hiera('master_ip')
 
 case $operatingsystem {
   Centos: { $ntp_service = "ntpd" }
   Ubuntu: { $ntp_service = "ntp" }
 }
 
-exec { "Initial time sync":
-  command => "service $ntp_service stop; killall $ntp_service; ntpdate $ntp_list",
+exec { 'Show ifaces':
+  command => "ip a",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  logoutput => true,
+} ->
+
+exec { 'Show routes':
+  command => "route -n",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  logoutput => true,
+} ->
+
+exec { 'Ping name':
+  command => "ping 0.pool.ntp.org",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  logoutput => true,
+} ->
+
+exec { 'Ping IP':
+  command => "ping 8.8.8.8",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  logoutput => true,
+} ->
+
+exec { "Initial time sync":
+  command => "service $ntp_service stop; killall $ntp_service; ntpdate -db $ntp_list",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  logoutput => true,
 }
