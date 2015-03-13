679b531772ff9522e9e17e88747c5c56d00b8b2c
Alexei Sheplyakov <asheplyakov@mirantis.com>
2015-03-13 11:24:14 +0300

    cluster: install pcs package on Debian/Ubuntu
    The python-pcs package is the packet construction set [1]. This package
    has been shipped with Debian/Ubuntu for quite a long time.
    For some reason the pacemaker/corosync configuration system [2] has been
    packaged for Ubuntu under the name `python-pcs'. Use the correct name
    (that is, `pcs', like its RPM counterpart) to resolve the collision.
    
    [1] https://github.com/gvnn3/PCS
    [2] https://github.com/feist/pcs
    
    Note: this patch should be merged along with the following ones:
    https://review.fuel-infra.org/4522
    https://review.openstack.org/164111
    
    blueprint support-ubuntu-trusty
    
    Change-Id: I460b550869d0fe0c4742c198ebc8c54b9548eae8
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/cluster/manifests/init.pp
+++ deployment/puppet/cluster/manifests/init.pp
@@ -10,33 +10,20 @@ class cluster (
     #todo: move half of openstack::corosync
     #to this module, another half -- to Neutron
 
-    case $::osfamily {
-      'RedHat': {
-        $pcs_package = 'pcs'
-      }
-      'Debian': {
-        $pcs_package = 'python-pcs'
-      }
-       default: {
-        fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem},\
- module ${module_name} only support osfamily RedHat and Debian")
-      }
-    }
-
     if defined(Stage['corosync_setup']) {
       class { 'openstack::corosync':
         bind_address      => $internal_address,
         unicast_addresses => $unicast_addresses,
         stage             => 'corosync_setup',
         corosync_version  => '2',
-        packages          => ['corosync', 'pacemaker', 'crmsh', $pcs_package],
+        packages          => ['corosync', 'pacemaker', 'crmsh', 'pcs'],
       }
     } else {
       class { 'openstack::corosync':
         bind_address      => $internal_address,
         unicast_addresses => $unicast_addresses,
         corosync_version  => '2',
-        packages          => ['corosync', 'pacemaker', 'crmsh', $pcs_package],
+        packages          => ['corosync', 'pacemaker', 'crmsh', 'pcs'],
       }
     }
 
