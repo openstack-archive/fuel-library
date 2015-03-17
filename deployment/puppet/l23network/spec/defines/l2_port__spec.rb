394c70a3d9798873911a5935f3748b368a117ae6
Sergey Vasilenko <svasilenko@mirantis.com>
2015-03-18 02:28:14 +0300

    Spec-tests for l23network::l2::port
    Partial-bug: #1310646
    Blueprint: refactor-l23-linux-bridges
    
    Change-Id: I8ca14db3593fa58392e8b8df52f52df31ed7ee7f

---
+++ deployment/puppet/l23network/spec/defines/l2_port__spec.rb
@@ -0,0 +1,121 @@
+require 'rspec-puppet'
+require 'spec_helper'
+require 'puppetlabs_spec_helper/puppetlabs_spec/puppet_internals'
+
+# Ubintu, manual -- no IP addresses, but interface in UP state
+#describe 'l23network::l3::ifconfig', :type => :define do
+#  let(:module_path) { '../' }
+#describe 'l23network::l3::ifconfig', :type => :define do
+describe 'l23network::l2::port', :type => :define do
+  let(:title) { 'Spec for l23network::l2::port' }
+  let(:facts) { {
+    :osfamily => 'Debian',
+    :operatingsystem => 'Ubuntu',
+    :kernel => 'Linux',
+    :l23_os => 'ubuntu',
+    :l3_fqdn_hostname => 'stupid_hostname',
+  } }
+
+  context 'Port without anythyng' do
+    let(:params) do
+      {
+        :name => 'eth4',
+      }
+    end
+
+    it do
+      should compile
+    end
+
+    it do
+      should contain_l23_stored_config('eth4').only_with({
+        'use_ovs' => nil,
+        'method'  => nil,
+        'ipaddr'  => nil,
+        'gateway' => nil,
+      })
+    end
+
+    it do
+      should contain_l2_port('eth4').only_with({
+        'ensure'  => 'present',
+        'use_ovs' => nil,
+      })
+      #.requires('L23_stored_config')
+    end
+  end
+
+  context 'Native linux subinterface' do
+    let(:params) do
+      {
+        :name => 'eth4.102',
+      }
+    end
+
+    it do
+      should compile
+    end
+
+    it do
+      should contain_l23_stored_config('eth4.102').only_with({
+        'use_ovs'   => nil,
+        'method'    => nil,
+        'ipaddr'    => nil,
+        'gateway'   => nil,
+        'vlan_id'   => '102',
+        'vlan_dev'  => 'eth4',
+        'vlan_mode' => 'eth'
+      })
+    end
+
+    it do
+      should contain_l2_port('eth4.102').only_with({
+        'ensure'  => 'present',
+        'use_ovs'   => nil,
+        'vlan_id'   => '102',
+        'vlan_dev'  => 'eth4',
+        'vlan_mode' => 'eth'
+      })
+      #.requires('L23_stored_config')
+    end
+  end
+
+  context 'Alternative VLAN definition' do
+    let(:params) do
+      {
+        :name     => 'vlan102',
+        :vlan_dev => 'eth4',
+        :vlan_id  => '102',
+      }
+    end
+
+    it do
+      should compile
+    end
+
+    it do
+      should contain_l23_stored_config('vlan102').only_with({
+        'use_ovs'   => nil,
+        'method'    => nil,
+        'ipaddr'    => nil,
+        'gateway'   => nil,
+        'vlan_id'   => '102',
+        'vlan_dev'  => 'eth4',
+        'vlan_mode' => 'vlan'
+      })
+    end
+
+    it do
+      should contain_l2_port('vlan102').only_with({
+        'ensure'  => 'present',
+        'use_ovs'   => nil,
+        'vlan_id'   => '102',
+        'vlan_dev'  => 'eth4',
+        'vlan_mode' => 'vlan'
+      })
+      #.requires('L23_stored_config')
+    end
+  end
+
+end
+###
