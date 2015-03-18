e9146c8f6fa903e04fdda6e57e374362bf0c623a
Dmitry Ilyin <dilyin@mirantis.com>
2015-03-18 18:39:40 +0300

    Refactor pre/post modular tests
    * Use common library for tests
    
    Related blueprint: fuel-library-modularization
    Fuel-CI: disable
    
    Change-Id: I8769748ae79d5a1597eb0f320e0a0ed9581d5a0b

--- deployment/puppet/osnailyfacter/modular/ceph/radosgw_post.rb
+++ deployment/puppet/osnailyfacter/modular/ceph/radosgw_post.rb
@@ -1,71 +1,11 @@
-require 'test/unit'
-require 'socket'
-
-def process_tree
-  return $process_tree if $process_tree
-  $process_tree = {}
-  ps = `ps haxo pid,ppid,cmd`
-  ps.split("\n").each do |p|
-    f = p.split
-    pid = f.shift.to_i
-    ppid = f.shift.to_i
-    cmd = f.join ' '
-
-    # create entry for this pid if not present
-    $process_tree[pid] = {
-        :children => []
-    } unless $process_tree.key? pid
-
-    # fill this entry
-    $process_tree[pid][:ppid] = ppid
-    $process_tree[pid][:pid] = pid
-    $process_tree[pid][:cmd] = cmd
-
-    unless ppid == 0
-      # create entry for parent process if not present
-      $process_tree[ppid] = {
-          :children => [],
-          :cmd => '',
-      } unless $process_tree.key? ppid
-
-      # fill parent's children
-      $process_tree[ppid][:children] << pid
-    end
-  end
-  $process_tree
-end
-
-def test_connection(host, port)
-  begin
-    s = TCPSocket.open(host, port)
-    s.close
-  rescue
-    return false
-  end
-  true
-end
-
-def radosgw_backend_online?
-  test_connection('localhost', '6780')
-end
-
-PROCESSES = %w(
-radosgw
-)
+require File.join File.dirname(__FILE__), '../test_common.rb'
 
 class RadosgwPostTest < Test::Unit::TestCase
-  def self.create_tests
-    PROCESSES.each do |process|
-      method_name = "test_iprocess_#{process}_running"
-      define_method method_name do
-        assert process_tree.find { |pid, proc| proc[:cmd].include? process }, "Process '#{process}' is not running!"
-      end
-    end
+  def test_radosgw_process_running
+    assert TestCommon::Process.running?('radosgw'), 'Radosgw process is not running!'
   end
 
   def test_radosgw_backend_online
-    assert radosgw_backend_online?, 'Can not connect to radoswg on this host!'
+    assert TestCommon::Network.connection?('localhost', 6780), 'Can not connect to radoswg port 6780 on this host!'
   end
 end
-
-RadosgwPostTest.create_tests
--- deployment/puppet/osnailyfacter/modular/netconfig/netconfig_post.rb
+++ deployment/puppet/osnailyfacter/modular/netconfig/netconfig_post.rb
@@ -1,86 +1,27 @@
-require 'hiera'
-require 'test/unit'
-
-def ips
-  return $ips if $ips
-  ip_out = `ip addr`
-  return unless $?.exitstatus == 0
-  ips = []
-  ip_out.split("\n").each do |line|
-    if line =~ /\s+inet\s+([\d\.]*)/
-      ips << $1
-    end
-  end
-  $ips = ips
-end
-
-def router
-  return $router if $router
-  routes = `ip route`
-  return unless $?.exitstatus == 0
-  routes.split("\n").each do |line|
-    if line =~ /^default via ([\d\.]*)/
-      return $router = $1
-    end
-  end
-  nil
-end
-
-def ping(host)
-  `ping -q -c 1 -W 3 '#{host}'`
-  $?.exitstatus == 0
-end
-
-def hiera
-  return $hiera if $hiera
-  $hiera = Hiera.new(:config => '/etc/puppet/hiera.yaml')
-end
-
-def fqdn
-  return $fqdn if $fqdn
-  $fqdn = hiera.lookup 'fqdn', nil, {}
-end
-
-def nodes
-  return $nodes if $nodes
-  $nodes = hiera.lookup 'nodes', [], {}
-end
-
-def role
-  return $role if $role
-  $role = hiera.lookup 'role', nil, {}
-end
-
-def master_ip
-  return $master_ip if $master_ip
-  $master_ip = hiera.lookup 'master_ip', nil, {}
-end
+require File.join File.dirname(__FILE__), '../test_common.rb'
 
 class NetconfigPostTest < Test::Unit::TestCase
 
   def test_management_ip_present
-    ip = nodes.find { |node| node['fqdn'] == fqdn }['internal_address']
-    assert ips.include?(ip), 'Management address is not set!'
+    ip = TestCommon::Settings.internal_address
+    assert TestCommon::Network.ips.include?(ip), 'Management address is not set!'
   end
 
   def test_public_ip_present
-    if %w(controller primary-controller).include? role
-      ip = nodes.find { |node| node['fqdn'] == fqdn }['public_address']
-      assert ips.include?(ip), 'Public address is not set!'
+    if %w(controller primary-controller).include? TestCommon::Settings.role
+      ip = TestCommon::Settings.public_address
+      assert TestCommon::Network.ips.include?(ip), 'Public address is not set!'
     end
   end
 
   def test_storage_ip_present
-    ip = nodes.find { |node| node['fqdn'] == fqdn }['storage_address']
-    assert ips.include?(ip), 'Storage address is not set!'
+    ip = TestCommon::Settings.storage_address
+    assert TestCommon::Network.ips.include?(ip), 'Storage address is not set!'
   end
 
   def test_can_ping_the_master_node
-    assert ping(master_ip), 'Cannot ping the master node!'
-  end
-
-  def test_can_ping_the_default_router
-    assert ping(router), 'Cannot ping the default router!'
+    ip = TestCommon::Settings.master_ip
+    assert TestCommon::Network.ping?(ip), 'Cannot ping the master node!'
   end
 
 end
