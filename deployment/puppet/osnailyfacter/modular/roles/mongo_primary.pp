534748186d3c9ac0f433db6cd693f47387d5aab5
Aleksandr Didenko <adidenko@mirantis.com>
2015-03-19 12:04:12 +0200

    Fetch all mongo roles for replica set
    We need to fetch both 'primary-mongo' and 'mongo' roles for
    replica set configuration.
    
    Change-Id: I3febf7e9c99e105e3018e48910f59f4f260dfde3
    Related-bug: #1428696

--- deployment/puppet/osnailyfacter/modular/roles/mongo_primary.pp
+++ deployment/puppet/osnailyfacter/modular/roles/mongo_primary.pp
@@ -13,7 +13,7 @@ class { 'openstack::mongo_primary':
   mongodb_bind_address        => [ '127.0.0.1', $internal_address ],
   ceilometer_metering_secret  => $ceilometer_hash['metering_secret'],
   ceilometer_db_password      => $ceilometer_hash['db_password'],
-  ceilometer_replset_members  => mongo_hosts($nodes_hash, 'array', 'mongo'),
+  ceilometer_replset_members  => mongo_hosts($nodes_hash, 'array'),
   replset                     => 'ceilometer',
   use_syslog                  => $use_syslog,
   debug                       => $debug,
