759a6a33b436f0c434f75814fb740885cf1e8448
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-03-20 17:25:54 +0200

    Revert of gateways for VIPs
    Revert of VIPs for management and public in haproxy ns
    
    Regression caused by
    https://bugs.launchpad.net/fuel/+bug/1433150
    
    Change-Id: I7df8efad4aa119896314e77c29d9b024599dc865
    Closes-Bug: 1431547

--- deployment/puppet/osnailyfacter/modular/cluster-haproxy/cluster-haproxy.pp
+++ deployment/puppet/osnailyfacter/modular/cluster-haproxy/cluster-haproxy.pp
@@ -4,5 +4,6 @@ class { 'cluster::haproxy':
   haproxy_maxconn    => '16000',
   haproxy_bufsize    => '32768',
   primary_controller => hiera('primary_controller'),
-  debug              => hiera('debug', false)
+  debug              => hiera('debug', false),
+  other_networks     => direct_networks('endpoints'),
 }
