31208e5ddbcd5d44ce8549e3bc845e3c0c0b27d2
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-03-23 17:54:46 +0300

    Add updates repository
    Change-Id: I87f848bd13f7402bd608e5d50c488392739ca0a3

---
+++ deployment/puppet/nailgun/manifests/updatesrepos.pp
@@ -0,0 +1,41 @@
+class nailgun::updatesrepos(
+  $production = $production,
+  $repo_root  = '/var/www/nailgun',
+  ){
+
+  $centos_dir = "${repo_root}/centos/updates/"
+  $ubuntu_dir = "${repo_root}/ubuntu/updates/"
+
+  file { "${repo_root}/centos/updates/":
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  Package['createrepo'] -> Exec["createrepo ${centos_dir}"]
+
+  package { 'createrepo':
+    ensure => latest,
+  }
+
+  exec { "createrepo ${centos_dir}":
+    path     => '/bin:/sbin:/usr/bin:/usr/sbin',
+    cwd      => $centos_dir,
+    creates  => "${centos_dir}/repodata/repomd.xml",
+  }
+
+  file { "${repo_root}/ubuntu/updates/":
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  file { "${repo_root}/ubuntu/updates/Packages":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+}
