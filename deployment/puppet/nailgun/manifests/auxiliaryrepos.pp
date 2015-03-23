84900019e34cdad5a7d2e6d868f51f687e996e70
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-03-23 17:54:46 +0300

    Add auxiliary repository
    Configures empty auxiliary repository
    for ubuntu and centos that is enabled
    by default.
    
    Change-Id: I87f848bd13f7402bd608e5d50c488392739ca0a3
    blueprint separate-mos-from-linux

---
+++ deployment/puppet/nailgun/manifests/auxiliaryrepos.pp
@@ -0,0 +1,65 @@
+class nailgun::auxiliaryrepos(
+  $repo_root  = '/var/www/nailgun',
+  ){
+
+  $centos_dir = "${repo_root}/centos/auxiliary/"
+  $ubuntu_dir = "${repo_root}/ubuntu/auxiliary/"
+
+  file { $centos_dir:
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  File[$centos_dir] ->
+    Package['createrepo'] ->
+      Exec["createrepo ${centos_dir}"] ->
+        Yumrepo['auxiliary']
+
+  yumrepo { 'auxiliary':
+    name     => 'auxiliary',
+    descr    => 'auxillary',
+    baseurl  => "file://${centos_dir}",
+    gpgcheck => '0',
+  }
+
+  package { 'createrepo':
+    ensure => latest,
+  }
+
+  exec { "createrepo ${centos_dir}":
+    path     => '/bin:/sbin:/usr/bin:/usr/sbin',
+    cwd      => $centos_dir,
+    creates  => "${centos_dir}/repodata/repomd.xml",
+  }
+
+  $release_files = [
+    "${ubuntu_dir}/dists/auxiliary/Release",
+    "${ubuntu_dir}/dists/auxiliary/main/binary-amd64/Release",
+    "${ubuntu_dir}/dists/auxiliary/restricted/binary-amd64/Release"]
+
+  Exec['create_ubuntu_repo_dirs'] ->
+    Exec['create_ubuntu_repo_Packages'] ->
+      File[$release_files]
+
+  exec { 'create_ubuntu_repo_dirs':
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+    command => "bash -c \"mkdir -p ${ubuntu_dir}/pool/{main,restricted} ${ubuntu_dir}/dists/auxiliary/{main,restricted}/binary-amd64/\"",
+    creates => "${ubuntu_dir}/pool",
+  }
+
+  exec { 'create_ubuntu_repo_Packages':
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+    command => "bash -c \"touch ${ubuntu_dir}/dists/auxiliary/{main,restricted}/binary-amd64/Packages\"",
+    creates => "${ubuntu_dir}/dists/auxiliary/main/binary-amd64/Packages",
+  }
+
+  file { $release_files:
+    ensure  => file,
+    source  => 'puppet:///modules/nailgun/Release-auxiliary',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+  }
+}
