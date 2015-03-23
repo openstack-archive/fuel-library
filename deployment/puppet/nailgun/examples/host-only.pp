b7e79a187d4752a9d41e85af0334a45b06c0b09a
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-03-23 17:54:46 +0300

    Add updates repository
    Change-Id: I87f848bd13f7402bd608e5d50c488392739ca0a3

--- deployment/puppet/nailgun/examples/host-only.pp
+++ deployment/puppet/nailgun/examples/host-only.pp
@@ -107,6 +107,10 @@ exec {'sync_deployment_tasks':
 
 class { "monit": }
 
+class { 'nailgun::updatesrepos':
+  repo_root  => "${repo_root}/${::fuel_version['VERSION']['openstack_version']}",
+}
+
 # Free disk space monitoring
 file { '/usr/bin/fuel_notify.py':
   source  => 'puppet:///modules/nailgun/fuel_notify.py',
