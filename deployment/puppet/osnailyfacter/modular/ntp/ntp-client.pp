5fd5f772c984de6ce6dcf631fe8e52473b6e1789
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-03-24 08:55:55 +0200

    Fix for multi-role ntpd
    Fix for multi-role ntpd. NTPD for Ubuntu doesn't accept
    config not equal to /etc/ntp.conf
    
    Change-Id: I0beda3b1d35bf2a3d7920994635c9f43a0830f23
    Closes-Bug: 1435564

--- deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
+++ deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
@@ -1,13 +1,13 @@
 notice('MODULAR: ntp-client.pp')
 
 $management_vip  = hiera('management_vrouter_vip')
-$ntp_server_conf = inline_template("<% if File.exist?('/etc/ntp.server.conf') -%>true<% end -%>")
+$nodes_hash      = hiera('nodes', {})
+$roles           = node_roles($nodes_hash, hiera('uid'))
 
-if ! $ntp_server_conf {
+if !(member($roles, 'controller') or member($roles, 'primary-controller')) {
   class { 'ntp':
     servers        => [$management_vip],
     service_ensure => running,
     service_enable => true,
   }
 }
-
