442c82e3fc3d7c9c50b7188f204c8f283c8dbcda
Stanislav Makar <smakar@mirantis.com>
2015-03-25 10:21:19 +0000

    Install openvswitch-datapath-dkms package
    * Install openvswitch-datapath-dkms package in any way for Ubuntu 14.04
    due to upgrading to OpenvSwitch 2.3
    * Clean up unnecessary kern_has_ovs_datapath fact
    
    Change-Id: I759b8507bdbc98b1b9931ca0ac2a066dfb7b3bb4
    Partial-Bug: #1403047
    Docimpact: OpenvSwitch 2.3 support

--- deployment/puppet/l23network/manifests/params.pp
+++ deployment/puppet/l23network/manifests/params.pp
@@ -1,45 +1,41 @@
 # L23network OS-aware constants
 #
 class l23network::params {
-  $need_datapath_module = !str2bool($::kern_has_ovs_datapath)
   $monolith_bond_providers = ['ovs']
 
   case $::osfamily {
     /(?i)debian/: {
-      $interfaces_dir     = '/etc/network/interfaces.d'
-      $interfaces_file    = '/etc/network/interfaces'
-      $ovs_service_name   = 'openvswitch-switch'
-      $ovs_status_cmd     = '/etc/init.d/openvswitch-switch status'
-      $lnx_vlan_tools     = 'vlan'
-      $lnx_bond_tools     = 'ifenslave'
-      $lnx_ethernet_tools = 'ethtool'
-      $lnx_bridge_tools   = 'bridge-utils'
-      $ovs_datapath_package_name = $need_datapath_module ? {
-        true    => 'openvswitch-datapath-lts-saucy-dkms',
-        default => false
-      }
-      $ovs_common_package_name = 'openvswitch-switch'
+      $interfaces_dir            = '/etc/network/interfaces.d'
+      $interfaces_file           = '/etc/network/interfaces'
+      $ovs_service_name          = 'openvswitch-switch'
+      $ovs_status_cmd            = '/etc/init.d/openvswitch-switch status'
+      $lnx_vlan_tools            = 'vlan'
+      $lnx_bond_tools            = 'ifenslave'
+      $lnx_ethernet_tools        = 'ethtool'
+      $lnx_bridge_tools          = 'bridge-utils'
+      $ovs_datapath_package_name = 'openvswitch-datapath-dkms'
+      $ovs_common_package_name   = 'openvswitch-switch'
     }
     /(?i)redhat/: {
-      $interfaces_dir     = '/etc/sysconfig/network-scripts'
-      $interfaces_file    = undef
-      $ovs_service_name   = 'openvswitch'
-      $ovs_status_cmd     = '/etc/init.d/openvswitch status'
-      $lnx_vlan_tools     = 'vconfig'
-      $lnx_bond_tools     = undef
-      $lnx_ethernet_tools = 'ethtool'
-      $lnx_bridge_tools   = 'bridge-utils'
+      $interfaces_dir            = '/etc/sysconfig/network-scripts'
+      $interfaces_file           = undef
+      $ovs_service_name          = 'openvswitch'
+      $ovs_status_cmd            = '/etc/init.d/openvswitch status'
+      $lnx_vlan_tools            = 'vconfig'
+      $lnx_bond_tools            = undef
+      $lnx_ethernet_tools        = 'ethtool'
+      $lnx_bridge_tools          = 'bridge-utils'
       $ovs_datapath_package_name = 'kmod-openvswitch'
       $ovs_common_package_name   = 'openvswitch'
     }
     /(?i)darwin/: {
-      $interfaces_dir     = '/tmp/1'
-      $interfaces_file    = undef
-      $ovs_service_name   = undef
-      $lnx_vlan_tools     = undef
-      $lnx_bond_tools     = undef
-      $lnx_ethernet_tools = undef
-      $lnx_bridge_tools   = undef
+      $interfaces_dir            = '/tmp/1'
+      $interfaces_file           = undef
+      $ovs_service_name          = undef
+      $lnx_vlan_tools            = undef
+      $lnx_bond_tools            = undef
+      $lnx_ethernet_tools        = undef
+      $lnx_bridge_tools          = undef
       $ovs_datapath_package_name = undef
       $ovs_common_package_name   = undef
     }
