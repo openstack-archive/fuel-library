a6f1d46840ba65939c8fd3bbba2b80c91da68273
Igor Zinovik <izinovik@mirantis.com>
2015-03-26 14:52:30 +0300

    Remove workarounds after implementing dual hypervisor support
    - remove dead statements that will never get executed
    - rename '*_multi_hv' resources to original names
    - rename templates
    - adjust unit tests
    
    Closes-bug: #1436739
    Change-Id: Ib2a7ad8ff90595bade49895d86d681700118fc9b

--- deployment/puppet/vmware/spec/defines/vmware_compute_ha_spec.rb
+++ deployment/puppet/vmware/spec/defines/vmware_compute_ha_spec.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
 
 describe 'vmware::compute::ha' do
-  let(:title) { 'cluster1' }
+  let(:title) { '0' }
 
-  let(:params) { { :index => '0' } }
+  let(:params) { {
+    :availability_zone_name => 'vCenter',
+    :vc_cluster             => 'prod-cluster',
+    :vc_host                => '10.10.0.1',
+    :vc_user                => 'admin@vsphere.local',
+    :vc_password            => 'pass',
+    :service_name           => 'prod'
+  } }
 
   it 'must create /etc/nova/nova-compute.d directory' do
     should contain_file('/etc/nova/nova-compute.d').with({
@@ -14,18 +21,18 @@ describe 'vmware::compute::ha' do
     })
   end
 
-  it 'should create service p_nova_compute_vmware_0' do
-    should contain_cs_resource('p_nova_compute_vmware_0').with({
+  it 'should create service p_nova_compute_vmware_vCenter-prod' do
+    should contain_cs_resource('p_nova_compute_vmware_vCenter-prod').with({
       'primitive_class' => 'ocf',
       'provided_by' => 'fuel',
     })
   end
 
-  it 'should create service p_nova_compute_vmware_0' do
-    should contain_service('p_nova_compute_vmware_0')
+  it 'should create service p_nova_compute_vmware_vCenter-prod' do
+    should contain_service('p_nova_compute_vmware_vCenter-prod')
   end
 
   it 'should apply configuration file before corosync resource' do
-    should contain_file('/etc/nova/nova-compute.d/vmware-0.conf').that_comes_before('Cs_resource[p_nova_compute_vmware_0]')
+    should contain_file('/etc/nova/nova-compute.d/vmware-vCenter_prod.conf').that_comes_before('Cs_resource[p_nova_compute_vmware_vCenter-prod]')
   end
 end
