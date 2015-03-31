6abd0e60e1a2e93d572d96b12ac59516040cb117
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-03-31 17:04:29 +0300

    Configure resolv.conf in post-deploy
    Configure resolv.conf to run in post-deploy stage
    
    Change-Id: I4600cfa97466fad4fc70dd2e6b03b1d7a02b2509
    Closes-Bug: 1438591

--- deployment/puppet/osnailyfacter/modular/dns/dns-client.pp
+++ deployment/puppet/osnailyfacter/modular/dns/dns-client.pp
@@ -1,6 +1,6 @@
 notice('MODULAR: dns-client.pp')
 
-$management_vip     = hiera('management_vrouter_vip')
+$management_vip = hiera('management_vrouter_vip')
 
 class { 'osnailyfacter::resolvconf':
   management_vip => $management_vip,
