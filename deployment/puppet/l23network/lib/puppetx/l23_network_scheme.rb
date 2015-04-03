320af17e3e42de2d30b6aaf9de2a5b57c0d08d94
Sergey Kolekonov <skolekonov@mirantis.com>
2015-04-03 13:14:38 +0300

    Set mtu for internal networks according to segmentation type
    Currently MTU for Neutron private networks is always set to 1500,
    which is not correct for GRE segmentation and causes performance degradation.
    MTU value should also depend on MTU settings of physical interfaces.
    The only way to change MTU for Neutron private network is to use an additional
    dnsmasq configuration file.
    
    Co-Authored-By: Sergey Vasilenko <svasilenko@mirantis.com>
    Change-Id: Iaa9631b58f268ec414c58d1b7c47de3573772fc5
    Closes-bug: #1411732

--- deployment/puppet/l23network/lib/puppetx/l23_network_scheme.rb
+++ deployment/puppet/l23network/lib/puppetx/l23_network_scheme.rb
@@ -7,6 +7,36 @@ module L23network
     def self.get_config(h)
       @network_scheme_hash[h.to_sym]
     end
+    def self.get_phys_dev_by_endpoint(endpoint, interfaces, transformations)
+      rv = []
+      all_ifaces = interfaces.keys.map(&:to_s)
+      ifaces = []
+
+      if all_ifaces.include? endpoint
+        ifaces.push(endpoint)
+        return ifaces
+      end
+
+      for i in 0..transformations.size-1  do
+        transform = transformations[i]
+        if transform[:name] != nil and transform[:name].include? endpoint
+          if transform[:vlandev] != nil
+            ifaces.push(transform[:vlandev])
+          else
+            endpoint = transform[:name].split(".")[0]
+            if all_ifaces.include? endpoint
+              ifaces.push(endpoint.to_s)
+            end
+          end
+        elsif transform[:bridge] == endpoint
+          ifaces.push(transform[:name].split(".")[0])
+        elsif transform[:bridges] != nil and transform[:bridges][0] == endpoint
+          endpoint = transform[:bridges][1]
+          ifaces.push(get_phys_dev_by_endpoint(endpoint, interfaces, transformations))
+        end
+      end
+      return ifaces.flatten.uniq
+    end
   end
 end
 # vim: set ts=2 sw=2 et :
