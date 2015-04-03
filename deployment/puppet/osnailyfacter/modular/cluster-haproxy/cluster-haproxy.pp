908e497c7e6c59964600c621234d0c87289108f9
Aleksandr Didenko <adidenko@mirantis.com>
2015-04-03 14:51:55 +0300

    Refactor direct_networks to not call Hiera directly
    This function simply forms a list of networks according to our
    network_scheme. We can pass $network_scheme['endpoints'] hash
    as a parameter to this function instead of calling Hiera directly
    from it.
    
    Change-Id: Ic940ee20f49ed8c741f5bda452f1007a914a0707
    Closes-bug: #1440028

--- deployment/puppet/osnailyfacter/modular/cluster-haproxy/cluster-haproxy.pp
+++ deployment/puppet/osnailyfacter/modular/cluster-haproxy/cluster-haproxy.pp
@@ -1,9 +1,11 @@
 notice('MODULAR: cluster-haproxy.pp')
 
+$network_scheme = hiera('network_scheme', {})
+
 class { 'cluster::haproxy':
   haproxy_maxconn    => '16000',
   haproxy_bufsize    => '32768',
   primary_controller => hiera('primary_controller'),
   debug              => hiera('debug', false),
-  other_networks     => direct_networks('endpoints'),
+  other_networks     => direct_networks($network_scheme['endpoints']),
 }
