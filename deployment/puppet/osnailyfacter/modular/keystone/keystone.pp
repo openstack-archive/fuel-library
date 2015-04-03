9186ac7124e36d5b5a4ccb372dd211a265a5982b
Dmitry Ilyin <dilyin@mirantis.com>
2015-04-03 20:39:08 +0300

    Enable keystone wsgi support
    * Run keystone as Apache vhost
    
    Change-Id: I85008079b0e922a4518c696a097238500132fa04
    Closes-Bug: 1313662

--- deployment/puppet/osnailyfacter/modular/keystone/keystone.pp
+++ deployment/puppet/osnailyfacter/modular/keystone/keystone.pp
@@ -48,7 +48,8 @@ $ceilometer_user_password = $ceilometer_hash['user_password']
 
 $cinder = true
 $ceilometer = $ceilometer_hash['enabled']
-$enabled = true
+$enabled = false
+$ssl = false
 
 $rabbit_password     = $rabbit_hash['password']
 $rabbit_user         = $rabbit_hash['user']
@@ -107,6 +108,22 @@ class { 'openstack::keystone':
   idle_timeout              => $idle_timeout,
 }
 
+####### WSGI ###########
+
+include 'apache'
+
+class { 'keystone::wsgi::apache' :
+  ssl => $ssl,
+}
+
+$file = '/usr/lib/python2.6/site-packages/keystone/common/cache/_memcache_pool.py'
+exec { 'patch_keystone_memcache_pool' :
+  command => "sed -i -e 's|class ConnectionPool(queue.Queue):|class ConnectionPool(queue.Queue, object):|g' '${file}'"
+}
+Package['python-keystone'] ->
+Exec['patch_keystone_memcache_pool'] ->
+Service['httpd']
+
 ###############################################################################
 
 class { 'keystone::roles::admin':
@@ -125,12 +142,11 @@ class { 'openstack::auth_file':
 }
 
 class { 'openstack::workloads_collector':
-  enabled              => $workloads_hash[enabled],
-  workloads_username   => $workloads_hash[username],
-  workloads_password   => $workloads_hash[password],
-  workloads_tenant     => $workloads_hash[tenant],
+  enabled              => $workloads_hash['enabled'],
+  workloads_username   => $workloads_hash['username'],
+  workloads_password   => $workloads_hash['password'],
+  workloads_tenant     => $workloads_hash['tenant'],
 }
-
 
 Exec <| title == 'keystone-manage db_sync' |> ->
 Class['keystone::roles::admin'] ->
@@ -153,6 +169,8 @@ haproxy_backend_status { 'keystone-admin' :
 
 Class['openstack::keystone'] -> Haproxy_backend_status<||>
 Service['keystone'] -> Haproxy_backend_status<||>
+Service<| title == 'httpd' |> -> Haproxy_backend_status<||>
+Haproxy_backend_status<||> -> Class['keystone::roles::admin']
 
 case $::osfamily {
   'RedHat': {
