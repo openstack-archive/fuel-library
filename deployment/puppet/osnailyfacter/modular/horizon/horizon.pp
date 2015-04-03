1f09f999070eca29c7dee5b683e6653e9456bf12
Dmitry Ilyin <dilyin@mirantis.com>
2015-04-03 20:39:08 +0300

    Enable keystone wsgi support
    * New task to deploy Apache and configure MPM
    * Run keystone as Apache vhost
    * Pull wsgi threads and priority configuration from upstream
      change-id: Ib05ac81381e169845b44b2ef7cb810a4d5db17de
    * Pull custom file source for wsgi scripts from upstream
      change-id: I941bf8804982e9081812e076f7a736f413220047
    * Fix for keystone spec tests (use concat 1.2.1 in fixtures)
    * Use keystone.py wsgi script from packages instead of downloading
      it from puppet module. Requires deb package with this patch:
      https://review.fuel-infra.org/6251
      Until it's megred, we'll use upstream wsgi script for Debian
    
    Change-Id: I85008079b0e922a4518c696a097238500132fa04
    Closes-Bug: 1313662

--- deployment/puppet/osnailyfacter/modular/horizon/horizon.pp
+++ deployment/puppet/osnailyfacter/modular/horizon/horizon.pp
@@ -15,7 +15,7 @@ class { 'openstack::horizon':
   secret_key        => $secret_key,
   cache_server_ip   => $controller_nodes,
   package_ensure    => hiera('horizon_package_ensure', 'installed'),
-  bind_address      => hiera('internal_address'),
+  bind_address      => '*',
   cache_server_port => '11211',
   neutron           => hiera('use_neutron'),
   keystone_host     => hiera('management_vip'),
