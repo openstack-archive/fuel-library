4c39102720bf02019f45a7531c2d0eb5a7da54e7
Dmitry Ilyin <dilyin@mirantis.com>
2015-04-10 17:51:52 +0300

    Improve Noop tests
    * Many fixes and improvements
    
    Related blueprint: deployment-dryrun
    
    Change-Id: I7cdb4ea455866480a773b803f4dddd66f0fd56d1
    Fuel-CI: disable

--- tests/noop/spec/hosts/swift/swift_spec.rb
+++ tests/noop/spec/hosts/swift/swift_spec.rb
@@ -3,12 +3,12 @@ require 'shared-examples'
 manifest = 'swift/swift.pp'
 
 describe manifest do
-  shared_examples 'puppet catalogue' do
-    settings = Noop.fuel_settings
-    role = settings['role']
-    storage_hash = Noop.fuel_settings['storage']
-    primary_controller_nodes = filter_nodes(settings['nodes'],'role','primary-controller')
-    controllers = primary_controller_nodes + filter_nodes(settings['nodes'],'role','controller')
+  shared_examples 'catalog' do
+    role = Noop.hiera 'role'
+    storage_hash = Noop.hiera['storage']
+    nodes = Noop.hiera 'nodes'
+    primary_controller_nodes = filter_nodes(nodes,'role','primary-controller')
+    controllers = primary_controller_nodes + filter_nodes(nodes,'role','controller')
     controller_internal_addresses = nodes_to_hash(controllers,'name','internal_address')
     controller_nodes = ipsort(controller_internal_addresses.values)
     memcached_servers = controller_nodes.map{ |n| n = n + ':11211' }
