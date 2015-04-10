7f0b36ac6b116a28be299566fab87b0bae7bffb4
Dmitry Ilyin <dilyin@mirantis.com>
2015-04-10 17:51:52 +0300

    Improve Noop tests
    * Many fixes and improvements
    
    Change-Id: I7cdb4ea455866480a773b803f4dddd66f0fd56d1
    Fuel-CI: disable

--- tests/noop/spec/hosts/openstack-network/openstack-network-controller_spec.rb
+++ tests/noop/spec/hosts/openstack-network/openstack-network-controller_spec.rb
@@ -3,27 +3,15 @@ require 'shared-examples'
 manifest = 'openstack-network/openstack-network-controller.pp'
 
 describe manifest do
-  shared_examples 'puppet catalogue' do
+  shared_examples 'catalog' do
 
     # TODO All this stuff should be moved to shared examples controller* tests.
 
-    settings = Noop.fuel_settings
-    internal_address = Noop.node_hash['internal_address']
-    rabbit_user = settings['rabbit']['user'] || 'nova'
-    use_neutron = settings['quantum'].to_s
-    role = settings['role']
-    rabbit_ha_queues = 'true'
-    primary_controller_nodes = filter_nodes(settings['nodes'],'role','primary-controller')
-    controllers = primary_controller_nodes + filter_nodes(settings['nodes'],'role','controller')
-    controller_internal_addresses = nodes_to_hash(controllers,'name','internal_address')
-    controller_nodes = ipsort(controller_internal_addresses.values)
-    memcached_servers = controller_nodes.map{ |n| n = n + ':11211' }.join(',')
-    horizon_bind_address = internal_address
-    admin_token = settings['keystone']['admin_token']
-    nova_quota = settings['nova_quota']
+    use_neutron = Noop.hiera 'use_neutron'
+    ceilometer_enabled = Noop.hiera_structure 'ceilometer/enabled'
 
     # Network
-    if settings['quantum']
+    if use_neutron
       it 'should declare openstack::network with neutron enabled' do
         should contain_class('openstack::network').with(
           'neutron_server' => 'true',
@@ -38,13 +26,11 @@ describe manifest do
     end
 
     # Ceilometer
-    if settings['ceilometer']['enabled']
-      if use_neutron == 'true'
-        it 'should configure notification_driver for neutron' do
-          should contain_neutron_config('DEFAULT/notification_driver').with(
-            'value' => 'messaging',
-          )
-        end
+    if ceilometer_enabled and use_neutron
+      it 'should configure notification_driver for neutron' do
+        should contain_neutron_config('DEFAULT/notification_driver').with(
+          'value' => 'messaging',
+        )
       end
     end
   end # end of shared_examples
