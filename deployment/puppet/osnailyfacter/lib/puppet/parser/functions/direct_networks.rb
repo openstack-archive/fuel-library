6f29764b767c624644457ed3c2f0fd06f8055057
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-04-14 17:18:41 +0300

    Add other_networks to vrouter ns
    - Remove host interface creation from ns_ntp ocf
    - Create new crm resource vrouter that handles other_networks
    - Update direct_networks.rb to parse custom routes
    
    Change-Id: Iea26e1755e7e6d6310ad7cdb77ef1b21d850ff36
    Closes-Bug: 1436868

--- deployment/puppet/osnailyfacter/lib/puppet/parser/functions/direct_networks.rb
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/direct_networks.rb
@@ -1,9 +1,8 @@
 Puppet::Parser::Functions::newfunction(:direct_networks, :type => :rvalue, :doc => <<-EOS
- parses network scheme and returns networks
- directly attached to the host
- EOS
- ) do |argv|
-
+parses network scheme and returns networks
+directly attached to the host
+EOS
+) do |argv|
   endpoints = argv[0]
   networks = []
 
@@ -11,6 +10,11 @@ Puppet::Parser::Functions::newfunction(:direct_networks, :type => :rvalue, :doc 
     if v.has_key?('IP') and v['IP'].is_a?(Array)
       v['IP'].each { |ip|
         networks << IPAddr.new(ip).to_s + "/" + ip.split('/')[1]
+      }
+    end
+    if v.has_key?('routes') and v['routes'].is_a?(Array)
+      v['routes'].each { |route|
+        networks << route['net']
       }
     end
   }
