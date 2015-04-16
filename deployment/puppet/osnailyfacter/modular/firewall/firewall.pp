389e830756c51ef9af7f962e8bd075d2c92811de
Stanislav Makar <smakar@mirantis.com>
2015-04-16 14:32:51 +0000

    Implement the dumping rabbitMQ users
    * Add the dumping rabbitMQ users during deployment
    * Add the possibility to restore users to the rabbitmq-server ocf script
      during rabbitMQ startup.
    * Enabled rabbitmq admin plugin, but restricts it to localhost traffic.
      This reverts Ic01c26200f6019a8112b1c5fb04a282e64b3b3e6 but adds
      firewall rules to mitigate the issue.
    
    Change-Id: I715f7c2ae527f7e105b9f6b7d82c443e8accf178
    Closes-bug: #1383258
    Related-bug: #1450443
    Co-Authored-By: Alex Schultz <aschultz@mirantis.com>

--- deployment/puppet/osnailyfacter/modular/firewall/firewall.pp
+++ deployment/puppet/osnailyfacter/modular/firewall/firewall.pp
@@ -2,7 +2,7 @@ notice('MODULAR: firewall.pp')
 
 # Workaround for fuel bug with firewall
 firewall {'003 remote rabbitmq ':
-  sport   => [ 4369, 5672, 15672, 41055, 55672, 61613 ],
+  sport   => [ 4369, 5672, 41055, 55672, 61613 ],
   source  => hiera('master_ip'),
   proto   => 'tcp',
   action  => 'accept',
@@ -17,6 +17,22 @@ firewall {'004 remote puppet ':
   require => Class['openstack::firewall'],
 }
 
+# allow local rabbitmq admin traffic for LP#1383258
+firewall {'005 local rabbitmq admin':
+  sport   => [ 15672 ],
+  iniface => 'lo',
+  proto   => 'tcp',
+  action  => 'accept',
+  require => Class['openstack::firewall'],
+}
+
+# reject all non-local rabbitmq admin traffic for LP#1450443
+firewall {'006 reject non-local rabbitmq admin':
+  sport   => [ 15672 ],
+  proto   => 'tcp',
+  action  => 'drop',
+  require => Class['openstack::firewall'],
+}
 
 prepare_network_config(hiera_hash('network_scheme'))
 class { 'openstack::firewall' :
