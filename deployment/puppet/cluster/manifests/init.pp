11b4f7df506b54df10fa138c5feb8d3e48ea926e
Bogdan Dobrelya <bdobrelia@mirantis.com>
2015-04-17 15:11:35 +0200

    Lower the CIB validator version for Ubuntu
    W/o this fix, the crm tool from the vivid version of the crmsh
    package will report ERROR: CIB not supported: validator 'pacemaker-2.0'
    
    The work around is to lower the validator version in CIB:
      validate-with="pacemaker-1.2"
    
    Closes-bug: #1445478
    
    Change-Id: I968b98178c0f015bafe445de732d7ee4af3783de
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/cluster/manifests/init.pp
+++ deployment/puppet/cluster/manifests/init.pp
@@ -34,6 +34,18 @@ class cluster (
     }
     File<| title == '/etc/corosync/corosync.conf' |> ~> Exec['stop-pacemaker'] ~> Service['corosync']
 
+    # NOTE(bogdando) #LP1445478 - lower the validator version for Ubuntu
+    if ($::osfamily == 'Debian') {
+      # Use retries as CIB require some time to become ready
+      exec { 'fix-crm-validator':
+        command   => 'cibadmin --modify --xml-text \'<cib validate-with="pacemaker-1.2"/>\'',
+        path      => '/bin:/usr/bin/:/sbin:/usr/sbin',
+        tries     => 10,
+        try_sleep => 30,
+        require   => Class['openstack::corosync'],
+      }
+    }
+
     file { 'ocf-fuel-path':
       ensure  => directory,
       path    =>'/usr/lib/ocf/resource.d/fuel',
