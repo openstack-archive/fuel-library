a2880709ad9dbc3465000db496c8df429d0a4b99
Dmitry Ilyin <dilyin@mirantis.com>
2015-04-22 20:56:37 +0300

    Pacemaker fixes for controller add/remove
    * Location constraint should be present for
      a pacemaker service to be started even if
      this service is actually started.
    
    * pcmk_nodes resource to add and remove
      corosync nodes
    
    Change-Id: I97035e633c33f94519b66526268620f9cd6cc958
    Closes-Bug: 1394188

---
+++ deployment/puppet/pacemaker/lib/puppet/type/pcmk_nodes.rb
@@ -0,0 +1,58 @@
+module Puppet
+  newtype(:pcmk_nodes) do
+    desc %q(Add and remove cluster nodes)
+
+    newparam(:name) do
+      isnamevar
+    end
+
+    newparam(:nodes, :array_matching => :all) do
+      desc 'Nodes data structure. Hash { "name" => "ip" }'
+      validate do |value|
+        unless value.is_a? Hash and value.any?
+          fail 'Nodes should be a non-empty hash { "name" => "ip" }!'
+        end
+      end
+    end
+
+    newproperty(:corosync_nodes, :array_matching => :all) do
+      defaultto { @resource[:nodes].keys if @resource[:nodes] }
+
+      def insync?(is)
+        return false unless is.is_a? Array and should.is_a? Array
+        is.sort = should.sort
+      end
+
+      def is_to_s(is)
+        is.inspect
+      end
+
+      def should_to_s(should)
+        should.inspect
+      end
+    end
+
+    newproperty(:pacemaker_nodes, :array_matching => :all) do
+      defaultto { @resource[:nodes].keys if @resource[:nodes] }
+      def insync?(is)
+        return false unless is.is_a? Array and should.is_a? Array
+        is.sort = should.sort
+      end
+
+      def is_to_s(is)
+        is.inspect
+      end
+
+      def should_to_s(should)
+        should.inspect
+      end
+    end
+
+    def validate
+      fail 'No corosync_nodes!' unless self[:corosync_nodes].is_a? Array and self[:corosync_nodes].any?
+      fail 'No pacemaker_nodes!' unless self[:pacemaker_nodes].is_a? Array and self[:pacemaker_nodes].any?
+    end
+
+  end
+end
+
