c8f365611dbc7a7acb7bea6c0911e7e1f91d5bff
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2015-04-23 19:45:58 +0300

    Changed 'swift_store_large_object_size' to default value
    We added the workaround for the old bug in Swift (#1430765),
    this bug was fixed for MOS 6.1 and we need to remove this workaround
    and use default value for this parameter.
    
    Change-Id: I2f124321b7d5aa8312a8ff1539957b6c0a6a9e81
    Closes-Bug: #1447703

--- deployment/puppet/openstack/manifests/glance.pp
+++ deployment/puppet/openstack/manifests/glance.pp
@@ -235,7 +235,6 @@ class openstack::glance (
         swift_store_user => "services:glance",
         swift_store_key=> $glance_user_password,
         swift_store_create_container_on_put => "True",
-        swift_store_large_object_size => '200',
         swift_store_auth_address => "http://${keystone_host}:5000/v2.0/"
       }
     }
