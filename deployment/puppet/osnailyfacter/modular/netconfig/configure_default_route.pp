6981f9b289e1fb4f9035bf139024c0a378a68841
Stanislav Makar <smakar@mirantis.com>
2015-04-23 08:51:36 +0000

    Reconfigure default route
    We have some roles which are deployed before vrouter deployment
    due to we need to reconfigure default route after vrouter is ready.
    
    Default route reconfiguration:
    * add new configure_default_route puppet function
    * add new configure_default_route task in post deploy
    * add tests
    
    Change-Id: I5db09111a9eecad058654e5463ffb13d077cee70
    Closes-bug: #1444962

---
+++ deployment/puppet/osnailyfacter/modular/netconfig/configure_default_route.pp
@@ -0,0 +1,14 @@
+notice('MODULAR: configure_default_route.pp')
+
+$network_scheme = hiera('network_scheme')
+$master_ip = hiera('master_ip')
+$management_vrouter_vip = hiera('management_vrouter_vip')
+
+prepare_network_config($network_scheme)
+$management_int = get_network_role_property('management', 'interface')
+$fw_admin_int = get_network_role_property('fw-admin', 'interface')
+$ifconfig = configure_default_route($network_scheme, $master_ip, $management_vrouter_vip, $fw_admin_int, $management_int )
+
+if $ifconfig != undef {
+  create_resources('l23network::l3::ifconfig', $ifconfig)
+}
