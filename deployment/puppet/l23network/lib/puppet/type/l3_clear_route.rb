cbac660146d175fe184ca6571ef6d85e056a8d3c
Stanislav Makar <smakar@mirantis.com>
2015-04-29 17:09:56 +0300

    Fix default route overwriting problem
    * Add simple puppet resource which clear all default gateways
      before creating l3_ifconfig resources
    * Add warnings to prevent from using 'ip route replace...'
      command in context of interface setup
    
    Fuel-CI: disable
    Change-Id: Ifa73c0a9a4f4df542ea8437b9894c3dce5cfee7e
    Related-bug: #1447638

---
+++ deployment/puppet/l23network/lib/puppet/type/l3_clear_route.rb
@@ -0,0 +1,51 @@
+# type for clearing runtime routes.
+
+Puppet::Type.newtype(:l3_clear_route) do
+    @doc = "Clear routes for destination with specified metric."
+    desc @doc
+
+    ensurable
+
+    newparam(:name) # workarround for following error:
+    # Error 400 on SERVER: Could not render to pson: undefined method `merge' for []:Array
+    # http://projects.puppetlabs.com/issues/5220
+
+
+    newproperty(:destination) do
+      desc "Destination network"
+      validate do |val|
+        val.strip!
+        if val.to_s.downcase != 'default'
+          raise ArgumentError, "Invalid IP address: '#{val}'" if \
+            not val.match(/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})(\/(\d{1,2}))?$/) \
+            or not ($1.to_i >= 0  and  $1.to_i <= 255) \
+            or not ($2.to_i >= 0  and  $2.to_i <= 255) \
+            or not ($3.to_i >= 0  and  $3.to_i <= 255) \
+            or not ($4.to_i >= 0  and  $4.to_i <= 255) \
+            or not ($6.to_i >= 0  and  $6.to_i <= 32)
+        end
+      end
+
+    end
+
+    newproperty(:metric) do
+      desc "Route metric"
+      newvalues(/^\d+$/, :absent, :none, :undef, :nil)
+      aliasvalue(:none,  :absent)
+      aliasvalue(:undef, :absent)
+      aliasvalue(:nil,   :absent)
+      aliasvalue(0,      :absent)
+      validate do |val|
+        min_metric = 0
+        max_metric = 65535
+        if ! (val.to_s == 'absent' or (min_metric .. max_metric).include?(val.to_i))
+          raise ArgumentError, "'#{val}' is not a valid metric (must be a integer value in range (#{min_metric} .. #{max_metric})"
+        end
+      end
+      munge do |val|
+        ((val == :absent)  ?  :absent  :  val.to_i)
+      end
+    end
+end
+
+# vim: set ts=2 sw=2 et :
