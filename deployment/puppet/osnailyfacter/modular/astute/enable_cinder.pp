472fec4df3cd7d6ba6e8cab2cf5e10fdf7b0a5e2
Igor Zinovik <izinovik@mirantis.com>
2015-04-29 15:30:12 +0300

    install 'cinder-volume' package prior enabling 'cinder-volume' service
    - astute deployment task enable_cinder.pp tries to start 'cinder-volume'
      service prior package 'cinder-volume' installation on Ubuntu
    
    Change-Id: I4b233304830c28d6a6849b972ca90f9a54650b9c
    Closes-bug: #1449093

--- deployment/puppet/osnailyfacter/modular/astute/enable_cinder.pp
+++ deployment/puppet/osnailyfacter/modular/astute/enable_cinder.pp
@@ -1,6 +1,11 @@
 include cinder::params
 
+$volume_package = $::cinder::params::volume_package
 $volume_service = $::cinder::params::volume_service
+
+package { $volume_service:
+  ensure     => installed,
+}
 
 service { $volume_service:
   ensure     => running,
@@ -8,3 +13,5 @@ service { $volume_service:
   hasstatus  => true,
   hasrestart => true,
 }
+
+Package[$volume_service]->Service[$volume_service]
