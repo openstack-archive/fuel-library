d4b18d6b07da2d1e278bd084951ee6448f4903e8
Bogdan Dobrelya <bdobrelia@mirantis.com>
2015-04-30 13:56:18 +0200

    Disable rabbitmq management plugin
    There is a known security issue exist in the
    management plugin for RabbitMQ <3.4.3, so it
    has to be disabled by default.
    
    Closes-bug: #1450443
    Closes-bug: #1455761
    
    Change-Id: Ic01c26200f6019a8112b1c5fb04a282e64b3b3e6
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/osnailyfacter/modular/rabbitmq/rabbitmq.pp
+++ deployment/puppet/osnailyfacter/modular/rabbitmq/rabbitmq.pp
@@ -80,6 +80,7 @@ if $queue_provider == 'rabbitmq' {
   )
 
   class { '::rabbitmq':
+    admin_enable               => false,
     repos_ensure               => false,
     package_provider           => $package_provider,
     package_source             => undef,
