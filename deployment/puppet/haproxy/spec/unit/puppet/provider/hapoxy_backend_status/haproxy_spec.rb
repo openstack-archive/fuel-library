8a8c822d483ca1d581cf2a091d80c57779e94a9c
Dmitry Ilyin <dilyin@mirantis.com>
2015-04-30 18:25:18 +0300

    Refactor Haproxy_backend_status
    * Clean the code up
    * Improved debugging
    
    Change-Id: Ie1ba15e2f40c903efdf970447676987a14156ed4
    Related-Bug: 1449219
    Related-Bug: 1459357

--- deployment/puppet/haproxy/spec/unit/puppet/provider/hapoxy_backend_status/haproxy_spec.rb
+++ deployment/puppet/haproxy/spec/unit/puppet/provider/hapoxy_backend_status/haproxy_spec.rb
@@ -1,15 +1,25 @@
 require 'puppet'
 
-provider_class = Puppet::Type.type(:haproxy_backend_status).provider(:haproxy)
+describe Puppet::Type.type(:haproxy_backend_status).provider(:haproxy) do
+  let (:resource) do
+    Puppet::Type::Haproxy_backend_status.new(
+      {
+          :name   => 'test',
+          :socket => '/var/run/haproxy.sock',
+      }
+    )
+  end
 
-describe provider_class do
-  before :each do
-    @resource = Puppet::Type::Haproxy_backend_status.new(
-        {
-            :name   => 'test',
-            :socket => '/var/run/haproxy.sock',
-        })
-    @provider = provider_class.new(@resource)
+  let (:provider) do
+    provider = resource.provider
+    if ENV['SPEC_PUPPET_DEBUG']
+      class << provider
+        def debug(msg)
+          puts msg
+        end
+      end
+    end
+    provider
   end
 
   let(:csv) {
@@ -20,62 +30,92 @@ test,BACKEND,0,0,0,0,800,0,0,0,0,0,,0,0,0,0,UP,1,1,0,,0,7403,0,,1,18,0,,0,,1,0,,
 test-down,FRONTEND,,,0,0,8000,0,0,0,0,0,0,,,,,OPEN,,,,,,,,,1,8,0,,,,0,0,0,0,,,,0,0,0,0,0,0,,0,0,0,,,0,0,0,0,,,,,,,,
 test-down,node-16,0,0,0,0,,0,0,0,,0,,0,0,0,0,DOWN,1,1,0,1,1,7402,7402,,1,8,1,,0,,2,0,,0,L4CON,,0,0,0,0,0,0,0,0,,,,0,0,,,,,-1,Connection refused,,0,0,0,0,
 test-down,BACKEND,0,0,0,0,800,0,0,0,0,0,,0,0,0,0,DOWN,0,0,0,,1,7402,7402,,1,8,0,,0,,1,0,,0,,,,0,0,0,0,0,0,,,,,0,0,0,0,0,0,-1,,,0,0,0,0,
+test-init,FRONTEND,,,0,0,8000,0,0,0,0,0,0,,,,,OPEN,,,,,,,,,1,8,0,,,,0,0,0,0,,,,0,0,0,0,0,0,,0,0,0,,,0,0,0,0,,,,,,,,
+test-init,node-16,0,0,0,0,,0,0,0,,0,,0,0,0,0,INIT,1,1,0,1,1,7402,7402,,1,8,1,,0,,2,0,,0,L4CON,,0,0,0,0,0,0,0,0,,,,0,0,,,,,-1
+test-init,BACKEND,0,0,0,0,800,0,0,0,0,0,,0,0,0,0,INIT,0,0,0,,1,7402,7402,,1,8,0,,0,,1,0,,0,,,,0,0,0,0,0,0,,,,,0,0,0,0,0,0,-1,,,0,0,0,0,
 eof
   }
 
   let(:stats) {
     {
-        'test' => :up,
-        'test-down' => :down,
+        'test' => 'UP',
+        'test-down' => 'DOWN',
+        'test-init' => 'INIT',
     }
   }
 
-  it 'should get status for :present or :absent checks for present backend' do
-    @resource[:ensure] = 'present'
-    @resource[:name] = 'test'
-    expect(@provider).to receive(:stats).at_least(:once).and_return(stats)
-    expect(@provider.ensure).to eq(:present)
-  end
-
-  it 'should get status for :present or :absent checks for absent backend' do
-    @resource[:ensure] = 'present'
-    @resource[:name] = 'test2'
-    expect(@provider).to receive(:stats).at_least(:once).and_return(stats)
-    expect(@provider.ensure).to eq(:absent)
-  end
-
-  it 'should get status for :up and :down for up backend' do
-    @resource[:ensure] = 'up'
-    @resource[:name] = 'test'
-    expect(@provider).to receive(:stats).at_least(:once).and_return(stats)
-    expect(@provider.ensure).to eq(:up)
-  end
-
-  it 'should get status for :up and :down for down backend' do
-    @resource[:ensure] = 'up'
-    @resource[:name] = 'test-down'
-    expect(@provider).to receive(:stats).at_least(:once).and_return(stats)
-    expect(@provider.ensure).to eq(:down)
-  end
-
-  it 'should get status for :up and :down for missing backend' do
-    @resource[:ensure] = 'up'
-    @resource[:name] = 'test2'
-    expect(@provider).to receive(:stats).at_least(:once).and_return(stats)
-    expect(@provider.ensure).to eq(:absent)
-  end
-
-  it 'should detect missing backend' do
-    @resource[:name] = 'mytest'
-    @resource[:ensure] = 'up'
-    expect(@provider).to receive(:stats).at_least(:once).and_return(stats)
-    expect(@provider.exists?).to eq(false)
-    expect(@provider.ensure).to eq(:absent)
+  before(:each) do
+    provider.stubs(:stats).returns(stats)
+    provider.stubs(:get_haproxy_debug_report).returns nil
   end
 
   it 'should parse csv data' do
-    expect(@provider).to receive(:csv).at_least(:once).and_return(csv)
-    expect(@provider.stats).to eq(stats)
+    provider.expects(:csv).returns csv
+    provider.unstub(:stats)
+    expect(provider.stats).to eq(stats)
+  end
+
+  it 'should get status for :present or :absent checks for present backend' do
+    resource[:ensure] = 'present'
+    resource[:name] = 'test'
+    expect(provider.ensure).to eq(:present)
+  end
+
+  it 'should get status for :present or :absent checks for absent backend' do
+    resource[:ensure] = 'present'
+    resource[:name] = 'test2'
+    expect(provider.ensure).to eq(:absent)
+  end
+
+  it 'should get status for :up and :down for up backend' do
+    resource[:ensure] = 'up'
+    resource[:name] = 'test'
+    expect(provider.ensure).to eq(:up)
+  end
+
+  it 'should get status for :up and :down for down backend' do
+    resource[:ensure] = 'up'
+    resource[:name] = 'test-down'
+    expect(provider.ensure).to eq(:down)
+  end
+
+  it 'should get status for :up and :down for missing backend' do
+    resource[:ensure] = 'up'
+    resource[:name] = 'test2'
+    expect(provider.ensure).to eq(:absent)
+  end
+
+  it 'should treat an unknown status as :present' do
+    resource[:ensure] = 'up'
+    resource[:name] = 'test-init'
+    expect(provider.ensure).to eq(:present)
+  end
+
+  it 'should treat a missing backend as :absent' do
+    resource[:name] = 'mytest'
+    resource[:ensure] = 'up'
+    expect(provider.ensure).to eq(:absent)
+  end
+
+  it 'should print the haproxy backend status report when status is extracted' do
+    provider.expects(:get_haproxy_debug_report)
+    provider.ensure
+  end
+
+  it 'should print the haproxy backend status report when waiting have finished' do
+    provider.expects(:get_haproxy_debug_report)
+    provider.ensure = :up
+  end
+
+  it 'should fail when there is no retries left and print the haproxy backend status' do
+    resource[:name] = 'test-down'
+    resource[:step] = 0
+    resource[:count] = 3
+    provider.expects(:get_haproxy_debug_report).once
+    provider.expects(:stats).returns(stats).times(3)
+    expect {
+      provider.ensure = :up
+    }.to raise_error
   end
 
 
