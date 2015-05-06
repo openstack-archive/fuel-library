ecb881c350f108cd655a1ecfac9efa69a98bfabf
Denis Egorenko <degorenko@mirantis.com>
2015-05-06 19:49:44 +0300

    Add Sahara templates for HDP 2.2.0
    We need to add Sahara templates for HDP plugin
    for version 2.2.0
    
    Change-Id: Ia1d663cee807d9b48355e6708f7d2409e6f3aa8e
    Closes-bug: #1452349

--- deployment/puppet/sahara/manifests/templates/create_templates.pp
+++ deployment/puppet/sahara/manifests/templates/create_templates.pp
@@ -164,11 +164,65 @@ class sahara::templates::create_templates (
     description => 'Hortonworks Data Platform (HDP) 2.0.6 cluster with manager, master and 3 worker nodes. The manager node is dedicated to Ambari 1.4.1 management console. The master node contains all management Hadoop processes. Workers contain Hadoop processes for data storage and processing.',
     plugin_name => 'hdp',
     node_groups => [
-      {'name' => 'hdp-2-master', 'count' => 1},
-      {'name' => 'hdp-2-manager', 'count' => 1},
-      {'name' => 'hdp-2-worker', 'count' => 3}
+      {'name' => 'hdp-2.0-master', 'count' => 1},
+      {'name' => 'hdp-2.0-manager', 'count' => 1},
+      {'name' => 'hdp-2.0-worker', 'count' => 3}
     ],
     hadoop_version => '2.0.6',
+  }
+
+  sahara_node_group_template { 'hdp-2_2-master' :
+    description => 'The master node contains all management Hadoop components like Ambari, NameNode, HistoryServer and ResourceManager. It also includes Oozie server required to run Hadoop jobs.',
+    plugin_name => 'hdp',
+    flavor_id => 'm1.large',
+    node_processes => [
+        "NAMENODE",
+        "SECONDARY_NAMENODE",
+        "ZOOKEEPER_SERVER",
+        "AMBARI_SERVER",
+        "HIVE_SERVER",
+        "HIVE_METASTORE",
+        "MYSQL_SERVER",
+        "WEBHCAT_SERVER",
+        "TEZ_CLIENT",
+        "HISTORYSERVER",
+        "RESOURCEMANAGER",
+        "OOZIE_SERVER",
+        "NAGIOS_SERVER",
+        "GANGLIA_SERVER"
+    ],
+    hadoop_version => '2.2.0',
+    auto_security_group => true,
+  }
+
+  sahara_node_group_template { 'hdp-2_2-worker' :
+    description => 'The worker node contains components that can be scaled by running more nodes. Each node includes everything required for data storage and processing.',
+    plugin_name => 'hdp',
+    flavor_id => 'm1.medium',
+    node_processes => [
+        "DATANODE",
+        "HDFS_CLIENT",
+        "ZOOKEEPER_CLIENT",
+        "HIVE_CLIENT",
+        "PIG",
+        "TEZ_CLIENT",
+        "MAPREDUCE2_CLIENT",
+        "YARN_CLIENT",
+        "NODEMANAGER",
+        "OOZIE_CLIENT"
+    ],
+    hadoop_version => '2.2.0',
+    auto_security_group => true,
+  }
+
+  sahara_cluster_template { 'hdp-2_2' :
+    description => 'Hortonworks Data Platform (HDP) 2.2.0 cluster with manager, master and 4 worker nodes. The master node contains all management Hadoop processes. Workers contain Hadoop processes for data storage and processing.',
+    plugin_name => 'hdp',
+    node_groups => [
+      {'name' => 'hdp-2.2-master', 'count' => 1},
+      {'name' => 'hdp-2.2-worker', 'count' => 4}
+    ],
+    hadoop_version => '2.2.0',
   }
 
 }
