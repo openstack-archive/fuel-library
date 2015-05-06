faa43781dc53ca3f8992c31fe23346a97cd3aedc
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-05-06 19:16:57 +0300

    Add basic SSL for master node UI
    Set port 8443 listening for SSL connections using standard
    localhost key and certificate generated during openssl installation.
    
    Remove SSLv3 from protocols and RC4 from ciphers.
    
    Implements-blueprint: ssl-endpoints
    Change-Id: I1cd44930d648df00070151f6dc825b7cbd387858

--- deployment/puppet/nailgun/manifests/nginx.pp
+++ deployment/puppet/nailgun/manifests/nginx.pp
@@ -1,3 +1,10 @@
+# Class configures overall nginx for Nailgun
+#
+# Parameters
+#
+# [*ssl_enabled*]
+#   (optional) enables certificate generation for nginx SSL
+#
 class nailgun::nginx(
   $production = "production",
   $repo_root = "/var/www/nailgun",
@@ -8,6 +15,7 @@ class nailgun::nginx(
   $ostf_host = '127.0.0.1',
   $keystone_host = '127.0.0.1',
   $nailgun_host = '127.0.0.1',
+  $ssl_enabled = false,
   ) {
 
   Exec  {path => '/usr/bin:/bin:/usr/sbin:/sbin'}
@@ -39,6 +47,25 @@ class nailgun::nginx(
     repo_root => $repo_root,
     notify => Service["nginx"],
   }
+
+  if $ssl_enabled {
+    openssl::certificate::x509 { 'nginx':
+      ensure       => present,
+      country      => 'US',
+      organization => 'Mirantis',
+      commonname   => 'fuel.master.local',
+      state        => 'California',
+      unit         => 'Fuel Deployment Team',
+      email        => "root@fuel.master.local",
+      days         => 3650,
+      base_dir     => '/etc/pki/tls/',
+      owner        => 'root',
+      group        => 'root',
+      force        => false,
+      cnf_tpl      => 'openssl/cert.cnf.erb',
+    }
+  }
+
   class { "nailgun::nginx-service":
     service_enabled => $service_enabled,
   }
@@ -50,6 +77,7 @@ class nailgun::nginx(
     keystone_host => $keystone_host,
     nailgun_host  => $nailgun_host,
     notify        => Service["nginx"],
+    ssl_enabled   => $ssl_enabled,
   }
 }
 
