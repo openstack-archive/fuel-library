f5c9be69a325b7bb1c0e91029c4857a7b321a3c5
Alex Schultz <aschultz@mirantis.com>
2015-05-05 20:55:20 -0500

    Connectivity tests after netconfig and ntp
    Adding a repository connectivity test task to be run after netconfig
    to ensure that software repository access is OK before proceeding
    with the rest of the deployment.  Additionally, adding in post
    connectivity tests after NTP server to ensure that those services
    will be able to reach the configured settings.
    
    With this change, an additional function has been added to the
    TestCommon class to perform an NTP request (TestCommon::Network.ntp?)
    to assist in validating the NTP servers provided and ensure they can
    be used.
    
    Change-Id: I6b0302ce403871384d377aceb7e94b09126b885e
    Closes-Bug: 1261940

---
+++ deployment/puppet/osnailyfacter/modular/netconfig/connectivity_tests.pp
@@ -0,0 +1,5 @@
+notice('MODULAR: connectivity_tests.pp')
+# This file intentionally left blank, the tests are in the post test.
+# We currently do not support the ability to run a shell command as
+# part of the regular deployment so we are calling puppet with this file
+# but the important work is done in the post test.
