33d164e5f6e5b9b887fce75b118116c3912e8850
Dmitry Ilyin <dilyin@mirantis.com>
2015-05-08 22:17:19 +0300

    Fixes for pcmk_nodes and crm_node
    * Use crm_node -n to get node's hostname
    * Check if node is in cluster in service provider
    * implement pacemaker nodes add in pcmk_nodes
    * compatibility between fqdn and hostname in pcmk_nodes
    * fix new node id and number generation in pcmk_nodes
    
    Change-Id: I2e40e25df975621506141dfe78efa884255f1642
    Related-Bug: 1451795

--- deployment/puppet/pacemaker/lib/puppet/type/pcmk_nodes.rb
+++ deployment/puppet/pacemaker/lib/puppet/type/pcmk_nodes.rb
@@ -20,12 +20,11 @@ module Puppet
       end
     end
 
-    newproperty(:corosync_nodes, :array_matching => :all) do
-      defaultto { @resource[:nodes].keys if @resource[:nodes] }
+    newproperty(:corosync_nodes) do
+      defaultto { @resource[:nodes] }
 
       def insync?(is)
-        return false unless is.is_a? Array and should.is_a? Array
-        is.sort == should.sort
+        compare_by_keys is, should, %w(id ip)
       end
 
       def is_to_s(is)
@@ -37,12 +36,11 @@ module Puppet
       end
     end
 
-    newproperty(:pacemaker_nodes, :array_matching => :all) do
-      defaultto { @resource[:nodes].keys if @resource[:nodes] }
+    newproperty(:pacemaker_nodes) do
+      defaultto { @resource[:nodes] }
 
       def insync?(is)
-        return false unless is.is_a? Array and should.is_a? Array
-        is.sort == should.sort
+        compare_by_keys is, should, 'id'
       end
 
       def is_to_s(is)
@@ -52,11 +50,50 @@ module Puppet
       def should_to_s(should)
         should.inspect
       end
+    end
+
+    newparam(:add_pacemaker_nodes) do
+      defaultto true
+    end
+
+    newparam(:remove_pacemaker_nodes) do
+      defaultto true
+    end
+
+    newparam(:add_corosync_nodes) do
+      defaultto true
+    end
+
+    newparam(:remove_corosync_nodes) do
+      defaultto true
     end
 
     def validate
-      fail 'No corosync_nodes!' unless self[:corosync_nodes].is_a? Array and self[:corosync_nodes].any?
-      fail 'No pacemaker_nodes!' unless self[:pacemaker_nodes].is_a? Array and self[:pacemaker_nodes].any?
+      fail 'No corosync_nodes!' unless self[:corosync_nodes].is_a? Hash and self[:corosync_nodes].any?
+      fail 'No pacemaker_nodes!' unless self[:pacemaker_nodes].is_a? Hash and self[:pacemaker_nodes].any?
+    end
+
+    def compare_by_keys(hash1, hash2, keys=nil)
+      return hash1 == hash2 unless keys
+      keys = [keys] unless keys.is_a? Array
+      fail 'First argument should be a Hash!' unless hash1.is_a? Hash
+      fail 'Second argument should be a Hash!' unless hash2.is_a? Hash
+      filtered_hash1 = filter_hash hash1, keys
+      filtered_hash2 = filter_hash hash2, keys
+      filtered_hash1 == filtered_hash2
+    end
+
+    def filter_hash(hash, keys)
+      data = {}
+      keys = [keys] unless keys.is_a? Array
+      fail 'First argument should be a Hash!' unless hash.is_a? Hash
+      hash.each do |hash_key, hash_value|
+        data[hash_key] = {}
+        keys.each do |key|
+          data[hash_key].store key, hash_value[key] if hash_value.key? key
+        end
+      end
+      data
     end
 
   end
