58a40bf6f115e8ffdef20fa262b6344f85e0c0c3
Dmitry Ilyin <dilyin@mirantis.com>
2015-05-08 22:17:19 +0300

    Fixes for pcmk_nodes and crm_node
    * Use crm_node -n to get node's hostname
    * Check if node is in cluster in service provider
    * implement pacemaker nodes add in pcmk_nodes
    * compatibility between fqdn and hostname in pcmk_nodes
    * fix new node id and number generation in pcmk_nodes
    
    Change-Id: I2e40e25df975621506141dfe78efa884255f1642
    Related-Bug: 1451795

--- deployment/puppet/osnailyfacter/modular/cluster/cluster.pp
+++ deployment/puppet/osnailyfacter/modular/cluster/cluster.pp
@@ -2,14 +2,24 @@ notice('MODULAR: cluster.pp')
 
 $nodes = hiera('nodes')
 $corosync_nodes = corosync_nodes($nodes)
+$role = hiera('role')
 
 class { '::cluster':
   internal_address  => hiera('internal_address'),
-  unicast_addresses => ipsort(values($corosync_nodes)),
+  unicast_addresses => $corosync_nodes,
 }
 
-pcmk_nodes { 'pacemaker' :
-  nodes => $corosync_nodes,
+if $role == 'primary-controller' {
+  pcmk_nodes { 'pacemaker' :
+    nodes => $corosync_nodes,
+    add_pacemaker_nodes => false,
+  }
 }
 
+Service <| title == 'corosync' |> {
+  subscribe => File['/etc/corosync/service.d'],
+  require   => File['/etc/corosync/corosync.conf'],
+}
+
+Service['corosync'] -> Pcmk_nodes<||>
 Pcmk_nodes<||> -> Service<| provider == 'pacemaker' |>
