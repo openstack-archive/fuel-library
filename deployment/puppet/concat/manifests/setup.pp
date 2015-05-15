3264ee70274767f1d8888be01ced03129ae13a5e
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2015-05-15 23:25:46 +0200

    Cherry-pick fix from puppetlabs-concat
    sha1: 820bc092f443bcf540240d8d2a2081e75b3145b7
    
    Concat 1.1.0 has idempotancy issue with `ensure => 'absent'`
    Bug described at
    https://tickets.puppetlabs.com/browse/MODULES-1311
    
    Fix fixtures for tests as 10.108.0.2 is not available during the tests
    
    Change-Id: I900980647d865d2712c823d04481ecebe91e9a0c
    Partial-Bug: 1455389
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/concat/manifests/setup.pp
+++ deployment/puppet/concat/manifests/setup.pp
@@ -19,10 +19,10 @@ class concat::setup {
   } else {
     fail ('$concat_basedir not defined. Try running again with pluginsync=true on the [master] and/or [main] section of your node\'s \'/etc/puppet/puppet.conf\'.')
   }
-  
+
   # owner and mode of fragment files (on windows owner and access rights should be inherited from concatdir and not explicitly set to avoid problems)
-  $fragment_owner = $osfamily ? { 'windows' => undef, default => $::id }
-  $fragment_mode  = $osfamily ? { 'windows' => undef, default => '0640' }
+  $fragment_owner = $::osfamily ? { 'windows' => undef, default => $::id }
+  $fragment_mode  = $::osfamily ? { 'windows' => undef, default => '0640' }
 
   $script_name = $::kernel ? {
     'windows' => 'concatfragments.rb',
@@ -31,9 +31,9 @@ class concat::setup {
 
   $script_path = "${concatdir}/bin/${script_name}"
 
-  $script_owner = $osfamily ? { 'windows' => undef, default => $::id }
+  $script_owner = $::osfamily ? { 'windows' => undef, default => $::id }
 
-  $script_mode = $osfamily ? { 'windows' => undef, default => '0755' }
+  $script_mode = $::osfamily ? { 'windows' => undef, default => '0755' }
 
   $script_command   = $::kernel ? {
     'windows' => "ruby.exe ${script_path}",
