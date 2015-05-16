e009eeab70d7e8423ab88fd92301d0df308dda29
Alex Schultz <aschultz@mirantis.com>
2015-05-15 20:54:38 -0500

    Mock url_available and ntp_available functions
    With the addition of the url_available and ntp_available puppet
    functions as part of the connectivity tests, we need to mock them out
    for our noop tests so we don't actually fail when an test url is used.
    
    Change-Id: I578e266e931f12d2e620a80994257871181e9f9b
    Partial-Bug: 1261940
    Closes-Bug: 1455702

--- tests/noop/spec/hosts/ntp/ntp-check_spec.rb
+++ tests/noop/spec/hosts/ntp/ntp-check_spec.rb
@@ -3,5 +3,10 @@ require 'shared-examples'
 manifest = 'ntp/ntp-check.pp'
 
 describe manifest do
+  before(:each) {
+    Puppet::Parser::Functions.newfunction(:ntp_available) { |args|
+      return true
+    }
+  }
   test_ubuntu_and_centos manifest
 end
