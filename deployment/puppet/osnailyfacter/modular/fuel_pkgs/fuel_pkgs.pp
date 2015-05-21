6e6d1cb23c89f5159a0112b4c496adeb30462111
Vladimir Kuklin <vkuklin@mirantis.com>
2015-05-21 20:21:42 +0300

    Ensure the rabbit-fence daemon started and respawned
    W/o this fix, if the daemon crashed, it
    would remain stopped. Also, the rabbit fence
    daemon will crash on start, when there is no
    /var/run/rabbitmq piddir exist.
    
    The solution is
    * add the respawn option to
    the daemon's upstart (TODO for Centos inittab)
    * and ensure the piddir
    created by upstart/init.d before starting the daemon.
    
    This also requires to add the rabbitmq and
    fuel-rabbit-fence packages as a dependency.
    
    Closes-bug: #1456791
    
    Change-Id: I4e71eb9e4aa4ff3b877aa89a37d82215740aaeab
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment/puppet/osnailyfacter/modular/fuel_pkgs/fuel_pkgs.pp
+++ deployment/puppet/osnailyfacter/modular/fuel_pkgs/fuel_pkgs.pp
@@ -1,6 +1,6 @@
 notice('MODULAR: fuel_pkgs.pp')
 
-$fuel_packages=['fuel-ha-utils','fuel-misc', 'fuel-rabbit-fence']
+$fuel_packages=['fuel-ha-utils','fuel-misc']
 notify{"this is the place where ${fuel_packages} should be installed":}
 #FIXME(algarendil): remove this if when we switch to pkg-based stuff
 if $::fuel_pkgs_exist == 'true'
