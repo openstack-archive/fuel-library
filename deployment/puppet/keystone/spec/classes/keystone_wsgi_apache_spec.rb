1fcbb01caa00bc9ace0bdba4c2e3134de7cfe5d9
Vladimir Kuklin <vkuklin@mirantis.com>
2015-05-21 05:32:26 +0300

    Set WSGI process display-name
    This commit is almost a cherry-pick of
    Id79c3a36b3f84938070f41c7171dfee20d1745e9
    
    It sets wsgi process name correctly
    so it sends correct syslog tag
    to the master node
    
    Change-Id: I25cdcd8300f1f56657f59637a999b9e282676da3
    Closes-bug: #1455756

--- deployment/puppet/keystone/spec/classes/keystone_wsgi_apache_spec.rb
+++ deployment/puppet/keystone/spec/classes/keystone_wsgi_apache_spec.rb
@@ -73,6 +73,13 @@ describe 'keystone::wsgi::apache' do
         'docroot_group'               => 'keystone',
         'ssl'                         => 'true',
         'wsgi_daemon_process'         => 'keystone_main',
+        'wsgi_daemon_process_options' => {
+          'user'         => 'keystone',
+          'group'        => 'keystone',
+          'processes'    => '1',
+          'threads'      => '42',
+          'display-name' => 'keystone-main',
+        },
         'wsgi_process_group'          => 'keystone_main',
         'wsgi_script_aliases'         => { '/' => "#{platform_parameters[:wsgi_script_path]}/main" },
         'require'                     => ['Class[Apache::Mod::Wsgi]', 'File[keystone_wsgi_main]']
@@ -118,6 +125,13 @@ describe 'keystone::wsgi::apache' do
         'docroot_group'               => 'keystone',
         'ssl'                         => 'false',
         'wsgi_daemon_process'         => 'keystone_main',
+        'wsgi_daemon_process_options' => {
+                  'user' => 'keystone',
+                 'group' => 'keystone',
+             'processes' => '37',
+               'threads' => '42',
+          'display-name' => 'keystone-main',
+        },
         'wsgi_process_group'          => 'keystone_main',
         'wsgi_script_aliases'         => { '/' => "#{platform_parameters[:wsgi_script_path]}/main" },
         'require'                     => ['Class[Apache::Mod::Wsgi]', 'File[keystone_wsgi_main]']
@@ -153,6 +167,13 @@ describe 'keystone::wsgi::apache' do
         'docroot_group'               => 'keystone',
         'ssl'                         => 'true',
         'wsgi_daemon_process'         => 'keystone_main',
+        'wsgi_daemon_process_options' => {
+                  'user' => 'keystone',
+                 'group' => 'keystone',
+             'processes' => '37',
+               'threads' => '42',
+          'display-name' => 'keystone-main',
+        },
         'wsgi_process_group'          => 'keystone_main',
         'wsgi_script_aliases'         => {
         '/main/endpoint'  => "#{platform_parameters[:wsgi_script_path]}/main",
