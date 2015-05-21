9e4bbca9e03b3bf032d8f1cec32453b027cc14a6
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-05-21 14:34:07 +0200

    Close connections to glance-{api,registry} after 11 minutes
    Change-Id: I5d8ba80038dafca12b06bef7804b28cae8e57207
    Closes-Bug: 1456683

--- deployment/puppet/openstack/manifests/ha/glance.pp
+++ deployment/puppet/openstack/manifests/ha/glance.pp
@@ -8,15 +8,19 @@ class openstack::ha::glance {
     public                 => true,
     require_service        => 'glance-api',
     haproxy_config_options => {
-        option => ['httpchk', 'httplog','httpclose'],
+        'option'         => ['httpchk', 'httplog','httpclose'],
+        'timeout server' => '11m',
     },
     balancermember_options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3',
   }
 
   openstack::ha::haproxy_service { 'glance-registry':
     # after neutron
-    order           => '090',
-    listen_port     => 9191,
-    require_service => 'glance-registry',
+    order                  => '090',
+    listen_port            => 9191,
+    require_service        => 'glance-registry',
+    haproxy_config_options => {
+        'timeout server' => '11m',
+    },
   }
 }
