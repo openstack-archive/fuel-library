49f31e22a028f6a85742d74820e33c11fbf7f463
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-05-22 19:11:43 +0300

    Adapt new haproxy module to Fuel
    Change-Id: I48038540d2a69a116521466369aaaddae02bceb3
    Implements: blueprint ssl-endpoints

--- deployment/puppet/haproxy/manifests/params.pp
+++ deployment/puppet/haproxy/manifests/params.pp
@@ -65,4 +65,7 @@ class haproxy::params {
     }
     default: { fail("The ${::osfamily} operating system is not supported with the haproxy module") }
   }
+  $use_include = false
+  $use_stats = true
+  $stats_port = '10000'
 }
