7ed671c67ffee8d8640854111ab8919412548ceb
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-05-22 16:47:12 +0300

    Implement SSL keys generation
    Keys are generated in astute prehook and saved in master
    node for further use. In current implementation only
    one key for haproxy is generated and then it will be used
    for all public OpenStack URLs.
    If self-signed certificates are created then new astute
    prehook task added will copy those certificates to
    apropriate nodes.
    
    Also this change adds new ssl modular task that checks
    if user uploaded certificate exists and saves it from
    yaml to local node filesystem.
    
    Implements: blueprint ssl-endpoints
    Depends-On: I2261dcfb55743b0fb31f4cbe42e7793176c8e1c0
    Change-Id: Ifcd9194b51129ea262363196a9963a96cc5760bb

---
+++ deployment/puppet/osnailyfacter/modular/ssl/ssl_keys_saving_post.rb
@@ -0,0 +1,14 @@
+require File.join File.dirname(__FILE__), '../test_common.rb'
+
+class SslKeysSavingPostTest < Test::Unit::TestCase
+
+  def has_public_ssl?
+    TestCommon::Settings.lookup 'public_ssl'
+  end
+
+  def test_ssl_keys_availability
+  	return unless has_public_ssl
+    assert File.file?('/var/lib/astute/haproxy/public_haproxy.pem'), 'No public keypair saved!'
+  end
+
+end
