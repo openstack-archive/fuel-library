0467eb3865705a276695f357f6b9ff27f65df6c1
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-05-22 16:47:12 +0300

    Implement SSL keys generation
    Keys generated in astute prehook and saved in master
    node for further use. In current implementation only
    one key for haproxy generated and then it will used
    for all public OpenStack URLs.
    If self-signed certificates created then new astute
    prehook task added will copy that certificates to
    apropriate nodes.
    
    Also this change add new ssl modular task that checks
    if user uploaded certificate exists and saves it from
    yaml to local node filesystem.
    
    Implements: blueprint ssl-endpoints
    Depends-On: I2261dcfb55743b0fb31f4cbe42e7793176c8e1c0
    Change-Id: Ifcd9194b51129ea262363196a9963a96cc5760bb

---
+++ deployment/puppet/osnailyfacter/modular/ssl/ssl_keys_saving.pp
@@ -0,0 +1,21 @@
+notice('MODULAR: ssl_keys_saving.pp')
+
+$public_ssl_hash = hiera('public_ssl')
+$pub_certificate_content = $public_ssl_hash['cert_data']
+$base_path = "/etc/pki/tls/certs"
+$astute_base_path = "/var/lib/astute/haproxy/"
+
+File {
+  owner => 'root',
+  group => 'root',
+  mode  => '0644',
+}
+
+file { [ $base_path, $astute_base_path ]:
+  ensure => directory,
+}->
+
+file { ["$base_path/public_haproxy.pem", "$astute_base_path/public_haproxy.pem"]:
+  ensure => present,
+  content => $pub_certificate_content,
+}
