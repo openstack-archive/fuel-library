6656abaa908dd0312f87e07ccda43d63e3719d6d
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-05-22 16:47:12 +0300

    Implement SSL keys generation
    Keys generated in astute prehook and saved in master
    node for further use. In current implementation only
    one key for haproxy generated and then it will used
    for all public OpenStack URLs.
    
    WORK IN PROGRESS
    
    Cross-Depend on: I2261dcfb55743b0fb31f4cbe42e7793176c8e1c0
    Fuel-CI: disable
    Change-Id: Ifcd9194b51129ea262363196a9963a96cc5760bb

---
+++ deployment/puppet/osnailyfacter/modular/astute/generate_haproxy_keys.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+while getopts ":i:h:o:p:" opt; do
+  case $opt in
+    i)  cluster_id=$OPTARG
+        ;;
+    h)  cn_name=$OPTARG
+        ;;
+    o)  open_ssl_keys=$OPTARG
+        ;;
+    p)  keys_path=$OPTARG
+        ;;
+  esac
+done
+BASE_PATH="$keys_path/$cluster_id"
+CONF_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+function generate_open_ssl_keys {
+  for i in $open_ssl_keys
+    do
+      local dir_path="$BASE_PATH/$i"
+      local key_path="$dir_path/$i.key"
+      local crt_path="$dir_path/$i.crt"
+      mkdir -p $dir_path
+      if [ ! -f $key_path ]; then
+        env SSL_CN_NAME="$cn_name" bash -c "openssl req -newkey rsa:2048 -nodes -keyout $key_path -x509 -days 3650 -out $crt_path -config $CONF_PATH/openssl.cnf 2>&1"
+        cat "$crt_path" "$key_path" > "$dir_path/$i.pem"
+      else
+        echo "Key $key_path already exists"
+      fi
+    done
+}
+
+generate_open_ssl_keys
