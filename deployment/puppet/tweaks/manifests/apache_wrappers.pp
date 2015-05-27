79dc73f8bcdcbe04994e6d7b80efef7da0cb849a
Dmitry Ilyin <dilyin@mirantis.com>
2015-05-27 13:36:40 +0300

    Create a wrapper to retry apache start and stop
    Try to stop, start end restart apache several times
    
    Change-Id: I906139554c7cf770ffdac29f95d3d97f29b87f43

---
+++ deployment/puppet/tweaks/manifests/apache_wrappers.pp
@@ -0,0 +1,26 @@
+class tweaks::apache_wrappers {
+  $init_dir = '/etc/init.d'
+  $sleep_time = '5'
+  $retry_count = '20'
+
+  if $::osfamily == 'RedHat' {
+    $service_name = 'httpd'
+  } elsif $::osfamily == 'Debian' {
+    $service_name = 'apache2'
+  } else {
+    fail("OS '${::operatingsystem}' is not supported!")
+  }
+
+  $init_script = "${init_dir}/${service_name}"
+
+  $start_command = "${init_script} start || sleep 60 && ${init_script} start"
+  $stop_command  = "${init_script} stop || sleep 60 && ${init_script} stop"
+
+  disable_garbage_collector()
+
+  Service <| name == $service_name or title == $service_name |> {
+    start      => $start_command,
+    stop       => $stop_command,
+    hasrestart => false,
+  }
+}
