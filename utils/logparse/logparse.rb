e855ec685aec68e1658988f835200a68b689d9b3
Dmitry Ilyin <dilyin@mirantis.com>
2015-05-28 16:27:27 +0300

    Update logparse
    Change-Id: I45a813859790a7ceb489ef4a2c1a8d00ab051c24

--- utils/logparse/logparse.rb
+++ utils/logparse/logparse.rb
@@ -2,10 +2,8 @@ require 'English'
 require 'time'
 
 log = []
-last_time = nil
-errors = 0
 top = 10
-evaltrace = false
+total = 0
 
 def show_seconds(seconds)
   "#{sprintf('%.02f',seconds)} sec (#{sprintf('%.02f',seconds / 60)} min)"
@@ -14,19 +12,8 @@ end
 while gets
   begin
 
-    # last line. exit this file!
-    if ($LAST_READ_LINE =~ /Finished catalog run in (\S+) seconds/)
-      total = $1.to_f
-      break
-    end
-
     # evaltrace line
     if ($LAST_READ_LINE =~ /.*\/?([A-Z0-9][0-9A-Za-z_]+)\[(.*)\].*Evaluated\s+in\s+(.*)\s+seconds/)
-      unless evaltrace
-        evaltrace = true
-        puts "Evaltrace found!"
-        log = []
-      end
       type = $1.to_sym
       title = $2
       duration = $3.to_f
@@ -38,48 +25,12 @@ while gets
       next if [ :Filebucket, :Stage, :Schedule, :Class].include? type
 
       log << { :time => time, :message => message, :level => level, :duration => duration, :number => $INPUT_LINE_NUMBER, :type => type, :title => title }
+      total += duration
 
       # line processed
       next
     end
 
-    # if we use evaltrace don't do normal parsing
-    next if evaltrace
-
-    # a puppet line with time
-    if ($LAST_READ_LINE =~ /^(\S+)\s+(\S+)\s+\((.*?)\)/)
-      time = Time.parse $1
-      level = $2
-      message = $3
-
-      # set duration of this line
-      if last_time
-        duration = time - last_time
-      else
-        duration = 0.0
-      end
-      last_time = time
-      
-      # line with resource
-      if (message =~ /.*\/([A-Z0-9][0-9A-Za-z_]+)\[(.*)\]/)
-        type = $1.to_sym
-        title = $2
-
-        log << { :time => time, :message => message, :level => level, :duration => duration, :number => $INPUT_LINE_NUMBER, :type => type, :title => title }
-        if (level == 'err:')
-          errors += 1
-        end
-
-        # line processed
-        next
-      end
-      
-      # line did not match
-      next
-
-    end
-    # end line with time
- 
   rescue
     # error. skip this line
     next
@@ -124,8 +75,4 @@ puts
 
 if total
   puts "Total: #{show_seconds(total)}"
-end
-
-if errors > 0
-  puts "Errors: #{errors}"
 end
