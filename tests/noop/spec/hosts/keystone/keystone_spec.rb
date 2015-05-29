51185376c60161a200c2deb41e81bced489662b1
Vladimir Kuklin <vkuklin@mirantis.com>
2015-05-29 19:20:45 +0300

    Disable Keystone WSGI support for 6.1
    Change-Id: I7dbc4e4097276663b5c38501d314b86dfe31a035
    Closes-Bug: 1459977

--- tests/noop/spec/hosts/keystone/keystone_spec.rb
+++ tests/noop/spec/hosts/keystone/keystone_spec.rb
@@ -16,12 +16,12 @@ describe manifest do
     memcached_servers = controller_nodes.map{ |n| n = n + ':11211' }.join(',')
     admin_token = Noop.hiera_structure 'keystone/admin_token'
 
-    # Keystone
     it 'should declare keystone class with admin_token' do
       should contain_class('keystone').with(
         'admin_token' => admin_token,
       )
     end
+
     it 'should configure memcache_pool keystone cache backend' do
       should contain_keystone_config('token/caching').with(:value => 'false')
       should contain_keystone_config('cache/enabled').with(:value => 'true')
@@ -33,45 +33,45 @@ describe manifest do
       should contain_keystone_config('cache/memcache_pool_unused_timeout').with(:value => '60')
     end
 
-    it 'should declare keystone::wsgi::apache class with 4 workers on 4 CPU system' do
-      should contain_class('keystone::wsgi::apache').with(
-        'threads' => '1',
-        'workers' => '4',
-      )
-    end
-
-    it 'should declare keystone::wsgi::apache class with 24 workers on 48 CPU system' do
-      facts[:processorcount] = 48
-      should contain_class('keystone::wsgi::apache').with(
-        'threads' => '1',
-        'workers' => '24',
-      )
-    end
-
-    it 'should setup keystone_wsgi_admin file properly' do
-      case facts[:operatingsystem]
-      when 'CentOS'
-        should contain_file('keystone_wsgi_admin').with(
-          'ensure'  => 'link',
-          'path'    => "/var/www/cgi-bin/keystone/admin",
-          'target'  => '/usr/share/keystone/keystone.wsgi',
-          'owner'   => 'keystone',
-          'group'   => 'keystone',
-          'mode'    => '0644',
-          'require' => "File[/var/www/cgi-bin/keystone]"
-        )
-      when 'Ubuntu'
-        should contain_file('keystone_wsgi_admin').with(
-          'ensure'  => 'file',
-          'path'    => "/usr/lib/cgi-bin/keystone/admin",
-          'source'  => 'puppet:///modules/keystone/httpd/keystone.py',
-          'owner'   => 'keystone',
-          'group'   => 'keystone',
-          'mode'    => '0644',
-          'require' => "File[/usr/lib/cgi-bin/keystone]"
-        )
-      end
-    end
+    # it 'should declare keystone::wsgi::apache class with 4 workers on 4 CPU system' do
+    #   should contain_class('keystone::wsgi::apache').with(
+    #     'threads' => '1',
+    #     'workers' => '4',
+    #   )
+    # end
+    #
+    # it 'should declare keystone::wsgi::apache class with 24 workers on 48 CPU system' do
+    #   facts[:processorcount] = 48
+    #   should contain_class('keystone::wsgi::apache').with(
+    #     'threads' => '1',
+    #     'workers' => '24',
+    #   )
+    # end
+    #
+    # it 'should setup keystone_wsgi_admin file properly' do
+    #   case facts[:operatingsystem]
+    #   when 'CentOS'
+    #     should contain_file('keystone_wsgi_admin').with(
+    #       'ensure'  => 'link',
+    #       'path'    => "/var/www/cgi-bin/keystone/admin",
+    #       'target'  => '/usr/share/keystone/keystone.wsgi',
+    #       'owner'   => 'keystone',
+    #       'group'   => 'keystone',
+    #       'mode'    => '0644',
+    #       'require' => "File[/var/www/cgi-bin/keystone]"
+    #     )
+    #   when 'Ubuntu'
+    #     should contain_file('keystone_wsgi_admin').with(
+    #       'ensure'  => 'file',
+    #       'path'    => "/usr/lib/cgi-bin/keystone/admin",
+    #       'source'  => 'puppet:///modules/keystone/httpd/keystone.py',
+    #       'owner'   => 'keystone',
+    #       'group'   => 'keystone',
+    #       'mode'    => '0644',
+    #       'require' => "File[/usr/lib/cgi-bin/keystone]"
+    #     )
+    #   end
+    # end
 
   end # end of shared_examples
 
