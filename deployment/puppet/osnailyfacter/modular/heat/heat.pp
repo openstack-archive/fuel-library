3d43b0975fe83ad5018031e1c3806aae997ecbe4
Aleksandr Didenko <adidenko@mirantis.com>
2015-05-29 17:39:25 -0500

    (WIP) Possibility to detach heat from controllers
    WORK IN PROGRESS
    
    Adds the possibility to use external (non-controllers) keystone,
    db, rabbitmq, etc for heat services.
    
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: I1ba75c74a83f780d2b623b0ad1e17ee38fecc0af

--- deployment/puppet/osnailyfacter/modular/heat/heat.pp
+++ deployment/puppet/osnailyfacter/modular/heat/heat.pp
@@ -2,13 +2,15 @@ notice('MODULAR: heat.pp')
 
 $controller_node_public   = hiera('controller_node_public')
 $controller_node_address  = hiera('controller_node_address')
-$heat_hash                = hiera('heat')
+$management_vip           = hiera('management_vip')
+$heat_hash                = hiera_hash('heat', {})
 $amqp_hosts               = hiera('amqp_hosts')
-$rabbit_hash              = hiera('rabbit_hash')
+$rabbit_hash              = hiera_hash('rabbit_hash', {})
 $max_retries              = hiera('max_retries')
 $max_pool_size            = hiera('max_pool_size')
 $max_overflow             = hiera('max_overflow')
 $idle_timeout             = hiera('idle_timeout')
+$service_endpoint         = hiera('service_endpoint', $management_vip)
 $debug                    = hiera('debug', false)
 $verbose                  = hiera('verbose', true)
 $use_syslog               = hiera('use_syslog', true)
@@ -16,10 +18,13 @@ $syslog_log_facility_heat = hiera('syslog_log_facility_heat')
 $deployment_mode          = hiera('deployment_mode')
 $internal_address         = hiera('internal_address')
 $database_password        = $heat_hash['db_password']
-$databse_user             = 'heat'
-$databse_name             = 'heat'
+$keystone_user            = pick($heat_hash['user'], 'heat')
+$keystone_tenant          = pick($heat_hash['tenant'], 'services')
+$db_host                  = pick($heat_hash['db_host'], $management_vip)
+$database_user            = pick($heat_hash['db_user'], 'heat')
+$database_name            = hiera('heat_db_name', 'heat')
 $read_timeout             = '60'
-$sql_connection           = "mysql://${databse_user}:${database_password}@${$controller_node_address}/${databse_name}?read_timeout=${read_timeout}"
+$sql_connection           = "mysql://${database_user}:${database_password}@${db_host}/${database_name}?read_timeout=${read_timeout}"
 
 ####### Disable upstart startup on install #######
 if $::operatingsystem == 'Ubuntu' {
@@ -44,17 +49,18 @@ if $::operatingsystem == 'Ubuntu' {
 
 class { 'openstack::heat' :
   external_ip              => $controller_node_public,
-
+  keystone_auth            => pick($heat_hash['keystone_auth'], true),
+  create_heat_db           => pick($heat_hash['create_heat_db'], true),
   api_bind_host            => $internal_address,
   api_cfn_bind_host        => $internal_address,
   api_cloudwatch_bind_host => $internal_address,
 
-  keystone_host            => $controller_node_address,
-  keystone_user            => 'heat',
+  keystone_host            => $service_endpoint,
+  keystone_user            => $keystone_user,
   keystone_password        => $heat_hash['user_password'],
-  keystone_tenant          => 'services',
+  keystone_tenant          => $keystone_tenant,
 
-  keystone_ec2_uri         => "http://${controller_node_address}:5000/v2.0",
+  keystone_ec2_uri         => "http://${service_endpoint}:5000/v2.0",
 
   rpc_backend              => 'heat.openstack.common.rpc.impl_kombu',
   amqp_hosts               => [$amqp_hosts],
@@ -62,7 +68,7 @@ class { 'openstack::heat' :
   amqp_password            => $rabbit_hash['password'],
 
   sql_connection           => $sql_connection,
-  db_host                  => $controller_node_address,
+  db_host                  => $db_host,
   db_password              => $database_password,
   max_retries              => $max_retries,
   max_pool_size            => $max_pool_size,
@@ -77,16 +83,11 @@ class { 'openstack::heat' :
   auth_encryption_key      => $heat_hash['auth_encryption_key'],
 }
 
-if ($deployment_mode == 'ha') or ($deployment_mode == 'ha_compact') {
-  include heat_ha::engine
+if hiera('heat_ha_engine', true){
+  if ($deployment_mode == 'ha') or ($deployment_mode == 'ha_compact') {
+    include heat_ha::engine
+  }
 }
-
-#file { '/usr/lib/ocf/resource.d/fuel' :
-#  ensure => 'directory',
-#  owner  => 'root',
-#  group  => 'root',
-#  mode   => '0755',
-#}
 
 #------------------------------
 
@@ -115,10 +116,10 @@ class { 'heat::docker_resource' :
 }
 
 class { 'heat::keystone::domain' :
-  auth_url          => "http://${controller_node_address}:35357/v2.0",
-  keystone_admin    => 'heat',
+  auth_url          => "http://${service_endpoint}:35357/v2.0",
+  keystone_admin    => $keystone_user,
   keystone_password => $heat_hash['user_password'],
-  keystone_tenant   => 'services',
+  keystone_tenant   => $keystone_tenant,
   domain_name       => 'heat',
   domain_admin      => 'heat_admin',
   domain_password   => $heat_hash['user_password'],
