da672a5a54e740925e0669c51e957246b2952de6
Eric Jacobs <eric@analog.sh>
2015-06-02 18:40:36 -0400

    Don't use more than 32 cpus for network queue affinities
    IF we have more than 32 cpus, discard any cpus > 32 for network queue
    cpu affinities.
    
    Closes-Bug: 1461306
    Change-Id: I6aff2af39f25a32c710d5cdf9708dad6b2a201c1

--- deployment/puppet/sysfs/lib/puppet/parser/functions/cpu_affinity_hex.rb
+++ deployment/puppet/sysfs/lib/puppet/parser/functions/cpu_affinity_hex.rb
@@ -5,6 +5,11 @@ EOS
   ) do |argv|
     number = argv[0].to_i
     fail "Argument should be the CPU number - integer value!" unless number.to_s == argv[0]
+    # sysfs is unable to use more than 8 bit mask for cpu affinity
+    # just use cpus 0-31 if > 32 cpus present
+    if number > 32 then
+      number = 32
+    end
     ((2 ** number) - 1).to_s(16)
   end
 end
