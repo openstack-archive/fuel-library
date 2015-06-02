180373f46549818eb319f4373e4eda208a449d1b
Sergey Vasilenko <svasilenko@mirantis.com>
2015-06-02 15:41:25 +0300

    workarounded Ubuntu bug: didn't execute ifup for OVS interfaces
    Interfaces will create in both modes onetime:
    * OVS port (with additional OVS-aware directives)
    * ethernet port (for executing at boot time)
    
    Change-Id: Id17dd4b0f875643d21fdfd62cb0fc872078e0d95
    Closes-bug: #1460972

--- deployment/puppet/l23network/lib/puppet/provider/l23_stored_config/ovs_ubuntu.rb
+++ deployment/puppet/l23network/lib/puppet/provider/l23_stored_config/ovs_ubuntu.rb
@@ -9,17 +9,16 @@ Puppet::Type.type(:l23_stored_config).provide(:ovs_ubuntu, :parent => Puppet::Pr
 
   self.unlink_empty_files = true
 
-  def self.property_mappings
-    rv = super
-    rv.merge!({
-      :onboot => 'allow-ovs'
-    })
-    return rv
-  end
+  # def self.property_mappings
+  #   rv = super
+  #   rv.merge!({
+  #     :onboot => 'allow-ovs'
+  #   })
+  #   return rv
+  # end
 
   def self.check_if_provider(if_data)
-    #((if_data[:if_provider] == 'allow-ovs')  ?  true  :  false)
-    if if_data[:if_provider] == 'allow-ovs'
+    if if_data[:if_provider] =~ /allow-\S/
         if_data[:if_provider] = :ovs
         true
     else
@@ -27,6 +26,28 @@ Puppet::Type.type(:l23_stored_config).provide(:ovs_ubuntu, :parent => Puppet::Pr
         false
     end
   end
+
+  def self.iface_file_header(provider)
+    rv = []
+
+    # Add onboot interfaces
+    if provider.onboot
+      rv << "auto #{provider.name}"
+    end
+
+    bridge = provider.bridge[0]
+    if provider.if_type.to_s == 'bridge'
+      rv << "allow-ovs #{provider.name}"
+    else
+      rv << "allow-#{bridge} #{provider.name}"
+    end
+    # Add iface header
+    rv << "iface #{provider.name} inet #{provider.method}"
+
+    return rv
+  end
+
+
 
   def self.mangle__type(val)
     :ethernet
