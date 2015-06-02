0276ca30852163670e474214c6591fa0f32a8728
Andrey Kirilochkin <akirilochkin@mirantis.com>
2015-06-02 17:58:50 -0500

    (WIP) Possibility to detach Keystone + Horizon from controllers
    WORK IN PROGRESS
    Adds the possibility to install Keystone + Horizon on
    separate node (non-controllers).
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: I9a509f08917c2574728d17ded740061d15e53f2c

---
+++ deployment/puppet/osnailyfacter/modular/kstn/keystone_post.rb
@@ -0,0 +1,32 @@
+require File.join File.dirname(__FILE__), '../test_common.rb'
+
+PUBLIC_PORT = 5000
+ADMIN_PORT = 35357
+
+class KeystonePostTest < Test::Unit::TestCase
+
+  def test_keystone_is_running
+    assert TestCommon::Process.running?('/usr/bin/keystone-all'), 'Keystone is not running!'
+  end
+
+  def test_keystone_public_url_accessible
+    url = "http://#{TestCommon::Settings.public_vip}:#{PUBLIC_PORT}"
+    assert TestCommon::Network.url_accessible?(url), "Public Keystone URL '#{url}' is not accessible!"
+  end
+
+  def test_keystone_admin_url_accessible
+    url = "http://#{TestCommon::Settings.management_vip}:#{ADMIN_PORT}"
+    assert TestCommon::Network.url_accessible?(url), "Admin Keystone URL '#{url}' is not accessible!"
+  end
+
+  def test_keystone_endpoint_list_run
+    TestCommon::Cmd.openstack_auth
+    cmd = 'keystone endpoint-list'
+    assert TestCommon::Process.run_successful?(cmd), "Could not run '#{cmd}'!"
+  end
+
+  def test_openrc_file_present
+    assert File.exist?('/root/openrc'), '/root/openrc is missing!'
+  end
+
+end
---
+++ deployment/puppet/osnailyfacter/modular/kstn/workloads_collector.pp
@@ -0,0 +1,11 @@
+notice('MODULAR: keystone/workloads_collector.pp')
+
+$workloads_hash        = hiera('workloads_collector', {})
+
+class { 'openstack::workloads_collector':
+  enabled               => $workloads_hash['enabled'],
+  workloads_username    => $workloads_hash['username'],
+  workloads_password    => $workloads_hash['password'],
+  workloads_tenant      => $workloads_hash['tenant'],
+  workloads_create_user => true,
+}
