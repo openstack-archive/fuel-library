0086e8c754f3d15628f3877abe9f8f2d26d14093
Kamil Sambor <ksambor@mirantis.com>
2015-06-03 17:31:42 +0200

    Add new tasks for configure and deploy vms
    * added basic template for vms
    * added manifest to spawn vms
    
    Implements: blueprint reduced-footprint
    Change-Id: I5a218975e3f441bc53b86066101595ad7cb5ef06

---
+++ deployment/puppet/osnailyfacter/modular/cluster/generate_vms.pp
@@ -0,0 +1,23 @@
+$libvirt_dir = '/etc/libvirt/qemu'
+$template_dir = '/var/lib/vms'
+$packages = ['qemu-utils', 'qemu-kvm', 'libvirt-bin']
+$libvirt_service_name = 'libvirt-bin'
+
+package { $packages:
+  ensure => 'installed',
+}
+service { $libvirt_service_name:
+  ensure  => 'running',
+  require => Package[$packages],
+}
+file { "${libvirt_dir}/autostart":
+  ensure  => 'directory',
+  require => Package[$packages],
+}
+exec { 'generate_vms':
+  command => "/etc/puppet/modules/osnailyfacter/modular/cluster/generate_vms.sh ${libvirt_dir} ${template_dir}",
+  onlyif  => "test `virsh -q list | wc -l` -lq 1",
+  path    => ['/usr/sbin', '/usr/bin' , '/sbin', '/bin'],
+  notify  => Service[$libvirt_service_name],
+  require => Package[$packages],
+}
