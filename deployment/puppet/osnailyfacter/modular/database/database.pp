62bf255303a6bd371038dc644899b8bf422b3da8
Nikita Koshikov <nkoshikov@mirantis.com>
2015-06-03 17:50:33 -0500

    (WIP) Possibility to detach database from controllers
    WORK IN PROGRESS
    - Added possibility to use different user/dbnames/password for neutron,cinder,nova,glance
      services.
    - Added possibility to overwrite some parameters needed for separate
      database deployment
    
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: I2b3239edd105715ceb88aa276ccc4afab4c046f0

--- deployment/puppet/osnailyfacter/modular/database/database.pp
+++ deployment/puppet/osnailyfacter/modular/database/database.pp
@@ -1,51 +1,100 @@
 notice('MODULAR: database.pp')
 
 $neutron              = hiera('use_neutron')
-$mysql_hash           = hiera('mysql')
-$keystone_hash        = hiera('keystone')
-$glance_hash          = hiera('glance')
-$nova_hash            = hiera('nova')
-$cinder_hash          = hiera('cinder')
+$mysql_hash           = hiera_hash('mysql',{})
+$keystone_hash        = hiera_hash('keystone',{})
+$glance_hash          = hiera_hash('glance',{})
+$nova_hash            = hiera_hash('nova',{})
+$cinder_hash          = hiera_hash('cinder',{})
 $internal_address     = hiera('internal_address')
 $network_scheme       = hiera('network_scheme', {})
 $neutron_db_password  = hiera('neutron_db_password', false)
 $controller_nodes     = hiera('controller_nodes')
+$database_nodes       = hiera('database_nodes',$controller_nodes)
 $use_syslog           = hiera('use_syslog', true)
 $primary_controller   = hiera('primary_controller')
+$primary_database     = hiera('primary_database', $primary_controller)
 $management_vip       = hiera('management_vip')
+$database_vip         = hiera('database_vip',$management_vip)
 
-$haproxy_stats_port  = '10000'
-$haproxy_stats_url   = "http://${management_vip}:${haproxy_stats_port}/;csv"
+$haproxy_stats_port  = hiera('haproxy_stats_port','10000')
+$haproxy_stats_url   = hiera('haproxy_stats_url',"http://${database_vip}:${haproxy_stats_port}/;csv")
 
 $mysql_root_password    = $mysql_hash['root_password']
 $mysql_bind_address     = '0.0.0.0'
 $mysql_account_security = true
 
-$keystone_db_user       = 'keystone'
-$keystone_db_dbname     = 'keystone'
-$keystone_db_password   = $keystone_hash['db_password']
+$keystone_db_user       = $keystone_hash['db']['user'] ? {
+     default  => $keystone_hash['db']['user'],
+     undef    => 'keystone',
+}
+$keystone_db_dbname     = $keystone_hash['db']['dbname'] ? {
+     default  => $keystone_hash['db']['dbname'],
+     undef    => 'keystone',
+}
+$keystone_db_password   = $keystone_hash['db']['password'] ? {
+     default  => $keystone_hash['db']['password'],
+     undef    => $keystone_hash['db_password'],
+}
 
-$glance_db_user         = 'glance'
-$glance_db_dbname       = 'glance'
-$glance_db_password     = $glance_hash['db_password']
+$glance_db_user         = $glance_hash['db']['user'] ? {
+     default  => $glance_hash['db']['user'],
+     undef    => 'glance',
+}
+$glance_db_dbname       = $glance_hash['db']['dbname'] ? {
+     default  => $glance_hash['db']['dbname'],
+     undef    => 'glance',
+}
+$glance_db_password     = $glance_hash['db']['password'] ? {
+     default  => $glance_hash['db']['password'],
+     undef    => $glance_hash['db_password'],
+}
 
-$nova_db_user           = 'nova'
-$nova_db_dbname         = 'nova'
-$nova_db_password       = $nova_hash['db_password']
+$nova_db_user           = $nova_hash['db']['user'] ? {
+     default  => $nova_hash['db']['user'],
+     undef    => 'nova',
+}
+$nova_db_dbname         = $nova_hash['db']['dbname'] ? {
+     default  => $nova_hash['db']['dbname'],
+     undef    => 'nova',
+}
+$nova_db_password       = $nova_hash['db']['password'] ? {
+     default  => $nova_hash['db']['password'],
+     undef    => $nova_hash['db_password'],
+}
 
-$cinder_db_user         = 'cinder'
-$cinder_db_dbname       = 'cinder'
-$cinder_db_password     = $cinder_hash['db_password']
+$cinder_db_user         = $cinder_hash['db']['user'] ? {
+     default  => $cinder_hash['db']['user'],
+     undef    => 'cinder',
+}
+$cinder_db_dbname       = $cinder_hash['db']['dbname'] ? {
+     default  => $cinder_hash['db']['dbname'],
+     undef    => 'cinder',
+}
+$cinder_db_password     = $cinder_hash['db']['password'] ? {
+     default  => $cinder_hash['db']['password'],
+     undef    => $cinder_hash['db_password'],
+}
 
-$neutron_db_user        = 'neutron'
-$neutron_db_dbname      = 'neutron'
+$neutron_db_user        = $neutron_hash['db']['user'] ? {
+     default  => $neutron_hash['db']['user'],
+     undef    => 'neutron',
+}
+$neutron_db_dbname      = $neutron_hash['db']['dbname'] ? {
+     default  => $neutron_hash['db']['dbname'],
+     undef    => 'neutron',
+}
+$neutron_db_password    = $neutron_hash['db']['password'] ? {
+     default  => $neutron_hash['db']['password'],
+     undef    => $neutron_db_password,
+}
 
 $enabled                  = true
 $allowed_hosts            = [ '%', $::hostname ]
-$galera_cluster_name      = 'openstack'
+$galera_cluster_name      = hiera('galera_cluster_name', 'openstack')
 $galera_node_address      = $internal_address
-$galera_nodes             = $controller_nodes
-$custom_mysql_setup_class = 'galera'
+$galera_nodes             = $database_nodes
+$custom_mysql_setup_class = hiera('custom_mysql_setup_class','galera')
 $mysql_skip_name_resolve  = true
 
 $status_user              = 'clustercheck'
@@ -80,7 +129,7 @@ class { 'openstack::db::mysql':
   allowed_hosts           => $allowed_hosts,
   enabled                 => $enabled,
   galera_cluster_name     => $galera_cluster_name,
-  primary_controller      => $primary_controller,
+  primary_controller      => $primary_database,
   galera_node_address     => $galera_node_address,
   galera_nodes            => $galera_nodes,
   custom_setup_class      => $custom_mysql_setup_class,
