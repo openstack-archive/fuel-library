24117080d0e5147a77220556bf7b1df8d41c7ddd
Kamil Sambor <ksambor@mirantis.com>
2015-06-03 17:31:42 +0200

    Add new tasks for configure and deploy vms
    * added basic template for vms
    * added manifest to spawn vms
    
    Implements: blueprint reduced-footprint
    Change-Id: I5a218975e3f441bc53b86066101595ad7cb5ef06

---
+++ tests/noop/spec/hosts/cluster/generate_vms_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'generate_vms/generate_vms.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+    libvirt_dir = '/etc/libvirt/qemu'
+    template_dir = '/var/lib/vms'
+    libvirt_service = 'libvirtd'
+    vms = nodes = Noop.hiera 'vms_conf'
+
+    it 'should exec generate_vms' do
+      should contain_exec('generate_vms').with(
+        'command'     => "/usr/bin/generate_vms.sh #{libvirt_dir} #{template_dir}",
+        'refreshonly' => 'true',
+        'notify'      => "Service[#{libvirt_service}]",
+      )
+    end
+
+    vms.each do | vm |
+      it "should define vm_config #{vm}" do
+        should contain_vm_config(vm).with(
+          'notify' => 'Exec[generate_vms]',
+        )
+      end
+    end
+
+    it "should create #{template_dir} directory" do
+      should contain_file(template_dir).with(
+        'ensure' => 'directory',
+      )
+    end
+
+    it "should create #{libvirt_dir}/autostart directory" do
+      should contain_file("#{libvirt_dir}/autostart").with(
+        'ensure' => 'directory',
+      )
+    end
+
+    it "should start #{libvirt_service}" do
+      should contain_service(libvirt_service).with(
+        'ensure' => 'running',
+      )
+    end
+
+  end
+  test_ubuntu_and_centos manifest
+end
