832bbeacc0ac7925f1564dd937c3e4d2e1c397cb
Mykyta Koshykov <mk455v@att.com>
2015-06-05 17:30:15 +0300

    Add fetch, store and setvar functions
    Change-Id: Idc2b3ae687aa60153dbbd1182540cf91233b5010
    Co-Authored-By: Dmitry Ilyin <dilyin@mirantis.com>

---
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/store.rb
@@ -0,0 +1,42 @@
+module Puppet::Parser::Functions
+  newfunction(:store, :arity => -2, :doc => <<-eos
+  Set a deep structure value
+  eos
+  ) do |args|
+
+    path_set = lambda do |data, path, value|
+      # wrong path
+      break false unless path.is_a? Array
+      # empty path
+      break false unless path.any?
+      # non-empty path and non-structure data -> return default
+      break false unless data.is_a? Hash or data.is_a? Array
+
+      key = path.shift
+      if data.is_a? Array
+        begin
+          key = Integer key
+        rescue ArgumentError
+          break value
+        end
+      end
+
+      if path.empty?
+        data[key] = value
+        break true
+      end
+
+      path_set.call data[key], path, value
+    end
+
+    data = args[0]
+    path = args[1]
+    value = args[2]
+    separator = args[3]
+    separator = '/' unless separator
+
+    path = '' unless path
+    path = path.split separator
+    path_set.call data, path, value
+  end
+end
