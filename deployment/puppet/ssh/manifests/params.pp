513060fc3aa8c855d3154f9f51e1815a7ad876ee
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-06-09 14:28:07 +0200

    Merge saz/ssh 2.8.1
    Commit: 3216cd87ae97ee74f06edd0e4868cedbc90e86d9
    Source: https://github.com/saz/puppet-ssh/
    
    Implements: blueprint upgrade-openstack-puppet-modules
    Change-Id: I8c709fe0d6c604a8e64839295030263dcce43c62

--- deployment/puppet/ssh/manifests/params.pp
+++ deployment/puppet/ssh/manifests/params.pp
@@ -28,9 +28,19 @@ class ssh::params {
       $ssh_config = '/etc/ssh/ssh_config'
       $ssh_known_hosts = '/etc/ssh/ssh_known_hosts'
       $service_name = 'sshd'
-      $sftp_server_path = '/usr/lib/openssh/sftp-server'
+      $sftp_server_path = '/usr/libexec/sftp-server'
     }
-    Archlinux: {
+    openbsd: {
+      $server_package_name = undef
+      $client_package_name = undef
+      $sshd_dir = '/etc/ssh'
+      $sshd_config = '/etc/ssh/sshd_config'
+      $ssh_config = '/etc/ssh/ssh_config'
+      $ssh_known_hosts = '/etc/ssh/ssh_known_hosts'
+      $service_name = 'sshd'
+      $sftp_server_path = '/usr/libexec/sftp-server'
+    }
+    archlinux: {
       $server_package_name = 'openssh'
       $client_package_name = 'openssh'
       $sshd_dir = '/etc/ssh'
@@ -40,7 +50,7 @@ class ssh::params {
       $service_name = 'sshd.service'
       $sftp_server_path = '/usr/lib/ssh/sftp-server'
     }
-    Suse: {
+    suse: {
       $server_package_name = 'openssh'
       $client_package_name = 'openssh'
       $sshd_dir = '/etc/ssh'
@@ -48,12 +58,12 @@ class ssh::params {
       $ssh_config = '/etc/ssh/ssh_config'
       $ssh_known_hosts = '/etc/ssh/ssh_known_hosts'
       case $::operatingsystem {
-        Sles: {
+        sles: {
           $service_name = 'sshd'
           $sftp_server_path = '/usr/lib64/ssh/sftp-server'
         }
-        Suse: {
-          $service_name = 'sshd.service'
+        opensuse: {
+          $service_name = 'sshd'
           $sftp_server_path = '/usr/lib/ssh/sftp-server'
         }
         default: {
@@ -73,6 +83,16 @@ class ssh::params {
           $service_name = 'sshd'
           $sftp_server_path = '/usr/lib/misc/sftp-server'
         }
+        amazon: {
+          $server_package_name = 'openssh-server'
+          $client_package_name = 'openssh-clients'
+          $sshd_dir = '/etc/ssh'
+          $sshd_config = '/etc/ssh/sshd_config'
+          $ssh_config = '/etc/ssh/ssh_config'
+          $ssh_known_hosts = '/etc/ssh/ssh_known_hosts'
+          $service_name = 'sshd'
+          $sftp_server_path = '/usr/libexec/openssh/sftp-server'
+        }
         default: {
           fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
         }
@@ -80,13 +100,29 @@ class ssh::params {
     }
   }
 
-  $sshd_default_options = {
-    'ChallengeResponseAuthentication' => 'no',
-    'X11Forwarding'                   => 'yes',
-    'PrintMotd'                       => 'no',
-    'AcceptEnv'                       => 'LANG LC_*',
-    'Subsystem'                       => "sftp ${sftp_server_path}",
-    'UsePAM'                          => 'yes',
+  # OpenBSDs openssh doesn't link against PAM, therefore
+  # it doesn't know about the UsePAM option
+  case $::osfamily {
+    openbsd: {
+      $sshd_default_options = {
+        'ChallengeResponseAuthentication' => 'no',
+        'X11Forwarding'                   => 'yes',
+        'PrintMotd'                       => 'no',
+        'AcceptEnv'                       => 'LANG LC_*',
+        'Subsystem'                       => "sftp ${sftp_server_path}",
+      }
+    }
+    default: {
+      $sshd_default_options = {
+        'ChallengeResponseAuthentication' => 'no',
+        'X11Forwarding'                   => 'yes',
+        'PrintMotd'                       => 'no',
+        'AcceptEnv'                       => 'LANG LC_*',
+        'Subsystem'                       => "sftp ${sftp_server_path}",
+        'UsePAM'                          => 'yes',
+      }
+
+    }
   }
 
   $ssh_default_options = {
@@ -95,4 +131,7 @@ class ssh::params {
       'HashKnownHosts'       => 'yes',
     },
   }
+
+  $user_ssh_directory_default_mode = '0700'
+  $user_ssh_config_default_mode    = '0600'
 }
