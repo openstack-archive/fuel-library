c8b8e8a037aff5455ab8aa3ac23f61b0bc773916
root <root@nailgun.test.domain.local>
2015-06-09 14:36:50 +0000

    Sync stdlib module from puppetlabs
      v4.6.0 73474b00b5ae3cbccec6cd0711311d6450139e51
      source https://github.com/puppetlabs/puppetlabs-stdlib
    
    Related blueprint upgrade-openstack-puppet-modules
    
    Change-Id: Ieca77090b908543e2af7f3ce75a4e45d404a3a6a

--- deployment/puppet/stdlib/spec/functions/uriescape_spec.rb
+++ deployment/puppet/stdlib/spec/functions/uriescape_spec.rb
@@ -5,20 +5,36 @@ describe "the uriescape function" do
   let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
 
   it "should exist" do
-    Puppet::Parser::Functions.function("uriescape").should == "function_uriescape"
+    expect(Puppet::Parser::Functions.function("uriescape")).to eq("function_uriescape")
   end
 
   it "should raise a ParseError if there is less than 1 arguments" do
-    lambda { scope.function_uriescape([]) }.should( raise_error(Puppet::ParseError))
+    expect { scope.function_uriescape([]) }.to( raise_error(Puppet::ParseError))
   end
 
   it "should uriescape a string" do
     result = scope.function_uriescape([":/?#[]@!$&'()*+,;= \"{}"])
-    result.should(eq(':/?%23[]@!$&\'()*+,;=%20%22%7B%7D'))
+    expect(result).to(eq(':/?%23[]@!$&\'()*+,;=%20%22%7B%7D'))
+  end
+
+  it "should uriescape an array of strings, while not touching up nonstrings" do
+    teststring = ":/?#[]@!$&'()*+,;= \"{}"
+    expectstring = ':/?%23[]@!$&\'()*+,;=%20%22%7B%7D'
+    result = scope.function_uriescape([[teststring, teststring, 1]])
+    expect(result).to(eq([expectstring, expectstring, 1]))
   end
 
   it "should do nothing if a string is already safe" do
     result = scope.function_uriescape(["ABCdef"])
-    result.should(eq('ABCdef'))
+    expect(result).to(eq('ABCdef'))
+  end
+
+  it "should accept objects which extend String" do
+    class AlsoString < String
+    end
+
+    value = AlsoString.new('abc')
+    result = scope.function_uriescape([value])
+    result.should(eq('abc'))
   end
 end
