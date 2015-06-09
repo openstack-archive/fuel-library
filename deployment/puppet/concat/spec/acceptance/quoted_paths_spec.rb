e06d7df61b29761f0a46598f2f86096b587762fa
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-06-09 08:27:23 +0200

    Merge puppetlabs/concat 2.0.1
    Commit: 5997e65f18eb78cd7dbce818eb29354e49d5a038
    Source: https://github.com/puppetlabs/puppetlabs-concat/
    
    Related blueprint merge-openstack-puppet-modules
    
    Change-Id: Idc9bb8647d311f797ef7d3ae5a95d0b90ad00ad1

--- deployment/puppet/concat/spec/acceptance/quoted_paths_spec.rb
+++ deployment/puppet/concat/spec/acceptance/quoted_paths_spec.rb
@@ -1,33 +1,42 @@
 require 'spec_helper_acceptance'
 
 describe 'quoted paths' do
+  basedir = default.tmpdir('concat')
+
   before(:all) do
-    shell('rm -rf "/tmp/concat test" /var/lib/puppet/concat')
-    shell('mkdir -p "/tmp/concat test"')
+    pp = <<-EOS
+      file { '#{basedir}':
+        ensure => directory,
+      }
+      file { '#{basedir}/concat test':
+        ensure => directory,
+      }
+    EOS
+    apply_manifest(pp)
   end
 
   context 'path with blanks' do
     pp = <<-EOS
-      concat { '/tmp/concat test/foo':
+      concat { '#{basedir}/concat test/foo':
       }
       concat::fragment { '1':
-        target  => '/tmp/concat test/foo',
+        target  => '#{basedir}/concat test/foo',
         content => 'string1',
       }
       concat::fragment { '2':
-        target  => '/tmp/concat test/foo',
+        target  => '#{basedir}/concat test/foo',
         content => 'string2',
       }
     EOS
 
     it 'applies the manifest twice with no stderr' do
-      expect(apply_manifest(pp, :catch_failures => true).stderr).to eq("")
-      expect(apply_manifest(pp, :catch_changes => true).stderr).to eq("")
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
     end
 
-    describe file('/tmp/concat test/foo') do
+    describe file("#{basedir}/concat test/foo") do
       it { should be_file }
-      it { should contain "string1\nsring2" }
+      its(:content) { should match /string1string2/ }
     end
   end
 end
