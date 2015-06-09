d4aead49e28d8d88d2ff37554183dccf24923ed7
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-06-09 14:28:07 +0200

    Merge saz/ssh 2.8.1
    Commit: 3216cd87ae97ee74f06edd0e4868cedbc90e86d9
    Source: https://github.com/saz/puppet-ssh/
    
    Related blueprint merge-openstack-puppet-modules
    
    Change-Id: I8c709fe0d6c604a8e64839295030263dcce43c62

--- deployment/puppet/ssh/manifests/server/config.pp
+++ deployment/puppet/ssh/manifests/server/config.pp
@@ -1,10 +1,11 @@
 class ssh::server::config {
+
   concat { $ssh::params::sshd_config:
     ensure => present,
-    owner  => 0,
-    group  => 0,
+    owner  => '0',
+    group  => '0',
     mode   => '0600',
-    notify => Service[$ssh::params::service_name],
+    notify => Service[$ssh::params::service_name]
   }
 
   concat::fragment { 'global config':
