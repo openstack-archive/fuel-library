bf5ec482cfdf6ec412a5c7685113936e750f582a
Stanislav Makar <smakar@mirantis.com>
2015-06-10 13:15:16 +0000

    Fix the problem with ceph deployment on scale lab
    Postpone ceph pool creation to post deploy:
    * Add task for ceph pool creation and put it in post deploy
    * Change ceph/compute.pp and move to post deploy
    * Remove from ceph/manifests/init.pp the pool creation code
    * Add NOOP tests
    
    Change-Id: I05b53042e24da8cb1693049bd95e682c8903c812
    Closes-bug: #1462451

---
+++ tests/noop/spec/hosts/ceph/ceph_compute_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'ceph/ceph_compute.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+    storage_hash = Noop.hiera 'storage'
+
+    if (storage_hash['images_ceph'] or storage_hash['objects_ceph'] or storage_hash['objects_ceph'])
+      it { should contain_class('ceph').with(
+           'osd_pool_default_size'    => storage_hash['osd_pool_size'],
+           'osd_pool_default_pg_num'  => storage_hash['pg_num'],
+           'osd_pool_default_pgp_num' => storage_hash['pg_num'],)
+         }
+      it { should contain_class('ceph::conf') }
+
+      it { should contain_ceph__pool('compute').with(
+          'pg_num'        => storage_hash['pg_num'],
+          'pgp_num'       => storage_hash['pg_num'],)
+        }
+
+      it { should contain_ceph__pool('compute').that_requires('Class[ceph::conf]') }
+      it { should contain_ceph__pool('compute').that_comes_before('Class[ceph::nova_compute]') }
+      it { should contain_class('ceph::nova_compute').that_requires('Ceph::Pool[compute]') }
+
+      if storage_hash['ephemeral_ceph']
+        it { should contain_class('ceph::ephemeral') }
+        it { should contain_class('ceph::conf').that_comes_before('Class[ceph::ephemeral]') }
+      end
+    end
+
+  end
+  test_ubuntu_and_centos manifest
+end
--- tests/noop/spec/hosts/ceph/compute_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-require 'shared-examples'
-manifest = 'ceph/compute.pp'
-
-describe manifest do
-  test_ubuntu_and_centos manifest
-end
