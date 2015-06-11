f180b74d420f7bd6cd3d1d1e52099371140b0f23
Sergey Kolekonov <skolekonov@mirantis.com>
2015-06-11 13:37:23 +0300

    Adapt synced neutron module
    - added workaround to temporary keep compatibility with existing MySQL module
    - pass $tunnel_types variable as it's required since Kilo
    - backported required changes from 6.1 and fixed unit-tests
    
    Partially Implements: blueprint upgrade-openstack-puppet-modules
    Change-Id: Iccb6cbc874ec1ff390bb008fb7ef61bfde4bba3a

--- deployment/puppet/openstack/manifests/db/mysql.pp
+++ deployment/puppet/openstack/manifests/db/mysql.pp
@@ -149,6 +149,7 @@ class openstack::db::mysql (
         password      => $neutron_db_password,
         dbname        => $neutron_db_dbname,
         allowed_hosts => $allowed_hosts,
+        mysql_module  => '0.9',
       }
     }
   }
