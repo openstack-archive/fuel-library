d0dd5911fe4a1b0a6dd7f842ebc56ce4f94901b5
Denis Egorenko <degorenko@mirantis.com>
2015-06-16 13:29:15 +0300

    Sync Xinetd module with upstream
    Sync from Puppetlabs
    v1.5.0 5e31388979e8f3a667899fad5cefa95289e22281
    
    Partial implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: I8836f09b0bc2332daea41af087dcb13268123dc8

--- deployment/puppet/xinetd/Gemfile
+++ deployment/puppet/xinetd/Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 group :development, :test do
   gem 'rake',                    :require => false
-  gem 'rspec-puppet',            :require => false
+  gem 'rspec-puppet', '~> 1.0',  :require => false
   gem 'puppetlabs_spec_helper',  :require => false
   gem 'puppet-lint',             :require => false
   gem 'serverspec',              :require => false
--- deployment/puppet/xinetd/spec/classes/xinetd_init_spec.rb
+++ deployment/puppet/xinetd/spec/classes/xinetd_init_spec.rb
@@ -6,9 +6,29 @@ describe 'xinetd' do
     { :osfamily => 'Debian' }
   end
 
-  it {
-    should contain_package('xinetd')
-    should contain_file('/etc/xinetd.conf')
-    should contain_service('xinetd')
-  }
+  describe 'with defaults' do
+    it {
+      should contain_package('xinetd')
+      should contain_file('/etc/xinetd.conf')
+      should contain_file('/etc/xinetd.d').with_ensure('directory')
+      should contain_file('/etc/xinetd.d').with_recurse(false)
+      should contain_file('/etc/xinetd.d').with_purge(false)
+      should contain_service('xinetd')
+    }
+  end
+
+  describe 'with managed confdir' do
+    let :params do
+      { :purge_confdir => true }
+    end
+
+    it {
+      should contain_package('xinetd')
+      should contain_file('/etc/xinetd.conf')
+      should contain_file('/etc/xinetd.d').with_ensure('directory')
+      should contain_file('/etc/xinetd.d').with_recurse(true)
+      should contain_file('/etc/xinetd.d').with_purge(true)
+      should contain_service('xinetd')
+    }
+  end
 end
