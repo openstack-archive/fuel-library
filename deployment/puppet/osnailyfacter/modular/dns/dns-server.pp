8d8b45e1982383af0d5b5a8438c35e6d2a9d85fc
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-06-17 16:55:18 +0300

    Secure dns and ntp
    Set dnsmasq and ntpd to listen on
    management VIP in vrouter namespace
    
    Change-Id: I94556e3a8765b3b5b337c68df6cf0ca848ee1645
    Closes-Bug: 1466090

--- deployment/puppet/osnailyfacter/modular/dns/dns-server.pp
+++ deployment/puppet/osnailyfacter/modular/dns/dns-server.pp
@@ -1,12 +1,14 @@
 notice('MODULAR: dns-server.pp')
 
-$dns_servers        = hiera('external_dns')
-$primary_controller = hiera('primary_controller')
-$master_ip          = hiera('master_ip')
+$dns_servers            = hiera('external_dns')
+$primary_controller     = hiera('primary_controller')
+$master_ip              = hiera('master_ip')
+$management_vrouter_vip = hiera('management_vrouter_vip')
 
 class { 'osnailyfacter::dnsmasq':
-  external_dns => strip(split($dns_servers['dns_list'], ',')),
-  master_ip    => $master_ip,
+  external_dns           => strip(split($dns_servers['dns_list'], ',')),
+  master_ip              => $master_ip,
+  management_vrouter_vip => $management_vrouter_vip,
 } ->
 
 class { 'cluster::dns_ocf':
