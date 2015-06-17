deecf48bff81387d795eac44f974d4180d2530f8
Dmitry Ilyin <dilyin@mirantis.com>
2015-06-17 20:10:49 +0300

    LDAP support: Keystone
    FuelCI: disable
    Change-Id: Ice5a2f977fa47c0292ee9137815ee1882067ac6e

---
+++ deployment/puppet/osnailyfacter/modular/ldap_params.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+ldap_manifest = File.join File.dirname(__FILE__), '../../keystone/manifests/ldap.pp'
+fail 'No manifest!' unless File.file? ldap_manifest
+
+parameters = []
+File.read(ldap_manifest).split("\n").each do |line|
+  parameters << $1 if line =~ %r(\s*\$(\w+)\s+= undef)
+end
+
+max_length = parameters.max_by { |p| p.length}.length
+
+parameters.each do |p|
+  puts "    #{p.ljust max_length} => structure($ldap_hash, '#{p}'),"
+end
+
