4c1d075cc0474bfd4ce25641a73aeb0ced867da1
iberezovskiy <iberezovskiy@mirantis.com>
2015-06-17 13:27:58 +0300

    Adapt synced puppetlabs-mongodb module for Fuel usage
      Key moments:
        * Adds Support for Keyfile Key:
          upstream commit 7bbcff8024152b4ec9ce53dbba00024523707162
        * Use default configuration file for 2.6.x versions
        * Old configutration file was removed
        * Remove useless openstack::mongo class
        * Return back parameters for autorization in MongoDB
        * Fix tests according to new configuration and restrictions
        * Check node status depending on auth_enabled parameter
        * Add ability to use admin credentials to run MongoDB commands
        * Add retries for executing MongoDB shell commands
        * Fix lint errors
        * Add noop tests for MongoDB parameters
    
      Implements: blueprint upgrade-openstack-puppet-modules
    
      Closes-bug: #1383639
      Related-bug: #1308990
    
    Change-Id: I3b9cbd35bf46483782d7b7eb7ef030fd31fdd544

--- tests/noop/spec/hosts/roles/mongo_primary_spec.rb
+++ tests/noop/spec/hosts/roles/mongo_primary_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'shared-examples'
+
 manifest = 'roles/mongo_primary.pp'
 
 describe manifest do
@@ -7,6 +8,52 @@ describe manifest do
     Puppet::Parser::Functions::newfunction(:file, :arity => -2, :type => :rvalue) do |vals|
       return 'key' if vals.first == '/var/lib/astute/mongodb/mongodb.key'
       raise Puppet::ParseError, "Could not find any files from #{vals.join(", ")}"
+    end
+  end
+
+  shared_examples 'catalog' do
+
+    debug = Noop.hiera 'debug'
+    use_syslog = Noop.hiera 'use_syslog'
+    ceilometer_hash = Noop.hiera_structure 'ceilometer'
+    nodes_hash = Noop.hiera_structure 'nodes'
+
+    it 'should configure MongoDB only with replica set' do
+      should contain_class('mongodb::server').with('replset' => 'ceilometer')
+    end
+
+    it 'should configure MongoDB with authentication enabled' do
+      should contain_class('mongodb::server').with('auth' => 'true')
+    end
+
+    it 'should configure verbosity level for MongoDB' do
+      if debug
+        should contain_class('mongodb::server').with('verbositylevel' => 'vv')
+      else
+        should contain_class('mongodb::server').with('verbositylevel' => 'v')
+      end
+    end
+
+    it 'should create keyfile for replica setup' do
+      should contain_class('mongodb::server').with('keyfile' => '/etc/mongodb.key')
+    end
+
+    it 'should not write logs to file if syslog is enabled' do
+      if use_syslog
+        should contain_class('mongodb::server').with('logpath' => 'false')
+      end
+    end
+
+    it 'should configure oplog size for local database' do
+      should contain_class('mongodb::server').with('oplog_size' => '10240')
+    end
+
+    it 'should capture data regarding performance' do
+      should contain_class('mongodb::server').with('profile' => '1')
+   end
+
+    it 'should store each database in separate directory' do
+      should contain_class('mongodb::server').with('directoryperdb' => 'true')
     end
   end
 
