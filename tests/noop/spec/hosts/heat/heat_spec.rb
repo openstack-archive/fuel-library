5f54943f63916ce95ef8a32fca437281eb0dc002
iberezovskiy <iberezovskiy@mirantis.com>
2015-06-17 17:46:03 +0300

    Adapt synced Heat module for Fuel usage
      Key moments:
        * Fix lint errors
        * Remove duplication of package->service dependencies
        * Workoaround for MySQL module
        * Fix name for heat-engine service in heat_ha class
        * Remove heat_config options from osnailyfacter class
        * Configure delegated roles from heat class
        * Add retries to heat domain creation command
          commit I2fbb97adde6743153fc81f8dba153356c382cb52
        * Add retry to heat_domain_id
          commit: I1630f54cdfa850a6b4dcbc9b714ebc6195de1fb0
        * Update in heat_domain_id_setter
          commit: I4c0df7600e10d57429eda854cd8aa6208e1c6539
        * Add noop tests for new parameters
    
      Implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: Ib9fa8acc54c124f8656d49accbdbbf733f2d3fd3

--- tests/noop/spec/hosts/heat/heat_spec.rb
+++ tests/noop/spec/hosts/heat/heat_spec.rb
@@ -3,6 +3,22 @@ require 'shared-examples'
 manifest = 'heat/heat.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+
+    it 'should set empty trusts_delegated_roles for heat authentication and engine' do
+      should contain_class('heat::keystone::auth').with(
+        'trusts_delegated_roles' => [],
+      )
+      should contain_class('heat::engine').with(
+        'trusts_delegated_roles' => [],
+      )
+      should contain_heat_config('DEFAULT/trusts_delegated_roles').with(
+        'value' => [],
+      )
+    end
+
+  end # end of shared_examples
+
   test_ubuntu_and_centos manifest
 end
 
