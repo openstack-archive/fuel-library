1fd8b54f154b9ecd1b264141b73c0830d725ca0e
Dmitry Ilyin <dilyin@mirantis.com>
2015-06-17 15:04:41 +0300

    Cleanup globals values
    * Remove unneded variables from globals
    * Fix broken noop tests
    
    Closes-Bug: 1478643
    Change-Id: Id18ce1f4b60f99292d18844682d840a54a3a436b

--- tests/noop/spec/hosts/sahara/keystone_spec.rb
+++ tests/noop/spec/hosts/sahara/keystone_spec.rb
@@ -4,9 +4,9 @@ manifest = 'sahara/keystone.pp'
 
 describe manifest do
   shared_examples 'catalog' do
-    public_address       = Noop.hiera('public_vip')
-    internal_address     = Noop.hiera('management_vip', public_address)
-    public_ssl           = Noop.hiera_structure('public_ssl/services')
+    public_address       = Noop.hiera 'public_vip'
+    internal_address     = Noop.hiera 'management_vip', public_address
+    public_ssl           = Noop.hiera_structure 'public_ssl/services'
 
     api_bind_port   = '8386'
     if public_ssl
@@ -14,11 +14,11 @@ describe manifest do
     else
       public_protocol = 'http'
     end
-    sahara_user     = Noop.hiera_structure('sahara_hash/user', 'sahara')
-    sahara_password = Noop.hiera_structure('sahara_hash/user_password')
-    tenant          = Noop.hiera_structure('sahara_hash/tenant', 'services')
-    region          = Noop.hiera_structure('sahara_hash/region', 'RegionOne')
-    service_name    = Noop.hiera_structure('sahara_hash/service_name', 'sahara')
+    sahara_user     = Noop.hiera_structure 'sahara/user', 'sahara'
+    sahara_password = Noop.hiera_structure 'sahara/user_password'
+    tenant          = Noop.hiera_structure 'sahara/tenant', 'services'
+    region          = Noop.hiera_structure 'sahara/region', 'RegionOne'
+    service_name    = Noop.hiera_structure 'sahara/service_name', 'sahara'
     public_url      = "#{public_protocol}://#{public_address}:#{api_bind_port}/v1.1/%(tenant_id)s"
     admin_url       = "http://#{internal_address}:#{api_bind_port}/v1.1/%(tenant_id)s"
     internal_url    = "http://#{internal_address}:#{api_bind_port}/v1.1/%(tenant_id)s"
