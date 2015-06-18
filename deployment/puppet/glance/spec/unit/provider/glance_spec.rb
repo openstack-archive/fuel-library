aeafa4b7fdd925d0733d43a34eab5ffb6c8e0506
Denis Egorenko <degorenko@mirantis.com>
2015-06-18 13:30:31 +0300

    Sync glance module with upstream
    master commit: 646a6cd15f2c358c6fde25dbb795615f45913d96
    
    Fuel-CI: disable
    Partially implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: Ifa9e699e4165f254664a9f6272adcc5b72e41cf8

--- deployment/puppet/glance/spec/unit/provider/glance_spec.rb
+++ deployment/puppet/glance/spec/unit/provider/glance_spec.rb
@@ -35,19 +35,25 @@ describe Puppet::Provider::Glance do
                'admin_tenant_name' => 'foo',
                'admin_user'        => 'user',
                'admin_password'    => 'pass'
+            },
+                'glance_store' =>
+            {
+                'os_region_name' => 'SomeRegion',
             }
           }
           Puppet::Util::IniConfig::File.expects(:new).returns(mock)
           mock.expects(:read).with('/etc/glance/glance-api.conf')
           klass.expects(:sleep).with(10).returns(nil)
           klass.expects(:glance).twice.with(
-            '-T',
+            '--os-tenant-name',
             'foo',
-            '-I',
+            '--os-username',
             'user',
-            '-K',
+            '--os-password',
             'pass',
-            '-N',
+            '--os-region-name',
+            'SomeRegion',
+            '--os-auth-url',
             'http://127.0.0.1:35357/v2.0/',
             ['test_retries']
           ).raises(Exception, valid_message).then.returns('')
--- deployment/puppet/glance/tests/site.pp
+++ deployment/puppet/glance/tests/site.pp
@@ -4,32 +4,32 @@
 Exec { logoutput => 'on_failure' }
 
 node glance_keystone_mysql {
-  class { 'mysql::server': }
-  class { 'keystone':
+  class { '::mysql::server': }
+  class { '::keystone':
     verbose      => true,
     debug        => true,
     catalog_type => 'sql',
     admin_token  => 'admin_token',
   }
-  class { 'keystone::db::mysql':
+  class { '::keystone::db::mysql':
     password => 'keystone',
   }
-  class { 'keystone::roles::admin':
+  class { '::keystone::roles::admin':
     email    => 'test@puppetlabs.com',
     password => 'ChangeMe',
   }
-  class { 'glance::api':
-    verbose           => true,
-    debug             => true,
-    auth_type         => 'keystone',
-    keystone_tenant   => 'services',
-    keystone_user     => 'glance',
-    keystone_password => 'glance_password',
-    sql_connection    => 'mysql://glance:glance@127.0.0.1/glance',
+  class { '::glance::api':
+    verbose             => true,
+    debug               => true,
+    auth_type           => 'keystone',
+    keystone_tenant     => 'services',
+    keystone_user       => 'glance',
+    keystone_password   => 'glance_password',
+    database_connection => 'mysql://glance:glance@127.0.0.1/glance',
   }
-  class { 'glance::backend::file': }
+  class { '::glance::backend::file': }
 
-  class { 'glance::db::mysql':
+  class { '::glance::db::mysql':
     password => 'glance',
     dbname   => 'glance',
     user     => 'glance',
@@ -38,16 +38,16 @@ node glance_keystone_mysql {
     # $cluster_id = 'localzone'
   }
 
-  class { 'glance::registry':
-    verbose           => true,
-    debug             => true,
-    auth_type         => 'keystone',
-    keystone_tenant   => 'services',
-    keystone_user     => 'glance',
-    keystone_password => 'glance_password',
-    sql_connection    => 'mysql://glance:glance@127.0.0.1/glance',
+  class { '::glance::registry':
+    verbose             => true,
+    debug               => true,
+    auth_type           => 'keystone',
+    keystone_tenant     => 'services',
+    keystone_user       => 'glance',
+    keystone_password   => 'glance_password',
+    database_connection => 'mysql://glance:glance@127.0.0.1/glance',
   }
-  class { 'glance::keystone::auth':
+  class { '::glance::keystone::auth':
     password => 'glance_pass',
   }
 }
