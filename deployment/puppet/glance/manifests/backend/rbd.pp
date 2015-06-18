0d99674c88206b522965414bc03f88020bd929c4
Denis Egorenko <degorenko@mirantis.com>
2015-06-18 13:30:31 +0300

    Sync glance module with upstream
    master commit: 646a6cd15f2c358c6fde25dbb795615f45913d96
    
    Fuel-CI: disable
    Partially implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: Ifa9e699e4165f254664a9f6272adcc5b72e41cf8

--- deployment/puppet/glance/manifests/backend/rbd.pp
+++ deployment/puppet/glance/manifests/backend/rbd.pp
@@ -1,17 +1,31 @@
- #
+# == class: glance::backend::rbd
+#
 # configures the storage backend for glance
 # as a rbd instance
 #
-#  $rbd_store_user - Optional.
+# === parameters:
 #
-#  $rbd_store_pool - Optional. Default:'images'
+#  [*rbd_store_user*]
+#    Optional.
 #
-#  $rbd_store_ceph_conf  - Optional. Default:'/etc/ceph/ceph.conf'
+#  [*rbd_store_pool*]
+#    Optional. Default:'images'
 #
-#  $rbd_store_chunk_size - Optional. Default:'8'
+#  [*rbd_store_ceph_conf*]
+#    Optional. Default:'/etc/ceph/ceph.conf'
 #
-#  $show_image_direct_url - Optional. Enables direct COW from glance to rbd
-#  DEPRECATED, use show_image_direct_url in glance::api
+#  [*rbd_store_chunk_size*]
+#    Optional. Default:'8'
+#
+#  [*show_image_direct_url*]
+#    Optional. Enables direct COW from glance to rbd
+#    DEPRECATED, use show_image_direct_url in glance::api
+#
+#  [*package_ensure*]
+#      (optional) Desired ensure state of packages.
+#      accepts latest or specific versions.
+#      Defaults to present.
+#
 
 class glance::backend::rbd(
   $rbd_store_user         = undef,
@@ -19,23 +33,24 @@ class glance::backend::rbd(
   $rbd_store_pool         = 'images',
   $rbd_store_chunk_size   = '8',
   $show_image_direct_url  = undef,
+  $package_ensure         = 'present',
 ) {
-  include glance::params
+  include ::glance::params
 
   if $show_image_direct_url {
     notice('parameter show_image_direct_url is deprecated, use parameter in glance::api')
   }
 
   glance_api_config {
-    'glance_store/rbd_store_ceph_conf':  value => $rbd_store_ceph_conf;
-    'glance_store/rbd_store_user':       value => $rbd_store_user;
-    'glance_store/rbd_store_pool':       value => $rbd_store_pool;
-    'glance_store/rbd_store_chunk_size': value => $rbd_store_chunk_size;
-    'glance_store/default_store':        value => 'rbd';
+    'glance_store/default_store':         value => 'rbd';
+    'glance_store/rbd_store_ceph_conf':   value => $rbd_store_ceph_conf;
+    'glance_store/rbd_store_user':        value => $rbd_store_user;
+    'glance_store/rbd_store_pool':        value => $rbd_store_pool;
+    'glance_store/rbd_store_chunk_size':  value => $rbd_store_chunk_size;
   }
 
   package { 'python-ceph':
-    ensure => 'present',
+    ensure => $package_ensure,
     name   => $::glance::params::pyceph_package_name,
   }
 
--- deployment/puppet/glance/manifests/backend/swift.pp
+++ deployment/puppet/glance/manifests/backend/swift.pp
@@ -1,22 +1,34 @@
+# == class: glance::backend::swift
 #
 # configures the storage backend for glance
 # as a swift instance
 #
-#  $swift_store_user - Required.
+# === parameters:
 #
-#  $swift_store_key - Required.
+#  [*swift_store_user*]
+#    Required. Swift store user.
 #
-#  $swift_store_auth_address - Optional. Default: '127.0.0.1:5000/v2.0/'
+#  [*swift_store_key*]
+#    Required. Swift store key.
 #
-#  $swift_store_container - Optional. Default: 'glance'
+#  [*swift_store_auth_address*]
+#    Optional. Default: '127.0.0.1:5000/v2.0/'
 #
-#  $swift_store_auth_version - Optional. Default: '2'
+#  [*swift_store_container*]
+#    Optional. Default: 'glance'
 #
-#  $swift_store_create_container_on_put - Optional. Default: 'False'
+#  [*swift_store_auth_version*]
+#    Optional. Default: '2'
 #
-#  $swift_store_large_object_size - Optional. Default: '5120'
+#  [*swift_store_large_object_size*]
+#    Optional. Default: '5120'
 #
-#  $swift_store_endpoint_type - Optional. Default: 'internalURL'
+#  [*swift_store_create_container_on_put*]
+#    Optional. Default: 'False'
+#
+#  [*swift_store_endpoint_type*]
+#    Optional. Default: 'internalURL'
+#
 class glance::backend::swift(
   $swift_store_user,
   $swift_store_key,
@@ -33,22 +45,22 @@ class glance::backend::swift(
     'glance_store/swift_store_user':         value => $swift_store_user;
     'glance_store/swift_store_key':          value => $swift_store_key;
     'glance_store/swift_store_auth_address': value => $swift_store_auth_address;
-    'glance_store/swift_store_container':    value => $swift_store_container;
-    'glance_store/swift_store_auth_version': value => $swift_store_auth_version;
-    'glance_store/swift_store_create_container_on_put':
+    'DEFAULT/swift_store_container':         value => $swift_store_container;
+    'DEFAULT/swift_store_auth_version':      value => $swift_store_auth_version;
+    'DEFAULT/swift_store_create_container_on_put':
       value => $swift_store_create_container_on_put;
-    'glance_store/swift_store_large_object_size':
+    'DEFAULT/swift_store_large_object_size':
       value => $swift_store_large_object_size;
     'glance_store/swift_store_endpoint_type':
       value => $swift_store_endpoint_type;
   }
 
   glance_cache_config {
-    'DEFAULT/swift_store_user':         value => $swift_store_user;
-    'DEFAULT/swift_store_key':          value => $swift_store_key;
-    'DEFAULT/swift_store_auth_address': value => $swift_store_auth_address;
-    'DEFAULT/swift_store_container':    value => $swift_store_container;
-    'DEFAULT/swift_store_auth_version': value => $swift_store_auth_version;
+    'glance_store/swift_store_user':         value => $swift_store_user;
+    'glance_store/swift_store_key':          value => $swift_store_key;
+    'glance_store/swift_store_auth_address': value => $swift_store_auth_address;
+    'DEFAULT/swift_store_container':         value => $swift_store_container;
+    'DEFAULT/swift_store_auth_version':      value => $swift_store_auth_version;
     'DEFAULT/swift_store_create_container_on_put':
       value => $swift_store_create_container_on_put;
     'DEFAULT/swift_store_large_object_size':
