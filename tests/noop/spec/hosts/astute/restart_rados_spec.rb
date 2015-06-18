a28a6971ba83c51ce9a3ced0abecde0b9f329968
Alex Schultz <aschultz@mirantis.com>
2015-06-18 13:24:53 -0500

    Ensure radosgw service enabled on boot
    Currently, we are not ensuring that the radowgw service is enabled to
    start on boot. This change removes the restart_rados.sh and replaces
    it with a puppet equivalent that also ensures that the service will be
    started on boot.
    
    Change-Id: I58bf4a20c9d08ba3e9624fd47e3b5780011c89b7
    Closes-Bug: 1466552

---
+++ tests/noop/spec/hosts/astute/restart_rados_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'astute/restart_rados.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+     case facts[:operatingsystem]
+     when 'Ubuntu'
+       service_name = 'radosgw'
+     when 'CentOS'
+       service_name = 'ceph-radosgw'
+     end
+
+    it "should contain restart of rados gateway" do
+      should contain_exec("restart-#{service_name}").with_command(
+          "service #{service_name} restart")
+    end
+    it "should contain radowgw service" do
+      should contain_service(service_name)
+    end
+  end
+  test_ubuntu_and_centos manifest
+end
