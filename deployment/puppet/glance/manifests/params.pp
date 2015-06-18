448b3723694430ec4f43d9ee4fc25abef9040d93
Denis Egorenko <degorenko@mirantis.com>
2015-06-18 13:30:31 +0300

    Sync glance module with upstream
    master commit: 370af254e417e2ee19d5a67605a589626de8cba1
    
    Fuel-CI: disable
    Partially implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: Ifa9e699e4165f254664a9f6272adcc5b72e41cf8

--- deployment/puppet/glance/manifests/params.pp
+++ deployment/puppet/glance/manifests/params.pp
@@ -3,7 +3,6 @@
 class glance::params {
 
   $client_package_name = 'python-glanceclient'
-  $pyceph_package_name = 'python-ceph'
 
   $cache_cleaner_command = 'glance-cache-cleaner'
   $cache_pruner_command  = 'glance-cache-pruner'
@@ -14,14 +13,20 @@ class glance::params {
       $registry_package_name = 'openstack-glance'
       $api_service_name      = 'openstack-glance-api'
       $registry_service_name = 'openstack-glance-registry'
-      $db_sync_command       = 'glance-manage db_sync'
+      $db_sync_command       = 'glance-manage --config-file=/etc/glance/glance-registry.conf db_sync'
+      if ($::operatingsystem != 'fedora' and versioncmp($::operatingsystemrelease, '7') < 0) {
+        $pyceph_package_name = 'python-ceph'
+      } else {
+        $pyceph_package_name = 'python-rbd'
+      }
     }
     'Debian': {
       $api_package_name      = 'glance-api'
       $registry_package_name = 'glance-registry'
       $api_service_name      = 'glance-api'
       $registry_service_name = 'glance-registry'
-      $db_sync_command       = 'glance-manage db_sync'
+      $db_sync_command       = 'glance-manage --config-file=/etc/glance/glance-registry.conf db_sync'
+      $pyceph_package_name   = 'python-ceph'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only support osfamily RedHat and Debian")
