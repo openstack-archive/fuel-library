fdd67f65f19de92c21400065b1fcc6bf8f803d58
Michał Górniak <mgorniak@mirantis.com>
2015-06-18 12:04:38 -0500

    Add possibility to disable each HA services using yaml file
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: Ia0d4958fe54e85219dd8f775794a8a5ad6b1aa00

--- deployment/puppet/openstack/manifests/ha/haproxy.pp
+++ deployment/puppet/openstack/manifests/ha/haproxy.pp
@@ -18,6 +18,8 @@ class openstack::ha::haproxy (
   Haproxy::Service        { use_include => true }
   Haproxy::Balancermember { use_include => true }
 
+  $disable_hash = hiera_hash('disable_ha_proxy', {})
+
   $controllers_server_names = filter_hash($controllers, 'name')
   $controllers_ipaddresses  = filter_hash($controllers, 'internal_address')
 
@@ -28,45 +30,58 @@ class openstack::ha::haproxy (
     internal_virtual_ip => $internal_virtual_ip,
   }
 
-  class { 'openstack::ha::horizon':
-    use_ssl      => $horizon_use_ssl,
-    server_names => hiera_array('horizon_names', $controllers_server_names),
-    ipaddresses  => hiera_array('horizon_ipaddresses', $controllers_ipaddresses),
+  if !$disable_hash[horizon] {
+    class { 'openstack::ha::horizon':
+      use_ssl      => $horizon_use_ssl,
+      server_names => hiera_array('horizon_names', $controllers_server_names),
+      ipaddresses  => hiera_array('horizon_ipaddresses', $controllers_ipaddresses),
+    }
   }
 
-  class { 'openstack::ha::keystone':
-    server_names => hiera_array('keystone_names', $controllers_server_names),
-    ipaddresses  => hiera_array('keystone_ipaddresses', $controllers_ipaddresses),
+
+  if !$disable_hash[keystone] {
+    class { 'openstack::ha::keystone':
+      server_names => hiera_array('keystone_names', $controllers_server_names),
+      ipaddresses  => hiera_array('keystone_ipaddresses', $controllers_ipaddresses),
+    }
   }
 
-  class { 'openstack::ha::nova':
-    server_names => hiera_array('nova_names', $controllers_server_names),
-    ipaddresses  => hiera_array('nova_ipaddresses', $controllers_ipaddresses),
+  if !$disable_hash[nova] {
+    class { 'openstack::ha::nova':
+      server_names => hiera_array('nova_names', $controllers_server_names),
+      ipaddresses  => hiera_array('nova_ipaddresses', $controllers_ipaddresses),
+    }
   }
 
-  class { 'openstack::ha::heat':
-    server_names => hiera_array('heat_names', $controllers_server_names),
-    ipaddresses  => hiera_array('heat_ipaddresses', $controllers_ipaddresses),
+  if !$disable_hash[heat] {
+    class { 'openstack::ha::heat':
+      server_names => hiera_array('heat_names', $controllers_server_names),
+      ipaddresses  => hiera_array('heat_ipaddresses', $controllers_ipaddresses),
+    }
   }
 
-  class { 'openstack::ha::glance':
-    server_names => hiera_array('glance_names', $controllers_server_names),
-    ipaddresses  => hiera_array('glance_ipaddresses', $controllers_ipaddresses),
+  if !$disable_hash[glance] {
+    class { 'openstack::ha::glance':
+      server_names => hiera_array('glance_names', $controllers_server_names),
+      ipaddresses  => hiera_array('glance_ipaddresses', $controllers_ipaddresses),
+    }
   }
 
-  class { 'openstack::ha::cinder':
-    server_names => hiera_array('cinder_names', $controllers_server_names),
-    ipaddresses  => hiera_array('cinder_ipaddresses', $controllers_ipaddresses),
+  if !$disable_hash[cinder] {
+    class { 'openstack::ha::cinder':
+      server_names => hiera_array('cinder_names', $controllers_server_names),
+      ipaddresses  => hiera_array('cinder_ipaddresses', $controllers_ipaddresses),
+    }
   }
 
-  if $neutron {
+  if $neutron and !$disable_hash[cinder] {
     class { 'openstack::ha::neutron':
       server_names => hiera_array('neutron_names', $controllers_server_names),
       ipaddresses  => hiera_array('neutron_ipaddresses', $controllers_ipaddresses),
     }
   }
 
-  if $custom_mysql_setup_class == 'galera' or $custom_mysql_setup_class == 'percona' {
+  if ($custom_mysql_setup_class == 'galera' or $custom_mysql_setup_class == 'percona') and !$disable_hash[mysqld] {
     class { 'openstack::ha::mysqld':
       is_primary_controller => $is_primary_controller,
       server_names          => hiera_array('mysqld_names', $controllers_server_names),
@@ -74,35 +89,35 @@ class openstack::ha::haproxy (
     }
   }
 
-  if $swift_proxies {
+  if $swift_proxies and !$disable_hash[swift_proxies] {
     class { 'openstack::ha::swift':
       server_names => hiera_array('swift_server_names', filter_hash($swift_proxies, 'name')),
       ipaddresses  => hiera_array('swift_ipaddresses', filter_hash($swift_proxies, 'storage_address')),
     }
   }
 
-  if $rgw_servers {
+  if $rgw_servers and !$disable_hash[rgw_servers] {
     class { 'openstack::ha::radosgw':
       server_names => hiera_array('radosgw_server_names', filter_hash($rgw_servers, 'name')),
       ipaddresses  => hiera_array('radosgw_ipaddresses', filter_hash($rgw_servers, 'internal_address')),
     }
   }
 
-  if $ceilometer {
+  if $ceilometer and !$disable_hash[ceilometer] {
     class { 'openstack::ha::ceilometer':
       server_names => hiera_array('ceilometer_names', $controllers_server_names),
       ipaddresses  => hiera_array('ceilometer_ipaddresses', $controllers_ipaddresses),
     }
   }
 
-  if $sahara {
+  if $sahara and !$disable_hash[sahara] {
      class { 'openstack::ha::sahara':
       server_names => hiera_array('sahara_names', $controllers_server_names),
       ipaddresses  => hiera_array('sahara_ipaddresses', $controllers_ipaddresses),
     }
   }
 
-  if $murano {
+  if $murano and !$disable_hash[murano] {
     class { 'openstack::ha::murano':
       server_names => hiera_array('murano_names', $controllers_server_names),
       ipaddresses  => hiera_array('murano_ipaddresses', $controllers_ipaddresses),
