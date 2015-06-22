ff1983a95e9e3b01de5887a69a8afe57f943ba24
vsaienko <vsaienko@mirantis.com>
2015-06-22 18:50:19 +0300

    sync keystone with upstream
      master de419015bb96d83135b306d9fb34e77e033c26cb
      sources https://github.com/openstack/puppet-keystone
    
    Partially Implements: blueprint upgrade-openstack-puppet-modules
    Fuel-CI: disable
    
    Change-Id: Idedef32288e214486d05225b09147b5b1c30cdab

--- deployment/puppet/keystone/examples/apache_dropin.pp
+++ deployment/puppet/keystone/examples/apache_dropin.pp
@@ -20,30 +20,30 @@
 
 Exec { logoutput => 'on_failure' }
 
-class { 'mysql::server': }
-class { 'keystone::db::mysql':
+class { '::mysql::server': }
+class { '::keystone::db::mysql':
   password => 'keystone',
 }
-class { 'keystone':
-  verbose        => true,
-  debug          => true,
-  sql_connection => 'mysql://keystone:keystone@127.0.0.1/keystone',
-  catalog_type   => 'sql',
-  admin_token    => 'admin_token',
-  enabled        => false,
+class { '::keystone':
+  verbose             => true,
+  debug               => true,
+  database_connection => 'mysql://keystone:keystone@127.0.0.1/keystone',
+  catalog_type        => 'sql',
+  admin_token         => 'admin_token',
+  enabled             => false,
 }
-class { 'keystone::roles::admin':
+class { '::keystone::roles::admin':
   email    => 'test@puppetlabs.com',
   password => 'ChangeMe',
 }
-class { 'keystone::endpoint':
+class { '::keystone::endpoint':
   public_url => "https://${::fqdn}:5000/",
   admin_url  => "https://${::fqdn}:35357/",
 }
 
 keystone_config { 'ssl/enable': value => true }
 
-include apache
-class { 'keystone::wsgi::apache':
+include ::apache
+class { '::keystone::wsgi::apache':
   ssl => true
 }
