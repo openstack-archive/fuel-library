aca2b31d2e9b8109ca93a4e5e718ef9f642d0a86
Tatyana Leontovich <tleontovich@mirantis.com>
2015-06-22 14:22:30 +0300

    Reject vrouter-host in  rps_cpus ()
    Problem:
    netconfig_post rb foolow checks failed on controller
    * test_rps_cpus_set
    * test_rps_cpus_config
    
    Caused by: we include vrouter-host interface in checks
    in function rps_cpu and ignore only lo.
    so add vrouter-host interface to rejected
    when we invoke rps_cpus for all data in /sys/class/net/
    
    Fuel-CI: disable
    
    Change-Id: Id96aaf32fad8dc5e7bf14a4bedf3c795518dfd21
    Closes-Bug: #1433087

--- deployment/puppet/osnailyfacter/modular/netconfig/netconfig_post.rb
+++ deployment/puppet/osnailyfacter/modular/netconfig/netconfig_post.rb
@@ -40,21 +40,21 @@ class NetconfigPostTest < Test::Unit::TestCase
   end
 
   def rps_cpus
-    Dir.glob('/sys/class/net/*/queues/rx-*/rps_cpus').reject { |node| node.start_with? '/sys/class/net/lo' }
+    Dir.glob('/sys/class/net/*/queues/rx-*/rps_cpus').reject { |node| node.start_with? '/sys/class/net/lo' or '/sys/class/net/vrouter'}
   end
 
   def test_rps_cpus_set
-    rps_cpus.each do |node|
-      assert File.read(node).chomp.end_with?(hex_mask), "Sysfs node: '#{node}' is not '#{hex_mask}'!"
+      rps_cpus.each do |node|
+        assert File.read(node).chomp.end_with?(hex_mask), "Sysfs node: '#{node}' is not '#{hex_mask}'!"
     end
   end
 
   def test_rps_cpus_config
-    assert File.exists?('/etc/sysfs.d/rps_cpus.conf'), 'RPS_CPUS sysfs config is missing!'
-    rps_cpus.each do |line|
-      line.gsub! %r(/sys/), ''
-      line = "#{line} = #{hex_mask}"
-      assert TestCommon::Config.has_line?('/etc/sysfs.d/rps_cpus.conf', line), "Line '#{line}' is missing in the rps_cpus.conf!"
+      assert File.exists?('/etc/sysfs.d/rps_cpus.conf'), 'RPS_CPUS sysfs config is missing!'
+      rps_cpus.each do |line|
+        line.gsub! %r(/sys/), ''
+        line = "#{line} = #{hex_mask}"
+        assert TestCommon::Config.has_line?('/etc/sysfs.d/rps_cpus.conf', line), "Line '#{line}' is missing in the rps_cpus.conf!"
     end
   end
 
