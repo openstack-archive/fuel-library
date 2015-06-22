9f9b3b3a246806031dd4d9ce8a0b36712170ed60
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-06-22 17:43:54 -0500

    Add ability to deploy vips on non-controllers
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: I288e10f6703e7d5fd9bc67eeb93576ba1a0d3b44

--- deployment/puppet/osnailyfacter/modular/virtual_ips/virtual_ips.pp
+++ deployment/puppet/osnailyfacter/modular/virtual_ips/virtual_ips.pp
@@ -7,6 +7,7 @@ $network_scheme              = hiera('network_scheme', {})
 $vip_management_cidr_netmask = netmask_to_cidr($primary_controller_nodes[0]['internal_netmask'])
 $vip_public_cidr_netmask     = netmask_to_cidr($primary_controller_nodes[0]['public_netmask'])
 $use_neutron                 = hiera('use_neutron', false)
+$deploy_vrouter              = true
 
 # todo:(sv): temporary commented. Will be uncommented while 'multiple-l2-network' feature re-implemented
 # if $use_neutron {
@@ -28,30 +29,38 @@ $management_vip_data = {
   ping_host_list       => "",
 }
 
-$management_vrouter_vip_data = {
-  namespace            => 'vrouter',
-  nic                  => $internal_int,
-  base_veth            => "${internal_int}-vrouter",
-  ns                   => 'vrouter',
-  ns_veth              => 'vr-mgmt',
-  ip                   => hiera('management_vrouter_vip'),
-  cidr_netmask         => $vip_management_cidr_netmask,
-  gateway              => 'none',
-  gateway_metric       => '0',
-  bridge               => $network_scheme['roles']['management'],
-  tie_with_ping        => false,
-  ping_host_list       => "",
-}
-
 cluster::virtual_ip { 'management' :
   vip => $management_vip_data,
 }
 
-cluster::virtual_ip { 'management_vrouter' :
-  vip => $management_vrouter_vip_data,
+
+if $deploy_vrouter {
+  $management_vrouter_vip_data = {
+    namespace            => 'vrouter',
+    nic                  => $internal_int,
+    base_veth            => "${internal_int}-vrouter",
+    ns                   => 'vrouter',
+    ns_veth              => 'vr-mgmt',
+    ip                   => hiera('management_vrouter_vip'),
+    cidr_netmask         => $vip_management_cidr_netmask,
+    gateway              => 'none',
+    gateway_metric       => '0',
+    bridge               => $network_scheme['roles']['management'],
+    tie_with_ping        => false,
+    ping_host_list       => "",
+  }
+
+  cluster::virtual_ip { 'management_vrouter' :
+    vip => $management_vrouter_vip_data,
+  }
+
+  $management_vips = ['management', 'management_vrouter']
+} else {
+  $management_vips = ['management']
 }
 
-$management_vips = ['management', 'management_vrouter']
+
+
 
 if $public_int {
   # todo:(sv): temporary commented. Will be uncommented while 'multiple-l2-network' feature re-implemented
@@ -72,31 +81,38 @@ if $public_int {
     other_networks       => $vip_publ_other_nets,
   }
 
-  $public_vrouter_vip_data = {
-    namespace               => 'vrouter',
-    nic                     => $public_int,
-    base_veth               => "${public_int}-vrouter",
-    ns_veth                 => 'vr-ex',
-    ns                      => 'vrouter',
-    ip                      => hiera('public_vrouter_vip'),
-    cidr_netmask            => $vip_public_cidr_netmask,
-    gateway                 => $network_scheme['endpoints']['br-ex']['gateway'],
-    gateway_metric          => '0',
-    bridge                  => $network_scheme['roles']['ex'],
-    ns_iptables_start_rules => "iptables -t nat -A POSTROUTING -o vr-ex -j MASQUERADE",
-    ns_iptables_stop_rules  => "iptables -t nat -D POSTROUTING -o vr-ex -j MASQUERADE",
-    collocation             => 'management_vrouter',
-  }
-
   cluster::virtual_ip { 'public' :
     vip => $public_vip_data,
   }
 
-  cluster::virtual_ip { 'public_vrouter' :
-    vip => $public_vrouter_vip_data,
+
+  if $deploy_vrouter {
+    $public_vrouter_vip_data = {
+      namespace               => 'vrouter',
+      nic                     => $public_int,
+      base_veth               => "${public_int}-vrouter",
+      ns_veth                 => 'vr-ex',
+      ns                      => 'vrouter',
+      ip                      => hiera('public_vrouter_vip'),
+      cidr_netmask            => $vip_public_cidr_netmask,
+      gateway                 => $network_scheme['endpoints']['br-ex']['gateway'],
+      gateway_metric          => '0',
+      bridge                  => $network_scheme['roles']['ex'],
+      ns_iptables_start_rules => "iptables -t nat -A POSTROUTING -o vr-ex -j MASQUERADE",
+      ns_iptables_stop_rules  => "iptables -t nat -D POSTROUTING -o vr-ex -j MASQUERADE",
+      collocation             => 'management_vrouter',
+    }
+
+    cluster::virtual_ip { 'public_vrouter' :
+      vip => $public_vrouter_vip_data,
+    }
+
+    $public_vips = ['public_vip', 'public_vrouter']
+  } else {
+    $public_vips = ['public_vip']
   }
 
-  $public_vips = ['public_vip', 'public_vrouter']
+
   $vips = concat($management_vips, $public_vips)
 } else {
   $vips = $management_vips
