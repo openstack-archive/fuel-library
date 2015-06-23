e8215c6ae2290cd347ec11d36c39de7d362bd545
Dmitry Ilyin <dilyin@mirantis.com>
2015-06-23 21:40:18 +0300

    Move DB creation from database to granules
    * Move database and user creation to a separete task
    * Either install local database or use an external one
    
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: Iaf3b7913e8c79c08025dbdaf5f2beff7337ab644
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/osnailyfacter/modular/database/database.pp
+++ deployment/puppet/osnailyfacter/modular/database/database.pp
@@ -1,111 +1,105 @@
 notice('MODULAR: database.pp')
 
-$neutron              = hiera('use_neutron')
-$mysql_hash           = hiera('mysql')
-$keystone_hash        = hiera('keystone')
-$glance_hash          = hiera('glance')
-$nova_hash            = hiera('nova')
-$cinder_hash          = hiera('cinder')
 $internal_address     = hiera('internal_address')
 $network_scheme       = hiera('network_scheme', {})
-$neutron_db_password  = hiera('neutron_db_password', false)
 $controller_nodes     = hiera('controller_nodes')
 $use_syslog           = hiera('use_syslog', true)
 $primary_controller   = hiera('primary_controller')
 $management_vip       = hiera('management_vip')
 
-$haproxy_stats_port  = '10000'
-$haproxy_stats_url   = "http://${management_vip}:${haproxy_stats_port}/;csv"
+$mysql_hash           = hiera_hash('mysql', {})
 
-$mysql_root_password    = $mysql_hash['root_password']
-$mysql_bind_address     = '0.0.0.0'
-$mysql_account_security = true
+$haproxy_stats_port   = '10000'
+$haproxy_stats_url    = "http://${management_vip}:${haproxy_stats_port}/;csv"
 
-$keystone_db_user       = 'keystone'
-$keystone_db_dbname     = 'keystone'
-$keystone_db_password   = $keystone_hash['db_password']
+$mysql_database_password  = fetch($mysql_hash, 'root_password')
+$mysql_database_enabled   = fetch($mysql_hash, 'enabled', true)
 
-$glance_db_user         = 'glance'
-$glance_db_dbname       = 'glance'
-$glance_db_password     = $glance_hash['db_password']
-
-$nova_db_user           = 'nova'
-$nova_db_dbname         = 'nova'
-$nova_db_password       = $nova_hash['db_password']
-
-$cinder_db_user         = 'cinder'
-$cinder_db_dbname       = 'cinder'
-$cinder_db_password     = $cinder_hash['db_password']
-
-$neutron_db_user        = 'neutron'
-$neutron_db_dbname      = 'neutron'
+$mysql_bind_address       = '0.0.0.0'
+$mysql_account_security   = true
 
 $enabled                  = true
 $allowed_hosts            = [ '%', $::hostname ]
 $galera_cluster_name      = 'openstack'
 $galera_node_address      = $internal_address
 $galera_nodes             = $controller_nodes
-$custom_mysql_setup_class = 'galera'
 $mysql_skip_name_resolve  = true
+$custom_setup_class       = 'galera'
 
 $status_user              = 'clustercheck'
-$status_password          = $mysql_hash['wsrep_password']
+$status_password          = fetch($mysql_hash, 'wsrep_password')
 $backend_port             = '3307'
 $backend_timeout          = '10'
-$man_net                  = $network_scheme['endpoints'][$network_scheme['roles']['management']]['IP']
 
-###############################################################################
+$management_interface     = fetch($network_scheme, 'roles/management')
+$management_network       = fetch($network_scheme, "endpoints/${management_interface}/IP")
 
-class { 'openstack::db::mysql':
-  mysql_root_password     => $mysql_root_password,
-  mysql_bind_address      => $mysql_bind_address,
-  mysql_account_security  => $mysql_account_security,
-  keystone_db_user        => $keystone_db_user,
-  keystone_db_password    => $keystone_db_password,
-  keystone_db_dbname      => $keystone_db_dbname,
-  glance_db_user          => $glance_db_user,
-  glance_db_password      => $glance_db_password,
-  glance_db_dbname        => $glance_db_dbname,
-  nova_db_user            => $nova_db_user,
-  nova_db_password        => $nova_db_password,
-  nova_db_dbname          => $nova_db_dbname,
-  cinder                  => $cinder,
-  cinder_db_user          => $cinder_db_user,
-  cinder_db_password      => $cinder_db_password,
-  cinder_db_dbname        => $cinder_db_dbname,
-  neutron                 => $neutron,
-  neutron_db_user         => $neutron_db_user,
-  neutron_db_password     => $neutron_db_password,
-  neutron_db_dbname       => $neutron_db_dbname,
-  allowed_hosts           => $allowed_hosts,
-  enabled                 => $enabled,
-  galera_cluster_name     => $galera_cluster_name,
-  primary_controller      => $primary_controller,
-  galera_node_address     => $galera_node_address,
-  galera_nodes            => $galera_nodes,
-  custom_setup_class      => $custom_mysql_setup_class,
-  mysql_skip_name_resolve => $mysql_skip_name_resolve,
-  use_syslog              => $use_syslog,
+#############################################################################
+
+if $mysql_database_enabled {
+
+  if $custom_setup_class {
+    file { '/etc/mysql/my.cnf':
+      ensure    => absent,
+      require   => Class['mysql::server']
+    }
+    $config_hash_real = {
+      'config_file' => '/etc/my.cnf'
+    }
+  } else {
+    $config_hash_real = { }
+  }
+
+  class { "mysql::server":
+    bind_address            => '0.0.0.0',
+    etc_root_password       => true,
+    root_password           => $mysql_database_password,
+    old_root_password       => '',
+    galera_cluster_name     => $galera_cluster_name,
+    primary_controller      => $primary_controller,
+    galera_node_address     => $galera_node_address,
+    galera_nodes            => $galera_nodes,
+    enabled                 => $enabled,
+    custom_setup_class      => $custom_setup_class,
+    mysql_skip_name_resolve => $mysql_skip_name_resolve,
+    use_syslog              => $use_syslog,
+    config_hash             => $config_hash_real,
+  }
+
+  class { 'osnailyfacter::mysql_access' :
+    user     => 'root',
+    password => $mysql_database_password,
+    host     => 'localhost',
+  }
+
+  # This removes default users and guest access
+  if $mysql_account_security and $custom_setup_class == undef {
+    class { 'mysql::server::account_security': }
+  }
+
+  class { 'openstack::galera::status':
+    status_user     => $status_user,
+    status_password => $status_password,
+    status_allow    => $galera_node_address,
+    backend_host    => $galera_node_address,
+    backend_port    => $backend_port,
+    backend_timeout => $backend_timeout,
+    only_from       => "127.0.0.1 240.0.0.2 ${management_network}",
+  }
+
+  haproxy_backend_status { 'mysql' :
+    name => 'mysqld',
+    url  => $haproxy_stats_url,
+  }
+
+  package { 'socat':
+    ensure => 'present'
+  }
+
+  Package['socat'] ->
+  Class['mysql::server'] ->
+  Class['osnailyfacter::mysql_access'] ->
+  Class['openstack::galera::status'] ->
+  Haproxy_backend_status['mysql']
+
 }
-
-class { 'openstack::galera::status':
-  status_user             => $status_user,
-  status_password         => $status_password,
-  status_allow            => $galera_node_address,
-  backend_host            => $galera_node_address,
-  backend_port            => $backend_port,
-  backend_timeout         => $backend_timeout,
-  only_from               => "127.0.0.1 240.0.0.2 ${man_net}",
-}
-
-haproxy_backend_status { 'mysql' :
-  name    => 'mysqld',
-  url     => $haproxy_stats_url,
-}
-
-package { 'socat': ensure => present }
-
-Package['socat'] -> Class['openstack::db::mysql']
-Class['openstack::db::mysql'] -> Class['openstack::galera::status']
-Class['openstack::galera::status'] -> Haproxy_backend_status['mysql']
-Class['mysql::server'] -> Haproxy_backend_status['mysql']
