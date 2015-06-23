4fd23cc63778b1e260f26ca49dcb3f8a104be11c
Dmitry Ilyin <dilyin@mirantis.com>
2015-06-23 21:40:18 +0300

    Move DB creation from database to granules
    * Move database and user creation to a separete task
    * Either install local database or use an external one
    
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: Iaf3b7913e8c79c08025dbdaf5f2beff7337ab644
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/mysql/lib/puppet/provider/database_user/mysql.rb
+++ deployment/puppet/mysql/lib/puppet/provider/database_user/mysql.rb
@@ -7,33 +7,46 @@ Puppet::Type.type(:database_user).provide(:mysql) do
   optional_commands :mysql      => 'mysql'
   optional_commands :mysqladmin => 'mysqladmin'
 
+  # Optional defaults file
+  def self.defaults_file
+    if File.file?('/root/.my.cnf')
+      "--defaults-extra-file=#{Facter.value(:root_home)}/.my.cnf"
+    else
+      nil
+    end
+  end
+
+  def defaults_file
+    self.class.defaults_file
+  end
+
   def self.instances
-    users = mysql("mysql", '-BNe' "select concat(User, '@',Host) as User from mysql.user").split("\n")
+    users = mysql(defaults_file, "mysql", '-BNe' "select concat(User, '@',Host) as User from mysql.user").split("\n")
     users.select{ |user| user =~ /.+@/ }.collect do |name|
       new(:name => name)
     end
   end
 
   def create
-    mysql("mysql", "-e", "create user '%s' identified by PASSWORD '%s'" % [ @resource[:name].sub("@", "'@'"), @resource.value(:password_hash) ])
+    mysql(defaults_file, "mysql", "-e", "create user '%s' identified by PASSWORD '%s'" % [ @resource[:name].sub("@", "'@'"), @resource.value(:password_hash) ])
   end
 
   def destroy
-    mysql("mysql", "-e", "drop user '%s'" % @resource.value(:name).sub("@", "'@'") )
+    mysql(defaults_file, "mysql", "-e", "drop user '%s'" % @resource.value(:name).sub("@", "'@'") )
   end
 
   def password_hash
-    mysql("mysql", "-NBe", "select password from user where CONCAT(user, '@', host) = '%s'" % @resource.value(:name)).chomp
+    mysql(defaults_file, "mysql", "-NBe", "select password from user where CONCAT(user, '@', host) = '%s'" % @resource.value(:name)).chomp
   end
 
   def password_hash=(string)
-    mysql("mysql", "-e", "SET PASSWORD FOR '%s' = '%s'" % [ @resource[:name].sub("@", "'@'"), string ] )
+    mysql(defaults_file, "mysql", "-e", "SET PASSWORD FOR '%s' = '%s'" % [ @resource[:name].sub("@", "'@'"), string ] )
   end
 
   def exists?
     tries=10
     begin
-        not mysql("mysql", "-NBe", "select '1' from user where CONCAT(user, '@', host) = '%s'" % @resource.value(:name)).empty?
+        not mysql(defaults_file, "mysql", "-NBe", "select '1' from user where CONCAT(user, '@', host) = '%s'" % @resource.value(:name)).empty?
     rescue
         debug("Can't connect to the mysql server: #{tries} tries to reconnect")
         sleep 5
@@ -43,7 +56,7 @@ Puppet::Type.type(:database_user).provide(:mysql) do
 
   def flush
     @property_hash.clear
-    mysqladmin "flush-privileges"
+    mysqladmin defaults_file, "flush-privileges"
   end
 
 end
---
+++ deployment/puppet/osnailyfacter/modular/keystone/keystone_db.pp
@@ -0,0 +1,43 @@
+$keystone_hash  = hiera_hash('keystone', {})
+$mysql_hash     = hiera_hash('mysql', {})
+$management_vip = hiera('management_vip', undef)
+$database_vip   = hiera('database_vip', undef)
+
+$mysql_root_user     = pick($mysql_hash['root_user'], 'root')
+$mysql_db_create     = pick($mysql_hash['db_create'], true)
+$mysql_root_password = $mysql_hash['root_password']
+
+$db_user     = pick($keystone_hash['db_user'], 'keystone')
+$db_name     = pick($keystone_hash['db_name'], 'keystone')
+$db_password = pick($keystone_hash['db_password'], $mysql_root_password)
+
+$db_host          = pick($keystone_hash['db_host'], $database_vip, $management_vip, 'localhost')
+$db_create        = pick($keystone_hash['db_create'], $mysql_db_create)
+$db_root_user     = pick($keystone_hash['root_user'], $mysql_root_user)
+$db_root_password = pick($keystone_hash['root_password'], $mysql_root_password)
+
+$allowed_hosts = [ $::hostname, 'localhost', '127.0.0.1', '%' ]
+
+if $db_create {
+
+  class { 'keystone::db::mysql':
+    user          => $db_user,
+    password      => $db_password,
+    dbname        => $db_name,
+    allowed_hosts => $allowed_hosts,
+  }
+
+  class { 'osnailyfacter::mysql_access' :
+    db_host     => $db_host,
+    db_user     => $db_root_user,
+    db_password => $db_root_password,
+  }
+
+  Class['osnailyfacter::mysql_access'] -> Class['keystone::db::mysql']
+
+}
+
+class mysql::config {}
+include mysql::config
+class mysql::server {}
+include mysql::server
