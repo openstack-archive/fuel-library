43a0df8e4b759b648f32412b37ee7c6cdc58ff39
Dmitry Ilyin <dilyin@mirantis.com>
2015-06-23 21:40:18 +0300

    Move DB creation from database to granules
    * Move database and user creation to a separete task
    * Either install local database or use an external one
    
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: Iaf3b7913e8c79c08025dbdaf5f2beff7337ab644
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/mysql/lib/puppet/provider/database/mysql.rb
+++ deployment/puppet/mysql/lib/puppet/provider/database/mysql.rb
@@ -7,8 +7,21 @@ Puppet::Type.type(:database).provide(:mysql) do
   optional_commands :mysql      => 'mysql'
   optional_commands :mysqladmin => 'mysqladmin'
 
+  # Optional defaults file
+  def self.defaults_file
+    if File.file?('/root/.my.cnf')
+      "--defaults-extra-file=#{Facter.value(:root_home)}/.my.cnf"
+    else
+      nil
+    end
+  end
+
+  def defaults_file
+    self.class.defaults_file
+  end
+
   def self.instances
-    mysql('-NBe', "show databases").split("\n").collect do |name|
+    mysql(defaults_file, '-NBe', "show databases").split("\n").collect do |name|
       new(:name => name)
     end
   end
@@ -17,7 +30,7 @@ Puppet::Type.type(:database).provide(:mysql) do
     tries=10
     begin
         debug("Trying to create database #{@resource[:name]} ")
-        mysql('-NBe', "create database `#{@resource[:name]}` character set #{resource[:charset]}")
+        mysql(defaults_file, '-NBe', "create database `#{@resource[:name]}` character set #{resource[:charset]}")
     rescue
         debug("Can't connect to the server: #{tries} tries to reconnect")
         sleep 5
@@ -26,20 +39,20 @@ Puppet::Type.type(:database).provide(:mysql) do
   end
 
   def destroy
-    mysqladmin('-f', 'drop', @resource[:name])
+    mysqladmin(defaults_file, '-f', 'drop', @resource[:name])
   end
 
   def charset
-    mysql('-NBe', "show create database `#{resource[:name]}`").match(/.*?(\S+)\s\*\//)[1]
+    mysql(defaults_file, '-NBe', "show create database `#{resource[:name]}`").match(/.*?(\S+)\s\*\//)[1]
   end
 
   def charset=(value)
-    mysql('-NBe', "alter database `#{resource[:name]}` CHARACTER SET #{value}")
+    mysql(defaults_file, '-NBe', "alter database `#{resource[:name]}` CHARACTER SET #{value}")
   end
 
   def exists?
     begin
-      mysql('-NBe', "show databases").match(/^#{@resource[:name]}$/)
+      mysql(defaults_file, '-NBe', "show databases").match(/^#{@resource[:name]}$/)
     rescue => e
       debug(e.message)
       return nil
