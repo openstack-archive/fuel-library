9120c4454a4e25bc838e961bcaf36c390cb8236c
Denis Egorenko <degorenko@mirantis.com>
2015-06-23 20:30:28 +0300

    Adapt synced glance module
    - add patch Ib44cf7825de33dc251812cb9138b821a397d77e7;
    - replace old glance.pp parameters in openstack module;
    - add workaround for mysql until mysql module will be updated;
    - remove deprecated parameters;
    
    Partially implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: I333de158808bf34e1602933978c1edc4d9d27bac

--- deployment/puppet/glance/manifests/db/mysql.pp
+++ deployment/puppet/glance/manifests/db/mysql.pp
@@ -51,16 +51,51 @@ class glance::db::mysql(
 
   validate_string($password)
 
-  ::openstacklib::db::mysql { 'glance':
-    user          => $user,
-    password_hash => mysql_password($password),
-    dbname        => $dbname,
-    host          => $host,
-    charset       => $charset,
-    collate       => $collate,
-    allowed_hosts => $allowed_hosts,
-  }
+  # TODO (degorenko): This workaround should be removed after mysql module upgrade
+  if ($mysql_module >= 2.2) {
+    ::openstacklib::db::mysql { 'glance':
+      user          => $user,
+      password_hash => mysql_password($password),
+      dbname        => $dbname,
+      host          => $host,
+      charset       => $charset,
+      collate       => $collate,
+      allowed_hosts => $allowed_hosts,
+    }
 
-  ::Openstacklib::Db::Mysql['glance'] ~> Exec<| title == 'glance-manage db_sync' |>
+    ::Openstacklib::Db::Mysql['glance'] ~> Exec<| title == 'glance-manage db_sync' |>
+  }
+  else {
+    Class['glance::db::mysql'] -> Exec<| title == 'glance-manage db_sync' |>
+    require mysql::python
+    Database[$dbname] ~> Exec<| title == 'glance-manage db_sync' |>
+
+    mysql::db { $dbname:
+      user         => $user,
+      password     => $password,
+      host         => $host,
+      charset      => $charset,
+      require      => Class['mysql::config'],
+    }
+
+    # Check allowed_hosts to avoid duplicate resource declarations
+    # If $host in $allowed_hosts, then remove it
+    if is_array($allowed_hosts) and delete($allowed_hosts,$host) != [] {
+      $real_allowed_hosts = delete($allowed_hosts,$host)
+    # If $host = $allowed_hosts, then set it to undef
+    } elsif is_string($allowed_hosts) and ($allowed_hosts != $host) {
+      $real_allowed_hosts = $allowed_hosts
+    }
+
+    if $real_allowed_hosts {
+      # TODO this class should be in the mysql namespace
+      glance::db::mysql::host_access { $real_allowed_hosts:
+        user          => $user,
+        password      => $password,
+        database      => $dbname,
+        mysql_module  => $mysql_module,
+      }
+    }
+  }
 
 }
