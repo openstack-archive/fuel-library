b381c4d841ba56f19cd940316e7664dceca2fa48
Dmitry Ilyin <dilyin@mirantis.com>
2015-06-23 21:40:18 +0300

    Move DB creation from database to granules
    * Move database and user creation to a separete task
    * Either install local database or use an external one
    
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: Iaf3b7913e8c79c08025dbdaf5f2beff7337ab644
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/osnailyfacter/modular/sahara/sahara_pre.rb
+++ deployment/puppet/osnailyfacter/modular/sahara/sahara_pre.rb
@@ -2,17 +2,21 @@ require File.join File.dirname(__FILE__), '../test_common.rb'
 
 class SaharaPreTest < Test::Unit::TestCase
 
-  def test_mysql_connection_without_auth
-    TestCommon::MySQL.no_auth
-    assert TestCommon::MySQL.connection?, 'Cannot connect to MySQL without auth!'
-  end
-
+  # def test_mysql_accessible_for_sahara
+  #   TestCommon::MySQL.pass = TestCommon::Settings.sahara['db_password']
+  #   TestCommon::MySQL.user = 'sahara'
+  #   TestCommon::MySQL.host = TestCommon::Settings.management_vip
+  #   TestCommon::MySQL.port = 3306
+  #   TestCommon::MySQL.db = 'sahara'
+  #   assert TestCommon::MySQL.connection?, 'Cannot connect to MySQL with Glance auth!'
+  # end
+  
   def test_amqp_accessible
     assert TestCommon::AMQP.connection?, 'Cannot connect to AMQP server!'
   end
 
   def test_haproxy_sahara_backend_present
-    assert TestCommon::HAProxy.backend_present?('sahara'), 'No murano haproxy backend!'
+    assert TestCommon::HAProxy.backend_present?('sahara'), 'No sahara haproxy backend!'
   end
 
   def test_horizon_haproxy_backend_online
