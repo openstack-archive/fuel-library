53d4dd3387653e4f9648433c611602cba46dc3a0
vsaienko <vsaienko@mirantis.com>
2015-06-23 17:42:13 +0300

    [DONOTMERGE] Adapt synced keystone module
      revert 'Migrate mysql backend to use openstacklib::db::mysql'
      6e108f23b8d6c0b82fbf3f200669224db0a7a5ba
    
    Change-Id: I33cbabd5e623fc9920107a3f8c2258bd7e47346e

--- deployment/puppet/keystone/spec/classes/keystone_db_mysql_spec.rb
+++ deployment/puppet/keystone/spec/classes/keystone_db_mysql_spec.rb
@@ -13,23 +13,59 @@ describe 'keystone::db::mysql' do
     { :osfamily => 'Debian' }
   end
 
-  let :params do
+  let :param_defaults do
     {
       'password'      => 'keystone_default_password',
+      'dbname'        => 'keystone',
+      'user'          => 'keystone',
+      'charset'       => 'utf8',
+      'collate'       => 'utf8_unicode_ci',
+      'host'          => '127.0.0.1',
+      'allowed_hosts' => ['127.0.0.%', '192.168.1.%']
     }
   end
 
-  describe 'with only required params' do
-    it { is_expected.to contain_openstacklib__db__mysql('keystone').with(
-      'user'          => 'keystone',
-      'password_hash' => '*B552157B14BCEDDCEAA06767A012F31BDAA9CE3D',
-      'dbname'        => 'keystone',
-      'host'          => '127.0.0.1',
-      'charset'       => 'utf8',
-      :collate        => 'utf8_general_ci',
-    )}
-  end
+#<<<<<<< HEAD
+#  describe 'with only required params' do
+#    it { is_expected.to contain_openstacklib__db__mysql('keystone').with(
+#      'user'          => 'keystone',
+#      'password_hash' => '*B552157B14BCEDDCEAA06767A012F31BDAA9CE3D',
+#      'dbname'        => 'keystone',
+#      'host'          => '127.0.0.1',
+#      'charset'       => 'utf8',
+#      :collate        => 'utf8_general_ci',
+#=======
+  [
+    {},
+    {
+      'password' => 'password',
+      'dbname'   => 'not_keystone',
+      'user'     => 'dan',
+      'host'     => '127.0.0.2',
+      'charset'  => 'utf8'
+    }
+  ].each do |p|
 
+    let :params do
+      p
+    end
+
+    let :param_values do
+      param_defaults.merge(p)
+    end
+
+    it { should contain_class('mysql::python') }
+
+    it { should contain_mysql__db(param_values['dbname']).with(
+      'user'     => param_values['user'],
+      'password' => param_values['password'],
+      'host'     => param_values['host'],
+      'charset'  => param_values['charset'],
+      'require'  => 'Class[Mysql::Config]'
+#>>>>>>> parent of 6e108f2... Migrate mysql backend to use openstacklib::db::mysql
+    )}
+
+  end
   describe "overriding allowed_hosts param to array" do
     let :params do
       {
@@ -38,6 +74,16 @@ describe 'keystone::db::mysql' do
       }
     end
 
+    it {should_not contain_keystone__db__mysql__host_access("127.0.0.1").with(
+      :user     => 'keystone',
+      :password => 'keystonepass',
+      :database => 'keystone'
+    )}
+    it {should contain_keystone__db__mysql__host_access("%").with(
+      :user     => 'keystone',
+      :password => 'keystonepass',
+      :database => 'keystone'
+    )}
   end
   describe "overriding allowed_hosts param to string" do
     let :params do
@@ -47,6 +93,11 @@ describe 'keystone::db::mysql' do
       }
     end
 
+    it {should contain_keystone__db__mysql__host_access("192.168.1.1").with(
+      :user     => 'keystone',
+      :password => 'keystonepass2',
+      :database => 'keystone'
+    )}
   end
 
   describe "overriding allowed_hosts param equals to host param " do
@@ -57,6 +108,11 @@ describe 'keystone::db::mysql' do
       }
     end
 
+    it {should_not contain_keystone__db__mysql__host_access("127.0.0.1").with(
+      :user     => 'keystone',
+      :password => 'keystonepass2',
+      :database => 'keystone'
+    )}
   end
 
 end
