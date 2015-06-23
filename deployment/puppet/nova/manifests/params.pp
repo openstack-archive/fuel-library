2caf08909bae9a5e5f21d4ecfa5c94d1ac6364ce
iberezovskiy <iberezovskiy@mirantis.com>
2015-06-23 15:55:12 +0300

    Adapt synced Nova module for Fuel usage
      Key moments:
        * Return implementation of type and provider for nova_floating_range
        * Make package 'bridge-utils' install optional
          commit - I744fe89e8d9dae79a5b822ac1a1ad4cee780bfc4
        * Add workaround for MySQL module
        * Fix duplication errors in openstack module
        * Revise patch for two dns servers
        * Fix lint issues
          commit: 7a4e067e9241a3fffec4b2d521f070f0bea1f50f
        * Add noop tests for new parameters
    
      Closes-bug: #1468230
      Closes-bug: #1469308
    
      Implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: Ie5732f4976d688f3788cc83d3b1bac7231fcd314

--- deployment/puppet/nova/manifests/params.pp
+++ deployment/puppet/nova/manifests/params.pp
@@ -85,7 +85,8 @@ class nova::params {
       $compute_service_name         = 'nova-compute'
       $conductor_service_name       = 'nova-conductor'
       $consoleauth_service_name     = 'nova-consoleauth'
-      $libvirt_service_name         = 'libvirt-bin'
+      # Workaround for bug LP #1469308
+      $libvirt_service_name         = 'libvirtd'
       $network_service_name         = 'nova-network'
       $objectstore_service_name     = 'nova-objectstore'
       $scheduler_service_name       = 'nova-scheduler'
@@ -107,7 +108,9 @@ class nova::params {
         default: {
           $spicehtml5proxy_package_name = 'nova-spiceproxy'
           $spicehtml5proxy_service_name = 'nova-spiceproxy'
-          $vncproxy_package_name    = 'nova-novncproxy'
+          # Workadroung for bug LP #1468230
+          #$vncproxy_package_name    = 'nova-novncproxy'
+          $vncproxy_package_name    = 'nova-consoleproxy'
           # some of the services need to be started form the special upstart provider
           $special_service_provider = 'upstart'
           $nova_log_group               = 'adm'
