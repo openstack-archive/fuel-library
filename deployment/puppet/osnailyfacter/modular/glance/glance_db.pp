f7bac092e6464ad7255ee58fcc0fe187f59d21ac
Dmitry Ilyin <dilyin@mirantis.com>
2015-06-23 21:40:18 +0300

    Move DB creation from database to granules
    * Move database and user creation to a separete task
    * Either install local database or use an external one
    
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: Iaf3b7913e8c79c08025dbdaf5f2beff7337ab644
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

---
+++ deployment/puppet/osnailyfacter/modular/glance/glance_db.pp
@@ -0,0 +1,48 @@
+$glance_hash    = hiera_hash('glance', {})
+$mysql_hash     = hiera_hash('mysql', {})
+$database_vip   = hiera('database_vip', undef)
+
+$mysql_db_host       = fetch($mysql_hash, 'db_host', $database_vip)
+$mysql_root_user     = fetch($mysql_hash, 'root_user', 'root')
+$mysql_root_password = fetch($mysql_hash, 'root_password')
+$mysql_db_create     = fetch($mysql_hash, 'db_create', true)
+
+$db_user     = fetch($glance_hash, 'db_user', 'glance')
+$db_name     = fetch($glance_hash, 'db_name', 'glance')
+$db_password = fetch($glance_hash, 'db_password')
+
+$db_host       = fetch($glance_hash, 'db_host', $mysql_db_host)
+$db_create     = fetch($glance_hash, 'db_create', $mysql_db_create)
+$root_user     = fetch($glance_hash, 'root_user', $mysql_root_user)
+$root_password = fetch($glance_hash, 'root_password', $mysql_root_password)
+
+$allowed_hosts = [ $::hostname, 'localhost', '127.0.0.1', '%' ]
+
+if $db_create {
+
+  # Create the Keystone db
+  class { 'glance::db::mysql':
+    user          => $db_user,
+    password      => $db_password,
+    dbname        => $db_name,
+    allowed_hosts => $allowed_hosts,
+  }
+
+  if $db_host and $root_user and $root_password {
+
+    # Work with remote database host
+    class { 'osnailyfacter::mysql_access' :
+      user     => $root_user,
+      password => $root_password,
+      host     => $db_host,
+    }
+
+    Class['osnailyfacter::mysql_access'] -> Class['glance::db::mysql']
+
+  }
+}
+
+class mysql::config {}
+include mysql::config
+class mysql::server {}
+include mysql::server
---
+++ deployment/puppet/osnailyfacter/modular/sahara/sahara_db.pp
@@ -0,0 +1,51 @@
+$sahara_hash    = hiera_hash('sahara', {})
+$mysql_hash     = hiera_hash('mysql', {})
+$sahara_enabled = fetch($sahara_hash, 'enabled', false)
+$database_vip   = hiera('database_vip', undef)
+
+$mysql_db_host       = fetch($mysql_hash, 'db_host', $database_vip)
+$mysql_root_user     = fetch($mysql_hash, 'root_user', 'root')
+$mysql_root_password = fetch($mysql_hash, 'root_password')
+$mysql_db_create     = fetch($mysql_hash, 'db_create', true)
+
+$db_user     = fetch($sahara_hash, 'db_user', 'sahara')
+$db_name     = fetch($sahara_hash, 'db_name', 'sahara')
+$db_password = fetch($sahara_hash, 'db_password')
+
+$db_host       = fetch($sahara_hash, 'db_host', $mysql_db_host)
+$db_create     = fetch($sahara_hash, 'db_create', $mysql_db_create)
+$root_user     = fetch($sahara_hash, 'root_user', $mysql_root_user)
+$root_password = fetch($sahara_hash, 'root_password', $mysql_root_password)
+
+$allowed_hosts = [ $::hostname, '127.0.0.1', '%' ]
+
+if $sahara_enabled and $db_create {
+
+  # Create the Keystone db
+  class { 'sahara::db::mysql':
+    user          => $db_user,
+    password      => $db_password,
+    dbname        => $db_name,
+    allowed_hosts => $allowed_hosts,
+  }
+
+  if $db_host and $root_user and $root_password {
+
+    # Work with remote database host
+    class { 'osnailyfacter::mysql_access' :
+      user     => $root_user,
+      password => $root_password,
+      host     => $db_host,
+    }
+
+    Class['osnailyfacter::mysql_access'] -> Class['sahara::db::mysql']
+
+  }
+}
+
+class mysql::config {}
+include mysql::config
+class mysql::server {}
+include mysql::server
+class sahara::api {}
+include sahara::api
