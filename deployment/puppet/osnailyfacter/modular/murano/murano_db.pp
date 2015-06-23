659b311c06ce1ab8d4d0c5d0849ca5c1e5e1324f
Dmitry Ilyin <dilyin@mirantis.com>
2015-06-23 21:40:18 +0300

    Move DB creation from database to granules
    * Move database and user creation to a separete task
    * Either install local database or use an external one
    
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: Iaf3b7913e8c79c08025dbdaf5f2beff7337ab644
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

---
+++ deployment/puppet/osnailyfacter/modular/murano/murano_db.pp
@@ -0,0 +1,51 @@
+$murano_hash      = hiera_hash('murano', {})
+$mysql_hash       = hiera_hash('mysql', {})
+$murano_enabled   = fetch($murano_hash, 'enabled', false)
+$database_vip     = hiera('database_vip', undef)
+
+$mysql_db_host       = fetch($mysql_hash, 'db_host', $database_vip)
+$mysql_root_user     = fetch($mysql_hash, 'root_user', 'root')
+$mysql_root_password = fetch($mysql_hash, 'root_password')
+$mysql_db_create     = fetch($mysql_hash, 'db_create', true)
+
+$db_user     = fetch($murano_hash, 'db_user', 'murano')
+$db_name     = fetch($murano_hash, 'db_name', 'murano')
+$db_password = fetch($murano_hash, 'db_password')
+
+$db_host       = fetch($murano_hash, 'db_host', $mysql_db_host)
+$db_create     = fetch($murano_hash, 'db_create', $mysql_db_create)
+$root_user     = fetch($murano_hash, 'root_user', $mysql_root_user)
+$root_password = fetch($murano_hash, 'root_password', $mysql_root_password)
+
+$allowed_hosts = [ $::hostname, '127.0.0.1', '%' ]
+
+if $murano_enabled and $db_create {
+
+  # Create the Keystone db
+  class { 'murano::db::mysql':
+    user          => $db_user,
+    password      => $db_password,
+    dbname        => $db_name,
+    allowed_hosts => $allowed_hosts,
+  }
+
+  if $db_host and $root_user and $root_password {
+
+    # Work with remote database host
+    class { 'osnailyfacter::mysql_access' :
+      user     => $root_user,
+      password => $root_password,
+      host     => $db_host,
+    }
+
+    Class['osnailyfacter::mysql_access'] -> Class['murano::db::mysql']
+
+  }
+}
+
+class mysql::config {}
+include mysql::config
+class mysql::server {}
+include mysql::server
+class murano::api {}
+include murano::api
