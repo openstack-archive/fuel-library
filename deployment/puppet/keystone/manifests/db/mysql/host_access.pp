149869c6104d864b96dd8335484c156c551d2e93
vsaienko <vsaienko@mirantis.com>
2015-06-23 17:42:13 +0300

    Adapt synced keystone module
      * wrap mysql-related things to if .. else
        to temporary provide compatibility with old mysql module
      * restore file:
        deployment/puppet/keystone/manifests/db/mysql/host_access.pp
      * add new parameters:
        public_url, admin_url, internal_url, mysql_module, token_caching
        cache_backend, revoke_driver
      * replase old parameters with new:
        idle_timeout -> database_idle_timeout
      * installing python-memcache has been moved to module
    
    Change-Id: I33cbabd5e623fc9920107a3f8c2258bd7e47346e

---
+++ deployment/puppet/keystone/manifests/db/mysql/host_access.pp
@@ -0,0 +1,34 @@
+#
+define keystone::db::mysql::host_access(
+  $user,
+  $password,
+  $database,
+  $mysql_module = '0.9'
+) {
+  if ($mysql_module >= 2.2) {
+    mysql_user { "${user}@${name}":
+      password_hash => mysql_password($password),
+      require       => Mysql_database[$database],
+    }
+
+    mysql_grant { "${user}@${name}/${database}.*":
+      privileges => ['ALL'],
+      options    => ['GRANT'],
+      table      => "${database}.*",
+      require    => Mysql_user["${user}@${name}"],
+      user       => "${user}@${name}"
+    }
+  } else {
+    database_user { "${user}@${name}":
+      password_hash => mysql_password($password),
+      provider      => 'mysql',
+      require       => Database[$database],
+    }
+    database_grant { "${user}@${name}/${database}":
+      # TODO figure out which privileges to grant.
+      privileges => 'all',
+      provider   => 'mysql',
+      require    => Database_user["${user}@${name}"]
+    }
+  }
+}
