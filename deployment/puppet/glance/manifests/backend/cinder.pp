7b357a8da26f7f902e34657406801b698ba62584
Denis Egorenko <degorenko@mirantis.com>
2015-06-23 20:30:28 +0300

    Adapt synced glance module
    - add patch Ib44cf7825de33dc251812cb9138b821a397d77e7;
    - replace old glance.pp parameters in openstack module;
    - add workaround for mysql until mysql module will be updated;
    - remove deprecated parameters;
    
    Partially implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: I333de158808bf34e1602933978c1edc4d9d27bac

--- deployment/puppet/glance/manifests/backend/cinder.pp
+++ deployment/puppet/glance/manifests/backend/cinder.pp
@@ -68,9 +68,9 @@ class glance::backend::cinder(
   }
 
   glance_api_config {
-    'DEFAULT/cinder_api_insecure':         value => $cinder_api_insecure;
-    'DEFAULT/cinder_catalog_info':         value => $cinder_catalog_info;
-    'DEFAULT/cinder_http_retries':         value => $cinder_http_retries;
+    'glance_store/cinder_api_insecure':    value => $cinder_api_insecure;
+    'glance_store/cinder_catalog_info':    value => $cinder_catalog_info;
+    'glance_store/cinder_http_retries':    value => $cinder_http_retries;
     'glance_store/default_store':          value => 'cinder';
   }
 
@@ -81,19 +81,19 @@ class glance::backend::cinder(
   }
 
   if $cinder_endpoint_template {
-    glance_api_config { 'DEFAULT/cinder_endpoint_template':   value => $cinder_endpoint_template; }
-    glance_cache_config { 'DEFAULT/cinder_endpoint_template': value => $cinder_endpoint_template; }
+    glance_api_config { 'glance_store/cinder_endpoint_template':   value => $cinder_endpoint_template; }
+    glance_cache_config { 'DEFAULT/cinder_endpoint_template':      value => $cinder_endpoint_template; }
   } else {
-    glance_api_config { 'DEFAULT/cinder_endpoint_template':   ensure => absent; }
-    glance_cache_config { 'DEFAULT/cinder_endpoint_template': ensure => absent; }
+    glance_api_config { 'glance_store/cinder_endpoint_template':   ensure => absent; }
+    glance_cache_config { 'DEFAULT/cinder_endpoint_template':      ensure => absent; }
   }
 
   if $cinder_ca_certificates_file {
-    glance_api_config { 'DEFAULT/cinder_ca_certificates_file':   value => $cinder_ca_certificates_file; }
-    glance_cache_config { 'DEFAULT/cinder_ca_certificates_file': value => $cinder_ca_certificates_file; }
+    glance_api_config { 'glance_store/cinder_ca_certificates_file':   value => $cinder_ca_certificates_file; }
+    glance_cache_config { 'DEFAULT/cinder_ca_certificates_file':      value => $cinder_ca_certificates_file; }
   } else {
-    glance_api_config { 'DEFAULT/cinder_ca_certificates_file':   ensure => absent; }
-    glance_cache_config { 'DEFAULT/cinder_ca_certificates_file': ensure => absent; }
+    glance_api_config { 'glance_store/cinder_ca_certificates_file':   ensure => absent; }
+    glance_cache_config { 'DEFAULT/cinder_ca_certificates_file':      ensure => absent; }
   }
 
 }
