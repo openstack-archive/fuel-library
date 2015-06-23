0118861e4a308d3947c8d953ea92951cee179a4c
vsaienko <vsaienko@mirantis.com>
2015-06-23 17:42:13 +0300

    Adapt synced keystone module
      * wrap mysql-related things to if .. else
        to temporary provide compatibility with old mysql module
      * add new parameters:
        public_url, admin_url, internal_url, mysql_module, token_caching
        cache_backend, revoke_driver
      * replase old parameters with new:
        idle_timeout -> database_idle_timeout
      * installing python-memcache has been moved to module
      * added noop tests for new variables
      * fix pep8 errors
    
    Partially Implements: blueprint upgrade-openstack-puppet-modules
    Change-Id: I33cbabd5e623fc9920107a3f8c2258bd7e47346e

--- deployment/puppet/keystone/files/httpd/keystone.py
+++ deployment/puppet/keystone/files/httpd/keystone.py
@@ -13,7 +13,8 @@
 #    under the License.
 
 #
-# This file was copied from https://github.com/openstack/keystone/raw/0b676730347c76c7f64a67c1ad0135663e99c4fc/httpd/keystone.py
+# This file was copied from
+# https://github.com/openstack/keystone/raw/0b676730347c76c7f64a67c1ad0135663e99c4fc/httpd/keystone.py
 # It's only required for platforms on which it is not packaged yet.
 # It should be removed when available everywhere in a package.
 #
