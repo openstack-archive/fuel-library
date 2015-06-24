d88975c4c219f46a939c917c44180938d9e6d1c8
Maksym Yatsenko <myatsenko@mirantis.com>
2015-06-24 18:05:58 +0300

    Adapt synced Cinder module for Fuel usage
    Key moments:
      *Workaround for openstacklib module
      *Workaround for mysql module
      *Update section for cinder_config
      *Remove duplicated params for cinder config
      *Add "host_access.pp" file and allowed hosts check
    
    Partially Implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: Icd537cb4f817fa0e4b2e2ac32eab531dfda6ac1c

--- tests/noop/spec/hosts/openstack-cinder/openstack-cinder_spec.rb
+++ tests/noop/spec/hosts/openstack-cinder/openstack-cinder_spec.rb
@@ -3,6 +3,33 @@ require 'shared-examples'
 manifest = 'openstack-cinder/openstack-cinder.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+
+max_pool_size = 20
+max_retries = '-1'
+max_overflow = 20
+
+if ! (Noop.hiera 'queue_provider')
+  queue_provider = 'rabbitmq'
+end
+
+if queue_provider
+it 'ensures cinder_config contains needed resources before cinder-* services run' do
+  should contain_cinder_config('oslo_messaging_rabbit/rabbit_ha_queues').with(
+   :before => ['Service[cinder-api]','Service[cinder-volume]','Service[cinder-scheduler]'],
+ )
+end
+end
+
+it 'should declare ::cinder class with correct database_max_* parameters' do
+  should contain_class('cinder').with(
+    'database_max_pool_size' => max_pool_size,
+    'database_max_retries'   => max_retries,
+    'database_max_overflow'  => max_overflow,
+  )
+  end
+  end # end of shared_examples
   test_ubuntu_and_centos manifest
+
 end
 
