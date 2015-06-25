42c00403d0a8395ea72727ffd9b9af215cd659a7
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-06-25 15:29:41 +0300

    Revise of ns-related OCF scripts
    * Introduce new ocf IPnamespace that manages
      namespaces and host ip/routing inside it
    * Change haproxy/vrouter pacemaker-related stuff
    * Change dns/ntp pacemaker-related stuff
    * Add noop tests
    
    Change-Id: I8b7540d6f1c5dc2300917c2c234bb54891d5f21e
    Closes-Bug: 1459678

--- tests/noop/spec/hosts/ntp/ntp-server_spec.rb
+++ tests/noop/spec/hosts/ntp/ntp-server_spec.rb
@@ -3,6 +3,31 @@ require 'shared-examples'
 manifest = 'ntp/ntp-server.pp'
 
 describe manifest do
+
+  shared_examples 'catalog' do
+    let(:role) do
+      Noop.hiera 'role'
+    end
+
+    let(:ntp_servers) do
+      Noop.hiera 'external_ntp'
+    end
+
+    let(:servers) do
+      ntp_servers['ntp_list'].split(',').compact.collect(&:strip)
+    end
+
+    it "should contain class ntp" do
+      should contain_class('ntp').with(
+        'servers' => servers,
+      ).that_comes_before('Class[cluster::ntp_ocf]')
+    end
+
+    it "should contain class cluster::ntp_ocf" do
+      should contain_class('cluster::ntp_ocf')
+    end
+  end
+
   test_ubuntu_and_centos manifest
 end
 
