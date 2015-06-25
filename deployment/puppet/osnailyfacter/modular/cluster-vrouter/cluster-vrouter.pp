193bc1722f5050b0d0c391d2957faf641e4b4322
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-06-25 15:29:41 +0300

    Revise of ns-related OCF scripts
    * Introduce new ocf IPnamespace that manages
      namespaces and host ip/routing inside it
    * Change haproxy/vrouter pacemaker-related stuff
    * Change dns/ntp pacemaker-related stuff
    * Add noop tests
    
    Change-Id: I8b7540d6f1c5dc2300917c2c234bb54891d5f21e
    Closes-Bug: 1459678

--- deployment/puppet/osnailyfacter/modular/cluster-vrouter/cluster-vrouter.pp
+++ deployment/puppet/osnailyfacter/modular/cluster-vrouter/cluster-vrouter.pp
@@ -2,7 +2,11 @@ notice('MODULAR: cluster-vrouter.pp')
 
 $network_scheme = hiera('network_scheme', {})
 
-class { 'cluster::vrouter_ocf':
-  primary_controller => hiera('primary_controller'),
-  other_networks     => direct_networks($network_scheme['endpoints']),
+cluster::namespace_ocf { 'vrouter':
+  primary_controller  => hiera('primary_controller'),
+  host_interface      => 'vrouter-host',
+  namespace_interface => 'vr-ns',
+  host_ip             => '240.0.0.5',
+  namespace_ip        => '240.0.0.6',
+  other_networks      => direct_networks($network_scheme['endpoints']),
 }
--- tests/noop/spec/hosts/ntp/ntp-client_spec.rb
+++ tests/noop/spec/hosts/ntp/ntp-client_spec.rb
@@ -3,6 +3,37 @@ require 'shared-examples'
 manifest = 'ntp/ntp-client.pp'
 
 describe manifest do
+
+  shared_examples 'catalog' do
+    let(:role) do
+      Noop.hiera 'role'
+    end
+
+    let(:management_vip) do
+      Noop.hiera 'management_vrouter_vip'
+    end
+
+    let(:is_controller?) do
+      %w(controller primary-controller).include? role
+    end
+
+    it 'does not try to setup the ntp server on a controller or a primary controller' do
+      expect(subject).not_to contain_class('Ntp') if is_controller?
+    end
+
+    it 'setups the ntp service on the non-controller nodes' do
+      expect(subject).to contain_class('Ntp') unless is_controller?
+    end
+
+    it "ntp service should sync time from the management_vip" do
+      next true if is_controller?
+
+      should contain_class('ntp').with(
+        'servers' => management_vip,
+      )
+    end
+  end
+
   test_ubuntu_and_centos manifest
 end
 
