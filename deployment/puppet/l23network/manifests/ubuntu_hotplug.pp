b14748f88eaf945f1738a845ecca82d5156d9799
Stanislav Makar <smakar@mirantis.com>
2015-06-25 22:27:23 +0000

    Disable hotplug for interfaces
    Ubuntu has the job network-interface which ups interface automatically
    when it appears in system. During puppet run we do not manage
    interface configurations completely. It leads to not understandable
    errors and hence long-time debugging.
    
    Change-Id: I9f3a7a92c1cdc73bc145cd95af2fa9defa9a6efc
    Closes-bug: #1457419

---
+++ deployment/puppet/l23network/manifests/ubuntu_hotplug.pp
@@ -0,0 +1,35 @@
+# Class l23network::ubuntu_hotplug disables the hotplug feature
+# for network interfaces on Ubuntu OS which uses Upstart
+# initialisation system
+
+class l23network::ubuntu_hotplug (
+  $enable = false,
+)
+{
+  Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
+
+  if !$enable {
+    $interface_array = split($::upstart_network_interface_instances, ',')
+
+    # Stop network-interface job instance for every interface due to we can
+    # not disable network-interface job if at least one instance exists
+    l23network::ubuntu_hotplug::network_interface { $interface_array: }
+
+    # Disable network-inreface job
+    exec {'disable-hotplug':
+      command => 'mv /etc/init/network-interface.conf /etc/init/network-interface.disable',
+      onlyif  => 'test -e /etc/init/network-interface.conf'
+    }
+
+    # Make config for loopback interface
+    l23_stored_config { 'lo':
+      ensure => 'present',
+      ipaddr => '127.0.0.1/8',
+      method => 'static',
+      onboot => true,
+    }
+
+    L23network::Ubuntu_hotplug::Network_interface<||> -> Exec['disable-hotplug'] -> L23_stored_config['lo']
+
+  }
+}
