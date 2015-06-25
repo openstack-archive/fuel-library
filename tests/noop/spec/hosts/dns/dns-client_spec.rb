23a4564bf9c699d480d483b7d873c7ed4cb37104
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-06-25 15:29:41 +0300

    Revise of ns-related OCF scripts
    * Introduce new ocf IPnamespace that manages
      namespaces and host ip/routing inside it
    * Change haproxy/vrouter pacemaker-related stuff
    * Change dns/ntp pacemaker-related stuff
    * Add noop tests
    
    Change-Id: I8b7540d6f1c5dc2300917c2c234bb54891d5f21e
    Closes-Bug: 1459678

--- tests/noop/spec/hosts/dns/dns-client_spec.rb
+++ tests/noop/spec/hosts/dns/dns-client_spec.rb
@@ -3,6 +3,18 @@ require 'shared-examples'
 manifest = 'dns/dns-client.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+
+    management_vip = Noop.hiera 'management_vrouter_vip'
+
+    it "should contain osnailyfacter::resolvconf" do
+      should contain_class('osnailyfacter::resolvconf').with(
+        'management_vip' => management_vip,
+      )
+    end
+
+  end
+
   test_ubuntu_and_centos manifest
 end
 
