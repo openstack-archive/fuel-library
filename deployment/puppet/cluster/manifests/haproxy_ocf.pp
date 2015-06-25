268e3b0191b37645e5fc79c1c14c0b31db05cb05
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-06-25 15:29:41 +0300

    Revise of ns-related OCF scripts
    Change-Id: I8b7540d6f1c5dc2300917c2c234bb54891d5f21e
    Closes-Bug: 1459678

--- deployment/puppet/cluster/manifests/haproxy_ocf.pp
+++ deployment/puppet/cluster/manifests/haproxy_ocf.pp
@@ -11,16 +11,6 @@ class cluster::haproxy_ocf (
 
   $service_name = 'p_haproxy'
 
-  #file {'haproxy-ocf':
-  #  path   =>'/usr/lib/ocf/resource.d/fuel/ns_haproxy',
-  #  mode   => '0755',
-  #  owner  => root,
-  #   group  => root,
-  #   source => 'puppet:///modules/cluster/ocf/ns_haproxy',
-  #  }
-  #Anchor['haproxy'] -> File['haproxy-ocf']
-  #File<| title == 'ocf-fuel-path' |> -> File['haproxy-ocf']
-
   if $primary_controller {
     cs_resource { $service_name:
       ensure          => present,
@@ -70,12 +60,18 @@ class cluster::haproxy_ocf (
           "clone_${service_name}"
       ],
     }
+    cs_rsc_colocation { 'haproxy-with-ns':
+      ensure     => present,
+      score      => 'INFINITY',
+      primitives => [
+          "clone_${service_name}",
+          'clone_p_ns_haproxy'
+      ],
+    }
 
-    #    File['haproxy-ocf'] -> Cs_resource[$service_name]
     Cs_resource[$service_name] -> Cs_rsc_colocation['vip_public-with-haproxy'] -> Service[$service_name]
     Cs_resource[$service_name] -> Cs_rsc_colocation['vip_management-with-haproxy'] -> Service[$service_name]
-    #} else {
-    # File['haproxy-ocf'] -> Service[$service_name]
+    Cs_resource[$service_name] -> Cs_rsc_colocation['haproxy-with-ns'] -> Service[$service_name]
   }
 
   if ($::osfamily == 'Debian') {
