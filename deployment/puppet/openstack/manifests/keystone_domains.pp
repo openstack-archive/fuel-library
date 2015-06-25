4607a69be01e4a22bc3f8095c3b696b826a7a86c
Mykyta Koshykov <mk455v@att.com>
2015-06-25 10:35:23 -0500

    Added multi-domain backend driver if ldap enabled
    token_provider can be specified - keystone[token_provider].
    
    Partial blueprint: detach-components-from-controllers
    
    Co-Authored-By: Andrey Kirilochkin <akirilochkin@mirantis.com>
    
    Change-Id: Idd01214ace10d1b5efb578208b007047cfa5f34e

---
+++ deployment/puppet/openstack/manifests/keystone_domains.pp
@@ -0,0 +1,11 @@
+define openstack::keystone_domains ($domain_driver) {
+  $contents = "[identity]\ndriver = ${domain_driver}\n"
+  file { "/etc/keystone/domains/keystone.${title}.conf":
+    ensure  => file,
+    content => $contents,
+    require => File['/etc/keystone/domains'],
+  }
+  file {'/etc/keystone/domains':
+    ensure  => directory,
+  }
+}
