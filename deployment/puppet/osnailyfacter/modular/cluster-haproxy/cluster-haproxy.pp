eef3bf1bdcf3c2105c35183c7895ffc7dee468c7
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-06-25 15:29:41 +0300

    Revise of ns-related OCF scripts
    * Introduce new ocf IPnamespace that manages
      namespaces and host ip/routing inside it
    * Change haproxy/vrouter pacemaker-related stuff
    * Change dns/ntp pacemaker-related stuff
    * Add noop tests
    
    Change-Id: I8b7540d6f1c5dc2300917c2c234bb54891d5f21e
    Closes-Bug: 1459678

--- deployment/puppet/osnailyfacter/modular/cluster-haproxy/cluster-haproxy.pp
+++ deployment/puppet/osnailyfacter/modular/cluster-haproxy/cluster-haproxy.pp
@@ -9,6 +9,15 @@ $primary_controller = hiera('primary_controller')
 #FIXME(mattymo): Replace with only VIPs for roles assigned to this node
 $stats_ipaddresses          = delete_undef_values([$management_vip, $database_vip, $service_endpoint, '127.0.0.1'])
 
+cluster::namespace_ocf { 'haproxy':
+  primary_controller  => hiera('primary_controller'),
+  host_interface      => 'hapr-host',
+  namespace_interface => 'hapr-ns',
+  host_ip             => '240.0.0.1',
+  namespace_ip        => '240.0.0.2',
+  other_networks      => direct_networks($network_scheme['endpoints']),
+} ->
+
 class { 'cluster::haproxy':
   haproxy_maxconn    => '16000',
   haproxy_bufsize    => '32768',
@@ -17,3 +26,4 @@ class { 'cluster::haproxy':
   other_networks     => direct_networks($network_scheme['endpoints']),
   stats_ipaddresses  => $stats_ipaddresses
 }
+
