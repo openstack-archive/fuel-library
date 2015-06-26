1f5b12fe6f2948dfe42045e507bba6b338b78955
Denis Egorenko <degorenko@mirantis.com>
2015-06-26 18:09:57 +0300

    Change old sql_connection to new database_connection
    Change-Id: Ief2e2d3d748dcf0bd77ac5b13b5ca56ab8befb42
    Closes-bug: #1469201

--- deployment/puppet/nailgun/examples/keystone-only.pp
+++ deployment/puppet/nailgun/examples/keystone-only.pp
@@ -20,7 +20,7 @@ case $production {
     class { 'keystone':
       admin_token      => $::fuel_settings['keystone']['admin_token'],
       catalog_type     => 'sql',
-      sql_connection   => "postgresql://${::fuel_settings['postgres']['keystone_user']}:${::fuel_settings['postgres']['keystone_password']}@${::fuel_settings['ADMIN_NETWORK']['ipaddress']}/${::fuel_settings['postgres']['keystone_dbname']}",
+      datebase_connection   => "postgresql://${::fuel_settings['postgres']['keystone_user']}:${::fuel_settings['postgres']['keystone_password']}@${::fuel_settings['ADMIN_NETWORK']['ipaddress']}/${::fuel_settings['postgres']['keystone_dbname']}",
       token_expiration => 86400,
       token_provider   => 'keystone.token.providers.uuid.Provider',
     }
