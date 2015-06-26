ff98d7d6595b37b1f84d69b0ce29dffbc2f7d70a
Stanislav Makar <smakar@mirantis.com>
2015-06-26 15:12:00 +0000

    Adapt Cinder to advanced networking
    * Use *cinder/api* network role
    * Use *cinder/iscsi* network role
    * Some noop tests fixes
    * Clean up not needed part
    
    Implements: blueprint templates-for-networking
    Change-Id: I6c957d6599d0ee028415f9813f2b54a6fdbcdf94

--- tests/noop/spec/hosts/roles/cinder_spec.rb
+++ tests/noop/spec/hosts/roles/cinder_spec.rb
@@ -3,6 +3,18 @@ require 'shared-examples'
 manifest = 'roles/cinder.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+
+  storage_hash = Noop.hiera 'storage'
+
+  if Noop.hiera 'use_ceph' and !(storage_hash['volumes_lvm'])
+      it { should contain_class('ceph') }
+  end
+
+  it { should contain_package('python-amqp') }
+  it { should contain_class('openstack::cinder') }
+
+  end
   test_ubuntu_and_centos manifest
 end
 
