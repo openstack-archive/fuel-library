5364233df80fc34fcb804629fd27a8d09eab3144
Mykola Golub <mgolub@mirantis.com>
2015-06-30 11:05:12 +0300

    Don't scan /var/lib/ceph with updatedb
    /var/lib/ceph usually contains a big set of files and significant IO
    slowdown may be observed during indexing, which actually is useless.
    
    Closes-Bug: #1469993
    Change-Id: I6995334867ba486ad036d1fd32492cf78e6a9ce1

---
+++ deployment/puppet/osnailyfacter/modular/ceph/updatedb.pp
@@ -0,0 +1,21 @@
+notice('MODULAR: ceph/updatedb.pp')
+
+$storage_hash = hiera('storage', {})
+
+if ($storage_hash['volumes_ceph'] or
+  $storage_hash['images_ceph'] or
+  $storage_hash['objects_ceph']
+) {
+  $use_ceph = true
+} else {
+  $use_ceph = false
+}
+
+if $use_ceph {
+
+  exec {"Ensure /var/lib/ceph in the updatedb PRUNEPATH":
+    path    => [ '/usr/bin', '/bin' ],
+    command => "sed -i -Ee 's|(PRUNEPATHS *= *\"[^\"]*)|\\1 /var/lib/ceph|' /etc/updatedb.conf",
+    unless  => "test ! -f /etc/updatedb.conf || grep 'PRUNEPATHS *= *.*/var/lib/ceph.*' /etc/updatedb.conf",
+  }
+}
