e00d085286947f5eec5ad28c2cebf0b6ae861be4
Mykyta Koshykov <mk455v@att.com>
2015-06-30 14:13:45 +0200

    Split keystone endpoint creation into respective tasks
    - Refactor ceilometer auth.pp. It's a part of
      https://review.openstack.org/#/c/201565/ though it's required by
      endpoint task separation
    - Moved keystone endpoint creations to separate tasks with initial noop
      tests
    
    Misc:
    - Created Unified structure in creation tasks. They are separated as
      <task>/db.pp and <task>/keystone.pp
    - Removed firewall.pp.dist.
    - Removed database requirement in ceilometer/tasks.yaml
    - Fixed notification in heat.pp
    - many linting fixes
    
    Partial blueprint: detach-components-from-controllers
    
    Co-Authored-By: Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
    Co-Authored-By: Sergii Golovatiuk <sgolovatiuk@mirantis.com>
    Co-Authored-By: Alex Schultz <aschultz@mirantis.com>
    
    Change-Id: I61376f2bbeade23ae1d49a82f4e908be28144e78
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

---
+++ deployment/puppet/osnailyfacter/modular/openstack-network/keystone.pp
@@ -0,0 +1,30 @@
+notice('MODULAR: openstack-network/keystone.pp')
+
+$neutron_hash     = hiera_hash('quantum_settings', {})
+$public_address   = hiera('public_vip')
+$internal_address = hiera('management_vip', $public_address)
+$region           = pick($neutron_hash['region'], 'RegionOne')
+
+$password            = $neutron_hash['keystone']['admin_password']
+$auth_name           = pick($neutron_hash['auth_name'], 'neutron')
+$configure_endpoint  = pick($neutron_hash['configure_endpoint'], true)
+$configure_user      = pick($neutron_hash['configure_user'], true)
+$configure_user_role = pick($neutron_hash['configure_user_role'], true)
+$service_name        = pick($neutron_hash['service_name'], $auth_name)
+$tenant              = pick($neutron_hash['tenant'], 'services')
+
+validate_string($public_address)
+validate_string($password)
+
+class { '::neutron::keystone::auth':
+  password            => $password,
+  auth_name           => $auth_name,
+  configure_endpoint  => $configure_endpoint,
+  configure_user      => $configure_user,
+  configure_user_role => $configure_user_role,
+  service_name        => $service_name,
+  public_address      => $public_address,
+  admin_address       => $internal_address,
+  internal_address    => $internal_address,
+  region              => $region,
+}
---
+++ deployment/puppet/osnailyfacter/modular/swift/keystone.pp
@@ -0,0 +1,32 @@
+notice('MODULAR: swift/keystone.pp')
+
+$swift_hash       = hiera_hash('swift', {})
+$public_address   = hiera('public_vip')
+$internal_address = hiera('management_vip', $public_address)
+$region           = pick($swift_hash['region'], 'RegionOne')
+$public_ssl_hash  = hiera('public_ssl')
+$public_protocol  = $public_ssl_hash['services'] ? {
+  true    => 'https',
+  default => 'http',
+}
+
+$password            = $swift_hash['user_password']
+$auth_name           = pick($swift_hash['auth_name'], 'swift')
+$configure_endpoint  = pick($swift_hash['configure_endpoint'], true)
+$service_name        = pick($swift_hash['service_name'], $auth_name)
+$tenant              = pick($swift_hash['tenant'], 'services')
+
+validate_string($public_address)
+validate_string($password)
+
+class { '::swift::keystone::auth':
+  password           => $password,
+  auth_name          => $auth_name,
+  configure_endpoint => $configure_endpoint,
+  service_name       => $service_name,
+  public_address     => $public_address,
+  public_protocol    => $public_protocol,
+  admin_address      => $internal_address,
+  internal_address   => $internal_address,
+  region             => $region,
+}
