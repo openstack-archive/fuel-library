112b350024556f86b653d48809b60f70511d6a09
vsaienko <vsaienko@mirantis.com>
2015-06-30 11:07:55 +0300

    Sync horizon module from upstream
      master: 5883fea38719c4fd37fd1f39c3a2296f97d7d3fb
      source: https://github.com/openstack/puppet-horizon
    
    Related blueprint upgrade-openstack-puppet-modules
    Fuel-CI: disable
    
    Change-Id: Ie23e1a17432a86c3b0f35d725e5c1797559bee6d

--- deployment/puppet/horizon/spec/classes/horizon_init_spec.rb
+++ deployment/puppet/horizon/spec/classes/horizon_init_spec.rb
@@ -25,17 +25,20 @@ describe 'horizon' do
 
     context 'with default parameters' do
       it {
-          should contain_package('python-lesscpy').with_ensure('present')
-          should contain_package('horizon').with_ensure('present')
+          is_expected.to contain_package('python-lesscpy').with_ensure('present')
+          is_expected.to contain_package('horizon').with(
+            :ensure => 'present',
+            :tag    => 'openstack'
+          )
       }
-      it { should contain_exec('refresh_horizon_django_cache').with({
-          :command     => '/usr/share/openstack-dashboard/manage.py compress',
+      it { is_expected.to contain_exec('refresh_horizon_django_cache').with({
+          :command     => '/usr/share/openstack-dashboard/manage.py collectstatic --noinput --clear && /usr/share/openstack-dashboard/manage.py compress --force',
           :refreshonly => true,
       })}
-      it { should contain_file(platforms_params[:config_file]).that_notifies('Exec[refresh_horizon_django_cache]') }
+      it { is_expected.to contain_concat(platforms_params[:config_file]).that_notifies('Exec[refresh_horizon_django_cache]') }
 
       it 'configures apache' do
-        should contain_class('horizon::wsgi::apache').with({
+        is_expected.to contain_class('horizon::wsgi::apache').with({
           :servername   => 'some.host.tld',
           :listen_ssl   => false,
           :servername   => 'some.host.tld',
@@ -44,7 +47,7 @@ describe 'horizon' do
       end
 
       it 'generates local_settings.py' do
-        verify_contents(subject, platforms_params[:config_file], [
+        verify_concat_fragment_contents(catalogue, 'local_settings.py', [
           'DEBUG = False',
           "ALLOWED_HOSTS = ['*', ]",
           "SECRET_KEY = 'elj1IWiLoWHgcyYxFVLj7cM5rGOOxWl0'",
@@ -57,6 +60,8 @@ describe 'horizon' do
           "    'enable_quotas': True,",
           "    'enable_security_group': True,",
           "    'enable_vpn': False,",
+          "    'enable_distributed_router': False,",
+          "    'enable_ha_router': False,",
           'API_RESULT_LIMIT = 1000',
           "LOGIN_URL = '#{platforms_params[:root_url]}/auth/login/'",
           "LOGOUT_URL = '#{platforms_params[:root_url]}/auth/logout/'",
@@ -66,13 +71,13 @@ describe 'horizon' do
         ])
 
         # From internals of verify_contents, get the contents to check for absence of a line
-        content = subject.resource('file', platforms_params[:config_file]).send(:parameters)[:content]
+        content = catalogue.resource('concat::fragment', 'local_settings.py').send(:parameters)[:content]
 
         # With default options, should _not_ have a line to configure SESSION_ENGINE
-        content.should_not match(/^SESSION_ENGINE/)
+        expect(content).not_to match(/^SESSION_ENGINE/)
       end
 
-      it { should_not contain_file(params[:file_upload_temp_dir]) }
+      it { is_expected.not_to contain_file(params[:file_upload_temp_dir]) }
     end
 
     context 'with overridden parameters' do
@@ -82,20 +87,24 @@ describe 'horizon' do
           :django_session_engine   => 'django.contrib.sessions.backends.cache',
           :keystone_default_role   => 'SwiftOperator',
           :keystone_url            => 'https://keystone.example.com:4682',
+          :log_handler             => 'syslog',
+          :log_level               => 'DEBUG',
           :openstack_endpoint_type => 'internalURL',
           :secondary_endpoint_type => 'ANY-VALUE',
           :django_debug            => true,
           :api_result_limit        => 4682,
           :compress_offline        => false,
           :hypervisor_options      => {'can_set_mount_point' => false, 'can_set_password' => true },
-          :neutron_options         => {'enable_lb' => true, 'enable_firewall' => true, 'enable_quotas' => false, 'enable_security_group' => false, 'enable_vpn' => true, 'profile_support' => 'cisco' },
+          :cinder_options          => {'enable_backup' => true },
+          :neutron_options         => {'enable_lb' => true, 'enable_firewall' => true, 'enable_quotas' => false, 'enable_security_group' => false, 'enable_vpn' => true,
+                                       'enable_distributed_router' => false, 'enable_ha_router' => false, 'profile_support' => 'cisco', },
           :file_upload_temp_dir    => '/var/spool/horizon',
           :secure_cookies          => true
         })
       end
 
       it 'generates local_settings.py' do
-        verify_contents(subject, platforms_params[:config_file], [
+        verify_concat_fragment_contents(catalogue, 'local_settings.py', [
           'DEBUG = True',
           "ALLOWED_HOSTS = ['*', ]",
           'CSRF_COOKIE_SECURE = True',
@@ -107,6 +116,7 @@ describe 'horizon' do
           'OPENSTACK_KEYSTONE_DEFAULT_ROLE = "SwiftOperator"',
           "    'can_set_mount_point': False,",
           "    'can_set_password': True,",
+          "    'enable_backup': True,",
           "    'enable_lb': True,",
           "    'enable_firewall': True,",
           "    'enable_quotas': False,",
@@ -116,14 +126,16 @@ describe 'horizon' do
           'OPENSTACK_ENDPOINT_TYPE = "internalURL"',
           'SECONDARY_ENDPOINT_TYPE = "ANY-VALUE"',
           'API_RESULT_LIMIT = 4682',
+          "            'level': 'DEBUG',",
+          "            'handlers': ['syslog'],",
           'COMPRESS_OFFLINE = False',
           "FILE_UPLOAD_TEMP_DIR = '/var/spool/horizon'"
         ])
       end
 
-      it { should_not contain_file(platforms_params[:config_file]).that_notifies('Exec[refresh_horizon_django_cache]') }
+      it { is_expected.not_to contain_file(platforms_params[:config_file]).that_notifies('Exec[refresh_horizon_django_cache]') }
 
-      it { should contain_file(params[:file_upload_temp_dir]) }
+      it { is_expected.to contain_file(params[:file_upload_temp_dir]) }
     end
 
     context 'with overridden parameters and cache_server_ip array' do
@@ -134,31 +146,41 @@ describe 'horizon' do
       end
 
       it 'generates local_settings.py' do
-        verify_contents(subject, platforms_params[:config_file], [
+        verify_concat_fragment_contents(catalogue, 'local_settings.py', [
           "        'LOCATION': [ '10.0.0.1:11211','10.0.0.2:11211', ],",
         ])
       end
 
-      it { should contain_exec('refresh_horizon_django_cache') }
+      it { is_expected.to contain_exec('refresh_horizon_django_cache') }
     end
 
-    context 'with deprecated parameters' do
+    context 'with tuskar-ui enabled' do
       before do
         params.merge!({
-          :keystone_host       => 'keystone.example.com',
-          :keystone_port       => 4682,
-          :keystone_scheme     => 'https',
-          :can_set_mount_point => true,
+          :tuskar_ui => true,
+          :tuskar_ui_ironic_discoverd_url      => 'http://127.0.0.1:5050',
+          :tuskar_ui_undercloud_admin_password => 'somesecretpassword',
+          :tuskar_ui_deployment_mode           => 'scale',
         })
       end
 
       it 'generates local_settings.py' do
-        verify_contents(subject, platforms_params[:config_file], [
-          'OPENSTACK_KEYSTONE_URL = "https://keystone.example.com:4682/v2.0"',
-          "    'can_set_mount_point': True,"
+        verify_concat_fragment_contents(catalogue, 'local_settings.py', [
+          'IRONIC_DISCOVERD_URL = "http://127.0.0.1:5050"',
+          'UNDERCLOUD_ADMIN_PASSWORD = "somesecretpassword"',
+          'DEPLOYMENT_MODE = "scale"',
         ])
       end
     end
+
+    context 'with wrong tuskar_ui_deployment_mode parameter value' do
+      before do
+        params.merge!({
+          :tuskar_ui_deployment_mode => 'wrong' })
+      end
+      it_raises 'a Puppet::Error', /'wrong' is not correct value for tuskar_ui_deployment_mode parameter. It must be either 'scale' or 'poc'./
+    end
+
 
     context 'with vhost_extra_params' do
       before do
@@ -168,7 +190,7 @@ describe 'horizon' do
       end
 
       it 'configures apache' do
-        should contain_class('horizon::wsgi::apache').with({
+        is_expected.to contain_class('horizon::wsgi::apache').with({
           :extra_params => { 'add_listen' => false },
         })
       end
@@ -187,7 +209,7 @@ describe 'horizon' do
       end
 
       it 'configures apache' do
-        should contain_class('horizon::wsgi::apache').with({
+        is_expected.to contain_class('horizon::wsgi::apache').with({
           :bind_address => nil,
           :listen_ssl   => true,
           :horizon_cert => '/etc/pki/tls/certs/httpd.crt',
@@ -203,7 +225,7 @@ describe 'horizon' do
       end
 
       it 'does not configure apache' do
-        should_not contain_class('horizon::wsgi::apache')
+        is_expected.not_to contain_class('horizon::wsgi::apache')
       end
     end
 
@@ -218,7 +240,7 @@ describe 'horizon' do
       end
 
       it 'AVAILABLE_REGIONS is configured' do
-        verify_contents(subject, platforms_params[:config_file], [
+        verify_concat_fragment_contents(catalogue, 'local_settings.py', [
           "AVAILABLE_REGIONS = [",
           "    ('http://region-1.example.com:5000/v2.0', 'Region-1'),",
           "    ('http://region-2.example.com:5000/v2.0', 'Region-2'),",
@@ -232,19 +254,19 @@ describe 'horizon' do
         params.merge!({
           :policy_files_path => '/opt/openstack-dashboard',
           :policy_files      => {
-            'identity' => 'keystone_policy.json',
             'compute'  => 'nova_policy.json',
+            'identity' => 'keystone_policy.json',
             'network'  => 'neutron_policy.json',
           }
         })
       end
 
       it 'POLICY_FILES_PATH and POLICY_FILES are configured' do
-        verify_contents(subject, platforms_params[:config_file], [
+        verify_concat_fragment_contents(catalogue, 'local_settings.py', [
           "POLICY_FILES_PATH = '/opt/openstack-dashboard'",
           "POLICY_FILES = {",
-          "    'identity': 'keystone_policy.json',",
           "    'compute': 'nova_policy.json',",
+          "    'identity': 'keystone_policy.json',",
           "    'network': 'neutron_policy.json',",
           "} # POLICY_FILES"
         ])
@@ -261,7 +283,7 @@ describe 'horizon' do
       end
 
       it 'uses the custom local_settings.py template' do
-        verify_contents(subject, platforms_params[:config_file], [
+        verify_concat_fragment_contents(catalogue, 'local_settings.py', [
           '# Custom local_settings.py',
           'DEBUG = True',
           "HORIZON_CONFIG = {",
@@ -290,7 +312,7 @@ describe 'horizon' do
         })
       end
 
-      it { should_not contain_file(params[:file_upload_temp_dir]) }
+      it { is_expected.not_to contain_file(params[:file_upload_temp_dir]) }
     end
   end
 
@@ -309,6 +331,12 @@ describe 'horizon' do
     end
 
     it_behaves_like 'horizon'
+
+    it 'sets WEBROOT in local_settings.py' do
+      verify_concat_fragment_contents(catalogue, 'local_settings.py', [
+        "WEBROOT = '/dashboard/'",
+      ])
+    end
   end
 
   context 'on Debian platforms' do
@@ -326,5 +354,11 @@ describe 'horizon' do
     end
 
     it_behaves_like 'horizon'
+
+    it 'sets WEBROOT in local_settings.py' do
+      verify_concat_fragment_contents(catalogue, 'local_settings.py', [
+        "WEBROOT = '/horizon/'",
+      ])
+    end
   end
 end
