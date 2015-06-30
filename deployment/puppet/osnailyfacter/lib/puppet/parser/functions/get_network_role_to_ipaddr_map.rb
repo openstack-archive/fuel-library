c9265ba511071676afba9b5de2ce3a68f818e212
Sergey Vasilenko <svasilenko@mirantis.com>
2015-06-30 15:13:27 +0300

    Add new parser function: get_network_role_to_ipaddr_map()
    blueprint: templates-for-networking
    
    Change-Id: Id6e7e830ac0ff6822e7f3d7a58517c987b705ed4

---
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/get_network_role_to_ipaddr_map.rb
@@ -0,0 +1,24 @@
+module Puppet::Parser::Functions
+  newfunction(:get_network_role_to_ipaddr_map, :type => :rvalue, :doc => <<-EOS
+Return a hash of nodes (node names are keys) that have one of the given roles.
+example:
+  get_network_role_to_ipaddr_map($nodes_hash, 'role')
+EOS
+  ) do |args|
+    errmsg = "get_network_role_to_ipaddr_map($nodes_hash, 'role')"
+    nodes, role = args
+    raise(Puppet::ParseError, "#{errmsg}: 1st argument should be a hash") if !nodes.is_a?(Hash)
+    raise(Puppet::ParseError, "#{errmsg}: 2nd argument should be an a network-role name") if !role.is_a?(String)
+    nodes = nodes['nodes'] if nodes.has_key?('nodes')
+    rv = {}
+    nodes.each do |node_name, node_props|
+      next if ! node_props.is_a?(Hash)
+      n = node_props['network_roles']
+      next if ! n.is_a?(Hash)
+      rv[node_name] = n[role] if n[role] != nil
+    end
+    return rv
+  end
+end
+
+# vim: set ts=2 sw=2 et :
