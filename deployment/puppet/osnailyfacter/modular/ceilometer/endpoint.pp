f48e5b90757261cdd02ca1d2da61a174f0ba046f
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-06-30 14:13:45 +0200

    (WIP) Split keystone endpoint creation into split tasks
    Partial blueprint: detach-components-from-controllers
    Change-Id: I61376f2bbeade23ae1d49a82f4e908be28144e78

---
+++ deployment/puppet/osnailyfacter/modular/ceilometer/endpoint.pp
@@ -0,0 +1,34 @@
+notice('MODULAR: ceilometer/endpoint.pp')
+
+$ceilometer_hash  = hiera_hash('ceilometer', {})
+$public_vip       = hiera('public_vip', undef)
+$internal_address = hiera('internal_address', undef)
+$management_vip   = hiera('management_vip', undef)
+$region           = hiera('region', 'RegionOne')
+
+$admin_address             = $management_vip
+$ceilometer_public_address = $public_vip
+$ceilometer_user_password  = $ceilometer_hash['user_password']
+$ceilometer_enabled        = $ceilometer_hash['enabled']
+
+if $internal_address {
+  $ceilometer_internal_address = $internal_address
+} else {
+  $ceilometer_internal_address = $ceilometer_public_address
+}
+
+if $admin_address {
+  $ceilometer_admin_address = $admin_address
+} else {
+  $ceilometer_admin_address = $ceilometer_internal_address
+}
+
+if $ceilometer_enabled {
+  class { 'ceilometer::keystone::auth':
+    password         => $ceilometer_user_password,
+    public_vip       => $ceilometer_public_address,
+    admin_address    => $ceilometer_admin_address,
+    internal_address => $ceilometer_internal_address,
+    region           => $region,
+  }
+}
---
+++ deployment/puppet/osnailyfacter/modular/openstack-network/openstack-network-endpoint.pp
@@ -0,0 +1,31 @@
+notice('MODULAR: openstack-network/openstack-network-endpoint.pp')
+
+$neutron_hash     = hiera_hash('quantum_settings', {})
+$public_vip       = hiera('public_vip', undef)
+$internal_address = hiera('internal_address', undef)
+$management_vip   = hiera('management_vip', undef)
+$region           = hiera('region', 'RegionOne')
+
+$admin_address          = $management_vip
+$neutron_public_address = $public_vip
+$neutron_user_password  = $neutron_hash['keystone']['admin_password']
+
+if $internal_address {
+  $neutron_internal_address = $internal_address
+} else {
+  $neutron_internal_address = $nova_public_address
+}
+
+if $admin_address {
+  $neutron_admin_address = $admin_address
+} else {
+  $neutron_admin_address = $nova_internal_address
+}
+
+class { 'neutron::keystone::auth':
+  password         => $neutron_user_password,
+  public_address   => $neutron_public_address,
+  admin_address    => $neutron_admin_address,
+  internal_address => $neutron_internal_address,
+  region           => $region,
+}
