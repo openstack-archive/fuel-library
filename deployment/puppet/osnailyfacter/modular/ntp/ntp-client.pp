082f8af137b0978b5973fccf95af8d467bef3c24
Dmitry Ilyin <dilyin@mirantis.com>
2015-07-01 18:42:02 +0300

    Make ntpd listen correct interfaces
    * Management and ns bridge on the controller
    * Management on other nodes
    * Remove client run on controllers
    * Add ubuntu service overrides for ntp
    
    Change-Id: Ic78394fddaafa82e085c55a1a0fdd3c46d4f0089

--- deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
+++ deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
@@ -1,19 +1,25 @@
 notice('MODULAR: ntp-client.pp')
 
 $management_vrouter_vip  = hiera('management_vrouter_vip')
+$internal_address        = hiera('internal_address')
 $nodes_hash              = hiera('nodes', {})
-$roles                   = node_roles($nodes_hash, hiera('uid'))
 
-if !(member($roles, 'controller') or member($roles, 'primary-controller')) {
-  class { 'ntp':
-    servers        => [$management_vrouter_vip],
-    interfaces     => ['lo'],
-    service_ensure => running,
-    service_enable => true,
-    iburst_enable  => true,
-    tinker         => true,
-    panic          => 0,
-    stepout        => 5,
-    minpoll        => 3,
+class { 'ntp':
+  servers        => [$management_vrouter_vip],
+  interfaces     => ['lo', $internal_address],
+  service_ensure => 'running',
+  service_enable => true,
+  iburst_enable  => true,
+  tinker         => true,
+  panic          => '0',
+  stepout        => '5',
+  minpoll        => '3',
+}
+
+if $::operatingsystem == 'Ubuntu' {
+  include ntp::params
+  tweaks::ubuntu_service_override { 'nova-cert':
+    package_name => $ntp::params::package_name,
+    service_name => $ntp::params::service_name,
   }
 }
--- deployment/puppet/osnailyfacter/modular/ntp/ntp-server.pp
+++ deployment/puppet/osnailyfacter/modular/ntp/ntp-server.pp
@@ -1,18 +1,26 @@
 notice('MODULAR: ntp-server.pp')
 
-$ntp_servers            = hiera('external_ntp')
+$ntp_servers = hiera('external_ntp')
 $management_vrouter_vip = hiera('management_vrouter_vip')
 
 class { 'ntp':
   servers        => strip(split($ntp_servers['ntp_list'], ',')),
-  interfaces     => [$management_vrouter_vip],
+  interfaces     => ['lo', 'vr-ns', $management_vrouter_vip],
   service_enable => false,
-  service_ensure => stopped,
+  service_ensure => 'stopped',
   iburst_enable  => true,
   tinker         => true,
-  panic          => 0,
-  stepout        => 5,
-  minpoll        => 3,
+  panic          => '0',
+  stepout        => '5',
+  minpoll        => '3',
 } ->
 
 class { 'cluster::ntp_ocf': }
+
+if $::operatingsystem == 'Ubuntu' {
+  include ntp::params
+  tweaks::ubuntu_service_override { 'nova-cert':
+    package_name => $ntp::params::package_name,
+    service_name => $ntp::params::service_name,
+  }
+}
