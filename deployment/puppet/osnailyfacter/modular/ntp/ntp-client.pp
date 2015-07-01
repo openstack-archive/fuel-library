933a51ae9d08b608f2a8969f31af5025a0683872
Dmitry Ilyin <dilyin@mirantis.com>
2015-07-01 18:42:02 +0300

    Make ntpd listen all interfaces
    * Remove ntpd listen restrictions
    * Remove client run on controllers
    * Add ubuntu service overrides for ntp
    
    TestImpact
    Change-Id: Ic78394fddaafa82e085c55a1a0fdd3c46d4f0089

--- deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
+++ deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
@@ -5,15 +5,24 @@ $nodes_hash              = hiera('nodes', {})
 $roles                   = node_roles($nodes_hash, hiera('uid'))
 
 if !(member($roles, 'controller') or member($roles, 'primary-controller')) {
+
   class { 'ntp':
     servers        => [$management_vrouter_vip],
-    interfaces     => ['lo'],
-    service_ensure => running,
+    service_ensure => 'running',
     service_enable => true,
     iburst_enable  => true,
     tinker         => true,
-    panic          => 0,
-    stepout        => 5,
-    minpoll        => 3,
+    panic          => '0',
+    stepout        => '5',
+    minpoll        => '3',
   }
+
+  if $::operatingsystem == 'Ubuntu' {
+    include ntp::params
+    tweaks::ubuntu_service_override { 'nova-cert':
+      package_name => $ntp::params::package_name,
+      service_name => $ntp::params::service_name,
+    }
+  }
+
 }
