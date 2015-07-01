7ee9b63556980aa47c5d052e41f4d51cb446fd25
Stanislav Makar <smakar@mirantis.com>
2015-07-01 09:38:22 +0000

    Clarify the role for get_nodes_hash_by_roles function
    Change-Id: I2a77d910fac66b18eb68609a9235797a1b77c93b
    Implements: blueprint templates-for-networking

--- deployment/puppet/osnailyfacter/lib/puppet/parser/functions/get_nodes_hash_by_roles.rb
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/get_nodes_hash_by_roles.rb
@@ -1,15 +1,15 @@
 module Puppet::Parser::Functions
   newfunction(:get_nodes_hash_by_roles, :type => :rvalue, :doc => <<-EOS
-Return a hash of nodes (node names are keys) that have one of the given roles.
+Return a hash of nodes (node names are keys) that have one of the given node roles.
 example:
-  get_nodes_hash_by_roles($network_metadata_hash, ['role1','role2'])
+  get_nodes_hash_by_roles($network_metadata_hash, ['node_role1','node_role2'])
 EOS
   ) do |args|
-    errmsg = "get_nodes_hash_by_roles($network_metadata_hash, ['role1','role2'])"
+    errmsg = "get_nodes_hash_by_roles($network_metadata_hash, ['node_role1','node_role2'])"
     n_metadata, roles = args
     raise(Puppet::ParseError, "#{errmsg}: 1st argument should be a hash") if !n_metadata.is_a?(Hash)
     raise(Puppet::ParseError, "#{errmsg}: 1st argument should be a valid network_metadata hash") if !n_metadata.has_key?('nodes')
-    raise(Puppet::ParseError, "#{errmsg}: 2nd argument should be an list of roles") if !roles.is_a?(Array)
+    raise(Puppet::ParseError, "#{errmsg}: 2nd argument should be an list of node roles") if !roles.is_a?(Array)
     nodes = n_metadata['nodes']
     nodes.reject do |node_name|
       (roles & nodes[node_name]['node_roles']).empty?
