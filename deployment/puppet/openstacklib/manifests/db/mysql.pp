10e27891b0f876573a8c7b45e0106409142a895e
vsaienko <vsaienko@mirantis.com>
2015-07-02 16:49:21 +0300

    Adapt synced openstacklib module
      * add mysql_module provide
        backward compatibility for mysql < 2.2
    
    Partially Implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: I5a31ecac08d30ff82abfe678b6eaad00849c317a

--- deployment/puppet/openstacklib/manifests/db/mysql.pp
+++ deployment/puppet/openstacklib/manifests/db/mysql.pp
@@ -37,6 +37,9 @@
 #    string or array of strings; optional; default to 'ALL'
 
 define openstacklib::db::mysql (
+#Temp mysql_module vars that needed untill mysql module is synced
+# do hardcode mysql_module=0.3 to avoid adding it to all modules
+  $mysql_module   = '0.3',
   $password_hash,
   $dbname         = $title,
   $user           = $title,
@@ -47,6 +50,7 @@ define openstacklib::db::mysql (
   $privileges     = 'ALL',
 ) {
 
+  if ($mysql_module >= 2.2){
   include ::mysql::client
 
   mysql_database { $dbname:
@@ -55,11 +59,23 @@ define openstacklib::db::mysql (
     collate => $collate,
     require => [ Class['mysql::server'], Class['mysql::client'] ],
   }
+  } else {
+
+    require mysql::python
+    mysql::db { $dbname:
+      user     => $user,
+      password => $password_hash,
+      host     => $host,
+      charset  => $charset,
+      require  => Class['mysql::config'],
+    }
+  }
 
   $allowed_hosts_list = unique(concat(any2array($allowed_hosts), [$host]))
   $real_allowed_hosts = prefix($allowed_hosts_list, "${dbname}_")
 
   openstacklib::db::mysql::host_access { $real_allowed_hosts:
+    mysql_module  => $mysql_module,
     user          => $user,
     password_hash => $password_hash,
     database      => $dbname,
