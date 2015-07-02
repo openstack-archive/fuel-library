a1291f31b88404cf0552bf20a04eded3fa332dd0
Dmitry Ilyin <dilyin@mirantis.com>
2015-07-02 16:13:01 +0300

    Purge unmanaged hosts records
    Change-Id: Ifa1c542ea74bc89a472b70fdbdb37a69bc74cee6
    Closes-Bug: 1467655

--- deployment/puppet/osnailyfacter/modular/hosts/hosts.pp
+++ deployment/puppet/osnailyfacter/modular/hosts/hosts.pp
@@ -1,5 +1,21 @@
 notice('MODULAR: hosts.pp')
 
+resources { 'host':
+  purge => true,
+}
+
+host { 'localhost' :
+  ensure       => 'present',
+  ip           => '127.0.0.1',
+  host_aliases => 'localhost.localdomain',
+}
+
+host { 'ipv6-localhost' :
+  ensure       => 'present',
+  ip           => '::1',
+  host_aliases => 'ip6-loopback',
+}
+
 class { "l23network::hosts_file":
   nodes => hiera('nodes'),
 }
