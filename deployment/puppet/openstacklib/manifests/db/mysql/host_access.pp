789a2a5027db888e556416120354274e68bf5590
vsaienko <vsaienko@mirantis.com>
2015-07-02 16:49:21 +0300

    Adapt synced openstacklib module
      * All this changes are temporary, they should be removed when
        mysql module is updated
      * add mysql_module parameter to provide backward compatibility
        for mysql < 2.2
      * disable rspec tests for openstacklib
      * bind default value of mysql_module to '0.3' to avoid
        changes in upstream modules
      * added noop tests for mysql databases
    
    It should fail noop tests and it's OK. It will start passing noop
    tests for Openstacklib::Db::Mysql::Host_access only when we sync
    all modules that use it with upstream.
    
    Partially Implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: I5a31ecac08d30ff82abfe678b6eaad00849c317a

--- deployment/puppet/openstacklib/manifests/db/mysql/host_access.pp
+++ deployment/puppet/openstacklib/manifests/db/mysql/host_access.pp
@@ -17,25 +17,51 @@
 #  [*privileges*]
 #    the privileges to grant to this user
 #
+#  [*mysql_module*]
+#    Temp mysql_module vars that needed until mysql module is synced
+#    do hardcode mysql_module=3.4 to avoid adding it to all modules
+#
 define openstacklib::db::mysql::host_access (
+# Temp mysql_module variable untill mysql is up to date
   $user,
   $password_hash,
   $database,
   $privileges,
+  $mysql_module = '0.3',
 ) {
   validate_re($title, '_', 'Title must be $dbname_$host')
 
   $host = inline_template('<%= @title.split("_").last %>')
 
-  mysql_user { "${user}@${host}":
-    password_hash => $password_hash,
-    require       => Mysql_database[$database],
-  }
+  if ($mysql_module >= 2.2) {
+    mysql_user { "${user}@${host}":
+      password_hash => $password_hash,
+      require       => Mysql_database[$database],
+    }
 
-  mysql_grant { "${user}@${host}/${database}.*":
-    privileges => $privileges,
-    table      => "${database}.*",
-    require    => Mysql_user["${user}@${host}"],
-    user       => "${user}@${host}",
+    mysql_grant { "${user}@${host}/${database}.*":
+      privileges => $privileges,
+      table      => "${database}.*",
+      require    => Mysql_user["${user}@${host}"],
+      user       => "${user}@${host}",
+    }
+  }
+  else {
+    if !defined(Database_user["${user}@${host}"]) {
+      database_user { "${user}@${host}":
+        password_hash => $password_hash,
+        provider      => 'mysql',
+        require       => Database[$database],
+      }
+    }
+
+    if !defined(Database_grant["${user}@${host}/${database}"]) {
+      database_grant { "${user}@${host}/${database}":
+        # TODO figure out which privileges to grant.
+        privileges => 'all',
+        provider   => 'mysql',
+        require    => Database_user["${user}@${host}"]
+      }
+    }
   }
 }
--- tests/noop/spec/hosts/database/database_spec.rb
+++ tests/noop/spec/hosts/database/database_spec.rb
@@ -3,6 +3,99 @@ require 'shared-examples'
 manifest = 'database/database.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+    keystone_db_user = 'keystone'
+    keystone_db_dbname = 'keystone'
+    keystone_db_password = Noop.hiera_structure 'keystone/db_password'
+    glance_db_user = 'glance'
+    glance_db_password = Noop.hiera_structure 'glance/db_password'
+    glance_db_dbname = 'glance'
+    nova_db_user = 'nova'
+    nova_db_password = Noop.hiera_structure 'nova/db_password'
+    nova_db_dbname = 'nova'
+    cinder_db_user = 'cinder'
+    cinder_db_password = Noop.hiera_structure 'cinder/db_password'
+    cinder_db_dbname = 'cinder'
+    hostname = Noop.hostname
+    allowed_hosts = ['%',Noop.hostname]
+    use_neutron = Noop.hiera 'use_neutron'
+
+    it 'should declare keystone::db::mysql class with user,password,dbname' do
+        should contain_class('keystone::db::mysql').with(
+            'user' => keystone_db_user,
+            'password' => keystone_db_password,
+            'dbname' => keystone_db_dbname,
+            'allowed_hosts' => allowed_hosts,
+        )
+    end
+    #TODO: uncomment in keystone module adaptation patch
+#    allowed_hosts.each do |host|
+#      it "should define openstacklib::db::mysql::host_access for #{keystone_db_dbname} DB for #{host}" do
+#        should contain_openstacklib__db__mysql__host_access("#{keystone_db_dbname}_#{host}")
+#      end
+#    end
+    it 'should declare glance:db::mysql class with user,password,dbname' do
+        should contain_class('glance::db::mysql').with(
+            'user' => glance_db_user,
+            'password' => glance_db_password,
+            'dbname' => glance_db_dbname,
+            'allowed_hosts' => allowed_hosts,
+        )
+    end
+    #TODO: uncomment in glance module adaptation patch
+#    allowed_hosts.each do |host|
+#      it "should define openstacklib::db::mysql::host_access for #{glance_db_dbname} DB for #{host}" do
+#        should contain_openstacklib__db__mysql__host_access("#{glance_db_dbname}_#{host}")
+#      end
+#    end
+    it 'should declare nova::db::mysql class with user,password,dbname' do
+        should contain_class('nova::db::mysql').with(
+            'user' => nova_db_user,
+            'password' => nova_db_password,
+            'dbname' => nova_db_dbname,
+            'allowed_hosts' => allowed_hosts,
+        )
+    end
+     #TODO: uncomment in cinder module adaptation patch
+#    allowed_hosts.each do |host|
+#      it "should define openstacklib::db::mysql::host_access for #{nova_db_dbname} DB for #{host}" do
+#        should contain_openstacklib__db__mysql__host_access("#{nova_db_dbname}_#{host}")
+#      end
+#    end
+    it 'should declare cinder::db::mysql class with user,password,dbname' do
+        should contain_class('cinder::db::mysql').with(
+            'user' => cinder_db_user,
+            'password' => cinder_db_password,
+            'dbname' => cinder_db_dbname,
+            'allowed_hosts' => allowed_hosts,
+        )
+    end
+     #TODO: uncomment in cinder module adaptation patch
+#    allowed_hosts.each do |host|
+#      it "should define openstacklib::db::mysql::host_access for #{cinder_db_dbname} DB for #{host}" do
+#        should contain_openstacklib__db__mysql__host_access("#{cinder_db_dbname}_#{host}")
+#      end
+#    end
+    if use_neutron
+        neutron_db_user = 'neutron'
+        neutron_db_password = Noop.hiera'neutron_db_password'
+        neutron_db_dbname = 'neutron'
+        it 'should declare neutron::db::mysql class with user,password,dbname' do
+            should contain_class('neutron::db::mysql').with(
+                'user' => neutron_db_user,
+                'password' => neutron_db_password,
+                'dbname' => neutron_db_dbname,
+                'allowed_hosts' => allowed_hosts,
+            )
+        end
+             #TODO: uncomment in neutron module adaptation patch
+#            allowed_hosts.each do |host|
+#              it "should define openstacklib::db::mysql::host_access for #{neutron_db_dbname} DB for #{host}" do
+#                should contain_openstacklib__db__mysql__host_access("#{neutron_db_dbname}_#{host}")
+#              end
+#            end
+    end
+
+  end # end of shared_examples
   test_ubuntu_and_centos manifest
 end
-
