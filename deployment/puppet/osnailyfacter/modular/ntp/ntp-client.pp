9d34cda813e31070188e6532834e7dee29e7a091
Dmitry Ilyin <dilyin@mirantis.com>
2015-07-02 14:54:19 +0300

    Revert "Revert "Secure dns and ntp""
    This reverts commit e0cc117d039b26e738a7f8f6a7e39fca3cd72557.
    
    Change-Id: Ife9b673752d94a67b200ecaefd66235338a9bb22

--- deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
+++ deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
@@ -1,12 +1,13 @@
 notice('MODULAR: ntp-client.pp')
 
-$management_vip  = hiera('management_vrouter_vip')
-$nodes_hash      = hiera('nodes', {})
-$roles           = node_roles($nodes_hash, hiera('uid'))
+$management_vrouter_vip  = hiera('management_vrouter_vip')
+$nodes_hash              = hiera('nodes', {})
+$roles                   = node_roles($nodes_hash, hiera('uid'))
 
 if !(member($roles, 'controller') or member($roles, 'primary-controller')) {
   class { 'ntp':
-    servers        => [$management_vip],
+    servers        => [$management_vrouter_vip],
+    interfaces     => ['lo'],
     service_ensure => running,
     service_enable => true,
     iburst_enable  => true,
