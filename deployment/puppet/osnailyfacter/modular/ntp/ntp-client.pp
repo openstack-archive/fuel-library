bd3345ae33f37136bd50b00931d7308d43520f97
Dmitry Ilyin <dilyin@mirantis.com>
2015-07-02 14:54:19 +0300

    Make ntpd listen all interfaces
    * Remove ntpd listen restrictions
    * Remove client run on controllers
    * Add ubuntu service overrides for ntp
    * ntp server should use only ipv4
    * update dnsmasq to only listen on management vrouter vip
    
    TestImpact
    Change-Id: Ic78394fddaafa82e085c55a1a0fdd3c46d4f0089
    Closes-Bug: 1466090
    Closes-Bug: 1470569

--- deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
+++ deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
@@ -1,18 +1,23 @@
 notice('MODULAR: ntp-client.pp')
 
-$management_vip  = hiera('management_vrouter_vip')
-$nodes_hash      = hiera('nodes', {})
-$roles           = node_roles($nodes_hash, hiera('uid'))
+$management_vrouter_vip  = hiera('management_vrouter_vip')
+$nodes_hash              = hiera('nodes', {})
+$roles                   = node_roles($nodes_hash, hiera('uid'))
 
-if !(member($roles, 'controller') or member($roles, 'primary-controller')) {
-  class { 'ntp':
-    servers        => [$management_vip],
-    service_ensure => running,
-    service_enable => true,
-    iburst_enable  => true,
-    tinker         => true,
-    panic          => 0,
-    stepout        => 5,
-    minpoll        => 3,
-  }
+
+class { 'ntp':
+  servers        => [$management_vrouter_vip],
+  service_ensure => 'running',
+  service_enable => true,
+  iburst_enable  => true,
+  tinker         => true,
+  panic          => '0',
+  stepout        => '5',
+  minpoll        => '3',
+}
+
+include ntp::params
+tweaks::ubuntu_service_override { 'ntpd':
+  package_name => $ntp::params::package_name,
+  service_name => $ntp::params::service_name,
 }
