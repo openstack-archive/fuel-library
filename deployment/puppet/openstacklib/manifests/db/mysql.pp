c47375c0cfba756c077da3a6667a0be089858103
vsaienko <vsaienko@mirantis.com>
2015-07-02 16:49:21 +0300

    Adapt synced openstacklib module
      * All this changes are temporary, they should be removed when
        mysql module is updated
      * add mysql_module parameter to provide backward compatibility
        for mysql < 2.2
      * disable rspec tests for openstacklib
      * bind default value of mysql_module to '0.3' to avoid
        changes in upstream modules
    
    Partially Implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: I5a31ecac08d30ff82abfe678b6eaad00849c317a

--- deployment/puppet/openstacklib/manifests/db/mysql.pp
+++ deployment/puppet/openstacklib/manifests/db/mysql.pp
@@ -8,6 +8,10 @@
 #    Password hash to use for the database user for this service;
 #    string; required
 #
+#  [*mysql_module*]
+#    Temp mysql_module vars that needed until mysql module is synced
+#    do hardcode mysql_module=3.4 to avoid adding it to all modules
+#
 #  [*dbname*]
 #    The name of the database
 #    string; optional; default to the $title of the resource, i.e. 'nova'
@@ -38,6 +42,7 @@
 
 define openstacklib::db::mysql (
   $password_hash,
+  $mysql_module   = '0.3',
   $dbname         = $title,
   $user           = $title,
   $host           = '127.0.0.1',
@@ -47,19 +52,32 @@ define openstacklib::db::mysql (
   $privileges     = 'ALL',
 ) {
 
-  include ::mysql::client
+  if ($mysql_module >= 2.2){
+    include ::mysql::client
 
-  mysql_database { $dbname:
-    ensure  => present,
-    charset => $charset,
-    collate => $collate,
-    require => [ Class['mysql::server'], Class['mysql::client'] ],
+    mysql_database { $dbname:
+      ensure  => present,
+      charset => $charset,
+      collate => $collate,
+      require => [ Class['mysql::server'], Class['mysql::client'] ],
+    }
+  } else {
+
+    require mysql::python
+    mysql::db { $dbname:
+      user     => $user,
+      password => $password_hash,
+      host     => $host,
+      charset  => $charset,
+      require  => Class['mysql::config'],
+    }
   }
 
   $allowed_hosts_list = unique(concat(any2array($allowed_hosts), [$host]))
   $real_allowed_hosts = prefix($allowed_hosts_list, "${dbname}_")
 
   openstacklib::db::mysql::host_access { $real_allowed_hosts:
+    mysql_module  => $mysql_module,
     user          => $user,
     password_hash => $password_hash,
     database      => $dbname,
