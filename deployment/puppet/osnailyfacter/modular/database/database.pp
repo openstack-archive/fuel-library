7b642395d63af0957ea3cb67135cf828ca8d5dad
Mykyta Koshykov <mk455v@att.com>
2015-07-06 23:35:46 +0200

    Improve MySQL composition layer
    - Added possibility to use different IP/port for ha_proxy for database
    - Include mysql-client to tasks when mysql-server is installed somewhere
      else
    
    Partial blueprint: detach-components-from-controllers
    
    Co-Authored-By: Nikita Koshikov <nkoshikov@mirantis.com>
    Co-Authored-By: Sergii Golovatiuk <sgolovatiuk@mirantis.com>
    Co-Authored-By: Matthew Mosesohn <mmosesohn@mirantis.com>
    Change-Id: I2b3239edd105715ceb88aa276ccc4afab4c046f0

--- deployment/puppet/osnailyfacter/modular/database/database.pp
+++ deployment/puppet/osnailyfacter/modular/database/database.pp
@@ -5,25 +5,22 @@ $management_network_range = hiera('management_network_range')
 $controller_nodes         = hiera('controller_nodes')
 $use_syslog               = hiera('use_syslog', true)
 $primary_controller       = hiera('primary_controller')
-$management_vip           = hiera('management_vip')
-$database_vip             = hiera('database_vip', undef)
 $mysql_hash               = hiera_hash('mysql', {})
 
-$haproxy_stats_port   = '10000'
-$haproxy_stats_url    = "http://${management_vip}:${haproxy_stats_port}/;csv"
+$mysql_database_password   = $mysql_hash['root_password']
+$mysql_database_enabled    = pick($mysql_hash['enabled'], true)
 
-$mysql_database_password  = $mysql_hash['root_password']
-$mysql_database_enabled   = pick($mysql_hash['enabled'], true)
-$mysql_db_host            = pick($database_vip, $management_vip, 'localhost')
 
-$mysql_bind_address       = '0.0.0.0'
+$galera_node_address       = $internal_address
+$galera_nodes              = pick($mysql_hash['galera_nodes'],
+                                  hiera_array('mysqld_ipaddresses', undef),
+                                  $controller_nodes)
+$galera_primary_controller = hiera('primary_database', $primary_controller)
+$mysql_bind_address        = '0.0.0.0'
 
-$enabled                  = true
 $galera_cluster_name      = 'openstack'
-$galera_node_address      = $internal_address
-$galera_nodes             = $controller_nodes
 $mysql_skip_name_resolve  = true
-$custom_setup_class       = 'galera'
+$custom_setup_class       = hiera('mysql_custom_setup_class', 'galera')
 
 # Get galera gcache factor based on cluster node's count
 $galera_gcache_factor     = count(unique(filter_hash(hiera('nodes', []), 'uid')))
@@ -34,6 +31,9 @@ $backend_port             = '3307'
 $backend_timeout          = '10'
 
 #############################################################################
+validate_string($status_password)
+validate_string($mysql_database_password)
+validate_string($status_password)
 
 if $mysql_database_enabled {
 
@@ -49,13 +49,17 @@ if $mysql_database_enabled {
     $config_hash_real = { }
   }
 
+  package { 'socat':
+    ensure => 'present',
+  }
+
   class { 'mysql::server':
     bind_address            => '0.0.0.0',
     etc_root_password       => true,
     root_password           => $mysql_database_password,
     old_root_password       => '',
     galera_cluster_name     => $galera_cluster_name,
-    primary_controller      => $primary_controller,
+    primary_controller      => $galera_primary_controller,
     galera_node_address     => $galera_node_address,
     galera_nodes            => $galera_nodes,
     galera_gcache_factor    => $galera_gcache_factor,
@@ -64,12 +68,6 @@ if $mysql_database_enabled {
     mysql_skip_name_resolve => $mysql_skip_name_resolve,
     use_syslog              => $use_syslog,
     config_hash             => $config_hash_real,
-  }
-
-  class { 'osnailyfacter::mysql_access':
-    db_user     => 'root',
-    db_password => $mysql_database_password,
-    db_host     => $mysql_db_host,
   }
 
   class { 'osnailyfacter::mysql_root':
@@ -104,13 +102,8 @@ if $mysql_database_enabled {
     only_from       => "127.0.0.1 240.0.0.2 ${management_network_range}",
   }
 
-  haproxy_backend_status { 'mysql' :
-    name => 'mysqld',
-    url  => $haproxy_stats_url,
-  }
-
-  package { 'socat':
-    ensure => 'present'
+  class { 'osnailyfacter::mysql_access':
+    db_password => $mysql_database_password,
   }
 
   Package['socat'] ->
@@ -118,7 +111,6 @@ if $mysql_database_enabled {
       Class['osnailyfacter::mysql_root'] ->
         Exec['initial_access_config'] ->
           Class['openstack::galera::status'] ->
-            Haproxy_backend_status['mysql'] ->
-              Class['osnailyfacter::mysql_access']
+            Class['osnailyfacter::mysql_access']
 
 }
