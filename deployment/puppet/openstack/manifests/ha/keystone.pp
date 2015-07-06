1e11aba999cbe8ab9b0012bffd9a3db0c15f8a33
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-07-06 12:54:26 +0300

    Memcache and Haproxy fixes for keystone
    * Set [cache]memcache_dead_retry and [memcache]dead_retry
      to 300
    * Change check in haproxy for keystone to rise 150
    
    Change-Id: I83d67b17d8a4d74b87e47bccfe6347edb33862ad
    Closes-Bug: 1471318

--- deployment/puppet/openstack/manifests/ha/keystone.pp
+++ deployment/puppet/openstack/manifests/ha/keystone.pp
@@ -13,7 +13,7 @@ class openstack::ha::keystone (
     haproxy_config_options => {
         option => ['httpchk', 'httplog','httpclose'],
     },
-    balancermember_options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3',
+    balancermember_options => 'check inter 10s fastinter 2s downinter 3s rise 150 fall 3',
 
   }
 
@@ -24,7 +24,7 @@ class openstack::ha::keystone (
     haproxy_config_options => {
         option => ['httpchk', 'httplog','httpclose'],
     },
-    balancermember_options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3',
+    balancermember_options => 'check inter 10s fastinter 2s downinter 3s rise 150 fall 3',
 
   }
 }
