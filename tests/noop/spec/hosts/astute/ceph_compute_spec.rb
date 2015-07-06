e7a17c11bafaa426c3859512a1313e18e3a8f872
Stanislav Makar <smakar@mirantis.com>
2015-07-06 14:03:52 +0000

    Noop tests for ceph_compute and cep_pools tasks
    Change-Id: I44c1dbb0f72312b2c9da9893d189fca9a7ac98b2
    Partial-bug: #1462451

--- tests/noop/spec/hosts/astute/ceph_compute_spec.rb
+++ tests/noop/spec/hosts/astute/ceph_compute_spec.rb
@@ -3,5 +3,29 @@ require 'shared-examples'
 manifest = 'astute/ceph_compute.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+    storage_hash = Noop.hiera 'storage'
+
+    if (storage_hash['images_ceph'] or storage_hash['objects_ceph'] or storage_hash['objects_ceph'])
+      it { should contain_ceph__pool('compute').with(
+          'pg_num'        => storage_hash['pg_num'],
+          'pgp_num'       => storage_hash['pg_num'],)
+        }
+
+      it { should contain_class('ceph').with(
+           'osd_pool_default_size'    => storage_hash['osd_pool_size'],
+           'osd_pool_default_pg_num'  => storage_hash['pg_num'],
+           'osd_pool_default_pgp_num' => storage_hash['pg_num'],)
+         }
+
+      it { should contain_class('ceph::conf') }
+      it { should contain_class('ceph::nova_compute') }
+
+      if storage_hash['ephemeral_ceph']
+        it { should contain_class('ceph::ephemeral') }
+      end
+    end
+
+  end
   test_ubuntu_and_centos manifest
 end
--- tests/noop/spec/hosts/astute/ceph_pools_spec.rb
+++ tests/noop/spec/hosts/astute/ceph_pools_spec.rb
@@ -3,5 +3,32 @@ require 'shared-examples'
 manifest = 'astute/ceph_pools.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+    storage_hash = Noop.hiera 'storage'
+
+    it { should contain_ceph__pool('images').with(
+              'pg_num'        => storage_hash['pg_num'],
+              'pgp_num'       => storage_hash['pg_num'],)
+      }
+    it { should contain_ceph__pool('volumes').with(
+              'pg_num'        => storage_hash['pg_num'],
+              'pgp_num'       => storage_hash['pg_num'],)
+      }
+    it { should contain_ceph__pool('backups').with(
+              'pg_num'        => storage_hash['pg_num'],
+              'pgp_num'       => storage_hash['pg_num'],)
+      }
+
+    if storage_hash['volumes_ceph']
+      it { should contain_service('cinder-volume') }
+      it { should contain_service('cinder-backup') }
+    end
+
+    if storage_hash['images_ceph']
+     it { should contain_service('glance-api') }
+    end
+
+  end
   test_ubuntu_and_centos manifest
+
 end
