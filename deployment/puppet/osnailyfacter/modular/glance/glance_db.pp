c062954b9fd83b30c85b0d945cf07233902ce768
Sergey Vasilenko <svasilenko@mirantis.com>
2015-07-07 20:18:10 +0300

    Prepare glance task for using network roles instead
    fixed defined networks adn addresses
    
    related-blueprint: templates-for-networking
    Change-Id: Ied461dd949f3a482b5fdbf6774667ace29e2f217

--- deployment/puppet/osnailyfacter/modular/glance/glance_db.pp
+++ deployment/puppet/osnailyfacter/modular/glance/glance_db.pp
@@ -13,12 +13,12 @@ $db_user     = pick($glance_hash['db_user'], 'glance')
 $db_name     = pick($glance_hash['db_name'], 'glance')
 $db_password = pick($glance_hash['db_password'], $mysql_root_password)
 
-$db_host          = pick($glance_hash['db_host'], $database_vip, $management_vip, 'localhost')
+$db_host          = pick($glance_hash['db_host'], $database_vip, 'localhost')
 $db_create        = pick($glance_hash['db_create'], $mysql_db_create)
 $db_root_user     = pick($glance_hash['root_user'], $mysql_root_user)
 $db_root_password = pick($glance_hash['root_password'], $mysql_root_password)
 
-$allowed_hosts = [ $::hostname, 'localhost', '127.0.0.1', '%' ]
+$allowed_hosts = [ hiera('node_name'), 'localhost', '127.0.0.1', '%' ]
 
 validate_string($mysql_root_user)
 
