dda247f3b54b04cc6db18858cd1f349ecb306166
Andrew Woodward <awoodward@mirantis.com>
2015-07-07 13:31:13 -0700

    Add support to controll ntp_servers selection properly.
    We currently set all of the nodes to sync to the controllers, but not
     everyone wants this. We can expose a parmater so that we can set them
     with specific values if desired, otherwise it goes back to managemnet
     vip.
    
    Change-Id: I2ab54babfe50165eabed77e6ce72e148103d1955

--- deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
+++ deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
@@ -1,18 +1,19 @@
 notice('MODULAR: ntp-client.pp')
 
-$management_vip  = hiera('management_vrouter_vip')
-$nodes_hash      = hiera('nodes', {})
-$roles           = node_roles($nodes_hash, hiera('uid'))
+$ntp_servers = hiera('ntp_servers', false)
 
-if !(member($roles, 'controller') or member($roles, 'primary-controller')) {
-  class { 'ntp':
-    servers        => [$management_vip],
-    service_ensure => running,
-    service_enable => true,
-    iburst_enable  => true,
-    tinker         => true,
-    panic          => 0,
-    stepout        => 5,
-    minpoll        => 3,
-  }
+if !($ntp_servers) {
+  $ntp_servers = [hiera('management_vrouter_vip'),]
+}
+
+class { 'ntp':
+  servers        => $ntp_servers,
+  interfaces     => ['lo'],
+  service_ensure => running,
+  service_enable => true,
+  iburst_enable  => true,
+  tinker         => true,
+  panic          => 0,
+  stepout        => 5,
+  minpoll        => 3,
 }
