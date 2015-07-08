8b42bcd4ea68b4c22bc01347414c40ce878a2bdf
Mykyta Koshykov <mk455v@att.com>
2015-07-08 16:38:05 -0500

    Split out haproxy configuration tasks
    This change allows for specific haproxy tasks to be enabled and disabled
    via hiera. In addition to being able to enable/disable tasks, the
    splitting out of tasks allows for haproxy configuration to be moved
    off of the controller and for specific items to be targeted to
    alternative hosts.
    
    Partial blueprint: detach-components-from-controllers
    Co-Authored-By: Alex Schultz <aschultz@mirantis.com>
    Co-Authored-By: Matthew Mosesohn <mmosesohn@mirantis.com>
    
    Change-Id: Iaea16127ba5565a418d158ca10e17db45958504d

--- deployment/puppet/osnailyfacter/modular/cluster-haproxy/cluster-haproxy.pp
+++ deployment/puppet/osnailyfacter/modular/cluster-haproxy/cluster-haproxy.pp
@@ -1,12 +1,17 @@
 notice('MODULAR: cluster-haproxy.pp')
 
 $network_scheme = hiera('network_scheme', {})
+$haproxy_hash = hiera_hash('haproxy_hash', {})
+$management_vip = hiera('management_vip')
+
+$primary_controller = pick($haproxy_hash['primary'], hiera('primary_controller'))
+$stats_vip = pick($haproxy_hash['stats_vip'], $management_vip)
 
 class { 'cluster::haproxy':
   haproxy_maxconn    => '16000',
   haproxy_bufsize    => '32768',
-  primary_controller => hiera('primary_controller'),
+  primary_controller => $primary_controller,
   debug              => hiera('debug', false),
   other_networks     => direct_networks($network_scheme['endpoints']),
-  stats_ipaddresses  => [hiera('management_vip'),'127.0.0.1']
+  stats_ipaddresses  => [$stats_vip, '127.0.0.1']
 }
