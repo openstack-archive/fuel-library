8409ee52e9ccee529d49d74ed44fdd4fffd9683e
Mykyta Koshykov <mk455v@att.com>
2015-07-08 16:38:05 -0500

    (WIP) Split out haproxy configuration tasks
    WORK IN PROGRESS
    
    This change allows for specific haproxy tasks to be enabled and disabled
    via hiera. In addition to being able to enable/disable tasks, the
    splitting out of tasks allows for haproxy configuration to be moved
    off of the controller.
    
    Partial blueprint: detach-components-from-controllers
    Co-Authored-By: Alex Schultz <aschultz@mirantis.com>
    
    Change-Id: Iaea16127ba5565a418d158ca10e17db45958504d

--- deployment/puppet/openstack/manifests/ha/keystone.pp
+++ deployment/puppet/openstack/manifests/ha/keystone.pp
@@ -1,30 +1,52 @@
+# == Class: openstack::ha::keystone
+#
 # HA configuration for OpenStack Keystone
+#
+# === Paramters
+#
+# [*internal_virtual_ip*]
+#   (required) String. This is the ipaddress to be used for the internal facing
+#   vip
+#
+# [*ipaddresses*]
+#   (reqiured) Array. This is an array of ipaddresses for the backend services
+#   to be loadbalanced
+#
+# [*public_virtual_ip*]
+#   (required) String. This is the ipaddress to be used for the external facing
+#   vip
+#
+# [*server_names*]
+#   (required) Array. This is an array of server names for the haproxy service
+#
 class openstack::ha::keystone (
-  $server_names,
+  $internal_virtual_ip,
   $ipaddresses,
+  $public_virtual_ip,
+  $server_names,
 ) {
 
-  openstack::ha::haproxy_service { 'keystone-1':
-    order                  => '020',
-    listen_port            => 5000,
-    public                 => true,
-    server_names           => $server_names,
-    ipaddresses            => $ipaddresses,
+  # defaults for any haproxy_service within this class
+  Openstack::Ha::Haproxy_service {
+    internal_virtual_ip => $internal_virtual_ip,
+    ipaddresses         => $ipaddresses,
+    public_virtual_ip   => $public_virtual_ip,
+    server_names        => $server_names,
     haproxy_config_options => {
-        option => ['httpchk', 'httplog','httpclose'],
+      option => ['httpchk', 'httplog','httpclose'],
     },
     balancermember_options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3',
+  }
 
+  openstack::ha::haproxy_service { 'keystone-1':
+    order       => '020',
+    listen_port => 5000,
+    public      => true,
   }
 
   openstack::ha::haproxy_service { 'keystone-2':
-    order                  => '030',
-    listen_port            => 35357,
-    public                 => false,
-    haproxy_config_options => {
-        option => ['httpchk', 'httplog','httpclose'],
-    },
-    balancermember_options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3',
-
+    order       => '030',
+    listen_port => 35357,
+    public      => false,
   }
 }
--- deployment/puppet/openstack/manifests/ha/murano.pp
+++ deployment/puppet/openstack/manifests/ha/murano.pp
@@ -1,25 +1,50 @@
+# == Class: openstack::ha::murano
+#
 # HA configuration for OpenStack Murano
+#
+# === Paramters
+#
+# [*internal_virtual_ip*]
+#   (required) String. This is the ipaddress to be used for the internal facing
+#   vip
+#
+# [*ipaddresses*]
+#   (reqiured) Array. This is an array of ipaddresses for the backend services
+#   to be loadbalanced
+#
+# [*public_virtual_ip*]
+#   (required) String. This is the ipaddress to be used for the external facing
+#   vip
+#
+# [*server_names*]
+#   (required) Array. This is an array of server names for the haproxy service
+#
 class openstack::ha::murano (
-  $server_names,
+  $internal_virtual_ip,
   $ipaddresses,
+  $public_virtual_ip,
+  $server_names,
 ) {
 
+  # defaults for any haproxy_service within this class
+  Openstack::Ha::Haproxy_service {
+    internal_virtual_ip => $internal_virtual_ip,
+    ipaddresses         => $ipaddresses,
+    public_virtual_ip   => $public_virtual_ip,
+    server_names        => $server_names,
+    public              => true,
+  }
+
   openstack::ha::haproxy_service { 'murano':
-    order           => '180',
-    listen_port     => 8082,
-    public          => true,
-    require_service => 'murano_api',
-    server_names    => $server_names,
-    ipaddresses     => $ipaddresses,
+    order       => '180',
+    listen_port => 8082,
   }
 
   openstack::ha::haproxy_service { 'murano_rabbitmq':
-    order          => '190',
-    listen_port    => 55572,
-    define_backups => true,
-    public         => true,
-    internal       => false,
-
+    order                  => '190',
+    listen_port            => 55572,
+    define_backups         => true,
+    internal               => false,
     haproxy_config_options => {
       'option'         => ['tcpka'],
       'timeout client' => '48h',
@@ -27,7 +52,6 @@ class openstack::ha::murano (
       'balance'        => 'roundrobin',
       'mode'           => 'tcp'
     },
-
     balancermember_options => 'check inter 5000 rise 2 fall 3',
   }
 }
