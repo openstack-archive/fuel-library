67211f9ce06580393919fb219193b47f66540762
Mykyta Koshykov <mk455v@att.com>
2015-07-08 16:38:05 -0500

    Split out haproxy configuration tasks
    This change allows for specific haproxy tasks to be enabled and disabled
    via hiera. In addition to being able to enable/disable tasks, the
    splitting out of tasks allows for haproxy configuration to be moved
    off of the controller and for specific items to be targeted to
    alternative hosts.
    
    Removed redundant haproxy_nodes hash which duplicates the feature
    of $servicename_{names,ipaddresses} in hiera.
    
    Partial blueprint: detach-components-from-controllers
    Co-Authored-By: Alex Schultz <aschultz@mirantis.com>
    Co-Authored-By: Matthew Mosesohn <mmosesohn@mirantis.com>
    
    Change-Id: Iaea16127ba5565a418d158ca10e17db45958504d

---
+++ deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-neutron.pp
@@ -0,0 +1,25 @@
+notice('MODULAR: openstack-haproxy-neutron.pp')
+
+# NOT enabled by default
+$use_neutron         = hiera('use_neutron', false)
+
+$controllers              = hiera('controllers')
+$controllers_server_names = filter_hash($controllers, 'name')
+$controllers_ipaddresses  = filter_hash($controllers, 'internal_address')
+
+if ($use_neutron) {
+  $server_names        = pick(hiera_array('neutron_names', undef),
+                              $controllers_server_names)
+  $ipaddresses         = pick(hiera_array('neutron_ipaddresses', undef),
+                              $controllers_ipaddresses)
+  $public_virtual_ip   = hiera('public_vip')
+  $internal_virtual_ip = hiera('management_vip')
+
+  # configure neutron ha proxy
+  class { '::openstack::ha::neutron':
+    internal_virtual_ip => $internal_virtual_ip,
+    ipaddresses         => $ipaddresses,
+    public_virtual_ip   => $public_virtual_ip,
+    server_names        => $server_names,
+  }
+}
