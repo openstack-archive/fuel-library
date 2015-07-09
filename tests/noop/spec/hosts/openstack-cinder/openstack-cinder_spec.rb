2d8a53ca829d0028e3cd15c48c049dbd34ffae71
Maksym Yatsenko <myatsenko@mirantis.com>
2015-07-09 11:55:55 +0300

    Update openstack-cinder_spec.rb test
    correct a check that ensures cinder_config contains
    "oslo_messaging_rabbit/rabbit_ha_queues" section/parameter.
    
    Change-Id: I8dff1258086ba8d44a33155a8fc915c23fee243f

--- tests/noop/spec/hosts/openstack-cinder/openstack-cinder_spec.rb
+++ tests/noop/spec/hosts/openstack-cinder/openstack-cinder_spec.rb
@@ -8,18 +8,13 @@ describe manifest do
 max_pool_size = 20
 max_retries = '-1'
 max_overflow = 20
+rabbit_ha_queues = Noop.hiera('rabbit_ha_queues')
 
-if ! (Noop.hiera 'queue_provider')
-  queue_provider = 'rabbitmq'
-end
-
-if queue_provider
-it 'ensures cinder_config contains needed resources before cinder-* services run' do
+ it 'ensures cinder_config contains "oslo_messaging_rabbit/rabbit_ha_queues" ' do
   should contain_cinder_config('oslo_messaging_rabbit/rabbit_ha_queues').with(
-   :before => ['Service[cinder-api]','Service[cinder-volume]','Service[cinder-scheduler]'],
- )
-end
-end
+    'value' => rabbit_ha_queues,
+  )
+ end
 
 it 'should declare ::cinder class with correct database_max_* parameters' do
   should contain_class('cinder').with(
@@ -27,9 +22,10 @@ it 'should declare ::cinder class with correct database_max_* parameters' do
     'database_max_retries'   => max_retries,
     'database_max_overflow'  => max_overflow,
   )
-  end
-  end # end of shared_examples
-  test_ubuntu_and_centos manifest
-
 end
 
+  end # end of shared_examples
+
+ test_ubuntu_and_centos manifest
+
+end
