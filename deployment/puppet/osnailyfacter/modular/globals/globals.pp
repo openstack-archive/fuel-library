d8132717ba927bf47fae0aa778c06dfb4499068b
Stanislav Makar <smakar@mirantis.com>
2015-07-10 15:57:51 +0000

    Adapt database to flexible networking
    * Use network role *mgmt/database* to configure database
    * Use database_vip instead of management_vip
    * Add NOOP tests
    
    blueprint: templates-for-networking
    Change-Id: I5d6048c15bd468c68896df7be28867063f018541

--- deployment/puppet/osnailyfacter/modular/globals/globals.pp
+++ deployment/puppet/osnailyfacter/modular/globals/globals.pp
@@ -253,6 +253,10 @@ $is_primary_swift_proxy = $primary_controller
 # todo: use special node-roles instead controllers in the future
 $horizon_nodes = get_nodes_hash_by_roles($network_metadata, ['primary-controller', 'controller'])
 
+# Define database-related variables:
+# todo: use special node-roles instead controllers in the future
+$database_nodes = get_nodes_hash_by_roles($network_metadata, ['primary-controller', 'controller'])
+
 # save all these global variables into hiera yaml file for later use
 # by other manifests with hiera function
 file { '/etc/hiera/globals.yaml' :
