bb247160210f169e750762b6b387c9d7d821388c
Stanislav Makar <smakar@mirantis.com>
2015-07-10 15:57:51 +0000

    Adapt database to flexible networking
    * Use network role *mgmt/database* to configure database
    * Use database_vip instead of management_vip
    * Add NOOP tests
    
    blueprint: templates-for-networking
    Change-Id: I5d6048c15bd468c68896df7be28867063f018541

--- tests/noop/spec/hosts/database/database_spec.rb
+++ tests/noop/spec/hosts/database/database_spec.rb
@@ -3,6 +3,16 @@ require 'shared-examples'
 manifest = 'database/database.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+    #nodes = Noop.hiera 'nodes'
+    it { should contain_class('mysql::server').that_comes_before('Class[osnailyfacter::mysql_root]') }
+    it { should contain_class('osnailyfacter::mysql_access') }
+    it { should contain_class('osnailyfacter::mysql_root').that_comes_before('Exec[initial_access_config]') }
+    it { should contain_class('openstack::galera::status').that_comes_before('Haproxy_backend_status[mysql]') }
+    it { should contain_haproxy_backend_status('mysql').that_comes_before('Class[osnailyfacter::mysql_access]') }
+    it { should contain_package('socat').that_comes_before('Class[mysql::server]') }
+  end
+
   test_ubuntu_and_centos manifest
 end
 
