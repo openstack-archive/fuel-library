aeafe05f59edddd0552f9f8fbec12d491e694846
iberezovskiy <iberezovskiy@mirantis.com>
2015-07-10 18:50:25 +0300

    Set 'libvirt' group for ceilometer user on compute node
      Our custom libvirt-bin package (taken from Debian)
      creates 'libvirt' group instead of 'libvirtd'.
      That's why we need to redefine groups for ceilometer user.
    
      Closes-bug: #1473416
    
    Change-Id: I6b83ce1ee9429b6a747a593838896d2a5c4bc832

---
+++ deployment/puppet/osnailyfacter/lib/facter/libvirt_package_version.rb
@@ -0,0 +1,17 @@
+require 'facter'
+Facter.add('libvirt_package_version') do
+  setcode do
+  version=nil
+  case Facter.value('osfamily')
+    when /(?i)(debian)/
+      pkg_grep_cmd = "apt-cache policy"
+      out = Facter::Util::Resolution.exec("#{pkg_grep_cmd} libvirt-bin")
+      version = out.grep('Candidate').split(/\s+/)[2] if out
+    when /(?i)(redhat)/
+      pkg_grep_cmd = "yum info"
+      out = Facter::Util::Resolution.exec("#{pkg_grep_cmd} libvirt-bin")
+      version = out.grep('Version').split(/\s+/)[3] if out
+  end
+  version
+  end
+end
