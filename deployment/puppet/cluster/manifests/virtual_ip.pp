0d2c52b26cee1075559f03ac60edc1e996a9fc25
Dmitry Ilyin <dilyin@mirantis.com>
2015-07-13 19:42:36 +0300

    Refactor virtual_ips task
    * Use function instead of hardcoded values
    * Use ocf_handlers for vrouter and vip manifests
    * Add prefix option to pcmk wrapper
    * Netmask_to_cidr should always return strings
    * Fix naming of interfaces in vrouter OCF
    * Fix wrong interface name for send_arp in IPaddr ocf
    
    Implements: blueprint templates-for-networking
    Related-Blueprint: detach-components-from-controllers
    
    Change-Id: I3527f2d85baac83b209c7af713466b593238555f

--- deployment/puppet/cluster/manifests/virtual_ip.pp
+++ deployment/puppet/cluster/manifests/virtual_ip.pp
@@ -20,117 +20,147 @@ define cluster::virtual_ip (
 ){
   $vip_name = "vip__${key}"
 
-  #File<| title == 'ns-ipaddr2-ocf' |> -> Cs_resource[$vip_name]
-
-  cs_resource { $vip_name:
-    ensure          => present,
-    primitive_class => 'ocf',
-    provided_by     => 'fuel',
-    primitive_type  => 'ns_IPaddr2',
-    parameters      => {
-      'nic'                  => $vip[nic],
-      'base_veth'            => $vip[base_veth],
-      'ns_veth'              => $vip[ns_veth],
-      'ip'                   => $vip[ip],
-      'iflabel'              => $vip[iflabel] ? {
-        undef   => 'ka',
-        default => $vip[iflabel]
-      },
-      'cidr_netmask'         => $vip[cidr_netmask] ? {
-        undef   => '24',
-        default => $vip[cidr_netmask]
-      },
-      'ns'                   => $vip[namespace] ? {
-        undef   => 'haproxy',
-        default => $vip[namespace]
-      },
-      'gateway'              => $vip[gateway] ? {
-        undef   => '',
-        default => $vip[gateway]
-      },
-      'gateway_metric'       => $vip[gateway_metric] ? {
-        undef   => '0',
-        default => $vip[gateway_metric]
-      },
-      'other_networks'       => $vip[other_networks] ? {
-        undef => 'false', '' => 'false',
-        default => $vip[other_networks]
-      },
-      'bridge'              => $vip[bridge] ? {
-        undef   => 'false',
-        ''      => 'false',
-        default => $vip[bridge]
-      },
-      'iptables_start_rules' => $vip[iptables_start_rules] ? {
-        undef   => 'false',
-        ''      => 'false',
-        default => "'${vip[iptables_start_rules]}'"
-      },
-      'iptables_stop_rules'  => $vip[iptables_stop_rules] ? {
-        undef   => 'false',
-        ''      => 'false',
-        default => "'${vip[iptables_stop_rules]}'"
-      },
-      'iptables_comment'     => $vip[iptables_comment] ? {
-        undef   => 'false',
-        ''      => 'false',
-        default => "'${vip[iptables_comment]}'"
-      },
-      'ns_iptables_start_rules' => $vip[ns_iptables_start_rules] ? {
-        undef   => 'false',
-        ''      => 'false',
-        default => "'${vip[ns_iptables_start_rules]}'"
-      },
-      'ns_iptables_stop_rules'  => $vip[ns_iptables_stop_rules] ? {
-        undef   => 'false',
-        ''      => 'false',
-        default => "'${vip[ns_iptables_stop_rules]}'"
-      },
+  $parameters = {
+    'nic'                  => $vip['nic'],
+    'base_veth'            => $vip['base_veth'],
+    'ns_veth'              => $vip['ns_veth'],
+    'ip'                   => $vip['ip'],
+    'iflabel'              => $vip['iflabel'] ? {
+      undef   => 'ka',
+      default => $vip['iflabel']
     },
-    metadata => {
-      'migration-threshold' => '3',   # will be try start 3 times before migrate to another node
-      'failure-timeout'     => '60',  # forget any fails of starts after this timeout
-      'resource-stickiness' => '1'
+    'cidr_netmask'         => $vip['cidr_netmask'] ? {
+      undef   => '24',
+      default => $vip['cidr_netmask']
     },
-    operations      => {
-      'monitor' => {
-        'interval' => '3',
-        'timeout'  => '30'
-      },
-      'start'   => {
-        'timeout' => '30'
-      },
-      'stop'    => {
-        'timeout' => '30'
-      },
+    'ns'                   => $vip['namespace'] ? {
+      undef   => 'haproxy',
+      default => $vip['namespace']
+    },
+    'gateway'              => $vip['gateway'] ? {
+      undef   => '',
+      default => $vip['gateway']
+    },
+    'gateway_metric'       => $vip['gateway_metric'] ? {
+      undef   => '0',
+      default => $vip['gateway_metric']
+    },
+    'other_networks'       => $vip['other_networks'] ? {
+      undef => 'false', '' => 'false',
+      default => $vip['other_networks']
+    },
+    'bridge'              => $vip['bridge'] ? {
+      undef   => 'false',
+      ''      => 'false',
+      default => $vip['bridge']
+    },
+    'iptables_start_rules' => $vip['iptables_start_rules'] ? {
+      undef   => 'false',
+      ''      => 'false',
+      default => "'${vip['iptables_start_rules']}'",
+    },
+    'iptables_stop_rules'  => $vip['iptables_stop_rules'] ? {
+      undef   => 'false',
+      ''      => 'false',
+      default => "'${vip['iptables_stop_rules']}'",
+    },
+    'iptables_comment'     => $vip['iptables_comment'] ? {
+      undef   => 'false',
+      ''      => 'false',
+      default => "'${vip['iptables_comment']}'",
+    },
+    'ns_iptables_start_rules' => $vip['ns_iptables_start_rules'] ? {
+      undef   => 'false',
+      ''      => 'false',
+      default => "'${vip['ns_iptables_start_rules']}'",
+    },
+    'ns_iptables_stop_rules'  => $vip['ns_iptables_stop_rules'] ? {
+      undef   => 'false',
+      ''      => 'false',
+      default => "'${vip['ns_iptables_stop_rules']}'",
     },
   }
+
+  $metadata = {
+    'migration-threshold' => '3',   # will be try start 3 times before migrate to another node
+    'failure-timeout'     => '60',  # forget any fails of starts after this timeout
+    'resource-stickiness' => '1'
+  }
+
+  $operations = {
+    'monitor' => {
+      'interval' => '3',
+      'timeout'  => '30',
+    },
+    'start'   => {
+      'timeout' => '30',
+    },
+    'stop'    => {
+      'timeout' => '30',
+    },
+  }
+
+  $primitive_type = 'ns_IPaddr2'
 
   service { $vip_name:
-    ensure   => 'running',
-    enable   => true,
-    provider => 'pacemaker',
+    ensure => 'running',
+    enable => true,
   }
 
-  Cs_resource[$vip_name] -> Service[$vip_name]
+  pacemaker_wrappers::service { $vip_name :
+    primitive_type   => $primitive_type,
+    parameters       => $parameters,
+    metadata         => $metadata,
+    operations       => $operations,
+    prefix           => false,
+  }
 
-  if $vip[collocation] {
-    cs_rsc_colocation { "${vip_name}-with-vip__${vip[collocation]}":
-      ensure     => present,
+  # I'am running before this other vip
+  # and this other vip cannot start without me running on this node
+  $colocation_before = $vip['colocation_before']
+  if $colocation_before {
+    $colocation_before_vip_name = "vip__${colocation_before}"
+    $colocation_before_constraint_name = "${colocation_before_vip_name}-with-${vip_name}"
+    cs_rsc_colocation { $colocation_before_constraint_name :
+      ensure     => 'present',
       score      => 'INFINITY',
       primitives => [
-          $vip_name,
-          "vip__${vip[collocation]}"
+        $colocation_before_vip_name,
+        $vip_name,
       ],
     }
 
-  Service["vip__${vip[collocation]}"] -> Cs_rsc_colocation["${vip_name}-with-vip__${vip[collocation]}"]
+    Cs_resource <| title == $vip_name |> -> Cs_resource <| title == $colocation_before_vip_name |>
+    Service <| title == $vip_name |> -> Service <| title == $colocation_before_vip_name |>
+    Service <| title == $colocation_before_vip_name |> -> Cs_rsc_colocation[$colocation_before_constraint_name]
+    Service <| title == $vip_name |> -> Cs_rsc_colocation[$colocation_before_constraint_name]
+  }
 
+  # I'm running after this other vip
+  # and I cannot start without other vip running on this node
+  $colocation_after = $vip['colocation_after']
+  if $colocation_after {
+    $colocation_after_vip_name = "vip__${colocation_after}"
+    $colocation_after_constraint_name = "${vip_name}-with-${colocation_after_vip_name}"
+    cs_rsc_colocation { $colocation_after_constraint_name :
+      ensure     => 'present',
+      score      => 'INFINITY',
+      primitives => [
+        $vip_name,
+        $colocation_after_vip_name,
+      ],
+    }
+
+    Cs_resource <| title == $colocation_after_vip_name |> -> Cs_resource <| title == $vip_name |>
+    Service <| title == $colocation_after_vip_name |> -> Service <| title == $vip_name |>
+    Service <| title == $colocation_after_vip_name |> -> Cs_rsc_colocation[$colocation_after_constraint_name]
+    Service <| title == $vip_name |> -> Cs_rsc_colocation[$colocation_after_constraint_name]
   }
 
 }
 
 Class['corosync'] -> Cluster::Virtual_ip <||>
+
 if defined(Corosync::Service['pacemaker']) {
   Corosync::Service['pacemaker'] -> Cluster::Virtual_ip <||>
 }
