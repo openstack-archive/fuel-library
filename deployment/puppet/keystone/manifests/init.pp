74348522c15e81bc75420d4d7f466017713bc5bc
Maksym Yatsenko <myatsenko@mirantis.com>
2015-07-13 10:52:28 +0300

    Enable Fernet tokens support.
      - use fernet token by default
      - generate and install keys for fernet
      - new "revoke_id" param was added,
        it will be pushed in upstream in keystone::init
      - add noop tests for new parameters
    
    Change-Id: Icec8ae329b02257d3638598a750233ae865169d4

--- deployment/puppet/keystone/manifests/init.pp
+++ deployment/puppet/keystone/manifests/init.pp
@@ -75,6 +75,10 @@
 #   (optional) Driver for token revocation.
 #   Defaults to 'keystone.contrib.revoke.backends.sql.Revoke'
 #
+# [*revoke_id*]
+#   (optional) Revoke token by token identifier.
+#   Setting revoke_by_id to true enables various forms of enumerating tokens.
+#
 # [*cache_dir*]
 #   (optional) Directory created when token_provider is pki.
 #   Defaults to /var/cache/keystone.
@@ -413,6 +417,7 @@ class keystone(
   $token_driver           = 'keystone.token.persistence.backends.sql.Token',
   $token_expiration       = 3600,
   $revoke_driver          = 'keystone.contrib.revoke.backends.sql.Revoke',
+  $revoke_id              = true,
   $public_endpoint        = false,
   $admin_endpoint         = false,
   $enable_ssl             = false,
@@ -905,6 +910,9 @@ class keystone(
       notify      => Service[$service_name],
       subscribe   => [Package['keystone'], Keystone_config['fernet_tokens/key_repository']],
     }
+
+   keystone_config {'token/revoke_by_id': value => $revoke_id}
+
   }
 
   if $fernet_key_repository {
