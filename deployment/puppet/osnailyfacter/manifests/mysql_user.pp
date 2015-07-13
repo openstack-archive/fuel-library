4b3d547b3c64f194fc88fc283bd2ae2928575d24
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-07-13 13:56:36 +0300

    Allow root mysql within environment
    * Allow connections to mysql as root only
      from IP-addresses within environment
    * Updated direct_networks function
    * Provided and updated noop tests
    
    Closes-Bug: 1471956
    Change-Id: Ia873292d6b05f612642e055d1d0dc156a863bc34

---
+++ deployment/puppet/osnailyfacter/manifests/mysql_user.pp
@@ -0,0 +1,43 @@
+# == Class definition osnailyfacter::mysql_user
+#
+# Class for mysql user creation
+#
+# [*user*]
+# Mysql username
+#
+# [*password*]
+#  Password to use with user
+#
+# [*access_networks*]
+#  Array of specific IPs or Networks or Hostnames
+#  to access the database with user
+#
+define osnailyfacter::mysql_user (
+  $user            = $name,
+  $password        = '',
+  $access_networks = '',
+) {
+
+  Exec {
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    creates => '/root/.my.cnf',
+  }
+
+  osnailyfacter::mysql_grant { $access_networks:
+    user    => $user,
+    before  => Exec['mysql_root_password'],
+    require => Exec['mysql_drop_test'],
+  }
+
+  exec { 'mysql_drop_test' :
+    command => "mysql -NBe \"drop database if exists test\"",
+  }
+
+  exec { 'mysql_root_password' :
+    command => "mysql -NBe \"update mysql.user set password = password('${password}') where user = \'${user}\'\"",
+  } ->
+
+  exec { 'mysql_flush_privileges' :
+    command => "mysql -NBe \"flush privileges\"",
+  }
+}
--- tests/noop/spec/hosts/cluster-haproxy/cluster-haproxy_spec.rb
+++ tests/noop/spec/hosts/cluster-haproxy/cluster-haproxy_spec.rb
@@ -8,40 +8,51 @@ describe manifest do
     networks = []
     endpoints = Noop.hiera_structure 'network_scheme/endpoints'
     management_vip = Noop.hiera 'management_vip'
-    endpoints.each{ |k,v|
-      if v['IP'].is_a?(Array)
-        v['IP'].each { |ip|
-          networks << IPAddr.new(ip).to_s + '/' + ip.split('/')[1]
-        }
+    filter = nil
+    netmask = nil
+
+    class IPAddr
+      def mask_length
+        @mask_addr.to_s(2).count '1'
       end
-      if v.has_key?('routes') and v['routes'].is_a?(Array)
-        v['routes'].each { |route|
-          networks << route['net']
-        }
+
+      def cidr
+        "#{to_s}/#{mask_length}"
       end
-    }
+
+      def netmask
+        "#{to_s}/#{_to_string(@mask_addr)}"
+      end
+    end
+
+    endpoints.each do |interface, parameters|
+      next unless parameters.has_key? 'IP' and parameters['IP'].is_a? Array
+      next if filter and interface != filter
+      parameters['IP'].each do |ip|
+        next unless ip
+        if netmask and netmask == 'netmask'
+          networks << IPAddr.new(ip).netmask
+        else
+          networks << IPAddr.new(ip).cidr
+        end
+      end
+      next unless parameters.has_key? 'routes' and parameters['routes'].is_a? Array
+      parameters['routes'].each do |route|
+        next unless route.has_key? 'net'
+        if netmask and netmask == 'netmask'
+          networks << IPAddr.new(route['net']).netmask
+        else
+          networks << IPAddr.new(route['net']).cidr
+        end
+      end
+    end
 
     it "should delcare cluster::haproxy with other_networks set to #{networks.join(' ')}" do
       should contain_class('cluster::haproxy').with(
         'other_networks' => networks.join(' '),
       )
     end
-    it "should contain stats fragment and listen only on lo and #{management_vip}" do
-        should contain_concat__fragment('haproxy-stats').with_content(
-            %r{\n\s*bind\s+127\.0\.0\.1:10000\s*$\n}
-        )
-        should contain_concat__fragment('haproxy-stats').with_content(
-            %r{\n\s*bind\s+#{management_vip}:10000\s*\n}
-        )
-    end
-    it "should not contain stats enable for defaults and global section" do
-        should contain_concat__fragment('haproxy-base').without_content(
-            %r{\n\s*stats\s+enable\s*$\n}
-        )
-    end
-
   end
 
   test_ubuntu_and_centos manifest
 end
-
