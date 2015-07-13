6430198a4c0754528c40072d588a857494155b1c
Dmitry Ilyin <dilyin@mirantis.com>
2015-07-13 19:42:36 +0300

    Refactor virtual_ips task
    * Use function instead of hardcoded values
    * Use ocf_handlers for vrouter and vip manifests
    * Add prefix option to pcmk wrapper
    * Netmask_to_cidr should always return strings
    * Fix naming of interfaces in vrouter OCF
    * Fix wrong interface name for send_arp in IPaddr ocf
    
    Implements: blueprint templates-for-networking
    Related-Blueprint: detach-components-from-controllers
    
    Change-Id: I3527f2d85baac83b209c7af713466b593238555f

--- deployment/puppet/cluster/manifests/vrouter_ocf.pp
+++ deployment/puppet/cluster/manifests/vrouter_ocf.pp
@@ -5,50 +5,52 @@
 class cluster::vrouter_ocf (
   $primary_controller,
   $other_networks = false,
-){
+) {
   $service_name = 'p_vrouter'
 
   if $primary_controller {
-    cs_resource { $service_name:
-      ensure          => present,
-      primitive_class => 'ocf',
-      provided_by     => 'fuel',
-      primitive_type  => 'ns_vrouter',
-      complex_type    => 'clone',
-      ms_metadata     => {
-        'interleave' => true,
+    $primitive_type = 'ns_vrouter'
+    $complex_type   = 'clone'
+    $ms_metadata = {
+      'interleave' => true,
+    }
+    $metadata = {
+      'migration-threshold' => '3',
+      'failure-timeout'     => '120',
+    }
+    $parameters = {
+      'ns'             => 'vrouter',
+      'other_networks' => "'$other_networks'",
+    }
+    $operations = {
+      'monitor' => {
+        'interval' => '30',
+        'timeout'  => '60'
       },
-      metadata        => {
-        'migration-threshold' => '3',
-        'failure-timeout'     => '120',
+      'start'   => {
+        'timeout' => '30'
       },
-      parameters      => {
-        'ns'             => 'vrouter',
-        'other_networks' => "'$other_networks'",
-      },
-      operations      => {
-        'monitor' => {
-          'interval' => '30',
-          'timeout'  => '60'
-        },
-        'start'   => {
-          'timeout' => '30'
-        },
-        'stop'    => {
-          'timeout' => '60'
-        },
+      'stop'    => {
+        'timeout' => '60'
       },
     }
 
-    Cs_resource[$service_name] -> Service[$service_name]
+    service { $service_name :
+      ensure     => 'running',
+      enable     => true,
+      hasstatus  => true,
+      hasrestart => true,
+    }
+
+    pacemaker_wrappers::service { $service_name :
+      primitive_type   => $primitive_type,
+      parameters       => $parameters,
+      metadata         => $metadata,
+      operations       => $operations,
+      ms_metadata      => $ms_metadata,
+      complex_type     => $complex_type,
+      prefix           => false,
+    }
   }
 
-  service { $service_name:
-    ensure     => 'running',
-    name       => $service_name,
-    enable     => true,
-    hasstatus  => true,
-    hasrestart => true,
-    provider   => 'pacemaker',
-  }
 }
