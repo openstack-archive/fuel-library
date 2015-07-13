791e7808f0b933f6e45fe2da136911d18b185041
Maksym Yatsenko <myatsenko@mirantis.com>
2015-07-13 10:52:28 +0300

    Enable Fernet tokens support
      - use fernet token by default
      - generate and install keys for fernet
      - new "revoke_id" param was added,
        it will be pushed in upstream in keystone::init
      - add noop tests for new parameters
    
    Change-Id: Icec8ae329b02257d3638598a750233ae865169d4

---
+++ deployment/puppet/osnailyfacter/lib/puppet/provider/install_fernet_keys/fernet_key.rb
@@ -0,0 +1,40 @@
+require 'fileutils'
+require 'etc'
+Puppet::Type.type(:install_fernet_keys).provide :fernet_key do
+  desc 'install fernet keys'
+
+  def exists?
+    return false unless File.exists? stagekey
+    return false unless File.exists? primkey
+  end
+
+  def create
+    FileUtils.mkdir_p ferndir unless File.exists? ferndir
+    FileUtils.cp @resource[:staged_key_path], stagekey
+    FileUtils.cp @resource[:primary_key_path], primkey
+    FileUtils.chown_R uid, gid, ferndir
+    FileUtils.chmod_R 0600, ferndir
+    FileUtils.chmod 0700, ferndir
+  end
+
+  def ferndir
+    File.join(@resource[:keystone_dir], 'fernet-keys')
+
+  end
+
+  def stagekey
+     "#{ferndir}/#{@resource[:staged_key_name]}"
+  end
+
+  def primkey
+     "#{ferndir}/#{@resource[:primary_key_name]}"
+  end
+
+  def uid
+    Etc.getpwnam(@resource[:user]).uid
+  end
+
+  def gid
+    Etc.getpwnam(@resource[:user]).gid
+  end
+end
