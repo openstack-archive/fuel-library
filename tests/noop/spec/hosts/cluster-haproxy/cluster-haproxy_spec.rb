cce86176c069ac6e81b0e0c48c8edadb9719360b
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-07-13 13:56:36 +0300

    Allow root mysql within environment
    * Allow connections to mysql as root only
      from IP-addresses within environment
    * Updated direct_networks function
    * Provided and updated noop tests
    
    Closes-Bug: 1471956
    Change-Id: Ia873292d6b05f612642e055d1d0dc156a863bc34

--- tests/noop/spec/hosts/cluster-haproxy/cluster-haproxy_spec.rb
+++ tests/noop/spec/hosts/cluster-haproxy/cluster-haproxy_spec.rb
@@ -8,18 +8,31 @@ describe manifest do
     networks = []
     endpoints = Noop.hiera_structure 'network_scheme/endpoints'
     management_vip = Noop.hiera 'management_vip'
-    endpoints.each{ |k,v|
-      if v['IP'].is_a?(Array)
-        v['IP'].each { |ip|
-          networks << IPAddr.new(ip).to_s + '/' + ip.split('/')[1]
-        }
+    filter = nil
+
+    class IPAddr
+      def mask_length
+        @mask_addr.to_s(2).count '1'
       end
-      if v.has_key?('routes') and v['routes'].is_a?(Array)
-        v['routes'].each { |route|
-          networks << route['net']
-        }
+
+      def cidr
+        "#{to_s}/#{mask_length}"
       end
-    }
+    end
+
+    endpoints.each do |interface, parameters|
+      next unless parameters.has_key? 'IP' and parameters['IP'].is_a? Array
+      next if filter and interface != filter
+      parameters['IP'].each do |ip|
+        next unless ip
+        networks << IPAddr.new(ip).cidr
+      end
+      next unless parameters.has_key? 'routes' and parameters['routes'].is_a? Array
+      parameters['routes'].each do |route|
+        next unless route.has_key? 'net'
+        networks << IPAddr.new(route['net']).cidr
+      end
+    end
 
     it "should delcare cluster::haproxy with other_networks set to #{networks.join(' ')}" do
       should contain_class('cluster::haproxy').with(
@@ -39,4 +52,3 @@ describe manifest do
 
   test_ubuntu_and_centos manifest
 end
-
