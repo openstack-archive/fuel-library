3ac50919b7f8a4f9be6fb6463a3778473471ba3a
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-07-14 17:06:18 +0300

    Switch to haproxy::listen for stats
    Switch enabling of haproxy stats to haproxy::listen
    
    Change-Id: I5e3016c0a36edb8b58c569529ef93d92389b7b43
    Closes-Bug: 1465611

--- tests/noop/spec/hosts/openstack-haproxy/openstack-haproxy_spec.rb
+++ tests/noop/spec/hosts/openstack-haproxy/openstack-haproxy_spec.rb
@@ -3,6 +3,13 @@ require 'shared-examples'
 manifest = 'openstack-haproxy/openstack-haproxy.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+
+    it "should delcare openstack::ha::haproxy_service" do
+      should contain_openstack__ha__haproxy_service('stats')
+    end
+  end
+
   test_ubuntu_and_centos manifest
 end
 
