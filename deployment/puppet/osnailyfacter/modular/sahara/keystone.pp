334fb0894a4d6c10ea78318a9b6e04569af80e85
Denis Egorenko <degorenko@mirantis.com>
2015-07-15 20:06:32 +0300

    Adapt synced sahara module
    - add patch I542a8b6ed3aaf18e4b352f821939fc5f262aa5eb;
    - add patch Id3aa75229e8626c71d6cb8398af3c2c22f37b592;
    - add new noop tests;
    - adapt Sahara modular manifest;
    
    Closes-bug: #1425513
    Related-bug: #1425514
    Partial implements: blueprint separate-sahara-package
    Partial implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: I9b3f4b25db5fc3c3dfc727faa5d2c493bc748966

---
+++ deployment/puppet/osnailyfacter/modular/sahara/keystone.pp
@@ -0,0 +1,32 @@
+notice('MODULAR: sahara/keystone.pp')
+
+$sahara_hash      = hiera_hash('sahara_hash', {})
+$public_ssl_hash  = hiera('public_ssl')
+$public_address   = hiera('public_vip')
+$internal_address = hiera('management_vip', $public_address)
+
+$api_bind_port   = '8386'
+$sahara_user     = pick($sahara_hash['user'], 'sahara')
+$sahara_password = pick($sahara_hash['user_password'])
+$tenant          = pick($sahara_hash['tenant'], 'services')
+$region          = pick($sahara_hash['region'], 'RegionOne')
+$service_name    = pick($sahara_hash['service_name'], 'sahara')
+$public_protocol = $public_ssl_hash['services'] ? {
+  true    => 'https',
+  default => 'http',
+}
+$public_url      = "${public_protocol}://${public_address}:${api_bind_port}/v1.1/%(tenant_id)s"
+$admin_url       = "http://${internal_address}:${api_bind_port}/v1.1/%(tenant_id)s"
+$internal_url    = "http://${internal_address}:${api_bind_port}/v1.1/%(tenant_id)s"
+
+class { 'sahara::keystone::auth':
+  auth_name    => $sahara_user,
+  password     => $sahara_password,
+  service_type => 'data_processing',
+  service_name => $service_name,
+  region       => $region,
+  tenant       => $tenant,
+  public_url   => $public_url,
+  admin_url    => $admin_url,
+  internal_url => $internal_url,
+}
