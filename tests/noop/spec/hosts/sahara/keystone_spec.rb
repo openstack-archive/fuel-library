5c0e88e598513ef779c274bab5a3c275237f0f5f
Denis Egorenko <degorenko@mirantis.com>
2015-07-15 20:06:32 +0300

    Adapt synced sahara module
    - add patch I542a8b6ed3aaf18e4b352f821939fc5f262aa5eb;
    - add patch Id3aa75229e8626c71d6cb8398af3c2c22f37b592;
    - add new noop tests;
    - adapt Sahara modular manifest;
    
    Closes-bug: #1425513
    Related-bug: #1425514
    Partial implements: blueprint separate-sahara-package
    Partial implements: blueprint upgrade-openstack-puppet-modules
    
    Change-Id: I9b3f4b25db5fc3c3dfc727faa5d2c493bc748966

---
+++ tests/noop/spec/hosts/sahara/keystone_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'sahara/keystone.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+    public_address       = Noop.hiera('public_vip')
+    internal_address     = Noop.hiera('management_vip', public_address)
+    public_ssl           = Noop.hiera_structure('public_ssl/services')
+
+    api_bind_port   = '8386'
+    if public_ssl
+      public_protocol = 'https'
+    else
+      public_protocol = 'http'
+    end
+    sahara_user     = Noop.hiera_structure('sahara_hash/user', 'sahara')
+    sahara_password = Noop.hiera_structure('sahara_hash/user_password')
+    tenant          = Noop.hiera_structure('sahara_hash/tenant', 'services')
+    region          = Noop.hiera_structure('sahara_hash/region', 'RegionOne')
+    service_name    = Noop.hiera_structure('sahara_hash/service_name', 'sahara')
+    public_url      = "#{public_protocol}://#{public_address}:#{api_bind_port}/v1.1/%(tenant_id)s"
+    admin_url       = "http://#{internal_address}:#{api_bind_port}/v1.1/%(tenant_id)s"
+    internal_url    = "http://#{internal_address}:#{api_bind_port}/v1.1/%(tenant_id)s"
+
+    it 'should declare sahara::keystone::auth class correctly' do
+      should contain_class('sahara::keystone::auth').with(
+        'auth_name'    => sahara_user,
+        'password'     => sahara_password,
+        'service_type' => 'data_processing',
+        'service_name' => service_name,
+        'region'       => region,
+        'tenant'       => tenant,
+        'public_url'   => public_url,
+        'admin_url'    => admin_url,
+        'internal_url' => internal_url,
+      )
+    end
+  end
+  test_ubuntu_and_centos manifest
+end
