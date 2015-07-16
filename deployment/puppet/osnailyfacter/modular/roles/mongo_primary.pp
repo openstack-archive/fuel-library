f29a3864c0aa37fdae108a5c867f440aa3e95d24
Stanislav Makar <smakar@mirantis.com>
2015-07-16 10:21:35 +0000

    Adapt mongo to flexible networking
    Use *mongo/db* network role
    
    Change-Id: Ic61e5acf5a6aa7cd6a7aac41341679a457315265
    Implements: blueprint templates-for-networking

--- deployment/puppet/osnailyfacter/modular/roles/mongo_primary.pp
+++ deployment/puppet/osnailyfacter/modular/roles/mongo_primary.pp
@@ -1,8 +1,9 @@
 notice('MODULAR: mongo_primary.pp')
 
+prepare_network_config(hiera('network_scheme', {}))
+$bind_address     = get_network_role_property('mongo/db', 'ipaddr')
 $use_syslog       = hiera('use_syslog', true)
 $debug            = hiera('debug', false)
-$internal_address = hiera('internal_address')
 $ceilometer_hash  = hiera('ceilometer')
 $nodes_hash       = hiera('nodes')
 $roles            = node_roles($nodes_hash, hiera('uid'))
@@ -16,7 +17,7 @@ else {
 }
 
 class { 'openstack::mongo_primary':
-  mongodb_bind_address        => [ '127.0.0.1', $internal_address ],
+  mongodb_bind_address        => [ '127.0.0.1', $bind_address ],
   ceilometer_metering_secret  => $ceilometer_hash['metering_secret'],
   ceilometer_db_password      => $ceilometer_hash['db_password'],
   ceilometer_replset_members  => mongo_hosts($nodes_hash, 'array', 'mongo'),
