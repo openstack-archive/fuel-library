0f86232ca3d46e23b72cb289f79a156f4e1a1cfb
Alex Schultz <aschultz@mirantis.com>
2015-07-18 22:16:59 -0500

    Move saz-ssh to librarian
    This change removes our copy of ssh and replaces it with a version
    managed via librarian-puppet.
    
    Partial blueprint: fuel-puppet-librarian
    
    Change-Id: I92d49e1c8c8b99d6cfcfff8eeae9a356490e1e2e

--- deployment/puppet/ssh/manifests/server/match_block.pp
+++
@@ -1,7 +0,0 @@
-define ssh::server::match_block ($options, $type = 'user', $order = 50,) {
-  concat::fragment { "match_block ${name}":
-    target  => $ssh::params::sshd_config,
-    content => template("${module_name}/sshd_match_block.erb"),
-    order   => $order,
-  }
-}
