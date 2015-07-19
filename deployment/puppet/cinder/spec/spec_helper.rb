96a5ee84f62d0297f5db691bb9a882b72b247944
Alex Schultz <aschultz@mirantis.com>
2015-07-18 22:47:14 -0500

    Upgrade and Move openstack-cinder to librarian
    This change removes our copy of cinder and replaces it with a version
    managed via librarian-puppet.
    
    Partial blueprint: fuel-puppet-librarian
    
    Change-Id: Iee13fe6af140ad20bb9cd0d7081d77c6eb6a53fb

--- deployment/puppet/cinder/spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
-require 'shared_examples'
-
-RSpec.configure do |c|
-  c.alias_it_should_behave_like_to :it_configures, 'configures'
-  c.alias_it_should_behave_like_to :it_raises, 'raises'
-end
