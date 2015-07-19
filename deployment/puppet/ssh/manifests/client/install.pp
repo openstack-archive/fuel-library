eba2faeb651ada8528c0d43f29d314ae2a292e3c
Alex Schultz <aschultz@mirantis.com>
2015-07-18 22:16:59 -0500

    Move saz-ssh to librarian
    This change removes our copy of ssh and replaces it with a version
    managed via librarian-puppet.
    
    Partial blueprint: fuel-puppet-librarian
    
    Change-Id: I92d49e1c8c8b99d6cfcfff8eeae9a356490e1e2e

--- deployment/puppet/ssh/manifests/client/install.pp
+++
@@ -1,9 +0,0 @@
-class ssh::client::install {
-  if $ssh::params::client_package_name {
-    if !defined(Package[$ssh::params::client_package_name]) {
-      package { $ssh::params::client_package_name:
-        ensure => $ssh::client::ensure,
-      }
-    }
-  }
-}
