460f18baaae6d51e7810fe4d093eb97c9ad5faf6
vsaienko <vsaienko@mirantis.com>
2015-07-20 11:12:12 +0300

    Add haproxy services for Ironic
    This change adds new haproxy services needed by ironic:
    * ironic-api - on management and public VIPs
    * ironic-api - on baremetal VIP
    * swift-baremetal - proxy for swift servers on baremetal VIP
    * noop tests
    
    Added baremetal_vip to globals
    
    Co-Authored-By: Vasyl Saienko <vsaienko@mirantis.com>
    
    Partially Implements: blueprint fuel-integrate-ironic
    
    Change-Id: I5ced9e5706fcba9fc60a07771ae51538945895a9

--- deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-swift.pp
+++ deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-swift.pp
@@ -4,11 +4,18 @@ $network_metadata = hiera_hash('network_metadata')
 $storage_hash     = hiera_hash('storage', {})
 $swift_proxies    = hiera_hash('swift_proxies', undef)
 $public_ssl_hash  = hiera('public_ssl')
+$ironic_hash      = hiera_hash('ironic', {})
 
 if !($storage_hash['images_ceph'] and $storage_hash['objects_ceph']) and !$storage_hash['images_vcenter'] {
   $use_swift = true
 } else {
   $use_swift = false
+}
+
+if $ironic_hash['enabled'] {
+  $baremetal_virtual_ip = hiera('baremetal_vip')
+} else {
+  $baremetal_virtual_ip = false
 }
 
 $swift_proxies_address_map = get_node_to_ipaddr_map_by_network_role($swift_proxies, 'swift/api')
@@ -22,10 +29,11 @@ if ($use_swift) {
 
   # configure swift ha proxy
   class { '::openstack::ha::swift':
-    internal_virtual_ip => $internal_virtual_ip,
-    ipaddresses         => $ipaddresses,
-    public_virtual_ip   => $public_virtual_ip,
-    server_names        => $server_names,
-    public_ssl          => $public_ssl_hash['services'],
+    internal_virtual_ip  => $internal_virtual_ip,
+    ipaddresses          => $ipaddresses,
+    public_virtual_ip    => $public_virtual_ip,
+    server_names         => $server_names,
+    public_ssl           => $public_ssl_hash['services'],
+    baremetal_virtual_ip => $baremetal_virtual_ip,
   }
 }
