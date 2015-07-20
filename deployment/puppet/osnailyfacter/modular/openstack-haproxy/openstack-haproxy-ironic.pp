c2d61936ce0333504a0a5bafc72faee3e5a7b103
vsaienko <vsaienko@mirantis.com>
2015-07-20 11:12:12 +0300

    Add haproxy services for Ironic
    This change adds new haproxy services needed by ironic:
    * ironic-api - on management and public VIPs
    * ironic-api - on baremetal VIP
    * swift-baremetal - proxy for swift servers on baremetal VIP
    * noop tests
    
    Co-Authored-By: Vasyl Saienko <vsaienko@mirantis.com>
    
    Partially Implements: blueprint fuel-integrate-ironic
    
    Change-Id: I5ced9e5706fcba9fc60a07771ae51538945895a9

---
+++ deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-ironic.pp
@@ -0,0 +1,26 @@
+notice('MODULAR: openstack-haproxy-ironic.pp')
+
+$ironic_hash         = hiera_hash('ironic',{})
+# NOT enabled by default
+$use_ironic          = pick($ironic_hash['enabled'], false)
+
+if ($use_ironic) {
+  $haproxy_nodes        = pick(hiera('haproxy_nodes', undef),
+                               hiera('controllers', undef))
+  $server_names         = pick(hiera_array('ironic_names', undef),
+                               filter_hash($haproxy_nodes, 'name'))
+  $ipaddresses          = pick(hiera_array('ironic_ipaddresses', undef),
+                               filter_hash($haproxy_nodes, 'internal_address'))
+  $public_virtual_ip    = hiera('public_vip')
+  $internal_virtual_ip  = pick(hiera('service_endpoint', undef), hiera('management_vip'))
+  $baremetal_virtual_ip = hiera('baremetal_vip')
+
+# configure ironic ha proxy
+  class { '::openstack::ha::ironic':
+    internal_virtual_ip  => $internal_virtual_ip,
+    ipaddresses          => $ipaddresses,
+    public_virtual_ip    => $public_virtual_ip,
+    baremetal_virtual_ip => $baremetal_virtual_ip,
+    server_names         => $server_names,
+  }
+}
