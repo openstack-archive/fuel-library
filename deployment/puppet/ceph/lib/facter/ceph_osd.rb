e19e9086d3bab9b67734529f2aa92d3a4df9f100
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-07-20 13:29:09 +0000

    Support Ceph OSD devices with existing data set
    Partition preservation feature allows to preserve data on Ceph OSD device, but
    later on Puppet will run 'ceph-deploy prepare' on every Ceph device in a system.
    This call destroys data set on those devices.
    
    To preserve data on Ceph OSD devices through deployment process, we need to
    check if the device has Ceph data and if so, skip running  'ceph-deploy prepare'
    on that device.
    
    Change-Id: I667fa6aab9d6f46c73bfb8ca0e267afede6049fb
    Implements: blueprint partition-preservation

--- deployment/puppet/ceph/lib/facter/ceph_osd.rb
+++ deployment/puppet/ceph/lib/facter/ceph_osd.rb
@@ -22,7 +22,15 @@ Facter.add("osd_devices_list") do
                 when "4FBD7E29-9D25-41B8-AFD0-062C0CEFF05D"
                     # Only use unmounted devices
                     if %x{grep -c #{device}#{p} /proc/mounts}.to_i == 0
-                        osds << "#{device}#{p}"
+                        mp = %x{mktemp -d}.strip()
+                        begin
+                            mount_result = %x{mount #{device}#{p} #{mp} && test -f #{mp}/fsid && echo 0 || echo 1}.to_i
+                        rescue
+                        else
+                            osds << ["#{device}#{p}", !mount_result.zero?]
+                        ensure
+                            %x{umount -f #{mp}}
+                        end
                     end
                 when "45B0969E-9B03-4F30-B4C6-B4B80CEFF106"
                     if %x{grep -c #{device}#{p} /proc/mounts}.to_i == 0
@@ -32,21 +40,21 @@ Facter.add("osd_devices_list") do
             }
         }
 
-        if journals.length > 0
-          osds.each { |osd|
-            journal = journals.shift
-            if (not journal.nil?) && (not journal.empty?)
-              devlink = %x{udevadm info -q property -n #{journal} | awk 'BEGIN {FS="="} {if ($1 == "DEVLINKS") print $2}'}
-              devlink = devlink.split(' ')
-              journal = (devlink.find { |s| s.include? 'by-id' } or journal)
-              output << "#{osd}:#{journal}"
-            else
-              output << osd
-            end
-          }
-        else
-            output = osds
-        end
+        osds.each { |osd, prepare|
+          journal = journals.shift
+          if (not journal.nil?) && (not journal.empty?)
+            devlink = %x{udevadm info -q property -n #{journal} | awk 'BEGIN {FS="="} {if ($1 == "DEVLINKS") print $2}'}
+            devlink = devlink.split(' ')
+            journal = (devlink.find { |s| s.include? 'by-id' } or journal)
+            osd_disk = "#{osd}:#{journal}"
+          else
+            osd_disk = osd
+          end
+          if prepare == true
+              osd_disk += "!new"
+          end
+          output << osd_disk
+        }
         output.join(" ")
     end
 end
