4fe859a2146bdd4be4b6989c2086c30978a01fe3
Sergey Vasilenko <svasilenko@mirantis.com>
2015-07-21 15:21:08 +0300

    Separate nova live migration to nova/migration network role
    This should work, because Nova submit to *live_migration_uri*
    host name, which compute node registered in the Conductor.
    If we make additional nostnames with more specific IP addresses
    and make this resolvable.
    
    cfg.StrOpt('live_migration_uri',
               default="qemu+tcp://%s/system",
               help='Migration target URI '
                    '(any included "%s" is replaced with '
                    'the migration target hostname)'),
    that it will substitute %s with hostname and we get prefixed resolvable by hostname url.
    
    Blueprint: templates-for-networking
    Change-Id: I3e4654ff74382cd66136c1758f055ef4ac4f2109

--- deployment/puppet/osnailyfacter/modular/hosts/hosts.pp
+++ deployment/puppet/osnailyfacter/modular/hosts/hosts.pp
@@ -1,12 +1,14 @@
 notice('MODULAR: hosts.pp')
 
 $hosts_file = '/etc/hosts'
-$host_resources = network_metadata_to_hosts(hiera_hash('network_metadata'))
+$network_metadata = hiera_hash('network_metadata')
+$ordinary_host_resources = network_metadata_to_hosts($network_metadata)
+$live_migration_host_resources = network_metadata_to_hosts($network_metadata, 'nova/migration', hiera('compute_name_prefix_for_live_migration'))
 
 Host {
     ensure => present,
     target => $hosts_file
 }
 
-create_resources(host, $host_resources)
-
+create_resources(host, $ordinary_host_resources)
+create_resources(host, $live_migration_host_resources)
