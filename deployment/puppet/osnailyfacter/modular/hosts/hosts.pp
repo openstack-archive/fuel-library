67b507ea528eb1f04495d8360469b247520f478a
Sergey Vasilenko <svasilenko@mirantis.com>
2015-07-21 15:21:08 +0300

    Separate nova live migration to nova/migration network role
    This should work, because Nova submit to *live_migration_uri*
    host name, which compute node registered in the Conductor.
    If we make additional nostnames with more specific IP addresses
    and make this resolvable -- it should work.
    
    Blueprint: templates-for-networking
    Change-Id: I3e4654ff74382cd66136c1758f055ef4ac4f2109

--- deployment/puppet/osnailyfacter/modular/hosts/hosts.pp
+++ deployment/puppet/osnailyfacter/modular/hosts/hosts.pp
@@ -1,5 +1,15 @@
 notice('MODULAR: hosts.pp')
 
-class { "l23network::hosts_file":
-  nodes => hiera('nodes'),
+$network_metadata = hiera_hash('network_metadata')
+
+osnailyfacter::hosts_file { 'all_nodes':
+  nodes_hash   => $network_metadata['nodes'],
+  network_role => 'mgmt/vip'
+}
+
+osnailyfacter::hosts_file { 'computes_for_live_migration':
+  nodes_hash   => get_nodes_hash_by_roles($network_metadata, ['compute']),
+  network_role => 'nova/migration',
+  name_prefix  => hiera('compute_name_prefix_for_live_migration'),
+  aliases      => false,
 }
