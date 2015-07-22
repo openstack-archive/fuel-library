014c09f7b9201a593c006945fe4817008f1185e4
Stanislav Makar <smakar@mirantis.com>
2015-07-22 11:41:30 +0000

    Use mongo_roles instead of mongo_nodes
    * Allow to change easliy mongo_roles in plugins.
    * Clean up not needed variables.
    
    Change-Id: I0f9bb79fff125bfeac50e73b05c26a68b2565e4b
    Implements: blueprint templates-for-networking
    Related-Blueprint: detach-components-from-controllers

--- deployment/puppet/osnailyfacter/modular/roles/mongo_primary.pp
+++ deployment/puppet/osnailyfacter/modular/roles/mongo_primary.pp
@@ -1,11 +1,12 @@
 notice('MODULAR: mongo_primary.pp')
 
 prepare_network_config(hiera('network_scheme', {}))
-$mongo_address_map = get_node_to_ipaddr_map_by_network_role(hiera_hash('mongo_nodes'), 'mongo/db')
+$mongo_nodes       = get_nodes_hash_by_roles(hiera('network_metadata'), hiera('mongo_roles'))
+$mongo_address_map = get_node_to_ipaddr_map_by_network_role($mongo_nodes, 'mongo/db')
 $bind_address      = get_network_role_property('mongo/db', 'ipaddr')
 $use_syslog        = hiera('use_syslog', true)
 $debug             = hiera('debug', false)
-$ceilometer_hash   = hiera('ceilometer')
+$ceilometer_hash   = hiera_hash('ceilometer_hash')
 $roles             = hiera('roles')
 $replset_name      = 'ceilometer'
 $mongodb_port      = hiera('mongodb_port', '27017')
