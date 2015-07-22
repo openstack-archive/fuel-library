fae94c288ecd98cceee39e7a9fd51c4c47a91fa9
Stanislav Makar <smakar@mirantis.com>
2015-07-22 11:41:30 +0000

    Use mongo_roles instead of mongo_nodes
    * Allow to change easliy mongo_roles in plugins.
    * Clean up not needed variables.
    
    Change-Id: I0f9bb79fff125bfeac50e73b05c26a68b2565e4b
    Implements: blueprint templates-for-networking
    Related-Blueprint: detach-components-from-controllers

--- deployment/puppet/osnailyfacter/modular/roles/mongo.pp
+++ deployment/puppet/osnailyfacter/modular/roles/mongo.pp
@@ -1,7 +1,6 @@
 notice('MODULAR: mongo.pp')
 
 prepare_network_config(hiera('network_scheme', {}))
-$mongo_address_map = get_node_to_ipaddr_map_by_network_role(hiera_hash('mongo_nodes'), 'mongo/db')
 $bind_address      = get_network_role_property('mongo/db', 'ipaddr')
 $use_syslog        = hiera('use_syslog', true)
 $debug             = hiera('debug', false)
