cf44a2c93f18825d752197d81bdfb19ab0be4684
Alex Schultz <aschultz@mirantis.com>
2015-07-22 16:23:37 -0500

    Install rubygem-deep_merge as part of hiera task
    This change adds the installation of the deep_merge package as part of
    the hiera configuration to enable the deeper merge behaviour.
    
    Change-Id: I62dd9f612e1533d0406affa5d1a820680ba2a509
    Related-Bug: 1462528
    Related-Bug: 1464766

--- tests/noop/spec/hosts/hiera/hiera_spec.rb
+++ tests/noop/spec/hosts/hiera/hiera_spec.rb
@@ -3,6 +3,45 @@ require 'shared-examples'
 manifest = 'hiera/hiera.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+    it 'should setup hiera' do
+      should contain_file('hiera_data_dir').with(
+        'ensure' => 'directory',
+        'path'   => '/etc/hiera'
+      )
+      should contain_file('hiera_config').with(
+        'ensure' => 'present',
+        'path'   => '/etc/hiera.yaml'
+      )
+      # ensure deeper merge_behavior is being set
+      should contain_file('hiera_config').with_content(/deeper/)
+      # check symlinks
+      should contain_file('hiera_data_astute').with(
+        'ensure' => 'symlink',
+        'path'   => '/etc/hiera/astute.yaml',
+        'target' => '/etc/astute.yaml'
+      )
+      should contain_file('hiera_puppet_config').with(
+        'ensure' => 'symlink',
+        'path'   => '/etc/puppet/hiera.yaml',
+        'target' => '/etc/hiera.yaml'
+      )
+    end
+    it 'should have ruby deep_merge installed' do
+      case facts[:operatingsystem]
+      when 'Ubuntu'
+        package_name = 'ruby-deep-merge'
+      when 'CentOS'
+        package_name = 'rubygem-deep_merge'
+      end
+
+      should contain_package('rubygem-deep_merge').with(
+        'ensure' => 'present',
+        'name'   => package_name
+      )
+    end
+  end
+
   test_ubuntu_and_centos manifest
 end
 
