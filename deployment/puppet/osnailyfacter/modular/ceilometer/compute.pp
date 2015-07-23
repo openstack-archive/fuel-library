2bcc90aad4b8b87c51f225b7a3b9f7c2e5d35407
Denis Egorenko <degorenko@mirantis.com>
2015-07-23 14:24:48 +0300

    Do not run modular Ceilo tasks when Ceilometer disabled
    For creating keystone endpoint was missed condition in tasks.yaml
    for Ceilomoter, which checks enabled Ceilometer or not.
    
    Also add this condition to anothers Ceilometer modular manifests
    and remove 'if' checks.
    
    Change-Id: Ie90607d7e910375a2d470bb9fba5290042d56330
    Closes-bug: #1477508

--- deployment/puppet/osnailyfacter/modular/ceilometer/compute.pp
+++ deployment/puppet/osnailyfacter/modular/ceilometer/compute.pp
@@ -17,35 +17,32 @@ $default_ceilometer_hash = {
 $region                     = hiera('region', 'RegionOne')
 $ceilometer_hash            = hiera_hash('ceilometer_hash', $default_ceilometer_hash)
 $ceilometer_region          = pick($ceilometer_hash['region'], $region)
-$ceilometer_enabled         = $ceilometer_hash['enabled']
 $amqp_password              = $rabbit_hash['password']
 $amqp_user                  = $rabbit_hash['user']
 $service_endpoint           = $management_vip
 $ceilometer_user_password   = $ceilometer_hash['user_password']
 $ceilometer_metering_secret = $ceilometer_hash['metering_secret']
 
-if ($ceilometer_enabled) {
-  class { 'openstack::ceilometer':
-    verbose                        => $verbose,
-    debug                          => $debug,
-    use_syslog                     => $use_syslog,
-    syslog_log_facility            => $syslog_log_facility,
-    amqp_hosts                     => hiera('amqp_hosts',''),
-    amqp_user                      => $amqp_user,
-    amqp_password                  => $amqp_password,
-    keystone_user                  => $ceilometer_hash['user'],
-    keystone_tenant                => $ceilometer_hash['tenant'],
-    keystone_region                => $ceilometer_region,
-    keystone_host                  => $service_endpoint,
-    keystone_password              => $ceilometer_user_password,
-    on_compute                     => true,
-    metering_secret                => $ceilometer_metering_secret,
-  }
+class { 'openstack::ceilometer':
+  verbose                        => $verbose,
+  debug                          => $debug,
+  use_syslog                     => $use_syslog,
+  syslog_log_facility            => $syslog_log_facility,
+  amqp_hosts                     => hiera('amqp_hosts',''),
+  amqp_user                      => $amqp_user,
+  amqp_password                  => $amqp_password,
+  keystone_user                  => $ceilometer_hash['user'],
+  keystone_tenant                => $ceilometer_hash['tenant'],
+  keystone_region                => $ceilometer_region,
+  keystone_host                  => $service_endpoint,
+  keystone_password              => $ceilometer_user_password,
+  on_compute                     => true,
+  metering_secret                => $ceilometer_metering_secret,
+}
 
-  # We need to restart nova-compute service in orderto apply new settings
-  include ::nova::params
-  service { 'nova-compute':
-    ensure => 'running',
-    name   => $::nova::params::compute_service_name,
-  }
+# We need to restart nova-compute service in orderto apply new settings
+include ::nova::params
+service { 'nova-compute':
+  ensure => 'running',
+  name   => $::nova::params::compute_service_name,
 }
--- deployment/puppet/osnailyfacter/modular/ceilometer/controller.pp
+++ deployment/puppet/osnailyfacter/modular/ceilometer/controller.pp
@@ -28,7 +28,7 @@ $default_mongo_hash = {
 
 $mongo_hash               = hiera_hash('mongo', $default_mongo_hash)
 
-if $mongo_hash['enabled'] and $ceilometer_hash['enabled'] {
+if $mongo_hash['enabled'] {
   $exteranl_mongo_hash    = hiera_hash('external_mongo')
   $ceilometer_db_user     = $exteranl_mongo_hash['mongo_user']
   $ceilometer_db_password = $exteranl_mongo_hash['mongo_password']
@@ -42,7 +42,6 @@ if $mongo_hash['enabled'] and $ceilometer_hash['enabled'] {
   $exteranl_mongo_hash    = {}
 }
 
-$ceilometer_enabled         = $ceilometer_hash['enabled']
 $ceilometer_user_password   = $ceilometer_hash['user_password']
 $ceilometer_metering_secret = $ceilometer_hash['metering_secret']
 $ceilometer_db_type         = 'mongodb'
@@ -56,49 +55,45 @@ $ha_mode                    = pick($ceilometer_hash['ha_mode'], true)
 prepare_network_config(hiera('network_scheme', {}))
 $api_bind_address           = get_network_role_property('ceilometer/api', 'ipaddr')
 
-if $ceilometer_hash['enabled'] {
-  if $external_mongo {
-    $mongo_hosts = $exteranl_mongo_hash['hosts_ip']
-    if $exteranl_mongo_hash['mongo_replset'] {
-      $mongo_replicaset = $exteranl_mongo_hash['mongo_replset']
-    } else {
-      $mongo_replicaset = undef
-    }
+if $external_mongo {
+  $mongo_hosts = $exteranl_mongo_hash['hosts_ip']
+  if $exteranl_mongo_hash['mongo_replset'] {
+    $mongo_replicaset = $exteranl_mongo_hash['mongo_replset']
   } else {
-    $mongo_hosts = join(values($mongo_address_map), ',')
-    # MongoDB is alsways configured with replica set
-    $mongo_replicaset = 'ceilometer'
+    $mongo_replicaset = undef
   }
+} else {
+  $mongo_hosts = join(values($mongo_address_map), ',')
+  # MongoDB is alsways configured with replica set
+  $mongo_replicaset = 'ceilometer'
 }
 
 ###############################################################################
 
-if ($ceilometer_enabled) {
-  class { 'openstack::ceilometer':
-    verbose              => $verbose,
-    debug                => $debug,
-    use_syslog           => $use_syslog,
-    syslog_log_facility  => $syslog_log_facility,
-    db_type              => $ceilometer_db_type,
-    db_host              => $mongo_hosts,
-    db_user              => $ceilometer_db_user,
-    db_password          => $ceilometer_db_password,
-    db_dbname            => $ceilometer_db_dbname,
-    swift_rados_backend  => $swift_rados_backend,
-    metering_secret      => $ceilometer_metering_secret,
-    amqp_hosts           => hiera('amqp_hosts',''),
-    amqp_user            => $amqp_user,
-    amqp_password        => $amqp_password,
-    rabbit_ha_queues     => $rabbit_ha_queues,
-    keystone_host        => $service_endpoint,
-    keystone_password    => $ceilometer_user_password,
-    keystone_user        => $ceilometer_hash['user'],
-    keystone_tenant      => $ceilometer_hash['tenant'],
-    keystone_region      => $ceilometer_region,
-    host                 => $api_bind_address,
-    ha_mode              => $ha_mode,
-    on_controller        => true,
-    ext_mongo            => $external_mongo,
-    mongo_replicaset     => $mongo_replicaset,
-  }
+class { 'openstack::ceilometer':
+  verbose             => $verbose,
+  debug               => $debug,
+  use_syslog          => $use_syslog,
+  syslog_log_facility => $syslog_log_facility,
+  db_type             => $ceilometer_db_type,
+  db_host             => $mongo_hosts,
+  db_user             => $ceilometer_db_user,
+  db_password         => $ceilometer_db_password,
+  db_dbname           => $ceilometer_db_dbname,
+  swift_rados_backend => $swift_rados_backend,
+  metering_secret     => $ceilometer_metering_secret,
+  amqp_hosts          => hiera('amqp_hosts',''),
+  amqp_user           => $amqp_user,
+  amqp_password       => $amqp_password,
+  rabbit_ha_queues    => $rabbit_ha_queues,
+  keystone_host       => $service_endpoint,
+  keystone_password   => $ceilometer_user_password,
+  keystone_user       => $ceilometer_hash['user'],
+  keystone_tenant     => $ceilometer_hash['tenant'],
+  keystone_region     => $ceilometer_region,
+  host                => $api_bind_address,
+  ha_mode             => $ha_mode,
+  on_controller       => true,
+  ext_mongo           => $external_mongo,
+  mongo_replicaset    => $mongo_replicaset,
 }
