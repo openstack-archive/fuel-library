a5febcaed4564ea85e8052306da9c828892e6948
Alex Schultz <aschultz@mirantis.com>
2015-07-27 12:18:50 -0500

    Updating rabbitmq log levels
    The log_levels configuration format has changed so this change updates
    our settings to be inline with the newer format. This change also adds
    a test to confirm the settings are being set for the new format.
    
    Change-Id: I55b23c880dad2251cd360686429052d316d42470
    Closes-Bug: 1477595

--- tests/noop/spec/hosts/rabbitmq/rabbitmq_spec.rb
+++ tests/noop/spec/hosts/rabbitmq/rabbitmq_spec.rb
@@ -3,6 +3,18 @@ require 'shared-examples'
 manifest = 'rabbitmq/rabbitmq.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+    # LP#1477595
+    it "should contain rabbitmq correct log levels" do
+      debug = Noop.hiera('debug', false)
+      if debug
+        log_levels = '{connection,debug}'
+      else
+        log_levels = '{connection,info}'
+      end
+      should contain_class('rabbitmq').with_config_variables(/#{log_levels}/)
+    end
+  end
   test_ubuntu_and_centos manifest
 end
 
