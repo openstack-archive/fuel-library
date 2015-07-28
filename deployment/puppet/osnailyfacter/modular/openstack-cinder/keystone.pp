4e7a28c806dc7d9193d46c6b9c4ff4830db3b539
vsaienko <vsaienko@mirantis.com>
2015-07-28 10:25:58 +0300

    replace keystone deprecated parameters
     * replace {public|admin|internal}_{address|protocol}
       with {public|admin}_url
     * replace keystone_auth_protocol,keystone_auth_host and
       service_port with auth_uri and identity_uri
     * add {public|internal|admin}_url_v2 as they were
       configured by default
     * add noop tests
    
    Related-Bug: #1477967
    
    Change-Id: Iabc6f1dd8946286f0130fa64ef9441385c28a2fd

--- deployment/puppet/osnailyfacter/modular/openstack-cinder/keystone.pp
+++ deployment/puppet/osnailyfacter/modular/openstack-cinder/keystone.pp
@@ -11,7 +11,10 @@ $public_protocol     = $public_ssl_hash['services'] ? {
   true    => 'https',
   default => 'http',
 }
+$admin_protocol      = 'http'
 $admin_address       = hiera('management_vip')
+$internal_protocol   = $admin_protocol
+$internal_address    = $admin_address
 $region              = pick($cinder_hash['region'], 'RegionOne')
 
 $password            = $cinder_hash['user_password']
@@ -21,6 +24,16 @@ $configure_user      = pick($cinder_hash['configure_user'], true)
 $configure_user_role = pick($cinder_hash['configure_user_role'], true)
 $service_name        = pick($cinder_hash['service_name'], 'cinder')
 $tenant              = pick($cinder_hash['tenant'], 'services')
+
+$port = '8776'
+
+$public_url      = "${public_protocol}://${public_address}:${port}/v1/%(tenant_id)s"
+$internal_url    = "${internal_protocol}://${internal_address}:${port}/v1/%(tenant_id)s"
+$admin_url       = "${admin_protocol}://${admin_address}:${port}/v1/%(tenant_id)s"
+
+$public_url_v2   = "${public_protocol}://${public_address}:${port}/v2/%(tenant_id)s"
+$internal_url_v2 = "${internal_protocol}://${internal_address}:${port}/v2/%(tenant_id)s"
+$admin_url_v2    = "${admin_protocol}://${admin_address}:${port}/v2/%(tenant_id)s"
 
 validate_string($public_address)
 validate_string($password)
@@ -32,9 +45,11 @@ class { '::cinder::keystone::auth':
   configure_user      => $configure_user,
   configure_user_role => $configure_user_role,
   service_name        => $service_name,
-  public_address      => $public_address,
-  public_protocol     => $public_protocol,
-  admin_address       => $admin_address,
-  internal_address    => $admin_address,
+  public_url          => $public_url,
+  internal_url        => $internal_url,
+  admin_url           => $admin_url,
+  public_url_v2       => $public_url_v2,
+  internal_url_v2     => $internal_url_v2,
+  admin_url_v2        => $admin_url_v2,
   region              => $region,
 }
