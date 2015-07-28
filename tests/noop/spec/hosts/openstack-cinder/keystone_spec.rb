de96954f43c2f32d4059a44331948d8fbf725491
vsaienko <vsaienko@mirantis.com>
2015-07-28 10:25:58 +0300

    replace keystone deprecated parameters
     * replace {public|admin|internal}_{address|protocol}
       with {public|admin}_url
     * replace keystone_auth_protocol,keystone_auth_host and
       service_port with auth_uri and identity_uri
     * add {public|internal|admin}_url_v2 as they were
       configured by default
     * add noop tests
    
    Related-Bug: #1477967
    
    Change-Id: Iabc6f1dd8946286f0130fa64ef9441385c28a2fd

--- tests/noop/spec/hosts/openstack-cinder/keystone_spec.rb
+++ tests/noop/spec/hosts/openstack-cinder/keystone_spec.rb
@@ -8,16 +8,36 @@ describe manifest do
       contain_class('cinder::keystone::auth')
     end
 
-  public_vip           = Noop.hiera('public_vip')
-  public_ssl           = Noop.hiera_structure('public_ssl/services')
+    public_vip = Noop.hiera('public_vip')
+    public_ssl = Noop.hiera_structure('public_ssl/services')
 
-  if public_ssl
-    public_address  = Noop.hiera_structure('public_ssl/hostname')
-  else
-    public_address  = public_vip
-  end
+    if public_ssl
+      public_address  = Noop.hiera_structure('public_ssl/hostname')
+      public_protocol = 'https'
+    else
+      public_address  = public_vip
+      public_protocol = 'http'
+    end
 
-  end
+    admin_address    = Noop.hiera('management_vip')
+    internal_address = admin_address
+
+    public_url   = "#{public_protocol}://#{public_address}:8776/v1/%(tenant_id)s"
+    admin_url    = "http://#{admin_address}:8776/v1/%(tenant_id)s"
+
+    public_url_v2   = "#{public_protocol}://#{public_address}:8776/v2/%(tenant_id)s"
+    admin_url_v2    = "http://#{admin_address}:8776/v2/%(tenant_id)s"
+
+    it 'class cinder::keystone::auth should  contain correct *_url' do
+      should contain_class('cinder::keystone::auth').with('public_url' => public_url)
+      should contain_class('cinder::keystone::auth').with('admin_url' => admin_url)
+      should contain_class('cinder::keystone::auth').with('internal_url' => admin_url)
+      should contain_class('cinder::keystone::auth').with('public_url_v2' => public_url_v2)
+      should contain_class('cinder::keystone::auth').with('admin_url_v2' => admin_url_v2)
+      should contain_class('cinder::keystone::auth').with('internal_url_v2' => admin_url_v2)
+    end
+
+  end #shared manifests
 
   test_ubuntu_and_centos manifest
 end
