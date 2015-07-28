5e4ca0d9722f1e74c2af454700221854689fab6a
Maksym Yatsenko <myatsenko@mirantis.com>
2015-07-28 14:15:23 +0300

    Fix using of public address with SSL for Heat
    Heat public endpoint should use public ssl hostname instead ip
    address.
    
    Change-Id: Id7c5dba88a573c5460a9c253074a78ecad09f976
    Closes-Bug: #1478649

--- tests/noop/spec/hosts/heat/keystone_spec.rb
+++ tests/noop/spec/hosts/heat/keystone_spec.rb
@@ -9,6 +9,20 @@ describe manifest do
         'trusts_delegated_roles' => [],
       )
     end
+
+  public_vip           = Noop.hiera('public_vip')
+  public_ssl           = Noop.hiera_structure('public_ssl/services')
+
+  api_bind_port   = '8004'
+
+  if public_ssl
+    public_address  = Noop.hiera_structure('public_ssl/hostname')
+    public_protocol = 'https'
+  else
+    public_address  = public_vip
+    public_protocol = 'https'
+  end
+  public_url      = "#{public_protocol}://#{public_address}:#{api_bind_port}/v1/%(tenant_id)s"
   end
 
   test_ubuntu_and_centos manifest
