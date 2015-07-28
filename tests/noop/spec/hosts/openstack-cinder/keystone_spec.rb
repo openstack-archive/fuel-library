c04db69199c92b2903a064c1c823ccb28ad9cf82
vsaienko <vsaienko@mirantis.com>
2015-07-28 12:58:55 +0300

    Fix using of public address with SSL for Cinder
     * Cinder public endpoint should use hostname
       instead of ip.
    
    Closes-Bug: #1478649
    
    Change-Id: Ife533fa514241b98f75173620ac01be909abe39f

--- tests/noop/spec/hosts/openstack-cinder/keystone_spec.rb
+++ tests/noop/spec/hosts/openstack-cinder/keystone_spec.rb
@@ -7,6 +7,16 @@ describe manifest do
     it 'should set empty trusts_delegated_roles for cinder auth' do
       contain_class('cinder::keystone::auth')
     end
+
+  public_vip           = Noop.hiera('public_vip')
+  public_ssl           = Noop.hiera_structure('public_ssl/services')
+
+  if public_ssl
+    public_address  = Noop.hiera_structure('public_ssl/hostname')
+  else
+    public_address  = public_vip
+  end
+
   end
 
   test_ubuntu_and_centos manifest
