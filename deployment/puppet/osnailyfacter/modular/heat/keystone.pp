b23b1c2f83ee89a8e3afed6b7ad298523b5ddb8e
Maksym Yatsenko <myatsenko@mirantis.com>
2015-07-28 14:15:23 +0300

    Fix using of public address with SSL for Heat
    Heat public endpoint should use public ssl hostname instead ip
    address.
    
    Closes-Bug: #1478649
    
    Change-Id: Id7c5dba88a573c5460a9c253074a78ecad09f976

--- deployment/puppet/osnailyfacter/modular/heat/keystone.pp
+++ deployment/puppet/osnailyfacter/modular/heat/keystone.pp
@@ -1,10 +1,14 @@
 notice('MODULAR: heat/keystone.pp')
 
 $heat_hash         = hiera_hash('heat', {})
-$public_address    = hiera('public_vip')
+$public_vip        = hiera('public_vip')
 $admin_address     = hiera('management_vip')
 $region            = pick($heat_hash['region'], 'RegionOne')
 $public_ssl_hash   = hiera('public_ssl')
+$public_address    = $public_ssl_hash['services'] ? {
+  true    => $public_ssl_hash['hostname'],
+  default => $public_vip,
+}
 $public_protocol   = $public_ssl_hash['services'] ? {
   true    => 'https',
   default => 'http',
@@ -21,38 +25,35 @@ $tenant              = pick($heat_hash['tenant'], 'services')
 validate_string($public_address)
 validate_string($password)
 
+$public_url          = "${public_protocol}://${public_address}:8004/v1/%(tenant_id)s"
+$admin_url           = "http://${admin_address}:8004/v1/%(tenant_id)s"
+$public_url_cfn      = "${public_protocol}://${public_address}:8000/v1"
+$admin_url_cfn       = "http://${admin_address}:8000/v1"
+
+
+
 class { '::heat::keystone::auth' :
   password               => $password,
   auth_name              => $auth_name,
-  public_address         => $public_address,
-  admin_address          => $admin_address,
-  internal_address       => $admin_address,
-  port                   => '8004',
-  version                => 'v1',
   region                 => $region,
   tenant                 => $keystone_tenant,
   email                  => "${auth_name}@localhost",
-  public_protocol        => $public_protocol,
-  admin_protocol         => 'http',
-  internal_protocol      => 'http',
   configure_endpoint     => true,
   trusts_delegated_roles => $trusts_delegated_roles,
+  public_url             => $public_url,
+  internal_url           => $admin_url,
+  admin_url              => $admin_url,
 }
 
 class { '::heat::keystone::auth_cfn' :
   password           => $password,
   auth_name          => "${auth_name}-cfn",
   service_type       => 'cloudformation',
-  public_address     => $public_address,
-  admin_address      => $admin_address,
-  internal_address   => $admin_address,
-  port               => '8000',
-  version            => 'v1',
   region             => $region,
   tenant             => $keystone_tenant,
   email              => "${auth_name}-cfn@localhost",
-  public_protocol    => $public_protocol,
-  admin_protocol     => 'http',
-  internal_protocol  => 'http',
   configure_endpoint => true,
+  public_url         => $public_url_cfn,
+  internal_url       => $admin_url_cfn,
+  admin_url          => $admin_url_cfn,
 }
