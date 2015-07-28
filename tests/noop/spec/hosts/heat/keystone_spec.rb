5a32347710fe7f0e0e4174f612c79e03a3885d72
Maksym Yatsenko <myatsenko@mirantis.com>
2015-07-28 14:15:23 +0300

    Fix using of public address with SSL for Heat
    Heat public endpoint should use public ssl hostname instead ip
    address.
    
    Closes-Bug: #1478649
    
    Change-Id: Id7c5dba88a573c5460a9c253074a78ecad09f976

--- tests/noop/spec/hosts/heat/keystone_spec.rb
+++ tests/noop/spec/hosts/heat/keystone_spec.rb
@@ -9,6 +9,39 @@ describe manifest do
         'trusts_delegated_roles' => [],
       )
     end
+
+  public_vip           = Noop.hiera('public_vip')
+  admin_address        = Noop.hiera('management_vip')
+  public_ssl           = Noop.hiera_structure('public_ssl/services')
+
+  if public_ssl
+    public_address  = Noop.hiera_structure('public_ssl/hostname')
+    public_protocol = 'https'
+  else
+    public_address  = public_vip
+    public_protocol = 'https'
+  end
+
+  public_url          = "#{public_protocol}://#{public_address}:8004/v1/%(tenant_id)s"
+  admin_url           = "http://#{admin_address}:8004/v1/%(tenant_id)s"
+  public_url_cfn      = "#{public_protocol}://#{public_address}:8000/v1"
+  admin_url_cfn       = "http://#{admin_address}:8000/v1"
+
+
+  it 'class heat::keystone::auth should contain correct *_url' do
+    should contain_class('heat::keystone::auth').with('public_url' => public_url)
+    should contain_class('heat::keystone::auth').with('admin_url' => admin_url)
+    should contain_class('heat::keystone::auth').with('internal_url' => admin_url)
+  end
+
+  it 'class heat::keystone::auth_cfn should contain correct *_url' do
+    should contain_class('heat::keystone::auth_cfn').with('public_url' => public_url_cfn)
+    should contain_class('heat::keystone::auth_cfn').with('admin_url' => admin_url_cfn)
+    should contain_class('heat::keystone::auth_cfn').with('internal_url' => admin_url_cfn)
+  end
+
+
+
   end
 
   test_ubuntu_and_centos manifest
