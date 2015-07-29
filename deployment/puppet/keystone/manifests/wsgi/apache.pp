214e1643aa73f3ffa23d8d008fc9b20eb5777bfb
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-07-29 18:15:04 +0300

    Extends LimitRequestFieldSize to 81900
    Extends LimitRequestFieldSize to 81900 for
    keystone apache vhost
    
    Change-Id: Ibfa120d3bd3051f0e619bd6d245ae2cd4e12cbaf
    Closes-Bug: 1475767

--- deployment/puppet/keystone/manifests/wsgi/apache.pp
+++ deployment/puppet/keystone/manifests/wsgi/apache.pp
@@ -89,6 +89,11 @@
 #     (optional) Wsgi script source.
 #     Defaults to undef.
 #
+#   [*vhost_custom_fragment*]
+#     (optional) Passes a string of custom configuration
+#     directives to be placed at the end of the vhost configuration.
+#     Defaults to undef.
+#
 # == Dependencies
 #
 #   requires Class['apache'] & Class['keystone']
@@ -113,25 +118,26 @@
 #   Copyright 2013 eNovance <licensing@enovance.com>
 #
 class keystone::wsgi::apache (
-  $servername         = $::fqdn,
-  $public_port        = 5000,
-  $admin_port         = 35357,
-  $bind_host          = undef,
-  $public_path        = '/',
-  $admin_path         = '/',
-  $ssl                = true,
-  $workers            = 1,
-  $ssl_cert           = undef,
-  $ssl_key            = undef,
-  $ssl_chain          = undef,
-  $ssl_ca             = undef,
-  $ssl_crl_path       = undef,
-  $ssl_crl            = undef,
-  $ssl_certs_dir      = undef,
-  $threads            = $::processorcount,
-  $priority           = '10',
-  $wsgi_script_ensure = 'file',
-  $wsgi_script_source = undef,
+  $servername            = $::fqdn,
+  $public_port           = 5000,
+  $admin_port            = 35357,
+  $bind_host             = undef,
+  $public_path           = '/',
+  $admin_path            = '/',
+  $ssl                   = true,
+  $workers               = 1,
+  $ssl_cert              = undef,
+  $ssl_key               = undef,
+  $ssl_chain             = undef,
+  $ssl_ca                = undef,
+  $ssl_crl_path          = undef,
+  $ssl_crl               = undef,
+  $ssl_certs_dir         = undef,
+  $threads               = $::processorcount,
+  $priority              = '10',
+  $wsgi_script_ensure    = 'file',
+  $wsgi_script_source    = undef,
+  $vhost_custom_fragment = undef,
 ) {
 
   include ::keystone::params
@@ -244,6 +250,7 @@ class keystone::wsgi::apache (
     wsgi_daemon_process_options => $wsgi_daemon_process_options_main,
     wsgi_process_group          => 'keystone_main',
     wsgi_script_aliases         => $wsgi_script_aliases_main_real,
+    custom_fragment             => $vhost_custom_fragment,
     require                     => File['keystone_wsgi_main'],
   }
 
@@ -269,6 +276,7 @@ class keystone::wsgi::apache (
       wsgi_daemon_process_options => $wsgi_daemon_process_options_admin,
       wsgi_process_group          => 'keystone_admin',
       wsgi_script_aliases         => $wsgi_script_aliases_admin,
+      custom_fragment             => $vhost_custom_fragment,
       require                     => File['keystone_wsgi_admin'],
     }
   }
