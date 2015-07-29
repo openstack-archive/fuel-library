b1b44aad2981f4ae38cdf58dc29aedf16e232a52
Maksym Yatsenko <myatsenko@mirantis.com>
2015-07-29 10:53:45 +0300

    Fix using of public address with SSL for Swift
    * Swift public endpoint should use public ssl hostname instead ip
    address
    * Replace {public|admin|internal}_address, public_protocol with {public|admin}_url
    * Update noop tests
    
    Closes-Bug: #1478649
    
    Change-Id: I09200a93b69d8f0f7565ef23cb3928d43df1c102

--- tests/noop/spec/hosts/swift/keystone_spec.rb
+++ tests/noop/spec/hosts/swift/keystone_spec.rb
@@ -7,6 +7,28 @@ describe manifest do
     it 'should set empty trusts_delegated_roles for swift auth' do
       contain_class('swift::keystone::auth')
     end
+
+  public_vip           = Noop.hiera('public_vip')
+  admin_address        = Noop.hiera('management_vip')
+  public_ssl           = Noop.hiera_structure('public_ssl/services')
+
+  if public_ssl
+    public_address  = Noop.hiera_structure('public_ssl/hostname')
+    public_protocol = 'https'
+  else
+    public_address  = public_vip
+    public_protocol = 'https'
+  end
+
+  public_url          = "#{public_protocol}://#{public_address}:8080/v1/AUTH_%(tenant_id)s"
+  admin_url           = "http://#{admin_address}:8080/v1/AUTH_%(tenant_id)s"
+
+  it 'class swift::keystone::auth should contain correct *_url' do
+    should contain_class('swift::keystone::auth').with('public_url' => public_url)
+    should contain_class('swift::keystone::auth').with('admin_url' => admin_url)
+    should contain_class('swift::keystone::auth').with('internal_url' => admin_url)
+  end
+
   end
 
   test_ubuntu_and_centos manifest
