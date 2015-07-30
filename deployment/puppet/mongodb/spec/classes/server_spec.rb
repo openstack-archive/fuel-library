1b38f34f19b05f4c84e022370ce3655c8281071b
iberezovskiy <iberezovskiy@mirantis.com>
2015-07-30 18:46:55 +0300

    Improvements for MongoDB replica set configuration
    * Remove upstream pull request which state is uncertain
      https://github.com/puppetlabs/puppetlabs-mongodb/pull/194
    * Move mongorc file creation in openstack class
    * Configure admin user in openstack class
    
    Change-Id: I108d2c4a70d5511c5d6601cfdf08191dabb31c1b

--- deployment/puppet/mongodb/spec/classes/server_spec.rb
+++ deployment/puppet/mongodb/spec/classes/server_spec.rb
@@ -9,39 +9,8 @@ describe 'mongodb::server' do
   end
 
   context 'with defaults' do
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_class('mongodb::server::install').
-        that_comes_before('Class[mongodb::server::config]') }
-    it { is_expected.to contain_class('mongodb::server::config').
-        that_comes_before('Class[mongodb::server::service]') }
-    it { is_expected.to contain_class('mongodb::server::service') }
-  end
-
-  context 'with create_admin => true' do
-    let(:params) do
-      {
-        :create_admin   => true,
-        :admin_username => 'admin',
-        :admin_password => 'password'
-      }
-    end
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_class('mongodb::server::install').
-        that_comes_before('Class[mongodb::server::config]') }
-    it { is_expected.to contain_class('mongodb::server::config').
-        that_comes_before('Class[mongodb::server::service]') }
-    it { is_expected.to contain_class('mongodb::server::service') }
-
-    it {
-        is_expected.to contain_mongodb_user('admin').with({
-          'username' => 'admin',
-          'ensure'   => 'present',
-          'database' => 'admin',
-          'roles'    => ['dbAdmin', 'dbOwner', 'userAdmin', 'userAdminAnyDatabase'],
-          'tries'    => 10,
-          'tag'      => 'admin'
-        })
-      }
+    it { should contain_class('mongodb::server::install') }
+    it { should contain_class('mongodb::server::config') }
   end
 
   context 'when deploying on Solaris' do
