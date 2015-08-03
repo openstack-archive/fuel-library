d35e4d6de0bccb758d7a7edd14073cb5edfa10d4
Sergey Vasilenko <svasilenko@mirantis.com>
2015-08-03 11:06:21 +0300

    Add 'vlan_ids' property to the l2_patch resource
    Change-Id: Ibc766ca716006cecfec11774ea97c016bb2f60f0
    Closes-bug: #1480151

--- deployment/puppet/l23network/spec/functions/get_pair_of_jack_names__spec.rb
+++ deployment/puppet/l23network/spec/functions/get_pair_of_jack_names__spec.rb
@@ -20,15 +20,23 @@ describe 'get_pair_of_jack_names' do
 
     it 'should throw an error on invalid arguments number' do
       should run.with_params().and_raise_error(Puppet::ParseError)
-      should run.with_params([1,2],[3,4]).and_raise_error(Puppet::ParseError)
+      should run.with_params([1,2],[3,4]).and_raise_error(TypeError)
     end
 
-    it 'should return numbered interface names' do
-      should run.with_params(['br-mgmt', 'br-ex']).and_return(["p_br-mgmt-0", "p_br-ex-1"])
+    it 'should return unnumbered interface names for ovs case' do
+      should run.with_params(['br-mgmt', 'br-ex'], 'ovs').and_return(["p_br-mgmt", "p_br-ex"])
     end
 
-    it 'should cut interface names for long interfaces' do
-      should run.with_params(['br-mmmmmmmmmmmmmmmmmmmmmmmmgmt', 'br-ex']).and_return(["p_br-mmmmmmmm-0", "p_br-ex-1"])
+    it 'should return numbered interface names for lnx case' do
+      should run.with_params(['br-mgmt', 'br-ex'], 'lnx').and_return(["p_br-mgmt-0", "p_br-ex-1"])
+    end
+
+    it 'should cut interface names for long interfaces for ovs case' do
+      should run.with_params(['br-mmmmmmmmmmmmmmmmmmmmmmmmgmt', 'br-ex'], 'ovs').and_return(["p_br-mmmmmmmmmm", "p_br-ex"])
+    end
+
+    it 'should cut interface names for long interfaces for lnx case' do
+      should run.with_params(['br-mmmmmmmmmmmmmmmmmmmmmmmmgmt', 'br-ex'], 'lnx').and_return(["p_br-mmmmmmmm-0", "p_br-ex-1"])
     end
 
   end
