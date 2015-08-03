800ee84f4cb1ad14de6d92f60f1f471a64ef3d7c
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-08-03 19:42:28 +0300

    Ensure mcollective container packages
    Rewrote class nailgun::mcollective to install
    necessary packages for mcollective container.
    The old nailgun::mcollective is long since
    obsolete.
    
    Change-Id: Ie6a53cc2ce05db5dfb6e6ea915ee5880cf125940
    Closes-Bug: #1461052

--- deployment/puppet/nailgun/manifests/mcollective.pp
+++ deployment/puppet/nailgun/manifests/mcollective.pp
@@ -1,29 +1,13 @@
-class nailgun::mcollective(
-  $mco_pskey = "unset",
-  $mco_user = "mcollective",
-  $mco_password = "marionette",
-  $mco_vhost = "mcollective",
-  $mco_host = "localhost",
-  $rabbitmq_host = "localhost",
-  ){
+class nailgun::mcollective
+{
+  define mcollective_safe_package(){
+    if ! defined(Package[$name]){
+      package { $name : ensure => latest; }
+    }
+  }
 
-  anchor { 'nailgun::mcollective start' :} ->
-  class { "mcollective::client":
-    pskey => $mco_pskey,
-    vhost => $mco_vhost,
-    user => $mco_user,
-    password => $mco_password,
-    host => $rabbitmq_host,
-    stomp => false,
-  } ->
-   class { "mcollective::server":
-    pskey => $mco_pskey,
-    vhost => $mco_vhost,
-    user => $mco_user,
-    password => $mco_password,
-    host => $rabbitmq_host,
-    stomp => false,
-  } ->
-  anchor { 'nailgun::mcollective end' :}
-
+  mcollective_safe_package { "fuel-agent": }
+  mcollective_safe_package { "fuel-image": }
+  mcollective_safe_package { "fuel-provisioning-scripts": }
+  mcollective_safe_package { "shotgun": }
 }
