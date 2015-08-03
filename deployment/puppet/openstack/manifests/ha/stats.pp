57f2cdc031ee0e795df8cd879cbacaaa68c552b8
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-08-03 16:46:21 +0300

    Fix haproxy-stats task
    * Added haproxy-stat as required for openstack-haproxy task
    * Removed option to pass public_vip to stats from the modular class
    
    Change-Id: Id6f51f1ab4fbeb95db1c556c951ec40305f1eace
    Closes-Bug: 1480606

--- deployment/puppet/openstack/manifests/ha/stats.pp
+++ deployment/puppet/openstack/manifests/ha/stats.pp
@@ -9,20 +9,20 @@
 #   vip
 #
 # [*public_virtual_ip*]
-#   (required) String. This is the ipaddress to be used for the external facing
+#   (optional) String. This is the ipaddress to be used for the external facing
 #   vip
 #
-class openstack::ha::stats ( $internal_virtual_ip,
-                             $public_virtual_ip ) {
+class openstack::ha::stats ($internal_virtual_ip,
+                            $public_virtual_ip = undef) {
   openstack::ha::haproxy_service { 'stats':
     public_virtual_ip      => $public_virtual_ip,
     internal_virtual_ip    => $internal_virtual_ip,
     order                  => '010',
     listen_port            => '10000',
     haproxy_config_options => {
-      'stats' => ['enable', 'uri /', 'refresh 5s', 'show-node', 'show-legends', 'hide-version'],
+      'stats' => ['enable', 'uri /', 'refresh 5s', 'show-node',
+                  'show-legends', 'hide-version'],
       'mode'  => 'http',
     },
   }
 }
-
