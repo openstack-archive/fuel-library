47cd50cfeb120017145bf2b22aee1263d8470e5f
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-08-03 16:46:21 +0300

    Fix haproxy-stats task
    * Added haproxy-stat as required for openstack-haproxy task
    * Removed option to pass public_vip to stats from the modular class
    
    Change-Id: Id6f51f1ab4fbeb95db1c556c951ec40305f1eace
    Closes-Bug: 1480606

--- deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-stats.pp
+++ deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-stats.pp
@@ -1,9 +1,7 @@
 notice('MODULAR: openstack-haproxy-stats.pp')
 
-$public_virtual_ip   = pick(hiera('public_service_endpoint', undef), hiera('public_vip'))
-$internal_virtual_ip = pick(hiera('service_endpoint', undef), hiera('management_vip'))
+$internal_virtual_ip = hiera('management_vip')
 
 class { '::openstack::ha::stats':
   internal_virtual_ip => $internal_virtual_ip,
-  public_virtual_ip   => $public_virtual_ip,
 }
