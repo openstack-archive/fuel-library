0625329aac175ce5303a64a838b0d1249a84f618
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-08-04 16:24:50 +0300

    Give possibility to pass IPs to Haproxy stats
    * Give possibility to pass multiple IPs to Haproxy listen
        for internal communications
    * Add new function array_to_hash, that converts array to
        hash with custom value
    
    Change-Id: I47e9eace77cb2a8d061a225f0b45dc0059b5ae76
    Closes-Bug: 1481342

--- deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-stats.pp
+++ deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-stats.pp
@@ -1,6 +1,6 @@
 notice('MODULAR: openstack-haproxy-stats.pp')
 
-$internal_virtual_ip = hiera('management_vip')
+$internal_virtual_ip = unique([hiera('management_vip'), hiera('database_vip'), hiera('service_endpoint')])
 
 class { '::openstack::ha::stats':
   internal_virtual_ip => $internal_virtual_ip,
