ee555e095ad12b0e13e939c158c90da3552bb1f4
Dmitry Ilyin <dilyin@mirantis.com>
2015-08-05 23:08:13 +0300

    Improve ntp and it's ocf script
    * Add check if ntp is running in a correct namespace
    * Refactor ntp task to use pacemaker_wrapper
    
    Change-Id: Id831f909caeb3a8cf588fa9de5d2b0e346d58b9a
    Closes-Bug: 1481618

--- deployment/puppet/osnailyfacter/modular/ntp/ntp-server.pp
+++ deployment/puppet/osnailyfacter/modular/ntp/ntp-server.pp
@@ -4,14 +4,14 @@ $ntp_servers = hiera('external_ntp')
 
 class { 'ntp':
   servers        => strip(split($ntp_servers['ntp_list'], ',')),
-  service_enable => false,
-  service_ensure => 'stopped',
+  service_enable => true,
+  service_ensure => 'running',
   iburst_enable  => true,
   tinker         => true,
   panic          => '0',
   stepout        => '5',
   minpoll        => '3',
-} ->
+}
 
 class { 'cluster::ntp_ocf': }
 
