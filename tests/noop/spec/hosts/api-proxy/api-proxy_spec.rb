fe3b0fdddd555ca9958a7f13cc5cb940a8c6a391
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-08-05 13:12:54 +0300

    Allow to set LimitRequestFieldSize for api
    Allow to set custom LimitRequestFieldSize for
    apache api proxy
    
    Change-Id: I70aa5fa2935e06278b1c683de96a32392d45d339
    Closes-Bug: 1481393

--- tests/noop/spec/hosts/api-proxy/api-proxy_spec.rb
+++ tests/noop/spec/hosts/api-proxy/api-proxy_spec.rb
@@ -4,13 +4,27 @@ manifest = 'api-proxy/api-proxy.pp'
 
 describe manifest do
   shared_examples 'catalog' do
+    let(:master_ip) do
+      Noop.hiera('master_ip')
+    end
+
+    let(:max_header_size) do
+      Noop.hiera('max_header_size', '81900')
+    end
+
     it {
       should contain_service('httpd').with(
            'hasrestart' => true,
            'restart'    => 'sleep 30 && apachectl graceful || apachectl restart'
       )
     }
+
+    it "should delcare osnailyfacter::apache_api_proxy" do
+      expect(subject).to contain_class('osnailyfacter::apache_api_proxy').with(
+        'master_ip'       => master_ip,
+        'max_header_size' => max_header_size,
+      )
+    end
   end
   test_ubuntu_and_centos manifest
 end
-
