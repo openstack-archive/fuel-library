76f8858b15118516987050040be652f36fe09da1
Artem Panchenko <apanchenko@mirantis.com>
2015-08-07 17:07:28 +0300

    Allow connections to VNC from 'nova/api' network
    Change-Id: I5768a13774e28126a6a0b29e66d599a14264b922
    Closes-bug: #1482659

--- deployment/puppet/osnailyfacter/modular/firewall/firewall.pp
+++ deployment/puppet/osnailyfacter/modular/firewall/firewall.pp
@@ -36,6 +36,6 @@ firewall {'006 reject non-local rabbitmq admin':
 
 prepare_network_config(hiera_hash('network_scheme'))
 class { 'openstack::firewall' :
-  nova_vnc_ip_range => get_network_role_property('mgmt/vip', 'network'),
+  nova_vnc_ip_range => get_network_role_property('nova/api', 'network'),
   libvirt_network   => get_network_role_property('nova/migration', 'network'),
 }
