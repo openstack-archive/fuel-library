3b70810ff24e2da3e6c1c4c0d7b699aedfb16594
Denis Egorenko <degorenko@mirantis.com>
2015-08-12 16:15:36 +0300

    Increase Heat requests timeouts
    During creating and using one Heat template for hundreds VMs happens
    timeout error, because validating and checking templates are needing
    much more time in such enviroments.
    
    Timeouts set in agreement with MOS-Scale team.
    
    Change-Id: I2c599762625e2a1bc6d3c86a6f1bba26c24f16d0
    Closes-bug: #1475274
    Closes-bug: #1483833

--- tests/noop/spec/hosts/openstack-haproxy/openstack-haproxy-heat_spec.rb
+++ tests/noop/spec/hosts/openstack-haproxy/openstack-haproxy-heat_spec.rb
@@ -3,6 +3,43 @@ require 'shared-examples'
 manifest = 'openstack-haproxy/openstack-haproxy-heat.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+#    server_names = Noop.hiera_array('heat_names')
+    public_ssl = Noop.hiera_structure('public_ssl')
+    it 'should configure heat haproxy' do
+      should contain_openstack__ha__haproxy_service('heat-api').with(
+        'order'                  => '160',
+        'listen_port'            => 8004,
+        'require_service'        => 'heat-api',
+
+        # common parameters
+#        'internal_virtual_ip'    => internal_virtual_ip,
+#        'ipaddresses'            => ipaddresses,
+#        'public_virtual_ip'      => public_virtual_ip,
+#        'server_names'           => server_names,
+        'public'                 => 'true',
+        'public_ssl'             => public_ssl,
+        'require_service'        => 'heat-api',
+        'haproxy_config_options' => {
+          'option'           => ['httpchk', 'httplog','httpclose'],
+          'timeout server' => '660s',
+         },
+        'balancermember_options' => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3',
+      )
+
+      should contain_openstack__ha__haproxy_service('heat-api-cfn').with(
+        'order'           => '161',
+        'listen_port'     => 8000,
+        'require_service' => 'heat-api',
+      )
+
+      should contain_openstack__ha__haproxy_service('heat-api-cloudwatch').with(
+        'order'           => '162',
+        'listen_port'     => 8003,
+        'require_service' => 'heat-api',
+      )
+    end
+  end
   test_ubuntu_and_centos manifest
 end
 
