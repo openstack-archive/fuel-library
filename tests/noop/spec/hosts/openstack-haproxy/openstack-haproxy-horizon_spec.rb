d43abcd8576e3c13124af1ffe535a595b2306aba
Alex Schultz <aschultz@mirantis.com>
2015-08-13 11:53:42 -0500

    Redirect http horizon to https when https enabled
    This change updates the haproxy configuration for horizon to redirect
    http traffic to https when https is enabled to ensure that only https is
    used. When ssl is not enabled, the horizon http endpoint is configured
    as a regular horizon endpoint.
    
    Change-Id: I2db1fce35d4686e746cc1753ceea5332ab1325cb
    Closes-Bug: 1484580

--- tests/noop/spec/hosts/openstack-haproxy/openstack-haproxy-horizon_spec.rb
+++ tests/noop/spec/hosts/openstack-haproxy/openstack-haproxy-horizon_spec.rb
@@ -3,6 +3,54 @@ require 'shared-examples'
 manifest = 'openstack-haproxy/openstack-haproxy-horizon.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+    it "should properly configure horizon haproxy based on ssl" do
+      public_ssl_horizon = Noop.hiera_structure('public_ssl/horizon', false)
+      if public_ssl_horizon
+        # http horizon should redirect to ssl horizon
+        should contain_openstack__ha__haproxy_service('horizon').with(
+          'server_names'           => [],
+          'ipaddresses'            => [],
+          'haproxy_config_options' => {
+            'redirect' => 'scheme https if !{ ssl_fc }'
+          }
+        )
+        should contain_openstack__ha__haproxy_service('horizon-ssl').with(
+          'order'                  => '017',
+          'listen_port'            => 443,
+          'balancermember_port'    => 80,
+          'public_ssl'             => public_ssl_horizon,
+          'haproxy_config_options' => {
+            'option'      => ['forwardfor', 'httpchk', 'httpclose', 'httplog'],
+            'stick-table' => 'type ip size 200k expire 30m',
+            'stick'       => 'on src',
+            'balance'     => 'source',
+            'timeout'     => ['client 3h', 'server 3h'],
+            'mode'        => 'http',
+            'reqadd'      => 'X-Forwarded-Proto:\ https',
+          },
+          'balancermember_options' => 'weight 1 check'
+        )
+      else
+        # http horizon only
+        should contain_openstack__ha__haproxy_service('horizon').with(
+          'haproxy_config_options' => {
+            'option'      => ['forwardfor', 'httpchk', 'httpclose', 'httplog'],
+            'stick-table' => 'type ip size 200k expire 30m',
+            'stick'       => 'on src',
+            'balance'     => 'source',
+            'timeout'     => ['client 3h', 'server 3h'],
+            'mode'        => 'http',
+            'reqadd'      => 'X-Forwarded-Proto:\ https',
+          }
+        )
+        should_not contain_openstack__ha__haproxy_service('horizon-ssl')
+      end
+    end
+
+
+  end
+
   test_ubuntu_and_centos manifest
 end
 
