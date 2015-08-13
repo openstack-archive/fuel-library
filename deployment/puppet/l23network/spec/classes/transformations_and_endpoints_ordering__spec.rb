cf298468c8fefab07ba33101f19504de1bd4382f
Sergey Vasilenko <svasilenko@mirantis.com>
2015-08-13 13:47:35 +0300

    Re-order resource creation from yaml file
    Current implementation makes following order:
      port(eth2)->port(eth2.102)->port(eth3)->port(eth3.103)
        ->ifconfig(eth2.102)->ifconfig(eth3.103)
    
    Proposed changes modify prdering to:
      port(eth2)->port(eth2.102)->ifconfig(eth2.102)->
      port(eth3)->port(eth3.103)->ifconfig(eth3.103)
    
    Change-Id: Ia9dcb3afdf9bad7cfc28ad8881d5c9b6e6473052
    Fuel-CI: disable
    Closes-bug: #1452617

--- deployment/puppet/l23network/spec/classes/transformations_and_endpoints_ordering__spec.rb
+++ deployment/puppet/l23network/spec/classes/transformations_and_endpoints_ordering__spec.rb
@@ -66,6 +66,9 @@ end
     it do
       should contain_L23network__L3__Ifconfig('eth2.102')
     end
+    it do
+      should contain_L23network__L3__Ifconfig('eth2.102').that_requires('L23network::L2::Port[eth2.102]')
+    end
 
     it do
       should contain_L23network__L2__Port('eth3')
@@ -80,6 +83,9 @@ end
 
     it do
       should contain_L23network__L3__Ifconfig('eth3.103')
+    end
+    it do
+      should contain_L23network__L3__Ifconfig('eth3.103').that_requires("L23network::L2::Port[eth3.103]")
     end
 
   end
@@ -150,12 +156,22 @@ end
     it do
       should contain_L23network__L2__Bond('bond0')
     end
+    # may be unnided, because chain of resources
+    it do
+      should contain_L23network__L2__Bond('bond0').that_requires('L23network::L2::Port[eth2]')
+    end
+    it do
+      should contain_L23network__L2__Bond('bond0').that_requires('L23network::L2::Port[eth3]')
+    end
 
     it do
       should contain_L23network__L3__Ifconfig('bond0')
+    end
+    it do
+      should contain_L23network__L3__Ifconfig('bond0').that_requires("L23network::L2::Bond[bond0]")
     end
 
   end
 end
 
-###
+# vim: set ts=2 sw=2 et :
