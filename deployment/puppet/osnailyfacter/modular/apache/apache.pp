0a11a467f35dfd6032e57dc78022684e1aca8cfb
Alex Schultz <aschultz@mirantis.com>
2015-08-18 14:57:03 -0500

    Increase connection backlogs for apache
    This change increases the net.core.somaxconn from the defaults of 128
    to 4096 and increases net.ipv4.tcp_max_syn_backlog from the defaults of
    128 to 8192.  These values only get applied as part of the apache
    modular task.
    
    Change-Id: I2d4c060ffb0c09096fc86bc7d22efba25de29cd4
    Closes-Bug: 1485644

--- deployment/puppet/osnailyfacter/modular/apache/apache.pp
+++ deployment/puppet/osnailyfacter/modular/apache/apache.pp
@@ -1,5 +1,9 @@
 notice('MODULAR: apache.pp')
 
+# adjustments to defaults for LP#1485644 for scale
+sysctl::value { 'net.core.somaxconn':           value => '4096' }
+sysctl::value { 'net.ipv4.tcp_max_syn_backlog': value => '8192' }
+
 class { 'osnailyfacter::apache':
   purge_configs => true,
   listen_ports  => hiera_array('apache_ports', ['80', '8888']),
--- tests/noop/spec/hosts/apache/apache_spec.rb
+++ tests/noop/spec/hosts/apache/apache_spec.rb
@@ -10,8 +10,12 @@ describe manifest do
         'default_vhost'    => 'false',
         'server_tokens'    => 'Prod',
         'server_signature' => 'Off',
-        'trace_enable'     => 'Off',
+        'trace_enable'     => 'Off'
       )
+    end
+    it 'should apply kernel tweaks for connections' do
+      should contain_sysctl__value('net.core.somaxconn').with_value('4096')
+      should contain_sysctl__value('net.ipv4.tcp_max_syn_backlog').with_value('8192')
     end
   end
   test_ubuntu_and_centos manifest
