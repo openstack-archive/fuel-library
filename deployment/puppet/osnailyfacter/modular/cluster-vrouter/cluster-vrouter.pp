84cb6dfbe52cfffb421d13703224aea725c73093
Alex Schultz <aschultz@mirantis.com>
2015-08-19 13:00:37 -0500

    Ensure vrouter pacemaker service runs
    This change updates the cluster-vrouter task to ensure the p_vrouter
    pacemaker service runs on all controllers. Without this change, the
    clone_p_vrouter service only runs on the primary-controller and is not
    run anywhere else if the primary-controller is lost.
    
    Change-Id: I60cbb28ee39c1a877a622dfcb96ba588af15ae7d
    Closes-Bug: 1486641

--- deployment/puppet/osnailyfacter/modular/cluster-vrouter/cluster-vrouter.pp
+++ deployment/puppet/osnailyfacter/modular/cluster-vrouter/cluster-vrouter.pp
@@ -3,6 +3,5 @@ notice('MODULAR: cluster-vrouter.pp')
 $network_scheme = hiera('network_scheme', {})
 
 class { 'cluster::vrouter_ocf':
-  primary_controller => hiera('primary_controller'),
-  other_networks     => direct_networks($network_scheme['endpoints']),
+  other_networks => direct_networks($network_scheme['endpoints']),
 }
