57ed03191bd27458a8513525438b04a567cc9bbb
Michael Polenchuk <mpolenchuk@mirantis.com>
2015-08-20 16:25:39 +0300

    Disable keystone service token
    Disable keystone service token in post deployment actions
    since that token doesn't represent a user, and carries no
    explicit authorization.
    
    Change-Id: I4f2877df1da087f10673edfc855868ebb2245be3
    Closes-Bug: #1486015

---
+++ deployment/puppet/osnailyfacter/modular/astute/service_token_off.pp
@@ -0,0 +1,20 @@
+#
+# Used as singular by post-deployment action to disable admin_token
+#
+
+include horizon::params
+
+keystone_config {
+  'DEFAULT/admin_token': ensure => absent;
+}
+
+service { 'httpd':
+  ensure     => 'running',
+  name       => $::horizon::params::http_service,
+  enable     => true,
+  hasrestart => true,
+  restart    => 'apachectl graceful',
+}
+
+Keystone_config <| |> ~> Service['httpd']
+
