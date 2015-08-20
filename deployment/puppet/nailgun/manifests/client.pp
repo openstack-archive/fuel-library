765c970e0140d0de457a91b3316b277976cac173
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2015-08-20 15:23:29 +0300

    Fixed issue with deprecation warning
    We need to use another configuration file for fuelclient
    and we also need to provide the ability to create new users
    on fuel master node with the correct configuration file for
    fuelclient.
    
    DocImpact
    
    Change-Id: I08fd7bf965af1552d16671636c4f706c5a2d6a11
    Closes-Bug: #1458361

--- deployment/puppet/nailgun/manifests/client.pp
+++ deployment/puppet/nailgun/manifests/client.pp
@@ -8,16 +8,21 @@ $keystone_port = '5000',
 {
   include nailgun::packages
 
-  file { '/etc/fuel/client':
+  file { '/etc/skel/.fuel-config':
     ensure => directory,
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
   }
-  file { '/etc/fuel/client/config.yaml':
+  file { '/etc/skel/.fuel-config/fuelclient.yaml':
+    require => file['/etc/skel/.fuel-config'],
     content => template('nailgun/fuelclient.yaml.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0600',
+  }
+  file_line { 'fuel client config file env variable':
+    line => 'export FUELCLIENT_CUSTOM_SETTINGS="~/.fuel-config/fuelclient.yaml"',
+    path => '/etc/skel/.bashrc',
   }
 }
