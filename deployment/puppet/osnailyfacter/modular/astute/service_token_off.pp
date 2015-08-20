55a1d0d2216868b6c5a48f4866fcaf39818a9a4d
Michael Polenchuk <mpolenchuk@mirantis.com>
2015-08-20 16:25:39 +0300

    Disable keystone service token
    Disable keystone service token in post deployment actions
    since that token doesn't represent a user, and carries no
    explicit authorization.
    
    Change-Id: I4f2877df1da087f10673edfc855868ebb2245be3
    Closes-Bug: #1486015

---
+++ deployment/puppet/osnailyfacter/modular/astute/service_token_off.pp
@@ -0,0 +1,43 @@
+notice('MODULAR: service_token_off.pp')
+
+####################################################################
+# Used as singular by post-deployment action to disable admin_token
+#
+
+$keystone_params = hiera_hash('keystone_hash', {})
+
+if $keystone_params['service_token_off'] {
+
+  include horizon::params
+  include keystone::params
+
+  keystone_config {
+    'DEFAULT/admin_token': ensure => absent;
+  }
+
+  # Get paste.ini source
+  $keystone_paste_ini = $::keystone::params::paste_config ? {
+    undef   => '/etc/keystone/keystone-paste.ini',
+    default => $::keystone::params::paste_config,
+  }
+
+  # Remove admin_token_auth middleware from public/admin/v3 pipelines
+  exec { 'remove_admin_token_auth_middleware':
+    path    => ['/bin', '/usr/bin'],
+    command => "sed -i.dist 's/ admin_token_auth//' $keystone_paste_ini",
+    onlyif  => "fgrep -q ' admin_token_auth' $keystone_paste_ini",
+  }
+
+  service { 'httpd':
+    ensure     => 'running',
+    name       => $::horizon::params::http_service,
+    enable     => true,
+    hasrestart => true,
+    restart    => 'apachectl graceful',
+  }
+
+  # Restart service that changes to take effect
+  Keystone_config<||> ~> Service['httpd']
+  Exec['remove_admin_token_auth_middleware'] ~> Service['httpd']
+
+}
