1045e444df95a70c246ffa68a1646dd4950ae982
Dmitry Ilyin <dilyin@mirantis.com>
2015-08-24 20:36:44 +0300

    Fix noops tests for the new network variables
    * Refactor old noop tests to support flexible network data
    * Repair puppet function helper
    * Script to remove old external modules before updating them
    * network_config_prepared function to check if network
      scheme is already loaded
    
    Change-Id: Ie2e7e2a8a7fbfa32a4172e018acba6e955973425
    Closes-Bug: 1486048
    Fuel-CI: disable

---
+++ deployment/remove_modules.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# remove all external puppet modules
+
+dir=`dirname $0`
+cd "${dir}" || exit 1
+
+cat Puppetfile | grep "^mod '" | awk -F "'" '{ print $2 }' | while read module; do
+  if [ -d "puppet/${module}" ]; then
+    echo "Remove: puppet/${module}"
+    rm -rf "puppet/${module}"
+  fi
+done
