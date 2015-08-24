471873c45e37e4088944930282e231de954795b7
Dmitry Ilyin <dilyin@mirantis.com>
2015-08-24 20:36:44 +0300

    Fix noops tests for the new network variables
    * Refactor old noop tests to support flexible nrtwork data
    * Repair puppet function helper
    * Script to remove old external modules before updating them
    * network_config_prepared function to check if network
      scheme is already loaded
    
    Change-Id: Ie2e7e2a8a7fbfa32a4172e018acba6e955973425
    Closes-Bug: 1486048

--- tests/noop/spec/hosts/murano/keystone_spec.rb
+++ tests/noop/spec/hosts/murano/keystone_spec.rb
@@ -4,24 +4,57 @@ manifest = 'murano/keystone.pp'
 
 describe manifest do
   shared_examples 'catalog' do
-    public_ip            = Noop.hiera('public_vip')
-    internal_address     = Noop.hiera('management_vip', public_ip)
-    service_endpoint     = Noop.hiera('service_endpoint', internal_address)
-    public_ssl           = Noop.hiera_structure('public_ssl/services')
 
-    api_bind_port   = '8082'
-    if public_ssl
-      public_protocol = 'https'
-      public_address = Noop.hiera_structure('public_ssl/hostname')
-    else
-      public_protocol = 'http'
-      public_address = public_ip
+    let(:service_endpoint) do
+      Noop.hiera 'service_endpoint'
     end
-    murano_password = Noop.hiera_structure('murano_hash/user_password')
-    tenant          = Noop.hiera_structure('murano_hash/tenant', 'services')
-    region          = Noop.hiera('region', 'RegionOne')
-    public_url      = "#{public_protocol}://#{public_address}:#{api_bind_port}"
-    admin_url       = "http://#{service_endpoint}:#{api_bind_port}"
+
+    let(:network_scheme) do
+      Noop.hiera_hash 'network_scheme'
+    end
+
+    let(:public_address) do
+      Noop.puppet_function 'prepare_network_config', network_scheme
+      Noop.puppet_function 'get_network_role_property', 'ex', 'ipaddr'
+    end
+
+    let(:public_ssl) do
+      Noop.hiera_structure('public_ssl/services')
+    end
+
+    let(:api_bind_port) do
+      '8082'
+    end
+
+    let(:admin_url) do
+      "http://#{service_endpoint}:#{api_bind_port}"
+    end
+
+    let(:public_url) do
+      api_bind_port   = '8082'
+      if public_ssl
+        public_protocol = 'https'
+        keystone_host = Noop.hiera_structure('public_ssl/hostname')
+      else
+        public_protocol = 'http'
+        keystone_host = public_address
+      end
+      "#{public_protocol}://#{keystone_host}:#{api_bind_port}"
+    end
+
+    let(:region) do
+      Noop.hiera('region', 'RegionOne')
+    end
+
+    let(:tenant) do
+      Noop.hiera_structure('murano_hash/tenant', 'services')
+    end
+
+    let(:murano_password) do
+      Noop.hiera_structure('murano_hash/user_password')
+    end
+
+    ##########################################################################
 
     it 'should declare murano::keystone::auth class correctly' do
       should contain_class('murano::keystone::auth').with(
@@ -31,9 +64,10 @@ describe manifest do
                  'tenant'       => tenant,
                  'public_url'   => public_url,
                  'admin_url'    => admin_url,
-                 'internal_url' => admin_url,
+                 'internal_url' => admin_url
              )
     end
+
   end
   test_ubuntu_and_centos manifest
 end
