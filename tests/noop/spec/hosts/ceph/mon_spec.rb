30f174c539b69ddde1148a0f11b362908c6c3ce7
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2015-08-27 18:25:20 +0200

    Configure ceph properly for ephemeral usage
    This change ensures that ceph mon and the ceph compute configurations
    are correctly applied when ceph is selected for only ephemeral volumes.
    Additionally, adding noop astute.yaml files for this type of deployment
    where ephemeral_ceph is enabled since none of the previous files have
    ephemeral_ceph set to true.
    
    Change-Id: I9e86c1180d2317eab89babfd96a222bd8abce357
    Co-Authored-By: Alex Schultz <aschultz@mirantis.com>
    Closes-Bug: 1489532

--- tests/noop/spec/hosts/ceph/mon_spec.rb
+++ tests/noop/spec/hosts/ceph/mon_spec.rb
@@ -7,7 +7,11 @@ describe manifest do
     storage_hash = Noop.hiera 'storage'
     ceph_monitor_nodes = Noop.hiera 'ceph_monitor_nodes'
 
-    if (storage_hash['images_ceph'] or storage_hash['objects_ceph'] or storage_hash['objects_ceph'])
+    if (storage_hash['volumes_ceph'] or
+        storage_hash['images_ceph'] or
+        storage_hash['objects_ceph'] or
+        storage_hash['ephemeral_ceph']
+       )
       it { should contain_class('ceph').with(
            'mon_hosts'                => ceph_monitor_nodes.keys,
            'osd_pool_default_size'    => storage_hash['osd_pool_size'],
@@ -16,7 +20,8 @@ describe manifest do
            'ephemeral_ceph'           => storage_hash['ephemeral_ceph'],
            )
          }
-
+    else
+      it { should_not contain_class('ceph') }
     end
   end
 
