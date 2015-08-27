b44db1bf4d56a0dd94c5351a4e19e01bd22d3a15
iberezovskiy <iberezovskiy@mirantis.com>
2015-08-27 14:22:18 +0300

    WIP (Switch to librarian)
    Change-Id: I8932baf06c926a03e76df924964631e82fb97ee6
    Fuel-CI: disable

--- deployment/puppet/rsyslog/manifests/service.pp
+++
@@ -1,7 +0,0 @@
-class rsyslog::service {
-    service { $rsyslog::params::service_name:
-        ensure  => running,
-        enable  => true,
-        require => Class["rsyslog::config"],
-    }
-}
