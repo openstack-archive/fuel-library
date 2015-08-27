0d3650d9753d3c4de87f36c9e6f99ca24b6102e6
iberezovskiy <iberezovskiy@mirantis.com>
2015-08-27 14:22:18 +0300

    WIP (Switch on librarian)
    Change-Id: I8932baf06c926a03e76df924964631e82fb97ee6
    Fuel-CI: disable

--- deployment/puppet/swift/spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
-require 'shared_examples'
-
-RSpec.configure do |c|
-  c.alias_it_should_behave_like_to :it_configures, 'configures'
-  c.alias_it_should_behave_like_to :it_raises, 'raises'
-  c.default_facts = { :concat_basedir => '/var/lib/puppet/concat' }
-end
