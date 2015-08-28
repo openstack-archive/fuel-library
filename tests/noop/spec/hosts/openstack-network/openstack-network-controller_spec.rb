8072d5406f944f8edb01eb6dc97034cd0993215c
Sergey Kolekonov <skolekonov@mirantis.com>
2015-08-28 13:54:43 +0300

    Avoid neighbour table overflow problem
    “Neighbour table overflow” error which occurs in large networks when there are
    two many ARP requests which the server is not able to reply.
    
    This problem can occur when there're a lot of connections in Neutron routers'
    namespaces. Tune systcl to avoid this problem.
    
    Closes-bug: #1488938
    Co-Authored-By: Eugene Nikanorov <enikanorov@mirantis.com>
    Co-Authored-By: Bogdan Dobrelya  <bdobrelia@mirantis.com>
    Change-Id: I00e954c4792fd4d7993fd5e36fef9be86af22196
    (cherry picked from commit 6d32ad9e3e2fefa7819bfa7207623aa6667bf8bb)

--- tests/noop/spec/hosts/openstack-network/openstack-network-controller_spec.rb
+++ tests/noop/spec/hosts/openstack-network/openstack-network-controller_spec.rb
@@ -10,6 +10,12 @@ describe manifest do
     use_neutron = Noop.hiera 'use_neutron'
     ceilometer_enabled = Noop.hiera_structure 'ceilometer/enabled'
 
+    it 'should apply kernel tweaks for connections' do
+      should contain_sysctl__value('net.ipv4.neigh.default.gc_thresh1').with_value('1024')
+      should contain_sysctl__value('net.ipv4.neigh.default.gc_thresh2').with_value('2048')
+      should contain_sysctl__value('net.ipv4.neigh.default.gc_thresh3').with_value('4096')
+    end
+
     # Network
     if use_neutron
       it 'should declare openstack::network with neutron enabled' do
