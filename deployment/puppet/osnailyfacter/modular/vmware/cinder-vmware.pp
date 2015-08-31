b3fe06f48983fcfd0294343e6e919ab5f62c4e6d
Michał Górniak <mgorniak@mirantis.com>
2015-08-31 16:18:28 -0500

    Add posibility to set debug/verbose per service
    Each service set up by fuel have they own hash which is pick up by
    hiera. Example:
    hiera_hash('rabbit_hash')
    and in all of those hashes we can put value 'verbose' and 'debug' to
    'true'/'false' to enable/disable debug/verbose for this particular
    service.
    
    Of course there is possibility to enable it globally in all services and
    those options are picked up like this:
    hiera('verbose')
    hiera('debug')
    
    Change-Id: Ia2f13ef542a723feb4d35b74f1f5de58ee11f8dd
    Closes-Bug: 1490744
    DocImpact: Set debug/verbose mode per service

--- deployment/puppet/osnailyfacter/modular/vmware/cinder-vmware.pp
+++ deployment/puppet/osnailyfacter/modular/vmware/cinder-vmware.pp
@@ -1,11 +1,11 @@
 notice('MODULAR: cinder-vmware.pp')
 
-$nodes_hash = hiera('nodes', {})
-$roles      = node_roles($nodes_hash, hiera('uid'))
+$nodes_hash   = hiera('nodes', {})
+$roles        = node_roles($nodes_hash, hiera('uid'))
+$cinder_hash  = hiera_hash('cinder_hash', {})
 
 if (member($roles, 'cinder-vmware')) {
-  $cinder   = hiera_hash('cinder_hash', {})
-  $debug    = hiera('debug', true)
-  $volumes  = get_cinder_vmware_data($cinder['instances'], $debug)
+  $debug    = pick($cinder_hash['debug'], hiera('debug', true))
+  $volumes  = get_cinder_vmware_data($cinder_hash['instances'], $debug)
   create_resources(vmware::cinder::vmdk, $volumes)
 }
