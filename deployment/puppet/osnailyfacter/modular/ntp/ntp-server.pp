643d3e9e2c98f3890ec5f9d4feb6c8258bd912f8
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-09-01 19:02:23 +0300

    disable NTP monitor service
    Disables NTP monitor, which is a potential
    DDOS attack vector.
    
    Added noop and functional tests for NTP
    
    Change-Id: I06bb375cda80f5790fa5367ddb33bcfda758ee95
    Closes-Bug: #1488731

--- deployment/puppet/osnailyfacter/modular/ntp/ntp-server.pp
+++ deployment/puppet/osnailyfacter/modular/ntp/ntp-server.pp
@@ -3,14 +3,15 @@ notice('MODULAR: ntp-server.pp')
 $ntp_servers = hiera('external_ntp')
 
 class { 'ntp':
-  servers        => strip(split($ntp_servers['ntp_list'], ',')),
-  service_enable => true,
-  service_ensure => 'running',
-  iburst_enable  => true,
-  tinker         => true,
-  panic          => '0',
-  stepout        => '5',
-  minpoll        => '3',
+  servers         => strip(split($ntp_servers['ntp_list'], ',')),
+  service_enable  => true,
+  service_ensure  => 'running',
+  disable_monitor => true,
+  iburst_enable   => true,
+  tinker          => true,
+  panic           => '0',
+  stepout         => '5',
+  minpoll         => '3',
 }
 
 class { 'cluster::ntp_ocf': }
