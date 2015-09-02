acd9b18559ebb24a09644b3e5ae2f7d3fd8236df
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2015-09-02 10:10:07 +0300

    Get rid of version.yaml facts
    /etc/version.yaml is built together with ISO and
    it is not a part of any rpm package which contradicts
    package based approach.
    
    This patch introduces 3 other puppet facts which
    are from the following files:
    - /etc/fuel_release
    - /etc/fuel_production
    - /etc/fuel_openstack_version
    These files are to be packaged into RPM packages and built
    using perestroika.
    
    Change-Id: I5f264fbc8f80a2e92920484ef6a6e28e6c8e7e58

---
+++ deployment/puppet/nailgun/lib/facter/fuel_release.rb
@@ -0,0 +1,11 @@
+require 'facter'
+
+fuel_release_path = '/etc/fuel_release'
+read_data = ""
+
+Facter.add('fuel_release') do
+  if File.exist?(fuel_release_path)
+    read_data = File.read(fuel_release_path).strip
+  end
+  setcode { read_data }
+end
