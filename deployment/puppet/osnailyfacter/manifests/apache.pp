b685f54711075d9c5b665f6d5c5be00ca6e5369a
Alex Schultz <aschultz@mirantis.com>
2015-09-04 14:35:39 -0500

    Fix logrotate to prevent downing all apache svcs
    This change introduces a random delay between 0 and 5 minutes for
    logrotate as part of the pre-rotate process for apache2. This sleep is
    an attempt to prevent all apache services from being restarted
    at the exact same time for a cluster. Additionally we have found that
    soemtimes apache will crash with mod_wsgi so this change updates the two
    places where we use a graceful restart to also include a restart of
    apache if the graceful is unsuccessful.
    
    Change-Id: I0585835ca67e4137e39056b51965c922bb0118cd
    Closes-Bug: 1491576

--- deployment/puppet/osnailyfacter/manifests/apache.pp
+++ deployment/puppet/osnailyfacter/manifests/apache.pp
@@ -1,7 +1,27 @@
-# Configure apache and listen ports
+# == Class: osnailyfacter::apache
+#
+#  Configure apache and listen ports. This class also manages the apache2
+#  logrotate configuration.
+#
+# === Parameters
+#
+# [*purge_configs*]
+#  (optional) Boolean flag to indicate if we should purge all the apache
+#  configs unless explicitly managed via puppet.
+#  Defaults to false
+#
+# [*listen_ports*]
+#  (optional) The ports to listen on for apache
+#  Defaults to '80'
+#
+# [*logrotate_rotate*]
+#  (optional) The number of times to be rotated before being removed.
+#  Defaults to '52'
+#
 class osnailyfacter::apache (
-  $purge_configs = false,
-  $listen_ports  = '80',
+  $purge_configs    = false,
+  $listen_ports     = '80',
+  $logrotate_rotate = '52',
 ) {
 
   define apache_port {
@@ -20,4 +40,39 @@ class osnailyfacter::apache (
   }
 
   apache_port { $listen_ports: }
+
+  # we need to override the logrotate file provided by apache to work around
+  # wsgi issues on the restart caused by logrotate.
+  # LP#1491576 and https://github.com/GrahamDumpleton/mod_wsgi/issues/81
+  file { '/etc/logrotate.d/apache2':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('osnailyfacter/apache2.logrotate.erb'),
+    require => Package['httpd']
+  }
+
+  # This will randomly rotate the array of delays based on hostname to allow
+  # for an idempotent delay to be applied. This will introduce a delay between
+  # 0 and 5 minutes to the logrotate process.
+  $delay = fqdn_rotate([0,1,2,3,4,5])
+
+  # Convert delay into seconds for the prerotation script
+  $apache2_logrotate_delay = $delay[0] * 60
+
+  file { '/etc/logrotate.d/httpd-prerotate':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { '/etc/logrotate.d/httpd-prerotate/apache2':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => template('osnailyfacter/apache2.prerotate.erb'),
+  }
 }
