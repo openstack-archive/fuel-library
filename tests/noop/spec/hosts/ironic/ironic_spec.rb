a8225d621cc48a73cb28881da1b20be2cd08205f
Andrey Shestakov <ashestakov@mirantis.com>
2015-09-07 21:24:51 +0300

    [Ironic] Add Ironic API service
    Following tasks introduces:
    * ironic-api - API service for Ironic
    * ironic-db - create and configure database for Ironic
    * ironic-keystone - create service and endpoints in Keystone
    * ironic-post-swift-key - post secret key for Swift tempurl
    
    Partially Implemets: blueprint fuel-integrate-ironic
    
    Change-Id: I168e7b831ccf12a9b3c7faf7063133ddf12ab670

---
+++ tests/noop/spec/hosts/ironic/ironic_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'ironic/ironic.pp'
+
+ironic_enabled = Noop.hiera_structure 'ironic/enabled'
+if ironic_enabled
+
+  describe manifest do
+    shared_examples 'catalog' do
+      rabbit_user = Noop.hiera_structure 'rabbit/user', 'nova'
+      rabbit_password = Noop.hiera_structure 'rabbit/password'
+
+      it 'should declare ironic class correctly' do
+        should contain_class('ironic').with(
+          'rabbit_userid'   => rabbit_user,
+          'rabbit_password' => rabbit_password,
+        )
+      end
+    end
+    test_ubuntu_and_centos manifest
+  end
+end
