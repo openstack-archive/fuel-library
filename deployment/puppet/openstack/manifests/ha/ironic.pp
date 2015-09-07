04cf544771cc2b75b2d7c1854e7d7765636f54ca
Andrey Shestakov <ashestakov@mirantis.com>
2015-09-07 18:37:14 +0300

    [Ironic] Add haproxy services for Ironic
    This change adds new haproxy services needed by ironic:
    * ironic-api - on management and public VIPs
    * ironic-api - on baremetal VIP
    * swift-baremetal - proxy for swift servers on baremetal VIP
    * noop tests
    
    Partially Implements: blueprint fuel-integrate-ironic
    
    Change-Id: I5ced9e5706fcba9fc60a07771ae51538945895a9

---
+++ deployment/puppet/openstack/manifests/ha/ironic.pp
@@ -0,0 +1,64 @@
+# == Class: openstack::ha::ironic
+#
+# HA configuration for OpenStack Ironic
+#
+# === Parameters
+#
+# [*internal_virtual_ip*]
+#   (required) String. This is the ipaddress to be used for the internal facing
+#   vip
+#
+# [*ipaddresses*]
+#   (required) Array. This is an array of ipaddresses for the backend services
+#   to be loadbalanced
+#
+# [*public_ssl*]
+#   (optional) Boolean. If true, enables SSL for $public_virtual_ip
+#   Defaults to false.
+#
+# [*public_virtual_ip*]
+#   (required) String. This is the ipaddress to be used for the external facing
+#   vip
+#
+# [*baremetal_virtual_ip*]
+#   (required) String. This is the ipaddress to be used for the baremetal facing
+#   vip
+#
+# [*server_names*]
+#   (required) Array. This is an array of server names for the haproxy service
+#
+class openstack::ha::ironic (
+  $internal_virtual_ip,
+  $ipaddresses,
+  $public_virtual_ip,
+  $server_names,
+  $baremetal_virtual_ip,
+  $public_ssl = false,
+) {
+
+  # defaults for any haproxy_service within this class
+  Openstack::Ha::Haproxy_service {
+    listen_port            => 6385,
+    internal_virtual_ip    => $internal_virtual_ip,
+    ipaddresses            => $ipaddresses,
+    public_virtual_ip      => $public_virtual_ip,
+    server_names           => $server_names,
+    haproxy_config_options => {
+      option => ['httpchk GET /', 'httplog','httpclose'],
+    },
+  }
+
+  openstack::ha::haproxy_service { 'ironic':
+    order                  => '180',
+    public                 => true,
+    public_ssl             => $public_ssl,
+  }
+
+  openstack::ha::haproxy_service { 'ironic-baremetal':
+    order                  => '185',
+    public                 => false,
+    public_ssl             => false,
+    public_virtual_ip      => false,
+    internal_virtual_ip    => $baremetal_virtual_ip,
+  }
+}
