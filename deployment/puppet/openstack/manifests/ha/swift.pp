c28a81974070e8178c17b0b36bc48919289b7f6d
Andrey Shestakov <ashestakov@mirantis.com>
2015-09-07 18:37:14 +0300

    [Ironic] Add haproxy services for Ironic
    This change adds new haproxy services needed by ironic:
    * ironic-api - on management and public VIPs
    * ironic-api - on baremetal VIP
    * swift-baremetal - proxy for swift servers on baremetal VIP
    * noop tests
    
    Partially Implements: blueprint fuel-integrate-ironic
    
    Change-Id: I5ced9e5706fcba9fc60a07771ae51538945895a9

--- deployment/puppet/openstack/manifests/ha/swift.pp
+++ deployment/puppet/openstack/manifests/ha/swift.pp
@@ -20,6 +20,10 @@
 #   (required) String. This is the ipaddress to be used for the external facing
 #   vip
 #
+# [*baremetal_virtual_ip*]
+#   (optional) String. This is the ipaddress to be used for the baremetal facing
+#   vip
+#
 # [*server_names*]
 #   (required) Array. This is an array of server names for the haproxy service
 #
@@ -29,25 +33,35 @@ class openstack::ha::swift (
   $public_virtual_ip,
   $server_names,
   $public_ssl = false,
+  $baremetal_virtual_ip = undef,
 ) {
 
   # defaults for any haproxy_service within this class
   Openstack::Ha::Haproxy_service {
+    listen_port         => 8080,
     internal_virtual_ip => $internal_virtual_ip,
     ipaddresses         => $ipaddresses,
     public_virtual_ip   => $public_virtual_ip,
     server_names        => $server_names,
-  }
-
-  openstack::ha::haproxy_service { 'swift':
-    order                  => '120',
-    listen_port            => 8080,
-    public                 => true,
-    public_ssl             => $public_ssl,
     haproxy_config_options => {
       'option' => ['httpchk', 'httplog', 'httpclose'],
     },
     balancermember_options => 'check port 49001 inter 15s fastinter 2s downinter 8s rise 3 fall 3',
+  }
 
+  openstack::ha::haproxy_service { 'swift':
+    order                  => '120',
+    public                 => true,
+    public_ssl             => $public_ssl,
+  }
+
+  if $baremetal_virtual_ip {
+    openstack::ha::haproxy_service { 'swift-baremetal':
+      order                  => '125',
+      public                 => false,
+      public_ssl             => false,
+      public_virtual_ip      => false,
+      internal_virtual_ip    => $baremetal_virtual_ip,
+    }
   }
 }
