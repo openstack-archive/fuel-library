c555228024a07ef05b194eeb26a0f73066f1c802
Andrey Shestakov <ashestakov@mirantis.com>
2015-09-07 18:37:14 +0300

    [Ironic] Add haproxy services for Ironic
    This change adds new haproxy services needed by ironic:
    * ironic-api - on management and public VIPs
    * ironic-api - on baremetal VIP
    * swift-baremetal - proxy for swift servers on baremetal VIP
    * noop tests
    
    Partially Implements: blueprint fuel-integrate-ironic
    
    Change-Id: I5ced9e5706fcba9fc60a07771ae51538945895a9

--- tests/noop/spec/hosts/openstack-haproxy/openstack-haproxy-ironic_spec.rb
+++ tests/noop/spec/hosts/openstack-haproxy/openstack-haproxy-ironic_spec.rb
@@ -1,19 +1,18 @@
 require 'spec_helper'
 require 'shared-examples'
-manifest = 'openstack-haproxy/openstack-haproxy-swift.pp'
+manifest = 'openstack-haproxy/openstack-haproxy-ironic.pp'
 
 describe manifest do
   shared_examples 'catalog' do
     ironic_enabled = Noop.hiera_structure 'ironic/enabled'
     if ironic_enabled
       baremetal_virtual_ip = Noop.hiera 'network_metadata/vips/baremetal/ipaddr'
-      it 'should declare ::openstack::ha::swift class with baremetal_virtual_ip' do
-        should contain_class('openstack::ha::swift').with(
+      it 'should declare openstack::ha::ironic class with baremetal_virtual_ip' do
+        should contain_class('openstack::ha::ironic').with(
           'baremetal_virtual_ip' => baremetal_virtual_ip,
         )
       end
-    end
+    end # ironic
   end # end of shared_examples
   test_ubuntu_and_centos manifest
 end
-
