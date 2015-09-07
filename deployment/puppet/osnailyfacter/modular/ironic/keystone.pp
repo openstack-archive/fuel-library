461f5a3dc989c68ea544fc3b1c805807dfbe2898
Andrey Shestakov <ashestakov@mirantis.com>
2015-09-07 21:24:51 +0300

    [Ironic] Add Ironic API service
    Following tasks introduces:
    * ironic-api - API service for Ironic
    * ironic-db - create and configure database for Ironic
    * ironic-keystone - create service and endpoints in Keystone
    * ironic-post-swift-key - post secret key for Swift tempurl
    
    Implemets: blueprint fuel-integrate-ironic (partially)
    
    Change-Id: I168e7b831ccf12a9b3c7faf7063133ddf12ab670

---
+++ deployment/puppet/osnailyfacter/modular/ironic/keystone.pp
@@ -0,0 +1,39 @@
+notice('MODULAR: ironic/keystone.pp')
+
+$ironic_hash                = hiera_hash('ironic', {})
+$public_vip                 = hiera('public_vip')
+$management_vip             = hiera('management_vip')
+$public_ssl_hash            = hiera('public_ssl')
+$ironic_tenant              = pick($ironic_hash['tenant'],'services')
+$ironic_user                = pick($ironic_hash['auth_name'],'ironic')
+$ironic_user_password       = pick($ironic_hash['user_password'],'ironic')
+$configure_endpoint         = pick($ironic_hash['configure_endpoint'], true)
+$configure_user             = pick($ironic_hash['configure_user'], true)
+$configure_user_role        = pick($ironic_hash['configure_user_role'], true)
+$service_name               = pick($ironic_hash['service_name'], 'ironic')
+
+$public_address = $public_ssl_hash['services'] ? {
+  true    => $public_ssl_hash['hostname'],
+  default => $public_vip,
+}
+$public_protocol = $public_ssl_hash['services'] ? {
+  true    => 'https',
+  default => 'http',
+}
+
+$region                     = hiera('region', 'RegionOne')
+$public_url                 = "${public_protocol}://${public_address}:6385"
+$admin_url                  = "http://${management_vip}:6385"
+$internal_url               = "http://${management_vip}:6385"
+
+class { 'ironic::keystone::auth':
+  password            => $ironic_user_password,
+  region              => $region,
+  public_url          => $public_url,
+  internal_url        => $internal_url,
+  admin_url           => $admin_url,
+  configure_endpoint  => $configure_endpoint,
+  configure_user      => $configure_user,
+  configure_user_role => $configure_user_role,
+  service_name        => $service_name,
+}
