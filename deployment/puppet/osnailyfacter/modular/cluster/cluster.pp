c46031c63853e837c24c81946ab4020bd4f4cd1b
Stanislav Makar <smakar@mirantis.com>
2015-09-09 20:21:18 +0000

    Fix the problem with first pacemaker start
    Sometimes during first start pacemaker can not connect to corosync via IPC.
    The root cause of this is that corosync is run under root user and pacemaker
    processes are run under hacluster/haclient user/group. Adding uidgid
    with names of pacemaker user/group helps to fix it.
    
    Change-Id: I1c103ec8e67f7af170dc2d548c3b49b5f1234b23
    Closes-bug: #1493483

--- deployment/puppet/osnailyfacter/modular/cluster/cluster.pp
+++ deployment/puppet/osnailyfacter/modular/cluster/cluster.pp
@@ -31,3 +31,19 @@ Service <| title == 'corosync' |> {
 
 Service['corosync'] -> Pcmk_nodes<||>
 Pcmk_nodes<||> -> Service<| provider == 'pacemaker' |>
+
+# Sometimes during first start pacemaker can not connect to corosync
+# via IPC due to pacemaker and corosync processes are run under different users
+if($::operatingsystem == 'Ubuntu') {
+  $pacemaker_run_uid = 'hacluster'
+  $pacemaker_run_gid = 'haclient'
+
+  file {'/etc/corosync/uidgid.d/pacemaker':
+    content =>"uidgid {
+   uid: ${pacemaker_run_uid}
+   gid: ${pacemaker_run_gid}
+}"
+  }
+
+  File['/etc/corosync/corosync.conf'] -> File['/etc/corosync/uidgid.d/pacemaker'] -> Service <| title == 'corosync' |>
+}
