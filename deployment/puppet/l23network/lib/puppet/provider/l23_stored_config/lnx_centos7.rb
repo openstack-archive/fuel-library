e73e3228b289deb449623b3dd5835e388099cfc5
Stanislav Makar <smakar@mirantis.com>
2015-09-11 15:59:27 +0000

    Add support of CentOS7 to l23network
    * Add CentOS6 and CentOS7 separation to l23_os fact.
    * Add CentOS7 support to l23_stored_config resource.
    * Remove *vconfig* package due to we do not use *vconfig* to configure vlan
     interfaces hence we do not need to install it. CentOS7 does not have this
     package anymore as well.
    
    Change-Id: Ia6cb9e4efc9465030fe0ed8c2e46922db1513a74
    Closes-bug: #1503157

--- deployment/puppet/l23network/lib/puppet/provider/l23_stored_config/lnx_centos7.rb
+++ deployment/puppet/l23network/lib/puppet/provider/l23_stored_config/lnx_centos7.rb
@@ -1,12 +1,12 @@
 require 'puppetx/filemapper'
 require File.join(File.dirname(__FILE__), '..','..','..','puppet/provider/l23_stored_config_centos')
 
-Puppet::Type.type(:l23_stored_config).provide(:lnx_centos6, :parent => Puppet::Provider::L23_stored_config_centos) do
+Puppet::Type.type(:l23_stored_config).provide(:lnx_centos7, :parent => Puppet::Provider::L23_stored_config_centos) do
 
   include PuppetX::FileMapper
 
-  confine    :l23_os => :centos6
-  defaultfor :l23_os => :centos6
+  confine    :l23_os => :centos7
+  defaultfor :l23_os => :centos7
 
   has_feature :provider_options
   #has_feature :hotpluggable
