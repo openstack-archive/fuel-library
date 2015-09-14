c622dfebfdc79eb78668ca92da6ffc7413e04df2
Sergey Vasilenko <svasilenko@mirantis.com>
2015-09-14 18:04:28 -0500

    Do not create config file for ovs2ovs patchcord
    Ubuntu with OVS < 2.4 does not support such configs at all.
    
    Change-Id: I9161e282f285dffc018a9d7d3fe91cb3760486ad
    Closes-bug: #1495534

--- deployment/puppet/l23network/lib/puppet/parser/functions/get_provider_for.rb
+++ deployment/puppet/l23network/lib/puppet/parser/functions/get_provider_for.rb
@@ -1,24 +1,13 @@
-
 module Puppet::Parser::Functions
   newfunction(:get_provider_for, :type => :rvalue, :doc => <<-EOS
-  Get the default provider of a type
+  Get resource provider by given name and type
   EOS
   ) do |argv|
-    type_name = argv[0]
-    res_name = argv[1]
+    type_name = argv[0].to_s
+    resource_name = argv[1].to_s
     fail('No type name provided!') if ! type_name
-    Puppet::Type.loadall()
-    type_name = type_name.capitalize.to_sym
-    return 'undef' if ! Puppet::Type.const_defined? type_name
-    type = Puppet::Type.const_get type_name
-# require 'pry'
-# binding.pry
-    type.loadall()
-    rv = type.instances.select{|i| i.name.to_s.downcase == res_name.to_s.downcase}.map{|j| j[:provider].to_s}
-# require 'pry'
-# binding.pry
-    rv = rv[0]
-    debug("Provider for '#{type_name}[#{res_name}]' is a '#{rv}'.")
-    return rv
+    # type.loadall()
+    resource = catalog.resources.select{|res| res.type.to_s==type_name and res.title.to_s==resource_name }[0]
+    ( resource.nil?  ?  nil  :  resource[:provider].to_s )
   end
 end
