454ec62e5f20d1d06010ad7d4fbef7ac22425763
vsaienko <vsaienko@mirantis.com>
2015-09-15 22:25:54 +0300

    add tweaks::apache to modular/apache/apache.pp
     * "apachectl graceful" that is called in keystone.pp
       leads to apache hung if it  was previously started with
       "service apache2 start". And started sucessfully if it
       was started with "apachectl graceful".
     * Removed sleep 30 before "apachectl graceful" since it is
       useless
     * Doing 60 sec sleep after apache reload. This sleep is needed
       in order to mark memcache servers as dead in keystone after
       reload. Otherwise 401 error might occur.
     * update noop tests
    
     Further more puppetlabs-apache starting from 1.5.0 version
     natively supports defining 'restart' command [1].
    
    [1] https://github.com/puppetlabs/puppetlabs-apache/commit/f1f554b57fd841adfcfcf263ed5afb2c4d958b09
    
    Closes-Bug: #1493372
    
    Change-Id: I44fdc65c6685041db6ac9d310f148736fc9ed430

--- deployment/puppet/tweaks/manifests/apache_wrappers.pp
+++ deployment/puppet/tweaks/manifests/apache_wrappers.pp
@@ -12,7 +12,7 @@ class tweaks::apache_wrappers (
   # as we have found that sometimes with mod_wsgi apache will crash on a
   # graceful restart - https://github.com/GrahamDumpleton/mod_wsgi/issues/81
   Service <| name == $service_name or title == $service_name |> {
-    restart    => 'sleep 30 && apachectl graceful || apachectl restart',
+    restart    => 'apachectl graceful || apachectl restart',
     hasrestart => true,
   }
 }
