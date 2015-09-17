eecff7773fa6a99088d67f8b87b77bcaaca97056
vsaienko <vsaienko@mirantis.com>
2015-09-17 10:28:44 +0300

    Create Heat Domain with Keystone_domain resource
     This patch replaces the usage of Exec to create the Heat domain, by
     using the Keystone_domain resource recently implemented in
     puppet-keystone.
    
    cherry picked from Upstream commit: 3ced80f)
    
    Change-Id: I8cd5af772867dfe00a131b119e12800a8e2ec021
    Closes-Bug: #1484066

--- deployment/puppet/heat/manifests/keystone/domain.pp
+++ deployment/puppet/heat/manifests/keystone/domain.pp
@@ -1,11 +1,22 @@
 # == Class: heat::keystone::domain
 #
-# Configures heat domain in Keystone.
-#
-# Note: Implementation is done by heat-keystone-setup-domain script temporarily
-#       because currently puppet-keystone does not support v3 API
+# Configures Heat domain in Keystone.
 #
 # === Parameters
+#
+# [*domain_name*]
+#   Heat domain name. Defaults to 'heat'.
+#
+# [*domain_admin*]
+#   Keystone domain admin user which will be created. Defaults to 'heat_admin'.
+#
+# [*domain_admin_email*]
+#   Keystone domain admin user email address. Defaults to 'heat_admin@localhost'.
+
+# [*domain_password*]
+#   Keystone domain admin user password. Defaults to 'changeme'.
+#
+# === Deprecated Parameters
 #
 # [*auth_url*]
 #   Keystone auth url
@@ -19,51 +30,54 @@
 # [*keystone_tenant*]
 #   Keystone admin tenant name
 #
-# [*domain_name*]
-#   Heat domain name. Defaults to 'heat'.
-#
-# [*domain_admin*]
-#   Keystone domain admin user which will be created. Defaults to 'heat_admin'.
-#
-# [*domain_password*]
-#   Keystone domain admin user password. Defaults to 'changeme'.
-#
 class heat::keystone::domain (
-  $auth_url          = undef,
-  $keystone_admin    = undef,
-  $keystone_password = undef,
-  $keystone_tenant   = undef,
-  $domain_name       = 'heat',
-  $domain_admin      = 'heat_admin',
-  $domain_password   = 'changeme',
+  $domain_name        = 'heat',
+  $domain_admin       = 'heat_admin',
+  $domain_admin_email = 'heat_admin@localhost',
+  $domain_password    = 'changeme',
+
+  # DEPRECATED PARAMETERS
+  $auth_url           = undef,
+  $keystone_admin     = undef,
+  $keystone_password  = undef,
+  $keystone_tenant    = undef,
 ) {
 
   include ::heat::params
 
-  $cmd_evn = [
-    "OS_TENANT_NAME=${keystone_tenant}",
-    "OS_USERNAME=${keystone_admin}",
-    "OS_PASSWORD=${keystone_password}",
-    "OS_AUTH_URL=${auth_url}",
-    "HEAT_DOMAIN=${domain_name}",
-    "HEAT_DOMAIN_ADMIN=${domain_admin}",
-    "HEAT_DOMAIN_PASSWORD=${domain_password}"
-  ]
-  exec { 'heat_domain_create':
-    path        => '/usr/bin',
-    command     => 'heat-keystone-setup-domain',
-    environment => $cmd_evn,
-    require     => Package['heat-common'],
-    logoutput   => 'on_failure',
-    # TODO (iberezovskiy): propose upstream patch for retries
-    tries       => 10,
-    try_sleep   => 3
+  if $auth_url {
+    warning('The auth_url parameter is deprecated and will be removed in future releases')
   }
+  if $keystone_admin {
+    warning('The keystone_admin parameter is deprecated and will be removed in future releases')
+  }
+  if $keystone_password {
+    warning('The keystone_password parameter is deprecated and will be removed in future releases')
+  }
+  if $keystone_tenant {
+    warning('The keystone_tenant parameter is deprecated and will be removed in future releases')
+  }
+
+  ensure_resource('keystone_domain', 'heat_domain', {
+    'ensure'  => 'present',
+    'enabled' => true,
+    'name'    => $domain_name
+  })
+  ensure_resource('keystone_user', 'heat_domain_admin', {
+    'ensure'   => 'present',
+    'enabled'  => true,
+    'name'     => $domain_admin,
+    'email'    => $domain_admin_email,
+    'password' => $domain_password,
+    'domain'   => $domain_name,
+  })
+  ensure_resource('keystone_user_role', "${domain_admin}@::${domain_name}", {
+    'roles' => ['admin'],
+  })
 
   heat_config {
     'DEFAULT/stack_domain_admin':          value => $domain_admin;
     'DEFAULT/stack_domain_admin_password': value => $domain_password, secret => true;
     'DEFAULT/stack_user_domain_name':      value => $domain_name;
   }
-
 }
