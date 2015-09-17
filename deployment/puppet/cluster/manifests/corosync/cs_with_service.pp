29ee4cd192e2b35fb58aa32f270a9f6f8f8ef607
Dmitry Ilyin <dilyin@mirantis.com>
2015-09-17 21:00:59 +0300

    Refactor to pcmk_ resources
    Use pcmk_* resources from the new pacemaker module
    instead of the old cs_* resources from the upstream
    corosync modules and custom cs_rsc_* resources from the
    duct tape patched corosync module.
    
    The new pacemaker module is rewritten from the scratch
    and provides improved performance, transparency and
    debuggability. It uses it's own XML engine and depends
    neither on "pcs" nor on "crm" applications.
    
    "corosync" modules will still be used to install the
    corosync cluster, and the "pacemaker" modules will
    be used to work with the cluster's configuration.
    
    Change-Id: Ifd5813c657031cdd7b86368cd971742483a5b8a1
    Related-Bug: 1414583

--- deployment/puppet/cluster/manifests/corosync/cs_with_service.pp
+++ deployment/puppet/cluster/manifests/corosync/cs_with_service.pp
@@ -4,26 +4,24 @@
 define cluster::corosync::cs_with_service (
   $first,
   $second,
-  $cib = undef,
   $score = 'INFINITY',
   $order = true,
   )
 {
-  cs_rsc_colocation { "${second}-with-${first}":
-    ensure     => present,
-    cib        => $cib,
-    primitives => [$second, $first],
+  pcmk_colocation { "${second}-with-${first}":
+    ensure     => 'present',
+    first      => $first,
+    second     => $second,
     score      => $score,
   }
 
   if $order {
-    cs_rsc_order { "${second}-after-${first}":
-      ensure   => present,
-      cib      => $cib,
+    pcmk_order { "${second}-after-${first}":
+      ensure   => 'present',
       first    => $first,
       second   => $second,
       score    => $score,
-      require  => Cs_rsc_colocation["${second}-with-${first}"]
+      require  => Pcmk_colocation["${second}-with-${first}"]
     }
   }
 
