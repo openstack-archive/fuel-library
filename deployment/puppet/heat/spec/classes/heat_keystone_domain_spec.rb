dc04a9c731291a6d5875db12810198eab2c841d3
vsaienko <vsaienko@mirantis.com>
2015-09-17 10:28:44 +0300

    Create Heat Domain with Keystone_domain resource
     This patch replaces the usage of Exec to create the Heat domain, by
     using the Keystone_domain resource recently implemented in
     puppet-keystone.
    
    cherry picked from Upstream commit: 3ced80f)
    
    Change-Id: I8cd5af772867dfe00a131b119e12800a8e2ec021
    Closes-Bug: #1484066

--- deployment/puppet/heat/spec/classes/heat_keystone_domain_spec.rb
+++ deployment/puppet/heat/spec/classes/heat_keystone_domain_spec.rb
@@ -3,13 +3,10 @@ require 'spec_helper'
 describe 'heat::keystone::domain' do
 
   let :params do {
-    :auth_url          => 'http://127.0.0.1:35357/v2.0',
-    :keystone_admin    => 'admin',
-    :keystone_password => 'admin_passwd',
-    :keystone_tenant   => 'admin',
-    :domain_name       => 'heat',
-    :domain_admin      => 'heat_admin',
-    :domain_password   => 'domain_passwd'
+    :domain_name        => 'heat',
+    :domain_admin       => 'heat_admin',
+    :domain_admin_email => 'heat_admin@localhost',
+    :domain_password    => 'domain_passwd'
     }
   end
 
@@ -21,21 +18,23 @@ describe 'heat::keystone::domain' do
       is_expected.to contain_heat_config('DEFAULT/stack_user_domain_name').with_value(params[:domain_name])
     end
 
-    it 'should exec helper script' do
-      is_expected.to contain_exec('heat_domain_create').with(
-        :command     => 'heat-keystone-setup-domain',
-        :path        => '/usr/bin',
-        :require     => 'Package[heat-common]',
-        :logoutput   => 'on_failure',
-        :environment => [
-            "OS_TENANT_NAME=#{params[:keystone_tenant]}",
-            "OS_USERNAME=#{params[:keystone_admin]}",
-            "OS_PASSWORD=#{params[:keystone_password]}",
-            "OS_AUTH_URL=#{params[:auth_url]}",
-            "HEAT_DOMAIN=#{params[:domain_name]}",
-            "HEAT_DOMAIN_ADMIN=#{params[:domain_admin]}",
-            "HEAT_DOMAIN_PASSWORD=#{params[:domain_password]}"
-        ]
+    it 'should create keystone domain' do
+      is_expected.to contain_keystone_domain('heat_domain').with(
+        :ensure  => 'present',
+        :enabled => 'true',
+        :name    => params[:domain_name]
+      )
+
+      is_expected.to contain_keystone_user('heat_domain_admin').with(
+        :ensure   => 'present',
+        :enabled  => 'true',
+        :name     => params[:domain_admin],
+        :email    => params[:domain_admin_email],
+        :password => params[:domain_password],
+        :domain   => params[:domain_name],
+      )
+      is_expected.to contain_keystone_user_role('heat_admin@::heat').with(
+        :roles => ['admin'],
       )
     end
   end
