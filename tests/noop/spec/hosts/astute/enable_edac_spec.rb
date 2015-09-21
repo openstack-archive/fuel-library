c7d2b64d0b21bec468c81db130c46f64c8232db5
Bulat Gaifullin <gaifullinbf@gmail.com>
2015-09-21 16:30:50 +0300

    EDAC support is enabled for slave nodes
    EDAC [1] provides hardware health checking, including ECC and PCI errors.
    
    [1]: https://www.kernel.org/doc/Documentation/edac.txt
    
    Change-Id: Iac2bbe4fb15281a999ba737df2d2fd30192c012c
    Closes-Bug: #1483629

---
+++ tests/noop/spec/hosts/astute/enable_edac_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'astute/enable_edac.pp'
+
+
+describe manifest do
+  shared_examples 'catalog' do
+    let(:facts) do
+      facts.merge(  { :augeasversion => '1.2.0' } )
+    end
+
+    it "should enable edac" do
+      should contain_kmod__load('edac_core')
+
+      ["check_pci_errors", "edac_mc_log_ce", "edac_mc_log_ue"].each do |option|
+        should contain_kmod__option("option #{option}").with(
+          :option => option,
+          :value => '1',
+          :module => 'edac_core'
+        )
+
+        config_file = "/sys/devices/system/edac/pci/#{option}"
+        is_expected.to contain_exec("update #{option}").with(
+          :path    => '/usr/bin:/usr/sbin:/sbin:/bin',
+          :command => "echo -n '1' > '#{config_file}'",
+          :onlyif  => "test -w '#{config_file}' && test '1' != `cat '#{config_file}'`"
+        )
+      end
+    end
+  end
+  test_ubuntu_and_centos manifest
+end
