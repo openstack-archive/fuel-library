d9dcaea1e2069b714f4dd92f58759d00853692a5
Ryan Moe <rmoe@mirantis.com>
2015-09-21 09:51:04 -0700

    Find bridge information by network role
    With network templates there is no guarantee that a bridge
    named 'br-mgmt' or 'br-ex' will exist. Another bridge can
    satisfy the required network roles however.
    
    Change-Id: I85b7955dc77d0b4f30ef0b07be7d5535698b9c22
    Closes-bug: #1498088

--- deployment/puppet/osnailyfacter/modular/virtual_ips/public_vip_ping.pp
+++ deployment/puppet/osnailyfacter/modular/virtual_ips/public_vip_ping.pp
@@ -1,8 +1,10 @@
 notice('MODULAR: public_vip_ping.pp')
 
+prepare_network_config(hiera('network_scheme', {}))
 $run_ping_checker = hiera('run_ping_checker', true)
 $network_scheme = hiera('network_scheme')
-$ping_host_list = $network_scheme['endpoints']['br-ex']['gateway']
+$public_iface = get_network_role_property('public/vip', 'interface')
+$ping_host_list = $network_scheme['endpoints'][$public_iface]['gateway']
 
 if $run_ping_checker {
   $vip = 'vip__public'
