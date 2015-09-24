aed51e07d453d8cfb60de6b75014df471ac4aafd
iberezovskiy <iberezovskiy@mirantis.com>
2015-09-24 13:07:44 +0300

    Fix show_image_direct_url value for glance-api
    show_image_direct_url should be set to false
    when swift is used in glance backend by default.
    It fixes potential vulnerability in glance.
    In another cases default for this parameters is true.
    
    Change-Id: I3dc8ffe65d98e8ce03fddeb966503901c543272e
    Closes-bug: #1498615

--- deployment/puppet/osnailyfacter/modular/glance/glance.pp
+++ deployment/puppet/osnailyfacter/modular/glance/glance.pp
@@ -51,7 +51,6 @@ $glance_vcenter_datastore       = $glance_hash['vc_datastore']
 $glance_vcenter_image_dir       = $glance_hash['vc_image_dir']
 $glance_vcenter_api_retry_count = '20'
 $glance_image_cache_max_size    = $glance_hash['image_cache_max_size']
-$glance_show_image_direct_url   = pick($glance_hash['show_image_direct_url'], true)
 $glance_pipeline                = pick($glance_hash['pipeline'], 'keystone')
 $glance_large_object_size       = pick($glance_hash['large_object_size'], '5120')
 
@@ -60,13 +59,16 @@ $rados_connect_timeout          = '30'
 if ($storage_hash['images_ceph']) {
   $glance_backend = 'ceph'
   $glance_known_stores = [ 'glance.store.rbd.Store', 'glance.store.http.Store' ]
+  $glance_show_image_direct_url = pick($glance_hash['show_image_direct_url'], true)
 } elsif ($storage_hash['images_vcenter']) {
   $glance_backend = 'vmware'
   $glance_known_stores = [ 'glance.store.vmware_datastore.Store', 'glance.store.http.Store' ]
+  $glance_show_image_direct_url = pick($glance_hash['show_image_direct_url'], true)
 } else {
   $glance_backend = 'swift'
   $glance_known_stores = [ 'glance.store.swift.Store', 'glance.store.http.Store' ]
   $swift_store_large_object_size = $glance_large_object_size
+  $glance_show_image_direct_url = pick($glance_hash['show_image_direct_url'], false)
 }
 
 ###############################################################################
