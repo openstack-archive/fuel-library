122e8445bebd417b7c71d1eb49f6d214d60439be
Armin ranjbar <zoup@zoup.org>
2015-09-25 20:32:36 +0330

    removes deprecated rsyslogd -c option on newer packages.
    -c options will be deprecated after 5.8.6-1ubuntu8.9, using it
    will cause rsyslogd to not start.
    
    Change-Id: I05c8565ebafae8df9ba7b54b9cfe6af3c4379620
    Closes-Bug: #1488237
    Co-Authored-By:Bartłomiej Piotrowski <bpiotrowski@mirantis.com>

--- deployment/puppet/rsyslog/manifests/config.pp
+++ deployment/puppet/rsyslog/manifests/config.pp
@@ -19,22 +19,11 @@ class rsyslog::config {
         path    => $::rsyslog::params::rsyslog_queues_dir,
     }
 
-case $osfamily {
-    'Debian': {
-      file { $rsyslog::params::rsyslog_default:
-        ensure  => file,
-        source  => "puppet:///modules/rsyslog/rsyslog_default",
-      }
+    file { $rsyslog::params::rsyslog_default:
+      ensure  => file,
+      content => template("${module_name}/rsyslog_default.erb"),
     }
-    'RedHat': {
-      file { "/etc/sysconfig/rsyslog":
-         content => template("rsyslog/rsyslog.erb"),
-      }
-    }
-    default: {
-        fail("Unsupported osfamily: ${osfamily} for os ${operatingsystem}")
-    }
-}
+
     file { $rsyslog::params::spool_dir:
         ensure  => directory,
     }
