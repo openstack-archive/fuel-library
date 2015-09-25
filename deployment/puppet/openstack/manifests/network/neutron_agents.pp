a02d3ff9af55c903ff29dd8cd6dd9243e31116cc
Sergey Kolekonov <skolekonov@mirantis.com>
2015-09-25 14:29:04 +0300

    Enable ARP responder for L2 population
    Local ARP responder is needed to serve ARP broadcasts locally on compute node
    by neutron OVS agent. This is a part of L2 population feature.
    
    Closes-bug: #1483584
    Change-Id: Ic1f1809fa93fa8fc715ff638a9e365b49a0e1af4

--- deployment/puppet/openstack/manifests/network/neutron_agents.pp
+++ deployment/puppet/openstack/manifests/network/neutron_agents.pp
@@ -94,6 +94,7 @@ class openstack::network::neutron_agents (
       tunnel_types               => $tunnel_types,
       enable_distributed_routing => $agent_mode ? { 'legacy' => false, default => true},
       l2_population              => $l2_population,
+      arp_responder              => $l2_population,
       manage_service             => true,
       enabled                    => true,
     }
