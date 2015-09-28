571a00e2644d10279014a738ca2883fcf6c35f20
Dmitry Ilyin <dilyin@mirantis.com>
2015-09-28 22:23:24 +0300

    Refactor to pcmk_ resources
    Use pcmk_* resources from the new pacemaker module
    instead of the old cs_* resources from the upstream
    corosync modules and custom cs_rsc_* resources from the
    duct tape patched corosync module.
    
    The new pacemaker module is rewritten from the scratch
    and provides improved performance, transparency and
    debuggability. It uses it's own XML engine and depends
    neither on "pcs" nor on "crm" applications.
    
    "corosync" modules will still be used to install the
    corosync cluster, and the "pacemaker" modules will
    be used to work with the cluster's configuration.
    
    Refactor pacemaker services to use pacemaker::service
    wrapper. It controlls primitive creation and run in the
    uniform way and creates ocf_handler helpers for
    manual primitive management.
    
    Add run_failed_log feature to fuel_noop_tests to
    replay the failed tests after they were fixed.
    
    Change-Id: Ifd5813c657031cdd7b86368cd971742483a5b8a1
    Related-Bug: 1414583

--- deployment/puppet/osnailyfacter/modular/virtual_ips/public_vip_ping.pp
+++ deployment/puppet/osnailyfacter/modular/virtual_ips/public_vip_ping.pp
@@ -2,9 +2,10 @@ notice('MODULAR: public_vip_ping.pp')
 
 $run_ping_checker = hiera('run_ping_checker', true)
 $network_scheme = hiera('network_scheme')
-$ping_host_list = $network_scheme['endpoints']['br-ex']['gateway']
+$ping_host_list = try_get_value($network_scheme, 'endpoints/br-ex/gateway', [])
+$primary_controller = hiera('primary_controller')
 
-if $run_ping_checker {
+if $run_ping_checker and $primary_controller {
   $vip = 'vip__public'
 
   cluster::virtual_ip_ping { $vip :
