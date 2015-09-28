8134f6e0c4812e29c51aa612e63fbe7b5a695bc8
Dmitry Ilyin <dilyin@mirantis.com>
2015-09-28 22:23:24 +0300

    Refactor to pcmk_ resources
    Use pcmk_* resources from the new pacemaker module
    instead of the old cs_* resources from the upstream
    corosync modules and custom cs_rsc_* resources from the
    duct tape patched corosync module.
    
    The new pacemaker module is rewritten from the scratch
    and provides improved performance, transparency and
    debuggability. It uses it's own XML engine and depends
    neither on "pcs" nor on "crm" applications.
    
    "corosync" modules will still be used to install the
    corosync cluster, and the "pacemaker" modules will
    be used to work with the cluster's configuration.
    
    Refactor pacemaker services to use pacemaker::service
    wrapper. It controlls primitive creation and run in the
    uniform way and creates ocf_handler helpers for
    manual primitive management.
    
    Add run_failed_log feature to fuel_noop_tests to
    replay the failed tests after they were fixed.
    
    Change-Id: Ifd5813c657031cdd7b86368cd971742483a5b8a1
    Related-Bug: 1414583

--- deployment/puppet/cluster/spec/classes/cluster_dns_ocf_spec.rb
+++ deployment/puppet/cluster/spec/classes/cluster_dns_ocf_spec.rb
@@ -2,61 +2,35 @@ require 'spec_helper'
 
 describe 'cluster::dns_ocf' do
 
-  let(:default_params) { {
-  } }
+  let(:default_params) do
+    {}
+  end
 
   shared_examples_for 'dns_ocf configuration' do
     let :params do
       default_params
     end
 
-    context 'with default params' do
-      it_raises 'a Puppet::Error', /primary_controller/
-    end
+    it 'configures with the params params' do
+      should contain_class('cluster::dns_ocf')
 
-    context 'with primary_controller = true' do
-      let :params do
-        default_params.merge!({
-          :primary_controller => true,
-        })
-      end
+      should contain_pcmk_resource('p_dns')
 
-      it 'configures with the params params' do
-        should contain_class('cluster::dns_ocf')
-        should contain_cs_resource('p_dns').with_before('Cs_rsc_colocation[dns-with-vrouter-ns]')
-        should contain_cs_rsc_colocation('dns-with-vrouter-ns').with(
-          :ensure => 'present',
-          :score  => 'INFINITY',
-          :primitives => [ 'clone_p_dns', 'clone_p_vrouter' ])
-        should contain_service('p_dns').with(
-          :name       => 'p_dns',
-          :enable     => true,
-          :ensure     => 'running',
-          :hasstatus  => true,
-          :hasrestart => true,
-          :provider   => 'pacemaker')
-      end
-    end
+      should contain_pcmk_colocation('dns-with-vrouter-ns').with(
+        :ensure => 'present',
+        :score  => 'INFINITY',
+        :first  => 'clone_p_vrouter',
+        :second => 'clone_p_dns'
+      ).that_requires('Pcmk_resource[p_dns]')
 
-    context 'with primary_controller = false' do
-      let :params do
-        default_params.merge!({
-          :primary_controller => false,
-        })
-      end
-
-      it 'configures with the params params' do
-        should_not contain_cs_resource('p_dns')
-        should_not contain_cs_rsc_colocation('dns-with-vrouter-ns')
-        should contain_service('p_dns').with(
-          :name       => 'p_dns',
-          :enable     => true,
-          :ensure     => 'running',
-          :hasstatus  => true,
-          :hasrestart => true,
-          :provider   => 'pacemaker')
-
-      end
+      should contain_service('p_dns').with(
+        :name       => 'p_dns',
+        :enable     => true,
+        :ensure     => 'running',
+        :hasstatus  => true,
+        :hasrestart => true,
+        :provider   => 'pacemaker',
+      ).that_requires('Pcmk_colocation[dns-with-vrouter-ns]')
     end
   end
 
