64c0cc73f6c085b7f66ad7a497ed6baf5578bc2d
Andrey Shestakov <ashestakov@mirantis.com>
2015-09-29 17:42:53 +0300

    [Ironic] Add ironic logs collector
    This change introduces new UDP listener for rsyslog
    
    Rsyslog listener on ironic node is need to collect logs from baremetal nodes.
    All received logs should be saved in /var/log/remote/ directory.
    
    Partially Implements: blueprint fuel-integrate-ironic
    Change-Id: I7385fbc13067cf0bb42beb6b86304e6671ea2f1d

--- tests/noop/spec/hosts/logging/logging_spec.rb
+++ tests/noop/spec/hosts/logging/logging_spec.rb
@@ -15,6 +15,15 @@ describe manifest do
         should_not contain_file('/var/log')
       end
     }
+    if Noop.hiera('node_role') == 'ironic'
+      it 'should contain 70-ironic.conf' do
+        should contain_file('/etc/rsyslog.d/70-ironic.conf').with(
+          'owner' => 'syslog',
+          'group' => 'syslog',
+          'mode'  => '0640',
+        )
+      end
+    end
   end
   test_ubuntu_and_centos manifest
 end
