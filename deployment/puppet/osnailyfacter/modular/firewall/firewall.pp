39e7875229c9c05afb6a3d88ca144c8bb8886ded
Andrey Shestakov <ashestakov@mirantis.com>
2015-09-29 16:28:18 +0300

    [Ironic] Add firewall rules for baremetal network
    This change introduces new iptables chain for baremetal network
    
    Following rules added:
    * new chain for baremetal network
    * allow pings from VIP on controller
    * allow rsyslog on conductor
    * allow TFTP on conductor
    * drop all other traffic from baremetal network
    
    Additional helper module for TFTP added.
    
    Change-Id: Idb756c48c88da0dbdd0a8850df4f268ceed42cf2

--- deployment/puppet/osnailyfacter/modular/firewall/firewall.pp
+++ deployment/puppet/osnailyfacter/modular/firewall/firewall.pp
@@ -1,5 +1,7 @@
 notice('MODULAR: firewall.pp')
 
+$ironic_hash = hiera_hash('ironic', {})
+
 # Workaround for fuel bug with firewall
 firewall {'003 remote rabbitmq ':
   sport   => [ 4369, 5672, 41055, 55672, 61613 ],
@@ -39,4 +41,82 @@ class { 'openstack::firewall' :
   nova_vnc_ip_range => get_network_role_property('nova/api', 'network'),
   libvirt_network   => get_network_role_property('management', 'network'),
   keystone_network  => get_network_role_property('keystone/api', 'network'),
+}
+
+if $ironic_hash['enabled'] {
+  $nodes_hash        = hiera('nodes', {})
+  $roles             = node_roles($nodes_hash, hiera('uid'))
+  $network_scheme    = hiera('network_scheme', {})
+  $network_metadata  = hiera_hash('network_metadata', {})
+  prepare_network_config($network_scheme)
+  $baremetal_int     = get_network_role_property('ironic/baremetal', 'interface')
+  $baremetal_vip     = $network_metadata['vips']['baremetal']['ipaddr']
+  $baremetal_ipaddr  = get_network_role_property('ironic/baremetal', 'ipaddr')
+  $baremetal_network = get_network_role_property('ironic/baremetal', 'network')
+
+  firewallchain { 'baremetal:filter:IPv4':
+    ensure => present,
+  } ->
+  firewall { '999 drop all baremetal':
+    chain  => 'baremetal',
+    action => 'drop',
+    proto  => 'all',
+  } ->
+  firewall {'00 baremetal-filter ':
+    proto   => 'all',
+    iniface => $baremetal_int,
+    jump    => 'baremetal',
+    require => Class['openstack::firewall'],
+  }
+
+  if member($roles, 'controller') or member($roles, 'primary-controller') {
+    firewall { '100 allow baremetal ping from VIP':
+      chain       => 'baremetal',
+      source      => $baremetal_vip,
+      destination => $baremetal_ipaddr,
+      proto       => 'icmp',
+      icmp        => 'echo-request',
+      action      => 'accept',
+    }
+  }
+
+  if member($roles, 'ironic') {
+    firewall { '101 allow baremetal-related':
+      chain       => 'baremetal',
+      source      => $baremetal_network,
+      destination => $baremetal_ipaddr,
+      proto       => 'all',
+      state       => ['RELATED', 'ESTABLISHED'],
+      action      => 'accept',
+    }
+
+    firewall { '102 allow baremetal-rsyslog':
+      chain       => 'baremetal',
+      source      => $baremetal_network,
+      destination => $baremetal_ipaddr,
+      proto       => 'udp',
+      dport       => '514',
+      action      => 'accept',
+    }
+
+    firewall { '103 allow baremetal-TFTP':
+      chain       => 'baremetal',
+      source      => $baremetal_network,
+      destination => $baremetal_ipaddr,
+      proto       => 'udp',
+      dport       => '69',
+      action      => 'accept',
+    }
+
+    exec { 'fix_ipt_modules':
+      command => '/bin/sed -i "s/^IPT_MODULES=.*/IPT_MODULES=\"nf_conntrack_ftp nf_nat_ftp nf_conntrack_netbios_ns nf_conntrack_tftp\"/g" /etc/default/ufw',
+      unless  => '/bin/grep "^IPT_MODULES=.*nf_conntrack_tftp" /etc/default/ufw > /dev/null',
+      notify  => Exec['load_tftp_mod']
+    }
+
+    exec { 'load_tftp_mod':
+      command     => '/sbin/modprobe nf_conntrack_tftp',
+      refreshonly => true,
+    }
+  }
 }
