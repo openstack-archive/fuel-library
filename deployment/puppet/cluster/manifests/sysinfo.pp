339a002e4ab1cba3b7d9cb5cd6b4af3b5f7253e3
Alex Schultz <aschultz@mirantis.com>
2015-09-30 11:18:51 -0500

    Fix sysinfo resource declaration
    This change replaces the use of clone resources as each node may have a
    different disk layout. Additionally this change replaces cs_location
    with cs_rsc_location which has a proper autorequire for the cs_resource
    being defined.
    
    Change-Id: Ifa8a6932c0f0ba2bc05bc0ea9278cbfbe5ae8bb9
    Closes-Bug: #1501410
    Closes-Bug: #1504085

--- deployment/puppet/cluster/manifests/sysinfo.pp
+++ deployment/puppet/cluster/manifests/sysinfo.pp
@@ -4,11 +4,6 @@
 #
 # === Parameters
 #
-# [*primary_controller*]
-#  (required) Boolean to indicate if this is the primary controller or not. The
-#  resources only get defined on the primary controller for the cluster but the
-#  location is defined on any node on this cluster that this should run on.
-#
 # [*disks*]
 #  (optional) array of mount points to monitor for free space. / is monitored
 #  by default it does not need to be specified.
@@ -31,7 +26,6 @@
 #  Defaults to present
 #
 class cluster::sysinfo (
-  $primary_controller,
   $disks            = [],
   $min_disk_free    = '100M',
   $disk_unit        = 'M',
@@ -39,32 +33,31 @@ class cluster::sysinfo (
   $monitor_ensure   = present,
 ) {
 
-  if $primary_controller {
-    cs_resource { 'sysinfo':
-      ensure          => $monitor_ensure,
-      primitive_class => 'ocf',
-      provided_by     => 'pacemaker',
-      primitive_type  => 'SysInfo',
-      complex_type    => 'clone',
-      parameters      => {
-        'disks'         => join(any2array($disks), ' '),
-        'min_disk_free' => $min_disk_free,
-        'disk_unit'     => $disk_unit,
-      },
-      operations      => { 'monitor' => { 'interval' => $monitor_interval } },
-    }
-
-    # Have service migrate if health turns red from the failed disk check
-    cs_property { 'node-health-strategy':
-      ensure   => present,
-      value    => 'migrate-on-red',
-      provider => 'crm',
-    }
+  # NOTE: We do not use a clone resource here as disks may be different per host
+  cs_resource { "sysinfo_${::fqdn}":
+    ensure          => $monitor_ensure,
+    primitive_class => 'ocf',
+    provided_by     => 'pacemaker',
+    primitive_type  => 'SysInfo',
+    parameters      => {
+      'disks'         => join(any2array($disks), ' '),
+      'min_disk_free' => $min_disk_free,
+      'disk_unit'     => $disk_unit,
+    },
+    operations      => { 'monitor' => { 'interval' => $monitor_interval } },
   }
 
-  cs_location { "clone_sysinfo-on-${::fqdn}":
-    primitive => 'clone_sysinfo',
-    node_name => $::fqdn,
-    score     => 'INFINITY',
+  # Have service migrate if health turns red from the failed disk check
+  cs_property { 'node-health-strategy':
+    ensure   => present,
+    value    => 'migrate-on-red',
+    provider => 'crm',
+  }
+
+  cs_rsc_location { "sysinfo-on-${::fqdn}":
+    primitive  => "sysinfo_${::fqdn}",
+    node_name  => $::fqdn,
+    node_score => 'INFINITY',
+    cib        => "sysinfo_${::fqdn}",
   }
 }
