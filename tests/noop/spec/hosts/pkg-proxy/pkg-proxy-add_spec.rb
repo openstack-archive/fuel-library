8e6348c430fba4e9dd1641b41ffd5bb673bd1bb4
Aleksandr Didenko <adidenko@mirantis.com>
2015-10-02 14:18:05 +0300

    Use nginx proxy for packages before netconfig
    When we use multirack topology then right after provisioning all
    nodes from non-default node-group have default route set to their
    rack router IP. So in case of isolated admin network (when local
    package mirrors and/or Internet are not accessible via admin net)
    such nodes won't be able to download and install any packages
    until we configure all the interfaces (management, public, etc).
    And network configuration itself requires some OVS packages.
    In order to solve this issue we setup nginx proxy on Fuel master
    node and configure Apt to use it. We remove it after netconfig
    task.
    
    Integration tests disabled since they won't pass until we merge
    Ida30b5de9f730821ae969032019adf409cfa82ca and upload ISO with it
    to CI slave nodes.
    Fuel-CI: disable
    
    Change-Id: I7d729a1ca6c602b033e697c26ca7ce3f429849dc
    Closes-bug: #1497378

---
+++ tests/noop/spec/hosts/pkg-proxy/pkg-proxy-add_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'pkg-proxy/pkg-proxy-add.pp'
+
+describe manifest do
+
+  shared_examples 'catalog' do
+
+    it {
+      if facts[:osfamily] == 'Debian'
+        should contain_class('apt').with(
+          'proxy' => {
+            'host' => Noop.hiera('master_ip'),
+            'port' => '2080',
+          }
+        )
+      end
+    }
+
+  end
+
+  test_ubuntu_and_centos manifest
+end
+
