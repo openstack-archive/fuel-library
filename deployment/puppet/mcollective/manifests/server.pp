8f11b80065564decbbdce71a223baa90d5c2d3bf
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-06 19:26:56 +0300

    [WIP] CentOS 7 Support (supervisord)
    Based on https://review.openstack.org/#/c/231579/50
    
    Change-Id: Ic69719bc454c5ac445cd028f559f2aeba441da11
    Fuel-CI: disable

--- deployment/puppet/mcollective/manifests/server.pp
+++ deployment/puppet/mcollective/manifests/server.pp
@@ -28,19 +28,20 @@ class mcollective::server(
   case $operatingsystem {
     /(?i)(centos|redhat)/:  {
       # THIS PACKAGE ALSO INSTALLS REQUIREMENTS
-      case $::rubyversion {
-        # ruby21-mcollective-common
-        # ruby21-rubygem-stomp
-        '2.1.1': {
-          $mcollective_package = "ruby21-mcollective"
-        }
-        # mcollective-common
-        # rubygems
-        # rubygem-stomp
-        '1.8.7': {
-          $mcollective_package = "mcollective"
-        }
-      }
+      $mcollective_package = "mcollective"
+#      case $::rubyversion {
+#        # ruby21-mcollective-common
+#        # ruby21-rubygem-stomp
+#        '2.+': {
+#          $mcollective_package = "mcollective"
+#        }
+#        # mcollective-common
+#        # rubygems
+#        # rubygem-stomp
+#        '1.8.7': {
+#          $mcollective_package = "mcollective"
+#        }
+#      }
     }
     default: {
       fail("Unsupported operating system: ${operatingsystem}")
@@ -51,9 +52,9 @@ class mcollective::server(
 
   file {"/etc/mcollective/server.cfg" :
     content => template("mcollective/server.cfg.erb"),
-    owner => root,
-    group => root,
-    mode => 0600,
+    owner => 'root',
+    group => 'root',
+    mode => '0600',
     require => Package[$mcollective_package],
     notify => Service['mcollective'],
   }
--- deployment/puppet/nailgun/examples/postgres-only.pp
+++ deployment/puppet/nailgun/examples/postgres-only.pp
@@ -1,11 +1,7 @@
 $fuel_settings = parseyaml($astute_settings_yaml)
 
-$postgres_default_version = '9.3'
-
 # install and configure postgresql server
 class { 'postgresql::globals':
-  version             => $postgres_default_version,
-  bindir              => "/usr/pgsql-${postgres_default_version}/bin",
   server_package_name => "postgresql-server",
   client_package_name => "postgresql",
 }
@@ -53,11 +49,12 @@ postgresql::server::db { $ostf_dbname:
   require => Class['::postgresql::server'],
 }
 
-Class['postgresql::server'] -> Postgres_config<||>
-Postgres_config { ensure => present }
-postgres_config {
-  log_directory     : value => "'/var/log/'";
-  log_filename      : value => "'pgsql'";
-  log_rotation_age  : value => "7d";
-}
-
+#FIXME(mattymo): These were added by sbog, but conflict with centos7 pgsql rpm
+#Class['postgresql::server'] -> Postgres_config<||>
+#Postgres_config { ensure => present }
+#postgres_config {
+#  log_directory     : value => "'/var/log/'";
+#  log_filename      : value => "'pgsql'";
+#  log_rotation_age  : value => "7d";
+#}
+ 
