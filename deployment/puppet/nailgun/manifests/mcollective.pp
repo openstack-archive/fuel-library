f419019e1c5407af06b72fa9b2a465c8720d5510
vsaienko <vsaienko@mirantis.com>
2015-10-07 22:25:21 +0300

    [Ironic] build bootstrap
     install additional packages to mcollective container:
      - fuel-agent-bootstrap-ironic-configs
      - fuel-bootstrap-image-builder
     add upload image task
    
    Partially Implements: blueprint fuel-integrate-ironic
    Depends-On: I99c9c8081a277431cf3fe65703059962cae3c746
    Change-Id: I01354d8865f99d3587cf315f7d28d30dae72d375

--- deployment/puppet/nailgun/manifests/mcollective.pp
+++ deployment/puppet/nailgun/manifests/mcollective.pp
@@ -5,4 +5,6 @@ class nailgun::mcollective
   ensure_packages('fuel-agent')
   ensure_packages('fuel-provisioning-scripts')
   ensure_packages('shotgun')
+  ensure_packages('ironic-fa-bootstrap-configs')
+  ensure_packages('fuel-bootstrap-image-builder')
 }
