a46c362cf5debf46d177b06154803dcbb018ab60
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2015-10-10 12:57:17 +0200

    Refactor OCF functions
    - Combine heat_engine_ubuntu and heat_engine_centos into single OCF
      script heat-engine
      - Remove unused dependency from /lib/lsb/init-functions and
        /etc/rc.d/init.d/functions
      - Remove start-stop-daemon and daemon to 'su' equivalent to make
        heat-engine script typical to all OpenStack services OCF scripts
    - Move port validation *_check_port from all OCF script to OCF library
      (validate_port)
       - ceilometer_agent_central_check_port()
       - ceilometer_alarm_evaluator_check_port()
       - heat_engine_check_port()
       - nova_compute_check_port()
       - write bats unit tests for validate_port
    - Remove 'ocf-' from neutron OCF scripts names to make them typical to all
      OpenStack services
      - Rename ocf-neutron-dhcp-agent to neutron-dhcp-agent
      - Rename ocf-neutron-l3-agent to neutron-l3-agent
      - Rename ocf-neutron-ovs-agent to neutron-ovs-agent
    - Rename rabbitmq to rabbitmq-server. This removes unneccessary mv in
      debian/rules and fuel-library8.0.spec
    - Some minor bash typos and old comments clean-up
    
    Change-Id: Ic4bcecdbc05f6306be63dea211df96a104cb2d36
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/heat_ha/manifests/engine.pp
+++ deployment/puppet/heat_ha/manifests/engine.pp
@@ -3,12 +3,6 @@ class heat_ha::engine inherits heat::engine {
 
   $primitive_type  = 'heat-engine'
 
-  #  if $::osfamily == 'RedHat' {
-  #  $ocf_script_template = 'heat/heat_engine_centos.ocf.erb'
-  #} else {
-  #  $ocf_script_template = 'heat/heat_engine_ubuntu.ocf.erb'
-  #}
-
   # migration-threshold is number of tries to
   # start resource on each controller node
   $metadata = {
@@ -39,7 +33,6 @@ class heat_ha::engine inherits heat::engine {
     complex_type        => 'clone',
     ms_metadata         => $ms_metadata,
     operations          => $operations,
-    #    ocf_script_template => $ocf_script_template,
   }
 
 }
