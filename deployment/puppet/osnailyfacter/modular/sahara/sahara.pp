d7a0d8486aa3eae84eacd2eb45596b530ed5cc4a
Denis Egorenko <degorenko@mirantis.com>
2015-10-12 16:56:17 +0300

    Set up db parameters for Sahara
    Specify db parameters for Sahara to avoid occasional errors during
    cluster spawning.
    
    Change-Id: Ib62dea8654878af2645f08508918bb877f8e6e15
    Closes-bug: #1497365

--- deployment/puppet/osnailyfacter/modular/sahara/sahara.pp
+++ deployment/puppet/osnailyfacter/modular/sahara/sahara.pp
@@ -43,6 +43,10 @@ if $sahara_hash['enabled'] {
   $db_name         = pick($sahara_hash['db_name'], 'sahara')
   $db_password     = pick($sahara_hash['db_password'])
   $db_host         = pick($sahara_hash['db_host'], $database_vip)
+  $max_pool_size   = min($::processorcount * 5 + 0, 30 + 0)
+  $max_overflow    = min($::processorcount * 5 + 0, 60 + 0)
+  $max_retries     = '-1'
+  $idle_timeout    = '3600'
   $read_timeout    = '60'
   $sql_connection  = "mysql://${db_user}:${db_password}@${db_host}/${db_name}?read_timeout=${read_timeout}"
 
@@ -65,6 +69,8 @@ if $sahara_hash['enabled'] {
     plugins             => [ 'ambari', 'cdh', 'mapr', 'spark', 'vanilla' ],
     log_facility        => $syslog_log_facility_sahara,
     database_connection => $sql_connection,
+    max_retries         => $max_retries,
+    idle_timeout        => $idle_timeout,
     auth_uri            => "http://${service_endpoint}:5000/v2.0/",
     identity_uri        => "http://${service_endpoint}:35357/",
     rpc_backend         => 'rabbit',
@@ -77,6 +83,11 @@ if $sahara_hash['enabled'] {
     rabbit_ha_queues    => $rabbit_ha_queues,
     rabbit_port         => $amqp_port,
     rabbit_hosts        => split($amqp_hosts, ',')
+  }
+
+  sahara_config {
+    'database/max_overflow':  value => $max_overflow;
+    'database/max_pool_size': value => $max_pool_size;
   }
 
   if $public_ssl_hash['services'] {
