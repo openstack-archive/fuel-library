0ac16d9ed1a4d72d9549a0ca39f7d4920f7ada66
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-10-13 20:01:03 +0300

    Fix keystone URL and realign keystone for sahara and murano
    Keystone url needs to point to service_endpoint to enable
    plugins to override the location of keystone service.
    
    Updated keystone tasks to align with other openstack
    services, but added FIXME for configure_user and
    configure_user_role. They should be fixed in their
    respective modules first.
    
    Change-Id: Ib36b8e2d522c02009605e428120acda1e89ee569
    Closes-Bug: #1499375

--- deployment/puppet/osnailyfacter/modular/sahara/keystone.pp
+++ deployment/puppet/osnailyfacter/modular/sahara/keystone.pp
@@ -1,16 +1,9 @@
 notice('MODULAR: sahara/keystone.pp')
 
-$sahara_hash     = hiera_hash('sahara_hash', {})
-$public_ssl_hash = hiera('public_ssl')
-$public_vip      = hiera('public_vip')
-$admin_address   = hiera('management_vip')
-$api_bind_port   = '8386'
-$sahara_user     = pick($sahara_hash['user'], 'sahara')
-$sahara_password = pick($sahara_hash['user_password'])
-$tenant          = pick($sahara_hash['tenant'], 'services')
-$region          = pick($sahara_hash['region'], hiera('region', 'RegionOne'))
-$service_name    = pick($sahara_hash['service_name'], 'sahara')
-$public_address = $public_ssl_hash['services'] ? {
+$sahara_hash         = hiera_hash('sahara_hash', {})
+$public_vip          = hiera('public_vip')
+$public_ssl_hash     = hiera('public_ssl')
+$public_address  = $public_ssl_hash['services'] ? {
   true    => $public_ssl_hash['hostname'],
   default => $public_vip,
 }
@@ -18,17 +11,39 @@ $public_protocol = $public_ssl_hash['services'] ? {
   true    => 'https',
   default => 'http',
 }
+$admin_address       = hiera('management_vip')
+$region              = pick($sahara_hash['region'], hiera('region', 'RegionOne'))
+$password            = $sahara_hash['user_password']
+$auth_name           = pick($sahara_hash['auth_name'], 'sahara')
+$configure_endpoint  = pick($sahara_hash['configure_endpoint'], true)
+#FIXME(mattymo): Add configure_user and configure_user_role to
+#  sahara::keystone::auth
+#$configure_user      = pick($sahara_hash['configure_user'], true)
+#$configure_user_role = pick($sahara_hash['configure_user_role'], true)
+$service_name        = pick($sahara_hash['service_name'], 'sahara')
+$tenant              = pick($sahara_hash['tenant'], 'services')
+
+
+validate_string($public_address)
+validate_string($password)
+
+$api_bind_port  = '8386'
 $public_url      = "${public_protocol}://${public_address}:${api_bind_port}/v1.1/%(tenant_id)s"
 $admin_url       = "http://${admin_address}:${api_bind_port}/v1.1/%(tenant_id)s"
+#################################################################
 
 class { 'sahara::keystone::auth':
-  auth_name    => $sahara_user,
-  password     => $sahara_password,
-  service_type => 'data_processing',
-  service_name => $service_name,
-  region       => $region,
-  tenant       => $tenant,
-  public_url   => $public_url,
-  admin_url    => $admin_url,
-  internal_url => $admin_url,
+  password            => $password,
+  auth_name           => $auth_name,
+  tenant              => $tenant,
+  configure_endpoint  => $configure_endpoint,
+#FIXME(mattymo): Add configure_user and configure_user_role to
+#  sahara::keystone::auth
+#  configure_user      => $configure_user,
+#  configure_user_role => $configure_user_role,
+  service_name        => $service_name,
+  public_url          => $public_url,
+  internal_url        => $admin_url,
+  admin_url           => $admin_url,
+  region              => $region,
 }
