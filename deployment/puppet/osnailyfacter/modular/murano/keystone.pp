a1d8337c211abd11ee74f2ddd4e992b29e0183c3
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-10-13 20:01:03 +0300

    Fix keystone URL and realign keystone for sahara and murano
    Keystone url needs to point to service_endpoint to enable
    plugins to override the location of keystone service.
    
    Updated keystone tasks to align with other openstack
    services, but added FIXME for configure_user and
    configure_user_role. They should be fixed in their
    respective modules first.
    
    Change-Id: Ib36b8e2d522c02009605e428120acda1e89ee569
    Closes-Bug: #1499375

--- deployment/puppet/osnailyfacter/modular/murano/keystone.pp
+++ deployment/puppet/osnailyfacter/modular/murano/keystone.pp
@@ -1,36 +1,50 @@
 notice('MODULAR: murano/keystone.pp')
 
-$murano_hash                = hiera_hash('murano_hash', {})
-$public_ip                  = hiera('public_vip')
-$management_ip              = hiera('management_vip')
-$public_ssl                 = hiera('public_ssl')
-$region                     = hiera('region', 'RegionOne')
-
-
-$public_protocol = $public_ssl['services'] ? {
+$murano_hash         = hiera_hash('murano_hash', {})
+$public_vip          = hiera('public_vip')
+$public_ssl_hash     = hiera('public_ssl')
+$public_address  = $public_ssl_hash['services'] ? {
+  true    => $public_ssl_hash['hostname'],
+  default => $public_vip,
+}
+$public_protocol = $public_ssl_hash['services'] ? {
   true    => 'https',
   default => 'http',
 }
+$admin_address       = hiera('management_vip')
+$region              = pick($murano_hash['region'], hiera('region', 'RegionOne'))
+$password            = $murano_hash['user_password']
+$auth_name           = pick($murano_hash['auth_name'], 'murano')
+$configure_endpoint  = pick($murano_hash['configure_endpoint'], true)
+#FIXME(mattymo): Add configure_user and configure_user_role to
+#  murano::keystone::auth
+#$configure_user      = pick($murano_hash['configure_user'], true)
+#$configure_user_role = pick($murano_hash['configure_user_role'], true)
+$service_name        = pick($murano_hash['service_name'], 'murano')
+$tenant              = pick($murano_hash['tenant'], 'services')
 
-$public_address = $public_ssl['services'] ? {
-  true    => $public_ssl['hostname'],
-  default => $public_ip,
-}
+
+validate_string($public_address)
+validate_string($password)
 
 $api_bind_port  = '8082'
-
-$tenant         = pick($murano_hash['tenant'], 'services')
 $public_url     = "${public_protocol}://${public_address}:${api_bind_port}"
 $admin_url      = "http://${management_ip}:${api_bind_port}"
 
 #################################################################
 
 class { 'murano::keystone::auth':
-  password     => $murano_hash['user_password'],
-  service_type => 'application_catalog',
-  region       => $region,
-  tenant       => $tenant,
-  public_url   => $public_url,
-  admin_url    => $admin_url,
-  internal_url => $admin_url,
+  password            => $password,
+  auth_name           => $auth_name,
+  tenant              => $tenant,
+  configure_endpoint  => $configure_endpoint,
+#FIXME(mattymo): Add configure_user and configure_user_role to
+#  murano::keystone::auth
+#  configure_user      => $configure_user,
+#  configure_user_role => $configure_user_role,
+  service_name        => $service_name,
+  public_url          => $public_url,
+  internal_url        => $admin_url,
+  admin_url           => $admin_url,
+  region              => $region,
 }
