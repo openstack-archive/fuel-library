e88da2f54c96d1de3d10297a3efd54501110b636
Michael Polenchuk <mpolenchuk@mirantis.com>
2015-10-16 17:09:04 +0300

    Set rings rx/tx to maximum that supports hardware
    All recent versions of the chipset have 4096, but the driver doesn't
    autodetect this. Increasing the number of descriptors can improve
    performance dramatically on some hosts.
    
    This is also the case with Intel 10GE NICs - the driver defaults to 256
    descriptors each for Rx and Tx, even if the hardware supports 4096.
    
    Change-Id: I1e5029e793399ff42002f3d203c6ae3bd2608be2
    Closes-Bug: #1456723

---
+++ deployment/puppet/l23network/lib/puppet/parser/functions/get_nic_maxrings.rb
@@ -0,0 +1,19 @@
+#
+#  get_nic_maxrings.rb
+#
+require 'puppetx/l23_utils'
+
+module Puppet::Parser::Functions
+  newfunction(:get_nic_maxrings, :type => :rvalue, :doc => <<-EOS
+Gets pre-set maximums of nic ring rx/tx settings by ethtool.
+    EOS
+  ) do |arguments|
+
+    raise(Puppet::ParseError, "get_nic_maxrings(): Wrong number of arguments " +
+      "given (#{arguments.size} for 1)") if arguments.size < 1
+
+    iface_name = arguments[0]
+    { 'rings' => L23network.get_ethtool_rings(iface_name) }
+  end
+end
+# vim: set ts=2 sw=2 et :
--- deployment/puppet/l23network/lib/puppet/provider/l2_base.rb
+++ deployment/puppet/l23network/lib/puppet/provider/l2_base.rb
@@ -1,4 +1,5 @@
 require 'puppetx/l23_ethtool_name_commands_mapping'
+require 'puppetx/l23_utils'
 require File.join(File.dirname(__FILE__), 'interface_toolset')
 
 class Puppet::Provider::L2_base < Puppet::Provider::InterfaceToolset
@@ -580,8 +581,17 @@ class Puppet::Provider::L2_base < Puppet::Provider::InterfaceToolset
     ethtool_k.split(/\n+/).select{|l| !l.match(/(^\s+|\[fixed\]|^Features)/)}.map{|x| x.split(/[\s\:]+/)}.each do |p|
       tmp[p[0]] = (p[1] == 'on')
     end
+
+    rings = {}
+    begin
+      rings_entries = ethtool_cmd('-g', if_name)
+      %w{RX TX}.each { |x| rings[x] = rings_entries[/#{x}:\s+(\d+)/, 1] }
+    rescue
+      debug("Cannot get device ring settings for #{if_name}: Operation not supported")
+    end
+
     return {
-      'offload' => tmp || empty_return
+      'offload' => tmp || empty_return, 'rings' => L23network.get_ethtool_rings(if_name, true)
     }
   end
 
