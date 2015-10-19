461ed3c7a6854143d5bd9d09f86adf9a2b96f44f
Alex Schultz <aschultz@mirantis.com>
2015-10-19 16:08:44 -0500

    Initial unit tests for cluster module
    This change includes an initial set of unit tests for the cluster puppet
    module.
    
    Change-Id: Ie151ee13006ba1ea6591d95cb5bae3147086bdb8
    Related-Bug: #1507751

---
+++ deployment/puppet/cluster/spec/classes/cluster_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+describe 'cluster' do
+
+  let(:default_params) { {
+    :internal_address => '127.0.0.1',
+  } }
+
+  shared_examples_for 'cluster configuration' do
+    let :params do
+      default_params
+    end
+
+
+    context 'with valid params' do
+      let :params do
+        default_params.merge!({
+          :corosync_nodes => { 'node-1' => { 'ip' => '127.0.0.1', 'id' => '1' } }
+        })
+      end
+
+      it 'configures with the params params' do
+        should contain_class('cluster')
+        should contain_class('openstack::corosync').with(
+          :bind_address     => '127.0.0.1',
+          :corosync_nodes   => params[:corosync_nodes],
+          :corosync_version => 2,
+          :packages         => ['corosync', 'pacemaker', 'crmsh', 'pcs'])
+        should contain_file('ocf-fuel-path').with(
+          :ensure  => 'directory',
+          :path    => '/usr/lib/ocf/resource.d/fuel',
+          :recurse => true,
+          :owner   => 'root',
+          :group   => 'root')
+      end
+    end
+
+  end
+
+  context 'on Debian platforms' do
+    let :facts do
+      { :osfamily => 'Debian',
+        :operatingsystem => 'Debian',
+        :hostname => 'hostname.example.com', }
+    end
+
+    it_configures 'cluster configuration'
+  end
+
+  context 'on RedHat platforms' do
+    let :facts do
+      { :osfamily => 'RedHat',
+        :operatingsystem => 'RedHat',
+        :hostname => 'hostname.example.com', }
+    end
+
+    it_configures 'cluster configuration'
+  end
+
+end
+
