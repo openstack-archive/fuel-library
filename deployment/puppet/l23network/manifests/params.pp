a9cfc4bae20ac71a36ee878f4388a210fd68b1ed
Stanislav Makar <smakar@mirantis.com>
2015-10-22 11:13:08 +0000

    Use kernel build-in openvsitch module for CentOS7/RHEL7
    l23network module fixes:
    * Add the posibility to use DKMS datapath openVswitch or kernel build-in module.
    * Fix the problem with usage of custom datapath openVswitch package name
    * Fix the problem with usage of custom openVswitch module name
    
    Change-Id: I7b006a9dc3b3ec5b22dd41cc15b57d5730e4810c
    Closes-bug: #1508515

--- deployment/puppet/l23network/manifests/params.pp
+++ deployment/puppet/l23network/manifests/params.pp
@@ -27,10 +27,7 @@ class l23network::params {
       $lnx_bond_tools            = undef
       $lnx_ethernet_tools        = 'ethtool'
       $lnx_bridge_tools          = 'bridge-utils'
-      $ovs_datapath_package_name = $::kernelmajversion ? {
-                                    '3.10'  => 'kmod-openvswitch-lt',
-                                    default => 'kmod-openvswitch',
-                                  }
+      $ovs_datapath_package_name = 'kmod-openvswitch'
       $ovs_common_package_name   = 'openvswitch'
       $ovs_kern_module_name      = 'openvswitch'
       $extra_tools               = 'iputils'
