3ddd53a1ba6a98b49ae7ebf57517fb0b0e61a09e
Dmitry Ilyin <dilyin@mirantis.com>
2015-10-28 16:23:52 +0300

    Noop tests refactoring
    Change-Id: I814667283ccfc6ebd7c16d961270f688e9fb9c4c

---
+++ tests/noop/spec/lib/coverage.rb
@@ -0,0 +1,30 @@
+class Noop
+  module Coverage
+
+    def coverage_base_dir
+      return @coverage_base_dir if @coverage_base_dir
+      @coverage_base_dir = File.expand_path File.join(spec_dir, '..', '..', 'coverage')
+    end
+
+    def coverage_prepare
+      FileUtils.mkdir_p coverage_base_dir unless File.directory? coverage_base_dir
+    end
+
+    def coverage_simplecov
+      coverage_prepare
+      require 'simplecov'
+      SimpleCov.start do
+        SimpleCov.coverage_dir coverage_base_dir
+        SimpleCov.use_merging
+        SimpleCov.merge_timeout 7200
+      end
+
+      SimpleCov.at_exit do
+        SimpleCov.result.format!
+        puts "Total coverage percent: #{SimpleCov.result.covered_percent.round 2}"
+      end
+    end
+
+  end
+  extend Coverage
+end
