580b3f5b2506b968ad942b13271e577cbb012c83
iberezovskiy <iberezovskiy@mirantis.com>
2015-10-28 16:07:13 +0300

    Set swift_partition to /var/lib/glance/node by default
    We had an ability to set swift partition in modular
    script, but it was removed in this commit:
    Ia2428b49a7192a2d7d56ffdc61a75de9ebc816c9
    
    We should use /var/lib/glance/node as swift partition
    by default as it was before and provide access
    to this folder for swift user.
    Also noop tests were added to cover this parameter.
    
    Change-Id: Ic38c726d5ae9fff1815e6a8b4158549c09cec84c
    Closes-bug: #1508482

--- deployment/puppet/osnailyfacter/modular/swift/swift.pp
+++ deployment/puppet/osnailyfacter/modular/swift/swift.pp
@@ -43,8 +43,22 @@ if !($storage_hash['images_ceph'] and $storage_hash['objects_ceph']) and !$stora
   $master_swift_proxy_nodes_list = values($master_swift_proxy_nodes)
   $master_swift_proxy_ip         = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/api'], '\/\d+$', '')
   $master_swift_replication_ip   = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/replication'], '\/\d+$', '')
+  $swift_partition               = hiera('swift_partition', '/var/lib/glance/node')
 
   if ($deploy_swift_storage){
+    if !defined(File['/var/lib/glance']) {
+      file {'/var/lib/glance':
+        group   => 'swift',
+        require => Package['swift'],
+      } -> Service <| tag == 'swift-service' |>
+    } else {
+      File['/var/lib/glance'] {
+        group   => 'swift',
+        require +> Package['swift'],
+      }
+      File['/var/lib/glance'] -> Service <| tag == 'swift-service' |>
+    }
+
     class { 'openstack::swift::storage_node':
       storage_type                => false,
       loopback_size               => '5243780',
