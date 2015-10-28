06549005918c85d24717db9de34d68bbcab26175
Dmitry Ilyin <dilyin@mirantis.com>
2015-10-28 16:23:52 +0300

    Noop tests refactoring
    Change-Id: I814667283ccfc6ebd7c16d961270f688e9fb9c4c

---
+++ tests/noop/spec/lib/debug.rb
@@ -0,0 +1,23 @@
+class Noop
+  module Debug
+
+    def debug(msg)
+      puts msg if ENV['SPEC_PUPPET_DEBUG']
+    end
+
+    def status_report(context)
+      <<-eos
+      =============================================
+      OS:       #{current_os context}
+      YAML:     #{astute_yaml_base}
+      Spec:     #{current_spec context}
+      Manifest: #{manifest_path}
+      Node:     #{fqdn}
+      Role:     #{role}
+      =============================================
+      eos
+    end
+
+  end
+  extend Debug
+end
