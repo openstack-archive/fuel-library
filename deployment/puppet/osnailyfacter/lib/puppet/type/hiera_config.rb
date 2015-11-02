78926da90c214c56b9802f54cc9cd79fa94341ac
Dmitry Ilyin <dilyin@mirantis.com>
2015-11-02 22:27:08 +0300

    Extend hiera task to improve plugin support
    * Add hiera_config type to mange hiera.yaml
    * Plugins can put their overrides to /etc/hiera/plugins/
    * Running hiera_config again will gather override files
      and insert records for them to the main config file after
      the basic elements
    * Or, it metadata_yaml_file is present, hiera_config can
      take the list of enabled plugins and add records for them
      instead of scanning the plugins directory
    
    Closes-Bug: 1512456
    DocImpact: add information about placing plugin data files to /etc/hiera/plugins/ instead of other files.
    Change-Id: Ifdb27708a2457e8c1430695bb2412594806d211f

---
+++ deployment/puppet/osnailyfacter/lib/puppet/type/hiera_config.rb
@@ -0,0 +1,73 @@
+Puppet::Type.newtype(:hiera_config) do
+  desc 'Manage Hiera yaml configuration'
+
+  newparam(:name) do
+    desc 'The path to the Hiera config file.'
+    isnamevar
+  end
+
+  newparam(:override_dir) do
+    desc 'Look for override files in this directory.
+         The directory should be inside the data directory and the path
+         should be relative to the basic data directory path.'
+    defaultto 'plugins'
+  end
+
+  newparam(:metadata_yaml_file) do
+    desc 'Look inside this YAML file for the list of enabled plugins'
+  end
+
+  newproperty(:data_dir) do
+    desc 'Basic directory with Hiera data elements'
+    defaultto '/etc/hiera'
+  end
+
+  newproperty(:hierarchy, :array_matching => :all) do
+    desc 'Basic hierarchy elements. This list of elements will be used at the
+          bottom of the hierarchy before any overrides are applied.'
+    defaultto []
+
+    def is_to_s(value)
+      value.inspect
+    end
+
+    def should_to_s(value)
+      value.inspect
+    end
+  end
+
+  newproperty(:hierarchy_override, :array_matching => :all) do
+    desc 'Override hierarchy elements. These list will be automaticly gathered
+          either from the metadata file of from the override directoiry scanning.
+          If you provide the list manually it will be used without any automatic
+          element gathering.'
+    defaultto []
+
+    def is_to_s(value)
+      value.inspect
+    end
+
+    def should_to_s(value)
+      value.inspect
+    end
+  end
+
+  newproperty(:logger) do
+    desc 'The Hiera logger type.'
+    newvalues 'noop', 'puppet', 'console'
+    defaultto 'noop'
+    munge do |value|
+      value.to_s
+    end
+  end
+
+  newproperty(:merge_behavior) do
+    desc 'Merge strategy for hash lookups.'
+    newvalues 'native', 'deep', 'deeper'
+    defaultto 'native'
+    munge do |value|
+      value.to_s
+    end
+  end
+
+end
--- deployment/puppet/osnailyfacter/modular/hiera/hiera.pp
+++ deployment/puppet/osnailyfacter/modular/hiera/hiera.pp
@@ -6,18 +6,23 @@ $deep_merge_package_name = $::osfamily ? {
 }
 
 $data_dir            = '/etc/hiera'
-$data                = [
-  'override/node/%{::fqdn}',
-  'override/class/%{calling_class}',
-  'override/module/%{calling_module}',
-  'override/plugins',
-  'override/common',
-  'class/%{calling_class}',
-  'module/%{calling_module}',
-  'nodes',
-  'globals',
-  'astute'
+$override_dir        = 'plugins'
+$override_dir_path   = "${data_dir}/${override_dir}"
+$metadata_file       = '/etc/astute.yaml'
+
+$data = [
+  "astute",
+  "globals",
+  "nodes",
+  "module/%{calling_module}",
+  "class/%{calling_class}",
+  "override/common",
+  "override/plugins",
+  "override/module/%{calling_module}",
+  "override/class/%{calling_class}",
+  "override/node/%{::fqdn}",
 ]
+
 $astute_data_file    = '/etc/astute.yaml'
 $hiera_main_config   = '/etc/hiera.yaml'
 $hiera_puppet_config = '/etc/puppet/hiera.yaml'
@@ -29,31 +34,27 @@ File {
   mode  => '0644',
 }
 
-$hiera_config_content = inline_template('
----
-:backends:
-  - yaml
-
-:hierarchy:
-<% @data.each do |name| -%>
-  - <%= name %>
-<% end -%>
-
-:yaml:
-  :datadir: <%= @data_dir %>
-:merge_behavior: deeper
-:logger: noop
-')
+hiera_config { $hiera_main_config :
+  data_dir           => $data_dir,
+  hierarchy          => $data,
+  override_dir       => $override_dir,
+  metadata_yaml_file => $metadata_file,
+  merge_behavior     => 'deeper',
+}
 
 file { 'hiera_data_dir' :
   ensure => 'directory',
   path   => $data_dir,
 }
 
+file { 'hiera_data_override_dir' :
+  ensure => 'directory',
+  path   => $override_dir_path,
+}
+
 file { 'hiera_config' :
   ensure  => 'present',
   path    => $hiera_main_config,
-  content => $hiera_config_content,
 }
 
 file { 'hiera_data_astute' :
