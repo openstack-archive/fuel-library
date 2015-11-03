ef79d565eccf0e9c2c4ddd8b07e365a6304bd6fd
Sergey Vasilenko <svasilenko@mirantis.com>
2015-11-03 14:35:16 +0300

    Refactor noop-tests for granular Neutron
    Fuel-CI: disable
    Change-Id: Iebe4051fd96d141fdd3e1f98818588770c9005fe

--- tests/noop/spec/hosts/openstack-network/common-config_spec.rb
+++ tests/noop/spec/hosts/openstack-network/common-config_spec.rb
@@ -3,6 +3,30 @@ require 'shared-examples'
 manifest = 'openstack-network/common-config.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+    if Noop.hiera('use_neutron')
+      context 'with Neutron' do
+        neutron_config = Noop.hiera('neutron_config')
+        # nets = neutron_config['predefined_networks']
+
+        # floating_net   = (neutron_config['default_floating_net'] or 'net04_ext')
+        # private_net    = (neutron_config['default_private_net'] or 'net04')
+        # default_router = (neutron_config['default_router'] or 'router04')
+
+        context 'Default router serves tenant networks' do
+          it 'should be created and serve gateway' do
+          end
+        end
+      end
+
+      it 'should apply kernel tweaks for connections' do
+        should contain_sysctl__value('net.ipv4.neigh.default.gc_thresh1').with_value('1024')
+        should contain_sysctl__value('net.ipv4.neigh.default.gc_thresh2').with_value('2048')
+        should contain_sysctl__value('net.ipv4.neigh.default.gc_thresh3').with_value('4096')
+      end
+
+    end
+  end
   test_ubuntu_and_centos manifest
 end
 
--- tests/noop/spec/hosts/openstack-network/openstack-network-compute_spec.rb
+++ tests/noop/spec/hosts/openstack-network/openstack-network-compute_spec.rb
@@ -61,17 +61,12 @@ describe manifest do
 
     ############################################################################
 
-    it 'should declare openstack::network with use_stderr disabled' do
-      should contain_class('openstack::network').with(
-                 'use_stderr' => 'false'
-             )
-    end
+    # it 'should declare openstack::network with use_stderr disabled' do
+    #   should contain_class('openstack::network').with(
+    #              'use_stderr' => 'false'
+    #          )
+    # end
 
-    it 'should apply kernel tweaks for connections' do
-      should contain_sysctl__value('net.ipv4.neigh.default.gc_thresh1').with_value('1024')
-      should contain_sysctl__value('net.ipv4.neigh.default.gc_thresh2').with_value('2048')
-      should contain_sysctl__value('net.ipv4.neigh.default.gc_thresh3').with_value('4096')
-    end
 
     enable = Noop.hiera('use_neutron')
     context 'with Neutron', :if => enable do
