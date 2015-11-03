de9f0e1153522f1f6bb95e9522ed6f527e736797
Sergey Vasilenko <svasilenko@mirantis.com>
2015-11-03 14:35:16 +0300

    Refactor noop-tests for granular Neutron
    Fuel-CI: disable
    Change-Id: Iebe4051fd96d141fdd3e1f98818588770c9005fe

--- tests/noop/spec/hosts/openstack-network/networks_spec.rb
+++ tests/noop/spec/hosts/openstack-network/networks_spec.rb
@@ -4,11 +4,45 @@ manifest = 'openstack-network/networks.pp'
 
 describe manifest do
   shared_examples 'catalog' do
-    if Noop.hiera('use_neutron')
-      neutron_config = Noop.hiera 'neutron_config'
+    context 'with Neutron', :if => (Noop.hiera('use_neutron') and Noop.hiera('primary_controller')) do
+      neutron_config = Noop.hiera('neutron_config')
       nets = neutron_config['predefined_networks']
 
-      if Noop.hiera 'primary_controller' and nets.has_key?('baremetal')
+      floating_net   = (neutron_config['default_floating_net'] or 'net04_ext')
+      private_net    = (neutron_config['default_private_net'] or 'net04')
+      default_router = (neutron_config['default_router'] or 'router04')
+
+      if nets.has_key?(private_net)
+        it 'should create private network' do
+          should contain_neutron_network(private_net).with(
+            'ensure'                    => 'present',
+            'provider_physical_network' => (nets[private_net]['L2']['physnet'] or false),
+            'provider_network_type'     => 'vxlan',
+            #'provider_segmentation_id'  => nets[private_net]['L2']['segment_id'],
+            'router_external'           => nets[private_net]['L2']['router_ext'],
+            'shared'                    => nets[private_net]['shared'],
+          )
+        end
+      end
+
+      if nets.has_key?(floating_net)
+        it 'should create network for floating' do
+          should contain_neutron_network(floating_net).with(
+            'ensure'                    => 'present',
+            'provider_physical_network' => (nets[floating_net]['L2']['physnet'] or false),
+            'provider_network_type'     => 'local',
+            'router_external'           => nets[floating_net]['L2']['router_ext'],
+            'shared'                    => nets[floating_net]['shared'],
+          )
+        end
+  #  provider_physical_network => $floating_net_physnet,
+  # provider_network_type     => 'local',
+  # router_external           => $floating_net_router_external,
+  # tenant_name               => $tenant_name,
+  # shared                    => $floating_net_shared
+     end
+
+      context 'with Ironic networks', :if => nets.has_key?('baremetal') do
         it 'should create baremetal network' do
           should contain_neutron_network('baremetal').with(
             'ensure'                    => 'present',
