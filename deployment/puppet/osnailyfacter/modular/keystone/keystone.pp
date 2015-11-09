513eaeed5b2f3d1cfe76a334f2bb9e3ef3047f6b
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-11-09 14:36:00 +0100

    Add OS_CACERT to openrc if SSL is enabled
    Change-Id: Ie38dc225d1aa2104ef7959644c4e16a2923fa15e
    Closes-bug: 1585562
    (cherry picked from commit ce2ac8ed8e41c3174f21e34aa446a65f4737e67e)

--- deployment/puppet/osnailyfacter/modular/keystone/keystone.pp
+++ deployment/puppet/osnailyfacter/modular/keystone/keystone.pp
@@ -66,6 +66,11 @@ $admin_protocol = get_ssl_property($ssl_hash, {}, 'keystone', 'admin', 'protocol
 $admin_address  = get_ssl_property($ssl_hash, {}, 'keystone', 'admin', 'hostname', [$service_endpoint, $management_vip])
 $admin_port     = '35357'
 
+$public_cert             = $public_ssl_hash['services']? {
+  true    => '/etc/pki/tls/certs/public_haproxy.pem',
+  default => undef,
+}
+
 $local_address_for_bind = get_network_role_property('keystone/api', 'ipaddr')
 
 $memcache_server_port   = hiera('memcache_server_port', '11211')
@@ -211,6 +216,7 @@ class { 'openstack::auth_file':
   auth_url            => $auth_url,
   murano_repo_url     => $murano_repo_url,
   murano_glare_plugin => $murano_glare_plugin,
+  cacert              => $public_cert
 }
 
 # Get paste.ini source
