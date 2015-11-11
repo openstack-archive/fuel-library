5c6fe56612cbca3db1ee8a22cb9c7355a988c36b
Kyrylo Galanov <kgalanov@mirantis.com>
2015-11-11 14:24:23 +0200

    Fix mock nbd block devices
    NBD client creates a number of empty devices like /dev/nbd0..15
    To fix the issue, before processing a partition script checks if
    that partition was present in lsblk output.
    
    Change-Id: I118c36a78e4661f82d4c11209ca98dca0fb2716a
    Closes-Bug: #1510851

--- deployment/puppet/ceph/lib/facter/ceph_osd.rb
+++ deployment/puppet/ceph/lib/facter/ceph_osd.rb
@@ -7,14 +7,16 @@ Facter.add("osd_devices_list") do
     end
 
     return unless File.exists?(sgdisk_exe)
-
     setcode do
         output = []
-        disks= []
+        disks = []
+        parts_list = []
         osds = []
         journals = []
-        devices = Facter::Util::Resolution.exec(%q{lsblk -nr -o KNAME,TYPE}).split("\n").map{|x| x.split}.keep_if{|x| x[1] == "disk"}
-        devices.each{|x| disks << x[0]}
+        devices = Facter::Util::Resolution.exec(%q{lsblk -nr -o KNAME,TYPE}).split("\n").map{|x| x.split}
+        devices.each{|x| disks << x[0] if x[1] == "disk"}
+        devices.each{|x| parts_list << x[0] if x[1] == "part"}
+        parts_list.map!{|x| x.gsub(/!/, '/')}
         # Finds OSD and journal devices based on Partition GUID
         disks.each { |d|
             # lsblk returns cciss devices as cciss!c0d0p1. The entries
@@ -23,18 +25,22 @@ Facter.add("osd_devices_list") do
             device = "/dev/#{d}"
             parts = Dir.glob("#{device}?*").sort
             parts.each { |p|
-                pnum = p.gsub(/#{device}p*/, '')
-                code = Facter::Util::Resolution.exec(%Q{sgdisk -i #{pnum} #{device}}).match(/Partition GUID code:\s+(\S+)\s/)[1]
-                case code
-                when "4FBD7E29-9D25-41B8-AFD0-062C0CEFF05D"
-                    # Only use unmounted devices
-                    if Facter::Util::Resolution.exec(%Q{grep -c #{p} /proc/mounts}).to_i == 0
-                        osds << p
-                    end
-                when "45B0969E-9B03-4F30-B4C6-B4B80CEFF106"
-                    if Facter::Util::Resolution.exec(%Q{grep -c #{p} /proc/mounts}).to_i == 0
-                        journals << p
-                    end
+                part = p.gsub(/\/dev\//, '')
+                if parts_list.include?(part) then
+                     pnum = p.gsub(/#{device}p*/, '')
+                     code = Facter::Util::Resolution.exec(%Q{sgdisk -i #{pnum} #{device}}).match(/Partition GUID code:\s+(\S+)\s/)[1]
+
+                     case code
+                     when "4FBD7E29-9D25-41B8-AFD0-062C0CEFF05D"
+                         # Only use unmounted devices
+                         if Facter::Util::Resolution.exec(%Q{grep -c #{p} /proc/mounts}).to_i == 0
+                             osds << p
+                         end
+                     when "45B0969E-9B03-4F30-B4C6-B4B80CEFF106"
+                         if Facter::Util::Resolution.exec(%Q{grep -c #{p} /proc/mounts}).to_i == 0
+                             journals << p
+                         end
+                     end
                 end
             }
         }
