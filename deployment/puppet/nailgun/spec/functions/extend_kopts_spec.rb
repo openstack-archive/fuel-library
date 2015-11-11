0b61d4d73f40f14699d3eb9b8fc6e83f3eb745cc
Valeriy Sakharov <vsakharov@mirantis.com>
2015-11-11 20:54:56 +0300

    Add support of extend_kopts for ubuntu_bootstrap
    This commit adds posibility to use different kernel boot options
    if user would like to modify it. The extend_kopts.rb takes two strings,
    compares them and modifies default kopts.
    
    Implements: Blueprint modify-fuel-library
    Change-Id: Iac69c8457bd0ce76a1082c98d679ff44f468bf1f

---
+++ deployment/puppet/nailgun/spec/functions/extend_kopts_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'extend_kopts' do
+
+  it 'number args' do
+    is_expected.to run.with_params('foo').and_raise_error(Puppet::ParseError, "extend_kopts(): wrong number of arguments - 1, must be 2")
+  end
+
+  it { should run.with_params("biosdevname=0 ifnames=1 rd.luks.uuid=30 panic=15 quiet debug udev.log-priority=2 systemd.gpt_auto=1","console=ttyS0,9600 console=tty0 console=tty1 panic=60 boot=live toram fetch=http://127.0.0.1:8080/active_bootstrap/root.squashfs biosdevname=0").and_return(" biosdevname=0 ifnames=1 rd.luks.uuid=30 panic=15 quiet debug udev.log-priority=2 systemd.gpt_auto=1 console=ttyS0,9600 console=tty0 console=tty1 boot=live toram fetch=http://127.0.0.1:8080/active_bootstrap/root.squashfs") }
+
+end
