914f2c9506b9550a51104f3282b5e540281b5414
Valeriy Sakharov <vsakharov@mirantis.com>
2015-11-11 20:54:56 +0300

    Add support of extend_kopts for ubuntu_bootstrap
    This commit adds posibility to use different kernel boot options
    if user would like to modify it. The extend_kopts.rb takes two strings,
    compares them and modifies default kopts.
    
    Implements: Blueprint modify-fuel-library
    Change-Id: Iac69c8457bd0ce76a1082c98d679ff44f468bf1f

---
+++ deployment/puppet/nailgun/lib/puppet/parser/functions/extend_kopts.rb
@@ -0,0 +1,38 @@
+module Puppet::Parser::Functions
+  newfunction(:extend_kopts, :type => :rvalue, :doc => <<-EOS
+    This function changes "kopts" parameter if user modified it
+    and return the string. It takes two arguments: string from
+    metadata.yaml from "extend_kopts" option and default string
+    in format "key1=value1 key2=value2 key3".
+    For example:
+
+    $metadata = loadyaml('path/to/metadata.yaml')
+    extend_kopts($metadata['extend_kopts'], 'key1=a key2=b")
+
+    Function compare two strings, make changes into default option
+    and return it.
+
+    So, if in the /path/to/metadata.yaml in the "extend_kopts" will be
+    "key3=c key4 key1=not_a", we will get in the output:
+    "key2=b key3=c key4 key1=not_a".
+  EOS
+  ) do |args|
+
+      begin
+
+        unless args.length == 2
+          raise Puppet::ParseError, ("wrong number of arguments: #{args.length}, must be 2")
+        end
+
+        hash_new_kopts = Hash[*args[0].scan(/([^=\s]+)=*([^\s]*)/).flatten]
+        hash_default_kopts = Hash[*args[1].scan(/([^=\s]+)=*([^\s]*)/).flatten]
+
+        new_default_kopts = hash_default_kopts.merge(hash_new_kopts)
+        empty_values = lambda {|v| v.empty? ? nil : v}
+        merged = new_default_kopts.update(new_default_kopts) {|k,v| empty_values.call(v)}
+        result_kopts = merged.collect {|key, value| value ? "#{key}=#{value}" : "#{key}"}.join(' ')
+
+      end
+    return result_kopts
+  end
+end
