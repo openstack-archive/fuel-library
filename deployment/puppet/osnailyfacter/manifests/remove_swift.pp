58d25004dc0a8219c89d6bbb06e7a239bae00b57
Alexey Shtokolov <ashtokolov@mirantis.com>
2015-11-13 20:44:22 +0300

    Fix openstack ci lint job for stable branches
    Change-Id: Id48f5d0fd61636f82c6f8face16adcf5a1a5c8bb

--- deployment/puppet/osnailyfacter/manifests/remove_swift.pp
+++ deployment/puppet/osnailyfacter/manifests/remove_swift.pp
@@ -4,7 +4,7 @@ class osnailyfacter::remove_swift {
   if $::osfamily == 'Debian' {
     exec { 'remove-swift-object' :
       command   => '/usr/bin/dpkg -r --force-all swift swift-account swift-object swift-container',
-      onlyif    => '/usr/bin/test "$(/usr/bin/dpkg-query --show -f=\'${Status}|${Version}\' swift-object)" == \'install ok installed|1.13.1.fuel5.0~mira1\'',
+      onlyif    => "/usr/bin/test \"$(/usr/bin/dpkg-query --show -f='${Status}|${Version}' swift-object)\" == 'install ok installed|1.13.1.fuel5.0~mira1'",
       logoutput => 'on_failure',
     }
     Exec['remove-swift-object'] -> Package <| title == 'swift' |>
