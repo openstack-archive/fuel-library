d98c9cea7c9c3e72272fddd6884056cb531a4719
Sergey Vasilenko <svasilenko@mirantis.com>
2015-11-16 14:55:35 +0300

    Create external (floating) as 'flat' with required attributes
    without adding his name to l3-agent config
    
    Fuel-CI: disable
    blueprint: dyn-gen-physnet-set
    Change-Id: I8acee213a19d26e3d1fc1ad7595ee15d35e97f4d

--- tests/noop/spec/hosts/openstack-network/plugins/ml2_spec.rb
+++ tests/noop/spec/hosts/openstack-network/plugins/ml2_spec.rb
@@ -28,18 +28,18 @@ describe manifest do
         segmentation_type = neutron_config.fetch('L2',{}).fetch('segmentation_type')
 
         if segmentation_type == 'vlan'
-          physnets_array = ["physnet2:#{pnets['physnet2']['bridge']}"]
+          physnets_array = ["physnet1:#{pnets['physnet1']['bridge']}", "physnet2:#{pnets['physnet2']['bridge']}"]
           network_type   = 'vlan'
           network_vlan_ranges_physnet2 = pnets.fetch('physnet2',{}).fetch('vlan_range')
-          network_vlan_ranges = ["physnet2:#{network_vlan_ranges_physnet2}"]
+          network_vlan_ranges = ["physnet1", "physnet2:#{network_vlan_ranges_physnet2}"]
           tunnel_id_ranges  = []
           physical_network_mtus = ["physnet2:1500"]
           overlay_net_mtu = '1500'
           tunnel_types = []
         else
-          physnets_array = []
+          physnets_array = ["physnet1:#{pnets['physnet1']['bridge']}"]
           network_type   = 'vxlan'
-          network_vlan_ranges = []
+          network_vlan_ranges = ["physnet1"]
           tunnel_id_ranges  = [neutron_config.fetch('L2',{}).fetch('tunnel_id_ranges')]
           physical_network_mtus = []
           overlay_net_mtu = '1450'
@@ -121,9 +121,6 @@ describe manifest do
         )}
         it { should contain_class('neutron::agents::ml2::ovs').with(
           'enable_tunneling' => (segmentation_type != 'vlan')
-        )}
-        it { should contain_class('neutron::agents::ml2::ovs').with(
-          'bridge_mappings' => bridge_mappings
         )}
 
         # check whether Neutron server started only on controllers
