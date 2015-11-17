2e6db5157c120e3fbb3b1f8dcc1030894a9c1729
Stanislav Makar <smakar@mirantis.com>
2015-11-17 10:10:42 +0000

    Refactor configure_default_route function
    Now if default route is needed to change we just modify network_scheme
    and call generate_network_config with this network_scheme, if no - do
    nothing.
    
    Change-Id: I33e88550af5d5cce2886254444ee5d450e578a1c
    Closes-bug: #1510072

--- deployment/puppet/osnailyfacter/spec/functions/configure_default_route__spec.rb
+++ deployment/puppet/osnailyfacter/spec/functions/configure_default_route__spec.rb
@@ -47,8 +47,8 @@ describe 'configure_default_route' do
 
   let(:management_vrouter_vip) { '192.168.0.2' }
 
-  let(:management_int) { 'br-mgmt' }
-  let(:fw_admin_int) { 'br-fw-admin' }
+  let(:management_int) { 'management' }
+  let(:fw_admin_int) { 'fw-admin' }
 
   before(:each) do
     puppet_debug_override()
@@ -64,22 +64,9 @@ describe 'configure_default_route' do
 
   it 'should configure default the default route if gateway ip is equal to master_ip' do
     arguments = [network_scheme, management_vrouter_vip, fw_admin_int, management_int]
-    scope.stubs(:function_create_resources).returns(true)
     ifconfig = scope.function_configure_default_route arguments
-    expect(ifconfig['br-mgmt']).to eq({
-                                     "ipaddr"=>["192.168.0.5/24"],
-                                       "vendor_specific"=>{
-                                           "vlans"=>"101",
-                                           "phy_interfaces"=>["eth0"]
-                                       },
-                                       "gateway"=>"192.168.0.2",
-                                       "require"=>"L23network::L3::Ifconfig[br-fw-admin]",
-                                   })
-    expect(ifconfig['br-fw-admin']).to eq({
-                                         "ipaddr"=>["10.20.0.6/24"],
-                                         "gateway"=>:absent,
-                                       })
-
+     expect(ifconfig[:endpoints][:'br-mgmt'][:gateway]).to eq('192.168.0.2')
+     expect(ifconfig[:endpoints][:'br-fw-admin'][:gateway]).to eq(nil)
   end
 
   let(:network_scheme_n) do
@@ -120,8 +107,7 @@ describe 'configure_default_route' do
 
 
   it 'should not configure interfaces if master_ip is not equal to the gateway' do
-    arguments = [network_scheme_n, management_vrouter_vip, 'br-fw-admin', 'br-mgmt']
-    scope.stubs(:function_create_resources).returns(true)
+    arguments = [network_scheme_n, management_vrouter_vip, 'fw-admin', 'management']
     ifconfig = scope.function_configure_default_route arguments
     expect(ifconfig).to eq({})
   end
