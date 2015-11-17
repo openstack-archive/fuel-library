32dad820f34e963d828f3d6688c6070637e894ee
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-11-17 17:04:25 +0300

    Add selective SSL support
    Add second-level hash for TLS support. If user mixing this hash into his
    astute.yaml, values from it will be taken to TLS-related tasks instead
    of ones from UI.
    
    Hash to use looks like:
    
    use_ssl:
      horizon: true
      horizon_public: true
      horizon_public_hostname: horizon.fuel.local
      horizon_public_usercert: true
      horizon_public_certdata: KeyPairStoredHere
    
      keystone: true
      keystone_public: true
      keystone_public_ip: <ip of external keystone>
      ...
      keystone_internal: true
      ...
      keystone_admin: true
      ...
    
      glance: true
      glance_public: true
      ...
    
    Related to blueprint selective-ssl
    Change-Id: Ic3034b3fea27a370b1f7cbd4e088f361fce96142

--- deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-ceilometer.pp
+++ deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-ceilometer.pp
@@ -4,7 +4,14 @@ $ceilometer_hash         = hiera_hash('ceilometer',{})
 # NOT enabled by default
 $use_ceilometer          = pick($ceilometer_hash['enabled'], false)
 $public_ssl_hash         = hiera('public_ssl')
+$ssl_hash                = hiera_hash('use_ssl', {})
 $ceilometer_address_map  = get_node_to_ipaddr_map_by_network_role(hiera_hash('ceilometer_nodes'), 'ceilometer/api')
+
+$public_ssl              = get_ssl_property($ssl_hash, $public_ssl_hash, 'ceilometer', 'public', 'usage', false)
+$public_ssl_path         = get_ssl_property($ssl_hash, $public_ssl_hash, 'ceilometer', 'public', 'path', [''])
+
+$internal_ssl            = get_ssl_property($ssl_hash, {}, 'ceilometer', 'internal', 'usage', false)
+$internal_ssl_path       = get_ssl_property($ssl_hash, {}, 'ceilometer', 'internal', 'path', [''])
 
 if ($use_ceilometer) {
   $server_names        = hiera_array('ceilometer_names', keys($ceilometer_address_map))
@@ -18,6 +25,9 @@ if ($use_ceilometer) {
     ipaddresses         => $ipaddresses,
     public_virtual_ip   => $public_virtual_ip,
     server_names        => $server_names,
-    public_ssl          => $public_ssl_hash['services'],
+    public_ssl          => $public_ssl,
+    public_ssl_path     => $public_ssl_path,
+    internal_ssl        => $internal_ssl,
+    internal_ssl_path   => $internal_ssl_path,
   }
 }
--- deployment/puppet/osnailyfacter/modular/ssl/ssl_add_trust_chain.pp
+++ deployment/puppet/osnailyfacter/modular/ssl/ssl_add_trust_chain.pp
@@ -1,31 +1,57 @@
 notice('MODULAR: ssl_add_trust_chain.pp')
 
-$public_ssl_hash = hiera('public_ssl')
-$ip = hiera('public_vip')
+$public_ssl_hash    = hiera('public_ssl')
+$ssl_hash           = hiera_hash('use_ssl', {})
+
+define file_link {
+  $service = $name
+  if !empty(file("/etc/pki/tls/certs/public_${service}.pem",'/dev/null')) {
+    file { "/usr/local/share/ca-certificates/${service}_public_haproxy.crt":
+      ensure => link,
+      target => "/etc/pki/tls/certs/public_${service}.pem",
+    }
+  }
+}
+
+if !empty($ssl_hash) {
+  $services = [ 'horizon', 'keystone', 'nova', 'heat', 'glance', 'cinder', 'neutron', 'swift', 'sahara', 'murano', 'ceilometer', 'radosgw']
+
+  file_link { $services: }
+
+} elsif !empty($public_ssl_hash) {
+  case $::osfamily {
+    /(?i)redhat/: {
+      file { '/etc/pki/ca-trust/source/anchors/public_haproxy.pem':
+        ensure => 'link',
+        target => '/etc/pki/tls/certs/public_haproxy.pem',
+      }
+    }
+
+    /(?i)debian/: {
+      file { '/usr/local/share/ca-certificates/public_haproxy.crt':
+        ensure => 'link',
+        target => '/etc/pki/tls/certs/public_haproxy.pem',
+      }
+    }
+    default: {
+      fail("Unsupported OS: ${::osfamily}/${::operatingsystem}")
+    }
+  }
+}
 
 case $::osfamily {
   /(?i)redhat/: {
-    file { '/etc/pki/ca-trust/source/anchors/public_haproxy.pem':
-      ensure => 'link',
-      target => '/etc/pki/tls/certs/public_haproxy.pem',
-    }->
-
     exec { 'enable_trust':
       path    => '/bin:/usr/bin:/sbin:/usr/sbin',
       command => 'update-ca-trust force-enable',
     }->
-
     exec { 'add_trust':
       path    => '/bin:/usr/bin:/sbin:/usr/sbin',
-      command => 'update-ca-trust extract',
+      command => 'update-ca-certificates',
     }
   }
-  /(?i)debian/: {
-    file { '/usr/local/share/ca-certificates/public_haproxy.crt':
-      ensure => 'link',
-      target => '/etc/pki/tls/certs/public_haproxy.pem',
-    }->
 
+  /(?i)debian/: {
     exec { 'add_trust':
       path    => '/bin:/usr/bin:/sbin:/usr/sbin',
       command => 'update-ca-certificates',
@@ -36,7 +62,5 @@ case $::osfamily {
   }
 }
 
-host { $public_ssl_hash['hostname']:
-  ensure => present,
-  ip     => $ip,
-}
+
+File <| |> -> Exec['add_trust']
