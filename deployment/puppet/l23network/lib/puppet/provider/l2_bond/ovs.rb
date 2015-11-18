ef8a8c19e47ae37c6f34f280a5e83febfa249d70
Sergey Vasilenko <svasilenko@mirantis.com>
2015-11-18 16:05:13 +0300

    Make able to work prefetch without OVS
    Change-Id: I92d43b290b0d8bdfaa8b9c48bf61fdf3a98ae8aa
    Related-bug: #1516060

--- deployment/puppet/l23network/lib/puppet/provider/l2_bond/ovs.rb
+++ deployment/puppet/l23network/lib/puppet/provider/l2_bond/ovs.rb
@@ -14,7 +14,7 @@ Puppet::Type.type(:l2_bond).provide(:ovs, :parent => Puppet::Provider::Ovs_base)
 
   def self.get_instances(big_hash)
     # didn't use .select{...} here for backward compatibility with ruby 1.8
-    big_hash[:port].reject{|k,v| !v[:port_type].include?('bond')}
+    big_hash.fetch(:port, {}).reject{|k,v| !v[:port_type].include?('bond')}
   end
 
   #-----------------------------------------------------------------
