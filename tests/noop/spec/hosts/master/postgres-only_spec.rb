1d26ae4a4fd5dbbad0d531315e17f0113d55f6a9
Dmitry Teselkin <dteselkin@mirantis.com>
2015-11-18 21:02:29 +0300

    Fix postgresql manifest
    Remove hardcoded postgresql version and paths, because they broke
    installation in CentOS7
    
    Is compatible with CentOS6 master node
    
    Implements: blueprint master-on-centos7
    
    Change-Id: I660c4aa402188e1b089eab14f9478e3baa30310c
    Co-Authored-By: Sergii Golovatiuk <sgolovatiuk@mirantis.com>
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- tests/noop/spec/hosts/master/postgres-only_spec.rb
+++ tests/noop/spec/hosts/master/postgres-only_spec.rb
@@ -3,5 +3,42 @@ require 'shared-examples'
 manifest = 'master/postgres-only.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+    context 'running on CentOS 6' do
+      let(:facts) do
+        Noop.centos_facts.merge({
+          :operatingsystemmajrelease => '6'
+        })
+      end
+      it 'should contain class postgresql::globals with proper bindir' do
+        should contain_class('postgresql::globals').with(
+          :bindir  => '/usr/pgsql-9.3/bin'
+        )
+      end
+      it 'should contain postgres_configs with proper values' do
+        should contain_postgres_config('log_rotation_age').with(
+          :value => '7d'
+        )
+        should contain_postgres_config('log_filename').with(
+          :value => 'pgsql'
+        )
+        should contain_postgres_config('log_directory').with(
+          :value => '/var/log/'
+        )
+      end
+    end
+    context 'running on CentOS 7' do
+      let(:facts) do
+        Noop.centos_facts.merge({
+          :operatingsystemmajrelease => '7'
+        })
+      end
+      it 'should contain class postgresql::globals with proper bindir' do
+        should contain_class('postgresql::globals').with(
+          :bindir => nil
+        )
+      end
+    end
+  end
   test_centos manifest
 end
