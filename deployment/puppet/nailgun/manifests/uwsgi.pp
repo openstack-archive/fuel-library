42d30d800257b249252f05a5e8411a7a32ab9fa6
Dmitry Teselkin <dteselkin@mirantis.com>
2015-11-19 21:40:53 +0300

    Fix site-packages path for python 2.7
    CentOS7 comes with python2.7 included, so we need to
    update path to site-packages directory
    
    Blueprint: master-on-centos7
    
    Is NOT compatible with CentOS6 master node
    
    Change-Id: Ifa7026ef5e090d740797567e99dae0f08e41c310

--- deployment/puppet/nailgun/manifests/uwsgi.pp
+++ deployment/puppet/nailgun/manifests/uwsgi.pp
@@ -8,6 +8,17 @@ class nailgun::uwsgi(
   $production,
 ) {
 
+  if $::osfamily == 'RedHat' {	4
+    case $operatingsystemmajrelease {	5
+      '6': {
+        $site_packages_path = inline_template("<%= @venv %>/lib/python2.6/site-packages")
+      }
+      '7': {
+        $site_packages_path = inline_template("<%= @venv %>/lib/python2.7/site-packages")
+      }
+    }
+  }
+
   if $::physicalprocessorcount > 4  {
     $physicalprocessorcount = 8
   } else {
