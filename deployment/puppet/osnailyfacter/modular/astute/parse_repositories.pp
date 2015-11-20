61f66983b75659df25b5812fa8985737c84da738
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-11-20 13:47:08 +0100

    [WIP] Replace upload_core_repos with Puppet tasks
    Related-Bug: 1508486
    Change-Id: I1b83e3bfaebecdb8455d5697e320f24fb4941536

---
+++ deployment/puppet/osnailyfacter/modular/astute/parse_repositories.pp
@@ -0,0 +1,16 @@
+notice('MODULAR: parse_repositories.pp')
+$repositories_yaml_file = '/tmp/repositories.yaml'
+
+$repo_setup = hiera_hash(repo_setup, {})
+if empty($repo_setup[repos]) {
+  fail('No repositories found in astute.yaml')
+}
+$repos = $repo_setup[repos]
+
+file { $repositories_yaml_file:
+  ensure  => 'present',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  content => template('osnailyfacter/repositories.yaml.erb')
+}
