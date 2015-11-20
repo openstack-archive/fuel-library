f2268a09893e8c34f79d8b817c9e474d2679c32a
Sergey Vasilenko <svasilenko@mirantis.com>
2015-11-20 23:04:28 +0300

    Use network_metadata hash instead deprecated hodes list
    for generate /etc/hosts file
    
    Change-Id: I81d429976b1f737d05d5f73d51404d663a817300
    Closes-bug: #1518439

---
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/network_metadata_to_hosts.rb
@@ -0,0 +1,21 @@
+#
+# array_or_string_to_array.rb
+#
+
+module Puppet::Parser::Functions
+  newfunction(:network_metadata_to_hosts, :type => :rvalue, :doc => <<-EOS
+              convert network_metadata hash to
+              hash for puppet `host` create_resources call
+    EOS
+  ) do |args|
+    hosts=Hash.new
+    nodes=args[0].fetch('nodes', {})
+    nodes.each do |name, props|
+      hosts[props['fqdn']]={:ip=>props['network_roles']['mgmt/vip'],:host_aliases=>[name]}
+      notice("Generating host entry #{name} #{props['network_roles']['mgmt/vip']} #{props['fqdn']}")
+    end
+    return hosts
+  end
+end
+
+# vim: set ts=2 sw=2 et :
