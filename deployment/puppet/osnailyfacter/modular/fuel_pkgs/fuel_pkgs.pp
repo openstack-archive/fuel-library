8d2938853b5241ccde5626bb42c3d1dbb4b5f6c2
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-11-20 13:47:08 +0100

    Replace upload_core_repos with Puppet tasks
    Currently the code responsible for configuring repositories on the
    managed nodes is kept directly it Nailgun. This commit introduces two
    new tasks, parse_repositories and setup_repositories, that completely
    replace upload_core_repos.
    
    Due to limitations of noop tests, information about repositories has to
    be accessible from Hiera, so the hiera task is executed right after
    deploy_start and sets up additional data source,
    /etc/hiera/repositories.yaml. This file is created by parse_repositories
    basing on astute.yaml, so the resulting data can be directly consumed by
    the apt Puppet module. Installation of the 'deep_merge' ruby gem has
    beem moved to fuel_pkgs task, which is executed after setting
    repositories. The commit also extends noop tests of this task.
    
    Related-Bug: 1508486
    Change-Id: I1b83e3bfaebecdb8455d5697e320f24fb4941536

--- deployment/puppet/osnailyfacter/modular/fuel_pkgs/fuel_pkgs.pp
+++ deployment/puppet/osnailyfacter/modular/fuel_pkgs/fuel_pkgs.pp
@@ -1,10 +1,14 @@
 notice('MODULAR: fuel_pkgs.pp')
 
+$deep_merge_package_name = $::osfamily ? {
+  'RedHat' => 'rubygem-deep_merge',
+  'Debian' => 'ruby-deep-merge',
+}
+
 $fuel_packages = [
   'fuel-ha-utils',
   'fuel-misc',
+  $deep_merge_package_name
 ]
 
-package { $fuel_packages :
-  ensure => 'latest',
-}
+ensure_packages($fuel_packages)
