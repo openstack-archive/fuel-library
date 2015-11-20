57f82f7823c636d64bdcffa7575a79b1c4bd6df1
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-11-20 13:47:08 +0100

    Replace upload_core_repos with Puppet tasks
    Currently the code responsible for configuring repositories on the
    managed nodes is kept directly it Nailgun. This commit introduces two
    new tasks, parse_repositories and setup_repositories, that completely
    replace upload_core_repos.
    
    Due to limitations of noop tests, information about repositories has to
    be accessible from Hiera, so the hiera task is executed right after
    deploy_start and sets up additional data source,
    /etc/hiera/repositories.yaml. This file is created by parse_repositories
    basing on astute.yaml, so the resulting data can be directly consumed by
    the apt Puppet module. Installation of the 'deep_merge' ruby gem has
    beem moved to fuel_pkgs task, which is executed after setting
    repositories. The commit also extends noop tests of this task.
    
    Related-Bug: 1508486
    Change-Id: I1b83e3bfaebecdb8455d5697e320f24fb4941536

--- deployment/puppet/osnailyfacter/modular/hiera/hiera.pp
+++ deployment/puppet/osnailyfacter/modular/hiera/hiera.pp
@@ -1,10 +1,5 @@
 notice('MODULAR: hiera.pp')
 
-$deep_merge_package_name = $::osfamily ? {
-  /RedHat/ => 'rubygem-deep_merge',
-  /Debian/ => 'ruby-deep-merge',
-}
-
 $data_dir            = '/etc/hiera'
 $data                = [
   'override/node/%{::fqdn}',
@@ -19,6 +14,7 @@ $data                = [
   'module/%{calling_module}',
   'nodes',
   'globals',
+  'repositories',
   'astute'
 ]
 $astute_data_file    = '/etc/astute.yaml'
@@ -71,8 +67,3 @@ file { 'hiera_puppet_config' :
   target => $hiera_main_config,
 }
 
-# needed to support the 'deeper' merge_behavior setting for hiera
-package { 'rubygem-deep_merge':
-  ensure => present,
-  name   => $deep_merge_package_name,
-}
