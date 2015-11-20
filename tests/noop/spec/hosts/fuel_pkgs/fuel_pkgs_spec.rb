54ebaf4e1478315ed243902a302bb4c738b2525b
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-11-20 13:47:08 +0100

    Replace upload_core_repos with Puppet tasks
    Related-Bug: 1508486
    Change-Id: I1b83e3bfaebecdb8455d5697e320f24fb4941536

--- tests/noop/spec/hosts/fuel_pkgs/fuel_pkgs_spec.rb
+++ tests/noop/spec/hosts/fuel_pkgs/fuel_pkgs_spec.rb
@@ -3,6 +3,22 @@ require 'shared-examples'
 manifest = 'fuel_pkgs/fuel_pkgs.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+    it 'should have ruby deep_merge installed' do
+      case facts[:operatingsystem]
+      when 'Ubuntu'
+        package_name = 'ruby-deep-merge'
+      when 'CentOS'
+        package_name = 'rubygem-deep_merge'
+      end
+
+      should contain_package('rubygem-deep_merge').with(
+        'ensure' => 'present',
+        'name'   => package_name
+      )
+    end
+  end
+
   test_ubuntu_and_centos manifest
 end
 
