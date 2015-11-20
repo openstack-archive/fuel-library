53c460ae5bb1d1b496abd9e333be65ca6bebde5c
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-11-20 13:47:08 +0100

    Replace upload_core_repos with Puppet tasks
    Related-Bug: 1508486
    Change-Id: I1b83e3bfaebecdb8455d5697e320f24fb4941536

---
+++ deployment/puppet/osnailyfacter/modular/astute/parse_repositories.pp
@@ -0,0 +1,32 @@
+notice('MODULAR: parse_repositories.pp')
+
+$repositories_yaml_file = '/tmp/repositories.yaml'
+$astute_yaml_file       = '/etc/astute.yaml'
+$astute_yaml_exists     = inline_template('<%= File.exists?(@astute_yaml_file) %>')
+
+if $astute_yaml_exists == 'false' {
+  fail("${astute_yaml_file} does not exist")
+}
+
+$astute_yaml = loadyaml($astute_yaml_file)
+$repos = $astute_yaml[repo_setup][repos]
+
+if empty($repos) {
+  fail('No repositories found in astute.yaml')
+}
+
+file { $repositories_yaml_file:
+  ensure  => 'present',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  content => template('osnailyfacter/repositories.yaml.erb'),
+  notify  => Exec['rm_leading_whitespace'],
+}
+
+exec { 'rm_leading_whitespace':
+  command     => "sed 's/^[ \t]*//' -i ${repositories_yaml_file}",
+  unless      => "grep -q '^--- ' ${repositories_yaml_file}",
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
+}
