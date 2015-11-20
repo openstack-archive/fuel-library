187548c3db27eb15550fb1cf2868ffd3874b1057
Sergey Vasilenko <svasilenko@mirantis.com>
2015-11-20 23:04:28 +0300

    Use network_metadata hash instead of deprecated nodes list
    for generate /etc/hosts file
    
    Change-Id: I81d429976b1f737d05d5f73d51404d663a817300
    Closes-bug: #1518439

--- deployment/puppet/osnailyfacter/modular/hosts/hosts.pp
+++ deployment/puppet/osnailyfacter/modular/hosts/hosts.pp
@@ -1,5 +1,12 @@
 notice('MODULAR: hosts.pp')
 
-class { "l23network::hosts_file":
-  nodes => hiera('nodes'),
+$hosts_file = '/etc/hosts'
+$host_resources = network_metadata_to_hosts(hiera_hash('network_metadata'))
+
+Host {
+    ensure => present,
+    target => $hosts_file
 }
+
+create_resources(host, $host_resources)
+
