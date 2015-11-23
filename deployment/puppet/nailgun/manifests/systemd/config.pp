e1bec4fe6df0e35e6d5165b354f1305c488656c3
Ivan Suzdal <isuzdal@mirantis.com>
2015-11-23 18:07:22 +0300

    Systemd syslog settings for astute, nailgun and ostf
    Services astute, nailgun, ostf were converted to systemd units
    in a set of requests:
    * astute  https://review.openstack.org/#/c/248128
    * nailgun https://review.openstack.org/#/c/248206
    * ostf    https://review.openstack.org/#/c/248096
    
    assassind, receiverd, statsenderd, oswl_* are a part of nailgun
    so there is no separate request for them (included in 248206)
    
    However, those commits add only service units.
    
    This commit adds conf files with customized settings for those services.
    
    Blueprint: master-on-centos7
    
    Compatible with CentOS6 master node
    
    Change-Id: I1808a0852d7c95079ae27540b77b1ae0035ab65b

---
+++ deployment/puppet/nailgun/manifests/systemd/config.pp
@@ -0,0 +1,24 @@
+define nailgun::systemd::config {
+  file { "/etc/systemd/system/${title}.service.d/":
+    ensure      => directory
+  }
+
+  file { "/etc/systemd/system/${title}.service.d/local.conf":
+    content     => template("nailgun/systemd/service_template.erb"),
+    owner       => 'root',
+    group       => 'root',
+    mode        => '0644',
+  }
+  ->
+  exec { "systemd_reload_${title}":
+    command     => '/bin/systemctl daemon-reload',
+    refreshonly => true,
+    subscribe   => File["/etc/systemd/system/${title}.service.d/local.conf"],
+  }
+  ->
+  service { "${title}":
+    ensure      => running,
+    enable      => true,
+    subscribe   => Exec["systemd_reload_${title}"],
+  }
+}
