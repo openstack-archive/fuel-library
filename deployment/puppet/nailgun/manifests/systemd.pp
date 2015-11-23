aebdb43c14d2f4e9da38b6b98536fe740ca12239
Ivan Suzdal <isuzdal@mirantis.com>
2015-11-23 18:07:22 +0300

    Systemd syslog settings for astute, nailgun and ostf
    Services astute, nailgun, ostf were converted  to systemd units
    in a set of requests:
    * astute  https://review.openstack.org/#/c/248128
    * nailgun https://review.openstack.org/#/c/248206
    * ostf    https://review.openstack.org/#/c/248096
    
    assassind, receiverd, statsenderd, oswl_* are a part of nailgun
    so there is no separate request for them (included in 248206)
    
    However, those commits add only service units.
    
    This commit adds conf files with customized settings for those services.
    
    Blueprint: master-on-centos7
    
    Compatible with CentOS6 master node
    
    Change-Id: I1808a0852d7c95079ae27540b77b1ae0035ab65b

---
+++ deployment/puppet/nailgun/manifests/systemd.pp
@@ -0,0 +1,37 @@
+# == Class: nailgun::systemd
+#
+# Apply local settings for nailgun services.
+#
+# At this moment only start/stop timeouts
+# and syslog identificators.
+#
+# === Parameters
+#
+# [*services*]
+#   (required) Array. This is an array of service names for which local
+#   changes will be applied.
+#
+# [*production*]
+#   (required) String. Determine environment.
+#   Changes applies only for 'prod' and 'docker' environments.
+#
+
+class nailgun::systemd (
+  $services,
+  $production
+) {
+
+if is_array($services) {
+  case $production {
+    'prod', 'docker': {
+      if !empty($services) {
+        nailgun::systemd::config { $services: }
+      }
+    }
+    default: { }
+  }
+} else {
+  fail("Wrong type parameter: '${services}'. Shoul be array")
+}
+
+}
