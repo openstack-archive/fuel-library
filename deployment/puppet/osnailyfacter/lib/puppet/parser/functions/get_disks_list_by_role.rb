c3fa1943a22e24fa1028f81aaed48d0f871a90e1
Denis Egorenko <degorenko@mirantis.com>
2015-11-23 17:03:59 +0300

    Add Cinder Block Device role
    This commit adds a new role for Cinder Storage - Cinder Block
    Device role.
    
    Also added new function for getting list of disks, which are
    allocated for some role.
    
    Added a new noop test for covering new role.
    
    blueprint cinder-block-device-driver
    Depends-On: I0279adffc3866608109efd23845ef2aa365e0704
    
    Change-Id: Ic574f0ea484ac11cfceef2a8e836ca5432084d58

---
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/get_disks_list_by_role.rb
@@ -0,0 +1,28 @@
+module Puppet::Parser::Functions
+  newfunction(:get_disks_list_by_role, :type => :rvalue, :doc => <<-EOS
+Return a list of disks (node roles are keys) that have the given node role.
+example:
+  get_disks_list_by_role($node_volumes, 'cinder')
+EOS
+  ) do |args|
+    errmsg = "get_disks_list_by_role($node_volumes, 'cinder')"
+    disks_metadata, role = args
+    raise(Puppet::ParseError, "#{errmsg}: 1st argument should be a hash") if !disks_metadata.is_a?(Hash)
+    raise(Puppet::ParseError, "#{errmsg}: 1st argument should be a valid node_volumes hash") if !disks_metadata.has_key?('volumes') and !disks_metadata.has_key?('name')
+    raise(Puppet::ParseError, "#{errmsg}: 2nd argument should be node role") if !roles.is_a?(String)
+    disks = []
+    disks_metadata.each do |disk|
+      if not disk[:volumes].nil? and not disk[:volumes].empty?
+        disk[:volumes].each do |volume|
+          if not volume[:vg].nil? and volume[:vg] == role and volume[:size].nil? and volume[:size] != 0
+            disks << disk[:name]
+            break
+          end
+        end
+      end
+    end
+    return disks
+  end
+end
+
+# vim: set ts=2 sw=2 et :
