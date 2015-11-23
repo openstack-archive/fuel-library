096495d7d752f24a53aa58fee0830a7447a28aac
Ivan Suzdal <isuzdal@mirantis.com>
2015-11-23 18:40:20 +0300

    Systemd units for docker containers
    Make every docker container a standard systemd unit, allowing
    it to be managed by standard way with systemctl.
    
    Also add systemd support to dockercrl.
    
    Blueprint: master-on-centos7
    
    Breaks: nothing
    
    Change-Id: I924534b43d083f93130d2af805609a81b302d7de

---
+++ deployment/puppet/docker/manifests/systemd.pp
@@ -0,0 +1,49 @@
+class docker::systemd (
+  $release = undef,
+  $containers_enable = true,
+  $start_containers  = false,
+  $restart_timeout = 30,
+  $containers = ['astute', 'cobbler', 'keystone', 'mcollective', 'nailgun',
+    'nginx', 'ostf', 'postgres', 'rabbitmq', 'rsync', 'rsyslog'],
+  $depends = {
+    'astute'      => 'rsync',
+    'cobbler'     => 'nginx',
+    'keystone'    => 'rabbitmq',
+    'mcollective' => 'cobbler',
+    'nailgun'     => 'rsyslog',
+    'nginx'       => 'ostf',
+    'ostf'        => 'nailgun',
+    'rsync'       => 'keystone',
+    'rsyslog'     => 'astute',
+    'rabbitmq'    => 'postgres'
+  },
+) {
+  # No empty release allowed
+  validate_string($release)
+
+  #define docker::systemd::config( $release, $depends, $timeout ) {
+  #  file { "/usr/lib/systemd/system/docker-${title}.service":
+  #    content => template('docker/systemd/template.service.erb'),
+  #    owner   => 'root',
+  #    group   => 'root',
+  #    mode    => '0644',
+  #    ensure  => file,
+  #  }
+  #  ->
+  #  # Do not run, because service should be just enabled
+  #  # and first run executed from dockerctl, but not from systemctl
+  #  exec { "Enable ${title} container":
+  #    command => "/bin/systemctl enable docker-${title}.service"
+  #  }
+  #}
+
+  docker::systemd::config {$containers:
+                            release => $release,
+                            depends => $depends,
+                            timeout => $restart_timeout}
+  ->
+  exec { 'Reload_systemd_docker_containers':
+    command => '/usr/bin/systemctl daemon-reload'
+  }
+}
+
