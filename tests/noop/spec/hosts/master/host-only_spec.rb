0e71e06506cf724f5b2466ea3117362de414929e
Ivan Suzdal <isuzdal@mirantis.com>
2015-11-23 18:40:20 +0300

    Systemd units for docker containers
    Make every docker container a standard systemd unit, allowing
    it to be managed by standard way with systemctl.
    
    Also add systemd support to dockercrl.
    
    Blueprint: master-on-centos7
    
    Breaks: nothing
    
    Change-Id: I924534b43d083f93130d2af805609a81b302d7de

--- tests/noop/spec/hosts/master/host-only_spec.rb
+++ tests/noop/spec/hosts/master/host-only_spec.rb
@@ -3,5 +3,62 @@ require 'shared-examples'
 manifest = 'master/host-only.pp'
 
 describe manifest do
+
+  shared_examples 'catalog' do
+
+    let(:params) { {
+      containers => ['astute', 'cobbler', 'keystone', 'mcollective', 'nailgun',
+    'nginx', 'ostf', 'postgres', 'rabbitmq', 'rsync', 'rsyslog']
+    } }
+
+    release = facts[:fuel_release]
+
+    context 'running on centos 6' do
+      let(:facts) do
+        Noop.centos_facts.merge({
+          :operatingsystemmajrelease == '6'
+        })
+      end
+      it 'configure containers supervisor' do
+        should contain_class('docker::supervisor').with({
+          :release => release,
+          :require => "File[/etc/supervisord.d/#{release}]",
+        })
+        params[:containers].each do |container|
+          should contain_file("/etc/supervisord.d/#{release}/#{container}.conf").with({
+            :owner => 'root',
+            :group => 'root',
+            :mode  => '0644'
+          })
+        end
+      end #it do
+    end #context
+
+    context 'running on centos 7' do
+      let(:facts) do
+        Noop.centos_facts.merge({
+          :operatingsystemmajrelease >= '7'
+        })
+      end
+
+      it 'configure containers systemd' do
+        should contain_class('docker::systemd').with({
+          :release => release
+        })
+        params[:containers].each do |container|
+          should contain_file("/usr/lib/systemd/system/docker-#{container}.service").with({
+            :owner => 'root',
+            :group => 'root',
+            :mode  => '0644',
+          })
+          shoul contain_service("docker-#{container}").with({
+            :ensure => 'undef',
+            :enable => 'true',
+          })
+        end
+      end #it do
+    end #context
+  end #shared_examples
+
   test_centos manifest
 end
