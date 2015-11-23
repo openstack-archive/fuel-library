d0bf8efdb4f9a822bc2125615dbe24d4324eca7b
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-11-23 20:12:46 +0300

    Add twemproxy as default proxy for memcache
    All openstack services will use twemproxy instead of use
    memcached directly.
    
    Change-Id: I469ba4456d550bdc1dacb1eb3634daa94b121883

---
+++ deployment/puppet/osnailyfacter/modular/twemproxy/twemproxy.pp
@@ -0,0 +1,11 @@
+notice('MODULAR: twemproxy.pp')
+
+$memcache_nodes        = get_nodes_hash_by_roles(hiera('network_metadata'), hiera('memcache_roles'))
+$memcache_address_map  = get_node_to_ipaddr_map_by_network_role($memcache_nodes, 'mgmt/memcache')
+
+$memcache_array = suffix(values($memcache_address_map), ':11211:1')
+
+
+class { 'twemproxy':
+  clients_array => $memcache_array,
+}
---
+++ deployment/puppet/twemproxy/manifests/config.pp
@@ -0,0 +1,11 @@
+class twemproxy::config inherits twemproxy {
+
+  if $twemproxy::package_manage {
+    file { '/etc/default/twemproxy':
+      owner => 'root',
+      group => 'root',
+      mode  => '0644',
+      content => template('twemproxy/config.erb'),
+    }
+  }
+}
