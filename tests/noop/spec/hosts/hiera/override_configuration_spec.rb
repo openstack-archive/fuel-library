f05683650cc6bf05bb7766d42fa2200536ece050
Bartosz Kupidura <bkupidura@mirantis.com>
2015-11-26 10:31:19 +0100

    Add config override hierarchy to hiera
    Change-Id: Ia6150e5a600160ecc69ac66da120f2a3e88a4bb0
    Implements: blueprint openstack-config-change
    Depends-on: I91ca0bc82b33d9555d3f558fd6138c5467a57e9c

---
+++ tests/noop/spec/hosts/hiera/override_configuration_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'hiera/override_configuration.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+    it 'should setup hiera override configuration' do
+      ['/etc/hiera/override', '/etc/hiera/override/configuration'].each do |f|
+        should contain_file(f).with(
+          'ensure' => 'directory',
+          'path'   => f
+        )
+      end
+    end
+  end
+
+  test_ubuntu_and_centos manifest
+end
+
