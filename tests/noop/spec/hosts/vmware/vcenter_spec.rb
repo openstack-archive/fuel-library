33ab5ec4fcdf60e883402e62ae3579274c14c315
Alexander Arzhanov <aarzhanov@mirantis.com>
2015-11-26 19:32:44 +0300

    Change protocol and hostname for novncproxy
    In order to provide VM console access via HTTPS, we need to provide
    the correct link to the proxy via the api.
    Also, the cloud administrator will need to properly configure the DNS
    for the clients to ensure they will be able to access the HTTPS
    endpoint as the DNS name must match the certificate name for the Nova
    VNC Proxy service.
    
    Change-Id: I99be041b547284edc57b76179f8acc2566662307
    Closes-bug: #1511422

--- tests/noop/spec/hosts/vmware/vcenter_spec.rb
+++ tests/noop/spec/hosts/vmware/vcenter_spec.rb
@@ -4,14 +4,33 @@ manifest = 'vmware/vcenter.pp'
 
 describe manifest do
   shared_examples 'catalog' do
-
+    use_vcenter = Noop.hiera('use_vcenter', false)
     network_manager = Noop.hiera_structure('novanetwork_parameters/network_manager')
-
-    if network_manager == 'VlanManager'
-      it 'should declare vmware::controller with vlan_interface option set to vmnic0' do
-        should contain_class('vmware::controller').with(
-          'vlan_interface' => 'vmnic0',
-        )
+    public_ssl = Noop.hiera_structure('public_ssl/services')
+    if use_vcenter
+      if network_manager == 'VlanManager'
+        it 'should declare vmware::controller with vlan_interface option set to vmnic0' do
+          should contain_class('vmware::controller').with(
+            'vlan_interface' => 'vmnic0',
+          )
+        end
+      end
+      if public_ssl
+        it 'should properly configure vncproxy WITH ssl' do
+          vncproxy_host = Noop.hiera_structure('public_ssl/hostname')
+          should contain_class('vmware::controller').with(
+            'vncproxy_host'     => vncproxy_host,
+            'vncproxy_protocol' => 'https',
+          )
+        end
+      else
+        it 'should properly configure vncproxy WITHOUT ssl' do
+          vncproxy_host = Noop.hiera('public_vip')
+          should contain_class('vmware::controller').with(
+            'vncproxy_host'     => vncproxy_host,
+            'vncproxy_protocol' => 'http',
+          )
+        end
       end
     end
 
@@ -19,4 +38,3 @@ describe manifest do
 
   test_ubuntu_and_centos manifest
  end
-
