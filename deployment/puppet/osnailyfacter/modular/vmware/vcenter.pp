844592a7c2b79bfaab51f758b1bd83839a418edf
Alexander Arzhanov <aarzhanov@mirantis.com>
2015-11-26 19:32:44 +0300

    Change protocol and hostname for novncproxy
    In order to provide VM console access via HTTPS, we need to provide
    the correct link to the proxy via the api.
    Also, the cloud administrator will need to properly configure the DNS
    for the clients to ensure they will be able to access the HTTPS
    endpoint as the DNS name must match the certificate name for the Nova
    VNC Proxy service.
    
    Change-Id: I99be041b547284edc57b76179f8acc2566662307
    Closes-bug: #1511422

--- deployment/puppet/osnailyfacter/modular/vmware/vcenter.pp
+++ deployment/puppet/osnailyfacter/modular/vmware/vcenter.pp
@@ -2,17 +2,25 @@ notice('MODULAR: vmware/vcenter.pp')
 
 $use_vcenter     = hiera('use_vcenter', false)
 $vcenter_hash    = hiera('vcenter_hash')
+$nova_hash       = hiera_hash('nova_hash', {})
 $public_vip      = hiera('public_vip')
 $use_neutron     = hiera('use_neutron', false)
 $ceilometer_hash = hiera('ceilometer',{})
 $debug           = hiera('debug', false)
+
+$public_ssl_hash = hiera('public_ssl')
+$vncproxy_host   = $public_ssl_hash['services'] ? {
+  true    => $public_ssl_hash['hostname'],
+  default => $public_vip,
+}
 
 if $use_vcenter {
   class { 'vmware':
     vcenter_settings => $vcenter_hash['computes'],
     vlan_interface   => $vcenter_hash['esxi_vlan_interface'],
     use_quantum      => $use_neutron,
-    vnc_address      => $public_vip,
+    vncproxy_host    => $vncproxy_host,
+    nova_hash        => $nova_hash,
     ceilometer       => $ceilometer_hash['enabled'],
     debug            => $debug,
   }
