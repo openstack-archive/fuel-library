8f5cd3c37a357095f537973e5865be4d9789a8b5
vsaienko <vsaienko@mirantis.com>
2015-11-27 17:42:58 +0200

    Set overview_days_range to 1
    This setting is limit the amount of data fetched
    by default when rendering the Overview panel.
    Upstream Change-Id: I5b65583e8f42e8c186a074f61f8bb8e0ac17854f
    
    Change-Id: I48b89e211792606b124ef6e3c6ae0f1ab3aa25b9
    Closes-Bug: #1508464

--- deployment/puppet/osnailyfacter/modular/horizon/horizon.pp
+++ deployment/puppet/osnailyfacter/modular/horizon/horizon.pp
@@ -9,6 +9,7 @@ $bind_address            = get_network_role_property('horizon', 'ipaddr')
 $neutron_advanced_config = hiera_hash('neutron_advanced_configuration', {})
 $public_ssl              = hiera('public_ssl')
 $ssl_no_verify           = $public_ssl['horizon']
+$overview_days_range     = pick($horizon_hash['overview_days_range'], 1)
 
 if $horizon_hash['secret_key'] {
   $secret_key = $horizon_hash['secret_key']
@@ -28,23 +29,24 @@ $neutron_options    = {'enable_distributed_router' => $neutron_dvr}
 $hypervisor_options = {'enable_quotas' => hiera('nova_quota')}
 
 class { 'openstack::horizon':
-  secret_key         => $secret_key,
-  cache_server_ip    => ipsort(values($memcache_address_map)),
-  package_ensure     => hiera('horizon_package_ensure', 'installed'),
-  bind_address       => $bind_address,
-  cache_server_port  => hiera('memcache_server_port', '11211'),
-  cache_backend      => 'django.core.cache.backends.memcached.MemcachedCache',
-  cache_options      => {'SOCKET_TIMEOUT' => 1,'SERVER_RETRIES' => 1,'DEAD_RETRY' => 1},
-  neutron            => hiera('use_neutron'),
-  keystone_url       => $keystone_url,
-  use_ssl            => hiera('horizon_use_ssl', false),
-  ssl_no_verify      => $ssl_no_verify,
-  verbose            => pick($horizon_hash['verbose'], hiera('verbose', true)),
-  debug              => pick($horizon_hash['debug'], hiera('debug')),
-  use_syslog         => hiera('use_syslog', true),
-  hypervisor_options => $hypervisor_options,
-  servername         => hiera('public_vip'),
-  neutron_options    => $neutron_options,
+  secret_key          => $secret_key,
+  cache_server_ip     => ipsort(values($memcache_address_map)),
+  package_ensure      => hiera('horizon_package_ensure', 'installed'),
+  bind_address        => $bind_address,
+  cache_server_port   => hiera('memcache_server_port', '11211'),
+  cache_backend       => 'django.core.cache.backends.memcached.MemcachedCache',
+  cache_options       => {'SOCKET_TIMEOUT' => 1,'SERVER_RETRIES' => 1,'DEAD_RETRY' => 1},
+  neutron             => hiera('use_neutron'),
+  keystone_url        => $keystone_url,
+  use_ssl             => hiera('horizon_use_ssl', false),
+  ssl_no_verify       => $ssl_no_verify,
+  verbose             => pick($horizon_hash['verbose'], hiera('verbose', true)),
+  debug               => pick($horizon_hash['debug'], hiera('debug')),
+  use_syslog          => hiera('use_syslog', true),
+  hypervisor_options  => $hypervisor_options,
+  servername          => hiera('public_vip'),
+  neutron_options     => $neutron_options,
+  overview_days_range => $overview_days_range,
 }
 
 $haproxy_stats_url = "http://${service_endpoint}:10000/;csv"
