b262cceca305b44082b9c25b06e47db7a482c6cb
Valyavskiy Viacheslav <vvalyavskiy@mirantis.com>
2015-12-03 17:46:30 +0300

    Add tests for merge_settings_yaml provider
    Change-Id: I03d3e20177dd55dc2e56a4e65e031e71c8736567

--- deployment/puppet/nailgun/spec/spec_helper.rb
+++ deployment/puppet/nailgun/spec/spec_helper.rb
@@ -1,8 +1,10 @@
 require 'rspec-puppet'
+require 'fakefs/spec_helpers'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.include FakeFS::SpecHelpers, fakefs: true
 end
