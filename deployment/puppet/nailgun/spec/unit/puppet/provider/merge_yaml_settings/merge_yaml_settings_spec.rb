b982fcde5b51bfbc6cd422d75179cc496a2f8664
Valyavskiy Viacheslav <vvalyavskiy@mirantis.com>
2015-12-03 17:46:30 +0300

    Add tests for merge_settings_yaml provider
    Change-Id: I03d3e20177dd55dc2e56a4e65e031e71c8736567

---
+++ deployment/puppet/nailgun/spec/unit/puppet/provider/merge_yaml_settings/merge_yaml_settings_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+require 'tmpdir'
+require 'yaml'
+
+provider_class = Puppet::Type.type(:merge_yaml_settings).provider(:ruby)
+describe provider_class,fakefs=true do
+
+  before :each do
+    @base_dir = Dir.tmpdir()
+    @sample_filepath = "#{@base_dir}/sample_file_path.yaml"
+    @dest_filepath = "#{@base_dir}/dest_file_path.yaml"
+
+    FileUtils.mkdir_p(@base_dir)
+  end
+
+  it 'should merge settings if both are present' do
+
+    sample_yaml_text = <<-EOF
+      SETTING_A: value1
+      SETTING_B: value2
+    EOF
+
+    result_yaml = {
+      'SETTING_A' => 'new_value1',
+      'SETTING_B' => 'new_value2',
+      'SETTING_C' => 'new_value'
+    }
+
+    settings = {
+      'SETTING_A' => 'new_value1',
+      'SETTING_B' => 'new_value2',
+      'SETTING_C' => 'new_value'
+    }
+
+    File.open(@sample_filepath, 'w') { |file| file.write(sample_yaml_text) }
+
+    resource = Puppet::Type::Merge_yaml_settings.new(
+      { 
+        :name => @dest_filepath,
+        :sample_settings => @sample_filepath,
+        :provider => 'ruby',
+        :override_settings => settings
+      }
+    )
+
+    provider = provider_class.new(resource)
+    provider.create()
+    expect(YAML.load(File.read(@dest_filepath))).to eq result_yaml
+  end
+end
