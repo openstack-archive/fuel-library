15302df063504bed7aa9347474c90a28e1921404
Andrey Shestakov <ashestakov@mirantis.com>
2015-12-04 13:53:12 +0200

    Set host on all ironic-compute to same value
    All ironic-computes should has same value of 'host' parameter to avoid problem
    with multiplication of hypervisors in nova.
    
    This is workaround, proper solution should be implemented in Ironic's upstream
    in next releases.
    
    Change-Id: I91f3028306177c037c8afa3c98fc56e176a9066e
    Closes-bug: #1522361

--- tests/noop/spec/hosts/roles/ironic-compute_spec.rb
+++ tests/noop/spec/hosts/roles/ironic-compute_spec.rb
@@ -16,6 +16,10 @@ describe manifest do
       it 'nova config should have reserved_host_memory_mb set to 0' do
         should contain_nova_config('DEFAULT/reserved_host_memory_mb').with(:value => '0')
       end
+
+      it 'nova-compute.conf should have host set to "ironic-compute"' do
+        should contain_file('/etc/nova/nova-compute.conf').with('content'  => "[DEFAULT]\nhost=ironic-compute")
+      end
     end
   end
 
