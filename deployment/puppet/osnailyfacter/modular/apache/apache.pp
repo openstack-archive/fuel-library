1cc308790445ea1c1c9234374fba2409f428827d
Dmitry Bilunov <dbilunov@mirantis.com>
2015-12-08 12:50:57 +0300

    Disable apache's purge_configs option
    Successive calls to "apache" fuel task results in a non-working Horizon
    and Keystone due to purge_configs option set to true which wipes the
    contents of several directories inside /etc/apache2/
    
    Fuel runs puppet independently for each "module" so it does not seem
    possible to utilize puppet's relationship metaparameters and other
    ordering manipulation tools to make sure that all config generators
    get reexecuted at this point.
    
    It seems reasonable just to disable `purge_configs' option since
    /etc/apache2/ directory structure already implements a framework
    for enabling/disabling entities such as virtual hosts, configuration
    snippets and modules.
    
    Change-Id: I974ef1423187d908684fa213122e8ba314cac917
    Closes-Bug: 1522857

--- deployment/puppet/osnailyfacter/modular/apache/apache.pp
+++ deployment/puppet/osnailyfacter/modular/apache/apache.pp
@@ -5,7 +5,7 @@ sysctl::value { 'net.core.somaxconn':           value => '4096' }
 sysctl::value { 'net.ipv4.tcp_max_syn_backlog': value => '8192' }
 
 class { 'osnailyfacter::apache':
-  purge_configs => true,
+  purge_configs => false,
   listen_ports  => hiera_array('apache_ports', ['80', '8888']),
 }
 
