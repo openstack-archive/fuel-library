8b665c6d98c84e6a8eac4802f37623f616dc7d54
Vladimir Kuklin <vkuklin@mirantis.com>
2015-12-10 20:16:07 +0300

    Get rid of usage of node role in Ceph manifests
    Change-Id: I8b51651adc2634544f510de8838ebaabcd1e7d43
    Implements: task-based-deployment-astute

--- deployment/puppet/osnailyfacter/modular/cluster/cluster.pp
+++ deployment/puppet/osnailyfacter/modular/cluster/cluster.pp
@@ -1,7 +1,7 @@
 notice('MODULAR: cluster.pp')
 
-if !(hiera('role') in hiera('corosync_roles')) {
-    fail('The node role is not in corosync roles')
+if ! roles_include(hiera('corosync_roles')) {
+  fail('The node role is not in corosync roles')
 }
 
 prepare_network_config(hiera_hash('network_scheme'))
