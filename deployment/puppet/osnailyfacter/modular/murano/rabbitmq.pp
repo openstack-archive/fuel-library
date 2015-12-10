4464e28b148ea1e2d2bcd8138f611a3e43c6db8f
Alexey Deryugin <aderyugin@mirantis.com>
2015-12-10 19:46:15 +0300

    Update Murano RabbitMQ task to deploy separate RabbitMQ instance
    Since murano-agent requires access to RabbitMQ from VMs Murano
    requires separate RabbitMQ instance.
    
    Change-Id: I538f7841398139ee73af40d820a8af7e7c4d9aba
    Closes-Bug: #1517896

--- deployment/puppet/osnailyfacter/modular/murano/rabbitmq.pp
+++ deployment/puppet/osnailyfacter/modular/murano/rabbitmq.pp
@@ -1,13 +1,101 @@
 notice('MODULAR: murano/rabbitmq.pp')
 
-$rabbit_hash                = hiera_hash('rabbit_hash', {})
+$rabbit_hash          = hiera_hash('rabbit_hash', {})
+
+$rabbit_user          = $rabbit_hash['user']
+$rabbit_password      = $rabbit_hash['password']
+$rabbit_vhost         = '/'
+
+$rabbit_port          = '55572'
+$rabbit_cluster_port  = '41056'
+$rabbit_node_name     = 'murano@localhost'
+$rabbit_service_name  = 'rabbitmq-server-murano'
+$rabbit_firewall_rule = '203 murano-rabbitmq'
 
 #################################################################
 
-rabbitmq_vhost { '/murano': }
-
-rabbitmq_user_permissions { "${rabbit_hash['user']}@/murano":
-    configure_permission => '.*',
-    read_permission      => '.*',
-    write_permission     => '.*',
+case $::osfamily {
+  'RedHat': {
+    $init_script_file = 'murano-rabbitmq-init-centos.erb'
+    $init_install_cmd = "chkconfig --add '/etc/init.d/${rabbit_service_name}'"
+  }
+  'Debian': {
+    $init_script_file = 'murano-rabbitmq-init-ubuntu.erb'
+    $init_install_cmd = "update-rc.d '${rabbit_service_name}' defaults"
+  }
+  default: {
+    fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}")
+  }
 }
+
+file { 'rabbitmq_config' :
+  path    => '/etc/rabbitmq/rabbitmq-murano.config',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => template('osnailyfacter/murano-rabbitmq.config.erb'),
+}
+
+file { 'init_script' :
+  path    => "/etc/init.d/${rabbit_service_name}",
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0755',
+  content => template("osnailyfacter/${init_script_file}"),
+}
+
+exec { 'install_init_script' :
+  command => $init_install_cmd,
+  path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+  unless  => "test -f /etc/init.d/${rabbit_service_name}"
+}
+
+service { $rabbit_service_name :
+  ensure => 'running',
+  name   => $rabbit_service_name,
+  enable => true,
+}
+
+firewall { $rabbit_firewall_rule :
+  dport  => [ $rabbit_port ],
+  proto  => 'tcp',
+  action => 'accept',
+}
+
+exec { 'remove_murano_guest' :
+  command => "rabbitmqctl -n '${rabbit_node_name}' delete_user guest",
+  onlyif  => "rabbitmqctl -n '${rabbit_node_name}' list_users | grep -qE '^guest\\s*\\['",
+  path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+}
+
+exec { 'create_murano_user' :
+  command => "rabbitmqctl -n '${rabbit_node_name}' add_user '${rabbit_user}' '${rabbit_password}'",
+  unless  => "rabbitmqctl -n '${rabbit_node_name}' list_users | grep -qE '^${rabbit_user}\\s*\\['",
+  path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+}
+
+exec { 'create_murano_vhost' :
+  command => "rabbitmqctl -n '${rabbit_node_name}' add_vhost '${rabbit_vhost}'",
+  unless  => "rabbitmqctl -n '${rabbit_node_name}' list_vhosts | grep -qE '^${rabbit_vhost}$'",
+  path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+}
+
+exec { 'set_murano_user_permissions' :
+  command => "rabbitmqctl -n '${rabbit_node_name}' set_permissions -p '${rabbit_vhost}' '${rabbit_user}' '.*' '.*' '.*'",
+  path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+}
+
+File['rabbitmq_config'] ->
+  File['init_script'] ->
+    Exec['install_init_script'] ->
+      Service['rabbitmq-server-murano']
+
+Firewall[$rabbit_firewall_rule] -> Service['rabbitmq-server-murano']
+File['rabbitmq_config'] ~> Service['rabbitmq-server-murano']
+File['init_script'] ~> Service['rabbitmq-server-murano']
+
+Service['rabbitmq-server-murano'] ->
+  Exec['remove_murano_guest'] ->
+    Exec['create_murano_user'] ->
+      Exec['create_murano_vhost'] ->
+        Exec['set_murano_user_permissions']
--- tests/noop/spec/hosts/murano/rabbitmq_spec.rb
+++ tests/noop/spec/hosts/murano/rabbitmq_spec.rb
@@ -6,18 +6,78 @@ describe manifest do
   shared_examples 'rabbitmq' do
 
     let(:rabbit_user) { Noop.hiera_structure('rabbit/user', 'murano') }
+    let(:rabbit_password) { Noop.hiera_structure('rabbit/password', 'secret') }
+    let(:rabbit_vhost) { '/' }
 
-    it 'should declare rabbitmq_vhost' do
-      should contain_rabbitmq_vhost('/murano')
+    let(:rabbit_port) { '55572' }
+    let(:rabbit_cluster_port) { '41056' }
+    let(:rabbit_node_name) { 'murano@localhost' }
+    let(:rabbit_service_name) { 'rabbitmq-server-murano' }
+    let(:rabbit_firewall_rule) { '203 murano-rabbitmq' }
+
+    if :osfamily == "RedHat"
+      let(:init_script_file) { 'murano-rabbitmq-init-centos.erb' }
+      let(:init_install_cmd) { "chkconfig --add '/etc/init.d/#{rabbit_service_name}'" }
+    elsif :osfamily == "Debian"
+      let(:init_script_file) { 'murano-rabbitmq-init-ubuntu.erb' }
+      let(:init_install_cmd) { "update-rc.d '#{rabbit_service_name}' defaults" }
     end
 
-    it 'should declare rabbitmq_user_permission' do
-      should contain_rabbitmq_user_permissions("#{rabbit_user}@/murano").with({
-        :configure_permission => '.*',
-        :read_permission      => '.*',
-        :write_permission     => '.*',
-      })
-    end
+    it { should contain_file('rabbitmq_config').with(
+      :path  => '/etc/rabbitmq/rabbitmq-murano.config',
+      :owner => 'root',
+      :group => 'root',
+      :mode  => '0644'
+    )}
+
+    it { should contain_file('rabbitmq_config').with_content(/\{rabbit, \[\{tcp_listeners, \[#{rabbit_port}\]\}\]\}/) }
+    it { should contain_file('rabbitmq_config').with_content(/\{inet_dist_listen_min, #{rabbit_cluster_port}\}/) }
+    it { should contain_file('rabbitmq_config').with_content(/\{inet_dist_listen_max, #{rabbit_cluster_port}\}/) }
+
+    it { should contain_file('init_script').with(
+      :path  => "/etc/init.d/#{rabbit_service_name}",
+      :owner => 'root',
+      :group => 'root',
+      :mode  => '0755'
+    )}
+
+    it { should contain_file('init_script').with_content(/export RABBITMQ_NODE_PORT=\"#{rabbit_port}\"/) }
+
+    it { should contain_exec('install_init_script').with(
+      :command => init_install_cmd,
+      :unless  => "test -f /etc/init.d/#{rabbit_service_name}"
+    )}
+
+    it { should contain_service(rabbit_service_name).with(
+      :ensure => 'running',
+      :name   => rabbit_service_name,
+      :enable => true
+    )}
+
+    it { should contain_firewall(rabbit_firewall_rule).with(
+      :dport  => [ rabbit_port ],
+      :proto  => 'tcp',
+      :action => 'accept'
+    )}
+
+    it { should contain_exec('remove_murano_guest').with(
+      :command => "rabbitmqctl -n '#{rabbit_node_name}' delete_user guest",
+      :onlyif  => "rabbitmqctl -n '#{rabbit_node_name}' list_users | grep -qE '^guest\\s*\\['"
+    )}
+
+    it { should contain_exec('create_murano_user').with(
+      :command => "rabbitmqctl -n '#{rabbit_node_name}' add_user '#{rabbit_user}' '#{rabbit_password}'",
+      :unless  => "rabbitmqctl -n '#{rabbit_node_name}' list_users | grep -qE '^#{rabbit_user}\\s*\\['"
+    )}
+
+    it { should contain_exec('create_murano_vhost').with(
+      :command => "rabbitmqctl -n '#{rabbit_node_name}' add_vhost '#{rabbit_vhost}'",
+      :unless  => "rabbitmqctl -n '#{rabbit_node_name}' list_vhosts | grep -qE '^#{rabbit_vhost}$'"
+    )}
+
+    it { should contain_exec('set_murano_user_permissions').with(
+      :command => "rabbitmqctl -n '#{rabbit_node_name}' set_permissions -p '#{rabbit_vhost}' '#{rabbit_user}' '.*' '.*' '.*'"
+    )}
   end
 
   test_ubuntu_and_centos manifest
