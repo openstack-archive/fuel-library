89d2a4dae5161bbd60cd10b598f28ef644d4a358
Andrey Shestakov <ashestakov@mirantis.com>
2015-12-11 12:58:03 +0200

    Create separate non-dvr router for baremetal when Neutron DVR enabled
    DVR is not working with Ironic yet, so when DVR is enabled in cluster
    baremetal servers should use separate non-distributed router.
    
    Change-Id: I8819f35ec3167a00a1c443b142d9f04d0cfc644b
    Partial-bug: #1523878
    (cherry picked from commit 7ebc59bb09e9eb54765c57452372f0fabbc7acf4)

--- deployment/puppet/osnailyfacter/modular/openstack-network/routers.pp
+++ deployment/puppet/osnailyfacter/modular/openstack-network/routers.pp
@@ -5,6 +5,7 @@ $neutron_advanced_config  = hiera_hash('neutron_advanced_configuration', { })
 # In case of L3 HA enabled this task must be executed on a post-deployment stage.
 # as HA routers can't be created when less then 2 L3 agents are available (Neutron limitation)
 $l3_ha                    = pick($neutron_advanced_config['neutron_l3_ha'], false)
+$dvr                      = pick($neutron_advanced_config['neutron_dvr'], false)
 $neutron_controller_roles = hiera('neutron_controller_roles', ['controller', 'primary-controller'])
 $controllers_num          = size(get_nodes_hash_by_roles(hiera_hash('network_metadata'), $neutron_controller_roles))
 
@@ -16,6 +17,7 @@ if $use_neutron {
   $floating_net          = try_get_value($neutron_config, 'default_floating_net', 'net04_ext')
   $private_net           = try_get_value($neutron_config, 'default_private_net', 'net04')
   $default_router        = try_get_value($neutron_config, 'default_router', 'router04')
+  $baremetal_router      = try_get_value($neutron_config, 'baremetal_router', 'baremetal')
   $nets                  = $neutron_config['predefined_networks']
 
   if ($l3_ha) and ($controllers_num < 2) {
@@ -33,9 +35,22 @@ if $use_neutron {
     }
 
     if has_key($nets, 'baremetal') {
-      neutron_router_interface { "${default_router}:baremetal__subnet":
+      if $dvr {
+        neutron_router { $baremetal_router:
+          ensure               => 'present',
+          gateway_network_name => $floating_net,
+          name                 => $baremetal_router,
+          tenant_name          => $keystone_admin_tenant,
+          distributed          => false,
+        } ->
+        neutron_router_interface { "${baremetal_router}:baremetal__subnet":
+          ensure  => 'present',
+        }
+      } else {
+        neutron_router_interface { "${default_router}:baremetal__subnet":
           ensure  => 'present',
           require => Neutron_router[$default_router]
+        }
       }
     }
   }
