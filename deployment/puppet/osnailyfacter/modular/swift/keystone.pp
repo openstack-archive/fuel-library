ce945f8de1a771cc55c8aeb96428e4ea958fdee8
Vladimir Kuklin <vkuklin@mirantis.com>
2015-12-11 23:16:14 +0300

    Specify openstack services cross-deps explicitly [WIP]
    In order to avoid race condition, introduce
    explicit cross-dependencies for openstack services
    
    Change-Id: Ib68d0e46f915578ad8513cd5a0b97f4658d4e078

--- deployment/puppet/osnailyfacter/modular/swift/keystone.pp
+++ deployment/puppet/osnailyfacter/modular/swift/keystone.pp
@@ -23,6 +23,9 @@ $configure_endpoint = pick($swift_hash['configure_endpoint'], true)
 $service_name       = pick($swift_hash['service_name'], 'swift')
 $tenant             = pick($swift_hash['tenant'], 'services')
 
+$backends_to_wait    = pick(hiera('backends_to_wait',['keystone-1','keystone-2']))
+$service_endpoint    = hiera('service_endpoint')
+
 validate_string($public_address)
 validate_string($password)
 
@@ -35,6 +38,12 @@ $public_url_s3       = "${public_protocol}://${public_address}:8080"
 $internal_url_s3     = "${internal_protocol}://${internal_address}:8080"
 $admin_url_s3        = "${admin_protocol}://${admin_address}:8080"
 
+$haproxy_stats_url = "http://${service_endpoint}:10000/;csv"
+
+class {'::osnailyfacter::wait_for_backend':
+  backends_list => $backends_to_wait,
+  url           => $haproxy_stats_url
+}->
 class { '::swift::keystone::auth':
   password           => $password,
   auth_name          => $auth_name,
