8bb01352311c52522d80679d71158470fcc32f6e
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-12-11 18:29:26 +0300

    Rewrite post test for ssl keys saving task
    Change-Id: I4fa25b87c4a5f31a92a266c3b31fba6698f13807
    Closes-Bug: #1523465

--- deployment/puppet/osnailyfacter/modular/ssl/ssl_keys_saving_post.rb
+++ deployment/puppet/osnailyfacter/modular/ssl/ssl_keys_saving_post.rb
@@ -2,13 +2,14 @@ require File.join File.dirname(__FILE__), '../test_common.rb'
 
 class SslKeysSavingPostTest < Test::Unit::TestCase
 
-  def has_public_ssl?
-    TestCommon::Settings.lookup 'public_ssl'
+  def public_ssl
+    ssl_hash = TestCommon::Settings.lookup 'public_ssl'
+    ssl_hash['horizon'] or ssl_hash['services']
   end
 
   def test_ssl_keys_availability
-    return unless has_public_ssl
-    assert File.file?('/var/lib/astute/haproxy/public_haproxy.pem'), 'No public keypair saved!'
+    assert File.file?('/var/lib/astute/haproxy/public_haproxy.pem'), 'No public keypair saved!' unless not public_ssl
+    assert !File.file?('/var/lib/astute/haproxy/public_haproxy.pem'), 'Keypair exist but should not!' unless public_ssl
   end
 
 end
