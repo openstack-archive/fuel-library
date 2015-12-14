fd2bf0ac464690bdf7029975fb8cc997b8255374
Michael Polenchuk <mpolenchuk@mirantis.com>
2015-12-14 17:15:53 +0300

    Enable X-Forwarded-For for RadosGW service
    Enable insertion of the X-Forwarded-For header to requests
    sent to RadosGW servers. Added "httpclose" on server side to ensure
    that every request will be rewritten and not only the first one of
    each session.
    
    Co-authored-by: Matthew Mosesohn <mmosesohn@mirantis.com>
    Change-Id: Ieed9d2cc7b06ba2b864b2751b76dce010178be65
    Closes-Bug: #1523906
    Closes-Bug: #1523895

--- deployment/puppet/openstack/spec/classes/openstack_ha_radosgw_spec.rb
+++ deployment/puppet/openstack/spec/classes/openstack_ha_radosgw_spec.rb
@@ -1,18 +1,32 @@
 require 'spec_helper'
 
   describe 'openstack::ha::radosgw' do
-    let(:params) { {:internal_virtual_ip  => '127.0.0.1',
-                    :ipaddresses          => ['127.0.0.2', '127.0.0.3'],
-                    :public_virtual_ip    => '192.168.0.1',
-                    :baremetal_virtual_ip => '192.168.0.2',
-                    :server_names         => ['node-1', 'node-2'],
-                    :public_ssl           => true,
-                    :public_ssl_path      => '/var/lib/fuel/haproxy/public_radosgw.pem',
-                 } }
-    let(:facts) { {:kernel => 'Linux',
-                   :concat_basedir => '/var/lib/puppet/concat',
-                   :fqdn           => 'some.host.tld'
-                } }
+    let :params do
+      {
+        :internal_virtual_ip  => '127.0.0.1',
+        :ipaddresses          => ['127.0.0.2', '127.0.0.3'],
+        :public_virtual_ip    => '192.168.0.1',
+        :baremetal_virtual_ip => '192.168.0.2',
+        :server_names         => ['node-1', 'node-2'],
+        :public_ssl           => true,
+        :public_ssl_path      => '/var/lib/fuel/haproxy/public_radosgw.pem',
+      }
+    end
+
+    let :facts do
+      {
+        :kernel         => 'Linux',
+        :concat_basedir => '/var/lib/puppet/concat',
+        :fqdn           => 'some.host.tld'
+      }
+    end
+
+    let :haproxy_config_opts do
+      {
+        'option'       => ['httplog', 'httpchk HEAD /', 'http-server-close', 'forwardfor'],
+        'http-request' => 'set-header X-Forwarded-Proto https if { ssl_fc }',
+      }
+    end
 
     it "should properly configure radosgw haproxy based on ssl" do
       should contain_openstack__ha__haproxy_service('radosgw').with(
@@ -22,10 +36,7 @@ require 'spec_helper'
         'public'                 => true,
         'public_ssl'             => true,
         'public_ssl_path'        => '/var/lib/fuel/haproxy/public_radosgw.pem',
-        'haproxy_config_options' => {
-          'option'       => ['httplog', 'httpchk GET /'],
-          'http-request' => 'set-header X-Forwarded-Proto https if { ssl_fc }',
-        },
+        'haproxy_config_options' => haproxy_config_opts,
       )
     end
 
@@ -36,10 +47,7 @@ require 'spec_helper'
         'balancermember_port'    => 6780,
         'public_virtual_ip'      => false,
         'internal_virtual_ip'    => '192.168.0.2',
-        'haproxy_config_options' => {
-          'option'       => ['httplog', 'httpchk GET /'],
-          'http-request' => 'set-header X-Forwarded-Proto https if { ssl_fc }',
-        },
+        'haproxy_config_options' => haproxy_config_opts,
       )
     end
   end
