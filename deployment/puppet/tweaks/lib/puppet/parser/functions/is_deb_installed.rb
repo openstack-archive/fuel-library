e9e78aefa614d4d1c44d445c5dc520fc93eab9fd
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-12-15 14:46:07 +0100

    Fix idempotence of tweaks::ubuntu_service_override
    To achieve that, the commit adds custom function named is_deb_installed,
    which uses dpkg-query to check the package existence. No service
    override magic happens if package is already installed.
    
    Change-Id: I771a3f80808edd104f7ff1dc2dfd9e130f7b64f7
    Closes-Bug: 1524327

---
+++ deployment/puppet/tweaks/lib/puppet/parser/functions/is_deb_installed.rb
@@ -0,0 +1,17 @@
+module Puppet::Parser::Functions
+  newfunction(:is_deb_installed,
+              :type => :rvalue,
+              :arity => 1,
+  ) do |args|
+    osfamily = Facter.value(:osfamily)
+
+    raise Puppet::ParseError, "is_deb_installed(): Only Debian-based systems are supported" unless osfamily == 'Debian'
+    raise Puppet::ParseError, "is_deb_installed(): Wrong number of arguments (#{args.size} given, 1 expected" unless args.size == 1
+
+    pkgname = args[0]
+    command = "dpkg-query -l '#{pkgname}'"
+    query = Puppet::Util::Execution.execute(command)
+
+    query.exitstatus ? 'true' : 'false'
+  end
+end
