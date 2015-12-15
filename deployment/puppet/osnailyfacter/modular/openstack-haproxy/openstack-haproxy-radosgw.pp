1c7acce83afbd470c22b4be894469301228855d1
Maksim Malchuk <mmalchuk@mirantis.com>
2015-12-16 00:23:02 +0300

    Remove hardcoded network roles from HAproxy manifests
    This commit removed some hardcoded network roles from HAproxy manifests
    and replaces them with globals. Also it fixes hiera override functionality
    for these globals.
    
    Change-Id: Ia6006b19f366cec8ac686e19e7b921e95051c1ac
    Closes-Bug: #1505604

--- deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-radosgw.pp
+++ deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-radosgw.pp
@@ -1,13 +1,12 @@
 notice('MODULAR: openstack-haproxy-radosgw.pp')
 
-$network_metadata = hiera_hash('network_metadata')
 $storage_hash     = hiera_hash('storage', {})
-$public_ssl_hash  = hiera('public_ssl')
+
+$public_ssl_hash  = hiera_hash('public_ssl')
 $ssl_hash         = hiera_hash('use_ssl', {})
 
 $public_ssl       = get_ssl_property($ssl_hash, $public_ssl_hash, 'radosgw', 'public', 'usage', false)
 $public_ssl_path  = get_ssl_property($ssl_hash, $public_ssl_hash, 'radosgw', 'public', 'path', [''])
-$ironic_hash      = hiera_hash('ironic', {})
 
 if !($storage_hash['images_ceph'] and $storage_hash['objects_ceph']) and !$storage_hash['images_vcenter'] {
   $use_swift = true
@@ -22,12 +21,16 @@ if !($use_swift) and ($storage_hash['objects_ceph']) {
 
 if $use_radosgw {
   $rgw_address_map     = get_node_to_ipaddr_map_by_network_role(hiera_hash('ceph_rgw_nodes'), 'ceph/radosgw')
+
   $server_names        = hiera_array('radosgw_server_names', keys($rgw_address_map))
   $ipaddresses         = hiera_array('radosgw_ipaddresses', values($rgw_address_map))
   $public_virtual_ip   = hiera('public_vip')
   $internal_virtual_ip = hiera('management_vip')
 
+  $ironic_hash         = hiera_hash('ironic', {})
+
   if $ironic_hash['enabled'] {
+    $network_metadata     = hiera_hash('network_metadata')
     $baremetal_virtual_ip = $network_metadata['vips']['baremetal']['ipaddr']
   }
 
