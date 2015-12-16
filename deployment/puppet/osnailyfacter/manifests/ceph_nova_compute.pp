7226c885572d2ae726414d3af41e4d89b8fe092a
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-12-16 23:18:34 +0200

    Moving to upstream ceph
    Remove fuel-ceph module
    Add upstream puppet-ceph instead
    Add new function for converting osd_devices_list to hash
    Add noop tests
    
    Related-Blueprint: fuel-upstream-ceph
    Change-Id: I75aa14cbc20cc22df38ab9193505af221d95e1af

--- deployment/puppet/osnailyfacter/manifests/ceph_nova_compute.pp
+++ deployment/puppet/osnailyfacter/manifests/ceph_nova_compute.pp
@@ -1,16 +1,15 @@
 # configure the nova_compute parts if present
-class ceph::nova_compute (
-  $rbd_secret_uuid     = $::ceph::rbd_secret_uuid,
-  $user                = $::ceph::compute_user,
-  $compute_pool        = $::ceph::compute_pool,
+class osnailyfacter::ceph_nova_compute (
+  $rbd_secret_uuid     = 'a5d0dd94-57c4-ae55-ffe0-7e3732a24455',
+  $user                = 'compute',
+  $compute_pool        = 'compute',
   $secret_xml          = '/root/.secret_attrs.xml',
 ) {
 
   include ::nova::params
 
   file { $secret_xml:
-    mode    => '0400',
-    content => template('ceph/secret.erb')
+    content => template('osnailyfacter/ceph_secret.erb')
   }
 
   ensure_resource('service', 'libvirt', {
@@ -23,14 +22,16 @@ class ceph::nova_compute (
     command => "virsh secret-define --file ${secret_xml} && \
       virsh secret-set-value --secret ${rbd_secret_uuid} \
       --base64 $(ceph auth get-key client.${user})",
+    path   => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     unless => "virsh secret-list | fgrep -qw ${rbd_secret_uuid}",
   }
 
   nova_config {
-    'libvirt/rbd_secret_uuid':          value => $rbd_secret_uuid;
-    'libvirt/rbd_user':                 value => $user;
+    'libvirt/rbd_secret_uuid': value => $rbd_secret_uuid;
+    'libvirt/rbd_user':        value => $user;
   }
 
   File[$secret_xml] ->
-  Service['libvirt'] -> Exec['Set Ceph RBD secret for Nova']
+  Service['libvirt'] ->
+  Exec['Set Ceph RBD secret for Nova']
 }
