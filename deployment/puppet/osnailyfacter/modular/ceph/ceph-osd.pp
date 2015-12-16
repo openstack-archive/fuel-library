4be0e04c6a6fa20f6207ff318b84f9f415d9d982
Alex Schultz <aschultz@mirantis.com>
2015-12-16 11:07:22 -0700

    Remove osd debug notice
    This change removes notices that print out the osd device list.  As they
    are debug statements, this change removes them.
    
    Change-Id: I2dddf653a22cba35ea41f1fde63fce32c6e675e7
    Closes-Bug: #1526902

--- deployment/puppet/osnailyfacter/modular/ceph/ceph-osd.pp
+++ deployment/puppet/osnailyfacter/modular/ceph/ceph-osd.pp
@@ -77,10 +77,6 @@ if $ceph_tuning_settings != {} {
   File<| title == '/root/ceph.conf' |> -> Ceph_conf <||>
 }
 
-$osd_devices = split($::osd_devices_list, ' ')
-#Class Ceph is already defined so it will do it's thing.
-notify {"ceph_osd: ${osd_devices}": }
-notify {"osd_devices:  ${::osd_devices_list}": }
 # TODO(bogdando) add monit ceph-osd services monitoring, if required
 
 #################################################################
