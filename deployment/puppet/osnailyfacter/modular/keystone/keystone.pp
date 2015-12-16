5482fe47af12043cba2611ba1fdcd2fd780f8c0e
Dmitry Nikishov <dnikishov@mirantis.com>
2015-12-16 11:50:39 -0600

    Create additional openrc for a non-root accounts
    Blueprint: fuel-nonroot-openstack-nodes
    Depends-On: I69831fe0327ef9ac55bed99301d2c3732b87ed88
    
    Change-Id: If48c34037ea4d678a7c343f523d8552b0b376e90

--- deployment/puppet/osnailyfacter/modular/keystone/keystone.pp
+++ deployment/puppet/osnailyfacter/modular/keystone/keystone.pp
@@ -114,6 +114,11 @@ $database_idle_timeout  = '3600'
 
 $external_lb = hiera('external_lb', false)
 
+$os_user_name     = hiera('os_user_name', 'fueladmin')
+$os_user_homedir  = hiera('os_user_homedir', '/home/fueladmin')
+$svc_user_name    = hiera('svc_user_name', 'fuel')
+$svc_user_homedir = hiera('svc_user_homedir', '/var/lib/fuel')
+
 ###############################################################################
 
 ####### KEYSTONE ###########
@@ -201,12 +206,34 @@ class { 'keystone::roles::admin':
   admin_tenant => $admin_tenant,
 }
 
-class { 'openstack::auth_file':
+openstack::auth_file { "root-openrc":
   admin_user          => $admin_user,
   admin_password      => $admin_password,
   admin_tenant        => $admin_tenant,
   region_name         => $region,
   auth_url            => $auth_url,
+}
+
+openstack::auth_file { "${os_user_name}-openrc":
+  admin_user          => $admin_user,
+  admin_password      => $admin_password,
+  admin_tenant        => $admin_tenant,
+  region_name         => $region,
+  auth_url            => $auth_url,
+  owner               => $os_user_name,
+  group               => $os_user_name,
+  path                => $os_user_homedir,
+}
+
+openstack::auth_file { "${svc_user_name}-openrc":
+  admin_user          => $admin_user,
+  admin_password      => $admin_password,
+  admin_tenant        => $admin_tenant,
+  region_name         => $region,
+  auth_url            => $auth_url,
+  owner               => $svc_user_name,
+  group               => $svc_user_name,
+  path                => $svc_user_homedir,
 }
 
 # Get paste.ini source
@@ -227,7 +254,7 @@ exec { 'add_admin_token_auth_middleware':
 Exec['add_admin_token_auth_middleware'] ->
 Exec <| title == 'keystone-manage db_sync' |> ->
 Class['keystone::roles::admin'] ->
-Class['openstack::auth_file']
+Openstack::Auth_file <||>
 
 $haproxy_stats_url = "http://${service_endpoint}:10000/;csv"
 
