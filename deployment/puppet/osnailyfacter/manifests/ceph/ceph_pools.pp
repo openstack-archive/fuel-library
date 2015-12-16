212978a80bfe387669c36765bba7d8115daa20c1
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-12-16 23:18:34 +0200

    Moving to upstream ceph
    Remove fuel-ceph module
    Add upstream puppet-ceph instead
    Add new function for converting osd_devices_list to hash
    Add noop tests
    
    Related-Blueprint: fuel-upstream-ceph
    Change-Id: I75aa14cbc20cc22df38ab9193505af221d95e1af

--- deployment/puppet/osnailyfacter/manifests/ceph/ceph_pools.pp
+++ deployment/puppet/osnailyfacter/manifests/ceph/ceph_pools.pp
@@ -1,52 +1,65 @@
 class osnailyfacter::ceph::ceph_pools {
 
-  notice('MODULAR: ceph/ceph_pools.pp')
+  notice('MODULAR: ceph/ceph_pools')
 
-  $storage_hash             = hiera_hash('storage', {})
-  $osd_pool_default_pg_num  = $storage_hash['pg_num']
-  $osd_pool_default_pgp_num = $storage_hash['pg_num']
-  # Cinder settings
-  $cinder_user              = 'volumes'
-  $cinder_pool              = 'volumes'
-  # Cinder Backup settings
-  $cinder_backup_user       = 'backups'
-  $cinder_backup_pool       = 'backups'
-  # Glance settings
-  $glance_user              = 'images'
-  $glance_pool              = 'images'
+  $storage_hash       = hiera('storage', {})
+  $fsid               = pick($storage_hash['fsid'], '066F558C-6789-4A93-AAF1-5AF1BA01A3AD')
+  $mon_key            = pick($storage_hash['mon_key'], 'AQDesGZSsC7KJBAAw+W/Z4eGSQGAIbxWjxjvfw==')
+  $cinder_user        = 'volumes'
+  $cinder_pool        = 'volumes'
+  $cinder_backup_user = 'backups'
+  $cinder_backup_pool = 'backups'
+  $glance_user        = 'images'
+  $glance_pool        = 'images'
 
-  Exec { path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    cwd     => '/root',
+  class {'ceph':
+    fsid => $fsid
   }
 
   $per_pool_pg_nums = $storage_hash['per_pool_pg_nums']
 
-  # DO NOT SPLIT ceph auth command lines! See http://tracker.ceph.com/issues/3279
-  ceph::pool {$glance_pool:
-    user          => $glance_user,
-    acl           => "mon 'allow r' osd 'allow class-read object_prefix rbd_children, allow rwx pool=${glance_pool}'",
-    keyring_owner => 'glance',
-    pg_num        => pick($per_pool_pg_nums[$glance_pool], '256'),
-    pgp_num       => pick($per_pool_pg_nums[$glance_pool], '256'),
+# DO NOT SPLIT ceph auth command lines! See http://tracker.ceph.com/issues/3279
+  ceph::pool { $glance_pool:
+    pg_num  => pick($per_pool_pg_nums[$glance_pool], '256'),
+    pgp_num => pick($per_pool_pg_nums[$glance_pool], '256'),
   }
 
-  ceph::pool {$cinder_pool:
-    user          => $cinder_user,
-    acl           => "mon 'allow r' osd 'allow class-read object_prefix rbd_children, allow rwx pool=${cinder_pool}, allow rx pool=${glance_pool}'",
-    keyring_owner => 'cinder',
-    pg_num        => pick($per_pool_pg_nums[$cinder_pool], '2048'),
-    pgp_num       => pick($per_pool_pg_nums[$cinder_pool], '2048'),
+  ceph::key { "client.${glance_user}":
+    secret  => $mon_key,
+    user    => 'glance',
+    group   => 'glance',
+    cap_mon => 'allow r',
+    cap_osd => "allow class-read object_prefix rbd_children, allow rwx pool=${glance_pool}",
+    inject  => true,
   }
 
-  ceph::pool {$cinder_backup_pool:
-    user          => $cinder_backup_user,
-    acl           => "mon 'allow r' osd 'allow class-read object_prefix rbd_children, allow rwx pool=${cinder_backup_pool}, allow rwx pool=${cinder_pool}'",
-    keyring_owner => 'cinder',
-    pg_num        => pick($per_pool_pg_nums[$cinder_backup_pool], '512'),
-    pgp_num       => pick($per_pool_pg_nums[$cinder_backup_pool], '512'),
+  ceph::pool { $cinder_pool:
+    pg_num  => pick($per_pool_pg_nums[$cinder_pool], '256'),
+    pgp_num => pick($per_pool_pg_nums[$cinder_pool], '256'),
   }
 
-  Ceph::Pool[$glance_pool] -> Ceph::Pool[$cinder_pool] -> Ceph::Pool[$cinder_backup_pool]
+  ceph::key { "client.${cinder_user}":
+    secret  => $mon_key,
+    user    => 'cinder',
+    group   => 'cinder',
+    cap_mon => 'allow r',
+    cap_osd => "allow class-read object_prefix rbd_children, allow rwx pool=${cinder_pool}, allow rx pool=${glance_pool}",
+    inject  => true,
+  }
+
+  ceph::pool { $cinder_backup_pool:
+    pg_num  => pick($per_pool_pg_nums[$cinder_backup_pool], '256'),
+    pgp_num => pick($per_pool_pg_nums[$cinder_backup_pool], '256'),
+  }
+
+  ceph::key { "client.${cinder_backup_user}":
+    secret  => $mon_key,
+    user    => 'cinder',
+    group   => 'cinder',
+    cap_mon => 'allow r',
+    cap_osd => "allow class-read object_prefix rbd_children, allow rwx pool=${cinder_backup_pool}, allow rwx pool=${cinder_pool}",
+    inject  => true,
+  }
 
   if ($storage_hash['volumes_ceph']) {
     include ::cinder::params
