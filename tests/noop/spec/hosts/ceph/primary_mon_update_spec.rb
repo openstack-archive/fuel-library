d46da3bc6ceff2e34691ffb9c8e0ed55ec6a3ea9
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-12-16 23:18:34 +0200

    Moving to upstream ceph
    Remove fuel-ceph module
    Add upstream puppet-ceph instead
    Add new function for converting osd_devices_list to hash
    Add noop tests
    
    Related-Blueprint: fuel-upstream-ceph
    Change-Id: I75aa14cbc20cc22df38ab9193505af221d95e1af

---
+++ tests/noop/spec/hosts/ceph/primary_mon_update_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'ceph/primary_mon_update.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+
+    ceph_monitor_nodes = Noop.hiera_hash('ceph_monitor_nodes')
+    mon_address_map = Noop.puppet_function 'get_node_to_ipaddr_map_by_network_role', ceph_monitor_nodes, 'ceph/public'
+    mon_ips = mon_address_map.values.join(',')
+    mon_hosts = mon_address_map.keys.join(',')
+
+    storage_hash = Noop.hiera 'storage'
+
+    if (storage_hash['volumes_ceph'] or
+        storage_hash['images_ceph'] or
+        storage_hash['objects_ceph'] or
+        storage_hash['ephemeral_ceph']
+       )
+
+      it 'should wait for ceph to be ready' do
+        should contain_exec('Wait for Ceph quorum')
+      end
+
+      it 'should add parameters to ceph.conf' do
+        should contain_ceph_config('global/mon_host').with(:value => mon_ips)
+        should contain_ceph_config('global/mon_initial_members').with(:value => mon_hosts)
+      end
+
+      it 'should reload Ceph' do
+        should contain_exec('reload Ceph for HA')
+      end
+    end
+  end
+  test_ubuntu_and_centos manifest
+
+end
+
