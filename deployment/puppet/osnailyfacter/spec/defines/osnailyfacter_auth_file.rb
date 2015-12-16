6dcd5b2c86509b99cfe1439ab90b25f089651739
Dmitry Nikishov <dnikishov@mirantis.com>
2015-12-16 11:50:39 -0600

    Create additional openrc for a non-root accounts
    Closes-Bug: #1555126
    Change-Id: If48c34037ea4d678a7c343f523d8552b0b376e90

---
+++ deployment/puppet/osnailyfacter/spec/defines/osnailyfacter_auth_file.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe 'osnailyfacter::auth_file' do
+
+  context 'with defaults' do
+    let (:title) { '/root/openrc' }
+
+    it 'should include openrc file' do
+      should contain_file('/root/openrc')
+    end
+  end
+
+  context 'with non-root user' do
+    let (:title) { '/home/fueladmin/openrc' }
+    let (:owner) { 'fueladmin' }
+    let (:group) { 'fueladmin' }
+
+    it 'should include openrc file for a non-root user with correct permissions' do
+      is_expected.to contain_file('/home/fueladmin/openrc').with(
+        'owner' => 'fueladmin',
+        'group' => 'fueladmin'
+      )
+    end
+  end
+
+end
+
