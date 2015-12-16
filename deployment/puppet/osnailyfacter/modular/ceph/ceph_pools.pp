fb86ad74bbe87e2ee9202d6d77c9d039ed40c76a
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-12-16 23:18:34 +0200

    [WIP] Moving to upstream ceph
    Need-in-astutes:
    admin_key: 'AQCTg71RsNIHORAAW+O6FCMZWBjmVfMIPk3MhQ=='
    mon_key: 'AQDesGZSsC7KJBAAw+W/Z4eGSQGAIbxWjxjvfw=='
    bootstrap_osd_key: 'AQABsWZSgEDmJhAAkAGSOOAJwrMHrM5Pz5On1A=='
    fsid: '066F558C-6789-4A93-AAF1-5AF1BA01A3AD'
    
    Change-Id: I75aa14cbc20cc22df38ab9193505af221d95e1af

--- deployment/puppet/osnailyfacter/modular/ceph/ceph_pools.pp
+++ deployment/puppet/osnailyfacter/modular/ceph/ceph_pools.pp
@@ -1,6 +1,7 @@
 notice('MODULAR: ceph/ceph_pools')
 
 $storage_hash             = hiera('storage', {})
+$secret                   = hiera('mon_key')
 $osd_pool_default_pg_num  = $storage_hash['pg_num']
 $osd_pool_default_pgp_num = $storage_hash['pg_num']
 # Cinder settings
@@ -13,6 +14,7 @@ $cinder_backup_pool       = 'backups'
 $glance_user              = 'images'
 $glance_pool              = 'images'
 
+package {'ceph':}
 
 Exec { path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
   cwd     => '/root',
@@ -20,27 +22,39 @@ Exec { path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 
 # DO NOT SPLIT ceph auth command lines! See http://tracker.ceph.com/issues/3279
 ceph::pool {$glance_pool:
-  user          => $glance_user,
-  acl           => "mon 'allow r' osd 'allow class-read object_prefix rbd_children, allow rwx pool=${glance_pool}'",
-  keyring_owner => 'glance',
   pg_num        => $osd_pool_default_pg_num,
   pgp_num       => $osd_pool_default_pg_num,
+} 
+
+ceph::key { "client.${glance_pool}":
+  secret  => $secret,
+  cap_mon => 'allow r',
+  cap_osd => "allow class-read object_prefix rbd_children, allow rwx pool=${glance_pool}",
+  inject  => true,
 }
 
 ceph::pool {$cinder_pool:
-  user          => $cinder_user,
-  acl           => "mon 'allow r' osd 'allow class-read object_prefix rbd_children, allow rwx pool=${cinder_pool}, allow rx pool=${glance_pool}'",
-  keyring_owner => 'cinder',
   pg_num        => $osd_pool_default_pg_num,
   pgp_num       => $osd_pool_default_pg_num,
 }
 
+ceph::key { "client.${cinder_pool}":
+  secret  => $secret,
+  cap_mon => 'allow r',
+  cap_osd => "allow class-read object_prefix rbd_children, allow rwx pool=${cinder_pool}, allow rx pool=${glance_pool}",
+  inject  => true,
+}
+
 ceph::pool {$cinder_backup_pool:
-  user          => $cinder_backup_user,
-  acl           => "mon 'allow r' osd 'allow class-read object_prefix rbd_children, allow rwx pool=${cinder_backup_pool}, allow rx pool=${cinder_pool}'",
-  keyring_owner => 'cinder',
   pg_num        => $osd_pool_default_pg_num,
   pgp_num       => $osd_pool_default_pg_num,
+}
+
+ceph::key { "client.${cinder_backup_pool}":
+  secret  => $secret,
+  cap_mon => 'allow r',
+  cap_osd => "allow class-read object_prefix rbd_children, allow rwx pool=${cinder_backup_pool}, allow rx pool=${cinder_pool}",
+  inject  => true,
 }
 
 Ceph::Pool[$glance_pool] -> Ceph::Pool[$cinder_pool] -> Ceph::Pool[$cinder_backup_pool]
