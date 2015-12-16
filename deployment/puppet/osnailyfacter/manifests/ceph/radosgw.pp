0d3350d6594bd0ab8ce493de0b06327bfe95baca
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-12-16 23:18:34 +0200

    Moving to upstream ceph
    Remove fuel-ceph module
    Add upstream puppet-ceph instead
    Add new function for converting osd_devices_list to hash
    Add noop tests
    
    Related-Blueprint: fuel-upstream-ceph
    Change-Id: I75aa14cbc20cc22df38ab9193505af221d95e1af

--- deployment/puppet/osnailyfacter/manifests/ceph/radosgw.pp
+++ deployment/puppet/osnailyfacter/manifests/ceph/radosgw.pp
@@ -2,78 +2,105 @@ class osnailyfacter::ceph::radosgw {
 
   notice('MODULAR: ceph/radosgw.pp')
 
-  $storage_hash     = hiera_hash('storage', {})
-  $keystone_hash    = hiera_hash('keystone', {})
-  $management_vip   = hiera('management_vip')
-  $service_endpoint = hiera('service_endpoint')
-  $radosgw_large_pool_name = '.rgw'
-  $mon_address_map  = get_node_to_ipaddr_map_by_network_role(hiera_hash('ceph_monitor_nodes'), 'ceph/public')
-  $external_lb      = hiera('external_lb', false)
-  $ssl_hash         = hiera_hash('use_ssl', {})
+  $gateway_name                     = 'radosgw.gateway'
+  $storage_hash                     = hiera('storage', {})
+  $radosgw_key                      = pick($storage_hash['radosgw_key'], 'AQCTg71RsNIHORAAW+O6FCMZWBjmVfMIPk3MhQ==')
+  $fsid                             = pick($storage_hash['fsid'], '066F558C-6789-4A93-AAF1-5AF1BA01A3AD')
+  $rgw_log_file                     = '/var/log/ceph/radosgw.log'
+  $use_syslog                       = hiera('use_syslog', true)
+  $rgw_large_pool_name              = '.rgw'
+  $rgw_large_pool_pg_nums           = pick($storage_hash['per_pool_pg_nums'][$rgw_large_pool_name], '512')
+  $keystone_hash                    = hiera('keystone', {})
+  $rgw_keystone_accepted_roles      = pick($storage_hash['radosgw_keystone_accepted_roles'], '_member_, Member, admin, swiftoperator')
+  $rgw_keystone_revocation_interval = '1000000'
+  $rgw_keystone_token_cache_size    = '10'
+  $service_endpoint                 = hiera('service_endpoint')
+  $management_vip                   = hiera('management_vip')
+  $public_vip                       = hiera('public_vip')
+
+  $ssl_hash                = hiera_hash('use_ssl', {})
   $admin_identity_protocol = get_ssl_property($ssl_hash, {}, 'keystone', 'admin', 'protocol', 'http')
   $admin_identity_address  = get_ssl_property($ssl_hash, {}, 'keystone', 'admin', 'hostname', [$service_endpoint, $management_vip])
   $admin_identity_url      = "${admin_identity_protocol}://${admin_identity_address}:35357"
 
+  prepare_network_config(hiera_hash('network_scheme'))
+  $ceph_cluster_network = get_network_role_property('ceph/replication', 'network')
+  $ceph_public_network  = get_network_role_property('ceph/public', 'network')
+
+  $mon_address_map = get_node_to_ipaddr_map_by_network_role(hiera_hash('ceph_monitor_nodes'), 'ceph/public')
+  $mon_ips         = join(values($mon_address_map), ',')
+  $mon_hosts       = join(keys($mon_address_map), ',')
+
   if $storage_hash['objects_ceph'] {
-    $ceph_primary_monitor_node = hiera('ceph_primary_monitor_node')
-    $primary_mons              = keys($ceph_primary_monitor_node)
-    $primary_mon               = $ceph_primary_monitor_node[$primary_mons[0]]['name']
-
-    prepare_network_config(hiera_hash('network_scheme', {}))
-    $ceph_cluster_network = get_network_role_property('ceph/replication', 'network')
-    $ceph_public_network  = get_network_role_property('ceph/public', 'network')
-    $rgw_ip_address       = get_network_role_property('ceph/radosgw', 'ipaddr')
-
-    # Listen directives with host required for ip_based vhosts
-    class { '::osnailyfacter::apache':
-      listen_ports => hiera_array('apache_ports', ['0.0.0.0:80', '0.0.0.0:8888']),
+    ceph::key { "client.${gateway_name}":
+      keyring_path => "/etc/ceph/client.${gateway_name}",
+      secret       => $radosgw_key,
+      cap_mon      => 'allow rw',
+      cap_osd      => 'allow rwx',
+      inject       => true,
     }
 
-    if ($::osfamily == 'Debian'){
-      apache::mod {'rewrite': }
-      apache::mod {'proxy': }
-      apache::mod {'proxy_fcgi': }
+    class { 'ceph':
+      fsid => $fsid,
     }
+
     include ::tweaks::apache_wrappers
-
     include ::ceph::params
 
-    class { '::ceph::radosgw':
-      # RadosGW settings
-      rgw_host                         => $::hostname,
-      rgw_ip                           => $rgw_ip_address,
-      rgw_port                         => '6780',
-      swift_endpoint_port              => '8080',
-      rgw_keyring_path                 => '/etc/ceph/keyring.radosgw.gateway',
-      rgw_socket_path                  => '/tmp/radosgw.sock',
-      rgw_frontends                    => 'fastcgi socket_port=9000 socket_host=127.0.0.1',
-      rgw_log_file                     => '/var/log/ceph/radosgw.log',
-      rgw_data                         => '/var/lib/ceph/radosgw',
-      rgw_dns_name                     => "*.${::domain}",
-      rgw_print_continue               => true,
+    #######################################
+    # Ugly hack to support our ceph package
+    #######################################
 
-      #rgw Keystone settings
-      rgw_use_pki                      => false,
-      rgw_use_keystone                 => true,
+    file { '/etc/init/radosgw.conf':
+      ensure  => present,
+      content => template('osnailyfacter/radosgw-init.erb'),
+      before  => Ceph::Rgw[$gateway_name],
+    }
+    #######################################
+
+    ceph::rgw { $gateway_name:
+      frontend_type      => 'apache-proxy-fcgi',
+      rgw_print_continue => true,
+      keyring_path       => "/etc/ceph/client.${gateway_name}",
+      rgw_data           => "/var/lib/ceph/radosgw-${gateway_name}",
+      rgw_dns_name       => "*.${::domain}",
+      log_file           => undef,
+    }
+
+    ceph::rgw::keystone { $gateway_name:
       rgw_keystone_url                 => $admin_identity_url,
       rgw_keystone_admin_token         => $keystone_hash['admin_token'],
-      rgw_keystone_token_cache_size    => '10',
-      rgw_keystone_accepted_roles      => '_member_, Member, admin, swiftoperator',
-      rgw_keystone_revocation_interval => '1000000',
-      rgw_s3_auth_use_keystone         => false,
-      rgw_nss_db_path                  => '/etc/ceph/nss',
-      rgw_large_pool_name              => $radosgw_large_pool_name,
-      rgw_large_pool_pg_nums           => pick($storage_hash['per_pool_pg_nums'][$radosgw_large_pool_name], '512'),
-
-      #rgw Log settings
-      use_syslog                       => hiera('use_syslog', true),
-      syslog_facility                  => hiera('syslog_log_facility_ceph', 'LOG_LOCAL0'),
-      syslog_level                     => hiera('syslog_log_level_ceph', 'info'),
+      rgw_keystone_token_cache_size    => $rgw_keystone_token_cache_size,
+      rgw_keystone_accepted_roles      => $rgw_keystone_accepted_roles,
+      rgw_keystone_revocation_interval => $rgw_keystone_revocation_interval,
     }
 
-    Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-      cwd  => '/root',
+    file { "/var/lib/ceph/radosgw/ceph-${gateway_name}":
+      ensure => directory,
     }
+
+    ceph::rgw::apache_proxy_fcgi { 'radosgw.gateway':
+      docroot              => '/var/www/radosgw',
+      rgw_port             => '6780',
+      apache_purge_configs => false,
+      apache_purge_vhost   => false,
+      custom_apache_ports  => hiera_array('apache_ports', ['0.0.0.0:80']),
+    }
+
+    if ! $use_syslog {
+      ceph_config {
+        'client.radosgw.gateway/log_file':      value => $rgw_log_file;
+        'client.radosgw.gateway/log_to_syslog': value => $use_syslog;
+      }
+    }
+
+    exec { "Create ${rgw_large_pool_name} pool":
+      command => "ceph -n client.radosgw.gateway osd pool create ${rgw_large_pool_name} ${rgw_large_pool_pg_nums} ${rgw_large_pool_pg_nums}",
+      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/bin',
+      unless  => "rados lspools | grep '^${rgw_large_pool_name}$'",
+    }
+
+    Ceph::Key["client.${gateway_name}"] -> Exec["Create ${rgw_large_pool_name} pool"]
   }
 
 }
---
+++ deployment/puppet/osnailyfacter/manifests/ceph_nova_compute.pp
@@ -0,0 +1,47 @@
+# configure the nova_compute parts if present
+class osnailyfacter::ceph_nova_compute (
+  $rbd_secret_uuid     = 'a5d0dd94-57c4-ae55-ffe0-7e3732a24455',
+  $user                = 'compute',
+  $compute_pool        = 'compute',
+  $secret_xml          = '/root/.secret_attrs.xml',
+  $libvirt_images_type = 'rbd',
+) {
+
+  include ::nova::params
+
+  service { $::nova::params::compute_service_name: }
+
+  nova_config {
+    'libvirt/images_type':      value => $libvirt_images_type;
+    'libvirt/inject_key':       value => false;
+    'libvirt/inject_partition': value => '-2';
+    'libvirt/images_rbd_pool':  value => $compute_pool;
+    'libvirt/rbd_secret_uuid':  value => $rbd_secret_uuid;
+    'libvirt/rbd_user':         value => $user;
+  }
+
+  file { $secret_xml:
+    content => template('osnailyfacter/ceph_secret.erb')
+  }
+
+  ensure_resource('service', 'libvirt', {
+    ensure => 'running',
+    name   => $::nova::params::libvirt_service_name,
+  })
+
+  exec {'Set Ceph RBD secret for Nova':
+    # TODO: clean this command up
+    command => "virsh secret-define --file ${secret_xml} && \
+      virsh secret-set-value --secret ${rbd_secret_uuid} \
+      --base64 $(ceph auth get-key client.${user})",
+    path   => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    unless => "virsh secret-list | fgrep -qw ${rbd_secret_uuid}",
+  }
+
+  Nova_config<||> ~>
+  Service[$::nova::params::compute_service_name]
+
+  File[$secret_xml] ->
+  Service['libvirt'] ->
+  Exec['Set Ceph RBD secret for Nova']
+}
