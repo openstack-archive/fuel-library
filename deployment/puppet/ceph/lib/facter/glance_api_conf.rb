206192232c7a702b66d3493b8b5848707dcd5733
Oleksiy Molchanov <omolchanov@mirantis.com>
2015-12-16 23:18:34 +0200

    Moving to upstream ceph
    Remove fuel-ceph module
    Add upstream puppet-ceph instead
    Add new function for converting osd_devices_list to hash
    Add noop tests
    
    Related-Blueprint: fuel-upstream-ceph
    Change-Id: I75aa14cbc20cc22df38ab9193505af221d95e1af

--- deployment/puppet/ceph/lib/facter/glance_api_conf.rb
+++
@@ -1,9 +0,0 @@
-Facter.add("glance_api_conf") do
-
-	setcode do
-
-		File.exists? '/etc/glance/glance-api.conf'
-
-	end
-
-end
--- tests/noop/spec/hosts/ceph/radosgw_spec.rb
+++ tests/noop/spec/hosts/ceph/radosgw_spec.rb
@@ -7,61 +7,83 @@ manifest = 'ceph/radosgw.pp'
 
 describe manifest do
   shared_examples 'catalog' do
-    storage_hash = Noop.hiera_hash 'storage'
-    ceph_monitor_nodes = Noop.hiera 'ceph_monitor_nodes'
+
+    radosgw_enabled   = Noop.hiera_structure('storage/objects_ceph', false)
+    ssl_hash          = Noop.hiera_structure('use_ssl', {})
+    storage_hash      = Noop.hiera_hash 'storage'
+
+    rgw_large_pool_name    = '.rgw'
+    rgw_large_pool_pg_nums = storage_hash['per_pool_pg_nums'][rgw_large_pool_name]
+    rgw_id                 = 'radosgw.gateway'
+    radosgw_auth_key       = "client.#{rgw_id}"
+
+    let(:gateway_name) {
+      'radosgw.gateway'
+    }
+
+    let(:fsid) {
+      Noop.hiera_structure 'storage/fsid', '066F558C-6789-4A93-AAF1-5AF1BA01A3AD'
+    }
 
     let(:ssl_hash) { Noop.hiera_hash 'use_ssl', {} }
 
-    let(:admin_auth_protocol) {
+    let(:service_endpoint) {
+      Noop.hiera_structure 'service_endpoint'
+    }
+
+    let(:admin_identity_protocol) {
       Noop.puppet_function 'get_ssl_property',ssl_hash,{},'keystone',
         'admin','protocol','http'
     }
 
-    let(:admin_auth_address) {
+    let(:admin_identity_address) {
       Noop.puppet_function 'get_ssl_property',ssl_hash,{},'keystone','admin',
       'hostname',
         [Noop.hiera('service_endpoint', Noop.hiera('management_vip'))]
     }
 
-    let(:admin_url) {
-      "#{admin_auth_protocol}://#{admin_auth_address}:35357"
+    let(:admin_identity_url) {
+      "#{admin_identity_protocol}://#{admin_identity_address}:35357"
     }
 
-    if storage_hash['objects_ceph']
-      rgw_large_pool_name = '.rgw'
-      rgw_large_pool_pg_nums = storage_hash['per_pool_pg_nums'][rgw_large_pool_name]
-      rgw_id = 'radosgw.gateway'
-      radosgw_auth_key = "client.#{rgw_id}"
+    let(:radosgw_key) do
+      Noop.hiera_structure 'storage/radosgw_key', 'AQCTg71RsNIHORAAW+O6FCMZWBjmVfMIPk3MhQ=='
+    end
 
-      it 'should configure apache mods' do
-        if facts[:osfamily] == 'Debian'
-          should contain_apache__mod('rewrite')
-          should contain_apache__mod('proxy')
-          should contain_apache__mod('proxy_fcgi')
-        else
-          should contain_apache__mod('rewrite')
-          should_not contain_apache__mod('proxy')
-          should_not contain_apache__mod('proxy_fcgi')
-        end
+    if radosgw_enabled
+      it 'should add radosgw key' do
+        should contain_ceph__key("client.#{gateway_name}").with(
+          'secret'       => radosgw_key,
+          'cap_mon'      => 'allow rw',
+          'cap_osd'      => 'allow rwx',
+          'inject'       => true,
+        )
       end
 
-      it { should contain_class('ceph::radosgw').with(
-           'rgw_frontends'    => 'fastcgi socket_port=9000 socket_host=127.0.0.1',
-           'rgw_keystone_url' => admin_url,
-           )
-        }
-
-      it { should contain_service('httpd').with(
-          :hasrestart => true,
-          :restart    => 'sleep 30 && apachectl graceful || apachectl restart',
+      it 'should deploy ceph' do
+        should contain_class('ceph').with(
+          'fsid' => fsid,
         )
-      }
+      end   
+
+      it 'should contain ceph::rgw' do
+        should contain_ceph__rgw(gateway_name).with(
+          'frontend_type' => 'apache-proxy-fcgi',
+        )
+      end
+
+      it 'should configure radosgw keystone' do
+        should contain_ceph__rgw__keystone(gateway_name).with(
+          'rgw_keystone_url' => admin_identity_url,
+        )
+      end
 
       it { should contain_exec("Create #{rgw_large_pool_name} pool").with(
            :command => "ceph -n #{radosgw_auth_key} osd pool create #{rgw_large_pool_name} #{rgw_large_pool_pg_nums} #{rgw_large_pool_pg_nums}",
            :unless  => "rados lspools | grep '^#{rgw_large_pool_name}$'"
          )
       }
+
     end
   end
 
