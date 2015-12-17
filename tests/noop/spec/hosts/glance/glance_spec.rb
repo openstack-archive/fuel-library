58d369ddbb023463ed7d1d1c9852e75a6f3732a8
Alex Schultz <aschultz@mirantis.com>
2015-12-17 14:38:47 -0700

    Update connection string generation
    This change leverages the os_database_connection function from
    openstacklib to generate the connection strings for our services. As
    part of this change, we are moving the database connection string
    generation to the osnailyfacter modular tasks and updating the
    openstack classes to consume a connection string rather than the
    seperate host, user, password, database name and database type.
    
    Additionally as part of this change, we are updating the mysql
    connection strings to only include read_time=60 if debian based packages
    are used. This is due to the fact that python-pymysql, which is used by
    Ubuntu packages, does not support the read_timeout option as part of the
    connection string.
    
    Change-Id: I45c51a05ba7cf9648b0202cc3c2216d185bd7eda
    Depends-On: I7ef078b76ac6cd7bea42a0ac92b383cefd47167a
    Closes-Bug: #1526938

--- tests/noop/spec/hosts/glance/glance_spec.rb
+++ tests/noop/spec/hosts/glance/glance_spec.rb
@@ -23,11 +23,26 @@ describe manifest do
     else
        pipeline = 'keystone'
     end
+    database_vip = Noop.hiera('database_vip')
+    glance_db_password = Noop.hiera_structure 'glance/db_password', 'glance'
+    glance_db_user = Noop.hiera_structure 'glance/db_user', 'glance'
+    glance_db_name = Noop.hiera_structure 'glance/db_name', 'glance'
 
     it 'should declare glance classes' do
       should contain_class('glance::api').with('pipeline' => pipeline)
       should contain_class('glance::registry').with('sync_db' => primary_controller)
       should contain_class('glance::notify::rabbitmq')
+    end
+
+    it 'should configure the database connection string' do
+        if facts[:os_package_type] == 'debian'
+            extra_params = '?charset=utf8&read_timeout=60'
+        else
+            extra_params = '?charset=utf8'
+        end
+        should contain_class('openstack::glance').with(
+            :db_connection => "mysql://#{glance_db_user}:#{glance_db_password}@#{database_vip}/#{glance_db_name}#{extra_params}"
+        )
     end
 
     it 'should configure glance api config' do
