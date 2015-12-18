16ae4bde95f22a8c8e19ae49dafa9700266196a3
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-12-18 19:23:01 +0300

    Sync firewall test rules with real rules node should have
    Put up-to-date rules to post-test for firewall manifest
    
    Change-Id: I9f41eb2882f22f7c813d8f0e26a640281a6f0249
    Closes-Bug: #1523458

--- deployment/puppet/osnailyfacter/modular/firewall/firewall_post.rb
+++ deployment/puppet/osnailyfacter/modular/firewall/firewall_post.rb
@@ -1,48 +1,75 @@
 require File.join File.dirname(__FILE__), '../test_common.rb'
 
-RULES = [
+COMMON_RULES = [
+    'accept all icmp requests',
     'accept all to lo interface',
-    'accept related established rules',
     'ssh',
+    'iscsi',
+    'ntp-server',
+    'notrack gre',
+    'accept gre',
+    'vxlan_udp_port',
+    ]
+
+CONTROLLER_RULES = [
+    'remote rabbitmq',
+    'remote puppet',
+    'local rabbitmq admin',
+    'reject non-local rabbitmq admin',
+    'allow connections from haproxy namespace',
     'http',
     'mysql',
     'keystone',
     'swift',
     'glance',
     'nova',
+    'nova internal - no ssl',
     'rabbitmq',
-    'memcached tcp',
-    'memcached udp',
+    'memcache tcp',
+    'memcache udp',
     'rsync',
-    'iscsi',
     'neutron',
-    'dns-server',
+    'dns-server udp',
+    'dns-server tcp',
     'dhcp-server',
-    'ntp-server',
     'corosync-input',
     'corosync-output',
+    'pcsd-server',
     'openvswitch db',
-    'nrpe-server',
-    'libvirt',
-    'libvirt migration',
-    'vnc ports',
     'ceilometer',
-    'notrack gre',
-    'accept gre',
-    'drop all other requests',
-    'remote puppet',
-    'remote rabbitmq',
+    'murano-rabbitmq',
+    'heat-api',
+    'heat-api-cfn',
+    'heat-api-cloudwatch',
 ]
 
+COMPUTE_RULES = [
+    'libvirt',
+    'libvirt-migration',
+]
+
+def role
+  TestCommon::Settings.role.to_s
+end
+
 class FirewallPostTest < Test::Unit::TestCase
-  def self.create_tests
-    RULES.each do |rule|
+  def self.create_tests(rules)
+    rules.each do |rule|
       method_name = "test_iptables_have_rule_#{rule.gsub ' ', '_'}"
       define_method method_name do
         assert TestCommon::Network.iptables_rules.include?(rule), "Iptables don't have the '#{rule}' rule!'"
       end
     end
   end
+
+  def self.run_tests
+    create_tests COMMON_RULES
+    if %w(controller primary-controller).include? role
+      create_tests CONTROLLER_RULES
+    elsif role == "compute"
+      create_tests COMPUTE_RULES
+    end
+  end
 end
 
-FirewallPostTest.create_tests
+FirewallPostTest.run_tests
