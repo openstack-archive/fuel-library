28626218554779904708c128df9019b39655731b
Aleksandr Didenko <adidenko@mirantis.com>
2015-12-21 16:31:01 +0100

    Add support for external load-balancers
    Add new hiera configuration key 'external_lb'. If it's set to
    true then we should not configure Haproxy LB on controllers and
    also we should switch 'haproxy_backend_status' to 'http' provider.
    
    We calculate 'external_lb' during 'globals.pp' task - if we have
    no VIPs with 'haproxy' namespace, then it's set to 'true'.
    
    It does not change the default deployment scheme.
    
    Also fixing minor issue with default provider for
    'haproxy_backend_status' type.
    
    Closes-bug: #1522749
    Change-Id: I5539d04d577465bd410494a6c0b7caff18857878

--- deployment/puppet/osnailyfacter/modular/virtual_ips/conntrackd.pp
+++ deployment/puppet/osnailyfacter/modular/virtual_ips/conntrackd.pp
@@ -1,5 +1,6 @@
 notice('MODULAR: conntrackd.pp')
 
+$network_metadata = hiera_hash('network_metadata', {})
 prepare_network_config(hiera('network_scheme', {}))
 $vrouter_name = hiera('vrouter_name', 'pub')
 
@@ -8,72 +9,74 @@ case $operatingsystem {
   Ubuntu: { $conntrackd_package = 'conntrackd' }
 }
 
+# If VIP has namespace set to 'false' or 'undef' then we do not configure
+# it under corosync cluster. So we should not configure colocation with it.
+if $network_metadata['vips']["vrouter_${vrouter_name}"]['namespace'] {
+  ### CONNTRACKD for CentOS 6 doesn't work under namespaces ##
+  if $operatingsystem == 'Ubuntu' {
+    $bind_address = get_network_role_property('mgmt/vip', 'ipaddr')
+    $mgmt_bridge = get_network_role_property('mgmt/vip', 'interface')
 
-### CONNTRACKD for CentOS 6 doesn't work under namespaces ##
+    package { $conntrackd_package:
+      ensure => installed,
+    } ->
 
-if $operatingsystem == 'Ubuntu' {
-  $bind_address = get_network_role_property('mgmt/vip', 'ipaddr')
-  $mgmt_bridge = get_network_role_property('mgmt/vip', 'interface')
+    file { '/etc/conntrackd/conntrackd.conf':
+      content => template('cluster/conntrackd.conf.erb'),
+    } ->
 
-  package { $conntrackd_package:
-    ensure => installed,
-  } ->
-
-  file { '/etc/conntrackd/conntrackd.conf':
-    content => template('cluster/conntrackd.conf.erb'),
-  } ->
-
-  cs_resource {'p_conntrackd':
-    ensure          => present,
-    primitive_class => 'ocf',
-    provided_by     => 'fuel',
-    primitive_type  => 'ns_conntrackd',
-    metadata        => {
-      'migration-threshold' => 'INFINITY',
-      'failure-timeout'     => '180s'
-    },
-    parameters => {
-      'bridge' => $mgmt_bridge,
-    },
-    complex_type => 'master',
-    ms_metadata  => {
-      'notify'          => 'true',
-      'ordered'         => 'false',
-      'interleave'      => 'true',
-      'clone-node-max'  => '1',
-      'master-max'      => '1',
-      'master-node-max' => '1',
-      'target-role'     => 'Master'
-    },
-    operations   => {
-      'monitor'  => {
-      'interval' => '30',
-      'timeout'  => '60'
-    },
-    'monitor:Master' => {
-      'role'         => 'Master',
-      'interval'     => '27',
-      'timeout'      => '60'
+    cs_resource {'p_conntrackd':
+      ensure          => present,
+      primitive_class => 'ocf',
+      provided_by     => 'fuel',
+      primitive_type  => 'ns_conntrackd',
+      metadata        => {
+        'migration-threshold' => 'INFINITY',
+        'failure-timeout'     => '180s'
       },
-    },
+      parameters => {
+        'bridge' => $mgmt_bridge,
+      },
+      complex_type => 'master',
+      ms_metadata  => {
+        'notify'          => 'true',
+        'ordered'         => 'false',
+        'interleave'      => 'true',
+        'clone-node-max'  => '1',
+        'master-max'      => '1',
+        'master-node-max' => '1',
+        'target-role'     => 'Master'
+      },
+      operations   => {
+        'monitor'  => {
+        'interval' => '30',
+        'timeout'  => '60'
+      },
+      'monitor:Master' => {
+        'role'         => 'Master',
+        'interval'     => '27',
+        'timeout'      => '60'
+        },
+      },
+    }
+
+    cs_colocation { "conntrackd-with-${vrouter_name}-vip":
+      primitives => [ 'master_p_conntrackd:Master', "vip__vrouter_${vrouter_name}" ],
+    }
+
+    File['/etc/conntrackd/conntrackd.conf'] -> Cs_resource['p_conntrackd'] -> Service['p_conntrackd'] -> Cs_colocation["conntrackd-with-${vrouter_name}-vip"]
+
+    service { 'p_conntrackd':
+      ensure   => 'running',
+      enable   => true,
+      provider => 'pacemaker',
+    }
+
+    # Workaround to ensure log is rotated properly
+    file { '/etc/logrotate.d/conntrackd':
+      content => template('openstack/95-conntrackd.conf.erb'),
+    }
+
+    Package[$conntrackd_package] -> File['/etc/logrotate.d/conntrackd']
   }
-
-  cs_colocation { "conntrackd-with-${vrouter_name}-vip":
-    primitives => [ 'master_p_conntrackd:Master', "vip__vrouter_${vrouter_name}" ],
-  }
-
-  File['/etc/conntrackd/conntrackd.conf'] -> Cs_resource['p_conntrackd'] -> Service['p_conntrackd'] -> Cs_colocation["conntrackd-with-${vrouter_name}-vip"]
-
-  service { 'p_conntrackd':
-    ensure   => 'running',
-    enable   => true,
-    provider => 'pacemaker',
-  }
-
-  # Workaround to ensure log is rotated properly
-  file { '/etc/logrotate.d/conntrackd':
-    content => template('openstack/95-conntrackd.conf.erb'),
-  }
-
-  Package[$conntrackd_package] -> File['/etc/logrotate.d/conntrackd']
 }
--- tests/noop/spec/hosts/ceph/radosgw_spec.rb
+++ tests/noop/spec/hosts/ceph/radosgw_spec.rb
@@ -6,6 +6,39 @@ describe manifest do
   shared_examples 'catalog' do
     storage_hash = Noop.hiera 'storage'
     ceph_monitor_nodes = Noop.hiera 'ceph_monitor_nodes'
+    public_ssl_hash = Noop.hiera('public_ssl')
+
+    let(:ssl_hash) { Noop.hiera_hash 'use_ssl', {} }
+
+    let(:internal_auth_protocol) {
+      Noop.puppet_function 'get_ssl_property',ssl_hash,{},'keystone',
+        'internal','protocol','http'
+    }
+
+    let(:internal_auth_address) {
+      Noop.puppet_function 'get_ssl_property',ssl_hash,{},'keystone',
+        'internal','hostname',
+        [Noop.hiera('service_endpoint', Noop.hiera('management_vip'))]
+    }
+
+    let(:admin_auth_protocol) {
+      Noop.puppet_function 'get_ssl_property',ssl_hash,{},'keystone',
+        'admin','protocol','http'
+    }
+
+    let(:admin_auth_address) {
+      Noop.puppet_function 'get_ssl_property',ssl_hash,{},'keystone','admin',
+      'hostname',
+        [Noop.hiera('service_endpoint', Noop.hiera('management_vip'))]
+    }
+
+    let(:internal_url) {
+      "#{internal_auth_protocol}://#{internal_auth_address}:5000"
+    }
+
+    let(:admin_url) {
+      "#{admin_auth_protocol}://#{admin_auth_address}:35357"
+    }
 
     if (storage_hash['images_ceph'] or storage_hash['objects_ceph'])
       rgw_large_pool_name = '.rgw'
@@ -40,6 +73,34 @@ describe manifest do
            :command => "ceph -n #{radosgw_auth_key} osd pool create #{rgw_large_pool_name} #{rgw_large_pool_pg_nums} #{rgw_large_pool_pg_nums}",
            :unless  => "rados lspools | grep '^#{rgw_large_pool_name}$'"
          )
+      }
+
+      it {
+        if Noop.hiera('external_lb', false)
+          url = internal_url
+          provider = 'http'
+        else
+          url = 'http://' + Noop.hiera('service_endpoint').to_s + ':10000/;csv'
+          provider = nil
+        end
+        should contain_haproxy_backend_status('keystone-public').with(
+          :url      => url,
+          :provider => provider
+        )
+      }
+
+      it {
+        if Noop.hiera('external_lb', false)
+          url = admin_url
+          provider = 'http'
+        else
+          url = 'http://' + Noop.hiera('service_endpoint').to_s + ':10000/;csv'
+          provider = nil
+        end
+        should contain_haproxy_backend_status('keystone-admin').with(
+          :url      => url,
+          :provider => provider
+        )
       }
 
     end
