eb8567d7a0e4e66afc63e72587bdba8409c3c435
Michael Polenchuk <mpolenchuk@mirantis.com>
2015-12-21 18:48:45 +0300

    Mend ceilometer_radosgw_user provider
    Fix exists() method in ceilometer_radosgw_user provider to properly
    check desired vs current state. Also outdated central/compute ceilometer
    agents have been replaced with polling one.
    ("notification_driver" option has been unified as well)
    
    Change-Id: Ieb8e850dbde05705c838aa0090279acfe969b92d
    Closes-Bug: #1526892

--- deployment/puppet/openstack/manifests/glance.pp
+++ deployment/puppet/openstack/manifests/glance.pp
@@ -96,7 +96,7 @@ class openstack::glance (
   $rbd_store_user                 = 'images',
   $rbd_store_pool                 = 'images',
   $rados_connect_timeout          = '0',
-  $ceilometer                     = false,
+  $notification_driver            = undef,
   $service_workers                = $::processorcount,
 ) {
   validate_string($glance_user_password)
@@ -230,14 +230,6 @@ class openstack::glance (
     }
   } else {
     $rabbit_hosts_real = $rabbit_hosts
-  }
-
-  # Configure rabbitmq notifications
-  # TODO(bogdando) sync qpid support from upstream
-  if $ceilometer {
-    $notification_driver = 'messaging'
-  } else {
-    $notification_driver = 'noop'
   }
 
   class { 'glance::notify::rabbitmq':
