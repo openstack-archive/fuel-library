8b53e8e513c8ac3022e092fccbafa071701b3686
Maksim Malchuk <mmalchuk@mirantis.com>
2015-12-23 20:24:13 +0300

    Debug and Notice messages during Fuel master start
    Instead of running 'dockerctl build all' command in the background
    and long wait for a debug output we've split the command to build
    each container one by one, but keep the original order. Doing this,
    we have much more information what is going on.
    
    Also this commit adds some important message to the log which used
    as an anchor for VirtualBox scripts.
    
    Change-Id: I77a1d3a32633390c1fe37bbeb81da5d0996972df

--- tests/noop/spec/hosts/master/host-upgrade_spec.rb
+++ tests/noop/spec/hosts/master/host-upgrade_spec.rb
@@ -3,5 +3,16 @@ require 'shared-examples'
 manifest = 'master/host-upgrade.pp'
 
 describe manifest do
+
+  before(:each) do
+    Noop.puppet_function_load :file
+    MockFunction.new(:file) do |function|
+      allow(function).to receive(:call).with(['/etc/dockerctl/config']).and_return('dockerctl_data')
+    end
+    let(:containers_line) do
+      containers_line = 'CONTAINER_SEQUENCE="postgres rabbitmq keystone rsync astute rsyslog nailgun ostf nginx cobbler mcollective"'
+    end
+  end
+
   test_centos manifest
 end
