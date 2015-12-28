aff0637717b78e207b26a2df2ede4d4949593a09
iberezovskiy <iberezovskiy@mirantis.com>
2015-12-28 19:11:58 +0300

    Fix read/write permissions on ceph pool for backups
    Cinder and Backup users should have read/write
    permissions on ceph 'backups' pool. Update noop tests.
    
    Change-Id: I8c05921dc52c3e6d4f7152d3f0e0c669790d0a44
    Closes-bug: #1529257

--- tests/noop/spec/hosts/ceph/ceph_pools_spec.rb
+++ tests/noop/spec/hosts/ceph/ceph_pools_spec.rb
@@ -4,35 +4,40 @@ manifest = 'ceph/ceph_pools.pp'
 
 describe manifest do
   shared_examples 'catalog' do
-    storage_hash = Noop.hiera 'storage'
+    storage_hash       = Noop.hiera 'storage'
+    glance_pool        = 'images'
+    cinder_pool        = 'volumes'
+    cinder_backup_pool = 'backups'
+
 
     if (storage_hash['images_ceph'] or storage_hash['objects_ceph'])
-      it { should contain_ceph__pool('images').with(
-                'pg_num'        => storage_hash['per_pool_pg_nums']['images'],
-                'pgp_num'       => storage_hash['per_pool_pg_nums']['images'],)
+      it { should contain_ceph__pool("#{glance_pool}").with(
+              'acl'     => "mon 'allow r' osd 'allow class-read object_prefix rbd_children, allow rwx pool=#{glance_pool}'",
+              'pg_num'  => storage_hash['per_pool_pg_nums']['images'],
+              'pgp_num' => storage_hash['per_pool_pg_nums']['images'],)
         }
-      it { should contain_ceph__pool('volumes').with(
-                'pg_num'        => storage_hash['per_pool_pg_nums']['volumes'],
-                'pgp_num'       => storage_hash['per_pool_pg_nums']['volumes'],)
+      it { should contain_ceph__pool("#{cinder_pool}").with(
+              'acl'     => "mon 'allow r' osd 'allow class-read object_prefix rbd_children, allow rwx pool=#{cinder_pool}, allow rx pool=#{glance_pool}'",
+              'pg_num'  => storage_hash['per_pool_pg_nums']['volumes'],
+              'pgp_num' => storage_hash['per_pool_pg_nums']['volumes'],)
         }
-      it { should contain_ceph__pool('backups').with(
-                'pg_num'        => storage_hash['per_pool_pg_nums']['backups'],
-                'pgp_num'       => storage_hash['per_pool_pg_nums']['backups'],)
+      it { should contain_ceph__pool("#{cinder_backup_pool}").with(
+              'acl'     => "mon 'allow r' osd 'allow class-read object_prefix rbd_children, allow rwx pool=#{cinder_backup_pool}, allow rwx pool=#{cinder_pool}'",
+              'pg_num'  => storage_hash['per_pool_pg_nums']['backups'],
+              'pgp_num' => storage_hash['per_pool_pg_nums']['backups'],)
         }
 
       if storage_hash['volumes_ceph']
-        it { should contain_ceph__pool('volumes').that_notifies('Service[cinder-volume]') }
-        it { should contain_ceph__pool('backups').that_notifies('Service[cinder-backup]') }
+        it { should contain_ceph__pool("#{cinder_pool}").that_notifies('Service[cinder-volume]') }
+        it { should contain_ceph__pool("#{cinder_backup_pool}").that_notifies('Service[cinder-backup]') }
         it { should contain_service('cinder-volume') }
         it { should contain_service('cinder-backup') }
       end
 
       if storage_hash['images_ceph']
-        it { should contain_ceph__pool('images').that_notifies('Service[glance-api]') }
+        it { should contain_ceph__pool("#{glance_pool}").that_notifies('Service[glance-api]') }
         it { should contain_service('glance-api') }
       end
-    end
-
   end
   test_ubuntu_and_centos manifest
 
