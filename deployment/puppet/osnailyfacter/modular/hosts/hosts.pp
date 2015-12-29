5724477c83cb0aad179484a8f60eca9e026f9715
Kyrylo Galanov <kgalanov@mirantis.com>
2015-12-29 12:41:26 +0200

    Introduce node name prefix for mgmt/messaging IPs
    RabbitMQ will resolve <prefix>-<fqdn> hostnames to valid mgmt/messaging IP
    
    Change-Id: Ifc2af16b08663655d365587ea6f45c87bfc68698
    Depends-On: I9813fa8c20d47e0ef1e251fe5ac8d01d08fe7703
    Closes-bug: #1528707
    DocImpact: https://bugs.launchpad.net/fuel/+bug/1535383
    (cherry picked from commit ca65b718de8c3ca015d72416643a88a78f09b7ce)

--- deployment/puppet/osnailyfacter/modular/hosts/hosts.pp
+++ deployment/puppet/osnailyfacter/modular/hosts/hosts.pp
@@ -1,12 +1,14 @@
 notice('MODULAR: hosts.pp')
 
 $hosts_file = '/etc/hosts'
-$host_resources = network_metadata_to_hosts(hiera_hash('network_metadata'))
+$network_metadata = hiera_hash('network_metadata')
+$host_resources = network_metadata_to_hosts($network_metadata)
+$messaging_host_resources = network_metadata_to_hosts($network_metadata, 'mgmt/messaging', hiera('node_name_prefix_for_messaging'))
 
 Host {
     ensure => present,
     target => $hosts_file
 }
 
-create_resources(host, $host_resources)
+create_resources(host, merge($host_resources, $messaging_host_resources))
 
--- tests/noop/spec/hosts/hosts/hosts_spec.rb
+++ tests/noop/spec/hosts/hosts/hosts_spec.rb
@@ -3,6 +3,32 @@ require 'shared-examples'
 manifest = 'hosts/hosts.pp'
 
 describe manifest do
+  shared_examples 'catalog' do
+
+    network_metadata = Noop.hiera_structure('network_metadata/nodes')
+    messaging_fqdn_prefix = Noop.hiera('node_name_prefix_for_messaging', 'messaging-')
+
+    it 'should create basic host entries' do
+      network_metadata.each do |node, params|
+        should contain_host(params['fqdn']).with({
+          :ip => params['network_roles']['mgmt/vip'],
+          :host_aliases => ["#{node}"],
+          :target => '/etc/hosts'
+        })
+      end
+    end
+
+    it 'should create host entries for messaging network with correct prefix' do
+      network_metadata.each do |node, params|
+        should contain_host("#{messaging_fqdn_prefix}#{params['fqdn']}").with({
+          :ip => params['network_roles']['mgmt/messaging'],
+          :host_aliases => ["#{messaging_fqdn_prefix}#{node}"],
+          :target => '/etc/hosts'
+        })
+      end
+
+    end
+  end
   test_ubuntu_and_centos manifest
 end
 
