b960a52cce48bf429f7c3fcbf3222b036f9e666c
Kyrylo Galanov <kgalanov@mirantis.com>
2015-12-29 12:41:26 +0200

    Introduce node name prefix for mgmt/messaging IPs
    RabbitMQ will resolve <prefix>-<fqdn> hostnames to valid mgmt/messaging IP
    
    Change-Id: Ifc2af16b08663655d365587ea6f45c87bfc68698
    Depends-On: I9813fa8c20d47e0ef1e251fe5ac8d01d08fe7703
    Closes-bug: #1528707

--- deployment/puppet/osnailyfacter/modular/hosts/hosts.pp
+++ deployment/puppet/osnailyfacter/modular/hosts/hosts.pp
@@ -1,7 +1,9 @@
 notice('MODULAR: hosts.pp')
 
 $hosts_file = '/etc/hosts'
-$host_resources = network_metadata_to_hosts(hiera_hash('network_metadata'))
+$network_metadata = hiera_hash('network_metadata')
+$host_resources = network_metadata_to_hosts($network_metadata)
+$messaging_host_resources = network_metadata_to_hosts($network_metadata, 'mgmt/messaging', hiera('node_name_prefix_for_messaging'))
 
 Host {
     ensure => present,
@@ -9,4 +11,5 @@ Host {
 }
 
 create_resources(host, $host_resources)
+create_resources(host, $messaging_host_resources)
 
