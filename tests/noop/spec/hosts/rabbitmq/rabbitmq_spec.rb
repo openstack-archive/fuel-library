920fcf6eade2fd62eae3eb788f25bc59491979fe
Kyrylo Galanov <kgalanov@mirantis.com>
2015-12-29 12:41:26 +0200

    Introduce node name prefix for mgmt/messaging IPs
    RabbitMQ will resolve <prefix>-<fqdn> hostnames to valid mgmt/messaging IP
    
    Change-Id: Ifc2af16b08663655d365587ea6f45c87bfc68698
    Depends-On: I9813fa8c20d47e0ef1e251fe5ac8d01d08fe7703
    Closes-bug: #1528707
    DocImpact: https://bugs.launchpad.net/fuel/+bug/1535383
    (cherry picked from commit ca65b718de8c3ca015d72416643a88a78f09b7ce)

--- tests/noop/spec/hosts/rabbitmq/rabbitmq_spec.rb
+++ tests/noop/spec/hosts/rabbitmq/rabbitmq_spec.rb
@@ -16,6 +16,13 @@ describe manifest do
       expect(environment_variables['ERL_EPMD_ADDRESS']).to eq node_ip_address
     end
 
+    it "should contain nodename" do
+      fqdn_prefix = Noop.hiera('node_name_prefix_for_messaging', 'messaging-')
+      node_name = "rabbit@#{fqdn_prefix}#{facts[:hostname]}"
+      environment_variables = Noop.resource_parameter_value self, 'class', 'rabbitmq', 'environment_variables'
+      expect(environment_variables['NODENAME']).to eq node_name
+    end
+
     # LP#1477595
     it "should contain rabbitmq correct log levels" do
       debug = Noop.hiera('debug', false)
@@ -54,6 +61,7 @@ describe manifest do
     it "should override service on package install" do
       should contain_tweaks__ubuntu_service_override('rabbitmq-server')
     end
+
   end
   test_ubuntu_and_centos manifest
 end
