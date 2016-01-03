1de028b4c0ca5d40b75a3fbfc837c428576120bf
Maksim Malchuk <mmalchuk@mirantis.com>
2016-01-03 04:09:01 +0300

    Fuel master password policy (PAM) enablement
    This commit changes default users password aging, length and complexity.
    This commit contains some syntax cleanups for the host-only.pp
    
    Change-Id: Ia52c3c0c589b2545aaa7469a3461c6024f068228
    Closes-Bug: #1519693

--- tests/noop/spec/hosts/master/host-only_spec.rb
+++ tests/noop/spec/hosts/master/host-only_spec.rb
@@ -120,6 +120,38 @@ describe manifest do
         end
       end #it do
     end #context
+
+    it 'should configure login.defs for password aging and length settings' do
+      should contain_augeas('Password aging and length settings').with(
+        :lens    => 'login_defs.lns',
+        :incl    => '/etc/login.defs',
+        :changes => [
+          'set PASS_MAX_DAYS 365',
+          'set PASS_MIN_DAYS 2',
+          'set PASS_MIN_LEN 8',
+          'set PASS_WARN_AGE 30',
+        ],
+      )
+    end
+
+    it 'should configure system-auth password complexity' do
+      should contain_augeas('Password complexity').with(
+        :lens    => 'pam.lns',
+        :incl    => '/etc/pam.d/system-auth',
+        :changes => [
+          "set *[type='password'][module='pam_pwquality.so' or module='pam_cracklib.so']/control requisite",
+          "rm *[type='password'][module='pam_pwquality.so' or module='pam_cracklib.so']/argument",
+          "set *[type='password'][module='pam_pwquality.so' or module='pam_cracklib.so']/argument[1] try_first_pass",
+          "set *[type='password'][module='pam_pwquality.so' or module='pam_cracklib.so']/argument[2] retry=3",
+          "set *[type='password'][module='pam_pwquality.so' or module='pam_cracklib.so']/argument[3] dcredit=-1",
+          "set *[type='password'][module='pam_pwquality.so' or module='pam_cracklib.so']/argument[4] ucredit=-1",
+          "set *[type='password'][module='pam_pwquality.so' or module='pam_cracklib.so']/argument[5] ocredit=-1",
+          "set *[type='password'][module='pam_pwquality.so' or module='pam_cracklib.so']/argument[6] lcredit=-1",
+        ],
+        :onlyif => "match *[type='password'][control='requisite'][module='pam_pwquality.so' or module='pam_cracklib.so'] size > 0",
+      )
+    end
+
   end #shared_examples
 
   test_centos manifest
