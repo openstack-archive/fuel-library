9f0b28dc42bd6e815e0fe5ce097a9803c06de501
Sergey Vasilenko <svasilenko@mirantis.com>
2016-01-05 17:56:18 +0300

    No more use nodes list.
    Nodes list is deprecated, beause incompatible with network-roles model.
    Network metadata should be used instead.
    
    This patchset implements:
    * remove nodes list from fixtures
    * remove unused function filter_nodes
    * remove some unused code, related to nodes list
    * new parser function get_node_name
    * fix parser function roles_include for using network_metadata
    * use roles_include() if need instead directly lookup into network_metadata
    * fix some manifests for use network_matadata/nodes
    
    Change-Id: I2e6b1cd5cb75263efc995aa9f62bd3073abed1b8
    Related-bug: #1531128

--- deployment/puppet/osnailyfacter/modular/vmware/cinder-vmware.pp
+++ deployment/puppet/osnailyfacter/modular/vmware/cinder-vmware.pp
@@ -1,10 +1,10 @@
 notice('MODULAR: cinder-vmware.pp')
 
-$nodes_hash   = hiera('nodes', {})
-$roles        = node_roles($nodes_hash, hiera('uid'))
-$cinder_hash  = hiera_hash('cinder_hash', {})
+$node_name        = hiera('node_name')
+$network_metadata = hiera_hash('network_metadata')
+$cinder_hash      = hiera_hash('cinder_hash', {})
 
-if (member($roles, 'cinder-vmware')) {
+if roles_include(['cinder-vmware']) {
   $debug    = pick($cinder_hash['debug'], hiera('debug', true))
   $volumes  = get_cinder_vmware_data($cinder_hash['instances'], $debug)
   create_resources(vmware::cinder::vmdk, $volumes)
