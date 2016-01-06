53c9bbea9b7ee1daaabe4cdd37779cdc8f009761
Alex Schultz <aschultz@mirantis.com>
2016-01-06 14:59:46 -0700

    Fix horizon caching backend for UCA
    This change updates the horizon caching backend to be configurable and
    changes the default where os_package_default is either not present or is
    set to debian. The MOS package provides an alternate memcache backend
    that is not available from other groups. With this change, we can now
    specify a cache_backend as part of the horizon configuration hash which
    will be used instead of the defaults specified in code.
    
    Change-Id: If1050db4bb54bc9f3ae355ffeb2ae7fbeb3cfc44
    Related-Blueprint: deploy-with-uca-packages

--- tests/noop/spec/hosts/horizon/horizon_spec.rb
+++ tests/noop/spec/hosts/horizon/horizon_spec.rb
@@ -85,8 +85,14 @@ describe manifest do
     end
 
     it 'should declare horizon class with correct values' do
+      if !facts.has_key?(:os_package_type) or facts[:os_package_type] == 'debian'
+          cache_backend = 'horizon.backends.memcached.HorizonMemcached'
+      else
+          cache_backend = 'django.core.cache.backends.memcached.MemcachedCache'
+      end
+
       should contain_class('horizon').with(
-                 'cache_backend'       => 'horizon.backends.memcached.HorizonMemcached',
+                 'cache_backend'       => cache_backend,
                  'cache_options'       => cache_options,
                  'log_handler'         => 'file',
                  'overview_days_range' => 1,
@@ -129,6 +135,7 @@ describe manifest do
         should_not contain_package('openstack-dashboard-apache')
       end
     end
+
   end
 
   test_ubuntu_and_centos manifest
