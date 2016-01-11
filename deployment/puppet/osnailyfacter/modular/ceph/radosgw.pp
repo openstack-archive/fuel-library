4f6c93eba1b88a98ab0a53846256632121499ac8
Alex Schultz <aschultz@mirantis.com>
2016-01-11 11:12:01 -0700

    Add apache mod_proxy for ceph radosgw
    With the switch to fcgi for radosgw we need to include mod_proxy as
    mod_proxy_fcgi requires it. This change pulls in the proxy module to
    resolve apache2 startup failures when ceph is enabled.
    
    Closes-Bug: #1505273
    (cherry picked from commit ac654f2a5bac67ee8bd8330cb4b8443a84b3ea28)
    Change-Id: I7044b58066e70b11ed9358579a2f369ce761ee06

--- deployment/puppet/osnailyfacter/modular/ceph/radosgw.pp
+++ deployment/puppet/osnailyfacter/modular/ceph/radosgw.pp
@@ -36,6 +36,7 @@ if $use_ceph and $storage_hash['objects_ceph'] {
   }
   if ($::osfamily == 'Debian'){
     apache::mod {'rewrite': }
+    apache::mod {'proxy': }
     apache::mod {'proxy_fcgi': }
   }
 
