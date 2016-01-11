0848c1fb217a17b7ffe15efbe4579a6b437ca6a1
Alex Schultz <aschultz@mirantis.com>
2016-01-11 11:12:01 -0700

    Add apache mod_proxy for ceph radosgw
    With the switch to fcgi for radosgw we need to include mod_proxy as
    mod_proxy_fcgi requires it. This change pulls in the proxy module to
    resolve apache2 startup failures when ceph is enabled.
    
    Closes-Bug: #1505273
    (cherry picked from commit ac654f2a5bac67ee8bd8330cb4b8443a84b3ea28)
    Change-Id: I7044b58066e70b11ed9358579a2f369ce761ee06

--- tests/noop/spec/hosts/ceph/radosgw_spec.rb
+++ tests/noop/spec/hosts/ceph/radosgw_spec.rb
@@ -11,6 +11,18 @@ describe manifest do
       rgw_id = 'radosgw.gateway'
       rgw_s3_auth_use_keystone = Noop.hiera 'rgw_s3_auth_use_keystone', true
 
+      it 'should configure apache mods' do
+        if facts[:osfamily] == 'Debian'
+          should contain_apache__mod('rewrite')
+          should contain_apache__mod('proxy')
+          should contain_apache__mod('proxy_fcgi')
+        else
+          should contain_apache__mod('rewrite')
+          should_not contain_apache__mod('proxy')
+          should_not contain_apache__mod('proxy_fcgi')
+        end
+      end
+
       it { should contain_class('ceph::radosgw').with(
            'primary_mon'   => ceph_monitor_nodes.keys[0],
            'rgw_frontends' => 'fastcgi socket_port=9000 socket_host=127.0.0.1',
