68fbcf2fd4bd57bcb8733d0b58003116a70bdb53
Aleksandr Didenko <adidenko@mirantis.com>
2016-01-13 14:44:31 +0100

    Don't create invalid floating IP ranges
    Since floating IP ranges is an array then it's impossible to
    override it via Hiera with deep_merge. It's possible to add new
    IP ranges only, but not delete/override existing ones.
    So if plugin developer wants to override floating IPs parameters
    then puppet will fail to create neutron floating subnets, since IP
    range from astute.yaml is still present in array after deep merge.
    
    We should not try to create floating IP ranges that does not match
    with floating CIDR. We can skip those ranges and put Puppet warning
    about it.
    
    Change-Id: I4902933c4f04132ec41f0a844cca852dddefa02a
    Closes-bug: #1533676

--- deployment/puppet/osnailyfacter/lib/puppet/parser/functions/format_allocation_pools.rb
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/format_allocation_pools.rb
@@ -1,19 +1,27 @@
+require 'ipaddr'
+
 Puppet::Parser::Functions::newfunction(:format_allocation_pools, :type => :rvalue, :doc => <<-EOS
 This function gets floating ranges and format allocation_pools attribute value for neutron subnet resource.
 EOS
 ) do |args|
 
-   raise ArgumentError, ("format_allocation_pools(): wrong number of arguments (#{args.length}; must be 1)") if args.length < 1
-   raise ArgumentError, ("format_allocation_pools(): wrong number of arguments (#{args.length}; must be 1)") if args.length > 1
+   raise ArgumentError, ("format_allocation_pools(): wrong number of arguments (#{args.length}; must be 2)") unless args.length == 2
 
    floating_ranges = args[0]
+   floating_cidr = args[1]
    raise ArgumentError, 'format_allocation_pools(): floating_ranges is not array!' if !(floating_ranges.is_a?(Array) or floating_ranges.is_a?(String))
+   raise ArgumentError, 'format_allocation_pools(): floating_cidr is missing' if !floating_cidr.is_a?(String)
 
-   debug "Formating allocation_pools for #{floating_ranges}"
+   debug "Formating allocation_pools for #{floating_ranges} subnet #{floating_cidr}"
+   floating_range = IPAddr.new(floating_cidr)
    allocation_pools = []
    if floating_ranges.is_a?(Array) # Is a temporary solution while network_data['L3']['floating'] is not array
      floating_ranges.each do | range |
-       allocation_pools << "start=#{range.split(':')[0]},end=#{range.split(':')[1]}"
+       if floating_range.include?(range.split(':')[0]) and floating_range.include?(range.split(':')[1])
+         allocation_pools << "start=#{range.split(':')[0]},end=#{range.split(':')[1]}"
+       else
+         warning("Skipping #{range} floating IP range because it does not match #{floating_cidr}.")
+       end
      end
    else # TODO: remove else part after python part is merged
      allocation_pools << "start=#{floating_ranges.split(':')[0]},end=#{floating_ranges.split(':')[1]}"
