b25b51becc6fdf10075f6c4c57d32fdf32233517
Alex Schultz <aschultz@mirantis.com>
2016-01-13 16:46:39 -0700

    [WIP] Switch to upstream mysql and galera modules
    This change switches out our mysql and galera modules for the
    puppetlabs-mysql and michaeltchapman-galera.
    
    Things touched:
     - mysql module removed
     - galera module removed
     - openstacklib module pulls clean upstream version witout the fuel
       specific mysql hacks
     - galera::client moved to openstack::galera::client
     - db modulars updated to use openstack::galera::client
     - database task updated to use galera module
     - create cluster::mysql to do pacemaker configuration for mysql
    
    Current status:
    - database task runs to completion with galera bootstrapped,
      clustercheck works so haproxy_backend_status is OK
    - database service now runs in pacemaker
    - need to address TODOs in database task
    
    Change-Id: Idd0957c677b87a2d8794e993417ef9e2f0ddf4a6

---
+++ deployment/puppet/cluster/manifests/mysql.pp
@@ -0,0 +1,92 @@
+# == Class: cluster::mysql
+#
+# Configure OCF service for mysql managed by corosync/pacemaker
+#
+# === Parameters
+#
+# [*primary_controller*]
+# [*mysql_user*]
+# [*mysql_password*]
+# [*mysql_socket*]
+#
+class cluster::mysql (
+  $primary_controller,
+  $mysql_user,
+  $mysql_password,
+  $mysql_config = '/etc/mysql/my.cnf',
+  $mysql_socket = '/var/run/mysqld/mysqld.sock',
+) {
+  $service_name = 'mysqld'
+
+  if $primary_controller {
+    cs_resource { "p_${service_name}":
+      ensure          => present,
+      primitive_class => 'ocf',
+      provided_by     => 'fuel',
+      primitive_type  => 'mysql-wss',
+      complex_type    => 'clone',
+      parameters      => {
+        'config'      => $mysql_config,
+        'test_user'   => $mysql_user,
+        'test_passwd' => $mysql_password,
+        'socket'      => $mysql_socket,
+      },
+      operations      => {
+        'monitor' => {
+          'interval' => '60',
+          'timeout'  => '55'
+        },
+        'start'   => {
+          'interval' => '0',
+          'timeout'  => '300'
+        },
+        'stop'    => {
+          'interval' => '0',
+          'timeout'  => '120'
+        },
+      },
+    }
+
+    Cs_resource["p_${service_name}"] ~>
+      Service[$service_name]
+    Cs_resource["p_${service_name}"] ->
+      Service[$service_name]
+  }
+
+  $init_file_contents = join([
+    "set wsrep_on='off';",
+    "delete from mysql.user where user='';",
+    "GRANT USAGE ON *.* TO '${status_user}'@'%' IDENTIFIED BY '${status_password}';",
+    "GRANT USAGE ON *.* TO '${status_user}'@'localhost' IDENTIFIED BY '${status_password}';",
+    "flush privileges;",
+  ], "\n")
+
+  # This file is used to prep the mysql instance with the monitor user so that
+  # pacemaker can check that the instance is UP.
+  file { 'init-file':
+    ensure  => present,
+    path    => '/tmp/wsrep-init-file',
+    content => $init_file_contents,
+    before  => Service[$service_name],
+  }
+
+  exec { 'rm-init-file':
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+    command => 'rm /tmp/wsrep-init-file',
+  }
+
+  exec { 'wait-for-access':
+    path      => '/bin:/sbin:/usr/bin:/usr/sbin',
+    command   => "mysql -u${status_user} -p${status_password} -Nbe \"select 'OK';\" | grep -q OK",
+    try_sleep => 10,
+    tries     => 60,
+  }
+
+  # TODO(aschultz): make this stuff more idempotent friendly
+  File['init-file'] ->
+    Service['mysqld'] ->
+      Exec['wait-for-access'] ->
+        Exec['rm-init-file']
+
+  #TODO(aschultz): do we need to pull in the waiting for sync?
+}
