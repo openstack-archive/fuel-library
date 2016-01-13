2dfdeff29e88ea9c7be6ba42285de371686f2580
iberezovskiy <iberezovskiy@mirantis.com>
2016-01-13 20:07:50 +0300

    Update puppet-keystone module to currnet Mitaka version
    Puppet-keystone module doesn't contain any custom changes.
    It's up to date with upstream version of 11 Jan.
    
    Additional changes:
    
      * Remove public_endpoint definition:
        Mitaka manifests treat false as a string False, rather
        than unsetting the value. This parameter is no longer
        used, so it should be simply removed.
    
    Change-Id: Ie192c0e2a69062da17fb4c1713f1e9d47b77651c

--- deployment/puppet/openstack/manifests/keystone.pp
+++ deployment/puppet/openstack/manifests/keystone.pp
@@ -103,8 +103,6 @@ class openstack::keystone (
     $token_driver = 'keystone.token.backends.sql.Token'
   }
 
-  $public_endpoint = false
-
   #### Fernet Token ####
   if $token_provider == 'keystone.token.providers.fernet.Provider' {
     file { "$fernet_key_repository":
@@ -146,19 +144,12 @@ class openstack::keystone (
       token_caching                => $token_caching,
       cache_backend                => $cache_backend,
       revoke_driver                => $revoke_driver,
-      public_endpoint              => $public_endpoint,
       admin_endpoint               => $admin_url,
       memcache_dead_retry          => '60',
       memcache_socket_timeout      => '1',
       memcache_pool_maxsize        =>'1000',
       memcache_pool_unused_timeout => '60',
-    }
-
-    # TODO (iberezovskiy): Move to globals (as it is done for sahara)
-    # after new sync with upstream because of
-    # https://github.com/openstack/puppet-keystone/blob/master/manifests/init.pp#L564
-    class { '::keystone::logging':
-      default_log_levels => $default_log_levels,
+      policy_driver                => 'keystone.policy.backends.sql.Policy',
     }
 
     Package<| title == 'keystone'|> ~> Service<| title == 'keystone'|>
@@ -183,7 +174,6 @@ class openstack::keystone (
       'DATABASE/max_retries':                            value => $max_retries;
       'DATABASE/max_overflow':                           value => $max_overflow;
       'identity/driver':                                 value =>'keystone.identity.backends.sql.Identity';
-      'policy/driver':                                   value =>'keystone.policy.backends.sql.Policy';
       'ec2/driver':                                      value =>'keystone.contrib.ec2.backends.sql.Ec2';
       'filter:debug/paste.filter_factory':               value =>'keystone.common.wsgi:Debug.factory';
       'filter:token_auth/paste.filter_factory':          value =>'keystone.middleware:TokenAuthMiddleware.factory';
