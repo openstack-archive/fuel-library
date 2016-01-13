3520e6e8deb9d3673a87a8f7bb0f021c54ed8f6e
Alex Schultz <aschultz@mirantis.com>
2016-01-13 16:46:39 -0700

    [WIP] Switch to upstream mysql and galera modules
    This change switches out our mysql and galera modules for the
    puppetlabs-mysql and michaeltchapman-galera.
    
    Things touched:
     - mysql module removed
     - galera module removed
     - openstacklib module pulls a upstream version (7 Jan)
       with mysql hacks removed (https://review.fuel-infra.org/#/c/16445/)
     - galera::client moved to openstack::galera::client
     - db modulars updated to use openstack::galera::client
     - database task updated to use galera module
     - create cluster::mysql to do pacemaker configuration for mysql
     - remove osnailyfacter::mysql_user
     - additional database noop tests
     - additional unit tests
    
    Current status:
    - database task runs to completion with galera bootstrapped,
      clustercheck works so haproxy_backend_status is OK
    - database service now runs in pacemaker
    
    Change-Id: Idd0957c677b87a2d8794e993417ef9e2f0ddf4a6

---
+++ deployment/puppet/cluster/manifests/mysql.pp
@@ -0,0 +1,130 @@
+# == Class: cluster::mysql
+#
+# Configure OCF service for mysql managed by corosync/pacemaker
+#
+# === Parameters
+#
+# [*primary_controller*]
+#  (required). Boolean. Flag to indicate if this on the primary contoller
+#
+# [*mysql_user*]
+#  (required). String. Mysql user to use for connection testing and status
+#  checks.
+#
+# [*mysql_password*]
+#  (requires). String. Password for the mysql user to checks with.
+#
+# [*mysql_config*]
+#  (optional). String. Location to the mysql.cnf to use when running the
+#  service.
+#  Defaults to '/etc/mysql/my.cnf'
+#
+# [*mysql_socket*]
+#  (optional). String. The socket file to use for connection checks.
+#  Defaults to '/var/run/mysqld/mysqld.sock'
+#
+class cluster::mysql (
+  $primary_controller,
+  $mysql_user,
+  $mysql_password,
+  $mysql_config = '/etc/mysql/my.cnf',
+  $mysql_socket = '/var/run/mysqld/mysqld.sock',
+) {
+  $service_name = 'mysqld'
+
+  if $primary_controller {
+    cs_resource { "p_${service_name}":
+      ensure          => present,
+      primitive_class => 'ocf',
+      provided_by     => 'fuel',
+      primitive_type  => 'mysql-wss',
+      complex_type    => 'clone',
+      parameters      => {
+        'config'      => $mysql_config,
+        'test_user'   => $mysql_user,
+        'test_passwd' => $mysql_password,
+        'socket'      => $mysql_socket,
+      },
+      operations      => {
+        'monitor' => {
+          'interval' => '60',
+          'timeout'  => '55'
+        },
+        'start'   => {
+          'interval' => '0',
+          'timeout'  => '300'
+        },
+        'stop'    => {
+          'interval' => '0',
+          'timeout'  => '120'
+        },
+      },
+    }
+
+    Cs_resource["p_${service_name}"] ~>
+      Service[$service_name]
+    Cs_resource["p_${service_name}"] ->
+      Service[$service_name]
+  }
+
+  Service <| title == 'mysqld' |> {
+    name     => 'p_mysqld',
+    provider => 'pacemaker',
+  }
+
+  # NOTE(aschultz): strings must contain single quotes only, see the
+  # create-init-file exec as to why
+  $init_file_contents = join([
+    "set wsrep_on='off';",
+    "delete from mysql.user where user='';",
+    "GRANT USAGE ON *.* TO '${mysql_user}'@'%' IDENTIFIED BY '${mysql_password}';",
+    "GRANT USAGE ON *.* TO '${mysql_user}'@'localhost' IDENTIFIED BY '${mysql_password}';",
+    "flush privileges;",
+  ], "\n")
+
+  $user_password_string = "-u${mysql_user} -p${mysql_password}"
+
+  # This file is used to prep the mysql instance with the monitor user so that
+  # pacemaker can check that the instance is UP.
+  # NOTE(aschultz): we are using an exec here because we only want to create
+  # the init file before the mysql service is not running. This is used to
+  # bootstrap the service so we only do it the first time. For idempotency
+  # this exec would be skipped when run a second time with mysql running.
+  exec { 'create-init-file':
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+    command => "echo \"${init_file_contents}\" > /tmp/wsrep-init-file",
+    unless  => "mysql ${user_password_string} -Nbe \"select 'OK';\" | grep -q OK",
+    require => Package['mysql-server'],
+    before  => Service[$service_name],
+  } ~>
+
+  exec { 'wait-initial-sync':
+    path        => '/bin:/sbin:/usr/bin:/usr/sbin',
+    command     => "mysql ${user_password_string} -Nbe \"show status like 'wsrep_local_state_comment'\" | grep -q -e Synced -e Initialized && sleep 10",
+    try_sleep   => 10,
+    tries       => 60,
+    refreshonly => true,
+  } ~>
+
+  exec { 'wait-for-synced-state':
+    path        => '/bin:/sbin:/usr/bin:/usr/sbin',
+    command     => "mysql ${user_password_string} -Nbe \"show status like 'wsrep_local_state_comment'\" | grep -q Synced && sleep 10",
+    try_sleep   => 5,
+    tries       => 60,
+    refreshonly => true,
+  }
+
+  exec { 'rm-init-file':
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+    command => 'rm /tmp/wsrep-init-file',
+    onlyif  => 'test -f /tmp/wsrep-init-file',
+  }
+
+
+  # TODO(aschultz): make this stuff more idempotent friendly
+  Exec['create-init-file'] ->
+    Service['mysqld'] ->
+      Exec['wait-initial-sync'] ->
+        Exec['wait-for-synced-state'] ->
+          Exec['rm-init-file']
+}
