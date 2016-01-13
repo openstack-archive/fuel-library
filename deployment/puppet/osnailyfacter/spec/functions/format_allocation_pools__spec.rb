16ac68357fea236415f3b7b3989f50398c6c406a
Aleksandr Didenko <adidenko@mirantis.com>
2016-01-13 14:44:31 +0100

    Don't create invalid floating IP ranges
    Since floating IP ranges is an array then it's impossible to
    override it via Hiera with deep_merge. It's possible to add new
    IP ranges only, but not delete/override existing ones.
    So if plugin developer wants to override floating IPs parameters
    then puppet will fail to create neutron floating subnets, since IP
    range from astute.yaml is still present in array after deep merge.
    
    We should not try to create floating IP ranges that does not match
    with floating CIDR. We can skip those ranges and put Puppet warning
    about it.
    
    Change-Id: I4902933c4f04132ec41f0a844cca852dddefa02a
    Closes-bug: #1533676

--- deployment/puppet/osnailyfacter/spec/functions/format_allocation_pools__spec.rb
+++ deployment/puppet/osnailyfacter/spec/functions/format_allocation_pools__spec.rb
@@ -27,23 +27,35 @@ describe 'function for formating allocation pools for neutron subnet resource' d
     end
 
     it 'error if no arguments' do
-      lambda { @scope.function_format_allocation_pools([]) }.should raise_error(ArgumentError, 'format_allocation_pools(): wrong number of arguments (0; must be 1)')
+      lambda { @scope.function_format_allocation_pools([]) }.should raise_error(ArgumentError, 'format_allocation_pools(): wrong number of arguments (0; must be 1 or 2)')
     end
 
-    it 'should require one argument' do
-      lambda { @scope.function_format_allocation_pools(['foo', 'wee']) }.should raise_error(ArgumentError, 'format_allocation_pools(): wrong number of arguments (2; must be 1)')
+    it 'should fail with wrong number of arguments' do
+      lambda { @scope.function_format_allocation_pools(['foo', 'wee', 'bla']) }.should raise_error(ArgumentError, 'format_allocation_pools(): wrong number of arguments (3; must be 1 or 2)')
     end
 
-    it 'should require flating ranges is Array' do
-      lambda { @scope.function_format_allocation_pools([{:fff => true}]) }.should raise_error(ArgumentError, 'format_allocation_pools(): floating_ranges is not array!')
+    it 'should require floating ranges is Array' do
+      lambda { @scope.function_format_allocation_pools([{:fff => true}, 'cidr']) }.should raise_error(ArgumentError, 'format_allocation_pools(): floating_ranges is not array!')
     end
 
-    it 'should be able to format allocation pool string' do
+    it 'should require floating cidr is String' do
+      lambda { @scope.function_format_allocation_pools([['foo', 'wee'], ['cidr']]) }.should raise_error(ArgumentError, 'format_allocation_pools(): floating_cidr is not string!')
+    end
+
+    it 'should be able to format allocation pool string with optional CIDR parameter' do
+      expect(@scope.function_format_allocation_pools([["10.109.1.151:10.109.1.254", "10.109.1.130:10.109.1.150"], "10.109.1.0/24"])).to eq(["start=10.109.1.151,end=10.109.1.254", "start=10.109.1.130,end=10.109.1.150"])
+    end
+
+    it 'should be able to format allocation pool string without optional CIDR parameter' do
       expect(@scope.function_format_allocation_pools([["10.109.1.151:10.109.1.254", "10.109.1.130:10.109.1.150"]])).to eq(["start=10.109.1.151,end=10.109.1.254", "start=10.109.1.130,end=10.109.1.150"])
     end
 
     it 'should be able to format allocation pool string for old structure' do
-      expect(@scope.function_format_allocation_pools(["10.109.1.133:10.109.1.169"])).to eq(["start=10.109.1.133,end=10.109.1.169"])
+      expect(@scope.function_format_allocation_pools(["10.109.1.133:10.109.1.169", "10.109.1.0/24"])).to eq(["start=10.109.1.133,end=10.109.1.169"])
+    end
+
+    it 'should be able to format allocation pool string and skip range that does not match CIDR' do
+      expect(@scope.function_format_allocation_pools([["10.109.1.151:10.109.1.254", "10.110.1.130:10.110.1.150"], "10.109.1.0/24"])).to eq(["start=10.109.1.151,end=10.109.1.254"])
     end
   end
 end
