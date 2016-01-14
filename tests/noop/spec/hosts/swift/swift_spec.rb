00b90dcf5b150cf8926bb580df44304e1de5b895
iberezovskiy <iberezovskiy@mirantis.com>
2016-01-14 20:47:44 +0300

    Update puppet-swift module to currnet Mitaka version
    Puppet-swift module doesn't containt any custom patches.
    Its state is upstream state on 11 Jan (latest current commit).
    
    Change-Id: Ib9571215d77f7fa3585aae32dd186a9a11520b97

--- tests/noop/spec/hosts/swift/swift_spec.rb
+++ tests/noop/spec/hosts/swift/swift_spec.rb
@@ -77,8 +77,8 @@ describe manifest do
           end
         end
         ['account', 'object', 'container'].each do | ring |
-          it "should define swift::ringbuilder::rebalance[#{ring}] before Service[swift-proxy]" do
-            should contain_swift__ringbuilder__rebalance(ring).that_comes_before('Service[swift-proxy]')
+          it "should define swift::ringbuilder::rebalance[#{ring}] before swift proxy service" do
+            should contain_swift__ringbuilder__rebalance(ring).that_comes_before('Service[swift-proxy-server]')
           end
         end
         ['account', 'object', 'container'].each do | ring |
