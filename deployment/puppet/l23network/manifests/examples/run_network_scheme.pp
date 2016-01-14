5036a3c11f2e4b00be15cebf804c19b8ccad9e97
Maksim Malchuk <mmalchuk@mirantis.com>
2016-01-14 09:54:16 +0300

    Fix some misspellings in the l23network module
    Contain only fix for some misspellings and puppet code cleanup.
    
    Change-Id: I1e042048ab438b4254ee4a8d02aecdcaa6bb9332
    Closes-Bug: #1533357
    Partial-Bug: #1533240

--- deployment/puppet/l23network/manifests/examples/run_network_scheme.pp
+++ deployment/puppet/l23network/manifests/examples/run_network_scheme.pp
@@ -3,11 +3,10 @@ class l23network::examples::run_network_scheme (
   $settings_yaml
 ){
 
-    class {'l23network': }
+    class {'::l23network': }
 
     $config = parseyaml($settings_yaml)
     prepare_network_config($config['network_scheme'])
     $sdn = generate_network_config()
     notice("SDN ${sdn}")
 }
-###
