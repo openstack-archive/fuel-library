c4c011b37eebc404e37803c6f62f8b334c0b0c6a
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-01-15 17:43:48 +0300

    Tighten frontend nginx and haproxy TLS ciphersuites
    To get rid from last VUWACONA vulnerabilities, add tight protocol
    and ciphersuite requisitions on UI HTTPS nginx and TLS-aware HAProxy
    instances.
    
    Also create new Diffie-Hellmann parameters for frontend nginx.
    
    Closes-Bug: #1532778
    Change-Id: Ib851038b883a84f4d59c3e4db8cc25608e215821

--- deployment/puppet/openstack/manifests/ha/haproxy_service.pp
+++ deployment/puppet/openstack/manifests/ha/haproxy_service.pp
@@ -112,7 +112,7 @@ define openstack::ha::haproxy_service (
   if $public {
     $public_bind_address = suffix(any2array($public_virtual_ip), ":${listen_port}")
     if $public_ssl {
-      $public_bind = array_to_hash($public_bind_address, ['ssl', 'crt', $public_ssl_path])
+      $public_bind = array_to_hash($public_bind_address, ['ssl', 'crt', $public_ssl_path, 'no-sslv3', 'no-tlsv10', 'no-tlsv11', 'no-tls-tickets', 'force-tlsv12', 'ciphers AES128+EECDH:AES128+EDH:AES256+EECDH:AES256+EDH:!SSLv3'])
     } else {
       $public_bind = array_to_hash($public_bind_address, "")
     }
@@ -122,7 +122,7 @@ define openstack::ha::haproxy_service (
   if $internal {
     $internal_bind_address = suffix(any2array($internal_virtual_ip), ":$listen_port")
     if $internal_ssl {
-      $internal_bind = array_to_hash($internal_bind_address, ['ssl', 'crt', $internal_ssl_path])
+      $internal_bind = array_to_hash($internal_bind_address, ['ssl', 'crt', $internal_ssl_path, 'no-sslv3', 'no-tlsv10', 'no-tlsv11', 'no-tls-tickets', 'force-tlsv12', 'ciphers AES128+EECDH:AES128+EDH:AES256+EECDH:AES256+EDH:!SSLv3'])
     } else {
       $internal_bind = array_to_hash($internal_bind_address, "")
     }
