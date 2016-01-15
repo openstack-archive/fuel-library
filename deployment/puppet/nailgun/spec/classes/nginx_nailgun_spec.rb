fae780ea983d0b2fa5e98469f242517af0c4434e
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-01-15 17:43:48 +0300

    Tighten frontend nginx and haproxy TLS ciphersuites
    To get rid from last VUWACONA vulnerabilities, add tight protocol
    and ciphersuite requisitions on UI HTTPS nginx and TLS-aware HAProxy
    instances.
    
    Also create new Diffie-Hellmann parameters for frontend nginx.
    
    Closes-Bug: #1532778
    Change-Id: Ib851038b883a84f4d59c3e4db8cc25608e215821

---
+++ deployment/puppet/nailgun/spec/classes/nginx_nailgun_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'nailgun::nginx-nailgun' do
+  context 'when TLS enabled' do
+    let :params do
+    {
+        :staticdir => '/var/www/static/',
+        :logdumpdir => '/var/log/',
+        :ssl_enabled => true,
+    }
+    end
+
+    it 'should create new Diffie-Hellmann parameters file' do
+      should contain_exec('create new dhparam file')
+    end
+  end
+end
