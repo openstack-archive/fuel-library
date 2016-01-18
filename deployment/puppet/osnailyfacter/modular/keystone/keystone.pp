de5011aaab4e7e7d358289320b2524d3aaab88e3
Nikolay Starodubtsev <nstarodubtsev@mirantis.com>
2016-01-18 13:55:44 +0300

    Glance artifacts repository can be properly enabled
    Previously environment variable 'MURANO_PACKAGES_SERVICE' was missed,
    and GLARE can't be properly configured even if it was enabled during
    deployment.
    
    Change-Id: I8bec86da2617d7fe315f9d7f65e26bf602b2244f
    Related-Bug: #1534583

--- deployment/puppet/osnailyfacter/modular/keystone/keystone.pp
+++ deployment/puppet/osnailyfacter/modular/keystone/keystone.pp
@@ -96,6 +96,14 @@ if has_key($murano_settings_hash, 'murano_repo_url') {
   $murano_repo_url = 'http://storage.apps.openstack.org'
 }
 
+$murano_hash    = hiera_hash('murano_hash', {})
+$murano_plugins = pick($murano_hash['plugins'], {})
+if $murano_plugins and $murano_plugins['glance_artifacts_plugin'] and $murano_plugins['glance_artifacts_plugin']['enabled'] {
+  $murano_glare_plugin = $murano_plugins['glance_artifacts_plugin']['enabled']
+} else {
+  $murano_glare_plugin = false
+}
+
 $external_lb = hiera('external_lb', false)
 
 ###############################################################################
@@ -190,12 +198,13 @@ class { 'keystone::roles::admin':
 }
 
 class { 'openstack::auth_file':
-  admin_user      => $admin_user,
-  admin_password  => $admin_password,
-  admin_tenant    => $admin_tenant,
-  region_name     => $region,
-  controller_node => $internal_address,
-  murano_repo_url => $murano_repo_url,
+  admin_user          => $admin_user,
+  admin_password      => $admin_password,
+  admin_tenant        => $admin_tenant,
+  region_name         => $region,
+  controller_node     => $internal_address,
+  murano_repo_url     => $murano_repo_url,
+  murano_glare_plugin => $murano_glare_plugin,
 }
 
 # Get paste.ini source
