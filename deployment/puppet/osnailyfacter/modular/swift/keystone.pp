d0f7db8e9654e2410af1055edbb4e612f21f93b0
Kyrylo Galanov <kgalanov@mirantis.com>
2016-01-19 15:36:28 +0200

    Handle configure_user params for Heat/Swift
    If deployment doesn't need to create users inside keystone - we have
    corresponding flags in Hiera.
    
    Depends-On: I15a9fa7f56552b63d62377d60a03d3857977872a
    Change-Id: I18d9e9760e9610637f1c8e757bb69234de2c9ce2
    Closes-Bug: #1527517

--- deployment/puppet/osnailyfacter/modular/swift/keystone.pp
+++ deployment/puppet/osnailyfacter/modular/swift/keystone.pp
@@ -17,11 +17,13 @@ $internal_address   = get_ssl_property($ssl_hash, {}, 'swift', 'internal', 'host
 $admin_protocol  = get_ssl_property($ssl_hash, {}, 'swift', 'admin', 'protocol', 'http')
 $admin_address   = get_ssl_property($ssl_hash, {}, 'swift', 'admin', 'hostname', [$management_vip])
 
-$password           = $swift_hash['user_password']
-$auth_name          = pick($swift_hash['auth_name'], 'swift')
-$configure_endpoint = pick($swift_hash['configure_endpoint'], true)
-$service_name       = pick($swift_hash['service_name'], 'swift')
-$tenant             = pick($swift_hash['tenant'], 'services')
+$password            = $swift_hash['user_password']
+$auth_name           = pick($swift_hash['auth_name'], 'swift')
+$configure_endpoint  = pick($swift_hash['configure_endpoint'], true)
+$configure_user      = pick($swift_hash['configure_user'], true)
+$configure_user_role = pick($swift_hash['configure_user_role'], true)
+$service_name        = pick($swift_hash['service_name'], 'swift')
+$tenant              = pick($swift_hash['tenant'], 'services')
 
 Class['::osnailyfacter::wait_for_keystone_backends'] -> Class['::swift::keystone::auth']
 
@@ -39,16 +41,18 @@ $admin_url_s3        = "${admin_protocol}://${admin_address}:8080"
 
 class {'::osnailyfacter::wait_for_keystone_backends':}
 class { '::swift::keystone::auth':
-  password           => $password,
-  auth_name          => $auth_name,
-  configure_endpoint => $configure_endpoint,
-  service_name       => $service_name,
-  public_url         => $public_url,
-  internal_url       => $internal_url,
-  admin_url          => $admin_url,
-  public_url_s3      => $public_url_s3,
-  internal_url_s3    => $internal_url_s3,
-  admin_url_s3       => $admin_url_s3,
-  region             => $region,
-  tenant             => $tenant,
+  password            => $password,
+  auth_name           => $auth_name,
+  configure_endpoint  => $configure_endpoint,
+  configure_user      => $configure_user,
+  configure_user_role => $configure_user_role,
+  service_name        => $service_name,
+  public_url          => $public_url,
+  internal_url        => $internal_url,
+  admin_url           => $admin_url,
+  public_url_s3       => $public_url_s3,
+  internal_url_s3     => $internal_url_s3,
+  admin_url_s3        => $admin_url_s3,
+  region              => $region,
+  tenant              => $tenant,
 }
