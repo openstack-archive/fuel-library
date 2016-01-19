32c4f78ce86fc34d4881b89dc1ffb35ddf31a3d6
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-01-19 19:29:50 +0300

    (WIP) set os_package_type fact to ubuntu
    This change also includes a pin for the haproxy package since UCA's
    version of haproxy is not compatible with fuel.
    
    Also pinned ceph to 0.94.5 instead of 8.0 from UCA.
    
    blueprint puppet-mitaka
    
    Change-Id: Icb42960d51b526b79da6deec4f46dc9fd871be2a

--- deployment/puppet/osnailyfacter/modular/fuel_pkgs/setup_repositories.pp
+++ deployment/puppet/osnailyfacter/modular/fuel_pkgs/setup_repositories.pp
@@ -2,7 +2,9 @@ notice('MODULAR: setup_repositories.pp')
 
 $repo_setup = hiera('repo_setup', {})
 $repos = $repo_setup['repos']
-
+$ceph_packages = ['ceph', 'ceph-common', 'libradosstriper1',
+  'python-rbd', 'python-rados', 'python-cephfs', 'libcephfs1', 'librados2',
+  'librbd1']
 if $::osfamily == 'Debian' {
   include ::apt
 
@@ -23,5 +25,27 @@ if $::osfamily == 'Debian' {
     notify_update => false,
   }
 
+  # TODO(aschultz): we need the mos fork haproxy when using UCA
+  apt::pin { 'haproxy-mos':
+    packages => 'haproxy',
+    version  => '1.5.3-*',
+    priority => '2000',
+  }
+
+  # TODO(mattymo): we need the mos fork ceph when using UCA
+  apt::pin { 'ceph-mos':
+    packages => $ceph_packages,
+    version  => '0.94*',
+    priority => '2000',
+  }
+
+  # TODO(mattymo): we need the mos fork rabbitmq when using UCA
+  apt::pin { 'rabbitmq-server-mos':
+    packages => 'rabbitmq-server',
+    version  => '3.5.6-*',
+    priority => '2000',
+  }
+
   Apt::Source<||> ~> Exec<| title == 'apt_update' |>
 }
+
