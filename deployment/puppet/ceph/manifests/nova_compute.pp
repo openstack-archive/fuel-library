730e8e5efbe1134a44e1c9140e4476b10b0e2746
Michael Polenchuk <mpolenchuk@mirantis.com>
2016-01-19 12:27:56 +0300

    Set ceph rbd secret unless it doesn't exist
    Avoid apply of exec/file resources every run.
    (fixed idempotency of ceph + nova_compute task)
    
    Change-Id: I973e7d6bc4fcbc23199303bfc184beea82ad6d46
    Closes-Bug: #1526899

--- deployment/puppet/ceph/manifests/nova_compute.pp
+++ deployment/puppet/ceph/manifests/nova_compute.pp
@@ -3,26 +3,25 @@ class ceph::nova_compute (
   $rbd_secret_uuid     = $::ceph::rbd_secret_uuid,
   $user                = $::ceph::compute_user,
   $compute_pool        = $::ceph::compute_pool,
+  $secret_xml          = '/root/.secret_attrs.xml',
 ) {
 
-  file {'/root/secret.xml':
+  file { $secret_xml:
+    mode    => '0400',
     content => template('ceph/secret.erb')
   }
 
-  if !defined(Service['libvirt'] ) {
-    service { 'libvirt':
-      name   => $::ceph::params::libvirt_service_name,
-      ensure => 'running',
-    }
-  }
+  ensure_resource('service', 'libvirt', {
+    name   => $::ceph::params::libvirt_service_name,
+    ensure => 'running',
+  })
 
   exec {'Set Ceph RBD secret for Nova':
     # TODO: clean this command up
-    command => "virsh secret-set-value --secret $( \
-      virsh secret-define --file /root/secret.xml | \
-      egrep -o '[0-9a-fA-F]{8}(-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}') \
-      --base64 $(ceph auth get-key client.${user}) && \
-      rm /root/secret.xml",
+    command => "virsh secret-define --file $secret_xml && \
+      virsh secret-set-value --secret ${rbd_secret_uuid} \
+      --base64 $(ceph auth get-key client.${user})",
+    unless => "virsh secret-list | fgrep -qw ${rbd_secret_uuid}",
   }
 
   nova_config {
@@ -30,6 +29,6 @@ class ceph::nova_compute (
     'libvirt/rbd_user':                 value => $user;
   }
 
-  File['/root/secret.xml'] ->
+  File[$secret_xml] ->
   Service['libvirt'] -> Exec['Set Ceph RBD secret for Nova']
 }
