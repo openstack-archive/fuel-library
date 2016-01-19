49199bbba2cf46fa104d816059a38dbae8c92c35
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-01-19 21:31:54 +0300

    Change the way to reach internal and public addresses from test
    Change-Id: I104e4a758d97ca622b8ca643d0e2a8638815d5a4
    Closes-Bug: #1523585

--- deployment/puppet/osnailyfacter/modular/memcached/memcached_post.rb
+++ deployment/puppet/osnailyfacter/modular/memcached/memcached_post.rb
@@ -5,13 +5,22 @@ class MemcachedPostTest < Test::Unit::TestCase
     assert TestCommon::Process.running?('memcached'), 'Memcached is not running!'
   end
 
+  def get_node
+    nodes = TestCommon::Settings.nodes
+    uid = TestCommon::Settings.uid
+    nodes.each do |node|
+      next if node['uid'] != uid
+      return node
+    end
+  end
+
   def test_memcached_on_internal
-    ip = TestCommon::Settings.internal_address
+		ip = get_node['internal_address']
     assert TestCommon::Network.connection?(ip, 11211), 'Cannot connect to memcached on the internal address!'
   end
 
   def test_memcached_no_public
-    ip = TestCommon::Settings.public_address
+		ip = get_node['public_address']
     assert TestCommon::Network.no_connection?(ip, 11211), 'Memcached should not be accessible from the public network!'
   end
 end
