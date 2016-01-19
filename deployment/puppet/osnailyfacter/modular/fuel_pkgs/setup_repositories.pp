346a95dd3a3fec9653d293ebf7e564cf28c7c852
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-01-19 19:29:50 +0300

    (WIP) set os_package_type fact to ubuntu
    This change also includes a pin for the haproxy package since UCA's
    version of haproxy is not compatible with fuel.
    
    Also pinned ceph to 0.94.5 instead of 8.0 from UCA.
    
    blueprint puppet-mitaka
    
    Change-Id: Icb42960d51b526b79da6deec4f46dc9fd871be2a

--- deployment/puppet/osnailyfacter/modular/fuel_pkgs/setup_repositories.pp
+++ deployment/puppet/osnailyfacter/modular/fuel_pkgs/setup_repositories.pp
@@ -23,5 +23,19 @@ if $::osfamily == 'Debian' {
     notify_update => false,
   }
 
+  # TODO(aschultz): we need the mos fork haproxy when using UCA
+  apt::pin { 'haproxy-mos':
+    packages => 'haproxy',
+    version  => '1.5.3-*',
+    priority => '2000',
+  }
+
+  # TODO(mattymo): we need the mos fork haproxy when using UCA
+  apt::pin { 'ceph-mos':
+    packages => 'ceph',
+    version  => '0.94.5-*',
+    priority => '2000',
+  }
+
   Apt::Source<||> ~> Exec<| title == 'apt_update' |>
 }
