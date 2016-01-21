8a8ae560e2ff92c08f12f40b63dd0c2e3b4eb7c8
Maksim Malchuk <mmalchuk@mirantis.com>
2016-01-21 20:28:45 +0300

    Increase kernel.pid_max value for ceph-osd nodes
    To meet Ceph requirements and eliminate accidental cascade
    OSD node failures this commit increases kernel.pid_max
    parameter during deploy ceph-osd nodes. Also this commit
    contain some puppet-lint cleanups for this module.
    
    Change-Id: I70a64ff7b85229bf0760be266302d9b32039989a
    Closes-Bug: #1536271

--- deployment/puppet/ceph/manifests/init.pp
+++ deployment/puppet/ceph/manifests/init.pp
@@ -3,8 +3,8 @@
 
 class ceph (
 # General settings
-  $mon_hosts,
-  $mon_ip_addresses,
+  $mon_hosts                          = undef,
+  $mon_ip_addresses                   = undef,
   $cluster_node_address               = $::ipaddress, # This should be the cluster service address
   $primary_mon                        = $::hostname,  # This should be the first controller
   $mon_addr                           = $::ipaddress, # This needs to be replaced with the address we want to bind the mon to (if this is a mon)
@@ -107,8 +107,8 @@ class ceph (
 
   if roles_include(['primary-controller', 'controller', 'ceph-mon', 'ceph-osd']) {
     service { 'ceph':
-      name    => $ceph::params::service_name,
       ensure  => 'running',
+      name    => $ceph::params::service_name,
       enable  => true,
       require => Class['ceph::conf']
     }
@@ -140,6 +140,12 @@ class ceph (
       include ceph::osds
       Class['ceph::conf'] -> Class['ceph::osds']
       Ceph_conf <||> ~> Service['ceph']
+
+      sysctl::value { 'kernel.pid_max':
+        value  => '4194303',
+      }
+
+      Sysctl::Value <| |> -> Service['ceph']
     }
   }
 
