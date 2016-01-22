a6edc7bf0f77b6339c5cca4011888a31b5013fc0
Sergey Kulanov <skulanov@mirantis.com>
2016-01-22 12:03:11 +0200

    [WIP] Remove CentOS based bootstrap image
    Related-bug: #1536658
    Implements-bp: #remove-centos-bootstrap-from-fuel
    
    Change-Id: I78eeecebebdd18de12745160ce85270528eea99b
    Fuel-CI: disable

--- deployment/puppet/nailgun/manifests/cobbler.pp
+++ deployment/puppet/nailgun/manifests/cobbler.pp
@@ -3,7 +3,6 @@
 class nailgun::cobbler(
   $cobbler_user                  = 'cobbler',
   $cobbler_password              = 'cobbler',
-  $bootstrap_flavor              = 'centos',
   $bootstrap_path,
   $bootstrap_meta,
   # network interface configuration timeout (in seconds)
@@ -40,11 +39,7 @@ class nailgun::cobbler(
   #Set real_server so Cobbler identifies its own IP correctly in Docker
   $real_server = $next_server
 
-  $bootstrap_profile = $bootstrap_flavor ? {
-    /(?i)centos/                 => 'bootstrap',
-    /(?i)ubuntu/                 => 'ubuntu_bootstrap',
-    default                      => 'bootstrap',
-  }
+  $bootstrap_profile = 'ubuntu_bootstrap'
 
   if $::osfamily == 'RedHat' {
     case $operatingsystemmajrelease {
@@ -109,29 +104,9 @@ class nailgun::cobbler(
     require => Class['::cobbler::server'],
   }
 
-  # THIS VARIABLE IS NEEDED FOR TEMPLATING centos-x86_64.ks
-  $ks_repo = $centos_repos
 
   case $production {
     'prod', 'docker': {
-
-      file { '/var/lib/cobbler/kickstarts/centos-x86_64.ks':
-        content => template('cobbler/kickstart/centos.ks.erb'),
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        require => Class['::cobbler::server'],
-      } ->
-
-      cobbler_distro { 'centos-x86_64':
-        kernel    => "${repo_root}/centos/x86_64/isolinux/vmlinuz",
-        initrd    => "${repo_root}/centos/x86_64/isolinux/initrd.img",
-        arch      => 'x86_64',
-        breed     => 'redhat',
-        osversion => 'rhel6',
-        ksmeta    => 'tree=http://@@server@@:8080/centos/x86_64/',
-        require   => Class['::cobbler::server'],
-      }
 
       file { '/var/lib/cobbler/kickstarts/ubuntu-amd64.preseed':
         content => template('cobbler/preseed/ubuntu-1404.preseed.erb'),
@@ -151,16 +126,6 @@ class nailgun::cobbler(
         require   => Class['::cobbler::server'],
       }
 
-      cobbler_profile { 'centos-x86_64':
-        kickstart => '/var/lib/cobbler/kickstarts/centos-x86_64.ks',
-        kopts     => 'biosdevname=0 sshd=1 dhcptimeout=120',
-        distro    => 'centos-x86_64',
-        ksmeta    => '',
-        menu      => false,
-        server    => $real_server,
-        require   => Cobbler_distro['centos-x86_64'],
-      }
-
       cobbler_profile { 'ubuntu_1404_x86_64':
         kickstart => '/var/lib/cobbler/kickstarts/ubuntu-amd64.preseed',
         kopts     => 'net.ifnames=0 biosdevname=0 netcfg/choose_interface=eth0 netcfg/dhcp_timeout=120 netcfg/link_detection_timeout=20',
@@ -171,16 +136,6 @@ class nailgun::cobbler(
         require   => Cobbler_distro['ubuntu_1404_x86_64'],
       }
 
-      cobbler_distro { 'bootstrap':
-        kernel    => "${repo_root}/bootstrap/linux",
-        initrd    => "${repo_root}/bootstrap/initramfs.img",
-        arch      => 'x86_64',
-        breed     => 'redhat',
-        osversion => 'rhel6',
-        ksmeta    => '',
-        require   => Class['::cobbler::server'],
-      }
-
       cobbler_distro { 'ubuntu_bootstrap':
         kernel    => "${bootstrap_path}/vmlinuz",
         initrd    => "${bootstrap_path}/initrd.img",
@@ -189,16 +144,6 @@ class nailgun::cobbler(
         osversion => 'trusty',
         ksmeta    => '',
         require   => Class['::cobbler::server'],
-      }
-
-      cobbler_profile { 'bootstrap':
-        distro    => 'bootstrap',
-        menu      => true,
-        kickstart => '',
-        kopts     => "intel_pstate=disable console=ttyS0,9600 console=tty0 biosdevname=0 url=${nailgun_api_url} mco_user=${mco_user} mco_pass=${mco_pass}",
-        ksmeta    => '',
-        server    => $real_server,
-        require   => Cobbler_distro['bootstrap'],
       }
 
       cobbler_profile { 'ubuntu_bootstrap':
@@ -223,7 +168,7 @@ class nailgun::cobbler(
       exec { 'cobbler_system_edit_default':
         command => "cobbler system edit --name=default \
                     --profile=${bootstrap_profile} --netboot-enabled=True",
-        unless => "cobbler system report --name default 2>/dev/null | grep -q -E '^Profile\\s*:\\s*${bootstrap_profile}'",
+        unless  => "cobbler system report --name default 2>/dev/null | grep -q -E '^Profile\\s*:\\s*${bootstrap_profile}'",
         require => Cobbler_profile[$bootstrap_profile],
       }
 
