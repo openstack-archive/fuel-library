7ffdbb62ec363ed141d78649821eac42b5d48d08
Ivan Suzdal <isuzdal@mirantis.com>
2016-01-25 20:10:09 +0300

    Enable and ensure running for docker* services
    Change-Id: I089dcf8025af91e4cc2e37dd273a385e0274d143

--- deployment/puppet/docker/manifests/systemd/config.pp
+++ deployment/puppet/docker/manifests/systemd/config.pp
@@ -29,7 +29,6 @@ define docker::systemd::config( $release, $depends, $timeout ) {
   # We use ensure => undef to prevent unnecessary start service
   # because at first boot time, the container is launched by dockerctl
   service { "docker-${title}":
-    ensure => undef,
     enable => true,
   }
 }
