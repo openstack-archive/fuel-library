cfca960392b547484e31498a8d0228eb06ff8f49
Maksim Malchuk <mmalchuk@mirantis.com>
2016-01-26 02:30:32 +0300

    Disable recommends and suggests package types from Ubuntu install
    This commit should reduce the amount of packages being
    downloaded and installed during the deployment process.
    
    Change-Id: Ib5357b1af93e0cb76fd69789147a216e276f1246
    Closes-Bug: #1517931

--- deployment/puppet/osnailyfacter/modular/fuel_pkgs/setup_repositories.pp
+++ deployment/puppet/osnailyfacter/modular/fuel_pkgs/setup_repositories.pp
@@ -1,7 +1,7 @@
 notice('MODULAR: setup_repositories.pp')
 
 $repo_setup = hiera('repo_setup', {})
-$repos = $repo_setup['repos']
+$repos      = $repo_setup['repos']
 
 if $::osfamily == 'Debian' {
   include ::apt
@@ -17,10 +17,21 @@ if $::osfamily == 'Debian' {
     create_resources(apt::pin, $pins)
   }
 
-  apt::conf { 'AllowUnathenticated':
-    content       => 'APT::Get::AllowUnauthenticated 1;',
-    priority      => '02',
+  Apt::Conf {
     notify_update => false,
+    priority      => '02',
+  }
+
+  apt::conf { 'allow-unathenticated':
+    content => 'APT::Get::AllowUnauthenticated 1;',
+  }
+
+  apt::conf { 'install-recommends':
+    content => 'APT::Install-Recommends "false";',
+  }
+
+  apt::conf { 'install-suggests':
+    content => 'APT::Install-Suggests "false";',
   }
 
   Apt::Source<||> ~> Exec<| title == 'apt_update' |>
--- tests/noop/spec/hosts/fuel_pkgs/setup_repositories_spec.rb
+++ tests/noop/spec/hosts/fuel_pkgs/setup_repositories_spec.rb
@@ -3,13 +3,27 @@ require 'shared-examples'
 manifest = 'fuel_pkgs/setup_repositories.pp'
 
 describe manifest do
-  before(:each) do
-    Noop.puppet_function_load :generate_apt_pins
-    MockFunction.new(:generate_apt_pins) do |function|
-      allow(function).to receive(:call).and_return({})
+  shared_examples 'catalog' do
+
+    before(:each) do
+      Noop.puppet_function_load :generate_apt_pins
+      MockFunction.new(:generate_apt_pins) do |function|
+        allow(function).to receive(:call).and_return({})
+      end
     end
+
+    it 'apt-get should allow unathenticated packages' do
+      should contain_apt__conf('allow-unathenticated').with_content('APT::Get::AllowUnauthenticated 1;')
+    end
+
+    it 'apt-get shouldn\'t install recommended packages' do
+      should contain_apt__conf('install-recommends').with_content('APT::Install-Recommends "false";')
+    end
+
+    it 'apt-get shouldn\'t install suggested packages' do
+      should contain_apt__conf('install-suggests').with_content('APT::Install-Suggests "false";')
+    end
+
   end
-
-  test_ubuntu_and_centos manifest
+  test_ubuntu manifest
 end
-
