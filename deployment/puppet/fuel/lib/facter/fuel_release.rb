dee326b04d12bd27ef1f9052eb10f3eea4f80282
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2016-01-25 16:35:59 +0300

    Add fuel puppet module
    This module is a fork of nailgun puppet module with
    some simplifications that make it easier to use and
    maintain.  Fuel master node is to be deployed using
    kind of task based procedure when {task}.pp
    are used one by one.
    
    Implements blueprint: get-rid-docker-containers
    Change-Id: I0139cad1c2ebb0cc846c8bca560533b0ac6489cf

---
+++ deployment/puppet/fuel/lib/facter/fuel_release.rb
@@ -0,0 +1,16 @@
+require 'facter'
+
+fuel_release_path = '/etc/fuel_release'
+# FIXME(kozhukalov): This default value is necessary
+# to solve chicken/egg problem. The thing is that
+# deployment tests use the ISO where this file is not
+# installed. This default value should be removed
+# once the ISO is updated.
+read_data = "8.0"
+
+Facter.add('fuel_release') do
+  if File.exist?(fuel_release_path)
+    read_data = File.read(fuel_release_path).strip
+  end
+  setcode { read_data }
+end
