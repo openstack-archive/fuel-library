5b5d4d4e6366fcda9bc33199d6ee43f7c39909a3
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2016-01-25 16:35:59 +0300

    Add fuel puppet module
    This module is a fork of nailgun puppet module with
    some simplifications that make it easier to use and
    maintain.  Fuel master node is to be deployed using
    kind of task based procedure when {task}.pp
    are used one by one.
    
    Implements blueprint: get-rid-docker-containers
    Change-Id: I0139cad1c2ebb0cc846c8bca560533b0ac6489cf

---
+++ deployment/puppet/fuel/examples/client.pp
@@ -0,0 +1,19 @@
+notice('MODULAR: client.pp')
+
+$fuel_settings = parseyaml($astute_settings_yaml)
+
+class { "fuel::nailgun::client":
+  server_address     => $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
+  keystone_user      => $::fuel_settings['FUEL_ACCESS']['user'],
+  keystone_password  => $::fuel_settings['FUEL_ACCESS']['password'],
+}
+
+# This exec needs python-fuelclient to be installed and nailgun running
+# Probably this should be moved to a separate task
+exec {'sync_deployment_tasks':
+  command   => 'fuel rel --sync-deployment-tasks --dir /etc/puppet/',
+  path      => '/usr/bin',
+  tries     => 12,
+  try_sleep => 10,
+  require   => Class['fuel::nailgun::client']
+}
