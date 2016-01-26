d1edb377cf2ba470dbfcf15f61b9f1598cbbbd6c
Denis V. Meltsaykin <dmeltsaykin@mirantis.com>
2016-01-26 12:12:10 +0000

    Revert "Clean rabbitmq dump of auto-delete queues"
    This reverts commit 50f973981227dbab143e1286475689350ead5841, which
    introduces regression during a controller addition to the existing
    environment, as puppet expects to use this new python-script and
    may fail if the packages were not updated on _every_ controller.
    
    Change-Id: Icac4aecf35bf82f8a7ffa8d98a6368f541b02a3e
    Related-Bug: #1497961

--- tests/noop/spec/hosts/astute/dump_rabbitmq_definitions_spec.rb
+++ tests/noop/spec/hosts/astute/dump_rabbitmq_definitions_spec.rb
@@ -6,14 +6,6 @@ describe manifest do
   shared_examples 'catalog' do
     it "should contain rabbitmq dump definitions exec" do
       should contain_exec('rabbitmq-dump-definitions')
-      should contain_exec('rabbitmq-dump-clean')
-      ['/etc/rabbitmq/definitions', '/etc/rabbitmq/definitions.full'].each do |f|
-        should contain_file(f).with(
-          :ensure => 'file',
-          :owner  => 'root',
-          :group  => 'root',
-          :mode   => '0600')
-      end
     end
   end
   test_ubuntu_and_centos manifest
