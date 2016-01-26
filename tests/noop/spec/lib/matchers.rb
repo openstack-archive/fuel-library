8a1de00c1bacd9a1dd7fee001d0a9e097e977d68
Vladimir Kuklin <vkuklin@mirantis.com>
2016-01-26 20:14:21 +0300

    Refactor pieces of noop testing framework
    This patch does the following:
    
    1) Adds puppet_spec library to a list of fetched
    specs to leverage some of additional matchers
    2) Makes spec_helper load puppet core spec directory
    which may also contain things we would want to reuse
    3) Introduces new matcher for transitional dependencies
    that allows one to not search for exact dependencies
    with `that_comes_before` matcher, but just check for
    resources dependencies
    
    Change-Id: Iad3b6f31a4c5be1ec306d9b4e7eedbd2cf720e95
    Related-bug: #1533279

---
+++ tests/noop/spec/lib/matchers.rb
@@ -0,0 +1,40 @@
+module FuelRelationshipGraphMatchers
+  class EnsureTransitionalDependency
+    def initialize(before, after)
+      @before = before
+      @after = after
+    end
+
+    def matches?(actual_graph)
+      @actual_graph = actual_graph
+
+      @dependents = actual_graph.dependents(
+        vertex_called(actual_graph, @before))
+      !@dependents.find_all { |d| d.ref =~ /#{Regexp.escape(@after)}/i }.empty?
+    end
+
+    def failure_message
+      msg = "expected deployment graph to contain a transitional dependency between\n"
+      msg << "#{@before} and #{@after} but it did not happen\n"
+      msg << "#{@before} dependents are: #{@dependents}\n"
+      msg
+    end
+
+    def failure_message_when_negated
+      msg = "expected deployment graph to NOT contain a transitional dependency between\n"
+      msg << "#{@before} and #{@after} but it did not happen\n"
+      msg << "#{@before} dependents are: #{@dependents}\n"
+      msg
+    end
+
+    private
+
+    def vertex_called(graph, name)
+      graph.vertices.find { |v| v.ref =~ /#{Regexp.escape(name)}/i }
+    end
+  end
+
+  def ensure_transitional_dependency(before, after)
+    EnsureTransitionalDependency.new(before, after)
+  end
+end
