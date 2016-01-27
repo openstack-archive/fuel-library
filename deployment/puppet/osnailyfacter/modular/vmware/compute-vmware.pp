63a7fc4301b68963fae7a1d205a0f90ff9a958d4
Igor Zinovik <izinovik@mirantis.com>
2016-01-27 16:25:17 +0300

    Fix manifest behaviour on hostname change
    Manifest relies on fact that 'target_node' value that is provided by
    Nailgun will always match pattern 'node-[0-9]', but backend provides
    hostname that can be changed by user.
    
    Closes-bug: #1534851
    Change-Id: I4a9984d683bd741785e1f9f70476ed74fcc0c478
    Cherry-picked from 60315936c

--- deployment/puppet/osnailyfacter/modular/vmware/compute-vmware.pp
+++ deployment/puppet/osnailyfacter/modular/vmware/compute-vmware.pp
@@ -8,11 +8,9 @@ $ceilometer_hash = hiera('ceilometer',{})
 $vcenter_hash = hiera('vcenter', {})
 $computes_hash = parse_vcenter_settings($vcenter_hash['computes'])
 
-$uid = hiera('uid')
-$node_name = "node-$uid"
 $defaults = {
-    current_node => $node_name,
-    vlan_interface   => $vcenter_hash['esxi_vlan_interface']
-            }
+  current_node   => hiera('node_name'),
+  vlan_interface => $vcenter_hash['esxi_vlan_interface']
+}
 
 create_resources(vmware::compute_vmware, $computes_hash, $defaults)
