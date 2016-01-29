87c550173093730e7fe756da37c429f43efa9472
Aleksandr Didenko <adidenko@mirantis.com>
2016-01-29 18:02:33 +0100

    Add noop astute.yaml fixture for external LB
    - Add new fixture with disabled VIPs creataion (i.e. external-lb)
    - Fix some outdated rspec tests to make them work properly with
      more recent fixture.
    
    Disabled integration tests since this patch affects noops only.
    
    Change-Id: I6c369cdc0262d7a681deda2a5a9ac58bdcea7136
    Fuel-CI: disable
    Partial-bug: #1535339

--- tests/noop/spec/hosts/virtual_ips/virtual_ips_spec.rb
+++ tests/noop/spec/hosts/virtual_ips/virtual_ips_spec.rb
@@ -9,9 +9,10 @@ describe manifest do
     Noop.hiera_structure('network_metadata/vips', {}).each do |name, params|
       next unless params['network_role']
       next unless params['node_roles']
-
-      it "should have '#{name}' VIP" do
-        expect(subject).to contain_cluster__virtual_ip(name)
+      if params['namespace']
+        it "should have '#{name}' VIP" do
+          expect(subject).to contain_cluster__virtual_ip(name)
+        end
       end
     end
   end
