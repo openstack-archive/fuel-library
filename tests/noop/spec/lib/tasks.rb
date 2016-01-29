3b945cbffe724222cc50f1ca0261e25fbced9e41
Maksim Malchuk <mmalchuk@mirantis.com>
2016-01-29 11:56:04 +0300

    Suppord wildcards in the tasks roles
    This commit adds wildcards support for noop tests.
    
    Change-Id: I2f32fe26bf90656b820c21167faa0e35697bc322
    Closes-Bug: #1539457

--- tests/noop/spec/lib/tasks.rb
+++ tests/noop/spec/lib/tasks.rb
@@ -38,10 +38,12 @@ class Noop
         next unless task['parameters']['puppet_manifest'] == manifest_path
         if task['role']
           return true if task['role'] == '*'
+          return true if task['role'].include?('/.*/')
           return true if task['role'].include?(role)
         end
         if task['groups']
           return true if task['groups'] == '*'
+          return true if task['groups'].include?('/.*/')
           return true if task['groups'].include?(role)
         end
       end
