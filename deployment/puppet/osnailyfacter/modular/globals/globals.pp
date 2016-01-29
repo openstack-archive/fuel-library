1dcdf4de6193c061f0da26f1dd1eba7046c22577
vsaienko <vsaienko@mirantis.com>
2016-01-30 00:29:45 +0200

    [WIP]Adapt ironic manifest
    Adapt ironic manifests to work with updated openstacklib/ironic modules
    
    Change-Id: I0c6e8c1356be231b76454d75db93d21038b88063

--- deployment/puppet/osnailyfacter/modular/globals/globals.pp
+++ deployment/puppet/osnailyfacter/modular/globals/globals.pp
@@ -97,6 +97,7 @@ $syslog_log_facility_heat       = hiera('syslog_log_facility_heat','LOG_LOCAL0')
 $syslog_log_facility_sahara     = hiera('syslog_log_facility_sahara','LOG_LOCAL0')
 $syslog_log_facility_ceilometer = hiera('syslog_log_facility_ceilometer','LOG_LOCAL0')
 $syslog_log_facility_ceph       = hiera('syslog_log_facility_ceph','LOG_LOCAL0')
+$syslog_log_facility_ironic     = hiera('syslog_log_facility_ironic','LOG_LOCAL0')
 
 $nova_report_interval           = hiera('nova_report_interval', 60)
 $nova_service_down_time         = hiera('nova_service_down_time', 180)
