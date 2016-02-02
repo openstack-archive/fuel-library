62f334f5d92e992d2f9b81b8c959c7fc5ca279aa
Vladimir Eremin <veremin@mirantis.com>
2016-02-02 20:33:07 +0300

    Added sriov provider for port
    New provider `sriov` introduced for add-port action.
    It accepts two vendor-specific arguments:
    * sriov_numvfs: number of VF's to enable
    * physnet: physnet to setup in nova-compute
    
    virtfn network interfaces is ignored now.
    
    Change-Id: Id42e09ef2cf955301cd64c28dcfcd60f61ae72af
    Implements: blueprint support-sriov

---
+++ deployment/puppet/l23network/lib/puppet/provider/l23_stored_config_sriov_centos.rb
@@ -0,0 +1,28 @@
+require File.join(File.dirname(__FILE__), '..','..','puppet/provider/l23_stored_config_centos')
+
+class Puppet::Provider::L23_stored_config_sriov_centos < Puppet::Provider::L23_stored_config_centos
+
+  def self.property_mappings
+    super.merge({
+      :sriov_numvfs => 'SRIOV_NUMFS',
+      :device_type  => 'DEVICETYPE',
+    })
+  end
+
+  def device_type
+    :sriov
+  end
+
+  def self.mangle__sriov_numvfs(data)
+    data.to_i
+  end
+
+  def self.unmangle_properties(provider, props)
+    rv = super
+    rv.merge({
+      'TYPE' => rv['DEVICETYPE'],
+    })
+  end
+end
+
+# vim: set ts=2 sw=2 et :
