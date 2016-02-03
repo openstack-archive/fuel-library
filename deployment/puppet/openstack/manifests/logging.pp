274eacef0c5bae91d163f9bd516ee3aca4a6aa1f
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-02-03 18:32:33 +0300

    Set proper rabbitmq filenames for logging
    Add right options to rsyslog for catching rabbitmq messages
    properly.
    
    Change-Id: I825ec437673bed40c88e06af500e2804f0f126b7
    Closes-Bug: #1541397

--- deployment/puppet/openstack/manifests/logging.pp
+++ deployment/puppet/openstack/manifests/logging.pp
@@ -71,14 +71,14 @@ class openstack::logging (
     # Configure logging templates for rsyslog client side
     # Rabbitmq does not support syslogging, use imfile
     ::rsyslog::imfile { "04-rabbitmq" :
-      file_name     => "/var/log/rabbitmq/rabbit@${hostname}.log",
+      file_name     => "/var/log/rabbitmq/rabbit@messaging-${hostname}.log",
       file_tag      => "rabbitmq",
       file_facility => "syslog",
       file_severity => $rabbit_log_level,
     }
 
     ::rsyslog::imfile { "04-rabbitmq-sasl" :
-      file_name     => "/var/log/rabbitmq/rabbit@${hostname}-sasl.log",
+      file_name     => "/var/log/rabbitmq/rabbit@messaging-${hostname}-sasl.log",
       file_tag      => "rabbitmq-sasl",
       file_facility => "syslog",
       file_severity => $rabbit_log_level,
@@ -91,11 +91,25 @@ class openstack::logging (
       file_severity => "ERROR",
     }
 
+    ::rsyslog::imfile { "04-rabbitmq-startup_log" :
+      file_name     => "/var/log/rabbitmq/startup_log",
+      file_tag      => "rabbitmq-startup_log",
+      file_facility => "syslog",
+      file_severity => $rabbit_log_level,
+    }
+
     ::rsyslog::imfile { "04-rabbitmq-shutdown_err" :
       file_name     => "/var/log/rabbitmq/shutdown_err",
       file_tag      => "rabbitmq-shutdown_err",
       file_facility => "syslog",
       file_severity => "ERROR",
+    }
+
+    ::rsyslog::imfile { "04-rabbitmq-shutdown_log" :
+      file_name     => "/var/log/rabbitmq/shutdown_log",
+      file_tag      => "rabbitmq-shutdown_log",
+      file_facility => "syslog",
+      file_severity => $rabbit_log_level,
     }
 
     ::rsyslog::imfile { '05-apache2-error':
