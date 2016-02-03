978850120316ea8fca5700c21dcf7d762055a3dc
Sergey Kolekonov <skolekonov@mirantis.com>
2016-02-03 18:47:04 +0300

    Adapt for Mitaka
    - remove workarounds for Neutron services/packages
    - don't use nova::objectstore class as it's outdated and not needed
    - set lock_path for Neutron as there's no default value for it
    - remove old workarounds connected with debian/ubuntu naming schemes
    - add a workaround to handle a situation with upstream neutron-netns-cleanup
    - sync Neutron DB right after neutron-server installation
    
    Partial-blueprint: puppet-mitaka-modules
    Change-Id: Id608e63b84ebdd57196c58aa065c32f1a6d11197

--- deployment/puppet/openstack/spec/classes/openstack_compute_spec.rb
+++ deployment/puppet/openstack/spec/classes/openstack_compute_spec.rb
@@ -80,6 +80,7 @@ describe 'openstack::compute' do
 
 
     context 'with a default config' do
+
       let :params do
         { :internal_address   => '127.0.0.1',
           :nova_user_password => 'password',
@@ -96,6 +97,11 @@ describe 'openstack::compute' do
       end
 
       it 'configures with the default params' do
+        if facts[:os_package_type] == 'debian' or facts[:osfamily] == 'RedHat'
+          libvirt_service_name = 'libvirtd'
+        else
+          libvirt_service_name = 'libvirt-bin'
+        end
         should contain_class('nova').with(
           :install_utilities => false,
           :ensure_package    => 'present',
@@ -139,7 +145,7 @@ describe 'openstack::compute' do
           :migration_suport     => p[:migration_support],
           :remove_unused_original_minimum_age_seconds => '86400',
           :compute_driver       => p[:compute_driver],
-          :libvirt_service_name => 'libvirtd'
+          :libvirt_service_name => libvirt_service_name,
         )
         should contain_augeas('libvirt-conf-uuid').with(
           :context => '/files/etc/libvirt/libvirtd.conf',
@@ -165,6 +171,7 @@ describe 'openstack::compute' do
         :hostname => 'hostname.example.com',
         :openstack_version => {'nova' => 'present' },
         :os_service_default => '<SERVICE DEFAULT>',
+        :os_package_type => 'debian',
       }
     end
 
@@ -177,6 +184,7 @@ describe 'openstack::compute' do
         :hostname => 'hostname.example.com',
         :openstack_version => {'nova' => 'present' },
         :os_service_default => '<SERVICE DEFAULT>',
+        :os_package_type    => 'rpm',
       }
     end
 
