e46b8b5c8a4e745dc40ed80b698cd3a3d0461905
Denis Egorenko <degorenko@mirantis.com>
2016-02-04 16:58:19 +0300

    [PATCH] Update Glance Swift backend configuration Currently, when Glance uses Swift as backend, all references to images saved in database with credentials, like: swift+http://services%3Aglance:EF3tluYM@192.168.0.2:5000/v2.0/glance/049cd701-77
    The problem is that when we want to change Glance credentials we can't use
    old images anymore, because new credentials and creds in database are
    different. For resolving this issue was implemented patch in Glance [1]
    and couple of patches in Glance_Store [2,3]. The main goal is using
    separate section (file with such section) for providing credentials
    for access to Swift and do not place reference with credentials to
    database.
    
    All changes above are covered in docs for configuring Swift backend [4].
    
    Also glance_cache_config now is not using for configuring Swift backend,
    hence all related parameters for this config removed.
    
    [1] https://review.openstack.org/#/c/98722
    [2] https://review.openstack.org/#/c/193422/
    [3] https://review.openstack.org/#/c/108036/
    [4] https://review.openstack.org/#/c/242583/
    
    Change-Id: Iaf0c1c6f2188c84267e41d2d4f9bda0dbf3dccad
    Closes-bug: #1499754

--- deployment/puppet/glance/spec/classes/glance_backend_swift_spec.rb
+++ deployment/puppet/glance/spec/classes/glance_backend_swift_spec.rb
@@ -22,59 +22,49 @@ describe 'glance::backend::swift' do
 
     it 'configures glance-api.conf' do
       is_expected.to contain_glance_api_config('glance_store/default_store').with_value('swift')
-      is_expected.to contain_glance_api_config('glance_store/swift_store_key').with_value('key')
-      is_expected.to contain_glance_api_config('glance_store/swift_store_user').with_value('user')
-      is_expected.to contain_glance_api_config('glance_store/swift_store_auth_version').with_value('2')
       is_expected.to contain_glance_api_config('glance_store/swift_store_large_object_size').with_value('5120')
-      is_expected.to contain_glance_api_config('glance_store/swift_store_auth_address').with_value('127.0.0.1:5000/v2.0/')
       is_expected.to contain_glance_api_config('glance_store/swift_store_container').with_value('glance')
       is_expected.to contain_glance_api_config('glance_store/swift_store_create_container_on_put').with_value(false)
       is_expected.to contain_glance_api_config('glance_store/swift_store_endpoint_type').with_value('internalURL')
       is_expected.to contain_glance_api_config('glance_store/swift_store_region').with_value('')
+      is_expected.to contain_glance_api_config('DEFAULT/swift_store_config_file').with_value('/etc/glance/glance-api.conf')
+      is_expected.to contain_glance_api_config('glance_store/default_swift_reference').with_value('ref1')
+      is_expected.to contain_glance_api_config('ref1/key').with_value('key')
+      is_expected.to contain_glance_api_config('ref1/user').with_value('user')
+      is_expected.to contain_glance_api_config('ref1/auth_version').with_value('2')
+      is_expected.to contain_glance_api_config('ref1/auth_address').with_value('127.0.0.1:5000/v2.0/')
     end
 
-    it 'configures glance-cache.conf' do
-      is_expected.to contain_glance_cache_config('glance_store/swift_store_key').with_value('key')
-      is_expected.to contain_glance_cache_config('glance_store/swift_store_user').with_value('user')
-      is_expected.to contain_glance_cache_config('glance_store/swift_store_auth_version').with_value('2')
-      is_expected.to contain_glance_cache_config('glance_store/swift_store_large_object_size').with_value('5120')
-      is_expected.to contain_glance_cache_config('glance_store/swift_store_auth_address').with_value('127.0.0.1:5000/v2.0/')
-      is_expected.to contain_glance_cache_config('glance_store/swift_store_container').with_value('glance')
-      is_expected.to contain_glance_cache_config('glance_store/swift_store_create_container_on_put').with_value(false)
-    end
   end
 
   describe 'when overriding parameters' do
     let :params do
       {
-        :swift_store_user                    => 'user',
-        :swift_store_key                     => 'key',
+        :swift_store_user                    => 'user2',
+        :swift_store_key                     => 'key2',
         :swift_store_auth_version            => '1',
         :swift_store_large_object_size       => '100',
         :swift_store_auth_address            => '127.0.0.2:8080/v1.0/',
         :swift_store_container               => 'swift',
         :swift_store_create_container_on_put => true,
         :swift_store_endpoint_type           => 'publicURL',
-        :swift_store_region                  => 'RegionTwo'
+        :swift_store_region                  => 'RegionTwo',
+        :default_swift_reference             => 'swift_creds',
       }
     end
 
     it 'configures glance-api.conf' do
       is_expected.to contain_glance_api_config('glance_store/swift_store_container').with_value('swift')
       is_expected.to contain_glance_api_config('glance_store/swift_store_create_container_on_put').with_value(true)
-      is_expected.to contain_glance_api_config('glance_store/swift_store_auth_version').with_value('1')
       is_expected.to contain_glance_api_config('glance_store/swift_store_large_object_size').with_value('100')
-      is_expected.to contain_glance_api_config('glance_store/swift_store_auth_address').with_value('127.0.0.2:8080/v1.0/')
       is_expected.to contain_glance_api_config('glance_store/swift_store_endpoint_type').with_value('publicURL')
       is_expected.to contain_glance_api_config('glance_store/swift_store_region').with_value('RegionTwo')
+      is_expected.to contain_glance_api_config('glance_store/default_swift_reference').with_value('swift_creds')
+      is_expected.to contain_glance_api_config('swift_creds/key').with_value('key2')
+      is_expected.to contain_glance_api_config('swift_creds/user').with_value('user2')
+      is_expected.to contain_glance_api_config('swift_creds/auth_version').with_value('1')
+      is_expected.to contain_glance_api_config('swift_creds/auth_address').with_value('127.0.0.2:8080/v1.0/')
     end
 
-    it 'configures glance-cache.conf' do
-      is_expected.to contain_glance_cache_config('glance_store/swift_store_container').with_value('swift')
-      is_expected.to contain_glance_cache_config('glance_store/swift_store_create_container_on_put').with_value(true)
-      is_expected.to contain_glance_cache_config('glance_store/swift_store_auth_version').with_value('1')
-      is_expected.to contain_glance_cache_config('glance_store/swift_store_large_object_size').with_value('100')
-      is_expected.to contain_glance_cache_config('glance_store/swift_store_auth_address').with_value('127.0.0.2:8080/v1.0/')
-    end
   end
 end
