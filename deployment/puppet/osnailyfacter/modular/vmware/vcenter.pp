375299d8b65b2fb3be03681f6deba45e6e5232e2
Alexander Arzhanov <aarzhanov@mirantis.com>
2016-02-04 11:20:14 +0000

    Change protocol and hostname for novncproxy
    Noop test, actual fixtures for this changes and other VMware specific
    changes we provide asap.
    
    In order to provide VM console access via HTTPS, we need to provide
    the correct link to the proxy via the api.
    Also, the cloud administrator will need to properly configure the DNS
    for the clients to ensure they will be able to access the HTTPS
    endpoint as the DNS name must match the certificate name for the Nova
    VNC Proxy service.
    
    Change-Id: Ic8da073bb54241c4533747687d106f5f58ffdb4b
    Closes-bug: #1511422

--- deployment/puppet/osnailyfacter/modular/vmware/vcenter.pp
+++ deployment/puppet/osnailyfacter/modular/vmware/vcenter.pp
@@ -1,19 +1,26 @@
 notice('MODULAR: vmware/vcenter.pp')
 
-$use_vcenter     = hiera('use_vcenter', false)
-$vcenter_hash    = hiera('vcenter_hash')
-$public_vip      = hiera('public_vip')
-$use_neutron     = hiera('use_neutron', false)
-$ceilometer_hash = hiera('ceilometer',{})
-$debug           = pick($vcenter_hash['debug'], hiera('debug', false))
+$use_vcenter       = hiera('use_vcenter', false)
+$vcenter_hash      = hiera('vcenter_hash')
+$public_vip        = hiera('public_vip')
+$use_neutron       = hiera('use_neutron', false)
+$ceilometer_hash   = hiera('ceilometer',{})
+$nova_hash         = hiera_hash('nova_hash', {})
+$public_ssl_hash   = hiera('public_ssl')
+$ssl_hash          = hiera_hash('use_ssl', {})
+$vncproxy_protocol = get_ssl_property($ssl_hash, $public_ssl_hash, 'nova', 'public', 'protocol', [$nova_hash['vncproxy_protocol'], 'http'])
+$vncproxy_host     = get_ssl_property($ssl_hash, $public_ssl_hash, 'nova', 'public', 'hostname', [$public_vip])
+$debug             = pick($vcenter_hash['debug'], hiera('debug', false))
 
 if $use_vcenter {
   class { 'vmware':
-    vcenter_settings => $vcenter_hash['computes'],
-    vlan_interface   => $vcenter_hash['esxi_vlan_interface'],
-    use_quantum      => $use_neutron,
-    vnc_address      => $public_vip,
-    ceilometer       => $ceilometer_hash['enabled'],
-    debug            => $debug,
+    vcenter_settings  => $vcenter_hash['computes'],
+    vlan_interface    => $vcenter_hash['esxi_vlan_interface'],
+    use_quantum       => $use_neutron,
+    vncproxy_protocol => $vncproxy_protocol,
+    vncproxy_host     => $vncproxy_host,
+    nova_hash         => $nova_hash,
+    ceilometer        => $ceilometer_hash['enabled'],
+    debug             => $debug,
   }
 }
