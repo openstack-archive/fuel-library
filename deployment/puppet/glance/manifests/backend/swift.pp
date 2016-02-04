fe863a5a353f1a65a07f99f4f5a120708e50caa6
Denis Egorenko <degorenko@mirantis.com>
2016-02-04 16:58:19 +0300

    [PATCH] Update Glance Swift backend configuration Currently, when Glance uses Swift as backend, all references to images saved in database with credentials, like: swift+http://services%3Aglance:EF3tluYM@192.168.0.2:5000/v2.0/glance/049cd701-77
    The problem is that when we want to change Glance credentials we can't use
    old images anymore, because new credentials and creds in database are
    different. For resolving this issue was implemented patch in Glance [1]
    and couple of patches in Glance_Store [2,3]. The main goal is using
    separate section (file with such section) for providing credentials
    for access to Swift and do not place reference with credentials to
    database.
    
    All changes above are covered in docs for configuring Swift backend [4].
    
    Also glance_cache_config now is not using for configuring Swift backend,
    hence all related parameters for this config removed.
    
    [1] https://review.openstack.org/#/c/98722
    [2] https://review.openstack.org/#/c/193422/
    [3] https://review.openstack.org/#/c/108036/
    [4] https://review.openstack.org/#/c/242583/
    
    Change-Id: Iaf0c1c6f2188c84267e41d2d4f9bda0dbf3dccad
    Closes-bug: #1499754

--- deployment/puppet/glance/manifests/backend/swift.pp
+++ deployment/puppet/glance/manifests/backend/swift.pp
@@ -32,6 +32,12 @@
 #  [*swift_store_region*] 
 #    Optional. Default: '' 
 #
+#  [*default_swift_reference*]
+#    Optional. The reference to the default swift
+#    account/backing store parameters to use for adding
+#    new images. String value.
+#    Default to 'ref1'.
+#
 class glance::backend::swift(
   $swift_store_user,
   $swift_store_key,
@@ -42,34 +48,26 @@ class glance::backend::swift(
   $swift_store_create_container_on_put = false,
   $swift_store_endpoint_type = 'internalURL',
   $swift_store_region = '',
+  $default_swift_reference = 'ref1',
 ) {
 
   glance_api_config {
-    'glance_store/default_store':            value => 'swift';
-    'glance_store/swift_store_user':         value => $swift_store_user;
-    'glance_store/swift_store_key':          value => $swift_store_key;
-    'glance_store/swift_store_auth_address': value => $swift_store_auth_address;
-    'glance_store/swift_store_region':       value => $swift_store_region;
-    'glance_store/swift_store_container':    value => $swift_store_container;
-    'glance_store/swift_store_auth_version': value => $swift_store_auth_version;
+    'glance_store/default_store':              value => 'swift';
+    'glance_store/swift_store_region':         value => $swift_store_region;
+    'glance_store/swift_store_container':      value => $swift_store_container;
     'glance_store/swift_store_create_container_on_put':
       value => $swift_store_create_container_on_put;
     'glance_store/swift_store_large_object_size':
       value => $swift_store_large_object_size;
     'glance_store/swift_store_endpoint_type':
       value => $swift_store_endpoint_type;
-  }
 
-  glance_cache_config {
-    'glance_store/swift_store_user':         value => $swift_store_user;
-    'glance_store/swift_store_key':          value => $swift_store_key;
-    'glance_store/swift_store_auth_address': value => $swift_store_auth_address;
-    'glance_store/swift_store_container':    value => $swift_store_container;
-    'glance_store/swift_store_auth_version': value => $swift_store_auth_version;
-    'glance_store/swift_store_create_container_on_put':
-      value => $swift_store_create_container_on_put;
-    'glance_store/swift_store_large_object_size':
-      value => $swift_store_large_object_size;
+    'DEFAULT/swift_store_config_file':         value => '/etc/glance/glance-api.conf';
+    'glance_store/default_swift_reference':    value => $default_swift_reference;
+    "${default_swift_reference}/user":         value => $swift_store_user;
+    "${default_swift_reference}/key":          value => $swift_store_key;
+    "${default_swift_reference}/auth_address": value => $swift_store_auth_address;
+    "${default_swift_reference}/auth_version": value => $swift_store_auth_version;
   }
 
 }
