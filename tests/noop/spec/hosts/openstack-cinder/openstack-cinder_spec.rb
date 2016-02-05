2783435fcaa693356dc71f8e17c318c238b38eeb
Dmitry Ilyin <dilyin@mirantis.com>
2016-02-05 18:50:47 +0300

    Switch to external noop tests
    * Use external noop tests library and fixtures
    
    Partial blueprint: deployment-data-dryrun
    Fuel-CI: disable
    Change-Id: Ie8d1f80bbee04b9bacb43ba4fa39a423624a8301

--- tests/noop/spec/hosts/openstack-cinder/openstack-cinder_spec.rb
+++ tests/noop/spec/hosts/openstack-cinder/openstack-cinder_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 require 'shared-examples'
 manifest = 'openstack-cinder/openstack-cinder.pp'
 
+# DISABLE_SPEC
+
 describe manifest do
   shared_examples 'catalog' do
 
--- tests/noop/spec/hosts/roles/compute_spec.rb
+++ tests/noop/spec/hosts/roles/compute_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 require 'shared-examples'
 manifest = 'roles/compute.pp'
 
+# DISABLE_SPEC
+
 describe manifest do
   shared_examples 'catalog' do
 
