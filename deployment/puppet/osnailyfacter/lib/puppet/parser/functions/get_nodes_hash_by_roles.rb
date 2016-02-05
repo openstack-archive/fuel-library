090dbc73435e7134b104262536b492fcc0138c44
Dmitry Ilyin <dilyin@mirantis.com>
2016-02-05 18:50:47 +0300

    Switch to external noop tests
    * Use external noop tests library and fixtures
    * Corrections and fixes for the spec library to make it work
    
    Partial blueprint: deployment-data-dryrun
    Fuel-CI: disable
    Change-Id: Ie8d1f80bbee04b9bacb43ba4fa39a423624a8301

--- deployment/puppet/osnailyfacter/lib/puppet/parser/functions/get_nodes_hash_by_roles.rb
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/get_nodes_hash_by_roles.rb
@@ -7,9 +7,9 @@ EOS
   ) do |args|
     errmsg = "get_nodes_hash_by_roles($network_metadata_hash, ['node_role1','node_role2'])"
     n_metadata, roles = args
-    raise(Puppet::ParseError, "#{errmsg}: 1st argument should be a hash") if !n_metadata.is_a?(Hash)
-    raise(Puppet::ParseError, "#{errmsg}: 1st argument should be a valid network_metadata hash") if !n_metadata.has_key?('nodes')
-    raise(Puppet::ParseError, "#{errmsg}: 2nd argument should be an list of node roles") if !roles.is_a?(Array)
+    raise(Puppet::ParseError, "#{errmsg}: 1st argument should be a hash, but got: #{n_metadata.inspect}") if !n_metadata.is_a?(Hash)
+    raise(Puppet::ParseError, "#{errmsg}: 1st argument should be a valid network_metadata hash, but got: #{n_metadata.inspect}") if !n_metadata.has_key?('nodes')
+    raise(Puppet::ParseError, "#{errmsg}: 2nd argument should be an list of node roles, but got: #{roles.inspect}") if !roles.is_a?(Array)
     nodes = n_metadata['nodes']
     # Using unrequired node_property bellow -- is a workaround for ruby 1.8
     nodes.reject do |node_name, node_property|
--- tests/noop/spec/hosts/murano/keystone_spec.rb
+++ tests/noop/spec/hosts/murano/keystone_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 require 'shared-examples'
 manifest = 'murano/keystone.pp'
 
+# DISABLE_SPEC
+
 describe manifest do
   shared_examples 'catalog' do
 
