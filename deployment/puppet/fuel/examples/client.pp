a6a467fe12afdd8a794c579111d376f0e87a491f
Roman Prykhodchenko <me@romcheg.me>
2016-02-08 17:08:30 +0100

    Update configuration file for fuel client
    Authentication settings for Fuel Client changed their
    format in order to correspond to OpenStack standards.
    
    This patch updates configuration template according to
    those changes.
    
    Related-bug: #1535417
    Change-Id: I6dc5a3961e8778d3f89c365b53723171fa7d8d40

--- deployment/puppet/fuel/examples/client.pp
+++ deployment/puppet/fuel/examples/client.pp
@@ -6,6 +6,8 @@ class { "fuel::nailgun::client":
   server_address     => $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
   keystone_user      => $::fuel_settings['FUEL_ACCESS']['user'],
   keystone_password  => $::fuel_settings['FUEL_ACCESS']['password'],
+  # Keystone tenant name cannot be configured in Fuel Menu yet
+  keystone_tenant    => 'admin',
 }
 
 # This exec needs python-fuelclient to be installed and nailgun running
