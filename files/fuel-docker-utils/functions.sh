openstack-archive/fuel-library

commit 02393e503db32ee3c790e249df11c967dfcd1af4

Fix disk space and error handling in dockerctl restore
Reduced free disk space for stateful restore to 2gb
(still 11gb for full restore). Nonzero command exit
codes only apply to the beginning of restore, but
container startup now is handled without "set -e".

Change-Id: Ifc0109e55adc7fbf30b51f5cfb259ea2ae5bf585
Partial-Bug: #1448211
Closes-Bug: #1455488

files/fuel-docker-utils/functions.sh
@@ -231,7 +231,7 @@ function start_container {
     else
       # Clean up broken mounts if needed
       id=$(get_container_id $container_name)
-      umount -l $(grep "$id" /proc/mounts | awk '{print $2}' | sort -r) 2>/dev/null
+      grep "$id" /proc/mounts | awk '{print $2}' | sort -r | xargs --no-run-if-empty -n1 umount -l 2>/dev/null
       ${DOCKER} start $container_name
     fi
     post_start_hooks $1
@@ -654,7 +654,7 @@ function restore {
 finish or cancel them. Run \"fuel task list\" for more details." 1>&2
     exit 1
   fi
-  verify_disk_space "restore"
+  verify_disk_space "restore" "$fullrestore"
   backupfile=$1
   if [ -z "$backupfile" ]; then
     #TODO(mattymo): Parse BACKUP_DIR for lrz files
@@ -685,6 +685,7 @@ finish or cancel them. Run \"fuel task list\" for more details." 1>&2
   [ "$fullrestore" == "1" ] && restore_images "$restoredir"
   [ "$fullrestore" == "1" ] && rename_images "$timestamp"
   restore_systemdirs "$restoredir"
+  set +e
   echo "Starting containers..."
   start_container all
   enable_supervisor
@@ -748,12 +749,19 @@ function verify_disk_space {
     echo "Backup or restore operation not specified." 1>&2
     exit 1
   fi
-
-  #11gb free space required to backup and restore
-  (( required = 11 * 1024 * 1024 ))
+  fullbackup=1
+  if [[ "$2" != "$fullbackup" ]]; then
+    #2gb free space required for light backup
+    (( required  = 2 * 1024 * 1024 ))
+    spaceerror="Insufficient disk space to perform $1. At least 2gb must be free on /var partition."
+  else
+     #11gb free space required to backup and restore
+     (( required = 11 * 1024 * 1024 ))
+    spaceerror="Insufficient disk space to perform $1. At least 11gb must be free on /var partition."
+  fi
   avail=$(df /var | grep /var | awk '{print $4}')
   if (( avail < required )); then
-    echo "Insufficient disk space to perform $1. At least 11gb must be free on /var partition." 1>&2
+    echo "$spaceerror" 1>&2
     exit 1
   fi
 }

commit 259622435a4c32a2839809a52cc468a42520c877

Verify backup integrity during backup and restore
md5sum of backup archive is now checked during backup
and verified before before restore.

Also fixes 02393e503db32ee3c790e249df11c967dfcd1af4
which accidentally forced full backups

Change-Id: Ib1ea740ed248838708a09b098cf14021014df18b
Closes-Bug: #1541539
(cherry picked from commit b8eb70d85567973b98764a4ccc4fb7140c1aef44)

files/fuel-docker-utils/functions.sh
@@ -729,14 +729,16 @@ function backup_compress {
   component_tars=($backup_dir/*.tar)
   ( cd $backup_dir && tar cf $backup_dir/fuel_backup${image_suffix}.tar *.tar *.sql)
   rm -rf "${component_tars[@]}"
+  lrzopts="-L2 -H -q"
   #Improve compression on bare metal
   if [ -z "$(virt-what)" ] ; then
-    lrzopts="-L2 -U"
-  else
-    lrzopts="-L2"
+    lrzopts="${lrzopts} -U"
   fi
-  lrzip $lrzopts "$backup_dir/fuel_backup${image_suffix}.tar" -o "$backup_dir/fuel_backup${image_suffix}.tar.lrz"
-
+  backupfile="${backup_dir}/fuel_backup${image_suffix}.tar.lrz"
+  lrzip $lrzopts "$backup_dir/fuel_backup${image_suffix}.tar" -o "${backupfile}"
+  echo "Verifying integrity of archive..."
+  lrzip -t "${backupfile}"
+  echo "Archive integrity is OK."
 }
 
 function backup_rsync_upload {
@@ -870,6 +872,8 @@ finish or cancel them. Run \"fuel task list\" for more details." 1>&2
   elif ! [[ "$backupfile" =~ lrz$ ]]; then
     echo "Archive does not have lrz extension." 1>&2
     exit 2
+  elif ! lrzip -t "$backupfile"; then
+    echo "Backup file is corrupt and does not match its contained md5sum."
   fi
   timestamp=$(echo $backupfile | sed -n 's/.*\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]_[0-9][0-9][0-9][0-9]\).*/\1/p')
   if [ -z "$timestamp" ]; then
@@ -983,8 +987,8 @@ function verify_disk_space {
     echo "Backup or restore operation not specified." 1>&2
     exit 1
   fi
-  fullbackup=1
-  if [[ "$2" != "$fullbackup" ]]; then
+
+  if [[ "$2" != "1" ]]; then
     #2gb free space required for light backup
     (( required  = 2 * 1024 * 1024 ))
     spaceerror="Insufficient disk space to perform $1. At least 2gb must be free on ${BACKUP_ROOT} partition."