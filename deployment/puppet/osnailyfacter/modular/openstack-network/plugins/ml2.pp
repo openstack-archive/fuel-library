0c45705876a002e16bf3a81d176770797d78cbc8
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-02-09 15:20:38 +0300

    Link neutron syncdb with service run.
    To have an ability for API calling, we must have neutron fully
    configured, so we must configure all resources, sync DB, then
    run neutron.
    
    We could move syncdb task to server-config manifest, but it is
    not good solution, cause it will be tricky to link it proper way. So,
    just run it always before neutron service.
    
    Change-Id: I528ee55f1766d59bd2320b59bd4e181b4df350ed
    Related-Blueprint: granular-task-idempotency

--- deployment/puppet/osnailyfacter/modular/openstack-network/plugins/ml2.pp
+++ deployment/puppet/osnailyfacter/modular/openstack-network/plugins/ml2.pp
@@ -118,6 +118,7 @@ if $use_neutron {
     } else {
       $service_ensure = 'stopped'
     }
+
     service { 'neutron-server':
       name       => $::neutron::params::server_service,
       enable     => $neutron_server_enable,
@@ -125,7 +126,8 @@ if $use_neutron {
       hasstatus  => true,
       hasrestart => true,
       tag        => 'neutron-service',
-    } ->
+    }
+
     exec { 'waiting-for-neutron-api':
       environment => [
         "OS_TENANT_NAME=${auth_tenant}",
@@ -139,7 +141,9 @@ if $use_neutron {
       tries       => '30',
       try_sleep   => '4',
       command     => 'neutron net-list --http-timeout=4 2>&1 > /dev/null',
-      provider    => 'shell'
+      provider    => 'shell',
+      subscribe   => Service['neutron-server'],
+      refreshonly => true,
     }
 
     $ha_agent = try_get_value($neutron_advanced_config, 'l2_agent_ha', true)
