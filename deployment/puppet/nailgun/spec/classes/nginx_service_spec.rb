85882ec5e70b5e6f6a499cc7ce1112ec56fd2d71
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2016-02-11 01:30:51 +0300

    Remove nailgun module
    Since we have switched to the fuel module, we
    can remove nailgun one.
    
    Closes-Bug: #1545032
    Change-Id: Ic7797df66c8968a162dc4433d87a2cf0568e4ab6

--- deployment/puppet/nailgun/spec/classes/nginx_service_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe 'nailgun::nginx_service' do
-  it 'should contain X-Frame-Options SAMEORIGIN header' do
-    should contain_file('/etc/nginx/nginx.conf').with_content(/^\s*add_header X-Frame-Options SAMEORIGIN;$/)
-  end
-end
