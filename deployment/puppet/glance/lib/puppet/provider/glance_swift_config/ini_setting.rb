c62b89154969cb330e201cb71922cd1e605a18c9
Denis Egorenko <degorenko@mirantis.com>
2016-02-10 19:10:07 +0300

    Use glance-swift conf for swift backend
    For working with Swift backend, Glance needs authentication parameters,
    which are supplied to Swift when making calls to its storage system.
    Glance will use information from the file specified under parameter
    swift_store_config_file. When this parameter is set to the same file
    (glance-api.conf), we have lot of non-critical errors in glance-api
    log, because of 'Invalid format of swift store config'.
    
    The same problem, when we have RadosGW.
    
    Change-Id: I18ee8f68f7ce793d2580685e79ed636556c57ddd
    Related-bug: #1540890

---
+++ deployment/puppet/glance/lib/puppet/provider/glance_swift_config/ini_setting.rb
@@ -0,0 +1,10 @@
+Puppet::Type.type(:glance_swift_config).provide(
+  :ini_setting,
+  :parent => Puppet::Type.type(:openstack_config).provider(:ini_setting)
+) do
+
+  def self.file_path
+    '/etc/glance/glance-swift.conf'
+  end
+
+end
