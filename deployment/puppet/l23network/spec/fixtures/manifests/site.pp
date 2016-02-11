f8e73e74eb4cf456d1b3721d77b1f26cd773dcb3
Sergey Vasilenko <svasilenko@mirantis.com>
2016-02-11 15:15:29 +0300

    Refactor spec-tests
    and add workarounds for run tests under OSx platform
    
    Also change fixtures for more optimal usage.
    
    Change-Id: Iaac54e16f9a5b2b540f636768635fa5d4ff782eb
    Closes-bug: #1544638

--- deployment/puppet/l23network/spec/fixtures/manifests/site.pp
+++ deployment/puppet/l23network/spec/fixtures/manifests/site.pp
@@ -1,1 +1,7 @@
 include ::l23network::params
+
+# this is a workaround for run spec tests not only on Linux platform
+if $::l23network::params::network_manager_name != undef {
+  Package<| title == $::l23network::params::network_manager_name |> { provider => apt }
+}
+###
