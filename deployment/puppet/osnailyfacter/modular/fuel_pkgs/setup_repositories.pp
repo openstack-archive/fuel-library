4468e5df7762af76ed2372cea75b49b6e7518b39
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-02-12 16:47:26 +0300

    Add upstream openstack repo support
    blueprint deploy-with-uca-packages
    
    Change-Id: Id7e9883563678a81b14df4d796c0b6122352e2e4

--- deployment/puppet/osnailyfacter/modular/fuel_pkgs/setup_repositories.pp
+++ deployment/puppet/osnailyfacter/modular/fuel_pkgs/setup_repositories.pp
@@ -2,6 +2,19 @@ notice('MODULAR: setup_repositories.pp')
 
 $repo_setup_hash = hiera_hash('repo_setup', {})
 $repos      = $repo_setup_hash['repos']
+$repo_type  = $repo_setup_hash['repo_type']
+
+if $repo_type and $repo_type != 'fuel' {
+  class { 'osnailyfacter::upstream_repo_setup':
+    repo_type       => $repo_type,
+    uca_repo_url    => $repo_setup_hash['uca_repo_url'],
+    debian_repo_url => $repo_setup_hash['debian_trusty_repo_url'],
+    pin_haproxy     => $repo_setup_hash['pin_haproxy'],
+    pin_rabbitmq    => $repo_setup_hash['pin_rabbitmq'],
+    pin_ceph        => $repo_setup_hash['pin_ceph'],
+    pin_priority    => '2000',
+  }
+}
 
 if $::osfamily == 'Debian' {
 
