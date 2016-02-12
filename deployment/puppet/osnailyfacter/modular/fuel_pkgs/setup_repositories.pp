5864f3a26e67697c76b66276033bf5b9e3ae17e2
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-02-12 16:47:26 +0300

    Add upstream openstack repo support
    blueprint deploy-with-uca-packages
    
    Change-Id: Id7e9883563678a81b14df4d796c0b6122352e2e4

--- deployment/puppet/osnailyfacter/modular/fuel_pkgs/setup_repositories.pp
+++ deployment/puppet/osnailyfacter/modular/fuel_pkgs/setup_repositories.pp
@@ -2,6 +2,19 @@ notice('MODULAR: setup_repositories.pp')
 
 $repo_setup = hiera('repo_setup', {})
 $repos      = $repo_setup['repos']
+$repo_type  = $repo_setup['repo_type']
+
+if $repo_type and $repo_type != 'fuel' {
+  class { 'osnailyfacter::upstream_repo_setup':
+    repo_type       => $repo_type,
+    uca_repo_url    => $repo_setup['uca_repo_url'],
+    debian_repo_url => $repo_setup['debian_trusty_repo_url'],
+    pin_haproxy     => $repo_setup['pin_haproxy'],
+    pin_rabbitmq    => $repo_setup['pin_rabbitmq'],
+    pin_ceph        => $repo_setup['pin_ceph'],
+    pin_priority    => '2000',
+  }
+}
 
 if $::osfamily == 'Debian' {
   include ::apt
@@ -35,4 +48,11 @@ if $::osfamily == 'Debian' {
   }
 
   Apt::Source<||> ~> Exec<| title == 'apt_update' |>
+}
+
+#FIXME(mattymo): broken deps for python-amqp in python-kombu (LP#1546430)
+if $repo_type == 'uca' {
+  package {'python-amqp':
+    ensure => '1.4.9-1~cloud0',
+  }
 }
