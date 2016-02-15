11c25fe9c3d6a735e25ea061b2d12eea39cfe086
Denis Egorenko <degorenko@mirantis.com>
2016-02-15 14:58:07 +0300

    Use glance-swift conf for swift backend
    For working with Swift backend, Glance needs authentication parameters,
    which are supplied to Swift when making calls to its storage system.
    Glance will use information from the file specified under parameter
    swift_store_config_file. When this parameter is set to the same file
    (glance-api.conf), we have lot of non-critical errors in glance-api
    log, because of 'Invalid format of swift store config'.
    
    The same problem, when we have RadosGW.
    
    Change-Id: I18ee8f68f7ce793d2580685e79ed636556c57ddd
    Related-bug: #1540890

--- deployment/puppet/glance/manifests/backend/swift.pp
+++ deployment/puppet/glance/manifests/backend/swift.pp
@@ -27,7 +27,7 @@ class glance::backend::swift(
   $swift_store_auth_version = '2',
   $swift_store_large_object_size = '5120',
   $swift_store_create_container_on_put = false,
-  $swift_store_endpoint_type = 'internalURL'
+  $swift_store_endpoint_type = 'internalURL',
   $default_swift_reference = 'ref1',
 ) {
 
@@ -42,8 +42,11 @@ class glance::backend::swift(
     'glance_store/swift_store_endpoint_type':
       value => $swift_store_endpoint_type;
 
-    'DEFAULT/swift_store_config_file':         value => '/etc/glance/glance-api.conf';
+    'glance_store/swift_store_config_file':    value => '/etc/glance/glance-swift.conf';
     'glance_store/default_swift_reference':    value => $default_swift_reference;
+  }
+
+  glance_swift_config {
     "${default_swift_reference}/user":         value => $swift_store_user;
     "${default_swift_reference}/key":          value => $swift_store_key;
     "${default_swift_reference}/auth_address": value => $swift_store_auth_address;
