aa768dfe43c7c3632ee3532b6d54e4fde00aac81
Maksym Yatsenko <myatsenko@mirantis.com>
2016-02-15 23:27:51 +0200

    Adjust 'report_interval', 'service_down_time', 'agent_down_time' parameters via hiera.
    Added a possilibity to adjust 'report_interval', 'service_down_time',
    'agent_down_time' parameters of cinder and neutron via hiera.
    
    Related-Bug: #1448151
    
    Change-Id: I4c509b79cd6fca749905d224b2d7c6d9a928cb71

--- deployment/puppet/osnailyfacter/modular/globals/globals.pp
+++ deployment/puppet/osnailyfacter/modular/globals/globals.pp
@@ -101,6 +101,10 @@ $syslog_log_facility_ironic     = hiera('syslog_log_facility_ironic','LOG_LOCAL0
 
 $nova_report_interval           = hiera('nova_report_interval', 60)
 $nova_service_down_time         = hiera('nova_service_down_time', 180)
+$cinder_report_interval         = hiera('cinder_report_interval', 60)
+$cinder_service_down_time       = hiera('cinder_service_down_time', 10)
+$neutron_report_interval        = hiera('neutron_report_interval', 10)
+$neutron_agent_down_time        = hiera('neutron_agent_down_time', 30)
 
 $custom_theme_path              = hiera('custom_theme_path', 'themes/vendor')
 
