edc6a338f652666bda64f8d461260cd3d266f5a9
Vitaly Kramskikh <vkramskikh@mirantis.com>
2016-02-16 20:57:45 +0700

    Change type of dns_list and ntp_list to text_list (library part)
    We have new type "text_list" which is more suitable for lists
    than plain "text".
    
    Implements: blueprint dynamic-fields
    
    Depends-On: I21a25af5b22594ce18ffdd35faeb17d5a7773dff
    
    Change-Id: I8d0ac355df31ef9d071df248d072718c1750e892

--- deployment/puppet/osnailyfacter/modular/ntp/ntp-check.pp
+++ deployment/puppet/osnailyfacter/modular/ntp/ntp-check.pp
@@ -3,4 +3,4 @@ notice('MODULAR: ntp-check.pp')
 $ntp_servers = hiera('external_ntp')
 # take the comma seperated list and turn it into an array of servers and then
 # pass it to the ntp_available function to check that at least 1 server works
-ntp_available(strip(split($ntp_servers['ntp_list'], ',')))
+ntp_available($ntp_servers['ntp_list'])
