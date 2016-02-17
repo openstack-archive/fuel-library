94165137d1d60d81b9fbcfa1728da8815bf8651e
iberezovskiy <iberezovskiy@mirantis.com>
2016-02-17 14:13:12 +0300

    Pull upstream version of mongodb module
    0.12.0 is finally released. Our customization is included
    in it as it was implemented in following pull request:
    https://github.com/puppetlabs/puppetlabs-mongodb/pull/194
    This patch also contains improvements for timeouts during
    replica initialization and user/db creation.
    
    Additional changes:
      * get rid of useless openstack/mongo class
      * add an ability to configure some mongo parameters
        through hiera
      * move firewall rule to appropriate place
    
    Closes-bug: #1538521
    Closes-bug: #1475948
    
    Change-Id: I757b77fcf836f37a8136fa0987cca13ff646712a

--- deployment/puppet/osnailyfacter/modular/roles/mongo.pp
+++ deployment/puppet/osnailyfacter/modular/roles/mongo.pp
@@ -1,28 +1,74 @@
 notice('MODULAR: mongo.pp')
 
 prepare_network_config(hiera_hash('network_scheme', {}))
-$mongo_hash        = hiera_hash('mongo', {})
-$mongo_nodes       = get_nodes_hash_by_roles(hiera_hash('network_metadata'), hiera('mongo_roles'))
-$mongo_address_map = get_node_to_ipaddr_map_by_network_role($mongo_nodes, 'mongo/db')
-$bind_address      = get_network_role_property('mongo/db', 'ipaddr')
-$use_syslog        = hiera('use_syslog', true)
-$debug             = pick($mongo_hash['debug'], hiera('debug', false))
-$ceilometer_hash   = hiera_hash('ceilometer_hash')
-$roles             = hiera('roles')
-$replset_name      = 'ceilometer'
-$mongodb_port      = hiera('mongodb_port', '27017')
+$mongo_hash          = hiera_hash('mongo', {})
+$mongo_nodes         = get_nodes_hash_by_roles(hiera_hash('network_metadata'), hiera('mongo_roles'))
+$mongo_address_map   = get_node_to_ipaddr_map_by_network_role($mongo_nodes, 'mongo/db')
+$bind_address        = get_network_role_property('mongo/db', 'ipaddr')
+$use_syslog          = hiera('use_syslog', true)
+$debug               = pick($mongo_hash['debug'], hiera('debug', false))
+$ceilometer_hash     = hiera_hash('ceilometer_hash')
+$roles               = hiera('roles')
+$replset_name        = 'ceilometer'
+$mongodb_port        = hiera('mongodb_port', '27017')
+$keyfile             = '/etc/mongodb.key'
+$astute_keyfile      = '/var/lib/astute/mongodb/mongodb.key'
+$ceilometer_database = pick($mongo_hash['ceilometer_database'], 'ceilometer')
 
-####################################################################
-class { 'openstack::mongo':
-  mongodb_bind_address       => [ '127.0.0.1', $bind_address ],
-  mongodb_port               => $mongodb_port,
-  ceilometer_metering_secret => $ceilometer_hash['metering_secret'],
-  ceilometer_db_password     => $ceilometer_hash['db_password'],
-  ceilometer_replset_members => values($mongo_address_map),
-  replset_name               => $replset_name,
-  mongo_version              => '2.6.10',
-  use_syslog                 => $use_syslog,
-  debug                      => $debug,
+if $debug {
+  $verbositylevel = "vv"
+} else {
+  $verbositylevel = "v"
+}
+
+if $use_syslog {
+  $logpath = false
+} else {
+  # undef to use defaults
+  $logpath = undef
+}
+
+file { $keyfile:
+  content => file($astute_keyfile),
+  owner   => 'mongodb',
+  mode    => '0600',
+  require => Package['mongodb_server'],
+  before  => Service['mongodb'],
+}
+
+class {'::mongodb::globals':
+  version => '2.6.10',
+} ->
+
+class {'::mongodb::client': } ->
+
+class {'::mongodb::server':
+  package_ensure => true,
+  port           => $mongodb_port,
+  verbose        => pick($mongo_hash['verbose'], false),
+  verbositylevel => $verbositylevel,
+  syslog         => $use_syslog,
+  logpath        => $logpath,
+  journal        => pick($mongo_hash['journal'], true),
+  bind_ip        => [ '127.0.0.1', $bind_address ],
+  auth           => true,
+  replset        => $replset_name,
+  keyfile        => $keyfile,
+  directoryperdb => pick($mongo_hash['directoryperdb'], true),
+  fork           => pick($mongo_hash['fork'], false),
+  profile        => pick($mongo_hash['profile'], '1'),
+  oplog_size     => pick($mongo_hash['oplog_size'], '10240'),
+  dbpath         => pick($mongo_hash['dbpath'], '/var/lib/mongo/mongodb'),
+  create_admin   => true,
+  admin_password => $ceilometer_hash['db_password'],
+  store_creds    => true,
+  replset_config => { "${replset_name}" => { ensure => present, members => values($mongo_address_map) } },
+} ->
+
+mongodb::db { $ceilometer_database:
+  user           => pick($mongo_hash['ceilometer_user'], 'ceilometer'),
+  password       => $ceilometer_hash['db_password'],
+  roles          => [ 'readWrite', 'dbAdmin' ],
 }
 
 if ! roles_include(['controller', 'primary-controller']) {
