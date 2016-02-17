c1decc616cc295f21839c612dc87c3f0cdaa36db
slava <vvalyavskiy@mirantis.com>
2016-02-17 15:27:15 +0300

    Add function for parsing services cgroups settings
    Current function was introduced to convert cgroups settings in JSON
    format to the HASH what will be processed by main cgroups puppet
    module.
    
    Change-Id: I03f6a0e7ec3038fe6c0d67f26d58ffccf9f581a7
    Implements: blueprint cgroups

---
+++ deployment/puppet/cgroups/spec/functions/prepare_cgroups_hash__spec.rb
@@ -0,0 +1,129 @@
+require 'spec_helper'
+
+describe Puppet::Parser::Functions.function(:prepare_cgroups_hash) do
+  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
+
+  subject do
+    function_name = Puppet::Parser::Functions.function(:prepare_cgroups_hash)
+    scope.method(function_name)
+  end
+
+  it 'should exist' do
+    subject == Puppet::Parser::Functions.function(:prepare_cgroups_hash)
+  end
+
+  Facter.stubs(:fact).with(:memorysize_mb).returns Facter.add(:memorysize_mb) { setcode { 1024 } }
+
+  context "transform simple hash" do
+    let(:sample) {
+      {
+        'cinder' => '{"blkio":{"blkio.weight":500, "blkio.test":800}, "memory":{"memory.soft_limit_in_bytes":700}}',
+        'keystone' => '{"cpu":{"cpu.shares":70}}'
+      }
+    }
+
+    let(:result) {
+      {
+        'cinder' => {
+          'blkio' => {
+            'blkio.weight' => 500,
+            'blkio.test' => 800
+          },
+          'memory' => {
+            'memory.soft_limit_in_bytes' => 700
+          },
+        },
+        'keystone' => {
+          'cpu' => {
+            'cpu.shares' => 70
+          }
+        }
+      }
+    }
+
+    it 'should transform hash with simple values' do
+      should run.with_params(sample).and_return(result)
+    end
+
+  end
+
+
+  context "transform hash with expression" do
+
+    let(:sample) {
+      {
+        'neutron' => '{"memory":{"memory.soft_limit_in_bytes":"%50, 300, 700"}}'
+      }
+    }
+
+    let(:result) {
+      {
+        'neutron' => {
+          'memory' => {
+            'memory.soft_limit_in_bytes' => 512
+          }
+        }
+      }
+    }
+
+    it 'should transform hash including expression to compute' do
+      should run.with_params(sample).and_return(result)
+    end
+
+  end
+
+  context "wrong JSON format" do
+
+    let(:sample) {
+      {
+        'neutron' => '{"memory":{"memory.soft_limit_in_bytes":"%50, 300, 700"}}}}'
+      }
+    }
+
+    let(:result) {
+      {}
+    }
+
+    it 'should handle wrong JSON format' do
+      should run.with_params(sample).and_return(result)
+    end
+
+  end
+
+  context "service's cgroup settings are not a HASH" do
+
+    let(:sample) {
+      {
+        'neutron' => '28'
+      }
+    }
+
+    let(:result) {
+      {}
+    }
+
+    it 'should handle wrong cgroup settings format' do
+      should run.with_params(sample).and_return(result)
+    end
+
+  end
+
+  context "cgroup limit is not an integer" do
+
+    let(:sample) {
+      {
+        'neutron' => '{"memory":{"memory.soft_limit_in_bytes":"test"}}'
+      }
+    }
+
+    let(:result) {
+      {}
+    }
+
+    it 'should handle wrong cgroup settings format' do
+      should run.with_params(sample).and_return(result)
+    end
+
+  end
+
+end
