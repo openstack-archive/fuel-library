d72f830d42280bbb6504491e2590366ed01d1ef7
slava <vvalyavskiy@mirantis.com>
2016-02-17 13:45:03 +0300

    Add cgroup task into tasks graph
    Change-Id: If072e54a285f88073e67db25950bd5cf4b51511b
    Implements: blueprint cgroups

---
+++ tests/noop/spec/hosts/cgroups/cgroups_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'cgroups/cgroups.pp'
+
+cgroups_hash = Noop.hiera_structure('cgroups', {})
+describe manifest do
+
+  before(:each) do
+    Noop.puppet_function_load :prepare_cgroups_hash
+    MockFunction.new(:prepare_cgroups_hash) do |function|
+      allow(function).to receive(:call).with(cgroups_hash).and_return({})
+    end
+  end
+
+  shared_examples 'catalog' do
+    if cgroups_hash
+      it 'should declare cgroups class correctly' do
+        should contain_class('cgroups').with(
+          'cgroups_set'  => {},
+        )
+      end
+    end
+  end
+  test_ubuntu_and_centos manifest
+end
