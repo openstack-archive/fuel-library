21fc211a65896ac6d0a11baf45e4cfd3c7a0bbaf
Alex Schultz <aschultz@mirantis.com>
2016-02-18 13:03:16 -0700

    Fix access for detached database plugin
    With the switch to the upstream mysql/galera modules we lost the
    additional grants used by the detached database plugin to allow for
    database creation and management from other hosts. This change brings
    back the additional grants for access along with tests.
    
    Additionally with this change we are adding an ensure_resource for the
    /root/.my.cnf because when the detached database used the file does not
    exist in the catalog to be switched to a symlink with the spaceship
    operator.
    
    Change-Id: I93c985db86ea1f1d27396ee0ef2920b9a9ee2802
    Closes-Bug: #1547150

---
+++ deployment/puppet/osnailyfacter/manifests/mysql_user_access.pp
@@ -0,0 +1,37 @@
+# == Class osnailyfacter::mysql_user_access
+#
+# Class that creates a user and gives root access
+#
+# === Parameters:
+#
+# [*title*]
+#  (required) The IP address of the networks to allow access from
+#
+# [*ensure*]
+#  (optional) Ensures to override .my.cnf
+#  Defaults to 'present'
+#
+# [*db_user*]
+#  (optional) The mysql user to create
+#  Defaults to 'root'
+#
+# [*db_password_hash*]
+#  (optional) The mysql passowrd hash for the user
+#  Defaults to ''
+#
+# [*access_networks*]
+#  (optional) Array of networks to allow access from
+#  Defaults to ['localhost']
+#
+class osnailyfacter::mysql_user_access (
+  $ensure           = 'present',
+  $db_user          = 'root',
+  $db_password_hash = '',
+  $access_networks  = ['localhost']
+) {
+  validate_array($access_networks)
+  ::osnailyfacter::mysql_grant { $access_networks:
+    user          => $db_user,
+    password_hash => $db_password_hash
+  }
+}
