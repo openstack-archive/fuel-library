5f7b216853487868fb50dfef69990ac00e45e53a
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-02-18 17:38:15 +0100

    Fix noop tests run settings
    Do not put debug messages.
    Show all test results instead of failed only.
    Also fix bashisms in rm commands.
    
    Related blueprint: deployment-data-dryrun
    
    Change-Id: I05438471873efb44e7432c6544ea44919e6e0a8c
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- tests/noop/purge_globals.sh
+++ tests/noop/purge_globals.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 DIR=`dirname $0`
 cd "${DIR}" || exit 1
-rm -v fuel-noop-fixtures/reports/*.{json,xml}
+rm -v fuel-noop-fixtures/reports/*.json
+rm -v fuel-noop-fixtures/reports/*.xml
 rm -v fuel-noop-fixtures/hiera/globals/*.yaml
