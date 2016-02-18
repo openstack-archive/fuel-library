a056b2b755ffaa25c4ab7243c280e7a6513a9706
Valeriy Sakharov <vsakharov@mirantis.com>
2016-02-18 19:36:36 +0300

    Add cgroups module
    This module provide the ability to use Cgroups on the nodes if user enabled it in
    the settings.yaml file.
    
    Change-Id: Iada5a84cab024fe1720798a747f577c43f2bc6f3
    Implements: blueprint cgroups

---
+++ deployment/puppet/cgroups/manifests/init.pp
@@ -0,0 +1,46 @@
+# == Class: cgroups
+#
+# CGroups is a Linux kernel feature that manage the resource usage (CPU, memory,
+# disk I/O, network, etc.) of a collection of processes.
+#
+# === Parameters
+#
+# [*cgroups_set*]
+#   (required) Hiera hash with cgroups settings
+# [*packages*]
+#   (required) Names of packages for CGroups
+#
+
+class cgroups(
+  $cgroups_set   = {},
+  $packages      = $cgroups::params::packages,
+)
+  inherits cgroups::params
+{
+
+  ensure_packages($packages)
+
+  File {
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+  }
+
+  file { '/etc/cgconfig.conf':
+      content => template('cgroups/cgconfig.conf.erb'),
+  }
+
+  file { '/etc/cgrules.conf':
+      content => template('cgroups/cgrules.conf.erb'),
+   }
+
+   class { '::cgroups::service': }
+
+   Package<||> ->
+   File<||> ->
+   Service['cgroup-lite'] ->
+   Service['cgconfigparser'] ->
+   Cgclassify<||> ->
+   Service['cgrulesengd']
+}
