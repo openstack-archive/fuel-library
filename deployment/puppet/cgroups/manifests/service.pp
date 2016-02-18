3b7a2b729f4cf022702f4856094981a12463811c
Valeriy Sakharov <vsakharov@mirantis.com>
2016-02-18 19:36:36 +0300

    Add cgroups module
    This module installs CGroups on the nodes if user enabled it in
    the settings.yaml file.
    
    Change-Id: Iada5a84cab024fe1720798a747f577c43f2bc6f3
    Implements: blueprint cgroups

---
+++ deployment/puppet/cgroups/manifests/service.pp
@@ -0,0 +1,21 @@
+class cgroups::service
+  inherits cgroups::params
+{
+  notify {$cgroups_set: }
+
+  service { 'cgroup-lite':
+    ensure => running,
+    enable => true,
+  }
+
+  service { 'cgconfigparser':
+    ensure  => running,
+    require => Service['cgroup-lite'],
+  }
+
+  service { 'cgrulesengd':
+    ensure  => running,
+  }
+
+  generate_cgclassify($cgroups_set)
+}
