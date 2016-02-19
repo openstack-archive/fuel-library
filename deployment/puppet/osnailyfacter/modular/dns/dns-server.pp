67a06471aad45d3bc6a4c2704db320b3a63c2105
Alex Schultz <aschultz@mirantis.com>
2016-02-19 14:11:21 -0700

    Fix expected input to osnailyfacter::dnsmasq
    The osnailyfacter::dnsmasq expects the external_dns parameter to be an
    array. This change adds validation to the dns server list as well as
    adding support for a single ip address provided as a string. If the list
    of dns servers is not an array or a single ip address (as string), a
    failure message is presented to let the user know that an invalid set of
    dns servers was provided.
    
    Change-Id: I6152b8d73a7b97b30d1d1fed9c8d1f055a9d933b
    Closes-Bug: #1547654

--- deployment/puppet/osnailyfacter/modular/dns/dns-server.pp
+++ deployment/puppet/osnailyfacter/modular/dns/dns-server.pp
@@ -7,11 +7,19 @@ $management_vrouter_vip = hiera('management_vrouter_vip')
 $network_metadata       = hiera_hash('network_metadata', {})
 $vrouter_name           = hiera('vrouter_name', 'pub')
 
+if is_array($dns_servers['dns_list']) {
+  $external_dns = $dns_servers['dns_list']
+} elsif is_ip_address($dns_servers['dns_list']) {
+  $external_dns = any2array($dns_servers['dns_list'])
+} else {
+  $external_dns = split($dns_servers['dns_list'], ',')
+}
+
 # If VIP has namespace set to 'false' or 'undef' then we do not configure it
 # under corosync cluster. So we should not configure anything listening it.
 if $network_metadata['vips']["vrouter_${vrouter_name}"]['namespace'] {
   class { 'osnailyfacter::dnsmasq':
-    external_dns           => $dns_servers['dns_list'],
+    external_dns           => $external_dns,
     master_ip              => $master_ip,
     management_vrouter_vip => $management_vrouter_vip,
   } ->
