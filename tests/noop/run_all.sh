a4405428b11a886a7ad07d36d9073839a3a92854
Dmitry Ilyin <dilyin@mirantis.com>
2016-02-24 20:53:29 +0300

    Prepare wrappers for noop update
    * Add script to purge saved catalogs
    * Remove external bundle update from setup script
      It should be working internally now
    * Improve setup script. Update fixtures repo
      if possible.
    * Reset external Puppet modules instead of
      just installing them
    * Switch to autodetection of job count
    
    Change-Id: I19852a73ff151273160f42beea2867cd9ce1c69f
    Depends-On: https://review.openstack.org/#/c/288424/

--- tests/noop/run_all.sh
+++ tests/noop/run_all.sh
@@ -2,6 +2,4 @@
 DIR=`dirname $0`
 cd "${DIR}" || exit 1
 echo "Running all spec tasks..."
-procs=$(grep -c processor /proc/cpuinfo || sysctl -n hw.logicalcpu)
-concur=${USER_SPECIFIED_JOBS:-$procs}
-./noop_tests.sh -j $concur -x $@
+./noop_tests.sh -j 'auto' -x $@
