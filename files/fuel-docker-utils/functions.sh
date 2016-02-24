openstack-archive/fuel-library

commit 4207659be2a20dbd0c3bd0f20dc50751694b0061

Changes in dockerct script:
  * Add posibility use systemctl for container start , stop instead using docker because systemd restarts container if it is stopped not by systemctl.
  * Fix  free space checking

Change-Id: I17fcb58ccd2384caebb277e904b3877e5ec461f1
Closes-Bug: #1536314
Closes-Bug: #1538052

files/fuel-docker-utils/functions.sh
@@ -284,7 +284,11 @@ function start_container {
       # Clean up broken mounts if needed
       id=$(get_container_id $container_name)
       grep "$id" /proc/mounts | awk '{print $2}' | sort -r | xargs --no-run-if-empty -n1 umount -l 2>/dev/null
-      ${DOCKER} start $container_name
+      if [ -x "$SYSTEMCTL" ] ; then
+        ${SYSTEMCTL} start "docker-${container}"
+      else
+        ${DOCKER} start $container_name
+      fi
     fi
     post_start_hooks $1
     if [ -z "$SUPERVISOR_PROCESS_NAME" -a -n "$(get_supervisor_pid)" ]; then
@@ -301,10 +305,21 @@ function start_container {
   unlock
 }
 
+function get_CONTAINER_NAMES_idx {
+  for idx in ${!CONTAINER_NAMES[@]} ; do
+    [  "${CONTAINER_NAMES[${idx}]}" == "$1" ] && echo $idx
+  done
+}
+
+
 function shutdown_container {
   echo "Stopping $1..."
   kill $2
-  ${DOCKER} stop $1
+  if [ -x "$SYSTEMCTL" ] ; then
+    ${SYSTEMCTL} stop "docker-$( get_CONTAINER_NAMES_idx $1) "
+  else
+    ${DOCKER} stop ${1}
+  fi
   exit 0
 }
 
@@ -368,8 +383,11 @@ function stop_container {
     if [ -z "$SUPERVISOR_PROCESS_NAME" -a -n "$(get_supervisor_pid)" ]; then
       supervisorctl stop "docker-${container}" > /dev/null
     fi
-
-    ${DOCKER} stop ${CONTAINER_NAMES[$container]}
+      if [ -x "$SYSTEMCTL" ] ; then
+        ${SYSTEMCTL} stop "docker-${container}"
+      else
+        ${DOCKER} stop ${CONTAINER_NAMES[$container]}
+      fi
   done
   unlock
 }
@@ -432,7 +450,11 @@ function inspect {
 }
 
 function restart_container {
-  ${DOCKER} restart ${CONTAINER_NAMES[$1]}
+  if [ -x "$SYSTEMCTL" ] ; then
+    ${SYSTEMCTL} restart "docker-${1}"
+  else
+    ${DOCKER} restart ${CONTAINER_NAMES[$1]}
+  fi
 }
 
 function container_lookup {
@@ -586,7 +608,7 @@ function backup {
   parse_backup_dir $1
   [[ $use_rsync -eq 1 ]] && ssh_copy_id
   mkdir -p $SYSTEM_DIRS $backup_dir
-  [[ "$backup_dir" =~ var ]] && [[ "$fullbackup" == "1" ]] && verify_disk_space "backup"
+  verify_disk_space "backup" "$fullbackup"
   if check_nailgun_tasks; then
     echo "There are currently running Fuel tasks. Please wait for them to \
 finish or cancel them." 1>&2
@@ -878,13 +900,13 @@ function verify_disk_space {
   if [[ "$2" != "$fullbackup" ]]; then
     #2gb free space required for light backup
     (( required  = 2 * 1024 * 1024 ))
-    spaceerror="Insufficient disk space to perform $1. At least 2gb must be free on /var partition."
+    spaceerror="Insufficient disk space to perform $1. At least 2gb must be free on ${BACKUP_ROOT} partition."
   else
      #11gb free space required to backup and restore
      (( required = 11 * 1024 * 1024 ))
-    spaceerror="Insufficient disk space to perform $1. At least 11gb must be free on /var partition."
+    spaceerror="Insufficient disk space to perform $1. At least 11gb must be free on ${BACKUP_ROOT} partition."
   fi
-  avail=$(df /var | grep /var | awk '{print $4}')
+  avail=$(df --output=avail ${BACKUP_ROOT} | tail -1)
   if (( avail < required )); then
     echo "$spaceerror" 1>&2
     exit 1

deployment/puppet/docker/templates/dockerctl_config.erb
@@ -16,6 +16,8 @@ if [ -z "$VERSION" ]; then
   VERSION="_VERSION_"
 fi
 
+[[ "$VERSION" < "8.0" ]] || SYSTEMCTL=/usr/bin/systemctl
+
 SYSTEMD="<%= @use_systemd.to_s %>"
 
 IMAGE_PREFIX="fuel"

files/fuel-docker-utils/dockerctl_config
@@ -10,6 +10,7 @@ FUEL_RELEASE_FILE="/etc/fuel_release"
 DOCKER_ENGINE="native"
 ADMIN_IP="10.20.0.2"
 LOCAL_IP="127.0.0.1"
+SYSTEMCTL="/usr/bin/systemctl"
 
 # Version of Fuel to deploy
 VERSION=${VERSION_OVERRIDE:-$(cat $FUEL_RELEASE_FILE)}

commit e7d0ab4f8b47bf4bb194836e3f147c609638b8dc

Fix disk space calculation for custom backup locations
This fixes a regression introduced by
4207659be2a20dbd0c3bd0f20dc50751694b0061 which broke behavior
where disk space checking was skipped for non-default locations.

Also fixes 02393e503db32ee3c790e249df11c967dfcd1af4 which
accidentally forced full backups.

Change-Id: I44174bf7f9aca2d4242a275a7dcedc4545489e8f
Closes-Bug: #1544204

files/fuel-docker-utils/functions.sh
@@ -609,7 +609,7 @@ function backup {
   #Sets backup_dir
   parse_backup_dir $1
   [[ $use_rsync -eq 1 ]] && ssh_copy_id
-  mkdir -p $SYSTEM_DIRS $backup_dir
+  mkdir -p $SYSTEM_DIRS $BACKUP_ROOT $backup_dir
   verify_disk_space "backup" "$fullbackup"
   if check_nailgun_tasks; then
     echo "There are currently running Fuel tasks. Please wait for them to \
@@ -665,7 +665,7 @@ function parse_backup_dir {
     backup_dir="${BACKUP_ROOT}/backup_${backup_id}"
     rsync_dest="$1"
   else
-    echo "Unrecognized backup destination. Valid options include:" 1>&2
+    echo "Unrecognized backup destination or path does not exist. Valid options include:" 1>&2
     echo "  (blank)           - backup to $BACKUP_ROOT" 1>&2
     echo "  /path/to/backup   - local backup directory" 1>&2
     echo "  user@server:/path - backup using rsync to server" 1>&2
@@ -983,17 +983,19 @@ function verify_disk_space {
     echo "Backup or restore operation not specified." 1>&2
     exit 1
   fi
-  fullbackup=1
-  if [[ "$2" != "$fullbackup" ]]; then
+  if [[ -z "$backup_dir" || "$use_sync" == "1" ]]; then
+    local backup_dir=$BACKUP_ROOT
+  fi
+  if [[ "$2" != 1 ]]; then
     #2gb free space required for light backup
     (( required  = 2 * 1024 * 1024 ))
-    spaceerror="Insufficient disk space to perform $1. At least 2gb must be free on ${BACKUP_ROOT} partition."
+    spaceerror="Insufficient disk space to perform $1. At least 2gb must be free on ${backup_dir} partition."
   else
      #11gb free space required to backup and restore
      (( required = 11 * 1024 * 1024 ))
-    spaceerror="Insufficient disk space to perform $1. At least 11gb must be free on ${BACKUP_ROOT} partition."
+    spaceerror="Insufficient disk space to perform $1. At least 11gb must be free on ${backup_dir} partition."
   fi
-  avail=$(df --output=avail ${BACKUP_ROOT} | tail -1)
+  avail=$(df --output=avail ${backup_dir} | tail -1)
   if (( avail < required )); then
     echo "$spaceerror" 1>&2
     exit 1