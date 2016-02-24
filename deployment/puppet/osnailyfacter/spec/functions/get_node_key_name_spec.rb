f58019de1b4e294e0fef5c29b2f54ed3aba439d6
Kyrylo Galanov <kgalanov@mirantis.com>
2016-02-24 12:08:30 +0200

    Remove hardcoded node name
    Switch from <node-N> naming template to arbitrary host names
    
    Change-Id: Ibef04adb54e56fe80b9468ef7f21b37fb909d968
    Closes-bug: #1548332

--- deployment/puppet/osnailyfacter/spec/functions/get_node_key_name_spec.rb
+++ deployment/puppet/osnailyfacter/spec/functions/get_node_key_name_spec.rb
@@ -10,12 +10,17 @@ describe 'the get_node_key_name function' do
   end
 
   it 'should be able to calculate node key name' do
-    scope.stubs(:function_hiera).with(['uid']).returns('121')
-    expect(scope.function_get_node_key_name []).to eq 'node-121'
+    scope.stubs(:function_hiera).with(['uid']).returns('1')
+    scope.stubs(:function_hiera_hash).with(['network_metadata']).returns({"nodes"=>
+               {"controller-21853"=>{"swift_zone"=>"2", "uid"=>"2", "fqdn"=>"controller-21853.test.domain.local"},
+                "controller-9864"=>{"swift_zone"=>"1", "uid"=>"1", "fqdn"=>"controller-9864.test.domain.local"}
+               }})
+    expect(scope.function_get_node_key_name []).to eq 'controller-9864'
   end
 
   it 'should raise error if UID not gived' do
     scope.stubs(:function_hiera).with(['uid']).returns(nil)
+    scope.stubs(:function_hiera_hash).with(['network_metadata']).returns({})
     expect{scope.function_get_node_key_name []}.to raise_error(Puppet::ParseError)
   end
 
