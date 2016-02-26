71008d6e3660a543158dfafdcf2f522254ff8877
Alex Schultz <aschultz@mirantis.com>
2016-02-25 21:19:29 -0700

    Fix openstack firewall tests under puppet4
    This change fixes the firewall tests by correcting the scope.new
    function call for puppet4.
    
    Change-Id: I522aa56844c9d1e3868520263b0e29a9732967da
    Closes-Bug: #1550123

--- deployment/puppet/openstack/spec/functions/prepare_firewall_rules__spec.rb
+++ deployment/puppet/openstack/spec/functions/prepare_firewall_rules__spec.rb
@@ -5,11 +5,7 @@ describe 'function to prepare hash of firewall rules for multiple networks' do
 
   def setup_scope
     @compiler = Puppet::Parser::Compiler.new(Puppet::Node.new("floppy", :environment => 'production'))
-    if Puppet.version =~ /^3\./
-      @scope = Puppet::Parser::Scope.new(@compiler)
-    else
-      @scope = Puppet::Parser::Scope.new(:compiler => @compiler)
-    end
+    @scope = Puppet::Parser::Scope.new(@compiler)
     @topscope = @topscope
     @scope.parent = @topscope
     Puppet::Parser::Functions.function(:prepare_firewall_rules)
