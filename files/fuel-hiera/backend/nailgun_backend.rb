059249947993192011f11d5bb02e2d4d880811d8
Sergey Ryabin <sryabin@mirantis.com>
2016-02-26 14:14:37 +0300

    Nailgun backend for hiera
    Implements: blueprint upload-deployment-facts-to-configdb
    Change-Id: I2f5dd6b6033565e2353b1eab75146c703856a8c5

---
+++ files/fuel-hiera/backend/nailgun_backend.rb
@@ -0,0 +1,119 @@
+require 'net/http'
+require 'json'
+
+def handle_request(req, url)
+  begin
+
+    use_ssl = url.scheme == 'https'
+    r = Net::HTTP.start(url.hostname, url.port, ':use_ssl' => use_ssl) do |http|
+      http.request(req)
+    end
+
+
+    if r.code != '200'
+      raise "Received error response from http server at #{url}: #{r.message}"
+    end
+
+  rescue Errno::ECONNREFUSED => detail
+    raise "Failed to connect to http server at #{url}: #{detail}"
+  rescue SocketError => detail
+    raise "Failed to connect to http server at #{url}: #{detail}"
+  end
+
+  r
+end
+
+def keystone_v2_authenticate(auth_url,
+                             username,
+                             password,
+                             tenant_name)
+
+  post_args = {
+    'auth' => {
+      'passwordCredentials' => {
+        'username' => username,
+        'password' => password,
+      },
+      'tenantName' => tenant_name
+    }
+  }
+
+  url = URI.parse("#{auth_url}/tokens")
+  req = Net::HTTP::Post.new url.to_s
+  req['content-type'] = 'application/json'
+  req.body = post_args.to_json
+
+  res = handle_request(req, url)
+  data = JSON.parse res.body
+  [data['access']['token']['id'], data]
+end
+
+class Hiera
+  module Backend
+    class Nailgun_backend
+      # TODO: refact cache
+      def initialize(_cache = nil)
+        @nailgun_config = Config[:nailgun]
+        @keystone_config = Config[:keystone]
+
+        Hiera.debug('Hiera nailgun backend starting')
+
+        @keystone_url = @keystone_config[:endpoint] + '/' \
+          + @keystone_config[:api]
+
+        @authtoken = keystone_v2_authenticate(
+          @keystone_url,
+          @keystone_config[:credentials][:user],
+          @keystone_config[:credentials][:pass],
+          @keystone_config[:credentials][:tenant])[0]
+
+        @nailgun_url = @nailgun_config[:endpoint] + '/api/' \
+          + @nailgun_config[:api]
+
+        @cache = @cluster_id = @node_id = nil
+      end
+
+      def lookup(key, scope, _order_override, _resolution_type)
+        if @node_id.nil?
+          (@node_id, @cluster_id) = nailgun_node_id(scope['fqdn'])
+          raise 'Failed to lookup node in nailgun' if @node_id.nil?
+          Hiera.debug("Nailgun node id #{@node_id}, cluster: #{@cluster_id}")
+        end
+
+        if @cache.nil?
+          Hiera.debug("Lookup #{key} from nailgun")
+          return nailgun_api_request(key)
+        else
+          Hiera.debug("Lookup #{key} from cache")
+          return @cache[key]
+        end
+      end
+
+      private
+
+      # TODO: single nailgun request method
+      def nailgun_node_id(fqdn)
+        url = URI.parse(@nailgun_url + '/nodes')
+        req = Net::HTTP::Get.new url.to_s
+        req['X-Auth-Token'] = @authtoken
+        res = handle_request(req, url)
+        data = JSON.parse res.body
+        data.each do |node|
+          return node['id'], node['cluster'] if node['fqdn'] == fqdn
+        end
+        [nil, nil]
+      end
+
+      def nailgun_api_request(key)
+        url = URI.parse(@nailgun_url + '/clusters/' + @cluster_id.to_s + \
+          '/orchestrator/deployment/defaults/?nodes=' + @node_id.to_s)
+        req = Net::HTTP::Get.new url.to_s
+        req['X-Auth-Token'] = @authtoken
+        res = handle_request(req, url)
+        data = JSON.parse res.body
+        @cache = data[0]
+        @cache[key]
+      end
+    end
+  end
+end
