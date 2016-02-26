3665602f77437ebc78adc35f920687659aa6fbde
Andrew Woodward <awoodward@mirantis.com>
2016-02-26 15:12:02 -0800

    Drop openstack::glance
    This uplifts the openstack::glance directly into the calling task.
    
    Change-Id: I31abcdd8f7f56f6a52c2ad48f5fcf698500d5b3a
    Blueprint: fuel-remove-conflict-openstack

--- deployment/puppet/osnailyfacter/modular/glance/glance.pp
+++ deployment/puppet/osnailyfacter/modular/glance/glance.pp
@@ -25,8 +25,6 @@ $service_workers       = pick($glance_hash['glance_workers'],
 $ironic_hash           = hiera_hash('ironic', {})
 $primary_controller    = hiera('primary_controller')
 
-$default_log_levels             = hiera_hash('default_log_levels')
-
 $db_type      = 'mysql'
 $db_host      = pick($glance_hash['db_host'], $database_vip)
 $db_user      = pick($glance_hash['db_user'], 'glance')
@@ -47,15 +45,14 @@ $db_connection = os_database_connection({
   'extra'    => $extra_params
 })
 
-$api_bind_address               = get_network_role_property('glance/api', 'ipaddr')
+$bind_host                      = get_network_role_property('glance/api', 'ipaddr')
 $enabled                        = true
 $max_retries                    = '-1'
 $idle_timeout                   = '3600'
 
 $rabbit_password                = $rabbit_hash['password']
-$rabbit_user                    = $rabbit_hash['user']
+$rabbit_userid                  = $rabbit_hash['user']
 $rabbit_hosts                   = split(hiera('amqp_hosts',''), ',')
-$rabbit_virtual_host            = '/'
 
 $glance_user                    = pick($glance_hash['user'],'glance')
 $glance_user_password           = $glance_hash['user_password']
@@ -68,7 +65,7 @@ $glance_vcenter_datastore       = $glance_hash['vc_datastore']
 $glance_vcenter_image_dir       = $glance_hash['vc_image_dir']
 $glance_vcenter_api_retry_count = '20'
 $glance_image_cache_max_size    = $glance_hash['image_cache_max_size']
-$glance_pipeline                = pick($glance_hash['pipeline'], 'keystone')
+$pipeline                       = pick($glance_hash['pipeline'], 'keystone')
 $glance_large_object_size       = pick($glance_hash['large_object_size'], '5120')
 
 $ssl_hash               = hiera_hash('use_ssl', {})
@@ -85,64 +82,17 @@ $rados_connect_timeout          = '30'
 
 if ($storage_hash['images_ceph'] and !$ironic_hash['enabled']) {
   $glance_backend = 'ceph'
-  $glance_known_stores = [ 'glance.store.rbd.Store', 'glance.store.http.Store' ]
-  $glance_show_image_direct_url = pick($glance_hash['show_image_direct_url'], true)
+  $known_stores = [ 'glance.store.rbd.Store', 'glance.store.http.Store' ]
+  $show_image_direct_url = pick($glance_hash['show_image_direct_url'], true)
 } elsif ($storage_hash['images_vcenter']) {
   $glance_backend = 'vmware'
-  $glance_known_stores = [ 'glance.store.vmware_datastore.Store', 'glance.store.http.Store' ]
-  $glance_show_image_direct_url = pick($glance_hash['show_image_direct_url'], true)
+  $known_stores = [ 'glance.store.vmware_datastore.Store', 'glance.store.http.Store' ]
+  $show_image_direct_url = pick($glance_hash['show_image_direct_url'], true)
 } else {
   $glance_backend = 'swift'
-  $glance_known_stores = [ 'glance.store.swift.Store', 'glance.store.http.Store' ]
+  $known_stores = [ 'glance.store.swift.Store', 'glance.store.http.Store' ]
   $swift_store_large_object_size = $glance_large_object_size
-  $glance_show_image_direct_url = pick($glance_hash['show_image_direct_url'], false)
-}
-
-###############################################################################
-
-class { 'openstack::glance':
-  verbose                        => $verbose,
-  debug                          => $debug,
-  default_log_levels             => $default_log_levels,
-  db_connection                  => $db_connection,
-  glance_user                    => $glance_user,
-  glance_user_password           => $glance_user_password,
-  glance_tenant                  => $glance_tenant,
-  glance_vcenter_host            => $glance_vcenter_host,
-  glance_vcenter_user            => $glance_vcenter_user,
-  glance_vcenter_password        => $glance_vcenter_password,
-  glance_vcenter_datacenter      => $glance_vcenter_datacenter,
-  glance_vcenter_datastore       => $glance_vcenter_datastore,
-  glance_vcenter_image_dir       => $glance_vcenter_image_dir,
-  glance_vcenter_api_retry_count => $glance_vcenter_api_retry_count,
-  auth_uri                       => $auth_uri,
-  identity_uri                   => $identity_uri,
-  glance_protocol                => 'http',
-  region                         => $region,
-  bind_host                      => $api_bind_address,
-  primary_controller             => $primary_controller,
-  enabled                        => $enabled,
-  glance_backend                 => $glance_backend,
-  registry_host                  => $glance_endpoint,
-  use_syslog                     => $use_syslog,
-  use_stderr                     => $use_stderr,
-  show_image_direct_url          => $glance_show_image_direct_url,
-  swift_store_large_object_size  => $swift_store_large_object_size,
-  pipeline                       => $glance_pipeline,
-  syslog_log_facility            => $syslog_log_facility,
-  glance_image_cache_max_size    => $glance_image_cache_max_size,
-  max_retries                    => $max_retries,
-  max_pool_size                  => $max_pool_size,
-  max_overflow                   => $max_overflow,
-  idle_timeout                   => $idle_timeout,
-  rabbit_password                => $rabbit_password,
-  rabbit_userid                  => $rabbit_user,
-  rabbit_hosts                   => $rabbit_hosts,
-  rabbit_virtual_host            => $rabbit_virtual_host,
-  known_stores                   => $glance_known_stores,
-  notification_driver            => $ceilometer_hash['notification_driver'],
-  service_workers                => $service_workers,
-  rados_connect_timeout          => $rados_connect_timeout,
+  $show_image_direct_url = pick($glance_hash['show_image_direct_url'], false)
 }
 
 ####### Disable upstart startup on install #######
@@ -154,3 +104,163 @@ if($::operatingsystem == 'Ubuntu') {
     package_name => 'glance-registry',
   }
 }
+
+
+# Install and configure glance-api
+class { 'glance::api':
+  verbose                => $verbose,
+  debug                  => $debug,
+  bind_host              => $bind_host,
+  auth_type              => 'keystone',
+  auth_uri               => $auth_uri,
+  identity_uri           => $identity_uri,
+  keystone_user          => $glance_user,
+  keystone_password      => $glance_user_password,
+  keystone_tenant        => $glance_tenant,
+  database_connection    => $db_connection,
+  enabled                => $enabled,
+  workers                => $service_workers,
+  registry_host          => $glance_endpoint,
+  use_syslog             => $use_syslog,
+  use_stderr             => $use_stderr,
+  log_facility           => $syslog_log_facility,
+  database_idle_timeout  => $idle_timeout,
+  database_max_pool_size => $max_pool_size,
+  database_max_retries   => $max_retries,
+  database_max_overflow  => $max_overflow,
+  show_image_direct_url  => $show_image_direct_url,
+  pipeline               => $pipeline,
+  known_stores           => $known_stores,
+  os_region_name         => $region,
+  delayed_delete         => false,
+  scrub_time             => '43200',
+  auth_region            => $region,
+  signing_dir            => '/tmp/keystone-signing-glance',
+  token_cache_time       => '-1',
+  image_cache_stall_time => '86400',
+  image_cache_max_size   => $glance_image_cache_max_size,
+}
+
+glance_api_config {
+  'DEFAULT/scrubber_datadir': value => '/var/lib/glance/scrubber';
+}
+
+
+# TODO (iberezovskiy): use glance::cache::logging class to setup
+# these parameters after new sync for glance module
+# (https://review.openstack.org/#/c/238096/)
+glance_cache_config {
+  'DEFAULT/image_cache_dir':  value => '/var/lib/glance/image-cache/';
+  'DEFAULT/os_region_name':   value => $region;
+}
+
+# Install and configure glance-registry
+class { 'glance::registry':
+  verbose                => $verbose,
+  debug                  => $debug,
+  bind_host              => $bind_host,
+  auth_uri               => $auth_uri,
+  identity_uri           => $identity_uri,
+  auth_type              => 'keystone',
+  keystone_user          => $glance_user,
+  keystone_password      => $glance_user_password,
+  keystone_tenant        => $glance_tenant,
+  database_connection    => $db_connection,
+  database_max_pool_size => $max_pool_size,
+  database_max_retries   => $max_retries,
+  database_max_overflow  => $max_overflow,
+  enabled                => $enabled,
+  use_syslog             => $use_syslog,
+  use_stderr             => $use_stderr,
+  log_facility           => $syslog_log_facility,
+  database_idle_timeout  => $idle_timeout,
+  workers                => $service_workers,
+  sync_db                => $primary_controller,
+  signing_dir            => '/tmp/keystone-signing-glance',
+  os_region_name         => $region,
+}
+
+# puppet-glance assumes rabbit_hosts is an array of [node:port, node:port]
+# but we pass it as a amqp_hosts string of 'node:port, node:port' in Fuel
+if !is_array($rabbit_hosts) {
+  $rabbit_hosts_real = split($rabbit_hosts, ',')
+  glance_api_config {
+    'DEFAULT/kombu_reconnect_delay': value => 5.0;
+  }
+} else {
+  $rabbit_hosts_real = $rabbit_hosts
+}
+
+class { 'glance::notify::rabbitmq':
+  rabbit_password              => $rabbit_password,
+  rabbit_userid                => $rabbit_userid,
+  rabbit_hosts                 => $rabbit_hosts_real,
+  notification_driver          => $ceilometer_hash['notification_driver'],
+}
+
+# syslog additional settings default/use_syslog_rfc_format = true
+if $use_syslog {
+  glance_api_config {
+    'DEFAULT/use_syslog_rfc_format': value => true;
+  }
+  glance_cache_config {
+    'DEFAULT/use_syslog_rfc_format': value => true;
+  }
+  glance_registry_config {
+    'DEFAULT/use_syslog_rfc_format': value => true;
+  }
+}
+
+# Configure file storage backend
+case $glance_backend {
+  'swift': {
+    if !defined(Package['swift']) {
+      include ::swift::params
+      package { 'swift':
+        ensure => present,
+        name   => $::swift::params::package_name,
+      }
+    }
+    Package['swift'] ~> Service['glance-api']
+    Package['swift'] -> Swift::Ringsync <||>
+    Package<| title == 'swift'|> ~> Service<| title == 'glance-api'|>
+    if !defined(Service['glance-api']) {
+      notify{ "Module ${module_name} cannot notify service glance-api on package swift update": }
+    }
+    class { 'glance::backend::swift':
+      swift_store_user                    => "${glance_tenant}:${glance_user}",
+      swift_store_key                     => $glance_user_password,
+      swift_store_create_container_on_put => 'True',
+      swift_store_large_object_size       => $swift_store_large_object_size,
+      swift_store_auth_address            => "${auth_uri}/v2.0/",
+      swift_store_region                  => $region,
+    }
+  }
+  'rbd', 'ceph': {
+    Ceph::Pool<| title == $::ceph::glance_pool |> ->
+    class { 'glance::backend::rbd':
+      rbd_store_user        => 'images',
+      rbd_store_pool        => 'images',
+      rados_connect_timeout => $rados_connect_timeout,
+    }
+  }
+  'vmware': {
+    class { 'glance::backend::vsphere':
+        vcenter_host            => $glance_vcenter_host,
+        vcenter_user            => $glance_vcenter_user,
+        vcenter_password        => $glance_vcenter_password,
+        vcenter_datacenter      => $glance_vcenter_datacenter,
+        vcenter_datastore       => $glance_vcenter_datastore,
+        vcenter_image_dir       => $glance_vcenter_image_dir,
+        vcenter_api_retry_count => $glance_vcenter_api_retry_count
+    }
+  }
+  default: {
+    class { "glance::backend::${glance_backend}": }
+  }
+}
+
+# Configure cache pruner and cache cleaner
+Class['glance::api'] ->
+class { 'glance::cache::pruner': } ->
+class { 'glance::cache::cleaner': }
--- tests/noop/spec/hosts/glance/glance_spec.rb
+++ tests/noop/spec/hosts/glance/glance_spec.rb
@@ -58,12 +58,6 @@ describe manifest do
       )
     end
 
-    it 'should declare openstack::glance class with 4 processess on 4 CPU & 32G system' do
-      should contain_class('openstack::glance').with(
-        'service_workers' => '4',
-      )
-    end
-
     it 'should configure workers for API, registry services' do
       fallback_workers = [[facts[:processorcount].to_i, 2].max, workers_max.to_i].min
       service_workers = glance_config.fetch('glance_workers', fallback_workers)
@@ -83,9 +77,10 @@ describe manifest do
         else
             extra_params = '?charset=utf8'
         end
-        should contain_class('openstack::glance').with(
-            :db_connection => "mysql://#{glance_db_user}:#{glance_db_password}@#{database_vip}/#{glance_db_name}#{extra_params}"
-        )
+
+        db_connection = "mysql://#{glance_db_user}:#{glance_db_password}@#{database_vip}/#{glance_db_name}#{extra_params}"
+        should contain_glance_api_config('database/connection').with(:value => db_connection)
+        should contain_glance_registry_config('database/connection').with(:value => db_connection)
     end
 
     it 'should configure glance api config' do
