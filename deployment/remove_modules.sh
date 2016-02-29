53f0ab32edbbd72ffa7c55a932fa609fada9eef3
Dmitry Ilyin <dilyin@mirantis.com>
2016-02-29 23:46:53 +0300

    Make a new Puppet modules script
    * Replace old update_modules script with
      new implementation
    * Old scripts left to be shortcuts
    
    Change-Id: I16238653090c4342de88553e2b9be39b4848b08a

--- deployment/remove_modules.sh
+++ deployment/remove_modules.sh
@@ -1,15 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 # remove all external puppet modules
 
-DEPLOYMENT_DIR=$(cd `dirname $0` && pwd -P)
-PUPPET_FILES=$(find $DEPLOYMENT_DIR -name Puppetfile)
+dir=`dirname $0`
+cd "${dir}" || exit 1
 
-for f in $PUPPET_FILES; do
-  cat $f | grep "^mod '" | awk -F "'" '{ print $2 }' | while read module; do
-    if [ -d "${DEPLOYMENT_DIR}/puppet/${module}" ]; then
-      echo "Remove: puppet/${module}"
-      rm -rf "${DEPLOYMENT_DIR}/puppet/${module}"
-    fi
-  done
-  rm -f "${f}.lock"
-done
+./puppet_modules.rb -tv remove
