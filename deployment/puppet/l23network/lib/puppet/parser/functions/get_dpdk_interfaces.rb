dabe1c7906d0adfe80c387e6f7c7b18fb0b8525a
Vladimir Eremin <veremin@mirantis.com>
2016-03-01 14:04:15 +0300

    dpdk_driver moved to interfaces
    dpdk_driver was moved from transformation to network_scheme interfaces
    to address spec change to enable OVS-DPDK bond.
    
    Implements: blueprint support-dpdk
    Partially implements: blueprint support-dpdk-bond
    Depends-On: I29a9f3403edf4c64b3257e3f6265fee1abffe01b
    Change-Id: Ie6d31da1f096a2fd6791d1b22f92417d9fd027eb

--- deployment/puppet/l23network/lib/puppet/parser/functions/get_dpdk_interfaces.rb
+++ deployment/puppet/l23network/lib/puppet/parser/functions/get_dpdk_interfaces.rb
@@ -1,28 +1,21 @@
 require 'puppetx/l23_network_scheme'
 
 Puppet::Parser::Functions::newfunction(:get_dpdk_interfaces, :type => :rvalue, :doc => <<-EOS
-    This function gets list of interfaces and port transformations and
-    returns bus_info addresses and intended drivers for dpdk transformations.
+    This function gets list of interfaces and returns bus_info addresses and
+    intended drivers for dpdk transformations.
     ex: get_dpdk_interfaces() => [["0000:01:00.0", "igb_uio"]]
     EOS
   ) do |args|
-  dpdk_interfaces = {}
-
   cfg = L23network::Scheme.get_config(lookupvar('l3_fqdn_hostname'))
   return [] unless cfg
 
-  interfaces = cfg[:interfaces]
-  cfg[:transformations].each do |transform|
-    next unless transform[:name] ||\
-         transform[:provider].to_s.upcase == "DPDKOVS" ||\
-         transform[:action] == "add-port"
-
-    if_name = transform[:name].to_sym
-    bus_info = interfaces[if_name][:vendor_specific][:bus_info] if interfaces.has_key?(if_name)
-    dpdk_driver = transform[:vendor_specific][:dpdk_driver] if transform[:vendor_specific]
-    dpdk_interfaces[bus_info] = dpdk_driver if bus_info and dpdk_driver
+  dpdk_interfaces = {}
+  cfg[:interfaces].each do |if_name, if_data|
+    vendor_specific = if_data[:vendor_specific] || {}
+    bus_info = vendor_specific[:bus_info]
+    dpdk_driver = vendor_specific[:dpdk_driver]
+    dpdk_interfaces[bus_info] = dpdk_driver if bus_info && dpdk_driver
   end
-
   dpdk_interfaces.sort
 end
 
