14960556290aeea4c4f8133ab17cea02fc3a980a
Vladimir Eremin <veremin@mirantis.com>
2016-03-01 19:46:32 +0300

    Added dpdkovs provider for bond
    Centos part will not be done in this patch.
    
    Partially implements: blueprint support-dpdk
    Implements: blueprint support-dpdk-bond
    Depends-On: Ie6d31da1f096a2fd6791d1b22f92417d9fd027eb
    Change-Id: I70b7db9abc48bb18a39ed9ead5bd6fcd4658d179

--- deployment/puppet/l23network/lib/puppet/provider/l23_stored_config/dpdkovs_ubuntu.rb
+++ deployment/puppet/l23network/lib/puppet/provider/l23_stored_config/dpdkovs_ubuntu.rb
@@ -21,6 +21,10 @@ Puppet::Type.type(:l23_stored_config).provide(:dpdkovs_ubuntu, :parent => Puppet
         if_data[:if_type] = "ethernet"
         if_data[:if_provider] = :dpdkovs
         true
+    elsif if_data[:if_type] =~ /dpdkovsbond/
+        if_data[:if_type] = "bond"
+        if_data[:if_provider] = :dpdkovs
+        true
     else
         if_data[:if_provider] = nil
         false
@@ -33,29 +37,94 @@ Puppet::Type.type(:l23_stored_config).provide(:dpdkovs_ubuntu, :parent => Puppet
       :if_type        => 'ovs_type',
       :bridge         => 'ovs_bridge',
       :dpdk_port      => 'dpdk_port',
+      :bond_slaves    => 'ovs_bonds',
+      :bond_mode      => 'ovs_options',
+      :bond_miimon    => 'ovs_options',
+      :bond_use_carrier => 'ovs_options',
+      :bond_lacp_rate => 'ovs_options',
+      :bond_lacp      => 'ovs_options',
+      :bond_xmit_hash_policy => '', # unused
+      :bond_ad_select => '',
+      :bond_updelay   => 'ovs_options',
+      :bond_downdelay => 'ovs_options',
     })
     return rv
   end
 
+  def self.oneline_properties
+    {
+      :bond_mode => {
+          :field    => 'bond_mode',
+          :store_to => 'ovs_options'
+      },
+      :bond_updelay => {
+          :field    => 'bond_updelay',
+          :store_to => 'ovs_options'
+      },
+      :bond_downdelay => {
+          :field    => 'bond_downdelay',
+          :store_to => 'ovs_options'
+      },
+      :bond_lacp => {
+          :field    => 'lacp',
+          :store_to => 'ovs_options'
+      },
+      :bond_lacp_rate  => {
+          :field    => 'other_config:lacp-time',
+          :store_to => 'ovs_options'
+      },
+      :bond_miimon  => {
+          :field    => 'other_config:bond-miimon-interval',
+          :store_to => 'ovs_options'
+      },
+      :bond_use_carrier  => {
+          :field    => 'other_config:bond-detect-mode',
+          :store_to => 'ovs_options'
+      },
+    }
+  end
+  def oneline_properties
+    self.class.collected_properties
+  end
   def self.iface_file_header(provider)
     header = []
     props  = {}
-
-    ports = self.get_dpdk_ports_mapping()
-    dpdk_port = ports.map { |i,p| i if p[:interface] == provider.name}.compact[0]
-
     bridge = provider.bridge[0]
     props[:bridge] = bridge
-    props[:dpdk_port] = dpdk_port
-
     header << self.puppet_header
     header << "allow-#{bridge} #{provider.name}"
     header << "iface #{provider.name} inet #{provider.method}"
     return header, props
   end
 
+  def dpdk_port
+    dpdk_ports = self.class.get_dpdk_ports_mapping
+    dpdk_port = dpdk_ports[self.name]
+  end
+
+  def self.mangle__bond_slaves(val)
+    ports_dpdk_mapping = self.get_dpdk_ports_mapping.invert
+    val.split(/[\s,]+/).map {|i| ports_dpdk_mapping[i]}.sort
+  end
+
+  def self.unmangle__bond_slaves(provider, val)
+    dpdk_ports_mapping = self.get_dpdk_ports_mapping
+    if val.size < 1 or [:absent, :undef].include? Array(val)[0].to_sym
+      nil
+    else
+      val.map {|i| dpdk_ports_mapping[i]}.sort.join(' ')
+    end
+  end
+
+  def self.unmangle__bond_use_carrier(provider, data)
+    values = [ 'miimon', 'carrier' ]
+    rv = values[data.to_i] if data.to_i <= values.size
+    rv ||= nil
+  end
+
   def self.unmangle__if_type(provider, val)
     val = 'DPDKOVSPort' if val.to_s == 'ethernet'
+    val = 'DPDKOVSBond' if val.to_s == 'bond'
     val
   end
 end
