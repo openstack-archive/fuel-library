e97cb1523b0ff7d9434c801338ab0ce6d41a6c27
Dmitry Ilyin <dilyin@mirantis.com>
2016-03-02 19:35:29 +0300

    Don't show successful tasks and examples
    * Don't clutter the output with green tasks and
      examples. I only want to see failed ones
    
    Change-Id: I229e08d39b4529b35ccc1d65b369fcac49730576

--- tests/noop/run_all.sh
+++ tests/noop/run_all.sh
@@ -4,4 +4,4 @@ cd "${DIR}" || exit 1
 echo "Running all spec tasks..."
 procs=$(grep -c processor /proc/cpuinfo || sysctl -n hw.logicalcpu)
 concur=${USER_SPECIFIED_JOBS:-$procs}
-./noop_tests.sh -j $concur -x $@
+./noop_tests.sh -j $concur -x -o $@
