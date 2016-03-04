ac4484832f6a40805eee0ecb019d3ec92becb0fc
Denis Egorenko <degorenko@mirantis.com>
2016-03-04 17:17:20 +0300

    Set proper public address for swift endpoint for Ceph
    Currently we are not handling situation when we have Ceph with RadosGW
    enabled with enabled TLS/SSL. As a result we have wrong addresses
    for Swift endpoints in Keystone. This commit fixes that.
    
    Also this patch creates radosgw_keystone task for managing endpoint and
    removes unused parameters from ceph-osd and radosgw modulars.
    
    Change-Id: Id86d85a13fbec27265156a5b12fad0fd892c7cd9
    Closes-bug: #1540133

---
+++ deployment/puppet/osnailyfacter/modular/ceph/radosgw_keystone.pp
@@ -0,0 +1,53 @@
+$storage_hash = hiera_hash('storage', {})
+
+if $storage_hash['objects_ceph'] {
+  notice('MODULAR: radosgw/keystone.pp')
+  $public_vip      = hiera('public_vip')
+  $region          = hiera('region', 'RegionOne')
+  $admin_address   = hiera('management_vip')
+  $public_ssl_hash = hiera_hash('public_ssl')
+
+  $public_address    = $public_ssl_hash['services'] ? {
+    true    => $public_ssl_hash['hostname'],
+    default => $public_vip,
+  }
+  $public_protocol   = $public_ssl_hash['services'] ? {
+    true    => 'https',
+    default => 'http',
+  }
+  $public_url        = "${public_protocol}://${public_address}:8080/swift/v1"
+  $admin_url         = "http://${admin_address}:8080/swift/v1"
+
+  $haproxy_stats_url = "http://${service_endpoint}:10000/;csv"
+
+  haproxy_backend_status { 'keystone-admin' :
+    name  => 'keystone-2',
+    count => '200',
+    step  => '6',
+    url   => $haproxy_stats_url,
+  }
+
+  haproxy_backend_status { 'keystone-public' :
+    name  => 'keystone-1',
+    count => '200',
+    step  => '6',
+    url   => $haproxy_stats_url,
+  }
+
+  Haproxy_backend_status['keystone-admin'] -> Keystone::Resource::Service_identity['radosgw']
+  Haproxy_backend_status['keystone-public'] -> Keystone::Resource::Service_identity['radosgw']
+
+  validate_string($public_address)
+
+  keystone::resource::service_identity { 'radosgw':
+    configure_user      => false,
+    configure_user_role => false,
+    service_type        => 'object-store',
+    service_description => 'Openstack Object-Store Service',
+    service_name        => 'swift',
+    region              => $region,
+    public_url          => $public_url,
+    admin_url           => $admin_url,
+    internal_url        => $admin_url,
+  }
+}
---
+++ tests/noop/spec/hosts/ceph/radosgw_keystone_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'ceph/radosgw_keystone.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+    objects_ceph = Noop.hiera_structure('storage_hash/objects_ceph', false)
+    if objects_ceph
+      public_vip    = Noop.hiera('public_vip')
+      admin_address = Noop.hiera('management_vip')
+      public_ssl    = Noop.hiera_structure('public_ssl/services')
+      region        = Noop.hiera_structure('region', 'RegionOne')
+
+      if public_ssl
+        public_address  = Noop.hiera_structure('public_ssl/hostname')
+        public_protocol = 'https'
+      else
+        public_address  = public_vip
+        public_protocol = 'http'
+      end
+
+      it { should contain_haproxy_backend_status('keystone-public').that_comes_before('Keystone::Resource::Service_identity[radosgw]') }
+      it { should contain_haproxy_backend_status('keystone-admin').that_comes_before('Keystone::Resource::Service_identity[radosgw]') }
+
+      it 'should configure radosgw keystone endpoint' do
+        should contain_keystone__resource__service_identity('radosgw').with(
+          'configure_user'      => false,
+          'configure_user_role' => false,
+          'service_type'        => 'object-store',
+          'service_description' => 'Openstack Object-Store Service',
+          'service_name'        => 'swift',
+          'region'              => region,
+          'public_url'          => "#{public_protocol}://#{public_address}:8080/swift/v1",
+          'admin_url'           => "http://#{admin_address}:8080/swift/v1",
+          'internal_url'        => "http://#{admin_address}:8080/swift/v1",
+        )
+      end
+    end
+  end
+  test_ubuntu_and_centos manifest
+end
