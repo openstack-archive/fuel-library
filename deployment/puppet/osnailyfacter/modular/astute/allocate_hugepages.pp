bb02f4b5f3c7ef79a318b79aca35647c00a9da43
Michael Polenchuk <mpolenchuk@mirantis.com>
2016-03-04 11:33:51 +0300

    Add hugepages configuration task
    Configure and activate number of Huge Pages of 2Mb/1Gb size
    by setting nr_hugepages kernel parameter.
    
    Depends-On: Ia2150c1fcfcbf0506ba247a3d6182d84c8dd39fc
    Change-Id: Ibfbfa5bd37d74f9618299af39336acb33a18cf69
    Partially-Implements: blueprint support-hugepages

---
+++ deployment/puppet/osnailyfacter/modular/astute/allocate_hugepages.pp
@@ -0,0 +1,12 @@
+notice('MODULAR: allocate_hugepages.pp')
+
+$hugepages = hiera('hugepages', [])
+
+include sysfs
+
+sysfs_config_value { 'hugepages':
+  ensure => 'present',
+  name   => '/etc/sysfs.d/hugepages.conf',
+  value  => map_sysfs_hugepages($hugepages),
+  sysfs  => '/sys/devices/system/node/node*/hugepages/hugepages-*kB/nr_hugepages',
+}
---
+++ tests/noop/spec/hosts/astute/allocate_hugepages_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'astute/allocate_hugepages.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+    hugepages = Noop.hiera 'hugepages', []
+    mapped_sysfs_hugepages = hugepages.empty? ? { 'default' => 0 } : {
+      'node0/hugepages/hugepages-2048kB' => 512,
+      'node1/hugepages/hugepages-1048576kB' => 8,
+      'default' => 0
+    }
+
+    it "should allocate defined hugepages" do
+      should contain_class('sysfs')
+      should contain_sysfs_config_value('hugepages').with(
+        'ensure' => 'present',
+        'name'   => '/etc/sysfs.d/hugepages.conf',
+        'value'  => mapped_sysfs_hugepages,
+        'sysfs'  => '/sys/devices/system/node/node*/hugepages/hugepages-*kB/nr_hugepages',
+      )
+    end
+  end
+  test_ubuntu_and_centos manifest
+end
