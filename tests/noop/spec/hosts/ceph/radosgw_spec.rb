cea1856489d4ab41d0c0d3411a15be7fa60d68a3
Denis Egorenko <degorenko@mirantis.com>
2016-03-04 17:17:20 +0300

    Set proper public address for swift endpoint for Ceph
    Currently we are not handling situation when we have Ceph with RadosGW
    enabled with enabled TLS/SSL. As a result we have wrong addresses
    for Swift endpoints in Keystone. This commit fixes that.
    
    Also this patch creates radosgw_keystone task for managing endpoint and
    removes unused parameters from ceph-osd and radosgw modulars.
    
    Change-Id: Id86d85a13fbec27265156a5b12fad0fd892c7cd9
    Closes-bug: #1540133

--- tests/noop/spec/hosts/ceph/radosgw_spec.rb
+++ tests/noop/spec/hosts/ceph/radosgw_spec.rb
@@ -5,16 +5,15 @@ manifest = 'ceph/radosgw.pp'
 describe manifest do
   shared_examples 'catalog' do
     storage_hash = Noop.hiera 'storage'
+    service_endpoint = Noop.hiera 'service_endpoint'
     ceph_monitor_nodes = Noop.hiera 'ceph_monitor_nodes'
 
-    if (storage_hash['images_ceph'] or storage_hash['objects_ceph'] or storage_hash['objects_ceph'])
+    if storage_hash['objects_ceph']
       it { should contain_class('ceph::radosgw').with(
-           'primary_mon'     => ceph_monitor_nodes.keys[0],
+             'rgw_keystone_url' => "#{service_endpoint}:35357"
            )
         }
 
-      it { should contain_haproxy_backend_status('keystone-public').that_comes_before('Class[ceph::keystone]') }
-      it { should contain_haproxy_backend_status('keystone-admin').that_comes_before('Class[ceph::keystone]') }
       it {
         should contain_service('httpd').with(
              'hasrestart' => true,
