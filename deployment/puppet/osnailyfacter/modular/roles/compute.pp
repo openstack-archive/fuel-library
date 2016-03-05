401450e4bf5e67b0ab1b49a1caf962cf706cdc3f
Andrew Woodward <awoodward@mirantis.com>
2016-03-04 16:48:33 -0800

    Update nova-compute and openstack::compute vars
    The list of depricated params in roles\compute.pp and openstack::compute
    is so large that its not realistic to review this as part of the
    drop openstack::compute change so they are proposed in parts.
    
    Change-Id: I6b5662e8a843c144c68532e31c477f086cf12f8d
    Blueprint: fuel-remove-conflict-openstack

--- deployment/puppet/osnailyfacter/modular/roles/compute.pp
+++ deployment/puppet/osnailyfacter/modular/roles/compute.pp
@@ -24,39 +24,25 @@ $public_vip                     = hiera('public_vip')
 $management_vip                 = hiera('management_vip')
 $database_vip                   = hiera('database_vip')
 $service_endpoint               = hiera('service_endpoint')
-$primary_controller             = hiera('primary_controller')
-$use_neutron                    = hiera('use_neutron', false)
 $sahara_hash                    = hiera('sahara', {})
 $murano_hash                    = hiera('murano', {})
 $mp_hash                        = hiera('mp')
 $verbose                        = pick($compute_hash['verbose'], true)
 $debug                          = pick($compute_hash['debug'], hiera('debug', true))
-$auto_assign_floating_ip        = hiera('auto_assign_floating_ip', false)
 $storage_hash                   = hiera_hash('storage_hash', {})
-$vcenter_hash                   = hiera('vcenter', {})
 $nova_hash                      = hiera_hash('nova_hash', {})
 $nova_custom_hash               = hiera_hash('nova_custom_hash', {})
 $rabbit_hash                    = hiera_hash('rabbit_hash', {})
-$glance_hash                    = hiera_hash('glance_hash', {})
 $keystone_hash                  = hiera_hash('keystone_hash', {})
-$swift_hash                     = hiera_hash('swift_hash', {})
 $cinder_hash                    = hiera_hash('cinder_hash', {})
 $ceilometer_hash                = hiera_hash('ceilometer_hash',{})
 $access_hash                    = hiera('access', {})
-$swift_proxies                  = hiera('swift_proxies')
-$swift_master_role              = hiera('swift_master_role', 'primary-controller')
 $neutron_mellanox               = hiera('neutron_mellanox', false)
 $syslog_hash                    = hiera('syslog', {})
 $base_syslog_hash               = hiera('base_syslog', {})
 $use_syslog                     = hiera('use_syslog', true)
 $use_stderr                     = hiera('use_stderr', false)
-$syslog_log_facility_glance     = hiera('syslog_log_facility_glance', 'LOG_LOCAL2')
-$syslog_log_facility_cinder     = hiera('syslog_log_facility_cinder', 'LOG_LOCAL3')
-$syslog_log_facility_neutron    = hiera('syslog_log_facility_neutron', 'LOG_LOCAL4')
-$syslog_log_facility_nova       = hiera('syslog_log_facility_nova','LOG_LOCAL6')
-$syslog_log_facility_keystone   = hiera('syslog_log_facility_keystone', 'LOG_LOCAL7')
-$syslog_log_facility_murano     = hiera('syslog_log_facility_murano', 'LOG_LOCAL0')
-$syslog_log_facility_sahara     = hiera('syslog_log_facility_sahara','LOG_LOCAL0')
+$syslog_log_facility            = hiera('syslog_log_facility_nova','LOG_LOCAL6')
 $nova_rate_limits               = hiera('nova_rate_limits')
 $nova_report_interval           = hiera('nova_report_interval')
 $nova_service_down_time         = hiera('nova_service_down_time')
@@ -87,23 +73,10 @@ if $glance_internal_ssl {
 $vncproxy_protocol                      = get_ssl_property($ssl_hash, $public_ssl_hash, 'nova', 'public', 'protocol', [$nova_hash['vncproxy_protocol'], 'http'])
 $vncproxy_host                          = get_ssl_property($ssl_hash, $public_ssl_hash, 'nova', 'public', 'hostname', [$public_vip])
 
-$db_host                                = pick($nova_hash['db_host'], $database_vip)
-
 $block_device_allocate_retries          = hiera('block_device_allocate_retries', 300)
 $block_device_allocate_retries_interval = hiera('block_device_allocate_retries_interval', 3)
 
-# TODO: openstack_version is confusing, there's such string var in hiera and hardcoded hash
-$hiera_openstack_version = hiera('openstack_version')
-$openstack_version = {
-  'keystone'   => 'installed',
-  'glance'     => 'installed',
-  'horizon'    => 'installed',
-  'nova'       => 'installed',
-  'novncproxy' => 'installed',
-  'cinder'     => 'installed',
-}
-
-$queue_provider = hiera('queue_provider', 'rabbitmq')
+$rpc_backend = hiera('queue_provider', 'rabbitmq')
 
 # Do the stuff
 if $neutron_mellanox {
@@ -112,30 +85,15 @@ if $neutron_mellanox {
   $mellanox_mode = 'disabled'
 }
 
-if $use_neutron {
-  $novanetwork_params        = {}
-  $network_provider          = 'neutron'
-  $neutron_config            = hiera_hash('quantum_settings')
-  $neutron_db_password       = $neutron_config['database']['passwd']
-  $neutron_user_password     = $neutron_config['keystone']['admin_password']
-  $neutron_metadata_proxy_secret = $neutron_config['metadata']['metadata_proxy_shared_secret']
-  $base_mac                  = $neutron_config['L2']['base_mac']
-} else {
-  $network_provider   = 'nova'
-  $floating_ips_range = hiera('floating_network_range')
-  $neutron_config     = {}
-  $novanetwork_params = hiera('novanetwork_parameters')
-}
+include ::osnailyfacter::test_compute
 
-if $primary_controller {
-  if ($mellanox_mode == 'ethernet') {
-    $test_vm_pkg = 'cirros-testvm-mellanox'
-  } else {
-    $test_vm_pkg = 'cirros-testvm'
-  }
-  package { 'cirros-testvm' :
-    ensure => 'installed',
-    name   => $test_vm_pkg,
+if ($::mellanox_mode == 'ethernet') {
+  $neutron_config      = hiera_hash('quantum_settings')
+  $neutron_private_net = pick($neutron_config['default_private_net'], 'net04')
+  $physnet             = $neutron_config['predefined_networks'][$neutron_private_net]['L2']['physnet']
+  class { '::mellanox_openstack::compute':
+    physnet => $physnet,
+    physifc => $neutron_mellanox['physical_port'],
   }
 }
 
@@ -143,46 +101,18 @@ $floating_hash = {}
 
 ##CALCULATED PARAMETERS
 
+# TODO(xarses): Wait Nova compute uses memcache?
 $memcached_server = hiera('memcached_addresses')
 $memcached_port   = hiera('memcache_server_port', '11211')
-$mountpoints      = filter_hash($mp_hash,'point')
 
-# SQLAlchemy backend configuration
-$max_pool_size = min($::processorcount * 5 + 0, 30 + 0)
-$max_overflow = min($::processorcount * 5 + 0, 60 + 0)
-$max_retries = '-1'
-$idle_timeout = '3600'
-
+# TODO(xarses): We need to validate this is needed
 if ($storage_hash['volumes_lvm']) {
   nova_config { 'keymgr/fixed_key':
     value => $cinder_hash[fixed_key];
   }
 }
 
-# Determine who should get the volume service
 
-if (roles_include(['cinder']) and $storage_hash['volumes_lvm']) {
-  $manage_volumes = 'iscsi'
-} elsif (roles_include(['cinder']) and $storage_hash['volumes_vmdk']) {
-  $manage_volumes = 'vmdk'
-} elsif ($storage_hash['volumes_ceph']) {
-  $manage_volumes = 'ceph'
-} else {
-  $manage_volumes = false
-}
-
-#Determine who should be the default backend
-
-if ($storage_hash['images_ceph']) {
-  $glance_backend = 'ceph'
-  $glance_known_stores = [ 'glance.store.rbd.Store', 'glance.store.http.Store' ]
-} elsif ($storage_hash['images_vcenter']) {
-  $glance_backend = 'vmware'
-  $glance_known_stores = [ 'glance.store.vmware_datastore.Store', 'glance.store.http.Store' ]
-} else {
-  $glance_backend = 'swift'
-  $glance_known_stores = [ 'glance.store.swift.Store', 'glance.store.http.Store' ]
-}
 
 # Use Swift if it isn't replaced by vCenter, Ceph for BOTH images and objects
 if !($storage_hash['images_ceph'] and $storage_hash['objects_ceph']) and !$storage_hash['images_vcenter'] {
@@ -197,46 +127,8 @@ $r_hostmem = roles_include(['ceph-osd']) ? {
   false => undef,
 }
 
-#HARDCODED PARAMETERS
-if hiera('use_vcenter', false) {
-  $multi_host = false
-} else {
-  $multi_host = true
-}
-
 $mirror_type = 'external'
 Exec { logoutput => true }
-
-include ::osnailyfacter::test_compute
-
-if ($::mellanox_mode == 'ethernet') {
-  $neutron_private_net = pick($neutron_config['default_private_net'], 'net04')
-  $physnet = $neutron_config['predefined_networks'][$neutron_private_net]['L2']['physnet']
-  class { '::mellanox_openstack::compute':
-    physnet => $physnet,
-    physifc => $neutron_mellanox['physical_port'],
-  }
-}
-
-$oc_public_interface = $public_int ? {
-  undef   => '',
-  default => $public_int
-}
-
-$oc_private_interface = $use_neutron ? {
-  true    => false,
-  default => hiera('private_int', undef)
-}
-
-$oc_fixed_range = $use_neutron ? {
-  true    => false,
-  default => hiera('fixed_network_range', undef)
-}
-
-$oc_neutron_user_password = $use_neutron ? {
-  true    => $neutron_config['keystone']['admin_password'],
-  default => undef
-}
 
 $oc_nova_hash = merge({ 'reserved_host_memory' => $r_hostmem }, $nova_hash)
 
@@ -246,60 +138,32 @@ $oc_nova_hash = merge({ 'reserved_host_memory' => $r_hostmem }, $nova_hash)
 # FIXME(bogdando) This should be changed once the host aggregates implemented, bp disable-new-computes
 class { '::openstack::compute':
   enabled                     => false,
-  public_interface            => $oc_public_interface,
-  private_interface           => $oc_private_interface,
   internal_address            => get_network_role_property('nova/api', 'ipaddr'),
   libvirt_type                => hiera('libvirt_type', undef),
   # FIXME(bogdando) remove after fixed upstream https://review.openstack.org/131710
   host_uuid                   => hiera('host_uuid', generate('/bin/sh', '-c', 'uuidgen')),
-  fixed_range                 => $oc_fixed_range,
-  network_manager             => hiera('network_manager', undef),
-  network_config              => hiera('network_config', {}),
-  multi_host                  => $multi_host,
-  queue_provider              => $queue_provider,
+  rpc_backend                 => $rpc_backend,
   amqp_hosts                  => hiera('amqp_hosts',''),
   amqp_user                   => pick($rabbit_hash['user'], 'nova'),
   amqp_password               => $rabbit_hash['password'],
-  rabbit_ha_queues            => $rabbit_ha_queues,
-  auto_assign_floating_ip     => $auto_assign_floating_ip,
   glance_api_servers          => $glance_api_servers,
   vncproxy_protocol           => $vncproxy_protocol,
   vncproxy_host               => $vncproxy_host,
-  vncserver_listen            => '0.0.0.0',
   migration_support           => true,
   debug                       => $debug,
   verbose                     => $verbose,
   use_stderr                  => $use_stderr,
-  cinder_volume_group         => 'cinder',
-  vnc_enabled                 => true,
-  manage_volumes              => $manage_volumes,
-  nova_user_password          => $nova_hash[user_password],
   nova_hash                   => $oc_nova_hash,
   cache_server_ip             => $memcached_server,
   cache_server_port           => $memcached_port,
-  service_endpoint            => $service_endpoint,
-  cinder                      => true,
-  cinder_iscsi_bind_addr      => get_network_role_property('cinder/iscsi', 'ipaddr'),
-  cinder_user_password        => $cinder_hash[user_password],
-  cinder_db_password          => $cinder_hash[db_password],
   notification_driver         => $ceilometer_hash['notification_driver'],
-  ceilometer_metering_secret  => $ceilometer_hash[metering_secret],
-  ceilometer_user_password    => $ceilometer_hash[user_password],
-  db_host                     => $db_host,
-  network_provider            => $network_provider,
-  neutron_user_password       => $oc_neutron_user_password,
-  base_mac                    => $base_mac,
   pci_passthrough             => nic_whitelist_to_json(get_nic_passthrough_whitelist('sriov')),
   network_device_mtu          => $network_device_mtu,
-
   use_syslog                  => $use_syslog,
-  syslog_log_facility         => $syslog_log_facility_nova,
-  syslog_log_facility_neutron => $syslog_log_facility_neutron,
-  nova_rate_limits            => $nova_rate_limits,
+  syslog_log_facility         => $syslog_log_facility,
   nova_report_interval        => $nova_report_interval,
   nova_service_down_time      => $nova_service_down_time,
   state_path                  => $nova_hash[state_path],
-  neutron_settings            => $neutron_config,
   storage_hash                => $storage_hash,
   config_drive_format         => $config_drive_format,
   use_huge_pages              => $use_huge_pages,
