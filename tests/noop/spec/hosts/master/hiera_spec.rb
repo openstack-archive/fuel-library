62b50d2aaa080908bd7437f035b413139f61b86c
Alex Schultz <aschultz@mirantis.com>
2016-03-08 15:24:51 -0700

    Fix noop coverage
    This change adds a noop test for the master hiera configuration and
    removes the ceph/ceph-init and openstack-network/compute-vcenter tests
    which no longer have matching tasks.
    
    Change-Id: I15bcbd04214b73449d2afb18b286846a82431af8
    Closes-Bug: #1554733

---
+++ tests/noop/spec/hosts/master/hiera_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'master/hiera.pp'
+
+# HIERA: master
+# FACTS: master_centos7 master_centos6
+
+describe manifest do
+  shared_examples 'catalog' do
+    it 'should setup hiera' do
+      should contain_file('hiera_data_dir').with(
+        'ensure' => 'directory',
+        'path'   => '/etc/hiera'
+      )
+      should contain_file('hiera_config').with(
+        'ensure' => 'present',
+        'path'   => '/etc/hiera.yaml'
+      )
+
+      # check symlinks
+      should contain_file('hiera_data_astute').with(
+        'ensure' => 'symlink',
+        'path'   => '/etc/hiera/astute.yaml',
+        'target' => '/etc/fuel/astute.yaml'
+      )
+      should contain_file('hiera_puppet_config').with(
+        'ensure' => 'symlink',
+        'path'   => '/etc/puppet/hiera.yaml',
+        'target' => '/etc/hiera.yaml'
+      )
+    end
+  end
+
+  run_test manifest
+end
