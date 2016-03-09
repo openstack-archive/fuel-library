988f7dcceaab75c8fe0e6728bcd85e4fcd180302
Alexander Noskov <anoskov@mirantis.com>
2016-03-09 16:24:33 +0300

    Refactor osnailyfacter/modular/vmware
    Refactor osnailyfacter/modular/vmware to be compatible with Puppet Master
    
    Blueprint: fuel-refactor-osnailyfacter-for-puppet-master-compatibility
    
    Change-Id: Ib6ad80d49e531486c5b5ec3e400e3ba7faf2bb73

--- deployment/puppet/osnailyfacter/modular/vmware/cinder-vmware.pp
+++ deployment/puppet/osnailyfacter/modular/vmware/cinder-vmware.pp
@@ -1,9 +1,1 @@
-notice('MODULAR: cinder-vmware.pp')
-
-$cinder_hash      = hiera_hash('cinder', {})
-
-if roles_include(['cinder-vmware']) {
-  $debug    = pick($cinder_hash['debug'], hiera('debug', true))
-  $volumes  = get_cinder_vmware_data($cinder_hash['instances'], $debug)
-  create_resources(vmware::cinder::vmdk, $volumes)
-}
+include ::osnailyfacter::vmware::cinder_vmware
