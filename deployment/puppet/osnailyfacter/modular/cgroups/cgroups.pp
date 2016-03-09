5405ced494481a47455a0a3615173cfcef80b9d2
Alexander Noskov <anoskov@mirantis.com>
2016-03-09 14:30:59 +0300

    Refactor osnailyfacter/modular/cgroups
    Refactor osnailyfacter/modular/cgroups to be compatible with Puppet Master
    
    Blueprint: fuel-refactor-osnailyfacter-for-puppet-master-compatibility
    
    Change-Id: I91c2869e3885ebbf53cfff1f352af8946237e88b

--- deployment/puppet/osnailyfacter/modular/cgroups/cgroups.pp
+++ deployment/puppet/osnailyfacter/modular/cgroups/cgroups.pp
@@ -1,11 +1,1 @@
-notice('MODULAR: cgroups.pp')
-
-$cgroups_config = hiera('cgroups', {})
-
-# Task should not be failed if configuration was not propagated
-unless empty($cgroups_config) {
-  $cgroups_set = prepare_cgroups_hash($cgroups_config)
-  class {'cgroups':
-    cgroups_set => $cgroups_set,
-  }
-}
+include ::osnailyfacter::cgroups::cgroups
