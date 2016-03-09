0407adaaa3ce27e426b8a64c3ed659a059ab1be6
Alexander Noskov <anoskov@mirantis.com>
2016-03-09 16:20:32 +0300

    Refactor osnailyfacter/modular/virtual_ips
    Refactor osnailyfacter/modular/virtual_ips to be compatible with Puppet Master
    
    Blueprint: fuel-refactor-osnailyfacter-for-puppet-master-compatibility
    
    Change-Id: I60e2f0a1dd77b05507100213487b03248578ba34

--- deployment/puppet/osnailyfacter/modular/virtual_ips/conntrackd.pp
+++ deployment/puppet/osnailyfacter/modular/virtual_ips/conntrackd.pp
@@ -1,16 +1,1 @@
-notice('MODULAR: conntrackd.pp')
-
-$network_metadata = hiera_hash('network_metadata', {})
-prepare_network_config(hiera_hash('network_scheme', {}))
-$vrouter_name = hiera('vrouter_name', 'pub')
-$bind_address = get_network_role_property('mgmt/vip', 'ipaddr')
-$mgmt_bridge = get_network_role_property('mgmt/vip', 'interface')
-
-# CONNTRACKD for CentOS 6 doesn't work under namespaces
-if $operatingsystem == 'Ubuntu' {
-  class { 'cluster::conntrackd_ocf' :
-    vrouter_name => $vrouter_name,
-    bind_address => $bind_address,
-    mgmt_bridge  => $mgmt_bridge,
-  }
-}
+include ::osnailyfacter::virtual_ips::conntrackd
