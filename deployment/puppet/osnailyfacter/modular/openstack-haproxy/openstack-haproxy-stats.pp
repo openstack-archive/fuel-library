18cb187e1b6f7274b22e8c46944370851949542e
Alexander Noskov <anoskov@mirantis.com>
2016-03-09 13:40:20 +0300

    Refactor osnailyfacter/modular/openstack-haproxy
    Refactor osnailyfacter/modular/openstack-haproxy to be compatible with Puppet Master
    
    Blueprint: fuel-refactor-osnailyfacter-for-puppet-master-compatibility
    
    Change-Id: Ic168ffd1dd0f7774c1569c73bfeeb5168babb4da

--- deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-stats.pp
+++ deployment/puppet/osnailyfacter/modular/openstack-haproxy/openstack-haproxy-stats.pp
@@ -1,10 +1,1 @@
-notice('MODULAR: openstack-haproxy-stats.pp')
-
-$external_lb         = hiera('external_lb', false)
-$internal_virtual_ip = unique([hiera('management_vip'), hiera('database_vip'), hiera('service_endpoint')])
-
-if !$external_lb {
-  class { '::openstack::ha::stats':
-    internal_virtual_ip => $internal_virtual_ip,
-  }
-}
+include ::osnailyfacter::openstack_haproxy::openstack_haproxy_stats
