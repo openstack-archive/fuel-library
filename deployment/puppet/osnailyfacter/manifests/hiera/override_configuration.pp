72f05f5b1252e5b21be7ef94ba5de00e95a199a7
Alexander Noskov <anoskov@mirantis.com>
2016-03-09 15:44:06 +0300

    Refactor osnailyfacter/modular/hiera
    Refactor osnailyfacter/modular/hiera to be compatible with Puppet Master
    
    Blueprint: fuel-refactor-osnailyfacter-for-puppet-master-compatibility
    
    Change-Id: Iab66ad28c357a46ab227d31293184a08c2b1af44

---
+++ deployment/puppet/osnailyfacter/manifests/hiera/override_configuration.pp
@@ -0,0 +1,18 @@
+class osnailyfacter::hiera::override_configuration {
+
+  notice('MODULAR: hiera/override_configuration.pp')
+
+  $hiera_data_dir            = '/etc/hiera'
+  $hiera_override_dir        = "${hiera_data_dir}/override"
+  $hiera_override_config_dir = "${hiera_override_dir}/configuration"
+
+  File {
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    ensure => 'directory',
+  }
+
+  file { [$hiera_override_dir, $hiera_override_config_dir]: }
+
+}
