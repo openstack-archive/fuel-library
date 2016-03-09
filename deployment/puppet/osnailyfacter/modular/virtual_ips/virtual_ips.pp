c70779745b76723784b0266b798b334757cf62c1
Alexander Noskov <anoskov@mirantis.com>
2016-03-09 16:20:32 +0300

    Refactor osnailyfacter/modular/virtual_ips
    Refactor osnailyfacter/modular/virtual_ips to be compatible with Puppet Master
    
    Blueprint: fuel-refactor-osnailyfacter-for-puppet-master-compatibility
    
    Change-Id: I60e2f0a1dd77b05507100213487b03248578ba34

--- deployment/puppet/osnailyfacter/modular/virtual_ips/virtual_ips.pp
+++ deployment/puppet/osnailyfacter/modular/virtual_ips/virtual_ips.pp
@@ -1,7 +1,1 @@
-notice('MODULAR: virtual_ips.pp')
-
-$network_metadata = hiera_hash('network_metadata', {})
-$network_scheme = hiera_hash('network_scheme', {})
-$roles = hiera('roles')
-
-generate_vips($network_metadata, $network_scheme, $roles)
+include ::osnailyfacter::virtual_ips::virtual_ips
