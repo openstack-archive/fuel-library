92325faf328a89703ede74c9bb4b75e70fc0f7f4
Andrew Woodward <awoodward@mirantis.com>
2016-03-09 15:48:09 -0800

    Drop openstack::compute
    This uplifts the openstack::compute directly into the calling task.
    
    Change-Id: Ib4cc7c70088343546a114c95375f1e3c9ac95b94
    Blueprint: fuel-remove-conflict-openstack

--- tests/noop/spec/hosts/roles/compute_spec.rb
+++ tests/noop/spec/hosts/roles/compute_spec.rb
@@ -313,16 +313,15 @@ describe manifest do
     end
 
     it 'should properly configure vncproxy with (non-)ssl' do
-      should contain_class('openstack::compute').with(
-        'vncproxy_host' => vncproxy_host
-      )
       should contain_class('nova::compute').with(
         'vncproxy_protocol' => vncproxy_protocol
+        'vncproxy_host'     => vncproxy_host
+        'vncproxy_port'     => vncproxy_port
       )
     end
 
     it 'should properly configure glance api servers with (non-)ssl' do
-      should contain_class('openstack::compute').with(
+      should contain_class('nova').with(
         'glance_api_servers' => glance_api_servers
       )
     end
@@ -347,11 +346,94 @@ describe manifest do
         nova_compute_rhostmem = 512 # default
       end
 
-      should contain_class('openstack::compute').with(
-        'nova_hash' => rhost_mem.merge(nova_hash)
-      )
       should contain_class('nova::compute').with(
         'reserved_host_memory' => nova_compute_rhostmem
+      )
+    end
+  end
+
+  it 'configures with the default params' do
+    if facts[:os_package_type] == 'debian' or facts[:osfamily] == 'RedHat'
+      libvirt_service_name = 'libvirtd'
+    else
+      libvirt_service_name = 'libvirt-bin'
+    end
+    should contain_class('nova').with(
+      :install_utilities => false,
+      :ensure_package    => 'present',
+      :rpc_backend       => p[:rpc_backend],
+      :rabbit_hosts      => [ params[:amqp_hosts] ],
+      :rabbit_userid     => p[:amqp_user],
+      :rabbit_password   => p[:amqp_password],
+      :kombu_reconnect_delay => '5.0',
+      :image_service     => 'nova.image.glance.GlanceImageService',
+      :glance_api_servers => p[:glance_api_servers],
+      :verbose           => p[:verbose],
+      :debug             => p[:debug],
+      :use_syslog        => p[:use_syslog],
+      :use_stderr        => p[:use_stderr],
+      :log_facility      => p[:syslog_log_facility],
+      :state_path        => p[:state_path],
+      :report_interval   => p[:nova_report_interval],
+      :service_down_time => p[:nova_service_down_time],
+      :notify_on_state_change => 'vm_and_task_state',
+      :memcached_servers => ['127.0.0.1:11211'],
+    )
+    should contain_class('nova::availability_zone').with(
+      :default_availability_zone => '<SERVICE DEFAULT>',
+      :default_schedule_zone     => '<SERVICE DEFAULT>',
+    )
+    should contain_class('nova::compute').with(
+      :ensure_package => 'present',
+      :enabled        => p[:enabled],
+      :vnc_enabled    => p[:vnc_enabled],
+      :vncserver_proxyclient_address => p[:internal_address],
+      :vncproxy_host  => p[:vncproxy_host],
+      :vncproxy_port  => '6080',
+      :force_config_drive => false,
+      :neutron_enabled => false,
+      :install_bridge_utils => p[:install_bridge_utils],
+      :network_device_mtu => '65000',
+      :instance_usage_audit => true,
+      :instance_usage_audit_period => 'hour',
+      :default_schedule_zone => nil,
+      :config_drive_format => p[:config_drive_format]
+    )
+    should contain_class('nova::compute::libvirt').with(
+      :libvirt_virt_type    => p[:libvirt_type],
+      :vncserver_listen     => p[:vncserver_listen],
+      :migration_suport     => p[:migration_support],
+      :remove_unused_original_minimum_age_seconds => '86400',
+      :compute_driver       => p[:compute_driver],
+      :libvirt_service_name => libvirt_service_name,
+    )
+    should contain_augeas('libvirt-conf-uuid').with(
+      :context => '/files/etc/libvirt/libvirtd.conf',
+      :changes => ["set host_uuid #{p[:host_uuid]}"],
+    ).that_notifies('Service[libvirt]')
+    if facts[:osfamily] == 'RedHat'
+      should contain_file_line('qemu_selinux')
+    elsif facts[:osfamily] == 'Debian'
+      should contain_file_line('qemu_apparmor')
+      should contain_file_line('apparmor_libvirtd')
+    end
+    should contain_class('nova::client')
+    should contain_install_ssh_keys('nova_ssh_key_for_migration')
+    should contain_file('/var/lib/nova/.ssh/config')
+
+    if facts[:operatingsystem] == 'Ubuntu'
+      should contain_package('cpufrequtils').with(
+        :ensure => 'present'
+      )
+      should contain_file('/etc/default/cpufrequtils').with(
+        :content => "GOVERNOR=\"performance\"\n",
+        :require => 'Package[cpufrequtils]',
+        :notify  => 'Service[cpufrequtils]',
+      )
+      should contain_service('cpufrequtils').with(
+        :ensure => 'running',
+        :enable => true,
+        :status => '/bin/true',
       )
     end
   end
