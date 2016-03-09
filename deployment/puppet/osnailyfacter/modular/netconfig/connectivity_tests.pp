307a4f3cf333d02181bd554206c010a2d393dd7d
Alexander Noskov <anoskov@mirantis.com>
2016-03-09 16:03:54 +0300

    Refactor osnailyfacter/modular/netconfig
    Refactor osnailyfacter/modular/netconfig to be compatible with Puppet Master
    
    Change-Id: Ic7e1241e02f9277e42d723e9d120c3a5f11aa853
    Blueprint: fuel-refactor-osnailyfacter-for-puppet-master-compatibility

--- deployment/puppet/osnailyfacter/modular/netconfig/connectivity_tests.pp
+++ deployment/puppet/osnailyfacter/modular/netconfig/connectivity_tests.pp
@@ -1,5 +1,1 @@
-notice('MODULAR: connectivity_tests.pp')
-# Pull the list of repos from hiera
-$repo_setup = hiera('repo_setup')
-# test that the repos are accessible
-url_available($repo_setup['repos'])
+include ::osnailyfacter::netconfig::connectivity_tests
