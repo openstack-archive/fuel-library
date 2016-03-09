fc2e991f7e763142ac73359a98232d2aac3a2b79
Alexander Noskov <anoskov@mirantis.com>
2016-03-09 14:56:53 +0300

    Refactor osnailyfacter/modular/fuel_pkgs
    Refactor osnailyfacter/modular/fuel_pkgs to be compatible with Puppet Master
    
    Blueprint: fuel-refactor-osnailyfacter-for-puppet-master-compatibility
    
    Change-Id: I07f5774b95a6f3671bed39de3a6e1eab3aa7c980

---
+++ deployment/puppet/osnailyfacter/manifests/fuel_pkgs/setup_repositories.pp
@@ -0,0 +1,50 @@
+class osnailyfacter::fuel_pkgs::setup_repositories {
+
+  notice('MODULAR: fuel_pkgs/setup_repositories.pp')
+
+  $repo_setup_hash = hiera_hash('repo_setup', {})
+  $repos      = $repo_setup_hash['repos']
+
+  if $::osfamily == 'Debian' {
+
+    class { '::apt':
+      purge  => {
+        'sources.list'   => true,
+        'sources.list.d' => true,
+        'preferences'    => false,
+        'preferences.d'  => false,
+      };
+    }
+
+    $repositories = generate_apt_sources($repos)
+    $pins         = generate_apt_pins($repos)
+
+    if ! empty($repositories) {
+      create_resources(apt::source, $repositories)
+    }
+
+    if ! empty($pins) {
+      create_resources(apt::pin, $pins)
+    }
+
+    Apt::Conf {
+      notify_update => false,
+      priority      => '02',
+    }
+
+    apt::conf { 'allow-unathenticated':
+      content => 'APT::Get::AllowUnauthenticated 1;',
+    }
+
+    apt::conf { 'install-recommends':
+      content => 'APT::Install-Recommends "false";',
+    }
+
+    apt::conf { 'install-suggests':
+      content => 'APT::Install-Suggests "false";',
+    }
+
+    Apt::Source<||> ~> Exec<| title == 'apt_update' |>
+  }
+
+}
