a5331584ce6a78ebf2f5ac576e612bb7300b5a09
Alexander Noskov <anoskov@mirantis.com>
2016-03-09 15:49:21 +0300

    Refactor osnailyfacter/modular/memcached
    Refactor osnailyfacter/modular/memcached to be compatible with Puppet Master
    
    Blueprint: fuel-refactor-osnailyfacter-for-puppet-master-compatibility
    
    Change-Id: I23d6876bed755d0ae723b063cefd565756224980

--- deployment/puppet/osnailyfacter/modular/memcached/memcached.pp
+++ deployment/puppet/osnailyfacter/modular/memcached/memcached.pp
@@ -1,8 +1,1 @@
-notice('MODULAR: memcached.pp')
-
-prepare_network_config(hiera_hash('network_scheme', {}))
-
-class { 'memcached':
-  listen_ip  => get_network_role_property('mgmt/memcache', 'ipaddr'),
-  max_memory => '50%',
-}
+include ::osnailyfacter::memcached::memcached
