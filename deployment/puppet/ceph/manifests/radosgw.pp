24fd5f31a9a8cd5420ec64f30e709dfa74bf64ff
Denis Egorenko <degorenko@mirantis.com>
2016-03-14 13:23:21 +0300

    Set proper public address for swift endpoint for Ceph
    Currently we are not handling situation when we have Ceph with RadosGW
    enabled with enabled TLS/SSL. As a result we have wrong addresses
    for Swift endpoints in Keystone. This commit fixes that.
    
    Also this patch creates radosgw_keystone task for managing endpoint and
    removes unused parameters from ceph-osd and radosgw modulars.
    
    Change-Id: Id86d85a13fbec27265156a5b12fad0fd892c7cd9
    Closes-bug: #1540133

--- deployment/puppet/ceph/manifests/radosgw.pp
+++ deployment/puppet/ceph/manifests/radosgw.pp
@@ -4,7 +4,6 @@ class ceph::radosgw (
   $rgw_id      = 'radosgw.gateway',
   $rgw_user    = $::ceph::params::user_httpd,
   $use_ssl     = $::ceph::use_ssl,
-  $public_ssl  = false,
   $primary_mon = $::ceph::primary_mon,
 
   # RadosGW settings
@@ -29,9 +28,6 @@ class ceph::radosgw (
   $rgw_keystone_accepted_roles      = $::ceph::rgw_keystone_accepted_roles,
   $rgw_keystone_revocation_interval = $::ceph::rgw_keystone_revocation_interval,
   $rgw_nss_db_path                  = $::ceph::rgw_nss_db_path,
-  $pub_ip                           = $::ceph::rgw_pub_ip,
-  $adm_ip                           = $::ceph::rgw_adm_ip,
-  $int_ip                           = $::ceph::rgw_int_ip,
 
   #rgw Log settings
   $use_syslog                       = $::ceph::use_syslog,
@@ -134,18 +130,6 @@ class ceph::radosgw (
       Exec['create nss db signing certs']
 
     } #END rgw_use_pki
-
-  class {'ceph::keystone':
-    pub_ip              => $pub_ip,
-    pub_protocol        => $public_ssl ? {
-      true    => 'https',
-      default => 'http',
-    },
-    adm_ip              => $adm_ip,
-    int_ip              => $int_ip,
-    swift_endpoint_port => $swift_endpoint_port,
-  }
-
   } #END rgw_use_keystone
 
   if ($::osfamily == 'Debian'){
--- tests/noop/spec/hosts/ceph/radosgw_spec.rb
+++ tests/noop/spec/hosts/ceph/radosgw_spec.rb
@@ -6,20 +6,8 @@ describe manifest do
   shared_examples 'catalog' do
     storage_hash = Noop.hiera 'storage'
     ceph_monitor_nodes = Noop.hiera 'ceph_monitor_nodes'
-    public_ssl_hash = Noop.hiera('public_ssl')
 
     let(:ssl_hash) { Noop.hiera_hash 'use_ssl', {} }
-
-    let(:internal_auth_protocol) {
-      Noop.puppet_function 'get_ssl_property',ssl_hash,{},'keystone',
-        'internal','protocol','http'
-    }
-
-    let(:internal_auth_address) {
-      Noop.puppet_function 'get_ssl_property',ssl_hash,{},'keystone',
-        'internal','hostname',
-        [Noop.hiera('service_endpoint', Noop.hiera('management_vip'))]
-    }
 
     let(:admin_auth_protocol) {
       Noop.puppet_function 'get_ssl_property',ssl_hash,{},'keystone',
@@ -32,15 +20,11 @@ describe manifest do
         [Noop.hiera('service_endpoint', Noop.hiera('management_vip'))]
     }
 
-    let(:internal_url) {
-      "#{internal_auth_protocol}://#{internal_auth_address}:5000"
-    }
-
     let(:admin_url) {
       "#{admin_auth_protocol}://#{admin_auth_address}:35357"
     }
 
-    if (storage_hash['images_ceph'] or storage_hash['objects_ceph'] or storage_hash['objects_ceph'])
+    if storage_hash['objects_ceph']
       rgw_id = 'radosgw.gateway'
 
       it 'should configure apache mods' do
@@ -56,52 +40,16 @@ describe manifest do
       end
 
       it { should contain_class('ceph::radosgw').with(
-           'primary_mon'   => ceph_monitor_nodes.keys[0],
-           'rgw_frontends' => 'fastcgi socket_port=9000 socket_host=127.0.0.1',
+           'rgw_frontends'    => 'fastcgi socket_port=9000 socket_host=127.0.0.1',
+           'rgw_keystone_url' => admin_url,
            )
         }
 
-      it 'should have explicit ordering between LB classes and particular actions' do
-        expect(graph).to ensure_transitive_dependency("Haproxy_backend_status[keystone-public]", "Class[ceph::keystone]")
-        expect(graph).to ensure_transitive_dependency("Haproxy_backend_status[keystone-admin]", "Class[ceph::keystone]")
-      end
-
-      it {
-        should contain_service('httpd').with(
-             'hasrestart' => true,
-             'restart'    => 'sleep 30 && apachectl graceful || apachectl restart',
-
+      it { should contain_service('httpd').with(
+          :hasrestart => true,
+          :restart    => 'sleep 30 && apachectl graceful || apachectl restart',
         )
       }
-
-      it {
-        if Noop.hiera('external_lb', false)
-          url = "#{internal_url}/v3"
-          provider = 'http'
-        else
-          url = 'http://' + Noop.hiera('service_endpoint').to_s + ':10000/;csv'
-          provider = Puppet::Type.type(:haproxy_backend_status).defaultprovider.name 
-        end
-        should contain_haproxy_backend_status('keystone-public').with(
-          :url      => url,
-          :provider => provider
-        )
-      }
-
-      it {
-        if Noop.hiera('external_lb', false)
-          url = "#{admin_url}/v3"
-          provider = 'http'
-        else
-          url = 'http://' + Noop.hiera('service_endpoint').to_s + ':10000/;csv'
-          provider = Puppet::Type.type(:haproxy_backend_status).defaultprovider.name
-        end
-        should contain_haproxy_backend_status('keystone-admin').with(
-          :url      => url,
-          :provider => provider
-        )
-      }
-
     end
   end
 
