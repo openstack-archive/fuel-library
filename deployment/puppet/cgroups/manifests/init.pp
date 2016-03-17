da3aa3099d8c31c87c14c52afb0df41addd9e479
Valeriy Sakharov <vsakharov@mirantis.com>
2016-03-17 14:07:56 +0300

    Subscribe cgrulesengd to the file changes
    This commit fix behavior when new processes aren't catched by
    Control Groups Engine Daemon.
    
    Change-Id: I1a5d5eaa15e1f3f52a97793ae6f69704d893a630
    Closes-bug: #1558432

--- deployment/puppet/cgroups/manifests/init.pp
+++ deployment/puppet/cgroups/manifests/init.pp
@@ -17,13 +17,15 @@ class cgroups(
   inherits cgroups::params
 {
   validate_hash($cgroups_set)
-  ensure_packages($packages)
+  ensure_packages($packages, { tag => 'cgroups' })
+
 
   File {
     ensure => file,
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
+    tag    => 'cgroups',
   }
 
   file { '/etc/cgconfig.conf':
@@ -38,10 +40,24 @@ class cgroups(
     cgroups_settings => $cgroups_set,
   }
 
-  Package<||> ->
-  File<||> ->
+  File <| tag == 'cgroups' |> ~>
+  Service['cgrulesengd']
+
+  Package <| tag == 'cgroups' |> ~>
+  Service['cgrulesengd']
+
+  Package <| tag == 'cgroups' |> ->
+  File <| tag == 'cgroups' |>
+
+  File <| tag == 'cgroups' |> ->
+  Service['cgroup-lite']
+
   Service['cgroup-lite'] ->
+  Service['cgconfigparser']
+
   Service['cgconfigparser'] ->
-  Cgclassify<||> ->
+  Cgclassify <||>
+
+  Cgclassify <||> ->
   Service['cgrulesengd']
 }
