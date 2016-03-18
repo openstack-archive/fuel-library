d2225d630e9992b256b7ebd1ad172f0b7d87670a
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-03-18 12:52:30 +0300

    Implement hardcoded nailgun tasks in library
    Nailgun implements new serializer for tasks which discarding other ones.
    It leads us to reimplement previously hardcoded tasks from nailgun in
    library.
    
    * Add generators for upload_configuration and upload_nodes_info.
      Previously, those tasks have got data from nailgun. Now special
      hash 'data' in task can have a 'yaql_exp' parameter that recieve
      a yaql [0] expression. root ('$') value in this expression is a
      mostly one-to-one settings representation yaml hash, values of which
      can be accessed via '.'. Typical values is 'configs' and 'nodes' which
      are according to openstack config and 'nodes.yaml' file data we
      will have on nodes
    
    * Change role regex from '*' to ['/.*/'] for all tasks which needed
      this. Current implementation of new serializer needs this explicitly
      and (I believe) thiat will be fixed later. Also, explicit declaration
      can show that it is a _regexp_, not just a globe
    
    * generate_vms task not ran by nailgun implicitly by searching in
      astute.yaml config. This behavior moved to task itself.
    
    * Conditions changes. Main changes done for 'additional_components'
      section, so now it move from
        "settings:additional_components.ceilometer.value == true"
      to
        "settings:ceilometer.enabled == true"
    
      Also there is 'common' section remove, so
        "settings:common.use_vcenter.value == true"
      now must be written as
        "settings:use_vcenter == true"
    
      Also there is no need to point any '.value' suffix in condition, so,
      for example,
        "settings:public_ssl.horizon.value == true"
      became a
        "settings:public_ssl.horizon == true"
    
    * New major tasks version added - 2.1.0. Now if serializer fabric found any
      task with 2.1.0 version or higher, new LCM serializer is used. If
      there are no such tasks, old serializer are used for backward
      compatibility
    
    [0] https://yaql.readthedocs.org/en/latest/
    
    Change-Id: I387e9fbfad8ca20f643ee43cbe830196d597c415
    Related-Blueprint: computable-task-fields-yaql
    Depends-On: Ie95a58c8cf86eac1a5c3dbd956fafc401e40fed6

--- tests/noop/spec/hosts/generate_vms/generate_vms_spec.rb
+++ tests/noop/spec/hosts/generate_vms/generate_vms_spec.rb
@@ -6,65 +6,68 @@ manifest = 'generate_vms/generate_vms.pp'
 
 describe manifest do
   shared_examples 'catalog' do
-    libvirt_dir = '/etc/libvirt/qemu'
-    template_dir = '/var/lib/nova'
-    libvirt_service = 'libvirtd'
-    packages = ['qemu-utils', 'qemu-kvm', 'libvirt-bin', 'xmlstarlet']
 
     vms = Noop.hiera 'vms_conf'
+    enabled = vms.collect{|x| x['created']}.compact.any?
 
-    it 'should exec generate_vms' do
-      should contain_exec('generate_vms').with(
-        'command'     => "/usr/bin/generate_vms.sh #{libvirt_dir} #{template_dir}",
-      )
-    end
+    if enabled
+      libvirt_dir = '/etc/libvirt/qemu'
+      template_dir = '/var/lib/nova'
+      libvirt_service = 'libvirtd'
+      packages = ['qemu-utils', 'qemu-kvm', 'libvirt-bin', 'xmlstarlet']
 
-    vms.each do | vm |
-      it "should define osnailyfacter::generate_vms::vm_config #{vm}" do
-        should contain_osnailyfacter__generate_vms__vm_config(vm).with(
+      vms.each do | vm |
+        it "should define osnailyfacter::generate_vms::vm_config #{vm}" do
+          should contain_osnailyfacter__generate_vms__vm_config(vm).with(
+            'before' => 'Exec[generate_vms]',
+            'require' => "File[#{template_dir}]",
+          )
+        end
+      end
+
+      it 'should exec generate_vms' do
+        should contain_exec('generate_vms').with(
+          'command'     => "/usr/bin/generate_vms.sh #{libvirt_dir} #{template_dir}",
+        )
+      end
+
+      it "should create #{template_dir} directory" do
+        should contain_file(template_dir).with(
+          'ensure' => 'directory',
+        )
+      end
+
+      it "should create #{libvirt_dir}/autostart directory" do
+        should contain_file("#{libvirt_dir}/autostart").with(
+          'ensure' => 'directory',
+        )
+      end
+
+      it "should start #{libvirt_service} service" do
+        should contain_service(libvirt_service).with(
+          'ensure' => 'running',
           'before' => 'Exec[generate_vms]',
-          'require' => "File[#{template_dir}]",
         )
       end
-    end
 
-    it "should create #{template_dir} directory" do
-      should contain_file(template_dir).with(
-        'ensure' => 'directory',
-      )
-    end
+      packages.each do | package |
+        it "should install #{package} package" do
+          should contain_package(package).with(
+            'ensure' => 'installed',
+          )
+        end
+      end
 
-    it "should create #{libvirt_dir}/autostart directory" do
-      should contain_file("#{libvirt_dir}/autostart").with(
-        'ensure' => 'directory',
-      )
-    end
-
-    it "should start #{libvirt_service} service" do
-      should contain_service(libvirt_service).with(
-        'ensure' => 'running',
-        'before' => 'Exec[generate_vms]',
-      )
-    end
-
-    packages.each do | package |
-      it "should install #{package} package" do
-        should contain_package(package).with(
-          'ensure' => 'installed',
-        )
+      it 'should set permissions for /dev/kvm under Ubuntu' do
+        if facts[:operatingsystem] == 'Ubuntu'
+          should contain_file('/dev/kvm').with(
+            :ensure => 'present',
+            :group  => 'kvm',
+            :mode   => '0660',
+          ).that_comes_before('Exec[generate_vms]')
+        end
       end
     end
-
-    it 'should set permissions for /dev/kvm under Ubuntu' do
-      if facts[:operatingsystem] == 'Ubuntu'
-        should contain_file('/dev/kvm').with(
-          :ensure => 'present',
-          :group  => 'kvm',
-          :mode   => '0660',
-        ).that_comes_before('Exec[generate_vms]')
-      end
-    end
-
   end
   test_ubuntu_and_centos manifest
 end
