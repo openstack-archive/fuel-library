10252629d93e478ebcf4747017a0e02270666967
dmburmistrov <dmburmistrov@mirantis.com>
2016-03-18 17:29:35 +0300

    Specify API version in Keystone url
    For now Aodh working with API v2.0 by default.
    There are issues to migrate on third API version
    quickly, but with current configuration
    "aodh-evaluator" don't work.
    
    Change-Id: I71a04dae92573aeb520117609c6a6630b72bdd03
    Closes-bug: #1553078

--- deployment/puppet/osnailyfacter/modular/aodh/aodh.pp
+++ deployment/puppet/osnailyfacter/modular/aodh/aodh.pp
@@ -79,7 +79,7 @@ $memcache_servers = "${memcache_address}:11211"
 
 $internal_auth_protocol = get_ssl_property($ssl_hash, {}, 'keystone', 'internal', 'protocol', 'http')
 $internal_auth_address  = get_ssl_property($ssl_hash, {}, 'keystone', 'internal', 'hostname', [$management_vip])
-$keystone_auth_uri      = "${internal_auth_protocol}://${internal_auth_address}:5000/"
+$keystone_auth_uri      = "${internal_auth_protocol}://${internal_auth_address}:5000/v2.0"
 $keystone_identity_uri  = "${internal_auth_protocol}://${internal_auth_address}:35357/"
 
 # backwards compatibility with previous ceilometer configuration around alarm
