672cf29b7afd7c31a517742de34e150e78bf02b3
Liubov Efremova <lefremova@mirantis.com>
2016-03-18 13:40:55 +0300

    Upstream keystone module is used now in 'service_token_off'
    'service_token_off' puppet manifest previously used special 'exec'
    resource for disabling admin_token_auth middleware in public/admin/v3
    pipelines.
    
    As the special class for this purpose has been created in the upstream
    keystone module, the 'exec' resource was replaced by the inclusion of
    this class.
    
    Change-Id: I42a8274a11f813eafdd0e5906a33a2fd6b737d03
    Closes-bug: #1546737

--- deployment/puppet/osnailyfacter/manifests/astute/service_token_off.pp
+++ deployment/puppet/osnailyfacter/manifests/astute/service_token_off.pp
@@ -10,24 +10,10 @@ class osnailyfacter::astute::service_token_off {
 
   if $keystone_params_hash['service_token_off'] {
 
-    include ::keystone::params
     include ::tweaks::apache_wrappers
 
     keystone_config {
       'DEFAULT/admin_token': ensure => absent;
-    }
-
-    # Get paste.ini source
-    $keystone_paste_ini = $::keystone::params::paste_config ? {
-      undef   => '/etc/keystone/keystone-paste.ini',
-      default => $::keystone::params::paste_config,
-    }
-
-    # Remove admin_token_auth middleware from public/admin/v3 pipelines
-    exec { 'remove_admin_token_auth_middleware':
-      path    => ['/bin', '/usr/bin'],
-      command => "sed -i.dist 's/ admin_token_auth//' ${keystone_paste_ini}",
-      onlyif  => "fgrep -q ' admin_token_auth' ${keystone_paste_ini}",
     }
 
     service { 'httpd':
@@ -38,7 +24,9 @@ class osnailyfacter::astute::service_token_off {
 
     # Restart service that changes to take effect
     Keystone_config<||> ~> Service['httpd']
-    Exec['remove_admin_token_auth_middleware'] ~> Service['httpd']
+
+    # Disable admin_token_auth middleware in public/admin/v3 pipelines
+    include ::keystone::disable_admin_token_auth
 
   }
 
--- tests/noop/spec/hosts/astute/service_token_off_spec.rb
+++ tests/noop/spec/hosts/astute/service_token_off_spec.rb
@@ -37,19 +37,8 @@ describe manifest do
         is_expected.to contain_keystone_config('DEFAULT/admin_token').with_ensure('absent')
       end
 
-      it 'should contain exec of remove AdminTokenAuthMiddleware from pipelines' do
-        case facts[:osfamily]
-          when 'Debian'
-            paste_ini = '/etc/keystone/keystone-paste.ini'
-          when 'RedHat'
-            paste_ini = '/usr/share/keystone/keystone-dist-paste.ini'
-        end
-
-        is_expected.to contain_exec('remove_admin_token_auth_middleware').with(
-          :path    => ['/bin', '/usr/bin'],
-          :command => "sed -i.dist 's/ admin_token_auth//' #{paste_ini}",
-          :onlyif  => "fgrep -q ' admin_token_auth' #{paste_ini}",
-        )
+      it 'should contain class to remove AdminTokenAuthMiddleware from pipelines' do
+        is_expected.to contain_class('keystone::disable_admin_token_auth')
       end
     end
 
