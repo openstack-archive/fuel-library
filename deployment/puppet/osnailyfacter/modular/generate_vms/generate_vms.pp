35893324a5ddd767041a886e5155228b1581e1b5
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-03-18 12:52:30 +0300

    Implement hardcoded nailgun tasks in library
    Nailgun implements new serializer for tasks which discarding other ones.
    It leads us to reimplement previously hardcoded tasks from nailgun in
    library.
    
    * Add generators for upload_configuration and upload_nodes_info.
      Previously, those tasks have got data from nailgun. Now special
      hash 'data' in task can have a 'generator' parameter that recieve
      a generator. Currently implemented one named 'from_deployment_info_as_yaml'
      and provide mostly one-to-one settings representation yaml hash,
      values of which can be accessed by value of second 'data' parameter,
      named 'generator_args'. Typical values is 'configs' and 'nodes' which
      are according to 'astute.yaml' and 'nodes.yaml' files we will have on
      nodes
    
    * Change role regex from '*' to ['/.*/'] for all tasks which needed
      this. Current implementation of new serializer needs this explicitly
      and (I believe) thiat will be fixed later. Also, explicit declaration
      can show that it is a _regexp_, not just a globe
    
    * generate_vms task not ran by nailgun implicitly by searching in
      astute.yaml config. This behavior moved to task itself.
    
    * Conditions changes. Main changes done for 'additional_components'
      section, so now it move from
        "settings:additional_components.ceilometer.value == true"
      to
        "settings:ceilometer.enabled == true"
    
      Also there is 'common' section remove, so
        "settings:common.use_vcenter.value == true"
      now must be written as
        "settings:use_vcenter == true"
    
      Also there is no need to point any '.value' suffix in condition, so,
      for example,
        "settings:public_ssl.horizon.value == true"
      became a
        "settings:public_ssl.horizon == true"
    
    * New major tasks version added - 2.1.0. Now if serializer fabric found any
      task with 2.1.0 version or higher, new LCM serializer is used. If
      there are no such tasks, old serializer are used for backward
      compatibility
    
    Change-Id: I387e9fbfad8ca20f643ee43cbe830196d597c415
    Related-Blueprint: unlock-settings-tab
    Depends-On: I3a4f34afcd8bdc28be3551def614a591e99486a5
    Depends-On: Ie94b4852308a930e26178c56d450a0479eb3378b

--- deployment/puppet/osnailyfacter/modular/generate_vms/generate_vms.pp
+++ deployment/puppet/osnailyfacter/modular/generate_vms/generate_vms.pp
@@ -1,62 +1,67 @@
 notice('MODULAR: generate_vms.pp')
 
-$libvirt_dir = '/etc/libvirt/qemu'
-$template_dir = '/var/lib/nova'
-$packages = ['qemu-utils', 'qemu-kvm', 'libvirt-bin', 'xmlstarlet']
-
 $vms = hiera_array('vms_conf')
+$enabled = str2bool(inline_template('<%= @vms.collect{|x| x["created"]}.compact.any? %>'))
 
-include ::nova::params
+if $enabled {
 
-define vm_config {
-  $details = $name
-  $id = $details['id']
+  $libvirt_dir = '/etc/libvirt/qemu'
+  $template_dir = '/var/lib/nova'
+  $packages = ['qemu-utils', 'qemu-kvm', 'libvirt-bin', 'xmlstarlet']
 
-  file { "${template_dir}/template_${id}_vm.xml":
-    owner   => 'root',
-    group   => 'root',
-    content => template('osnailyfacter/vm_libvirt.erb'),
-  }
-}
 
-package { $packages:
-  ensure => 'installed',
-}
+  include ::nova::params
 
-service { $::nova::params::libvirt_service_name:
-  ensure  => 'running',
-  require => Package[$packages],
-  before  => Exec['generate_vms'],
-}
+  define vm_config {
+    $details = $name
+    $id = $details['id']
 
-file { "${libvirt_dir}/autostart":
-  ensure  => 'directory',
-  require => Package[$packages],
-}
-
-file { "${template_dir}":
-  ensure  => 'directory',
-}
-
-vm_config { $vms:
-  before  => Exec['generate_vms'],
-  require => File["${template_dir}"],
-}
-
-exec { 'generate_vms':
-  command     => "/usr/bin/generate_vms.sh ${libvirt_dir} ${template_dir}",
-  path        => ['/usr/sbin', '/usr/bin' , '/sbin', '/bin'],
-  require     => [File["${template_dir}"], File["${libvirt_dir}/autostart"]],
-}
-
-if $::operatingsystem == 'Ubuntu' {
-  # TODO(mpolenchuk): Remove when LP#1057024 has been resolved.
-  # https://bugs.launchpad.net/ubuntu/+source/qemu-kvm/+bug/1057024
-  file { '/dev/kvm':
-    ensure => present,
-    group  => 'kvm',
-    mode   => '0660',
+    file { "${template_dir}/template_${id}_vm.xml":
+      owner   => 'root',
+      group   => 'root',
+      content => template('osnailyfacter/vm_libvirt.erb'),
+    }
   }
 
-  Package<||> ~> File['/dev/kvm'] -> Exec['generate_vms']
+  package { $packages:
+    ensure => 'installed',
+  }
+
+  service { $::nova::params::libvirt_service_name:
+    ensure  => 'running',
+    require => Package[$packages],
+    before  => Exec['generate_vms'],
+  }
+
+  file { "${libvirt_dir}/autostart":
+    ensure  => 'directory',
+    require => Package[$packages],
+  }
+
+  file { "${template_dir}":
+    ensure  => 'directory',
+  }
+
+  vm_config { $vms:
+    before  => Exec['generate_vms'],
+    require => File["${template_dir}"],
+  }
+
+  exec { 'generate_vms':
+    command     => "/usr/bin/generate_vms.sh ${libvirt_dir} ${template_dir}",
+    path        => ['/usr/sbin', '/usr/bin' , '/sbin', '/bin'],
+    require     => [File["${template_dir}"], File["${libvirt_dir}/autostart"]],
+  }
+
+  if $::operatingsystem == 'Ubuntu' {
+    # TODO(mpolenchuk): Remove when LP#1057024 has been resolved.
+    # https://bugs.launchpad.net/ubuntu/+source/qemu-kvm/+bug/1057024
+    file { '/dev/kvm':
+      ensure => present,
+      group  => 'kvm',
+      mode   => '0660',
+    }
+
+    Package<||> ~> File['/dev/kvm'] -> Exec['generate_vms']
+  }
 }
