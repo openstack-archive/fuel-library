b4e62e041a8137a7d3cf81d7550e2a39ec652319
Andrew Woodward <awoodward@mirantis.com>
2016-03-19 21:12:12 -0700

    Refactor out opensack::swift::* classes
    We have tradtionally employed a combined storage and swift proxy role/task
    but they are kept seperate in both our aged openstack::swift::* and in the
    swift module its self.
    
    In this review, we will refactor the single swift.pp task to separate proxy.pp
    and storage.pp tasks. In these tasks, we are only altering what was previously
    in swift.pp with the inclusion of the openstack::swift::* classes directly in
    task. In order to minimise review burden, we will follow up with another
    review to flatten out the openstack::swift::* calls
    
    Change-Id: I1d5d6024a7491b0c58bbd65f958d2f61e797233c
    Blueprint: fuel-remove-conflict-openstack

--- deployment/puppet/osnailyfacter/modular/swift/storage.pp
+++ deployment/puppet/osnailyfacter/modular/swift/storage.pp
@@ -1,3 +1,73 @@
+notice('MODULAR: swift/storage.pp')
+
+$network_scheme             = hiera_hash('network_scheme', {})
+$network_metadata           = hiera_hash('network_metadata', {})
+prepare_network_config($network_scheme)
+
+$swift_hash                 = hiera_hash('swift')
+$is_primary_swift_proxy     = hiera('is_primary_swift_proxy', false)
+$proxy_port                 = hiera('proxy_port', '8080')
+$storage_hash               = hiera_hash('storage')
+$swift_storage_ipaddr       = get_network_role_property('swift/replication', 'ipaddr')
+$debug                      = pick($swift_hash['debug'], hiera('debug', false))
+$verbose                    = pick($swift_hash['verbose'], hiera('verbose', false))
+$deploy_swift_storage       = hiera('deploy_swift_storage', true)
+
+# Use Swift if it isn't replaced by vCenter, Ceph for BOTH images and objects
+if !($storage_hash['images_ceph'] and $storage_hash['objects_ceph']) and !$storage_hash['images_vcenter'] {
+  $master_swift_proxy_nodes      = get_nodes_hash_by_roles($network_metadata, [$swift_master_role])
+  $master_swift_proxy_nodes_list = values($master_swift_proxy_nodes)
+  $master_swift_proxy_ip         = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/api'], '\/\d+$', '')
+  $master_swift_replication_ip   = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/replication'], '\/\d+$', '')
+  $swift_partition               = hiera('swift_partition', '/var/lib/glance/node')
+
+  if ($deploy_swift_storage){
+    if !defined(File['/var/lib/glance']) {
+      file {'/var/lib/glance':
+        ensure  => 'directory',
+        group   => 'swift',
+        require => Package['swift'],
+      } -> Service <| tag == 'swift-service' |>
+    } else {
+      File['/var/lib/glance'] {
+        ensure  => 'directory',
+        group   => 'swift',
+        require +> Package['swift'],
+      }
+      File['/var/lib/glance'] -> Service <| tag == 'swift-service' |>
+    }
+
+    class { 'openstack::swift::storage_node':
+      storage_type                => false,
+      loopback_size               => '5243780',
+      storage_mnt_base_dir        => $swift_partition,
+      storage_devices             => filter_hash($mp_hash,'point'),
+      swift_zone                  => $master_swift_proxy_nodes_list[0]['swift_zone'],
+      swift_local_net_ip          => $swift_storage_ipaddr,
+      master_swift_proxy_ip       => $master_swift_proxy_ip,
+      master_swift_replication_ip => $master_swift_replication_ip,
+      sync_rings                  => ! $is_primary_swift_proxy,
+      debug                       => $debug,
+      verbose                     => $verbose,
+      log_facility                => 'LOG_SYSLOG',
+    }
+  }
+
+
+}
+
+# 'ceilometer' class is being declared inside openstack::ceilometer class
+# which is declared inside openstack::controller class in the other task.
+# So we need a stub here for dependency from swift::proxy::ceilometer
+class ceilometer {}
+include ceilometer
+
+# Class[Swift::Proxy::Cache] requires Class[Memcached] if memcache_servers
+# contains 127.0.0.1. But we're deploying memcached in another task. So we
+# need to add this stub here.
+class memcached {}
+include memcached
+
 #
 class openstack::swift::storage_node (
   $swift_zone,
