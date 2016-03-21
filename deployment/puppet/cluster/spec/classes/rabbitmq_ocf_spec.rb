a2bdb3f62bc2a9f0df53bbccbc749a123aa010ce
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-03-21 14:13:11 +0100

    Put the RabbitMQ OCF RA policy to /usr/sbin
    * Fix failing pcs resource list command
      and move the policy file from the ocf to policy dir
    * Configure the custom policy file to be picked
      in the /usr/sbin/set_rabbitmq_policy as the
      fuel-libraryX package installs it.
    * As the upstream rabbitmq-server package does not
      install one, use the default policy OCF path param
      as the /usr/local/sbin/...
    * Add the policy_file param and unit tests to the
      cluster::rabbitmq_ocf
    
    Closes-bug: #1558627
    
    Change-Id: I4937bde611b06c3e39385a322053610c98584d79
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

---
+++ deployment/puppet/cluster/spec/classes/rabbitmq_ocf_spec.rb
@@ -0,0 +1,137 @@
+require 'spec_helper'
+
+describe 'cluster::rabbitmq_ocf' do
+
+  shared_examples_for 'rabbitmq_ocf configuration' do
+
+    let(:pre_condition) {
+      'include rabbitmq::service'
+    }
+    let(:params) {{
+      :primitive_type          => 'rabbitmq-server',
+      :service_name            => 'rabbitmq-server',
+      :port                    => '5672',
+      :host_ip                 => '127.0.0.1',
+      :debug                   => false,
+      :ocf_script_file         => 'cluster/ocf/rabbitmq',
+      :command_timeout         => '',
+      :erlang_cookie           => 'EOKOWXQREETZSHFNTPEY',
+      :admin_user              => 'nil',
+      :admin_pass              => 'nil',
+      :enable_rpc_ha           => false,
+      :enable_notifications_ha => true,
+      :fqdn_prefix             => 'nil',
+      :pid_file                => 'nil',
+      :policy_file             => 'nil',
+    }}
+
+    let(:metadata) {{
+      'migration-threshold' => '10',
+      'failure-timeout'     => '30s',
+      'resource-stickiness' => '100',
+    }}
+
+    let(:complex_metadata) {{
+      'notify'          => 'true',
+      'ordered'         => 'false',
+      'interleave'      => 'true',
+      'master-max'      => '1',
+      'master-node-max' => '1',
+      'target-role'     => 'Master'
+    }}
+
+    let(:monitor) {{
+      'interval' => '30',
+      'timeout'  => '180'
+    }}
+
+    let(:monitor_master) {{
+      'role' => 'Master',
+    'interval' => '27',
+    'timeout'  => '180'
+    }}
+
+    let(:monitor_slave) {{
+      'role'            => 'Slave',
+      'interval'        => '103',
+      'timeout'         => '180',
+      'OCF_CHECK_LEVEL' => '30'
+    }}
+
+    let(:start) {{
+      'interval' => '0',
+      'timeout'  => '360'
+    }}
+
+    let(:stop) {{
+      'interval' => '0',
+      'timeout'  => '120'
+    }}
+
+    let(:promote) {{
+      'interval' => '0',
+      'timeout'  => '120'
+    }}
+
+    let(:demote) {{
+      'interval' => '0',
+      'timeout'  => '120'
+    }}
+
+    let(:notify) {{
+      'interval' => '0',
+      'timeout'  => '180'
+    }}
+
+    let(:operations) {{
+      'monitor'        => monitor,
+      'monitor:Master' => monitor_master,
+      'monitor:Slave'  => monitor_slave,
+      'start'          => start,
+      'stop'           => stop,
+      'promote'        => promote,
+      'demote'         => demote,
+      'notify'        => notify,
+    }}
+
+    let(:parameters) {{
+      'host_ip'                  => params[:host_ip],
+      'node_port'                => params[:port],
+      'debug'                    => params[:debug],
+      'command_timeout'          => params[:command_timeout],
+      'erlang_cookie'            => params[:erlang_cookie],
+      'admin_user'               => params[:admin_user],
+      'admin_password'           => params[:admin_pass],
+      'enable_rpc_ha'            => params[:enable_rpc_ha],
+      'enable_notifications_ha'  => params[:enable_notifications_ha],
+      'fqdn_prefix'              => params[:fqdn_prefix],
+      'pid_file'                 => params[:pid_file],
+      'policy_file'              => params[:policy_file],
+    }}
+
+    it 'configures with the default params' do
+      should contain_class('cluster::rabbitmq_ocf')
+      should contain_pacemaker__service(params[:service_name]).with(
+        :primitive_type   => params[:primitive_type],
+        :complex_type     => 'master',
+        :complex_metadata => complex_metadata,
+        :metadata         => metadata,
+        :operations       => operations,
+        :parameters       => parameters
+      )
+    end
+  end
+
+  context 'on Debian platforms' do
+    let :facts do
+      { :osfamily => 'Debian',
+        :operatingsystem => 'Debian',
+        :lsbdistid => 'Debian'
+      }
+    end
+
+    it_configures 'rabbitmq_ocf configuration'
+  end
+
+end
+
