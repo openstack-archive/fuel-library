33b289c3555f5b870d7c7dd83ee12cd8bc275d36
Alexander Noskov <anoskov@mirantis.com>
2016-03-21 14:33:31 +0300

    Refactor osnailyfacter/modular/openstack_controller
    Refactor osnailyfacter/modular/openstack_controller to be compatible with Puppet Master
    
    Blueprint: fuel-refactor-osnailyfacter-for-puppet-master-compatibility
    
    Change-Id: I2b31d8b0b9c760f6ed600a3e64e3440c82cf8f0e

--- tests/noop/spec/hosts/openstack-controller/openstack-controller_spec.rb
+++ tests/noop/spec/hosts/openstack-controller/openstack-controller_spec.rb
@@ -389,11 +389,11 @@ describe manifest do
         should contain_exec('create-m1.micro-flavor').with(
            'command' => 'bash -c "nova flavor-create --is-public true m1.micro auto 64 0 1"',
            'unless'  => 'bash -c \'for tries in {1..10}; do
-                    nova flavor-list | grep m1.micro;
-                    status=("${PIPESTATUS[@]}");
-                    (( ! status[0] )) && exit "${status[1]}";
-                    sleep 2;
-                  done; exit 1\'',
+                      nova flavor-list | grep m1.micro;
+                      status=("${PIPESTATUS[@]}");
+                      (( ! status[0] )) && exit "${status[1]}";
+                      sleep 2;
+                    done; exit 1\'',
         )
       end
     end
