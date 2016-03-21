b3e229a2edbb3b7c7f86afdd67d52be802f216f2
Sergey Kulanov <skulanov@mirantis.com>
2016-03-21 13:17:41 +0200

    Bump version to 10.0
    Fuel-CI: disable
    
    Change-Id: Ifdaf5597bc3578ed31e22c58985cd06fefeb5d92
    Related-bug: #1558589

--- deployment/puppet/fuel/lib/facter/fuel_openstack_version.rb
+++ deployment/puppet/fuel/lib/facter/fuel_openstack_version.rb
@@ -1,12 +1,6 @@
 require 'facter'
 
 fuel_openstack_version_path = '/etc/fuel_openstack_version'
-# FIXME(kozhukalov): This default value is necessary
-# to solve chicken/egg problem. The thing is that
-# deployment tests use the ISO where this file is not
-# installed. This default value should be removed
-# once the ISO is updated.
-read_data = "mitaka-9.0"
 
 Facter.add('fuel_openstack_version') do
   if File.exist?(fuel_openstack_version_path)
--- deployment/puppet/fuel/lib/facter/fuel_release.rb
+++ deployment/puppet/fuel/lib/facter/fuel_release.rb
@@ -1,12 +1,6 @@
 require 'facter'
 
 fuel_release_path = '/etc/fuel_release'
-# FIXME(kozhukalov): This default value is necessary
-# to solve chicken/egg problem. The thing is that
-# deployment tests use the ISO where this file is not
-# installed. This default value should be removed
-# once the ISO is updated.
-read_data = "9.0"
 
 Facter.add('fuel_release') do
   if File.exist?(fuel_release_path)
