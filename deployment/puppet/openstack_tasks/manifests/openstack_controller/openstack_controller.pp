1015de96efd4f74ee8a3a0acf7975b6411bbe554
Alexey Deryugin <aderyugin@mirantis.com>
2016-03-23 16:43:32 +0300

    Create usefull security groups by default
    We need to create some default security groups, that will
    allow to use OpenStack cloud immediately after the deployment.
    
    By default it will create the following security groups:
    1. global_http - security group which opens HTTP/HTTPS for external traffic.
    2. global_ssh - security group which opens SSH port for external traffic.
    3. allow_all - security group which allows all traffic
    for any TCP/UDP ports from external network.
    
    Change-Id: I23ea837cbe92b5091f07de291f0e9f5f40e6fd44
    Closes-Bug: #1349819

--- deployment/puppet/openstack_tasks/manifests/openstack_controller/openstack_controller.pp
+++ deployment/puppet/openstack_tasks/manifests/openstack_controller/openstack_controller.pp
@@ -424,6 +424,71 @@ class openstack_tasks::openstack_controller::openstack_controller {
       try_sleep => 2,
       require   => Class['::nova'],
     }
+
+    if pick($nova_hash['create_default_security_groups'], true) {
+      Nova_security_group<||> -> Nova_security_rule<||>
+      Nova_security_rule {
+        ensure      => present,
+        ip_protocol => 'tcp',
+        ip_range    => '0.0.0.0/0',
+      }
+  
+      nova_security_group { 'global_http':
+        ensure      => present,
+        description => 'Allow HTTP traffic'
+      } ->
+  
+      nova_security_rule { 'http_01':
+        from_port => '80',
+        to_port => '80',
+        security_group => 'global_http'
+      } ->
+
+      nova_security_rule { 'http_02':
+        from_port => '443',
+        to_port => '443',
+        security_group => 'global_http'
+      }
+
+      nova_security_group { 'global_ssh':
+        ensure      => present,
+        description => 'Allow SSH traffic'
+      } ->
+
+      nova_security_rule { 'ssh_01':
+        from_port => '22',
+        to_port => '22',
+        security_group => 'global_ssh'
+      }
+
+      nova_security_group { 'allow_all':
+        ensure      => present,
+        description => 'Allow all traffic'
+      } ->
+
+      nova_security_rule { 'all_01':
+        from_port => '1',
+        to_port => '65535',
+        security_group => 'allow_all'
+      } ->
+
+      nova_security_rule { 'all_02':
+        ip_protocol => 'udp',
+        from_port => '1',
+        to_port => '65535',
+        security_group => 'allow_all'
+      }
+    } else {
+      nova_security_group { 'global_http':
+        ensure => absent
+      }
+      nova_security_group { 'global_ssh':
+        ensure => absent
+      }
+      nova_security_group { 'allow_all':
+        ensure => absent
+      }
+    }
   }
 
   nova_config {
