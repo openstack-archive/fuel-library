ae621ccf8d9fb665f58396731124faedbccc3ffa
Alexey Deryugin <aderyugin@mirantis.com>
2016-03-23 16:43:32 +0300

    Create usefull security groups by default
    We need to create some default security groups, that will
    allow to use OpenStack cloud immediately after the deployment.
    
    By default it will create the following security groups:
    1. global_http - security group which opens HTTP/HTTPS for external traffic.
    2. global_ssh - security group which opens SSH port for external traffic.
    3. allow_all - security group which allows all traffic
    for any TCP/UDP ports from external network.
    
    Change-Id: I23ea837cbe92b5091f07de291f0e9f5f40e6fd44
    Closes-Bug: #1349819

--- tests/noop/spec/hosts/openstack-controller/openstack-controller_spec.rb
+++ tests/noop/spec/hosts/openstack-controller/openstack-controller_spec.rb
@@ -421,6 +421,58 @@ describe manifest do
                     done; exit 1\'',
         )
       end
+
+      it 'should create default security groups' do
+        if Noop.puppet_function('pick', nova_hash['create_default_security_groups'], true)
+          should contain_nova_security_group('global_http')
+
+          should contain_nova_security_rule('http_01').with(
+            'ip_protocol' => 'tcp',
+            'from_port' => '80',
+            'to_port' => '80',
+            'ip_range' => '0.0.0.0/0',
+            'security_group' => 'global_http'
+          )
+          should contain_nova_security_rule('http_02').with(
+            'ip_protocol' => 'tcp',
+            'from_port' => '443',
+            'to_port' => '443',
+            'ip_range' => '0.0.0.0/0',
+            'security_group' => 'global_http'
+          )
+
+          should contain_nova_security_group('global_ssh')
+
+          should contain_nova_security_rule('ssh_01').with(
+            'ip_protocol' => 'tcp',
+            'from_port' => '22',
+            'to_port' => '22',
+            'ip_range' => '0.0.0.0/0',
+            'security_group' => 'global_ssh'
+          )
+
+          should contain_nova_security_group('allow_all')
+
+          should contain_nova_security_rule('all_01').with(
+            'ip_protocol' => 'tcp',
+            'from_port' => '1',
+            'to_port' => '65535',
+            'ip_range' => '0.0.0.0/0',
+            'security_group' => 'allow_all'
+          )
+          should contain_nova_security_rule('all_02').with(
+            'ip_protocol' => 'udp',
+            'from_port' => '1',
+            'to_port' => '65535',
+            'ip_range' => '0.0.0.0/0',
+            'security_group' => 'allow_all'
+          )
+        else
+          should contain_nova_security_group('global_http').with('ensure' => 'absent')
+          should contain_nova_security_group('global_ssh').with('ensure' => 'absent')
+          should contain_nova_security_group('allow_all').with('ensure' => 'absent')
+        end
+      end
     end
 
     ironic_enabled = Noop.hiera_structure 'ironic/enabled'
