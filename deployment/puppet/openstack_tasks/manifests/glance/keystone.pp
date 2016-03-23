c1f8ff445cb0edd8851afc597bc3fbd1663913d9
Denis Egorenko <degorenko@mirantis.com>
2016-03-23 20:49:30 +0300

    Implement Glance Glare service
    Glance V3 experimental API has been removed in Mitaka in
    favour of standalone Glance Artifacts Repository (GLARE) API.
    
    This patch adds a new service, haproxy settings and tests.
    
    Change-Id: I1df8b9aa8698619e726ad583a1a5ad6d5d671e07
    Closes-bug: #1555697
    (cherry picked from commit 781814fe6d24433184261018d1966de47a2e40a8)

--- deployment/puppet/openstack_tasks/manifests/glance/keystone.pp
+++ deployment/puppet/openstack_tasks/manifests/glance/keystone.pp
@@ -2,21 +2,31 @@ class openstack_tasks::glance::keystone {
 
   notice('MODULAR: glance/keystone.pp')
 
-  $glance_hash         = hiera_hash('glance', {})
-  $public_vip          = hiera('public_vip')
-  $public_ssl_hash     = hiera_hash('public_ssl')
-  $management_vip      = hiera('management_vip')
-  $region              = pick($glance_hash['region'], hiera('region', 'RegionOne'))
-  $password            = $glance_hash['user_password']
-  $auth_name           = pick($glance_hash['auth_name'], 'glance')
-  $configure_endpoint  = pick($glance_hash['configure_endpoint'], true)
-  $configure_user      = pick($glance_hash['configure_user'], true)
-  $configure_user_role = pick($glance_hash['configure_user_role'], true)
-  $service_name        = pick($glance_hash['service_name'], 'glance')
-  $tenant              = pick($glance_hash['tenant'], 'services')
-  $ssl_hash            = hiera_hash('use_ssl', {})
+  $glance_hash               = hiera_hash('glance', {})
+  $glance_glare_hash         = hiera_hash('glance_glare', {})
+  $public_vip                = hiera('public_vip')
+  $public_ssl_hash           = hiera_hash('public_ssl')
+  $management_vip            = hiera('management_vip')
+  $region                    = pick($glance_hash['region'], hiera('region', 'RegionOne'))
+  $password                  = $glance_hash['user_password']
+  $auth_name                 = pick($glance_hash['auth_name'], 'glance')
+  $configure_endpoint        = pick($glance_hash['configure_endpoint'], true)
+  $configure_user            = pick($glance_hash['configure_user'], true)
+  $configure_user_role       = pick($glance_hash['configure_user_role'], true)
+  $service_name              = pick($glance_hash['service_name'], 'glance')
+  $tenant                    = pick($glance_hash['tenant'], 'services')
+  $glare_password            = $glance_glare_hash['user_password']
+  $glare_auth_name           = pick($glance_glare_hash['auth_name'], 'glare')
+  $glare_configure_endpoint  = pick($glance_glare_hash['configure_endpoint'], true)
+  $glare_configure_user      = pick($glance_glare_hash['configure_user'], true)
+  $glare_configure_user_role = pick($glance_glare_hash['configure_user_role'], true)
+  $glare_service_name        = pick($glance_glare_hash['service_name'], 'glare')
+  $glare_tenant              = pick($glance_glare_hash['tenant'], 'services')
+  $glare_region              = pick($glance_glare_hash['region'], hiera('region', 'RegionOne'))
+  $ssl_hash                  = hiera_hash('use_ssl', {})
 
   Class['::osnailyfacter::wait_for_keystone_backends'] -> Class['::glance::keystone::auth']
+  Class['::osnailyfacter::wait_for_keystone_backends'] -> Class['::glance::keystone::glare_auth']
 
   $public_protocol     = get_ssl_property($ssl_hash, $public_ssl_hash, 'glance', 'public', 'protocol', 'http')
   $public_address      = get_ssl_property($ssl_hash, $public_ssl_hash, 'glance', 'public', 'hostname', [$public_vip])
@@ -25,9 +35,20 @@ class openstack_tasks::glance::keystone {
   $admin_protocol      = get_ssl_property($ssl_hash, {}, 'glance', 'admin', 'protocol', 'http')
   $admin_address       = get_ssl_property($ssl_hash, {}, 'glance', 'admin', 'hostname', [$management_vip])
 
+  $glare_public_protocol     = get_ssl_property($ssl_hash, $public_ssl_hash, 'glare', 'public', 'protocol', 'http')
+  $glare_public_address      = get_ssl_property($ssl_hash, $public_ssl_hash, 'glare', 'public', 'hostname', [$public_vip])
+  $glare_internal_protocol   = get_ssl_property($ssl_hash, {}, 'glare', 'internal', 'protocol', 'http')
+  $glare_internal_address    = get_ssl_property($ssl_hash, {}, 'glare', 'internal', 'hostname', [$management_vip])
+  $glare_admin_protocol      = get_ssl_property($ssl_hash, {}, 'glare', 'admin', 'protocol', 'http')
+  $glare_admin_address       = get_ssl_property($ssl_hash, {}, 'glare', 'admin', 'hostname', [$management_vip])
+
   $public_url = "${public_protocol}://${public_address}:9292"
   $internal_url = "${internal_protocol}://${internal_address}:9292"
   $admin_url  = "${admin_protocol}://${admin_address}:9292"
+
+  $glare_public_url = "${glare_public_protocol}://${glare_public_address}:9494"
+  $glare_internal_url = "${glare_internal_protocol}://${glare_internal_address}:9494"
+  $glare_admin_url  = "${glare_admin_protocol}://${glare_admin_address}:9494"
 
   validate_string($public_address)
   validate_string($password)
@@ -46,6 +67,20 @@ class openstack_tasks::glance::keystone {
     admin_url           => $admin_url,
     region              => $region,
     tenant              => $tenant,
+  }
+
+  class { '::glance::keystone::glare_auth':
+    password            => $glare_password,
+    auth_name           => $glare_auth_name,
+    configure_endpoint  => $glare_configure_endpoint,
+    configure_user      => $glare_configure_user,
+    configure_user_role => $glare_configure_user_role,
+    service_name        => $glare_service_name,
+    public_url          => $glare_public_url,
+    internal_url        => $glare_internal_url,
+    admin_url           => $glare_admin_url,
+    region              => $glare_region,
+    tenant              => $glare_tenant,
   }
 
 }
