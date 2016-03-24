be92d3fe2b9c8377ccdd5cfec248e4ba07130e0b
Alexey Deryugin <aderyugin@mirantis.com>
2016-03-24 16:49:49 +0300

    Add ability to configure heat-cfn keystone auth_name via hiera
    Change-Id: Ib8d02b6d79196f9e2bf30cead976d9e61c468e1f
    Closes-Bug: #1561236

--- deployment/puppet/openstack_tasks/manifests/heat/cfn_keystone.pp
+++ deployment/puppet/openstack_tasks/manifests/heat/cfn_keystone.pp
@@ -1,6 +1,6 @@
-class openstack_tasks::heat::keystone {
+class openstack_tasks::heat::cfn_keystone {
 
-  notice('MODULAR: heat/keystone.pp')
+  notice('MODULAR: heat/cfn_keystone.pp')
 
   $heat_hash         = hiera_hash('heat', {})
   $public_vip        = hiera('public_vip')
@@ -19,14 +19,15 @@ class openstack_tasks::heat::keystone {
   $admin_address     = get_ssl_property($ssl_hash, {}, 'heat', 'admin', 'hostname', [hiera('heat_endpoint', ''), $management_vip])
 
   $password            = $heat_hash['user_password']
-  $auth_name           = pick($heat_hash['auth_name'], 'heat')
+  $cfn_auth_name       = pick($heat_hash['cfn_auth_name'], 'heat-cfn')
   $configure_endpoint  = pick($heat_hash['configure_endpoint'], true)
   $configure_user      = pick($heat_hash['configure_user'], true)
   $configure_user_role = pick($heat_hash['configure_user_role'], true)
   $service_name        = pick($heat_hash['service_name'], 'heat')
   $tenant              = pick($heat_hash['tenant'], 'services')
+  $cfn_auth_email      = pick($heat_hash['cfn_auth_email'], "${cfn_auth_name}@localhost")
 
-  Class['::osnailyfacter::wait_for_keystone_backends'] -> Class['::heat::keystone::auth']
+  Class['::osnailyfacter::wait_for_keystone_backends'] -> Class['::heat::keystone::auth_cfn']
 
   validate_string($public_address)
   validate_string($password)
@@ -37,18 +38,19 @@ class openstack_tasks::heat::keystone {
 
   class { '::osnailyfacter::wait_for_keystone_backends': }
 
-  class { '::heat::keystone::auth' :
-    password               => $password,
-    auth_name              => $auth_name,
-    region                 => $region,
-    tenant                 => $keystone_tenant,
-    email                  => "${auth_name}@localhost",
-    configure_endpoint     => true,
-    configure_user         => $configure_user,
-    configure_user_role    => $configure_user_role,
-    trusts_delegated_roles => $trusts_delegated_roles,
-    public_url             => $public_url,
-    internal_url           => $internal_url,
-    admin_url              => $admin_url,
+  class { '::heat::keystone::auth_cfn' :
+    password            => $password,
+    auth_name           => $cfn_auth_name,
+    service_type        => 'cloudformation',
+    region              => $region,
+    tenant              => $keystone_tenant,
+    email               => $cfn_auth_email,
+    configure_endpoint  => true,
+    configure_user      => $configure_user,
+    configure_user_role => $configure_user_role,
+    public_url          => $public_url_cfn,
+    internal_url        => $internal_url_cfn,
+    admin_url           => $admin_url_cfn,
   }
+
 }
