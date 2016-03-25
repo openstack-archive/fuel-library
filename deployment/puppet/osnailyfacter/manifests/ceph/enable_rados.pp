dabb78de831e4e90bd7333539924881a1179cfa7
Andrey Shestakov <ashestakov@mirantis.com>
2016-03-25 13:28:29 +0200

    Add RadosGW readiness check
    After radosgw service start it requires some time to get operational state
    This change introduces check script which waits until radosgw start to serve
    http requests.
    
    Change-Id: Iaeb3f60c0da114de1fea3f0ec3aa69acd05b037a
    Closes-bug: #1545651

--- deployment/puppet/osnailyfacter/manifests/ceph/enable_rados.pp
+++ deployment/puppet/osnailyfacter/manifests/ceph/enable_rados.pp
@@ -30,13 +30,35 @@ class osnailyfacter::ceph::enable_rados {
 
     Service['radosgw'] ->
     File[$radosgw_override_file] ~>
-    Service['radosgw-all']
+    Service['radosgw-all'] ->
+    ::Osnailyfacter::Wait_for_backend['object-storage']
   }
   else {
     service { $radosgw_service:
       ensure => running,
       enable => true,
     }
+
+    Service[$radosgw_service] -> ::Osnailyfacter::Wait_for_backend['object-storage']
   }
 
+  $management_vip   = hiera('management_vip')
+  $service_endpoint = hiera('service_endpoint')
+  $ssl_hash         = hiera_hash('use_ssl', {})
+
+  $rgw_protocol = get_ssl_property($ssl_hash, {}, 'radosgw', 'internal', 'protocol', 'http')
+  $rgw_address  = get_ssl_property($ssl_hash, {}, 'radosgw', 'internal', 'hostname', [$service_endpoint, $management_vip])
+  $rgw_url = "${rgw_protocol}://${rgw_address}:8080"
+
+  $lb_hash = {
+    'object-storage' => {
+      name     => 'object-storage',
+      provider => 'http',
+      url      => $rgw_url
+    }
+  }
+
+  ::osnailyfacter::wait_for_backend {'object-storage':
+    lb_hash     => $lb_hash
+  }
 }
