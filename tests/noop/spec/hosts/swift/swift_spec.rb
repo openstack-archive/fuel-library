698ad7d164181e374ccbe48a30cde6e951c84994
Alex Schultz <aschultz@mirantis.com>
2016-03-25 10:26:33 -0600

    Fix missing swift noop tests
    This change fixes the file names for the swift tasks that were created
    as part of the openstack::swift::* class refactor. This change also adds
    a test for the swift task that now include the proxy and storage code
    for backwards compatibility.
    
    Change-Id: I7da80d5f4976586e0712714b0fca7b5666b82f0c
    Related-Bug: #1561890

---
+++ tests/noop/spec/hosts/swift/swift_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'swift/swift.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+    it 'should contain storage and proxy tasks' do
+      should contain_class('openstack_tasks::swift::storage')
+      should contain_class('openstack_tasks::swift::proxy')
+    end
+  end
+
+  test_ubuntu_and_centos manifest
+end
+
