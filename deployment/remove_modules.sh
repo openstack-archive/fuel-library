dba285339cb00c44ee6433448d44066ca05d8570
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-03-25 10:56:57 +0100

    Fix tests and scripts for multiple Puppetfiles
    This change updates the scripts that manage puppet modules via
    puppet-librarian-simple to dynamically search for multiple Puppetfiles.
    Additionally the logic around which modules to test has also been updatd
    to support multiple Puppetfiles.
    
    Partial-bug: #1561890
    Closes-Bug: #1553385
    
    Change-Id: I57a9337091d4737aed6bec3e3cec5519d46345a3
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>
    Co-Authored-By: Alex Schultz <aschultz@mirantis.com>

--- deployment/remove_modules.sh
+++ deployment/remove_modules.sh
@@ -1,13 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 # remove all external puppet modules
 
 dir=`dirname $0`
 cd "${dir}" || exit 1
 
-cat Puppetfile | grep "^mod '" | awk -F "'" '{ print $2 }' | while read module; do
-  if [ -d "puppet/${module}" ]; then
-    echo "Remove: puppet/${module}"
-    rm -rf "puppet/${module}"
-  fi
+PUPPET_FILES=$(find $dir -name Puppetfile)
+
+for f in $PUPPET_FILES; do
+  cat $f | grep "^mod '" | awk -F "'" '{ print $2 }' | while read module; do
+    if [ -d "puppet/${module}" ]; then
+      echo "Remove: puppet/${module}"
+      rm -rf "puppet/${module}"
+    fi
+  done
 done
 rm -f 'Puppetfile.lock'
