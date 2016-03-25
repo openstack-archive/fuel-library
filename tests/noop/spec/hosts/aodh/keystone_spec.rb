29c5039ce761e208ce0a96fa384e56fe1004e713
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-03-25 14:34:08 +0100

    WIP Map noop fixtures for integration tests
    Use custom mappings (#RUN notation) of noop fixtures
    for noop integration tests.
    Iteration 1: map all:all
    
    Closes-bug: #1561890
    
    Change-Id: I6ca3363ea4b2fae1ec73d61122caef6764ba79d1
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- tests/noop/spec/hosts/aodh/keystone_spec.rb
+++ tests/noop/spec/hosts/aodh/keystone_spec.rb
@@ -1,3 +1,24 @@
+# RUN: neut_vxlan_dvr.murano.sahara-primary-controller.yaml ubuntu
+# RUN: neut_vxlan_dvr.murano.sahara-primary-controller.overridden_ssl.yaml ubuntu
+# RUN: neut_vxlan_dvr.murano.sahara-controller.yaml ubuntu
+# RUN: neut_vxlan_dvr.murano.sahara-compute.yaml ubuntu
+# RUN: neut_vxlan_dvr.murano.sahara-cinder.yaml ubuntu
+# RUN: neut_vlan_l3ha.ceph.ceil-primary-mongo.yaml ubuntu
+# RUN: neut_vlan_l3ha.ceph.ceil-primary-controller.yaml ubuntu
+# RUN: neut_vlan_l3ha.ceph.ceil-controller.yaml ubuntu
+# RUN: neut_vlan_l3ha.ceph.ceil-compute.yaml ubuntu
+# RUN: neut_vlan_l3ha.ceph.ceil-ceph-osd.yaml ubuntu
+# RUN: neut_vlan.ironic.controller.yaml ubuntu
+# RUN: neut_vlan.ironic.conductor.yaml ubuntu
+# RUN: neut_vlan.compute.ssl.yaml ubuntu
+# RUN: neut_vlan.compute.ssl.overridden.yaml ubuntu
+# RUN: neut_vlan.compute.nossl.yaml ubuntu
+# RUN: neut_vlan.cinder-block-device.compute.yaml ubuntu
+# RUN: neut_vlan.ceph.controller-ephemeral-ceph.yaml ubuntu
+# RUN: neut_vlan.ceph.compute-ephemeral-ceph.yaml ubuntu
+# RUN: neut_vlan.ceph.ceil-primary-controller.overridden_ssl.yaml ubuntu
+# RUN: neut_vlan.ceph.ceil-compute.overridden_ssl.yaml ubuntu
+# RUN: neut_gre.generate_vms.yaml ubuntu
 require 'spec_helper'
 require 'shared-examples'
 manifest = 'aodh/keystone.pp'
