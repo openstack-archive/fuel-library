01ac143513aad79ec49744bb73aecc80cc6ac96a
Alex Schultz <aschultz@mirantis.com>
2016-03-28 16:35:14 -0600

    Rewrite upload_cirros using puppet
    This change replaces the upload_cirros ruby script with a puppet
    equivalent that will wait for the glance backends and use the
    glance_image provider to upload the image to glance.
    
    Change-Id: I3bf439539e5db87fd3598e779165934a78ed9570
    Related-Bug: #1556068

---
+++ deployment/puppet/osnailyfacter/manifests/astute/upload_cirros.pp
@@ -0,0 +1,28 @@
+# == Class: osnailyfacter::astute::upload_cirros
+#
+# Task to upload a basic cirros image to the evironment.
+#
+# == Paramters:
+#
+# N/A
+#
+class osnailyfacter::astute::upload_cirros {
+
+  notice('MODULAR: astute/cirros_image_upload.pp')
+
+  $test_vm_image = hiera_hash('test_vm_image')
+
+  include ::osnailyfacter::wait_for_glance_backends
+
+  #TODO(aschultz): extend glance_image to support extra options
+  glance_image { $test_vm_image['img_name']:
+    ensure           => present,
+    container_format => $test_vm_image['container_format'],
+    disk_format      => $test_vm_image['disk_format'],
+    is_public        => $test_vm_image['public'],
+    min_ram          => $test_vm_image['min_ram'],
+    source           => $test_vm_image['img_path']
+  }
+
+  Class['osnailyfacter::wait_for_glance_backends'] -> Glance_image<||>
+}
