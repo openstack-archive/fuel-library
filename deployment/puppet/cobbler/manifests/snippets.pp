a57ceec064f48b8c1805c690c247d3cf6b03b509
Vitaly Parakhin <vparakhin@mirantis.com>
2016-03-30 13:25:20 +0300

    Remove deprecated cobbler parts for classic provisioning
    Since Fuel 6.1 we no longer support CentOS envs,
    so let's remove related cobbler config entries,
    snippets etc.
    In Fuel 7.0 we dropped classic provisioning mode,
    fully switching to IBP. Remove cobbler distro and
    profile for debian-installer, since it's no longer
    used [1]
    
    [1] https://review.openstack.org/#/c/299340/
    
    Related-Bug: #1549346
    Related-Bug: #1570267
    Change-Id: I5262bbd726112e1a32513d71ac04b1fb6767983f

--- deployment/puppet/cobbler/manifests/snippets.pp
+++ deployment/puppet/cobbler/manifests/snippets.pp
@@ -14,58 +14,6 @@
 
 class cobbler::snippets {
 
-  ::cobbler::snippet {'send2syslog': }
-  ::cobbler::snippet {'target_logs_to_master': }
-  ::cobbler::snippet {'target_logs_to_master_ubuntu': }
-  ::cobbler::snippet {'kickstart_ntp': }
-  ::cobbler::snippet {'ntp_to_masternode': }
-  ::cobbler::snippet {'ntp_to_masternode_ubuntu': }
-  ::cobbler::snippet {'pre_install_network_config': }
-  ::cobbler::snippet {'pre_install_partition': }
-  ::cobbler::snippet {'pre_install_partition_lvm': }
-  ::cobbler::snippet {'nailgun_repo': }
-  ::cobbler::snippet {'ssh_disable_gssapi': }
-  ::cobbler::snippet {'sshd_auth_pubkey_only': }
-  ::cobbler::snippet {'disable_pxe':}
-  ::cobbler::snippet {'post_part_compute':}
-  ::cobbler::snippet {'post_part_controller':}
-  ::cobbler::snippet {'post_part_storage':}
-  ::cobbler::snippet {'post_install_network_config_fuel':}
-  ::cobbler::snippet {'puppet_register_if_enabled_fuel':}
-  ::cobbler::snippet {'url_proxy':}
-  ::cobbler::snippet {'puppet_install_if_enabled':}
-  ::cobbler::snippet {'puppet_conf':}
-  ::cobbler::snippet {'puppet_register_if_enabled':}
-  ::cobbler::snippet {'ntp_register_if_enabled':}
-  ::cobbler::snippet {'mcollective_install_if_enabled':}
-  ::cobbler::snippet {'mcollective_conf':}
-  ::cobbler::snippet {'post_install_network_config':}
-  ::cobbler::snippet {'cinder_create_lvm_group':}
-  ::cobbler::snippet {'cinder_create_lvm_group__ubuntu':}
-  ::cobbler::snippet {'centos_authorized_keys': }
-  ::cobbler::snippet {'centos_blacklist_i2c_piix4':}
-  ::cobbler::snippet {'centos_static_net':}
-  ::cobbler::snippet {'ofed_install_with_sriov':}
-  ::cobbler::snippet {'ubuntu_authorized_keys':}
-  ::cobbler::snippet {'ubuntu_blacklist_i2c_piix4':}
-  ::cobbler::snippet {'ubuntu_disable_pxe':}
-  ::cobbler::snippet {'ubuntu_puppet_config':}
-  ::cobbler::snippet {'ubuntu_mcollective_config':}
-  ::cobbler::snippet {'ubuntu_network':}
-  ::cobbler::snippet {'ubuntu_network_console_and_syslog':}
-  ::cobbler::snippet {'ubuntu_partition':}
-  ::cobbler::snippet {'ubuntu_partition_late':}
-  ::cobbler::snippet {'ubuntu_static_net':}
-  ::cobbler::snippet {'ubuntu_repos_late':}
-  ::cobbler::snippet {'ubuntu_remove_repos_late':}
-  ::cobbler::snippet {'ubuntu_precise_packages_late':}
-  ::cobbler::snippet {'ubuntu_trusty_packages_late':}
-  ::cobbler::snippet {'anaconda_ssh_console':}
-  ::cobbler::snippet {'anaconda_yum':}
-  ::cobbler::snippet {'centos_post_kernel_lt_if_enabled':}
-  ::cobbler::snippet {'centos_pkg_kernel_lt_if_enabled':}
-  ::cobbler::snippet {'centos_ofed_prereq_pkgs_if_enabled':}
-
   case $::operatingsystem {
     /(?i)(debian|ubuntu)/:  {
       file { '/usr/bin/late_command.py' :
