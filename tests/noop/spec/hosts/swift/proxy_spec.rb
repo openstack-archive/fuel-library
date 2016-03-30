6b1a2c8850fc58a3052dd647b544f892e019f7ce
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-03-30 15:58:17 +0200

    Fix unmatched integration noop tests for swift
    Partial-bug: #1563215
    
    Change-Id: Icb9aac594315867bdc614bbe98ce46b72b7d78ac
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- tests/noop/spec/hosts/swift/proxy_spec.rb
+++ tests/noop/spec/hosts/swift/proxy_spec.rb
@@ -1,3 +1,11 @@
+# RUN: neut_vlan.ceph.ceil-primary-controller.overridden_ssl ubuntu
+# RUN: neut_vlan.ceph.controller-ephemeral-ceph ubuntu
+# RUN: neut_vlan.ironic.controller ubuntu
+# RUN: neut_vlan_l3ha.ceph.ceil-controller ubuntu
+# RUN: neut_vlan_l3ha.ceph.ceil-primary-controller ubuntu
+# RUN: neut_vxlan_dvr.murano.sahara-primary-controller ubuntu
+# RUN: neut_vxlan_dvr.murano.sahara-primary-controller.overridden_ssl ubuntu
+
 require 'spec_helper'
 require 'shared-examples'
 manifest = 'swift/proxy.pp'
@@ -315,8 +323,5 @@ describe manifest do
     end
   end
 
-
-  end
   test_ubuntu_and_centos manifest
 end
-
