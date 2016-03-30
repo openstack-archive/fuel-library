94cb081b531be7c79670c57481e50429f3a5396a
Aleksandr Didenko <adidenko@mirantis.com>
2016-03-30 12:30:21 +0200

    (WIP) Make network configuration idempotent
    WORK IN PROGRESS
    
    Remove configure_default_route task to make network configuration
    idempotent.
    
    Change-Id: I229957b60c85ed94c2d0ba829642dd6e465e9eca
    Partial-bug: #1541309

--- tests/noop/spec/hosts/netconfig/netconfig_spec.rb
+++ tests/noop/spec/hosts/netconfig/netconfig_spec.rb
@@ -33,6 +33,9 @@ describe manifest do
     set_rps          = Noop.hiera 'set_rps', true
     dpdk_config      = Noop.hiera_hash 'dpdk', {}
     enable_dpdk      = dpdk_config.fetch 'enabled', false
+    mgmt_vrouter_vip = Noop.hiera 'management_vrouter_vip'
+    roles            = Noop.hiera 'roles', []
+    mongo_roles      = ['primary-mongo', 'mongo']
 
     it { should contain_class('l23network').with('use_ovs' => use_neutron) }
     it { should contain_sysctl__value('net.ipv4.conf.all.arp_accept').with('value' => '1') }
@@ -86,6 +89,19 @@ describe manifest do
       it 'should skip dpdk-specific options for OVS' do
         should contain_class('l23network::l2::dpdk').with('use_dpdk' => false)
       end
+    end
+    if network_scheme['endpoints'].has_key?('br-ex')
+      it { should contain_l23network__l3__ifconfig('br-ex').with(
+        'gateway' => network_scheme['endpoints']['br-ex']['gateway']
+      )}
+    elsif !(roles & mongo_roles).empty?
+      it { should contain_l23network__l3__ifconfig('br-fw-admin').with(
+        'gateway' => network_scheme['endpoints']['br-fw-admin']['gateway']
+      )}
+    else
+      it { should contain_l23network__l3__ifconfig('br-mgmt').with(
+        'gateway' => mgmt_vrouter_vip
+      )}
     end
   end
 
