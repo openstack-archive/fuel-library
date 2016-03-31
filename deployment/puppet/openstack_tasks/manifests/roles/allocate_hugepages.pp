70a23a87d6b8326a1f3288f014345aeb9b763c31
slava <vvalyavskiy@mirantis.com>
2016-03-31 22:04:14 +0300

    Skip hugepages allocation if no HP info is present
    Nailgun doesn't include into deployment info hugepages
    allocation if 1GB hugepages are present. So, we should
    skip its configuration in this case.
    
    Change-Id: Ieaeeee3a0fe4ef23f662bec72571b855d3381c3e
    Closes-bug: #1560532

--- deployment/puppet/openstack_tasks/manifests/roles/allocate_hugepages.pp
+++ deployment/puppet/openstack_tasks/manifests/roles/allocate_hugepages.pp
@@ -4,20 +4,24 @@ class openstack_tasks::roles::allocate_hugepages {
 
   $hugepages = hiera('hugepages', [])
 
-  include ::sysfs
+  # TODO: (vvalyavskiy) currently, it was decided to not include 'hugepages' mapping data into
+  # deployment info if 1GB hugepages is enabled. So, it means that no hugepages count should
+  # be configured in runtime in this case.
+  unless empty($hugepages) {
+    include ::sysfs
 
-  sysfs_config_value { 'hugepages':
-    ensure => 'present',
-    name   => '/etc/sysfs.d/hugepages.conf',
-    value  => map_sysfs_hugepages($hugepages),
-    sysfs  => '/sys/devices/system/node/node*/hugepages/hugepages-*kB/nr_hugepages',
-    # TODO: (vvalyavskiy) should be wiped out as soon as kernel will be upgraded to 3.16 version
-    exclude => '/sys/devices/system/node/node*/hugepages/hugepages-1048576kB/nr_hugepages',
+    sysfs_config_value { 'hugepages':
+      ensure => 'present',
+      name   => '/etc/sysfs.d/hugepages.conf',
+      value  => map_sysfs_hugepages($hugepages),
+      sysfs  => '/sys/devices/system/node/node*/hugepages/hugepages-*kB/nr_hugepages',
+      # TODO: (vvalyavskiy) should be wiped out as soon as kernel will be upgraded to 3.16 version
+      exclude => '/sys/devices/system/node/node*/hugepages/hugepages-1048576kB/nr_hugepages',
+    }
+
+    # LP 1507921
+    sysctl::value { 'vm.max_map_count':
+      value  => max_map_count_hugepages($hugepages),
+    }
   }
-
-  # LP 1507921
-  sysctl::value { 'vm.max_map_count':
-    value  => max_map_count_hugepages($hugepages),
-  }
-
 }
