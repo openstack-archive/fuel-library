3705331deab0b8ffdf02684331519f0964311bda
Artem Panchenko <apanchenko@mirantis.com>
2016-04-04 16:47:03 +0300

    Fix logic for enabling 'swiftcheck' service
    Currently used logic for enabling swiftcheck
    service is not correct. Additional checks for Keystone
    availability from Swift node should be added if management
    VIP and Swift proxy address are from different L3 subnets.
    
    Change-Id: I9513fc9da02abdc24cc61a60c33181bb0fc9235b
    Related-bug: #1548275

---
+++ deployment/puppet/l23network/spec/functions/has_ip_in_network__spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'has_ip_in_network' do
+  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
+
+  it 'should exist' do
+    Puppet::Parser::Functions.function('has_ip_in_network').should == 'function_has_ip_in_network'
+  end
+
+  it 'should throw an error on invalid arguments number' do
+    lambda {
+      scope.function_has_ip_in_network(['foo'])
+    }.should(raise_error(ArgumentError))
+  end
+
+  it 'should raise an error if invalid address or network is specified' do
+    lambda {
+      scope.function_has_ip_in_network(['foo', 'bar'])
+    }.should(raise_error(ParseError))
+  end
+
+  it 'should return true if IP address is from CIDR' do
+    cidr = '192.168.0.0/16'
+    ipaddr = '192.168.33.66'
+    scope.function_has_ip_in_network([ipaddr, cidr]).should == true
+  end
+
+  it 'should return false if IP address is not from CIDR' do
+    cidr = '192.168.0.0/255.255.0.0'
+    ipaddr = '172.16.0.1'
+    scope.function_has_ip_in_network([ipaddr, cidr]).should == false
+  end
+
+end
