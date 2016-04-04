ab71cfbe9bc6a9b87af14fb56b418eb703bd522b
Vladimir Kuklin <vkuklin@mirantis.com>
2016-04-04 15:31:14 +0300

    [WIP] Fix support for changing settings for some of the services
    Supports:
      cinder
      glance
      mysql
      rabbitmq
      haproxy
    
    Not Yet Done:
      horizon
      apache
    
    Closes-bug: #1580522
    
    Change-Id: I2933846f78c94933419d4dfd3dfbe49178172ff9

--- deployment/puppet/openstack_tasks/manifests/openstack_cinder/openstack_cinder.pp
+++ deployment/puppet/openstack_tasks/manifests/openstack_cinder/openstack_cinder.pp
@@ -44,6 +44,21 @@ class openstack_tasks::openstack_cinder::openstack_cinder {
   })
 
   $queue_provider = hiera('queue_provider', 'rabbit')
+  $override_configuration = hiera_hash('configuration', {})
+
+  # override cinder.conf options
+  override_resources { 'cinder_config':
+    data => $override_configuration['cinder']
+  }
+
+  # override cinder api paste options
+  override_resources { 'cinder_api_paste_ini':
+    data => $override_configuration['cinder_api_paste_ini']
+  }
+
+  Override_resources <||> ~> Service <| tag == 'cinder-service' |>
+
+
 
   $keystone_auth_protocol = get_ssl_property($ssl_hash, {}, 'keystone', 'internal', 'protocol', 'http')
   $keystone_auth_host     = get_ssl_property($ssl_hash, {}, 'keystone', 'internal', 'hostname', [hiera('keystone_endpoint', ''), $service_endpoint, $management_vip])
