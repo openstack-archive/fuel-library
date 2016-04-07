e304ba3309c108218314be53ce3c3e7cb0401805
stewie925 <st6218@att.com>
2016-04-07 16:12:30 -0700

    switch ntp server vrouter namespace colocation constraints
    * Changed the ntp_ocf.pp file and cluster_ntp_ocf_spec.rb files
    * to switch the 'first' and 'second' parameter values
    
    Change-Id: I8c88ac59d8679590db8061c87d19a87870f9432f
    Closes-Bug: #1567646

--- deployment/puppet/cluster/spec/classes/cluster_ntp_ocf_spec.rb
+++ deployment/puppet/cluster/spec/classes/cluster_ntp_ocf_spec.rb
@@ -6,12 +6,12 @@ describe 'cluster::ntp_ocf' do
 
     it 'configures with the default params' do
       should contain_class('cluster::ntp_ocf')
-      should contain_pcmk_resource('p_ntp').with_before(["Pcmk_colocation[ntp-with-vrouter-ns]", "Service[ntp]"])
+      should contain_pcmk_resource('p_ntp').with_before(["Pcmk_colocation[ntp-with-vrouter-ns]", "Service[ntp]"]) 
       should contain_pcmk_colocation('ntp-with-vrouter-ns').with(
         :ensure => 'present',
         :score  => 'INFINITY',
-        :first  => 'clone_p_ntp',
-        :second => 'clone_p_vrouter')
+        :first  => 'clone_p_vrouter', 
+        :second => 'clone_p_ntp')
     end
   end
 
