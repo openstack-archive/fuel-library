7a2bc9e5f5a8c5dd8153c4f5de1ac65b8d91937d
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2016-04-07 17:36:35 +0200

    Remove docker from Library
    Change-Id: Ib7f16c105b8dec9e0cc3294bfb9a715581e1f1e8
    Closes-Bug: 1567495
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/docker/spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
-require 'shared_examples'
-
-RSpec.configure do |c|
-  c.alias_it_should_behave_like_to :it_configures, 'configures'
-  c.alias_it_should_behave_like_to :it_raises, 'raises'
-end
-
-at_exit { RSpec::Puppet::Coverage.report! }
