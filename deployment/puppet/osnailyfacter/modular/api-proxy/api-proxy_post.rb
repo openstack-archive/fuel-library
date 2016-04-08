6b43a189799f2482230f8df8f2baf156b91e8059
Dmitry Ilyin <dilyin@mirantis.com>
2016-04-08 15:58:11 +0300

    Remove pre/post test framework
    It's not used and most likely will never be
    it only garbages the repository
    
    Change-Id: Ic84dec528d13ee24836a8eabfa85cc13a4f62587
    Related-Bug: 1462997
    Related-Bug: 1525242

--- deployment/puppet/osnailyfacter/modular/api-proxy/api-proxy_post.rb
+++
@@ -1,8 +0,0 @@
-require File.join File.dirname(__FILE__), '../test_common.rb'
-
-class ApiProxyPostTest < Test::Unit::TestCase
-  def test_api_proxy_online
-    assert TestCommon::Network.connection?('localhost', 8888), 'Cannot connect to API proxy!'
-  end
-end
-
--- deployment/puppet/osnailyfacter/modular/firewall/firewall_pre.rb
+++
@@ -1,7 +0,0 @@
-require File.join File.dirname(__FILE__), '../test_common.rb'
-
-class FirewallPreTest < Test::Unit::TestCase
-  def test_iptables_installed
-    assert TestCommon::Process.command_present?('iptables'), 'Iptables is not installed!'
-  end
-end
