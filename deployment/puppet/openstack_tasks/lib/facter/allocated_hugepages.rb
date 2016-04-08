57e7d4561bc69c3f4be65327fb8773db90c8e839
Sergey Kolekonov <skolekonov@mirantis.com>
2016-04-08 16:11:47 +0300

    Fix simultaneous usage of 1Gb and 2Mb huge pages
    Currently it's impossible to use 1Gb and 2Mb huge pages on one compute node
    as a helper script from qemu package allows to use only one mount point
    for huge pages, so 1 Gb huge pages support has to be managed explicitly.
    
    This patch adds the following steps:
    - create a mount point for 1 Gb huge pages and mount pages in runtime
    - create a persistent record in /etc/fstab to mount huge pages on boot
    - add related settings for libvirt
    - update apparmor settings to allow an additional mount point
    
    Due to LP #1560532 Nailgun doesn't provide information about allocated
    huge pages when 1 Gb huge pages are used, so this information is computed
    in a custom fact.
    
    Closes-bug: #1566293
    
    Change-Id: Iba421abdc354afa7d89f6f10a94c6ba3edb99148
    (cherry picked from commit 10c3d88b55fbcd4427c4cb39690af9e0ae60175b)

---
+++ deployment/puppet/openstack_tasks/lib/facter/allocated_hugepages.rb
@@ -0,0 +1,18 @@
+# This fact returns a hash with numbers of allocated 1G and 2M huge pages
+# Currently Nailgun doesn't provide this informaion due to LP #1560532
+
+hugepages = {"1G"=>false, "2M"=>false}
+
+hps_path = {
+  '1G' => '/sys/kernel/mm/hugepages/hugepages-1048576kB/nr_hugepages',
+  '2M' => '/sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages'
+}
+hps_path.each do |hp, path|
+  hugepages[hp] = File.read(path).to_i != 0 rescue false
+end
+
+Facter.add(:allocated_hugepages) do
+  setcode do
+    hugepages
+  end
+end
