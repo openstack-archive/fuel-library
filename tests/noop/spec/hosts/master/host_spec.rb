690f7de082f616b1cb5e1cf1104ca30f88dfa733
Maksim Malchuk <mmalchuk@mirantis.com>
2016-04-08 11:48:59 +0300

    Set default iptables INPUT policy to DROP
    It was observed that on Fuel master node the default firewall INPUT
    policy is set to 'ACCEPT' rather than to 'DROP'.
    This leads to exposure of unnecessary services over a potentially
    untrusted networks.
    
    This patch updates default firewall INPUT policy to 'DROP'. Also it adds
    user-defined chains which are not purged (and thus preserved between
    iptables.pp applications).
    
    Change-Id: Ia9ab6d019be81aebcf5eaba25336e6f19b2c6a1a
    Partial-Bug: #1524750
    Depends-On: I57e9f58c6bad32b23b179499f0514edf5357bd31

--- tests/noop/spec/hosts/master/host_spec.rb
+++ tests/noop/spec/hosts/master/host_spec.rb
@@ -84,6 +84,84 @@ describe manifest do
       is_expected.to contain_class('fuel::iptables').with parameters
     end
 
+    it 'should contain correct firewall rules' do
+
+      it 'should set firewall chains to DROP by default' do
+        should contain_firewallchain('INPUT:filter:IPv4').with(
+          :policy => 'drop',
+          :purge  => true,
+        )
+        should contain_firewallchain('FORWARD:filter:IPv4').with(
+          :policy => 'drop',
+          :purge  => true,
+        )
+        should contain_firewallchain('POSTROUTING:nat:IPv4').with(
+          :policy => 'drop',
+          :purge  => true,
+        )
+        should contain_firewallchain('POSTROUTING:mangle:IPv4').with(
+          :policy => 'drop',
+          :purge  => true,
+        )
+      end
+
+      it "shouldn't purge the custom external filters" do
+        should contain_firewallchain('ext-filter-input:filter:IPv4').with(
+          :purge => false,
+        )
+        should contain_firewallchain('ext-filter-forward:filter:IPv4').with(
+          :purge => false,
+        )
+        should contain_firewallchain('ext-nat-postrouting:nat:IPv4').with(
+          :purge => false,
+        )
+        should contain_firewallchain('ext-mangle-postrouting:mangle:IPv4').with(
+          :purge => false,
+        )
+      end
+
+      it 'should contain the firewall rules' do
+        should contain_firewall('000 allow loopback')
+        should contain_firewall('010 ssh')
+        should contain_firewall('020 ntp')
+        should contain_firewall('030 ntp_udp')
+        should contain_firewall('040 snmp')
+        should contain_firewall('050 nailgun_web')
+        should contain_firewall('060 nailgun_internal')
+        should contain_firewall('070 nailgun_internal_block_ext')
+        should contain_firewall('080 postgres_local')
+        should contain_firewall('090 postgres')
+        should contain_firewall('100 postgres_block_ext')
+        should contain_firewall('110 ostf_admin')
+        should contain_firewall('120 ostf_local')
+        should contain_firewall('130 ostf_block_ext')
+        should contain_firewall('140 rsync')
+        should contain_firewall('150 rsyslog')
+        should contain_firewall('160 rsyslog')
+        should contain_firewall('170 rabbitmq_admin_net')
+        should contain_firewall('180 rabbitmq_local')
+        should contain_firewall('190 rabbitmq_block_ext')
+        should contain_firewall('200 fuelweb_port')
+        should contain_firewall('210 keystone_admin')
+        should contain_firewall('220 keystone_admin_port admin_net')
+        should contain_firewall('230 nailgun_repo_admin')
+        should contain_firewall('240 allow icmp echo-request')
+        should contain_firewall('250 allow icmp echo-reply')
+        should contain_firewall('260 allow icmp dest-unreach')
+        should contain_firewall('270 allow icmp time-exceeded')
+        should contain_firewall('970 externally defined rules: ext-filter-input')
+        should contain_firewall('980 accept related established rules')
+        should contain_firewall('999 iptables denied')
+        should contain_firewall('010 forward admin_net')
+        should contain_firewall('970 externally defined rules')
+        should contain_firewall('980 forward admin_net conntrack')
+        should contain_firewall('010 forward_admin_net')
+        should contain_firewall('980 externally defined rules: ext-nat-postrouting')
+        should contain_firewall('010 recalculate dhcp checksum')
+        should contain_firewall('980 externally defined rules: ext-mangle-postrouting')
+      end
+    end
+
     it 'should declare "openstack::clocksync" class with parameters' do
       parameters = {
           :ntp_servers     => [fuel_settings['NTP1'], fuel_settings['NTP2'], fuel_settings['NTP3'], ''].reject {|v| v.to_s.empty? },
