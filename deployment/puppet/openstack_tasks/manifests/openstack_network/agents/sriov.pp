949c2e795e2099f9571be608d4f98ad0abd094ca
Sergey Kolekonov <skolekonov@mirantis.com>
2016-04-12 21:50:02 +0300

    Pass extensions to Neutron SRIOV agent
    In order to use QoS for SR-IOV ports qos extension should be enabled for
    sriov agent
    
    Change-Id: Ice9fcfe8d6ed81ba2ae8e0cb6963f20ded5f8758
    Closes-bug: #1563820

--- deployment/puppet/openstack_tasks/manifests/openstack_network/agents/sriov.pp
+++ deployment/puppet/openstack_tasks/manifests/openstack_network/agents/sriov.pp
@@ -2,9 +2,11 @@ class openstack_tasks::openstack_network::agents::sriov {
 
   notice('MODULAR: openstack_network/agents/sriov.pp')
 
-  $use_neutron     = hiera('use_neutron', false)
-  $network_scheme  = hiera_hash('network_scheme', {})
-  $neutron_config  = hiera_hash('neutron_config')
+  $use_neutron             = hiera('use_neutron', false)
+  $network_scheme          = hiera_hash('network_scheme', {})
+  $neutron_config          = hiera_hash('neutron_config')
+  $neutron_advanced_config = hiera_hash('neutron_advanced_configuration', {})
+  $enable_qos              = pick($neutron_advanced_config['neutron_qos'], false)
 
   prepare_network_config($network_scheme)
   $pci_passthrough_whitelist = get_nic_passthrough_whitelist('sriov')
@@ -14,6 +16,7 @@ class openstack_tasks::openstack_network::agents::sriov {
 
     class { '::neutron::agents::ml2::sriov':
       physical_device_mappings => $physical_device_mappings,
+      extensions               => $enable_qos ? { true => ['qos'], default => ''},
       manage_service           => true,
       enabled                  => true,
     }
