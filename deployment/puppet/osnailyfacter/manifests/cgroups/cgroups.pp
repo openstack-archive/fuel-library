9ba20fd0a3f67e1a558fb152b1a752fad0bb3211
Michael Polenchuk <mpolenchuk@mirantis.com>
2016-04-20 12:23:07 +0300

    Avoid using cgroups with no settings
    Check parsed cgroups settings (which exclude
    service "metadata" key) instead of raw ones.
    
    Change-Id: I4ffec8c692e6ffbfc3fba8f04c7870ada531b47a
    Closes-Bug: #1571681

--- deployment/puppet/osnailyfacter/manifests/cgroups/cgroups.pp
+++ deployment/puppet/osnailyfacter/manifests/cgroups/cgroups.pp
@@ -3,10 +3,10 @@ class osnailyfacter::cgroups::cgroups {
   notice('MODULAR: cgroups/cgroups.pp')
 
   $cgroups_config = hiera('cgroups', {})
+  $cgroups_set = prepare_cgroups_hash($cgroups_config)
 
   # Task should not be failed if configuration was not propagated
-  unless empty($cgroups_config) {
-    $cgroups_set = prepare_cgroups_hash($cgroups_config)
+  unless empty($cgroups_set) {
     class { '::cgroups':
       cgroups_set => $cgroups_set,
     }
