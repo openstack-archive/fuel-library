c923fb21ef584e2e09bd4c514f01872d3874abac
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-04-22 12:43:04 +0300

    Purge deleted /etc/hosts entries for deleted ndoes
    Deleted nodes now will get stored in a file
    /etc/hiera/deleted_nodes.yaml and be inserted into
    the hiera hierarchy.
    
    host entries for any records present in deleted_nodes.yaml
    will be purged when evaluating hosts task.
    
    The source YAML for deleted nodes is generated on master
    because nodes that are deployed from scratch or from Error
    state will not be able to identify deleted nodes due to
    certain orchestration limitations.
    
    Change-Id: I01db215e77a3532a6fa7bf46ab7e20e281e8c165
    Closes-Bug: #1513401
    (cherry picked from commit eacb493e38a31747dc61a43a081adf00adbeb8eb)

--- deployment/puppet/osnailyfacter/manifests/hosts/hosts.pp
+++ deployment/puppet/osnailyfacter/manifests/hosts/hosts.pp
@@ -4,14 +4,20 @@ class osnailyfacter::hosts::hosts {
 
   $hosts_file = '/etc/hosts'
   $network_metadata = hiera_hash('network_metadata')
+  $messaging_prefix = hiera('node_name_prefix_for_messaging')
   $host_resources = network_metadata_to_hosts($network_metadata)
-  $messaging_host_resources = network_metadata_to_hosts($network_metadata, 'mgmt/messaging', hiera('node_name_prefix_for_messaging'))
+  $messaging_host_resources = network_metadata_to_hosts($network_metadata, 'mgmt/messaging', $messaging_prefix)
+  $host_hash = merge($host_resources, $messaging_host_resources)
+
+  $deleted_nodes = hiera('deleted_nodes', [])
+  $deleted_messaging_nodes = prefix($deleted_nodes, $messaging_prefix)
 
   Host {
-      ensure => present,
       target => $hosts_file
   }
 
-  create_resources(host, merge($host_resources, $messaging_host_resources))
-
+  create_resources(host, $host_hash)
+  if !empty($deleted_nodes) {
+    create_resources(host, concat($deleted_nodes, $deleted_messaging_nodes), {ensure => absent})
+  }
 }
