5479b630c023ff3a34113daa3b5f68a78be0d415
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-04-22 12:57:11 +0300

    Purge nova services for deleted nodes
    Deleted hosts create downed services in nova, which should
    be cleaned up on the next deploy cycle.
    
    Change-Id: Ie644062c9befec1745adcc8ca9e76a895cf0304c
    Depends-On: I01db215e77a3532a6fa7bf46ab7e20e281e8c165
    Depends-On: I303ab218bc6a48cf2c60727feecc522040a80a68
    Partial-Bug: #1471172

---
+++ tests/noop/spec/hosts/astute/purge_service_entries_spec.rb
@@ -0,0 +1,22 @@
+# ROLE: primary-controller
+# ROLE: controller
+
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'astute/purge_service_entries.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+
+    deleted_nodes = Noop.hiera('deleted_nodes', [])
+
+    if !deleted_nodes.empty?
+      it 'should purge deleted nodes' do
+        deleted_nodes.each do |deleted|
+          is_expected.to contain_nova_service(deleted).with_ensure('absent')
+        end
+      end
+    end
+  end
+  test_ubuntu_and_centos manifest
+end
