049d584ca6e17bf0660c3fcfb290409798cc6def
Maciej Relewicz <mrelewicz@mirantis.com>
2016-04-26 19:59:21 +0200

    Fix task 'primary-swift-proxy' idempotency.
    Due to issue in concat module, which is used by puppet rsync module, we
    have to merge swift/proxy.pp and swift/storage.pp tasks. Concat module
    have purge set by default to true. So each call rsync::server::module
    cleans settings set by previous call. In our case call in storage.pp
    cleans settings set call in proxy.pp. As workaround we need to merge all
    calls related with rsync module into one task.
    
    Change-Id: I49e946144df2f9913320fd996663f89926096907
    Closes-Bug: #1572789

--- deployment/puppet/openstack_tasks/manifests/swift/proxy_storage.pp
+++ deployment/puppet/openstack_tasks/manifests/swift/proxy_storage.pp
@@ -1,6 +1,6 @@
-class openstack_tasks::swift::proxy {
+class openstack_tasks::swift::proxy_storage {
 
-  notice('MODULAR: swift/proxy.pp')
+  notice('MODULAR: swift/proxy_storage.pp')
 
   $network_scheme             = hiera_hash('network_scheme', {})
   $network_metadata           = hiera_hash('network_metadata', {})
@@ -34,6 +34,9 @@ class openstack_tasks::swift::proxy {
   $ssl_hash                   = hiera_hash('use_ssl', {})
   $rabbit_hash                = hiera_hash('rabbit')
   $rabbit_hosts               = hiera('amqp_hosts')
+#storage settings
+  $mp_hash                    = hiera('mp')
+  $deploy_swift_storage       = hiera('deploy_swift_storage', true)
 
   $internal_auth_protocol = get_ssl_property($ssl_hash, {}, 'keystone', 'internal', 'protocol', [pick($swift_hash['auth_protocol'], 'http')])
   $internal_auth_address  = get_ssl_property($ssl_hash, {}, 'keystone', 'internal', 'hostname', [hiera('service_endpoint', ''), $management_vip])
@@ -54,6 +57,7 @@ class openstack_tasks::swift::proxy {
     $master_swift_proxy_nodes_list = values($master_swift_proxy_nodes)
     $master_swift_proxy_ip         = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/api'], '\/\d+$', '')
     $master_swift_replication_ip   = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/replication'], '\/\d+$', '')
+    $swift_partition               = hiera('swift_partition', '/var/lib/glance/node')
 
     if $is_primary_swift_proxy {
       ring_devices {'all':
@@ -100,19 +104,7 @@ class openstack_tasks::swift::proxy {
         rabbit_hosts                   => split($rabbit_hosts, ', '),
       }
 
-      # Check swift proxy and internal VIP are from the same IP network. If no
-      # then it's possible to get network failure, so proxy couldn't access
-      # Keystone via VIP. In such cases swift health check returns OK, but all
-      # requests forwarded from HAproxy fail, see LP#1459772 In order to detect
-      # such bad swift backends we enable a service which checks Keystone
-      # availability from swift node. HAProxy monitors that service to get
-      # proper backend status.
-      # NOTE: this is the same logic in the HAproxy configuration so if it's
-      # updated there, this must be updated. See LP#1548275
-      $swift_api_network = get_network_role_property('swift/api', 'network')
-      $bind_to_one       = has_ip_in_network($management_vip, $swift_api_network)
-
-      if !$bind_to_one {
+      if $swift_api_ipaddr == $swift_storage_ipaddr {
         $storage_nets = get_routable_networks_for_network_role($network_scheme, 'swift/replication', ' ')
         $mgmt_nets = get_routable_networks_for_network_role($network_scheme, 'swift/api', ' ')
 
@@ -136,6 +128,38 @@ class openstack_tasks::swift::proxy {
 
       Class['openstack::swift::proxy'] -> Class['swift::dispersion']
       Service<| tag == 'swift-service' |> -> Class['swift::dispersion']
+    }
+
+    if ($deploy_swift_storage){
+      if !defined(File['/var/lib/glance']) {
+        file {'/var/lib/glance':
+          ensure  => 'directory',
+          group   => 'swift',
+          require => Package['swift'],
+        } -> Service <| tag == 'swift-service' |>
+      } else {
+        File['/var/lib/glance'] {
+          ensure  => 'directory',
+          group   => 'swift',
+          require +> Package['swift'],
+        }
+        File['/var/lib/glance'] -> Service <| tag == 'swift-service' |>
+      }
+
+      class { 'openstack::swift::storage_node':
+        storage_type                => false,
+        loopback_size               => '5243780',
+        storage_mnt_base_dir        => $swift_partition,
+        storage_devices             => filter_hash($mp_hash,'point'),
+        swift_zone                  => $master_swift_proxy_nodes_list[0]['swift_zone'],
+        swift_local_net_ip          => $swift_storage_ipaddr,
+        master_swift_proxy_ip       => $master_swift_proxy_ip,
+        master_swift_replication_ip => $master_swift_replication_ip,
+        sync_rings                  => ! $is_primary_swift_proxy,
+        debug                       => $debug,
+        verbose                     => $verbose,
+        log_facility                => 'LOG_SYSLOG',
+      }
     }
   }
 
@@ -247,6 +271,14 @@ class openstack_tasks::swift::proxy {
         swift_hash_suffix => $swift_hash_suffix,
         package_ensure    => $package_ensure,
         max_header_size   => $swift_max_header_size,
+      }
+    }
+
+    if !defined(Class['rsync::server']) {
+      class { '::rsync::server':
+      use_xinetd => true,
+      address    => $local_net_ip,
+      use_chroot => 'no',
       }
     }
 
@@ -394,6 +426,140 @@ class openstack_tasks::swift::proxy {
       }
 
       Swift::Ringsync <| |> ~> Service['swift-proxy-server']
+    }
+  }
+
+  # FIXME(bogdando) requires decomposition and unit tests
+  class openstack::swift::storage_node (
+    $swift_zone,
+    $swift_hash_suffix          = 'swift_secret',
+    $swift_max_header_size      = '32768',
+    $swift_local_net_ip         = $::ipaddress_eth0,
+    $storage_type               = 'loopback',
+    $storage_base_dir           = '/srv/loopback-device',
+    $storage_mnt_base_dir       = '/srv/node',
+    $storage_devices            = [
+      '1',
+      '2'],
+    $storage_weight             = 1,
+    $package_ensure             = 'present',
+    $loopback_size              = '1048756',
+    $master_swift_proxy_ip,
+    $master_swift_replication_ip,
+    $rings                      = [
+      'account',
+      'object',
+      'container'],
+    $sync_rings                 = true,
+    $incoming_chmod             = 'Du=rwx,g=rx,o=rx,Fu=rw,g=r,o=r',
+    $outgoing_chmod             = 'Du=rwx,g=rx,o=rx,Fu=rw,g=r,o=r',
+    # if the cinder management components should be installed
+    $cinder                     = true,
+    $manage_volumes             = false,
+    $nv_physical_volume         = undef,
+    $cinder_volume_group        = 'cinder-volumes',
+    $cinder_user_password       = 'cinder_user_pass',
+    $cinder_db_password         = 'cinder_db_pass',
+    $cinder_db_user             = 'cinder',
+    $cinder_db_dbname           = 'cinder',
+    $cinder_iscsi_bind_addr     = false,
+    $cinder_rate_limits         = false,
+    $db_host                    = '127.0.0.1',
+    $service_endpoint           = '127.0.0.1',
+    $use_syslog                 = false,
+    $syslog_log_facility_cinder = 'LOG_LOCAL3',
+    $debug                      = false,
+    $verbose                    = true,
+    # Rabbit details necessary for cinder
+    $rabbit_nodes               = false,
+    $rabbit_password            = 'rabbit_pw',
+    $rabbit_host                = false,
+    $rabbit_user                = 'nova',
+    $rabbit_ha_virtual_ip       = false,
+    $queue_provider             = 'rabbitmq',
+    $qpid_password              = 'qpid_pw',
+    $qpid_user                  = 'nova',
+    $qpid_nodes                 = ['127.0.0.1'],
+    $log_facility               = 'LOG_LOCAL2',
+    ) {
+    if !defined(Class['swift']) {
+      class { 'swift':
+        swift_hash_suffix => $swift_hash_suffix,
+        package_ensure    => $package_ensure,
+        max_header_size   => $swift_max_header_size,
+      }
+    }
+
+    if $storage_type == 'loopback' {
+      # create xfs partitions on a loopback device and mount them
+      swift::storage::loopback { $storage_devices:
+        base_dir     => $storage_base_dir,
+        mnt_base_dir => $storage_mnt_base_dir,
+        seek         => $loopback_size,
+        require      => Class['swift'],
+      }
+    }
+
+    # create dirs for devices
+    define device_directory($devices) {
+      if(!defined(File[$devices])) {
+        file { $devices:
+          ensure       => 'directory',
+          owner        => 'swift',
+          group        => 'swift',
+          recurse      => true,
+          recurselimit => 1,
+        }
+      }
+    }
+    if ($storage_devices != undef) {
+      anchor {'swift-device-directories-start': } ->
+      device_directory { $storage_devices:
+        devices => $storage_mnt_base_dir,
+      }
+    }
+
+    # install all swift storage servers together
+    class { 'swift::storage::all':
+      storage_local_net_ip => $swift_local_net_ip,
+      devices              => $storage_mnt_base_dir,
+      log_facility         => $log_facility,
+      # We use directory for swift
+      mount_check          => false,
+    }
+    # override log_name defaults for Swift::Storage::Server
+    # TODO (adidenko) move this into Hiera when it's ready
+    Swift::Storage::Server <| title == '6000' |> {
+      log_name => 'swift-object-server',
+    }
+    Swift::Storage::Server <| title == '6001' |> {
+      log_name       => 'swift-container-server',
+      allow_versions => true,
+    }
+    Swift::Storage::Server <| title == '6002' |> {
+      log_name => 'swift-account-server',
+    }
+
+    Swift::Storage::Server <| |> {
+      incoming_chmod => $incoming_chmod,
+      outgoing_chmod => $outgoing_chmod,
+    }
+
+    validate_string($master_swift_replication_ip)
+
+    if $sync_rings {
+      if member($rings, 'account') and !defined(Swift::Ringsync['account']) {
+        swift::ringsync { 'account': ring_server => $master_swift_replication_ip }
+      }
+
+      if member($rings, 'object') and !defined(Swift::Ringsync['object']) {
+        swift::ringsync { 'object': ring_server => $master_swift_replication_ip }
+      }
+
+      if member($rings, 'container') and !defined(Swift::Ringsync['container']) {
+        swift::ringsync { 'container': ring_server => $master_swift_replication_ip }
+      }
+      Swift::Ringsync <| |> ~> Class["swift::storage::all"]
     }
   }
 }
