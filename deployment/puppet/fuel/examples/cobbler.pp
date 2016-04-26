58feb78920692fa728b077a3add79561af8b74b6
Maksim Malchuk <mmalchuk@mirantis.com>
2016-04-26 15:05:08 +0300

    Load metadata.yaml only if file exist
    Change-Id: Ife6c633f6c9a28ab2bef27651ed6fb0772581040
    Closes-Bug: #1574676
    Signed-off-by: Maksim Malchuk <mmalchuk@mirantis.com>

--- deployment/puppet/fuel/examples/cobbler.pp
+++ deployment/puppet/fuel/examples/cobbler.pp
@@ -7,9 +7,13 @@ $admin_network               = $::fuel_settings['ADMIN_NETWORK']
 $nailgun_api_url             = "http://${::fuel_settings['ADMIN_NETWORK']['ipaddress']}:8000/api"
 $bootstrap_settings          = pick($::fuel_settings['BOOTSTRAP'], {})
 $bootstrap_path              = pick($bootstrap_settings['path'], '/var/www/nailgun/bootstraps/active_bootstrap')
-$bootstrap_meta              = pick(loadyaml("${bootstrap_path}/metadata.yaml"), {})
+$bootstrap_meta              = {}
 $bootstrap_ethdevice_timeout = pick($bootstrap_settings['ethdevice_timeout'], '120')
 $dhcp_gw                     = $::fuel_settings['ADMIN_NETWORK']['dhcp_gateway']
+
+if !empty(file("${bootstrap_path}/metadata.yaml", '/dev/null')) {
+  $bootstrap_meta = loadyaml("${bootstrap_path}/metadata.yaml")
+}
 
 if $dhcp_gw {
   $dhcp_gateway = $dhcp_gw
