8f607eef918ce14235766c1b784ee48dd560a9c6
Oleksiy Molchanov <omolchanov@mirantis.com>
2016-04-26 12:46:52 +0300

    Fix ceph yaqls
    * Ceph yaqls update
    * Remove unneeded predeployment ceph-keys sharing
    * Remove non-idempotent osd-activate service
    * Remove unneeded quorum check and ceph reload
    * Small code style changes
    * Added cross-dependencies for osd and mon
    
    Related-Blueprint: fuel-upstream-ceph
    Change-Id: I044000f231c4d8a7534ac806069e51b4e6995406

--- deployment/puppet/osnailyfacter/manifests/ceph/ceph_pools.pp
+++ deployment/puppet/osnailyfacter/manifests/ceph/ceph_pools.pp
@@ -12,93 +12,99 @@ class osnailyfacter::ceph::ceph_pools {
   $glance_user        = 'images'
   $glance_pool        = 'images'
 
-  if empty($mon_key) {
-    fail('Please provide mon_key')
-  }
-  if empty($fsid) {
-    fail('Please provide fsid')
-  }
-
-  class {'ceph':
-    fsid => $fsid
-  }
-
   $per_pool_pg_nums = $storage_hash['per_pool_pg_nums']
 
+  if ($storage_hash['volumes_ceph'] or
+      $storage_hash['images_ceph'] or
+      $storage_hash['objects_ceph'] or
+      $storage_hash['ephemeral_ceph']
+  ) {
+
+    if empty($mon_key) {
+      fail('Please provide mon_key')
+    }
+    if empty($fsid) {
+      fail('Please provide fsid')
+    }
+
+    class {'ceph':
+      fsid => $fsid
+    }
+
 # DO NOT SPLIT ceph auth command lines! See http://tracker.ceph.com/issues/3279
-  ceph::pool { $glance_pool:
-    pg_num  => pick($per_pool_pg_nums[$glance_pool], '256'),
-    pgp_num => pick($per_pool_pg_nums[$glance_pool], '256'),
-  }
-
-  ceph::key { "client.${glance_user}":
-    secret  => $mon_key,
-    user    => 'glance',
-    group   => 'glance',
-    cap_mon => 'allow r',
-    cap_osd => "allow class-read object_prefix rbd_children, allow rwx pool=${glance_pool}",
-    inject  => true,
-  }
-
-  ceph::pool { $cinder_pool:
-    pg_num  => pick($per_pool_pg_nums[$cinder_pool], '256'),
-    pgp_num => pick($per_pool_pg_nums[$cinder_pool], '256'),
-  }
-
-  ceph::key { "client.${cinder_user}":
-    secret  => $mon_key,
-    user    => 'cinder',
-    group   => 'cinder',
-    cap_mon => 'allow r',
-    cap_osd => "allow class-read object_prefix rbd_children, allow rwx pool=${cinder_pool}, allow rx pool=${glance_pool}",
-    inject  => true,
-  }
-
-  ceph::pool { $cinder_backup_pool:
-    pg_num  => pick($per_pool_pg_nums[$cinder_backup_pool], '256'),
-    pgp_num => pick($per_pool_pg_nums[$cinder_backup_pool], '256'),
-  }
-
-  ceph::key { "client.${cinder_backup_user}":
-    secret  => $mon_key,
-    user    => 'cinder',
-    group   => 'cinder',
-    cap_mon => 'allow r',
-    cap_osd => "allow class-read object_prefix rbd_children, allow rwx pool=${cinder_backup_pool}, allow rwx pool=${cinder_pool}",
-    inject  => true,
-  }
-
-  if ($storage_hash['volumes_ceph']) {
-    include ::cinder::params
-    service { 'cinder-volume':
-      ensure     => 'running',
-      name       => $::cinder::params::volume_service,
-      hasstatus  => true,
-      hasrestart => true,
+    ceph::pool { $glance_pool:
+      pg_num  => pick($per_pool_pg_nums[$glance_pool], '256'),
+      pgp_num => pick($per_pool_pg_nums[$glance_pool], '256'),
     }
 
-    Ceph::Pool[$cinder_pool] ~> Service['cinder-volume']
-
-    service { 'cinder-backup':
-      ensure     => 'running',
-      name       => $::cinder::params::backup_service,
-      hasstatus  => true,
-      hasrestart => true,
+    ceph::key { "client.${glance_user}":
+      secret  => $mon_key,
+      user    => 'glance',
+      group   => 'glance',
+      cap_mon => 'allow r',
+      cap_osd => "allow class-read object_prefix rbd_children, allow rwx pool=${glance_pool}",
+      inject  => true,
     }
 
-    Ceph::Pool[$cinder_backup_pool] ~> Service['cinder-backup']
-  }
-
-  if ($storage_hash['images_ceph']) {
-    include ::glance::params
-    service { 'glance-api':
-      ensure     => 'running',
-      name       => $::glance::params::api_service_name,
-      hasstatus  => true,
-      hasrestart => true,
+    ceph::pool { $cinder_pool:
+      pg_num  => pick($per_pool_pg_nums[$cinder_pool], '256'),
+      pgp_num => pick($per_pool_pg_nums[$cinder_pool], '256'),
     }
 
-    Ceph::Pool[$glance_pool] ~> Service['glance-api']
-  }
+    ceph::key { "client.${cinder_user}":
+      secret  => $mon_key,
+      user    => 'cinder',
+      group   => 'cinder',
+      cap_mon => 'allow r',
+      cap_osd => "allow class-read object_prefix rbd_children, allow rwx pool=${cinder_pool}, allow rx pool=${glance_pool}",
+      inject  => true,
+    }
 
+    ceph::pool { $cinder_backup_pool:
+      pg_num  => pick($per_pool_pg_nums[$cinder_backup_pool], '256'),
+      pgp_num => pick($per_pool_pg_nums[$cinder_backup_pool], '256'),
+    }
+
+    ceph::key { "client.${cinder_backup_user}":
+      secret  => $mon_key,
+      user    => 'cinder',
+      group   => 'cinder',
+      cap_mon => 'allow r',
+      cap_osd => "allow class-read object_prefix rbd_children, allow rwx pool=${cinder_backup_pool}, allow rwx pool=${cinder_pool}",
+      inject  => true,
+    }
+
+    if ($storage_hash['volumes_ceph']) {
+      include ::cinder::params
+      service { 'cinder-volume':
+        ensure     => 'running',
+        name       => $::cinder::params::volume_service,
+        hasstatus  => true,
+        hasrestart => true,
+      }
+
+      Ceph::Pool[$cinder_pool] ~> Service['cinder-volume']
+
+      service { 'cinder-backup':
+        ensure     => 'running',
+        name       => $::cinder::params::backup_service,
+        hasstatus  => true,
+        hasrestart => true,
+      }
+
+      Ceph::Pool[$cinder_backup_pool] ~> Service['cinder-backup']
+    }
+
+    if ($storage_hash['images_ceph']) {
+      include ::glance::params
+      service { 'glance-api':
+        ensure     => 'running',
+        name       => $::glance::params::api_service_name,
+        hasstatus  => true,
+        hasrestart => true,
+      }
+
+      Ceph::Pool[$glance_pool] ~> Service['glance-api']
+    }
+  }
 }
