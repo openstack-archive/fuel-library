f7aaa3a110ac779e668656f09ee1dfa0cbd860ee
Dmitry Ilyin <dilyin@mirantis.com>
2016-04-26 12:15:37 -0500

    No more use loadall() for default_profider_for() function
    Change-Id: Id16605cc834e0071328bec667fcc27502a2c0f30
    Closes-Bug: #1575269
    (cherry picked from commit b82c12e9f36e8c23f8401eb9bd3c1f4ed9f07bfa)

--- deployment/puppet/l23network/lib/puppet/parser/functions/default_provider_for.rb
+++ deployment/puppet/l23network/lib/puppet/parser/functions/default_provider_for.rb
@@ -1,19 +1,17 @@
-
 module Puppet::Parser::Functions
   newfunction(:default_provider_for, :type => :rvalue, :doc => <<-EOS
   Get the default provider of a type
   EOS
   ) do |argv|
     type_name = argv[0]
-    fail('No type name provided!') if ! type_name
-    Puppet::Type.loadall()
-    type_name = type_name.capitalize.to_sym
-    return 'undef' if ! Puppet::Type.const_defined? type_name
-    type = Puppet::Type.const_get type_name
+    fail 'No type name provided!' unless type_name
+    type_name = type_name.to_s.downcase.to_sym
+    type = Puppet::Type.type type_name
+    return nil unless type
     provider = type.defaultprovider
-    return 'undef' if ! provider
-    rv = provider.name.to_s
-    debug("Default provider for type '#{type_name}' is a '#{rv}'.")
-    return rv
+    return nil unless provider
+    provider_name = provider.name.to_s
+    debug "default_provider_for() default provider for type '#{type_name}' is a '#{provider_name}'"
+    return provider_name
   end
 end
