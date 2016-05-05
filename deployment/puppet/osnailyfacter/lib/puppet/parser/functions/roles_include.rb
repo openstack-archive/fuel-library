1dde2da3f77cacb2ad36461e80e8d91db310d62b
Dmitry Ilyin <dilyin@mirantis.com>
2016-05-05 16:33:38 +0300

    Puppet4 Support: library
    Change-Id: I423e3e8b8995cc3a09b470089f4bc0b363b01398

--- deployment/puppet/osnailyfacter/lib/puppet/parser/functions/roles_include.rb
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/roles_include.rb
@@ -10,8 +10,13 @@ EOS
     raise Puppet::ParseError, 'Only one argument with role or array of roles should be provided!' if args.size != 1
     intended_roles = args.first
     intended_roles = [intended_roles] unless intended_roles.is_a? Array
-    network_metadata = function_hiera_hash ['network_metadata']
-    node_name = function_get_node_key_name []
+    if Puppet.version.to_f >= 4.0
+      network_metadata = call_function 'hiera_hash', 'network_metadata'
+    else
+      network_metadata = function_hiera_hash ['network_metadata']
+    end
+    Puppet::Parser::Functions.autoloader.load :get_node_key_name unless Puppet::Parser::Functions.autoloader.loaded? :get_node_key_name
+    node_name = function_get_node_key_name([])
     node_roles = network_metadata.fetch('nodes', {}).fetch(node_name, {}).fetch('node_roles', [])
 
     (node_roles & intended_roles).any?
