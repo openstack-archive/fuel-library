1a402fd7d4b630b29a87c65bc9ea19a0be1e5542
Dmitry Ilyin <dilyin@mirantis.com>
2016-05-05 21:08:27 +0300

    Generate vips: puppet4 and latest 3.8 support
    The generate_vips function is refactored to work in pair
    with the create_resources functions. It's much better for testing
    and debugging.
    Input data of the vurual ip definition is split into many parameters
    and the generate vips function is tought to support the changed
    interfacs as well as to work with Puppet4's undef values.
    
    
    Change-Id: I317dc6f889074fe89f1780b8edb840c32fa860c9

--- deployment/puppet/cluster/manifests/virtual_ip.pp
+++ deployment/puppet/cluster/manifests/virtual_ip.pp
@@ -2,70 +2,36 @@
 #
 # Configure VirtualIP resource for corosync/pacemaker.
 #
-# === Parameters
-#
-# [*key*]
-#   Name of virtual IP resource
-#
-# [*vip*]
-#   Specify dictionary of VIP parameters, ex:
-#   {
-#       bridge => 'br0',
-#       ip     => '10.1.1.253'
-#   }
-#
 define cluster::virtual_ip (
-  $vip,
-  $key = $name,
+  $bridge,
+  $ip,
+  $ns_veth,
+  $gateway = 'none',
+  $base_veth = undef,
+  $iflabel = 'ka',
+  $cidr_netmask = '24',
+  $ns = 'haproxy',
+  $gateway_metric = undef,
+  $other_networks = undef,
+  $iptables_comment = undef,
+  $ns_iptables_start_rules = undef,
+  $ns_iptables_stop_rules = undef,
+  $also_check_interfaces = undef,
+  $primitive_type = 'ns_IPaddr2',
+  $use_pcmk_prefix = false,
+  $vip_prefix = 'vip__',
+  $additional_parameters = {},
+  $colocation_before = undef,
+  $colocation_after = undef,
+  $colocation_score = 'INFINITY',
+  $colocation_ensure = 'present',
+  $colocation_separator = '-with-',
 ){
+  validate_string($primitive_type)
+  validate_bool($use_pcmk_prefix)
+  validate_hash($additional_parameters)
 
-  $vip_name = "vip__${key}"
-
-  if (is_ip_address($vip['gateway']) or ($vip['gateway'] == 'link')) {
-    $gateway = $vip['gateway']
-  } else {
-    $gateway = 'none'
-  }
-
-  $parameters = {
-    'gateway'              => $gateway,
-    'bridge'               => $vip['bridge'],
-    'base_veth'            => $vip['base_veth'],
-    'ns_veth'              => $vip['ns_veth'],
-    'ip'                   => $vip['ip'],
-    'iflabel'              => $vip['iflabel'] ? {
-      undef   => 'ka',
-      default => $vip['iflabel']
-    },
-    'cidr_netmask'         => $vip['cidr_netmask'] ? {
-      undef   => '24',
-      default => $vip['cidr_netmask']
-    },
-    'ns'                   => $vip['namespace'] ? {
-      undef   => 'haproxy',
-      default => $vip['namespace']
-    },
-    'gateway_metric'       => $vip['gateway_metric'] ? {
-      undef   => undef,
-      default => $vip['gateway_metric']
-    },
-    'other_networks'       => $vip['other_networks'] ? {
-      undef => undef, false => undef,
-      default => $vip['other_networks']
-    },
-    'iptables_comment'     => $vip['iptables_comment'] ? {
-      undef   => undef, false => undef,
-      default => $vip['iptables_comment'],
-    },
-    'ns_iptables_start_rules' => $vip['ns_iptables_start_rules'] ? {
-      undef   => undef, false => undef,
-      default => $vip['ns_iptables_start_rules'],
-    },
-    'ns_iptables_stop_rules'  => $vip['ns_iptables_stop_rules'] ? {
-      undef   => undef, false => undef,
-      default => $vip['ns_iptables_stop_rules'],
-    },
-  }
+  $vip_name = "${vip_prefix}${name}"
 
   $metadata = {
     'migration-threshold' => '3',   # will be try start 3 times before migrate to another node
@@ -88,7 +54,23 @@ define cluster::virtual_ip (
     },
   }
 
-  $primitive_type = 'ns_IPaddr2'
+  $parameters = resource_parameters(
+    'bridge', $bridge,
+    'ip', $ip,
+    'cidr_netmask', $cidr_netmask,
+    'iflabel', $iflabel,
+    'ns', $ns,
+    'base_veth', $base_veth,
+    'ns_veth', $ns_veth,
+    'gateway', $gateway,
+    'gateway_metric', $gateway_metric,
+    'ns_iptables_start_rules', $ns_iptables_start_rules,
+    'ns_iptables_stop_rules', $ns_iptables_stop_rules,
+    'iptables_comment', $iptables_comment,
+    'also_check_interfaces', $also_check_interfaces,
+    'other_networks', $other_networks,
+    $additional_parameters
+  )
 
   service { $vip_name:
     ensure => 'running',
@@ -100,18 +82,17 @@ define cluster::virtual_ip (
     parameters       => $parameters,
     metadata         => $metadata,
     operations       => $operations,
-    prefix           => false,
+    prefix           => $use_pcmk_prefix,
   }
 
   # I'am running before this other vip
   # and this other vip cannot start without me running on this node
-  $colocation_before = $vip['colocation_before']
   if $colocation_before {
-    $colocation_before_vip_name = "vip__${colocation_before}"
-    $colocation_before_constraint_name = "${colocation_before_vip_name}-with-${vip_name}"
+    $colocation_before_vip_name = "${vip_prefix}${colocation_before}"
+    $colocation_before_constraint_name = "${colocation_before_vip_name}${colocation_separator}${vip_name}"
     pcmk_colocation { $colocation_before_constraint_name :
-      ensure     => 'present',
-      score      => 'INFINITY',
+      ensure     => $colocation_ensure,
+      score      => $colocation_score,
       first      => $vip_name,
       second     => $colocation_before_vip_name,
     }
@@ -124,13 +105,12 @@ define cluster::virtual_ip (
 
   # I'm running after this other vip
   # and I cannot start without other vip running on this node
-  $colocation_after = $vip['colocation_after']
   if $colocation_after {
-    $colocation_after_vip_name = "vip__${colocation_after}"
-    $colocation_after_constraint_name = "${vip_name}-with-${colocation_after_vip_name}"
+    $colocation_after_vip_name = "${vip_prefix}${colocation_after}"
+    $colocation_after_constraint_name = "${vip_name}${colocation_separator}${colocation_after_vip_name}"
     pcmk_colocation { $colocation_after_constraint_name :
-      ensure     => 'present',
-      score      => 'INFINITY',
+      ensure     => $colocation_ensure,
+      score      => $colocation_score,
       first      => $colocation_after_vip_name,
       second     => $vip_name,
     }
