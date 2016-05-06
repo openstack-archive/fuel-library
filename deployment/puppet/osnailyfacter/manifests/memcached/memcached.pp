05d4a7e5afeef951de23972cc4b378859f14f828
Maksym Yatsenko <myatsenko@mirantis.com>
2016-05-07 00:16:35 +0300

    [WIP]
    Memcached parameters update
    
    Change-Id: I08d49984638c46f2bda0ee4b2aee1e86690b2cf9

--- deployment/puppet/osnailyfacter/manifests/memcached/memcached.pp
+++ deployment/puppet/osnailyfacter/manifests/memcached/memcached.pp
@@ -4,10 +4,13 @@ class osnailyfacter::memcached::memcached {
 
   prepare_network_config(hiera_hash('network_scheme', {}))
 
+  $threads = $::processorcount*4
+
   class { '::memcached':
-    listen_ip  => get_network_role_property('mgmt/memcache', 'ipaddr'),
-    max_memory => '50%',
-    item_size  => '10m',
+    listen_ip       => get_network_role_property('mgmt/memcache', 'ipaddr'),
+    max_memory      => '50%',
+    item_size       => '10m',
+    processorcount  => $threads,
   }
 
 }
