83837e942942b262abc0ee29c8e756b2d7cd35ed
Denis Egorenko <degorenko@mirantis.com>
2016-05-11 21:18:21 +0300

    Create separate task for uploading Murano package
    When we have enabled Glare for Murano, Core library package will
    be uploaded to current Glance backend - Swift or RadosGW - hence
    we must be sure that both already prepared and ready to use.
    
    Change-Id: If0a684aa9b0410e28a82b1501633c0ebdb64dc67
    Closes-bug: #1572785

---
+++ deployment/puppet/osnailyfacter/modular/murano/upload_murano_package.pp
@@ -0,0 +1,3 @@
+class { '::openstack_tasks::murano::upload_murano_package' :}
+
+warning('osnailyfacter/modular/./murano/upload_murano_package.pp is deprecated in mitaka and will be removed in newton. Please use openstack_tasks/examples/./murano/upload_murano_package.pp')
