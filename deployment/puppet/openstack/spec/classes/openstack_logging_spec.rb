50aa72b3d41c6c4be24bea03ea8ad2ba0e2fbb3e
Maksim Malchuk <mmalchuk@mirantis.com>
2016-05-12 22:12:36 +0300

    Remove duplicate configurations of the rsyslog
    This commit removes the duplicate configuration settings configured
    by the incorrect usage of the rsyslog::client class for local and
    remote logging. Also, this commit adds some comments, notes and
    contains styling fixes.
    
    Depends-On: Ib95db5545889e8ae2b86dcf787c4adb7413d5698
    Change-Id: Iefef30379a62c8fab123e03c35c302de8e39a4a3
    Closes-Bug: #1579081
    Partial-Bug: #1580200
    Signed-off-by: Maksim Malchuk <mmalchuk@mirantis.com>

--- deployment/puppet/openstack/spec/classes/openstack_logging_spec.rb
+++ deployment/puppet/openstack/spec/classes/openstack_logging_spec.rb
@@ -56,6 +56,7 @@ describe 'openstack::logging' do
          '21-keystone-common-wsgi',
          '30-cinder',
          '40-glance',
+         '50-default',
          '50-neutron',
          '51-ceilometer',
          '53-aodh',
@@ -73,9 +74,7 @@ describe 'openstack::logging' do
            should contain_file("/etc/rsyslog.d/#{item}.conf")
         end
         should contain_class('rsyslog::client').with(
-          :log_remote                => p[:log_remote],
-          :log_local                 => p[:log_local],
-          :log_auth_local            => p[:log_auth_local],
+          :log_remote                => false,
           :high_precision_timestamps => p[:show_timezone]
         )
         should contain_rsyslog__snippet('00-disable-EscapeControlCharactersOnReceive')
