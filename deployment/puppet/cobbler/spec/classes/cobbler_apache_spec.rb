ba71ad472768d08cbc2307a3292bafc83b44e5f0
Kyrylo Galanov <kgalanov@mirantis.com>
2016-05-17 13:17:20 +0200

    Use puppetlabs/apache to set up cobbler's httpd configuration
    We used to have ERb templates to set up httpd, used inside cobbler.
    Puppetlabs already has an apache class which provides better
    maintainability.
    
    By using it we won't need to support two versions of configuration
    templates (for httpd 2.2 and 2.4).
    
    Change-Id: I49593960e613aaa8ba6d024cbc48c665bd62eb3c
    Closes-Bug: 1535357

---
+++ deployment/puppet/cobbler/spec/classes/cobbler_apache_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe 'cobbler::apache' do
+
+  shared_examples_for 'cobbler configuration' do
+
+    context 'with default params' do
+
+      it 'configures with the default params' do
+        should contain_apache__vhost('cobbler non-ssl').with(
+          :servername      => '_default_',
+          :docroot         => '/var/www/html',
+          :custom_fragment => 'RewriteEngine on
+      RewriteCond %{HTTPS} off
+      RewriteRule ^/$ https://%{HTTP_HOST}:8443%{REQUEST_URI} [R=301,L]
+      RewriteCond %{HTTPS} off
+      RewriteCond %{REQUEST_URI} !^/(cblr|cobbler)
+      RewriteRule (.*) http://%{HTTP_HOST}:8000%{REQUEST_URI} [R=301,L]',
+          :aliases         => [
+            ["alias", "/cobbler/boot"], ["path", "/var/lib/tftpboot"]
+          ],
+          :directories     => [
+            ["path", "/var/lib/tftpboot"], ["options", ["Indexes", "FollowSymLinks"]]
+          ],
+        )
+
+        should contain_apache__vhost('cobbler ssl').with(
+          :servername      => '_default_',
+          :port            => 443,
+          :docroot         => '/var/www/html',
+          :ssl             => true,
+          :ssl_cert        => "/var/lib/fuel/keys/master/cobbler/cobbler.crt",
+          :ssl_key         => "/var/lib/fuel/keys/master/cobbler/cobbler.key",
+          :custom_fragment => 'RewriteEngine on
+      RewriteRule ^/$ https://%{HTTP_HOST}:8443%{REQUEST_URI} [R=301,L]
+      CustomLog logs/ssl_request_log "%t %h %{SSL_PROTOCOL}x %{SSL_CIPHER}x \"%r\" %b"',
+          :ssl_cipher      => 'ALL:!ADH:!EXPORT:!SSLv2:!MEDIUM:!LOW:+HIGH',
+          :setenvif        => [ 'User-Agent ".*MSIE.*" nokeepalive ssl-unclean-shutdown downgrade-1.0 force-response-1.0' ],
+        )
+      end
+    end
+  end
+
+  context 'on Debian platforms' do
+    let :facts do
+      @default_facts.merge(
+      { :osfamily => 'Debian',
+        :operatingsystem => 'Debian',
+        :operatingsystemrelease => '7',
+      })
+    end
+
+    it_configures 'cobbler configuration'
+  end
+
+  context 'on RedHat platforms' do
+    let :facts do
+      @default_facts.merge(
+      { :osfamily => 'RedHat',
+        :operatingsystem => 'RedHat',
+        :operatingsystemrelease => '7.0',
+      })
+    end
+
+    it_configures 'cobbler configuration'
+  end
+
+end
+
