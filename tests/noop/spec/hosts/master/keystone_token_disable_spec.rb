3729f17074ecbbc52424d351e7c79bca21abbce6
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-05-19 14:36:14 +0300

    Delete service_token from keystone on Fuel Master
    Deletes keystone service token after deploying keystone
    to minimize security risk.
    
    Change-Id: I776644f727ce086369954f383a09b48b60bf11a5
    Depends-On: Idb7694b19792a6c43c2752867da7c34b995513d0
    Closes-Bug: #1582893

---
+++ tests/noop/spec/hosts/master/keystone_token_disable_spec.rb
@@ -0,0 +1,36 @@
+# ROLE: primary-controller
+# ROLE: controller
+
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'master/keystone_token_disable.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+
+    keystone_params  = Noop.hiera_structure 'keystone'
+    disable_token = Noop.puppet_function('str2bool', keystone_params['service_token_off'])
+
+    if disable_token
+      it 'should remove admin_token option' do
+        is_expected.to contain_keystone_config('DEFAULT/admin_token').with_ensure('absent')
+      end
+
+      it 'should contain exec of remove AdminTokenAuthMiddleware from pipelines' do
+        case facts[:osfamily]
+          when 'Debian'
+            paste_ini = '/etc/keystone/keystone-paste.ini'
+          when 'RedHat'
+            paste_ini = '/usr/share/keystone/keystone-dist-paste.ini'
+        end
+
+        is_expected.to contain_exec('remove_admin_token_auth_middleware').with(
+          :path    => ['/bin', '/usr/bin'],
+          :command => "sed -i.dist 's/ admin_token_auth//' #{paste_ini}",
+          :onlyif  => "fgrep -q ' admin_token_auth' #{paste_ini}",
+        )
+      end
+    end
+  end
+  run_test manifest
+end
