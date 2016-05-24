7be192b72bc25591e60dc5e1f4df86380edb2b66
Alexander Arzhanov <aarzhanov@mirantis.com>
2016-05-24 17:08:52 +0300

    Fix noop tests for new fixtures
    This patch makes it possible to enable
    new fixtures for I9e6363e20c59fd023a64be1013fa2697e80fdbcc
    
    -Added the compute-vmware role in globals and hiera
     tasks for noop script
    -Fix compute-vmware running service, this service
     started with enable_compute
    -Fix noop tests
    
    Change-Id: Ibaf3a0bbbfb1bb8308bb55de8e905ffccb57a0d6
    Partial-Bug: 1541849
    Related-Bug: 1535339

--- tests/noop/spec/hosts/roles/cinder_spec.rb
+++ tests/noop/spec/hosts/roles/cinder_spec.rb
@@ -28,6 +28,7 @@ describe manifest do
   cinder_db_user = Noop.hiera_structure 'cinder/db_user', 'cinder'
   cinder_db_name = Noop.hiera_structure 'cinder/db_name', 'cinder'
   cinder = Noop.puppet_function 'roles_include', 'cinder'
+  cinder_vmware = Noop.puppet_function 'roles_include', 'cinder-vmware'
   cinder_block_device = Noop.puppet_function 'roles_include', 'cinder-block-device'
   hostname = Noop.hiera('fqdn')
 
@@ -38,6 +39,8 @@ describe manifest do
       'ceph'
     elsif storage_hash['volumes_block_device']
       'block'
+    elsif cinder_vmware
+      'vmdk'
     else
       false
     end
