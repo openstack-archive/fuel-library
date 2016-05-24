1d79bfbcf1d311e52db1f534a8a4a656e144c410
Maksim Malchuk <mmalchuk@mirantis.com>
2016-05-24 14:04:55 +0300

    Disable directory listing in the Nginx
    Due to security reasons, sensitive information shouldn't be exposed.
    Whenever possible we should disable directory listing in the Nginx:
     * in the local repos we show an empty page for dirs without
       an index file.
     * in the dashboard site directory listing disabled completely.
    
    Change-Id: If4fb2ae1361697beb484b832c9e52507a34381ab
    Closes-Bug: #1584797
    Signed-off-by: Maksim Malchuk <mmalchuk@mirantis.com>

--- deployment/puppet/nailgun/manifests/nginx-repo.pp
+++ deployment/puppet/nailgun/manifests/nginx-repo.pp
@@ -2,6 +2,11 @@ class nailgun::nginx-repo(
   $repo_root = "/var/www/nailgun",
   ){
 
+  file { "${repo_root}/index.html":
+    ensure  => present,
+    content => '',
+  }
+
   file { "/etc/nginx/conf.d/repo.conf":
     content => template("nailgun/nginx_nailgun_repo.conf.erb"),
     owner => 'root',
