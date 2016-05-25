60da52ac4446bbe5542688aa20b85887f45fe368
Denis V. Meltsaykin <dmeltsaykin@mirantis.com>
2016-05-25 17:27:26 +0300

    Clean RabbitMQ definitions file
    Added a script to clean up the rabbitmq definitions file which is
    being generated during the post-deployment phase of a deployment.
    Previosly the definitions file contained all RPC queues presented in
    RabbitMQ after the successful deployment. These queues are one-off, and
    their restoration via OCF-script leads to growth of unconsumed messages,
    which, in turn, lead to RabbitMQ slowness and malfunctions.
    
    This is a rework of the previously reverted change
    Ie3f3194b3eaaa8dc28d1cd3442090b586e177306 which caused deployment
    failures in some scenarios involving partial update. This happened
    because the script was delivered in a separate package which has not
    been updated during the partial update. Now the script is
    delivered in the puppet's directory instead. Also introduced
    additional regexp-matching logic to instantly remove all the RPC queues,
    which were left in the previous version of the fix.
    
    Change-Id: Ib2f8237692d7dab47b83ac886ff370686a5d22fe
    Closes-Bug: #1497961

--- deployment/puppet/osnailyfacter/modular/astute/dump_rabbitmq_definitions.pp
+++ deployment/puppet/osnailyfacter/modular/astute/dump_rabbitmq_definitions.pp
@@ -13,10 +13,16 @@ $rabbit_enabled  = pick($rabbit_hash['enabled'], true)
 if ($rabbit_enabled) {
   $rabbit_api_endpoint = 'http://localhost:15672/api/definitions'
   $rabbit_credentials  = "${rabbit_hash['user']}:${rabbit_hash['password']}"
+  $filter = '/tmp/filter_rabbitmq_definitions.py'
+
+  file { $filter:
+    ensure  => file,
+    content => template('osnailyfacter/filter_rabbitmq_definitions.py'),
+  }
 
   exec { 'rabbitmq-dump-definitions':
     path    => ['/usr/bin', '/usr/sbin', '/sbin', '/bin'],
-    command => "curl -u ${rabbit_credentials} ${rabbit_api_endpoint} -o ${definitions_dump_file}",
+    command => "curl -u ${rabbit_credentials} ${rabbit_api_endpoint} | python ${filter} > ${definitions_dump_file}",
   }
 
   file { $definitions_dump_file:
