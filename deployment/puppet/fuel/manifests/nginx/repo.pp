c2066a9f9b2b273cf1861fa8af4d0e46e6ea6865
Maksim Malchuk <mmalchuk@mirantis.com>
2016-05-25 19:54:59 +0300

    Unified (one) error message for all invalid requests
    It is possible based on the server error messages, to enumerate
    directories and files on the application. This commit change the
    default Nginx error messages to Unified one message with 200 (OK)
    HTTP status code. For 404 (NOTFOUND) error we should preserve the
    status code but provide the same Unified message because the
    Package managers (in the Debian/Ubuntu at least) should know the
    status of the file existence in the repos.
    
    Change-Id: I6b996c5b1f8a1972379365571cf1f261e61b9ac5
    Closes-Bug: #1585176
    Signed-off-by: Maksim Malchuk <mmalchuk@mirantis.com>

--- deployment/puppet/fuel/manifests/nginx/repo.pp
+++ deployment/puppet/fuel/manifests/nginx/repo.pp
@@ -4,16 +4,23 @@ class fuel::nginx::repo(
   $service_enabled = true,
   ) inherits fuel::nginx {
 
+  File {
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
   file { "${repo_root}/index.html":
-    ensure  => present,
     content => '',
+  }
+
+  file { "${repo_root}/error.html":
+    content => template('fuel/nginx/error.html.erb'),
   }
 
   file { '/etc/nginx/conf.d/repo.conf':
     content => template('fuel/nginx/repo.conf.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
