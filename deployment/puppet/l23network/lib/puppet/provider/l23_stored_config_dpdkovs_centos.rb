4b63fcfb568ca8127e0bded4e283512968330b10
Dmitry Ilyin <dilyin@mirantis.com>
2016-05-25 14:57:39 +0300

    l23network module misc fixes for puppet4
    Fuel-CI: disable
    Change-Id: Iee4b5b6dc56c87613a0ddd28739510bca997338c

--- deployment/puppet/l23network/lib/puppet/provider/l23_stored_config_dpdkovs_centos.rb
+++ deployment/puppet/l23network/lib/puppet/provider/l23_stored_config_dpdkovs_centos.rb
@@ -1,5 +1,5 @@
-require 'puppetx/l23_dpdk_ports_mapping'
-require File.join(File.dirname(__FILE__), '..','..','puppet/provider/l23_stored_config_centos')
+require_relative '../../puppetx/l23_dpdk_ports_mapping'
+require_relative 'l23_stored_config_centos'
 
 class Puppet::Provider::L23_stored_config_dpdkovs_centos < Puppet::Provider::L23_stored_config_centos
 
