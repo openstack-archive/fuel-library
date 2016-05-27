c1bcd2ef614d254b41d1b2c22362c2202259d06a
dmburmistrov <dmburmistrov@mirantis.com>
2016-05-27 16:19:17 +0300

    [WIP] Ensure corosync files & packages
    Ubuntu 16.04 and Debian Stretch splitted
    corosync package. "fuel-rabbit-fence" package
    needs "notifyd"-part, so depends are updated.
    
    This patch ensures corosync necessary
    directories.
    
    blueprint: mos-xenial
    
    Closes-bug: #1587075
    
    Change-Id: I241a7976ad0c347167938a3b2656f633bc988046

---
+++ deployment/puppet/cluster/lib/facter/corosync_notifyd_status.rb
@@ -0,0 +1,15 @@
+require 'facter/util/resolution'
+
+Facter.add(:corosync_notifyd_status) do
+  setcode do
+    ret = false
+    case Facter.value('osfamily')
+      when /(?i)(debian)/
+        out = Facter::Util::Resolution.exec("dpkg-query -W -f='${Status}' corosync-notifyd 2>&1")
+        if 'install ok installed' == out
+          ret = true
+        end
+    end
+    ret
+  end
+end
