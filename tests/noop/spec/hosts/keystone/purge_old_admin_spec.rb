2861be2186646291545e518402585c0652955b2b
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-06-01 18:30:23 +0300

    Purge old openstack admin access user if changed
    If operator has changed admin user, old one will now be stored in
    /etc/hiera/old_admin_user.yaml as an access hash. Then it will be
    deleted after new user creation.
    
    Change-Id: I30213c02c4a370aee9db1597cf32dd8f08ae6539
    Closes-Bug: #1578348

---
+++ tests/noop/spec/hosts/keystone/purge_old_admin_spec.rb
@@ -0,0 +1,19 @@
+# ROLE: primary-controller
+
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'keystone/purge_old_admin.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+
+    access_hash = Noop.hiera('old_access', {})
+
+    if !access_hash.empty?
+      it 'should purge old admin user' do
+        is_expected.to contain_keystone_user(access_hash['user']).with_ensure('absent')
+      end
+    end
+  end
+  test_ubuntu_and_centos manifest
+end
