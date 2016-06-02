fcd24d6b2d920d97cfa1ea0e34e683b4e253bd00
Maksim Malchuk <mmalchuk@mirantis.com>
2016-06-02 19:12:10 +0000

    Revert "Configure Cinder backend sections for controllers"
    This reverts commit 1c920488e7c1c00b4df815974458cca638af7952.
    
    Change-Id: I177b3fdbe0a73e68fa7de3fb00fb6a22eb6de46b

--- deployment/puppet/openstack_tasks/manifests/openstack_cinder/openstack_cinder.pp
+++ deployment/puppet/openstack_tasks/manifests/openstack_cinder/openstack_cinder.pp
@@ -203,10 +203,6 @@ class openstack_tasks::openstack_cinder::openstack_cinder {
 
     class { 'cinder::volume': }
 
-    class { 'cinder::backends':
-      enabled_backends => [$volume_backend_name],
-    }
-
     # TODO(xarses): figure out if this is used anymore, it was a param, but
     # we don't set it, and it's only used my mlnx
     $iser = false
@@ -219,7 +215,7 @@ class openstack_tasks::openstack_cinder::openstack_cinder {
 
     case $manage_volumes {
       true, 'iscsi': {
-        cinder::backend::iscsi { $volume_backend_name:
+        cinder::backend::iscsi { 'DEFAULT':
           iscsi_ip_address    => $iscsi_bind_host,
           volume_group        => $volume_group,
           volume_backend_name => $volume_backend_name,
@@ -239,10 +235,10 @@ class openstack_tasks::openstack_cinder::openstack_cinder {
       'ceph': {
         if defined(Class['::ceph']) {
           Ceph::Pool<| title == $::ceph::cinder_pool |> ->
-          Cinder::Backend::Rbd[$volume_backend_name]
+          Cinder::Backend::Rbd['DEFAULT']
         }
 
-        cinder::backend::rbd { $volume_backend_name:
+        cinder::backend::rbd { 'DEFAULT':
           rbd_pool            => $rbd_pool,
           rbd_user            => $rbd_user,
           rbd_secret_uuid     => $rbd_secret_uuid,
