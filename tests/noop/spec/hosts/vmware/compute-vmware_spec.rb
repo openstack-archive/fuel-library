6104d6ca9d305bb173ff98fc1434f93c7b7fbfea
Alexander Arzhanov <aarzhanov@mirantis.com>
2016-06-03 15:16:08 +0300

    Disable unsupported force_config_drive for compute-vmware
    - Update templates
    - Update noop tests
    
    Change-Id: Ia3e7e0533c120baadb2dbfe54bb9ef41741d55f3
    Closes-Bug: #1588762
    (cherry picked from commit 36c70f4de42b72aa6af27b1af9682369225d7295)

--- tests/noop/spec/hosts/vmware/compute-vmware_spec.rb
+++ tests/noop/spec/hosts/vmware/compute-vmware_spec.rb
@@ -11,26 +11,28 @@ describe manifest do
     ceilometer_enabled = Noop.hiera_structure('ceilometer/enabled')
     computes = Noop.hiera_structure('vcenter/computes', [])
 
+    it 'should have force_config_drive option set to False' do
+      should contain_file('/etc/nova/nova-compute.conf').with_content(%r{\n\s*force_config_drive=False\n})
+    end
+
     if ceilometer_enabled and computes.any?
 
       it 'should have cache_prefix option set to $host' do
-        should contain_file('/etc/nova/nova-compute.conf').with_content(
-            %r{\n\s*cache_prefix=\$host\n}
-        )
+        should contain_file('/etc/nova/nova-compute.conf').with_content(%r{\n\s*cache_prefix=\$host\n})
       end
 
       if network_manager == 'VlanManager'
         it 'should have vlan_interface option set to vmnic0' do
-          should contain_file('/etc/nova/nova-compute.conf').with_content(
-              %r{\n\s*vlan_interface=vmnic0\n}
-          )
+          should contain_file('/etc/nova/nova-compute.conf').with_content(%r{\n\s*vlan_interface=vmnic0\n})
         end
       end
 
       it 'should have /etc/ceilometer/ceilometer.conf' do
-        should contain_file('/etc/ceilometer/ceilometer.conf').with_content(
-            %r{\n\s*hypervisor_inspector=vsphere\n}
-        )
+        should contain_file('/etc/ceilometer/ceilometer.conf').with_content(%r{\n\s*hypervisor_inspector=vsphere\n})
+      end
+
+      it 'should have force_config_drive option set to False' do
+        should contain_file('/etc/nova/nova-compute.conf').with_content(%r{\n\s*force_config_drive=False\n})
       end
 
     end
