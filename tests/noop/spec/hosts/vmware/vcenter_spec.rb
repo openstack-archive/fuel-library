c20429b4e5d5ec322ec0d1c0a753f33a98f30266
Alexander Arzhanov <aarzhanov@mirantis.com>
2016-06-06 16:10:19 +0300

    Fix vnc console on vCenter environment
    - Configure vnc console on new section "[vnc]"
    - Describe parameters
    - Fix typos
    - Update noop tests
    
    Change-Id: I7152c5a35b4d20343225d81f2488ae26f86a4827
    Closes-Bug: #1566287
    Closes-Bug: #1565788

--- tests/noop/spec/hosts/vmware/vcenter_spec.rb
+++ tests/noop/spec/hosts/vmware/vcenter_spec.rb
@@ -8,16 +8,34 @@ manifest = 'vmware/vcenter.pp'
 describe manifest do
   shared_examples 'catalog' do
 
-    network_manager = Noop.hiera_structure('novanetwork_parameters/network_manager')
+    use_vcenter = Noop.hiera('use_vcenter', 'false')
+    ssl_hash = Noop.hiera_hash 'use_ssl', {}
+    vncproxy_protocol = 'https'
+    vncproxy_port = '6080'
+    vcnproxy_path = '/vnc_auto.html'
 
-    if network_manager == 'VlanManager'
-      it 'should declare vmware::controller with vlan_interface option set to vmnic0' do
-        should contain_class('vmware::controller').with(
-          'vlan_interface' => 'vmnic0',
-        )
-      end
+    if !ssl_hash.empty?
+      vncproxy_host = Noop.hiera_structure('use_ssl/nova_public_hostname')
+    elsif Noop.hiera_structure('public_ssl/services')
+      vncproxy_host = Noop.hiera_structure('public_ssl/hostname')
+    else
+      vncproxy_host = Noop.hiera('public_vip')
+      vncproxy_protocol = 'http'
     end
 
+    if use_vcenter
+      it 'should properly configure vncproxy with (non-)ssl' do
+        should contain_class('vmware::controller').with(:vncproxy_host => vncproxy_host)
+        should contain_class('vmware::controller').with(:vncproxy_protocol => vncproxy_protocol)
+        should contain_class('vmware::controller').with(:vncproxy_port => vncproxy_port)
+        should contain_class('vmware::controller').with(:vncproxy_path => vcnproxy_path)
+      end
+      it 'should configure novncproxy_base_url setting' do
+        should contain_nova_config('vnc/novncproxy_base_url').with(
+          :value => "#{vncproxy_protocol}://#{vncproxy_host}:#{vncproxy_port}#{vcnproxy_path}"
+        )
+      end
+    end # end of use_vcenter
   end # end of shared_examples
 
   test_ubuntu_and_centos manifest
