591347fb5155179fb502f69803e6d4ff7302a510
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-07 17:53:08 +0300

    Puppet4 support: osnailyfacter spec
    Change-Id: I6383dbee7d6b5bac0a8c0bc3173744237f908dbb
    Related-Bug: 1586480

--- deployment/puppet/osnailyfacter/spec/functions/network_metadata_to_hosts__spec.rb
+++ deployment/puppet/osnailyfacter/spec/functions/network_metadata_to_hosts__spec.rb
@@ -4,7 +4,8 @@ require 'yaml'
 
 describe 'network_metadata_to_hosts' do
 
-let(:network_metadata) {"""
+  let(:network_metadata_yaml) do
+    <<-eof
 ---
   nodes:
     node-5:
@@ -85,45 +86,45 @@ let(:network_metadata) {"""
       - primary-controller
       namespace: vrouter
       ipaddr: 10.88.0.9
-"""}
-
-
-  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
-
-  before(:each) do
-    puppet_debug_override()
+    eof
   end
 
-  subject do
-    function_name = Puppet::Parser::Functions.function('network_metadata_to_hosts')
-    scope.method(function_name)
+  let(:network_metadata) do
+    YAML.load(network_metadata_yaml)
+  end
+
+  before(:each) do
+    puppet_debug_override
   end
 
   it 'should exist' do
-    expect(subject).to eq scope.method('function_network_metadata_to_hosts')
+    is_expected.not_to be_nil
   end
 
   it 'should return hash for creating ordinary set of "host" puppet resources by create_resources()' do
-    expect(scope.function_network_metadata_to_hosts([YAML.load(network_metadata)])).to eq({
-      'ctrl-006.domain.local' => {:ip => '10.88.0.8', :host_aliases => ['ctrl-006']},
-      'ctrl-005.domain.local' => {:ip => '10.88.0.6', :host_aliases => ['ctrl-005']},
-      'ctrl-004.domain.local' => {:ip => '10.88.0.7', :host_aliases => ['ctrl-004']},
-    })
+    is_expected.to run.with_params(network_metadata).and_return(
+        {
+            'ctrl-006.domain.local' => {:ip => '10.88.0.8', :host_aliases => ['ctrl-006']},
+            'ctrl-005.domain.local' => {:ip => '10.88.0.6', :host_aliases => ['ctrl-005']},
+            'ctrl-004.domain.local' => {:ip => '10.88.0.7', :host_aliases => ['ctrl-004']},
+        }
+    )
   end
 
   it 'should return hash for creating prefixed set of "host" puppet resources by create_resources()' do
-    expect(scope.function_network_metadata_to_hosts([YAML.load(network_metadata), 'nova/migration', 'xxx-'])).to eq({
-      'xxx-ctrl-006.domain.local' => {:ip => '10.77.0.8', :host_aliases => ['xxx-ctrl-006']},
-      'xxx-ctrl-005.domain.local' => {:ip => '10.77.0.6', :host_aliases => ['xxx-ctrl-005']},
-      'xxx-ctrl-004.domain.local' => {:ip => '10.77.0.7', :host_aliases => ['xxx-ctrl-004']},
-    })
+    is_expected.to run.with_params(network_metadata, 'nova/migration', 'xxx-').and_return(
+        {
+            'xxx-ctrl-006.domain.local' => {:ip => '10.77.0.8', :host_aliases => ['xxx-ctrl-006']},
+            'xxx-ctrl-005.domain.local' => {:ip => '10.77.0.6', :host_aliases => ['xxx-ctrl-005']},
+            'xxx-ctrl-004.domain.local' => {:ip => '10.77.0.7', :host_aliases => ['xxx-ctrl-004']},
+        }
+    )
   end
 
-  it { expect{scope.function_network_metadata_to_hosts([])}.to raise_error(ArgumentError, /Wrong number of arguments given/) }
+  it { is_expected.to run.with_params().and_raise_error(ArgumentError, /Wrong number of arguments given/) }
 
   it 'should throw exception on wrong number of parameters' do
-    expect{scope.function_network_metadata_to_hosts([YAML.load(network_metadata), 'nova/migration'])}.to \
-      raise_error(ArgumentError, /opts are required/)
+    is_expected.to run.with_params(network_metadata, 'nova/migration').and_raise_error(ArgumentError, /opts are required/)
   end
 
 end
