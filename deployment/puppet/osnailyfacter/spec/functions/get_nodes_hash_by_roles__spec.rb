cf2aa49882a06267c7ad4ed26de73af48c44ca06
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-07 17:53:08 +0300

    Puppet4 support: osnailyfacter spec
    Change-Id: I6383dbee7d6b5bac0a8c0bc3173744237f908dbb
    Related-Bug: 1586480

--- deployment/puppet/osnailyfacter/spec/functions/get_nodes_hash_by_roles__spec.rb
+++ deployment/puppet/osnailyfacter/spec/functions/get_nodes_hash_by_roles__spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 require 'yaml'
 
-describe Puppet::Parser::Functions.function(:get_nodes_hash_by_roles) do
-let(:network_metadata) do
-YAML.load("
+describe 'get_nodes_hash_by_roles' do
+  let(:network_metadata_yaml) do
+    <<-eof
 ---
   nodes:
     node-55:
@@ -22,57 +22,97 @@ YAML.load("
         - mongo
         - cinder
         - xxx
-")
-end
+    eof
+  end
 
-  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
+  let(:network_metadata) do
+    YAML.load(network_metadata_yaml)
+  end
 
-  subject do
-    function_name = Puppet::Parser::Functions.function(:get_nodes_hash_by_roles)
-    scope.method(function_name)
+  it 'should exist' do
+    is_expected.not_to be_nil
   end
 
   context "get_nodes_hash_by_roles($nodes_hash, ['role1','role2']) usage" do
 
-    it 'should exist' do
-      subject == Puppet::Parser::Functions.function(:get_nodes_hash_by_roles)
-    end
-
     it 'should return nodes hash for "controller" role' do
-      rv = scope.function_get_nodes_hash_by_roles([network_metadata, ['controller']])
-      rv.keys.sort.should == ['node-55']
+      is_expected.to run.with_params(network_metadata, ['controller']).and_return(
+          {
+              "node-55" => {
+                  "node_roles" => ["controller", "mongo", "cinder"],
+              }
+          }
+      )
     end
 
     it 'should return nodes hash for "mongo" role' do
-      rv = scope.function_get_nodes_hash_by_roles([network_metadata, ['mongo']])
-      rv.keys.sort.should == ['node-55', 'node-77']
+      is_expected.to run.with_params(network_metadata, ['mongo']).and_return(
+          {
+              "node-55" => {
+                  "node_roles" => ["controller", "mongo", "cinder"],
+              },
+              "node-77" => {
+                  "node_roles" => ["mongo", "cinder", "xxx"],
+              },
+          }
+      )
     end
 
     it 'should return nodes hash for "cinder" role' do
-      rv = scope.function_get_nodes_hash_by_roles([network_metadata, ['cinder']])
-      rv.keys.sort.should == ['node-55', 'node-66', 'node-77']
+      is_expected.to run.with_params(network_metadata, ['cinder']).and_return(
+          {
+              "node-55" => {
+                  "node_roles" => ["controller", "mongo", "cinder"]
+              },
+              "node-66" => {
+                  "node_roles" => ["compute", "cinder", "xxx", "yyy"]
+              },
+              "node-77" => {
+                  "node_roles" => ["mongo", "cinder", "xxx"]
+              }
+          }
+      )
     end
 
     it 'should return nodes hash for "controller"+"xxx" role' do
-      rv = scope.function_get_nodes_hash_by_roles([network_metadata, ['controller', 'xxx']])
-      rv.keys.sort.should == ['node-55', 'node-66', 'node-77']
+      is_expected.to run.with_params(network_metadata, ['controller', 'xxx']).and_return(
+          {
+              "node-55" => {
+                  "node_roles" => ["controller", "mongo", "cinder"]
+              },
+              "node-66" => {
+                  "node_roles" => ["compute", "cinder", "xxx", "yyy"]
+              },
+              "node-77" => {
+                  "node_roles" => ["mongo", "cinder", "xxx"]
+              }
+          }
+      )
     end
 
     it 'should return nodes hash for "controller"+"yyy" role' do
-      rv = scope.function_get_nodes_hash_by_roles([network_metadata, ['controller', 'yyy']])
-      rv.keys.sort.should == ['node-55', 'node-66']
+      is_expected.to run.with_params(network_metadata, ['controller', 'yyy']).and_return(
+          {
+              "node-55" => {
+                  "node_roles" => ["controller", "mongo", "cinder"]
+              },
+              "node-66" => {
+                  "node_roles" => ["compute", "cinder", "xxx", "yyy"]
+              },
+          }
+      )
     end
 
     it 'should raise Puppet::ParseError if 1st argument not a Hash' do
-      should run.with_params('xxx', ['controller', 'yyy']).and_raise_error(Puppet::ParseError)
+      is_expected.to run.with_params('xxx', ['controller', 'yyy']).and_raise_error(Puppet::ParseError)
     end
 
     it 'should raise Puppet::ParseError if 1st argument has wrong format' do
-      should run.with_params({:a=>1, :b=>2}, ['controller', 'yyy']).and_raise_error(Puppet::ParseError)
+      is_expected.to run.with_params({:a => 1, :b => 2}, ['controller', 'yyy']).and_raise_error(Puppet::ParseError)
     end
 
     it 'should raise Puppet::ParseError if 2nd argument not an array' do
-      should run.with_params(network_metadata, 'cinder').and_raise_error(Puppet::ParseError)
+      is_expected.to run.with_params(network_metadata, 'cinder').and_raise_error(Puppet::ParseError)
     end
 
   end
