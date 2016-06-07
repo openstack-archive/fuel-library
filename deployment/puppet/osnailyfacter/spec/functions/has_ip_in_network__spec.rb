1241d4ab0285207f170ba65353128512dee66935
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-07 17:54:11 +0300

    Puppet4 support: openstack tasks
    Change-Id: I097d43cc74100ee1eb86d2d5891460585d9cbfd1
    Related-Bug: 1586480
    Fuel-CI: disable

--- deployment/puppet/osnailyfacter/spec/functions/has_ip_in_network__spec.rb
+++ deployment/puppet/osnailyfacter/spec/functions/has_ip_in_network__spec.rb
@@ -1,29 +1,34 @@
 require 'spec_helper'
 
 describe 'has_ip_in_network' do
+  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
 
   it 'should exist' do
-    is_expected.not_to be_nil
+    Puppet::Parser::Functions.function('has_ip_in_network').should == 'function_has_ip_in_network'
   end
 
   it 'should throw an error on invalid arguments number' do
-    is_expected.to run.with_params('foo').and_raise_error(ArgumentError)
+    lambda {
+      scope.function_has_ip_in_network(['foo'])
+    }.should(raise_error(ArgumentError))
   end
 
   it 'should raise an error if invalid address or network is specified' do
-    is_expected.to run.with_params('foo', 'bar').and_raise_error(Puppet::ParseError)
+    lambda {
+      scope.function_has_ip_in_network(['foo', 'bar'])
+    }.should(raise_error(Puppet::ParseError))
   end
 
   it 'should return true if IP address is from CIDR' do
     cidr = '192.168.0.0/16'
     ipaddr = '192.168.33.66'
-    is_expected.to run.with_params(ipaddr, cidr).and_return(true)
+    scope.function_has_ip_in_network([ipaddr, cidr]).should == true
   end
 
   it 'should return false if IP address is not from CIDR' do
     cidr = '192.168.0.0/255.255.0.0'
     ipaddr = '172.16.0.1'
-    is_expected.to run.with_params(ipaddr, cidr).and_return(false)
+    scope.function_has_ip_in_network([ipaddr, cidr]).should == false
   end
 
 end
