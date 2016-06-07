ea6967aafb42e341a30628022194a2163945aa51
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-07 19:01:52 +0300

    Fix the apt_fuel provider
    * Update method is used for 'latest' implementation
      and may not be overrided.
    
    Closes-Bug: 1589884
    
    Change-Id: Ie5529cd62263b97a7f210901e1166698d92d869a
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- deployment/puppet/osnailyfacter/lib/puppet/provider/package/apt_fuel.rb
+++ deployment/puppet/osnailyfacter/lib/puppet/provider/package/apt_fuel.rb
@@ -8,29 +8,12 @@ Puppet::Type.type(:package).provide :apt_fuel, :parent => :apt, :source => :apt 
 
   defaultfor :operatingsystem => [:ubuntu]
 
-  def default_lock_timeout
-    300
-  end
-
-  def lock_file
-    '/var/lib/dpkg/lock'
-  end
-
-  def lock_sleep
-    2
-  end
-
-  def retry_count
-    3
-  end
-
-  def retry_sleep
-    5
-  end
-
-  def timeout
-    @property_hash.fetch(:timeout, default_lock_timeout)
-  end
+  @default_lock_timeout = 300
+  @lock_file = '/var/lib/dpkg/lock'
+  @lock_sleep = 2
+  @retry_count = 3
+  @retry_sleep = 5
+  @timeout = @property_hash.fetch(:timeout, default_lock_timeout)
 
   # Check whether ``apt-get`` or ``dpkg`` is currently active.
   #
@@ -39,8 +22,9 @@ Puppet::Type.type(:package).provide :apt_fuel, :parent => :apt, :source => :apt 
   # momentarily trying to acquire the lock. This means that the current process
   # needs to have sufficient privileges.
   # @return [true,false]
-  def locked?
-    status = open(lock_file, 'w') do |lock|
+  def locked?(file)
+    debug 'Call: locked?'
+    status = open(file, 'w') do |lock|
       flock_struct = [Fcntl::F_RDLCK, 0, 0, 0, 0].pack('ssqqi')
       lock.fcntl Fcntl::F_GETLK, flock_struct
       flock_struct.unpack('ssqqi').first
@@ -57,45 +41,54 @@ Puppet::Type.type(:package).provide :apt_fuel, :parent => :apt, :source => :apt 
 
   # Wait for the lock file to be unlocked
   # and the ``dpkg`` not being currently active.
+  # @yields block when there is no lock
   def wait_for_lock
-    Timeout::timeout(timeout, Puppet::Error) do
-      while locked? do
+    debug 'Call: wait_for_lock'
+    Timeout::timeout(timeout, Timeout::Error) do
+      while locked? lock_file do
         debug "#{lock_file} is locked, retrying..."
         sleep lock_sleep
       end
     end
+    yield
   end
 
   def install
+    debug 'Call: install'
     (1..retry_count).each do |try|
       begin
-        wait_for_lock
-        super
-        info "Attempt #{try} of #{retry_count} was successful!" if try > 1
+        wait_for_lock do
+          super
+        end
+        info "Attempt '#{try}' of '#{retry_count}' was successful!" if try > 1
         break
-      rescue Puppet::ExecutionFailure => exception
-        warning "Attempt #{try} of #{retry_count} have failed: #{exception.message}"
-        raise exception if try == retry_count
+      rescue Puppet::ExecutionFailure, Timeout::Error => exception
+        warning "Attempt '#{try}' of '#{retry_count}' has failed: #{exception.message}"
+        raise exception if try >= retry_count
         sleep retry_sleep
-        update
+        apt_get_update
       end
     end
   end
 
-  def update
-    wait_for_lock
-    Timeout::timeout(timeout, Puppet::Error) do
+  def apt_get_update
+    debug 'Call: apt_get_update'
+    wait_for_lock do
       aptget '-q', '-y', :update
     end
   end
 
   def uninstall
-    wait_for_lock
-    super
+    debug 'Call: uninstall'
+    wait_for_lock do
+      super
+    end
   end
 
   def purge
-    wait_for_lock
-    super
+    debug 'Call: purge'
+    wait_for_lock do
+      super
+    end
   end
 end
