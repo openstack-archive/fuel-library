db1e983f8a49a0c6d0817175cdb3835b842f2efa
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-07 17:53:08 +0300

    Puppet4 support: osnailyfacter spec
    Change-Id: I6383dbee7d6b5bac0a8c0bc3173744237f908dbb
    Related-Bug: 1586480

--- deployment/puppet/osnailyfacter/spec/functions/get_node_key_name_spec.rb
+++ deployment/puppet/osnailyfacter/spec/functions/get_node_key_name_spec.rb
@@ -11,11 +11,13 @@ describe 'the get_node_key_name function' do
 
   it 'should be able to calculate node key name' do
     scope.stubs(:function_hiera).with(['uid']).returns('121')
+    scope.stubs(:call_function).with('hiera', 'uid').returns('121')
     expect(scope.function_get_node_key_name []).to eq 'node-121'
   end
 
   it 'should raise error if UID not gived' do
     scope.stubs(:function_hiera).with(['uid']).returns(nil)
+    scope.stubs(:call_function).with('hiera', 'uid').returns(nil)
     expect{scope.function_get_node_key_name []}.to raise_error(Puppet::ParseError)
   end
 
