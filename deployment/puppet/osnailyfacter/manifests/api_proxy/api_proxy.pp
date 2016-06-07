5952972eca70eecff6ebef363f9453a34fbb856f
Maksim Malchuk <mmalchuk@mirantis.com>
2016-06-07 14:51:58 +0300

    Allow api-proxy to connect glare-api port 9494 for OSTF
    Change-Id: Idb5e573371a620ab5ec64b8bd2332d3b6f608acc
    Closes-Bug: #1589739
    Signed-off-by: Maksim Malchuk <mmalchuk@mirantis.com>

--- deployment/puppet/osnailyfacter/manifests/api_proxy/api_proxy.pp
+++ deployment/puppet/osnailyfacter/manifests/api_proxy/api_proxy.pp
@@ -4,7 +4,7 @@ class osnailyfacter::api_proxy::api_proxy {
 
   $max_header_size        = hiera('max_header_size', '81900')
   $apache_api_proxy_ports = hiera('apache_api_proxy_ports',
-   ['443', '563', '5000', '6385', '8000', '8003', '8004', '8042', '8080', '8082', '8386', '8773', '8774', '8776', '8777', '9292', '9696'])
+   ['443', '563', '5000', '6385', '8000', '8003', '8004', '8042', '8080', '8082', '8386', '8773', '8774', '8776', '8777', '9292', '9494', '9696'])
 
   # Listen directives with host required for ip_based vhosts
   class { '::osnailyfacter::apache':
