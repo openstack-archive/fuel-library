c8d7e1dc0e18267dbf0508f53ce93d312b3910a4
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-07 17:54:11 +0300

    Puppet4 support: openstack tasks
    Change-Id: I097d43cc74100ee1eb86d2d5891460585d9cbfd1
    Related-Bug: 1586480
    Fuel-CI: disable

--- deployment/puppet/openstack_tasks/manifests/roles/compute.pp
+++ deployment/puppet/openstack_tasks/manifests/roles/compute.pp
@@ -230,7 +230,7 @@ class openstack_tasks::roles::compute {
       path    => '/etc/libvirt/qemu.conf',
       line    => $libvirt_hugetlbfs_mount,
       match   => '^hugetlbfs_mount =.*$',
-      require => Package[$::nova::params::libvirt_package_name],
+      require => Package['libvirt'],
       notify  => Service['libvirt'],
     }
     file_line { 'libvirt_1g_hugepages_apparmor':
@@ -238,7 +238,7 @@ class openstack_tasks::roles::compute {
       path    => '/etc/apparmor.d/abstractions/libvirt-qemu',
       after   => 'owner "/run/hugepages/kvm/libvirt/qemu/',
       line    => '  owner "/mnt/hugepages_1GB/libvirt/qemu/**" rw,',
-      require => Package[$::nova::params::libvirt_package_name],
+      require => Package['libvirt'],
       notify  => Exec['refresh_apparmor'],
     }
     file_line { '1g_hugepages_fstab':
@@ -426,7 +426,7 @@ class openstack_tasks::roles::compute {
       file_line { 'qemu_selinux':
         path    => '/etc/libvirt/qemu.conf',
         line    => 'security_driver = "selinux"',
-        require => Package[$::nova::params::libvirt_package_name],
+        require => Package['libvirt'],
         notify  => Service['libvirt']
       }
     }
@@ -434,7 +434,7 @@ class openstack_tasks::roles::compute {
       file_line { 'qemu_apparmor':
         path    => '/etc/libvirt/qemu.conf',
         line    => 'security_driver = "apparmor"',
-        require => Package[$::nova::params::libvirt_package_name],
+        require => Package['libvirt'],
         notify  => Service['libvirt']
       }
 
