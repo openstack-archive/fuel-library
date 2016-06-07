2107aeb46851529c859526d85aa4bfad153e1916
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-07 17:53:08 +0300

    Puppet4 support: osnailyfacter spec
    Change-Id: I6383dbee7d6b5bac0a8c0bc3173744237f908dbb
    Related-Bug: 1586480

--- deployment/puppet/osnailyfacter/spec/functions/amqp_host_spec.rb
+++ deployment/puppet/osnailyfacter/spec/functions/amqp_host_spec.rb
@@ -1,78 +1,66 @@
 require 'spec_helper'
 
-describe 'the amqp_hosts function' do
-  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
+describe 'amqp_hosts' do
+
+  before(:each) do
+    scope.stubs(:lookupvar).with('::fqdn', {}).returns('somehost')
+  end
 
   it 'should exist' do
-    expect(
-        Puppet::Parser::Functions.function('amqp_hosts')
-    ).to eq('function_amqp_hosts')
+    is_expected.not_to be_nil
   end
 
   it 'should raise an error if there is less than 1 arguments' do
-    expect {
-      scope.function_amqp_hosts([])
-    }.to raise_error
+    is_expected.to run.with_params().and_raise_error(Puppet::ParseError)
   end
 
   it 'should convert the array on nodes to host:port pairs' do
-    scope.expects(:lookupvar).with('::fqdn', {}).returns('127.0.0.1')
-    expect(
-        scope.function_amqp_hosts([%w(192.168.0.1 192.168.0.2 192.168.0.3), '5673'])
-    ).to eq '192.168.0.3:5673, 192.168.0.1:5673, 192.168.0.2:5673'
+    is_expected.to run.with_params(%w(192.168.0.1 192.168.0.2 192.168.0.3), '5673')
+                       .and_return('192.168.0.3:5673, 192.168.0.1:5673, 192.168.0.2:5673')
   end
 
   it 'should use port 5673 by default if it was not provided' do
-    scope.expects(:lookupvar).with('::fqdn', {}).returns('127.0.0.1')
-    expect(
-        scope.function_amqp_hosts([%w(192.168.0.1 192.168.0.2 192.168.0.3)])
-    ).to eq '192.168.0.3:5673, 192.168.0.1:5673, 192.168.0.2:5673'
+    is_expected.to run.with_params(%w(192.168.0.1 192.168.0.2 192.168.0.3))
+                       .and_return('192.168.0.3:5673, 192.168.0.1:5673, 192.168.0.2:5673')
   end
 
   it 'should use different order for different fqdns' do
-    scope.expects(:lookupvar).with('::fqdn', {}).returns('192.168.0.1')
-    expect(
-        scope.function_amqp_hosts([%w(192.168.0.1 192.168.0.2 192.168.0.3), '5673'])
-    ).to eq '192.168.0.1:5673, 192.168.0.2:5673, 192.168.0.3:5673'
+    scope.stubs(:lookupvar).with('::fqdn', {}).returns('otherhost')
+    is_expected.to run.with_params(%w(192.168.0.1 192.168.0.2 192.168.0.3), '5673')
+                       .and_return('192.168.0.1:5673, 192.168.0.2:5673, 192.168.0.3:5673')
   end
 
   it 'should be able to use another port value' do
-    scope.expects(:lookupvar).with('::fqdn', {}).returns('127.0.0.1')
-    expect(
-        scope.function_amqp_hosts([%w(192.168.0.1 192.168.0.2 192.168.0.3), '123'])
-    ).to eq '192.168.0.3:123, 192.168.0.1:123, 192.168.0.2:123'
+    is_expected.to run.with_params(%w(192.168.0.1 192.168.0.2 192.168.0.3), '123')
+                       .and_return('192.168.0.3:123, 192.168.0.1:123, 192.168.0.2:123')
   end
 
   it 'should move prefered host to the first place if i was found in the list' do
-    scope.expects(:lookupvar).with('::fqdn', {}).returns('127.0.0.1')
-    expect(
-        scope.function_amqp_hosts([%w(192.168.0.1 192.168.0.2 192.168.0.3), '5673', '192.168.0.1'])
-    ).to eq '192.168.0.1:5673, 192.168.0.3:5673, 192.168.0.2:5673'
+    scope.stubs(:lookupvar).with('::fqdn', {}).returns('otherhost')
+    is_expected.to run.with_params(%w(192.168.0.1 192.168.0.2 192.168.0.3), '5673', '192.168.0.3')
+                       .and_return('192.168.0.3:5673, 192.168.0.1:5673, 192.168.0.2:5673')
   end
 
   it 'should ignore prefered host if it is not in the list' do
-    scope.expects(:lookupvar).with('::fqdn', {}).returns('127.0.0.1')
-    expect(
-        scope.function_amqp_hosts([%w(192.168.0.1 192.168.0.2 192.168.0.3), '5673', '172.16.0.1'])
-    ).to eq '192.168.0.3:5673, 192.168.0.1:5673, 192.168.0.2:5673'
+    scope.stubs(:lookupvar).with('::fqdn', {}).returns('otherhost')
+    is_expected.to run.with_params(%w(192.168.0.1 192.168.0.2 192.168.0.3), '5673', '172.16.0.1')
+                       .and_return('192.168.0.1:5673, 192.168.0.2:5673, 192.168.0.3:5673')
   end
 
-  it 'should be able to work with comma separated host list' do
-    scope.expects(:lookupvar).with('::fqdn', {}).returns('127.0.0.1')
-    expect(
-        scope.function_amqp_hosts(['192.168.0.1, 192.168.0.2,192.168.0.3', '5673'])
-    ).to eq '192.168.0.3:5673, 192.168.0.1:5673, 192.168.0.2:5673'
+  it 'should be able to work with comma-separated host list' do
+    is_expected.to run.with_params('192.168.0.1, 192.168.0.2,192.168.0.3', '5673')
+                       .and_return('192.168.0.3:5673, 192.168.0.1:5673, 192.168.0.2:5673')
   end
 
   it 'should be able to work with a single host' do
-    expect(
-        scope.function_amqp_hosts(['192.168.0.1', '5673'])
-    ).to eq '192.168.0.1:5673'
+    is_expected.to run.with_params('192.168.0.1', '5673')
+                       .and_return('192.168.0.1:5673')
   end
 
   it 'should not spoil the input data' do
     hosts = %w(192.168.0.1 192.168.0.2 192.168.0.3)
-    amqp_hosts = scope.function_amqp_hosts([hosts])
+    is_expected.not_to run.with_params(%w(192.168.0.1 192.168.0.2 192.168.0.3))
+                       .and_raise_error(Puppet::Error)
     expect(hosts).to eq(%w(192.168.0.1 192.168.0.2 192.168.0.3))
   end
 
