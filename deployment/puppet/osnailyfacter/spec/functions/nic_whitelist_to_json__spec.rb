306af9b6dad8cb102dd410ffe06c54c30d7863c4
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-07 17:52:47 +0300

    Puppet4 support: osnailyfacter
    * Multiple fixes in the spec functions
    * Disabled tests for wait_for_nova_backends
      They are unstable and very dependant on
      Puppet version. They should be rewritten later.
    * Removed setvar and store function.
      They are not used anywhere and may
      be harmful if used.
    
    fetch_value function
    
    The functions for extracting values from a nested structure
    have a long and funny history. Currently this function is
    named "dig" and is used in the Fule linrary by this name.
    But in Puppet 4.5 another incompatible "dig" function was
    introduced and the original "dig" in the stdlib was renamed
    to dig44.
    
    The dig function from the stdlib doesn't support :undef values
    and has problems with other values too. This function is
    based on the "dig44" but has some improvements needed for
    Puppet4.5+.
    
    Change-Id: I9f7a07a582a0d67b5db570378b856cf0f4c7c48e
    Related-Bug: 1586480

--- deployment/puppet/osnailyfacter/spec/functions/nic_whitelist_to_json__spec.rb
+++ deployment/puppet/osnailyfacter/spec/functions/nic_whitelist_to_json__spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe 'nic_whitelist_to_json' do
-  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
+
   let(:nic_whitelist) do
    [{"devname"=>"eth1", "physical_network"=>"physnet2"}]
   end
+
   let(:nic_whitelist_json) do
    "[{\"devname\":\"eth1\",\"physical_network\":\"physnet2\"}]"
   end
@@ -14,19 +15,19 @@ describe 'nic_whitelist_to_json' do
   end
 
   it 'should exist' do
-    expect(Puppet::Parser::Functions.function('nic_whitelist_to_json')).to eq('function_nic_whitelist_to_json')
+    is_expected.not_to be_nil
   end
 
   it 'should fail if more then one argument given' do
-    expect{scope.function_nic_whitelist_to_json([nic_whitelist, 'eee'])}.to raise_error(Puppet::ParseError, /one argument is allowed/)
+    is_expected.to run.with_params(nic_whitelist, 'eee').and_raise_error(Puppet::ParseError)
   end
 
   it 'should return without arguments' do
-    expect(scope.function_nic_whitelist_to_json([])).to eq(nil)
+    is_expected.to run.with_params().and_return(nil)
   end
 
   it 'should convert nic whitelist to json' do
-    expect(scope.function_nic_whitelist_to_json([nic_whitelist])).to eq(nic_whitelist_json)
+    is_expected.to run.with_params(nic_whitelist).and_return(nic_whitelist_json)
   end
 
 end
