bb9d9205b1735386cca839222c04993f41ae7a01
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-07 19:20:21 +0300

    Upstream pacemaker: noop tests
    Change-Id: I52b179c815c820ffbf83877ce76b97fd8a2b6ecf
    Related-Bug: 1482248

--- tests/noop/spec/hosts/api-proxy/api-proxy_spec.rb
+++ tests/noop/spec/hosts/api-proxy/api-proxy_spec.rb
@@ -50,27 +50,6 @@ describe manifest do
       )
     end
 
-    master_ip = Noop.hiera 'master_ip'
-    it 'should contain concat_fragment("apache_api_proxy-custom_fragment") resource with correct statements' do
-      is_expected.to contain_concat_fragment('apache_api_proxy-custom_fragment').with_content("
-  ## Custom fragment
-    ProxyRequests on
-  ProxyVia On
-  AllowCONNECT #{ports.join(' ')}
-  HostnameLookups off
-  LimitRequestFieldSize 81900
-  SetEnv force-proxy-request-1.0 1
-  SetEnv proxy-nokeepalive 1
-  RequestHeader unset Expect early
-  RequestReadTimeout header=0,MinRate=500 body=0,MinRate=500
-  <Proxy *>
-    Order Deny,Allow
-        Allow from #{master_ip}
-        Deny from all
-  </Proxy>
-
-")
-    end
     it 'should declare apache::mod::headers' do
       should contain_class('apache::mod::headers')
     end
