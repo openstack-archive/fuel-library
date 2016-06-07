bad1c31f935e4c002d9610f8783b5a000e050ee6
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-07 19:01:52 +0300

    Fix the apt_fuel provider
    * Update method is used for 'latest' implementation
      and may not be overrided.
    
    Closes-Bug: 1589884
    
    Change-Id: Ie5529cd62263b97a7f210901e1166698d92d869a

--- deployment/puppet/osnailyfacter/lib/puppet/provider/package/apt_fuel.rb
+++ deployment/puppet/osnailyfacter/lib/puppet/provider/package/apt_fuel.rb
@@ -40,6 +40,7 @@ Puppet::Type.type(:package).provide :apt_fuel, :parent => :apt, :source => :apt 
   # needs to have sufficient privileges.
   # @return [true,false]
   def locked?
+    debug 'Call: locked?'
     status = open(lock_file, 'w') do |lock|
       flock_struct = [Fcntl::F_RDLCK, 0, 0, 0, 0].pack('ssqqi')
       lock.fcntl Fcntl::F_GETLK, flock_struct
@@ -57,45 +58,50 @@ Puppet::Type.type(:package).provide :apt_fuel, :parent => :apt, :source => :apt 
 
   # Wait for the lock file to be unlocked
   # and the ``dpkg`` not being currently active.
+  # @yields block when there is no lock
   def wait_for_lock
-    Timeout::timeout(timeout, Puppet::Error) do
+    debug 'Call: wait_for_lock'
+    Timeout::timeout(timeout, Timeout::Error) do
       while locked? do
         debug "#{lock_file} is locked, retrying..."
         sleep lock_sleep
       end
     end
+    yield
   end
 
   def install
+    debug 'Call: install'
     (1..retry_count).each do |try|
       begin
-        wait_for_lock
-        super
-        info "Attempt #{try} of #{retry_count} was successful!" if try > 1
+        wait_for_lock { super }
+        info "Attempt '#{try}' of '#{retry_count}' was successful!" if try > 1
         break
-      rescue Puppet::ExecutionFailure => exception
-        warning "Attempt #{try} of #{retry_count} have failed: #{exception.message}"
-        raise exception if try == retry_count
+      rescue Puppet::ExecutionFailure, Timeout::Error => exception
+        warning "Attempt '#{try}' of '#{retry_count}' have failed: #{exception.message}"
+        raise exception if try >= retry_count
         sleep retry_sleep
-        update
+        apt_get_update
       end
     end
   end
 
-  def update
-    wait_for_lock
-    Timeout::timeout(timeout, Puppet::Error) do
-      aptget '-q', '-y', :update
+  def apt_get_update
+    debug 'Call: apt_get_update'
+    wait_for_lock do
+      Timeout::timeout(timeout) do
+        aptget '-q', '-y', :update
+      end
     end
   end
 
   def uninstall
-    wait_for_lock
-    super
+    debug 'Call: uninstall'
+    wait_for_lock { super }
   end
 
   def purge
-    wait_for_lock
-    super
+    debug 'Call: purge'
+    wait_for_lock { super }
   end
 end
