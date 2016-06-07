232e2a1af1f77ec42beb8139fe463535664b5d79
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-07 17:52:47 +0300

    Puppet4 support: osnailyfacter
    * Multiple fixes in the spec functions
    * Disabled tests for wait_for_nova_backends
      They are unstable and very dependant on
      Puppet version. They should be rewritten later.
    * Removed setvar and store function.
      They are not used anywhere and may
      be harmful if used.
    
    fetch_value function
    
    The functions for extracting values from a nested structure
    have a long and funny history. Currently this function is
    named "dig" and is used in the Fule linrary by this name.
    But in Puppet 4.5 another incompatible "dig" function was
    introduced and the original "dig" in the stdlib was renamed
    to dig44.
    
    The dig function from the stdlib doesn't support :undef values
    and has problems with other values too. This function is
    based on the "dig44" but has some improvements needed for
    Puppet4.5+.
    
    Change-Id: I9f7a07a582a0d67b5db570378b856cf0f4c7c48e
    Related-Bug: 1586480

--- deployment/puppet/osnailyfacter/spec/functions/filter_nodes_with_enabled_option__spec.rb
+++ deployment/puppet/osnailyfacter/spec/functions/filter_nodes_with_enabled_option__spec.rb
@@ -2,69 +2,72 @@ require 'yaml'
 require 'spec_helper'
 
 describe 'filter_nodes_with_enabled_option' do
-  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
-  let(:compute_nodes) do
-  YAML.load("
+  let(:compute_nodes_yaml) do
+  <<-eof
 ---
-  node-1:
-    swift_zone: '1'
-    uid: '1'
-    fqdn: node-1.test.domain.local
-    node_roles:
-    - compute
-    name: node-1
-    nova_hugepages_enabled: true
-  node-2:
-    swift_zone: '1'
-    uid: '2'
-    fqdn: node-2.test.domain.local
-    node_roles:
-    - compute
-    name: node-2
-    nova_hugepages_enabled: true
-    nova_cpu_pinning_enabled: true
-  node-3:
-    swift_zone: '1'
-    uid: '3'
-    fqdn: node-3.test.domain.local
-    node_roles:
-    - compute
-    name: node-3
-    nova_cpu_pinning_enabled: true
-  ")
+node-1:
+  swift_zone: '1'
+  uid: '1'
+  fqdn: node-1.test.domain.local
+  node_roles:
+  - compute
+  name: node-1
+  nova_hugepages_enabled: true
+node-2:
+  swift_zone: '1'
+  uid: '2'
+  fqdn: node-2.test.domain.local
+  node_roles:
+  - compute
+  name: node-2
+  nova_hugepages_enabled: true
+  nova_cpu_pinning_enabled: true
+node-3:
+  swift_zone: '1'
+  uid: '3'
+  fqdn: node-3.test.domain.local
+  node_roles:
+  - compute
+  name: node-3
+  nova_cpu_pinning_enabled: true
+eof
+  end
+
+  let(:compute_nodes) do
+    YAML.load(compute_nodes_yaml)
   end
 
   before(:each) do
-    puppet_debug_override()
+    puppet_debug_override
   end
 
   it 'should exist' do
-    expect(Puppet::Parser::Functions.function('filter_nodes_with_enabled_option')).to eq('function_filter_nodes_with_enabled_option')
+    is_expected.not_to be_nil
   end
 
   it 'should fail if wrong argument count given' do
-    expect{scope.function_filter_nodes_with_enabled_option([{'node-1'=>{'uid'=>'1'}}, 'nova_hugepages_enabled', 'eee'])}.to raise_error(Puppet::ParseError, /takes exactly 2 arguments/)
-    expect{scope.function_filter_nodes_with_enabled_option([])}.to raise_error(Puppet::ParseError, /takes exactly 2 arguments/)
+    is_expected.to run.with_params({'node-1'=>{'uid'=>'1'}}, 'nova_hugepages_enabled', 'eee').and_raise_error(Puppet::ParseError, /takes exactly 2 arguments/)
+    is_expected.to run.with_params().and_raise_error(Puppet::ParseError, /takes exactly 2 arguments/)
   end
 
   it 'should fail if the first argument is not a hash' do
-    expect{scope.function_filter_nodes_with_enabled_option([['node-1'],'eee'])}.to raise_error(Puppet::ParseError, /must be a hash/)
+    is_expected.to run.with_params(['node-1'],'eee').and_raise_error(Puppet::ParseError, /must be a hash/)
   end
 
   it 'should fail if the second argument is not a string' do
-    expect{scope.function_filter_nodes_with_enabled_option([{'node-1'=>{'uid'=>'1'}}, ['fqdn']])}.to raise_error(Puppet::ParseError, /must be a string/)
+    is_expected.to run.with_params({'node-1'=>{'uid'=>'1'}}, ['fqdn']).and_raise_error(Puppet::ParseError, /must be a string/)
   end
 
   it 'should return array of nodes fqdn with nova_hugepages_enabled' do
-    expect(scope.function_filter_nodes_with_enabled_option([compute_nodes, 'nova_hugepages_enabled'])).to eq(['node-1.test.domain.local','node-2.test.domain.local'])
+    is_expected.to run.with_params(compute_nodes, 'nova_hugepages_enabled').and_return(['node-1.test.domain.local','node-2.test.domain.local'])
   end
 
   it 'should return array of nodes fqdn with nova_cpu_pinning_enabled' do
-    expect(scope.function_filter_nodes_with_enabled_option([compute_nodes, 'nova_cpu_pinning_enabled'])).to eq(['node-2.test.domain.local','node-3.test.domain.local'])
+    is_expected.to run.with_params(compute_nodes, 'nova_cpu_pinning_enabled').and_return(['node-2.test.domain.local','node-3.test.domain.local'])
   end
 
   it 'should return empty array when missing option is specified' do
-    expect(scope.function_filter_nodes_with_enabled_option([compute_nodes, 'abc'])).to eq([])
+    is_expected.to run.with_params(compute_nodes, 'abc').and_return([])
   end
 
 end
