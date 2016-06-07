f743b8aed4525946cd5b08e058e4bc435ffa2366
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-07 17:50:49 +0300

    Puppet4 support: l23network spec
    * Finish l23network refactoring
    
    Change-Id: If6d0ca88f4ba87b52e24281eb0f59b5943ec6ca0
    Related-Bug: 1586480

--- deployment/puppet/l23network/spec/functions/get_dpdk_interfaces__spec.rb
+++ deployment/puppet/l23network/spec/functions/get_dpdk_interfaces__spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 require 'yaml'
-require 'puppetx/l23_hash_tools'
+require_relative '../../lib/puppetx/l23_hash_tools'
 
 describe Puppet::Parser::Functions.function(:get_dpdk_interfaces) do
 let(:network_scheme) do
