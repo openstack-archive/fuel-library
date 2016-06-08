1cb83bbc8452807ccba91e29e9b23f0a8585a07a
dmburmistrov <dmburmistrov@mirantis.com>
2016-06-08 17:39:55 +0300

    [WIP] one patch to rule them all
    * use upstream libvirt (ubuntu)
    * use upstream vncproxy package (ubuntu)
    * fix keystone relationships
    * temporary hack for deleted nodes (LP #1590094)
    
    Change-Id: I6f8e604c0ad6dfa1c4d7ee62fe83ed53d4d29e96

--- deployment/puppet/osnailyfacter/manifests/astute/purge_service_entries.pp
+++ deployment/puppet/osnailyfacter/manifests/astute/purge_service_entries.pp
@@ -6,7 +6,8 @@ class osnailyfacter::astute::purge_service_entries {
   # Used as singular by post-deployment action to purge nova services
   # from deleted hosts
   #
-  $deleted_hosts = hiera('deleted_nodes',[])
+###  $deleted_hosts = hiera('deleted_nodes',[])
+  $deleted_hosts = []
   ensure_resource('nova_service', $deleted_hosts, {'ensure' => 'purged'})
 
 }
