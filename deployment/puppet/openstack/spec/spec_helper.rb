b0415714d9b82b7c810b6d2971a572847cc847d5
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-08 14:33:57 +0300

    Puppet4 support: cluster module
    Change-Id: I56462596cd5b636d8e7edd146f0ce14dd5471d4c

--- deployment/puppet/openstack/spec/spec_helper.rb
+++ deployment/puppet/openstack/spec/spec_helper.rb
@@ -1,4 +1,6 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-facts'
+include RspecPuppetFacts
 
 RSpec.configure do |c|
   c.alias_it_should_behave_like_to :it_configures, 'configures'
@@ -7,3 +9,16 @@ RSpec.configure do |c|
 end
 
 at_exit { RSpec::Puppet::Coverage.report! }
+
+def supported_os
+  [
+      {
+          'operatingsystem' => 'CentOS',
+          'operatingsystemrelease' => ['7.0'],
+      },
+      {
+          'operatingsystem' => 'Ubuntu',
+          'operatingsystemrelease' => ['14.04'],
+      },
+  ]
+end
