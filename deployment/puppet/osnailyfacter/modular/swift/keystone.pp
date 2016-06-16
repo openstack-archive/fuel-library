bacbf5894036e54c3b2d15fe27816b5f16c37c70
Oleksiy Molchanov <omolchanov@mirantis.com>
2016-06-16 18:50:05 +0300

    Trigger swift-keystone only if object storage is disabled
    Change-Id: I129ed6ebc1e68109e3c6a4dd0271f6ebb390dc1e
    Closes-Bug: 1590700

--- deployment/puppet/osnailyfacter/modular/swift/keystone.pp
+++ deployment/puppet/osnailyfacter/modular/swift/keystone.pp
@@ -1,45 +1,50 @@
 notice('MODULAR: swift/keystone.pp')
 
-$swift_hash       = hiera_hash('swift', {})
-$public_vip       = hiera('public_vip')
-$admin_address    = hiera('management_vip')
-$region           = pick($swift_hash['region'], 'RegionOne')
-$public_ssl_hash  = hiera('public_ssl')
-$public_address   = $public_ssl_hash['services'] ? {
-  true    => $public_ssl_hash['hostname'],
-  default => $public_vip,
-}
-$public_protocol  = $public_ssl_hash['services'] ? {
-  true    => 'https',
-  default => 'http',
-}
+$storage_hash = hiera('storage', {})
 
-$password            = $swift_hash['user_password']
-$auth_name           = pick($swift_hash['auth_name'], 'swift')
-$configure_endpoint  = pick($swift_hash['configure_endpoint'], true)
-$service_name        = pick($swift_hash['service_name'], 'swift')
-$tenant              = pick($swift_hash['tenant'], 'services')
+if !$storage_hash['objects_ceph'] {
 
-validate_string($public_address)
-validate_string($password)
+  $swift_hash       = hiera_hash('swift', {})
+  $public_vip       = hiera('public_vip')
+  $admin_address    = hiera('management_vip')
+  $region           = pick($swift_hash['region'], 'RegionOne')
+  $public_ssl_hash  = hiera('public_ssl')
+  $public_address   = $public_ssl_hash['services'] ? {
+    true    => $public_ssl_hash['hostname'],
+    default => $public_vip,
+  }
+  $public_protocol  = $public_ssl_hash['services'] ? {
+    true    => 'https',
+    default => 'http',
+  }
 
-$public_url          = "${public_protocol}://${public_address}:8080/v1/AUTH_%(tenant_id)s"
-$admin_url           = "http://${admin_address}:8080/v1/AUTH_%(tenant_id)s"
+  $password            = $swift_hash['user_password']
+  $auth_name           = pick($swift_hash['auth_name'], 'swift')
+  $configure_endpoint  = pick($swift_hash['configure_endpoint'], true)
+  $service_name        = pick($swift_hash['service_name'], 'swift')
+  $tenant              = pick($swift_hash['tenant'], 'services')
+
+  validate_string($public_address)
+  validate_string($password)
+
+  $public_url          = "${public_protocol}://${public_address}:8080/v1/AUTH_%(tenant_id)s"
+  $admin_url           = "http://${admin_address}:8080/v1/AUTH_%(tenant_id)s"
 
 # Amazon S3 endpoints
-$public_url_s3       = "${public_protocol}://${public_address}:8080"
-$admin_url_s3        = "http://${admin_address}:8080"
+  $public_url_s3       = "${public_protocol}://${public_address}:8080"
+  $admin_url_s3        = "http://${admin_address}:8080"
 
-class { '::swift::keystone::auth':
-  password           => $password,
-  auth_name          => $auth_name,
-  configure_endpoint => $configure_endpoint,
-  service_name       => $service_name,
-  public_url         => $public_url,
-  internal_url       => $admin_url,
-  admin_url          => $admin_url,
-  public_url_s3      => $public_url_s3,
-  internal_url_s3    => $admin_url_s3,
-  admin_url_s3       => $admin_url_s3,
-  region             => $region,
+  class { '::swift::keystone::auth':
+    password           => $password,
+    auth_name          => $auth_name,
+    configure_endpoint => $configure_endpoint,
+    service_name       => $service_name,
+    public_url         => $public_url,
+    internal_url       => $admin_url,
+    admin_url          => $admin_url,
+    public_url_s3      => $public_url_s3,
+    internal_url_s3    => $admin_url_s3,
+    admin_url_s3       => $admin_url_s3,
+    region             => $region,
+  }
 }
--- tests/noop/spec/hosts/swift/keystone_spec.rb
+++ tests/noop/spec/hosts/swift/keystone_spec.rb
@@ -4,38 +4,42 @@ manifest = 'swift/keystone.pp'
 
 describe manifest do
   shared_examples 'catalog' do
-    it 'should set empty trusts_delegated_roles for swift auth' do
-      contain_class('swift::keystone::auth')
-    end
+    storage_hash = Noop.hiera_hash('storage')
 
-    public_vip           = Noop.hiera('public_vip')
-    admin_address        = Noop.hiera('management_vip')
-    public_ssl           = Noop.hiera_structure('public_ssl/services')
+    if !storage_hash['objects_ceph']
+      it 'should set empty trusts_delegated_roles for swift auth' do
+        contain_class('swift::keystone::auth')
+      end
 
-    if public_ssl
-      public_address  = Noop.hiera_structure('public_ssl/hostname')
-      public_protocol = 'https'
-    else
-      public_address  = public_vip
-      public_protocol = 'http'
-    end
+      public_vip           = Noop.hiera('public_vip')
+      admin_address        = Noop.hiera('management_vip')
+      public_ssl           = Noop.hiera_structure('public_ssl/services')
 
-    public_url          = "#{public_protocol}://#{public_address}:8080/v1/AUTH_%(tenant_id)s"
-    admin_url           = "http://#{admin_address}:8080/v1/AUTH_%(tenant_id)s"
+      if public_ssl
+        public_address  = Noop.hiera_structure('public_ssl/hostname')
+        public_protocol = 'https'
+      else
+        public_address  = public_vip
+        public_protocol = 'http'
+      end
 
-    public_url_s3       = "#{public_protocol}://#{public_address}:8080"
-    admin_url_s3        = "http://#{admin_address}:8080"
+      public_url          = "#{public_protocol}://#{public_address}:8080/v1/AUTH_%(tenant_id)s"
+      admin_url           = "http://#{admin_address}:8080/v1/AUTH_%(tenant_id)s"
 
-    it 'class swift::keystone::auth should contain correct *_url' do
-      should contain_class('swift::keystone::auth').with('public_url' => public_url)
-      should contain_class('swift::keystone::auth').with('admin_url' => admin_url)
-      should contain_class('swift::keystone::auth').with('internal_url' => admin_url)
-    end
+      public_url_s3       = "#{public_protocol}://#{public_address}:8080"
+      admin_url_s3        = "http://#{admin_address}:8080"
 
-    it 'class swift::keystone::auth should contain correct S3 endpoints' do
-      should contain_class('swift::keystone::auth').with('public_url_s3' => public_url_s3)
-      should contain_class('swift::keystone::auth').with('admin_url_s3' => admin_url_s3)
-      should contain_class('swift::keystone::auth').with('internal_url_s3' => admin_url_s3)
+      it 'class swift::keystone::auth should contain correct *_url' do
+        should contain_class('swift::keystone::auth').with('public_url' => public_url)
+        should contain_class('swift::keystone::auth').with('admin_url' => admin_url)
+        should contain_class('swift::keystone::auth').with('internal_url' => admin_url)
+      end
+
+      it 'class swift::keystone::auth should contain correct S3 endpoints' do
+        should contain_class('swift::keystone::auth').with('public_url_s3' => public_url_s3)
+        should contain_class('swift::keystone::auth').with('admin_url_s3' => admin_url_s3)
+        should contain_class('swift::keystone::auth').with('internal_url_s3' => admin_url_s3)
+      end
     end
   end
 
