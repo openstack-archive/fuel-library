d7df4f82ada70def19364ac562d1b6f860b3f50a
Oleksiy Molchanov <omolchanov@mirantis.com>
2016-06-23 16:00:01 +0300

    Add rabbitmq service check
    * Check whether rabbitmq is online before doing any rabbitmq queries
    * Add new provider for custom service checks
    
    Change-Id: I1fb27ab2d731dc3fa7daa3694c9da3a01f318695
    Closes-Bug: 1591213

---
+++ deployment/puppet/osnailyfacter/lib/puppet/provider/service_status/rabbit_status.rb
@@ -0,0 +1,42 @@
+Puppet::Type.type(:rabbit_status).provide(:ruby) do
+  desc 'Wait for custom service to become online'
+
+  defaultfor :kernel => :linux
+
+  # get the current custom service status value
+  # @return [:online, :offline]
+  def ensure
+    debug 'Call: ensure'
+    out = status
+    debug "Return: #{out}"
+    out
+  end
+
+  # wait for custom service to switch to needed state
+  # @param value [:online, :offline]
+  def ensure=(value)
+    debug "Call: ensure=(#{value})"
+    debug "Waiting for custome service: '#{@resource[:name]}' to change its status to: '#{value}'"
+    @resource[:count].times do
+      if status == value
+        return true
+      end
+      sleep @resource[:step]
+    end
+    fail "Timeout waiting for custom service: '#{@resource[:name]}' to become: '#{value}' after #{@resource[:count] * @resource[:step]} seconds!"
+  end
+
+  # get custom service status
+  # @return [:online, :offline]
+  def status
+    rv = system(@resource[:check_cmd])
+    status = $?.exitstatus
+    debug "Got status: '#{status}'"
+    if status == @resource[:exitcode]
+      :online
+    else
+      :offline
+    end
+  end
+
+end
