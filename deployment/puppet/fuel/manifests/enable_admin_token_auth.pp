a7b4bfd93a97ea43624ae99d68dbe5f0e7f3a6f4
Dmitry Ilyin <dilyin@mirantis.com>
2016-06-27 20:17:39 +0300

    Ensure that keystone has admin token enabled
    * Ensure that admin_token_auth filter is enabled
      in the paste file before working with keystone
      Puppet types.
    
    Change-Id: I2baad2f6f775f3aa9c6c9624ec53f08fed479fb1
    Closes-Bug: 1596622

---
+++ deployment/puppet/fuel/manifests/enable_admin_token_auth.pp
@@ -0,0 +1,37 @@
+# Enable the admin_token_auth in the keystone paste.ini file
+#
+class fuel::enable_admin_token_auth {
+
+  ini_subsetting { 'public_api/admin_token_auth':
+    ensure     => 'present',
+    path       => '/etc/keystone/keystone-paste.ini',
+    section    => 'pipeline:public_api',
+    setting    => 'pipeline',
+    subsetting => 'admin_token_auth',
+  }
+
+  ini_subsetting { 'admin_api/admin_token_auth':
+    ensure     => 'present',
+    path       => '/etc/keystone/keystone-paste.ini',
+    section    => 'pipeline:admin_api',
+    setting    => 'pipeline',
+    subsetting => 'admin_token_auth',
+  }
+
+  ini_subsetting { 'api_v3/admin_token_auth':
+    ensure     => 'present',
+    path       => '/etc/keystone/keystone-paste.ini',
+    section    => 'pipeline:api_v3',
+    setting    => 'pipeline',
+    subsetting => 'admin_token_auth',
+  }
+
+  Package <| title == 'keystone' |> ->
+  Ini_subsetting[
+    'public_api/admin_token_auth',
+    'admin_api/admin_token_auth',
+    'api_v3/admin_token_auth'
+  ] ~>
+  Service <| title == 'keystone' or title == 'httpd' |>
+
+}
