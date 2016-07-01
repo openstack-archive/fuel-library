a55cbdb8c812a3d1c4fd5be3a42d11b0e26809b9
Maksim Malchuk <mmalchuk@mirantis.com>
2016-07-02 00:55:38 +0300

    Add atop configuration settings
    Now the atop setting are configurable from UI and can be overriden in
    hiera. Also, this change adds idempotency and fix noop tests.
    
    Conflicts:
    	deployment/puppet/osnailyfacter/manifests/tools/tools.pp has not
            presented in 8.0 yet so changes has been moved to
            deployment/puppet/osnailyfacter/modular/tools/tools.pp
    	tests/noop/spec/hosts/tools/tools_spec.rb
    
    DocImpact
    Depends-On: Ie0e69f58183193455d5d036b45668a3476f1a693
    Change-Id: I59cea9ea5ca31c20db2cf3eaaebe09bad0abdcf8
    Closes-Bug: #1597250
    Signed-off-by: Maksim Malchuk <mmalchuk@mirantis.com>
    (cherry picked from commit a7f03d7c55446dd035b499046b6ffc86ec112bed)

--- tests/noop/spec/hosts/tools/tools_spec.rb
+++ tests/noop/spec/hosts/tools/tools_spec.rb
@@ -22,6 +22,11 @@ cloud_init_services = [
   'cloud-log-shutdown',
 ]
 
+atop_hash     = Noop.hiera 'atop', {}
+atop_enabled  = Noop.puppet_function 'pick', atop_hash['service_enabled'], true
+atop_interval = Noop.puppet_function 'pick', atop_hash['interval'], 20
+atop_rotate   = Noop.puppet_function 'pick', atop_hash['rotate'], 7
+
 puppet = Noop.hiera('puppet')
 
 describe manifest do
@@ -37,7 +42,11 @@ describe manifest do
     end
 
     it 'should declare tools classes' do
-      should contain_class('osnailyfacter::atop')
+      should contain_class('osnailyfacter::atop').with(
+          'service_enabled' => atop_enabled,
+          'interval'        => atop_interval,
+          'rotate'          => atop_rotate,
+      )
       should contain_class('osnailyfacter::ssh')
       should contain_class('osnailyfacter::puppet_pull').with(
         'modules_source'   => puppet['modules'],
