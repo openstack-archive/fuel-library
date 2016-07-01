3d6aa6140b03072f79210b3cb6d982d4b8e749db
Maksim Malchuk <mmalchuk@mirantis.com>
2016-07-02 00:55:38 +0300

    Add atop configuration settings
    Now the atop setting are configurable from UI and can be overriden in
    hiera. Also, this change adds idempotency and fix noop tests.
    
    DocImpact
    Depends-On: Ie0e69f58183193455d5d036b45668a3476f1a693
    Change-Id: I59cea9ea5ca31c20db2cf3eaaebe09bad0abdcf8
    Closes-Bug: #1597250
    Signed-off-by: Maksim Malchuk <mmalchuk@mirantis.com>
    (cherry picked from commit a7f03d7c55446dd035b499046b6ffc86ec112bed)
    (cherry picked from commit a55cbdb8c812a3d1c4fd5be3a42d11b0e26809b9)

--- tests/noop/spec/hosts/tools/tools_spec.rb
+++ tests/noop/spec/hosts/tools/tools_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 require 'shared-examples'
 manifest = 'tools/tools.pp'
 
+atop_hash     = Noop.hiera 'atop', {}
+atop_enabled  = Noop.puppet_function 'pick', atop_hash['service_enabled'], true
+atop_interval = Noop.puppet_function 'pick', atop_hash['interval'], 20
+atop_rotate   = Noop.puppet_function 'pick', atop_hash['rotate'], 7
+
 describe manifest do
   shared_examples 'catalog' do
     it "should contain ssh host keygen exec for Debian OS only" do
@@ -13,8 +18,15 @@ describe manifest do
         should_not contain_exec('host-ssh-keygen')
       end
     end
+
+    it 'should declare tools classes' do
+      should contain_class('osnailyfacter::atop').with(
+          'service_enabled' => atop_enabled,
+          'interval'        => atop_interval,
+          'rotate'          => atop_rotate,
+      )
+    end
   end
 
   test_ubuntu_and_centos manifest
 end
-
