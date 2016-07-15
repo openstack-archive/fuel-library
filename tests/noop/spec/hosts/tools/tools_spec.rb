16d4569e6918fef37e47f404fcc0c892374d0596
Maksim Malchuk <mmalchuk@mirantis.com>
2016-07-15 18:40:56 +0300

    Don't configure the Postfix MTA for Ubuntu 16.04 LTS
    The bootstrap with Ubuntu 16.04 LTS doesn't contain the Postfix MTA
    by default. This commit set the configuration for the Postfix only
    in the older Fuel releases where Postfix is exist.  Also, this change
    configures Postfix to Local delivery mode only.
    
    Change-Id: I0d47a102ed36b6ab69874cd92735cd46dcf5f593
    Closes-Bug: 1603478
    Signed-off-by: Maksim Malchuk <mmalchuk@mirantis.com>
    Co-Authored-By: Alex Schultz <aschultz@mirantis.com>

--- tests/noop/spec/hosts/tools/tools_spec.rb
+++ tests/noop/spec/hosts/tools/tools_spec.rb
@@ -68,15 +68,23 @@ describe manifest do
       )
     end
 
-    it 'should configure postfix with correct hostname' do
-      should contain_service('postfix')
-      should contain_augeas('configure postfix').with(
-        'context' => '/files/etc/postfix/main.cf',
-        'changes' => [
-          "set /files/etc/postfix/main.cf/mydestination #{facts[:fqdn]},localhost",
-          "set /files/etc/postfix/main.cf/myhostname #{facts[:fqdn]}",
-        ],
-      ).that_notifies('Service[postfix]')
+    unless Puppet::Util::Package.versioncmp(facts[:operatingsystemmajrelease], '16') >= 0
+
+      it 'should configure postfix with correct settings' do
+        should contain_package('postfix')
+        should contain_service('postfix')
+        should contain_augeas('configure postfix').with(
+          'context' => '/files/etc/postfix/main.cf',
+          'changes' => [
+            "set /files/etc/postfix/main.cf/mydestination #{facts[:fqdn]},localhost",
+            "set /files/etc/postfix/main.cf/myhostname #{facts[:fqdn]}",
+            "set /files/etc/postfix/main.cf/inet_interfaces loopback-only",
+            "set /files/etc/postfix/main.cf/default_transport error",
+            "set /files/etc/postfix/main.cf/relay_transport error",
+          ],
+        ).that_notifies('Service[postfix]')
+      end
+
     end
 
     it 'should declare osnailyfacter::acpid on virtual machines' do
