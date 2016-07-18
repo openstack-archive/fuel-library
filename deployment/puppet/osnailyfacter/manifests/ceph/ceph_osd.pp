bc7584d4d675251b5778b450e8852e6cd63eba8d
Oleksiy Molchanov <omolchanov@mirantis.com>
2016-07-18 17:07:53 +0300

    Moving to upstream ceph
    This backport includes all 4 ceph-related patches that were
    applied to master branch and give possibility to use puppet
    upstream ceph module:
    206192232c7a702b66d3493b8b5848707dcd5733
    4790ef88dedd2d1d1f90477997224eda70ae8f39
    8f607eef918ce14235766c1b784ee48dd560a9c6
    9d6023cf772d87f9cd0f99958843d2ae16879784
    
    Change-Id: Icb2c7b68cdf23d6df24f426a33c73a8d7d41901f
    Closes-Bug: 1603455

--- deployment/puppet/osnailyfacter/manifests/ceph/ceph_osd.pp
+++ deployment/puppet/osnailyfacter/manifests/ceph/ceph_osd.pp
@@ -1,67 +1,112 @@
 class osnailyfacter::ceph::ceph_osd {
 
-  notice('MODULAR: ceph/ceph_osd.pp')
+  notice('MODULAR: ceph-osd.pp')
 
-  # Pulling hiera
-  $storage_hash              = hiera_hash('storage', {})
-  $verbose                   = pick($storage_hash['verbose'], true)
+  $storage_hash              = hiera('storage', {})
+  $admin_key                 = $storage_hash['admin_key']
+  $bootstrap_osd_key         = $storage_hash['bootstrap_osd_key']
+  $fsid                      = $storage_hash['fsid']
+  $osd_pool_default_size     = $storage_hash['osd_pool_size']
+  $osd_pool_default_pg_num   = $storage_hash['pg_num']
+  $osd_pool_default_pgp_num  = $storage_hash['pg_num']
+  $osd_pool_default_min_size = pick($storage_hash['osd_pool_default_min_size'], '1')
+  $osd_journal_size          = pick($storage_hash['osd_journal_size'],  '2048')
   $debug                     = pick($storage_hash['debug'], hiera('debug', true))
-  $network_scheme            = hiera_hash('network_scheme', {})
-  $use_syslog                = hiera('use_syslog', true)
-  $mon_address_map           = get_node_to_ipaddr_map_by_network_role(hiera_hash('ceph_monitor_nodes'), 'ceph/public')
-  $ceph_primary_monitor_node = hiera('ceph_primary_monitor_node')
-  $primary_mons              = keys($ceph_primary_monitor_node)
-  $primary_mon               = $ceph_primary_monitor_node[$primary_mons[0]]['name']
-  prepare_network_config($network_scheme)
-  $ceph_cluster_network      = get_network_role_property('ceph/replication', 'network')
-  $ceph_public_network       = get_network_role_property('ceph/public', 'network')
-  $ceph_tuning_settings_hash = hiera_hash('ceph_tuning_settings', {})
+  $ceph_tuning_settings      = hiera('ceph_tuning_settings', {})
+  $ssl_hash                  = hiera_hash('use_ssl', {})
+
+  $filestore_xattr_use_omap           = pick($storage_hash['filestore_xattr_use_omap'], true)
+  $osd_recovery_max_active            = pick($storage_hash['osd_recovery_max_active'], '1')
+  $osd_max_backfills                  = pick($storage_hash['osd_max_backfills'], '1')
+  $rbd_cache_writethrough_until_flush = pick($storage_hash['rbd_cache_writethrough_until_flush'], true)
+  $rbd_cache                          = pick($storage_hash['rbd_cache'], true)
+  $log_to_syslog                      = hiera('use_syslog', true)
+  $log_to_syslog_level                = pick($storage_hash['ceph_syslog_level'], 'info')
+  $log_to_syslog_facility             = pick($storage_hash['ceph_syslog_facility'], 'LOG_LOCAL0')
+
+  prepare_network_config(hiera_hash('network_scheme'))
+  $ceph_cluster_network = get_network_role_property('ceph/replication', 'network')
+  $ceph_public_network  = get_network_role_property('ceph/public', 'network')
+
+  $mon_address_map     = get_node_to_ipaddr_map_by_network_role(hiera_hash('ceph_monitor_nodes'), 'ceph/public')
+  $mon_host            = join(values($mon_address_map), ',')
+  $mon_initial_members = join(keys($mon_address_map), ',')
+
+  if empty($admin_key) {
+    fail('Please provide admin_key')
+  }
+  if empty($bootstrap_osd_key) {
+    fail('Please provide bootstrap_osd_key')
+  }
+  if empty($fsid) {
+    fail('Please provide fsid')
+  }
 
   class { '::ceph':
-    primary_mon              => $primary_mon,
-    mon_hosts                => keys($mon_address_map),
-    mon_ip_addresses         => values($mon_address_map),
-    osd_pool_default_size    => $storage_hash['osd_pool_size'],
-    osd_pool_default_pg_num  => $storage_hash['pg_num'],
-    osd_pool_default_pgp_num => $storage_hash['pg_num'],
-    use_rgw                  => false,
-    glance_backend           => $glance_backend,
-    cluster_network          => $ceph_cluster_network,
-    public_network           => $ceph_public_network,
-    use_syslog               => $use_syslog,
-    syslog_log_level         => hiera('syslog_log_level_ceph', 'info'),
-    syslog_log_facility      => hiera('syslog_log_facility_ceph','LOG_LOCAL0'),
-    ephemeral_ceph           => $storage_hash['ephemeral_ceph'],
+    fsid                      => $fsid,
+    mon_initial_members       => $mon_initial_members,
+    mon_host                  => $mon_host,
+    cluster_network           => $ceph_cluster_network,
+    public_network            => $ceph_public_network,
+    osd_pool_default_size     => $osd_pool_default_size,
+    osd_pool_default_pg_num   => $osd_pool_default_pg_num,
+    osd_pool_default_pgp_num  => $osd_pool_default_pgp_num,
+    osd_pool_default_min_size => $osd_pool_default_min_size,
+    osd_journal_size          => $osd_journal_size,
   }
 
-  if $ceph_tuning_settings_hash != {} {
-    ceph_conf {
+  ceph_config {
+    'global/filestore_xattr_use_omap'           : value => $filestore_xattr_use_omap;
+    'global/osd_recovery_max_active'            : value => $osd_recovery_max_active;
+    'global/osd_max_backfills'                  : value => $osd_max_backfills;
+    'client/rbd_cache_writethrough_until_flush' : value => $rbd_cache_writethrough_until_flush;
+    'client/rbd_cache'                          : value => $rbd_cache;
+    'global/log_to_syslog'                      : value => $log_to_syslog;
+    'global/log_to_syslog_level'                : value => $log_to_syslog_level;
+    'global/log_to_syslog_facility'             : value => $log_to_syslog_facility;
+  }
+
+  Ceph::Key {
+    inject => false,
+  }
+
+  ceph::key { 'client.admin':
+    secret  => $admin_key,
+    cap_mon => 'allow *',
+    cap_osd => 'allow *',
+    cap_mds => 'allow',
+  }
+
+  ceph::key {'client.bootstrap-osd':
+    keyring_path => '/var/lib/ceph/bootstrap-osd/ceph.keyring',
+    secret       => $bootstrap_osd_key,
+  }
+
+  $osd_devices_hash = osd_devices_hash($::osd_devices_list)
+
+  class { '::ceph::osds':
+    args => $osd_devices_hash,
+  }
+
+  if $ceph_tuning_settings != {} {
+    ceph_config {
       'global/debug_default'                    : value => $debug;
-      'global/max_open_files'                   : value => $ceph_tuning_settings_hash['max_open_files'];
-      'osd/osd_mkfs_type'                       : value => $ceph_tuning_settings_hash['osd_mkfs_type'];
-      'osd/osd_mount_options_xfs'               : value => $ceph_tuning_settings_hash['osd_mount_options_xfs'];
-      'osd/osd_op_threads'                      : value => $ceph_tuning_settings_hash['osd_op_threads'];
-      'osd/filestore_queue_max_ops'             : value => $ceph_tuning_settings_hash['filestore_queue_max_ops'];
-      'osd/filestore_queue_committing_max_ops'  : value => $ceph_tuning_settings_hash['filestore_queue_committing_max_ops'];
-      'osd/journal_max_write_entries'           : value => $ceph_tuning_settings_hash['journal_max_write_entries'];
-      'osd/journal_queue_max_ops'               : value => $ceph_tuning_settings_hash['journal_queue_max_ops'];
-      'osd/objecter_inflight_ops'               : value => $ceph_tuning_settings_hash['objecter_inflight_ops'];
-      'osd/filestore_queue_max_bytes'           : value => $ceph_tuning_settings_hash['filestore_queue_max_bytes'];
-      'osd/filestore_queue_committing_max_bytes': value => $ceph_tuning_settings_hash['filestore_queue_committing_max_bytes'];
-      'osd/journal_max_write_bytes'             : value => $ceph_tuning_settings_hash['journal_queue_max_bytes'];
-      'osd/journal_queue_max_bytes'             : value => $ceph_tuning_settings_hash['journal_queue_max_bytes'];
-      'osd/ms_dispatch_throttle_bytes'          : value => $ceph_tuning_settings_hash['ms_dispatch_throttle_bytes'];
-      'osd/objecter_infilght_op_bytes'          : value => $ceph_tuning_settings_hash['objecter_infilght_op_bytes'];
-      'osd/filestore_max_sync_interval'         : value => $ceph_tuning_settings_hash['filestore_max_sync_interval'];
+      'global/max_open_files'                   : value => $ceph_tuning_settings['max_open_files'];
+      'osd/osd_mkfs_type'                       : value => $ceph_tuning_settings['osd_mkfs_type'];
+      'osd/osd_mount_options_xfs'               : value => $ceph_tuning_settings['osd_mount_options_xfs'];
+      'osd/osd_op_threads'                      : value => $ceph_tuning_settings['osd_op_threads'];
+      'osd/filestore_queue_max_ops'             : value => $ceph_tuning_settings['filestore_queue_max_ops'];
+      'osd/filestore_queue_committing_max_ops'  : value => $ceph_tuning_settings['filestore_queue_committing_max_ops'];
+      'osd/journal_max_write_entries'           : value => $ceph_tuning_settings['journal_max_write_entries'];
+      'osd/journal_queue_max_ops'               : value => $ceph_tuning_settings['journal_queue_max_ops'];
+      'osd/objecter_inflight_ops'               : value => $ceph_tuning_settings['objecter_inflight_ops'];
+      'osd/filestore_queue_max_bytes'           : value => $ceph_tuning_settings['filestore_queue_max_bytes'];
+      'osd/filestore_queue_committing_max_bytes': value => $ceph_tuning_settings['filestore_queue_committing_max_bytes'];
+      'osd/journal_max_write_bytes'             : value => $ceph_tuning_settings['journal_queue_max_bytes'];
+      'osd/journal_queue_max_bytes'             : value => $ceph_tuning_settings['journal_queue_max_bytes'];
+      'osd/ms_dispatch_throttle_bytes'          : value => $ceph_tuning_settings['ms_dispatch_throttle_bytes'];
+      'osd/objecter_infilght_op_bytes'          : value => $ceph_tuning_settings['objecter_infilght_op_bytes'];
+      'osd/filestore_max_sync_interval'         : value => $ceph_tuning_settings['filestore_max_sync_interval'];
     }
-    # File /root/ceph.conf is symlink which is created after /etc/ceph/ceph.conf in ceph::conf class
-    File<| title == '/root/ceph.conf' |> -> Ceph_conf <||>
   }
-
-  # TODO(bogdando) add monit ceph-osd services monitoring, if required
-
-  #################################################################
-
-  # vim: set ts=2 sw=2 et :
-
 }
