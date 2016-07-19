d46070c6aca479a0c267add9024b4633292a69ef
Alex Schultz <aschultz@mirantis.com>
2016-07-19 11:07:33 -0600

    Update upload_cirros to support multiple images
    This change creates a puppet parser function to convert the
    test_vm_image from hiera into a glance_image compatible array of hashes
    that can be used with create_resources. Previously vmware deployments
    included an extra TestVM to be uploaded but during the rewrite of the
    upload_cirros task this ability was dropped.
    
    Change-Id: I3f0f6da32134a4a15f2c4764c5334bb8e85309c8
    Closes-Bug: #1603393

---
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/generate_glance_images.rb
@@ -0,0 +1,31 @@
+module Puppet::Parser::Functions
+  newfunction(
+      :generate_glance_images,
+      :type  => :rvalue,
+      :arity => 1,
+      :doc   => <<-EOS
+Takes an array of glance images (in form used in astute.yaml) as argument.
+Returns a hash compatible with the glance_image type provided by the glance
+module.
+      EOS
+  ) do |args|
+    images = args[0]
+    raise Puppet::ParseError, "generate_glance_images(): Requires an array to work with" unless images.is_a? Array
+
+    result = {}
+    images.each do |image|
+
+      params = {
+        'container_format' => image['container_format'],
+        'disk_format'      => image['disk_format'],
+        'is_public'        => image['public'],
+        'min_ram'          => image['min_ram'],
+        'source'           => image['img_path'],
+        'properties'       => image['properties'],
+      }
+
+      result.store image['img_name'], params
+    end
+    result
+  end
+end
