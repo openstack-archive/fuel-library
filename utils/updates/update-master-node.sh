daf9c2f3cf0a16309e31223898c08a66a4248271
Oleksiy Molchanov <omolchanov@mirantis.com>
2016-07-22 19:34:50 +0300

    Add update-master-node script
    This is initial script for updating fuel 9.x. In future
    it will use latest parameter for fuel packages in puppet.
    
    Change-Id: Ic4ef722b861d260c3679dca9c74f6cc62052c376
    Closes-Bug: 1605602

---
+++ utils/updates/update-master-node.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+yum -y update
+
+TASKS="
+hiera
+host
+cobbler
+postgresql
+mcollective
+astute
+keystone
+keystone_token_disable
+rabbitmq
+nailgun
+ostf
+nginx_repo
+nginx_services
+client
+rsyslog
+puppetsync
+"
+
+for task in $TASKS; do
+    cat <<EOF
+##################################
+DEPLOYMENT TASK: $task
+##################################
+EOF
+    puppet apply -d -v --color false --detailed-exitcodes \
+        "/etc/puppet/modules/fuel/examples/${task}.pp"
+    PUPPET_RUN=$?
+    if [[ $PUPPET_RUN -eq 1 ]] || [[ $PUPPET_RUN -gt 2 ]]; then
+        echo "The were failures while running task: ${task} with exit code: ${PUPPET_RUN}"
+        exit 1
+    else
+        echo "Deployment task has succeeded: ${task} with exit code: ${PUPPET_RUN}"
+    fi
+done
+
+SERVICES="
+astute
+cobblerd
+mcollective
+nailgun
+nginx
+openstack-keystone
+ostf
+postgresql
+rabbitmq-server
+"
+
+for service in $SERVICES; do
+echo "Restarting $service"
+systemctl restart $service
+done
