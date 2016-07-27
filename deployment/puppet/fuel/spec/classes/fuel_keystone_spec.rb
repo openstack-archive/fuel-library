6dfdadaeae2977fdb806317b7e58e26800694a3c
Alex Schultz <aschultz@mirantis.com>
2016-07-27 08:05:19 -0600

    [sync/mitaka] Set upper limit on keystone workers
    This change sets an upper limit on the keystone workers to 16. It will
    use the processor count to determine the correct number of workers for
    smaller environments so it uses the smaller of the values between
    processorcount or 16. This change sets the number of admin workers and
    public workers to the lesser of these two values for a maximum of 32
    workers total.
    
    Change-Id: Ibbc2704441806776ded62e83b26b043188ad1a42
    Closes-Bug: #1602434

--- deployment/puppet/fuel/spec/classes/fuel_keystone_spec.rb
+++ deployment/puppet/fuel/spec/classes/fuel_keystone_spec.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require "spec_helper"
 
 describe "fuel::keystone" do
@@ -61,3 +62,28 @@ describe "fuel::keystone" do
 
 end
 
+=======
+require 'spec_helper'
+
+describe 'fuel::keystone', :type => :class do
+  context 'on supported platform' do
+    let(:facts) {{
+      :osfamily               => 'RedHat',
+      :lsbdistid              => 'CentOS',
+      :operatingsystem        => 'CentOS',
+      :operatingsystemrelease => '7.2',
+      :processorcount         => 48,
+    }}
+    context 'with default parameters' do
+      describe 'limits keystone workers' do
+        it {
+          should contain_class('keystone').with(
+            :public_workers => 16,
+            :admin_workers  => 16
+          )
+        }
+      end
+    end
+  end
+end
+>>>>>>> 7bd5878... Set upper limit on keystone workers
