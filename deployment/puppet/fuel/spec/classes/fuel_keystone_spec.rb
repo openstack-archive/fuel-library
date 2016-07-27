3acec74f4e855f0c444943fafd6e2b0adc5a163a
Alex Schultz <aschultz@mirantis.com>
2016-07-27 08:05:19 -0600

    Set upper limit on keystone workers
    This change sets an upper limit on the keystone workers to 16. It will
    use the processor count to determine the correct number of workers for
    smaller environments so it uses the smaller of the values between
    processorcount or 16.
    
    Change-Id: Ibbc2704441806776ded62e83b26b043188ad1a42
    Closes-Bug: #1602434

---
+++ deployment/puppet/fuel/spec/classes/fuel_keystone_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'fuel::keystone', :type => :class do
+  context 'on supported platform' do
+    let(:facts) {{
+      :osfamily               => 'RedHat',
+      :lsbdistid              => 'CentOS',
+      :operatingsystem        => 'CentOS',
+      :operatingsystemrelease => '7.2',
+      :processorcount         => 48,
+    }}
+    context 'with default parameters' do
+      describe 'limits keystone workers' do
+        it {
+          should contain_class('keystone').with_workers(16)
+        }
+      end
+    end
+  end
+end
