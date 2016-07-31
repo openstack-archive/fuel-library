f316e042b4a3e3c78cac6f83a99a5e18cb6d93aa
Dmitry Teselkin <dteselkin@mirantis.com>
2016-07-31 15:10:28 +0300

    Update ubuntu_override tweak for systemd
    Change-Id: I81870293cb607b0c19eb8cb28f8f99902eed9d6a
    blueprint: mos-xenial

--- deployment/puppet/tweaks/manifests/ubuntu_service_override.pp
+++ deployment/puppet/tweaks/manifests/ubuntu_service_override.pp
@@ -19,39 +19,90 @@ define tweaks::ubuntu_service_override (
 ) {
   if $::operatingsystem == 'Ubuntu' {
     if ! is_pkg_installed($package_name) {
-      # https://people.debian.org/~hmh/invokerc.d-policyrc.d-specification.txt
-      # use policy-rc.d to really ensure services don't get started on
-      # installation as service override files are only used if a job
-      # configuration file exists (see man 5 init)
-      $policyrc_file = '/usr/sbin/policy-rc.d'
+      if $::service_provider == 'systemd' {
+        # Use 'systemd' mask / unmask to disable service actions
+        $systemd_service_file = "/etc/systemd/system/${service_name}.service"
 
-      # use ensure resource as we only want a single instance of the
-      # policy-rc.d file in the catalog
-      ensure_resource('file', 'create-policy-rc.d', {
-        ensure  => present,
-        path    => $policyrc_file,
-        content => "#!/bin/bash\nexit 101",
-        mode    => '0755',
-        owner   => 'root',
-        group   => 'root'
-      })
-      # use ensure resource as we only want a single remove exec in the catalog
-      ensure_resource('exec', 'remove-policy-rc.d', {
-        path    => [ '/sbin', '/bin', '/usr/bin', '/usr/sbin' ],
-        command => "rm -f ${policyrc_file}",
-        onlyif  => "test -f ${policyrc_file}",
-      })
+        ensure_resources(
+          'exec',
+          {
+            'systemctl-daemon-reload-on-disable' => {},
+            'systemctl-daemon-reload-on-enable'  => {}
+          },
+          {
+            path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+            command => 'systemctl daemon-reload',
+          }
+        )
 
-      File['create-policy-rc.d'] ->
-        Package <| name == $package_name |> { provider =>  'apt_fuel' } ->
-          Exec['remove-policy-rc.d']
-      File['create-policy-rc.d'] ->
-        Package <| title == $package_name |> { provider => 'apt_fuel' } ->
-          Exec['remove-policy-rc.d']
-      Exec['remove-policy-rc.d'] ->
-        Service <| name == $service_name |>
-      Exec['remove-policy-rc.d'] ->
-        Service <| title == $service_name |>
+        ensure_resource('file', "create-override-${service_name}", {
+          ensure => link,
+          backup => '.bak',
+          path   => $systemd_service_file,
+          target => '/dev/null',
+        })
+
+        ensure_resource('exec', "remove-override-${service_name}", {
+          path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+          command => "mv -f ${systemd_service_file}.bak ${systemd_service_file} || rm -f ${systemd_service_file}",
+          onlyif  => "test $(readlink -nf ${systemd_service_file}) = /dev/null",
+        })
+
+        File["create-override-${service_name}"] ->
+          Exec['systemctl-daemon-reload-on-disable'] ->
+            Exec["remove-override-${service_name}"] ->
+              Exec['systemctl-daemon-reload-on-enable']
+
+        File["create-override-${service_name}"] ->
+          Package <| name == $package_name |> { provider =>  'apt_fuel' } ->
+            Exec['systemctl-daemon-reload-on-disable']
+        File["create-override-${service_name}"] ->
+          Package <| title == $package_name |> { provider => 'apt_fuel' } ->
+            Exec['systemctl-daemon-reload-on-disable']
+
+        Exec['systemctl-daemon-reload-on-enable'] ->
+          Service <| name == $service_name |>
+        Exec['systemctl-daemon-reload-on-enable'] ->
+          Service <| title == $service_name |>
+      } else {
+        # Use 'sysvinit' policy-rc.d mechanism.
+        # This will not work with upstart if using commands like 'service xxx start'
+        # It was here before so using it as a kind of default.
+
+        # https://people.debian.org/~hmh/invokerc.d-policyrc.d-specification.txt
+        # use policy-rc.d to really ensure services don't get started on
+        # installation as service override files are only used if a job
+        # configuration file exists (see man 5 init)
+        $policyrc_file = '/usr/sbin/policy-rc.d'
+
+        # use ensure resource as we only want a single instance of the
+        # policy-rc.d file in the catalog
+        ensure_resource('file', 'create-policy-rc.d', {
+          ensure  => present,
+          path    => $policyrc_file,
+          content => "#!/bin/bash\nexit 101",
+          mode    => '0755',
+          owner   => 'root',
+          group   => 'root'
+        })
+        # use ensure resource as we only want a single remove exec in the catalog
+        ensure_resource('exec', 'remove-policy-rc.d', {
+          path    => [ '/sbin', '/bin', '/usr/bin', '/usr/sbin' ],
+          command => "rm -f ${policyrc_file}",
+          onlyif  => "test -f ${policyrc_file}",
+        })
+
+        File['create-policy-rc.d'] ->
+          Package <| name == $package_name |> { provider =>  'apt_fuel' } ->
+            Exec['remove-policy-rc.d']
+        File['create-policy-rc.d'] ->
+          Package <| title == $package_name |> { provider => 'apt_fuel' } ->
+            Exec['remove-policy-rc.d']
+        Exec['remove-policy-rc.d'] ->
+          Service <| name == $service_name |>
+        Exec['remove-policy-rc.d'] ->
+          Service <| title == $service_name |>
+      }
     }
   }
 }
