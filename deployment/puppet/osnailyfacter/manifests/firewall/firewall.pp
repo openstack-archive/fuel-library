9bc450b2bd516af37e5398ca069ca934ce0a6e73
slava <vvalyavskiy@mirantis.com>
2016-08-01 14:44:17 +0300

    Decomposition of keystone tasks
    Implements: blueprint role-decomposition
    Change-Id: I125065585554f17787079772febec05d0a063bf3

--- deployment/puppet/osnailyfacter/manifests/firewall/firewall.pp
+++ deployment/puppet/osnailyfacter/manifests/firewall/firewall.pp
@@ -275,6 +275,16 @@ class osnailyfacter::firewall::firewall {
     }
   }
 
+  $keystone_role = intersection($roles, hiera('keystone_roles'))
+  if $keystone_role {
+    openstack::firewall::multi_net {'102 keystone':
+      port        => [$keystone_public_port, $keystone_admin_port],
+      proto       => 'tcp',
+      action      => 'accept',
+      source_nets => $keystone_networks,
+    }
+  }
+
   $controller_role = intersection($roles, ['primary-controller', 'controller'])
   if $controller_role {
     firewall {'004 remote puppet ':
@@ -294,13 +304,6 @@ class osnailyfacter::firewall::firewall {
       dport  => [$http_port, $https_port],
       proto  => 'tcp',
       action => 'accept',
-    }
-
-    openstack::firewall::multi_net {'102 keystone':
-      port        => [$keystone_public_port, $keystone_admin_port],
-      proto       => 'tcp',
-      action      => 'accept',
-      source_nets => $keystone_networks,
     }
 
     firewall {'103 swift':
