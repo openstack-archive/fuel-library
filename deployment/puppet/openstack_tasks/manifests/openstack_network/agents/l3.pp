eb7382b39b7b8f4977f27ca8670e385e547bd517
Ivan Ponomarev <iponomarev@mirantis.com>
2016-08-08 14:26:33 +0300

    Decomposition of neutron tasks
    Implements: blueprint role-decomposition
    Change-Id: Ib65fe6327c252c82a1cbb8e5196a90a40193acd4

--- deployment/puppet/openstack_tasks/manifests/openstack_network/agents/l3.pp
+++ deployment/puppet/openstack_tasks/manifests/openstack_network/agents/l3.pp
@@ -5,15 +5,15 @@ class openstack_tasks::openstack_network::agents::l3 {
   $neutron_advanced_config = hiera_hash('neutron_advanced_configuration', { })
   $dvr = pick($neutron_advanced_config['neutron_dvr'], false)
 
-  $neutron_controller_roles = hiera('neutron_controller_roles', ['controller', 'primary-controller'])
+  $neutron_controller_roles = hiera('neutron_roles')
   $neutron_compute_roles    = hiera('neutron_compute_nodes', ['compute'])
-  $controller               = roles_include($neutron_controller_roles)
+  $neutron_controller       = roles_include($neutron_controller_roles)
   $compute                  = roles_include($neutron_compute_roles)
 
-  if $controller or ($dvr and $compute) {
+  if $neutron_controller or ($dvr and $compute) {
     # override neutron options
     $override_configuration = hiera_hash('configuration', {})
-  $override_values = values($override_configuration)
+    $override_values = values($override_configuration)
     if !empty($override_values) and has_key($override_values[0], 'data') {
       # Create resources of type 'override_resources'. These, in turn,
       # will either update existing resources in the catalog with new data,
@@ -26,12 +26,12 @@ class openstack_tasks::openstack_network::agents::l3 {
     }
   }
 
-  if $controller or ($dvr and $compute) {
+  if $neutron_controller or ($dvr and $compute) {
     $debug                   = hiera('debug', true)
     $metadata_port           = '8775'
     $network_scheme          = hiera_hash('network_scheme', {})
 
-    if $controller {
+    if $neutron_controller {
       if $dvr {
         $agent_mode = 'dvr_snat'
       } else {
@@ -56,9 +56,9 @@ class openstack_tasks::openstack_network::agents::l3 {
     }
 
     if ($ha_agent) and !($compute) {
-      $primary_controller = hiera('primary_controller')
+      $primary_neutron = has_primary_role(intersection(hiera('neutron_roles'), hiera('roles')))
       cluster::neutron::l3 { 'default-l3' :
-        primary => $primary_controller,
+        primary => $primary_neutron,
       }
     }
 
