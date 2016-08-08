8e1713e30933791653cced45d654f163ebb2b5fc
Ivan Ponomarev <iponomarev@mirantis.com>
2016-08-08 14:26:33 +0300

    Decomposition of neutron tasks
    Implements: blueprint role-decomposition
    Change-Id: Ib65fe6327c252c82a1cbb8e5196a90a40193acd4

--- deployment/puppet/osnailyfacter/manifests/globals/globals.pp
+++ deployment/puppet/osnailyfacter/manifests/globals/globals.pp
@@ -370,6 +370,14 @@ class osnailyfacter::globals::globals {
     $glance_known_stores = false
   }
 
+  # Define neutron-related variables:
+  $neutron_tagged_nodes = get_nodes_hash_by_tags($network_metadata, ['primary-modular-neutron', 'modular-neutron'])
+  $neutron_nodes = empty($neutron_tagged_nodes) ? {
+    true    => $controller_nodes,
+    false   => $neutron_tagged_nodes,
+    default => $controller_nodes,
+  }
+
   # Define keystone-related variables:
   $keystone_tagged_nodes = get_nodes_hash_by_tags($network_metadata, ['primary-modular-keystone', 'modular-keystone'])
   $keystone_nodes = empty($keystone_tagged_nodes) ? {
@@ -396,7 +404,8 @@ class osnailyfacter::globals::globals {
   $corosync_roles = hiera('corosync_roles', ['primary-controller', 'controller'])
   $corosync_tags = hiera('corosync_tags', ['primary-controller', 'controller',
                                            'primary-modular-rabbitmq', 'modular-rabbitmq',
-                                           'primary-modular-database', 'modular-database'])
+                                           'primary-modular-database', 'modular-database',
+                                           'primary-modular-neutron', 'modular-neutron'])
 
   # Define cinder-related variables
   # todo: use special node-roles instead controllers in the future
@@ -480,10 +489,6 @@ class osnailyfacter::globals::globals {
 
   # Define mongo-related variables
   $mongo_roles = ['primary-mongo', 'mongo']
-
-  # Define neutron-related variables:
-  # todo: use special node-roles instead controllers in the future
-  $neutron_nodes = $controller_nodes
 
   #Define Ironic-related variables:
   $ironic_api_nodes = $controller_nodes
