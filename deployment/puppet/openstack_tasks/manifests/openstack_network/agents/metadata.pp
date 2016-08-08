0c63a75590ccc30a13c1a87ebf3b92e91b631c84
Ivan Ponomarev <iponomarev@mirantis.com>
2016-08-08 14:26:33 +0300

    Decomposition of neutron tasks
    Implements: blueprint role-decomposition
    Change-Id: Ib65fe6327c252c82a1cbb8e5196a90a40193acd4

--- deployment/puppet/openstack_tasks/manifests/openstack_network/agents/metadata.pp
+++ deployment/puppet/openstack_tasks/manifests/openstack_network/agents/metadata.pp
@@ -2,9 +2,9 @@ class openstack_tasks::openstack_network::agents::metadata {
 
   notice('MODULAR: openstack_network/agents/metadata.pp')
 
-  $neutron_controller_roles = hiera('neutron_controller_roles', ['controller', 'primary-controller'])
+  $neutron_controller_roles = hiera('neutron_roles')
   $neutron_compute_roles    = hiera('neutron_compute_nodes', ['compute'])
-  $controller               = roles_include($neutron_controller_roles)
+  $neutron_controller       = roles_include($neutron_controller_roles)
   $compute                  = roles_include($neutron_compute_roles)
   $neutron_advanced_config  = hiera_hash('neutron_advanced_configuration', { })
   $neutron_config           = hiera_hash('neutron_config')
@@ -19,7 +19,7 @@ class openstack_tasks::openstack_network::agents::metadata {
                              min(max($::processorcount, 2), $workers_max))
   }
 
-  if $controller or ($dvr and $compute) {
+  if $neutron_controller or ($dvr and $compute) {
     # override neutron options
     $override_configuration = hiera_hash('configuration', {})
   $override_values = values($override_configuration)
@@ -35,7 +35,7 @@ class openstack_tasks::openstack_network::agents::metadata {
     }
   }
 
-  if $controller or ($dvr and $compute) {
+  if $neutron_controller or ($dvr and $compute) {
     $debug                  = hiera('debug', true)
     $ha_agent               = dig44($neutron_advanced_config, ['metadata_agent_ha'], true)
     $service_endpoint       = hiera('service_endpoint')
@@ -59,9 +59,9 @@ class openstack_tasks::openstack_network::agents::metadata {
     }
 
     if ($ha_agent) and !($compute) {
-      $primary_controller = hiera('primary_controller')
+      $primary_neutron = has_primary_role(intersection(hiera('neutron_roles'), hiera('roles')))
       class { '::cluster::neutron::metadata' :
-        primary => $primary_controller,
+        primary => $primary_neutron,
       }
     }
 
