ce8b0692bdc5d3a3e5209f0737225e95a93ad3f6
Ivan Ponomarev <iponomarev@mirantis.com>
2016-08-08 14:26:33 +0300

    Decomposition of neutron tasks
    Implements: blueprint role-decomposition
    Change-Id: Ib65fe6327c252c82a1cbb8e5196a90a40193acd4

--- tests/noop/spec/hosts/openstack-network/server-config_spec.rb
+++ tests/noop/spec/hosts/openstack-network/server-config_spec.rb
@@ -55,7 +55,7 @@ describe manifest do
       compute                          = Noop.puppet_function 'roles_include', neutron_compute_roles
 
       it 'Configure database options for neutron::server' do
-        sync_db     = Noop.hiera('primary_controller')
+        sync_db     = Noop.hiera('primary_neutron')
         db_type     = 'mysql'
         db_password = neutron_config.fetch('database', {}).fetch('passwd')
         db_user     = neutron_config.fetch('database', {}).fetch('user', 'neutron')
