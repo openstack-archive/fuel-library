ed268b63ae37930f47b5b23a16f8069f7874a792
Alexander Arzhanov <aarzhanov@mirantis.com>
2016-08-11 17:15:41 +0300

    Configure Glance vSphere backend, compute-vmware, cinder-vmware SSL verification settings
    These changes will allow the cover use cases for
    cinder-vmware, compute-vmware and Glance vSphere
    backend:
    
    1. Bypass vCenter certificate verification
    (default). Certificate verification turn off.
    This case is useful for faster deployment and
    for testing environment.
    
    2. vCenter is using a Self-Signed certificate.
    In this case the user must upload custom CA bundle
    file certificate.
    
    3. vCenter server certificate was emitted by know
    CA (e.g.GeoTrust). In this case user have to leave
    CA certificate bundle upload field empty.
    
    Change-Id: I34e3396fe68efd73f7e9a9534c7eb9a5e34477e0
    Implements: blueprint custom-ca-bundle-verify-vcenter-cert

--- deployment/puppet/vmware/manifests/compute_vmware.pp
+++ deployment/puppet/vmware/manifests/compute_vmware.pp
@@ -1,25 +1,107 @@
-#    Copyright 2015 Mirantis, Inc.
-
-# FIXME(mattymo): Use standard class documentation format
-# vmware::compute_vmware resource deploys nova-compute service and configures it for use
-# with vmwareapi.VCDriver (vCenter server as hypervisor).  Depends on nova::params class.
-
-# Variables:
-# availability_zone_name - availability zone which nova-compute will be assigned
-# vc_cluster             - name of vSphere cluster
-# vc_host                - IP address or hostname of the vCenter server
-# vc_user                - username for vCenter server
-# vc_password            - password for vCenter server
-# service_name           - name that will form hypervisor name together with
-#                          'availability_zone_name' in nova-compute.conf
-# current_node           - name of node that we are executing manifets (e.g. 'node-4')
-# target_node            - name of node where nova-compute must be deployed
-#                          if it matches current_node we are deploying nova-compute service
-# datastore_regex        - regex that specifies vCenter datastores to use
-# api_retry_count        - number of tries on failures
-# use_quantum            - shows if neutron is enabled
-# service_enabled        - manage nova-compute service (Default: false)
-
+#
+# Copyright 2016 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Define: vmware::compute_vmware
+#
+# This resource deploys nova-compute service and configures it for use with
+# vmwareapi.VCDriver (vCenter server as hypervisor).
+# Depends on nova::params class.
+#
+# === Parameters
+#
+# [*availability_zone_name*]
+#   (required) Availability zone which nova-compute will be assigned.
+#
+# [*vc_cluster*]
+#   (required) Name of a VMware Cluster ComputeResource.
+#
+# [*vc_host*]
+#   (required) Hostname or IP address for connection to VMware vCenter host.
+#
+# [*vc_user*]
+#   (required) Username for connection to VMware vCenter host.
+#
+# [*vc_password*]
+#   (required) Password for connection to VMware vCenter host.
+#
+# [*service_name*]
+#   (required) Name that will form hypervisor name together with
+#   'availability_zone_name' in nova-compute.conf.
+#
+# [*current_node*]
+#   (required) Name of node that we are executing manifest (e.g. 'node-4').
+#
+# [*target_node*]
+#   (required) Name of node where nova-compute must be deployed. If it matches
+#   'current_node' we are deploying nova-compute service.
+#
+# [*vlan_interface*]
+#   (optional) Physical ethernet adapter name for vlan networking.
+#
+# [*vc_insecure*]
+#   (optional) If true, the vCenter server certificate is not verified.
+#   If false, then the default CA truststore is used for verification. This
+#   option is ignored if “ca_file” is set.
+#   Defaults to 'True'.
+#
+# [*vc_ca_file*]
+#   (optional) The hash name of the CA bundle file and data in format of:
+#   Example:
+#   "{"vc_ca_file"=>{"content"=>"RSA", "name"=>"vcenter-ca.pem"}}"
+#   Defaults to undef.
+#
+# [*datastore_regex*]
+#   (optional) Regex to match the name of a datastore.
+#   Defaults to undef.
+#
+# [*api_retry_count*]
+#   (required) The number of times we retry on failures, e.g.,
+#   socket error, etc.
+#   Defaults to '5'.
+#
+# [*maximum_objects*]
+#   (required) The maximum number of ObjectContent data objects that should be
+#   returned in a single result. A positive value will cause the operation to
+#   suspend the retrieval when the count of objects reaches the specified
+#   maximum. The server may still limit the count to something less than the
+#   configured value. Any remaining objects may be retrieved with additional
+#   requests.
+#   Defaults to '100'.
+#
+# [*nova_compute_conf*]
+#   (required) Path used for compute-vmware conf.
+#   Defaults to '/etc/nova/nova-compute.conf'.
+#
+# [*task_poll_interval*]
+#   (required) The interval used for polling of remote tasks.
+#   Defaults to '5.0'.
+#
+# [*use_linked_clone*]
+#   (required) Whether to use linked clone.
+#   Defaults to true.
+#
+# [*wsdl_location*]
+#   (optional) Optional VIM Service WSDL Location
+#   e.g 'http://<server>/vimService.wsdl'. Optional over-ride to default
+#   location for bug workarounds.
+#   Defaults to undef.
+#
+# [*service_enabled*]
+#   (optional) Manage nova-compute service.
+#   Defaults to false.
+#
 define vmware::compute_vmware(
   $availability_zone_name,
   $vc_cluster,
@@ -30,11 +112,13 @@ define vmware::compute_vmware(
   $current_node,
   $target_node,
   $vlan_interface,
+  $vc_insecure        = true,
+  $vc_ca_file         = undef,
   $datastore_regex    = undef,
-  $api_retry_count    = 5,
-  $maximum_objects    = 100,
+  $api_retry_count    = '5',
+  $maximum_objects    = '100',
   $nova_compute_conf  = '/etc/nova/nova-compute.conf',
-  $task_poll_interval = 5.0,
+  $task_poll_interval = '5.0',
   $use_linked_clone   = true,
   $wsdl_location      = undef,
   $service_enabled    = false,
@@ -48,8 +132,17 @@ define vmware::compute_vmware(
     $service_ensure = 'stopped'
   }
 
-  # We skip deployment if current node name is not same as target_node
+  # We skip deployment if current node name is not same as target_node.
   if ($target_node == $current_node) {
+    class { '::vmware::ssl::ssl':
+        vc_insecure    => $vc_insecure,
+        vc_ca_file     => $vc_ca_file,
+        vc_ca_filepath => '/etc/nova/vcenter-ca.pem',
+    }
+
+    $compute_vcenter_ca_filepath   = $::vmware::ssl::ssl::vcenter_ca_filepath
+    $compute_vcenter_insecure_real = $::vmware::ssl::ssl::vcenter_insecure_real
+
     # $cluster is used inside template
     $cluster = $name
     file { $nova_compute_conf:
@@ -77,6 +170,7 @@ define vmware::compute_vmware(
 
     Package['python-oslo.vmware']->
     Package['nova-compute']->
+    Class['::vmware::ssl::ssl']->
     File[$nova_compute_conf]->
     Service['nova-compute']
   }
