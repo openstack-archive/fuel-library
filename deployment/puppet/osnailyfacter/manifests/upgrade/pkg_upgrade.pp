23a6be354f8fb968aeb772fc1d22cb16e62169a0
Denis Egorenko <degorenko@mirantis.com>
2016-08-15 18:17:52 +0300

    [WIP] Add upgrade stuff for MU
    If corosync package was upgraded it restarts corosync service
    and it kills pacemaker service. So we need to start it again.
    Also deny apt to stop pacemaker service during upgrade
    because it leads to unload of all pacemaker resources
    (all services under it will be stopped). That's not appropriate
    behaviour during the update process. Puppet will manage services
    restart only one by one.
    
    Closes-bug: #1614893
    
    Change-Id: I0d7231aa5900318f75f71c698f3e1c07f8e5cfbe

---
+++ deployment/puppet/osnailyfacter/manifests/upgrade/pkg_upgrade.pp
@@ -0,0 +1,38 @@
+class osnailyfacter::upgrade::pkg_upgrade {
+  # hardcode with retries and sleeps for resolving lock issue
+  # should be rewritten
+  exec { 'do_upgrade':
+    command     => 'apt-get dist-upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"',
+    environment => [ 'DEBIAN_FRONTEND=noninteractive' ],
+    path        => ['/usr/bin', '/usr/local/sbin', '/usr/sbin', '/sbin', '/bin' ],
+    timeout     => 1700,
+    try_sleep   => 10,
+    tries       => 5,
+    logoutput   => true,
+  }
+  if roles_include(['controller', 'primary-controller']) {
+    $content_policy = "#!/bin/bash\n[[ \"\$1\" == \"pacemaker\" ]] && exit 101\n"
+    $policyrc_file  = '/usr/sbin/policy-rc.d'
+
+    ensure_resource('file', 'create-policy-rc.d', {
+      ensure  => present,
+      path    => $policyrc_file,
+      content => $content_policy,
+      mode    => '0755',
+      owner   => 'root',
+      group   => 'root',
+      before  => Exec['do_upgrade']
+    })
+
+    exec { 'remove_policy':
+      command => "rm -rf ${policyrc_file}",
+      path    => '/bin',
+      require => Exec['do_upgrade'],
+    }
+
+    ensure_resource('service', 'pacemaker', {
+      ensure  => running,
+      require => Exec['remove_policy']
+    })
+  }
+}
--- deployment/puppet/osnailyfacter/modular/cluster-vrouter/cluster-vrouter.pp
+++ deployment/puppet/osnailyfacter/modular/cluster-vrouter/cluster-vrouter.pp
@@ -1,1 +1,2 @@
 include ::osnailyfacter::cluster_vrouter::cluster_vrouter
+include ::osnailyfacter::upgrade::restart_services
