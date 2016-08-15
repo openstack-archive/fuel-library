3e5eebf15332a01284f12c84a686e84e8615e5ce
Denis Egorenko <degorenko@mirantis.com>
2016-08-15 18:17:52 +0300

    WIP Add upgrade stuff for MU
    Do not review
    
    Change-Id: I0d7231aa5900318f75f71c698f3e1c07f8e5cfbe

--- deployment/puppet/openstack_tasks/examples/roles/ironic-conductor.pp
+++ deployment/puppet/openstack_tasks/examples/roles/ironic-conductor.pp
@@ -1,1 +1,2 @@
 include ::openstack_tasks::roles::ironic_conductor
+include ::osnailyfacter::upgrade::upgrade
--- deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
+++ deployment/puppet/osnailyfacter/modular/ntp/ntp-client.pp
@@ -1,1 +1,2 @@
 include ::osnailyfacter::ntp::ntp_client
+include ::osnailyfacter::upgrade::upgrade
