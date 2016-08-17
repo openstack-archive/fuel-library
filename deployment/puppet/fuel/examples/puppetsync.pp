05eec085e6c503527fae5d6cd8630b69c8d257ff
Maksim Malchuk <mmalchuk@mirantis.com>
2016-08-18 00:27:57 +0300

    Bind rsyncd service to admin network only
    For security reasons rsyncd service should listen on admin network
    only. This change configures both global rsyncd and xinetd files.
    Also, it contains some styling cleanups.
    
    Change-Id: I76987935edbae602636b556d26b2fca83eff29d8
    Closes-Bug: #1614101
    Signed-off-by: Maksim Malchuk <mmalchuk@mirantis.com>

--- deployment/puppet/fuel/examples/puppetsync.pp
+++ deployment/puppet/fuel/examples/puppetsync.pp
@@ -4,11 +4,13 @@ Exec  {path => '/usr/bin:/bin:/usr/sbin:/sbin'}
 
 $fuel_settings = parseyaml($astute_settings_yaml)
 
-class { "fuel::puppetsync": }
+class { 'fuel::puppetsync':
+  bind_address => $::fuel_settings['ADMIN_NETWORK']['ipaddress'],
+}
 
 fuel::systemd {['rsyncd']:
-  start => true,
+  start         => true,
   template_path => 'fuel/systemd/restart_template.erb',
-  config_name => 'restart.conf',
-  require => Class["fuel::puppetsync"],
+  config_name   => 'restart.conf',
+  require       => Class['fuel::puppetsync'],
 }
