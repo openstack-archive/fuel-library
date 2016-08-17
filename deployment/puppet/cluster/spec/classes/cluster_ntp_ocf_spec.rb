8b5b3fa3f53e698985ed8501cdeaef37933a72e0
Dmitry Ilyin <dilyin@mirantis.com>
2016-08-17 13:22:46 -0500

    Puppet4 support: cluster module
    Related-Bug: 1586480
    Change-Id: I9fb6fe6a0d0b283af5b6783e022cd5d5052addea

--- deployment/puppet/cluster/spec/classes/cluster_ntp_ocf_spec.rb
+++ deployment/puppet/cluster/spec/classes/cluster_ntp_ocf_spec.rb
@@ -5,23 +5,35 @@ describe 'cluster::ntp_ocf' do
   shared_examples_for 'ntp_ocf configuration' do
 
     it 'configures with the default params' do
+      service_name = platform_params[:service_name]
       should contain_class('cluster::ntp_ocf')
-      should contain_pcmk_resource('p_ntp').with_before(["Pcmk_colocation[ntp-with-vrouter-ns]", "Service[ntp]"]) 
-      should contain_pcmk_colocation('ntp-with-vrouter-ns').with(
+      should contain_pcmk_resource("p_#{service_name}").with_before(["Pcmk_colocation[ntp-with-vrouter-ns]", "Service[#{service_name}]"])
+      should contain_pcmk_colocation("ntp-with-vrouter-ns").with(
         :ensure => 'present',
         :score  => 'INFINITY',
-        :first  => 'clone_p_vrouter', 
-        :second => 'clone_p_ntp')
+        :first  => 'clone_p_vrouter',
+        :second => "clone_p_#{service_name}")
     end
   end
 
-  context 'on Debian platforms' do
-    let :facts do
-      { :osfamily => 'Debian',
-        :operatingsystem => 'Debian', }
-    end
+  on_supported_os(supported_os: supported_os).each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts.merge(common_facts) }
 
-    it_configures 'ntp_ocf configuration'
+      let :platform_params do
+        if facts[:osfamily] == 'Debian'
+          {
+              :service_name => 'ntp'
+          }
+        else
+          {
+              :service_name => 'ntpd'
+          }
+        end
+      end
+
+      it_configures 'ntp_ocf configuration'
+    end
   end
 
 end
