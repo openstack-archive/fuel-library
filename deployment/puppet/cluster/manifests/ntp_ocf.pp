0aaebe0ebb4a7fd8c07f43a4d5a1dc5266bee4c7
Dmitry Ilyin <dilyin@mirantis.com>
2016-08-18 17:32:04 -0500

    Puppet4 support: ntp
    Fix ntp service duplicate declaration
    
    Related-Bug: 1586480
    Change-Id: I26faf5d6e3f79959c8502c6374aac37c105f91bb

--- deployment/puppet/cluster/manifests/ntp_ocf.pp
+++ deployment/puppet/cluster/manifests/ntp_ocf.pp
@@ -2,7 +2,7 @@
 #
 # Configure OCF service for NTP managed by corosync/pacemaker
 #
-class cluster::ntp_ocf inherits ntp::params {
+class cluster::ntp_ocf inherits ::ntp {
   $primitive_type = 'ns_ntp'
   $complex_type   = 'clone'
 
@@ -49,17 +49,6 @@ class cluster::ntp_ocf inherits ntp::params {
     complex_metadata    => $complex_metadata,
     complex_type        => $complex_type,
     prefix              => true,
-  }
-
-  if ! defined(Service[$service_name]) {
-    service { $service_name:
-      name       => $service_name,
-      enable     => true,
-      ensure     => 'running',
-      hasstatus  => true,
-      hasrestart => true,
-      provider   => 'pacemaker',
-    }
   }
 
   Pcmk_resource["p_${service_name}"] ->
