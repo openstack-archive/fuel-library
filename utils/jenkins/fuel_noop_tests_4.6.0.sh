8691081580cd1c2d1df684c1261fc2e6257bc72f
Dmitry Ilyin <dilyin@mirantis.com>
2016-08-19 13:30:18 -0500

    Puppet4 support: l23network
    * nic_whitelist_to_json function should return nil if there are no whitelists
    * Remove the mutators for the frozen strings in the validator.
    
    Related-Bug: 1586480
    Change-Id: Ib540dc243647fffeb4b2d101f9f902b9e4c1c104

--- utils/jenkins/fuel_noop_tests_4.6.0.sh
+++ utils/jenkins/fuel_noop_tests_4.6.0.sh
@@ -3,6 +3,11 @@ DIR=`dirname $0`
 cd $DIR || exit 1
 
 export PUPPET_GEM_VERSION="4.6.0"
+
+# TODO: there are problems with strict variables in the l23network module. They should be fixed before strict variables can be enabled.
+# TODO: https://bugs.launchpad.net/fuel/+bug/1618964
+export STRICT_VARIABLES="no"
+
 echo "Running the noop tests with Puppet version: ${PUPPET_GEM_VERSION}"
 ../../tests/noop/noop_tests.sh -tB
 ../../tests/noop/run_globals.sh -b $@
