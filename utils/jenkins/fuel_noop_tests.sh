3ce5cb553330e7d95c6c24e9c869f4052c5018f1
Dmitry Ilyin <dilyin@mirantis.com>
2016-08-19 13:30:18 -0500

    Puppet4 support: noop tests fixes
    * nic_whitelist_to_json function should return nil if there are no whitelists
    * Remove the mutators for the frozen strings in the validator.
    
    * Disable STRICT_VARIABLE checks
    * Switch to the Puppet version 4.5.0
    
    Related-Bug: 1586480
    Change-Id: Ib540dc243647fffeb4b2d101f9f902b9e4c1c104

--- utils/jenkins/fuel_noop_tests.sh
+++ utils/jenkins/fuel_noop_tests.sh
@@ -2,18 +2,14 @@
 DIR=`dirname $0`
 cd $DIR || exit 1
 
-../../tests/noop/setup_and_diagnostics.sh $@
+./fuel_noop_tests3.sh
 if [ $? -gt 0 ]; then
-  echo "Noop tests setup have failed!"
+  echo "Noop tests for Puppet 3 have failed!"
   exit 1
 fi
-./fuel_noop_tests_3.8.7.sh
-if [ $? -gt 0 ]; then
-  echo "Noop tests for Puppet 3.8.7 have failed!"
-  exit 1
-fi
-./fuel_noop_tests_4.6.0.sh
-if [ $? -gt 0 ]; then
-  echo "Noop tests for Puppet 4.6.0 have failed!"
-  exit 1
-fi
+
+#./fuel_noop_tests4.sh
+#if [ $? -gt 0 ]; then
+#  echo "Noop tests for Puppet 4 have failed!"
+#  exit 1
+#fi
