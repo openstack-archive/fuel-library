d68d9bcca17fe1de3a2df91a35bb24e0b6f1c50a
Dmitry Ilyin <dilyin@mirantis.com>
2016-08-19 13:30:18 -0500

    Puppet4 support: l23network
    * get_nic_passthrough_whitelist should always return an array
    * Remove the mutators for the frozen strings in the validator
    
    Related-Bug: 1586480
    Change-Id: Ib540dc243647fffeb4b2d101f9f902b9e4c1c104

--- deployment/puppet/l23network/lib/puppet/parser/functions/get_nic_passthrough_whitelist.rb
+++ deployment/puppet/l23network/lib/puppet/parser/functions/get_nic_passthrough_whitelist.rb
@@ -4,23 +4,28 @@ Puppet::Parser::Functions::newfunction(:get_nic_passthrough_whitelist, :type => 
     This function gets pci_passthrough_whitelist mapping from transformations
     Returns NIL if no transformations with this provider found or list
     ex: pci_passthrough_whitelist('sriov')
-    EOS
-  ) do |argv|
+EOS
+) do |argv|
   provider = argv[0].to_s.upcase
 
   cfg = L23network::Scheme.get_config(lookupvar('l3_fqdn_hostname'))
   transformations = cfg[:transformations]
-  rv = []
+  whitelist = []
 
   transformations.each do |transform|
-    if transform[:provider].to_s.upcase == provider and\
-       transform[:action] == "add-port" and\
-       transform[:vendor_specific][:physnet]
-      rv.push({"devname" => transform[:name], "physical_network" => transform[:vendor_specific][:physnet]})
-    end
+    next unless transform[:provider].to_s.upcase == provider
+    next unless transform[:action] == 'add-port'
+    next unless transform[:vendor_specific].is_a? Hash
+    next unless transform[:vendor_specific][:physnet]
+    whitelist.push(
+        {
+            'devname' => transform[:name],
+            'physical_network' => transform[:vendor_specific][:physnet],
+        }
+    )
   end
 
-  rv unless rv.empty?
+  whitelist
 end
 
 # vim: set ts=2 sw=2 et :
