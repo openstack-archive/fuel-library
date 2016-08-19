e9a91367a0dfc032ab3140e5af217918dd7488ff
Dmitry Ilyin <dilyin@mirantis.com>
2016-08-19 17:35:01 -0500

    Puppet4 support: enable both Puppet4 and Puppet3 noop tests
    Change-Id: I8b037b214d49c49d8a05a2442520dd393795f75a
    Related-Bug: 1586480

--- utils/jenkins/fuel_noop_tests.sh
+++ utils/jenkins/fuel_noop_tests.sh
@@ -3,6 +3,17 @@ DIR=`dirname $0`
 cd $DIR || exit 1
 
 ../../tests/noop/setup_and_diagnostics.sh $@
-../../tests/noop/run_globals.sh -b $@
-../../tests/noop/run_all.sh -b $@
-../../tests/noop/show_failed_tasks.sh -b $@
+if [ $? -gt 0 ]; then
+  echo "Noop tests setup have failed!"
+  exit 1
+fi
+./fuel_noop_tests_3.8.7.sh
+if [ $? -gt 0 ]; then
+  echo "Noop tests for Puppet 3.8.7 have failed!"
+  exit 1
+fi
+./fuel_noop_tests_4.6.0.sh
+if [ $? -gt 0 ]; then
+  echo "Noop tests for Puppet 4.6.0 have failed!"
+  exit 1
+fi
