0785cd58228b49edf7eb4d191c4d8239927e0d6f
Dmitry Ilyin <dilyin@mirantis.com>
2016-08-19 17:20:27 -0500

    Puppet4 support: fetch_value function
    The functions for extracting values from a nested structure
    have a long and funny history. Currently this function is
    named "dig" and is used in the Fule linrary by this name.
    But in Puppet 4.5 another incompatible "dig" function was
    introduced and the original "dig" in the stdlib was renamed
    to dig44.
    
    The dig function from the stdlib doesn't support :undef values
    and has problems with other values too. This function is
    based on the "dig44" but has some improvements needed for
    Puppet4.5+.
    
    Related-Bug: 1586480
    Change-Id: Ieb4ae544d7ceab06c115a18df676cf8fa61e39a0

--- deployment/puppet/osnailyfacter/spec/functions/fetch_value_spec.rb
+++ deployment/puppet/osnailyfacter/spec/functions/fetch_value_spec.rb
@@ -27,84 +27,79 @@ describe 'fetch_value' do
 
   context 'single values' do
     it 'should exist' do
-      is_expected.not_to eq(nil)
+      is_expected.not_to be_nil
     end
 
-    it 'should be able to return a single value' do
-      is_expected.to run.with_params('test').and_return('test')
+    it 'should require two arguments' do
+      is_expected.to run.with_params().and_raise_error(ArgumentError)
     end
 
-    it 'should use the default value if data is a single value and path is present' do
-      is_expected.to run.with_params('test', 'path', 'default').and_return('default')
+    it 'should fail if the data is not a structure' do
+      is_expected.to run.with_params('test', []).and_raise_error(Puppet::Error)
     end
 
-    it 'should return default if there is no data' do
-      is_expected.to run.with_params(nil, nil, 'default').and_return('default')
+    it 'should fail if the path is not an array' do
+      is_expected.to run.with_params({}, '').and_raise_error(Puppet::Error)
     end
 
-    it 'should be able to use data structures as default values' do
-      is_expected.to run.with_params('test', 'path', data).and_return(data)
+    it 'should return the value if the value is string' do
+      is_expected.to run.with_params(data, ['d'], 'default').and_return('1')
+    end
+
+    it 'should return true if the value is true' do
+      is_expected.to run.with_params(data, ['b'], 'default').and_return(true)
+    end
+
+    it 'should return false if the value is false' do
+      is_expected.to run.with_params(data, ['c'], 'default').and_return(false)
     end
 
     it 'should return the default if the value is nil' do
-      is_expected.to run.with_params(data, 'f', 'default').and_return('default')
+      is_expected.to run.with_params(data, ['f'], 'default').and_return('default')
     end
 
-    it 'should return the default if the value is :undef' do
-      is_expected.to run.with_params(data, 'e', 'default').and_return('default')
+    it 'should return the default if the value is :undef (same as nil)' do
+      is_expected.to run.with_params(data, ['e'], 'default').and_return('default')
+    end
+
+    it 'should return the default if the path is not found' do
+      is_expected.to run.with_params(data, ['missing'], 'default').and_return('default')
     end
   end
 
   context 'structure values' do
-    it 'should be able to extracts a single hash value' do
-      is_expected.to run.with_params(data, 'd', 'default').and_return('1')
-    end
 
     it 'should be able to extract a deeply nested hash value' do
-      is_expected.to run.with_params(data, 'a/g', 'default').and_return('2')
-    end
-
-    it 'should return the default value if the path is not found' do
-      is_expected.to run.with_params(data, 'missing', 'default').and_return('default')
+      is_expected.to run.with_params(data, %w(a g), 'default').and_return('2')
     end
 
     it 'should return the default value if the path is too long' do
-      is_expected.to run.with_params(data, 'a/g/c/d', 'default').and_return('default')
+      is_expected.to run.with_params(data, %w(a g c d), 'default').and_return('default')
     end
 
-    it 'should support an array index in the path' do
-      is_expected.to run.with_params(data, 'a/e/1', 'default').and_return('f1')
+    it 'should support an array index (number) in the path' do
+      is_expected.to run.with_params(data, ['a', 'e', 1], 'default').and_return('f1')
+    end
+
+    it 'should support an array index (string) in the path' do
+      is_expected.to run.with_params(data, %w(a e 1), 'default').and_return('f1')
     end
 
     it 'should return the default value if an array index is not a number' do
-      is_expected.to run.with_params(data, 'a/b/c', 'default').and_return('default')
+      is_expected.to run.with_params(data, %w(a b c), 'default').and_return('default')
     end
 
     it 'should return the default value if and index is out of array length' do
-      is_expected.to run.with_params(data, 'a/e/5', 'default').and_return('default')
+      is_expected.to run.with_params(data, %w(a e 5), 'default').and_return('default')
     end
 
     it 'should be able to path though both arrays and hashes' do
-      is_expected.to run.with_params(data, 'a/e/2/x/y', 'default').and_return('z')
-    end
-
-    it 'should be able to return "true" value' do
-      is_expected.to run.with_params(data, 'b', 'default').and_return(true)
-      is_expected.to run.with_params(data, 'm', true).and_return(true)
-    end
-
-    it 'should be able to return "false" value' do
-      is_expected.to run.with_params(data, 'c', 'default').and_return(false)
-      is_expected.to run.with_params(data, 'm', false).and_return(false)
+      is_expected.to run.with_params(data, %w(a e 2 x y), 'default').and_return('z')
     end
 
     it 'should return "nil" if value is not found and no default value is provided' do
-      is_expected.to run.with_params(data, 'a/1').and_return(nil)
+      is_expected.to run.with_params(data, %w(a 1)).and_return(nil)
     end
 
-    it 'should be able to use a custom path separator' do
-      is_expected.to run.with_params(data, 'a::g', 'default', '::').and_return('2')
-      is_expected.to run.with_params(data, 'a::c', 'default', '::').and_return('default')
-    end
   end
 end
