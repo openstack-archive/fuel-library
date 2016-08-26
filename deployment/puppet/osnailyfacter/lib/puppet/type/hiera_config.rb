a14dda72f7a48b4e122b358cbe200a5e94c70425
Dmitry Ilyin <dilyin@mirantis.com>
2016-08-26 15:22:36 -0500

    Refactoring of the hiera_config resource
    Additional features required by the plugin authors
    
    * Collect additional files from "plugins" directory.
    * Collect additional files from the enabled plugins in the metadata
    * Collect additional files from the "hiera" key of each plugin
      in the "plugins" section and "hiera" section of each plugin's
      metadata section.
    * Add top and buttom additional sets of elements.
    
    Cherry picked from: efe27d1c7e2a792a30e28a5e48fe5bbfb1cd552b
    Related-Bug: 1618139
    Change-Id: I95c31b9f8133d48627afc620a9fe20dbd5cf9512

--- deployment/puppet/osnailyfacter/lib/puppet/type/hiera_config.rb
+++ deployment/puppet/osnailyfacter/lib/puppet/type/hiera_config.rb
@@ -3,20 +3,16 @@ Puppet::Type.newtype(:hiera_config) do
 
   ensurable
 
-  newparam(:name) do
+  newparam(:path) do
     desc 'The path to the Hiera config file.'
     isnamevar
   end
 
-  newparam(:override_dir) do
+  newparam(:plugins_dir) do
     desc 'Look for override files in this directory.
          The directory should be inside the data directory and the path
          should be relative to the basic data directory path.'
     defaultto 'plugins'
-  end
-
-  newparam(:override_suffix) do
-    desc 'Add suffix to all override files'
   end
 
   newparam(:metadata_yaml_file) do
@@ -31,9 +27,13 @@ Puppet::Type.newtype(:hiera_config) do
     defaultto '/etc/hiera'
   end
 
-  newproperty(:hierarchy, :array_matching => :all) do
+  newparam(:override_suffix) do
+    desc 'Add suffix to all override files'
+  end
+
+  newproperty(:hierarchy_bottom, :array_matching => :all) do
     desc 'Basic hierarchy elements. This list of elements will be used at the
-          bottom of the hierarchy before any overrides are applied.'
+          bottom of the hierarchy before any plugin overrides are applied.'
     defaultto []
 
     def is_to_s(value)
@@ -45,9 +45,23 @@ Puppet::Type.newtype(:hiera_config) do
     end
   end
 
-  newproperty(:hierarchy_override, :array_matching => :all) do
-    desc 'Override hierarchy elements. These list will be automaticly gathered
-          either from the metadata file of from the override directoiry scanning.
+  newproperty(:hierarchy_top, :array_matching => :all) do
+    desc 'Top hierarchy elements. This list of elements will be used at the
+          top of the hierarchy after the plugin overrides are applied.'
+    defaultto []
+
+    def is_to_s(value)
+      value.inspect
+    end
+
+    def should_to_s(value)
+      value.inspect
+    end
+  end
+
+  newproperty(:hierarchy_plugins, :array_matching => :all) do
+    desc 'Plugins hierarchy elements. These list will be automatically gathered
+          either from the metadata file of from the override directory scanning.
           If you provide the list manually it will be used without any automatic
           element gathering.'
     defaultto []
@@ -77,6 +91,46 @@ Puppet::Type.newtype(:hiera_config) do
     munge do |value|
       value.to_s
     end
+  end
+
+  newproperty(:backends, :array_matching => :all) do
+    desc 'The list of Hiera backends'
+    defaultto ['yaml']
+  end
+
+  newproperty(:additions) do
+    desc 'Additional configuration options to merge with the generated Hiera config file.
+          Will not override the existing managed config values.'
+
+    validate do |value|
+      fail "Additional config options should be a Hash! Got: #{value.inspect}" unless value.is_a? Hash
+    end
+
+    munge do |hash|
+      additions = {}
+      hash.each do |key, value|
+        key = key.to_s.to_sym
+        next if resource.managed_keys.include? key
+        additions.store key, value
+      end
+      additions
+    end
+
+    def is_to_s(value)
+      value.inspect
+    end
+
+    def should_to_s(value)
+      value.inspect
+    end
+
+    defaultto {}
+  end
+
+  # managed config file keys
+  # @return [Array]
+  def managed_keys
+    [:logger, :backends, :yaml, :hierarchy, :merge_behavior]
   end
 
 end
