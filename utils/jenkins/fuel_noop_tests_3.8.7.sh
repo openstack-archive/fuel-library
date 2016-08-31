cb8013936ce11a4b9fa2ecb4008fac7d3d77cf65
Dmitry Ilyin <dilyin@mirantis.com>
2016-08-31 14:28:12 -0500

    Puppet4 support: switch to Puppet 4.5.0
    Change-Id: I4eb68c1d39cf16af9a0ab5efd1e844e05cddaf4b
    Related-Bug: 1586480

--- utils/jenkins/fuel_noop_tests_3.8.7.sh
+++ utils/jenkins/fuel_noop_tests_3.8.7.sh
@@ -7,4 +7,6 @@ echo "Running the noop tests with Puppet version: ${PUPPET_GEM_VERSION}"
 ../../tests/noop/noop_tests.sh -tB
 ../../tests/noop/run_globals.sh -b $@
 ../../tests/noop/run_all.sh -b $@
+ec="${?}"
 ../../tests/noop/show_failed_tasks.sh -b $@
+exit "${ec}"
