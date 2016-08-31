2bfe12f2f3d829774d1048cd2650e6b7972409ca
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-08-31 17:24:58 +0300

    Move gateway ping to connectivity tests
    Gateway ping is actually function of connectivity checker, not netconfig
    one, so it should be moved from there. Also it breaks the case when
    netconfig tasks ran on bootstrap node for network verification graph
    when there is no default gateway yet.
    
    Change-Id: I8c5fbd1b78eeb0abbae57c66a5b0c1d17b81fad2
    Blueprint: graph-concept-extension

--- tests/noop/spec/hosts/netconfig/connectivity_tests_spec.rb
+++ tests/noop/spec/hosts/netconfig/connectivity_tests_spec.rb
@@ -14,10 +14,17 @@ require 'shared-examples'
 manifest = 'netconfig/connectivity_tests.pp'
 
 describe manifest do
-  before(:each) {
-    Puppet::Parser::Functions.newfunction(:url_available) { |args|
-      return true
-    }
-  }
+  before(:each) do
+    Noop.puppet_function_load :url_available
+    MockFunction.new(:url_available) do |function|
+      allow(function).to receive(:call).and_return(true)
+    end
+  end
+
+  shared_examples 'catalog' do
+    default_gateway  = Noop.puppet_function, 'get_default_gateways'
+    it { should contain_ping_host(default_gateway.join()).with('ensure' => 'up') }
+  end
+
   test_ubuntu_and_centos manifest
 end
