60bddba89cf0e890f8a6d36de05c8492e424d9c1
Dmitry Ilyin <dilyin@mirantis.com>
2016-08-31 17:59:15 -0500

    Puppet4 support: noop tests fixes
    * Disable STRICT_VARIABLE checks
    * Switch to the Puppet version 4.5.0
    * Disable Noop tests for Puppet4
      they should be moved to a separate job
    
    Related-Bug: 1586480
    Change-Id: Ib540dc243647fffeb4b2d101f9f902b9e4c1c104

--- utils/jenkins/fuel_noop_tests.sh
+++ utils/jenkins/fuel_noop_tests.sh
@@ -2,18 +2,4 @@
 DIR=`dirname $0`
 cd $DIR || exit 1
 
-../../tests/noop/setup_and_diagnostics.sh $@
-if [ $? -gt 0 ]; then
-  echo "Noop tests setup have failed!"
-  exit 1
-fi
-./fuel_noop_tests_3.8.7.sh
-if [ $? -gt 0 ]; then
-  echo "Noop tests for Puppet 3.8.7 have failed!"
-  exit 1
-fi
-./fuel_noop_tests_4.6.0.sh
-if [ $? -gt 0 ]; then
-  echo "Noop tests for Puppet 4.6.0 have failed!"
-  exit 1
-fi
+./fuel_noop_tests3.sh
