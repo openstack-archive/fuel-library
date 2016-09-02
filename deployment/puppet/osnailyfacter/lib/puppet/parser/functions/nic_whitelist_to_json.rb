8eb3b5c503e2459060ee74716d1eed919da869be
Dmitry Ilyin <dilyin@mirantis.com>
2016-09-02 10:24:13 -0500

    Puppet4 support: nic_passthrough_whitelist
    Change-Id: I7be4b1e976a7bb5ab76924a20ff3872aa6371fab
    Related-Bug: 1586480

--- deployment/puppet/osnailyfacter/lib/puppet/parser/functions/nic_whitelist_to_json.rb
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/nic_whitelist_to_json.rb
@@ -11,6 +11,8 @@ EOS
 
   nic_whitelist = argv[0]
   return nil unless nic_whitelist
-  nic_whitelist.to_json
+  return nil if nic_whitelist == ''
+  data = nic_whitelist.to_json
+  debug "nic_whitelist_to_json() return: #{data.inspect}"
+  data
 end
-
