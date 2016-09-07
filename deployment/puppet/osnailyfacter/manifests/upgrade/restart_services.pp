9ec5363f47ada7fea5234b0179e9c54af123852e
Vladimir Maliaev <vmaliaev@mirantis.com>
2016-09-07 13:55:31 +0000

    Override packages version and notify all services in current catalog. Services will be restarted only if package(s) are updated.
    Change-Id: Id99bfae07ea0a9e63ad319076743a6101f2675f8

--- deployment/puppet/osnailyfacter/manifests/upgrade/restart_services.pp
+++ deployment/puppet/osnailyfacter/manifests/upgrade/restart_services.pp
@@ -1,6 +1,13 @@
 class osnailyfacter::upgrade::restart_services {
   $mu_upgrade = hiera_hash('mu_upgrade', {})
+  $static_packages = hiera_hash('static_versions',{})
+  $group_packages = hiera_hash('group_versions',{})
+  $initial_group_hash = getpackagegrouphash($group_packages, $static_packages)
+
   if $mu_upgrade['enabled'] {
     notify { 'restarting services': } ~> Service<||>
+  } else {
+    $pkg_hash = deep_merge($initial_group_hash, $static_packages)
+    override_pkg_version($pkg_hash)
   }
 }
