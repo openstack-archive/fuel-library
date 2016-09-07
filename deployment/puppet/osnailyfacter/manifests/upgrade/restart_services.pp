e124046927b2ebb559307a0879f3469634be2fca
Vladimir Maliaev <vmaliaev@mirantis.com>
2016-09-07 13:55:31 +0000

    Override package versions from hiera hash and notify all services in current catalog.
    It takes package names and versions from hiera hash to upgrade and override "ensure" parameter in package resource
    If any package from the current puppet catalog is upgraded, all the services from the same catalog should be restarted.
    If there are no upgraded packages, than no services to be restarted.
    
    Closes-bug: #1616553
    
    Change-Id: Id99bfae07ea0a9e63ad319076743a6101f2675f8

--- deployment/puppet/osnailyfacter/manifests/upgrade/restart_services.pp
+++ deployment/puppet/osnailyfacter/manifests/upgrade/restart_services.pp
@@ -1,6 +1,11 @@
 class osnailyfacter::upgrade::restart_services {
   $mu_upgrade = hiera_hash('mu_upgrade', {})
+  $group_packages = hiera_hash('upgrade_packages',{})
+  $extended_group_hash = getpackagegrouphash($group_packages,{})
+
   if $mu_upgrade['enabled'] {
     notify { 'restarting services': } ~> Service<||>
+  } else {
+    override_pkg_version($extended_group_hash)
   }
 }
