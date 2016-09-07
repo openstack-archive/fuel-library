97b916ff17cf0176420e391ce9ec256fba548624
Vladimir Maliaev <vmaliaev@mirantis.com>
2016-09-07 13:55:31 +0000

    Upgrade a single package or a bunch of packages
    It takes package names and versions from hiera hash to upgrade
    and override "ensure" parameter in package resource.
    If any package from the current puppet catalog is upgraded,
    all the services in the same catalog should be restarted.
    
    If there are no upgraded packages, then no services to be restarted.
    
    Closes-Bug: #1623038
    Related-Bug: #1616553
    
    Change-Id: Id99bfae07ea0a9e63ad319076743a6101f2675f8

---
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/get_package_group_hash.rb
@@ -0,0 +1,27 @@
+module Puppet::Parser::Functions
+  newfunction(:get_package_group_hash, :arity => -2, :type => :rvalue, :doc => <<-'ENDHEREDOC') do |args|
+    Return a hash with package names as keys and {ensure => "version of a package"} as values.
+    The keys of input hash contain regular expressions which are used to match a list of installed packages.
+    For example:
+      Input:  {"^.*apache2-[b,d].*$" => latest}
+      Output: {
+                apache2-bin         => {ensure => latest},
+                apache2-data        => {ensure => latest}, }
+    ENDHEREDOC
+
+    h = Hash.new()
+    case lookupvar('osfamily')
+      when "Debian"
+        p_list = `dpkg-query -W -f='${binary:Package}\n'`.split("\n")
+      when "RedHat"
+        p_list = `rpm -qa`.split("\n")
+      end
+
+    args[0].each {|key,value|
+      next if !key.ascii_only?
+      p_list.select{ |pkg| pkg[/#{key}/] }.each {|elem| h[elem] = value}
+    }
+
+    return h
+  end
+end
