6579d0d85d4416d4360c67c335952d4a31bd9782
Ukov Dmitry <dukov@mirantis.com>
2016-09-07 19:02:25 +0300

    Introducing override for all resources in catalog
    * override_resources type extended with additional parameter that allows
      to control whether create new resource if it is not in catalog
    * override_resources is wrapped with create_resources
    * user can put any resource definition in hiera to override its configuration
    
    Change-Id: Ic1f7d856666eb84ced3cef986d8765edf707b481
    Blueprint: puppet-resource-override

--- tests/noop/spec/hosts/common.rb
+++ tests/noop/spec/hosts/common.rb
@@ -10,12 +10,48 @@ shared_examples 'compute_service_common' do
     end
   end
 
+
   it do
     is_expected.not_to contain_service(nova_compute_service_name).with(:ensure => 'running')
   end
 
 end
 
+shared_examples 'override_resources' do
+  let(:configuration_override) do
+    Noop.hiera_structure 'configuration'
+  end
+
+  let(:package_config) do
+    configuration_override.fetch('package', {}).fetch('data', {})
+  end
+
+  let(:ral_catalog) do
+    Noop.create_ral_catalog self
+  end
+
+  it 'should create/update params with override_resources' do
+    configuration_override.each do |resource, config|
+      is_expected.to contain_override_resources(resource).with(:data => config.fetch('data', config))
+    end
+  end
+
+  if Noop.task_spec.to_s != 'murano/murano_spec.rb'
+    it 'should use "override_resources" to update the catalog' do
+      configuration_override.each do |resource, config|
+        config.fetch('data', config).each do |title, params|
+          params['value'] = ['True'] if params['value'].is_a? TrueClass
+          contain_resource = send("contain_#{resource}", title)
+          expect(ral_catalog).to contain_resource.with(params)
+        end
+      end
+    end
+  end
+
+end
+
 shared_examples 'common' do
   include_examples 'compute_service_common' unless Noop.current_spec_is? 'roles/enable_compute'
+  include_examples 'override_resources'
 end
+
