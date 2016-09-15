5d4e2e7f758a28f909d5de592d97fbbfae801bea
Pavel Glushchak <pglushchak@virtuozzo.com>
2016-09-15 13:58:13 +0300

    Make checksum_udp.pp compatible with rhel7
    Command `iptables-save > /etc/sysconfig/iptables` do
    the same as `/etc/init.d/iptables save` but it works on
    both rhel6 and rhel7.
    
    Change-Id: I8c382826a00f911d785e6d9ef476713d7c639842
    Signed-off-by: Pavel Glushchak <pglushchak@virtuozzo.com>
    Closes-Bug: #1623893

--- deployment/puppet/openstack/manifests/checksum_udp.pp
+++ deployment/puppet/openstack/manifests/checksum_udp.pp
@@ -5,7 +5,7 @@ class openstack::checksum_udp ($port = '514') {
   case $operatingsystem {
     /(?i)(centos|redhat|oraclelinux)/ : {
       exec { "checksum_fill_udp":
-        command => "iptables -t mangle -A POSTROUTING -p udp --dport ${port} -j CHECKSUM --checksum-fill; /etc/init.d/iptables save",
+        command => "iptables -t mangle -A POSTROUTING -p udp --dport ${port} -j CHECKSUM --checksum-fill; iptables-save > /etc/sysconfig/iptables",
         unless  => "iptables -t mangle -S POSTROUTING | grep -q \"^-A POSTROUTING -p udp -m udp --dport ${port} -j CHECKSUM --checksum-fill\""
       }
     }
