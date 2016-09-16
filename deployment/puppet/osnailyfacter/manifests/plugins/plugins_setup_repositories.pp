56bea1b8af0136f762df9970c5e08e891037bfc8
Vladimir Kuklin <vkuklin@mirantis.com>
2016-09-16 12:59:20 +0000

    Revert "Fix plugins repositories pins generation"
    This reverts commit 6c941ce5e6b1f0f346d2387a08418e4c37814f0a.
    
    Change-Id: I400a017ae8a5cad4db735b0617f71f6d88e95900

--- deployment/puppet/osnailyfacter/manifests/plugins/plugins_setup_repositories.pp
+++ deployment/puppet/osnailyfacter/manifests/plugins/plugins_setup_repositories.pp
@@ -8,16 +8,9 @@ class osnailyfacter::plugins::plugins_setup_repositories {
     include ::apt
 
     $repositories = generate_plugins_repos($plugins)
-    $repositories_w_prios = generate_plugins_repos($plugins, true)
-    $pins = generate_plugins_pins($repositories_w_prios)
 
     if ! empty($repositories) {
       create_resources(apt::source, $repositories)
-    }
-
-    if !empty($pins)
-    {
-      create_resources(apt::pin, $pins)
     }
 
   }
