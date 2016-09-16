8cd104a6a9ef27426cf66164db0cc23948f49667
Vladimir Kuklin <vkuklin@mirantis.com>
2016-09-16 12:59:11 +0000

    Revert "Upgrade a single package or a bunch of packages"
    This reverts commit cd186656d89c161e77b07c54dd47c88c9d10aacb.
    
    Change-Id: I95ecd1cc0d0ff238d84810d52b5d384b95ad943e

--- deployment/puppet/osnailyfacter/manifests/upgrade/restart_services.pp
+++ deployment/puppet/osnailyfacter/manifests/upgrade/restart_services.pp
@@ -1,12 +1,6 @@
 class osnailyfacter::upgrade::restart_services {
   $mu_upgrade = hiera_hash('mu_upgrade', {})
-  $group_packages = hiera_hash('upgrade_packages',{})
-
   if $mu_upgrade['enabled'] {
     notify { 'restarting services': } ~> Service<||>
-  }
-  elsif !empty($group_packages) {
-    $extended_group_hash = getpackagegrouphash($group_packages)
-    override_pkg_version($extended_group_hash)
   }
 }
