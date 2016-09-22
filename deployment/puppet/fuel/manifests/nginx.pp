c19aa7525af9d5e77d4f70ffbc554e07e1e6d463
Maksim Malchuk <mmalchuk@mirantis.com>
2016-09-22 15:20:47 +0300

    Don't remove default package files instead of empty them
    Change-Id: I6e06552d44f1b81fc18b944e05d94065fbfc77ea
    Closes-Bug: #1626468
    Signed-off-by: Maksim Malchuk <mmalchuk@mirantis.com>

--- deployment/puppet/fuel/manifests/nginx.pp
+++ deployment/puppet/fuel/manifests/nginx.pp
@@ -8,9 +8,10 @@ class fuel::nginx inherits fuel::params {
   file { ['/etc/nginx/conf.d/default.conf',
           '/etc/nginx/conf.d/virtual.conf',
           '/etc/nginx/conf.d/ssl.conf']:
-    ensure => 'absent',
-    notify => Service['nginx'],
-    before => File["/etc/nginx/nginx.conf"],
+    ensure  => 'file',
+    content => "# This file managed by Puppet\n# and should exists to prevent fail on package upgrade\n",
+    notify  => Service['nginx'],
+    before  => File["/etc/nginx/nginx.conf"],
   }
 
   if ( $service_enabled == false ){
