e9c7f453be0b20edc40bd9ecd3718010f46b8bc7
Rodion Tikunov <rtikunov@mirantis.com>
2016-09-28 13:21:21 +0300

    Fix for all possible storage cases
     * Enable swift only if images and objects ceph are disabled
     * Set S3 endpoint using radosgw task
    
    Change-Id: I8611669217827ccc8aad64b89557783470b3d0b3
    Closes-Bug: #1604879
    Closes-Bug: #1590700
    (cherry picked from commit f1dd09eef838de7093bba9902d7d0bb90fd98d9d)

--- deployment/puppet/osnailyfacter/modular/ceph/radosgw_keystone.pp
+++ deployment/puppet/osnailyfacter/modular/ceph/radosgw_keystone.pp
@@ -21,6 +21,10 @@ if $storage_hash['objects_ceph'] {
   $internal_url      = "${internal_protocol}://${internal_address}:8080/swift/v1"
   $admin_url         = "${admin_protocol}://${admin_address}:8080/swift/v1"
 
+  $public_url_s3     = "${public_protocol}://${public_address}:8080"
+  $internal_url_s3   = "${internal_protocol}://${internal_address}:8080"
+  $admin_url_s3      = "${admin_protocol}://${admin_address}:8080"
+
   class {'::osnailyfacter::wait_for_keystone_backends': }
 
   keystone::resource::service_identity { 'radosgw':
@@ -33,6 +37,18 @@ if $storage_hash['objects_ceph'] {
     public_url          => $public_url,
     admin_url           => $admin_url,
     internal_url        => $internal_url,
+  }->
+
+  keystone::resource::service_identity { 'radosgw_s3':
+    configure_user      => false,
+    configure_user_role => false,
+    service_type        => 's3',
+    service_description => 'Openstack Object-Store Service',
+    service_name        => 'swift_s3',
+    region              => $region,
+    public_url          => $public_url_s3,
+    admin_url           => $admin_url_s3,
+    internal_url        => $internal_url_s3,
   }
 
   Class['::osnailyfacter::wait_for_keystone_backends'] -> Keystone::Resource::Service_Identity['radosgw']
