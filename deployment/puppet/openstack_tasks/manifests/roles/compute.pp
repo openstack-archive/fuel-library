65b782595a459848cafe75d3a59c9b97fbe8522e
Pavel Glushchak <pglushchak@virtuozzo.com>
2016-10-04 18:34:29 +0300

    libvirt: don't enable security_driver if selinux disabled
    Change-Id: Ifdc8f5d18ae35651b3e27399217bbd22593122be
    Closes-Bug: #1630284
    Signed-off-by: Pavel Glushchak <pglushchak@virtuozzo.com>
    (cherry picked from commit 72d80348b774995214e88b45a490112fc3fe2187)

--- deployment/puppet/openstack_tasks/manifests/roles/compute.pp
+++ deployment/puppet/openstack_tasks/manifests/roles/compute.pp
@@ -490,18 +490,33 @@ class openstack_tasks::roles::compute {
 
   case $::osfamily {
     'RedHat': {
-      file_line { 'qemu_selinux':
-        path    => '/etc/libvirt/qemu.conf',
-        line    => 'security_driver = "selinux"',
-        require => Package[$::nova::params::libvirt_package_name],
-        notify  => Service['libvirt']
+      if str2bool("${::selinux}") {
+        file_line { 'qemu_selinux':
+          path    => '/etc/libvirt/qemu.conf',
+          line    => 'security_driver = "selinux"',
+          require => Package[$::nova::params::libvirt_package_name],
+          notify  => Service['libvirt']
+        }
+      } else {
+        file_line { 'qemu_selinux_disabled':
+          ensure            => absent,
+          path              => '/etc/libvirt/qemu.conf',
+          match             => '^security_driver',
+          match_for_absence => true,
+          require           => Package[$::nova::params::libvirt_package_name],
+          notify            => Service['libvirt']
+        }
       }
     }
     'Debian': {
+      service { 'apparmor':
+        ensure => running,
+      }
+
       file_line { 'qemu_apparmor':
         path    => '/etc/libvirt/qemu.conf',
         line    => 'security_driver = "apparmor"',
-        require => Package[$::nova::params::libvirt_package_name],
+        require => [Package[$::nova::params::libvirt_package_name], Service['apparmor']],
         notify  => Service['libvirt']
       }
 
