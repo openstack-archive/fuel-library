e2f79d574099da6b17824e1838d1e59f50d79769
Mykyta Karpin <mkarpin@mirantis.com>
2016-10-31 16:47:07 +0200

    Use local memcached for keystonemiddleware in nova
    In order to increase nova performance after failover,
    we need to point nova to local memcached instance for keystone tokens
    
    Change-Id: Ibd9ce480201685e4ff92e87490f5beff49272e87
    Closes-Bug: #1621541
    (cherry picked from commit f1022e3222b4a21821dcdbb302ce12a34711b1ad)

--- tests/noop/spec/hosts/openstack-controller/openstack-controller_spec.rb
+++ tests/noop/spec/hosts/openstack-controller/openstack-controller_spec.rb
@@ -38,6 +38,10 @@ describe manifest do
 
     let(:memcached_servers) { Noop.hiera 'memcached_servers' }
 
+    let(:memcached_port) { Noop.hiera 'memcached_server_port', '11211' }
+    let(:memcached_address) { Noop.puppet_function 'get_network_role_property', 'mgmt/memcache', 'ipaddr' }
+    let(:memcached_authtoken_server) { "#{memcached_address}:#{memcached_port}" }
+
     primary_controller = Noop.hiera 'primary_controller'
     service_endpoint = Noop.hiera 'service_endpoint'
     management_vip = Noop.hiera 'management_vip'
@@ -181,7 +185,7 @@ describe manifest do
 
     it 'nova config should contain right memcached servers list' do
       should contain_nova_config('keystone_authtoken/memcached_servers').with(
-        'value' => memcached_servers.join(','),
+        'value' => memcached_authtoken_server,
       )
     end
 
@@ -217,7 +221,7 @@ describe manifest do
         :database_max_retries   => max_retries,
         :database_max_overflow  => max_overflow,
         :notify_on_state_change => 'vm_and_task_state',
-        :memcached_servers      => memcached_servers,
+        :memcached_servers      => memcached_authtoken_server,
       )
     end
 
