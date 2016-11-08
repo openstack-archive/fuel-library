50567c37ae639b5bc5cfda6da308f93dc6cd9046
Dmitry Ilyin <dilyin@mirantis.com>
2016-11-08 07:38:18 -0800

    Refactoring of the override_resources type for the IaC feature
    Change-Id: I81099ac41c3e2abdaf6fde3a19161709b7ba4b8c

--- deployment/puppet/osnailyfacter/lib/puppet/type/override_resources.rb
+++ deployment/puppet/osnailyfacter/lib/puppet/type/override_resources.rb
@@ -1,19 +1,65 @@
+# This Puppet metatype's purpose is to update the parameters of other type in the catalog
+# with the values from the configuration data structure, or even create the new ones.
+#
+# Configuration structure:
+#
+# configuration:
+#   nova_config:
+#     DEFAULT/debug
+#       ensure: present
+#       value: true
+#     DEFAULT/verbose
+#       ensure: absent
+#
+#   file:
+#     test_file:
+#       ensure: present
+#       path: /tmp/test
+#       content: 123
+#     web_file:
+#       ensure: present
+#       path: /var/www/html/index.html
+#       content: '<html/>'
+#
+#   service:
+#     apache:
+#       ensure: running
+#       enable: true
+#       require: File[web_file]
+#
+# Applying this structure will update the parameters of specified resources to the values in
+# the configuration structure, and, if the "create" option is set, will create the missing ones.
+# The affected Puppet types can be filtered using the "types" and "titles" options. You can provide
+# the list of Puppet type names and/or the list of resource titles you want to be processed.
+#
 Puppet::Type.newtype(:override_resources) do
 
-  newparam(:type) do
+  newparam(:name) do
+    desc 'The uniq name of this override)resources type. Serves no purpose other then reference.'
     isnamevar
   end
 
-  newparam(:data) do
+  newparam(:configuration) do
+    desc 'The configuration data structure to work with.'
     defaultto {}
   end
 
   newparam(:defaults) do
+    desc 'The default parameters. They will be applied to every resource created or updated.'
     defaultto {}
   end
 
-  newparam(:create_res) do
+  newparam(:create) do
+    desc 'Should this type try to create the new resources in the configuration data or only update the existing ones?'
     defaultto true
+  end
+
+  newparam(:types) do
+    desc 'The list of Puppet types to process. If the list is empty all the types in the configuration data will be processed.'
+  end
+
+  newparam(:titles) do
+    desc 'The list of Puppet resource titles to process. If the list is empty all the resources in the configuration data will be processed.'
   end
 
   def update_resource(type, title, parameters = {})
@@ -42,7 +88,7 @@ Puppet::Type.newtype(:override_resources) do
     type = self[:type]
     data = self[:data] || {}
     defaults = self[:defaults] || {}
-    create_res = self[:create_res] || false
+    create = self[:create] || false
 
     fail 'Title should be a resource type to override!' unless type and not type.empty?
     fail 'Data should contain resource hash!' unless data.is_a? Hash
@@ -56,7 +102,7 @@ Puppet::Type.newtype(:override_resources) do
       if resource
         debug "#{type}[#{title}]: found in the catalog, updating it"
         update_resource type, title, parameters
-      elsif create_res
+      elsif create
         debug "#{type}[#{title}]: was not found in the catalog, creating it"
         new_resources << create_resource(type, title, parameters)
       end
