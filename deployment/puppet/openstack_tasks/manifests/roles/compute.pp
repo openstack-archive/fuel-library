74cb5ecc0caefb09ae6439e358220d7e90bad039
Dmitry Ilyin <dilyin@mirantis.com>
2016-11-08 07:38:18 -0800

    Refactoring of the override_resources type for the IaC feature
    Change-Id: I81099ac41c3e2abdaf6fde3a19161709b7ba4b8c

--- deployment/puppet/openstack_tasks/manifests/roles/compute.pp
+++ deployment/puppet/openstack_tasks/manifests/roles/compute.pp
@@ -9,25 +9,14 @@ class openstack_tasks::roles::compute {
   $nova_service_down_time = hiera('nova_service_down_time', '180')
   prepare_network_config($network_scheme)
 
-  $override_values = values($override_configuration)
-  if !empty($override_values) and has_key($override_values[0], 'data') {
-    # Create resources of type 'override_resources'. These, in turn,
-    # will either update existing resources in the catalog with new data,
-    # or create these resources, if they do not actually exist.
-    create_resources(override_resources, $override_configuration)
-  } else {
-    # override nova options
-    override_resources { 'nova_config':
-      data => $override_configuration['nova_config']
-    }
+  $override_configuration = hiera_hash(configuration, {})
+  $override_configuration_options = hiera_hash(configuration_options, {})
+  
+  override_resources {'override-resources':
+    configuration => $override_configuration,
+    options       => $override_configuration_options,
+  }   
 
-    # override nova-api options
-    override_resources { 'nova_paste_api_ini':
-      data => $override_configuration['nova_paste_api_ini']
-    }
-
-    Override_resources <||> ~> Service <| tag == 'nova-service' |>
-  }
 
   # Pulling hiera
   $compute_hash                   = hiera_hash('compute', {})
