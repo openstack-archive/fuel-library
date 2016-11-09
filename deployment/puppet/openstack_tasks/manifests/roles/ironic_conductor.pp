118aa2095da50be9a418cff5ea6a5f4c39388e08
Sergey Abramov <sabramov@mirantis.com>
2016-11-09 12:43:30 +0300

    Use memcache for keystone_authtoken
    This change updates where we calculate the memcache server information
    to the globals hiera data and updates all the openstack services to
    use the heira memcached_servers key for consistency. Additionally all
    api services have been updated to configure the memcached servers for
    the keystone_authtoken middleware configuration to improve the
    responsiveness of the service.
    
    Change-Id: Id1034e22d79c3ea6b25575d9bcf8e8750a02365d
    Closes-Bug: #1597512
    (cherry picked from commit df416f53d2dcb21c700802f5c2602d19f6d6c70c)

--- deployment/puppet/openstack_tasks/manifests/roles/ironic_conductor.pp
+++ deployment/puppet/openstack_tasks/manifests/roles/ironic_conductor.pp
@@ -31,6 +31,7 @@ class openstack_tasks::roles::ironic_conductor {
   $ironic_user                = pick($ironic_hash['auth_name'],'ironic')
   $ironic_user_password       = pick($ironic_hash['user_password'],'ironic')
   $ironic_swift_tempurl_key   = pick($ironic_hash['swift_tempurl_key'],'ironic')
+  $memcached_servers          = hiera('memcached_servers')
 
   $db_type                    = 'mysql'
   $db_host                    = pick($ironic_hash['db_host'], $database_vip)
@@ -101,6 +102,7 @@ class openstack_tasks::roles::ironic_conductor {
     'keystone_authtoken/admin_tenant_name': value => $ironic_tenant;
     'keystone_authtoken/admin_user':        value => $ironic_user;
     'keystone_authtoken/admin_password':    value => $ironic_user_password, secret => true;
+    'keystone_authtoken/memcached_servers': value => join(any2array($memcached_servers), ',');
     'glance/swift_temp_url_key':            value => $ironic_swift_tempurl_key;
     'glance/swift_endpoint_url':            value => "http://${baremetal_vip}:8080";
     'glance/temp_url_endpoint_type':        value => $temp_url_endpoint_type;
