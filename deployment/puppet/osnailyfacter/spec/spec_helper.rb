e53917e647f00a968ffc32da4e8bc9f180ee17e2
Dmitry Ilyin <dilyin@mirantis.com>
2016-11-22 15:29:30 -0800

    [sync/mitaka] Remove loader: osnailyfacter
    Change-Id: Ic8c1815440069956fa7239a8923795cfbd9c198b

--- deployment/puppet/osnailyfacter/spec/spec_helper.rb
+++ deployment/puppet/osnailyfacter/spec/spec_helper.rb
@@ -2,19 +2,31 @@ require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'webmock/rspec'
 
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+PROJECT_ROOT = File.expand_path '..', File.dirname(__FILE__)
 
-PROJECT_ROOT = File.expand_path('..', File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(PROJECT_ROOT, "lib"))
-
-# Add fixture lib dirs to LOAD_PATH. Work-around for PUP-3336
-Dir["#{fixture_path}/modules/*/lib"].entries.each do |lib_dir|
-  $LOAD_PATH << lib_dir
+def add_plugin_lib_dir(*plugin_dirs)
+  plugin_dirs.each do |plugin_dir|
+    lib_dir = File.join plugin_dir, 'lib'
+    next unless File.directory? lib_dir
+    # puts "Add lib dir to the load path: '#{lib_dir}'"
+    $LOAD_PATH << lib_dir
+  end
 end
 
+def add_fixtures_lib_dirs
+  fixture_modules_dir = File.join PROJECT_ROOT, 'spec', 'fixtures', 'modules'
+  return unless File.directory? fixture_modules_dir
+  Dir.entries(fixture_modules_dir).each do |plugin|
+    next if %w(. ..).include? plugin
+    plugin_dir = File.join fixture_modules_dir, plugin
+    add_plugin_lib_dir plugin_dir
+  end
+end
+
+add_plugin_lib_dir PROJECT_ROOT
+add_fixtures_lib_dirs
+
 RSpec.configure do |c|
-  c.module_path = File.join(fixture_path, 'modules')
-  c.manifest_dir = File.join(fixture_path, 'manifests')
   c.mock_with(:mocha)
   c.alias_it_should_behave_like_to :it_configures, 'configures'
 
