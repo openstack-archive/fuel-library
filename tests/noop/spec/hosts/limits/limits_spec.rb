300392049fed6e2bdfbb2395a9e9ae6aac745a3c
Oleksiy Molchanov <omolchanov@mirantis.com>
2016-11-30 13:16:49 +0200

    Refactor limits for max open files
    * Move mof limits to separate task
    * Add mof limits for non-controller nodes
    * Add mof limits for libvirt
    
    Closes-Bug: 1644318
    Change-Id: I56d041a16fc9ef4ec1853b6b8880974a5dd70757

---
+++ tests/noop/spec/hosts/limits/limits_spec.rb
@@ -0,0 +1,54 @@
+# ROLE: primary-mongo
+# ROLE: primary-controller
+# ROLE: mongo
+# ROLE: ironic
+# ROLE: controller
+# ROLE: compute
+# ROLE: cinder-vmware
+# ROLE: cinder-block-device
+# ROLE: cinder
+# ROLE: ceph-osd
+require 'spec_helper'
+require 'shared-examples'
+manifest = 'limits/limits.pp'
+
+describe manifest do
+  shared_examples 'catalog' do
+
+    let(:roles) do
+      Noop.hiera 'roles'
+    end
+
+    let(:limits) do
+      Noop.hiera 'limits', {}
+    end
+
+    let(:general_mof_limit) do
+      Noop.puppet_function 'pick', limits['general_mof_limit'], '102400'
+    end
+
+    let(:libvirt_mof_limit) do
+      Noop.puppet_function 'pick', limits['libvirt_mof_limit'], '102400'
+    end
+
+    it 'should configure general max open files limit' do
+      should contain_limits__limits('*/nofile').with(
+        'hard' => general_mof_limit,
+        'soft' => general_mof_limit
+      )
+      end
+      should contain_limits__limits('root/nofile').with(
+        'hard' => general_mof_limit,
+        'soft' => general_mof_limit 
+      )
+      end
+    end
+
+    if Noop.puppet_function 'member', roles, 'compute'
+      it 'should configure libvirt max open files limit' do
+        should contain_file('/etc/init/libvirtd.override').with( 'content' => "limit nofile #{libvirt_mof_limits} #{libvirt_mof_limit}" )
+      end
+    end
+
+  end
+end
