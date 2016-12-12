14b64f3f456d545b8861807a0cf4ede63ed3564b
Mykyta Karpin <mkarpin@mirantis.com>
2016-12-12 17:33:31 +0200

    Enable filtering for Cinder LVM volumes
    Currently the default behavior of LVM is
    to scan all available block devices in
    /dev folder, this leads to issues on environments
    with Cinder LVM volumes. VM's volumes are recognized
    by parent host's OS and are scanned by LVM. If VM's
    volume contains LVM inside, it is also detected by
    parent host's OS. Such VM's volumes can not be
    deleted.
    
    This patch modifies LVM filter on Cinder node in
    order to exclude LVs present in Cinder volume group
    from LVM scan. During testing it was found that
    initramds should be updated upon changing of
    filtering rules.
    
    Closes-Bug: #1645624
    
    Change-Id: I49e286e92095259e0196b6dd49dc10da0ebb4c20

--- deployment/puppet/openstack_tasks/manifests/openstack_cinder/openstack_cinder.pp
+++ deployment/puppet/openstack_tasks/manifests/openstack_cinder/openstack_cinder.pp
@@ -75,6 +75,20 @@ class openstack_tasks::openstack_cinder::openstack_cinder {
   if roles_include(['cinder']) and $storage_hash['volumes_lvm'] {
     $manage_volumes = 'iscsi'
     $volume_backend_name = $storage_hash['volume_backend_names']['volumes_lvm']
+
+    $cinder_lvm_filter = "\"r|^/dev/${volume_group}/.*|\""
+
+    augeas { 'lvm-conf-set-cinder-filter':
+      context => '/files/etc/lvm/lvm.conf/devices/dict/',
+      changes => "set global_filter/list/1/str ${cinder_lvm_filter}",
+      tag     => 'lvm-conf-augeas'
+    }
+    exec { 'Update initramfs':
+      command => 'update-initramfs -u -k all',
+      path    => '/usr/bin:/bin:/usr/sbin:/sbin'
+    }
+    Augeas<| tag == 'lvm-conf-augeas'|> ~> Exec<| title == 'Update initramfs' |>
+
   } elsif ($storage_hash['volumes_ceph']) {
     $manage_volumes = 'ceph'
     $volume_backend_name = $storage_hash['volume_backend_names']['volumes_ceph']
