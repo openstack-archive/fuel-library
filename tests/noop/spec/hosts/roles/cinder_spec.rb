842fef7d257f931c5e14add6577e4f8337ed921d
Mykyta Karpin <mkarpin@mirantis.com>
2016-12-12 17:33:31 +0200

    Enable filtering for Cinder LVM volumes
    Currently the default behavior of LVM is
    to scan all available block devices in
    /dev folder, this leads to issues on environments
    with Cinder LVM volumes. Guest's volumes are recognized
    by parent host's OS and are scanned by LVM. If guest's
    volume contains LVM inside, it is also detected by
    parent host's OS. Such volumes can not be
    deleted.
    
    This patch modifies LVM filter on Cinder node in
    order to exclude LVs present in Cinder volume group
    from LVM scan. During testing it was found that
    initramds should be updated upon changing of
    filtering rules.
    
    Closes-Bug: #1645624
    Change-Id: I49e286e92095259e0196b6dd49dc10da0ebb4c20
    (backported from commits 8c3b894c636a615d3553b8043b32613d39da8b66
    f935feff37f902ca8152bd92a9aee04ea2a2bd60)

--- tests/noop/spec/hosts/roles/cinder_spec.rb
+++ tests/noop/spec/hosts/roles/cinder_spec.rb
@@ -8,6 +8,28 @@ describe manifest do
   storage_hash = Noop.hiera 'storage_hash'
   volume_backend_name = storage_hash['volume_backend_names']
   use_ceph = Noop.hiera 'use_ceph'
+  volume_group = Noop.hiera('cinder_volume_group', 'cinder')
+  cinder = Noop.puppet_function 'roles_include', 'cinder'
+
+  if cinder and storage_hash['volumes_lvm']
+    it 'sets up filtering for LVM Cinder volumes' do
+
+      cinder_lvm_filter = "\"r|^/dev/#{volume_group}/.*|\""
+
+      should contain_file_line('lvm-conf-set-cinder-filter').with(
+        'ensure' => 'present',
+        'path'   => '/etc/lvm/lvm.conf',
+        'line'   => "global_filter = #{cinder_lvm_filter}",
+        'match'  => 'global_filter\ \=\ ',
+        'tag'    => 'lvm-conf-file-line'
+      ).that_notifies('Exec[Update initramfs]')
+
+      should contain_exec('Update initramfs').with(
+        'command'     => 'update-initramfs -u -k all',
+        'path'        => '/usr/bin:/bin:/usr/sbin:/sbin',
+        'refreshonly' => 'true')
+    end
+  end
 
   if use_ceph and !(storage_hash['volumes_lvm']) and !(member($roles, 'cinder-vmware'))
       it { should contain_class('ceph') }
