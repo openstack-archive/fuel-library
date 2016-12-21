e800ce73c7ec903aa8af3992bce0f7f61727a9ba
Mikhail <mzhnichkov@mirantis.com>
2016-12-21 15:45:36 +0400

    Configure number of multiq for dpdk
    We take the value of multiq from pmd_core_mask parameter
    
    Change-Id: I9b76b680d8215740450ef25c734b7adb8dddc4e0
    Closes-Bug: 1643920

--- deployment/puppet/l23network/spec/unit/puppet/provider/l23_stored_config/dpdkovs_ubuntu__port__spec.rb
+++ deployment/puppet/l23network/spec/unit/puppet/provider/l23_stored_config/dpdkovs_ubuntu__port__spec.rb
@@ -4,10 +4,11 @@ describe Puppet::Type.type(:l23_stored_config).provider(:dpdkovs_ubuntu) do
   let(:input_data) {
     {
       :'enp1s0f0' => {
-                 :name     => 'enp1s0f0',
-                 :if_type  => 'ethernet',
-                 :bridge   => 'br-prv',
-                 :provider => 'dpdkovs_ubuntu',
+                 :name          => 'enp1s0f0',
+                 :if_type       => 'ethernet',
+                 :bridge        => 'br-prv',
+                 :provider      => 'dpdkovs_ubuntu',
+                 :multiq_thread => '0x300002',
                },
     }
   }
@@ -71,6 +72,7 @@ describe Puppet::Type.type(:l23_stored_config).provider(:dpdkovs_ubuntu) do
       it { expect(cfg_file).to match(/ovs_type\s+DPDKOVSPort/) }
       it { expect(cfg_file).to match(/ovs_bridge\s+br-prv/) }
       it { expect(cfg_file).to match(/dpdk_port\s+dpdk0/) }
+      it { expect(cfg_file).to match(/multiq_threads\s+3/) }
       it { expect(cfg_file.split(/\n/).reject{|x| x=~/(^\s*$)|(^#.*$)/}.length). to eq(5) }
     end
   end
@@ -83,6 +85,7 @@ describe Puppet::Type.type(:l23_stored_config).provider(:dpdkovs_ubuntu) do
       it { expect(res[:bridge]).to eq "br-prv" }
       it { expect(res[:if_provider].to_s).to eq 'dpdkovs' }
       it { expect(res[:dpdk_port].to_s).to eq 'dpdk0' }
+      it { expect(res[:multiq_threads].to_s).to eq '3' }
     end
   end
 end
