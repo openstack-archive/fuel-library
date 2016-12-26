a1e3e168962fbfa26bd4187bf9c1447d2d59181c
Michael Polenchuk <mpolenchuk@mirantis.com>
2016-12-26 16:08:23 +0400

    Bring in syslog transport option
    Add option to set syslog transport by means of
    deployment facts modification:
      base_syslog:
        syslog_transport: udp | tcp
    
    DocImpact
    Depends-On: I19c307d795236e588eb99d7f81dcb10d79ff7da7
    Closes-Bug: #1651437
    Change-Id: I944ca4617b0dc754188cf9835d590e0666d9f2bf

--- tests/noop/spec/hosts/logging/logging_spec.rb
+++ tests/noop/spec/hosts/logging/logging_spec.rb
@@ -16,6 +16,8 @@ manifest = 'logging/logging.pp'
 
 describe manifest do
   shared_examples 'catalog' do
+    base_syslog = Noop.hiera('base_syslog')
+
     it {
       if facts[:operatingsystem] == 'Ubuntu'
         should contain_file('/var/log').with(
@@ -40,7 +42,31 @@ describe manifest do
     it {
       should contain_class('cluster::haproxy::rsyslog')
     }
+
+    it 'should contain class openstack::logging' do
+      is_expected.to contain_class('openstack::logging').with(
+        :role               => 'client',
+        :show_timezone      => true,
+        :log_remote         => true,
+        :log_local          => true,
+        :log_auth_local     => true,
+        :rotation           => 'weekly',
+        :keep               => '4',
+        :minsize            => '10M',
+        :maxsize            => '100M',
+        :rservers           => [
+          ['remote_type', Noop.puppet_function('pick', base_syslog['syslog_transport'], 'tcp')],
+          ['server', base_syslog['syslog_server']],
+          ['port', base_syslog['syslog_port']]
+        ],
+        :virtual            => Noop.puppet_function('str2bool', facts[:is_virtual]),
+        :rabbit_fqdn_prefix => Noop.hiera('node_name_prefix_for_messaging', 'messaging-'),
+        :rabbit_log_level   => 'NOTICE',
+        :debug              => false,
+        :ironic_collector   => Noop.puppet_function('roles_include', 'ironic'),
+      )
+    end
+
   end
   test_ubuntu_and_centos manifest
 end
-
