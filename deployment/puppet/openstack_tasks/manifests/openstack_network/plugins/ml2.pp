94f8642e0c9a82c73e52f145d757776f425a9e10
Igor Gajsin <igajsin@mirantis.com>
2016-12-26 13:32:38 +0300

    Restart ovs-agent when redeploy an environment
    There are 2 tasks: common-config and network-plugins-l2. Normally the
    common-config configures neutron.conf and then plugns-l2 starts
    the ovs-agent.
    
    But there is the wrong behaviour when new controllers are added to an
    environment:
      1. configure and deploy an env.
      2. common-config configures neutron.conf
      3. plugins-l2 starts ovs-agent.
      4. add new controllers to the env and redeploy it.
      5. common-config configures neutron.conf.
      6. plugins-l2 knows nothing about changes in the config.
    
    So ovs-agent doesn't restarted and works with old configuration data.
    
    This commit makes ml2 task to check md5 hash of neutron.conf and
    restart agent when config is changed.
    
    Change-Id: Ia26f4a5105407ce322a5185455093d69a938edfc
    Closes-bug: #1613724

--- deployment/puppet/openstack_tasks/manifests/openstack_network/plugins/ml2.pp
+++ deployment/puppet/openstack_tasks/manifests/openstack_network/plugins/ml2.pp
@@ -107,6 +107,11 @@ class openstack_tasks::openstack_network::plugins::ml2 {
     'securitygroup/enable_security_group': value => $enable_security_group;
   }
 
+  if is_neutron_conf_updated() {
+    notify{'neutron.conf has been changed, going to restart ovs agend':
+    } ~> Service['neutron-ovs-agent-service']
+  }
+
   Neutron_agent_ovs<||> ~> Service['neutron-ovs-agent-service']
 
   class { '::neutron::agents::ml2::ovs':
