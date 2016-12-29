dbe90e6a402e7b1ac1482f0b25fd05988d567ff4
Mikhail <mzhnichkov@mirantis.com>
2016-12-30 02:43:03 +0400

    Configure number of multiq for dpdk
    Change-Id: I34781a752fba5cc35cbe4190619a8869d07c4993
    Closes-Bug: 1643920
    Depends-On: I9b3160b7e0659d11445ca3927aa38e523a93b3a9

--- deployment/puppet/l23network/spec/unit/puppet/provider/l23_stored_config/dpdkovs_ubuntu__port__spec.rb
+++ deployment/puppet/l23network/spec/unit/puppet/provider/l23_stored_config/dpdkovs_ubuntu__port__spec.rb
@@ -71,7 +71,8 @@ describe Puppet::Type.type(:l23_stored_config).provider(:dpdkovs_ubuntu) do
       it { expect(cfg_file).to match(/ovs_type\s+DPDKOVSPort/) }
       it { expect(cfg_file).to match(/ovs_bridge\s+br-prv/) }
       it { expect(cfg_file).to match(/dpdk_port\s+dpdk0/) }
-      it { expect(cfg_file.split(/\n/).reject{|x| x=~/(^\s*$)|(^#.*$)/}.length). to eq(5) }
+      it { expect(cfg_file).to match(/multiq_threads\s+3/) }
+      it { expect(cfg_file.split(/\n/).reject{|x| x=~/(^\s*$)|(^#.*$)/}.length). to eq(6) }
     end
   end
 
@@ -83,6 +84,7 @@ describe Puppet::Type.type(:l23_stored_config).provider(:dpdkovs_ubuntu) do
       it { expect(res[:bridge]).to eq "br-prv" }
       it { expect(res[:if_provider].to_s).to eq 'dpdkovs' }
       it { expect(res[:dpdk_port].to_s).to eq 'dpdk0' }
+      it { expect(res[:multiq_threads].to_s).to eq '3' }
     end
   end
 end
