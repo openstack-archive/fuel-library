b471194e98eefcf3695addc2a7a3de9ee51e4b32
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2017-01-10 07:18:31 +0000

    Revert "Revert "Base code for remove OVS usage ability""
    Closes-Bug: #1655246
    This reverts commit 314744074ce68ae75cfbf68f5afc88f8ccd11adb.
    
    Change-Id: I7488791c11afe726b056dc33bd362867cf53174e

---
+++ deployment/puppet/osnailyfacter/spec/functions/remove_ovs_usage_spec.rb
@@ -0,0 +1,102 @@
+require 'spec_helper'
+
+describe 'remove_ovs_usage' do
+
+  let(:input_without_transformations) {
+    {
+      'version' => 1.2,
+      'transformations' => {},
+    }
+  }
+
+  let(:output_without_transformations) {
+    {
+      'use_ovs' => false,
+    }
+  }
+
+  let(:i_w_nonpatch) {
+    {
+      'version' => 1.2,
+      'transformations' => [
+        {
+          'provider' => 'ovs',
+          'action' => 'foo',
+          'name' => 'bar',
+        }
+      ],
+    }
+  }
+
+  let(:o_w_nonpatch) {
+    {
+      'use_ovs' => false,
+      'network_scheme' => {
+        'transformations' => [
+          {
+            'action' => 'override',
+            'override' => 'bar',
+            'provider' => 'lnx',
+          }
+        ]
+      }
+    }
+  }
+
+  let(:i_w_patch) {
+    {
+      'version' => 1.2,
+      'transformations' => [
+        {
+          'provider' => 'ovs',
+          'action' => 'add-patch',
+          'name' => 'bar',
+          'bridges' => [
+            'bridge-0',
+            'bridge-1',
+          ]
+        }
+      ],
+    }
+  }
+
+  let(:o_w_patch) {
+    {
+      'use_ovs' => false,
+      'network_scheme' => {
+        'transformations' => [
+          {
+            'action' => 'override',
+            'override' => 'patch-bridge-0:bridge-1',
+            'provider' => 'lnx',
+          }
+        ]
+      }
+    }
+  }
+
+  it 'should exist' do
+    is_expected.not_to be_nil
+  end
+
+  it 'should expect 1 argument' do
+    is_expected.to run.with_params().and_raise_error(Puppet::ParseError)
+    is_expected.to run.with_params(1, 2).and_raise_error(Puppet::ParseError)
+  end
+
+  it 'should expect a hash as given argument' do
+    is_expected.to run.with_params('foo').and_raise_error(Puppet::ParseError)
+  end
+
+  it 'should return a hash with disabled ovs in any case' do
+    is_expected.to run.with_params(input_without_transformations).and_return(output_without_transformations.to_yaml() + "\n")
+  end
+
+  it 'should return a proper hash when override action is not add-patch' do
+    is_expected.to run.with_params(i_w_nonpatch).and_return(o_w_nonpatch.to_yaml() + "\n")
+  end
+
+  it 'should return a proper hash when override action is an add-patch' do
+    is_expected.to run.with_params(i_w_patch).and_return(o_w_patch.to_yaml() + "\n")
+  end
+end
