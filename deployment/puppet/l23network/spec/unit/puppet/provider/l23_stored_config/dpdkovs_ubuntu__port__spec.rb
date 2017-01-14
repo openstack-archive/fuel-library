0c205fbef76b2cbd2f8d61950a12f46e7b2890ab
Mikhail <mzhnichkov@mirantis.com>
2017-01-14 19:09:20 +0400

    Correct MTU
    Correct MTU for interface with 'i40e' driver
    Closes-Bug: #1587310
    
    Change-Id: I2cee98194c4d1761feeb1999804c0e24a467eb2c

--- deployment/puppet/l23network/spec/unit/puppet/provider/l23_stored_config/dpdkovs_ubuntu__port__spec.rb
+++ deployment/puppet/l23network/spec/unit/puppet/provider/l23_stored_config/dpdkovs_ubuntu__port__spec.rb
@@ -11,6 +11,7 @@ describe Puppet::Type.type(:l23_stored_config).provider(:dpdkovs_ubuntu) do
                  :vendor_specific => {
                    :max_queues => 3
                  },
+                 :mtu             => 1500,
                },
     }
   }
@@ -20,6 +21,13 @@ describe Puppet::Type.type(:l23_stored_config).provider(:dpdkovs_ubuntu) do
       'enp1s0f0' => 'dpdk0'
     }
   }
+
+  let(:dpdk_ports_mapping_i40e) {
+    {
+      'enp1s0f1' => 'dpdk0'
+    }
+  }
+
 
   let(:resources) do
     resources = {}
@@ -75,7 +83,8 @@ describe Puppet::Type.type(:l23_stored_config).provider(:dpdkovs_ubuntu) do
       it { expect(cfg_file).to match(/ovs_bridge\s+br-prv/) }
       it { expect(cfg_file).to match(/dpdk_port\s+dpdk0/) }
       it { expect(cfg_file).to match(/multiq_threads\s+3/) }
-      it { expect(cfg_file.split(/\n/).reject{|x| x=~/(^\s*$)|(^#.*$)/}.length). to eq(6) }
+      it { expect(cfg_file).to match(/mtu_request\s+1500/) }
+      it { expect(cfg_file.split(/\n/).reject{|x| x=~/(^\s*$)|(^#.*$)/}.length). to eq(7) }
     end
   end
 
@@ -88,6 +97,7 @@ describe Puppet::Type.type(:l23_stored_config).provider(:dpdkovs_ubuntu) do
       it { expect(res[:if_provider].to_s).to eq 'dpdkovs' }
       it { expect(res[:dpdk_port].to_s).to eq 'dpdk0' }
       it { expect(res[:multiq_threads].to_s).to eq '3' }
+      it { expect(res[:mtu].to_s).to eq '1500' }
     end
   end
 end
