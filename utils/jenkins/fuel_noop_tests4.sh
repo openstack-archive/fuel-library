920cf3c656f4296e712b1b593a7d978e83acf501
Mykyta Karpin <mkarpin@mirantis.com>
2017-02-02 11:34:11 +0200

    Use Puppet 4.5.2 in puppet4 noop tests
    As we plan to package puppet 4.5.2,
    we need to run noop tests with this version
    instead of 4.5.0.
    
    Change-Id: I4b5303166b028205c004cf50f7b47416d47ac312

--- utils/jenkins/fuel_noop_tests4.sh
+++ utils/jenkins/fuel_noop_tests4.sh
@@ -2,7 +2,7 @@
 DIR=`dirname $0`
 cd $DIR || exit 1
 
-export PUPPET_GEM_VERSION="4.5.0"
+export PUPPET_GEM_VERSION="4.5.2"
 export STRICT_VARIABLES="no"
 
 echo "Running the noop tests with Puppet version: ${PUPPET_GEM_VERSION}"
