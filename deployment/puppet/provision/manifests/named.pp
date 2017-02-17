0b5ab4ff80414adad996012b9152de0719888274
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2017-02-17 21:20:09 +0300

    Remove cobbler and use dhcpd named and tftp
    Implements: blueprint get-rid-cobbler-dnsmasq
    Change-Id: I80c62056dcfdf70b3786e163960d0307849e019c

---
+++ deployment/puppet/provision/manifests/named.pp
@@ -0,0 +1,51 @@
+class provision::named (
+  $domain_name        = $::provision::params::domain_name,
+  $dns_address        = $::provision::params::dns_address,
+  $forwarders         = $::provision::params::forwarders,
+  $ddns_key           = $::provision::params::ddns_key,
+  $ddns_key_algorithm = $::provision::params::ddns_key_algorithm,
+  $ddns_key_name      = $::provision::params::ddns_key_name,
+) inherits provision::params {
+
+  package { "named" :
+    name => $::provision::params::named_package,
+  }
+
+  file { "/var/named" :
+    ensure => directory,
+    owner => 'named',
+    group => 'named',
+    mode => '0750',
+    require => Package["named"],
+  }
+
+  file { $::provision::params::named_conf :
+    ensure  => present,
+    content => template('provision/named.conf.erb'),
+    owner   => 'named',
+    group   => 'named',
+    mode    => '0640',
+    require => Package["named"],
+    notify  => Service["named"],
+  }
+
+  file { "/var/named/${domain_name}" :
+    ensure  => present,
+    content => template('provision/zone.erb'),
+    owner   => 'named',
+    group   => 'named',
+    mode    => '0644',
+    require => Package["named"],
+    notify  => Service["named"],
+  }
+
+  service { "named" :
+    name    => $::provision::params::named_service,
+    ensure  => running,
+    enable  => true,
+    hasrestart => false,
+    hasstatus => false,
+    require => Package["named"],
+  }
+
+}
