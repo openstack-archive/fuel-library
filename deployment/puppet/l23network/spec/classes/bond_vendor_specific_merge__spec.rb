b43956ce2cc129a5e9aa9c236e683a7b4a706b24
Sergey Vasilenko <svasilenko@mirantis.com>
2017-02-28 14:34:25 +0300

    Support bonds of subinterfaces while merge interface_properties
    Change-Id: I27ede232a37c4e8c2d669f5dd8936a60baf04b40
    Related-bug: #1643920
    Closes-bug: #1666229

--- deployment/puppet/l23network/spec/classes/bond_vendor_specific_merge__spec.rb
+++ deployment/puppet/l23network/spec/classes/bond_vendor_specific_merge__spec.rb
@@ -65,6 +65,133 @@ describe 'l23network::examples::run_network_scheme', :type => :class do
 
   end
 
+  context 'pass vendor_specific through interface_properties for bonds implicit subinterfaces' do
+    let(:title) { 'xxx' }
+    let(:facts) {
+      {
+        :osfamily => 'Debian',
+        :operatingsystem => 'Ubuntu',
+        :kernel => 'Linux',
+        :l23_os => 'ubuntu',
+        :l3_fqdn_hostname => 'stupid_hostname',
+      }
+    }
+
+    let(:params) do {
+      :settings_yaml => '''
+        network_scheme:
+          version: 1.1
+          provider: lnx
+          interfaces:
+            eth2: {}
+          transformations:
+            - action: add-bond
+              name: bond23
+              interfaces:
+                - eth2.11
+                - eth2.12
+              interface_properties:
+                vendor_specific:
+                  aaa: bbb
+          endpoints: {}
+          roles: {}
+      ''',
+    } end
+
+    before(:each) do
+      puppet_debug_override()
+    end
+
+    it do
+      is_expected.to compile.with_all_deps
+    end
+
+    it do
+      is_expected.to contain_l2_bond('bond23').with({
+        'ensure' => 'present',
+        'slaves' => ['eth2.11', 'eth2.12'],
+      })
+    end
+
+    ['eth2.11', 'eth2.12'].each do |iface|
+      it do
+        is_expected.to contain_l2_port(iface).with({
+          'ensure'  => 'present',
+          'bond_master'  => 'bond23',
+          'vendor_specific' => {
+            'aaa' => 'bbb',
+          }
+        })
+      end
+    end
+  end
+
+  context 'pass vendor_specific through interface_properties for bonds explicit subinterfaces' do
+    let(:title) { 'xxx' }
+    let(:facts) {
+      {
+        :osfamily => 'Debian',
+        :operatingsystem => 'Ubuntu',
+        :kernel => 'Linux',
+        :l23_os => 'ubuntu',
+        :l3_fqdn_hostname => 'stupid_hostname',
+      }
+    }
+
+    let(:params) do {
+      :settings_yaml => '''
+        network_scheme:
+          version: 1.1
+          provider: lnx
+          interfaces:
+            eth2: {}
+          transformations:
+            - action: add-port
+              name: eth2.11
+            - action: add-port
+              name: eth2.12
+            - action: add-bond
+              name: bond23
+              interfaces:
+                - eth2.11
+                - eth2.12
+              interface_properties:
+                vendor_specific:
+                  aaa: bbb
+          endpoints: {}
+          roles: {}
+      ''',
+    } end
+
+    before(:each) do
+      puppet_debug_override()
+    end
+
+    it do
+      is_expected.to compile.with_all_deps
+    end
+
+    it do
+      is_expected.to contain_l2_bond('bond23').with({
+        'ensure' => 'present',
+        'slaves' => ['eth2.11', 'eth2.12'],
+      })
+    end
+
+    ['eth2.11', 'eth2.12'].each do |iface|
+      it do
+        is_expected.to contain_l2_port(iface).with({
+          'ensure'  => 'present',
+          'bond_master'  => 'bond23',
+          'vendor_specific' => {
+            'aaa' => 'bbb',
+          }
+        })
+      end
+    end
+
+  end
+
   context 'merge vendor_specific from port to interface_properties' do
     let(:title) { 'xxx' }
     let(:facts) {
@@ -175,6 +302,59 @@ describe 'l23network::examples::run_network_scheme', :type => :class do
           }
         })
       end
+    end
+
+  end
+
+
+  context 'no interface_properties in the bond given' do
+    let(:title) { 'xxx' }
+    let(:facts) {
+      {
+        :osfamily => 'Debian',
+        :operatingsystem => 'Ubuntu',
+        :kernel => 'Linux',
+        :l23_os => 'ubuntu',
+        :l3_fqdn_hostname => 'stupid_hostname',
+      }
+    }
+
+    let(:params) do {
+      :settings_yaml => '''
+        network_scheme:
+          version: 1.1
+          provider: lnx
+          interfaces:
+            eth2:
+              vendor_specific:
+                ifnumber: 2
+            eth3:
+              vendor_specific:
+                ifnumber: 3
+          transformations:
+            - action: add-bond
+              name: bond23
+              interfaces:
+                - eth2
+                - eth3
+          endpoints: {}
+          roles: {}
+      ''',
+    } end
+
+    before(:each) do
+      puppet_debug_override()
+    end
+
+    it do
+      is_expected.to compile.with_all_deps
+    end
+
+    it do
+      is_expected.to contain_l2_bond('bond23').with({
+        'ensure' => 'present',
+        'slaves' => ['eth2', 'eth3'],
+      })
     end
 
   end
