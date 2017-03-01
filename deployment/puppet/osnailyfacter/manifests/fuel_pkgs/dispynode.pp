a4c9b08e5979bbc867a47b8bcf9e1aec3eadf5fe
Igor Gajsin <igajsin@mirantis.com>
2017-03-01 17:46:07 +0300

    install and launch dispnynode.pp
    The dispynode.pp should work on each of the node in a cloud. This
    patch-set installs and starts that process on the master and slaves
    nodes of an environment.
    
    Change-Id: I7eb6ddf823ea9ab85eb8b0ceaa6eb3243a21ce85
    Implements: blueprint distributed-serialization

---
+++ deployment/puppet/osnailyfacter/manifests/fuel_pkgs/dispynode.pp
@@ -0,0 +1,82 @@
+class osnailyfacter::fuel_pkgs::dispynode(
+  $master = undef,
+)
+{
+
+  notice('MODULAR: fuel_pkgs/dispynode.pp')
+
+  $user           = 'dispy'
+  $workdir        = "/var/lib/${user}"
+  $project        = 'dispynode.py'
+  $init_file      = "/etc/init.d/${project}"
+  $init_file_conf = "/etc/init/${project}.conf"
+  $init_service   = "/lib/systemd/system/${project}.service"
+  $init_serv_link = "/etc/systemd/system/multi-user.target.wants/${project}.service"
+
+  if $master {
+    $hostname       = hiera(HOSTNAME)
+    $domainname     = hiera(DNS_DOMAIN)
+    $fqdn           = "${hostname}.${domainname}"
+    $admin_iface_ip = 'localhost'
+    $su             = '/usr/bin/su'
+  }
+  else {
+    $fqdn           = hiera(fqdn)
+    $ns             = hiera_hash(network_scheme, {})
+    $admin_net      = split($ns['endpoints']['br-fw-admin']['IP'][0], '/')
+    $admin_iface_ip = $admin_net[0]
+    $su             = '/bin/su'
+  }
+
+  package {'python-dispy':
+    ensure => installed,
+  }
+
+  group {'dispy_group':
+    name   => $user
+    ensure => present,
+  }
+
+  user {'dispy_user':
+    name    => $user,
+    ensure  => present,
+    gid     => $user,
+    require => Group[$user]
+  }
+
+  file {'workdir':
+    path    => $workdir,
+    ensure  => 'directory',
+    owner   => $user,
+    group   => $user,
+    mode    => '0750',
+    require => User[$user]
+  }
+
+  file {'dispy_init':
+    path    => $init_service,
+    ensure  => present,
+    content => template('osnailyfacter/dispynode.service.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => User[$user]
+  }
+
+  file {'dispy_init_link':
+    path    => $init_serv_link
+    ensure  => 'link',
+    target  => $init_service,
+    require => File[$init_service]
+  }
+
+  service {'dispy_service':
+    name    => $project,
+    ensure  => 'running',
+    require => [
+                File[$init_serv_link],
+                File[$workdir],
+                Package['python-dispy'],
+                ]
+  }
+}
