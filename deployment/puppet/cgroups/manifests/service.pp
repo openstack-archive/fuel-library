280a29e51822e531126435ec3487b6a17e5532af
Michael Polenchuk <mpolenchuk@mirantis.com>
2017-03-09 14:06:18 +0400

    Conform cgroups to systemd based services
    Update cgroups configuration to be running on ubuntu 16.04.
    Remove "cgroup-lite" package/service since systemd
    mounts cgroups sanely itself.
    
    Change-Id: I250f1ba92fbe1ceac415a73b640fd00d6e2898e4
    Closes-Bug: #1669747
    (cherry picked from commit 4f33d4aabb859894167728ec347ee36b2c17d19d)

--- deployment/puppet/cgroups/manifests/service.pp
+++ deployment/puppet/cgroups/manifests/service.pp
@@ -1,26 +1,16 @@
 class cgroups::service (
   $cgroups_settings = {},
-)
-{
-  service { 'cgroup-lite':
-    ensure => running,
-    enable => true,
-  }
+) {
 
-  service { 'cgconfigparser':
-    ensure    => running,
-    hasstatus => false,
-    status    => '/bin/true',
-    restart   => 'service cgconfigparser restart',
-    require   => Service['cgroup-lite'],
-  }
-
-  service { 'cgrulesengd':
-    ensure => running,
+  service { 'cgconfig':
+    ensure   => running,
+    enable   => true,
+    provider => 'init',
   }
 
   $cgclass_res = map_cgclassify_opts($cgroups_settings)
   unless empty($cgclass_res) {
     create_resources('cgclassify', $cgclass_res, { 'ensure' => present })
   }
+
 }
