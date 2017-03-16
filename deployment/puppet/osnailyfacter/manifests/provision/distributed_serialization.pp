a5ca58e4d5101aa00cb589097707061d4e9c9d8d
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2017-03-16 16:40:58 +0300

    Add distributed serialization of yaql contexts on nodes
    Serialization of contexts now distributed over cluster nodes and
    processed by separate workers. Workers number on each node is
    equal to node processors count. To prevent high CPU utilization,
    priority of workers processes is set to minimum on slave nodes.
    
    Master node calculation also processed by set of workers, but priority
    of that ones set to 0 by default. Also there is serialization scheduler
    started on master node which orchestrates serialization process between
    workers.
    
    Change-Id: I71bf0e605af963444ac62b303d6786ecf72c7b00
    Implements: blueprint distributed-serialization

---
+++ deployment/puppet/osnailyfacter/manifests/provision/distributed_serialization.pp
@@ -0,0 +1,54 @@
+class osnailyfacter::provision::distributed_serialization(
+  $scheduler_host     = undef,
+  $scheduler_port     = '8002',
+  $worker_count       = undef,
+  $worker_base_port   = 35000,
+  $nice_level         = '19',
+){
+  notice('MODULAR: provision/distributed_serialization.pp')
+
+  if $scheduler_host == undef {
+    $master_ip      = hiera('master_ip')
+    $scheduler_node = "${master_ip}:${scheduler_port}"
+  } else {
+    $scheduler_node = "${scheduler_host}:${scheduler_port}"
+  }
+
+  if $worker_count == undef {
+    $max_workers = $::processorcount
+  } else {
+    $max_workers = $worker_count
+  }
+
+  $user = 'serializer'
+
+  package {'python-distributed':
+    ensure => installed,
+  } ->
+
+  if !defined(Group["$user"]) {
+    group { "$user":
+      ensure  => present,
+      require => Package["python-distributed"],
+    }
+  }
+
+  if !defined(User["$user"]) {
+    user { "$user":
+      ensure  => present,
+      gid     => $user,
+      require => Group[$user]
+    }
+  }
+
+  $workers_range = range("1", $max_workers)
+
+  osnailyfacter::provision::dsworker { $workers_range:
+    base_port      => $worker_base_port,
+    scheduler_node => $scheduler_node,
+    nice_level     => $nice_level,
+    user           => $user,
+    group          => $user,
+    require        => User[$user],
+  }
+}
---
+++ deployment/puppet/osnailyfacter/manifests/provision/dsworker.pp
@@ -0,0 +1,37 @@
+define osnailyfacter::provision::dsworker(
+  $base_port      = 35000,
+  $enabled        = true,
+  $nice_level     = '19',
+  $scheduler_node = undef,
+  $user           = 'serializer',
+  $group          = 'serializer',
+) {
+  if !is_bool($enabled) {
+    fail('"enabled" variable value must be boolean')
+  }
+
+  if !is_integer($name) {
+    fail("Distributed serialization worker name should be an integer, not ${name}")
+  }
+
+  if $scheduler_node == undef {
+    fail("Scheduler node not defined for distributed serialization worker ${name}")
+  }
+
+  $port_number  = $base_port + $name
+  $systemd_base = '/etc/systemd/system'
+  $service_name = "dsworker-${number}"
+
+  file { "${systemd_base}/${service_name}.service":
+    ensure  => present,
+    content => template('osnailyfacter/dsworker.service.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  } ->
+
+  service { $service_name:
+    ensure => $enabled,
+    enable => $enabled,
+  }
+}
