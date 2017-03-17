26d2e74b12d5dcebba6f2d7b401a824419364207
Vasyl Saienko <vsaienko@mirantis.com>
2017-03-17 21:38:31 +0200

    Fix building ironic bootstrap
    This patch fixes issues with building ironic bootstrap:
      * fix if condition that is a trigger to build ironic bootstrap
      * build fuel image always, ironic bootstrap only when ironic is enabled
    
    Change-Id: Ib7ee0c656ffa71ce913e1924bb80260744319a3a
    Closes-Bug: #1653204

--- deployment/puppet/osnailyfacter/manifests/provision/build_image.pp
+++ deployment/puppet/osnailyfacter/manifests/provision/build_image.pp
@@ -9,7 +9,9 @@ class osnailyfacter::provision::build_image(
   }
   $cluster_id = $data['cluster']['id']
 
-  if $data['ironic']['enabled'] == 'true' {
+  if $data['ironic']['enabled'] == true {
+    # TODO(vsaienko): Use the same system packages for fuel image and ironic bootstrap, but exclude
+    # not needed for ironic like nailgun-agent, etc...
     $ironic_packages = [
       'openssh-server',
       'ntp',
@@ -21,9 +23,7 @@ class osnailyfacter::provision::build_image(
       'squashfs-tools',
       'linux-firmware',
       'msmtp-mta',
-      'hpsa-dkms',
       'i40e-dkms',
-      'linux-firmware-nonfree',
       'xz-utils',
       'linux-headers-generic',
     ]
@@ -35,25 +35,26 @@ class osnailyfacter::provision::build_image(
     $out_dir = "/var/www/nailgun/bootstrap/ironic/${cluster_id}/"
     $out_params = "--output-dir ${out_dir}"
 
-    $extra_params = "--extra-dir /usr/share/ironic-fa-bootstrap-configs/ --no-compress --no-default-extra-dirs --no-default-packages"
-    $log_params = '--log-file /var/log/fuel-ironic-bootstrap-image-build.log'
+    $ironic_extra_params = "--extra-dir /usr/share/ironic-fa-bootstrap-configs/ --no-compress --no-default-extra-dirs --no-default-packages"
+    $ironic_log_params = '--log-file /var/log/fuel-ironic-bootstrap-image-build.log'
 
     exec { 'generate_image_with_ironic':
-      command => "fuel-bootstrap build ${package_list} ${ssh_params} ${out_params} ${extra_params} ${log_params}",
-      path    => ['/bin', '/usr/bin'],
-      unless  => "test -e /var/www/nailgun/bootstrap/ironic/${cluster_id}/vmlinuz",
-    }
-
-  } else {
-    $build_dir = '--image_build_dir /var/lib/fuel/ibp'
-    $log_params = "--log-file /var/log/fuel-agent-env-${cluster_id}.log"
-    $extra_params = '--data_driver nailgun_build_image'
-
-    exec { 'generate_image_with_fuel':
-      command => "fa_build_image ${build_dir} ${log_params} ${extra_params} --input_data_file ${data_file}",
+      command => "fuel-bootstrap build ${package_list} ${ssh_params} ${out_params} ${ironic_extra_params} ${ironic_log_params}",
       path    => ['/bin', '/usr/bin'],
       timeout => 1800,
       unless  => "test -e /var/www/nailgun/bootstrap/ironic/${cluster_id}/vmlinuz",
     }
+
+  }
+
+  $build_dir = '--image_build_dir /var/lib/fuel/ibp'
+  $log_params = "--log-file /var/log/fuel-agent-env-${cluster_id}.log"
+  $extra_params = '--data_driver nailgun_build_image'
+
+  exec { 'generate_image_with_fuel':
+    command => "fa_build_image ${build_dir} ${log_params} ${extra_params} --input_data_file ${data_file}",
+    path    => ['/bin', '/usr/bin'],
+    timeout => 1800,
+    unless  => "test -e /var/www/nailgun/bootstrap/ironic/${cluster_id}/vmlinuz",
   }
 }
