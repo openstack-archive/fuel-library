0be0c79bfdf9949030f1b3447bf60407dae69f88
Michael Polenchuk <mpolenchuk@mirantis.com>
2017-04-13 13:30:50 +0400

    [ceph] Support discard/fstrim feature
    Enable discard/fstrim option by default to optimize disk usage
    with ceph backend. Also boolean option of storage:disk_discard
    has been added to turn on/off this feature since it could bring
    in some performance penalty on non-ssd disks.
    
    DocImpact
    Closes-Bug: #1682100
    
    Change-Id: Ib2d582827f4f9bca2e0c7c1d3ebef7383194e6ab

--- deployment/puppet/osnailyfacter/manifests/astute/upload_cirros.pp
+++ deployment/puppet/osnailyfacter/manifests/astute/upload_cirros.pp
@@ -10,13 +10,22 @@ class osnailyfacter::astute::upload_cirros {
 
   notice('MODULAR: astute/upload_cirros.pp')
 
-  package { 'cirros-testvm' :
-      ensure => 'installed',
-      name   => 'cirros-testvm',
+  ensure_packages('cirros-testvm')
+
+  $storage_hash = hiera_hash('storage', {})
+  $hw_disk_discard = pick($storage_hash['disk_discard'], true)
+
+  if $hw_disk_discard {
+    $extra_opts = {
+      'properties' => {
+        'hw_scsi_model' => 'virtio-scsi',
+        'hw_disk_bus'   => 'scsi',
+      }
+    }
   }
 
   $test_vm_images = hiera('test_vm_image')
-  $glance_images = generate_glance_images(flatten([$test_vm_images]))
+  $glance_images = generate_glance_images(flatten([$test_vm_images]), pick($extra_opts, {}))
   $defaults = {
     'ensure' => 'present',
   }
