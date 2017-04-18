3c9906802edf923c49eeb206b5d13d6a898026ec
Anton Chevychalov <achevychalov@mirantis.com>
2017-04-18 17:54:19 +0300

    Fix style to pass rake lint
    Change-Id: I99e1f20e1a64c8156f94babefbfe63302011ef2a
    Closes-Bug: #1683814

--- deployment/puppet/rabbitmq/tests/full.pp
+++ deployment/puppet/rabbitmq/tests/full.pp
@@ -1,16 +1,16 @@
 class { 'rabbitmq::repo::apt':
   pin => '900',
-}->
-class { 'rabbitmq::server':
+}
+-> class { 'rabbitmq::server':
   delete_guest_user => true,
 #  version           => '2.4.1',
-}->
-rabbitmq_user { 'dan':
+}
+-> rabbitmq_user { 'dan':
   admin    => true,
   password => 'pass',
   provider => 'rabbitmqctl',
-}->
-rabbitmq_vhost { 'myhost':
+}
+-> rabbitmq_vhost { 'myhost':
   provider => 'rabbitmqctl',
 }
 rabbitmq_user_permissions { 'dan@myhost':
