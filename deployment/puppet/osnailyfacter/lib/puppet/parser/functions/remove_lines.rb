f15428754cdaf1e14f4b8bebfa4f54f9d6382914
Oleksiy Molchanov <omolchanov@mirantis.com>
2017-06-19 17:59:19 +0300

    Add new task for removing neutron-related iptables ipset rules
    Change-Id: Ic82b26c9ae538de1fe815dd048fbd007810a7696

---
+++ deployment/puppet/osnailyfacter/lib/puppet/parser/functions/remove_lines.rb
@@ -0,0 +1,29 @@
+require 'fileutils'
+require 'tempfile'
+
+module Puppet::Parser::Functions
+  newfunction(:remove_lines,
+:doc => <<-EOS
+Remove lines from file that match a regexp
+EOS
+  ) do |args|
+
+    raise(Puppet::ParseError, 'Function should contain path and matching string separated by comma') if args.size != 2
+
+    filename = args[0]
+    match    = args[1]
+    basename = File.basename filename
+
+    t_file = Tempfile.new("/tmp/#{basename}")
+
+    File.open(filename).each do |line|
+      if line !~ /#{match}/
+        t_file.puts line
+      end
+    end
+
+    FileUtils.mv(t_file.path, filename)
+
+  end
+end
+
