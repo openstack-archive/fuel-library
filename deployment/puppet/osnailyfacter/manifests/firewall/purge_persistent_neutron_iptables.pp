b96c6c0371fc00684bafe2446cb8fc98992f55b6
Oleksiy Molchanov <omolchanov@mirantis.com>
2017-06-19 17:59:19 +0300

    Add new task for removing neutron-related iptables ipset rules
    Change-Id: Ic82b26c9ae538de1fe815dd048fbd007810a7696

---
+++ deployment/puppet/osnailyfacter/manifests/firewall/purge_persistent_neutron_iptables.pp
@@ -0,0 +1,8 @@
+class osnailyfacter::firewall::purge_persistent_neutron_iptables {
+
+  notice('MODULAR: firewall/purge_persistent_neutron_iptables.pp')
+
+  remove_lines('/etc/iptables/rules.v4', '-A neutron-openvswi-.* -m set --match-set .* src -j RETURN')
+  remove_lines('/etc/iptables/rules.v6', '-A neutron-openvswi-.* -m set --match-set .* src -j RETURN')
+}
+
